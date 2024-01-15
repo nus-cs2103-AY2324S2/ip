5b016ec8b024d4443b0d53ce91f58e87c672f38e
Allysa Tan <allysa@u.nus.edu>
2024-01-15 13:54:51 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        intro();
+        sc.nextLine();
+        exit();
     }
+    private static void intro() {
+        String logo = "        _  _        \n  __ _ | || | _   _ \n / _` || || || | | |\n| (_| || || || |_| |\n \\__,_||_||_| \\__, |\n              |___/ \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Ally \n" + "What can I do for you?");
+    }
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
 }
