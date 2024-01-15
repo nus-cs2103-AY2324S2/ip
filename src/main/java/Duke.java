2a71916db7c13ebefb04af4ee96e3d65076da0b6
Allysa Tan <allysa@u.nus.edu>
2024-01-15 14:03:10 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,20 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         intro();
-        sc.nextLine();
+        echo(sc);
         exit();
     }
     private static void intro() {
         String logo = "        _  _        \n  __ _ | || | _   _ \n / _` || || || | | |\n| (_| || || || |_| |\n \\__,_||_||_| \\__, |\n              |___/ \n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Ally \n" + "What can I do for you?");
+    }
+    private static void echo(Scanner sc) {
+        String task = sc.nextLine();
+        while (!task.equals("bye")) {
+            System.out.println(task);
+            task = sc.nextLine();
+        }
     }
     private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
