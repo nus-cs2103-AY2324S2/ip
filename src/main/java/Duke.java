11494469d1f66a98959694e688cd61b79d096867
TresaAnnThomas <teeesa728@gmail.com>
2024-01-18 12:35:17 +0800

    Added echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,6 +10,22 @@ public class Duke {
         System.out.println("-------------------------------");
         System.out.println("Hello! I'm Tango. \nWhat can I do for you today?" );
         System.out.println("-------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String text1 = scan.nextLine();
+            int indentation = text1.length() + 1;
+            String spaces = " ".repeat(indentation);
+
+            if (text1.equals("bye")) {
+                System.out.println("-------------------------------");
+                System.out.println(spaces + "Bye. Hope to see you again soon!");
+                System.out.println("-------------------------------");
+                break;
+            }
+
+            System.out.println("-------------------------------");
+            System.out.println(spaces + text1);
+            System.out.println("-------------------------------");
+        }
     }
 }
