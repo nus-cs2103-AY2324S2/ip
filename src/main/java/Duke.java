56702a6e5b53b4b19f7a83e8606d773471a586a6
TresaAnnThomas <teeesa728@gmail.com>
2024-01-18 11:54:46 +0800

    Added simple greet and exit messages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------");
+        System.out.println("Hello! I'm Tango. \nWhat can I do for you today?" );
+        System.out.println("-------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
