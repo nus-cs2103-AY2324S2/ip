bde35d258c2f0cb11cdcb840cd7c26bdac8e3023
wind <cbzxiaoyao@outlook.com>
2024-01-19 21:52:27 +0800

    Finished

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,13 +14,22 @@ public class Duke {
         System.out.println("Hello I'm " + botName + "\n"
                 + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        List<String> items = new ArrayList<>(100);
         while(true) {
             String input = scanner.nextLine();
             if(input.equals("bye")) {
                System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            if(input.equals("list")) {
+                for(int i = 0; i < items.size(); i++) {
+                    int number = i + 1;
+                    System.out.println(number + ". " + items.get(i));
+                }
+                continue;
+            }
+            items.add(input);
+            System.out.println("added: " + input);
 
         }
     }
