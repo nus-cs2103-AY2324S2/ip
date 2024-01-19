36cc380517d018a197c70952c4fdcc84a95d00b0
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-19 20:46:20 +0800

    Implement Level-0

---
+++ src/main/java/ConvoBot.java
@@ -0,0 +1,13 @@
+public class ConvoBot {
+    public static void main(String[] args) {
+        String welcomeMsg = "____________________________________________________________\n"
+        + " Hello! I'm ConvoBot\n"
+        + " What can I do for you?";
+        System.out.println(welcomeMsg);
+
+        String exitMsg = "____________________________________________________________\n"
+        + " Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________";
+        System.out.println(exitMsg);
+    }
+}
