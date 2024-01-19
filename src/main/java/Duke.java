14afb651afee2fcd785120fe293e93169f36bc1c
Zer0Legion <lengkhai@gmail.com>
2024-01-19 15:33:11 +0800

    implement add/list with ArrayList<String>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,63 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Duke {
-    private static String LINE_BREAK = "---------------------------------------------\n";
-    public static void hello() {
-        String message = "Squid: HMm human. What do you want again?\n\n";
+    private static String LINE_BREAK = "\n---------------------------------------------\n";
+
+
+    private static ArrayList<String> tasks;
+
+    private static void Squid() {
+        tasks = new ArrayList<>();
+    }
+
+    private static void add(String task) {
+        tasks.add(task);
+        echo(String.format("Added %s. When are you going to add 'feed Squid'?", task));
+    }
+
+    private static void list() {
+        echo("Here are your tasks. Sucks to be you, my only 2 tasks are eating and sleeping.");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("[ ] " + tasks.get(i));
+        }
+    }
+
+    private static void hello() {
+        String message = "Squid: HMm human. What do you want again?";
         System.out.println(LINE_BREAK + message + LINE_BREAK);
     }
 
-    public static void bye() {
-        String message = "\nYou're done. Time for my food.\n";
-        System.out.println(LINE_BREAK + message);
+    private static void bye() {
+        String message = "You're done. Time for my food.";
+        System.out.println(message);
     }
 
-    public static void echo(String message) {
+    private static void echo(String message) {
         System.out.println("Squid: " + message);
     }
 
     public static void main(String[] args) {
+        Squid();
         hello();
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
+            System.out.println(LINE_BREAK);
             if (Objects.equals(input, "bye")) {
                 bye();
+                System.out.println(LINE_BREAK);
                 break;
+            } else if (Objects.equals(input, "list")) {
+                list();
+            } else {
+                add(input);
             }
-            echo(input);
+            System.out.println(LINE_BREAK);
 
         }
 
