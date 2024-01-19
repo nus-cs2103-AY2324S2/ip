8bff9cfd536f0b09142360329d38d83a49f83d76
dillontkh <user@maquebuk-eh.local>
2024-01-19 21:19:14 +0800

    Added welcome and goodbye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
 public class Duke {
+    /**
+     * Length of horizontal line in characters.
+     * Printed for visual separation.
+     */
+    private static int lineLength = 30;
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        sendWelcomeMessage();
+        sendGoodbyeMessage();
+    }
+
+    /**
+     * Sends a welcome message upon starting the bot, with horizontal lines
+     * printed for visual separation.
+     */
+    public static void sendWelcomeMessage() {
+        printHorizontalLine(lineLength);
+        System.out.println("Hello! I'm Nollid.\n"
+                + "What can I do for you?");
+        printHorizontalLine(lineLength);
+    }
+
+    /**
+     * Sends a goodbye message upon exiting the bot, with horizontal lines
+     * printed for visual separation.
+     */
+    public static void sendGoodbyeMessage() {
+
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine(lineLength);
+    }
+
+    /**
+     * Prints a horizontal line with unicode character U+2500.
+     * @param length Length of line in characters.
+     */
+    public static void printHorizontalLine(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print("─");
+        }
+        System.out.println();
     }
 }
