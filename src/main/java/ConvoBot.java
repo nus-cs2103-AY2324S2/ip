a0151dc301c637d96a83f77945b72605f9584d67
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-19 21:12:30 +0800

    Refactor welcome and exit strings into functions

--- src/main/java/ConvoBot.java
+++ src/main/java/ConvoBot.java
@@ -1,13 +1,18 @@
 public class ConvoBot {
-    private static final String welcomeMsg = "____________________________________________________________\n"
-        + " Hello! I'm ConvoBot\n"
-        + " What can I do for you?";
-    private static final String exitMsg = "____________________________________________________________\n"
-        + " Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________";
-
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+    private static void printWelcomeMsg() {
+        System.out.println(" Hello! I'm ConvoBot\n What can I do for you?");
+    }
+    private static void printExitMsg() {
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        System.out.println(welcomeMsg);
-        System.out.println(exitMsg);
+        printHorizontalLine();
+        printWelcomeMsg();
+        printHorizontalLine();
+        printExitMsg();
+        printHorizontalLine();
     }
 }
