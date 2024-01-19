fce44c69f502af415abbc3b54ed7d0af91257cb3
ForAeons <w.xy020203@gmail.com>
2024-01-19 18:41:12 +0800

    feat: implement level 2

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+  private final String description;
+
+  public Task(String description) {
+    this.description = description;
+  }
+
+  public String getDetails() {
+    return description;
+  }
+}
