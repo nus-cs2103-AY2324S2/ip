bc6032b1f956c86057c5ac5ed1b79ebaaf4998c0
TresaAnnThomas <teeesa728@gmail.com>
2024-01-19 23:11:16 +0800

    created a task class, removed indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,41 +1,84 @@
 import java.util.*;
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String markStatus(int index) {
+        isDone = true;
+        return "------------------------------- \n" +
+                "Nice! I've marked this as done: \n " + "[" + getStatusIcon() + "] " + description +
+                "\n-------------------------------";
+    }
+
+    public String unmarkStatus(int index) {
+        isDone = false;
+        return "------------------------------- \n" +
+                "OK, I've marked this task as not done yet: \n " + "[" + getStatusIcon() + "] " + description +
+                "\n-------------------------------";
+    }
+}
+
 public class Duke {
+
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("-------------------------------");
-        System.out.println("Hello! I'm Tango. \nWhat can I do for you today?" );
+        System.out.println("Hello! I'm Tango. \nWhat can I do for you today?");
         System.out.println("-------------------------------");
 
         while (true) {
-            String text1 = scan.nextLine();
-            int indentation = text1.length() + 1;
-            String spaces = " ".repeat(indentation);
+            String input = scan.nextLine().trim();
+            String[] words = input.split("\\s+");
 
-            if (text1.equals("bye")) {
+            if (words[0].equalsIgnoreCase("bye")) {
                 System.out.println("-------------------------------");
-                System.out.println(spaces + "Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-------------------------------");
                 break;
-            }
-
-            else if (text1.equals("list")) {
+            } else if (words[0].equalsIgnoreCase("list")) {
                 System.out.println("-------------------------------");
-                for (int i = 0; i < arr.size(); i++) {
-                    System.out.println(spaces + (i + 1) + ". " + arr.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
+                    System.out.println((i + 1) + ".[" + (task != null ? task.getStatusIcon() : "") + "] " + task.description);
                 }
                 System.out.println("-------------------------------");
-            }
-
-            else {
+            } else if (words[0].equalsIgnoreCase("mark") && words.length > 1) {
+                int index = Integer.parseInt(words[1]) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    Task task = tasks.get(index);
+                    System.out.println(task.markStatus(index));
+                } else {
+                    System.out.println("-------------------------------");
+                    System.out.println("You have not created a task " + index + " yet");
+                    System.out.println("-------------------------------");
+                }
+            } else if (words[0].equalsIgnoreCase("unmark") && words.length > 1) {
+                int index = Integer.parseInt(words[1]) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    Task task = tasks.get(index);
+                    System.out.println(task.unmarkStatus(index));
+                } else {
+                    System.out.println("-------------------------------");
+                    System.out.println("You have not created a task " + index + " yet");
+                    System.out.println("-------------------------------");
+                }
+            } else {
                 System.out.println("-------------------------------");
-                System.out.println(spaces + "added: " + text1);
-                arr.add(text1);
+                System.out.println("added: " + input);
+                tasks.add(new Task(input));
                 System.out.println("-------------------------------");
             }
         }
