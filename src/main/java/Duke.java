95fc4257204e799ae65de4b5425770c8d7ecd160
radeon2525 <e1070572@u.nus.edu>
2024-01-19 21:26:07 +0800

    This is Level-1 of Lucifer

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -9,6 +10,15 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Lucifer\n" + "What can I do for you?");
         System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        String user_word = sc.nextLine();
+        while (!user_word.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(user_word);
+            System.out.println("____________________________________________________________");
+            user_word = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
