5b5cbd2b597479346c879146d5f96f8bc00409fa
tankh99 <khanghou99@gmail.com>
2024-01-19 19:54:39 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I'm Baron. What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("I don't know how to respond. Bye");
     }
 }
