c4d40bf5a328a20d640abb6db32beba6987beec1
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-19 21:51:41 +0800

    Implement Level-2

--- src/main/java/ConvoBot.java
+++ src/main/java/ConvoBot.java
@@ -1,27 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConvoBot {
+    private static final String leftPadding = "    ";
     private static void printHorizontalLine(boolean newline) {
-        System.out.println("    ____________________________________________________________");
+        System.out.println(leftPadding + "____________________________________________________________");
         if (newline) System.out.println();
     }
     private static void printWelcomeMsg() {
-        System.out.println("     Hello! I'm ConvoBot");
-        System.out.println("     What can I do for you?");
+        System.out.println(leftPadding + " Hello! I'm ConvoBot");
+        System.out.println(leftPadding + " What can I do for you?");
     }
     private static void printExitMsg() {
-        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(leftPadding + " Bye. Hope to see you again soon!");
     }
     public static void main(String[] args) {
         printHorizontalLine(false);
         printWelcomeMsg();
         printHorizontalLine(true);
 
+        ArrayList<String> list = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
+
         while (!userInput.equals("bye")) {
             printHorizontalLine(false);
-            System.out.println("     " + userInput);
+            if (userInput.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i+1;
+                    System.out.println(leftPadding + " " + Integer.toString(index) + ". " + list.get(i));
+                }
+            } else {
+                list.add(userInput);
+                System.out.println("     added: " + userInput);
+            }
             printHorizontalLine(true);
             userInput = scanner.nextLine();
         }
