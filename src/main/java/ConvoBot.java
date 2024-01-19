ae7d644e144f44db2ab0f94f9b58e6378a873133
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-19 21:21:10 +0800

    Implement Level-1

--- src/main/java/ConvoBot.java
+++ src/main/java/ConvoBot.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class ConvoBot {
-    private static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
+    private static void printHorizontalLine(boolean newline) {
+        System.out.println("    ____________________________________________________________");
+        if (newline) System.out.println();
     }
     private static void printWelcomeMsg() {
-        System.out.println(" Hello! I'm ConvoBot\n What can I do for you?");
+        System.out.println("     Hello! I'm ConvoBot");
+        System.out.println("     What can I do for you?");
     }
     private static void printExitMsg() {
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("     Bye. Hope to see you again soon!");
     }
     public static void main(String[] args) {
-        printHorizontalLine();
+        printHorizontalLine(false);
         printWelcomeMsg();
-        printHorizontalLine();
+        printHorizontalLine(true);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            printHorizontalLine(false);
+            System.out.println("     " + userInput);
+            printHorizontalLine(true);
+            userInput = scanner.nextLine();
+        }
+
+        printHorizontalLine(false);        
         printExitMsg();
-        printHorizontalLine();
+        printHorizontalLine(true);
     }
 }
