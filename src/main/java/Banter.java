0166b503cd03225e405d471f308f13a5ae569bb6
solomonng2001 <solomonng2001@gmail.com>
2024-01-20 01:09:54 +0800

    rename-chatbot-and-add-greet-and-exit-responses

---
+++ src/main/java/Banter.java
@@ -0,0 +1,8 @@
+import constants.Responses;
+
+public class Banter {
+    public static void main(String[] args) {
+        System.out.println(Responses.BANTER_LOGO);
+        System.out.println(Responses.GREET_AND_EXIT);
+    }
+}
