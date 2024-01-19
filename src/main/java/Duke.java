7bd40bb7a7907a1875b43b46076cb9e817711ae8
radeon2525 <e1070572@u.nus.edu>
2024-01-19 22:01:29 +0800

    This is level-2 which includes list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -12,12 +15,26 @@ public class Duke {
         System.out.println("____________________________________________________________");
         Scanner sc = new Scanner(System.in);
         String user_word = sc.nextLine();
+        List l = new ArrayList();
         while (!user_word.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(user_word);
+            if (user_word.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int x = 0; x < l.size(); x++) {
+                    System.out.println(x + 1 + ". " + l.get(x));
+                }
+            }
+            if(!user_word.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + user_word);
+            }
+            l.add(user_word);
+            //System.out.println(user_word);
             System.out.println("____________________________________________________________");
             user_word = sc.nextLine();
+
+
         }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
