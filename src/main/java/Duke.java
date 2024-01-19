9dd93726ca1536c83c49190f5d3be476708c7816
Zer0Legion <lengkhai@gmail.com>
2024-01-19 15:15:06 +0800

    Implement echo; update welcome and bye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,37 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+
 public class Duke {
+    private static String LINE_BREAK = "---------------------------------------------\n";
     public static void hello() {
-        String lineBreak = "---------------------------------------------\n";
-        String message = "Hello! I am Squid. How can I help you today?\n\n";
-        System.out.println(lineBreak + message + lineBreak);
+        String message = "Squid: HMm human. What do you want again?\n\n";
+        System.out.println(LINE_BREAK + message + LINE_BREAK);
     }
 
     public static void bye() {
-        String message = "\nBye! I am going to look for pets and food.\n";
+        String message = "\nYou're done. Time for my food.\n";
+        System.out.println(LINE_BREAK + message);
+    }
+
+    public static void echo(String message) {
+        System.out.println("Squid: " + message);
     }
 
     public static void main(String[] args) {
         hello();
-        bye();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "bye")) {
+                bye();
+                break;
+            }
+            echo(input);
+
+        }
+
     }
 }
