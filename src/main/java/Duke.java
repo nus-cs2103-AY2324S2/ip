6122fcfddd27da769471af48e5dfb382203ad79e
Joseph Chan <cyqjoseph@gmail.com>
2024-01-19 14:48:13 +0800

    Done Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        printIntroMessage();
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        do {
+            userInput = scanner.nextLine();
+            printHorizontalLine();
+            switch (userInput) {
+                case "bye":
+                    break;
+                default:
+                    System.out.println(userInput);
+                    printHorizontalLine();
+                    break;
+            }
+        } while (!userInput.equals("bye"));
+        printExitMessage();
+
+    }
+
+    public static void printIntroMessage() {
         printHorizontalLine();
-        System.out.println("Hello! I'm SoyBot!");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm SoyBot!");
+        System.out.println("\tWhat can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printExitMessage() {
+        System.out.println("\tBye. Hope to see you again soon!");
         printHorizontalLine();
     }
 
     public static void printHorizontalLine() {
-        System.out.println("_________________________________________________");
+        System.out.println("\t_________________________________________________");
     }
+
+
 }
