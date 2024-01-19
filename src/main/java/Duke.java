69992862b3bb5bb5026382997f66220e8ff1e0cc
jerremy <jerrremyng@gmail.com>
2024-01-19 23:43:25 +0800

    Echos command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm fakegpt\nWhat can I do for you?\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        while (!userInput.toLowerCase().equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
