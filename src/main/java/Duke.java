a95264ae9560560fdae8ae4cb8dcd73647893ebc
David <david@dzhy.dev>
2024-01-20 23:55:55 +0800

    command prefix -> command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,11 @@ public class Duke {
     private static final String INDENT = "    ";
     private static final String LIST_COMMAND = "list";
     private static final String EXIT_COMMAND = "bye";
-    private static final String MARK_PREFIX = "mark ";
-    private static final String UNMARK_PREFIX = "unmark ";
-    private static final String TODO_PREFIX = "todo ";
-    private static final String DEADLINE_PREFIX = "deadline ";
-    private static final String EVENT_PREFIX = "event ";
+    private static final String MARK_COMMAND = "mark";
+    private static final String UNMARK_COMMAND = "unmark";
+    private static final String TODO_COMMAND = "todo";
+    private static final String DEADLINE_COMMAND = "deadline";
+    private static final String EVENT_COMMAND = "event";
 
     public static void main(String[] args) {
         printBanner();
@@ -33,7 +33,7 @@ public class Duke {
                 messages[0] = "Here are the tasks in your list:";
                 System.arraycopy(TASKS.toString().split("\n"), 0, messages, 1, TASKS.size());
                 print(messages);
-            } else if (input.startsWith(MARK_PREFIX)) {
+            } else if (input.startsWith(MARK_COMMAND)) {
                 String indexStr = input.substring(5);
                 int index = Integer.parseInt(indexStr);
                 TASKS.getTask(index).done();
@@ -42,7 +42,7 @@ public class Duke {
                     TASKS.getTask(index).toString()
                 };
                 print(messages);
-            } else if (input.startsWith(UNMARK_PREFIX)) {
+            } else if (input.startsWith(UNMARK_COMMAND)) {
                 String indexStr = input.substring(7);
                 int index = Integer.parseInt(indexStr);
                 TASKS.getTask(index).undone();
@@ -51,14 +51,14 @@ public class Duke {
                     TASKS.getTask(index).toString()
                 };
                 print(messages);
-            } else if (input.startsWith(TODO_PREFIX)) {
+            } else if (input.startsWith(TODO_COMMAND)) {
                 Task task = new Todo(input);
                 TASKS.addTask(task);
                 printAddedTask(task);
-            } else if (input.startsWith(DEADLINE_PREFIX)) {
+            } else if (input.startsWith(DEADLINE_COMMAND)) {
                 Matcher matcher = DUE_PATTERN.matcher(input);
                 if (matcher.find()) {
-                    String description = input.substring(DEADLINE_PREFIX.length(), matcher.start() - 1);
+                    String description = input.substring(DEADLINE_COMMAND.length() + 1, matcher.start() - 1);
                     String due = matcher.group(1);
                     Task task = new Deadline(description, due);
                     TASKS.addTask(task);
@@ -66,10 +66,10 @@ public class Duke {
                 } else {
                     print("Please specify the due date of the deadline task using /by [DateTime].");
                 }
-            } else if (input.startsWith(EVENT_PREFIX)) {
+            } else if (input.startsWith(EVENT_COMMAND)) {
                 Matcher matcher = EVENT_PATTERN.matcher(input);
                 if (matcher.find()) {
-                    String description = input.substring(EVENT_PREFIX.length(), matcher.start() - 1);
+                    String description = input.substring(EVENT_COMMAND.length() + 1, matcher.start() - 1);
                     String from = matcher.group(1);
                     String to = matcher.group(2);
                     Task task = new Event(description, from, to);
