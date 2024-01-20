296bf48387ea18686a0284b8b317cd416cf99779
David <david@dzhy.dev>
2024-01-20 17:06:45 +0800

    Renamed chatbot and finish initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(" ██████╗ █████╗ ██████╗ ██████╗ ██╗   ██╗");
+        System.out.println("██╔════╝██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝");
+        System.out.println("██║     ███████║██████╔╝██████╔╝ ╚████╔╝ ");
+        System.out.println("██║     ██╔══██║██╔═══╝ ██╔═══╝   ╚██╔╝  ");
+        System.out.println("╚██████╗██║  ██║██║     ██║        ██║   ");
+        System.out.println(" ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝        ╚═╝   ");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Cappy");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
