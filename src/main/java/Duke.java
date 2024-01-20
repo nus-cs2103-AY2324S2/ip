171f2d075dee6b0b7d59be940a40712e1db9363e
JOHN\Flare <derpicon12@gmail.com>
2024-01-20 16:00:45 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
 
         String input;
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        List<String> textList = new ArrayList<String>();
+        List<Task> taskList = new ArrayList<Task>();
 
         greet();
 
@@ -25,10 +25,15 @@ public class Duke {
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
-                list(textList);
+                list(taskList);
+            } else if (input.split(" ")[0].equals("mark")) {
+                mark(taskList, input.split(" ")[1]);
+            } else if (input.split(" ")[0].equals("unmark")) {
+                unmark(taskList, input.split(" ")[1]);
             } else {
                 echo(input);
-                textList.add(input);
+                Task add = new Task(input);
+                taskList.add(add);
             }
         }
 
@@ -60,14 +65,39 @@ public class Duke {
         System.out.println(output);
     }
 
-    public static void list(List<String> list) {
+    public static void list(List<Task> list) {
         String text;
         System.out.println("\t____________________________________________________________");
         for (int i = 0; i < list.size(); i++) {
-            text = "\t" + (i+1) + ". " + list.get(i);
+            text = "\t" + (i+1) + "." + list.get(i).toString();
             System.out.println(text);
         }
         System.out.println("\t____________________________________________________________\n");
+    }
+
+    public static void mark(List<Task> list, String num) {
+        Task curr = list.get(Integer.parseInt(num) - 1); // TODO error checking
+        curr.mark();
+
+        String text = "\t____________________________________________________________\n"
+                + "\tNice! I've marked this task as done:\n"
+                + "\t" + curr.toString() + "\n"
+                + "\t____________________________________________________________\n";
+
+        System.out.println(text);
+
+    }
+
+    public static void unmark(List<Task> list, String num) {
+        Task curr = list.get(Integer.parseInt(num) - 1); // TODO error checking
+        curr.unmark();
+
+        String text = "\t____________________________________________________________\n"
+                + "\tOK, I've marked this task as not done yet:\n"
+                + "\t" + curr.toString() + "\n"
+                + "\t____________________________________________________________\n";
+
+        System.out.println(text);
     }
 }
 
