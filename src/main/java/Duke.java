d7e86539df03e377a0d464fd5f02632aecfc7246
tengcharmaine <e0969596@u.nus.edu>
2024-01-20 22:06:26 +0800

    changed chatbot name + implemented skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "BotChat";
+        System.out.println("____________________________________________________________\n" +
+                           " Hello! I'm " + name + "\n What can I do for you?\n" +
+                           "____________________________________________________________\n" +
+                           " Bye. Hope to see you again soon!\n");
     }
 }
