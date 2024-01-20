cf049dca8cdedde214fff41e86cab60c0f563951
NusMinato <E0985934@u.nus.edu>
2024-01-20 22:05:38 +0800

    no message

--- src/main/java/Detective.java
+++ src/main/java/Detective.java
@@ -1,8 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Detective {
     public static void main(String[] args) {
         String name = "Detective";
         String line = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
         System.out.println(line + "\nHello! I'm [" + name + "]" + "\nWhat can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        String input = sc.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println(line + "\n" + input + "\n" + line);
+            input = sc.nextLine();
+        }
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
 }
