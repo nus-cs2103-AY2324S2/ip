7ea9a42b8613d90a6d2d65d045acaddf8ddcf3c5
dom <songjunjiedominic@gmail.com>
2024-01-20 12:13:03 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,25 +3,47 @@ import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String message = null;
         Scanner scanner = new Scanner(System.in);
         System.out.println("------------------------------------------");
         System.out.println("Hello! I'm Bob!");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------------------");
-        while (!Objects.equals(message, "bye")) {
-            message = scanner.nextLine();
-            if (!Objects.equals(message, "bye") && !Objects.equals(message, "list")) {
+        while (true) {
+           message = scanner.nextLine();
+            if (message.equals("bye")) {
+                break;
+            } else if (message.startsWith("add ")) {
+                String description = message.substring(4);
+                Task task = new Task(description);
+                list.add(task);
                 System.out.println("------------------------------------------");
-                list.add(message);
-                System.out.println("added: " + message);
+                System.out.println("added: " + task.getDescription());
                 System.out.println("------------------------------------------");
             } else if (Objects.equals(message, "list")) {
                 System.out.println("------------------------------------------");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i + 1 + ". " + list.get(i));
+                    Task task = list.get(i);
+                    System.out.println((i + 1) + ". " + (task.toString()));
                 }
+                System.out.println("------------------------------------------");
+            } else if (message.startsWith("mark ")) {
+                int index = Integer.parseInt(message.substring(5)) - 1;
+                Task task = list.get(index);
+                task.markAsDone();
+                System.out.println("------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.getDescription());
+                System.out.println("------------------------------------------");
+            } else if (message.startsWith("unmark ")) {
+                int index = Integer.parseInt(message.substring(7)) - 1;
+                Task task = list.get(index);
+                task.unMarkAsDone();
+                System.out.println("------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task.getDescription());
                 System.out.println("------------------------------------------");
             }
         }
