227f3fb716139cafac2c7ef7a9e4b9a9364584f7
SimHanKiong <simhankiong@gmail.com>
2024-01-20 21:48:25 +0800

    Reached Level 0

---
+++ src/main/java/Johnny.java
@@ -0,0 +1,7 @@
+public class Johnny {
+
+    public static void main(String[] args) {
+        System.out.println("Johnny here. What do you want?");
+        System.out.println("Bye. I'm going back to sleep.");
+    }
+}
