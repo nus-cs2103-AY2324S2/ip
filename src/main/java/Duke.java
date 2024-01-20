54c9ed69c20e7ca3e3b4d15b85a78e9c6328c968
JOHN\Flare <derpicon12@gmail.com>
2024-01-20 15:34:00 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,28 +9,49 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        String input;
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 
         greet();
 
+        // Active
+        while (true){
+            // Take user input
+            input = scanner.nextLine();  // Read user input
+
+            if (input.equals("bye")) {
+                break;
+            } else {
+                echo(input);
+            }
+        }
 
         exit();
     }
 
     public static void greet() {
-        String text = "____________________________________________________________\n"
-                + "Hello! I'm Teemo!\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n";
+        String text = "\t____________________________________________________________\n"
+                + "\tHello! I'm Teemo!\n"
+                + "\tWhat can I do for you?\n"
+                + "\t____________________________________________________________\n";
 
         System.out.println(text);
     }
 
     public static void exit() {
-        String text = "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!"
-                + "____________________________________________________________\n";
+        String text = "\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n";
 
         System.out.println(text);
+    }
+
+    public static void echo(String text) {
+        String output = "\t____________________________________________________________\n"
+                + "\t" + text + "\n"
+                + "\t____________________________________________________________\n";
+
+        System.out.println(output);
     }
 }
 
