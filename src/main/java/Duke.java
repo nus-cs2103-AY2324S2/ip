3a157f370e1a01e4ec2e7ea458bf47f908ea405d
kaiyi27 <e1070556@u.nus.edu>
2024-01-20 11:52:19 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Artemis");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Artemis");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while (true) {
+            System.out.println();
+            String str = sc.nextLine();
+            if (!str.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     " + str);
+                System.out.println("    ____________________________________________________________");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        sc.close();
     }
 }
