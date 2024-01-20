bc438f7916688fc61e097a25d9436a3e5bf0d32b
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-01-21 06:48:24 +0800

    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,9 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] items = new String[100];
+        int itemCount = 0;
+
         System.out.println("Hello! I'm GuanGuanBot");
         System.out.println("What can I do for you?");
 
@@ -11,8 +14,14 @@ public class Duke {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                for (int i=0; i<itemCount; i++) {
+                    System.out.printf("%s. %s%n", i+1, items[i]);
+                }
             } else {
-                System.out.println(command);
+                items[itemCount] = command;
+                itemCount++;
+                System.out.println("added: " + command);
             }
         }
 
