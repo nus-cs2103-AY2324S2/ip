b2989fabb5530e9a678cce48f0e356b13e364dcc
kaiyi27 <e1070556@u.nus.edu>
2024-01-20 13:48:20 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String[] store = new String[100];
         Boolean[] mark = new Boolean[100];
+        String[] status = new String[100];
         Arrays.fill(mark, false);
         int count = 0;
 
@@ -22,8 +23,8 @@ public class Duke {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 1; i <= count; ++i) {
-                    String checkedbox = mark[i - 1] ? "[X] " : "[ ] ";
-                    System.out.println("     " + i + "." + checkedbox + store[i - 1]);
+                    String checkbox = mark[i - 1] ? "[X] " : "[ ] ";
+                    System.out.println("     " + i + "." + status[i - 1] + checkbox + store[i - 1]);
                 }
                 System.out.println("    ____________________________________________________________");
             } else if (input.contains("mark")) {
@@ -34,15 +35,42 @@ public class Duke {
                     mark[markIndex] = true;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("     Nice! I've marked this task as done:");
-                    System.out.println("       [X] " + store[markIndex]);
+                    System.out.println("       " + status[markIndex] + "[X] " + store[markIndex]);
                     System.out.println("    ____________________________________________________________");
                 } else if (token[0].equals("unmark")) {
                     mark[markIndex] = false;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("     OK, I've marked this task as not done yet:");
-                    System.out.println("       [ ] " + store[markIndex]);
+                    System.out.println("       " + status[count] + "[ ] " + store[markIndex]);
                     System.out.println("    ____________________________________________________________");
                 }
+            } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
+                String time = "";
+                String[] token = input.split("/");
+
+                if (input.contains("todo")) {
+                    token[0] = input.replace("todo ", "");
+                    status[count] = "[T]";
+                } else if (input.contains("deadline")) {
+                    input = input.replace("deadline ", "");
+                    status[count] = "[D]";
+                    time = "(" + token[1].replace("by", "by:") + ")";
+                } else if (input.contains("event")) {
+                    input = input.replace("event ", "");
+                    status[count] = "[E]";
+                    time = "(" + token[1].replace("from", "from:") + token[2].replace("to", "to:") + ")";
+                }
+
+                token[0] = token[0] + time;
+                store[count] = token[0];
+
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + status[count] + "[ ] " + token[0]);
+                System.out.println("     Now you have " + (count + 1) + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+
+                count++;
             } else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + input);
