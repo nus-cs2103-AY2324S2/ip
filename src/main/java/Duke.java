207a08442e24709ecdc83bf1934538816cda2fb0
Zhi Heng <tanzhiheng26@gmail.com>
2024-01-20 18:09:54 +0800

    Echo user input until user enters "bye", add indentation to chatbot messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Duke.horizontalLine();
-        System.out.println("Hello! I'm Ezra.\nWhat can I do for you?");
+        System.out.println("\tHello! I'm Ezra.\n\tWhat can I do for you?");
         Duke.horizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        Duke.horizontalLine();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do {
+            input = scanner.nextLine();
+            echo(input);
+        } while(!input.equals("bye"));
     }
 
     public static void horizontalLine() {
+        System.out.print('\t');
         for (int i = 0; i < 60; i++) {
             System.out.print("─");
         }
         System.out.println();
+    }
+
+    public static void echo(String input) {
+        horizontalLine();
+        if (input.equals("bye")) {
+            System.out.println("\tBye. Hope to see you again soon!");
+        } else {
+            System.out.println("\t" + input);
+        }
+        horizontalLine();
     }
 }
