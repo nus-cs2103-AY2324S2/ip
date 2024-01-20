20bd62f65765f49b1876b2ffd200550495c50a06
wallacexuhanxiao <wallacexuhanxiao@gmail.com>
2024-01-20 17:48:39 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Hanxiao. \nWhat can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    Hello! I'm Hanxiao. \n  What can I do for you?");
+        String inputs = sc.nextLine();
+        while (!inputs.equals("bye")) {
+            System.out.println("    "+inputs);
+            inputs = sc.nextLine();
+        }
+
+        System.out.println("    Bye. Hope to see you again soon!");
     }
 }
