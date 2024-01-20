9d7b0896ac362f09769bf660d40b0ca8e900c408
yashma-sonara <yashma.sonara04@gmail.com>
2024-01-20 23:32:09 +0800

    Completed Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "Chucklbot";
         System.out.println("Hello I'm " + logo + "\nWhat can I do for you? \n \nBye! Hope to see you again soon.");
+        Scanner sc = new Scanner(System.in);
+        String byeMessage = "Bye! Hope to see you again soon.";
+        while (true) {
+            String curr = sc.next();
+            if (curr.equals("Bye") || curr.equals("bye")) {
+                System.out.println();
+                break;
+            }
+            System.out.println(curr);
+        }
+
+
     }
 }
