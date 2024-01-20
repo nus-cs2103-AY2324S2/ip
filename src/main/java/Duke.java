9e925273c35dbb56c2a97e5686cc1b71a13982a2
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-01-21 06:44:19 +0800

    echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm GuanGuanBot");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm GuanGuanBot");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
