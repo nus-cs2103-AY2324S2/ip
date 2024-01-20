f35e80e3192033e0c5a4c22724e1551539df97f9
SimHanKiong <simhankiong@gmail.com>
2024-01-20 22:10:49 +0800

    Reached Level 1

--- src/main/java/Johnny.java
+++ src/main/java/Johnny.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Johnny {
 
     public static void main(String[] args) {
-        System.out.println("Johnny here. What do you want?");
+        System.out.println("Johnny here. What do you want?\n");
+        Johnny.takeCommands();
         System.out.println("Bye. I'm going back to sleep.");
     }
+
+    public static void takeCommands() {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String command = scanner.nextLine();
+
+            if(command.equals("bye")) {
+                break;
+            }
+
+            System.out.println(command + "\n");
+        }
+
+        scanner.close();
+    }
+
 }
