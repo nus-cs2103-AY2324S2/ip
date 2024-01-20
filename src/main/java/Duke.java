4ecf7c2b148e1d42cafe3e314e83efb36f3d6009
JOHN\Flare <derpicon12@gmail.com>
2024-01-20 15:15:06 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        greet();
+
+
+        exit();
+    }
+
+    public static void greet() {
+        String text = "____________________________________________________________\n"
+                + "Hello! I'm Teemo!\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        System.out.println(text);
+    }
+
+    public static void exit() {
+        String text = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!"
+                + "____________________________________________________________\n";
+
+        System.out.println(text);
     }
 }
+
