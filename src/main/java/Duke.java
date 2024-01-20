e2083395382e29f4c009b9eb00eccfef259b3600
NeoHW <neohaowei@gmail.com>
2024-01-21 02:06:12 +0800

    Level 1. Echo Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
     // class variable
     static String CHATBOT_NAME = "ByteBuddy";
     static String solidLineBreak = "____________________________________________________________";
+    static String START_MESSAGE = "Hello! I'm " + CHATBOT_NAME + "\n" + "\tWhat can I do for you?";
+    static String BYE_MESSAGE = "Sad to see you leave :(";
+
     public static void main(String[] args) {
-        System.out.println(solidLineBreak);
-        System.out.println("Hello! I'm " + CHATBOT_NAME);
-        System.out.println("What can I do for you?");
-        System.out.println(solidLineBreak);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(solidLineBreak);
+        Scanner sc = new Scanner(System.in);
+
+        // start
+        printWithSolidLineBreak(START_MESSAGE);
+
+        // repeating user commands
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                printWithSolidLineBreak(command);
+            }
+        }
+
+        // bye
+        printWithSolidLineBreak(BYE_MESSAGE);
+    }
+
+    public static void printWithSolidLineBreak(String s) {
+        System.out.println("\t" + solidLineBreak);
+        System.out.println("\t" + s);
+        System.out.println("\t" + solidLineBreak);
     }
 }
