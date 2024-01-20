64a7f1d1358501c04f45fecd0e2d6f81e7541f9e
Miffi <miffi@decadent.rest>
2024-01-21 01:00:55 +0800

    Add greeting and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
+    private static String name = "The Cat that Lives in your Walls";
+
+    private static void line() {
+        for (int i = 0; i < 72; i++) {
+            System.out.print('_');
+        }
+        System.out.print('\n');
+    }
+
+    private static void hello() {
+        line();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        line();
+    }
+
+    private static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        line();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        hello();
+        bye();
     }
 }
