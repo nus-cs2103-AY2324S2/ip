3c042299431fb3bc392963bdb0af7226dbbbd063
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-01-21 06:52:36 +0800

    mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,24 +4,41 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int itemCount = 0;
 
         System.out.println("Hello! I'm GuanGuanBot");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
 
         while (true) {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 break;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i=0; i<itemCount; i++) {
                     System.out.printf("%s. %s%n", i+1, items[i]);
                 }
+            } else if (command.startsWith("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                items[index].markDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(items[index]);
+
+            } else if (command.startsWith("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                items[index].unmarkDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(items[index]);
             } else {
-                items[itemCount] = command;
+                Task todo = new Task(command);
+                items[itemCount] = todo;
                 itemCount++;
-                System.out.println("added: " + command);
+
+                System.out.println(todo);
             }
         }
 
