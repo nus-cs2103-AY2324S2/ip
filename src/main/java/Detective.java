cd4f6dace1a8ae0a2679e0b654b0255af23e1f46
NusMinato <E0985934@u.nus.edu>
2024-01-20 21:55:45 +0800

    no message

---
+++ src/main/java/Detective.java
@@ -0,0 +1,8 @@
+public class Detective {
+    public static void main(String[] args) {
+        String name = "Detective";
+        String line = "____________________________________________________________";
+        System.out.println(line + "\nHello! I'm [" + name + "]" + "\nWhat can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
