dd639e2b1d7e4227bfe4cac11b1f0e230d94b0c7
linnn-7 <linbinjulie@gmail.com>
2024-01-20 16:17:18 +0800

    initial version that greets and exit

---
+++ src/main/java/MissA.java
@@ -0,0 +1,9 @@
+public class MissA {
+    public static void main(String[] args) {
+        String logo = "What can I do for you?\n"
+                + "______________________\n"
+                + "Bye. Have a nice day!\n"
+                + "______________________\n";
+        System.out.println("Hello from MissA ^_^\n" + logo);
+    }
+}
