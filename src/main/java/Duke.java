0dcd66ba72cf21f0bc6d7d336ba6478a4bf2fba3
Dayn <daynangzj@gmail.com>
2024-01-20 23:01:01 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        String greeting = "____________________________________________________________\n" +
-                " Hello! I'm Megatron\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
-        String exit = " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+        // templates
+        String line = "\t____________________________________________________________\n";
+        String intro = line +
+                "\t Hello! I'm Megatron\n" +
+                "\t What can I do for you?\n" +
+                line;
+        String outro = line +
+                "\t Bye. Hope to see you again soon!\n" +
+                line;
 
-        System.out.println(greeting + exit);
+        Scanner sc = new Scanner(System.in);
+        String cmd = "";
+
+        System.out.println(intro);
+
+        while (!cmd.equals("bye")) {
+            cmd = sc.nextLine();
+            if (cmd.equals("bye")) break;
+
+            System.out.println(
+                    line + "\t" + cmd + "\n" + line
+            );
+        }
+
+        System.out.println(outro);
     }
 }
