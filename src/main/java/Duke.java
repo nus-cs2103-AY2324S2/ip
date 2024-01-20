b628cf9e2e061099bb6841c98047885b0bff0c78
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-01-20 13:52:55 +0800

    mimics user and exits when user inputs bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("--------------------------");
         System.out.println("Welcome!! I'm Belle <3.");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------");
+
+        boolean exit = false;
+        String input = "";
+
+        while (!exit) {
+            input = sc.next();
+            if (input.equals("bye")) {
+                exit = true;
+                break;
+            }
+            System.out.println("--------------------------");
+            System.out.println(input);
+            System.out.println("--------------------------");
+        }
+
+        System.out.println("--------------------------");
         System.out.println("Till next time!! Goodbye.");
         System.out.println("--------------------------");
+
     }
 }
