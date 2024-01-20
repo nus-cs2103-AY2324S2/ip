017bf36aca03ea601a8d600cd17578627deba792
teojunda <teojunda@gmail.com>
2024-01-21 01:18:38 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line_break = "________________________";
+        String name = "Beepo";
+        String greeting = " Hello! I'm " + name + "\n What can I do for you?";
+        String exit = " Bye. Hope to see you again soon!";
+        System.out.println(line_break);
+        System.out.println(greeting);
+        System.out.println(line_break);
+        System.out.println(exit);
+        System.out.println(line_break);
     }
 }
