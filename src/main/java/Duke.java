bee517627339fbea2b539bbc7681bc922099dcd4
Wilson W <wilsonwidyadhana681@gmail.com>
2024-01-20 20:44:24 +0800

    Added echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String welcome = "Hello! I'm TalkingBot\nWhat can I do for you?";
-        String bye = "Bye. Hope to see you again soon!";
         System.out.println(welcome);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean continueIter = true;
+
+
+        while (continueIter) {
+            String curCommand = scanner.nextLine();
+            switch (curCommand) {
+                case "bye":
+                    continueIter = false;
+                    break;
+                default:
+                    System.out.println(curCommand);
+            }
+        }
+
+        String bye = "Bye. Hope to see you again soon!";
         System.out.println(bye);
+        scanner.close();
     }
 }
