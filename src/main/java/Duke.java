7bab8dfcaf871b612e42281e3919b2a78faa0fd8
Zhi Heng <tanzhiheng26@gmail.com>
2024-01-20 21:32:07 +0800

    Change static fields and methods to non-static.
    This allows each instance of the chatbot to have its own task list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 public class Duke {
-    private static ArrayList<Task> tasks = new ArrayList<>();
+    private ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
-        Duke.greet();
+        Duke chatbot = new Duke();
+        chatbot.greet();
         Scanner scanner = new Scanner(System.in);
         String input = "";
         do {
             input = scanner.nextLine();
-            read(input);
+            chatbot.read(input);
         } while(!input.equals("bye"));
     }
 
@@ -17,13 +18,13 @@ public class Duke {
         System.out.println("\t────────────────────────────────────────────────────────────");
     }
 
-    public static void greet() {
+    public void greet() {
         Duke.horizontalLine();
         System.out.println("\tHello! I'm Ezra.\n\tWhat can I do for you?");
         Duke.horizontalLine();
     }
 
-    public static void read(String input) {
+    public void read(String input) {
         if (input.isEmpty()) {
             return;
         }
@@ -45,44 +46,44 @@ public class Duke {
         Duke.horizontalLine();
     }
 
-    public static void listTasks() {
-        if (Duke.tasks.size() == 0) {
+    public void listTasks() {
+        if (this.tasks.size() == 0) {
             System.out.println("\tThere are no tasks in your list.");
             return;
         }
         System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0; i < Duke.tasks.size(); i++) {
+        for (int i = 0; i < this.tasks.size(); i++) {
             System.out.printf(
                     "\t%d.%s\n",
                     i + 1,
-                    Duke.tasks.get(i).toString()
+                    this.tasks.get(i).toString()
             );
         }
     }
 
-    public static void mark(int taskIndex) {
-        if (taskIndex < 0 || taskIndex >= Duke.tasks.size()) {
+    public void mark(int taskIndex) {
+        if (taskIndex < 0 || taskIndex >= this.tasks.size()) {
             System.out.println("Invalid task number");
             return;
         }
-        Duke.tasks.get(taskIndex).isDone = true;
+        this.tasks.get(taskIndex).isDone = true;
         System.out.println("\tNice! I have marked this task as done:");
         System.out.printf(
                 "\t  %s\n",
-                Duke.tasks.get(taskIndex).toString()
+                this.tasks.get(taskIndex).toString()
         );
     }
 
-    public static void unmark(int taskIndex) {
-        if (taskIndex < 0 || taskIndex >= Duke.tasks.size()) {
+    public void unmark(int taskIndex) {
+        if (taskIndex < 0 || taskIndex >= this.tasks.size()) {
             System.out.println("Invalid task number");
             return;
         }
-        Duke.tasks.get(taskIndex).isDone = false;
+        this.tasks.get(taskIndex).isDone = false;
         System.out.println("\tOK, I've marked this task as not done yet:");
         System.out.printf(
                 "\t  %s\n",
-                Duke.tasks.get(taskIndex).toString()
+                this.tasks.get(taskIndex).toString()
         );
     }
 }
