ae5f2ce8215d73222029925edb638ecb32b36c59
NeoHW <neohaowei@gmail.com>
2024-01-21 02:34:59 +0800

    Level 2. Add, List Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;  // Import the Scanner class
 public class Duke {
     // class variable
     static String CHATBOT_NAME = "ByteBuddy";
     static String solidLineBreak = "____________________________________________________________";
-    static String START_MESSAGE = "Hello! I'm " + CHATBOT_NAME + "\n" + "\tWhat can I do for you?";
+    static String START_MESSAGE = "Hello! I'm " + CHATBOT_NAME + "\n" + "\t What can I do for you?";
     static String BYE_MESSAGE = "Sad to see you leave :(";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Task> taskList = new ArrayList<>();
 
         // start
         printWithSolidLineBreak(START_MESSAGE);
@@ -17,8 +19,12 @@ public class Duke {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")){
+                printTaskList(taskList);
             } else {
-                printWithSolidLineBreak(command);
+                taskList.add(new Task(command));
+                String toPrint = "added: " + command;
+                printWithSolidLineBreak(toPrint);
             }
         }
 
@@ -28,7 +34,15 @@ public class Duke {
 
     public static void printWithSolidLineBreak(String s) {
         System.out.println("\t" + solidLineBreak);
-        System.out.println("\t" + s);
+        System.out.println("\t " + s);
+        System.out.println("\t" + solidLineBreak);
+    }
+
+    public static void printTaskList(ArrayList<Task> taskList) {
+        System.out.println("\t" + solidLineBreak);
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println("\t " + (i+1) + ". " + taskList.get(i));
+        }
         System.out.println("\t" + solidLineBreak);
     }
 }
