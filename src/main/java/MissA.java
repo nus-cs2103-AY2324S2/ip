4d00c0ab9ce6edb5387d0e2cf5b8651688d4ba7d
linnn-7 <linbinjulie@gmail.com>
2024-01-20 16:56:39 +0800

    echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/MissA.java
+++ src/main/java/MissA.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 public class MissA {
+    // greet, exit sentences
+    private String greeting = "What can I do for you?\n"
+            + "______________________\n";
+    private String goodBye = "Bye. Have a nice day!\n"
+            + "______________________\n";
+    private String emptyLine = "______________________\n";
+
     public static void main(String[] args) {
-        String logo = "What can I do for you?\n"
-                + "______________________\n"
-                + "Bye. Have a nice day!\n"
-                + "______________________\n";
-        System.out.println("Hello from MissA ^_^\n" + logo);
+        MissA missA = new MissA();
+
+        // greet users when first enter the program
+        System.out.println("Hello from MissA ^_^\n" + missA.greeting);
+
+        //obtain user input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(missA.emptyLine + userInput + "\n" + missA.emptyLine);
+            userInput = scanner.nextLine();
+        }
+        System.out.println(missA.emptyLine + missA.goodBye);
     }
 }
