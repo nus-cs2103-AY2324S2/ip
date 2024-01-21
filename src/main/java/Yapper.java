0947bc317ef96dcd755a383b58a484b660f6e263
qinxutan <e0505785@u.nus.edu>
2024-01-21 21:40:59 +0800

    Rename Duke to Yapper

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,8 @@
+public class Yapper {
+    public static void main(String[] args) {
+        String chatbotName = "Yapper";
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
