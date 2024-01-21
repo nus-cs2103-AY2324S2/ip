c7b7f7f34d9b0f9a63a0f06b4bc31050ec37c854
kwuunnn <e0958174@u.nus.edu>
2024-01-21 22:34:24 +0800

    feat: Implement actions on user input
    Skeletal version with simple greeting and bye
    
    Let's
    * Echo commands entered by user
    * Exit with bye statement when user inputs 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String horizontalLine = "_____________________________________________________\n";
 
-        String greeting = horizontalLine + "Hello! I'm KwunBot!\nWhat can I do for you?";
+        String greeting = horizontalLine + "Hello! I'm KwunBot!\nWhat can I do for you?\n" + horizontalLine;
         System.out.println(greeting);
 
         String goodbye = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
-        System.out.println(goodbye);
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String req = sc.nextLine(); // Read user input
+
+            if (req.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            }
+            System.out.println(horizontalLine + req + "\n" + horizontalLine);
+        }
     }
 }
