5ac5fc2b5d1a2d1412a68ba54b513d4d54ff8aef
Tejas Garrepally <code@gtejas.com>
2024-01-21 23:13:50 +0800

    Implemented a simple skeletal version that simply greets the user and exits

--- src/main/java/MissMinutes.java
+++ src/main/java/MissMinutes.java
@@ -8,6 +8,16 @@ public class MissMinutes {
                 " | |  | | \\__ \\__ \\ | |  | | | | | | |_| | ||  __/\\__ \\\n" +
                 " |_|  |_|_|___/___/ |_|  |_|_|_| |_|\\__,_|\\__\\___||___/\n" +
                 "                                                       \n";
-        System.out.println("Hello from\n" + logo);
+
+        String separator = "-".repeat(60);
+
+        System.out.println(separator);
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you\n");
+
+        System.out.println(separator);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
