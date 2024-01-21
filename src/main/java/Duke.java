760c2753cd626dc800b05b9ea5276f802594afa7
cheahTJ <cheahtzejuen@gmail.com>
2024-01-21 22:56:21 +0800

    Added echoing feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String greetingMsg = "Hello! I'm PingMeBot\n" + "What can I do for you?";
         String exitMsg = "Bye. Hope to see you again soon!";
-        System.out.println(greetingMsg + "\n" + exitMsg);
+        System.out.println(greetingMsg);
+
+        while (true) {
+            String userInput = sc.nextLine();
+            if (!userInput.equals("bye")) {
+                System.out.println("\n" + userInput);
+            } else {
+                System.out.println("\n" + exitMsg);
+                break;
+            }
+        }
     }
 }
