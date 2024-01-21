2a67bd793db78e27d37514604adce6d9e78639c7
ziiqii <ziiiqiii@gmail.com>
2024-01-21 17:08:00 +0800

    Change chatbot name

--- src/main/java/UkeCat.java
+++ src/main/java/UkeCat.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class UkeCat {
     public static void main(String[] args) {
         String line = "________________________________________________________________________";
         String logo =
