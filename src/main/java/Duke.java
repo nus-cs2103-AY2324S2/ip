f974246acc31fe6d9f496bba52ece0eff8c1f204
NeoHW <neohaowei@gmail.com>
2024-01-22 01:04:55 +0800

    Level 4. ToDos, Events, Deadlines Add support for tracking three types of tasks:
    Level 4. ToDos, Events, Deadlines
    Add support for tracking three types of tasks:
    	ToDos: tasks without any date/time attached to it e.g., visit new theme park
    	Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
    	Events: tasks that start at a specific date/time and ends at a specific date/time e.g., (a) team project meeting 2/10/2019 2-4pm (b) orientation week 4/10/2019 to 11/10/2019

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import javax.crypto.spec.PSource;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;  // Import the Scanner class
+import java.util.stream.Collectors;
+
 public class Duke {
     // class variable
     static String CHATBOT_NAME = "ByteBuddy";
@@ -7,20 +12,20 @@ public class Duke {
     static String START_MESSAGE = "Hello! I'm " + CHATBOT_NAME + "\n" + "\t What can I do for you?";
     static String BYE_MESSAGE = "Sad to see you leave :(";
 
+    static ArrayList<Task> taskList;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<>();
+        taskList = new ArrayList<>();
 
         // start
         printWithSolidLineBreak(START_MESSAGE);
-
-
 
         // repeating user commands
         label:
         while (true) {
             String command = sc.next();
-            String info = sc.nextLine();
+            String info = sc.nextLine().trim();
             switch (command) {
                 case "bye":
                     break label;
@@ -37,11 +42,29 @@ public class Duke {
                     String unmarkToPrint =taskList.get(unmarkIndex).unmarkAsDone();
                     printWithSolidLineBreak(unmarkToPrint);
                     break;
-                default:
-                    taskList.add(new Task(command + info));
-                    String toPrint = "added: " + command + info;
-                    printWithSolidLineBreak(toPrint);
+                case "todo":
+                    Task todo = new Todo(info);
+                    taskList.add(todo);
+                    printTaskAddedWithSolidLineBreak(todo);
                     break;
+                case "deadline":
+                    List<String> deadlineInfo = Arrays.stream(info.split("/")).map(String::trim).collect(Collectors.toList());
+                    Task deadline = new Deadline(deadlineInfo.get(0), deadlineInfo.get(1).substring(3));
+                    taskList.add(deadline);
+                    printTaskAddedWithSolidLineBreak(deadline);
+                    break;
+                case "event":
+                    List<String> eventInfo = Arrays.stream(info.split("/")).map(String::trim).collect(Collectors.toList());
+                    Task event = new Event(eventInfo.get(0), eventInfo.get(1).substring(5), eventInfo.get(2).substring(3));
+                    taskList.add(event);
+                    printTaskAddedWithSolidLineBreak(event);
+                    break;
+                default:
+                    System.out.println("we are in default!! something is wrong :(");
+//                    taskList.add(new Task(command + info));
+//                    String toPrint = "added: " + command + info;
+//                    printWithSolidLineBreak(toPrint);
+//                    break;
             }
         }
 
@@ -52,6 +75,14 @@ public class Duke {
     public static void printWithSolidLineBreak(String s) {
         System.out.println("\t" + solidLineBreak);
         System.out.println("\t " + s);
+        System.out.println("\t" + solidLineBreak);
+    }
+
+    public static void printTaskAddedWithSolidLineBreak(Task task) {
+        System.out.println("\t" + solidLineBreak);
+        System.out.println("\t Got it. I've Added this task:");
+        System.out.println("\t\t " + task);
+        System.out.println("\t Now you have " + taskList.size() + " tasks in the list.");
         System.out.println("\t" + solidLineBreak);
     }
 
