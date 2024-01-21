b565e3e5182d2a7969f7e86e52905efea7b6e4dd
Rikko1204 <chenkuanmin1204@gmail.com>
2024-01-21 15:58:21 +0800

    Chatbot skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private String name;
+
+    public Duke(String name) {
+        this.name = name;
+    }
+    public String greetings() {
+        String reply = "Hello! I'm " + this.name + ". \n"
+                + "What can I do for you? \n";
+        return reply;
+    }
+
+    public String quitApplication() {
+        String reply = "Bye. Hope to see you again soon!";
+        return reply;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter new name for your chatbot. \n");
+        String name = scanner.nextLine();
+
+        Duke duke = new Duke(name);
+        System.out.println(duke.greetings());
+        System.out.println(duke.quitApplication());
     }
 }
