4e2c409f65cdc2389fad29e9ab2e756d42dcd01a
tengcharmaine <e0969596@u.nus.edu>
2024-01-21 13:34:20 +0800

    abstracted out the echo function into another method called userInput

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,22 +9,26 @@ public class Duke {
                            " Hello! I'm " + name + "\n What can I do for you?\n" +
                            "____________________________________________________________\n");
 
+        userInput();
+    }
+
+    private static void userInput() {
         //Scanner to scan what the user is inputting
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            //Echo input
-            System.out.println("____________________________________________________________\n" +
-                               " Okay! Carrying out " + input + "\n" +
-                               "____________________________________________________________\n");
-
-            //Bye input + close scanner + sxit program
+            //Bye input + close scanner + exit program
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________\n" +
-                                   " Bye. Hope to see you again soon!\n" +
-                                   "____________________________________________________________\n");
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
                 scanner.close();
                 System.exit(0);
+            } else {
+                //Echo input
+                System.out.println("____________________________________________________________\n" +
+                                   " Okay! Carrying out " + input + "\n" +
+                                   "____________________________________________________________\n");
             }
         }
     }
