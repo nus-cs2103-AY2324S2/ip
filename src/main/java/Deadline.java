c2fa549079fe38e82ec36e41369b86095c2dfc7f
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-01-21 23:13:28 +0800

    Deadline.java: Fix toString

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -19,6 +19,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return String.format("[T]%s (by: %s)", super.toString(), deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), deadline);
     }
 }
