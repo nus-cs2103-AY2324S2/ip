a5adbbf004a8e8733a780400d4f585f1ad5975a3
ziiqii <ziiiqiii@gmail.com>
2024-01-21 17:21:04 +0800

    Change chatbot name, greet and exit msgs

---
+++ src/main/java/UkeCat.java
@@ -0,0 +1,18 @@
+public class UkeCat {
+    public static void main(String[] args) {
+        String line = "________________________________________________________________________";
+        String logo =
+                  "                    /~(_)~\\.    Hi! I'm UkeCat, a cat with an ukulele!\n"
+                + " /= ••\\      /~(_)~\\        \\   \\| /\\           What can I do for you?\n"
+                + "K=|=|=|=|=|=|=|=|=(  )===]  |     (`~ o7 \n"
+                + " \\= ••/      \\_(~)_/        /    c\\   c\\ \n"
+                + "                    \\_(~)_/`     U`U_, )=~~ \n";
+        String bye = "Meow! See you again!";
+
+        System.out.println(line);
+        System.out.print(logo);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+}
