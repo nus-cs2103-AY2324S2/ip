d01245cb7fc85593aba02d71a347dd8ac4b7948e
rik <vslimrik@gmail.com>
2024-01-21 16:13:01 +0800

    feat: support tracking for different types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Duke {
@@ -15,10 +14,13 @@ public class Duke {
         boolean isExiting = false;
         while (!isExiting) {
             String[] inputs = br.readLine().split(" ");
+            String command = inputs[0];
+            String message = String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length));
             System.out.print(LINE);
-            switch (inputs[0]) {
+            switch (command) {
                 case "bye":
                     isExiting = true;
+                    System.out.println("\t Bye! Hope to see you again soon!");
                     break;
                 case "list":
                     if (tasks.isEmpty()) {
@@ -29,7 +31,7 @@ public class Duke {
                     }
                     break;
                 case "mark":
-                    int index = Integer.parseInt(inputs[1]);
+                    int index = Integer.parseInt(message);
                     if (index <= tasks.size() && index > 0) {
                         tasks.get(index - 1).mark();
                         System.out.println("\t Nice! I've marked this task as done:\n\t\t"
@@ -39,7 +41,7 @@ public class Duke {
                     }
                     break;
                 case "unmark":
-                    int unmarkIndex = Integer.parseInt(inputs[1]);
+                    int unmarkIndex = Integer.parseInt(message);
                     if (unmarkIndex <= tasks.size() && unmarkIndex > 0) {
                         tasks.get(unmarkIndex - 1).unmark();
                         System.out.println("\t OK, I've marked this task as not done yet:\n\t\t"
@@ -48,29 +50,51 @@ public class Duke {
                         System.out.println("\t Error: index out of bounds");
                     }
                     break;
+                case "todo":
+                    Task todo = new Todo(message);
+                    updateTasks(todo);
+                    break;
+                case "deadline":
+                    if (!message.contains("/by")) {
+                        System.out.println("\t Please specify deadline!");
+                        break;
+                    }
+                    String[] deadlineArgs = message.split("/by");
+                    String deadline_desc = deadlineArgs[0].trim();
+                    String by = deadlineArgs[1].trim();
+                    Task deadline = new Deadline(deadline_desc, by);
+                    updateTasks(deadline);
+                    break;
+                case "event":
+                    String[] eventArgs = message.split("/from");
+                    String event_desc = eventArgs[0].trim();
+                    String[] eventDurationArgs = eventArgs[1].split("/to");
+                    String start = eventDurationArgs[0].trim();
+                    String end = eventDurationArgs[1].trim();
+                    Task event = new Event(event_desc, start, end);
+                    updateTasks(event);
+                    break;
                 default:
-                    echo(inputs[0]);
-                    Task task = new Task(inputs[0]);
-                    tasks.add(task);
+                    break;
             }
             System.out.print(LINE);
         }
-        exit();
     }
 
     // greet user
     public static void greet() {
-        System.out.println(LINE + "\t Hello! I'm " + CHAT_BOT_NAME + "\n\t"
+        System.out.println(LINE + "\t Hello! I'm " + CHAT_BOT_NAME + "\n\t "
                 + "What can I do for you?\n" + LINE);
     }
 
-    // exit statement
-    public static void exit() {
-        System.out.println(LINE + "\t Bye! Hope to see you again soon!\n" + LINE);
-    }
-
-    // echo user inputs
-    public static void echo(String input) {
-        System.out.println("\t added: " + input);
+    public static void updateTasks(Task task) {
+        tasks.add(task);
+        if (tasks.size() > 1) {
+            System.out.println("\t Got it. I've added this task:\n\t\t " + task
+                    + "\n\t Now you have " + tasks.size() + " tasks in the list.");
+        } else {
+            System.out.println("\t Got it. I've added this task:\n\t\t " + task
+                    + "\n\t Now you have 1 task in the list.");
+        }
     }
 }
