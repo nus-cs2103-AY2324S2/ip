21f384f9a0c2769ed31577c5612b4df221382a7d
JiaXinEu <tis8cjiaxineu@gmail.com>
2024-01-21 20:59:53 +0800

    Rename, greet, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____   __  _  _  __    ___  ___  ____  ___  ____\n"
+                + " (_  _) /__\\( \\/ )/__\\  / __)/ __)(_  _)/ __)(_  _)\n"
+                + " .-_)( /(__)\\\\  //(__)\\ \\__ \\\\__ \\ _)(_ \\__ \\  )(\n"
+                + "\\____)(__)(__)\\/(__)(__)(___/(___/(____)(___/ (__)\n";
+
+        String line = "________________________________________________________________";
+        System.out.println(line + "\n" + logo + "\nHello! I'm JavAssist");
+        System.out.println("What can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
