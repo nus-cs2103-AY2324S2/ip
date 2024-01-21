f3961ec9fa6df327a6b77e74ef61239e44f786a2
yashma-sonara <yashma.sonara04@gmail.com>
2024-01-22 00:53:37 +0800

    Completed Level 4 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
               int num = sc.nextInt();
               store.get(num - 1).setStatus();
             } else {
-                addList(store, curr);
+                addList(store, curr, sc);
             }
 
         }
@@ -29,14 +29,52 @@ public class Duke {
 
     // if list entered
     public static void displayTasks(ArrayList<Task> store) {
+        System.out.println("Here are the items in your list :) ");
         for (int i = 0; i < store.size(); i++) {
             System.out.println((i + 1) + ". " + store.get(i));
         }
     }
 
-    public static void addList(ArrayList<Task> store, String added) {
-        Task toBeAdded = new Task(added);
+    public static void addList(ArrayList<Task> store, String added, Scanner sc) {
+        Task toBeAdded;
+
+        if (added.equals("deadline")) {
+            sc.useDelimiter("/by");
+            String description = sc.next();
+            sc.skip("/by");
+            sc.reset();
+            String time = sc.next();
+            toBeAdded = new Deadline(description, time);
+        }
+         else if (added.equals("todo")) {
+
+            String description = sc.nextLine();
+            toBeAdded = new Todo(description);
+        } else if (added.equals("event")) {
+            // event project meeting /from Mon 2pm /to 4pm
+            sc.useDelimiter("/from");
+            String description = sc.next();
+            System.out.println(description);
+            sc.skip("/from");
+            sc.reset();
+            sc.useDelimiter("/to");
+            String from = sc.next();
+            System.out.println(from);
+            sc.reset();
+            sc.skip("/to");
+            String to = sc.next();
+            System.out.println(to);
+            toBeAdded = new Event(description, from, to);
+
+        } else {
+             // backward compatibility
+            toBeAdded = new Task(added);
+        }
+
         store.add(toBeAdded);
-        System.out.println("added: " + added);
+
+        System.out.println("Got it! I've added this task.\n" + toBeAdded + "\nNow you have " + Task.getNumOfTasks() + " tasks in your list");
     }
+
 }
+
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,21 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    private static int numOfTasks = 0;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        numOfTasks++;
+
     }
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public static int getNumOfTasks() {
+        return numOfTasks;
     }
 
     public void setStatus() {
