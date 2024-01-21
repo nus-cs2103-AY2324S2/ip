d76adfcd9bad3399b80651aec189df58f396c0e6
iynixil <lixinyilxy02@gmail.com>
2024-01-21 19:13:25 +0800

    added echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Dave";
         String horizontalLine = "____________________________________________________________\n";
+        String exitCmd = "bye";
+
+        System.out.println(horizontalLine + "\nDave: Nice to meet you, I'm the ever-helpful " + name + "!\nHow may I be of service today?\n" + horizontalLine);
         
-        System.out.println(horizontalLine + "\nNice to meet you, I'm the ever-helpful " + name + "!\nHow may I be of service today?");
-        System.out.println(horizontalLine + "Thank you for your patronage, goodbye and have a nice day!");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals(exitCmd)) {
+            System.out.println(String.format("%s\nDave: %s\n%s", horizontalLine, input, horizontalLine));
+            input = sc.nextLine();
+        }
+
+        System.out.println(String.format("%s\nDave: Thank you for your patronage, goodbye and have a nice day!\n%s", horizontalLine, horizontalLine));
+
+        sc.close();
     }
 }
