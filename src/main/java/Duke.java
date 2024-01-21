360af882aadde15be0c5681f4bc15a966b3e6496
redcolorbicycle <liuzhengyang10@gmail.com>
2024-01-21 20:33:09 +0800

    Level 1 Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String name = "Bearducky";
-        System.out.println("Quack!git  My name is " + name + ". I would be glad to help you in exchange for some bread.");
+        System.out.println("Quack! My name is " + name + ". I would be glad to help you in exchange for some bread.");
+        String terminate = "";
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("[sad quacking]\n");
+                break;
+            } else {
+                System.out.println(userInput + "\n");
+            }
+        }
     }
 }
