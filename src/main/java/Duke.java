f535a3d1f9d0ba8c79848a790c9895e3e1268b9d
rik <vslimrik@gmail.com>
2024-01-21 12:49:26 +0800

    feat: echo user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,33 @@
-public class Duke {
-    static String horizontalLine = "――――――――――――――――――――――――――――――――――――――――";
-    static String chatbotName = "Bob";
+import java.io.*;
 
-    public static void main(String[] args) {
+public class Duke {
+    static String horizontalLine = "\t――――――――――――――――――――――――――――――――――――――――\n";
+    static String chatBotName = "Bob";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         greet();
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(horizontalLine + "\t" + input + "\n" + horizontalLine);
+            }
+        }
         exit();
     }
 
+    // greet user
     public static void greet() {
-        System.out.println(horizontalLine + "\n" + "Hello! I'm " + chatbotName + "\n"
+        System.out.println(horizontalLine + "\t" + "Hello! I'm " + chatBotName + "\n\t"
                 + "What can I do for you?\n" + horizontalLine);
     }
 
+    // exit statement
     public static void exit() {
-        System.out.println(horizontalLine + "\n" + "Bye! Hope to see you again soon!\n" + horizontalLine);
+        System.out.println(horizontalLine + "\t" + "Bye! Hope to see you again soon!\n" + horizontalLine);
     }
 
 }
