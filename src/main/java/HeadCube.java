ee75f2946f46c933caffceebe1153d7754acebec
headcube1 <ywesley16@gmail.com>
2024-01-21 23:15:38 +0800

    Changed name as well as added greeting and exit

---
+++ src/main/java/HeadCube.java
@@ -0,0 +1,14 @@
+public class HeadCube {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm HeadCube\n" + "What can I do for you?\n");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
