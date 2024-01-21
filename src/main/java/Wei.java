e39a46762577000d99822e86a1c4aab5b8f45cd1
swtan346 <swtan346@gmail.com>
2024-01-21 22:00:42 +0800

    add, list

--- src/main/java/Wei.java
+++ src/main/java/Wei.java
@@ -3,23 +3,42 @@ import java.util.Scanner;
 
 public class Wei {
     public static void main(String[] args) {
-        String greet = "Hello! I'm Wei.\n" + "What can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!\n";
+        String greet = "Hello! I'm Wei.\n" + "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
         String split = "______________________________";
-        System.out.println(greet + split);
+        // greet
+        System.out.println(greet);
+        System.out.println(split);
 
+        String[] text = new String[100];
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-
             if(Objects.equals(input, "bye")) {
                 break;
+            } else if (Objects.equals(input, "list")) {
+                if(text[0] == null) {
+                    System.out.println(split);
+                    continue;
+                }
+                // list
+                for (int i = 0; i < count; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + text[i]);
+                }
+                System.out.println(split);
+            } else {
+                // add
+                text[count] = input;
+                count++;
+                // echo
+                System.out.println("added: " + input);
+                System.out.println(split);
             }
-
-            //echo
-            System.out.println(input + "\n" + split);
         }
 
-        System.out.println(exit + split);
+        // exit
+        System.out.println(exit);
+        System.out.println(split);
     }
 }
