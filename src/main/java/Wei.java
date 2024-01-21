d947550352730bd3a62b5d0fb379672431ff3dac
swtan346 <swtan346@gmail.com>
2024-01-21 17:30:34 +0800

    echo

--- src/main/java/Wei.java
+++ src/main/java/Wei.java
@@ -1,7 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Wei {
     public static void main(String[] args) {
         String greet = "Hello! I'm Wei.\n" + "What can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println(greet + "\n" + exit);
+        String exit = "Bye. Hope to see you again soon!\n";
+        String split = "______________________________";
+        System.out.println(greet + split);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if(Objects.equals(input, "bye")) {
+                break;
+            }
+
+            //echo
+            System.out.println(input + "\n" + split);
+        }
+
+        System.out.println(exit + split);
     }
 }
