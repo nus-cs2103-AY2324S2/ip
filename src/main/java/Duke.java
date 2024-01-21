dfa26a9793c3c55370cf39e9aee90ea5fb7c5ce6
taeewonnn <htw10192000@gmail.com>
2024-01-21 23:41:23 +0800

    Chatbot is able to echo the command the user is typing in

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm KAI\n" + "What can i do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
