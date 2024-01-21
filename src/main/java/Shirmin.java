16a07f8dfced2d934b97623f8e7056e708888214
nichee <E0957568@u.nus.edu>
2024-01-21 20:57:36 +0800

    add echo function, refactor chatbot skeleton

--- src/main/java/Shirmin.java
+++ src/main/java/Shirmin.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
 public class Shirmin {
 
-    public static void displayLine(){
+    public static void displayLine() {
         System.out.println("_".repeat(50));
     }
-    public static void main(String[] args){
+    public static void echo(String line) {
+        displayLine();
+        System.out.println(line);
+        displayLine();
+    }
+    public static void greet() {
         displayLine();
         System.out.println("Hello! I'm Shirmin" + "\n" + "What can I do for you?");
         displayLine();
+    }
+    public static void exit() {
+        displayLine();
         System.out.println("Bye. Hope to see you again soon!");
         displayLine();
+    }
+    public static void main(String[] args){
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        String curr = scanner.nextLine();
+        while(!curr.equals("bye")) {
+            echo(curr);
+            curr = scanner.nextLine();
+        }
+        exit();
     }
 }
