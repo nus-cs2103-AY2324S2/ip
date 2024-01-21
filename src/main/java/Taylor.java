9d679fbc739f9a6b82f82d2a5d56e1c8ebb9e503
AidenLYT <ytstirlingite@hotmail.com>
2024-01-22 02:06:10 +0800

    Improved response

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Taylor {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Taylor");
         System.out.println("What can I do for you?");
+
+        while(true) {
+            Scanner type = new Scanner(System.in);
+            String input = type.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
