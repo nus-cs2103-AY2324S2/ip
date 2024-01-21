32796a2a86cbad4a26b65583d7a20edc14e44d7d
qinxutan <e0505785@u.nus.edu>
2024-01-21 22:05:52 +0800

    Echo

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class Yapper {
     public static void main(String[] args) {
         String chatbotName = "Yapper";
-        System.out.println(" Hello! I'm " + chatbotName);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Hello! I'm " + chatbotName + ". I talk a lot, hence my name, but I will be sure to keep you company.");
         System.out.println(" What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while (true) {
+            System.out.println("User: ");
+            String userInput = scanner.nextLine();
+            System.out.println(chatbotName + ": " + userInput);
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
+
