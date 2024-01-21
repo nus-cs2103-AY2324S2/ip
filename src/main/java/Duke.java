5416f4aef6a596c4dc1924a22a7979082d9839d4
iynixil <lixinyilxy02@gmail.com>
2024-01-21 20:50:05 +0800

    added completion functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,22 +12,35 @@ public class Duke {
         // read input
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        String[] inputList = new String[100];
+        Task[] taskList = new Task[100];
         int listIdx = 0;
 
         // user input
         while (!input.equals(exitCmd)) {
             if (input.equals("list")) {
-                System.out.println(horizontalLine);
+                System.out.println(horizontalLine + "\nHere are the tasks in your list:\n");
                 for (int i = 0; i < listIdx; i++) {
-                    System.out.println(String.format("%d. %s", i+1, inputList[i]));
+                    System.out.println(String.format("%d. %s", i+1, taskList[i].toString()));
                 }
                 System.out.println(horizontalLine);
-                
             } else {
-                inputList[listIdx] = input;
-                listIdx++;
-                System.out.println(String.format("%s\nDave added: %s\n%s", horizontalLine, input, horizontalLine));
+                String[] inputArr = input.split(" ");
+                if (inputArr[0].equals("mark") || inputArr[0].equals("unmark")) {
+                    int taskNumber = Integer.parseInt(inputArr[1]) - 1;
+
+                    if (inputArr[0].equals("mark")) {
+                        taskList[taskNumber].isCompleted();
+                        System.out.println(String.format("%s\nGood job! I'll mark this task as done:\n%s\n%s", horizontalLine, taskList[taskNumber].toString(), horizontalLine));    
+                    } else {
+                        taskList[taskNumber].isNotCompleted();
+                        System.out.println(String.format("%s\nAlright, I believe you'll get this done eventually:\n%s\n%s", horizontalLine, taskList[taskNumber].toString(), horizontalLine));
+                    }
+                } else {
+                    Task newTask = new Task(input);
+                    taskList[listIdx] = newTask;
+                    listIdx++;
+                    System.out.println(String.format("%s\nDave added: %s\n%s", horizontalLine, input, horizontalLine));
+                }
             }
             input = sc.nextLine();
 
