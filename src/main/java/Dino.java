9ac6d38e1a48f541264937e73c295fbfd75bd62c
jiahui0309 <e0970466@u.nus.edu>
2024-01-21 21:01:29 +0800

    Level-2: Add, List

--- src/main/java/Dino.java
+++ src/main/java/Dino.java
@@ -1,25 +1,59 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Dino {
-    public static void welcome() {
+    private ArrayList<Task> taskList;
+    public Dino() {
+        this.taskList = new ArrayList<>();
+    }
+    public void welcome() {
         System.out.println("Hola! I'm Dino.\nWhat are you doing here?");
     }
 
-    public static void goodbye() {
+    public void goodbye() {
         System.out.println("Goodbye! It was nice meeting you.");
     }
 
-    public static void echo() {
+    public void echo() {
         Scanner sc = new Scanner(System.in);
         String chat = sc.nextLine();
         while (!chat.equalsIgnoreCase("bye")) {
             System.out.println(chat);
             chat = sc.nextLine();
         }
-        Dino.goodbye();
+    }
+
+    public void addTask(Task task) {
+        this.taskList.add(task);
+    }
+
+    public void listTask() {
+        for (int i = 0; i < taskList.size(); i++) {
+            String task = taskList.get(i).toString();
+            int index = i + 1;
+            System.out.println(index + ". " + task);
+        }
     }
 
     public static void main(String[] args) {
-        Dino.welcome();
-        Dino.echo();
+        Dino mrDino = new Dino();
+        Scanner sc = new Scanner(System.in);
+        mrDino.welcome();
+
+        String newTask = sc.nextLine();
+
+        while (true) {
+            if (newTask.equals("list")) {
+                mrDino.listTask();
+            } else if (newTask.equals("bye")) {
+                mrDino.goodbye();
+                return;
+            } else {
+                Task task = new Task(newTask);
+                mrDino.addTask(task);
+                System.out.println("added: " + newTask);
+            }
+            newTask = sc.nextLine();
+        }
+
     }
 }
