aee073ebf942c77104038721acf520662b756338
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-01-21 21:01:18 +0800

    delete task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Task[] items = new Task[100];
+        ArrayList<Task> items = new ArrayList<>();
         int itemCount = 0;
         boolean isValid = true;
 
@@ -25,28 +26,28 @@ public class Duke {
         }
     }
 
-    private static Pair<Boolean, Integer> handleInput(String command, Task[] items, int itemCount) throws DukeException {
+    private static Pair<Boolean, Integer> handleInput(String command, ArrayList<Task> items, int itemCount) throws DukeException {
         if (command.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
             return Pair.of(false, itemCount);
         } else if (command.equals("list")) {
             System.out.println("Here are the tasks in your list:");
             for (int i=0; i<itemCount; i++) {
-                System.out.printf("%s. %s%n", i+1, items[i]);
+                System.out.printf("%s. %s%n", i+1, items.get(i));
             }
         } else if (command.startsWith("mark")) {
             int index = Integer.parseInt(command.split(" ")[1]) - 1;
-            items[index].markDone();
+            items.get(index).markDone();
 
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println(items[index]);
+            System.out.println(items.get(index));
 
         } else if (command.startsWith("unmark")) {
             int index = Integer.parseInt(command.split(" ")[1]) - 1;
-            items[index].unmarkDone();
+            items.get(index).unmarkDone();
 
             System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println(items[index]);
+            System.out.println(items.get(index));
         }
 
         else if (command.startsWith("todo")) {
@@ -57,7 +58,8 @@ public class Duke {
             System.out.println("Got it. I've added this task:");
 
             Todo todo = new Todo(command.substring(5, command.length()));
-            items[itemCount] = todo;
+
+            items.add(todo);
             itemCount++;
 
             System.out.println(todo);
@@ -75,7 +77,7 @@ public class Duke {
             String by = splittedCommand[1];
 
             Deadline deadline = new Deadline(task, by);
-            items[itemCount] = deadline;
+            items.add(deadline);
             itemCount++;
 
             System.out.println(deadline);
@@ -96,10 +98,23 @@ public class Duke {
             String to = splittedTime[1];
 
             Event event = new Event(task, from, to);
-            items[itemCount] = event;
+            items.add(event);
             itemCount++;
 
             System.out.println(event);
+            System.out.printf("Now you have %s tasks in the list.%n", itemCount);
+        } else if (command.startsWith("delete")) {
+            if (command.length() <= 7) {
+                throw new DukeException("OOPS!!! Task ID cannot be empty.");
+            }
+
+            System.out.println("Noted. I've removed this task:");
+
+            int index = Integer.parseInt(command.split(" ")[1]) - 1;
+            Task task = items.remove(index);
+            itemCount -= 1;
+            System.out.println(task);
+
             System.out.printf("Now you have %s tasks in the list.%n", itemCount);
         } else {
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
