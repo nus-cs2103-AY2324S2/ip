c96cf507693edbfbf19b6b652020345f69c82852
AidenLYT <ytstirlingite@hotmail.com>
2024-01-22 01:39:21 +0800

    Changed name

---
+++ src/main/java/Taylor.java
@@ -0,0 +1,7 @@
+public class Taylor {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Taylor");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
