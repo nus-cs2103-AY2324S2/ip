10949b76df08c57d387049323c5f76393f07d017
Lin-Shuang-Shuang <e0970548@u.nus.edu>
2024-01-21 19:53:06 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
 
     private final String name;
+    private ArrayList<String> taskList = new ArrayList<>();
     private final String line = "____________________________________________________________\n";
     public Duke(String name) {
         this.name = name;
@@ -15,19 +16,32 @@ public class Duke {
         return this.line + "Bye. Hope to see you again soon!\n" +  this.line ;
     }
 
+    public String printList(){
+        String list = this.line;
+        for (int i = 0; i<this.taskList.size(); i++) {
+            String task = Integer.toString(i+1) + ". " + this.taskList.get(i) + "\n";
+            list += task;
+
+        }
+        return list + this.line;
+    }
+
     public void startChat() {
         System.out.println(this.greet());
         Scanner scanner = new Scanner(System.in);
 
-        while(scanner.hasNext()){
+        while(scanner.hasNext()) {
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("bye")){
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println(this.exit());
                 break;
 
-            } else {
-                System.out.println(this.line + command + "\n"+this.line);
+            } else if (command.equalsIgnoreCase("list")){
+                System.out.println(this.printList());
 
+            }else {
+                System.out.println(this.line + "added: "+ command + "\n"+this.line);
+                taskList.add(command);
             }
         }
     }
