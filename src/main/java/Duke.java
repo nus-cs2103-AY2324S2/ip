d764f67ca0bb4bf4f38a9d157397c83bfdb0cf0a
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-01-21 16:19:51 +0800

    Add echo feature, refactor greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Class running the main chatbot program.
+ */
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Duke.greet();
+        Duke.echo(sc);
+        Duke.exit();
+    }
+
+    /**
+     * Greet behaviour of chatbot.
+     */
+    private static void greet() {
         System.out.println("Hello, I'm Ted.");
         System.out.println("What can I do for you today?");
+    }
+
+    /**
+     * Echo behaviour of chatbot.
+     * Chatbot will output user input word for word.
+     * Behaviour will terminate only if user inputs a case-insensitive "bye" command.
+     *
+     * @param sc Scanner reading user inputs.
+     */
+    private static void echo(Scanner sc) {
+        while (true) {
+            String msg = sc.nextLine();
+            if (msg.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(msg);
+        }
+    }
+
+    /**
+     * Exit behaviour of chatbot.
+     */
+    private static void exit() {
         System.out.println("Bye. See you again.");
     }
 }
