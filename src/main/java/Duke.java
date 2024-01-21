873bce7a5a8abf6ec81cc4d76d0d794719f34528
redcolorbicycle <liuzhengyang10@gmail.com>
2024-01-22 01:00:17 +0800

    Level 2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String name = "Bearducky";
         System.out.println("Quack! My name is " + name + ". I would be glad to help you in exchange for some bread.");
-        String terminate = "";
         Scanner scanner = new Scanner(System.in);
         String userInput;
+        List<String> tasks = new ArrayList<>();
         while (true) {
             userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("[sad quacking]\n");
                 break;
+            } else if (userInput.equals("list")) {
+                for (String a: tasks) {
+                    System.out.println(a);
+                }
+                System.out.println("\n");
             } else {
-                System.out.println(userInput + "\n");
+                System.out.println("added: " +userInput + "\n");
+                tasks.add(userInput);
             }
         }
     }
