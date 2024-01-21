8795dc5b9f69b98cc45dba57497d8c88be790f7d
unknown <xuelinglow09@gmail.com>
2024-01-22 01:57:22 +0800

    Duke Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm MichelleBot! \nWhat can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon! \\(^-^)/ ");
+                break; // Exit the loop and end the program
+            }
+            System.out.println("Your input is: " + input);
+        }
+        scanner.close();
     }
 }
