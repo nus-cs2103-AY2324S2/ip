afb568083abaa7ac17607380d4d16d33df1cf72a
Ondre Tann <ondre.tannzxc@gmail.com>
2024-01-21 22:33:04 +0800

    Added greeting and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "_______________________________________________________\n" +
+                "Hello! I'm Thames and I'll be your assistant chatbot.\n" +
+                "What can I do for you today?\n" +
+                "_______________________________________________________\n";
+
+        String exit = "_______________________________________________________\n" +
+                "Bye. Hope to see you soon!\n" +
+                "_______________________________________________________\n";
+        System.out.println(greeting + exit);
     }
 }
