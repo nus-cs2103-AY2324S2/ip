785698687ca829002e8840434327d115aff17b26
blaukc <brandonlau1@hotmail.com>
2024-01-22 00:09:38 +0800

    Add echo loop

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Dude {
     static final String spacer = "____________________________________________________________\n";
     static final String name = "Dude";
@@ -8,8 +10,22 @@ public class Dude {
     private static String getGoodbye() {
         return spacer + "Bye. Hope to see you again soon!\n" + spacer;
     }
+
+    private static void echo(String input) {
+        System.out.println(input);
+    }
     public static void main(String[] args) {
         System.out.println(getGreeting());
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input == "bye") {
+                break;
+            } else {
+                echo(input);
+            }
+        }
         System.out.println(getGoodbye());
     }
 }
