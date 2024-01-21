abbfbc49758e61bae01fd04c98b121be74e6b7f7
ziiqii <ziiiqiii@gmail.com>
2024-01-21 16:45:27 +0800

    Implement simple greet and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "________________________________________________________________________";
+        String logo =
+                  "                    /~(_)~\\.    Hi! I'm UkeCat, a cat with an ukulele!\n"
+                + " /= ••\\      /~(_)~\\        \\   \\| /\\           What can I do for you?\n"
+                + "K=|=|=|=|=|=|=|=|=(  )===]  |     (`~ o7 \n"
+                + " \\= ••/      \\_(~)_/        /    c\\   c\\ \n"
+                + "                    \\_(~)_/`     U`U_, )=~~ \n";
+        String bye = "Meow! See you again!";
+
+        System.out.println(line);
+        System.out.print(logo);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
     }
 }
