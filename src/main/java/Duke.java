0424a8d75ded333a05043593022ce8586fee1a3f
JiaXinEu <tis8cjiaxineu@gmail.com>
2024-01-21 23:08:32 +0800

    Add class Task to support mark task as done or not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
 
         Scanner read = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println(line + "\n" + logo + "\n\t Hello! I'm JavAssist");
         System.out.println("\t What can I do for you?\n" + line);
@@ -23,22 +23,41 @@ public class Duke {
             System.out.println(line);
             if (input.equals("list")) {
                 print(list);
-                System.out.println(line);
-            } else if (!input.equals("bye")) {
-                list.add(input);
-                System.out.println("\t added: " + input + "\n" + line);
-            } else {
+            } else if (input.matches("mark\\s\\d+")) {
+                String[] s = input.split(" ");
+                int num = Integer.parseInt(s[1]);
+                if (num <= list.size()) {
+                    Task t = list.get(num - 1);
+                    t.done();
+                    System.out.println("\t Nice! I've marked this task as done:");
+                    System.out.println("\t\t" + t.printTask());
+                }
+            } else if (input.matches("unmark\\s\\d+")) {
+                String[] s = input.split(" ");
+                int num = Integer.parseInt(s[1]);
+                if (num <= list.size()) {
+                    Task t = list.get(num - 1);
+                    t.undo();
+                    System.out.println("\t Nice! I've marked this task as not done yet:");
+                    System.out.println("\t\t" + t.printTask());
+                }
+            } else if (input.equals("bye")) {
                 break;
+            } else {
+                list.add(new Task(input));
+                System.out.println("\t added: " + input);
             }
+            System.out.println(line);
         }
 
         System.out.println("\t Bye. Hope to see you again soon!\n" + line);
     }
 
-    private static void print(ArrayList<String> list) {
+    private static void print(ArrayList<Task> list) {
         int count = 1;
-        for (String s : list) {
-            System.out.println("\t " + count + ". " + s);
+        for (Task t : list) {
+            System.out.println("\t " + count + "." + t.printTask());
+            count++;
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String task;
+    private boolean done;
+
+    public Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    public String printTask() {
+        return String.format("[" + (this.done ? "X" : " ") + "] " + this.task);
+    }
+
+    public void done() {
+        this.done = true;
+    }
+
+    public void undo() {
+        this.done = false;
+    }
+}
