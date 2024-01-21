7c148d860a88a24c581523af3dbc83ef3358cbee
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-01-21 18:53:48 +0800

    ChatBot.java: Add indentation and read input

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 /**
- * ChatBot encapsulates the behaviour of a Chat Bot.
+ * ChatBot encapsulates the behaviour of a Chatbot.
  *
  * @author Titus Chew
  */
 public class ChatBot {
     private final String chatBotName;
+    private final Scanner scanner = new Scanner(System.in);
+    private static final String COMMAND_BYE = "bye";
+    private static final String INDENT = "    ";
 
+    /**
+     * Class constructor.
+     *
+     * @param chatBotName the name of the chatbot
+     */
     public ChatBot(String chatBotName) {
         this.chatBotName = chatBotName;
     }
@@ -14,26 +24,47 @@ public class ChatBot {
      * Greets the user when entering the application.
      */
     public void greet() {
-        insertLine();
-        System.out.printf(
-                "Hello! I'm %s!\n" +
-                "What can I do for you?\n",
-                chatBotName);
+        printMessage(String.format(
+                INDENT + "Hello! I'm %s!\n" +
+                INDENT + "What can I do for you?",
+                chatBotName));
     }
 
     /**
      * Greets the user when exiting the application.
      */
     public void exit() {
-        insertLine();
-        System.out.println("Bye! Hope to see you again soon!");
+        printMessage(INDENT + "Bye! Hope to see you again soon!");
     }
 
     /**
      * Inserts a horizontal line.
      */
     private void insertLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println(INDENT + "____________________________________________________________");
     }
 
+    /**
+     * Prints a message to the console.
+     *
+     * @param message the message to print in the console
+     */
+    private void printMessage(String message) {
+        insertLine();
+        System.out.println(message);
+        insertLine();
+    }
+
+    /**
+     * Reads and parses user input for commands.
+     */
+    public void run() {
+        greet();
+        String userInput = scanner.nextLine();
+        while (!userInput.equals(COMMAND_BYE)) {
+            printMessage(INDENT + userInput);
+            userInput = scanner.nextLine();
+        }
+        exit();
+    }
 }
