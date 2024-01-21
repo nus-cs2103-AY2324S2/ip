611e25c41e98a93988a8e7592ea32d290e11503f
iynixil <lixinyilxy02@gmail.com>
2024-01-21 19:34:21 +0800

    added 'add' and 'list' functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,34 @@ public class Duke {
         String horizontalLine = "____________________________________________________________\n";
         String exitCmd = "bye";
 
-        System.out.println(horizontalLine + "\nDave: Nice to meet you, I'm the ever-helpful " + name + "!\nHow may I be of service today?\n" + horizontalLine);
-        
+        // greeting
+        System.out.println(String.format("%s\n%s: Nice to meet you, I'm the ever-helpful %s!\nHow may I be of service today?\n%s", horizontalLine, name, name, horizontalLine));
+
+        // read input
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        String[] inputList = new String[100];
+        int listIdx = 0;
 
+        // user input
         while (!input.equals(exitCmd)) {
-            System.out.println(String.format("%s\nDave: %s\n%s", horizontalLine, input, horizontalLine));
+            if (input.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < listIdx; i++) {
+                    System.out.println(String.format("%d. %s", i+1, inputList[i]));
+                }
+                System.out.println(horizontalLine);
+                
+            } else {
+                inputList[listIdx] = input;
+                listIdx++;
+                System.out.println(String.format("%s\nDave added: %s\n%s", horizontalLine, input, horizontalLine));
+            }
             input = sc.nextLine();
+
         }
 
+        // end program if user says bye
         System.out.println(String.format("%s\nDave: Thank you for your patronage, goodbye and have a nice day!\n%s", horizontalLine, horizontalLine));
 
         sc.close();
