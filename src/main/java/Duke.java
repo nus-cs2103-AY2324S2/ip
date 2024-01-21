51732762494a70185022d118d5824d3d2e0ed4e9
kwuunnn <e0958174@u.nus.edu>
2024-01-21 23:27:47 +0800

    feat: Implement list
    Chatbot can echo user inputs
    
    Let's
    * Add the ability to store tasks in list
    * Implement 'list' command to show current list of tasks stored

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,15 +9,31 @@ public class Duke {
 
         String goodbye = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
 
+        String[] tasks = new String[100]; // Assume there are no more than 100 tasks
+        int counter = 0;
+
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String req = sc.nextLine(); // Read user input
+            String input = sc.nextLine(); // Read user input
 
-            if (req.equals("bye")) {
+            if (input.equals("bye")) {
                 System.out.println(goodbye);
                 break;
             }
-            System.out.println(horizontalLine + req + "\n" + horizontalLine);
+
+            if (input.equals("list")) {
+                System.out.println(horizontalLine);
+
+                for (int i = 0; i < counter; i++) {
+                    System.out.printf("%d. %s\n", i + 1, tasks[i]);
+                }
+                System.out.println("\n" + horizontalLine);
+                continue;
+            }
+
+            System.out.println(horizontalLine + "added: " + input + "\n" + horizontalLine);
+            tasks[counter] = input;
+            counter++;
         }
     }
 }
