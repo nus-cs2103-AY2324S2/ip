97bb6cbb2dfd795ea9d2669caeb84222cda5d700
blaukc <brandonlau1@hotmail.com>
2024-01-21 22:49:19 +0800

    Change name to Dude

---
+++ src/main/java/Dude.java
@@ -0,0 +1,12 @@
+public class Dude {
+    public static void main(String[] args) {
+
+        String greeting = "____________________________________________________________\n"
+            + "Hello! I'm Dude\n"
+            + "What can I do for you?\n"
+            + "____________________________________________________________\n"
+            + "Bye. Hope to see you again soon!\n"
+            + "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
