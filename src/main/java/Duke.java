a11e150073d5d4efa9672830c041d2e4c5e73d4d
Ella-e <mzyu_bj@163.com>
2024-01-21 16:31:46 +0800

    feat: Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
-public class Duke {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Plaudern\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+import java.util.Scanner;
 
+public class Duke {
+    private static String EXITKEY = "bye";
+    public static void main(String[] args) {
+        String userInput = "";
+        System.out.println("Hello! I'm Plaudern\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while(!userInput.equals(EXITKEY)) {
+            userInput = scanner.nextLine();
+            System.out.println(userInput);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
