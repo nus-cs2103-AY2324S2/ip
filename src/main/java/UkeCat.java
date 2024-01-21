96fee4822c3c4d929846378af7aae0ce2a085b51
ziiqii <ziiiqiii@gmail.com>
2024-01-21 18:18:28 +0800

    Add echo to bot

--- src/main/java/UkeCat.java
+++ src/main/java/UkeCat.java
@@ -1,18 +1,55 @@
+import java.util.Scanner;
+
 public class UkeCat {
     public static void main(String[] args) {
         String line = "________________________________________________________________________";
-        String logo =
+        String welcome =
                   "                    /~(_)~\\.    Hi! I'm UkeCat, a cat with an ukulele!\n"
                 + " /= ••\\      /~(_)~\\        \\   \\| /\\           What can I do for you?\n"
                 + "K=|=|=|=|=|=|=|=|=(  )===]  |     (`~ o7 \n"
                 + " \\= ••/      \\_(~)_/        /    c\\   c\\ \n"
                 + "                    \\_(~)_/`     U`U_, )=~~ \n";
-        String bye = "Meow! See you again!";
+        String bye =
+            "  +--------------------+" + "    /\\_/\\\n" +
+            "  |Meow~ See you again!|" + "   =~.~=|\n" +
+            "  +--------------------+" + "     \\  \\/\n";
 
+        Scanner sc = new Scanner(System.in);
+
+        // Welcome msg
         System.out.println(line);
-        System.out.print(logo);
+        System.out.print(welcome);
         System.out.println(line);
-        System.out.println(bye);
-        System.out.println(line);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            // Exit condition
+            if (input.equals("bye")) {
+                System.out.println(bye);
+                sc.close();
+                System.exit(0);
+            }
+
+            // Hi
+            if (input.equals("hi")) {
+                String hiHuman =
+                    "  +-------------------------------------------------------------------------------------------------------------+\n" +
+                    "  |Greetings, sentient being known as a human, I wish to convey my heartfelt salutations to you.                |"+"    /\\_/\\\n" +
+                    "  |As we commence our digital discourse, I extend a warm welcome and express my enthusiasm for this interaction.|"+"   =X.X=|\n" +
+                    "  |May our engagement be both enlightening and mutually beneficial in the realm of virtual communication.       |"+"     \\  \\/\n" +
+                    "  +-------------------------------------------------------------------------------------------------------------+";
+                System.out.println(hiHuman);
+                continue;
+            }
+
+            // Echo text
+            input = "You've entered: " + input;
+            int n = input.length();
+            String horizontal = "  +" + "-".repeat(n) + "+";
+            System.out.println(horizontal + "    /\\_/\\");
+            System.out.println("  |" + input + "|" + "   =O.O=|");
+            System.out.println(horizontal + "     \\  \\/");
+        }
     }
 }
