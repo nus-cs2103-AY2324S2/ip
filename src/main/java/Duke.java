e12f6a76517c848dd6e774595ac8b3719e4c57a2
rik <vslimrik@gmail.com>
2024-01-21 11:50:50 +0800

    - [x] renamed chatbot - [x] implemented greet and exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    static String horizontalLine = "――――――――――――――――――――――――――――――――――――――――";
+    static String chatbotName = "Bob";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println(horizontalLine + "\n" + "Hello! I'm " + chatbotName + "\n"
+                + "What can I do for you?\n" + horizontalLine);
+    }
+
+    public static void exit() {
+        System.out.println(horizontalLine + "\n" + "Bye! Hope to see you again soon!\n" + horizontalLine);
     }
 }
