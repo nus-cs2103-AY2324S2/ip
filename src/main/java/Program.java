c72db1e402d09bb8c321d2fa686b1fc93595b05f
darryl-chan <darrylchan8@yahoo.com.sg>
2024-01-21 13:25:31 +0800

    Added echoing

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,23 +1,50 @@
+import java.util.Scanner;
+
 public class Program {
 
     private final String NAME = "Linus";
     private final String BORDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private Boolean running;
+    private Scanner userInputScanner;
+
+    public Program() {
+        this.running = true;
+        this.userInputScanner = new Scanner(System.in);
+    }
 
     public void start() {
-        System.out.println(this.BORDER);
+        this.greeting();
+        
+        while (this.running) {
+            String userInput = this.userInputScanner.nextLine();
+            this.readUserInput(userInput);
+        }
+
+    }
+
+    private void readUserInput(String input) {
+        if (input.toLowerCase().equals("bye")) {
+            this.end();
+        } else {
+            this.textFormat(input);
+        }
+    }
+
+    private void greeting() {
         String greeting = String.format("Hello I'm %s", this.NAME);
         String request = "What can I do for you?";
-        this.textFormat(String.format("%s \n%s", greeting, request));
-        this.end();
+        this.textFormat(String.format("%s\n\t%s", greeting, request));
     }
 
     private void end() {
-        String exit = "Bye. Hope to see you again soon!";
+        String exit = "Goodbye. See you later!";
         this.textFormat(exit);
+        this.running = false;
     }
 
     private void textFormat(String text) {
-        System.out.println(String.format("%s \n%s", 
+        System.out.println(String.format("\t%s\n\t%s\n\t%s", 
+        this.BORDER,
         text, 
         this.BORDER));
     }
