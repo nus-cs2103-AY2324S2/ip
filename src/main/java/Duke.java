5eda7fec57f3068705321501ea3a8e36e94f0618
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-01-22 01:06:10 +0800

    Add echo to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println(
                 "Hello! I'm Nico\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!\n");
+                "What can I do for you?");
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            String command = userInput.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
