c9c18dcdb470302587ccb084ab119019dd2d3c23
ZHANGTIANYAO1 <88520881+ZHANGTIANYAO1@users.noreply.github.com>
2024-01-21 21:44:14 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    public static void speak(String str) {
+        System.out.println(str);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        String logo = " ____    _    ___  ____ ___ ____   ____   ___ _____ \n" +
+                "/ ___|  / \\  / _ \\|  _ \\_ _/ ___| | __ ) / _ \\_   _|\n" +
+                "\\___ \\ / _ \\| | | | |_) | | |  _  |  _ \\| | | || |  \n" +
+                " ___) / ___ \\ |_| |  __/| | |_| | | |_) | |_| || |  \n" +
+                "|____/_/   \\_\\___/|_|  |___\\____| |____/ \\___/ |_|\n";
+        speak(logo);
+        speak("Hello! I'm SAOPIG BOT\nWhat can I do for you?");
+        speak("Bye. Hope to see you again soon!");
     }
 }
