3c905d9dd262579888ae4007bcda6dcd1f566dcd
nichee <E0957568@u.nus.edu>
2024-01-21 20:40:18 +0800

    Add Shirmin.java

---
+++ src/main/java/Shirmin.java
@@ -0,0 +1,13 @@
+public class Shirmin {
+
+    public static void displayLine(){
+        System.out.println("_".repeat(50));
+    }
+    public static void main(String[] args){
+        displayLine();
+        System.out.println("Hello! I'm Shirmin" + "\n" + "What can I do for you?");
+        displayLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        displayLine();
+    }
+}
