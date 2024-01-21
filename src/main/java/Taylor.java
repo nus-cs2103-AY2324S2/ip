54df70b18948aca56d1d141c11c3fd3d50e94f28
AidenLYT <ytstirlingite@hotmail.com>
2024-01-22 02:12:41 +0800

    Improved response

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Taylor {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Taylor");
         System.out.println("What can I do for you?");
+        HashMap<Integer, String> listing = new HashMap<>();
+        Integer pos = 1;
 
         while(true) {
             Scanner type = new Scanner(System.in);
@@ -11,8 +14,15 @@ public class Taylor {
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                listing.forEach((key, value) -> {
+                    System.out.println(key + ". " + value);
+                });
+            } else {
+                listing.put(pos, input);
+                pos++;
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
