ada1b8e89a5d426de7d570c7c4f283cac177bcf2
unknown <xuelinglow09@gmail.com>
2024-01-22 02:17:07 +0800

    Duke Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm MichelleBot! \nWhat can I do for you?");
         System.out.println("____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        boolean keepRunning = true;
+        ArrayList<String> theList = new ArrayList<>();
+        do {
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon! \\(^-^)/ ");
-                break; // Exit the loop and end the program
+            
+            switch(input.toLowerCase()) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon! \\(^-^)/ ");
+                    keepRunning = false;
+                    break;
+
+                case "list":
+                    for (int i = 0; i < theList.size(); i++) {
+                        int itemNumber = i + 1;
+                        String listItem = theList.get(i);
+                        System.out.println(itemNumber + ". " + listItem);
+                    }
+                    break;
+
+                default:
+                    System.out.println("added: " + input);
+                    theList.add(input);
             }
-            System.out.println("Your input is: " + input);
-        }
+            System.out.println("____________________________________________________________");
+
+        } while (keepRunning);
+
         scanner.close();
     }
 }
