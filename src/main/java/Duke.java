cc797739594f1d4861641633066f30c500aa6bad
TanJiaJun <e0725056@u.nus.edu>
2024-01-22 03:01:00 +0800

    Level 0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "__________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm ChatterPal!");
+        System.out.println("What can I do for you?");
+
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
