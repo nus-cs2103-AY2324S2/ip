6223d0a5dced07628de14275beacdea360ecb797
Lalelulilulela <e0967877@u.nus.edu>
2024-01-21 18:14:33 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I am Lelu :) \nWhat can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        System.out.println(exit);
     }
 }
