a0cc6ee6a0745b67d30beee0104fa50dc5a03a05
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-01-22 01:22:16 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static int echo(Scanner sc) {
+        String res = sc.nextLine();
+        if (res.equals("bye")) {
+            return -1;
+        }
+        System.out.println(res + "\n");
+        return 0;
+    }
     public static void main(String[] args) {
         String msg =
                 "Hello! I'm Refinement\n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon!";
+                + "What can I do for you?\n\n";
         System.out.println(msg);
+
+        Scanner sc = new Scanner(System.in);
+        while (echo(sc) == 0);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
