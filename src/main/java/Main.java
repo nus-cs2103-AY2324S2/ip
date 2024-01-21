841bfaca6d36555906adeafa1390f193ff516f27
Miffi <miffi@decadent.rest>
2024-01-21 12:28:35 +0800

    Extract I/O logic into Main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    private static void cat() {
+        System.out.println(" |\\ /| ");
+        System.out.println("=(O O)=");
+        System.out.println(" /   \\ ");
+    }
+
+    private static void line() {
+        for (int i = 0; i < 72; i++) {
+            System.out.print('_');
+        }
+        System.out.print('\n');
+    }
+
+    private static void hello() {
+        cat();
+        System.out.println("Hello! I'm the cat that lives in your walls.");
+        System.out.println("What do you need?");
+        line();
+    }
+
+    private static void bye() {
+        line();
+        System.out.println("*The cat recedes into the wall with a bored look on its face*");
+        line();
+    }
+
+    public static void repl() {
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+        while (sc.hasNextLine() && !(input = sc.nextLine()).equals("bye")) {
+            line();
+            System.out.println(input);
+            line();
+        }
+    }
+
+    public static void main(String[] args) {
+        hello();
+        repl();
+        bye();
+    }
+}
