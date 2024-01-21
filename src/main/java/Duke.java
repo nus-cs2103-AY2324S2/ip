b7b99e80701bf8863e86ccbccfab1fc54c2fa233
tengcharmaine <e0969596@u.nus.edu>
2024-01-21 14:54:58 +0800

    added markTask and unmarkTasks method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int max_tasks = 100;
-    private static final String[] tasks = new String[max_tasks];
+    private static final Task[] tasks = new Task[max_tasks];
     private static int taskcount = 0;
 
     public static void main(String[] args) {
@@ -31,6 +31,12 @@ public class Duke {
             } else if (input.equals("list")) {
                 //List tasks
                 listTasks(input);
+            } else if (input.startsWith("mark")) {
+                //mark tasks as complete
+                markTask(input);
+            }else if (input.startsWith("unmark")) {
+                //unmark tasks
+                unmarkTask(input);
             } else {
                 //Add task to list
                 addTask(input);
@@ -52,15 +58,48 @@ public class Duke {
     private static void addTask(String input) {
         //Check that taskcount does not exceed maxtask
         if (taskcount < max_tasks) {
-            tasks[taskcount] = input;
+            Task task = new Task(input);
+            tasks[taskcount] = task;
             taskcount++;
             System.out.println("____________________________________________________________\n" +
-                               " Okay! Adding " + input + " to list\n" +
+                               " Okay! Adding " + task + " to your list\n" +
                                "____________________________________________________________\n");
         } else {
             System.out.println("____________________________________________________________\n" +
                                " Ohno :( Your task list is full. Complete some tasks first. \n" +
                                "____________________________________________________________\n");
+        }
+    }
+
+    //Method to mark tasks
+    private static void markTask(String input) {
+        int taskindex = Integer.parseInt(input.substring(5)) - 1;
+        if (taskindex >= 0 && taskindex < taskcount) {
+            tasks[taskindex].mark();
+            System.out.println("____________________________________________________________\n" +
+                               " Nice! This task has been marked as done:\n" +
+                               "   " + tasks[taskindex] + "\n" +
+                               "____________________________________________________________\n");
+        } else {
+            System.out.println("____________________________________________________________\n" +
+                               " Invalid task index inputted. Please try again.\n" +
+                               "____________________________________________________________\n");
+        }
+    }
+
+    //Method to unmark tasks
+    private static void unmarkTask(String input) {
+        int taskindex = Integer.parseInt(input.substring(7)) - 1;
+        if (taskindex >= 0 && taskindex < taskcount) {
+            tasks[taskindex].unmark();
+            System.out.println("____________________________________________________________\n" +
+                    " Okay. This task has been unmarked. \n" +
+                    "   " + tasks[taskindex] + "\n" +
+                    "____________________________________________________________\n");
+        } else {
+            System.out.println("____________________________________________________________\n" +
+                    " Invalid task index inputted. Please try again.\n" +
+                    "____________________________________________________________\n");
         }
     }
 }
