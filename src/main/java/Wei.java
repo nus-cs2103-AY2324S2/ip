e3d1524aca999aee8c5fd1957c5dd8c684cf4f86
swtan346 <swtan346@gmail.com>
2024-01-21 15:21:54 +0800

    rename

---
+++ src/main/java/Wei.java
@@ -0,0 +1,9 @@
+public class Wei {
+    public static void main(String[] args) {
+        String logo = "\\      / \\      /||==== ||\n"
+                + " \\    /   \\    / ||____ ||\n"
+                + "  \\  /     \\  /  ||     ||\n"
+                + "   \\/       \\/   ||==== ||\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
