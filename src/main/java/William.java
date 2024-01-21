fc61586fce542e31e5a3ff16bb6a1053f3ee5f71
KhoonSun47 <justforwks@gmail.com>
2024-01-21 15:45:47 +0800

    Level 0. Rename, Greet, Exit.

---
+++ src/main/java/William.java
@@ -0,0 +1,18 @@
+public class William {
+    public static void main(String[] args) {
+        /*
+         * String logo = " ____        _        \n"
+         * + "|  _ \\ _   _| | _____ \n"
+         * + "| | | | | | | |/ / _ \\\n"
+         * + "| |_| | |_| |   <  __/\n"
+         * + "|____/ \\__,_|_|\\_\\___|\n";
+         * System.out.println("Hello from\n" + logo);
+         */
+
+        String logo = "William";
+
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon");
+    }
+}
