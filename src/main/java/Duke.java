04346c548e6e5a56bd8e2c38831a5dd85362e7ad
JiaXinEu <tis8cjiaxineu@gmail.com>
2024-01-21 21:25:17 +0800

    Add list to store and display text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,6 +11,9 @@ public class Duke {
         String line = "\t________________________________________________________________";
 
         Scanner read = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+
         System.out.println(line + "\n" + logo + "\n\t Hello! I'm JavAssist");
         System.out.println("\t What can I do for you?\n" + line);
         String input;
@@ -17,13 +21,24 @@ public class Duke {
             System.out.println();
             input = read.nextLine();
             System.out.println(line);
-            if (!input.equals("bye")) {
-                System.out.println("\t " + input + "\n" + line);
+            if (input.equals("list")) {
+                print(list);
+                System.out.println(line);
+            } else if (!input.equals("bye")) {
+                list.add(input);
+                System.out.println("\t added: " + input + "\n" + line);
             } else {
                 break;
             }
         }
 
         System.out.println("\t Bye. Hope to see you again soon!\n" + line);
+    }
+
+    private static void print(ArrayList<String> list) {
+        int count = 1;
+        for (String s : list) {
+            System.out.println("\t " + count + ". " + s);
+        }
     }
 }
