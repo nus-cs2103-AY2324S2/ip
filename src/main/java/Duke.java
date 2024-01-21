eac43d9159f8125bc7e5c3996571d6b1b0e24387
Lin-Shuang-Shuang <e0970548@u.nus.edu>
2024-01-21 19:14:12 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private final String name;
+    private final String line = "____________________________________________________________\n";
     public Duke(String name) {
         this.name = name;
     }
 //
     public String greet() {
-        return "Hello! I'm " + this.name + "\n" + "What can I do for you?";
+        return this.line + "Hello! I'm " + this.name + "\n" + "What can I do for you?\n" + this.line;
     }
     public String exit() {
-        return "Bye. Hope to see you again soon!";
+        return this.line + "Bye. Hope to see you again soon!\n" +  this.line ;
+    }
+
+    public void startChat() {
+        System.out.println(this.greet());
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")){
+                System.out.println(this.exit());
+                break;
+
+            } else {
+                System.out.println(this.line + command + "\n"+this.line);
+
+            }
+        }
     }
 
     public static void main(String[] args) {
         Duke Lery = new Duke("Lery");
 
-        System.out.println(Lery.greet());
-        System.out.println(Lery.exit());
+
+        Lery.startChat();
 
     }
 
