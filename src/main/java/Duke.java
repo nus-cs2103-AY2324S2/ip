7aa3d91ac917525a04bf5be7029c1287f4284091
ALZT <zhengtinglim.2602@gmail.com>
2024-01-21 14:01:16 +0800

    Duke Level-0 + updated gitignore for Java class files

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
+        String hLine = "________________________________________________\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String greetMsg = "Hello! I'm Hatsune Miku!\n"
+                + " What can I do for you?";
+        String exitMsg = "Bye. Hope to see you again soon!";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(hLine);
+        System.out.println(greetMsg);
+        System.out.println(hLine);
+        System.out.println(exitMsg);
+        System.out.println(hLine);
     }
 }
