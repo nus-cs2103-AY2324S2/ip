7ac99e6d7d45c29607e9474ab97f24381f1c9743
tsulim <limxiujia18@gmail.com>
2024-01-22 01:42:08 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
+    public static final String DIVIDER = "────────────────────────────────────────────────────────────";
+    public static final String GREETING = "Hello! I'm Seiki\nHow may I assist you today?";
+    public static final String FAREWELL = "Goodbye! If you ever need assistance in the future, don't hesitate to reach out. Take care!";
+    public static final String LOGO = "  _____      _  _     _\n"
+                                    + " / ____|    (_)| | _ (_)\n"
+                                    + "| (___  ___  _ | |/ / _\n"
+                                    + " \\___ \\/ _ \\| ||   / | |\n"
+                                    + " ____) | __/| || | \\ | |\n"
+                                    + "|_____/\\___||_||_|\\_\\|_|\n";
+
+    public static void start() {
+        System.out.println(DIVIDER);
+        System.out.println(LOGO);
+        System.out.println(GREETING);
+        System.out.println(DIVIDER);
+    }
+
+    public static void end() {
+        System.out.println(FAREWELL);
+        System.out.println(DIVIDER);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        start();
+        end();
     }
 }
