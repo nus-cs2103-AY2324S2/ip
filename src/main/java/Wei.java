99925017ff663a4d1e46304ab1977e1c2993924b
swtan346 <swtan346@gmail.com>
2024-01-21 16:48:41 +0800

    greet, exit

--- src/main/java/Wei.java
+++ src/main/java/Wei.java
@@ -1,9 +1,7 @@
 public class Wei {
     public static void main(String[] args) {
-        String logo = "\\      / \\      /||==== ||\n"
-                + " \\    /   \\    / ||____ ||\n"
-                + "  \\  /     \\  /  ||     ||\n"
-                + "   \\/       \\/   ||==== ||\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm Wei.\n" + "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greet + "\n" + exit);
     }
 }
