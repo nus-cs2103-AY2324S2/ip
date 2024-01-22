e0bf798737ee1b12930e7f5f678398736cc1d871
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-01-23 01:29:18 +0800

    Add todo, deadline, and event to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,16 +25,34 @@ public class Duke {
                 t.unmark();
                 System.out.println("Ok, I've marked this task as not done yet:");
                 System.out.println(t);
-            } else if (command.contains("mark")) {
+            } else if (command.contains("mark") ) {
                 Task t = tasks[Integer.valueOf(command.split(" ")[1]) - 1];
                 t.mark();
                 System.out.println("Nice, I've marked this task as done:");
                 System.out.println(t);
-            } else {
-                System.out.println("added: "+command);
-                Task t = new Task(command);
+            } else if (command.contains("todo") ||
+                    command.contains("deadline") ||
+                    command.contains("event")) {
+                Task t = null;
+                if (command.contains("todo")) {
+                    t = new Todo(command.split(" ",2)[1]);
+                } else if (command.contains("deadline")) {
+                    String desc = command.split(" ",2)[1].split(" /by", 2)[0];
+                    String by = command.split("/by ",2)[1];
+                    t = new Deadline(desc, by);
+                } else if (command.contains("event")) {
+                    String desc = command.split(" ",2)[1].split(" /from", 2)[0];
+                    String from = command.split("/from ",2)[1].split(" /", 2)[0];
+                    String to = command.split("/to ",2)[1];
+                    t = new Event(desc, from, to);
+                }
                 tasks[taskCount] = t;
                 taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(t);
+                System.out.println("Now you have " + taskCount + " task(s) in the list.");
+            } else {
+                System.out.println("unknown command");
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,8 +23,4 @@ public class Task {
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
-
-    public static void main(String[] args) {
-
-    }
 }
