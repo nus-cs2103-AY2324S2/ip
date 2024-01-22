41175700f0cdc1b2d64e761e027879198aeab149
sethteo <hongruiseth.teo@gmail.com>
2024-01-23 01:41:01 +0800

    Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String DIVIDER = "____________________________________________________________";
+        String WELCOME_TEXT = "Hello! I'm SlayBot\nWhat can I do for you?";
+        String BYE_TEXT = "Bye. Hope to see you again soon!";
+        String logo = "\n" +
+                "   _____ _             ____        _   \n" +
+                "  / ____| |           |  _ \\      | |  \n" +
+                " | (___ | | __ _ _   _| |_) | ___ | |_ \n" +
+                "  \\___ \\| |/ _` | | | |  _ < / _ \\| __|\n" +
+                "  ____) | | (_| | |_| | |_) | (_) | |_ \n" +
+                " |_____/|_|\\__,_|\\__, |____/ \\___/ \\__|\n" +
+                "                  __/ |                \n" +
+                "                 |___/                 \n";
+        System.out.println(logo);
+        System.out.println(DIVIDER + "\n" + WELCOME_TEXT + "\n" + DIVIDER + "\n" +
+                BYE_TEXT);
+
+
     }
 }
