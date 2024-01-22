143b6bb9a1632144e97871ac683ef61c29e798f1
Ragnapop <jinhunl@gmail.com>
2024-01-22 15:12:18 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.exit;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,6 +15,19 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm \n" + name);
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n\n");
+
+
+        BufferedReader bfn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String str = bfn.readLine();
+        while (true) {
+            System.out.println(str);
+            str = bfn.readLine();
+            if (str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n\n");
+                System.exit(1);
+
+            }
+        }
     }
 }
