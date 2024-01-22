1e3eecd84e9f9a5550a531b1e737851f1a0bbce3
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-22 21:31:05 +0800

    Added content for Level-2

--- src/main/java/Kervyn.java
+++ src/main/java/Kervyn.java
@@ -1,8 +1,11 @@
+import java.lang.reflect.Array;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Kervyn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> userRequests = new ArrayList<>();
         String chatbotName = "Kervyn";
 
         System.out.println("\tHello! I'm " + chatbotName);
@@ -11,10 +14,17 @@ public class Kervyn {
         String userInput;
         do {
             userInput = scanner.nextLine();
-            if (!Objects.equals(userInput, "bye")) {
-                System.out.println("\t" + userInput);
+            if (!Objects.equals(userInput, "bye") && !Objects.equals(userInput, "list")) {
+                System.out.println("\tadded: " + userInput);
+                userRequests.add(userInput);
             }
 
+            if (Objects.equals(userInput, "list")) {
+                for (int i = 0; i < userRequests.size(); i ++) {
+                    System.out.println("\t" + (i + 1) + ". " +  userRequests.get(i));
+
+                }
+            }
         } while (!Objects.equals(userInput, "bye"));
 
         System.out.println("\tBye. Hope to see you again soon!");
