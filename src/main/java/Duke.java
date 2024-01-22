ef94e7656dbcf2754da2b59278f5b7b35987196c
Zen Khoon <e0957872@u.nus.edu>
2024-01-22 15:05:09 +0800

    Add basic skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+
+    private void greet(){
+        System.out.println("Hello! I'm Balom.\nWhat can I do for you today?\n");
+    }
+    private void bye(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke Balom = new Duke();
+        Balom.greet();
+        Balom.bye();
+
     }
 }
