c442b3e0abffd56f0bd6c27f0f07258a4f307505
wongkj12 <wongkj12@gmail.com>
2024-01-23 01:40:46 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        // Initialization
         String name = "MR. WONG";
         say("Hey man. I'm " + name + "\nWhat can I do for you?");
 
+        // Stores user items
+        ArrayList<String> userItems = new ArrayList<>();
+
+        // Input/output
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         while (!userInput.equals("bye")) {
             userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 say("Bye. Hope to see you again soon!");
+            } else if (userInput.equals("list")) {
+                say(displayList(userItems));
             } else {
-                say(userInput);
+                say("added: " + userInput);
+                userItems.add(userInput);
             }
         }
         scanner.close();
@@ -23,5 +32,13 @@ public class Duke {
         System.out.println(horizontal);
         System.out.println(msg);
         System.out.println(horizontal);
+    }
+
+    public static String displayList(ArrayList<String> userItems) {
+        String d = "";
+        for (int i = 1; i <= userItems.size(); ++i) {
+            d += (i + ". " + userItems.get(i-1) + '\n');
+        }
+        return d;
     }
 }
