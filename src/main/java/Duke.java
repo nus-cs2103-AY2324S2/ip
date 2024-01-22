3c4b46aa767a4c9b68c35b8f04bfd2110c380231
maze508 <ac0bge@gmail.com>
2024-01-22 11:11:40 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String[] store = new String[100];
+        int itemCounts = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm MazeDeneroBot");
         System.out.println("What can I do for you?");
@@ -11,9 +15,17 @@ public class Duke {
             String command = scan.nextLine();
             if (command.equals("bye")) {
                 break;
-            } else {
+            } else if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(command);
+                for (int i=0; i<itemCounts; i++) {
+                    System.out.println(i+1 + ". " + store[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                store[itemCounts] = command;
+                itemCounts++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + command);
                 System.out.println("____________________________________________________________");
             }
         }
