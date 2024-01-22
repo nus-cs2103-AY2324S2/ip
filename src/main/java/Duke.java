0acccb6ba6af4dd687ddc23fea82d0bab4884544
danielleloh <szehan.loh@gmail.com>
2024-01-22 22:21:57 +0800

    Edited to echo commands entered by user, exits when user enters bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Scribbles.");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you soon!");
+
+        String input = sc.nextLine(); // takes in input from user
+        while (!(input.equals("bye"))) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
