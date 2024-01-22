d942a3b8ec2a2e229a9b1b66008a8495b53a91e2
TanJiaJun <e0725056@u.nus.edu>
2024-01-22 21:16:05 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String input;
         String line = "_________________________________";
+        ArrayList<String> storage = new ArrayList<String>();
         System.out.println(line);
         System.out.println("Hello! I'm ChatterPal!");
         System.out.println("What can I do for you?");
+        System.out.println(line);
         Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,9 +18,19 @@ public class Duke {
         input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                System.out.println("list");
+                System.out.println(line);
+                for (int j=0; j<storage.size(); j++) {
+                    System.out.printf("%d. %s\n", j+1, storage.get(j));
+                }
+                System.out.println(line);
+
             } else if (input.equals("blah")) {
                 System.out.println("blah");
+            } else {
+                storage.add(input);
+                System.out.println(line);
+                System.out.printf("added: %s\n", input);
+                System.out.println(line);
             }
             input = sc.nextLine();
         }
