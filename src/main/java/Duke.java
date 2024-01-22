3bdbedde4c5281311d89ab7a753957f01358cd61
TanJiaJun <e0725056@u.nus.edu>
2024-01-22 23:31:33 +0800

    Level-3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String input;
-        String line = "_________________________________";
-        ArrayList<String> storage = new ArrayList<String>();
-        System.out.println(line);
+        String line = "_________________________________\n";
+        Storage storage = new Storage();
+        System.out.print(line);
         System.out.println("Hello! I'm ChatterPal!");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.print(line);
         Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,23 +16,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         input = sc.nextLine();
+        String[] inputParts = input.split(" ");
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(line);
-                for (int j=0; j<storage.size(); j++) {
-                    System.out.printf("%d. %s\n", j+1, storage.get(j));
-                }
+                System.out.println(storage);
                 System.out.println(line);
 
-            } else if (input.equals("blah")) {
-                System.out.println("blah");
+            } else if (inputParts[0].equals("mark")) {
+                Task t = storage.get(Integer.parseInt(inputParts[1]) - 1);
+                t.markAsDone();
+                System.out.println(line);
+                System.out.println("Great job on completing the task!");
+                System.out.println(t.toString());
+                System.out.println(line);
+            } else if (inputParts[0].equals("unmark")) {
+                Task t = storage.get(Integer.parseInt(inputParts[1]) - 1);
+                t.markAsUndone();
+                String output = line ;
+                output += "OK, I've marked this task as not done yet: \n" +
+                        t.toString() + line;
+                System.out.println(output);
             } else {
-                storage.add(input);
+                Task newTask = new Task(input);
+                storage.add(newTask);
                 System.out.println(line);
                 System.out.printf("added: %s\n", input);
                 System.out.println(line);
             }
             input = sc.nextLine();
+            inputParts = input.split(" ");
         }
         System.out.println(line);
         System.out.println("Farewell! Can't wait to catch up with you again. Until next time, " +
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 public class Storage {
-    public ArrayList<String> storage;
+    public ArrayList<Task> storage;
+    public Storage() {
+        this.storage = new ArrayList<Task>();
+    }
+    public void add(Task t) {
+        this.storage.add(t);
+    }
+    public int size() {
+        return this.storage.size();
+    }
+    public Task get(int i) {
+        return this.storage.get(i);
+    }
+    @Override
+    public String toString() {
+        String output = "";
+        for (int i=0; i<this.storage.size(); i++) {
+            output += String.format("%d. ", i+1);
+            output += this.storage.get(i).toString();
+        }
+        return output;
+    }
 
 }
