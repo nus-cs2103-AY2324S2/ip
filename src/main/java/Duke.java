9cbbfd0783be5b786f37486e5f9409160f77cf16
alvinnzz <alvinng01@live.com>
2024-01-22 22:39:50 +0800

    Created function to add task to list and print list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     static String botName = "Corgi";
+    static String[] tasks = new String[100];
+    static int numOfTasks = 0;
     public static void greet(){
         String greetMessage = String.format(
                 "____________________________________________________________\n" +
@@ -21,6 +23,21 @@ public class Duke {
                 input +
                 "\n____________________________________________________________\n");
     }
+    public static void addToList(String input){
+        tasks[numOfTasks++] = input;
+        String message = String.format("____________________________________________________________\n" +
+                " added: %s\n" +
+                "____________________________________________________________\n", input);
+        System.out.println(message);
+    }
+    public static void printList(){
+        System.out.println("____________________________________________________________\n");
+        for (int i = 0; i < numOfTasks; i++) {
+            String currentTask = String.format("%d: %s", i + 1, tasks[i]);
+            System.out.println(currentTask);
+        }
+        System.out.println("____________________________________________________________\n");
+    }
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
@@ -29,8 +46,11 @@ public class Duke {
             input = sc.nextLine();
             if(input.toLowerCase().equals("bye")){
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                printList();
+            } else{
+                addToList(input);
             }
-            echo(input);
         }
         goodbye();
     }
