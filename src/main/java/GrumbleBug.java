e5081782fe13bbe7bf319afe7e6ad362c32007d5
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-01-22 22:17:26 +0800

    First commit, L0

---
+++ src/main/java/GrumbleBug.java
@@ -0,0 +1,11 @@
+public class GrumbleBug {
+    public static void main(String[] args) {
+        String logo = "_______________________________________\n"
+                + "Oh hey, adventurer.\n"
+                + "I'm GrumbleBug.\n"
+                + "What's your- you know what, I don't care.\n"
+                + "Go away. Byebye...\n"
+                + "_______________________________________\n";
+        System.out.println(logo);
+    }
+}
