957af52a8034ae9cf560b8351d4a2eaedb6a3d11
Ryan Tan <ryantan111100@gmail.com>
2024-01-23 00:22:15 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
+import java.util.*;
+
 public class Duke {
     private static final String BOT_NAME = "Felix";
     public static void printHorizontalLine(int length) {
         for (int i = 0; i < length; i++) System.out.print('_');
         System.out.println();
     }
-
     public static void printIntroduction() {
         printHorizontalLine(60);
         System.out.printf("Hello! I'm %s\nWhat can I do for you?\n",BOT_NAME);
@@ -17,13 +18,25 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        boolean loop = true;
         printIntroduction();
-        printGoodbye();
+        while (loop) {
+            String line = scanner.nextLine();
+            printHorizontalLine(60);
+            if (line.equals("bye")) {
+                loop = false;
+                printGoodbye();
+            } else {
+                System.out.println(line);
+                printHorizontalLine(60);
+            }
+        }
     }
 }
