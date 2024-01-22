7e68227cd8f56e1690494fd4e56f8a245161ba0c
0-yibai <2802756281@qq.com>
2024-01-22 11:13:56 +0800

    a skeletal version that greets the user and exits

---
+++ src/main/java/ChatBox.java
@@ -0,0 +1,14 @@
+public class ChatBox {
+
+    public ChatBox() {
+    }
+
+    public void launch() {
+        System.out.println("____________________________________________________________\n"
+                + "Hello! I'm Wis.\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n");
+    }
+}
--- src/main/java/Wis.java
+++ src/main/java/Wis.java
@@ -1,12 +1,6 @@
 public class Wis {
     public static void main(String[] args) {
-        String logo =  "__        _____ ____\n"
-            + "\\ \\      / /_ _/ ___|\n"
-            + " \\ \\ /\\ / / | |\\___ \\\n"
-            + "  \\ V  V /  | | ___) |\n"
-            + "   \\_/\\_/  |___|____/\n";
-
-
-        System.out.println("Hello from\n" + logo);
+        ChatBox chatBox = new ChatBox();
+        chatBox.launch();
     }
 }
