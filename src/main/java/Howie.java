bfc7c1e439c9ee7e6c102395b9120f6e3d33b660
smolegz <zhuohui.koo@gmail.com>
2024-01-22 16:11:36 +0800

    Completed Level-1

--- src/main/java/Howie.java
+++ src/main/java/Howie.java
@@ -1,14 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Howie {
     public static void printVLine() {
         System.out.println("---------------------------");
     }
-    public static void main(String[] args) {
 
+    public static void intro() {
         printVLine();
         System.out.println("Hello! I'm Howie");
         System.out.println("What can I do for you?");
         printVLine();
-        System.out.println("Bye. Hope to see you again soon!!");
+    }
+
+    public static void main(String[] args) throws IOException {
+        intro();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = br.readLine();
+            if (!s.equals("bye")) {
+                printVLine();
+                System.out.println(s);
+                printVLine();
+            } else {
+                break;
+            }
+        }
+        printVLine();
+        System.out.println("Bye! I'll see you when I see you!");
         printVLine();
     }
 }
