38e52d6be6a6fea34bc8ec5325285390f22a84f9
0-yibai <2802756281@qq.com>
2024-01-22 10:46:18 +0800

    gave the chatbot a new name

---
+++ src/main/java/Wis.java
@@ -0,0 +1,12 @@
+public class Wis {
+    public static void main(String[] args) {
+        String logo =  "__        _____ ____\n"
+            + "\\ \\      / /_ _/ ___|\n"
+            + " \\ \\ /\\ / / | |\\___ \\\n"
+            + "  \\ V  V /  | | ___) |\n"
+            + "   \\_/\\_/  |___|____/\n";
+
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
