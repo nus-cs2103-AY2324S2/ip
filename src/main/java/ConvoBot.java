7025cede5668b20113ff069657feb4b6d524b2fa
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-22 23:32:03 +0800

    Implement Level-5 and A-Exceptions

--- src/main/java/ConvoBot.java
+++ src/main/java/ConvoBot.java
@@ -34,52 +34,84 @@ public class ConvoBot {
             printHorizontalLine(false);
             ArrayList<String> inputList = new ArrayList<>(Arrays.asList(userInput.split(" ")));
 
-            if (userInput.equals("list")) {
-                System.out.println(leftPadding + " " + "Here are the tasks in your list:"); 
-                for (int i = 0; i < taskList.size(); i++) {
-                    int index = i+1;
-                    System.out.println(leftPadding + " " + Integer.toString(index)
-                        + "." + taskList.get(i).toString());
-                }
-            } else if (inputList.get(0).equals("mark") && inputList.size() == 2) {
-                int i = Integer.parseInt(inputList.get(1)) - 1;
-                taskList.get(i).markAsDone();
-                System.out.println(leftPadding + " " + "Nice! I've marked this task as done:");
-                System.out.println(leftPadding + " " + taskList.get(i).toString());
-            } else if (inputList.get(0).equals("unmark") && inputList.size() == 2) {
-                int i = Integer.parseInt(inputList.get(1)) - 1;
-                taskList.get(i).markAsNotDone();
-                System.out.println(leftPadding + " " + "OK, I've marked this task as not done yet:");
-                System.out.println(leftPadding + " " + taskList.get(i).toString());
-            } else {
-                Task task;
-                if (inputList.get(0).equals("todo")) {
-                    task = new ToDo(userInput.substring(5));
-                } else if (inputList.get(0).equals("deadline")) {
-                    int j = inputList.indexOf("/by");
-                    if (j == -1) {
-                        // todo: handle error
+            try {
+                if (userInput.equals("list")) {
+                    System.out.println(leftPadding + " " + "Here are the tasks in your list:"); 
+                    for (int i = 0; i < taskList.size(); i++) {
+                        int index = i+1;
+                        System.out.println(leftPadding + " " + Integer.toString(index)
+                            + "." + taskList.get(i).toString());
                     }
-                    String description = String.join(" ", inputList.subList(1, j));
-                    String by = String.join(" ", inputList.subList(j+1, inputList.size()));
-                    task = new Deadline(description, by);
-                } else if (inputList.get(0).equals("event")) {
-                    int j = inputList.indexOf("/from");
-                    int k = inputList.indexOf("/to");
-                    if (j == -1 || k == -1) {
-                        // todo: handle error
+                } else if (inputList.get(0).equals("mark")) {
+                    int i = -1;
+                    try {
+                        i = Integer.parseInt(inputList.get(1)) - 1;
+                    } catch (NumberFormatException | IndexOutOfBoundsException e) {
                     }
-                    String description = String.join(" ", inputList.subList(1, j));
-                    String from = String.join(" ", inputList.subList(j+1, k));
-                    String to = String.join(" ", inputList.subList(k+1, inputList.size()));
-                    task = new Event(description, from, to);
+
+                    if (inputList.size() != 2 || i < 0 || i >= taskList.size()) {
+                        throw new ConvoBotException("Invalid input. You must specify a valid index to mark as done.");
+                    }
+                    taskList.get(i).markAsDone();
+                    System.out.println(leftPadding + " " + "Nice! I've marked this task as done:");
+                    System.out.println(leftPadding + " " + taskList.get(i).toString());
+                } else if (inputList.get(0).equals("unmark")) {
+                    int i = -1;
+                    try {
+                        i = Integer.parseInt(inputList.get(1)) - 1;
+                    } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    }
+                    
+                    if (inputList.size() != 2 || i < 0 || i >= taskList.size()) {
+                        throw new ConvoBotException("Invalid input. You must specify a valid index to mark as done.");
+                    }
+                    taskList.get(i).markAsNotDone();
+                    System.out.println(leftPadding + " " + "OK, I've marked this task as not done yet:");
+                    System.out.println(leftPadding + " " + taskList.get(i).toString());
                 } else {
-                    task = new Task(userInput);
+                    Task task;
+                    if (inputList.get(0).equals("todo")) {
+                        if (userInput.length() == 4) {
+                            throw new ConvoBotException("Invalid input. The description of a todo cannot be empty.");
+                        }
+                        task = new ToDo(userInput.substring(5));
+                    } else if (inputList.get(0).equals("deadline")) {
+                        int j = inputList.indexOf("/by");
+                        if (j == 1) {
+                            throw new ConvoBotException("Invalid input. The description of a deadline cannot be empty.");
+                        }
+                        if (j == -1 || j == inputList.size() - 1) {
+                            throw new ConvoBotException("Invalid input. Start date missing.");
+                        }
+                        String description = String.join(" ", inputList.subList(1, j));
+                        String by = String.join(" ", inputList.subList(j+1, inputList.size()));
+                        task = new Deadline(description, by);
+                    } else if (inputList.get(0).equals("event")) {
+                        int j = inputList.indexOf("/from");
+                        int k = inputList.indexOf("/to");
+                        if (j == 1 || k == 1) {
+                            throw new ConvoBotException("Invalid input. The description of an event cannot be empty.");
+                        }
+                        if (j == -1 || j == inputList.size() - 1 || inputList.get(j+1).equals("/to")) {
+                            throw new ConvoBotException("Invalid input. Start date missing.");
+                        }
+                        if (k == -1 || k == inputList.size() - 1) {
+                            throw new ConvoBotException("Invalid input. End date missing.");
+                        }
+                        String description = String.join(" ", inputList.subList(1, j));
+                        String from = String.join(" ", inputList.subList(j+1, k));
+                        String to = String.join(" ", inputList.subList(k+1, inputList.size()));
+                        task = new Event(description, from, to);
+                    } else {
+                        throw new ConvoBotException("Invalid input. Input must start with list, mark, unmark, todo, deadline or event.");
+                    }
+                    taskList.add(task);
+                    System.out.println(leftPadding + " " + "Got it. I've added this task:");
+                    System.out.println(leftPadding + "   " + task.toString());
+                    System.out.println(leftPadding + " Now you have " + Integer.toString(taskList.size()) + " tasks in the list.");
                 }
-                taskList.add(task);
-                System.out.println(leftPadding + " " + "Got it. I've added this task:");
-                System.out.println(leftPadding + "   " + task.toString());
-                System.out.println(leftPadding + " Now you have " + Integer.toString(taskList.size()) + " tasks in the list.");
+            } catch (ConvoBotException e) {
+                System.out.println(leftPadding + " " + e.toString());
             }
             printHorizontalLine(true);
             userInput = scanner.nextLine();
---
+++ src/main/java/ConvoBotException.java
@@ -0,0 +1,13 @@
+public class ConvoBotException extends Exception {
+    public ConvoBotException() {
+        super();
+    }
+
+    public ConvoBotException(String message) {
+        super(message);
+    }
+
+    public ConvoBotException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
