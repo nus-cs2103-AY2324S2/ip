96500d4f2f122482c2ff8571913ebb0733a6e96c
Theodore Koo <e0958222@u.nus.edu>
2024-01-22 21:02:20 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Riz.java
@@ -0,0 +1,9 @@
+public class Riz {
+    public static void main(String[] args) {
+        String greetings = "Hello... I'm Riz...\n"
+        + "What can I help you with today?";
+        System.out.println(greetings);
+        String exit = "Goodbye... Hope to see you again soon...";
+        System.out.println(exit);
+    }
+}
