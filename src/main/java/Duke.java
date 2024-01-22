647e118db61561c482c0c0b9b540972ff57d554f
tsulim <limxiujia18@gmail.com>
2024-01-22 14:31:49 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     public static final String DIVIDER = "────────────────────────────────────────────────────────────";
     public static final String GREETING = "Hello! I'm Seiki\nHow may I assist you today?";
@@ -17,11 +21,27 @@ public class Duke {
     }
 
     public static void end() {
+        System.out.println(DIVIDER);
         System.out.println(FAREWELL);
         System.out.println(DIVIDER);
     }
-    public static void main(String[] args) {
+    public static void echo(String input) {
+        System.out.println(DIVIDER);
+        System.out.println(input);
+        System.out.println(DIVIDER);
+    }
+    public static void main(String[] args) throws IOException {
         start();
-        end();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String input = br.readLine();
+            if (input.toLowerCase().equals("bye")) {
+                end();
+                break;
+            } else {
+                echo(input);
+            }
+        }
     }
 }
