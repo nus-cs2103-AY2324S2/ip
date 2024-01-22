3ea68dfb7c0dd76ca631d3ae26776be15e360dad
redcolorbicycle <liuzhengyang10@gmail.com>
2024-01-22 20:54:37 +0800

    Handle basic errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,12 +46,15 @@ public class Duke {
                     System.out.println("[sad quacking] Can I have my bread now?\n");
                     break label;
                 case "list":
+                    if (tasks.isEmpty()) {
+                        System.out.println("You're a lazy bastard. Go get something to do.");
+                        break;
+                    }
                     int i = 1;
                     for (Task a : tasks) {
                         System.out.println(i + ". " + a);
                         i++;
                     }
-                    System.out.println("\n");
                     break;
             }
 
@@ -72,30 +75,52 @@ public class Duke {
                         System.out.println("Task added! You now have " + tasks.size() +" tasks to attend to.");
                         break;
                     case "event":
+                        if (temp.length == 1) {
+                            System.out.println("Event cannot be blank");
+                            break;
+                        }
                         String eventname = "";
                         for (int a = 1; a < temp.length; a++) {
                             eventname = eventname.concat(temp[a]);
                             eventname = eventname.concat(" ");
                         }
-                        Task ne = new Event(eventname, inputs[1], inputs[2]);
-                        tasks.add(ne);
-                        System.out.println("Task added! You now have " + tasks.size() +" tasks to attend to.");
-                        break;
+                        try {
+                            Task ne = new Event(eventname, inputs[1], inputs[2]);
+                            tasks.add(ne);
+                            System.out.println("Task added! You now have " + tasks.size() + " tasks to attend to.");
+                            break;
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            System.out.println("Please enter an event with the format event eventname /start time /finish time!");
+                            break;
+                        }
                     case "deadline":
                         String deadlinename = "";
+                        if (temp.length == 1) {
+                            System.out.println("Deadline cannot be blank");
+                            break;
+                        }
                         for (int a = 1; a < temp.length; a++) {
                             deadlinename = deadlinename.concat(temp[a]);
                             deadlinename = deadlinename.concat(" ");
                         }
-                        Task nd = new Deadline(deadlinename, inputs[1]);
-                        tasks.add(nd);
-                        System.out.println("Task added! You now have " + tasks.size() +" tasks to attend to.");
-                        break;
+                        try {
+                            String deadline = inputs[1];
+                            if (!deadline.startsWith("by ")) {
+                                System.out.println("Please enter a deadline with the format deadline deadlinename /by datetime!");
+                                continue;
+                            }
+                            Task nd = new Deadline(deadlinename, deadline);
+                            tasks.add(nd);
+                            System.out.println("Task added! You now have " + tasks.size() +" tasks to attend to.");
+                            break;
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            System.out.println("Please enter a deadline with the format /by datetime!");
+                        }
 
                 }
 
             } else if (!userInput.toLowerCase().equals("bye") && !userInput.toLowerCase().equals("list")) {
-                System.out.println("[quack] I don't understand that command. If you would like to add a task to the Duckalendar,\n" +
+                System.out.println("[quack] I don't understand that command. If you would like to add a task to the Duckalendar," +
                         " please specify with the task type - \"todo\", \"deadline\" or \"event\" followed by a space in front of the task" +
                         " name.\n For deadlines, please add a /by followed by the deadline (eg. /by Monday) . For events, please add a / followed by the start time, then another / followed by the end time.\n" +
                         " If you would like me to list the things you are procrastinating, please enter the word \"list\"." +
