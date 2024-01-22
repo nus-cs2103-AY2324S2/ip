d2851a31efa6a862e948030b06be105cff24b802
AidenLYT <ytstirlingite@hotmail.com>
2024-01-23 02:05:59 +0800

    Updated ChatBot to fir packages

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -1,3 +1,11 @@
+import Actions.Action;
+
+import Exceptions.DukeException;
+import Executes.DeleteTask;
+import Executes.InsertTask;
+import Executes.ListTask;
+import Executes.MarkTask;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,29 +29,18 @@ public class Taylor {
                 String[] act = input.split(" ", 2);
                 String action = act[0];
 
-                /**
-                 * Switch between actions
-                 */
                 switch (action) {
                     case "bye":
                         break label;
 
                     case "list":
-                        System.out.println("Here are the tasks in your list:");
-                        int pos = 1;
-                        if (listing.isEmpty()) {
-                            System.out.println("List is empty.");
-                        } else {
-                            for (Action acting : listing) {
-                                System.out.println(pos++ + ". " + acting);
-                            }
-                        }
+                        ListTask.exec(listing);
 
                         break;
                     case "mark":
                     case "unmark":
                         try {
-                            markTask(input, listing);
+                            MarkTask.exec(input, listing);
                         } catch (DukeException err) {
                             System.out.println("Error: " + err.getMessage());
                         }
@@ -53,14 +50,14 @@ public class Taylor {
                     case "deadline":
                     case "event":
                         try {
-                            insertTask(input, listing);
+                            InsertTask.exec(input, listing);
                         } catch (DukeException err) {
                             System.out.println("Error: " + err.getMessage());
                         }
                         break;
                     case "delete":
                         try {
-                            deleteTask(input, listing);
+                            DeleteTask.exec(input, listing);
                         } catch (DukeException err) {
                             System.out.println("Error: " + err.getMessage());
                         }
@@ -74,165 +71,5 @@ public class Taylor {
         }
         System.out.println("Bye. Hope to see you again soon!");
         type.close();
-    }
-
-    /**
-     * Mark or unmark a task
-     * @param input
-     * @param actionList
-     * @throws DukeException
-     */
-    public static void markTask(String input, List<Action> actionList) throws DukeException {
-        String[] markWhat = input.split(" ");
-        String what = markWhat[0];
-
-        try {
-            int num = Integer.parseInt(markWhat[1]) - 1;
-
-            if (num < 0 || num >= actionList.size()) {
-                throw new DukeException("Invalid task number");
-            }
-
-            if (what.equals("mark")) {
-                actionList.get(num).markIt();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(actionList.get(num));
-            } else if (what.equals("unmark")) {
-                actionList.get(num).unMark();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(actionList.get(num));
-            } else {
-                throw new DukeException("Invalid command -  Only use mark/unmark");
-            }
-        } catch (ArrayIndexOutOfBoundsException err) {
-            throw new DukeException("Please insert task number!");
-        }
-    }
-
-    /**
-     * Add a task: todo, deadline, event
-     * @param input
-     * @param actionList
-     * @throws DukeException
-     */
-    public static void insertTask(String input, List<Action> actionList) throws DukeException{
-        String[] parts = input.split(" ", 2);
-        if (parts.length < 2 || parts[1].trim().isBlank()) {
-            throw new DukeException("The description of the task is empty.");
-        }
-        String content = parts[1];
-
-        switch (parts[0]) {
-            case "todo":
-                todoTask(content, actionList);
-                break;
-            case "deadline":
-                deadlineTask(content, actionList);
-                break;
-            case "event":
-                eventTask(content, actionList);
-                break;
-        }
-    }
-
-    /**
-     * Add todo task
-     * @param content
-     * @param actionList
-     */
-    public static void todoTask(String content, List<Action> actionList) {
-        System.out.println("Got it. I've added this task:");
-        Todo task = new Todo(content);
-        actionList.add(task);
-        System.out.println(task);
-        System.out.println("Now you have " + actionList.size() + " tasks in the list.");
-    }
-
-    /**
-     * Add deadline Task
-     * @param content
-     * @param actionList
-     * @throws DukeException
-     */
-    public static void deadlineTask(String content, List<Action> actionList) throws DukeException {
-        try {
-            String[] splitter = content.split("/by");
-
-            if (splitter.length < 2 || splitter[0].trim().isBlank() || splitter[1].trim().isBlank()) {
-                throw new DukeException("Invalid format. Please type in the following format: " +
-                        "deadline <action> /by <time>");
-            }
-
-            System.out.println("Got it. I've added this task:");
-            Deadline dl = new Deadline(splitter[0], splitter[1]);
-            actionList.add(dl);
-            System.out.println(dl);
-            System.out.println("Now you have " + actionList.size() + " tasks in the list.");
-
-        } catch (ArrayIndexOutOfBoundsException err) {
-            throw new DukeException("Invalid format. Please type in the following format: deadline <action> /by <time>");
-        }
-    }
-
-    /**
-     * Add event task
-     * @param content
-     * @param actionList
-     * @throws DukeException
-     */
-    public static void eventTask(String content, List<Action> actionList) throws DukeException{
-        try {
-            String[] splitter = content.split("/from");
-
-            if (splitter.length < 2 || splitter[0].trim().isBlank() || splitter[1].trim().isBlank()) {
-                throw new DukeException("Invalid format. Please type in the following format: " +
-                        "event <action> /from <time> /to <time>");
-            }
-
-            try {
-                String[] splitter1 = splitter[1].split("/to");
-
-                if (splitter1.length < 2 || splitter1[0].trim().isBlank() || splitter1[1].trim().isBlank()) {
-                    throw new DukeException("Invalid format. Please type in the following format: " +
-                            "event <action> /from <time> /to <time>");
-                }
-                Event eve = new Event(splitter[0], splitter1[0], splitter1[1]);
-                actionList.add(eve);
-
-                System.out.println(eve);
-                System.out.println("Now you have " + actionList.size() + " tasks in the list.");
-            } catch (ArrayIndexOutOfBoundsException err) {
-                throw new DukeException("Invalid format. Please type in the following format: " +
-                        "event <action> /from <time> /to <time>");
-            }
-        } catch (ArrayIndexOutOfBoundsException err) {
-            throw new DukeException("Invalid format. Please type in the following format: " +
-                    "event <action> /from <time> /to <time>");
-        }
-    }
-
-    /**
-     * Delete a task
-     * @param input
-     * @param actionList
-     * @throws DukeException
-     */
-    public static void deleteTask(String input, List<Action> actionList) throws DukeException {
-        try {
-            String[] parts = input.split(" ", 2);
-            int pos = Integer.parseInt(parts[1]);
-
-            if (pos > actionList.size() || pos <= 0) {
-                throw new DukeException("Invalid task number");
-            }
-
-            System.out.println("Noted. I've removed this tasks:");
-            System.out.println(actionList.get(pos - 1));
-            actionList.remove(pos - 1);
-            System.out.println("Now you have " + actionList.size() + " tasks in the list.");
-
-        } catch (ArrayIndexOutOfBoundsException err) {
-            throw new DukeException("Please include index of task to be removed");
-        }
     }
 }
