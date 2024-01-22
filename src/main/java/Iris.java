9c83edad4560831890fd9ba96312481bbdff8cb4
jxunze <jinxunzejxz@gmail.com>
2024-01-22 14:54:57 +0800

    Add list functionality

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Iris {
     private BufferedReader reader;
     private final String INDENT = "    ";
     private final String CHATDELIMITER = INDENT + "_____________________";
+    private List<String> cachedMessages;
     public Iris() {
         this.reader = new BufferedReader(
                 new InputStreamReader(System.in));
+        this.cachedMessages = new ArrayList<>();
     }
 
     public void start(){
@@ -24,6 +28,29 @@ public class Iris {
             }
             this.print(line);
         }
+    }
+
+    public void add() throws IOException {
+        while (true) {
+            String line = this.reader.readLine();
+            switch (line) {
+                case "bye": return;
+                case "list":
+                    this.print(this.listMessages());
+                    break;
+                default:
+                    this.cachedMessages.add(line);
+                    this.print("Added: " + line);
+            }
+        }
+    }
+
+    private String listMessages() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < this.cachedMessages.size(); i++) {
+            str.append((i+1) + ". " + this.cachedMessages.get(i) + "\n");
+        }
+        return str.toString();
     }
 
     public void exit() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Iris iris = new Iris();
         iris.start();;
-        iris.echo();
+        iris.add();
         iris.exit();
     }
 }
