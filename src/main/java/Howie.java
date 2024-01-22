c6fa6e23bee24d6f262af2c2240a58901c812d94
smolegz <zhuohui.koo@gmail.com>
2024-01-23 00:05:32 +0800

    Completed Level-5 and Level-6

--- src/main/java/Howie.java
+++ src/main/java/Howie.java
@@ -11,8 +11,8 @@ public class Howie {
 
     public static void intro() {
         printVLine();
-        System.out.println("Hello! I'm Howie");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Howie! My hobby is to keep track of TASKS. Let me help you!");
+        System.out.println("So, what can I do for you?");
         printVLine();
     }
 
@@ -20,6 +20,7 @@ public class Howie {
         printVLine();
         System.out.println("Bye! I'll see you when I see you :)");
         printVLine();
+        System.exit(0);
     }
 
     public static void addToList(Task t) {
@@ -32,11 +33,51 @@ public class Howie {
 
     public static void printAllTask(List<Task> tasks) {
         printVLine();
-        System.out.println("Here are the list of your tasks:");
+        if (tasks.size() == 0) {
+            System.out.println("Your list is now EMPTY! Time for you to have a break!");
+            printVLine();
+            return;
+        }
+        System.out.println("You have " + tasks.size() + " left. There are:");
         for (int i=1; i<=tasks.size(); i++) {
             Task t = tasks.get(i-1);
             System.out.println(i +"." + t);
         }
+        printVLine();
+    }
+    public static void delete(List<Task> tasks, int i) {
+        if (i > tasks.size() || i <= 0) {
+            printVLine();
+            System.out.println("Hmm...I can't delete something that isn't there :O");
+            printVLine();
+        } else {
+            Task t = tasks.remove(i-1);
+            printVLine();
+            System.out.println("Okay! The following task has been removed:\n" + t);
+            printAllTask(tasks);
+        }
+    }
+    public static void emptyTaskMessage() {
+        printVLine();
+        System.out.println("Hey! You've just entered an unnamed task... Try to give a description/name of your task :)");
+        printVLine();
+    }
+
+    public static void invalidFormat() {
+        printVLine();
+        System.out.println("I see you've entered an invalid format. Type 'help' if you're unsure :)");
+        printVLine();
+    }
+
+    public static void helpMessage() {
+        printVLine();
+        System.out.println("Below is a list of available commands for me:\n" +
+                "   todo [task_name]\n" +
+                "   event [task_name] /from [date] /to [date]\n" +
+                "   deadline [task_name] /by [date]\n" +
+                "   list - to see all tasks\n" +
+                "   mark/unmark [index]\n" +
+                "   bye - to exit the programme");
         printVLine();
     }
     public static void main(String[] args) throws Exception {
@@ -59,11 +100,11 @@ public class Howie {
                     try {
                         Task toMark = tasks.get(Integer.parseInt(input[1])-1);
                         printVLine();
-                        System.out.println("Acknowledged!\n" + toMark.setDone());
+                        System.out.println("Acknowledged!!\n" + toMark.setDone());
                         printVLine();
                     } catch (IndexOutOfBoundsException e) {
                         printVLine();
-                        System.out.println("Opps! I can't seem to find the task.");
+                        System.out.println("Oppss...I can't seem to find the task you're looking for. Type 'list' to see the the tasks that you have!");
                         printVLine();
                     }
                     break;
@@ -75,7 +116,7 @@ public class Howie {
                         printVLine();
                     } catch (IndexOutOfBoundsException e) {
                         printVLine();
-                        System.out.println("Cannot find task");
+                        System.out.println("Oppss...I can't seem to find the task you're looking for. Type 'list' to see the the tasks that you have!");
                         printVLine();
                     }
                     break;
@@ -93,8 +134,14 @@ public class Howie {
                         current.append(input[i]).append(" ");
                     }
                     by = current;
-                    Deadline dlTask = new Deadline(name.toString(), by.toString().trim());
-                    addToList(dlTask);
+                    if (name.length() == 0) {
+                        emptyTaskMessage();
+                    } else if (name.compareTo(by) == 0) {
+                        invalidFormat();
+                    } else {
+                        Deadline dlTask = new Deadline(name.toString(), by.toString().trim());
+                        addToList(dlTask);
+                    }
                     break;
                 case "event":
                     name =  new StringBuilder();
@@ -114,21 +161,39 @@ public class Howie {
                         }
                         current.append(input[i]).append(" ");
                     }
-                    Event eventTask = new Event(name.toString(), from.toString(), to.toString().trim());
-                    addToList(eventTask);
+                    if (name.length() == 0) {
+                        emptyTaskMessage();
+                    } else if (from.length() == 0 || to.length() == 0) {
+                        invalidFormat();
+                    } else {
+                        Event eventTask = new Event(name.toString(), from.toString(), to.toString().trim());
+                        addToList(eventTask);
+                    }
                     break;
                 case "todo":
                     name = new StringBuilder();
                     for (int j=1; j<input.length; j++) {
                         name.append(input[j]).append(" ");
                     }
-                    Task todo = new Task(name.toString().trim());
-                    addToList(todo);
+                    if (name.length() == 0) {
+                        emptyTaskMessage();
+                    } else {
+                        Task todo = new Task(name.toString().trim());
+                        addToList(todo);
+                    }
                     break;
+                case "delete":
+                    delete(tasks, Integer.parseInt(input[1]));
+                    break;
+                case "help":
+                    helpMessage();
+                    break;
+                default:
+                    printVLine();
+                    System.out.println("Aww! I do not recognise this command. Type 'help' if you're unsure :)");
+                    printVLine();
             }
-            if (s1.equals("bye")) {
-                System.exit(0);
-            }
+
         }
     }
 }
