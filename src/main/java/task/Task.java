8cbc4e648c2df2c0493339940756718ee12bb974
taeewonnn <htw10192000@gmail.com>
2024-01-22 12:57:18 +0800

    Task class with a taskList collection to hold the task objects, with necessary functionalities present. Duke class able to run with the parsing class and create a list of tasks to do

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,2 +1,22 @@
-package task;public class Task {
+package task;
+
+public class Task {
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
 }
--- src/main/java/task/taskList.java
+++ src/main/java/task/taskList.java
@@ -1,2 +1,24 @@
-package task;public class taskList {
+package task;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class taskList {
+    private final List<Task> listOfTasks;
+
+    public taskList() {
+        listOfTasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        listOfTasks.add(task);
+        System.out.println("added: " + task);
+    }
+
+    public void listDownTask() {
+        int size = listOfTasks.size();
+        for(int i = 1; i <= size; i++) {
+            System.out.println(i +": " + listOfTasks.get(i - 1));
+        }
+    }
 }
