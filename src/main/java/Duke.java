0a61d8dfac4461c78f757c8932ac3a0f8affd7da
0liverkong <e1089628@u.nus.edu>
2024-01-22 20:50:25 +0800

    Completed Week 2 Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        String logo = " ____              _    \n"
+                + "|  _ \\  ___   ___ | | __      ╱|、\n"
+                + "| | | |/ _ \\ / _ \\| |/ /    (˚ˎ 。7  \n"
+                + "| |_| | |_| | |_| |   <      |、˜〵 \n"
+                + "|____/ \\___/ \\___/|_|\\_\\     じしˍ,)ノ\n";
+        System.out.println(line);
+        System.out.println("Hello from Dook! :D meow\n" + logo);
+        System.out.println("What can I do for you? uwu");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
