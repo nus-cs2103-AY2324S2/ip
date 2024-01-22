678f3ca273c8863095278dc78e9523787cb96b2e
redcolorbicycle <liuzhengyang10@gmail.com>
2024-01-22 15:43:35 +0800

    Level 3 Mark/Unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,35 +4,52 @@ public class Duke {
 
         //name and introduction
         String name = "Bearducky";
-        System.out.println("Quack! My name is " + name + ". I would be glad to help you in exchange for some bread.");
+        System.out.println("Quack! My name is " + name + ". I would like to earn some bread. How may I " +
+                "help?");
 
         //Set up scanner, store user's inputs, structure to store tasks
         Scanner scanner = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>();
 
+        label:
         while (true) {
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
-                System.out.println("[sad quacking]\n");
-                break;
-            } else if (userInput.equals("list")) {
-                for (Task a : tasks) {
-                    System.out.println("[" + a.getStatusIcon() + "] " + a.getName());
+            int x = tasks.size();
+
+            //marking or not
+            if (userInput.toLowerCase().startsWith("mark ") || userInput.toLowerCase().startsWith("unmark ")) {
+                String[] inputs = userInput.split(" ");
+                try {
+                    int num = Integer.parseInt(inputs[1]);
+                    if (inputs[0].equals("mark")) {
+                        tasks.get(num - 1).Mark();
+                    } else {
+                        tasks.get(num - 1).unMark();
+                    }
+                    System.out.println("[busy quacking]");
+                } catch (NumberFormatException e) {
+                    System.out.println("[angry quacking] I can only mark numbers!");
+                } catch (IndexOutOfBoundsException a) {
+                    System.out.println("[exasperated quacking] You're not that busy - numbers from 1 to " + x + " only, please.");
                 }
-                System.out.println("\n");
-            } else if (userInput.equals("mark")) {
-                System.out.println("Please provide the task number to mark\n");
-                int markTask = scanner.nextInt();
-                scanner.nextLine();
-                tasks.get(markTask - 1).Mark();
-            } else if (userInput.equals("unmark")) {
-                System.out.println("Please provide the task number to unmark\n");
-                int unmarkTask = scanner.nextInt();
-                scanner.nextLine();
-                tasks.get(unmarkTask - 1).unMark();
-            } else {
-                System.out.println("added: " +userInput + "\n");
-                tasks.add(new Task(userInput));
+                continue;
+            }
+
+            //other hard commands
+            switch (userInput.toLowerCase()) {
+                case "bye":
+                    System.out.println("[sad quacking] Can I have my bread now?\n");
+                    break label;
+                case "list":
+                    for (Task a : tasks) {
+                        System.out.println(a);
+                    }
+                    System.out.println("\n");
+                    break;
+                default:
+                    System.out.println("added: " + userInput + "\n");
+                    tasks.add(new Task(userInput));
+                    break;
             }
         }
     }
