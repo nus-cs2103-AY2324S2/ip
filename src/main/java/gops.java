4c7b22467f0f471b565d736d89875ee14071ac7d
bgopi23 <baskargopinath01@gmail.com>
2024-01-22 22:19:43 +0800

    Update gops.java

--- src/main/java/gops.java
+++ src/main/java/gops.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class gops {
     public static void main(String[] args) {
+        Scanner inputTaker = new Scanner(System.in);
         System.out.println("Hello! I'm gops");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String userReply = inputTaker.nextLine();
+        while (userReply != null && !userReply.equals("bye")) {
+            System.out.println(userReply);
+            userReply = inputTaker.nextLine();
+        }
+        if (userReply.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
     }
 }
