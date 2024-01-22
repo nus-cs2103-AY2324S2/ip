7f6a4f41deda9f98b1d48f0cc7a815df79658854
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-22 21:03:37 +0800

    Added content for Level-0

---
+++ src/main/java/Kervyn.java
@@ -0,0 +1,8 @@
+public class Kervyn {
+    public static void main(String[] args) {
+        String chatbotName = "Kervyn";
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
