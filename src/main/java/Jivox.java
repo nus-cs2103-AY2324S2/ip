7733b217df2e89360c2052fa343fd4c67d8e6a51
dedsecrattle <itsprabxxx@gmail.com>
2024-01-23 02:11:48 +0800

    Level-1 Add Echo

--- src/main/java/Jivox.java
+++ src/main/java/Jivox.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jivox {
     public void greet(){
         addDivider();
@@ -16,5 +18,21 @@ public class Jivox {
         System.out.println("============================================================");
     }
 
+    public void run(){
+        boolean isRunning = true;
+        Scanner sc = new Scanner(System.in);
+        do {
+            String input = sc.nextLine();
+            if(input.equals("bye")){
+                isRunning = false;
+            }
+            else{
+                addDivider();
+                System.out.println(input);
+                addDivider();
+            }
+        } while (isRunning);
+
+    }
 
 }
