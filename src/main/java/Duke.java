6a11adf6f62c9e06f5ec8c0d306c311b754677e2
shawnnlimm <shawnlim2525@gmail.com>
2024-01-22 12:21:26 +0800

    feat: Add echo command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("____________________");
         System.out.println("Hello! I'm Waffles");
         System.out.println("What can I do for you?");
         System.out.println("____________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________");
+                break;
+            } else {
+                echoCommand(command);
+            }
+        }
+
+        sc.close();
+
     }
+
+    private static void echoCommand(String command) {
+        System.out.println(command);
+    }
+
 }
