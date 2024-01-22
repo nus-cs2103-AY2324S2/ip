6f442e28c4be1d422995bb0fbf5cb2cc26326471
Sherwyn Ng <>
2024-01-22 17:10:36 +0800

    added "Level 3. Mark as Done"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.io.BufferedReader;
-import java.sql.Array;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Duke {
@@ -14,20 +11,41 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         String input = br.readLine();
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         while (!input.equals("bye")) {
-            switch (input) {
+            String[] separatedInput = input.split(" ");
+            switch (separatedInput[0]) {
                 case "list":
                     System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < lst.size(); i++) {
-                        System.out.println(i + 1 + ". " + lst.get(i));
+                        System.out.println(i + 1 + "." + "[" + lst.get(i).getStatusIcon()  + "] " + lst.get(i).getDescription());
                     }
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "mark":
+                    int indexToMark = Integer.parseInt(separatedInput[1]);
+                    Task markTask = lst.get(indexToMark - 1);
+                    markTask.markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[X] " + markTask.getDescription());
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "unmark":
+                    int indexToUnmark = Integer.parseInt(separatedInput[1]);
+                    Task unmarkTask = lst.get(indexToUnmark - 1);
+                    unmarkTask.markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[ ] " + unmarkTask.getDescription());
                     System.out.println("____________________________________________________________");
                     break;
                 default:
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + input);
-                    lst.add(input);
+                    Task toAddTask = new Task(input);
+                    lst.add(toAddTask);
                     System.out.println("____________________________________________________________");
             }
             input = br.readLine();
