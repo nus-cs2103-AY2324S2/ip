cdd9e50349da794606d98e74efbaa518344c4f05
Drustan Yeo <drustanyjt@gmail.com>
2024-01-22 11:15:43 +0800

    changed name to fluffy

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String name = "Fluffy";
+
+        // print out a solid line
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+
+        // print out a solid line
+        System.out.println("____________________________________________________________");
+
+        // print out a goodbye message
+        System.out.println("Bye. Hope to see you again soon!");
+        // print out a solid line
+        System.out.println("____________________________________________________________");
     }
 }
