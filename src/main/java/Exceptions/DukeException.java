3db314bad80161023e9ab206405c3f9f491716b8
AidenLYT <ytstirlingite@hotmail.com>
2024-01-23 02:05:29 +0800

    Packaged Exceptions

--- src/main/java/Exceptions/DukeException.java
+++ src/main/java/Exceptions/DukeException.java
@@ -1,8 +1,6 @@
+package Exceptions;
+
 public class DukeException extends RuntimeException{
-    /**
-     * Constructor for Exception class
-     * @param message
-     */
     public DukeException(String message) {
         super(message);
     }
