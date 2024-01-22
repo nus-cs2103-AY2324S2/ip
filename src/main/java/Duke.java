efe005129733bc6cdcefac8d046cb52fdd04b596
yeoshuheng <yeoshuheng@gmail.com>
2024-01-22 19:55:01 +0800

    feat: integrated todo, deadlines and events into duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,17 +15,17 @@ public class Duke {
 
       ArrayList<Task> history = new ArrayList<>();
 
-      while (!current_input.equals("bye")) {
+      while (true) {
         current_input = sc.nextLine();
         String[] current_input_split = current_input.split(" ");
-
+        if (current_input.equals("bye")) {break;}
         if (current_input.equals("list")) {
           System.out.println("_________________________\n" +
             "Get off your ass and starting doing work! " + "\n");
           for (int i = 0; i < history.size(); i++) {
             Task curr = history.get(i);
             System.out.println((i + 1) + "." +
-              curr.getStatusIcon() + " " + curr.getDescription());
+              curr.getFullStatus());
           }
           System.out.println("_________________________\n");
         } else if (current_input_split[0].equals("mark")) {
@@ -33,7 +34,7 @@ public class Duke {
           focus_task.mark();
           String print_out = "_________________________\n" +
             "Marking this done! " + "\n" +
-            focus_task.getStatusIcon() + " " + focus_task.getDescription() + "\n" +
+            focus_task.getFullStatus() + "\n" +
             "_________________________\n";
           System.out.println(print_out);
         } else if (current_input_split[0].equals("unmark")) {
@@ -42,15 +43,38 @@ public class Duke {
           focus_task.unmark();
           String print_out = "_________________________\n" +
             "Ok this is not done... " + "\n" +
-            focus_task.getStatusIcon() + " " + focus_task.getDescription() + "\n" +
+            focus_task.getFullStatus() + "\n" +
             "_________________________\n";
           System.out.println(print_out);
         } else {
-          history.add(new Task(current_input));
-          String to_print = "_________________________\n" +
-            "added: " + current_input + "\n" +
-            "_________________________\n";
-          System.out.println(to_print);
+          boolean valid = true;
+          Task event = null;
+          String[] data = extractDescriptionData(current_input_split);
+          if (data.length == 0) {
+            System.out.println("invalid data");
+          }
+          switch (current_input_split[0]) {
+            case "event":
+              event = new Events(data[0], data[1], data[2]);
+              history.add(event);
+              break;
+            case "todo":
+              event = new ToDos(data[0]);
+              history.add(event);
+              break;
+            case "deadline":
+              event = new Deadlines(data[0], data[1]);
+              history.add(event);
+              break;
+          }
+          if (valid) {
+            String to_print = "_________________________\n" +
+              "added: " + event.getFullStatus() + "\n" +
+              "_________________________\n" +
+              "Now you have " + history.size() + " items in your list!";
+            System.out.println(to_print);
+          }
+          valid = true;
         }
       }
 
@@ -59,5 +83,55 @@ public class Duke {
         "_________________________\n";
       System.out.println(final_print);
       sc.close();
+    }
+
+    public static String[] extractDescriptionData(String[] descriptionArray) {
+      String[] ret = new String[3];
+      String task_desc;
+      switch(descriptionArray[0]) {
+        case "todo":
+          task_desc = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, 1, descriptionArray.length));
+          ret[0] = task_desc;
+          break;
+        case "event":
+          Integer start_idx = null; Integer end_idx = null;
+          for (int i = 0; i < descriptionArray.length; i++) {
+            if (descriptionArray[i].equals("/from")) {
+              start_idx = i;
+            }
+            if (descriptionArray[i].equals("/to")) {
+              end_idx = i;
+            }
+          }
+          if (start_idx.equals(null) || end_idx.equals(null)) {
+            return ret;
+          }
+          task_desc = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, 1, start_idx));
+          String start = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, start_idx + 1, end_idx));
+          String end = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, end_idx + 1, descriptionArray.length));
+          ret[0] = task_desc; ret[1] = start; ret[2] = end;
+          break;
+        case "deadline":
+          start_idx = null;
+          for (int i = 0; i < descriptionArray.length; i++) {
+            if (descriptionArray[i].equals("/by")) {
+              start_idx = i;
+            }
+          }
+          if (start_idx.equals(null)) {
+            return ret;
+          }
+          task_desc = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, 1, start_idx));
+          start = String.join(" ",
+            Arrays.copyOfRange(descriptionArray, start_idx + 1, descriptionArray.length));
+          ret[0] = task_desc; ret[1] = start;
+          break;
+      }
+      return ret;
     }
 }
