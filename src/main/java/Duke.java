b7e948ab8112139e0742d36a7add6d1b5fe66c94
Ella-e <mzyu_bj@163.com>
2024-01-22 11:03:52 +0800

    feat: Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static String EXITKEY = "bye";
+    private static String LISTKEY = "list";
     public static void main(String[] args) {
         String userInput = "";
+        ArrayList<String> inputArr = new ArrayList<>();
         System.out.println("Hello! I'm Plaudern\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while(!userInput.equals(EXITKEY)) {
             userInput = scanner.nextLine();
-            System.out.println(userInput);
+            if (LISTKEY.equals(userInput)) {
+                for (int i = 0; i < inputArr.size(); i++) {
+                    System.out.println((i+1)+". " + inputArr.get(i));
+                }
+            } else {
+                inputArr.add(userInput);
+                System.out.println("added: " + userInput);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
