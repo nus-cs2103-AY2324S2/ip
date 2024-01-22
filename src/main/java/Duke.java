abb2fe7886ab9c25337d64d85dca4b697c8a7312
ANTIFRAGILE\yusia <yusian89@gmail.com>
2024-01-22 23:11:17 +0800

    Modify Duke.java
    Giving new display style to the chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm DevGPT\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("DevGPT:\n\tHello! I'm DevGPT\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("User:");
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("DevGPT:\n\t" + input);
+            }
+        }
+        scanner.close();
+
+        System.out.println("DevGPT:\n\tBye. Hope to see you again soon!");
+
     }
 }
