06bf4ccc9410a2917e5d5027b49ffe0431cfdf3f
ZHANGTIANYAO1 <88520881+ZHANGTIANYAO1@users.noreply.github.com>
2024-01-22 23:15:20 +0800

    add custom exception

--- src/main/java/Saopig.java
+++ src/main/java/Saopig.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class Saopig {
     enum Command {
---
+++ src/main/java/SaopigException.java
@@ -0,0 +1,10 @@
+public class SaopigException {
+    public SaopigException(String message) {
+        super();
+    }
+
+    // Constructor with a message and a cause
+    public SaopigException(String message, Throwable cause) {
+        super();
+    }
+}
