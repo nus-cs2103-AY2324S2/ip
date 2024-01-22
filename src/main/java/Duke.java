c4319254e67add6ecea3e517eed12962972e9904
GERARDJM018 <e0985922@u.nus.edu>
2024-01-23 03:10:40 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Duke {
+
+    private static String name = "GanAnWo";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner inp = new Scanner(System.in);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello! I'm " + name + "\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!");
     }
 }
