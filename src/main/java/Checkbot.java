dc83f5a3bb61fb69ec0adf3f1f5a8d97a45f4310
Zhekai <limzhekai1999@live.com>
2024-01-22 15:29:57 +0800

    add echo function to Checkbot

--- src/main/java/Checkbot.java
+++ src/main/java/Checkbot.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Checkbot {
     public static void main(String[] args) {
-        String txt = "____________________________________________________________\n"
-                + "Hello, I'm Checkbot, your personal assistant.\n"
-                + "How can I help you?\n"
-                + "____________________________________________________________\n"
-                + "Goodbye!\n"
-                + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + txt);
+        String txt = "  ____________________________________________________________\n"
+                + "  Hello, I'm Checkbot, your personal assistant.\n"
+                + "  How can I help you?\n";
+        System.out.println(txt);
+        String input = "";
+        while (!input.equals("bye")) {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            String toPrint = input;
+            if (input.equals("bye")) {
+                toPrint = "Goodbye!";
+            }
+            txt = "  ____________________________________________________________\n"
+                    + "  " + toPrint + "\n"
+                    + "  ____________________________________________________________\n";
+            System.out.println(txt);
+        }
     }
 }
