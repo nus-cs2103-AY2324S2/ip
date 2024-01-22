730302dc06d8ad31eab3a6dd9bb3dbf987f365d7
danielwangcinch <daniel.wang@cinch.sg>
2024-01-22 16:55:17 +0800

    feat: echo

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Tony {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String greeting = "_______________________\n"
                     + "Hello! I'm Tony!\n"
                     + "What can I do for you?\n"
@@ -7,7 +10,18 @@ public class Tony {
         String goodbye = "Bye. Hope to see you again soon!\n"
                 + "_________________________\n";
         System.out.println(greeting);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            line();
+            System.out.println(input);
+            line();
+            input = scanner.nextLine();
+        }
+        line();
         System.out.println(goodbye);
         System.exit(0);
+    }
+    private static void line() {
+        System.out.println("_______________________\n");
     }
 }
