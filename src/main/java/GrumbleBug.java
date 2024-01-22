074a26e86b8fa0d18766aeecb104713cf26d2eba
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-01-22 23:06:35 +0800

    Add list feature

--- src/main/java/GrumbleBug.java
+++ src/main/java/GrumbleBug.java
@@ -1,28 +1,46 @@
+import javax.sound.midi.SysexMessage;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GrumbleBug {
+
+    public static void printList(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
     public static void main(String[] args) {
         String starter = "GrumbleBug:"
                 + "_______________________________________\n"
                 + "Oh hey, adventurer.\n"
                 + "I'm GrumbleBug.\n"
-                + "What's your- you know what, I don't care.\n"
+                + "You wanna list me some things?\n"
                 + "_______________________________________\n"
                 + "\n"
-                + "To respond, type something. To leave, reply with 'bye'.\n";
+                + "Reply to add things to your list.\n"
+                + "To see your list, type 'list'.\n"
+                + "To leave, reply with 'bye'.\n";
         System.out.println(starter);
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> myList = new ArrayList<>();
 
         while (true) {
             String input1 = sc.nextLine();
             if (input1.equals("bye")) {
                 System.exit(0);
-            } else { // GrumbleBug will echo
-                String echo = "GrumbleBug:"
+            } else if (input1.equals("list")) { // show the list!
+                String reply = "GrumbleBug:"
                     + "_______________________________________\n"
-                    + input1
+                    + "You have these things in your list...";
+                System.out.println(reply);
+                GrumbleBug.printList(myList);
+            } else { // add to list
+                myList.add(input1);
+                String reply = "GrumbleBug:"
+                    + "_______________________________________\n"
+                    + "Fine, added your stupid " + input1 + "."
                     + "\n_______________________________________\n";
-                System.out.println(echo);
+                System.out.println(reply);
             }
         }
     }
