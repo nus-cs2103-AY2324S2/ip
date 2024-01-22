a06bf625a2f589d4f42f0020d678bd6ad282f048
ongzhili <ongchili234@gmail.com>
2024-01-22 19:10:26 +0800

    Add, List functionalities added

--- src/main/java/Lamball.java
+++ src/main/java/Lamball.java
@@ -7,16 +7,26 @@ public class Lamball {
             "     What can I do for you?\n" +
             "    ____________________________________________________________\n";
     String goodbyeMessage = "    ____________________________________________________________\n" +
-            "     Goodbye!\n" +
+            "     See you again!\n" +
             "    ____________________________________________________________\n";
 
     String[] tasks;
+    int counter;
     public Lamball() {
         tasks = new String[100];
+        counter = 0;
     }
 
     public void greetingMessage() {
         System.out.println(this.welcomeMessage);
+    }
+
+    public void printList() {
+        System.out.println(indent);
+        for(int i = 0; i < counter; i++) {
+            System.out.println("    " + (i + 1) + ". " + tasks[i] + "\n");
+        }
+        System.out.println(indent);
     }
 
     public boolean parse(String msg) {
@@ -25,8 +35,13 @@ public class Lamball {
             case "bye":
                 System.out.println(goodbyeMessage);
                 return false;
+            case "list":
+                printList();
+                return true;
             default:
-                System.out.println(indent + "    " + msg + "\n" + indent);
+                System.out.println(indent + "    added:" + msg + "\n" + indent);
+                tasks[counter] = msg;
+                counter++;
                 return true;
         }
     }
