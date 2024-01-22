da80f928a34b3731a8c6233efae234dcce8154c3
REGAN\regan <e0957884@u.nus.edu>
2024-01-22 11:32:57 +0800

    Added Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,63 @@
+import java.util.Scanner;
 public class Duke {
-    String name = "Aerial";
+    String name = "XVX-016 Aerial";
+    String command = "";
+    Scanner scanner1;
 
     public Duke() {
     }
 
     public void greeting() {
         horizontalLines();
-        System.out.println("HELLO, Nice to meet you. I am " + this.name);
-        System.out.println("What can I do for you?");
+        indent();
+        System.out.println("HELLO, Nice to meet you. I am " + this.name + "! \uD83E\uDD16");
+        indent();
+        System.out.println("What are we doing today?");
         horizontalLines();
     }
 
     public void bye() {
-        System.out.println("See you next time!");
+        indent();
+        System.out.println("See you next time! ♥( ˆ⌣ ˆԅ)");
         horizontalLines();
+        this.scanner1.close();
     }
 
     public void horizontalLines() {
-        System.out.println("\n____________________________________________________________");
+        System.out.println("\n    ____________________________________________________________");
+    }
+
+    public void echo() {
+        horizontalLines();
+        indent();
+        System.out.println("╭( ๐ _๐)╮");
+        indent();
+        System.out.println("\uD83D\uDDE8️ You said THIS: ");
+        indent();
+        System.out.println(this.command);
+        horizontalLines();
+        input();
+    }
+
+    public void indent() {
+        System.out.print("    ");
+    }
+
+    public void input() {
+        String commandInput = scanner1.nextLine();
+        this.command = commandInput;
+
+        if (this.command.equals("bye")) {
+            bye();
+        } else {
+            echo();
+        }
     }
 
     public static void main(String[] args) {
-        Duke Duke = new Duke();
-        Duke.greeting();
-        Duke.bye();
+        Duke Duke1 = new Duke();
+        Duke1.greeting();
+        Duke1.scanner1 = new Scanner(System.in);
+        Duke1.input();
     }
-    /*
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
-
-     */
 }
