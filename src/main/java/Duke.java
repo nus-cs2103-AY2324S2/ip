8af24765ba7001b1f2795e3d6038766a6c2ec10d
Doye <doyelee0313@gmail.com>
2024-01-22 21:26:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,21 +16,32 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner sc = new Scanner(System.in);
-        String order = "";
+        String order = sc.nextLine();
+        List<String> array = new ArrayList<>();
 
         while (!order.equals("bye")) {
 
-            order = sc.nextLine();
-
-            System.out.println("____________________________________________________________");
-            System.out.println(order);
-            System.out.println("____________________________________________________________");
+            if (order.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for(int i = 0; i < array.size(); i++) {
+                    System.out.println((i +1 ) + ": " + array.get(i));
+                }
+                System.out.println("____________________________________________________________");
+                order = sc.nextLine();
+            } else {
+                array.add(order);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + order);
+                System.out.println("____________________________________________________________");
+                order = sc.nextLine();
+            }
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
+        if (order.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+        }
         sc.close();
     }
 }
