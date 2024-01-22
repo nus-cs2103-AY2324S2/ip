41bcc5d41dbedf6fdfadb35ed8022478c5c792e0
E0735389 <97508964+E0735389@users.noreply.github.com>
2024-01-23 02:35:51 +0700

    Level 0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
+    static final String CHATBOT_NAME = "Echo";
+    static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(DIVIDER);
+        System.out.println("Hello! I'm " + CHATBOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
