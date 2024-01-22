33e3c7707c5891862a845af3a38298bcf3cc2954
kokerinks <limkokliang01@gmail.com>
2024-01-22 16:43:05 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String botName = "KokBot";
@@ -8,9 +9,20 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         welcome(botName);
+        Scanner scanner = new Scanner(System.in);
+        lineBreak();
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            lineBreak();
+            input = scanner.nextLine();
+        }
         farewell();
     }
 
+    public static void lineBreak() {
+        System.out.println("____________________________________________________________\n");
+    }
     public static void welcome(String botName) {
         System.out.println(String.format("""
                 ____________________________________________________________
@@ -19,10 +31,17 @@ public class Duke {
                  """, botName));
     }
 
+    public static void echo(String text) {
+        System.out.println(String.format("""
+                ____________________________________________________________
+                 %s""", text));
+    }
+
     public static void farewell() {
         System.out.println("""
                 ____________________________________________________________
                  Bye. Hope to see you again soon!
+                ____________________________________________________________
                  """);
     }
 }
