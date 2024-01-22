2cd31372542e233988aad2e0d0f57ba4484d9433
Justin <justinlim60@gmail.com>
2024-01-22 17:06:44 +0800

    echoing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String message = "____________________________________________________________\n" +
         "Hello! I'm Jux\n" +
-        "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-       "Bye. Hope to see you again soon!\n" +
+        "What can I do for you?\n";
+        String end = "____________________________________________________________\n" +
+            "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________"
         ;
         System.out.println(message);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("You entered: " + input);
+        }
+        System.out.println(end);
+
+
     }
 }
