4fa78eba58d472818dd900454e5008f97460ce2a
yespiggy <skylarye7@gmail.com>
2024-01-22 10:53:41 +0800

    Rename, Greet, Exit

---
+++ src/main/java/LilyBot.java
@@ -0,0 +1,7 @@
+public class LilyBot {
+    public static void main(String[] args) {
+        String sayHi = "Hello! I'm LilyBot\nWhat can I do for you?\n";
+        String sayBye = "Bye. Hope to see you again soon!\n";
+        System.out.println(sayHi + sayBye);
+    }
+}
