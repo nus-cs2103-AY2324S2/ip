55e6c2514649bfadf8d48e9cbcdf1bee0a61c49c
Zhekai <limzhekai1999@live.com>
2024-01-22 15:12:33 +0800

    renamed Duke to Checkbot

---
+++ src/main/java/Checkbot.java
@@ -0,0 +1,11 @@
+public class Checkbot {
+    public static void main(String[] args) {
+        String txt = "____________________________________________________________\n"
+                + "Hello, I'm Checkbot, your personal assistant.\n"
+                + "How can I help you?\n"
+                + "____________________________________________________________\n"
+                + "Goodbye!\n"
+                + "____________________________________________________________\n";
+        System.out.println("Hello from\n" + txt);
+    }
+}
