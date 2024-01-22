05b25589a501cd2865cc61c1af45e4e684a8ddac
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-01-22 22:32:29 +0800

    Add echo dialogue

--- src/main/java/GrumbleBug.java
+++ src/main/java/GrumbleBug.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class GrumbleBug {
     public static void main(String[] args) {
-        String logo = "_______________________________________\n"
+        String starter = "GrumbleBug:"
+                + "_______________________________________\n"
                 + "Oh hey, adventurer.\n"
                 + "I'm GrumbleBug.\n"
                 + "What's your- you know what, I don't care.\n"
-                + "Go away. Byebye...\n"
-                + "_______________________________________\n";
-        System.out.println(logo);
+                + "_______________________________________\n"
+                + "\n"
+                + "To respond, type something. To leave, reply with 'bye'.\n";
+        System.out.println(starter);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input1 = sc.nextLine();
+            if (input1.equals("bye")) {
+                System.exit(0);
+            } else { // GrumbleBug will echo
+                String echo = "GrumbleBug:"
+                    + "_______________________________________\n"
+                    + input1
+                    + "\n_______________________________________\n";
+                System.out.println(echo);
+            }
+        }
     }
 }
