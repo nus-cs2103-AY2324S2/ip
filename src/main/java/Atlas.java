d68e2e9095f4b42d2ccb8556d8e25135ee4cf29d
zhikaiong2001 <ongzk2001@gmail.com>
2024-01-22 15:15:29 +0800

    Level 1. Echo

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Atlas {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
         final String horizontalLine = "____________________________________________________________";
         greet();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         exit();
 
     }
