556e0b300ac97a68e8b079fa35ed7f07f0c939cd
AndrewOng2066 <e0959899@u.nus.edu>
2024-01-22 18:44:50 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,19 +16,44 @@ public class Duke {
         System.out.println("      What can I do for you?");
         System.out.println("    ____________________________________________________________\n");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> savedText = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String userInput = sc.nextLine().trim();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < savedText.size(); i++) {
-                    System.out.println("      " + (i + 1) + ". " + savedText.get(i));
+                System.out.println("      Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("      " + (i + 1) + ".["+ tasks.get(i).getStatusIcon() +"] " + tasks.get(i).getDescription());
                 }
                 System.out.println("    ____________________________________________________________\n");
+            } else if (userInput.substring(0,5).trim().equals("mark")) {
+                String[] splitInput = userInput.split(" ");
+                int choiceMark = Integer.parseInt(splitInput[1]);
+                if (choiceMark <= tasks.size()) {
+                    tasks.get(choiceMark - 1).markAsDone();
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("      Nice! I've marked this task as done: ");
+                    System.out.println("        " + "[X] " + tasks.get(choiceMark - 1).getDescription());
+                    System.out.println("    ____________________________________________________________\n");
+                } else {
+                    System.out.println("Invalid choice");
+                }
+            } else if (userInput.substring(0,7).trim().equals("unmark")) {
+                String[] splitInput = userInput.split(" ");
+                int choiceMark = Integer.parseInt(splitInput[1]);
+                if (choiceMark <= tasks.size()) {
+                    tasks.get(choiceMark - 1).markAsUndone();
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("      OK, I've marked this task as not done yet: ");
+                    System.out.println("        " + "[ ] " + tasks.get(choiceMark - 1).getDescription());
+                    System.out.println("    ____________________________________________________________\n");
+                } else {
+                    System.out.println("Invalid choice");
+                }
             } else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("      added: " + userInput);
-                savedText.add(userInput);
+                tasks.add(new Task(userInput));
                 System.out.println("    ____________________________________________________________\n");
             }
             userInput = sc.nextLine().trim();
@@ -35,5 +62,32 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
         System.out.println("      Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
+    }
+}
+
+class Task {
+
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
     }
 }
