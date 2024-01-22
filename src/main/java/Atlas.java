8732b5f21497a76e51cdd34ba6bb82c6c07a9f8c
zhikaiong2001 <ongzk2001@gmail.com>
2024-01-22 15:52:37 +0800

    Added Task class and edited Atlas such as to add the ability to mark tasks as done and undone

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Atlas {
     private static final int MAX_TASKS = 100;
     private final String horizontalLine = "____________________________________________________________";
-    private static String[] tasks = new String[MAX_TASKS];
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCounter = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +16,14 @@ public class Atlas {
             }
             else if (input.equals("list")) {
                 listTasks();
+            }
+            else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                markTask(taskNumber);
+            }
+            else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1; // Adjust for array index
+                unmarkTask(taskNumber);
             }
             else {
                 addTask(input);
@@ -35,14 +43,29 @@ public class Atlas {
     }
 
     private static void addTask(String task) {
-        tasks[taskCounter] = task;
+        tasks[taskCounter] = new Task(task);
         taskCounter++;
         System.out.println(" added: " + task);
     }
 
     private static void listTasks() {
         for (int i = 0; i < taskCounter; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
         }
     }
+
+    private static void markTask(int i) {
+        tasks[i].toggle();
+        System.out.println("Nice! I've marked this task as done:");
+        String str = String.format("[%s] %s", tasks[i].getStatusIcon(), tasks[i].getDescription());
+        System.out.println(str);
+    }
+
+    private static void unmarkTask(int i) {
+        tasks[i].toggle();
+        System.out.println("OK, I've marked this task as not done yet");
+        String str = String.format("[%s] %s", tasks[i].getStatusIcon(), tasks[i].getDescription());
+        System.out.println(str);
+    }
+
 }
