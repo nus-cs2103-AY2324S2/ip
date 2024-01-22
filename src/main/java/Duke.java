46b1919e3922b0a0a279d9e42983f4c7244771d2
AndrewOng2066 <e0959899@u.nus.edu>
2024-01-22 12:59:29 +0800

    Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye. Applied indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,21 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm AndrewOng2066");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm AndrewOng2066");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + userInput);
+            System.out.println("    ____________________________________________________________");
             userInput = sc.nextLine();
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
