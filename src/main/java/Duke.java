73d2dc90b47dcbb2297335383549c22dcf37a599
Lin-Shuang-Shuang <e0970548@u.nus.edu>
2024-01-22 19:17:31 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
+
 public class Duke {
 
     private final String name;
-    private ArrayList<String> taskList = new ArrayList<>();
+    private ArrayList<Task> taskList = new ArrayList<>();
     private final String line = "____________________________________________________________\n";
     public Duke(String name) {
         this.name = name;
@@ -19,8 +21,9 @@ public class Duke {
     public String printList(){
         String list = this.line;
         for (int i = 0; i<this.taskList.size(); i++) {
-            String task = Integer.toString(i+1) + ". " + this.taskList.get(i) + "\n";
-            list += task;
+            Task task = this.taskList.get(i);
+            String message = Integer.toString(i+1) + ".[" + task.getStatusIcon() +"] " + task.getDescription() + "\n";
+            list += message;
 
         }
         return list + this.line;
@@ -38,10 +41,16 @@ public class Duke {
 
             } else if (command.equalsIgnoreCase("list")){
                 System.out.println(this.printList());
+            } else if (command.startsWith("mark ")){
+                Integer id = Integer.parseInt(command.substring(5));
+                System.out.println(this.line + this.taskList.get(id-1).markAsDone() + "\n" + this.line);
 
+            } else if (command.startsWith("unmark ")){
+                Integer id = Integer.parseInt(command.substring(7));
+                System.out.println(this.line + this.taskList.get(id-1).markAsDone() + "\n" + this.line);
             }else {
                 System.out.println(this.line + "added: "+ command + "\n"+this.line);
-                taskList.add(command);
+                taskList.add(new Task(command));
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String markAsDone() {
+        this.isDone = !isDone;
+        String msg = this.isDone ? "Nice! I've marked this task as done:\n"
+                : "OK, I've marked this task as not done yet:\n";
+
+        return msg + "[" + this.getStatusIcon() + "] " + this.description;
+    }
+//    public String unmarkAsDone() {
+//        this.isDone = ;
+//        String mark = "OK, I've marked this task as not done yet:\n" + "[" +
+//                this.getStatusIcon() + "] " + this.description;
+//        return mark;
+//    }
+
+}
