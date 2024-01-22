8ee6bf1fc054afa2b1066e9fd86868963aebb9e4
teojunda <teojunda@gmail.com>
2024-01-22 16:47:56 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
 public class Duke {
+    private Scanner scanner = new Scanner(System.in);
+
+    private void sendSystemMessage(String... strs) {
+        for (String s: strs) {
+            System.out.println("\t" + s);
+        }
+    }
     private void greet() {
-        System.out.println(TextTemplate.LINE_BREAK);
-        System.out.println(TextTemplate.GREETING);
-        System.out.println(TextTemplate.LINE_BREAK);
+        this.sendSystemMessage(TextTemplate.LINE_BREAK, TextTemplate.GREETING, TextTemplate.LINE_BREAK);
     }
 
     private void bye() {
-        System.out.println(TextTemplate.EXIT);
-        System.out.println(TextTemplate.LINE_BREAK);
+        this.sendSystemMessage(TextTemplate.EXIT, TextTemplate.LINE_BREAK);
     }
+
     public void run() {
         this.greet();
+        while (true) {
+            String input = scanner.nextLine();
+            this.sendSystemMessage(TextTemplate.LINE_BREAK);
+            if (input.equals("bye")) {
+                break;
+            } else {
+                this.sendSystemMessage(input, TextTemplate.LINE_BREAK);
+            }
+        }
         this.bye();
     }
 
