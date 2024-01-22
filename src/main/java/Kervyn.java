d5582bc4f72fbeea156679b7e909327a3a0a1fc4
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-22 21:16:11 +0800

    Added content for Level-1

--- src/main/java/Kervyn.java
+++ src/main/java/Kervyn.java
@@ -1,8 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Kervyn {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String chatbotName = "Kervyn";
-        System.out.println("Hello! I'm " + chatbotName);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        System.out.println("\tHello! I'm " + chatbotName);
+        System.out.println("\tWhat can I do for you?");
+
+        String userInput;
+        do {
+            userInput = scanner.nextLine();
+            if (!Objects.equals(userInput, "bye")) {
+                System.out.println("\t" + userInput);
+            }
+
+        } while (!Objects.equals(userInput, "bye"));
+
+        System.out.println("\tBye. Hope to see you again soon!");
     }
 }
