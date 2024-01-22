1a43034f10a5f3c4e75a6487265c778bd8ed7d5a
ANTIFRAGILE\yusia <yusian89@gmail.com>
2024-01-22 23:38:52 +0800

    Modify Duke.java and add Task.java
    User have ability to mark and unmark a task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,23 +4,39 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        List<String> tasksList = new ArrayList<>();
+        List<Task> tasksList = new ArrayList<Task>();
         System.out.println("DevGPT:\n\tHello! I'm DevGPT\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("User:");
             String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.contains("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                System.out.println("DevGPT:");
+            } else if (input.contains("list")) {
+                System.out.println("DevGPT:\n\t Here are the tasks in your list:");
                 for (int i = 0; i < tasksList.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + tasksList.get(i));
+                    Task task = tasksList.get(i);
+                    System.out.println("\t" + (i + 1) + ". " + "[" + task.getStatusIcon() + "]" + " " + task.description);
                 }
+
+            } else if (input.contains("unmark")) {
+                String[] split = input.split(" ");
+                int index = Integer.parseInt(split[1]);
+                Task task = tasksList.get(index - 1);
+                task.markAsUndone();
+                System.out.println("DevGPT:\n\t Got it! I've marked this task as not done yet:");
+                System.out.println("\t" + "[" + task.getStatusIcon() + "]" + " " + task.description);
+            } else if (input.contains("mark")) {
+                String[] split = input.split(" ");
+                int index = Integer.parseInt(split[1]);
+                Task task = tasksList.get(index - 1);
+                task.markAsDone();
+                System.out.println("DevGPT:\n\t Nice! I've marked this task as done:");
+                System.out.println("\t" + "[" + task.getStatusIcon() + "]" + " " + task.description);
             } else {
-                tasksList.add(input);
-                System.out.println("DevGPT:\n\t" + "added:" + input);
+                tasksList.add(new Task(input));
+                System.out.println("DevGPT:\n\t" + "added: " + input);
             }
 
         }
