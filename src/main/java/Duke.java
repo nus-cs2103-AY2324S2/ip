ae7f5f60da2dcd769650ec70990135eed9d78d07
REGAN\regan <e0957884@u.nus.edu>
2024-01-22 10:26:58 +0800

    Added Name, Greeting and Bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,31 @@
 public class Duke {
+    String name = "Aerial";
+
+    public Duke() {
+    }
+
+    public void greeting() {
+        horizontalLines();
+        System.out.println("HELLO, Nice to meet you. I am " + this.name);
+        System.out.println("What can I do for you?");
+        horizontalLines();
+    }
+
+    public void bye() {
+        System.out.println("See you next time!");
+        horizontalLines();
+    }
+
+    public void horizontalLines() {
+        System.out.println("\n____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        Duke Duke = new Duke();
+        Duke.greeting();
+        Duke.bye();
+    }
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,4 +34,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+
+     */
 }
