6543138469cce831a998e13e8e98aacb8edbf437
AidenLYT <ytstirlingite@hotmail.com>
2024-01-22 22:25:11 +0800

    Added comments

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,8 @@
 public class DukeException extends RuntimeException{
+    /**
+     * Constructor for Exception class
+     * @param message
+     */
     public DukeException(String message) {
         super(message);
     }
