583bfb33bd15549841a1023c84b63c0e8005c045
wongkj12 <wongkj12@gmail.com>
2024-01-23 01:04:59 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "MR. WONG";
+        say("Hey man. I'm " + name + "\nWhat can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                say("Bye. Hope to see you again soon!");
+            } else {
+                say(userInput);
+            }
+        }
+        scanner.close();
+    }
+
+    public static void say(String msg) {
         String horizontal = "_________________________________";
         System.out.println(horizontal);
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
-        System.out.println(horizontal);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(msg);
         System.out.println(horizontal);
     }
 }
