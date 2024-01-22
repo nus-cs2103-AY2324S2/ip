328563673d4ba3109553639378e9d003daccfee0
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-01-22 18:09:52 +0800

    Level-3

--- src/main/java/Jayne.java
+++ src/main/java/Jayne.java
@@ -9,16 +9,30 @@ public class Jayne {
         TaskList taskList = new TaskList();
 
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
+            String[] parts = input.split(" ", 2);
 
             //entered bye
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(dash + "\nBye. Hope to see you again soon!\n" + dash);
                 break;
             }
+
             if (input.equalsIgnoreCase("list")) {
                 System.out.println(dash);
                 taskList.display();
+                System.out.println(dash);
+            } else if (parts[0].equalsIgnoreCase("mark") && parts.length > 1){
+                int taskNumber = Integer.parseInt(parts[1]);
+                taskList.markTaskAsDone(taskNumber);
+                System.out.println(dash);
+                System.out.println("Nice! I've marked this task as done:\n  " + taskList.getTask(taskNumber));
+                System.out.println(dash);
+            } else if (parts[0].equalsIgnoreCase("unmark") && parts.length > 1) {
+                int taskNumber = Integer.parseInt(parts[1]);
+                taskList.markTaskAsNotDone(taskNumber);
+                System.out.println(dash);
+                System.out.println("OK, I've marked this task as not done yet:\n  " + taskList.getTask(taskNumber));
                 System.out.println(dash);
             } else {
                 // Add other inputs to the task list
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,19 +2,40 @@ import java.util.ArrayList;
 import java.util.List;
 //test
 public class TaskList {
-    private final List<String> taskArray;
+    private final List<Task> taskArray;
 
     public TaskList() {
         this.taskArray = new ArrayList<>();
     }
 
-    public void add(String item) {
-        taskArray.add(item);
+    public void add(String taskDescription) {
+        taskArray.add(new Task(taskDescription));
+    }
+
+    public Task getTask(int indx) {
+        if (indx >= 0 && indx <= taskArray.size()) {
+            return taskArray.get(indx - 1);
+        }
+        System.out.println("Index invalid");
+        return null;
+    }
+
+    public void markTaskAsDone(int taskNumber) {
+        if (taskNumber >= 1 && taskNumber <= taskArray.size()) {
+            taskArray.get(taskNumber - 1).markAsDone();
+        }
+    }
+
+    public void markTaskAsNotDone(int taskNumber) {
+        if (taskNumber >= 1 && taskNumber <= taskArray.size()) {
+            taskArray.get(taskNumber - 1).markAsNotDone();
+        }
     }
 
     public void display() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskArray.size(); i++) {
-            System.out.println((i + 1) + ". " + taskArray.get(i));
+            System.out.println((i + 1) + ". " + taskArray.get(i).toString());
         }
     }
 }
