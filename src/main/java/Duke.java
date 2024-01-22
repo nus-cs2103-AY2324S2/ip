03c33df27bacbeb84e00e2e26338bca068a21abc
Zen Khoon <e0957872@u.nus.edu>
2024-01-22 16:05:01 +0800

    Add add, list
    User is now able to add input into a list and view it

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private int lastTaskNo = 0;
+
+    private String[] tasks = new String[100];
     private void greet(){
         System.out.println("Hello! I'm Balom.\n" +
                 "What can I do for you today?\n" +
@@ -11,17 +14,36 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private void showTasks(){
+        //Direct user if empty list
+        if(lastTaskNo == 0){
+            System.out.println("Add tasks to list first! Type something other than List/list or Bye/bye.");
+        }
+        for(int i = 0; i< lastTaskNo; i++){
+            System.out.println(tasks[i]);
+            System.out.println();
+        }
+    }
     private void chatting(){
         Scanner scanner = new Scanner(System.in);
         while(true){
+            //get input
             String echo = scanner.nextLine();
-            System.out.println(echo);
-            System.out.println();
 
+            //catch end word or list to display list
             if(echo.equals("bye") || echo.equals("Bye")){
                 break;
+            } else if (echo.equals("list")|| echo.equals("List")) {
+                showTasks();
+            } else {
+                // add to tasks
+                tasks[lastTaskNo] = Integer.toString(lastTaskNo + 1) + "." + echo;
+                lastTaskNo++;
+                System.out.println("added " + echo);
+                System.out.println();
             }
         }
+
         bye();
     }
     public static void main(String[] args) {
