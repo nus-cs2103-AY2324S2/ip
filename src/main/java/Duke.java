2726627ab823e020ddf86914728c1c82667ec2cf
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-01-23 00:41:27 +0800

    Add mark tasks to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println(
@@ -13,15 +13,27 @@ public class Duke {
         while (true) {
             String command = userInput.nextLine();
             if (command.equals("list")) {
+                System.out.println("Here are your tasks in your list");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i+1) + ". " + tasks[i]);
+                    System.out.println((i+1) + "." + tasks[i]);
                 }
             } else if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.exit(0);
+            } else if (command.contains("unmark")) {
+                Task t = tasks[Integer.valueOf(command.split(" ")[1]) - 1];
+                t.unmark();
+                System.out.println("Ok, I've marked this task as not done yet:");
+                System.out.println(t);
+            } else if (command.contains("mark")) {
+                Task t = tasks[Integer.valueOf(command.split(" ")[1]) - 1];
+                t.mark();
+                System.out.println("Nice, I've marked this task as done:");
+                System.out.println(t);
             } else {
                 System.out.println("added: "+command);
-                tasks[taskCount] = command;
+                Task t = new Task(command);
+                tasks[taskCount] = t;
                 taskCount++;
             }
         }
