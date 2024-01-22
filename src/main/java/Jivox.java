db1ea2b5e69e9fdad2bc53557701c8f8632fff75
dedsecrattle <itsprabxxx@gmail.com>
2024-01-23 02:17:50 +0800

    Level-2 Implement Add and List

--- src/main/java/Jivox.java
+++ src/main/java/Jivox.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jivox {
+
+
+    ArrayList<String> list = new ArrayList<>();
     public void greet(){
         addDivider();
         System.out.println(" Hello! I'm Jivox");
@@ -11,6 +15,21 @@ public class Jivox {
     public void exit(){
         addDivider();
         System.out.println(" Bye. Hope to see you again soon!");
+        addDivider();
+    }
+
+    public void add(String input){
+        this.list.add(input);
+        addDivider();
+        System.out.println("added: " + input);
+        addDivider();
+    }
+
+    public void showList(){
+        addDivider();
+        for(int i = 0; i < this.list.size(); i++){
+            System.out.println((i+1) + ". " + this.list.get(i));
+        }
         addDivider();
     }
 
@@ -25,11 +44,11 @@ public class Jivox {
             String input = sc.nextLine();
             if(input.equals("bye")){
                 isRunning = false;
+            } else if(input.equals("list")){
+                this.showList();
             }
             else{
-                addDivider();
-                System.out.println(input);
-                addDivider();
+               this.add(input);
             }
         } while (isRunning);
 
