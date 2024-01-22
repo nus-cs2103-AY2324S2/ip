c5e5234ce22af764aa55e00a113eb9967f9014a2
pjanthony2001 <104015990+pjanthony2001@users.noreply.github.com>
2024-01-22 22:39:44 +0800

    Updated UserInterface to fulfill Level-2 requrements

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -4,7 +4,9 @@ public class UserInterface {
     /*
     This class is for the the user interface.
     */
-
+    int numList = 0;
+    Scanner scan = new Scanner(System.in);
+    String[] list = new String[100];
     String greeting = "Hi! My name is HAL9000";
     String exit = "Bye! See ya soon";
 
@@ -15,23 +17,44 @@ public class UserInterface {
         System.out.println(exit);
     }
 
-    public void echo(String string) {
-        System.out.println(string);
-    }
-
-    public void poll(Scanner scan) {
-        String input = scan.nextLine();
-        if (!input.equals("bye")) {
-            echo(input);
-            poll(scan);
+    public void displayList() {
+        for (int i = 0; i < numList; i++) {
+            System.out.println(String.format("%d. %s", i+1 ,list[i]));
         }
-        return;
+    }
+
+    public void addToList(String input) {
+        list[numList] = input;
+        numList++;
+        System.out.println("added: " + input);
+    }
+
+    public void poll() {
+
+        boolean polling = true;
+
+        while (polling) {
+            String input = scan.nextLine();
+            switch (input) {
+                case "bye":
+                    polling = false;
+                    break;
+                
+                case "list":
+                    displayList();
+                    break;
+            
+                default:
+                    addToList(input);
+                    break;
+            }
+        }
 
     }
+
     public void start() {
         greet();
-        Scanner scan = new Scanner(System.in);
-        poll(scan);
+        poll();
         exit();
     }
 }
