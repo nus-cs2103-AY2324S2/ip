5d5891f090c624bba21f26616b4af48fbc24f327
kokerinks <limkokliang01@gmail.com>
2024-01-22 16:15:34 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String botName = "KokBot";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        welcome(botName);
+        farewell();
+    }
+
+    public static void welcome(String botName) {
+        System.out.println(String.format("""
+                ____________________________________________________________
+                 Hello! I'm %s
+                 What can I do for you?
+                 """, botName));
+    }
+
+    public static void farewell() {
+        System.out.println("""
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                 """);
     }
 }
