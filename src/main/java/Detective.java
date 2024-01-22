b47dae4cf7261703e2588ace3fe59a1b24f24c0b
NusMinato <E0985934@u.nus.edu>
2024-01-22 09:14:03 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Detective.java
+++ src/main/java/Detective.java
@@ -6,7 +6,7 @@ public class Detective {
         Scanner sc = new Scanner(System.in);
         String name = "Detective";
         String line = "____________________________________________________________";
-        String[] myList = new String[100];
+        Task[] myList = new Task[100];
         int count = 0;
 
         System.out.println(line + "\nHello! I'm [" + name + "]" + "\nWhat can I do for you?\n" + line);
@@ -14,22 +14,42 @@ public class Detective {
         while (!Objects.equals(input, "bye")) {
             if (Objects.equals(input, "list")) {
                 System.out.println(line);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < 100; i++) {
                     if (myList[i] == null) {
                         break;
                     }
-                    System.out.println((i + 1) + ". " + myList[i]);
+                    System.out.println((i + 1) + "." + myList[i].getStatusIcon() + myList[i].name);
                 }
                 System.out.println(line);
                 input = sc.nextLine();
             } else {
-                if (count >= 100) {
-                    System.out.println(line + "\n" + "The list is full, add failed");
+                String[] inputContent = input.split(" ");
+                if (Objects.equals(inputContent[0], "mark") && inputContent.length == 2) {
+                    int taskNum = Integer.parseInt(inputContent[1]) - 1;
+                    myList[taskNum].markAsDone();
+                    System.out.println(line);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(myList[taskNum].getStatusIcon() + myList[taskNum].name);
+                    System.out.println(line);
                     input = sc.nextLine();
-                } else {
-                    myList[count++] = input;
-                    System.out.println(line + "\n" + "added: " + input + "\n" + line);
+                } else if (Objects.equals(inputContent[0], "unmark") && inputContent.length == 2) {
+                    int taskNum = Integer.parseInt(inputContent[1]) - 1;
+                    myList[taskNum].markAsNotDone();
+                    System.out.println(line);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(myList[taskNum].getStatusIcon() + myList[taskNum].name);
+                    System.out.println(line);
                     input = sc.nextLine();
+                } else { //add tasks
+                    if (count >= 100) {
+                        System.out.println(line + "\n" + "The list is full, add failed");
+                        input = sc.nextLine();
+                    } else {
+                        myList[count++] = new Task(input);
+                        System.out.println(line + "\n" + "added: " + input + "\n" + line);
+                        input = sc.nextLine();
+                    }
                 }
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String name;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X] " : "[ ] "); //mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
