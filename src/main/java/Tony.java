b822df0408b9d9c3297e188363d0390917aa4534
danielwangcinch <daniel.wang@cinch.sg>
2024-01-22 16:36:12 +0800

    feat: greeting/goodbye

---
+++ src/main/java/Tony.java
@@ -0,0 +1,13 @@
+public class Tony {
+    public static void main(String[] args) {
+        String greeting = "_______________________\n"
+                    + "Hello! I'm Tony!\n"
+                    + "What can I do for you?\n"
+                    + "_________________________\n";
+        String goodbye = "Bye. Hope to see you again soon!\n"
+                + "_________________________\n";
+        System.out.println(greeting);
+        System.out.println(goodbye);
+        System.exit(0);
+    }
+}
