c4eb355e830fe054eaf88d58634a490a410a0a33
TanJiaJun <e0725056@u.nus.edu>
2024-01-22 20:51:48 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String input;
+        String line = "_________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm ChatterPal!");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "__________________________________________";
-        System.out.println(line);
-        System.out.println("Hello! I'm ChatterPal!");
-        System.out.println("What can I do for you?");
-
+        input = sc.nextLine();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("list");
+            } else if (input.equals("blah")) {
+                System.out.println("blah");
+            }
+            input = sc.nextLine();
+        }
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
---
+++ src/main/java/Storage.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public class Storage {
+    public ArrayList<String> storage;
+
+}
