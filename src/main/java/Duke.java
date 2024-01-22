9e5e584793712ea6159ffe9ca2e9c6e6a57c2b80
Ma Yuehan <>
2024-01-22 17:56:06 +0800

    Implement an initial skeletal version of it that simply greets the user and exits.Change name to BotYue

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+            /*String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+
+        dialogue();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void dialogue() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm BotYue");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 }
