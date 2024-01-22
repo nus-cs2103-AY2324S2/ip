dfc9410cd5f8bfd9f2d1915535efc03e9d05c1a4
AidenLYT <ytstirlingite@hotmail.com>
2024-01-22 17:28:20 +0800

    Modified for new functionalities

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -19,7 +19,7 @@ public class Taylor {
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 listing.forEach((key, value) -> {
-                    System.out.println(key + ". " + "[" + value.getMarking() + "] " + value.getExecute());
+                    System.out.println(key + ". " + value);
                 });
 
             } else if (input.contains("mark")) {
@@ -29,18 +29,52 @@ public class Taylor {
                 if (what.equals("mark")) {
                     listing.get(num).markIt();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("[" + listing.get(num).getMarking() + "] " + listing.get(num).getExecute());
-                } else if (what.equals("unmark")){
+                    System.out.println(listing.get(num));
+                } else if (what.equals("unmark")) {
                     listing.get(num).unMark();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("[" + listing.get(num).getMarking() + "] " + listing.get(num).getExecute());
+                    System.out.println(listing.get(num));
                 }
 
             } else {
-                Action whatAction = new Action(input);
-                listing.put(pos, whatAction);
-                pos++;
-                System.out.println("added: " + input);
+                System.out.println("Got it. I've added this task:");
+
+                /**
+                 *  Retrieve the action to be done
+                 */
+                String[] parts = input.split(" ", 2);
+                String content = parts[1];
+
+                switch (parts[0]) {
+                    case "todo":
+                        Todo task = new Todo(content);
+                        listing.put(pos++, task);
+                        System.out.println(task);
+
+                        break;
+                    case "deadline":
+                        /**
+                         * Retrieve the action and time
+                         */
+                        String[] splitter = content.split("/by");
+                        Deadline dl = new Deadline(splitter[0], splitter[1]);
+                        listing.put(pos++, dl);
+                        System.out.println(dl);
+
+                        break;
+                    case "event":
+                        /**
+                         * Retrieve the action and time by first
+                         * getting action, then split the 2 times
+                         */
+                        String[] splitter1 = content.split("/from");
+                        String[] splitter2 = splitter1[1].split("/to");
+                        Event eve = new Event(splitter1[0], splitter2[0], splitter2[1]);
+                        listing.put(pos++, eve);
+                        System.out.println(eve);
+                        break;
+                }
+                System.out.println("Now you have " + listing.size() + " tasks in the list.");
             }
         }
 
