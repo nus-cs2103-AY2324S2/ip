2bf549a08de984bfc17f30cfcfbcd62a7abd0d0c
Ragnapop <jinhunl@gmail.com>
2024-01-22 14:57:58 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String name = "Bingus";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm \n" + name);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n\n");
     }
 }
