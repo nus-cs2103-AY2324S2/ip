d251f090ec092d0fe4aa8353b02c8a10bd2ccbb4
bgopi23 <baskargopinath01@gmail.com>
2024-01-22 22:12:13 +0800

    Update gops.java

---
+++ src/main/java/gops.java
@@ -0,0 +1,7 @@
+public class gops {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm gops");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
