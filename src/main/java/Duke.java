993cad5bc37adf54cebf4755f7a49003bbfd1b8c
yeoshuheng <yeoshuheng@gmail.com>
2024-01-22 12:30:54 +0800

    feat: renamed bot + added greet function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "shu heng";
+        String name_display = "_________________________\n" +
+          "Hello! I'm " + name + "\n" +
+          "What can I do for you?\n" +
+          "_________________________\n" +
+          "Bye. Hope to see you again soon!\n" +
+          "_________________________\n";
+      System.out.println(name_display);
     }
 }
