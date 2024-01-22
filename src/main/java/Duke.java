97caac60e7aa7583cda5200a3406f43c953cffb4
redcolorbicycle <liuzhengyang10@gmail.com>
2024-01-22 14:17:41 +0800

    Level 3 Mark/Unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,38 @@
 import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+
+        //name and introduction
         String name = "Bearducky";
         System.out.println("Quack! My name is " + name + ". I would be glad to help you in exchange for some bread.");
+
+        //Set up scanner, store user's inputs, structure to store tasks
         Scanner scanner = new Scanner(System.in);
-        String userInput;
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+
         while (true) {
-            userInput = scanner.nextLine();
+            String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("[sad quacking]\n");
                 break;
             } else if (userInput.equals("list")) {
-                for (String a: tasks) {
-                    System.out.println(a);
+                for (Task a : tasks) {
+                    System.out.println("[" + a.getStatusIcon() + "] " + a.getName());
                 }
                 System.out.println("\n");
+            } else if (userInput.equals("mark")) {
+                System.out.println("Please provide the task number to mark\n");
+                int markTask = scanner.nextInt();
+                scanner.nextLine();
+                tasks.get(markTask - 1).Mark();
+            } else if (userInput.equals("unmark")) {
+                System.out.println("Please provide the task number to unmark\n");
+                int unmarkTask = scanner.nextInt();
+                scanner.nextLine();
+                tasks.get(unmarkTask - 1).unMark();
             } else {
                 System.out.println("added: " +userInput + "\n");
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,27 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getName() {
+        return description;
+    }
+
+    public void Mark() {
+        this.isDone = true;
+    }
+
+    public void unMark() {
+        this.isDone = false;
+    }
+
+    //...
 }
