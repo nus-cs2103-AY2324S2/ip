b52a46351b839ae1283e74bc6edd5e383e14a302
ANTIFRAGILE\yusia <yusian89@gmail.com>
2024-01-22 23:23:40 +0800

    Modify Duke.java
    Allows user to recall tasks from list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> tasksList = new ArrayList<>();
         System.out.println("DevGPT:\n\tHello! I'm DevGPT\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("User:");
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("DevGPT:");
+                for (int i = 0; i < tasksList.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasksList.get(i));
+                }
             } else {
-                System.out.println("DevGPT:\n\t" + input);
+                tasksList.add(input);
+                System.out.println("DevGPT:\n\t" + "added:" + input);
             }
+
         }
         scanner.close();
-
         System.out.println("DevGPT:\n\tBye. Hope to see you again soon!");
 
     }
