fe058cc25f41c5a742aceadee7547a8356fd089c
teojunda <teojunda@gmail.com>
2024-01-22 16:27:11 +0800

    Created new TextTemplate class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,21 @@
 public class Duke {
+    private void greet() {
+        System.out.println(TextTemplate.LINE_BREAK);
+        System.out.println(TextTemplate.GREETING);
+        System.out.println(TextTemplate.LINE_BREAK);
+    }
+
+    private void bye() {
+        System.out.println(TextTemplate.EXIT);
+        System.out.println(TextTemplate.LINE_BREAK);
+    }
+    public void run() {
+        this.greet();
+        this.bye();
+    }
+
     public static void main(String[] args) {
-        String line_break = "________________________";
-        String name = "Beepo";
-        String greeting = " Hello! I'm " + name + "\n What can I do for you?";
-        String exit = " Bye. Hope to see you again soon!";
-        System.out.println(line_break);
-        System.out.println(greeting);
-        System.out.println(line_break);
-        System.out.println(exit);
-        System.out.println(line_break);
+        Duke duke = new Duke();
+        duke.run();
     }
 }
---
+++ src/main/java/TextTemplate.java
@@ -0,0 +1,5 @@
+public class TextTemplate {
+    public static final String LINE_BREAK = "________________________";
+    public static final String GREETING = " Hello! I'm Beepo.\n What can I do for you?";
+    public static final String EXIT = " Bye. Hope to see you again soon!";
+}
