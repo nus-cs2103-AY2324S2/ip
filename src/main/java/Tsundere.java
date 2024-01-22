b65db25cfa42f537788a40df614663291702fe02
macareonie <ryanchiang189@gmail.com>
2024-01-22 16:46:33 +0800

    Add Add task and list function

--- src/main/java/Tsundere.java
+++ src/main/java/Tsundere.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tsundere {
@@ -6,6 +7,8 @@ public class Tsundere {
     private static final String bye ="(\uD83D\uDCA2,,>﹏<,,) b-baka!";
 
     private static final Scanner sc = new Scanner(System.in);
+
+    private static final ArrayList<String> arr = new ArrayList<>();
 
     public static void greet() {
         System.out.println("______________________________________________\n" +
@@ -24,22 +27,38 @@ public class Tsundere {
     }
 
     public static void echo() {
-
         String str = sc.nextLine();
-        while (!str.equals("bye"))  {
+        while (!str.equals("bye")) {
             System.out.println("______________________________________________\n" +
                     str +
                     "\n______________________________________________\n");
             str = sc.nextLine();
         }
-
         exit();
+    }
 
+    public static void addTasks() {
+        String str = sc.nextLine();
+        while (!str.equals("bye")) {
+            if (str.equals("list")) {
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println((i + 1) + ". " + arr.get(i));
+                }
+                System.out.println();
+            } else {
+                System.out.println("______________________________________________\n" +
+                        "Got it: " + str +
+                        "\n______________________________________________\n");
+                arr.add(str);
+            }
+            str = sc.nextLine();
+        }
+        exit();
     }
     public static void main(String[] args) {
 
         greet();
-        echo();
+        addTasks();
 
     }
 }
