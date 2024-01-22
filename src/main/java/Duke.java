d7a18ec7d90b8d54bd83415404da2a616510c068
0liverkong <e1089628@u.nus.edu>
2024-01-22 20:58:13 +0800

    Completed Week 2 Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -9,8 +11,19 @@ public class Duke {
         System.out.println(line);
         System.out.println("Hello from Dook! :D meow\n" + logo);
         System.out.println("What can I do for you? uwu");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line + "\n");
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+        System.out.println("Bye :(. Hope to see you again soon! ≽^- ˕ -^≼");
         System.out.println(line);
     }
 }
