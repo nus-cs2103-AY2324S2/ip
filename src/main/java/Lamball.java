17ce4b45a3502da8cd6861c926252d9e510f2f62
ongzhili <ongchili234@gmail.com>
2024-01-22 18:18:02 +0800

    Gave chatbot a new name Skeletal Message added

---
+++ src/main/java/Lamball.java
@@ -0,0 +1,11 @@
+public class Lamball {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Lamball!\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println("Hello from\n" + logo);
+    }
+}
