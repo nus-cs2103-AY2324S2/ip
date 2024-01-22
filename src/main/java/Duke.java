e2c4b6d50bf66e58b7a0b89161e4c2b7c58a55e7
Ma Yuehan <>
2024-01-22 18:59:03 +0800

    Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,41 @@
+import java.util.Scanner;
 public class Duke {
+    public static void main(String[] args) {
+        dialogue();
+
+        Scanner scanner = new Scanner(System.in);
+        String inputs;
+
+        do {
+            inputs = scanner.nextLine();
+            if (!inputs.equalsIgnoreCase("bye")) {
+                echo(inputs);
+            }
+        } while (!inputs.equalsIgnoreCase("bye"));
+
+        System.out.println("   ____________________________________________________________");
+        System.out.println("   Bye. Hope to see you again soon!");
+        System.out.println("   ____________________________________________________________");
+    }
+
+    public static void dialogue() {
+        System.out.println("   ____________________________________________________________");
+        System.out.println("   Hello! I'm QuantumBot");
+        System.out.println("   What can I do for you?");
+        System.out.println("   ____________________________________________________________");
+    }
+
+    public static void echo(String input) {
+        System.out.println("   ____________________________________________________________");
+        System.out.println("   " + input);
+        System.out.println("   ____________________________________________________________");
+    }
+}
+
+
+/*Level 0
+public class Duke {
+
     public static void main(String[] args) {
 
             /*String logo = " ____        _        \n"
@@ -6,7 +43,7 @@ public class Duke {
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
 
         dialogue();
         System.out.println("Bye. Hope to see you again soon!");
@@ -18,4 +55,4 @@ public class Duke {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
     }
-}
+}*/
