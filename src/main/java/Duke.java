a13b58677c129f26002bda8707ee303917705f8f
Zen Khoon <e0957872@u.nus.edu>
2024-01-22 15:43:29 +0800

    Revert "Add echo"
    This reverts commit 1af93215c597b59b2b60e6d418ed6450a1401133.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,15 @@
-import java.util.Scanner;
-
 public class Duke {
 
     private void greet(){
-        System.out.println("Hello! I'm Balom.\n" +
-                "What can I do for you today?\n" +
-                "Please start typing something.\n");
+        System.out.println("Hello! I'm Balom.\nWhat can I do for you today?\n");
     }
     private void bye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
-
-    private void chatting(){
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            String echo = scanner.nextLine();
-            System.out.println(echo);
-            System.out.println();
-
-            if(echo.equals("bye") || echo.equals("Bye")){
-                break;
-            }
-        }
-        bye();
-    }
     public static void main(String[] args) {
         Duke Balom = new Duke();
         Balom.greet();
-        Balom.chatting();
+        Balom.bye();
+
     }
 }
