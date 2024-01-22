5f8a3515fd15533b5dd211a6c1777513affde9e0
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-01-22 16:49:31 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Jayne.java
@@ -0,0 +1,9 @@
+public class Jayne {
+    public static void main(String[] args) {
+        String dash = "___________________________________";
+        System.out.println(dash);
+        System.out.println("Hello, I'm Jayne");
+        System.out.println("What can I do for you?\n" + dash);
+        System.out.println("Bye. Hope to see you again soon!\n" + dash);
+    }
+}
