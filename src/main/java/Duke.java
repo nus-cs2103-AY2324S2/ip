6739ff73aa6d4af3799191726bdf57645473cd97
JiaXinEu <tis8cjiaxineu@gmail.com>
2024-01-23 00:16:26 +0800

    Add enum to handle input type

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private enum InputType {
+        LIST,
+        MARK,
+        UNMARK,
+        DELETE,
+        BYE,
+        TODO,
+        DEADLINE,
+        EVENT,
+        UNKNOWN
+    }
+
     public static void main(String[] args) {
         String logo = "\t  ____   __  _  _  __    ___  ___  ____  ___  ____\n\t"
                 + " (_  _) /__\\( \\/ )/__\\  / __)/ __)(_  _)/ __)(_  _)\n\t"
@@ -16,73 +28,127 @@ public class Duke {
 
         System.out.println(line + "\n" + logo + "\n\t Hello! I'm JavAssist ૮ฅ・ﻌ・აฅ");
         System.out.println("\t What can I do for you?\n" + line);
+
         String input, lowerInput;
-        while(true) {
+
+        boolean flag = true;
+
+        while (flag) {
             System.out.println();
             input = read.nextLine();
-            input = input.trim();
-            lowerInput = input.toLowerCase();
+            lowerInput = input.trim().toLowerCase();
+            InputType type = getCommandType(lowerInput);
             System.out.println(line);
-            if (input.equals("list")) {
-                print(list);
-            } else if (lowerInput.startsWith("mark")) {
-                if (lowerInput.matches("mark\\s\\d+")) {
-                    mark(input, true, list);
-                } else {
-                    System.out.println("\t OOPS!!! Your mark instruction is unclear ૮(˶˃ᆺ˂˶)");
-                    System.out.println("\t Try 'mark [task number to mark as done]'.");
-                }
-            } else if (lowerInput.startsWith("unmark")) {
-                if (lowerInput.matches("unmark\\s\\d+")) {
-                    mark(input, false, list);
-                } else {
-                    System.out.println("\t OOPS!!! Your unmark instruction is unclear ૮(˶˃ᆺ˂˶)");
-                    System.out.println("\t Try 'unmark [task number to mark as not done]'.");
-                }
-            } else if (lowerInput.startsWith("delete")) {
-                if (lowerInput.matches("delete\\s\\d+")) {
-                    delete(input, list);
-                } else {
-                    System.out.println("\t OOPS!!! Your delete instruction is unclear ૮₍ ´ ꒳ `₎ა");
-                    System.out.println("\t Try 'delete [task number to be deleted]'.");
-                }
-            } else if (lowerInput.equals("bye")) {
-                break;
-            } else if (lowerInput.startsWith("todo")){
-                if (lowerInput.matches("todo\\s.+")) {
-                    Task t = new Todo(input.substring(5));
-                    add(t, list);
-                } else {
-                    System.out.println("\t OOPS!!! The description of a todo cannot be empty ૮₍´˶• . • ⑅ ₎ა");
-                    System.out.println("\t Try 'todo [task description]'.");
-                }
-            } else if (lowerInput.startsWith("deadline")){
-                if (lowerInput.matches("deadline\\s.+\\s/by\\s.+")) {
-                    String[] task = input.split("/by");
-                    Task t = new Deadline(task[0].substring(9).trim(), task[1].trim());
-                    add(t, list);
-                } else {
-                    System.out.println("\t OOPS!!! The description and due of a deadline cannot be empty ૮₍´˶• . • ⑅ ₎ა");
-                    System.out.println("\t Try 'deadline [task description] /by [date/time]'.");
-                }
-            } else if (lowerInput.startsWith("event")) {
-                if (lowerInput.matches("event\\s.+\\s/from\\s.+\\s/to\\s.+")) {
-                    String[] task = input.split("/from");
-                    String[] time = task[1].split("/to");
-                    Task t = new Event(task[0].substring(6).trim(), time[0].trim(), time[1].trim());
-                    add(t, list);
-                } else {
-                    System.out.println("\t OOPS!!! The description, start and end time of an event cannot be empty ૮₍´˶• . • ⑅ ₎ა");
-                    System.out.println("\t Try 'event [task description] /from [start date/time] /to [end date/time]'.");
-                }
-            } else {
-                System.out.println("\t OOPS!!! Sorry I don't know what that means ૮₍ ´• ˕ •` ₎ა");
-                System.out.println("\t Try keywords: todo, deadline, event, list, mark, unmark, delete.");
+
+            switch (type) {
+                case LIST:
+                    print(list);
+                    System.out.println(line);
+                    break;
+
+                case MARK:
+                    if (lowerInput.matches("mark\\s\\d+")) {
+                        mark(input, true, list);
+                    } else {
+                        System.out.println("\t OOPS!!! Your mark instruction is unclear ૮(˶˃ᆺ˂˶)");
+                        System.out.println("\t Try 'mark [task number to mark as done]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case UNMARK:
+                    if (lowerInput.matches("unmark\\s\\d+")) {
+                        mark(input, false, list);
+                    } else {
+                        System.out.println("\t OOPS!!! Your unmark instruction is unclear ૮(˶˃ᆺ˂˶)");
+                        System.out.println("\t Try 'unmark [task number to mark as not done]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case DELETE:
+                    if (lowerInput.matches("delete\\s\\d+")) {
+                        delete(input, list);
+                    } else {
+                        System.out.println("\t OOPS!!! Your delete instruction is unclear ૮₍ ´ ꒳ `₎ა");
+                        System.out.println("\t Try 'delete [task number to be deleted]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case BYE:
+                    flag = false;
+                    break;
+
+                case TODO:
+                    if (lowerInput.matches("todo\\s.+")) {
+                        Task t = new Todo(input.substring(5));
+                        add(t, list);
+                    } else {
+                        System.out.println("\t OOPS!!! The description of a todo cannot be empty ૮₍´˶• . • ⑅ ₎ა");
+                        System.out.println("\t Try 'todo [task description]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case DEADLINE:
+                    if (lowerInput.matches("deadline\\s.+\\s/by\\s.+")) {
+                        String[] task = input.split("/by");
+                        Task t = new Deadline(task[0].substring(9).trim(), task[1].trim());
+                        add(t, list);
+                    } else {
+                        System.out.println("\t OOPS!!! The description and due of a deadline cannot be empty ૮₍´˶• . • ⑅ ₎ა");
+                        System.out.println("\t Try 'deadline [task description] /by [date/time]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case EVENT:
+                    if (lowerInput.matches("event\\s.+\\s/from\\s.+\\s/to\\s.+")) {
+                        String[] task = input.split("/from");
+                        String[] time = task[1].split("/to");
+                        Task t = new Event(task[0].substring(6).trim(), time[0].trim(), time[1].trim());
+                        add(t, list);
+                    } else {
+                        System.out.println("\t OOPS!!! The description, start and end time of an event cannot be empty ૮₍´˶• . • ⑅ ₎ა");
+                        System.out.println("\t Try 'event [task description] /from [start date/time] /to [end date/time]'.");
+                    }
+                    System.out.println(line);
+                    break;
+
+                case UNKNOWN:
+                    System.out.println("\t OOPS!!! Sorry I don't know what that means ૮₍ ´• ˕ •` ₎ა");
+                    System.out.println("\t Try keywords: todo, deadline, event, list, mark, unmark, delete.");
+                    System.out.println(line);
+                    break;
             }
-            System.out.println(line);
+
         }
 
         System.out.println("\t Bye. Hope to see you again soon! ʕ •ɷ•ʔฅ\n" + line);
+    }
+
+    private static InputType getCommandType(String input) {
+
+        if (input.equals("list")) {
+            return InputType.LIST;
+        } else if (input.startsWith("mark")) {
+            return InputType.MARK;
+        } else if (input.startsWith("unmark")) {
+            return InputType.UNMARK;
+        } else if (input.startsWith("delete")) {
+            return InputType.DELETE;
+        } else if (input.equals("bye")) {
+            return InputType.BYE;
+        } else if (input.startsWith("todo")) {
+            return InputType.TODO;
+        } else if (input.startsWith("deadline")) {
+            return InputType.DEADLINE;
+        } else if (input.startsWith("event")) {
+            return InputType.EVENT;
+        } else {
+            return InputType.UNKNOWN;
+        }
     }
 
     private static void print(ArrayList<Task> list) {
@@ -109,7 +175,7 @@ public class Duke {
     private static void delete(String input, ArrayList<Task> list) {
         String[] s = input.split("\\s");
         int num = Integer.parseInt(s[1]);
-        if (num <= list.size()) {
+        if (num <= list.size() && num >= 1) {
             Task t = list.remove(num - 1);
             System.out.println("\t Noted ૮ ˶ᵔ ᵕ ᵔ˶ ა I've removed this task:");
             System.out.println("\t\t " + t.printTask());
@@ -122,7 +188,7 @@ public class Duke {
     private static void mark(String input, boolean done, ArrayList<Task> list) {
         String[] s = input.split("\\s");
         int num = Integer.parseInt(s[1]);
-        if (num <= list.size()) {
+        if (num <= list.size() && num >= 1) {
             Task t = list.get(num - 1);
             if (!done) {
                 t.undo();
