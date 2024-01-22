2e7dc4b115b01d0aa2034e5ff0a9b06c646f6d9c
DingBao-sys <dinghaosim@gmail.com>
2024-01-22 23:19:54 +0800

    Finished Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String[] token = new String[] {
+                "____________________________________________________________",
+                "Hello! I'm chinesepoliceman",
+                "What can I do for you?",
+                "____________________________________________________________",
+                "Bye. Hope to see you again soon!",
+                "____________________________________________________________"
+        };
+        for (String i : token) {
+            System.out.println(i);
+        }
+        return;
     }
 }
