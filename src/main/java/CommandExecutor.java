4d5f6fc40de81a2a75ac39ea07e35ad26c665a3c
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2024-01-23 01:14:51 +0800

    Level 6. Delete A-Enums

--- src/main/java/CommandExecutor.java
+++ src/main/java/CommandExecutor.java
@@ -3,6 +3,18 @@ import LetoTasks.TaskListCommands;
 
 import java.util.Scanner;
 
+enum Commands {
+    BYE,
+    LIST,
+    TODO,
+    EVENT,
+    DEADLINE,
+    MARK,
+    UNMARK,
+    DELETE,
+    DEFAULT
+}
+
 public class CommandExecutor {
     private static Scanner sc = new Scanner(System.in);
 //    private HashMap<String, Function>
@@ -17,28 +29,51 @@ public class CommandExecutor {
             String inputs = sc.nextLine();
             String[] commands = inputs.split(" ");
 
-            switch (commands[0].toLowerCase()) {
-                case "bye":
+            Commands cmd;
+            try {
+                cmd = Commands.valueOf(commands[0].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                cmd = Commands.DEFAULT;
+            }
+
+
+            switch (cmd) {
+                case BYE:
                     bye = true;
                     break;
-                case "list":
+                case LIST:
                     System.out.println();
                     TaskListCommands.printList();
                     System.out.println();
                     break;
-                case "mark":
+                case MARK:
                     TaskListCommands.markTaskCompleted(inputs);
                     break;
-                case "unmark":
+                case UNMARK:
                     TaskListCommands.markTaskUncompleted(inputs);
                     break;
-                case "todo":
-                case "event":
-                case "deadline":
+                case TODO:
+                case EVENT:
+                case DEADLINE:
                     TaskListCommands.addToList(inputs);
                     break;
+                case DELETE:
+                    TaskListCommands.deleteTask(inputs);
+                    break;
                 default:
-                    System.out.println("  <<Duke Leto>>\n   >> Supported commands: list, mark, unmark, todo, event, deadline");
+                    System.out.println("  <<Duke Leto>>\n   >> Supported commands:\n" +
+                            "  list     -  list all tasks\n" +
+                            "  mark     -  mark task as done\n" +
+                            "                usage: mark <index>\n" +
+                            "  unmark   -  unmark task as done, :(\n" +
+                            "                usage: unmark <index>\n" +
+                            "  todo     -  create a task of type todo \n" +
+                            "                usage: todo <details>\n" +
+                            "  deadline -  create a task of type deadline\n" +
+                            "                usage: event <details> /by <end_time>\n" +
+                            "  event    -  create a task of type event    \n" +
+                            "                usage: event <details> /from <start_time> /to <end_time>\n" +
+                            "  bye      -  exit");
 
             } // End switch
 
--- src/main/java/LetoTasks/TaskListCommands.java
+++ src/main/java/LetoTasks/TaskListCommands.java
@@ -1,8 +1,10 @@
 package LetoTasks;
 
+import java.util.ArrayList;
+
 public class TaskListCommands {
-    private static final Task[] list = new Task[100];
-    private static int taskNextIndex = 0;
+    private static final ArrayList<Task> list = new ArrayList<>(100);
+//    private static int taskNextIndex = 0;
 
     public TaskListCommands() {}
 
@@ -24,9 +26,11 @@ public class TaskListCommands {
             if (t == null) {
                 throw new LetoInvalidTaskException("This task does not fit known tasks (event, deadline, todo)");
             }
-            TaskListCommands.list[TaskListCommands.taskNextIndex] = t;
-            TaskListCommands.taskNextIndex++;
-            System.out.println("  << Duke Leto >>\n  > LetoTasks.Task added, " + t.toString() + "\n  > You have " + TaskListCommands.taskNextIndex + " tasks.");
+            TaskListCommands.list.add(t);
+//            TaskListCommands.list[TaskListCommands.taskNextIndex] = t;
+//            TaskListCommands.taskNextIndex++;
+            System.out.println("  << Duke Leto >>\n  > Task added, [" + t.toString() +
+                    "]\n  > You have " + TaskListCommands.list.size() + " tasks.");
         } catch (LetoInvalidTaskException e) {
             e.printException();
         }
@@ -36,15 +40,17 @@ public class TaskListCommands {
         Task temp;
         try {
             int index = getIndexFromInput(inputs);
-            temp = TaskListCommands.list[index];
+//            temp = TaskListCommands.list[index];
+            temp = TaskListCommands.list.get(index);
             if (temp == null) {
-                throw new LetoInvalidTaskException("Task is null, try creating a task first!");
+                // should never happen
+                throw new LetoInvalidTaskException("WARNING Task is null, try creating a task first!");
             }
             if (temp.isCompleted()) {
                 throw new LetoInvalidTaskException("Task already completed");
             } else {
                 temp.markCompleted();
-                System.out.println("  << Duke Leto >>\n  > LetoTasks.Task marked as completed! Congratulations");
+                System.out.println("  << Duke Leto >>\n  > Task marked as completed! Congratulations");
             }
         } catch (LetoInvalidTaskException e) {
             e.printException();
@@ -55,16 +61,30 @@ public class TaskListCommands {
         Task temp;
         try {
             int index = getIndexFromInput(inputs);
-            temp = TaskListCommands.list[index];
+//            temp = TaskListCommands.list[index];
+            temp = TaskListCommands.list.get(index);
             if (temp == null) {
-                throw new LetoInvalidTaskException("Task is null, try creating a task first!");
+                // should not happen btw
+                throw new LetoInvalidTaskException("WARNING Task is null, try creating a task first!");
             }
             if (!temp.isCompleted()) {
                 throw new IndexOutOfBoundsException("Task is already not completed (╬▔皿▔)╯");
             } else {
                 temp.markUncompleted();
-                System.out.println("  << Duke Leto >>\n  > LetoTasks.Task marked as uncompleted! Things happen, dont worry we account for it");
+                System.out.println("  << Duke Leto >>\n  > Task marked as uncompleted! Things happen, dont worry we account for it");
             }
+        } catch (LetoInvalidTaskException e) {
+            e.printException();
+        }
+    }
+
+    public static void deleteTask(String inputs) {
+        try {
+            int index = getIndexFromInput(inputs);
+            Task t = TaskListCommands.list.get(index);
+            TaskListCommands.list.remove(index);
+            System.out.println("  << Duke Leto >>\n  > Task deleted, [" + t.toString() +
+                    "]\n  > You have " + TaskListCommands.list.size() + " tasks.");
         } catch (LetoInvalidTaskException e) {
             e.printException();
         }
@@ -77,23 +97,23 @@ public class TaskListCommands {
                 throw new LetoInvalidTaskException("You need to enter a task index number");
             }
             int i = Integer.parseInt(inputs[1]) - 1;
-            if (TaskListCommands.taskNextIndex == 0) {
+            if (TaskListCommands.list.isEmpty()) {
                 throw new LetoInvalidTaskException("Good news at least, you have no task!");
             }
-            if (i >= TaskListCommands.taskNextIndex || i < 0) {
-                throw new LetoBadTaskIndexException(TaskListCommands.taskNextIndex);
+            if (i >= TaskListCommands.list.size() || i < 0) {
+                throw new LetoBadTaskIndexException(TaskListCommands.list.size());
             }
             return i;
         } catch (NumberFormatException e) {
-            throw new LetoInvalidTaskException("We cannot get task index from input");
+            throw new LetoInvalidTaskException("We cannot get task index from your input, it should be an integer, `(un)mark _int_`");
         }
     }
 
     public static void printList() {
         System.out.println("  << Duke Leto >> ");
-        System.out.println("  ┌ < LetoTasks.Task list >");
-        for (int i = 0; i < taskNextIndex; i++) {
-            System.out.println("  ├ " + (i+1) + ": " + TaskListCommands.list[i].toString());
+        System.out.println("  ┌ < Task list >");
+        for (int i = 0; i < TaskListCommands.list.size(); i++) {
+            System.out.println("  ├ " + (i+1) + ": " + TaskListCommands.list.get(i).toString());
         }
         System.out.println("  └─ end of list");
     }
