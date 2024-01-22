5de55ccc225e1ebe70a1fd99c2b34b0d84b22a85
Theodore Koo <e0958222@u.nus.edu>
2024-01-22 22:03:35 +0800

    Level-1: Echo

--- src/main/java/Riz.java
+++ src/main/java/Riz.java
@@ -1,9 +1,23 @@
+import java.util.*;
 public class Riz {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String dotted = "-----------------------------------";
+        //greetings
         String greetings = "Hello... I'm Riz...\n"
-        + "What can I help you with today?";
+                + "What can I help you with today?\n"
+                + dotted;
         System.out.println(greetings);
-        String exit = "Goodbye... Hope to see you again soon...";
-        System.out.println(exit);
+        boolean running = true;
+        while (running) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                running = false;
+                System.out.println("Bye... Hope to see you again...\n" + dotted);
+            } else {
+                System.out.println(input + "...");
+                System.out.println(dotted);
+            }
+        }
     }
 }
