8098d57da7a3d20d9a43d842e29e4375fb90021f
getsquared <articferretles@gmail.com>
2024-01-22 12:53:28 +0800

    Add Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        printMessage(logo);
-        printMessage("Hello! I'm Toothless :D\nWhat can I do for you?");
-        printMessage("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        String introMessage = " /\\_/\\\n" +
+                "\t( o.o )\n" +
+                "\t > ^ <\n" +
+                "\tNya-ice to meet you! I'm Toothless :D\n" +
+                "\tWhat can I do for you?";
+        printMessage(introMessage);
+
+        while (isRunning) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.strip().toLowerCase().equals("bye")) {
+                isRunning = false;
+            } else {
+                printMessage(userInput);
+            }
+        }
+
+        printMessage("Bye. Purr-lease chat again soon!");
     }
 
     public static void printMessage(String message) {
-        System.out.println(message);
-        String line = "____________________________________________________________";
+        String line = "\t____________________________________________________________";
+        System.out.println(line);
+        System.out.println("\t" + message);
         System.out.println(line);
     }
 }
