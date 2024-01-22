c5447627eb6c37c73d6ba3ceb206952b5f273316
headcube1 <ywesley16@gmail.com>
2024-01-23 01:11:41 +0800

    added delete method

--- src/main/java/HeadCube.java
+++ src/main/java/HeadCube.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class HeadCube {
-    private static Task[] tasks = new Task[100];
-    private static int taskCount = 0;
+    private static List<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +20,8 @@ public class HeadCube {
                 } else if (split[0].equals("mark")){
                     mark(Integer.parseInt(split[1]));
 
+                } else if (split[0].equals("delete")) {
+                    delete(Integer.parseInt(split[1]));
                 } else {
                     add(input);
                 }
@@ -44,40 +48,45 @@ public class HeadCube {
             if (split.length < 2 || split[1].isBlank()) {
                 throw new HeadCubeException("Todo cannot be empty!!");
             }
-            tasks[taskCount] = new ToDos(split[1]);
+            tasks.add(new ToDos(split[1]));
         } else if (event.equals("deadline")) {
             String[] parts = split[1].split(" /by ",2);
             description = parts[0];
             String by = parts[1];
-            tasks[taskCount] = new Deadlines(description,by);
+            tasks.add(new Deadlines(description,by));
         } else if (event.equals("event")) {
             String[] parts = split[1].split(" /from ", 2);
             description = parts[0];
             String[] times = parts[1].split(" /to ", 2);
             String start = times[0];
             String end = times[1];
-            tasks[taskCount] = new Events(description, start, end);
+            tasks.add(new Events(description, start, end));
         } else {
             throw new HeadCubeException("I do not understand what that means!!");
         }
 
-        System.out.println("Got it. I've added this task:\n  " + tasks[taskCount]);
-        taskCount++;
-        System.out.println("Now you have " + taskCount + " tasks in the list.\n");
+        System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.\n");
     }
 
     public static void list() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
         }
         System.out.println();
     }
 
     public static void mark(int taskNumber) {
-        Task task = tasks[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.done();
         System.out.println("Nice! I've marked this task as done:\n  " + task);
+    }
+
+    public static void delete(int taskNumber) {
+        Task removedTask = tasks.remove(taskNumber - 1);
+        System.out.println("Noted. I've removed this task:\n  " + removedTask);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.\n");
     }
 
 }
