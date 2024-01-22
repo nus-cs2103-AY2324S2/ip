ad632980476e980761ec5843e78747c18fe51579
Kenneth Seet <kenurfseet@gmail.com>
2024-01-22 18:02:25 +0800

    changed array to arraylist in store

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,38 +1,35 @@
 import java.lang.StringBuilder;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 public class Store {
-    private Task[] tasks = new Task[100];
-    private int size = 0;
+    private ArrayList<Task> tasks = new ArrayList<>();
     Store() {}
     public void add(Task t) {
-        this.tasks[this.size] = t;
-        this.size++;
+        this.tasks.add(t);
     }
 
     public void delete(int i) {
-        for (int j = i; j < this.size; j++) {
-            this.tasks[j] = this.tasks[j + 1];
-        }
-        this.size--;
+        this.tasks.remove(i);
     }
 
     public int getSize() {
-        return this.size;
+        return this.tasks.size();
     }
 
     public Task mark(int i) {
-        return this.tasks[i - 1].mark();
+        return this.tasks.get(i - 1).mark();
     }
 
     public Task unmark(int i) {
-        return this.tasks[i - 1].unmark();
+        return this.tasks.get(i - 1).unmark();
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < this.size; i++) {
-            sb.append(i + 1).append(".").append(this.tasks[i].toString());
-            if (i != this.size - 1) {
+        for (int i = 0; i < this.tasks.size(); i++) {
+            sb.append(i + 1).append(".").append(this.tasks.get(i).toString());
+            if (i != this.tasks.size() - 1) {
                 sb.append("\n");
             }
         }
