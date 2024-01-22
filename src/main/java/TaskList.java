ccfa34e09a0aff6e8bea82f91c268d6437e13aa1
Javiery3889 <javiery3889@gmail.com>
2024-01-22 13:34:05 +0800

    Fixed list numbering bug

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -21,7 +21,7 @@ public class TaskList {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 1; i <= this.tasks.size(); i++) {
             Task task = getTask(i);
-            String title = String.format("%d. %s", i+1, task.toString());
+            String title = String.format("%d. %s", i, task.toString());
             // do not add new line when for last task in list
             if (i == this.tasks.size()) {
                 stringBuilder.append(title); 
