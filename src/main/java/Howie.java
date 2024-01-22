f17044d595fde2003b9d72b8588e911d953df88f
smolegz <zhuohui.koo@gmail.com>
2024-01-22 16:01:58 +0800

    Completed Level-0

---
+++ src/main/java/Howie.java
@@ -0,0 +1,14 @@
+public class Howie {
+    public static void printVLine() {
+        System.out.println("---------------------------");
+    }
+    public static void main(String[] args) {
+
+        printVLine();
+        System.out.println("Hello! I'm Howie");
+        System.out.println("What can I do for you?");
+        printVLine();
+        System.out.println("Bye. Hope to see you again soon!!");
+        printVLine();
+    }
+}
