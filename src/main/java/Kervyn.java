938db60a245a3241d07a1fb937c9f16f639e9825
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-22 21:47:40 +0800

    Refactored if conditions into a switch case

--- src/main/java/Kervyn.java
+++ src/main/java/Kervyn.java
@@ -14,24 +14,26 @@ public class Kervyn {
         String userInput;
         do {
             userInput = scanner.nextLine();
-            if (!Objects.equals(userInput, "bye") && !Objects.equals(userInput, "list")) {
-                System.out.println("\tadded: " + userInput);
-                // Create a new Task for each new item
-                Task newTask = new Task(userInput, false);
-                userRequests.add(newTask);
-            }
+            switch (userInput) {
+                case "bye":
+                    System.out.println("\tBye. Hope to see you again soon!");
+                    break;
+                case "list":
+                    System.out.println("Here are the tasks on your list:");
+                    for (int i = 0; i < userRequests.size(); i++) {
+                        Task task = userRequests.get(i);
+                        char check = task.getStatus() ? 'X' : ' ';
+                        System.out.println("\t" + (i + 1) + "." + "[" + check + "] " + task.getName());
 
-            if (Objects.equals(userInput, "list")) {
-                System.out.println("Here are the tasks on your list:");
-                for (int i = 0; i < userRequests.size(); i ++) {
-                    Task task =  userRequests.get(i);
-                    char check =  task.getStatus() ? 'X' : ' ';
-                    System.out.println("\t" + (i + 1) + "." + "[" + check + "] " +  task.getName());
-
-                }
+                    }
+                    break;
+                default:
+                    System.out.println("\tadded: " + userInput);
+                    // Create a new Task for each new item
+                    Task newTask = new Task(userInput, false);
+                    userRequests.add(newTask);
             }
         } while (!Objects.equals(userInput, "bye"));
 
-        System.out.println("\tBye. Hope to see you again soon!");
     }
 }
