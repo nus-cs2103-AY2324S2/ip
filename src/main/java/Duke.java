d6bf987a7e03bf990c075eda8d3ed9a93d5a436c
Jeremy Wang <newstarhoseo@gmail.com>
2024-01-22 21:24:29 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke aurora1 = new Duke();
+        aurora1.exeAurora();
     }
+
+    /**
+     * Method for execution.
+     */
+    public void exeAurora() {
+        printALine();
+        printOpeningMessage();
+        printExitMessage();
+    }
+
+    /**
+     * Method to print the greeting message.
+     */
+    public void printOpeningMessage() {
+        String openingMessage = "How are you feeling? I'm Aurora, your personal schedule assistant. \n"
+                + "What can I do for you?";
+        System.out.println(openingMessage);
+        printALine();
+    }
+
+    /**
+     * Method to print the exit message.
+     */
+    public void printExitMessage() {
+        String exitMessage = "Thank you for consulting with me, have a good day.";
+        System.out.println(exitMessage);
+        printALine();
+    }
+
+    /**
+     * Method to print a line for separation.
+     */
+    public void printALine() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+
 }
