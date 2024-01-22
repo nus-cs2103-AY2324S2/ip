cf882333dc398c4e1d425fd539b32839a14505b1
Theodore Koo <e0958222@u.nus.edu>
2024-01-22 23:22:41 +0800

    Level-3 : Mark as Done

--- src/main/java/Riz.java
+++ src/main/java/Riz.java
@@ -10,27 +10,41 @@ public class Riz {
                 + dotted;
         System.out.println(greetings);
         boolean running = true;
+
         while (running) {
             String input = scanner.nextLine();
-            switch (input) {
-                case "bye":
-                    running = false;
-                    System.out.println("Bye... Hope to see you again...\n" + dotted);
-                    break;
-                case "list":
-                    int size = tasks.size();
-                    for (int i = 0; i < size; i++) {
-                        int curr = i + 1;
-                        String result = curr + ". " + tasks.get(i).toString() +"...";
-                        System.out.println(result);
-                    }
-                    System.out.println(dotted);
-                    break;
-                default:
-                    Task task = new Task(input);
-                    tasks.add(task);
-                    System.out.println("added: " + task + "...");
-                    System.out.println(dotted);
+            String[] token = input.split(" ", 2);
+            if (token[0].equals("bye")) {
+                running = false;
+                System.out.println("Bye... Hope to see you again...\n" + dotted);
+            } else if (token[0].equals("mark")) {
+                int curr = Integer.parseInt(token[1]) - 1;
+                Task task = tasks.get(curr);
+                task.mark();
+                System.out.println("Awesome..., I've marked this task as completed...");
+                System.out.println(task);
+                System.out.println(dotted);
+            } else if (token[0].equals("unmark")) {
+                int curr = Integer.parseInt(token[1]) - 1;
+                Task task = tasks.get(curr);
+                task.unmark();
+                System.out.println("Oops... Guess it's not done yet...");
+                System.out.println(task);
+                System.out.println(dotted);
+            } else if (token[0].equals("list")) {
+                int size = tasks.size();
+                System.out.println("Here are the items in your To-Do List...");
+                for (int i = 0; i < size; i++) {
+                    int curr = i + 1;
+                    String result = curr + ". " + tasks.get(i).toString() + "...";
+                    System.out.println(result);
+                }
+                System.out.println(dotted);
+            } else {
+                Task task = new Task(input);
+                tasks.add(task);
+                System.out.println("added: " + task.add() + "...");
+                System.out.println(dotted);
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,8 +6,25 @@ public class Task {
         this.completed = false;
     }
 
+    // To mark the task as completed
+    public void mark() {
+        this.completed = true;
+    }
+
+    // To mark the task as uncompleted
+    public void unmark() {
+        this.completed = false;
+    }
+
+    public String add(){
+        return this.task;
+    }
     @Override
     public String toString() {
-        return this.task;
+        if (completed) {
+            return "[X] " + this.task;
+        } else {
+            return "[ ] " + this.task;
+        }
     }
 }
