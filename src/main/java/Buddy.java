8335479bfddeb52d41ad63725c4e8ae2d95ccfbd
mahadhir247 <mahadhir123456@gmail.com>
2024-01-22 20:22:44 +0800

    Added echo function that repeats commands entered by user and exits when user types 'bye'

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Buddy {
     private String lineBreak = "____________________________________________________________\n";
 
@@ -11,16 +13,34 @@ public class Buddy {
             "              |____/ \\__,_|\\__,_|\\__,_|\\__, |\n" +
             "                                        __/ |\n" +
             "                                       |___/ \n";
-        System.out.println(lineBreak + logo + lineBreak + " Hello friend!\n" + " How can I help you?");
+        System.out.println(lineBreak + logo + lineBreak + " Hello friend!\n" + " How can I help you?\n" + lineBreak);
     }
 
     private void exit() {
         System.out.println(lineBreak + " Bye. Hope to see you again soon!\n" + lineBreak);
     }
 
+    private void echo() {
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
+
+        while (running) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                running = false;
+            } else {
+                String output = input + "\n";
+                System.out.println(lineBreak + output + lineBreak);
+            }
+        }
+        sc.close();
+    }
+
     public static void main(String[] args) {
         Buddy buddy = new Buddy();
         buddy.greet();
+        buddy.echo();
         buddy.exit();
     }
 }
