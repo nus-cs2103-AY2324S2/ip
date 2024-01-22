f8aaf09dde3ba4c045289280529f51f168f53e0d
chiageng <chiageng12@gmail.com>
2024-01-22 23:43:55 +0800

    Add a Task class. Add the ability to mark task as done and not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,24 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Duke {
+    /**
+     * Print out a line on the screen
+     */
     public static void drawLine() {
-        /**
-         * Print out a line on the screen
-         */
         System.out.println("-----------------------------------------------------");
     }
 
-    public static void addTask(String task, List<String> storage) {
-        /**
-         * Add input task into storage
-         */
-        storage.add(task);
+    /**
+     * Add input task into storage
+     */
+    public static void addTask(String task, List<Task> storage) {
+
+        Task newTask = new Task(task);
+        storage.add(newTask);
         drawLine();
         System.out.println("Added: " + task);
         drawLine();
     }
 
-    public static void listTask(List<String> storage) {
+    /**
+     * List out all the task from the storage
+     * @param storage where the task are kept in
+     */
+    public static void listTask(List<Task> storage) {
         drawLine();
         for (int i = 0; i < storage.size(); i++) {
             System.out.println((i + 1) + ". " + storage.get(i));
@@ -27,19 +33,45 @@ public class Duke {
         drawLine();
     }
 
+    /**
+     * To mark a task as done
+     * @param storage where the task is kept in
+     * @param index index of the task to be marked as done
+     */
+    public static void markDone(List<Task> storage, int index) {
+        Task curr = storage.get(index - 1);
+        drawLine();
+        curr.markDone();
+        drawLine();
+    }
+
+    /**
+     * To unmark a task
+     * @param storage where the task is kept in
+     * @param index index of the task to be unmarked
+     */
+    public static void markUndone(List<Task> storage, int index) {
+        Task curr = storage.get(index - 1);
+        drawLine();
+        curr.markUndone();
+        drawLine();
+    }
+
+    /**
+     * Provide commands to communicate with chatbot
+     */
     public static void startChat() {
-        /**
-         * Provide commands to communicate with chatbot
-         */
+
         drawLine();
         System.out.println("Hello! I'm Colin");
         System.out.println("What can I do for you?");
         drawLine();
         Scanner scanner = new Scanner(System.in);
-        List<String> storage = new ArrayList<>();
+        List<Task> storage = new ArrayList<>();
 
         while (true) {
             String command = scanner.nextLine();
+            String[] commandArr = command.split(" ");
 
             if (command.equals("bye")) {
                 drawLine();
@@ -48,6 +80,10 @@ public class Duke {
                 break;
             } else if (command.equals("list")) {
                 listTask(storage);
+            } else if (commandArr[0].equals("mark")) {
+                markDone(storage, Integer.valueOf(commandArr[1]));
+            } else if (commandArr[0].equals("unmark")) {
+                markUndone(storage, Integer.valueOf(commandArr[1]));
             } else {
                 addTask(command, storage);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,54 @@
+/**
+ * Task is a class with description, can record whether a task is done or not done
+ */
+public class Task {
+    /**
+     * Description of the task
+     */
+    private String description;
+    /**
+     * Record a task is done or not done
+     */
+    private boolean isDone;
+
+    /**
+     * Constructor of task class
+     * @param description The description of the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * To mark a task as done
+     */
+    public void markDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(this);
+    }
+
+
+    /**
+     * To mark a task as undone
+     */
+    public void markUndone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet");
+        System.out.println(this);
+    }
+
+    /**
+     * String representation of task
+     * @return string representation of task for done and not done task
+     */
+    @Override
+    public String toString() {
+        if (isDone) {
+            return "[X] " + this.description;
+        } else {
+            return "[ ] " + this.description;
+        }
+    }
+}
