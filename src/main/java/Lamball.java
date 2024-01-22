2a113ab83d42673e3791aab4b676ddedf964928d
ongzhili <ongchili234@gmail.com>
2024-01-22 19:00:35 +0800

    Improved input parse handling

--- src/main/java/Lamball.java
+++ src/main/java/Lamball.java
@@ -2,26 +2,32 @@
 import java.util.Scanner;
 public class Lamball {
     String indent = "    ____________________________________________________________\n";
-    String logo = "    ____________________________________________________________\n" +
+    String welcomeMessage = "    ____________________________________________________________\n" +
             "     Hello! I'm Lamball!\n" +
             "     What can I do for you?\n" +
             "    ____________________________________________________________\n";
+    String goodbyeMessage = "    ____________________________________________________________\n" +
+            "     Goodbye!\n" +
+            "    ____________________________________________________________\n";
 
+    String[] tasks;
     public Lamball() {
-
+        tasks = new String[100];
     }
 
     public void greetingMessage() {
-        System.out.println(this.logo);
+        System.out.println(this.welcomeMessage);
     }
 
     public boolean parse(String msg) {
-        System.out.println(indent + "    " + msg + "\n" + indent);
-        // Check if the user wants to exit
-        if (msg.equalsIgnoreCase("bye")) {
-            System.out.println("Duke: Goodbye! Exiting Duke.");
-            return false;
+        String[] parts = msg.split(" ", 2);
+        switch(parts[0]) {
+            case "bye":
+                System.out.println(goodbyeMessage);
+                return false;
+            default:
+                System.out.println(indent + "    " + msg + "\n" + indent);
+                return true;
         }
-        return true;
     }
 }
