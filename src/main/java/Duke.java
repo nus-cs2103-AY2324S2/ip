613cbc0b3b5e35a53f8afdb903b0a4aa872729f9
maze508 <ac0bge@gmail.com>
2024-01-22 17:01:49 +0800

    Level-6: Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,6 +28,8 @@ public class Duke {
                     addEvent(command);
                 } else if (command.startsWith("todo")) {
                     addTodo(command);
+                } else if (command.startsWith("delete")) {
+                    deleteTask(command);
                 } else {
                     throw new DukeException("Invalid Command. I'm sorry, but I don't know what that means :-(");
                 }
@@ -135,6 +137,24 @@ public class Duke {
         Todo todo = new Todo(task);
         store.add(todo);
         printAddedTask(todo);
+    }
+
+    private static void deleteTask(String command) throws DukeException {
+        try {
+            int idx = Integer.parseInt(command.split(" ")[1]) - 1;
+            if (idx < 0 || idx >= store.size()) {
+                throw new DukeException("The task index is out of range.");
+            }
+            Task removedTask = store.remove(idx);
+
+            System.out.println("____________________________________________________________");
+            System.out.println("Noted. I've removed this task:");
+            System.out.println(removedTask);
+            System.out.println("Now you have " + store.size() + " tasks in the list.");
+            System.out.println("____________________________________________________________");
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index provided is not a valid number.");
+        }
     }
 
     private static void printAddedTask(Task task) {
