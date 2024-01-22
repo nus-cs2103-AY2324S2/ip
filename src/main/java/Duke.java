c17800b7c5c3d7edd018462ddc8d11eb6ed5a0cc
jovantanyk <jovantan630@gmail.com>
2024-01-22 15:44:27 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String bot_name = "Paimon";
+        System.out.println("Hello, I'm " +  bot_name + "\nWhat can I help you with today?" + "\n--------------------");
+        System.out.println("See you again later!" + "\n--------------------");
     }
 }
