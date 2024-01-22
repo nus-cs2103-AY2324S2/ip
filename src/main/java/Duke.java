4b97529c743efa60f0602c4b7474f134c21e18a7
chiageng <chiageng12@gmail.com>
2024-01-22 23:04:08 +0800

    Improve the skeletal version of Duke so that it exit when user type command bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void drawLine() {
         /**
@@ -5,11 +6,35 @@ public class Duke {
          */
         System.out.println("-----------------------------------------------------");
     }
-    public static void main(String[] args) {
+
+    public static void startChat() {
+        /**
+         * Provide commands to communicate with chatbot
+         */
+        drawLine();
         System.out.println("Hello! I'm Colin");
         System.out.println("What can I do for you?");
         drawLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        drawLine();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                drawLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                drawLine();
+                break;
+            } else {
+                drawLine();
+                System.out.println(command);
+                drawLine();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        startChat();
     }
 }
