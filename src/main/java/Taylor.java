84d061d3674c7dd73554d217ae83afa606db8b15
AidenLYT <ytstirlingite@hotmail.com>
2024-01-23 02:20:18 +0800

    Updated with the use of ENUMS

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Taylor {
+    enum Activity {
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, INVALID
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Taylor");
         System.out.println("What can I do for you?");
@@ -29,16 +33,18 @@ public class Taylor {
                 String[] act = input.split(" ", 2);
                 String action = act[0];
 
-                switch (action) {
-                    case "bye":
+                Activity activity = getActivity(action);
+
+                switch (activity) {
+                    case BYE:
                         break label;
 
-                    case "list":
+                    case LIST:
                         ListTask.exec(listing);
 
                         break;
-                    case "mark":
-                    case "unmark":
+                    case MARK:
+                    case UNMARK:
                         try {
                             MarkTask.exec(input, listing);
                         } catch (DukeException err) {
@@ -46,23 +52,23 @@ public class Taylor {
                         }
 
                         break;
-                    case "todo":
-                    case "deadline":
-                    case "event":
+                    case TODO:
+                    case DEADLINE:
+                    case EVENT:
                         try {
                             InsertTask.exec(input, listing);
                         } catch (DukeException err) {
                             System.out.println("Error: " + err.getMessage());
                         }
                         break;
-                    case "delete":
+                    case DELETE:
                         try {
                             DeleteTask.exec(input, listing);
                         } catch (DukeException err) {
                             System.out.println("Error: " + err.getMessage());
                         }
                         break;
-                    default:
+                    case INVALID:
                         System.out.println("Invalid input. ChatBot can only handle " +
                                 "'todo', 'deadline', 'event', 'bye', 'list' tasks");
                         break;
@@ -71,5 +77,13 @@ public class Taylor {
         }
         System.out.println("Bye. Hope to see you again soon!");
         type.close();
+    }
+
+    private static Activity getActivity(String action) {
+        try {
+            return Activity.valueOf(action.toUpperCase());
+        } catch (IllegalArgumentException err) {
+            return Activity.INVALID;
+        }
     }
 }
