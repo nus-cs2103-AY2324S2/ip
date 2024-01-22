203b4e00ba3ae88df971ce76e3ba0f104cfde3d0
hjungwoo01 <hjw1129@gmail.com>
2024-01-22 12:50:35 +0800

    Rename chatbot, add user greeting, and implement program exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.io.PrintWriter;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        String chatBotName = "Nicky";
+        pw.println("____________________________________________________________");
+        pw.println("Hello! I'm " + chatBotName + "\nWhat can I do for you?");
+        pw.println("____________________________________________________________");
+        pw.println("Bye. Hope to see you again soon!");
+        pw.println("____________________________________________________________");
+        pw.flush();
+        pw.close();
     }
 }
