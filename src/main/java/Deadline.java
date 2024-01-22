adc3aa433e26ba8341cd67afceb0020059e88eb6
Ragnapop <jinhunl@gmail.com>
2024-01-22 20:02:20 +0800

    Level 3. Mark as Done

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,19 @@
+public class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+
+    @Override
+    public String getTaskIcon() {
+        return "D";
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " (by: " + by + ")";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import static java.lang.System.exit;
 
 public class Duke {
+
+
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,20 +17,57 @@ public class Duke {
         String name = "Bingus";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm \n" + name);
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?\n");// initial introductory message
+
+        Task list[] = new Task[100];// array to store tasks given
 
 
-        BufferedReader bfn = new BufferedReader(
-                new InputStreamReader(System.in));
-        String str = bfn.readLine();
+
+        int n = 0;
+        Scanner bfn = new Scanner(
+                new InputStreamReader(System.in));// scanner to read user input
+        String str = bfn.nextLine();
+
+
         while (true) {
-            System.out.println(str);
-            str = bfn.readLine();
-            if (str.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n\n");
-                System.exit(1);
+            String[] tokens = str.split("\\s+");// split read string into individual components to read keywords
+            String identifier = tokens[0];// store keywords
 
+            if (str.equals("list")) {
+                for(int a = 0; a < n;a++ ) {
+                    System.out.println(a+1 + ". " + list[a].ToString());
+                }
+            }// if keyword is list, open list
+
+            else if (str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.exit(1);// if keyword is bye, exit the program
             }
+
+            else if (identifier.equals("mark")){
+                int no = Integer.parseInt(tokens[1])+1;
+                list[no].isDone = true;
+                System.out.println("Nice! I've marked this task as done:\n");
+                System.out.println(list[no].ToString());
+            }
+
+            else if (identifier.equals("unmark")){
+                int no = Integer.parseInt(tokens[1])+1;
+                list[no].isDone = false;
+                System.out.println("OK, I've marked this task as not done yet:\n");
+                System.out.println(list[no].ToString());
+            }
+
+
+            else {
+                System.out.println(str);
+
+                list[n] = new Task(str);
+                n++;
+            }
+
+
+            str = bfn.nextLine();
         }
     }
 }
