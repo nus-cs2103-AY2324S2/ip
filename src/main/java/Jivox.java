284ae0f094b6efc19f31bd2a06c06f55b1e29ab2
dedsecrattle <itsprabxxx@gmail.com>
2024-01-23 02:07:47 +0800

    Level-0 Add Jivox and Delete Duke

---
+++ src/main/java/Jivox.java
@@ -0,0 +1,20 @@
+public class Jivox {
+    public void greet(){
+        addDivider();
+        System.out.println(" Hello! I'm Jivox");
+        System.out.println(" What can I do for you?");
+        addDivider();
+    }
+
+    public void exit(){
+        addDivider();
+        System.out.println(" Bye. Hope to see you again soon!");
+        addDivider();
+    }
+
+    public void addDivider(){
+        System.out.println("============================================================");
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    private static Jivox bot = new Jivox();
+
+    public static void main(String[] args) {
+        bot.greet();
+        bot.exit();
+    }
+}
