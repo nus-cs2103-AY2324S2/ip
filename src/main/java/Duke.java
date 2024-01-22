8cec1b6a58ae5e23cec598e1fae47ab49f511d8d
AndrewOng2066 <e0959899@u.nus.edu>
2024-01-22 13:13:09 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,20 +10,30 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Hello! I'm AndrewOng2066");
-        System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("      Hello! I'm AndrewOng2066");
+        System.out.println("      What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
+        ArrayList<String> savedText = new ArrayList<>();
+        String userInput = sc.nextLine().trim();
         while (!userInput.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + userInput);
-            System.out.println("    ____________________________________________________________");
-            userInput = sc.nextLine();
+            if (userInput.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < savedText.size(); i++) {
+                    System.out.println("      " + (i + 1) + ". " + savedText.get(i));
+                }
+                System.out.println("    ____________________________________________________________\n");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("      added: " + userInput);
+                savedText.add(userInput);
+                System.out.println("    ____________________________________________________________\n");
+            }
+            userInput = sc.nextLine().trim();
         }
 
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("      Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 }
