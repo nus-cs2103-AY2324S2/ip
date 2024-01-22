e568f76cf255a9590cbcf744492c83dfb12f9b60
yeoshuheng <yeoshuheng@gmail.com>
2024-01-22 22:10:59 +0800

    feat: added more exceptions to handle invalid index

---
+++ src/main/java/HistoryIndexException.java
@@ -0,0 +1,5 @@
+public class HistoryIndexException extends DukeException {
+  public HistoryIndexException() {
+    super("That's not a valid index you can choose! Check the possible index with /list!");
+  }
+}
