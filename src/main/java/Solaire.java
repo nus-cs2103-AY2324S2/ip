16ea506ab6fb9e036522b09b121d7488d7d38554
marcus-ny <kyalsin321@gmail.com>
2024-01-23 06:27:17 +0800

    implement level-2
    - added a To-do-list that stores list of user inputs and print them on request

--- src/main/java/Solaire.java
+++ src/main/java/Solaire.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Solaire {
-
+    private ArrayList<String> toDoList = new ArrayList<>();
+    private Scanner scn = new Scanner(System.in);
     
     public void startConversation() {
         greet();
@@ -12,32 +13,56 @@ public class Solaire {
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println(input);
+            processInput(input);
         }
+        scn.close();
         waveBye();
     }
 
+    private void lineBreak() {
+        System.out.println("--------------------------------------------------\n");
+    }
     private void greet() {
-        String greetingMessage = "--------------------------------------------------\n"
-        + "Oh hello there. I'm Solaire of Astora.\n"
+        String greetingMessage = "Oh hello there. I'm Solaire of Astora.\n"
         + "The sun is a wondrous body. Like a magnificent father!\n"
-        + "If only I could be so grossly incandescent!\n"
-        + "--------------------------------------------------\n";
+        + "If only I could be so grossly incandescent!\n";
 
         System.out.println(greetingMessage);
+        lineBreak();
     }
 
     private void waveBye() {
-        String farewellMessage = "Farewell!" +
-        "--------------------------------------------------\n";
+        String farewellMessage = "Farewell!";
         System.out.println(farewellMessage);
+        lineBreak();
     }
 
     private String acceptInput() {
-        Scanner scn = new Scanner(System.in);
-        String input = scn.nextLine().toLowerCase();
-
+        
+        String input = this.scn.nextLine().toLowerCase();
         return input;
+    }
+
+    private void processInput(String input) {
+        if (input.equals("list")) {
+            showList();
+        } else {
+            addToList(input);
+        }
+    }
+
+    private void addToList(String item) {
+        this.toDoList.add(item);
+        System.out.println("Added " + item + " to list");
+        lineBreak();
+    }
+
+    private void showList() {
+        System.out.println("Your list is as follows:\n " + "-------------------");
+        for (String item : toDoList) {
+            System.out.println(item);
+        }
+        lineBreak();
     }
 
 
