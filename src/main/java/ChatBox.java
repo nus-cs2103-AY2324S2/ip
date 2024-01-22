50d957df22af17fccd8701e52af2a417dddae3fb
0-yibai <2802756281@qq.com>
2024-01-22 11:45:34 +0800

    added echo and "bye"-exit

--- src/main/java/ChatBox.java
+++ src/main/java/ChatBox.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
+
 public class ChatBox {
+    Scanner scanner = new Scanner(System.in);
+    String input;
 
     public ChatBox() {
+        this.input = "";
     }
 
     public void launch() {
-        System.out.println("____________________________________________________________\n"
-                + "Hello! I'm Wis.\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n");
+        greet();
+        while (!isExitSignal()) {
+            echo();
+            this.input = scanner.nextLine();
+        }
+        bye();
     }
+
+    private void greet() {
+        System.out.println("    ____________________________________________________________\n"
+                + "    Hello! I'm Wis.\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________");
+    }
+
+    private boolean isExitSignal() {
+        return this.input.equals("bye");
+    }
+
+    private void echo() {
+        if (this.input.isEmpty()) {
+            return;
+        }
+        System.out.println("    ____________________________________________________________\n"
+                + "    " + this.input + "\n"
+                + "    ____________________________________________________________");
+
+    }
+
+    private void bye() {
+        System.out.println("    ____________________________________________________________\n"
+                + "    Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________");
+    }
+
 }
