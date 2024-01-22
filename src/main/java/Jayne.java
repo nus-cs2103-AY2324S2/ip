bc281f62a15c17a88f58efd5486b7d2b2e5a2de7
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-01-22 17:04:20 +0800

    Level-1

--- src/main/java/Jayne.java
+++ src/main/java/Jayne.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 public class Jayne {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String dash = "___________________________________";
         System.out.println(dash);
         System.out.println("Hello, I'm Jayne");
         System.out.println("What can I do for you?\n" + dash);
-        System.out.println("Bye. Hope to see you again soon!\n" + dash);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            //enterd bye
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n" + dash);
+                break;
+            }
+
+            //Echo
+            System.out.println(dash + "\n" + input + "\n" + dash);
+        }
+
+        scanner.close();
     }
 }
