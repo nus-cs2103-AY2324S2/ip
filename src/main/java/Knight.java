941b014f7905cb9799955a315ec1e74512b9d159
narwhalsilent <110954060+narwhalsilent@users.noreply.github.com>
2024-01-23 22:02:07 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Knight.java
@@ -0,0 +1,15 @@
+public class Knight {
+    public static void main(String[] args) {
+        String logo = "┓┏┓  •  ┓  \n"
+                + "┃┫ ┏┓┓┏┓┣┓╋\n"
+                + "┛┗┛┛┗┗┗┫┛┗┗\n"
+                + "       ┛\n";
+
+
+        System.out.println("Greetings, Your Excellency! Thy humble\n"
+                + logo
+                + "doth stand before thee. How may I serve thee on this day?");
+
+        System.out.println("Farewell, Your Excellency! May we cross paths once more in the near future.");
+    }
+}
