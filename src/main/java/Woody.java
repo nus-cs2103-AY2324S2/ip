6c6de77497ee07324315effc467b8f3327ca1c22
ryanlimdx <ryanlimdx@gmail.com>
2024-01-23 19:27:43 +0800

    renamed file and class with chatbot's new name

---
+++ src/main/java/Woody.java
@@ -0,0 +1,12 @@
+public class Woody {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Woody\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.print(greeting);
+    }
+}
