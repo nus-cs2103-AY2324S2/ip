f57d0c26a08e4664f3be1e166ecbf30b9ba36c46
soons1 <103474763+soons1@users.noreply.github.com>
2024-01-23 16:52:57 +0800

    Updated to fit Level 1 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     private static String lineBreak = "______________________________________________";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         greet();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String command = "";
+        while (!(command = br.readLine()).equals("bye")) {
+            System.out.println(lineBreak);
+            System.out.println(" " + command);
+            System.out.println(lineBreak);
+        }
         exit();
     }
 
