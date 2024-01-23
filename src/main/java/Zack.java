7cf068fc7f517eb0b7f88152cfb684b114503f99
zhuuyicheng <zhu.yicheng@yahoo.com>
2024-01-23 18:28:22 +0800

    Added Task.java to implement A-Classes

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Zack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         // Greeting message
@@ -16,28 +16,39 @@ public class Zack {
         // Listen for commands and exits when user types "bye"
         while (true) {
             String input = scanner.nextLine();
+            String[] sections = input.split(" ", 2);
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (sections.length > 1 && sections[0].equals("mark")) {
+                int index = Integer.parseInt(sections[1]) - 1;
+                tasks[index].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+                System.out.println("____________________________________________________________\n");
+            } else if (sections.length > 1 && sections[0].equals("unmark")) {
+                int index = Integer.parseInt(sections[1]) - 1;
+                tasks[index].unmark();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+                System.out.println("____________________________________________________________\n");
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________\n");
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
                 System.out.println("____________________________________________________________\n");
             }
-
-            // Echo the command
-
         }
     }
 
