8da455e775e41f8a32e584b57032519e069d7848
bryanyee33 <bryanyee33@gmail.com>
2024-01-23 15:12:27 +0800

    Add listing functionality

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,14 +4,30 @@ public class Bob {
         String line = "____________________________________________________________\n";
         Scanner sc = new Scanner(System.in);
         String input = "";
+        String[] inputs = new String[100];
+        int curr = 0;
 
         System.out.println(line + "Hello! I'm Bob.\nWhat can I do for you?");
 
-        while (!"bye".equals(input)) {
+        while (true) {
             System.out.println(line);
             input = sc.nextLine();
-            System.out.println(line + input);
+            System.out.print(line);
+
+            if ("bye".equals(input)) {
+                break;
+            }
+
+            if ("list".equals(input)) {
+                for (int i = 0; i < curr; i++) {
+                    System.out.println(i + 1 + ". " + inputs[i]);
+                }
+            } else {
+                inputs[curr++] = input;
+                System.out.println("added: " + input);
+            }
         }
+
         System.out.println("Bye! You'll be back ;)\n" + line);
     }
 }
