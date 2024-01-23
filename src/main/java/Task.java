04e85cdea4d6ca29b2a7315540fd1438fe0c6a60
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-01-24 00:46:38 +0800

    Add Task and TaskStatus
    TaskStatus is an enum to track marked and unmarked tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    String text;
+    TaskStatus status;
+
+    Task(String text, TaskStatus status) {
+        this.text = text;
+        this.status = status;
+    }
+
+    public void mark() {
+        this.status = TaskStatus.MARKED;
+    }
+
+    public void unmark() {
+        this.status = TaskStatus.UNMARKED;
+    }
+
+    @Override
+    public String toString() {
+        String mark = status == TaskStatus.MARKED ? "X" : " ";
+        return "[" + mark + "] " + text;
+    }
+}
---
+++ src/main/java/TaskStatus.java
@@ -0,0 +1,4 @@
+public enum TaskStatus {
+    MARKED,
+    UNMARKED
+}
