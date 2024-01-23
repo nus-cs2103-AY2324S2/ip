8eb7d2b20cf02784b3aa201bbafa033299dd27d2
agreatdayy <xy.lee1099@gmail.com>
2024-01-23 13:12:35 +0800

    level 2 - add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,6 +12,8 @@ public class Duke {
                     + " ---- -   - -  -  ---  ---   --      \n";
         String horizontalLine = "_____________________________________________________\n";
         String userInput = "";
+        String[] list = new String[100];
+        int listCount = 0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println(logo + horizontalLine
@@ -24,12 +26,20 @@ public class Duke {
 
         while (!userInput.equalsIgnoreCase("bye")) {
             System.out.println(horizontalLine + ">> " + userInput + " <<");
-//            // If input = "help", list down the command words.
-//            if (userInput.equals("help")) {
-//                System.out.println("Oink! Here are the Command Words:\n'list' - displays the list of task\n'task' - to add new task\n"
-//                        + "'deadline' - to add task with deadline\n'event' - to add an event\n"
-//                        + "'bye' - to exit the chatbot");
-//            }
+            // If input = "help", list down the command words.
+            if (userInput.equalsIgnoreCase("help")) {
+                System.out.println("Oink! Here are the Command Words:\n'list' - displays the list of task\n'task' - to add new task\n"
+                        + "'deadline' - to add task with deadline\n'event' - to add an event\n"
+                        + "'bye' - to exit the chatbot");
+            } else if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < listCount; i++) {
+                    System.out.println(list[i]);
+                }
+            } else {
+                listCount++;
+                list[listCount-1] = listCount + ". " + userInput;
+                System.out.println("added " + userInput);
+            }
             System.out.println(horizontalLine);
             userInput = scan.nextLine();
         }
