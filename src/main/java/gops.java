644654445353991e73f07aa35e070be6429714c5
bgopi23 <baskargopinath01@gmail.com>
2024-01-23 12:31:30 +0800

    Update gops.java

--- src/main/java/gops.java
+++ src/main/java/gops.java
@@ -1,9 +1,26 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class gops {
+    public static class ListItem {
+        protected String itemDescription;
+        protected boolean itemStatus = false;
+        public ListItem(String itemDescription) {
+            this.itemDescription = itemDescription;
+        }
+        public String doneOrNot() {
+            if (itemStatus) {
+                return "X";
+            } else {
+                return " ";
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner inputTaker = new Scanner(System.in);
-        String[] userDataArray = new String[100];
+        ListItem[] toDoList = new ListItem[100];
         int messageCount = 0;
 
         System.out.println("Hello! I'm gops");
@@ -11,13 +28,30 @@ public class gops {
 
         String userReply = inputTaker.nextLine();
         while (userReply != null && !userReply.equals("bye")) {
-            if (userReply.equals("list")) {
+            if (userReply.contains("unmark")) {
+                int toDoListIndex =  Integer.parseInt(userReply.substring(userReply.length() -1)) - 1;
+                toDoList[toDoListIndex].itemStatus = false;
+                System.out.println("OK! I've marked this task as not done yet:");
+                System.out.println("[" + toDoList[toDoListIndex].doneOrNot() + "] " + toDoList[toDoListIndex].itemDescription);
+                userReply = inputTaker.nextLine();
+            }
+            else if (userReply.contains("mark")) {
+                int toDoListIndex =  Integer.parseInt(userReply.substring(userReply.length() -1)) - 1;
+                toDoList[toDoListIndex].itemStatus = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + toDoList[toDoListIndex].doneOrNot() + "] " + toDoList[toDoListIndex].itemDescription);
+                userReply = inputTaker.nextLine();
+            }
+
+            else if (userReply.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < messageCount; i++) {
-                    System.out.println(i + 1 + ". " + userDataArray[i]);
+                    System.out.println(i + 1 + "." + "[" + toDoList[i].doneOrNot() + "] " + toDoList[i].itemDescription);
                 }
                 userReply = inputTaker.nextLine();
             } else {
-                userDataArray[messageCount] = userReply;
+                ListItem toDo = new ListItem(userReply);
+                toDoList[messageCount] = toDo;
                 messageCount += 1;
                 System.out.println("added: " + userReply);
                 userReply = inputTaker.nextLine();
