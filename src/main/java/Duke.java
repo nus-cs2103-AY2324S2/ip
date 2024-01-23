184283c7ec90275b93a421cc28e2d497db231da5
RunjiaChen <runjia@u.nus.edu>
2024-01-23 22:46:34 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
 public class Duke {
     public static void main(String[] args) {
+        Duke d = new Duke();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        d.greet();
+        d.bye();
     }
-    abcd
+
+    public String greet() {
+        return "Hello, I'm Baymax " + "\n" + "What can I do for you?";
+    }
+
+    public String bye() {
+        return "Bye. Hope to see you again soon!";
+    }
+
 }
