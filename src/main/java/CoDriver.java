e2aadd7b956b45c014052c3b3911dd8d0f2d0574
1rbg <gabrielchua29@gmail.com>
2024-01-23 19:53:10 +0800

    Revert "Implemented echo functionality"
    This reverts commit 7c39a479ac71385d284eff9e1305c54f90c61525.

--- src/main/java/CoDriver.java
+++ src/main/java/CoDriver.java
@@ -1,34 +1,15 @@
-import java.util.Objects;
-import java.util.Scanner;
-
 public class CoDriver {
     public static void main(String[] args) {
         greeting();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String command = scanner.nextLine();
-            if (Objects.equals(command, "bye")) {
-                break;
-            }
-            printSepLine();
-            System.out.println(command);
-            printSepLine();
-        }
-        goodbye();
     }
 
     private static void printSepLine() {
         System.out.println("------------------------------------------------");
     }
-
     private static void greeting() {
         printSepLine();
         System.out.println("Hello! I'm CoDriver, your everyday AI companion!");
         System.out.println("What can I do for you?");
-        printSepLine();
-    }
-
-    private static void goodbye() {
         printSepLine();
         System.out.println("Bye. Hope to see you again soon!");
         printSepLine();
