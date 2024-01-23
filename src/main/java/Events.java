9b27fad6080faf60961a49f09c26dcad30714ad1
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-01-23 20:19:09 +0800

    feat: finished impl for Events.java

---
+++ src/main/java/Events.java
@@ -0,0 +1,16 @@
+class Events extends Task {
+
+    private String from;
+    private String to;
+
+    public Events(String description, boolean isDone, String from, String to) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+    }
+}
