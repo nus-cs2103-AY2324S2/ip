29c99ca6a67a8e1330477f72169c0d53237d70c1
nigel <nigellim27022001@gmail.com>
2024-01-23 17:50:04 +0800

    level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -30,6 +31,8 @@ public class Duke {
             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀\n" +
             "⠀⠀⠀⠀⠀⠀⠀⠘⠦⣠⠀⠀⠠⠀⡀⠀⠀⣘⢦⠀⢤⡄⠀⠈⠡⡄⡀⠀⡀⣄⠈⠀⠀⠐⠀⠀⠀⠈⠀⠀⠀⠀\n" +
             "⠀⠀⠀⠀⠐⠶⠠⠄⠠⠀⠀⠤⠀⠤⠄⠀⡰⡉⠈⠱⠘⠀⡄⣀⠀⠀⠀⠠⠀⠭⠰⠶⠤⠆⠰⡶⠘⢤⠄⠀⢀⠀";
+
+    private static ArrayList<String> storage = new ArrayList<>();
     private static void greeting() {
         System.out.println("Hello! I'm Pengu\n" + "What can I do for you? \n" + Duke.logo +
                 "\nDid you know that the noise penguins make are called \"honks\"");
@@ -40,6 +43,19 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon! **HONK HONK**");
         System.out.println("―――――――――――――――――――――――――――――――――――");
     }
+
+    private static void addStorage(String text){
+        Duke.storage.add(text);
+    }
+
+    private static void listStorage(){
+        int storageSize = Duke.storage.size();
+        for (int k = 0; k < storageSize; k++){
+            int curr = k + 1;
+            System.out.println(curr + ". " + Duke.storage.get(k));
+        }
+    }
+
     public static void main(String[] args) {
         greeting();
         Scanner s = new Scanner(System.in);
@@ -48,12 +64,12 @@ public class Duke {
             if (userInput.toLowerCase().equals("bye")) {
                 exit();
                 break;
-            } else if (userInput.toLowerCase().equals("honk")) {
-                System.out.println("What are you? A PENGUIN?? *HONKS ANGRILY*\n" +
-                        "―――――――――――――――――――――――――――――――――――");
+            } else if (userInput.toLowerCase().equals("list")){
+                listStorage();
                 continue;
             }
-            System.out.println(userInput + " *HONK! HONK!*\n" +
+            addStorage(userInput);
+            System.out.println("Added: " + userInput + "\n*Honk! Honk!*\n" +
                     "―――――――――――――――――――――――――――――――――――");
         }
     }
