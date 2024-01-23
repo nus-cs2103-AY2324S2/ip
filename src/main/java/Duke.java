2e236ac2bf4845e8b9b90bb98fd106a68d660c7d
Alphajae11 <leejaegeun20@gmail.com>
2024-01-23 21:30:51 +0800

    Accomplished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greet = "Hello! I'm DOUMMI\n" +
                 "What can I do for you?";
 
+        System.out.println(greet);
+
+        Scanner cmd = new Scanner(System.in);
+
+        while (!cmd.hasNext("bye")) {
+            String userCmd = cmd.nextLine();
+            System.out.println(userCmd);
+        }
+
+
         String bye = "Hope to see you again soon!";
 
-        System.out.println(greet);
         System.out.println(bye);
     }
 }
