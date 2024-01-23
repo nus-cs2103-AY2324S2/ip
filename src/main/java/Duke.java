6e862ae31a2ae771e40f0bef1ea467e4d185d23e
yorklim <yorklim1016@gmail.com>
2024-01-23 15:38:22 +0800

    Added Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Duke {
 
+    Scanner scanner = new Scanner(System.in);
     private String name = "NotDuke";
 
     private void greet() {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+    }
+
+    private void input() {
+        String input = scanner.next();
+        if (input.equals("bye")) {
+            return;
+        }
+        echo(input);
+        input();
+    }
+
+    private void echo(String input) {
+        System.out.println(input);
     }
 
     private void exit() {
@@ -13,6 +28,7 @@ public class Duke {
     public static void main(String[] args) {
         Duke bot = new Duke();
         bot.greet();
+        bot.input();
         bot.exit();
     }
 }
