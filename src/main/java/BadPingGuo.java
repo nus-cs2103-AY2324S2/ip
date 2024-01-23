94f35d68a7dd4fc30cefb2df098c76e647e613c7
Guo Yuhao <guoyuhao4@gmail.com>
2024-01-24 02:12:52 +0800

    Initialise entry and exit lines

--- src/main/java/BadPingGuo.java
+++ src/main/java/BadPingGuo.java
@@ -1,10 +1,13 @@
-public class Duke {
+public class BadPingGuo {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Welcome to White Space!");
+        System.out.println("Waiting for something to happen?");
+        System.out.println("--------------------------------");
+        System.out.println("Everything is going to be okay.");
     }
 }
