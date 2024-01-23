a4d75d55ff12bf41ad86e0efbf1bdcf1df7efb0c
Carl <e0959394@u.nus.edu.sg>
2024-01-23 18:48:15 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = 
+        " _               _          \n" +
+        "| |   _   _  ___| | ___   _ \n" +
+        "| |  | | | |/ __| |/ / | | |      |\\__/,|   (`\\\n" +
+        "| |__| |_| | (__|   <| |_| |    _.|o o  |_   ) )\n" +
+        "|_____\\__,_|\\___|_|\\_\\\\__, |  -(((---(((--------\n" +
+        "                      |___/ ";
+
+        String greeting = "Hello! I'm Lucky the cat. \nWhat can I do for you?";
+        String bye = "Goodbye my friend. See you soon!";
+        String divider = "____________________________________________________________";
+        System.out.println(divider + "\n" + logo + "\n\n" + greeting + "\n" + divider);
+        System.out.println(bye);
     }
 }
