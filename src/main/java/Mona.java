7ef83492afb404a24b43ed024d88d833f9167227
gavingoh99 <gavingoh99@gmail.com>
2024-01-23 22:08:41 +0800

    added features for level 1

---
+++ src/main/java/Mona.java
@@ -0,0 +1,11 @@
+public class Mona {
+    public static void main(String[] args) {
+        String introduction = "____________________________________________________________\n"
+                + " Hello! I'm Mona\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(introduction);
+    }
+}
