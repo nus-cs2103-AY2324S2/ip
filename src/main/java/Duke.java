da1bf45d3f586fdfc4acbde14e61487e19fcc99f
theman-oj10 <manojnarender@gmail.com>
2024-01-23 12:03:03 +0800

    Duke Class: change name and initial greet of Duke
    Duke class has some default greet behaviour and a default name.
    
    The default behaviour and name do not fit the project requirements.
    
    Changing the default behaviour and name will make the class adhere to project requirements.
    
    Let's
    * Change the name to Alfred
    * Add the Initial greet as per project requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "------------------------------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Alfred");
+        System.out.println("What can I do for you?\n");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(line);
     }
 }
