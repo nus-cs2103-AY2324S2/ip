747341aa5b557aa51e175e91b9417d58af018031
yorklim <yorklim1016@gmail.com>
2024-01-23 14:38:28 +0800

    Added Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
+
+    private String name = "NotDuke";
+
+    private void greet() {
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+    }
+
+    private void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke bot = new Duke();
+        bot.greet();
+        bot.exit();
     }
 }
