dc65b4749cc72ab744cdda55052cd2affd5910e5
Kappaccinoh <lim.jia28@gmail.com>
2024-01-23 22:32:45 +0800

    Added task class and modified campus class logic

--- src/main/java/Campus.java
+++ src/main/java/Campus.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains the logic for the ChatBot named 'Campus'
+ */
 public class Campus {
-    static List<String> tasks = new ArrayList<>();
+    static List<Task> tasks = new ArrayList<>();
 
+    /**
+     * Main Driver Logic of the Campus Class which handles user inputs and sorts them into cases
+     */
     public static void main(String[] args) {
         Campus.greet();
 
+        // Main Logic to keep the Chat Bot running until a user decides to quit talking by sending 'bye'
         String userInput;
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -19,20 +26,25 @@ public class Campus {
             } else if (Objects.equals(userInput, "list")) {
                 Campus.display();
             } else {
-                Campus.add(userInput);
+                Task task = new Task(userInput);
+                Campus.add(task);
             }
         }
 
         Campus.exit();
     }
 
+    /**
+     * Displays all the tasks currently stored in the Campus Class by iterating through the list of type Task
+     * and adding it to a final string
+     */
     public static void display() {
         int numOfTasks = Campus.tasks.size();
-        String listOfTasks = "";
+        StringBuilder listOfTasks = new StringBuilder();
         for (int i = 0; i < numOfTasks; i++) {
             int listNum = i + 1;
-            String task = Campus.tasks.get(i);
-            listOfTasks += String.format("%s. %s\n", listNum, task);
+            Task task = Campus.tasks.get(i);
+            listOfTasks.append(String.format("%s. %s\n", listNum, task.toString()));
         }
 
         String message = "------------------------------\n"
@@ -42,11 +54,15 @@ public class Campus {
         System.out.println(message);
     }
 
-    public static void add(String input) {
-        Campus.tasks.add(input);
+    /**
+     * Adds a Task instance to the list of tasks in the Campus Class
+     * @param task an instance of the Task Class
+     */
+    public static void add(Task task) {
+        Campus.tasks.add(task);
 
         String message = "------------------------------\n"
-                + String.format("added: %s\n", input)
+                + String.format("added: %s\n", task.toString())
                 + "------------------------------\n";
 
         System.out.println(message);
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+/**
+ * Class object which represents a Task, which has a name and a status completion
+ */
+public class Task {
+
+    boolean completed;
+    final String taskName;
+
+    /**
+     * @param taskName
+     * completed? - is set to false by default
+     */
+    Task (String taskName) {
+        this.taskName = taskName;
+        this.completed = false;
+    }
+    public static void main(String[] args) {
+
+    }
+
+    public void markComplete() {
+        this.completed = true;
+    }
+
+    public void markIncomplete() {
+        this.completed = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.taskName;
+    }
+}
