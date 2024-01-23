ea8589a13ff75fdb96e6dec8962f036b5303ed0f
Austin Tan Jun Heng <austintjh19@gmail.com>
2024-01-23 16:29:58 +0800

    Level 1. Echo. { Removed chat interface class }

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        ChatInterface chat = new ChatInterface();
-        chat.startChat();
+        final Osiris chatBot = new Osiris();
+        chatBot.startChat();
     }
 }
--- src/main/java/Osiris.java
+++ src/main/java/Osiris.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Osiris {
 
     public static final String NAME = "Osiris";
+
+    public void startChat(){
+        Scanner scanner = new Scanner(System.in);
+        boolean terminateChat = false;
+
+        this.printSeparator();
+        this.outputIntroductions();
+        this.printSeparator();
+
+        while (!terminateChat){
+
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                terminateChat = true;
+            } else {
+                this.echoUserInput(userInput);
+            }
+        }
+
+        this.printSeparator();
+        this.outputGoodbyes();
+        this.printSeparator();
+    }
 
     public void outputIntroductions() {
         String introductions = String.format("Hello! I'm %s. \nWhat can I do for you?", Osiris.NAME);
@@ -10,5 +36,17 @@ public class Osiris {
     public void outputGoodbyes() {
         String goodbyes = "Bye. Hope to see you again soon!";
         System.out.println(goodbyes);
+    }
+
+    private void echoUserInput(String input) {
+        System.out.print("     ");
+        this.printSeparator();
+        System.out.println("     " + input);
+        System.out.print("     ");
+        this.printSeparator();
+    }
+
+    private void printSeparator() {
+        System.out.println("----------------------------------------");
     }
 }
