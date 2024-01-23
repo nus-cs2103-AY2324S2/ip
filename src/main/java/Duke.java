0e48e8eb3f494fdc256044b74ac48fb75aedc1df
Chu Jie <chujie2003@gmail.com>
2024-01-23 17:41:24 +0800

    finished level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hello! My name is Mitsuki, nice to meet you!\n" + "What can I do for you today?\n");
+        String answer = "nil";
+        while (!answer.equals("bye")) {
+            answer = scan.next();
+            if (!answer.equals("bye")) {
+                System.out.println("Did you say: " + answer + "?");
+            }
+        }
+
 
         System.out.println("Bye! Hope to see you again soon!\n");
         System.exit(0);
+
     }
 }
