63116ce9540102c99cf4013863ca12ca9eead691
bertrandong <bertrandongchangheng@gmail.com>
2024-01-23 13:14:53 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userInput;
+        ArrayList<String> taskList = new ArrayList<>();
 
         System.out.println("Hello! I'm Bert!");
         System.out.println("What can I do for you?");
@@ -12,8 +14,14 @@ public class Duke {
             userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + "." + taskList.get(i));
+                }
+            } else {
+                taskList.add(userInput);
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
         } while (true);
 
         System.out.println("Bye! Hope to see you again!");
