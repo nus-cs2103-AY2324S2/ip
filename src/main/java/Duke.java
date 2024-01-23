1db093bc1d465e5b7ff691b5e751d68cc4dcb0ba
qinboan <e0486958@u.nus.edu>
2024-01-24 00:51:55 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class Duke {
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?\n");
@@ -12,11 +14,25 @@ public class Duke {
             if ("bye".equals(input)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if ("list".equals(input)) {
+                listTasks();
+            } else {
+                addTask(input);
             }
-
-            System.out.println(input + "\n");
         }
 
         scanner.close();
+    }
+    private static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task + "\n");
+    }
+
+    private static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        System.out.println("");
     }
 }
