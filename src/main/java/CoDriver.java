9fcc1cdc2b1baa268485e720858049c54b851ed4
1rbg <gabrielchua29@gmail.com>
2024-01-23 19:41:06 +0800

    Level-0

---
+++ src/main/java/CoDriver.java
@@ -0,0 +1,17 @@
+public class CoDriver {
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    private static void printSepLine() {
+        System.out.println("------------------------------------------------");
+    }
+    private static void greeting() {
+        printSepLine();
+        System.out.println("Hello! I'm CoDriver, your everyday AI companion!");
+        System.out.println("What can I do for you?");
+        printSepLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printSepLine();
+    }
+}
