1067639dc42fb8f52df0e0cf87ddf40c2682dcf6
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-01-23 20:52:35 +0800

    Rename Duke to Chimp and setup greet text

---
+++ src/main/java/Chimp.java
@@ -0,0 +1,11 @@
+public class Chimp {
+    public static void main(String[] args) {
+        String greet = "____________________________________________________________\n" +
+                " Hello! I'm Chimp\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greet);
+    }
+}
