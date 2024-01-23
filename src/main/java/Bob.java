e1391037a7721a4121905aa17e5c7e9b4b8172eb
bryanyee33 <bryanyee33@gmail.com>
2024-01-23 16:17:17 +0800

    Add Task class and add checked Task feature

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,7 +4,7 @@ public class Bob {
         String line = "____________________________________________________________\n";
         Scanner sc = new Scanner(System.in);
         String input = "";
-        String[] inputs = new String[100];
+        Task[] list = new Task[100];
         int curr = 0;
 
         System.out.println(line + "Hello! I'm Bob.\nWhat can I do for you?");
@@ -18,12 +18,41 @@ public class Bob {
                 break;
             }
 
+            // list all Tasks
             if ("list".equals(input)) {
                 for (int i = 0; i < curr; i++) {
-                    System.out.println(i + 1 + ". " + inputs[i]);
+                    System.out.println(i + 1 + "." + list[i]);
                 }
+
+            // mark Task as done
+            } else if (input.length() >= 4 && "mark".equals(input.substring(0, 4))) {
+                try {
+                    int index = Integer.parseInt(input.substring(5));
+                    list[index - 1].markAsDone();
+                    System.out.println("Did you actually finish this? \uD83E\uDD14:\n" +
+                                       "  " + list[index - 1]);
+
+                } catch (NumberFormatException | NullPointerException |
+                         StringIndexOutOfBoundsException e) {
+                    System.out.println("Incorrect mark format!\nDo: mark <task_number>");
+                }
+
+            // mark Task as undone
+            } else if (input.length() >= 6 && "unmark".equals(input.substring(0, 6))) {
+                try {
+                    int index = Integer.parseInt(input.substring(7));
+                    list[index - 1].markAsUndone();
+                    System.out.println("I knew you didn't finish it \uD83D\uDE0F:\n" +
+                                       "  " + list[index - 1]);
+
+                } catch (NumberFormatException | NullPointerException |
+                         StringIndexOutOfBoundsException e) {
+                    System.out.println("Incorrect unmark format!\nDo: unmark <task_number>");
+                }
+
+            // add Task
             } else {
-                inputs[curr++] = input;
+                list[curr++] = new Task(input);
                 System.out.println("added: " + input);
             }
         }
