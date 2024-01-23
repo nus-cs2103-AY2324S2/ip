0c8dcce393d2856db855ed04a8ae176d7e067ad3
JOELPC\joelw <omegabeeminecraft@gmail.com>
2024-01-24 03:57:27 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Duke {
+    static String line = "____________________________________________________________";
+    static String name = "Alfred";
+
+    public static void separate(){
+        System.out.println(line);
+    }
+    public static void intro() {
+        separate();
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        separate();
+    }
+    public static void leave() {
+        System.out.println("Bye. Hope to see you again soon!");
+        separate();
+    }
+    
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        intro();
+        leave();
     }
 }
