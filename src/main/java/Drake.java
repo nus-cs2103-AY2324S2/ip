8812e1586c8ab979e68b1940155255345909a3e4
Anant Shanker <shankeranant@gmail.com>
2024-01-24 00:22:03 +0800

    Add echo feature

--- src/main/java/Drake.java
+++ src/main/java/Drake.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Drake {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Drake");
-        System.out.println(" What can I do for you?");
+        System.out.println(" What's up everyone. I'm Drake.");
+        System.out.println(" How can I help?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" See you later, alligator! ");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(' ' + input);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
+
+
     }
 }
