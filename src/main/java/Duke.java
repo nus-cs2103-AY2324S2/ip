11c0b4a3d515dfe7a87d22e74aa1fcb45a26f17d
fy17ohhh <hester03sfy@gmail.com>
2024-01-23 23:11:30 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static boolean exitProg = false;
+    public static String line = "\n——————————————————————————————————————————\n";
+
     public static void main(String[] args) {
-        String line = "\n——————————————————————————————————————————\n";
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
-        System.out.println(line + "Hello! I'm Jade\nWhat can I do for you?" + line + "Bye. Hope to see you again soon!" + line);
+
+        String logo = "  ____   ___    ____     ______  \n"
+                    + "  |  |  / _ \\ |  ___ \\  / |____/ \n"
+                    + "  |  | | | | | | |  | | | |____  \n"
+                    + "  |  | | |_| | | |  | | | |____| \n"
+                    + "|\\|  | | ___ | | |__| | | |____  \n"
+                    + " \\___| |_| |_| |_____/  \\_|____\\ \n";
+
+        System.out.println(String.format("%s%sHello! I'm Jade\nWhat can I do for you?%s", logo, line, line ,line));
+        Scanner scanner = new Scanner(System.in);
+        while(!exitProg) {
+            String input = scanner.nextLine();
+            echo(input);
+        }
+    }
+
+    public static void echo(String command) {
+        switch(command) {
+            case "bye":
+                System.out.println(line + "Bye. Hope to see you again soon!" + line);
+                exitProg = true;
+                break;
+            default:
+                System.out.println(line + command + line);
+                break;
+        }
+
     }
 }
