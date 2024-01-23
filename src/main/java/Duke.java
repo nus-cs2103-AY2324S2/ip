b6a2bbce3e087cbd7e8aa053001965da4cbb338a
menatahari <natasyabuhari@gmail.com>
2024-01-24 01:14:04 +0800

    update line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,33 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Buto {
+public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = "    ____________________________________________________________\n";
         ArrayList<String> textList = new ArrayList<>();
 
         System.out.println(
-                "    ____________________________________________________________\n" +
+                line +
                         "     Hello! I'm Buto\n" +
                         "     What can I do for you?\n" +
-                        "    ____________________________________________________________\n"
+                        line
         );
 
         String command = br.readLine();
-        
+
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                System.out.println("    ____________________________________________________________\n");
+                System.out.println(line);
                 for (int i = 1; i <= textList.size(); i++) {
-                    System.out.println(String.format("    %d. %s\n", i, textList.get(i-1)));
+                    System.out.printf("    %d. %s\n", i, textList.get(i-1));
                 }
-                System.out.println("    ____________________________________________________________\n");
+                System.out.println(line);
             } else {
                 textList.add(command);
                 System.out.println(
-                        "    ____________________________________________________________\n" +
+                        line +
                                 String.format("    added: %s\n", command) +
-                                "    ____________________________________________________________\n"
+                                line
                 );
             }
             command = br.readLine();
@@ -38,9 +39,9 @@ public class Buto {
         br.close();
 
         System.out.println(
-                "    ____________________________________________________________\n" +
+                line +
                         "     Bye. Hope to see you again soon!\n" +
-                        "    ____________________________________________________________\n"
+                        line
         );
     }
 }
