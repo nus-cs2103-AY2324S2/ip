c42ef829e11aa5a0b44b243f12df1e228720e9d8
tanqinyong <tqinyong@yahoo.com.sg>
2024-01-23 22:17:38 +0800

    [Level-0] Changed chatbot name and greetings.

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,20 @@
+/** Encapsulates the generic greeting for chatbot.
+ *  Made an abstract class as it doesn't make sense to make a greeting object.
+ * @author Tan Qin Yong
+ */
+public abstract class Greeting {
+    // Prints the default greeting message.
+    public static void print() {
+        String name = "Felix";
+        String logo = " _____    _ _      \n"
+                + "|  ___|__| (_)_  __\n"
+                + "| |_ / _ \\ | \\ \\/ /\n"
+                + "|  _|  __/ | |>  < \n"
+                + "|_|  \\___|_|_/_/\\_\\\n";
+        System.out.println(logo);
+        System.out.println("------------------------------------");
+        System.out.println("I'm " + name + "!");
+        System.out.println("What may I do for you today? ");
+        System.out.println("------------------------------------");
+    }
+}
