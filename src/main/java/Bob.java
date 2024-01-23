fc2002cb92dcbecbd9df6963afdf6b823ade7412
bryanyee33 <bryanyee33@gmail.com>
2024-01-23 14:21:36 +0800

    Rename bot and change initial message

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line + "Hello! I'm Bob.\nWhat can I do for you?\n"
+                         + line + "Bye. Hope to see you again soon!\n" + line);
+    }
+}
