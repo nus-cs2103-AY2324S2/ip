17ecd4094a2954a9b1ba1fbaff39a849b5b832dc
bertrandong <bertrandongchangheng@gmail.com>
2024-01-23 11:29:41 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
         System.out.println("Hello! I'm Bert!");
         System.out.println("What can I do for you?");
+
+        do {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(userInput);
+        } while (true);
+
         System.out.println("Bye! Hope to see you again!");
+        scanner.close();
     }
 }
