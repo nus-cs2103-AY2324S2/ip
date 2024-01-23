fc88ddc08c515a2294e514262e748505b156f1a2
zhuuyicheng <zhu.yicheng@yahoo.com>
2024-01-23 17:13:25 +0800

    Level-0

---
+++ src/main/java/Zack.java
@@ -0,0 +1,14 @@
+public class Zack {
+    public static void main(String[] args) {
+        // Greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Zack");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+}
