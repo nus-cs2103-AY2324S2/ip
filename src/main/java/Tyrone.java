74e36c270ca52ab084ad1116419465902eaf6fec
jmsandiegoo <jmsandiego0710@gmail.com>
2024-01-23 12:23:52 +0800

    Added echo and bye functionality

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -1,18 +1,40 @@
-public class Tyrone {
-    public static void main(String[] args) {
-        String logo = "████████╗██╗   ██╗██████╗  ██████╗ ███╗   ██╗███████╗\n" +
-                "╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔═══██╗████╗  ██║██╔════╝\n" +
-                "   ██║    ╚████╔╝ ██████╔╝██║   ██║██╔██╗ ██║█████╗  \n" +
-                "   ██║     ╚██╔╝  ██╔══██╗██║   ██║██║╚██╗██║██╔══╝  \n" +
-                "   ██║      ██║   ██║  ██║╚██████╔╝██║ ╚████║███████╗\n" +
-                "   ╚═╝      ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝\n";
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
-        System.out.println(logo +
-                "\n____________________________________________________________\n" +
-                "Yo, what's crackin' fam! I'm Tyrone, your digital homie.\n" +
-                "What's the word? So I can help you out today.\n" +
-                "\n____________________________________________________________\n" +
-                "Peace out! Crossin' my fingers for a speedy reunion, ya feel?\n" +
-                "\n____________________________________________________________\n");
+public class Tyrone {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out, true);
+
+        String logo =
+                "    ████████╗██╗   ██╗██████╗  ██████╗ ███╗   ██╗███████╗\n" +
+                        "    ╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔═══██╗████╗  ██║██╔════╝\n" +
+                        "       ██║    ╚████╔╝ ██████╔╝██║   ██║██╔██╗ ██║█████╗  \n" +
+                        "       ██║     ╚██╔╝  ██╔══██╗██║   ██║██║╚██╗██║██╔══╝  \n" +
+                        "       ██║      ██║   ██║  ██║╚██████╔╝██║ ╚████║███████╗\n" +
+                        "       ╚═╝      ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝\n";
+
+        writer.println(logo +
+                "    Yo, what's crackin' fam! I'm Tyrone, your digital homie.\n" +
+                "    What's the word? So I can help you out today.\n" +
+                "\n    ____________________________________________________________\n");
+
+        while (true) {
+            String cmd = reader.readLine().toLowerCase();
+            if (cmd.equals("bye")) {
+                writer.println(Tyrone.formatStringOutput("\"Peace out! Crossin' my fingers for a speedy reunion, ya feel?"));
+                break;
+            } else {
+                writer.println(Tyrone.formatStringOutput(cmd));
+            }
+        }
+    }
+
+    public static String formatStringOutput(String content) {
+        return ("\n    ____________________________________________________________\n" +
+                "    " + content + "\n" +
+                "\n    ____________________________________________________________\n");
     }
 }
