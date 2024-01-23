ba0ff061bff938dbb3c9837d9cca7e0abf0bba6a
ongzhili <ongchili234@gmail.com>
2024-01-23 18:16:11 +0800

    Modified welcome Message Cleared Level 3 and A-classes requirement

--- src/main/java/Lamball.java
+++ src/main/java/Lamball.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Lamball {
     String indent = "    ____________________________________________________________\n";
     String welcomeMessage = "    ____________________________________________________________\n" +
-            "     Hello! I'm Lamball!\n" +
+            "     Hello! I'm Lamball, your helpful sheep!\n" +
             "     What can I do for you?\n" +
             "    ____________________________________________________________\n";
     String goodbyeMessage = "    ____________________________________________________________\n" +
             "     See you again!\n" +
             "    ____________________________________________________________\n";
 
-    String[] tasks;
+    Task[] tasks;
     int counter;
     public Lamball() {
-        tasks = new String[100];
+        tasks = new Task[100];
         counter = 0;
     }
 
@@ -24,14 +24,30 @@ public class Lamball {
     public void printList() {
         System.out.println(indent);
         for(int i = 0; i < counter; i++) {
-            System.out.println("    " + (i + 1) + ". " + tasks[i] + "\n");
+            System.out.println("    " + (i + 1) + ". " + tasks[i].toString() + "");
         }
         System.out.println(indent);
     }
 
     public boolean parse(String msg) {
         String[] parts = msg.split(" ", 2);
+        Task temp;
+        int idx;
         switch(parts[0]) {
+            case "mark":
+                System.out.println(indent + "    I have maaarked the task as done:\n");
+                idx = Integer.valueOf(parts[1]) - 1;
+                temp = tasks[idx];
+                temp.mark();
+                System.out.println("        "+ temp.toString() + "\n" + indent);
+                return true;
+            case "unmark":
+                System.out.println(indent + "    I have maaarked the task as undone:\n" + indent);
+                idx = Integer.valueOf(parts[1]) - 1;
+                temp = tasks[idx];
+                temp.unMark();
+                System.out.println("        " + temp.toString() + "\n" + indent);
+                return true;
             case "bye":
                 System.out.println(goodbyeMessage);
                 return false;
@@ -40,7 +56,7 @@ public class Lamball {
                 return true;
             default:
                 System.out.println(indent + "    added:" + msg + "\n" + indent);
-                tasks[counter] = msg;
+                tasks[counter] = new Task(msg);
                 counter++;
                 return true;
         }
