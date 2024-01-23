d13d92b813bc74f2c83f110b8300104aa20a2c69
LWS49 <e0970244@u.nus.edu>
2024-01-23 11:21:33 +0800

    added Level-3

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -6,26 +6,45 @@ public class Luke {
 
         String name = "Luke";
 
-        String[] text = new String[100];
+        ArrayList<Task> taskList = new ArrayList<Task>();
 
-        int noWords = 0;
+        int noTasks = 0;
 
         System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
 
         Scanner scanner = new Scanner (System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
+            String[] splited = input.split(" ");
             if (input.equals("list")) {
-                for (int i = 0; i < noWords; i++) {
-                    System.out.println((i + 1) + ". " + text[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < noTasks; i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i).printTask());
                 }
+            } else if (splited[0].equals("mark")) {
+
+                int index = Integer.valueOf(splited[1]);
+                Task task = taskList.get(index - 1);
+                task.setToDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(task.printTask());
+
+            } else if (splited[0].equals("unmark")) {
+
+                int index = Integer.valueOf(splited[1]);
+                Task task = taskList.get(index - 1);
+                task.setToNotDone();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(task.printTask());
+
             } else {
-                text[noWords] = input;
-                noWords++;
+                noTasks++;
+                Task task = new Task(noTasks, input);
+                taskList.add(task);
                 System.out.println("added: " + input);
             }
             input = scanner.nextLine();
-        };
+        }
         System.out.println("Bye. Hope to see you again soon!");
 
     }
