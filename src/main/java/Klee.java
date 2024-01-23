b08467edf1aa361e4b03a492b35585c1c405c0ae
BryanL2303 <bryanlee0033@gmail.com>
2024-01-23 22:27:29 +0800

    Completed level 1

--- src/main/java/Klee.java
+++ src/main/java/Klee.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Klee {
     public static void main(String[] args) {
         String divider = "____________________________________________________________________________";
         System.out.println(divider);
         System.out.println("Hello! My name is Klee.");
         System.out.println("Are you here to break Klee out of solitary confinement?");
+        System.out.println(divider);
+        Scanner getInput = new Scanner(System.in);
+        while (true) {
+            String input = getInput.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(divider);
+                System.out.println(input);
+                System.out.println(divider);
+            }
+        };
         System.out.println(divider);
         System.out.println("Goodbye. Klee will go back to solitary confinement now...");
         System.out.println(divider);
