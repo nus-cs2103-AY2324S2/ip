ce6e4cfe45ee02de407210f6dfda427598ccbc80
ivan24004 <ivan@ivant.com>
2024-01-23 10:02:04 +0800

    Added javadocs to the Outputter class

--- src/main/java/io/Outputter.java
+++ src/main/java/io/Outputter.java
@@ -4,6 +4,10 @@ package io;
  * Class that takes in messages and outputs them in the proper format.
  */
 public class Outputter {
+    /**
+     * Display message in a certain format.
+     * @param msg message to be outputted, you do not need to end it with a newline.
+     */
     public static void outputMessage(Message msg) {
         System.out.println(msg.getMessage());
     }
