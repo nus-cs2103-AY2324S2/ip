825c5362ae670f58b5fb1172aacfc93c729bbb89
ivan24004 <ivan@ivant.com>
2024-01-23 09:27:53 +0800

    Added an inputter static class

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -2,12 +2,8 @@ import io.Message;
 import io.Outputter;
 
 public class Chatbot {
-    public Chatbot() {
-
-    }
-
-    public void IoLoop() {
-        Outputter.outputMessage(new Message("Hello! I'm Remi\n What can I do for you?\n"));
+    public static void IoLoop() {
+        Outputter.outputMessage(new Message("Hello! I'm Remi\n" + "What can I do for you?\n"));
         Outputter.outputMessage(new Message("Bye. Hope to see you again soon!\n"));
     }
 }
---
+++ src/main/java/io/Inputter.java
@@ -0,0 +1,16 @@
+package io;
+
+import java.util.Scanner;
+
+public class Inputter {
+    private static Scanner sc = new Scanner(System.in);
+
+    /**
+     * This gets the last line of input from the user.
+     *
+     * @return a message containing the last input of the user
+     */
+    public static Message inputMessage() {
+        return new Message(sc.nextLine());
+    }
+}
