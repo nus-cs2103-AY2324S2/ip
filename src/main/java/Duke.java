ae6fd852104fd52dddc97a6f8be2b917af27c478
larainezo <larainelwx@gmail.com>
2024-01-23 23:35:52 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,35 +12,58 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
          */
 
-        System.out.println("\t\t------------------------------------------");
+        String lineBreak = "\t\t------------------------------------------";
+
+        System.out.println(lineBreak);
         System.out.println("\t\tHello! I'm Duchess");
         System.out.println("\t\tWhat can I do for you?");
-        System.out.println("\t\t------------------------------------------");
+        System.out.println(lineBreak);
 
         Scanner scan = new Scanner( System.in );
         String input = scan.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                System.out.println("\t\t------------------------------------------");
+                System.out.println(lineBreak);
                 System.out.println("\t\t" + list.size() + " item(s) in the list");
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println("\t\t" + i + ". " + list.get(i - 1));
+                    Task t = list.get(i - 1);
+                    System.out.println("\t\t" + i + ".[" + t.getStatusIcon() + "] " + t.getDescription());
                 }
-                System.out.println("\t\t------------------------------------------");
+                System.out.println(lineBreak);
+            }
+            else if (input.contains("unmark")) {
+                System.out.println(input.charAt(7));
+                int item_index = Character.getNumericValue(input.charAt(7));
+                Task t = list.get(item_index-1);
+                t.markAsUndone();
+                System.out.println(lineBreak);
+                System.out.println("\t\tOK, I've marked this task as not done yet:");
+                System.out.println("\t\t  [" + t.getStatusIcon() + "] " + t.getDescription());
+                System.out.println(lineBreak);
+            }
+            else if (input.contains("mark")) {
+                int item_index = Character.getNumericValue(input.charAt(5));
+                Task t = list.get(item_index-1);
+                t.markAsDone();
+                System.out.println(lineBreak);
+                System.out.println("\t\tNice! I've marked this task as done:");
+                System.out.println("\t\t  [" + t.getStatusIcon() + "] " + t.getDescription());
+                System.out.println(lineBreak);
             }
             else {
-                System.out.println("\t\t------------------------------------------");
+                System.out.println(lineBreak);
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println("\t\tAdded: " + input);
-                list.add(input);
-                System.out.println("\t\t------------------------------------------");
+                System.out.println(lineBreak);
             }
             input = scan.nextLine();
         }
 
-        System.out.println("\t\t------------------------------------------");
+        System.out.println(lineBreak);
         System.out.println("\t\tBye. Hope to see you again soon!");
-        System.out.println("\t\t------------------------------------------");
+        System.out.println(lineBreak);
     }
 }
