612d82811e21f27f1c6276818d778c448e35211b
Felix Chan You Yuan <e0959017@u.nus.edu>
2024-01-23 12:15:49 +0800

    Add echos command implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private final String horizontalLine = "____________________________________________________________";
+    private final String horizontalLine = "____________________________________________________________\n";
     private final String name;
 
     public Duke(String name) {
@@ -9,7 +11,7 @@ public class Duke {
 
     public void greetUser() {
         System.out.println(this.horizontalLine
-                + "\nHello! I'm "
+                + "Hello! I'm "
                 + this.name
                 + "\n"
                 + "What can I do for you?\n"
@@ -20,11 +22,32 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!\n" + this.horizontalLine);
     }
 
+    public void echo() {
+
+        Scanner myScannerObj = new Scanner(System.in);
+        System.out.print("Message Zenify: ");
+        while (myScannerObj.hasNext()) {
+            System.out.print(this.horizontalLine);
+            String command = myScannerObj.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                this.exit();
+                break;
+            } else {
+                System.out.println(" " + command);
+            }
+            System.out.print(this.horizontalLine);
+            System.out.print("\nMessage Zenify: ");
+        }
+
+        myScannerObj.close();
+
+    }
+
     public static void main(String[] args) {
 
         Duke zenifyBot = new Duke("Zenify");
         zenifyBot.greetUser();
-        zenifyBot.exit();
+        zenifyBot.echo();
 
     }
 }
