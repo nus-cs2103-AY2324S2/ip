9070e10046460f062d5cd71e20f31816a6b6bacc
gavingoh99 <gavingoh99@gmail.com>
2024-01-23 22:58:24 +0800

    added features for level 3

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -12,7 +12,7 @@ public class Mona {
                 + "   Bye. Hope to see you again soon!\n"
                 + "  ____________________________________________________________";
         Scanner sc = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
         System.out.println(introduction);
         while (true) {
             String input = sc.nextLine();
@@ -23,14 +23,34 @@ public class Mona {
             if (input.equals("list")) {
                 System.out.println("  ____________________________________________________________");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+                    Task currTask = tasks.get(i);
+                    System.out.println("    " + (i + 1) + ". [" + currTask.getStatusIcon() + "] " + currTask.getDescription());
                 }
                 System.out.println("  ____________________________________________________________");
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task currTask = tasks.get(taskIndex);
+                currTask.markAsDone();
+                String response = "  ____________________________________________________________\n"
+                        + "     Nice! I've marked this task as done: \n"
+                        + "     [" + currTask.getStatusIcon() + "] " + currTask.getDescription() + "\n"
+                        + "  ____________________________________________________________\n";
+                System.out.println(response);
+            } else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task currTask = tasks.get(taskIndex);
+                currTask.markAsNotDone();
+                String response = "  ____________________________________________________________\n"
+                        + "     OK, I've marked this task as not done yet: \n"
+                        + "     [" + currTask.getStatusIcon() + "] " + currTask.getDescription() + "\n"
+                        + "  ____________________________________________________________\n";
+                System.out.println(response);
             } else {
                 String response = "  ____________________________________________________________\n"
                         + "     added: " + input + "\n"
                         + "  ____________________________________________________________\n";
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 System.out.println(response);
             }
         }
