2ec8b3e04b2e5b5af227ce766ba8f397a19ffde5
Ryan Chia <e0959970@u.nus.edu>
2024-01-23 22:24:03 +0800

    add classes for Commands and Tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Task {
+    private static final ArrayList<Task> taskList = new ArrayList<>();
+    private final String name;
+
+    private Task(String name) {
+        this.name = name;
+    }
+    
+    public static void add(String name) {
+        taskList.add(new Task(name));
+    }
+    
+    public static String taskString() {
+        // here's your """""effectively final""""" value bro
+        var numBox = new Object() {
+            int num = 1;
+        };
+        return taskList.stream()
+                .reduce(
+                        new StringBuilder(),
+                        (curr, acc) -> {
+                            curr.append(numBox.num).append(". ").append(acc.describe());
+                            if (numBox.num < taskList.size()) {
+                                curr.append("\n");
+                            }
+                            numBox.num++;
+                            return curr;
+                        }, 
+                        StringBuilder::append)
+                .toString();
+    }
+
+    public String describe() {
+        return name;
+    }    
+}
