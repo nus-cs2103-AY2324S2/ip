fc10a5e52bebc2058841fd4d7220d1a3d73e64ed
tsulim <limxiujia18@gmail.com>
2024-01-23 18:11:15 +0800

    Added new classes

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String taskTitle;
+    protected boolean isDone;
+
+    public Task(String taskTitle) {
+        this.taskTitle = taskTitle;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "✓" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return this.taskTitle + " " + this.getStatusIcon();
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    protected ArrayList<Task> taskList;
+    protected int taskCount;
+
+    public TaskList() {
+        this.taskList = new ArrayList<>();
+        this.taskCount = 0;
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+        taskCount++;
+    }
+
+    public Task getTaskByNumber(int taskNum) {
+        return taskList.get(taskNum - 1);
+    }
+
+    public void printList() {;
+        if (taskCount == 0) {
+            System.out.println("There are currently no task added.");
+        } else {
+            System.out.println("Here are the task(s) you have:");
+            for (int i = 0; i < taskList.size(); i++) {
+                int taskNum = i + 1;
+                String taskString = "→ " + taskNum + ". " + taskList.get(i).toString();
+                System.out.println(taskString);
+            }
+        }
+    }
+}
