f2371e323df159bb0bc7a94db500b7b877d60c02
BryanL2303 <bryanlee0033@gmail.com>
2024-01-23 22:14:39 +0800

    Completed level 0

---
+++ src/main/java/Klee.java
@@ -0,0 +1,11 @@
+public class Klee {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________________________";
+        System.out.println(divider);
+        System.out.println("Hello! My name is Klee.");
+        System.out.println("Are you here to break Klee out of solitary confinement?");
+        System.out.println(divider);
+        System.out.println("Goodbye. Klee will go back to solitary confinement now...");
+        System.out.println(divider);
+    }
+}
