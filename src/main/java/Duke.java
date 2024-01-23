a19c0b42e06a037e72a138e1ece955a5067f0fde
LifHoshi <e0907413@u.nus.edu>
2024-01-23 22:45:44 +0800

    Rename,Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Ping\n"+
+                "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
