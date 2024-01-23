717a618b2416ab0c47ba9832cd9700e69d7db6a9
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-01-23 20:13:48 +0800

    feat: added 3 child classes and finished ToDos

---
+++ src/main/java/ToDos.java
@@ -0,0 +1,10 @@
+class ToDos extends Task{
+    public ToDos(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
