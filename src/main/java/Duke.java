70e740bb146cd01ce79304da661e29031f29dc02
shawnnlimm <shawnlim2525@gmail.com>
2024-01-23 17:14:46 +0800

    feat: Add mark as done/undone feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static ArrayList<Task> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("____________________");
         System.out.println("Hello! I'm Waffles");
@@ -9,42 +12,76 @@ public class Duke {
         System.out.println("____________________");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
 
         while (true) {
-            String command = sc.nextLine();
+            String userInput = sc.nextLine();
+            String[] splitInputs = userInput.split("\\s+");
 
-            if (command.equalsIgnoreCase("list")) {
-                listTasks(tasks);
-            } else if (command.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________");
-                break;
+            if (splitInputs.length > 1) {
+                String command = splitInputs[0];
+                if (command.equalsIgnoreCase("mark")) {
+                    String idx = splitInputs[1];
+                    executeMarkTask(idx);
+                } else if (command.equalsIgnoreCase("unmark")) {
+                    String idx = splitInputs[1];
+                    executeUnmarkTask(idx);
+                } else {
+                    echoAndAddTask(userInput);
+                }
+
             } else {
-                echoAndAddTask(command, tasks);
+                String command = splitInputs[0];
+                if (command.equalsIgnoreCase("list")) {
+                    listTasks();
+                } else if (command.equalsIgnoreCase("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________");
+                    break;
+                } else {
+                    echoAndAddTask(userInput);
+                }
             }
+
         }
 
         sc.close();
 
     }
 
-    private static void echoAndAddTask(String task, ArrayList<String> taskList) {
-        taskList.add(task);
-        String output = String.format("added: %s", task);
+    private static void echoAndAddTask(String taskDescription) {
+        Task newTask = new Task(taskDescription);
+        taskList.add(newTask);
+        String output = String.format("added: %s", taskDescription);
         System.out.println("____________________");
         System.out.println(output);
         System.out.println("____________________");
 
     }
 
-    private static void listTasks(ArrayList<String> taskList) {
+    private static void listTasks() {
         System.out.println("____________________");
         for (int i = 0; i < taskList.size(); i++) {
-            String task = String.format("%d. %s", i + 1, taskList.get(i));
-            System.out.println(task);
+            Task task = taskList.get(i);
+            String output = String.format("%d. %s", i + 1, task);
+            System.out.println(output);
         }
         System.out.println("____________________");
+    }
+
+    public static String executeMarkTask(String taskIndex) {
+        Task t = taskList.get(Integer.parseInt(taskIndex) - 1);
+        t.markAsDone();
+        System.out.println("Nice!, I've marked this task as done:");
+        System.out.println(t);
+        return t.toString();
+    }
+
+    public static String executeUnmarkTask(String taskIndex) {
+        Task t = taskList.get(Integer.parseInt(taskIndex) - 1);
+        t.markAsUndone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(t);
+        return t.toString();
     }
 
 }
