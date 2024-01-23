3a2c7b83e0b4996315589a53970cc9b8dc1818b6
mahadhir247 <mahadhir123456@gmail.com>
2024-01-23 20:55:22 +0800

    Added ability to mark/unmark tasks

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,4 +1,3 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,35 +21,62 @@ public class Buddy {
         System.out.println(lineBreak + " Bye. Hope to see you again soon!\n" + lineBreak);
     }
 
-    private void echo() {
+    private void run() {
         boolean running = true;
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (running) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
-            if (input.equals("bye")) {
-                running = false;
-            } else if (input.equals("list")) {
-                System.out.print(lineBreak);
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". "+ taskList.get(i));
+            if (!input.equals("")) {
+                int idx = input.indexOf(" ");
+                String command;
+
+                if (idx >= 0) {
+                    command = input.substring(0, idx);
+                } else {
+                    command = input;
                 }
-                System.out.println(lineBreak);
-            } else {
-                taskList.add(input);
-                String output = "added: " + input + "\n";
-                System.out.println(lineBreak + output + lineBreak);
+
+                running = command(command, input, taskList);
             }
         }
         sc.close();
     }
 
+    private boolean command(String cmd, String input, ArrayList<Task> taskList) {
+        switch (cmd) {
+            case "bye":
+                return false;
+            case "list":
+                System.out.print(lineBreak);
+                System.out.println("Here you go bud!:");
+
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". "+ taskList.get(i));
+                }
+                System.out.println(lineBreak);
+                return true;
+            case "mark":
+            case "unmark":
+                int idx = Integer.parseInt(input.substring(input.indexOf(" ") + 1)) - 1;
+                Task task = taskList.get(idx);
+                task.changeStatus();
+                System.out.println(lineBreak + task + "\n" + lineBreak);
+                return true;
+            default:
+                taskList.add(new Task(input));
+                String output = "added: " + input + "\n";
+                System.out.println(lineBreak + output + lineBreak);
+                return true;
+        }
+    }
+
     public static void main(String[] args) {
         Buddy buddy = new Buddy();
         buddy.greet();
-        buddy.echo();
+        buddy.run();
         buddy.exit();
     }
 }
