72968f9eeb3cf552a0115b3ca5c2b9e8b9922ac4
bryanyee33 <bryanyee33@gmail.com>
2024-01-23 23:10:12 +0800

    Change to ArrayList

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,47 +1,40 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Bob {
     public static boolean startsWith(String keyword, String input) {
         return input.length() >= keyword.length() &&
                 keyword.equals(input.substring(0, keyword.length()));
     }
 
-    private static Task getTaskFromIndex(String keyword, String input, Task[] list) {
+    private static Task getTaskFromIndex(String keyword, String input, ArrayList<Task> list) {
         try {
             int num = Integer.parseInt(input.substring(keyword.length() + 1));
-            Task task = list[num - 1];
-            if (task == null) {
-                throw new BobException("OH NOSE! Task number has not yet been assigned..");
-            }
-            return task;
+            return list.get(num - 1);
 
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OH NOSE! Task number cannot be empty..");
         } catch (NumberFormatException e) {
             System.out.println("OH NOSE! Task number has to be an integer..");
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("OH NOSE! Task number must be in valid range..");
-        } catch (BobException e) {
-            System.out.println(e.getMessage());
         }
         return null;
     }
 
-    private static boolean addTask(Task[] list, int nxtIdx, Task task) {
+    private static void addTask(ArrayList<Task> list, Task task) {
         if (task != null) {
-            list[nxtIdx] = task;
+            list.add(task);
             System.out.println("This task has been added:\n  " + task +
-                               "\n" + "Now you have " + (nxtIdx + 1) +
+                               "\n" + "Now you have " + list.size() +
                                " task(s) in total.");
-            return true;
         }
-        return false;
     }
 
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         Scanner sc = new Scanner(System.in);
-        Task[] list = new Task[100];
-        int nxtIdx = 0;
+        ArrayList<Task> list = new ArrayList<>();
         String input;
 
         System.out.println(line + "Hello, I'm Bob.\nI might help if I feel like it.");
@@ -58,8 +51,8 @@ public class Bob {
             // list all Tasks
             if ("list".equals(input)) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < nxtIdx; i++) {
-                    System.out.println((i + 1) + "." + list[i]);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + "." + list.get(i));
                 }
 
             // mark Task as done
@@ -81,19 +74,13 @@ public class Bob {
                 }
 
             } else if (startsWith(Todo.keyword, input)) {
-                if (addTask(list, nxtIdx, Todo.of(input))) {
-                    nxtIdx++;
-                }
+                addTask(list, Todo.of(input));
 
             } else if (startsWith(Deadline.keyword, input)) {
-                if (addTask(list, nxtIdx, Deadline.of(input))) {
-                    nxtIdx++;
-                }
+                addTask(list, Deadline.of(input));
 
             } else if (startsWith(Event.keyword, input)) {
-                if (addTask(list, nxtIdx, Event.of(input))) {
-                    nxtIdx++;
-                }
+                addTask(list, Event.of(input));
 
             } else {
                 System.out.println("OH NOSE! Wakaranai... :(");
