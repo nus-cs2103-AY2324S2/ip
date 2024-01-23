11fd4d8b518e392e2b04d5326d461da8972eb5d1
menatahari <natasyabuhari@gmail.com>
2024-01-23 17:21:51 +0800

    echo user command

--- src/main/java/Buto.java
+++ src/main/java/Buto.java
@@ -1,12 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Buto {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(
-                "____________________________________________________________\n" +
-                        " Hello! I'm Buto\n" +
-                        " What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        " Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n"
+                "    ____________________________________________________________\n" +
+                        "     Hello! I'm Buto\n" +
+                        "     What can I do for you?\n" +
+                        "    ____________________________________________________________\n"
+        );
+
+        String command = br.readLine();
+        while (!command.equals("bye")) {
+            System.out.println(
+                    "    ____________________________________________________________\n" +
+                            String.format("    %s\n", command) +
+                            "    ____________________________________________________________"
+            );
+            command = br.readLine();
+        }
+
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________\n"
         );
     }
 }
