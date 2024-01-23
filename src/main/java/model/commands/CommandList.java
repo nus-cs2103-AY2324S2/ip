e645549b685f32cdd7c4362bad47b1b31308c8e5
ivan24004 <ivan@ivant.com>
2024-01-23 22:51:20 +0800

    added the todo, deadline, and event commands

--- src/main/java/model/commands/CommandList.java
+++ src/main/java/model/commands/CommandList.java
@@ -1,9 +1,7 @@
 package model.commands;
 
 import io.Message;
-import model.Chatbot;
-import model.Task;
-import model.TaskList;
+import model.*;
 
 import java.util.HashMap;
 
@@ -11,10 +9,40 @@ public class CommandList {
     private HashMap<String, Command> commandLookup;
     private TaskList taskList;
     private Chatbot chatbot;
+
+    /**
+     * Finds the option of the form ".../option...".
+     * Example: "going home /by: friday", should return the string "friday"
+     *
+     * @param option the option to be found, include the "/" at the start
+     * @param input the input line to be scanned
+     * @return
+     */
+    private String findOption(String option, String input) {
+        int ctr = 0;
+        int idx = input.indexOf(option);
+
+        /// TODO: handle this
+        if (idx == -1) {
+            return "";
+        } else {
+            int endIdx = input.indexOf("/", idx + 1);
+            if (endIdx == -1) {
+                endIdx = input.length();
+            }
+            return input.substring(idx + option.length() + 1, endIdx - 1);
+        }
+    }
+
+    private String getLabel(String input) {
+        int idx = input.indexOf("/");
+        return input.substring(0, idx - 1);
+    }
+
     private void loadCommands() {
         commandLookup.put("exit", (args) -> {
             chatbot.exitIoLoop();
-            return new Message("Bye. Hope to see you again soon!");
+            return new Message("Bye. Please finish some of your tasks.");
         });
         commandLookup.put("list", (args) -> {
             return new Message(taskList.toString());
@@ -28,6 +56,28 @@ public class CommandList {
             int idx = Integer.parseInt(args);
             taskList.unmarkTask(idx);
             return new Message("Why did you say it was finished then? \uD83D\uDE44\n" + taskList.getTask(idx));
+        });
+        commandLookup.put("todo", (args) -> {
+            ToDo todo = new ToDo(args);
+            taskList.addTask(todo);
+            return new Message(String.format("I've added the task.\n%s\nYou still have %d tasks in the list.", todo, 0));
+        });
+
+        commandLookup.put("deadline", (args) -> {
+            String label = getLabel(args);
+            String by = findOption("/by", args);
+            Deadline deadline = new Deadline(label, by);
+            taskList.addTask(deadline);
+            return new Message(String.format("I've added the task.\n%s\nYou still have %d tasks in the list.", deadline, 0));
+        });
+
+        commandLookup.put("event", (args) -> {
+            String label = getLabel(args);
+            String from = findOption("/from", args);
+            String to = findOption("/to", args);
+            Event event = new Event(label, from, to);
+            taskList.addTask(event);
+            return new Message(String.format("I've added the task.\n%s\nYou still have %d tasks in the list.", event, 0));
         });
     }
 
