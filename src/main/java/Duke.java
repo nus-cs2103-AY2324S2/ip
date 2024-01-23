a8412bf155422aa33a4c7881faee25e4a739fc40
yorklim <yorklim1016@gmail.com>
2024-01-23 20:58:26 +0800

    Added Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,43 +24,42 @@ public class Duke {
             if (command.equals("list")) {
                 list();
             } else if (command.equals("unmark")) {
-                unmark(input[1]);
+                unmark(input);
             } else if (command.equals("mark")) {
-                mark(input[1]);
+                mark(input);
             } else if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
-                add(rawInput);
+                add(input);
+            } else if (command.equals("delete")) {
+                delete(input);
             } else {
                 throw new DukeInvalidCommand(rawInput);
             }
         } catch (DukeException e) {
             System.out.println(e);
-        } catch (Exception a) {
-            System.out.println(a);
-        } finally{
-            input();
         }
+
+        input();
     }
 
-    private void add(String input) throws DukeMissingArgument{
+    private void add(String[] input) throws DukeMissingArgument{
         Task task;
-        String[] info = input.split(" ", 2);
-        String command = info[0];
+        String command = input[0];
         if (command.equals("todo")) {
             try {
-                task = new ToDos(info[1]);
+                task = new ToDos(input[1]);
             } catch (ArrayIndexOutOfBoundsException e) {
                 throw new DukeMissingArgument(1,command);
             }
         } else if (command.equals("deadline")) {
             try {
-                String[] inputs = info[1].split(" /by ", 2);
-                task = new Deadlines(inputs[0], inputs[1]);
+                String[] values = input[1].split(" /by ", 2);
+                task = new Deadlines(values[0], values[1]);
             } catch (ArrayIndexOutOfBoundsException e) {
                 throw new DukeMissingArgument(2,command);
             }
         } else if (command.equals("event")) {
             try {
-                String[] event = info[1].split(" /from ", 2);
+                String[] event = input[1].split(" /from ", 2);
                 String[] time = event[1].split(" /to ");
                 task = new Events(event[0], time[0], time[1]);
             } catch (ArrayIndexOutOfBoundsException e) {
@@ -74,9 +73,8 @@ public class Duke {
         int length = list.size();
         System.out.println("Got it. I've added this task:");
         task.taskInfo();
-        System.out.print("Now you have " + length + " task");
-        if (length > 1) {System.out.print("s");}
-        System.out.println(" in the list.");
+        liststatus();
+
     }
 
     private void list() {
@@ -88,37 +86,60 @@ public class Duke {
             System.out.print(++index + ".");
             item.taskInfo();
         }
-
     }
-    private void mark(String input) throws DukeException {
+
+    private void liststatus() {
+        int length = list.size();
+        System.out.print("Now you have " + length + " task");
+        if (length > 1) {System.out.print("s");}
+        System.out.println(" in the list.");
+    }
+    private void mark(String[] input) throws DukeException {
         try {
-            int index = Integer.valueOf(input);
+            int index = Integer.valueOf(input[1]);
             Task task = list.get(index - 1);
             task.mark();
-        } catch (StringIndexOutOfBoundsException e1) {
+        } catch (ArrayIndexOutOfBoundsException e1) {
+            throw new DukeMissingArgument(1, input[0]);
+        } catch (NumberFormatException e2) {
+            throw new DukeInvalidArgument();
+        } catch (IndexOutOfBoundsException e3) {
+            int index = Integer.valueOf(input[1]);
+            throw new DukeTaskNotFound(index);
+        }
+    }
+
+    private void unmark(String[] input) throws DukeException {
+        try {
+            int index = Integer.valueOf(input[1]);
+            Task task = list.get(index - 1);
+            task.unmark();
+        } catch (ArrayIndexOutOfBoundsException e1) {
+            throw new DukeMissingArgument(1, input[0]);
+        } catch (NumberFormatException e2) {
+            throw new DukeInvalidArgument();
+        } catch (IndexOutOfBoundsException e3) {
+            int index = Integer.valueOf(input[1]);
+            throw new DukeTaskNotFound(index);
+        }
+    }
+    private void delete(String[] input) throws DukeException {
+        try {
+            int index = Integer.valueOf(input[1]);
+            Task removed = list.remove(index - 1);
+            System.out.println("Noted. I've removed this task:");
+            removed.taskInfo();
+            liststatus();
+        } catch (ArrayIndexOutOfBoundsException e1) {
             throw new DukeMissingArgument(1, "mark");
         } catch (NumberFormatException e2) {
             throw new DukeInvalidArgument();
         } catch (IndexOutOfBoundsException e3) {
-            int index = Integer.valueOf(input);
+            int index = Integer.valueOf(input[1]);
             throw new DukeTaskNotFound(index);
         }
     }
 
-    private void unmark(String input) throws DukeException {
-        try {
-            int index = Integer.valueOf(input);
-            Task task = list.get(index - 1);
-            task.unmark();
-        } catch (StringIndexOutOfBoundsException e1) {
-            throw new DukeMissingArgument(1, "unmark");
-        } catch (NumberFormatException e2) {
-            throw new DukeInvalidArgument();
-        } catch (IndexOutOfBoundsException e3) {
-            int index = Integer.valueOf(input);
-            throw new DukeTaskNotFound(index);
-        }
-    }
 
     private void exit() {
         System.out.println("Bye. Hope to see you again soon!");
