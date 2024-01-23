f98d545f6a175474356b16425a6b7b0ee57977a2
ruijietay <47778098+ruijietay@users.noreply.github.com>
2024-01-23 22:20:51 +0800

    Given new name to chatbot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "░█▀▀░█▀█░█▀█░█▀█░█▀█░█▀▀░\n"
+                + "░█░░░█▀█░█▀▀░█░█░█░█░█▀▀░\n"
+                + "░▀▀▀░▀░▀░▀░░░▀▀▀░▀░▀░▀▀▀░";
+        System.out.printf("Hello! I'm \n%s\n What can I do for you?\n Bye.%n", logo);
     }
 }
