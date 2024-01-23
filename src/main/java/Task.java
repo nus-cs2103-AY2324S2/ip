2274ff2d11068b1f74d6912ba21c64a2d0711a8b
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-01-23 11:37:43 +0800

    Overloading for Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,11 +2,35 @@ public class Task {
     protected String description;
     protected int id;
     protected boolean isDone;
+    protected String startDate;
+    protected String endDate;
+    protected char type;
 
+    // To-do
     public Task(String description, int id) {
         this.description = description;
         this.isDone = false;
         this.id = id;
+        this.type = 'T';
+    }
+
+    // Deadline
+    public Task(String description, int id, String end) {
+        this.description = description;
+        this.isDone = false;
+        this.id = id;
+        this.endDate = end;
+        this.type = 'D';
+    }
+
+    // Event
+    public Task(String description, int id, String start, String end) {
+        this.description = description;
+        this.isDone = false;
+        this.id = id;
+        this.type = 'E';
+        this.startDate = start;
+        this.endDate = end;
     }
 
     public String getStatusIcon() {
@@ -14,7 +38,9 @@ public class Task {
     }
 
     public String getFullStatus() {
-        return this.id + ". [" + this.getStatusIcon() + "] " + this.description;
+        return this.id + ". [" + this.getStatusIcon() + "] ["
+                + this.type + "] "
+                + this.description;
     }
 
     public void setDone(boolean doneness) {
