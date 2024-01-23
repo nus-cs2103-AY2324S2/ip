e26b4b37030a074f23014d7dc7573177be8c6fe6
gavingoh99 <gavingoh99@gmail.com>
2024-01-23 22:29:58 +0800

    added features for level 1 corrected

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Mona {
     public static void main(String[] args) {
-        String introduction = "____________________________________________________________\n"
-                + " Hello! I'm Mona\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        String introduction = "  ____________________________________________________________\n"
+                + "   Hello! I'm Mona\n"
+                + "   What can I do for you?\n"
+                + "  ____________________________________________________________\n";
+        String farewell = "  ____________________________________________________________\n"
+                + "   Bye. Hope to see you again soon!\n"
+                + "  ____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
         System.out.println(introduction);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(farewell);
+                break;
+            }
+            String response = "  ____________________________________________________________\n"
+                    + "  " + input + "\n"
+                    + "  ____________________________________________________________\n";
+            System.out.print(response);
+        }
+
     }
 }
