ec79127b455c257b65716ca31d752ee4f0d58d3c
zhuuyicheng <zhu.yicheng@yahoo.com>
2024-01-23 17:30:35 +0800

    Level-1. Echo

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class Zack {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Greeting message
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Zack");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
 
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        // Listen for commands and exits when user types "bye"
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // Echo the command
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+        }
     }
 
 }
