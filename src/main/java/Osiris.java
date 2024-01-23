f564c76903a7879e1eb17d46ce40be306d4a15e5
Austin Tan Jun Heng <austintjh19@gmail.com>
2024-01-23 16:10:03 +0800

    Level 1. Echo. { Added New Class ChatInterface. }

--- src/main/java/Osiris.java
+++ src/main/java/Osiris.java
@@ -3,19 +3,12 @@ public class Osiris {
     public static final String NAME = "Osiris";
 
     public void outputIntroductions() {
-        this.printSeparator();
         String introductions = String.format("Hello! I'm %s. \nWhat can I do for you?", Osiris.NAME);
         System.out.println(introductions);
     }
 
     public void outputGoodbyes() {
-        this.printSeparator();
         String goodbyes = "Bye. Hope to see you again soon!";
         System.out.println(goodbyes);
-        this.printSeparator();
-    }
-
-    private void printSeparator() {
-        System.out.println("----------------------------------------");
     }
 }
