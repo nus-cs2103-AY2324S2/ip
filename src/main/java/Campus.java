32de7ba2cecad0d9c2b3891a2506d6936cc31e27
Kappaccinoh <lim.jia28@gmail.com>
2024-01-23 18:17:57 +0800

    Echo

--- src/main/java/Campus.java
+++ src/main/java/Campus.java
@@ -1,14 +1,47 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Campus {
     public static void main(String[] args) {
+        Campus.greet();
+
+        String userInput;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.nextLine();
+            if (Objects.equals(userInput, "bye")) {
+                scanner.close();
+                break;
+            }
+            Campus.echo(userInput);
+        }
+
+        Campus.exit();
+    }
+
+    public static void echo(String input) {
+        String message = "------------------------------\n"
+                + String.format("%s\n", input)
+                + "------------------------------\n";
+
+        System.out.println(message);
+    }
+
+    public static void greet() {
         String message = "------------------------------\n"
                 + "Hello! I'm Campus\n"
                 + "What can I do for you?\n"
                 + "\n"
-                + "------------------------------\n"
+                + "------------------------------\n";
+
+        System.out.println(message);
+    }
+
+    public static void exit() {
+        String message = "------------------------------\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "\n"
                 + "------------------------------\n";
-
         System.out.println(message);
     }
 }
