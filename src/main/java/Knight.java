78627ae02b152f0429a690e96caf26e519be906f
narwhalsilent <110954060+narwhalsilent@users.noreply.github.com>
2024-01-23 22:22:18 +0800

    Echo

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -1,15 +1,29 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Knight {
     public static void main(String[] args) {
-        String logo = "┓┏┓  •  ┓  \n"
-                + "┃┫ ┏┓┓┏┓┣┓╋\n"
-                + "┛┗┛┛┗┗┗┫┛┗┗\n"
-                + "       ┛\n";
+        String logo = "    ┓┏┓  •  ┓  \n"
+                + "    ┃┫ ┏┓┓┏┓┣┓╋\n"
+                + "    ┛┗┛┛┗┗┗┫┛┗┗\n"
+                + "           ┛\n";
 
 
-        System.out.println("Greetings, Your Excellency! Thy humble\n"
+        System.out.println("    Greetings, Your Excellency! Thy humble\n"
                 + logo
-                + "doth stand before thee. How may I serve thee on this day?");
+                + "    doth stand before thee. How may I serve thee on this day?");
 
-        System.out.println("Farewell, Your Excellency! May we cross paths once more in the near future.");
+        Scanner scan = new Scanner( System.in );
+        String command;
+
+        while (true) {
+            command = scan.nextLine();
+
+            if (command.equals("bye")) break;
+
+            System.out.println("    " + command);
+        }
+
+        System.out.println("    Farewell, Your Excellency! May we cross paths once more in the near future.");
     }
 }
