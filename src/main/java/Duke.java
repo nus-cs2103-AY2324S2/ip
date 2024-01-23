3da8bd45a060a32c0768d5b40ad6baf3b6a44787
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-01-23 15:49:17 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,14 +11,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Dune");
         System.out.println("What can I do for you?");
+        List<String> toDos = new ArrayList<>();
         System.out.println("");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String text = scanner.nextLine();  // Read user input
+            if (text.equals("list")) {
+                for (int i = 0; i < toDos.size(); i++) {
+                    System.out.println("    " + i + ". " + toDos.get(i));
+                }
+                continue;
+            }
             if (text.equals("bye")) {
                 break;
             }
-            System.out.println("    " + text);  // Output user input
+            toDos.add(text);
+            System.out.println("    added:" + text);  // Output user input
         }
 
         System.out.println("Bye. Hope to see you again soon!");
