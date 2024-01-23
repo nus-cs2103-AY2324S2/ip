176f17eb842832f1d80fb256040133b117f60636
zhuuyicheng <zhu.yicheng@yahoo.com>
2024-01-23 18:01:06 +0800

    Level-2. Add, List

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Zack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         // Greeting message
         System.out.println("____________________________________________________________");
@@ -20,12 +22,22 @@ public class Zack {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________\n");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________\n");
             }
 
             // Echo the command
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________\n");
+
         }
     }
 
