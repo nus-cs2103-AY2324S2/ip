dd68d4d72e42084e8addb0cd92e9bff32a89daee
yleeyilin <yiiiliiinn@gmail.com>
2024-01-23 22:11:55 +0800

    greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(greet());
+        System.out.println(exit());
+    }
+
+    public static String greet() {
+        return "Hello! I'm YLEXI. \nWhat can I do for you?"; 
+    }
+
+    public static String exit() {
+        return "Bye. Hope to see you again soon!";
     }
 }
