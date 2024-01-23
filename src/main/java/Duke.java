5af40d84448cf4c369129d2d5820c27e040850e0
Chu Jie <chujie2003@gmail.com>
2024-01-23 17:29:39 +0800

    edited level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Tommy.\n" + "What can I do for you?\n");
+        System.out.println("Hello! My name is Mitsuki, nice to meet you!\n" + "What can I do for you today?\n");
 
         System.out.println("Bye! Hope to see you again soon!\n");
         System.exit(0);
