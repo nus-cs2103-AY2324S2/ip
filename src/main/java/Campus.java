03c5432b4d30bb16a01b89ed5e054761a2732237
Kappaccinoh <lim.jia28@gmail.com>
2024-01-23 17:52:18 +0800

    Rename Greet Exit

---
+++ src/main/java/Campus.java
@@ -0,0 +1,14 @@
+public class Campus {
+    public static void main(String[] args) {
+        String message = "------------------------------\n"
+                + "Hello! I'm Campus\n"
+                + "What can I do for you?\n"
+                + "\n"
+                + "------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "\n"
+                + "------------------------------\n";
+
+        System.out.println(message);
+    }
+}
