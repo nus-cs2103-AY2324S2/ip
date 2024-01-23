d99ac2c8067c72b6479412b2106e55f73b6989b5
ANTIFRAGILE\yusia <yusian89@gmail.com>
2024-01-23 19:49:45 +0800

    Add Deadline.java, Event.java, Todo.java
    Added support for tracking the three types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,27 +13,53 @@ public class Duke {
 
             if (input.contains("bye")) {
                 break;
-            } else if (input.contains("list")) {
+            }
+
+            String command = input.split(" ")[0];
+
+
+            if (command.equals("todo")) {
+                Task task = new Todo(input);
+                tasksList.add(task);
+                System.out.println("DevGPT:\n\t" + " Got it. I've added this task: \n\t" + task.toString());
+                System.out.println("DevGPT:\n\t Now you have " + tasksList.size() + " tasks in the list.");
+            } else if (command.equals("deadline")) {
+                String details = input.split(" ", 2)[1];
+                String description = details.split("/by ")[0];
+                String by = details.split("/by ")[1];
+                Task task = new Deadline(description, by);
+                tasksList.add(task);
+                System.out.println("DevGPT:\n\t" + " Got it. I've added this task: " + task.toString());
+                System.out.println("DevGPT:\n\t Now you have " + tasksList.size() + " tasks in the list.");
+            } else if (command.equals("event")) {
+                String details = input.split(" ", 2)[1];
+                String description = details.split("/from")[0];
+                String from =  details.split("/from")[1].split("/to")[0];
+                String to = details.split("/to")[1];
+                Task task = new Event(description, from, to);
+                tasksList.add(task);
+                System.out.println("DevGPT:\n\t" + " Got it. I've added this task: " + task.toString());
+                System.out.println("DevGPT:\n\t Now you have " + tasksList.size() + " tasks in the list.");
+            } else if (command.equals("list")) {
                 System.out.println("DevGPT:\n\t Here are the tasks in your list:");
                 for (int i = 0; i < tasksList.size(); i++) {
                     Task task = tasksList.get(i);
-                    System.out.println("\t" + (i + 1) + ". " + "[" + task.getStatusIcon() + "]" + " " + task.description);
+                    System.out.println("\t\t" + (i + 1) + ". " + task.toString());
                 }
-
-            } else if (input.contains("unmark")) {
+            } else if (command.equals("unmark")) {
                 String[] split = input.split(" ");
                 int index = Integer.parseInt(split[1]);
                 Task task = tasksList.get(index - 1);
                 task.markAsUndone();
                 System.out.println("DevGPT:\n\t Got it! I've marked this task as not done yet:");
-                System.out.println("\t" + "[" + task.getStatusIcon() + "]" + " " + task.description);
-            } else if (input.contains("mark")) {
+                System.out.println("\t" + task.toString());
+            } else if (input.equals("mark")) {
                 String[] split = input.split(" ");
                 int index = Integer.parseInt(split[1]);
                 Task task = tasksList.get(index - 1);
                 task.markAsDone();
                 System.out.println("DevGPT:\n\t Nice! I've marked this task as done:");
-                System.out.println("\t" + "[" + task.getStatusIcon() + "]" + " " + task.description);
+                System.out.println("\t" + task.toString());
             } else {
                 tasksList.add(new Task(input));
                 System.out.println("DevGPT:\n\t" + "added: " + input);
