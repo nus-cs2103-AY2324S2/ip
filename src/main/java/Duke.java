f4fb094c5f7cc3cc9defd611df6220dd702a464c
null null <117985016+xinen26@users.noreply.github.com>
2024-01-24 00:11:42 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    private static void line() {
+        System.out.println("____________________________________________________________");
+    }
+    private static void intro() {
+        line();
+        System.out.println(" Hello I'm NoisyChatter");
+        System.out.println(" What can I do for you?");
+        line();
+
+    }
+
+    private static void farewell() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        line();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        intro();
+        farewell();
     }
 }
+
