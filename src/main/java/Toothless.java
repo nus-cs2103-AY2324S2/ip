3f369398882df76950b1a1c957ec8427d9882da1
Hong Jun <zappy.tan1234@gmail.com>
2024-01-23 12:40:47 +0800

    Toothless will now echo the command entered by the user, and exit when the user types the command "bye"

--- src/main/java/Toothless.java
+++ src/main/java/Toothless.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Toothless {
     private static String indentation = "____________________________________________________________\n";
     private static String chatBotName = "Toothless";
@@ -6,6 +8,13 @@ public class Toothless {
     private static String exitString = "Bye. Hope to see you again soon!\n" + indentation;
 
     public static void main(String[] args){
-        System.out.println(indentation + greetingString + exitString);
+        Scanner sc = new Scanner(System.in);
+        String echo;
+        System.out.println(indentation + greetingString);
+        do {
+            echo = sc.nextLine();
+            System.out.println(indentation + echo + "\n" + indentation);
+        } while (!echo.equals("bye"));
+        System.out.println(exitString);
     }
 }
