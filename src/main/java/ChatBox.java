728fca634369848b22e17793492dedf9a4ce2895
0-yibai <2802756281@qq.com>
2024-01-23 10:13:11 +0800

    allow adding tasks into list

--- src/main/java/ChatBox.java
+++ src/main/java/ChatBox.java
@@ -1,47 +1,75 @@
 import java.util.Scanner;
 
 public class ChatBox {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner;
     String input;
+    String[] tasks;
+    int taskCount;
+    boolean isExitSignal;
 
     public ChatBox() {
+        this.scanner = new Scanner(System.in);
         this.input = "";
+        this.tasks = new String[100];
+        this.taskCount = 0;
+        this.isExitSignal = false;
     }
 
     public void launch() {
-        greet();
-        while (!isExitSignal()) {
-            echo();
+        printGreet();
+        while (!isExitSignal) {
             this.input = scanner.nextLine();
+            parseInput();
         }
-        bye();
+        printBye();
     }
 
-    private void greet() {
-        System.out.println("    ____________________________________________________________\n"
-                + "    Hello! I'm Wis.\n"
-                + "    What can I do for you?\n"
-                + "    ____________________________________________________________");
+    private void parseInput() {
+        if (this.input.equals("list")) {
+            printList();
+        } else if (this.input.equals("bye")) {
+            this.isExitSignal = true;
+        } else {
+            addTask();
+        }
     }
 
-    private boolean isExitSignal() {
-        return this.input.equals("bye");
+    private void printDecorator() {
+        System.out.println("    ____________________________________________________________");
     }
 
-    private void echo() {
+    private void printGreet() {
+        printDecorator();
+        System.out.println("    Hello! I'm Wis.\n"
+                + "    What can I do for you?\n");
+        printDecorator();
+    }
+
+    private void addTask() {
         if (this.input.isEmpty()) {
             return;
         }
-        System.out.println("    ____________________________________________________________\n"
-                + "    " + this.input + "\n"
-                + "    ____________________________________________________________");
-
+        printDecorator();
+        this.tasks[this.taskCount] = this.input;
+        this.taskCount++;
+        System.out.println("    added: " + this.input);
+        printDecorator();
     }
 
-    private void bye() {
-        System.out.println("    ____________________________________________________________\n"
-                + "    Bye. Hope to see you again soon!\n"
-                + "    ____________________________________________________________");
+    private void printList() {
+        printDecorator();
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i+1) + ". " + tasks[i]);
+        }
+        printDecorator();
     }
+
+    private void printBye() {
+        printDecorator();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printDecorator();
+    }
+
+
 
 }
