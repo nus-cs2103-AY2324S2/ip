9681d62f4793424f57c9bc3c992774c45bcf6fc0
Ty-stan0417 <yisheng0417@gmail.com>
2024-01-23 22:52:05 +0800

    Level-0

---
+++ src/main/java/Tyler.java
@@ -0,0 +1,7 @@
+public class Tyler {
+    public static void main(String[] args) {
+        System.out.println("Hello from Tyler");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again");
+    }
+}
