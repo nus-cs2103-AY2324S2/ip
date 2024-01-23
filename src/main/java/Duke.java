487f5c208baf463cde0e1716dc40407b79efb22f
sindhurajain <sindhura394@gmail.com>
2024-01-24 05:21:36 +0800

    Implemented Level 1 and javadoc comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * This class represents a chat application.
+ */
 public class Duke {
+    private Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.startChat();
     }
-    
+
     /**
      * Initiates the chat by invoking the sayHi() and sayBye() methods.
      */
     public void startChat() {
         sayHi();
+
+        boolean exited = false;
+
+        while (!exited) {
+            String toEcho = sc.nextLine();
+            if (toEcho.equals("bye")) {
+                exited = true;
+            }
+            else {
+                echoMessage(toEcho);
+            }
+        }
         sayBye();
     }
 
@@ -24,5 +42,14 @@ public class Duke {
      */
     public void sayBye() {
         System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    /**
+     * Echoes the user's input back to the console.
+     *
+     * @param input The user's input to be echoed.
+     */
+    public void echoMessage(String input) {
+        System.out.println(input + "\n");
     }
 }
