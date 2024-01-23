bbf8daa899ce75c726271e0ea2554d9a735a9b61
Felix Chan You Yuan <e0959017@u.nus.edu>
2024-01-23 13:38:32 +0800

    Add new task to list and display back

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,8 +6,11 @@ public class Duke {
     private final String horizontalLine = "____________________________________________________________\n";
     private final String name;
 
+    private final ArrayList<String> tasks;
+
     public Duke(String name) {
         this.name = name;
+        this.tasks = new ArrayList<>();
     }
 
     public void greetUser() {
@@ -28,12 +32,14 @@ public class Duke {
         System.out.print("Message Zenify: ");
         while (myScannerObj.hasNext()) {
             System.out.print(this.horizontalLine);
-            String command = myScannerObj.nextLine();
-            if (command.equalsIgnoreCase("bye")) {
+            String task = myScannerObj.nextLine();
+            if (task.equalsIgnoreCase("bye")) {
                 this.exit();
                 break;
+            } else if (task.equalsIgnoreCase("list")) {
+                this.toDoList();
             } else {
-                System.out.println(" " + command);
+                this.addToDo(task);
             }
             System.out.print(this.horizontalLine);
             System.out.print("\nMessage Zenify: ");
@@ -41,6 +47,23 @@ public class Duke {
 
         myScannerObj.close();
 
+    }
+
+    private void addToDo(String task) {
+        tasks.add(task);
+        System.out.println(" added: " + task);
+    }
+
+    private void toDoList() {
+        if (tasks.isEmpty()) {
+            System.out.println(" Please add task!");
+        } else {
+            int taskCount = 0;
+            for (String task : tasks) {
+                taskCount++;
+                System.out.println(" " + taskCount + ". " + task);
+            }
+        }
     }
 
     public static void main(String[] args) {
