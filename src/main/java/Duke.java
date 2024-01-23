5209d4ac3b4e86f7be3dd8f17f69b8f40a03e58a
Felix Chan You Yuan <e0959017@u.nus.edu>
2024-01-23 11:34:49 +0800

    Added rename chatbot, greet and exit implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
 public class Duke {
+
+    private final String horizontalLine = "____________________________________________________________";
+    private final String name;
+
+    public Duke(String name) {
+        this.name = name;
+    }
+
+    public void greetUser() {
+        System.out.println(this.horizontalLine
+                + "\nHello! I'm "
+                + this.name
+                + "\n"
+                + "What can I do for you?\n"
+                + this.horizontalLine);
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!\n" + this.horizontalLine);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Duke zenifyBot = new Duke("Zenify");
+        zenifyBot.greetUser();
+        zenifyBot.exit();
+
     }
 }
