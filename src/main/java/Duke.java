171e5b946a755ddc14dcc43e7cc1d15ad479b947
soons1 <103474763+soons1@users.noreply.github.com>
2024-01-23 17:04:59 +0800

    Updated to fit Level 2 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Duke {
     private static String lineBreak = "______________________________________________";
+    private static ArrayList<String> lst = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         greet();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String command = "";
         while (!(command = br.readLine()).equals("bye")) {
-            System.out.println(lineBreak);
-            System.out.println(" " + command);
-            System.out.println(lineBreak);
+            if (command.equals("list")) {
+                printLst();
+            } else {
+                addLst(command);
+            }
         }
         exit();
     }
@@ -19,6 +23,20 @@ public class Duke {
     public static void greet() {
         System.out.println(lineBreak);
         System.out.println(" Hello! I'm SnowBoy\n" + " What can I do for you?");
+        System.out.println(lineBreak);
+    }
+
+    public static void addLst(String command) {
+        lst.add(command);
+        System.out.println(lineBreak);
+        System.out.println(" added: " + command);
+        System.out.println(lineBreak);
+    }
+    public static void printLst() {
+        System.out.println(lineBreak);
+        for (int i = 0; i < lst.size(); i++) {
+            System.out.println(" " + (i + 1) + ". " + lst.get(i));
+        }
         System.out.println(lineBreak);
     }
 
