881543dfa1b76af1039f419fafc10fd7cf5762c8
Ryan Chia <e0959970@u.nus.edu>
2024-01-23 13:28:57 +0800

    Changed name and aesthetic Added custom printing for chatbot output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
 public class Duke {
+
+    /**
+     * Prints a message to the terminal, decorated with the Louie icon.
+     *
+     * @param message The message to print.
+     */
+    public static void print(String message) {
+        boolean isFirst = true;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (String s : message.split("\n")) {
+            sb.append("    ");
+            if (isFirst) {
+                sb.append("(>^.^<)");
+                isFirst = false;
+            } else {
+                sb.append("       ");
+            }
+            sb.append(" ").append(s).append("\n");
+        }
+        sb.append("\n");
+        System.out.print(sb);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.print("Hello, my name is... Louie!!!! \nWhat can I do for you today?");
+        Duke.print("Ok, going to sleep...");
+
     }
 }
