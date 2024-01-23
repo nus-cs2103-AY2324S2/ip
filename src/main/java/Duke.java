510b94a24c792ef0e1435b610ea1dd8fc3fd895b
delishad21 <soojohan@gmail.com>
2024-01-23 14:44:10 +0800

    Implemented task adding

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     private static final String spacer = "____________________________________________________________\n";
+    private static ArrayList<String> toDoList = new ArrayList<>();
 
     private static void startupMessage() {
         String name = "CBBW";
@@ -18,11 +20,23 @@ public class Duke {
         Scanner s = new Scanner(System.in);
         while (true) {
             String echo = s.nextLine();
+
             if (echo.equals("bye")) {
                 s.close();
                 break;
             }
-            System.out.println(spacer + echo + "\n" + spacer);
+
+            if (echo.equals("list")) {
+                System.out.println(spacer);
+                for (int i = 1; i <= toDoList.size(); i++) {
+                    System.out.println(i + "." + toDoList.get(i - 1) + "\n");
+                }
+                System.out.println(spacer);
+                continue;
+            }
+
+            toDoList.add(echo);
+            System.out.println(spacer + "added: " + echo + "\n" + spacer);
         }
         goodbyeMessage();
     }
