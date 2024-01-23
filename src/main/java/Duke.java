2ad9a0c473b4457926889b2dd2e156e729908c06
sethteo <hongruiseth.teo@gmail.com>
2024-01-23 18:21:32 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String DIVIDER = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
         String WELCOME_TEXT = "Hello! I'm SlayBot\nWhat can I do for you?";
         String BYE_TEXT = "Bye. Hope to see you again soon!";
         String logo = "\n" +
@@ -13,8 +16,17 @@ public class Duke {
                 "                  __/ |                \n" +
                 "                 |___/                 \n";
         System.out.println(logo);
-        System.out.println(DIVIDER + "\n" + WELCOME_TEXT + "\n" + DIVIDER + "\n" +
-                BYE_TEXT);
+        System.out.println(DIVIDER + "\n" + WELCOME_TEXT + "\n" + DIVIDER);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(DIVIDER + "\n" + BYE_TEXT + "\n" + DIVIDER);
+                break;
+            } else {
+                System.out.println(DIVIDER + "\n" + input + "\n" + DIVIDER);
+            }
+        }
 
     }
 }
