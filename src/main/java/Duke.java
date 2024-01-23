1ff9cfbd1e01187afbc69ffcecbc477dd8c000a7
AwJoey <awjoey26@gmail.com>
2024-01-24 00:51:18 +0800

    Added the ability to mark tasks as done/undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,20 +10,47 @@ public class Duke {
                 "================================ \n");
 
         String input = sc.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println( "--------------------------------");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + ". " + list.get(i));
+                    Task iTask = list.get(i);
+                    String iCross = iTask.getStatus() ? "[X]" : "[ ]";
+                    System.out.println((i+1) + ". " + iCross + " " + iTask.getTaskName());
                 }
                 System.out.println( "-------------------------------- \n");
             }
+            else if (input.contains("mark")) {
+                String[] inputSplit = input.split(" ");
+                int taskNo = Integer.parseInt(inputSplit[1]) - 1;
+                Task dTask = list.get(taskNo);
+
+                if (inputSplit[0].equals("mark")) {
+                    dTask.done();
+                    String dCross = dTask.getStatus() ? "[X]" : "[ ]";
+
+                    System.out.println("-------------------------------- \n" +
+                            "Nice! I've marked task " + taskNo + " as done: \n" +
+                            dCross + " " + dTask.getTaskName() + "\n" +
+                            "-------------------------------- \n");
+                }
+                else {
+                    dTask.undone();
+                    String dCross = dTask.getStatus() ? "[X]" : "[ ]";
+
+                    System.out.println("-------------------------------- \n" +
+                            "Sure, I've marked task " + taskNo + " as not done yet: \n" +
+                            dCross + " " + dTask.getTaskName() + "\n" +
+                            "-------------------------------- \n");
+                }
+            }
             else {
-                list.add(input);
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println( "-------------------------------- \n" +
-                                    "added: " + input + "\n" +
+                                    "added: " + t.getTaskName() + "\n" +
                                     "-------------------------------- \n");
             }
             input = sc.nextLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private String taskName;
+    private boolean status;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        status = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void done() {
+        status = true;
+    }
+
+    public void undone() {
+        status = false;
+    }
 }
+
