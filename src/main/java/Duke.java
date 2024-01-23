5a631f6b3aa0733db78357f56ba9bb7f3732bc12
Li_Zizhen <e1075569@u.nus.edu>
2024-01-23 17:22:56 +0800

    solved: level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-
         zhen zh = new zhen();
         zh.greeting();
-        zh.print_message("Hello! I'm ZHEN\n What can I do for you? ");
-        while(true){
-            String msg = zh.getuserinput();
-            if (msg.equals("bye")){
-                break;
-            }
-            zh.print_message(msg);
-        }
-        zh.print_message("Bye. Hope to see you again soon");
+        zhen.print_message("Hello! I'm ZHEN\n What can I do for you? ");
+//        zh.echo(); // level1
+        zh.store_task(); // level2
+        zhen.print_message("Bye. Hope to see you again soon");
     }
-
-
 }
 class zhen{
     private Scanner obj = new Scanner(System.in);
@@ -32,12 +20,54 @@ class zhen{
     public void greeting(){
         System.out.println("Hello from\n" + logo);
     }
-    public void print_message(String msg){
+    public static void print_message(String msg){
         System.out.println("\n ----------------------------------");
         System.out.println(" "+msg);
         System.out.println("\n ----------------------------------");
     }
     public String getuserinput(){
         return obj.nextLine();
+    }
+    public void echo(){
+        while(true){
+            String msg = getuserinput();
+            if (msg.equals("bye")){
+                break;
+            }
+            print_message(msg);
+        }
+    }
+    public void store_task(){
+        database db = new database();
+        while(true){
+            String msg = getuserinput();
+            if (msg.equals("bye")){
+                break;
+            }
+            if (msg.equals("list")){
+                zhen.print_message(db.toString());
+                continue;
+            }
+            db.insert(msg);
+        }
+    }
+}
+class database{
+    private ArrayList<String> strlist = new ArrayList<>();
+    public void insert(String msg){
+        strlist.add(msg);
+        zhen.print_message("added: "+msg);
+    }
+    @Override
+    public String toString(){
+        String str = "";
+        for (int i=0; i<strlist.size();i++){
+            if (i == strlist.size()-1){
+                str = str + (i+1)+". "+ strlist.get(i);
+                break;
+            }
+            str = str + (i+1)+". "+ strlist.get(i)+ "\n ";
+        }
+        return str;
     }
 }
