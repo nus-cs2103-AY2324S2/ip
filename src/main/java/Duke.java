4fbaf41535edc99daab66808905759df42ec66a9
Cedricaca <>
2024-01-23 13:51:52 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,8 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String[] List = new String[100];
+        int length = 0;
         System.out.println("Hello! I'm Dukey.");
 	    System.out.println("What can I do for you?");
 
@@ -14,8 +15,22 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                if (length == 0) {
+                    System.out.println("You have no items in your list!");
+                    continue;
+                } else {
+                    for (int x = 0; x < length; x++) {
+                        String item = List[x];
+                        int numeric = x + 1;
+                        System.out.println(numeric + ". " + item);
+                    }
+                    continue;
+                }
             }
-            System.out.println(input);
+            List[length] = input;
+            length = length + 1;
+            System.out.println("added: " + input);
         }
 
     }
