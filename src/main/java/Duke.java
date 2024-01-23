28cdf3e807e4c86dd8549f1282eb98af01399153
Ryan Tan <ryantan111100@gmail.com>
2024-01-23 21:55:57 +0800

    Added TaskList, Task classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 public class Duke {
     private static final String BOT_NAME = "Felix";
-    public static void printHorizontalLine(int length) {
+    private TaskList tasks;
+
+    Duke() {this.tasks = new TaskList();}
+    public void printHorizontalLine(int length) {
         for (int i = 0; i < length; i++) System.out.print('_');
         System.out.println();
     }
-    public static void printIntroduction() {
+    public void printIntroduction() {
         printHorizontalLine(60);
         System.out.printf("Hello! I'm %s\nWhat can I do for you?\n",BOT_NAME);
         printHorizontalLine(60);
     }
 
-    public static void printGoodbye() {
+    public void printGoodbye() {
         System.out.println("Bye. Hope to see you again soon!");
         printHorizontalLine(60);
     }
 
     public static void main(String[] args) {
+        Duke duke = new Duke();
         Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -26,16 +30,21 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         boolean loop = true;
-        printIntroduction();
+        duke.printIntroduction();
         while (loop) {
             String line = scanner.nextLine();
-            printHorizontalLine(60);
+            duke.printHorizontalLine(60);
             if (line.equals("bye")) {
                 loop = false;
-                printGoodbye();
+                duke.printGoodbye();
+            } else if (line.equals("list")) {
+                System.out.println(duke.tasks);
+                duke.printHorizontalLine(60);
             } else {
-                System.out.println(line);
-                printHorizontalLine(60);
+                Task task = new Task(line);
+                System.out.printf("added: %s\n", task);
+                duke.tasks.addTask(task);
+                duke.printHorizontalLine(60);
             }
         }
     }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class TaskList {
+    private ArrayList<Task> taskList;
+
+    TaskList() {
+        this.taskList = new ArrayList<Task>();
+    }
+
+    public void addTask(Task t) {
+        this.taskList.add(t);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer output = new StringBuffer();
+        for (int i = 0; i < this.taskList.size(); i++) {
+            output.append(String.format("%d. %s", i + 1, this.taskList.get(i)));
+            if (i < this.taskList.size() - 1) output.append("\n");
+        }
+        return output.toString();
+    }
+}
