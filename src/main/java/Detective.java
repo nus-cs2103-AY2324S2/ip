0a293f4adc66eb99dd57df7c12fbaa2c219c42b6
NusMinato <E0985934@u.nus.edu>
2024-01-23 22:43:53 +0800

    Add support for deleting tasks from the list.

--- src/main/java/Detective.java
+++ src/main/java/Detective.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -7,8 +8,7 @@ public class Detective {
 
     public static void main(String[] args) {
         String name = "Detective";
-        Task[] myList = new Task[100];
-        int count = 0;
+        ArrayList<Task> myList = new ArrayList<>();
 
         System.out.println(line + "\nHello! I'm [" + name + "]" + "\nWhat can I do for you?\n" + line);
         String input = sc.nextLine();
@@ -18,112 +18,148 @@ public class Detective {
                 case "list":
                     System.out.println(line);
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < 100; i++) {
-                        if (myList[i] == null) {
-                            break;
-                        }
-                        System.out.println((i + 1) + "." + myList[i]);
+                    for (int i = 0; i < myList.size(); i++) {
+                        System.out.println((i + 1) + "." + myList.get(i));
                     }
                     System.out.println(line);
                     input = sc.nextLine();
                     break;
                 case "mark":
-                    int markTaskNum = Integer.parseInt(inputContent[1]) - 1;
-                    myList[markTaskNum].markAsDone();
+                    int markTaskNum;
+                    try {
+                        markTaskNum = Integer.parseInt(inputContent[1]) - 1;
+                        myList.get(markTaskNum).markAsDone();
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        handleException("Please tell me mark which one!");
+                        input = sc.nextLine();
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        handleException("I haven't record this task!");
+                        input = sc.nextLine();
+                        break;
+                    }
                     System.out.println(line);
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(myList[markTaskNum]);
+                    System.out.println(myList.get(markTaskNum));
                     System.out.println(line);
                     input = sc.nextLine();
                     break;
                 case "unmark":
-                    int unmarkTaskNum = Integer.parseInt(inputContent[1]) - 1;
-                    myList[unmarkTaskNum].markAsNotDone();
+                    int unmarkTaskNum;
+                    try {
+                        unmarkTaskNum = Integer.parseInt(inputContent[1]) - 1;
+                        myList.get(unmarkTaskNum).markAsNotDone();
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        handleException("Please tell me unmark which one!");
+                        input = sc.nextLine();
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        handleException("I haven't record this task!");
+                        input = sc.nextLine();
+                        break;
+                    }
                     System.out.println(line);
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(myList[unmarkTaskNum]);
+                    System.out.println(myList.get(unmarkTaskNum));
+                    System.out.println(line);
+                    input = sc.nextLine();
+                    break;
+                case "delete":
+                    int deleteTaskNum;
+                    Task needDelete;
+                    try {
+                        deleteTaskNum = Integer.parseInt(inputContent[1]) - 1;
+                        needDelete = myList.get(deleteTaskNum);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        handleException("Please tell me delete which one!");
+                        input = sc.nextLine();
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        handleException("I haven't record this task!");
+                        input = sc.nextLine();
+                        break;
+                    }
+                    myList.remove(deleteTaskNum);
+                    System.out.println(line);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(needDelete);
+                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
                     System.out.println(line);
                     input = sc.nextLine();
                     break;
                 default: //add tasks to myList
-                    if (count >= 100) {
-                        System.out.println(line);
-                        System.out.println("The list is full, add failed");
-                        System.out.println(line);
-                        input = sc.nextLine();
-                    } else {
-                        switch (inputContent[0]) {
-                            case "todo":
-                                ToDo toDoTask;
-                                try {
-                                    toDoTask = new ToDo(inputContent[1]);
-                                } catch (ArrayIndexOutOfBoundsException e) {
-                                    handleEmptyTaskException("What do you want to do?");
-                                    input = sc.nextLine();
-                                    break;
-                                }
-                                myList[count++] = toDoTask;
-                                System.out.println(line);
-                                System.out.println("Got it. I've added this task:");
-                                System.out.println(toDoTask);
-                                System.out.println("Now you have " + count + " tasks in the list.");
-                                System.out.println(line);
+                    switch (inputContent[0]) {
+                        case "todo":
+                            ToDo toDoTask;
+                            try {
+                                toDoTask = new ToDo(inputContent[1]);
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                handleException("What do you want to do?");
                                 input = sc.nextLine();
                                 break;
-                            case "deadline":
-                                Deadline deadlineTask;
-                                try {
-                                    String[] ddlInfo = inputContent[1].split(" /");
-                                    String ddlName = ddlInfo[0];
-                                    String ddlBy = ddlInfo[1].split("by ")[1];
-                                    deadlineTask = new Deadline(ddlName, ddlBy);
-                                } catch (ArrayIndexOutOfBoundsException e) {
-                                    handleEmptyTaskException("What is the deadline?");
-                                    input = sc.nextLine();
-                                    break;
-                                }
-                                myList[count++] = deadlineTask;
-                                System.out.println(line);
-                                System.out.println("Got it. I've added this task:");
-                                System.out.println(deadlineTask);
-                                System.out.println("Now you have " + count + " tasks in the list.");
-                                System.out.println(line);
+                            }
+                            myList.add(toDoTask);
+                            System.out.println(line);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println(toDoTask);
+                            System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                            System.out.println(line);
+                            input = sc.nextLine();
+                            break;
+                        case "deadline":
+                            Deadline deadlineTask;
+                            try {
+                                String[] ddlInfo = inputContent[1].split(" /");
+                                String ddlName = ddlInfo[0];
+                                String ddlBy = ddlInfo[1].split("by ")[1];
+                                deadlineTask = new Deadline(ddlName, ddlBy);
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                handleException("What is the deadline?");
                                 input = sc.nextLine();
                                 break;
-                            case "event":
-                                Event eventTask;
-                                try {
-                                    String[] evtInfo = inputContent[1].split(" /");
-                                    String evtName = evtInfo[0];
-                                    String evtFrom = evtInfo[1].split("from ")[1];
-                                    String evtTo = evtInfo[2].split("to ")[1];
-                                    eventTask = new Event(evtName, evtFrom, evtTo);
-                                } catch (ArrayIndexOutOfBoundsException e) {
-                                    handleEmptyTaskException("What is the event?");
-                                    input = sc.nextLine();
-                                    break;
-                                }
-                                myList[count++] = eventTask;
-                                System.out.println(line);
-                                System.out.println("Got it. I've added this task:");
-                                System.out.println(eventTask);
-                                System.out.println("Now you have " + count + " tasks in the list.");
-                                System.out.println(line);
+                            }
+                            myList.add(deadlineTask);
+                            System.out.println(line);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println(deadlineTask);
+                            System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                            System.out.println(line);
+                            input = sc.nextLine();
+                            break;
+                        case "event":
+                            Event eventTask;
+                            try {
+                                String[] evtInfo = inputContent[1].split(" /");
+                                String evtName = evtInfo[0];
+                                String evtFrom = evtInfo[1].split("from ")[1];
+                                String evtTo = evtInfo[2].split("to ")[1];
+                                eventTask = new Event(evtName, evtFrom, evtTo);
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                handleException("What is the event?");
                                 input = sc.nextLine();
                                 break;
-                            default:
-                                System.out.println(line);
-                                System.out.println("OOPS!!! Sorry, but I don't know what that means. qwq");
-                                System.out.println(line);
-                                input = sc.nextLine();
-                        }
+                            }
+                            myList.add(eventTask);
+                            System.out.println(line);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println(eventTask);
+                            System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                            System.out.println(line);
+                            input = sc.nextLine();
+                            break;
+                        default:
+                            System.out.println(line);
+                            System.out.println("OOPS!!! Sorry, but I don't know what that means. qwq");
+                            System.out.println(line);
+                            input = sc.nextLine();
                     }
+
             }
         }
         System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
 
-    private static void handleEmptyTaskException(String message) {
+    private static void handleException(String message) {
         System.out.println(line);
         System.out.println("OOPS!!! " + message + " qwq");
         System.out.println(line);
