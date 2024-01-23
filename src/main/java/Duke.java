d39ad666e0d3561cbe78f457fe662ce70f94ed0c
sindhurajain <sindhura394@gmail.com>
2024-01-24 04:45:37 +0800

    Implemented Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.startChat();
+    }
+    public void startChat() {
+        sayHi();
+        sayBye();
+    }
+    public void sayHi() {
+        System.out.println("Hello! I'm myChats\n" + "What can I do for you?\n");
+    }
+    public void sayBye() {
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
