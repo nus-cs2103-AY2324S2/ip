faf315c33d0a542c9c0cd0bf2f0bcf40ac013ddd
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-01-23 10:30:17 +0800

    feat: Level-0:Rename, Greet, Exit

---
+++ src/main/java/Pan.java
@@ -0,0 +1,6 @@
+public class Pan {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Pan\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
