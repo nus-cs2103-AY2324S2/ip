d2c2c417cecb2ce6002b47aca6b753fdf8bbe8b1
Giselle Tan <giselletan1008@gmail.com>
2024-01-24 03:02:36 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,49 +4,107 @@ public class Duke {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, String> lsts = new HashMap<>();
+        HashMap<Integer, Task> lsts = new HashMap<>();
         int counter = 0;
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Emis!\n \tWhat can I do for you?");
         while (sc.hasNextLine()) {
             String user_input = sc.nextLine();
-            if (user_input.equals("bye")) {
-                exit();
-            } else if (user_input.equals("list")) {
-                print_list(lsts);
+            Integer spaceIndex = user_input.indexOf(" ");
+
+            if (spaceIndex == -1) {
+                if (user_input.equals("bye")) {
+                    exit();
+                } else if (user_input.equals("list")) {
+                    print_list(lsts);
+                } else {
+                    counter += 1;
+                    addTasks(user_input, counter, lsts);
+                }
             } else {
-                counter += 1;
-                add(user_input, counter, lsts);
+                String action = user_input.substring(0, user_input.indexOf(" "));
+                try {
+                    Integer taskNo = Integer.parseInt(user_input.substring(spaceIndex + 1));
+                    if (taskNo <= 0 || taskNo > counter) {
+                        System.out.println("Task does not exist");
+                    } else {
+                        Task t = lsts.get(taskNo);
+                
+                        if (action.equals("mark")) {
+                            t.markAsDone();
+                            lsts.replace(taskNo, t);
+                        } else if (action.equals("unmark")) {
+                            t.markAsUndone();
+                            lsts.replace(taskNo, t);
+                        } 
+                    }
+                } catch (NumberFormatException nfe) {
+                    counter += 1;
+                    addTasks(user_input, counter, lsts);
+                }
             }
         }
-        exit();
     }
 
     public static void exit() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
+        System.exit(0);
     }
 
-    public static void echo(String userInput) {
+    public static void print_list(HashMap<Integer, Task> hs) {
         System.out.println("\t____________________________________________________________");
-        System.out.println("\t" + userInput);
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void print_list(HashMap<Integer, String> hs) {
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
         for (Integer i : hs.keySet()) {
-            System.out.println("\t" + i + ". " + hs.get(i));
+            System.out.println("\t" + hs.get(i).toString());
         }
         System.out.println("\t____________________________________________________________");
     }
 
-    public static void add(String input, int counter, HashMap<Integer, String> hs) {
-        hs.put(counter, input);
+    public static void addTasks(String input, int counter, HashMap<Integer, Task> hs) {
+        Task t = new Task(input, counter);
+        hs.put(counter, t);
         System.out.println("\t____________________________________________________________");
         System.out.println("\tadded: " + input);
         System.out.println("\t____________________________________________________________");
+    }
+
+    public static class Task {
+        protected String description;
+        protected int taskNumber;
+        protected boolean isDone;
+
+        public Task(String description, int taskNumber) {
+            this.description = description;
+            this.taskNumber = taskNumber;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\tNice! I've marked this task as done:");
+            System.out.println("\t\t[" + this.getStatusIcon() + "] " + this.description);
+            System.out.println("\t____________________________________________________________");
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\tOK, I've marked this task as not done yet:");
+            System.out.println("\t\t[" + this.getStatusIcon() + "] " + this.description);
+            System.out.println("\t____________________________________________________________");
+        }
+
+        @Override
+        public String toString() {
+            return taskNumber + ". [" + this.getStatusIcon() + "] " + this.description;
+        }
     }
 }
