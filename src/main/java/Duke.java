802fd846fe08a8bd0f303bb4f45bcd7d4c012967
Li_Zizhen <e1075569@u.nus.edu>
2024-01-23 16:33:23 +0800

    solve: level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -5,22 +6,38 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-        String logo="ZZZZZ   H   H  EEEEE  N   N\n" +
-                "   Z    H   H  E      NN  N\n" +
-                "  Z     HHHHH  EEEE   N N N\n" +
-                " Z      H   H  E      N  NN\n" +
-                "ZZZZZ   H   H  EEEEE  N   N\n";
+
+        zhen zh = new zhen();
+        zh.greeting();
+        zh.print_message("Hello! I'm ZHEN\n What can I do for you? ");
+        while(true){
+            String msg = zh.getuserinput();
+            if (msg.equals("bye")){
+                break;
+            }
+            zh.print_message(msg);
+        }
+        zh.print_message("Bye. Hope to see you again soon");
+    }
+
+
+}
+class zhen{
+    private Scanner obj = new Scanner(System.in);
+    private String logo="ZZZZZ   H   H  EEEEE  N   N\n" +
+            "   Z    H   H  E      NN  N\n" +
+            "  Z     HHHHH  EEEE   N N N\n" +
+            " Z      H   H  E      N  NN\n" +
+            "ZZZZZ   H   H  EEEEE  N   N\n";
+    public void greeting(){
         System.out.println("Hello from\n" + logo);
-        print_line();
-        print_message("Hello! I'm ZHEN");
-        print_message("What can I do for you?");
-        print_line();
-        print_message("Bye. Hope to see you again soon");
     }
-    public static void print_line(){
-        System.out.println("\n----------------------------------");
-    }
-    public static void print_message(String msg){
+    public void print_message(String msg){
+        System.out.println("\n ----------------------------------");
         System.out.println(" "+msg);
+        System.out.println("\n ----------------------------------");
+    }
+    public String getuserinput(){
+        return obj.nextLine();
     }
 }
