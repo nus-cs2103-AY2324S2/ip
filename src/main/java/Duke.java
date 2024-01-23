ab388df48cdf76ed7cb79e9da4dec32fd50acdcd
yleeyilin <yiiiliiinn@gmail.com>
2024-01-23 22:30:53 +0800

    add and list functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] instrList = new String[100]; 
+    private static int ptr = 0; 
+
     public static void main(String[] args) {
         System.out.println(greet());
 
@@ -8,7 +11,12 @@ public class Duke {
 
         String instr = sc.nextLine();
         while (!instr.equals("bye")) {
-            System.out.println(echo(instr)); 
+            if (instr.equals("list")) {
+                listOut();
+            } else {
+                addText(instr);
+                System.out.println(echo(instr)); 
+            }
             instr = sc.nextLine();
         }
 
@@ -26,6 +34,16 @@ public class Duke {
     }
 
     public static String echo(String instr) {
-        return instr;
+        return "added: " + instr;
+    }
+
+    public static void listOut() {
+        for (int i = 0; i < ptr; i++) {
+            System.out.println(i + 1 + ". " + instrList[i]);
+        }
+    }
+
+    public static void addText(String instr) {
+        instrList[ptr++] = instr;
     }
 }
