f62efbabb01393ee79d3a7604561eb786fe3ed5f
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-01-23 15:33:48 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +10,14 @@ public class Duke {
         System.out.println("Hello I'm Dune");
         System.out.println("What can I do for you?");
         System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String text = scanner.nextLine();  // Read user input
+            if (text.equals("bye")) {
+                break;
+            }
+            System.out.println("    " + text);  // Output user input
+        }
 
         System.out.println("Bye. Hope to see you again soon!");
     }
