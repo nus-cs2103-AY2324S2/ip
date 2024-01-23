b1c043b61b4eee9dab54a321be47f1bfcdc8a058
AwJoey <awjoey26@gmail.com>
2024-01-23 19:52:33 +0800

    edited design of greeting messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println( "-------------------------------- \n" +
+        System.out.println( "================================ \n" +
                             "Hello I'm Axolotl! \n" +
-                            "What can I do for you? \n");
+                            "What can I do for you? \n" +
+                            "================================ \n");
 
-        System.out.println( "-------------------------------- \n" +
+        System.out.println( "================================ \n" +
                             "Bye. Hope to see you again soon! \n" +
-                            "-------------------------------- \n");
+                            "================================ \n");
 
     }
 }
