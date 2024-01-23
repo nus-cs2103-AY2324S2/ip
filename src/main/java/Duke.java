79fae651dc7379caacd0a82dffa21a725064e6d1
hjungwoo01 <hjw1129@gmail.com>
2024-01-23 11:43:23 +0800

    Implemented Task class and added mark/unmark task functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,20 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
+        List<Task> tasks = new ArrayList<>();
+
         String chatBotName = "Nicky";
         pw.println("____________________________________________________________");
         pw.println("Hello! I'm " + chatBotName + "\nWhat can I do for you?");
         pw.println("____________________________________________________________");
         pw.flush();
-
-        String[] userResponses = new String[100];
-        int i = 0;
 
         while (true) {
             String currentResponse = br.readLine();
@@ -22,14 +23,23 @@ public class Duke {
 
             if (currentResponse.equals("bye")) {
                 break;
+            } else if (currentResponse.startsWith("mark ")) {
+                int index = Integer.parseInt(currentResponse.substring(5)) - 1;
+                tasks.get(index).markAsDone();
+                pw.println("Nice! I've marked this task as done:\n  " + tasks.get(index));
+            } else if (currentResponse.startsWith("unmark ")) {
+                int index = Integer.parseInt(currentResponse.substring(7)) - 1;
+                tasks.get(index).markAsNotDone();
+                pw.println("OK, I've marked this task as not done yet:\n  " + tasks.get(index));
             } else if (currentResponse.equals("list")) {
-                for (int j = 0; j < i; j++) {
-                    pw.println((j + 1) + ". " + userResponses[j]);
+                pw.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    pw.println((i + 1) + ". " + tasks.get(i));
                 }
             } else {
+                Task newTask = new Task(currentResponse);
+                tasks.add(newTask);
                 pw.println("added: " + currentResponse);
-                userResponses[i] = currentResponse;
-                i++;
             }
 
             pw.println("____________________________________________________________");
