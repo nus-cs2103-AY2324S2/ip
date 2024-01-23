cb863168585a44a91432145450700791da4af6ab
Giselle Tan <giselletan1008@gmail.com>
2024-01-24 01:28:41 +0800

    Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                System.out.println("Hello from\n" + logo);
+         */
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Emis!\nWhat can I do for you?");
+        exit();
+    }
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
