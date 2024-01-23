1786e32b0fd24bbfd6803871c324c69295ccba29
billyhoce <billyhoce@gmail.com>
2024-01-24 02:59:24 +0800

    Added scanner to take user input and ability to echo back messages

--- src/main/java/MeanDuke.java
+++ src/main/java/MeanDuke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MeanDuke{
     public static void main(String[] args) {
         String logo =
@@ -12,7 +14,15 @@ public class MeanDuke{
         String intro = logo + "What do you want this time?\n" + spacer;
         System.out.println(intro);
 
-        String outro = "Finally you're finished, thought you will never stop yapping.\n" + spacer;
+        //Echoes back user input until the user inputs "end", which stops the conversation
+        Scanner inputScanner= new Scanner(System.in);
+        String userInput = inputScanner.nextLine();
+        while (!userInput.equals("end")) {
+            System.out.println(spacer + userInput + "\n" + spacer);
+            userInput = inputScanner.nextLine();
+        };
+
+        String outro = spacer + "Finally you're finished, thought you would never stop yapping.\n" + spacer;
         System.out.println(outro);
     }
 }
