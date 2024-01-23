887927794935dd21c0bc4c8772007423b3a12795
theman-oj10 <manojnarender@gmail.com>
2024-01-23 12:42:33 +0800

    Duke Class: implement Echo Functionality
    Duke class only had a Greet and Exit functionality.
    
    Added Echo functionality to fit the project requirements.
    
    Adding Echo Functionality will make the class adhere to project requirements.
    
    Let's
    * Add Echo method
    * Abstract the existing greet and bye functionalities into methods for better code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String line = "------------------------------------------------------------";
+    static final String line = "________________________________________________________________________________________";
+
+    public void greet() {
         System.out.println(line);
         System.out.println("Hello! I'm Alfred");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+    public void bye() {
         System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+    public void echo(String input) {
+        System.out.println(line);
+        System.out.println(input);
+        System.out.println(line);
+    }
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.greet();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                duke.bye();
+                break;
+            }
+            duke.echo(input);
+        }
     }
 }
