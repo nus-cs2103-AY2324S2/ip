5ec09ba5608719c8b79d1c89d0a5fbceeae4815d
Chu Jie <chujie2003@gmail.com>
2024-01-23 17:52:24 +0800

    finished level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,24 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello! My name is Mitsuki, nice to meet you!\n" + "What can I do for you today?\n");
+
         String answer = "nil";
+
+        String[] list = new String[100];
+        int i = 0;
+
         while (!answer.equals("bye")) {
-            answer = scan.next();
-            if (!answer.equals("bye")) {
-                System.out.println("Did you say: " + answer + "?");
+            answer = scan.nextLine();
+            if (!answer.equals("bye") && !answer.equals("list")) {
+                list[i] = answer;
+                i++;
+                System.out.println("Added: '" + answer + "' to your list!");
+            } else if (answer.equals("list")) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.println(j + ". " + list[j - 1]);
+                }
             }
         }
 
