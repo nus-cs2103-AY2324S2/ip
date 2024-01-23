643d9b2e56fb912edd38a583be50fd13a5ab09a5
lynnetteeee <lynnette.oxh@gmail.com>
2024-01-24 00:59:56 +0800

    finish level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line = "__________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Floofy");
         System.out.println("What can I do for you?");
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine(); // Read user input
+            if ("bye".equalsIgnoreCase(userInput)) {
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(userInput);
+                System.out.println(line);
+            }
+        }
+        scanner.close();
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
