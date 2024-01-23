9ac28d51bcb27be7c5542498bbe05fa7f971b0a0
Alphajae11 <leejaegeun20@gmail.com>
2024-01-23 15:00:27 +0800

    Accomplished level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm DOUMMI\n" +
+                "What can I do for you?";
+
+        String bye = "Hope to see you again soon!";
+
+        System.out.println(greet);
+        System.out.println(bye);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
