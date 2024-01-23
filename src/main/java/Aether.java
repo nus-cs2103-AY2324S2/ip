0fed9cf894b19ccc49b5a15f22c7e69a47d21549
khushi833 <71274442+khushi833@users.noreply.github.com>
2024-01-23 22:55:45 +0800

    rename, greet, exit

---
+++ src/main/java/Aether.java
@@ -0,0 +1,15 @@
+public class Aether {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("_________________________________");
+        System.out.println("Hello! I'm Aether!");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
