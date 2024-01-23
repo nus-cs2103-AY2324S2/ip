6b31b9d815782b7bc929e5d41fe8812670542de7
sindhurajain <sindhura394@gmail.com>
2024-01-24 04:56:56 +0800

    javadocs for Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,25 @@ public class Duke {
         Duke duke = new Duke();
         duke.startChat();
     }
+    
+    /**
+     * Initiates the chat by invoking the sayHi() and sayBye() methods.
+     */
     public void startChat() {
         sayHi();
         sayBye();
     }
+
+    /**
+     * Displays a starting message to greet the user.
+     */
     public void sayHi() {
         System.out.println("Hello! I'm myChats\n" + "What can I do for you?\n");
     }
+
+    /**
+     *  Displays an exit message.
+     */
     public void sayBye() {
         System.out.println("Bye. Hope to see you again soon!\n");
     }
