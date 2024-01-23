6b1a123f319e3a71a1761aea0a46b41b2fc014a5
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-01-23 16:42:33 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,21 +11,54 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Dune");
         System.out.println("What can I do for you?");
-        List<String> toDos = new ArrayList<>();
+        List<Task> toDos = new ArrayList<>();
         System.out.println("");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String text = scanner.nextLine();  // Read user input
             if (text.equals("list")) {
+                // print out tasks line by line
                 for (int i = 0; i < toDos.size(); i++) {
                     System.out.println("    " + i + ". " + toDos.get(i));
                 }
                 continue;
-            }
-            if (text.equals("bye")) {
+            } else if (text.equals("bye")) {
+                // exit program
                 break;
             }
-            toDos.add(text);
+
+            // check if the first 4 characters matches mark
+            String upTo4Characters = text.substring(0, Math.min(text.length(), 4));
+            if (upTo4Characters.equals("mark")) {
+                try {
+                    String remaining = text.substring(4);
+                    int start = 0;
+                    // if the first character is blank space
+                    // might throw IndexOutOfBounds exception
+                    if (remaining.charAt(0) == ' ') {
+                        start = 1;
+                    }
+                    // substring might throw IndexOutOfBounds exception
+                    // parseInt might throw NumberFormatException
+                    int index = Integer.parseInt(remaining.substring(start));
+                    // Index... exception
+                    toDos.get(index).complete();
+                    System.out.println("    " + "Nice! I've marked this task as done:");
+                    System.out.println("    " + toDos.get(index));
+
+                } catch (IndexOutOfBoundsException i) {
+                    System.out.println("    " + "Give a valid index to mark");
+                } catch (NumberFormatException n) {
+                    System.out.println("    " + "Remaining characters do not match an integer");
+                } finally {
+                    continue;
+                }
+            }
+
+            // check if first 6 characters match unmark
+
+
+            toDos.add(new Task(text));
             System.out.println("    added:" + text);  // Output user input
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+
+    String description;
+    boolean done;
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public void complete() {
+        this.done = true;
+    }
+
+    public void incomplete() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        String done = this.done ? "[X]": "[ ]";
+        return done + " " + this.description;
+    }
+}
