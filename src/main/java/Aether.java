671c9d337403ad9f48871015a5386f3a7bbee9b4
khushi833 <71274442+khushi833@users.noreply.github.com>
2024-01-23 23:09:20 +0800

    rename, greet, exit

--- src/main/java/Aether.java
+++ src/main/java/Aether.java
@@ -5,11 +5,12 @@ public class Aether {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("_________________________________");
+//                System.out.println("Hello from\n" + logo);
+        System.out.println("_____________________________");
         System.out.println("Hello! I'm Aether!");
         System.out.println("What can I do for you?");
-        System.out.println("_________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________");
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("_____________________________");
     }
 }
