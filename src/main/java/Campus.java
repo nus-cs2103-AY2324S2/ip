cb37e7a95cd0b455c3804075c99a2969cb0fa607
Kappaccinoh <lim.jia28@gmail.com>
2024-01-23 18:35:55 +0800

    Add List

--- src/main/java/Campus.java
+++ src/main/java/Campus.java
@@ -1,7 +1,11 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Campus {
+    static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Campus.greet();
 
@@ -12,16 +16,37 @@ public class Campus {
             if (Objects.equals(userInput, "bye")) {
                 scanner.close();
                 break;
+            } else if (Objects.equals(userInput, "list")) {
+                Campus.display();
+            } else {
+                Campus.add(userInput);
             }
-            Campus.echo(userInput);
         }
 
         Campus.exit();
     }
 
-    public static void echo(String input) {
+    public static void display() {
+        int numOfTasks = Campus.tasks.size();
+        String listOfTasks = "";
+        for (int i = 0; i < numOfTasks; i++) {
+            int listNum = i + 1;
+            String task = Campus.tasks.get(i);
+            listOfTasks += String.format("%s. %s\n", listNum, task);
+        }
+
         String message = "------------------------------\n"
-                + String.format("%s\n", input)
+                + String.format("%s\n", listOfTasks)
+                + "------------------------------\n";
+
+        System.out.println(message);
+    }
+
+    public static void add(String input) {
+        Campus.tasks.add(input);
+
+        String message = "------------------------------\n"
+                + String.format("added: %s\n", input)
                 + "------------------------------\n";
 
         System.out.println(message);
