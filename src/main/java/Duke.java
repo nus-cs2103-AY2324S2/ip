61f3e98fef5cf1b2c77f47e076a5e11b9b6a8dd5
yespiggy <skylarye7@gmail.com>
2024-01-23 14:28:05 +0800

    Level-3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,9 @@ public class Duke {
     public static void main(String[] args) {
         String sayHi = "Helloo! I'm LilyBot ;)\nWhat's up'?\n";
         String divider = "_____________________________________________";
-        ArrayList<String> ls = new ArrayList<>();
+        String markDone = "Good job! I've marked this task as done: ";
+        String markNotDone = "Okie, Rest well. Marked this task as not done yet: ";
+        ArrayList<Task> ls = new ArrayList<>();
         int counter = 0;
 
         //Scanner for getting user input
@@ -17,14 +19,35 @@ public class Duke {
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                for (String i : ls) {
+                System.out.println("Here're the tasks in ur list:");
+                for (Task i : ls) {
                     counter++;
-                    System.out.println(counter + ". " + i);
+                    System.out.println(counter + ". " +
+                            "[" + i.getStatusIcon() + "] " + i.getDescription());
                 }
                 System.out.println(divider);
                 command = myCom.nextLine();
+            } else if (command.startsWith("unmark")) {
+                int taskNum = Integer.valueOf(command.replaceAll("[^0-9]", ""));
+                Task tk = ls.get(taskNum - 1);
+
+                tk.unmark();
+                System.out.println(markNotDone+"\n"+
+                        "  [" + tk.getStatusIcon() + "] " + tk.getDescription());
+                System.out.println(divider);
+                command = myCom.nextLine();
+            } else if (command.startsWith("mark")) {
+                int taskNum = Integer.valueOf(command.replaceAll("[^0-9]", ""));
+                Task tk = ls.get(taskNum - 1);
+
+                tk.mark();
+                System.out.println(markDone+"\n"+
+                        "  [" + tk.getStatusIcon() + "] " + tk.getDescription());
+                System.out.println(divider);
+                command = myCom.nextLine();
             } else {
-                ls.add(command);
+                Task t = new Task(command);
+                ls.add(t);
                 System.out.println("  added: "  + command + "\n" + divider);
                 command = myCom.nextLine();
             }
