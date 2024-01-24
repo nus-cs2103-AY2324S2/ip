1de678a29dfd11bd7dd3a290a70d8e71af411a29
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-01-25 02:15:06 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,20 +19,42 @@ public class Duke {
         String barrier = "____________________________________________________________";
         Boolean exit = true;
         ArrayList<String> todoList = new ArrayList<String>();
+        ArrayList<Boolean> checkList = new ArrayList<Boolean>();
         System.out.println(Greeting);
         while (exit) {
             Scanner myObj = new Scanner(System.in);
             String userInput = myObj.nextLine();
+            String[] inputList = userInput.split(" ");
             if (userInput.equals("bye")) {
                 exit = false;
             } else if (userInput.equals("list")) {
                 System.out.println(barrier);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < todoList.size(); i++) {
-                    System.out.println(i+1 + ". " + todoList.get(i));
+                    if (checkList.get(i)) {
+                        System.out.println(i+1 + ".[X] " + todoList.get(i));
+                    } else {
+                        System.out.println(i+1 + ".[ ] " + todoList.get(i));
+                    }
                 }
+                System.out.println(barrier);
+            } else if (inputList[0].equals("mark")) {
+                int position = Integer.parseInt(inputList[1]);
+                System.out.println(barrier);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + todoList.get(position-1));
+                checkList.set(position-1, true);
+                System.out.println(barrier);
+            } else if (inputList[0].equals("unmark")) {
+                int position = Integer.parseInt(inputList[1]);
+                System.out.println(barrier);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + todoList.get(position-1));
+                checkList.set(position-1, false);
                 System.out.println(barrier);
             } else {
                 todoList.add(userInput);
+                checkList.add(false);
                 System.out.println(barrier);
                 System.out.println("added: " + userInput);
                 System.out.println(barrier);
