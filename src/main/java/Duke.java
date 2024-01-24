471ca5ff0616b56574dfe518d6c3d238777ed96b
saiutkarsh33 <122259179+saiutkarsh33@users.noreply.github.com>
2024-01-24 11:20:53 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = "Hello! I'm TFamilyBot\n"
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        String prompt = "Hello! I'm TFamilyBot\n"
                 + "What can I do for you? \n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n";     
-        System.out.println(logo);
+                + "____________________________________________________________\n";   
+        pw.println(prompt);
+
+        while (true) {
+            String io = br.readLine();
+
+            pw.println("____________________________________________________________\n");
+
+            if (io.equals("bye")) {
+                pw.println("Bye. Hope to see you again soon!");
+                pw.println("____________________________________________________________\n");
+                break;
+            }
+
+            pw.println(io);
+            pw.println("____________________________________________________________\n");
+        }
     }
 }
