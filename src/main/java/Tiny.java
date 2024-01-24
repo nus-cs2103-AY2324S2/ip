81ff1a1a1157ca52dd13d53f74ab9527f1c023df
Yskie <shikang@u.nus.edu>
2024-01-24 21:44:42 +0800

    Echo and Bye Function

--- src/main/java/Tiny.java
+++ src/main/java/Tiny.java
@@ -1,14 +1,29 @@
 import java.io.*;
 
 public class Tiny {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PrintWriter out = new PrintWriter(System.out);
-        out.println("____________________________________________________________");
-        out.println(" Hello! I'm Tiny!");
-        out.println(" What can I do for you?");
-        out.println("____________________________________________________________");
-        out.println(" Bye. Hope to see you again soon!");
-        out.println("____________________________________________________________");
+        out.println("   ____________________________________________________________");
+        out.println("   Hello! I'm Tiny!");
+        out.println("   What can I do for you?");
+        out.println("   ____________________________________________________________\n");
+        out.flush();
+
+        while (true) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();   
+            if (input.equals("bye")) break;
+            out.println("   ____________________________________________________________");
+            out.println("   " + input);
+            out.println("   ____________________________________________________________\n");
+            out.flush();
+        }
+        out.println("   ____________________________________________________________");
+        out.println("   Bye. Hope to see you again soon!");
+        out.println("   ____________________________________________________________");
+
+
+
         out.flush();
     }
 }
