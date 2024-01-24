d53f6771c41b57d5668d18a9e797f1dbfa4d63f9
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-24 15:32:47 +0800

    Renamed Duke.java to Lindi.java, implemented simple greet and exit

---
+++ src/main/java/Lindi.java
@@ -0,0 +1,21 @@
+public class Lindi {
+    public static void printSeparator() {
+        System.out.println("-".repeat(50));
+    }
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String name = "Lindi";  // Log It N Do It -> LINDI
+        printSeparator();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        printSeparator();
+        System.out.println("Bye. Hope to see you again soon!");
+        printSeparator();
+    }
+}
