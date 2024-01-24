85d8cd9e0393b1491ce0c3ebef8a8cf1f72d810d
Ashley Chua <ashleycxr2444@gmail.com>
2024-01-25 02:34:14 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Georgie";
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! I'm " + name + ".");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(" " + userInput);
+
+        }
+
+        scanner.close();
     }
 }
