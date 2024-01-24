c13626ffd2b01c9c20d29c4ef0c9c0eb94a1ee47
AidanG <agoh0185@gmail.com>
2024-01-24 12:48:13 +0800

    feat: Removed Duke.java and added Steven.java

---
+++ src/main/java/Steven.java
@@ -0,0 +1,10 @@
+public class Steven {
+
+    public static void main(String args[]) {
+
+        String bootMsg = ("=========\nThis is Steven!\nHow can I advise?\n=========");
+        System.out.println(bootMsg);
+
+        System.out.println("I'll see you soon then!");
+    }
+}
