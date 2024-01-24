8306dadcb2c824a4d0d3f8e1edf26d33014f1d55
Alteqa <e0969816@u.nus.edu>
2024-01-25 02:09:00 +0800

    mark and unmark done

--- src/main/java/hirwan.java
+++ src/main/java/hirwan.java
@@ -14,15 +14,35 @@ public class hirwan {
         while(true) {
             Scanner scanner = new Scanner(System.in);
             String text = scanner.nextLine();
+            boolean mark = text.startsWith("mark");
+            boolean unmark = text.startsWith("unmark");
             if (text.equals("bye")) {
                 break;
             } else if(text.equals("list")) {
                 for (String element : List) {
                     System.out.println(element);
                 }
+            } else if(mark) {
+                String number = text.substring(5);
+                int numberint = Integer.parseInt(number);
+                if (numberint >= List.size()) {
+                    System.out.println("Sorry please enter a valid index!");
+                }
+                String temp = List.get(numberint - 1).substring(7);
+                List.set(numberint - 1, numberint + ". [X] " + temp);
+                System.out.println("Nice! I've marked this task as done: \n" + "[X] " + temp);
+            } else if(unmark) {
+                String number = text.substring(7);
+                int numberint = Integer.parseInt(number);
+                if (numberint >= List.size()) {
+                    System.out.println("Sorry please enter a valid index!");
+                }
+                String temp = List.get(numberint - 1).substring(9);
+                List.set(numberint - 1, numberint + ". [ ] " + temp);
+                System.out.println("OK, I've marked this task as not done yet: \n" + "[ ] " + temp);
             } else {
                 System.out.println("added: " + text);
-                List.add(count + ". " + text);
+                List.add(count + ". [ ] " + text);
                 count++;
             }
         }
