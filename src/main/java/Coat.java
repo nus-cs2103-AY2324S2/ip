1d02c8d6b29adf1e0a7d536dc79e3936c36269a7
aman zainal <aman@u.nus.edu>
2024-01-25 00:55:03 +0800

    added simple echo logic

--- src/main/java/Coat.java
+++ src/main/java/Coat.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Coat {
     public static void main(String[] args) {
-        System.out.println(Messages.WELCOME);
+        System.out.printf(Messages.WELCOME);
 
-        System.out.println(Messages.EXIT);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.printf("-> ");
+            String userInput = scanner.nextLine();
 
+            // Exit condition: say "bye"
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.printf(Messages.EXIT);
+                break;
+            }
+
+            // Level-1: Echo user input
+            System.out.printf(">>> %s\n", userInput);
+        }
+
+
+        scanner.close();
     }
 }
