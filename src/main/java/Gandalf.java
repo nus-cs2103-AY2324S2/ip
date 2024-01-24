f06dbb2ce274183e5a080759fc7ea788e63b6cdf
dabzpengu <mahathir66@gmail.com>
2024-01-25 02:30:30 +0800

    renamed, greeted and exit

---
+++ src/main/java/Gandalf.java
@@ -0,0 +1,7 @@
+public class Gandalf {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm Gandalf");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
