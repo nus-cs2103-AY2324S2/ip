4f29acbdb5e59a03b609ea878e8a729de9f38f34
ThitipatC <thitipatchaiyakul@gmail.com>
2024-01-24 23:08:00 +0800

    Create chatbot that echoes until bye is typed in

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                "Hello! I'm HASSTNT\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-
-        System.out.println(logo);
+        String welcome_message = "____________________________________________________________\n" +
+        "Hello! I'm HASSTNT\n" +
+        "What can I do for you?\n" +
+                "____________________________________________________________\n";
+        System.out.println(welcome_message);
+        conversation();
     }
-}
+    public static void conversation(){
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            input = scanner.nextLine();
+            System.out.println(input);
+        } while (!input.equalsIgnoreCase("bye"));
+        System.out.println("____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+        scanner.close();
+    }
+    }
