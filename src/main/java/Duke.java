18f2d3fa75941ea2dcd26f4f586ed4d08770246a
Abdul Khaliq <48424170+breezetall@users.noreply.github.com>
2024-01-24 14:19:33 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     public static String line = "-----------------------------";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println(line);
         System.out.println("Hello! I'm Datuk");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you today?\n");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        boolean finish = false;
+
+        while(!finish) {
+            while(br.ready()) {
+                String[] text = br.readLine().split(" ");
+
+                if (text[0].equals("bye")) {
+                    finish = true;
+                } else {
+                    System.out.println(line);
+                    for (String x : text) {
+                        System.out.print(x);
+                        System.out.print(" ");
+                    }
+                    System.out.println();
+                    System.out.println(line);
+                }
+            }
+        }
+
+        System.out.println(line);
+        System.out.println("Bye. See you later!");
         System.out.println(line);
     }
 }
