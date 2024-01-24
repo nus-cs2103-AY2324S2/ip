53450164821896b68715fb617cd2d67207ebb7e5
Jaime Ng <jaimengjm@gmail.com>
2024-01-24 20:14:46 +0800

    Add unmarkAsDone and markAsDone to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,4 +10,12 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); //mark done task with X
     }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
 }
