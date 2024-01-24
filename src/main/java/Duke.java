ada4e28872fccfb286e1d11c5c8a5e8047ca6da4
Max Yang <>
2024-01-24 11:59:05 +0800

    Echoes user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String divider = "---------------------------------------------------------------";
         String name = "Dwight";
-        System.out.printf("%s\nHello! I'm %s\nWhat Can I do for you?\n%s\nBye. Hope to see you again soon!\n%s"
-                , divider, name, divider, divider);
+        System.out.printf("%s\nHello! I'm %s\nWhat Can I do for you?\n%s\n"
+                , divider, name, divider);
+        String input;
+        while (scanner.hasNext()) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.printf("\t%s\n\tBye. Hope to see you again soon!\n\t%s\n", divider,divider);
+                scanner.close();
+                return;
+            } else {
+                System.out.printf("\t%s\n\t%s\n\t%s\n", divider, input, divider);
+            }
+        }
+
     }
 }
