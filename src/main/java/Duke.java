a11fc68b93ad7821ed2344b66273cb4b9e21fc0f
JOELPC\joelw <omegabeeminecraft@gmail.com>
2024-01-25 05:05:46 +0800

    level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,10 +21,40 @@ public class Duke {
         System.out.println(indent + "Bye. Hope to see you again soon!");
         spacing();
     }
-    public void echo() {
+    public task identify(String request){
+        if (request.startsWith("todo")) {
+            String[] reqList = request.split("todo ");
+            todo current = new todo(reqList[1]);
+            return current;
+        } else if (request.startsWith("deadline")) {
+            String[] reqList = request.split("deadline ");
+            String[] timeList = reqList[1].split("/by ");
+            String name = timeList[0];
+            String time = timeList[1];
+            deadline current = new deadline(name, time);
+            return current;
+
+        } else if (request.startsWith("event")) {
+            String[] reqList = request.split("event ");
+            String[] startList = reqList[1].split(" /from ");
+            String name = startList[0];
+            String[] endList = startList[1].split(" /to ");
+            String start = endList[0];
+            String end = endList[1];
+
+            event current = new event(name, start, end);
+            return current;
+
+        } else{
+            task current = new task(request);
+            return current;
+        }
+    }
+
+    public void start() {
         intro();
         Scanner input = new Scanner(System.in);
-        ArrayList<task> todo = new ArrayList<task>();
+        ArrayList<task> taskList = new ArrayList<task>();
         while(true) {
             String current = input.nextLine();
             if(current.equals("bye")) {
@@ -35,43 +65,41 @@ public class Duke {
                 separate();
                 System.out.println(indent + "Here are the tasks in your list:");
                 int count = 1;
-                for (task i : todo) {
-                    if (i.isDone()) { /* if done*/
-                        System.out.println(indent + Integer.toString(count) + ".[X] " + i.getName());
-                    } else { /* not done*/
-                        System.out.println(indent + Integer.toString(count) + ".[ ] " + i.getName());
-                    }
+                for (task i : taskList) {
+                    System.out.println(indent + Integer.toString(count) + "." + i.getStatus());
                     count++;
                 }
                 spacing();
             } else if (current.startsWith("mark")) {
-                /* adding to done*/
                 String[] marking = current.split(" ");
                 int position = Integer.parseInt(marking[1]) - 1;
-                task curr = todo.get(position);
+                task curr = taskList.get(position);
                 curr.makeDone();
 
                 separate();
                 System.out.println(indent + "Nice! I've marked this task as done:");
-                System.out.println(indent + "  [X] " + todo.get(position).getName());
+                System.out.println(indent + taskList.get(position).getStatus());
                 spacing();
 
             } else if (current.startsWith("unmark")) {
                 /* removing from done */
                 String[] marking = current.split(" ");
                 int position = Integer.parseInt(marking[1]) - 1;
-                task curr = todo.get(position);
+                task curr = taskList.get(position);
                 curr.makeUndone();
 
                 separate();
                 System.out.println(indent + "OK, I've marked this task as not done yet:");
-                System.out.println(indent + "  [ ] " + todo.get(position).getName());
+                System.out.println(indent + taskList.get(position).getStatus());
                 spacing();
             } else {
-                task newTask = new task(current);
-                todo.add(newTask);
+                task newTask = identify(current);
+                taskList.add(newTask);
                 separate();
-                System.out.println(indent + "added: " + current);
+                System.out.println(indent + "Got it. I've added this task:");
+                System.out.println(indent + "  " + newTask.getStatus());
+                System.out.println(indent + "Now you have " + Integer.toString(taskList.size()) +
+                        " tasks in the list.");
                 spacing();
             }
         }
@@ -79,6 +107,7 @@ public class Duke {
     public class task {
         private boolean done = false;
         private String name;
+        private String type; /* T, D or E*/
 
         public task(String name){
             this.name = name;
@@ -96,10 +125,71 @@ public class Duke {
         public void makeUndone(){
             this.done = false;
         }
+        /* add getType for each task type*/
+        public String getType(){
+            return "";
+        }
+
+        public String getTime(){
+            return "";
+        }
+        public String getStatus(){
+            if (this.isDone()) {
+                return this.getType() + "[X] " + this.getName() + this.getTime();
+            } else {
+                return this.getType() + "[ ] " + this.getName() + this.getTime();
+            }
+        }
+    }
+    public class todo extends task{
+
+        public todo(String name) {
+            super(name);
+        }
+        @Override
+        public String getType(){
+            return "[T]";
+        }
+
+    }
+    public class deadline extends task{
+        private String end;
+        public deadline(String name, String end) {
+            super(name);
+            this.end = end;
+        }
+        @Override
+        public String getType(){
+            return "[D]";
+        }
+    @Override
+        public String getTime(){
+            return "(by: " + end + ")";
+        }
+    }
+
+    public class event extends task{
+        private String start;
+        private String end;
+
+        public event(String name, String start, String end) {
+            super(name);
+            this.start = start;
+            this.end = end;
+        }
+        @Override
+        public String getType(){
+            return "[E]";
+        }
+        @Override
+        public String getTime(){
+            return " (from: " + start + " to: " + end + ")";
+        }
+
     }
 
     public static void main(String[] args) {
         Duke duke = new Duke();
-        duke.echo();
+        duke.start();
     }
 }
