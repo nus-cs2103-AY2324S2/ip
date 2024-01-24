824decfbff46b65aa5f081e9d0583cdd7f8863a3
tanqinyong <tqinyong@yahoo.com.sg>
2024-01-24 14:14:23 +0800

    [Level-1] Updated Felix(Duke) to echo commands entered by the user and exit when user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Greeting.print();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String echoCommand = sc.nextLine();
+            if (echoCommand.equalsIgnoreCase("bye")) {
+                System.out.println("Goodbye! Till we meet again ~");
+                break;
+            }
+            System.out.println("------------------------------------");
+            System.out.println(echoCommand);
+            System.out.println("------------------------------------");
+        }
+
+
     }
 }
