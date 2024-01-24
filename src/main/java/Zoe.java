9211cdf6bba4fee6f3341bd2838fd61dc2fc109b
DOMOPC\domin <Dominiclimyongen1@gmail.com>
2024-01-24 21:44:06 +0800

    Add and List directly implemented into main.

--- src/main/java/Zoe.java
+++ src/main/java/Zoe.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.*;
 public class Zoe {
     public static void main(String[] args) {
+        ArrayList<String> addedCommands = new ArrayList<String>();
         System.out.println("Hello! I'm Zoe");
         System.out.println("What can I do for you?");
 
@@ -8,7 +10,16 @@ public class Zoe {
         String command = scanner.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < addedCommands.size(); i++) {
+                    System.out.println(String.format
+                            ("%d. %s", i + 1, addedCommands.get(i)));
+                }
+            } else {
+                addedCommands.add(command);
+                System.out.println("added: " + command);
+            }
+
             command = scanner.nextLine();
         }
 
