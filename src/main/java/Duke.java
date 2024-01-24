f8ca4694e270f032a99ee74a3a3955c43bac3ee9
Ko-Khan <e0970313@u.nus.edu>
2024-01-24 13:19:24 +0800

    Bit nows supports adding and listing! Type anything to add it to the list and cmd "list" to list out everything in the list!

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,17 +8,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";*/
         System.out.println("Hi! This is Bit!\nWhat shall we do today?\n");
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int total = 0;
 
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                toList(list, total);
+            } else {
+                list[total] = input;
+                total++;
             }
-            System.out.println(input);
 
         }
-
-
         System.out.println("Alright. See you soon!");
+    }
+
+    public static void toList(String[] list, int total) {
+        for (int i = 0; i < total; i++) {
+            if (list[i] == null) {
+                break;
+            }
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+
     }
 }
