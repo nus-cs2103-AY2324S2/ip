14ff4a822c05e7d32e2dc0fd32e51248ea35c7c1
guohuang <guohuang88@gmail.com>
2024-01-24 12:53:47 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm GHBot");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            if (str.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(str);
+        }
     }
 }
