2a186bc80413949aa0398f90600b0304f2414f05
ziyang27 <tanziyang39@gmail.com>
2024-01-24 11:20:59 +0800

    Echo

--- src/main/java/Dibo.java
+++ src/main/java/Dibo.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 public class Dibo {
     private static String name = "Dibo the Gift Chatbot";
     public static void main(String[] args) {
+        // Greeting the user
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println();
+
+        // Getting the command
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        // Responding
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+
+        // Exiting
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
