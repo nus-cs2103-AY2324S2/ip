0017d012394e973d184fe888562d50400f285c70
Jaime Ng <jaimengjm@gmail.com>
2024-01-24 22:00:18 +0800

    Add Event class with toString method

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,11 +1,10 @@
 public class ToDo extends Task {
-    protected String description;
     public ToDo(String description) {
         super(description);
     }
 
     @Override
     public String toString() {
-        return description;
+        return "[T]" + super.toString();
     }
 }
