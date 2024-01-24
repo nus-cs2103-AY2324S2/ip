d924130f4dc189cb3dc98acdc2842ea0dee9253f
jyztintan <justintanwk2001@gmail.com>
2024-01-24 15:06:12 +0800

    Refactor code to improve readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,20 +26,17 @@ public class Duke {
                 Integer index = Integer.parseInt(inputs[1]);
                 Task task = taskList[index - 1];
                 task.unmarkDone();
-            } else if (message.startsWith("todo ")) {
-                Task task = new ToDo(message);
+            } else if (message.startsWith("todo")) {
+                Task task = initTask(message, "todo");
                 addTasktoTaskList(task);
-            } else if (message.startsWith("deadline ")) {
-                String[] inputs = message.split("/by");
-                Task task = new Deadline(inputs[0].trim(), inputs[1].trim());
+            } else if (message.startsWith("deadline")) {
+                Task task = initTask(message, "deadline");
                 addTasktoTaskList(task);
-            } else if (message.startsWith("event ")) {
-                String[] inputs = message.split("/from");
-                String[] innerInputs = inputs[1].split("/to");
-                Task task = new Event(inputs[0].trim(), innerInputs[0].trim(), innerInputs[1].trim());
+            } else if (message.startsWith("event")) {
+                Task task = initTask(message, "event");
                 addTasktoTaskList(task);
             } else {
-                Task task = new Task(message);
+                Task task = initTask(message, "");
                 addTasktoTaskList(task);
             }
         }
@@ -75,16 +72,32 @@ public class Duke {
         }
     }
 
-//    public static void initTask(String message, String taskType) {
-//        Task task;
-//        try {
-//            if (taskType.equals("todo")) {
-//
-//            }
-//        }
-//    }
+    public static Task initTask(String message, String taskType) {
+        Task task;
+        try {
+            if (taskType.equals("todo")) {
+                task = new ToDo(message);
+            } else if (taskType.equals("deadline")) {
+                String[] inputs = message.split("/by");
+                task = new Deadline(inputs[0].trim(), inputs[1].trim());
+            } else if (taskType.equals("event")) {
+                String[] inputs = message.split("/from");
+                String[] innerInputs = inputs[1].split("/to");
+                task = new Event(inputs[0].trim(), innerInputs[0].trim(), innerInputs[1].trim());
+            } else {
+                task = new Task(message);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Whats the task, yapper???");
+            return null;
+        }
+        return task;
+    }
 
     public static void addTasktoTaskList(Task task) {
+        if (task == null) {
+            return;
+        }
         taskList[listSize] = task;
         Duke.echo(task.toString());
         listSize++;
