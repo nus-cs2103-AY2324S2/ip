c8a51e3660ae2a98aeaa4ce088f924a3c8fa7595
Max Yang <>
2024-01-24 11:04:15 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "---------------------------------------------------------------";
+        String name = "Dwight";
+        System.out.printf("%s\nHello! I'm %s\nWhat Can I do for you?\n%s\nBye. Hope to see you again soon!\n%s"
+                , divider, name, divider, divider);
     }
 }
