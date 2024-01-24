f823d93cf3666a633a780f33959a49cdb20d4d97
tayziyi <ziyitay@Zis-MacBook-Pro.local>
2024-01-24 11:26:47 +0800

    Level-0

---
+++ src/main/java/SecretaryW.java
@@ -0,0 +1,8 @@
+public class SecretaryW {
+    public static void main(String[] args) {
+        String line = "------------------------------------------\n";
+        String greeting = "Hello! I'm SecretaryW\n" + "What can I do for you?\n";
+        String farewell = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + greeting + line + farewell);
+    }
+}
