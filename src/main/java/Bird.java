9637137b76ee426c26ac7d91ff5dfdf55f9f6cf1
Apzure <block4556@gmail.com>
2024-01-24 22:54:37 +0800

    Level-1

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Bird {
     public static void main(String[] args) {
-        System.out.println("Hello from I'm Bird");
+         
         System.out.println(" /\\_/\\");
         System.out.println("((@v@))");
         System.out.println("():::()");
         System.out.println(" VV-VV");
         System.out.println("What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        scanner.close(); 
     }
 }
