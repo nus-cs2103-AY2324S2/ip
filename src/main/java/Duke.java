35f8530c584b9cf51888f0a205772f9cbb09e5c7
RunjiaChen <runjia@u.nus.edu>
2024-01-24 15:59:06 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
-    private String[] tasks;
+    private Task[] tasks;
     private int counter;
 
 
 
     public Duke() {
-        this.tasks = new String[100];
+        this.tasks = new Task[100];
         this.counter = 0;
     }
     public static void main(String[] args) {
@@ -35,16 +35,25 @@ public class Duke {
         System.out.println("added: " + task);
     }
 
-    public void addTask(String task) {
-        this.tasks[counter] = task;
+    public void addTask(String name) {
+        this.tasks[counter] = new Task(name);
         this.counter += 1;
-        System.out.println("added: " + task);
+        System.out.println("added: " + name);
     }
 
     public void listTask() {
         for (int i = 0; i < counter; i++) {
             System.out.println(i + 1 + ". " + this.tasks[i]);
         }
+        return;
+    }
+
+    public void mark(int i) {
+        this.tasks[i].doTask();
+    }
+
+    public void unmark(int i) {
+        this.tasks[i].undoTask();
     }
 
     public void runBot() {
@@ -54,15 +63,24 @@ public class Duke {
 
         while (active) {
             String cmd = s.nextLine();
-
-            
-
             if (cmd.equals("bye")) {
                 active = false;
                 this.bye();
             } else if (cmd.equals("list")) {
+//                System.out.println("correct");
                 this.listTask();
+            } else if (cmd.contains("unmark")) {
+                    String[] lst = cmd.split(" ");
+                    int pos = Integer.parseInt(lst[1]);
+                    this.tasks[pos].undoTask();
+            } else if (cmd.contains("mark")) {
+                String[] lst = cmd.split(" ");
+                int pos = Integer.parseInt(lst[1]);
+                this.tasks[pos].doTask();
+
             } else {
+//                System.out.println("wrong" + cmd.equals("list"));
+//                System.out.println(cmd);
                 this.addTask(cmd);
             }
 
@@ -76,5 +94,51 @@ public class Duke {
 
 
 
+}
+
+
+class Task {
+
+    private String name;
+
+    private boolean done;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (done) {
+            return "[X]" + this.name;
+        } else {
+            return "[ ]" + this.name;
+        }
+
+    }
+
+    public void doTask() {
+        if (!done) {
+            this.done = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(this.toString());
+        } else {
+            System.out.println("This task is already done");
+        }
+
+    }
+
+    public void undoTask() {
+        if (done) {
+            this.done = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(this.toString());
+        } else {
+            System.out.println("This task has not been done");
+
+        }
+
+    }
 }
 
