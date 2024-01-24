38d820e692dd2ec68a6347ae69d49fd23dd76637
bennyLCK <e0969875@u.nus.edu>
2024-01-24 18:29:15 +0800

    Implement Add, List to Bond

--- src/main/java/Bond.java
+++ src/main/java/Bond.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Bond {
     public static void main(String[] args) {
@@ -10,15 +12,29 @@ public class Bond {
 
         Scanner sc = new Scanner(System.in);
 
+        ArrayList<String> List = new ArrayList<String>();
+
         while (true) {
 
             String userInput = sc.nextLine();
 
             if (userInput.equals("bye")) {
-                System.out.println(String.format("Bye. Hope to see you again soon! \n%s", line));
+                System.out.println(String.format("\nBye. Hope to see you again soon! \n%s", line));
                 System.exit(0);
             } else {
-                System.out.println(String.format("\n    %s \n%s", userInput, line));
+
+                if (userInput.equals("list")) {
+                    ListIterator toPrint = List.listIterator();
+
+                    while (toPrint.hasNext()) {
+                        System.out.println(String.format("\n    %d. %s", toPrint.nextIndex() + 1, toPrint.next()));
+                    }
+
+                    System.out.println(line);
+                } else {
+                    List.add(userInput);
+                    System.out.println(String.format("\n    Added: %s \n%s", userInput, line));
+                }
             }
         }
     }
