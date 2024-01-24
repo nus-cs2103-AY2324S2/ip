6d81f9d16dc19c2a25e8842eafe87f42696858ab
laney0808 <e0969207@u.nus.edu>
2024-01-25 00:49:18 +0800

    add, list

--- src/main/java/Rick.java
+++ src/main/java/Rick.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Rick {
+    public static ArrayList<String> list = new ArrayList<>();
     public static void main(String[] args) {
         hello();
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 exit();
                 break;
+            } else if (input.equalsIgnoreCase("list")){
+                list();
             } else {
-                echo(input);
+                add_to_list(input);
             }
         }
     }
@@ -30,5 +35,21 @@ public class Rick {
     }
     public static void echo(String args){
         reply(args);
+    }
+
+    public static void list() {
+        String divider = "\n____________________________________________________________\n";
+        System.out.println(divider);
+        String output = "";
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1) + ". " + list.get(i));
+        }
+        System.out.println(divider);
+    }
+
+    public static void add_to_list(String arg) {
+        list.add(arg);
+        String output = "added: " + arg;
+        reply(output);
     }
 }
