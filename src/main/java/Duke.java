53b82d489539d01d4813bd47c87b9b690ceb87fb
Benson <bensonang36@gmail.com>
2024-01-24 17:00:12 +0800

    Added Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,19 @@
 public class Duke {
+    private String name;
+    public Duke(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hello! I'm " + this.name + "\nWhat can I do for you?\n");
+    }
+
+    public void exit(){
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println('Testing\n');
+        Duke chatbot = new Duke("Bob");
+        chatbot.greet();
+        chatbot.exit();
     }
 }
