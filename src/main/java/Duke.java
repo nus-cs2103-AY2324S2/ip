14323664094115ceb9ccd2037ee289ead12c21a0
jamessinmaojun <jamessinmaojun@gmail.com>
2024-01-24 17:13:36 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,47 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String divider = "--------------------------------------------------";
+    String divider = "--------------------------------------------------";
+    ArrayList<Task> taskList = new ArrayList<Task>();
 
+    public boolean parseCommand() {
         Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
 
-        System.out.println(divider);
-        System.out.println("Hello! I'm TheGiantPeach\nWhat can I do for you?");
-        System.out.println(divider);
+        System.out.println(this.divider);
 
-        while (true) {
-            String command = scanner.nextLine();
-
-            System.out.println(divider);
-
-            if (Objects.equals(command, "bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+        if (Objects.equals(command, "bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            return false;
+        } else if (Objects.equals(command, "list")) {
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println(i + 1 + ". " + taskList.get(i).description);
             }
-
-            System.out.println(command);
-            System.out.println(divider);
+        } else {
+            this.taskList.add(new Task(command));
+            System.out.println("added: " + command);
         }
 
-        System.out.println(divider);
+        System.out.println(this.divider);
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Duke theGiantPeach = new Duke();
+
+        System.out.println(theGiantPeach.divider);
+        System.out.println("Hello! I'm TheGiantPeach\nWhat can I do for you?");
+        System.out.println(theGiantPeach.divider);
+
+        while (true) {
+            if (!theGiantPeach.parseCommand()) {
+                break;
+            }
+        }
+
+        System.out.println(theGiantPeach.divider);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,7 @@
+public class Task {
+    String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+}
