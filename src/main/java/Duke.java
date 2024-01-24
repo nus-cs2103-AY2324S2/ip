49c29dfd840f28a7c585fafccf84d4c585513b90
Timothy <102574520+LargeCrowd@users.noreply.github.com>
2024-01-24 17:33:31 +0800

    Update Duke.java
    Level-0 changes. Program exits after greeting with new name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        String name = "Big Boy";
+        String greeting = "Hello! I'm " + name + ", your new friend!" + "\n" + "What can I do for you?";
+        String line = "____________________________________________________________";
+        String exit = "Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(exit);
+        System.out.println(line);
     }
 }
