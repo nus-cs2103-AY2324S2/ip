8a2b6ea0e79ab2497b5c1c9107ff6f16c83ddb92
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-01-24 17:28:49 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String Greeting = "____________________________________________________________\n" +
+                " Hello! I'm VICTOR\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        String Ending = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(Greeting + Ending);
     }
 }
