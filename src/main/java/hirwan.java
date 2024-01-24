4345d52f0adf16412ea781ae59c7f459346cd268
Alteqa <e0969816@u.nus.edu>
2024-01-24 23:26:28 +0800

    Changed bot name and programme

---
+++ src/main/java/hirwan.java
@@ -0,0 +1,9 @@
+public class hirwan {
+    public static void main(String[] args) {
+        String logo = "I'm hirwan \n"
+                + "what can I do for you? \n"
+                + "Bye. Hope to see you again soon!";
+
+        System.out.println("Hello! " + logo);
+    }
+}
