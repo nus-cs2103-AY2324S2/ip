853403d4e6436a88595bd004a0b59df5368745f3
appleraincoat <james20111951@gmail.com>
2024-01-24 17:59:09 +0800

    Removed ability to echo, keeps a lsit of inputs now and prints it upon request.

--- src/main/java/Ypxmm.java
+++ src/main/java/Ypxmm.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Ypxmm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<String>();
         System.out.println("Hallo boss, I'm Ypxmm.");
         System.out.println("Need me do what for you?");
         while (true) {
@@ -10,8 +11,16 @@ public class Ypxmm {
             if (input.equals("bye")) {
                 System.out.println("Okok I zao first, see you again bro!");
                 break;
+            } else if (input.equals("list")) {
+                int count = 1;
+                for (String s : tasks) {
+                    System.out.println(count + ". " + s);
+                    count ++;
+                }
+            } else {
+                tasks.add(input);
+                System.out.println(input + " added");
             }
-            System.out.println(input);
         }
         sc.close();
     }
