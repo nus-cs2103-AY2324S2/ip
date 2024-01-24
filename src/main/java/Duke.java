e1b6d3d7e6a956692f364330f868d55873bf2b52
Wongqingyee <qingyeewong@gmail.com>
2024-01-25 01:36:28 +0800

    Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Chitty-Chatty.\n" +
+                "What can I do for you?");
+
+        System.out.println("\nBye. Hope to see you again soon!");
     }
 }
