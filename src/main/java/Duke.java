ba9446239aae55e2e54a007a2c8aedf2d410392f
Abdul Khaliq <48424170+breezetall@users.noreply.github.com>
2024-01-24 14:34:48 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Duke {
     public static String line = "-----------------------------";
+    public static ArrayList<String> ls;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ls = new ArrayList<>();
 
         System.out.println(line);
-        System.out.println("Hello! I'm Datuk");
-        System.out.println("What can I do for you today?\n");
+        System.out.println("Greetings friend! I am Datuk");
+        System.out.println("How can I serve you today?\n");
         System.out.println(line);
 
         boolean finish = false;
 
-        while(!finish) {
-            while(br.ready()) {
-                String[] text = br.readLine().split(" ");
+        while (!finish) {
+            while (br.ready()) {
+                String text = br.readLine();
 
-                if (text[0].equals("bye")) {
+                if (text.equals("bye")) {
                     finish = true;
+                } else if (text.equals("list")) {
+                    PrintList();
                 } else {
-                    System.out.println(line);
-                    for (String x : text) {
-                        System.out.print(x);
-                        System.out.print(" ");
-                    }
-                    System.out.println();
-                    System.out.println(line);
+                    AddList(text);
                 }
             }
         }
 
         System.out.println(line);
-        System.out.println("Bye. See you later!");
+        System.out.println("Farewell!");
+        System.out.println(line);
+    }
+
+    public static void PrintList() {
+        System.out.println(line);
+        for (int i = 0; i < ls.size(); i++) {
+            System.out.println(i + 1 + ". " + ls.get(i));
+        }
+        System.out.println(line);
+    }
+
+    public static void AddList(String cmd) {
+        ls.add(cmd);
+        System.out.println(line);
+        System.out.println("added: " + cmd);
         System.out.println(line);
     }
 }
