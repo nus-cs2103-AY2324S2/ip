08ef6488e6ef550740e302dc56ea6ee4c77b7123
Ko-Khan <e0970313@u.nus.edu>
2024-01-24 15:55:45 +0800

    Created a new class Task. Bit can now mark and unmark tasks as complete or incomplete respectively.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,10 @@
 import  java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
+
         System.out.println("Hi! This is Bit!\nWhat shall we do today?\n");
         Scanner scanner = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int total = 0;
 
         while(true) {
@@ -17,8 +13,25 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 toList(list, total);
+            } else if (input.contains("mark ")) {
+                String[] parts = input.split(" ");
+                try {
+                    int i = Integer.parseInt(parts[1]);
+                    if (parts[0].equals("mark")) {
+                        mark(i, list);
+                    } else if (parts[0].equals("unmark")) {
+                        unmark(i, list);
+                    } else {
+                        list[total] = new Task(input);
+                        total++;
+                    }
+
+                } catch (NumberFormatException e) {
+                    list[total] = new Task(input);
+                    total++;
+                }
             } else {
-                list[total] = input;
+                list[total] = new Task(input);
                 total++;
             }
 
@@ -26,13 +39,36 @@ public class Duke {
         System.out.println("Alright. See you soon!");
     }
 
-    public static void toList(String[] list, int total) {
+    public static void toList(Task[] list, int total) {
         for (int i = 0; i < total; i++) {
             if (list[i] == null) {
                 break;
             }
-            System.out.println((i + 1) + ". " + list[i]);
+            System.out.println((i + 1) + "." + list[i].toString());
+        }
+    }
+
+    public static void mark(int i, Task[] list) {
+        i -= 1;
+        if(list[i] == null) {
+           System.out.println("Sorry, but I couldn't find that task!");
+           return;
         }
 
+        list[i].complete();
+        System.out.println("Alright! Task has been done and dusted:\n" +list[i].toString());
     }
+
+    public static void unmark(int i, Task[] list) {
+        i -= 1;
+        if(list[i] == null) {
+            System.out.println("Sorry, but I couldn't find that task!");
+            return;
+        }
+
+        list[i].incomplete();
+        System.out.println("Done! Task is now incomplete:\n" + list[i].toString());
+    }
+
+
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+
+    private boolean isDone;
+    private final String description;
+    public Task(String description) {
+        isDone = false;
+        this.description = description;
+
+    }
+
+    public void complete() {
+        this.isDone = true;
+    }
+
+    public void incomplete() {this.isDone = false; }
+    @Override
+    public String toString() {
+        if(this.isDone) {
+            return "[X] "+ description;
+        } else {
+            return "[ ] " + description;
+        }
+    }
+}
