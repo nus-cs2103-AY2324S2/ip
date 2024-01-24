9d62dc89eab1061a65297261f932d2873613aa96
JerryO3 <jieruioon@gmail.com>
2024-01-24 12:53:18 +0800

    modified Duke and Greet to perform echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println(Greet.logo);
         System.out.println(Greet.greet);
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        if (command.equals("bye")) {
+            System.out.println(Greet.bye);
+        } else {
+            System.out.println(Echo.echo(command));
+        }
     }
 }
