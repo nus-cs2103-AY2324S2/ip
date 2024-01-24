a2d1008c39fffc04dd134665d797ccbecbe7a0f0
triciiaaa <angjeanee@gmail.com>
2024-01-24 16:45:28 +0800

    Level-1 Increment + Added styling with lines and indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -7,8 +9,27 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Chronos.");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        System.out.println("        ------------------------------------------------------------");
+        System.out.println("        Hello! I'm Chronos.");
+        System.out.println("        What can I do for you?");
+        System.out.println("        ------------------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("        ------------------------------------------------------------");
+                System.out.println("        Bye. Hope to see you again soon!");
+                System.out.println("        ------------------------------------------------------------");
+                break;
+            } else {
+                System.out.println("        ------------------------------------------------------------");
+                System.out.println("        " + command);
+                System.out.println("        ------------------------------------------------------------");
+            }
+        }
     }
 }
