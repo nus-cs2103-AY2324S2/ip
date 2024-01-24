c5b339d785b2bd60106ead0793fd41f02ee29212
Timothy <102574520+LargeCrowd@users.noreply.github.com>
2024-01-24 18:05:21 +0800

    Update Duke.java
    Level-1 changes. Takes an input then repeats it

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
 
         String name = "Big Boy";
         String greeting = "Hello! I'm " + name + ", your new friend!" + "\n" + "What can I do for you?";
         String line = "____________________________________________________________";
         String exit = "Bye. Hope to see you again soon!";
 
+        // add input. program repeats user's input until user types "bye"
         System.out.println(line);
         System.out.println(greeting);
+        System.out.println(line);
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println("\t" + input);
+            System.out.println(line);
+            input = in.nextLine();
+        }
+        
+        in.close();
         System.out.println(line);
         System.out.println(exit);
         System.out.println(line);
