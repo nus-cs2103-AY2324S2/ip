b285a9059251e4cd968bb867574544e6bba26ba6
DESKTOP-KIADS1H\Chuan Yao <kaizerfang2@gmail.com>
2024-01-24 11:54:32 +0800

    Added Task.java file. Added the ability to mark tasks as done and undone.

--- src/main/java/Demon.java
+++ src/main/java/Demon.java
@@ -8,7 +8,7 @@ public class Demon {
         System.out.println("Hello Master, I'm Demon 👿\nWhat can I do for you today?");
         String input = sc.nextLine();
         // list to store actions specified by user.
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         System.out.println("Entered: '" + input + "'");
         while (!input.equalsIgnoreCase("bye")) {
@@ -16,13 +16,41 @@ public class Demon {
                 System.out.println("---------------------------------------------------------");
                 System.out.print("List of things to do 📑:\n");
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println("\t" + i + ". " + list.get(i-1));
+                    Task item = list.get(i-1);
+                    System.out.println("\t" + i + ".[" + item.getStatusIcon() + "] " + item.description);
                 }
                 System.out.println("--------------------------------------------------------");
                 System.out.println("Anything else? Please let me know: ");
                 input = sc.nextLine();
+            } else if (input.contains("unmark")) {
+                try {
+                    int num = Integer.parseInt(input.split(" ")[1]);
+                    Task item = list.get(num-1);
+                    item.markNotDone();
+                    System.out.println("Sure Master, I've marked this task as not done ❌:");
+                    System.out.println("[" + item.getStatusIcon() + "] " + item.description);
+                    System.out.println("Anything else? Please let me know: ");
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println(e + ". Please provide valid integer.");
+                } finally {
+                    input = sc.nextLine();
+                }
+            } else if (input.contains("mark")) {
+                try {
+                    int num = Integer.parseInt(input.split(" ")[1]);
+                    Task item = list.get(num-1);
+                    item.markDone();
+                    System.out.println("Sure Master, I've marked this task as done ✅:");
+                    System.out.println("[" + item.getStatusIcon() + "] " + item.description);
+                    System.out.println("Anything else? Please let me know: ");
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println(e + ". Please provide valid integer.");
+                } finally {
+                    input = sc.nextLine();
+                }
+
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("--------------------------------------------------------");
                 System.out.println("🔖 Added to list of to-do: " + input);
                 System.out.println("--------------------------------------------------------");
