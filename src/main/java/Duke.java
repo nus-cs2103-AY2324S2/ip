c7c682f090f49bd752794366aa5b9dbadef60987
jovantanyk <jovantan630@gmail.com>
2024-01-24 15:15:47 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,40 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String bot_name = "Paimon";
-        System.out.println("-------------------->" + "\nGreetings Traveller!\nI'm " +  bot_name + ", your friendly guide! What shall we do today?" + "\n-------------------->");
+        greeting();
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<String>();
         while (true) {
             String userInput = scanner.nextLine();
-            if (Objects.equals(userInput, "bye")) {
+            if (userInput.equals("bye")) {
                 break;
             }
-            System.out.println("> " + userInput);
+            if (userInput.equals("list")) {
+                printList(taskList);
+            } else {
+                taskList.add(userInput);
+                System.out.println("Okay Traveller, I've added " + userInput + " to your list!");
+            }
         }
+        goodbye();
+    }
+    private static void printList(ArrayList<String> list) {
+        int size = list.size();
+        System.out.println("Here is  your list so far! \n----->");
+        for (int i = 0; i < size; i++) {
+            System.out.println(Integer.toString(i+1) + ": " + list.get(i));
+        }
+        System.out.println("----->");
+    }
+    private static void greeting() {
+        System.out.println("-------------------->" + "\nGreetings Traveller!\nI'm Paimon, your friendly guide! What shall we do today?" + "\n-------------------->");
+    }
+    private static void goodbye() {
         System.out.println("-------------------->" + "\nSee you later Traveller!" + "\n-------------------->");
     }
 }
