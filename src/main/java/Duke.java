301c2c775ebd72bd0e3d7196ff7d9d472a702fd5
larainezo <larainelwx@gmail.com>
2024-01-24 09:22:30 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
 
-        /**
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -100,5 +100,6 @@ public class Duke {
         System.out.println(lineBreak);
         System.out.println("\t\tBye. Hope to see you again soon!");
         System.out.println(lineBreak);
+
     }
 }
