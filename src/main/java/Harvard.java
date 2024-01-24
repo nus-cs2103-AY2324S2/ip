061ec43b5259a70377f03d894bf14cb8eca14dc7
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-01-24 12:42:18 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -10,7 +10,7 @@ public class Harvard {
                 + "____________________________________________________________\n";
         System.out.println(initial);
 
-        List<String> tasks = new ArrayList<String>();
+        List<Task> tasks = new ArrayList<Task>();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -20,20 +20,41 @@ public class Harvard {
                 break;
             }
 
-            switch (echoInput) {
+            String command = echoInput.split(" ")[0];
 
+            if (command.equals("mark") || command.equals("unmark")) {
+                int index = Integer.parseInt(echoInput.split(" ")[1]);
+                Task targetTask = tasks.get(index - 1);
+
+                System.out.println("____________________________________________________________\n");
+                if (command.equals("mark")) {
+                    targetTask.mark();
+                    System.out.println("Nice! I've marked this task as done:\n");
+                } else {
+                    targetTask.unmark();
+                    System.out.println("OK, I've marked this task as not done yet:\n");
+                }
+
+                System.out.println(targetTask.getStatusIcon() + " " + targetTask.getDescription() + "\n");
+                System.out.println("____________________________________________________________\n");
+                continue;
+            }
+
+            switch (echoInput) {
                 case "list":
                     System.out.println("____________________________________________________________\n");
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println(i + ". " + tasks.get(i - 1));
+                    System.out.println("Here are the tasks in your list:\n");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + ". " + tasks.get(i).getStatusIcon() + " " + tasks.get(i).getDescription());
                     }
                     System.out.println("____________________________________________________________\n");
                     continue;
 
                 default:
-                    tasks.add(echoInput);
+                    Task taskItem = new Task(echoInput);
+                    tasks.add(taskItem);
                     System.out.println("____________________________________________________________\n");
-                    System.out.println("added: " + echoInput);
+                    System.out.println("added: " + taskItem.getDescription());
                     System.out.println("____________________________________________________________\n");
             }
 
@@ -43,5 +64,31 @@ public class Harvard {
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
         System.out.println(exit);
+    }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "[X]" : "[ ]"); // mark done task with X
+        }
+
+        public void mark() {
+            this.isDone = true;
+        }
+
+        public void unmark() {
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
     }
 }
