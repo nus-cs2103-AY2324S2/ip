c3c60a185103f864d50a945e5c9d6d59840b9d06
Ashley <>
2024-01-24 22:59:10 +0800

    add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____ \n"
                     + "|  _ \\   ___   ___ \n"
                     + "| |_| | / _ \\ / _ \\\n"
@@ -9,10 +13,23 @@ public class Duke {
         String msg = "------------------------------------------------ \n"
                 + "Hello! I'm Bee! \n"
                 + "What can I do for you? \n"
-                + "------------------------------------------------ \n"
-                + "Bye. Hope to see you again soon! \n"
                 + "------------------------------------------------ \n";
 
         System.out.println(logo + "\n" + msg);
+
+        boolean output = true;
+        String input;
+
+        while (output) {
+            System.out.println(" ");
+            input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                output = false;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
