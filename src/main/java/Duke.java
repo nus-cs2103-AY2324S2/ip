31e01f6d8f23639d057eb8df7f7608e9d6ad3623
jayne1010 <e0970489@u.nus.edu>
2024-01-24 15:16:11 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+
 public class Duke {
     // Fixed-size array to store tasks
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
+
     public static void main(String[] args) {
         // Greeting
         System.out.println("____________________________________________________________");
@@ -28,13 +30,22 @@ public class Duke {
             } else if (userInput.equalsIgnoreCase("list")) {
                 // Display the list of tasks
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("mark")) {
+                // Mark a task as done
+                int taskIndex = Integer.parseInt(userInput.substring(5).trim()) - 1;
+                markTaskAsDone(taskIndex);
+            } else if (userInput.startsWith("unmark")) {
+                // Mark a task as not done
+                int taskIndex = Integer.parseInt(userInput.substring(7).trim()) - 1;
+                unmarkTaskAsDone(taskIndex);
             } else {
                 // Add the task to the array
-                tasks[taskCount++] = userInput;
+                tasks[taskCount++] = new Task(userInput, false);
                 // Display confirmation
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + userInput);
@@ -44,5 +55,54 @@ public class Duke {
 
         // Close the scanner
         scanner.close();
+    }
+
+    // Mark a task as done
+    private static void markTaskAsDone(int index) {
+        if (index >= 0 && index < taskCount) {
+            tasks[index].setDone(true);
+            System.out.println("____________________________________________________________");
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   " + tasks[index]);
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid task index. Please enter a valid task index.");
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    // Mark a task as not done
+    private static void unmarkTaskAsDone(int index) {
+        if (index >= 0 && index < taskCount) {
+            tasks[index].setDone(false);
+            System.out.println("____________________________________________________________");
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("   " + tasks[index]);
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid task index. Please enter a valid task index.");
+            System.out.println("____________________________________________________________");
+        }
+    }
+}
+
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
     }
 }
