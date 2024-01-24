53af075fec256ebaba39943239918d592c2f8ac7
ReflectiveObsidian <E0958151@u.nus.edu>
2024-01-24 14:16:14 +0800

    Replaced old greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+    static String name = "Pyrite";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontal_line = "____________________________________________________________";
+        String greeting = "Hello! I'm " + name + "\n"
+                + "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(horizontal_line);
+        System.out.println(greeting);
+        System.out.println(horizontal_line);
+        System.out.println(farewell);
+        System.out.println(horizontal_line);
+
     }
 }
