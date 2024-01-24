e6e58acb11d0abeff0d6f73ef3ce286167013b31
Yuhi Narita <yuhi.narita26@gmail.com>
2024-01-24 15:29:52 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String bot_name = "Calvin";
-        String greetings = " Hello! I'm " + bot_name + "\n"
-                + " What can I do for you? \n\n"
-                + " Bye. Hope to see you again soon!\n";
-        System.out.println(greetings);
+        String greeting_msg = " Hello! I'm " + bot_name + "\n"
+                + " What can I do for you? \n\n";
+        String exit_msg = " Bye. Hope to see you again soon!\n";
+        System.out.println(greeting_msg);
+
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String user_input = reader.next();
+            if (user_input.equals("bye")) {
+                break;
+            }
+            System.out.println(user_input);
+        }
+        System.out.println(exit_msg);
     }
 }
