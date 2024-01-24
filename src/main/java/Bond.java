7eab5f28a0c836ed64f58e8c1267c26969ca1cfd
bennyLCK <e0969875@u.nus.edu>
2024-01-24 17:04:28 +0800

    Rename the chatbot, implement Greet and Exit messages

---
+++ src/main/java/Bond.java
@@ -0,0 +1,13 @@
+public class Bond {
+    public static void main(String[] args) {
+
+        String line = "____________________________________________________________";
+
+        String logo = "Bond";
+        System.out.println(String.format("Hello! I'm %s \nWhat can I do for you? \n%s\n", logo, line));
+
+        System.out.println(String.format("Bye. Hope to see you again soon! \n%s", line));
+
+        System.exit(0);
+    }
+}
