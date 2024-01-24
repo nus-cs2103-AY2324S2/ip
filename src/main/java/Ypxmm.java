301514198e0c45709c3a0c0605cc68736cfe9a52
appleraincoat <james20111951@gmail.com>
2024-01-24 17:48:56 +0800

    Added the ability to echo messages, exiting on input "bye".

--- src/main/java/Ypxmm.java
+++ src/main/java/Ypxmm.java
@@ -1,8 +1,18 @@
+import java.util.*;
+
 public class Ypxmm {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hallo boss, I'm Ypxmm.");
         System.out.println("Need me do what for you?");
-        System.out.println("Okok I zao first, see you again bro!");
-
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Okok I zao first, see you again bro!");
+                break;
+            }
+            System.out.println(input);
+        }
+        sc.close();
     }
 }
