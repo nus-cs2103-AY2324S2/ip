4d088eb0e61e0966a4a61e08ce0f5fed3b7e939f
theman-oj10 <manojnarender@gmail.com>
2024-01-24 12:14:35 +0800

    Create semi-automatic testing for core functionality
    All testing is manual so far.
    
    Creating semi-automatic testing scripts using input/output redirection improves code quality and reduces bugs.
    
    Let's
    * Update the given bash script
    * Create a custom test case

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp "../src/main/java" -Xlint:none -d "../bin" "../src/main/java/chatbot/"*.java;
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -cp "../bin:../src/main/java" chatbot.Alfred < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
