4bea9d6e76a183c6e0b8b1211073a99fc5992ebb
Apzure <block4556@gmail.com>
2024-01-24 22:49:43 +0800

    Level-0

---
+++ src/main/java/Bird.java
@@ -0,0 +1,11 @@
+public class Bird {
+    public static void main(String[] args) {
+        System.out.println("Hello from I'm Bird");
+        System.out.println(" /\\_/\\");
+        System.out.println("((@v@))");
+        System.out.println("():::()");
+        System.out.println(" VV-VV");
+        System.out.println("What can I do for you?");
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
