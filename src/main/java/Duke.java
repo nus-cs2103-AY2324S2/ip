45c5cebe027fe1fde6bf19ecf6b9565fc2386da2
nigel <nigellim27022001@gmail.com>
2024-01-24 16:08:34 +0800

    level-5: Error Handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
 
@@ -16,13 +14,14 @@ public class Duke {
         System.out.println("―――――――――――――――――――――――――――――――――――");
     }
 
-    private static void addTask(String description) {
+    private static void addTask(String description) throws DukeException{
         Task newTask;
+        validTaskCommand(description);
         if (description.toLowerCase().startsWith("todo")) {
             String[] descriptionArr = description.split(" ");
             StringBuilder descBuilder = new StringBuilder();
-            for(int k = 1; k < descriptionArr.length; k++){
-                if (k == descriptionArr.length - 1){
+            for (int k = 1; k < descriptionArr.length; k++) {
+                if (k == descriptionArr.length - 1) {
                     descBuilder.append(descriptionArr[k]);
                 } else {
                     descBuilder.append(descriptionArr[k] + " ");
@@ -33,16 +32,16 @@ public class Duke {
             String[] descriptionArr = description.split(" ");
             int byIndex = Arrays.asList(descriptionArr).indexOf("/by");
             StringBuilder descBuilder = new StringBuilder();
-            for(int k = 1; k < byIndex; k++){
-                if (k == byIndex - 1){
+            for (int k = 1; k < byIndex; k++) {
+                if (k == byIndex - 1) {
                     descBuilder.append(descriptionArr[k]);
                 } else {
                     descBuilder.append(descriptionArr[k] + " ");
                 }
             }
             StringBuilder byBuilder = new StringBuilder();
-            for(int k = byIndex + 1; k < descriptionArr.length; k++){
-                if (k == descriptionArr.length - 1){
+            for (int k = byIndex + 1; k < descriptionArr.length; k++) {
+                if (k == descriptionArr.length - 1) {
                     byBuilder.append(descriptionArr[k]);
                 } else {
                     byBuilder.append(descriptionArr[k] + " ");
@@ -56,8 +55,8 @@ public class Duke {
             String[] descriptionArr = description.split(" ");
             int fromIndex = Arrays.asList(descriptionArr).indexOf("/from");
             StringBuilder descBuilder = new StringBuilder();
-            for(int k = 1; k < fromIndex; k++){
-                if (k == fromIndex - 1){
+            for (int k = 1; k < fromIndex; k++) {
+                if (k == fromIndex - 1) {
                     descBuilder.append(descriptionArr[k]);
                 } else {
                     descBuilder.append(descriptionArr[k] + " ");
@@ -65,28 +64,25 @@ public class Duke {
             }
             int toIndex = Arrays.asList(descriptionArr).indexOf("/to");
             StringBuilder fromBuilder = new StringBuilder();
-            for(int k = fromIndex + 1; k < toIndex; k++){
-                if (k == toIndex - 1){
+            for (int k = fromIndex + 1; k < toIndex; k++) {
+                if (k == toIndex - 1) {
                     fromBuilder.append(descriptionArr[k]);
                 } else {
                     fromBuilder.append(descriptionArr[k] + " ");
                 }
             }
             StringBuilder toBuilder = new StringBuilder();
-            for(int k = toIndex + 1; k < descriptionArr.length; k++){
-                if (k == descriptionArr.length - 1){
+            for (int k = toIndex + 1; k < descriptionArr.length; k++) {
+                if (k == descriptionArr.length - 1) {
                     toBuilder.append(descriptionArr[k]);
                 } else {
                     toBuilder.append(descriptionArr[k] + " ");
                 }
             }
             newTask = new Event(descBuilder.toString(), fromBuilder.toString(), toBuilder.toString());
-        }
-        else {
-            newTask = new Task(description);
-        }
+        } 
         Duke.storage.add(newTask);
-        System.out.println( String.format("*Honk! Honk!* Pengu has added this task:\n" + newTask.toString()
+        System.out.println(String.format("*Honk! Honk!* Pengu has added this task:\n" + newTask.toString()
                 + "\nGet back to work! you have %s tasks in the list\n"
                 + "―――――――――――――――――――――――――――――――――――", Duke.storage.size()));
     }
@@ -116,7 +112,27 @@ public class Duke {
         System.out.println("―――――――――――――――――――――――――――――――――――");
     }
 
-    public static void main(String[] args) {
+    private static boolean validTaskCommand(String str) throws DukeException {
+        List<String> strArr = Arrays.asList(str.split(" "));
+        String keyword = str.split(" ")[0].toLowerCase();
+        System.out.println(keyword);
+        if (!(keyword.equals("todo") || keyword.equals("deadline") || keyword.equals("event"))){
+            throw new DukeException("*HONK* Pengu has never seen such a command before, some commands Pengu can do are: list, todo, deadline");
+        } else if (keyword.equals("todo") && !(strArr.size() > 1)) {
+            throw new DukeException("*HONK* Pengu needs a To Do Task description to record this down");
+        } else if (keyword.equals("deadline") && !(strArr.contains("/by"))) {
+            throw new DukeException("*HONK* Pengu needs a /by followed by a end date for your task");
+        } else if (keyword.equals("event") && !(strArr.contains("/from") && (strArr.contains("/to")))) {
+            throw new DukeException("*HONK* Pengu needs a /from followed by a from date and a /to followed by a end date for your task");
+        } else if (keyword.equals("deadline") && strArr.get(1).equals("/by")) {
+            throw new DukeException("Honk* Pengu cannot accept a deadline task without a description");
+        } else if (keyword.equals("event") && (strArr.get(1).equals("/from") || strArr.get(1).equals("/to"))) {
+            throw new DukeException("*Honk* Pengu cannot accept a event without a description");
+        } else {
+            return true;
+        }
+    }
+    public static void main(String[] args) throws DukeException {
         greeting();
         Scanner s = new Scanner(System.in);
         while (true){
