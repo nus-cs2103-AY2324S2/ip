85ed588bbf9dbd359839870993ecbd2afb695549
HENRY\YS <yusoonng3@gmail.com>
2024-01-24 14:55:31 +0800

    Changed the name of the chatbot + added a new intro + outro to the bot

---
+++ src/main/java/TheAdvisor.java
@@ -0,0 +1,8 @@
+public class TheAdvisor {
+    public static void main(String[] args) {
+        String intro = "Hello, I am TheAdvisor. The one and only advisor you will ever need in your investing " +
+                "journey. What can I do for you?";
+        String out = "Thank you for using TheAdvisor. Have a great day";
+        System.out.println(intro + "\n" + out);
+    }
+}
