95345187ecb81680f1f2d8ff5172cc7ce8940e23
Jerome-j <jerome@outlook.sg>
2024-01-24 08:58:13 +0800

    Level-1: Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String welcomeMessage = "____________________________________________________________\n" +
-                "Hello! I'm JeromeGPT \n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+
+        String userInput;
+
+        // To read in user input
+        Scanner sc = new Scanner(System.in);
+
+        String welcomeMessage = "\t ____________________________________________________________\n" +
+                "\t Hello! I'm JeromeGPT \n" +
+                "\t What can I do for you?\n" +
+                "\t ____________________________________________________________";
 
         System.out.println(welcomeMessage);
+
+        while (true) {
+            // Keep reading user input until they type "bye"
+            userInput = sc.nextLine();
+
+
+            if (userInput.equals("bye")) {
+                // Use this construct because we don't want to echo the bye message.
+                break;
+            } else {
+                // Continuously echo user input if it is not a bye.
+                System.out.println("\t ____________________________________________________________");
+                System.out.println("\t " + userInput);
+                System.out.println("\t ____________________________________________________________");
+            }
+        }
+
+
+        String goodByeMessage = "\t ____________________________________________________________\n" +
+                "\t Bye. Hope to see you again soon!\n" +
+                "\t ____________________________________________________________\n";
+
+        System.out.println(goodByeMessage);
     }
 }
