40cef2988905988f4bad4333782efb0fa10d7e04
Yetong <tangyetong666@gmail.com>
2024-01-24 22:04:33 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class Duke {
 
     final static String HORIZONTAL_LINE = "____________________________________________________________ \n";
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        List<String> userMsgList = new ArrayList<String>();
+        List<Task> userTaskList = new ArrayList<>();
 
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello from Kewgy! \n");
@@ -16,28 +17,60 @@ public class Duke {
         System.out.println("Type \"bye\" to exit! \n");
         System.out.println(HORIZONTAL_LINE);
 
-        String userMsg = reader.next();
-        while(true) {
-            if (Objects.equals(userMsg, "bye")) break;
-
+        String userMsg = reader.nextLine();
+        while (!Objects.equals(userMsg, "bye")) {
             if (Objects.equals(userMsg, "list")) {
                 System.out.println(HORIZONTAL_LINE);
-                for (int i = 0; i < userMsgList.size(); i++) {
-                    System.out.println(i + ": " + userMsgList.get(i));
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 1; i < userTaskList.size() + 1; i++) {
+                    System.out.println(i + ": " + userTaskList.get(i - 1));
                 }
                 System.out.println(HORIZONTAL_LINE);
-            } else {
-                System.out.println(HORIZONTAL_LINE);
-                userMsgList.add(userMsg);
-                System.out.println("added: " + userMsg);
-                System.out.println(HORIZONTAL_LINE);
+
+                userMsg = reader.nextLine();
+                continue;
+            }
+            String[] userMsgParsed = userMsg.split(" ", 2);
+
+            if (userMsgParsed[0].equals("mark")) {
+                if (checkValidMarkCommand(userMsgParsed, userTaskList)) {
+                    System.out.println(HORIZONTAL_LINE);
+                    userTaskList.get(Integer.parseInt(userMsgParsed[1]) - 1).setDone(true);
+                    System.out.println(userTaskList.get(Integer.parseInt(userMsgParsed[1]) - 1));
+                    System.out.println(HORIZONTAL_LINE);
+
+                    userMsg = reader.nextLine();
+                    continue;
+                }
+            } else if (userMsgParsed[0].equals("unmark")) {
+                if (checkValidMarkCommand(userMsgParsed, userTaskList)) {
+                    System.out.println(HORIZONTAL_LINE);
+                    userTaskList.get(Integer.parseInt(userMsgParsed[1]) - 1).setDone(false);
+                    System.out.println(userTaskList.get(Integer.parseInt(userMsgParsed[1]) - 1));
+                    System.out.println(HORIZONTAL_LINE);
+
+                    userMsg = reader.nextLine();
+                    continue;
+                }
             }
 
-            userMsg = reader.next();
+            System.out.println(HORIZONTAL_LINE);
+            userTaskList.add(new Task(userMsg));
+            System.out.println("added: " + userMsg);
+            System.out.println(HORIZONTAL_LINE);
+
+            userMsg = reader.nextLine();
         }
 
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye! Hope to see you again soon! \n");
         System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static boolean checkValidMarkCommand(String[] userMsgParsed, List<Task> userTaskList) {
+        return userMsgParsed.length > 1 &&
+                userMsgParsed[1].chars().allMatch(Character::isDigit) &&
+                Integer.parseInt(userMsgParsed[1]) < userTaskList.size() &&
+                Integer.parseInt(userMsgParsed[1]) > 0;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setDone(boolean done) {
+        if (done) {
+            System.out.println("Nice! I've marked this task as done:\n");
+        } else {
+            System.out.println("OK, I've marked this task as not done yet:\n");
+        }
+        this.isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
