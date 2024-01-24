6144ec0d9e9c45481830f17ed9e6cf63bb49cd4a
Alteqa <e0969816@u.nus.edu>
2024-01-24 23:48:00 +0800

    added echo

--- src/main/java/hirwan.java
+++ src/main/java/hirwan.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 public class hirwan {
     public static void main(String[] args) {
         String logo = "I'm hirwan \n"
+                + "_________________________________\n"
                 + "what can I do for you? \n"
-                + "Bye. Hope to see you again soon!";
+                + "_________________________________\n";
 
         System.out.println("Hello! " + logo);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
+                break;
+            }
+            System.out.println(text);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
