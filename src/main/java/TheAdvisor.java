86ecf62af75f6091257277c438193ebe67139581
HENRY\YS <yusoonng3@gmail.com>
2024-01-24 15:26:35 +0800

    Added the function to give a list of strings which will be output when user inputs "list" Removed the echo function

--- src/main/java/TheAdvisor.java
+++ src/main/java/TheAdvisor.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class TheAdvisor {
     public static void main(String[] args) throws IOException {
@@ -8,15 +9,23 @@ public class TheAdvisor {
         String intro = "Hello, I am TheAdvisor. The one and only advisor you will ever need in your investing " +
                 "journey. What can I do for you?";
         System.out.println(intro + "\n");
+        ArrayList<String> output = new ArrayList<>();
 
         while (true) {
-            String toEcho = br.readLine();
-            if (toEcho.equals("bye")) {
+            String toAdd = br.readLine();
+            if (toAdd.equals("bye")) {
                 System.out.println("    Goodbye. Thank you for using TheAdvisor chatbox and I hope that my advice has managed" +
                         "to help you in your investing journey!");
                 break;
+            } else if (toAdd.equals("list")){
+                int counter = 1;
+                for (int i = 0; i < output.size(); i++) {
+                    System.out.println("    " + counter + ". " + output.get(i));
+                    counter++;
+                }
             } else {
-                System.out.println("    " + toEcho);
+                output.add(toAdd);
+                System.out.println("    added: " + toAdd);
             }
         }
     }
