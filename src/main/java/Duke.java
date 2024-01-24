b00bd6f0aa70ed13afa3d53fbd5800311c0e939c
Puri Virakarin <puri.prv@gmail.com>
2024-01-24 16:55:08 +0800

    Implement an initial skeletal

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+
+  public static void main(String[] args) {
+    String welcomeMessage =
+      "____________________________________________________________\n" +
+      "Hello! I'm ATSISBot\n" +
+      "What can I do for you?\n" +
+      "____________________________________________________________\n" +
+      "Bye. Hope to see you again soon!\n" +
+      "____________________________________________________________\n";
+    System.out.println(welcomeMessage);
+  }
 }
