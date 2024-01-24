94bb7b427f78ff8f941f397fce5f4ef2e5893119
LohYuTong <lohyutong10@gmail.com>
2024-01-25 01:41:30 +0800

    Level 3. Mark as Done

--- src/main/java/Youdon.java
+++ src/main/java/Youdon.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Youdon {
     public static void main(String[] args) {
         // initialise array and index
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int index = 0;
 
         // string constants
@@ -33,16 +33,53 @@ public class Youdon {
             // if input == "list", return tasklist
             if (data.equals("list")) {
                 System.out.println(line);
+                System.out.println("Here are your tasks:");
                 for(int i = 0; taskList[i] != null; i++) {
-                    System.out.println((i + 1) + ". " + taskList[i]);
+                    System.out.println((i + 1) + ".[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
                 }
                 System.out.println(line);
                 data = input.nextLine();
                 continue;
             }
 
-            // if input not "bye" or "list", add to tasklist
-            taskList[index] = data;
+            if (data.contains(" ")) {
+                // if input data has 2 parts, split into action & task number
+                String[] parts = data.split(" ");
+                String action = parts[0];
+                String task = parts[1];
+                int taskNumber = Integer.parseInt(task);
+
+                // if input == "mark", mark the specified task as done
+                if (action.equals("mark")) {
+                    // mark task as done in array
+                    taskList[taskNumber - 1].isDone = true;
+
+                    // print out changes
+                    System.out.println(line);
+                    System.out.println("Nicely done! The task has been marked as done: ");
+                    System.out.println("  [" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].description);
+                    System.out.println(line);
+                    data = input.nextLine();
+                    continue;
+                }
+
+                // if input == "unmark", mark the specified task as undone
+                if (action.equals("unmark")) {
+                    // mark task as undone in array
+                    taskList[taskNumber - 1].isDone = false;
+
+                    // print out changes
+                    System.out.println(line);
+                    System.out.println("Okies! The task has been marked as undone: ");
+                    System.out.println("  [" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].description);
+                    System.out.println(line);
+                    data = input.nextLine();
+                    continue;
+                }
+            }
+
+            // add task to tasklist
+            taskList[index] = new Task(data);
             index++;
 
             // print that task has been added
