d346178697e043cb9d8a0e2015e5310eb1f7e7f8
JOELPC\joelw <omegabeeminecraft@gmail.com>
2024-01-24 22:34:36 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,43 @@
 import java.util.*;
 public class Duke {
-    static String line = "____________________________________________________________";
+    static String line = "    ____________________________________________________________";
     static String name = "Alfred";
 
     public static void separate(){
         System.out.println(line);
     }
+    public static void spacing() {
+        System.out.println("");
+    }
     public static void intro() {
         separate();
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        System.out.println("    Hello! I'm " + name + "\n    What can I do for you?");
         separate();
+        spacing();
     }
     public static void leave() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
         separate();
+        spacing();
     }
-    
-    public static void main(String[] args) {
+    public static void echo() {
         intro();
-        leave();
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            String echoed = input.nextLine();
+            if(echoed.equals("bye")) {
+                leave();
+                break;
+            } else {
+                separate();
+                System.out.println("    " + echoed);
+                separate();
+                spacing();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        echo();
     }
 }
