020d7475e924c614c3092defee4fa211331998ab
ReflectiveObsidian <E0958151@u.nus.edu>
2024-01-24 15:41:13 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Pyrite.java
+++ src/main/java/Pyrite.java
@@ -9,11 +9,9 @@ public class Pyrite {
     Task[] list = new Task[100];
     int list_count = 0;
     private void printList(Task[] list, int list_count) {
-        System.out.println(this.horizontal_line);
         for (int i = 0; i < list_count; i++) {
             System.out.println("\t" + Integer.toString(i + 1) + ". " + list[i].getFullStatusString());
         }
-        System.out.println(horizontal_line);
     }
     public void begin() {
         System.out.println(this.horizontal_line);
@@ -21,21 +19,40 @@ public class Pyrite {
         System.out.println(this.horizontal_line);
         Scanner scanner = new Scanner(System.in);
         String input;
+        // Solution below inspired by https://stackoverflow.com/questions/31690570/java-scanner-command-system
         while (true) {
             System.out.println();
             input = scanner.nextLine();
+            System.out.println(this.horizontal_line);
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
                 printList(this.list, this.list_count);
             } else {
-                this.list[this.list_count] = new Task(input);
-                this.list_count++;
-
-                System.out.println(this.horizontal_line);
-                System.out.println("\t" + "added: " + input);
-                System.out.println(this.horizontal_line);
+                // Commands with parameters
+                String parameters[] = input.split(" ");
+                if (parameters[0].equals("mark")) {
+                    int id = Integer.parseInt(parameters[1]);
+                    list[id].setDone(true);
+                    System.out.println("\t"
+                            + "Nice! I've marked this task as done:\n"
+                            + "\t"
+                            + list[id].getFullStatusString());
+                } else if (parameters[0].equals("unmark")) {
+                    int id = Integer.parseInt(parameters[1]);
+                    list[id].setDone(false);
+                    System.out.println("\t"
+                            + "OK, I've marked this task as not done yet:\n"
+                            + "\t"
+                            + list[id].getFullStatusString());
+                } else {
+                    // Normal list item, add to list
+                    this.list[this.list_count] = new Task(input);
+                    this.list_count++;
+                    System.out.println("\t" + "added: " + input);
+                }
             }
+            System.out.println(this.horizontal_line);
         }
 
         System.out.println(this.horizontal_line);
