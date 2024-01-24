9032a3ba87e69b123e3faa93e5aeed8039a7cd9c
ashleyclx <e0958221@u.nus.edu>
2024-01-24 17:23:32 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,17 +5,25 @@ public class Duke {
     private static final String INDENT = "    ";
     public static void main(String[] args) {
         Duke.hello();
+        int taskCount = 0;
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
         while (true) {
             String input = in.nextLine();
             if (input.equals("bye")) {
                 Duke.bye();
                 break;
+            } else if (input.equals("list")) {
+                System.out.print(LINE);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(INDENT + (i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.print(Duke.LINE + Duke.INDENT + input + "\n" + Duke.LINE);
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.print(LINE + INDENT + "added: " + input + "\n" + LINE);
             }
         }
-
     }
 
     public static void hello() {
@@ -26,12 +34,12 @@ public class Duke {
                 "      +#+          +#+     +#+    +#+             +#+             +#+              +#+    +#+    \n" +
                 "     #+#          #+#     #+#    #+#             #+#             #+#              #+#    #+#     \n" +
                 "    ###          ###     ###    ###             ###             ##########       ###    ###      \n\n";
-        System.out.println(Duke.LINE + "    What's poppin' fam, it's ya boi \n\n" + logo +
-                "    Hit me up with those deets and let's vibe together!" + Duke.LINE);
+        System.out.println(LINE + "    What's poppin' fam, it's ya boi \n\n" + logo +
+                "    Hit me up with those deets and let's vibe together!" + LINE);
     }
 
     public static void bye() {
-        System.out.print(Duke.LINE + "    Peace out, fam! Stay lit and keep those good vibes rollin'!\n" + Duke.LINE);
+        System.out.print(LINE + "    Peace out, fam! Stay lit and keep those good vibes rollin'!\n" + LINE);
     }
 }
 
