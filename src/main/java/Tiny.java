163b293c334a2713f7a0d6e36bff3f30cf082107
Yskie <shikang@u.nus.edu>
2024-01-24 21:29:11 +0800

    Implemented Skeletal Version of Tiny

---
+++ src/main/java/Tiny.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Tiny {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        out.println("____________________________________________________________");
+        out.println(" Hello! I'm Tiny!");
+        out.println(" What can I do for you?");
+        out.println("____________________________________________________________");
+        out.println(" Bye. Hope to see you again soon!");
+        out.println("____________________________________________________________");
+        out.flush();
+    }
+}
