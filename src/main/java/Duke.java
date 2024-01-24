0ebd3dd7c6e968edfb9b17b5f3092d716bf5d03f
caitlyntang <caitlyntong6@gmail.com>
2024-01-24 22:09:54 +0800

    complete renaming and adding skeleton version of chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   __     __   _  _    _  _           \n"
+                    + " / __ \\  |  \\ | | \\ \\/ / | |            \n"
+                    + "| |__| | |   \\| |  \\  /  | |     \n"
+                    + "|  __  | | |\\   |  /  \\  | |\n"
+                    + "|_|  |_| |_| \\__| /_/\\_\\ |_|\n";
+        String name = "Anxi";
+        String line = "-----------------------------";
+
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm " + name + "\r\nWhat can I do for you? \r\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\r\n" + line);
     }
 }
