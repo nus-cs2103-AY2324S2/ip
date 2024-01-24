dd89622fc701f0bddd2dbf55f083b66a9e53174d
Chua Zi Long <e0957913@u.nus.edu>
2024-01-24 15:41:56 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm SCZL");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                listTasks();
+            } else {
+                addTask(userInput);
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println(" added: " + task);
+    }
+
+    private static void listTasks() {
+        System.out.println("____________________________________________________________");
+
+        if (taskCount == 0) {
+            System.out.println(" No tasks yet.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
     }
 }
