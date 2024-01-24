70f4b4a57bc03080d7f74ec1fbd22587ae90010d
Timothy <102574520+LargeCrowd@users.noreply.github.com>
2024-01-24 22:20:14 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,6 +8,9 @@ public class Duke {
         String greeting = "Hello! I'm " + name + ", your new friend!" + "\n" + "What can I do for you?";
         String line = "____________________________________________________________";
         String exit = "Bye. Hope to see you again soon!";
+        ArrayList<String> stringList = new ArrayList<>();
+        int itemIndex = 1;
+
 
         // add input. program repeats user's input until user types "bye"
         System.out.println(line);
@@ -18,11 +22,24 @@ public class Duke {
 
         while (!input.equals("bye")) {
             System.out.println(line);
-            System.out.println("\t" + input);
+
+            if (input.equals("list")) {
+                for (String item : stringList) {
+                    System.out.println(item);
+                }
+                System.out.println(line);
+                input = in.nextLine();
+                continue;
+            } else {
+                System.out.println("added: " + input);
+                stringList.add(itemIndex + ". " + input);
+                itemIndex++;
+            }
+
             System.out.println(line);
             input = in.nextLine();
         }
-        
+
         in.close();
         System.out.println(line);
         System.out.println(exit);
