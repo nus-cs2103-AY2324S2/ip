7670fc8c856944dde6d3dcba34df3e2392ab5dc4
Tan Jie Ling <tanjieling3@gmail.com>
2024-01-24 16:48:43 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Greetings
         String logo = "Tommy";
         String divider = "____________________________";
         System.out.println(divider);
@@ -7,6 +10,18 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
+        //Echo
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(divider);
+            System.out.println(userInput);
+            System.out.println(divider);
+
+            userInput = scanner.nextLine();
+        }
+
+        //Farewell
+        System.out.println(divider);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
     }
