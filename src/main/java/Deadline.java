aad60ace74f9f95af41bcc1ab2cde93f47bb2fdc
bachletuankhai <bachletuankhai@gmail.com>
2024-01-25 03:23:08 +0800

    Add Deadline class for task with due date

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+  private String dueTime;
+
+  public Deadline(String description, String due) {
+    super(description);
+    this.dueTime = due;
+  }
+
+  @Override
+  public String toString() {
+    return "[D]" + super.toString() + " (due: " + dueTime + ")";
+  }
+}
