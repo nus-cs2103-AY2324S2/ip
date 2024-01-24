45fd5a8a707835acd94bf12108865f1e073ff3a7
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-01-25 02:36:25 +0800

    Level_2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,32 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        String[] l = new String[100];
         String horizontalLine = "_".repeat(60);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
 
-        String userInput = sc.nextLine();
+        String userInput = sc.nextLine().trim();
         while (!userInput.equals("bye")) {
-            System.out.println(horizontalLine);
-            System.out.println(userInput);
-            System.out.println(horizontalLine);
-            userInput = sc.nextLine();
+            if (userInput.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 1; i <= counter; i++) {
+                    System.out.println(i + ". " + l[i - 1]);
+                }
+                System.out.println(horizontalLine);
+                userInput = sc.nextLine().trim();
+            } else {
+                l[counter] = userInput;
+                counter++;
+                System.out.println(horizontalLine);
+                System.out.println("added: " + userInput);
+                System.out.println(horizontalLine);
+                userInput = sc.nextLine().trim();
+            }
+
         }
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
