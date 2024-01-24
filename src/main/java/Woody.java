ddb015e4044fbe41ac885bb227381626b731e97e
ryanlimdx <ryanlimdx@gmail.com>
2024-01-24 11:15:43 +0800

    echo

--- src/main/java/Woody.java
+++ src/main/java/Woody.java
@@ -1,12 +1,28 @@
+import java.util.*;
+import java.util.Random;
 public class Woody {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n" +
+        String line = "____________________________________________________________\n";
+        String[] catchphrases = {};
+        String greeting =
+                line +
                 " Hello! I'm Woody\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                line;
 
         System.out.print(greeting);
+
+        Scanner input = new Scanner(System.in);
+        String currInput = input.nextLine() + "\n";
+
+        String[] terminateKeywords = {"bye", "BYE", "Bye"};
+        List<String> exitProgramme = Arrays.asList(terminateKeywords);
+
+        while (!exitProgramme.contains(currInput)) {
+            System.out.println(currInput + line);
+            currInput = input.nextLine();
+        }
+
+        System.out.println("Bye! \n" + line);
     }
 }
