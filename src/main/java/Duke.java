aeca1b28bed7fef9e7d58d29ddc5cb1039e01072
Zhi Wei <tanzhiwei7@gmail.com>
2024-01-24 21:10:41 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Homie");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in); // Create scanner
+        String command = scanner.nextLine();  // Read user command
+        while (!command.equals("bye")) {
+            System.out.println(command);  // Echo command
+            command = scanner.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
