489b955753a294100337fdf66ae56b48abc5f3b7
bgopi23 <baskargopinath01@gmail.com>
2024-01-24 17:46:29 +0800

    Handle Errors

--- src/main/java/gops.java
+++ src/main/java/gops.java
@@ -65,8 +65,6 @@ public class gops {
     }
 
 
-
-
     public static void main(String[] args) {
         Scanner inputTaker = new Scanner(System.in);
         Todo[] toDoList = new Todo[100];
@@ -79,47 +77,87 @@ public class gops {
         while (userReply != null && !userReply.equals("bye")) {
             if (userReply.contains("todo")) {
                 String[] splitter = userReply.split(" ", 2);
-                Todo todoObject = new Todo(splitter[1]);
-                toDoList[messageCount] = todoObject;
-                messageCount += 1;
-                System.out.println("Got it. I've added this task:");
-                todoObject.Printer();
-                System.out.println("Now you have " + messageCount + " tasks in the list.");
-                userReply = inputTaker.nextLine();
+                try {
+                    if (splitter.length == 1) {
+                        throw new gopsException();
+                    }
+                    Todo todoObject = new Todo(splitter[1]);
+                    toDoList[messageCount] = todoObject;
+                    messageCount += 1;
+                    System.out.println("Got it. I've added this task:");
+                    todoObject.Printer();
+                    System.out.println("Now you have " + messageCount + " tasks in the list.");
+                    userReply = inputTaker.nextLine();
+                } catch (gopsException e) {
+                    System.out.println("Please follow the format for setting todo\ntodo [your-task-here]");
+                    userReply = inputTaker.nextLine();
+                }
             }  else if (userReply.contains("deadline")) {
                 String[] firstSplitter = userReply.split(" ", 2);
-                String[] seocndSplitter = firstSplitter[1].split("/by", 2);
-                Deadline deadlineObject = new Deadline(seocndSplitter[0], seocndSplitter[1]);
-                toDoList[messageCount] = deadlineObject;
-                messageCount += 1;
-                System.out.println("Got it. I've added this task:");
-                deadlineObject.Printer();
-                System.out.println("Now you have " + messageCount + " tasks in the list.");
-            userReply = inputTaker.nextLine();
+                try {
+                    if (firstSplitter.length == 1) {
+                        throw new gopsException();
+                    }
+                    String[] seocndSplitter = firstSplitter[1].split("/by", 2);
+                    Deadline deadlineObject = new Deadline(seocndSplitter[0], seocndSplitter[1]);
+                    toDoList[messageCount] = deadlineObject;
+                    messageCount += 1;
+                    System.out.println("Got it. I've added this task:");
+                    deadlineObject.Printer();
+                    System.out.println("Now you have " + messageCount + " tasks in the list.");
+                    userReply = inputTaker.nextLine();
+                } catch (gopsException e) {
+                    System.out.println("Please follow the format for setting deadlines\ndeadline [your-task-here] /by [deadline-of-your-task]");
+                    userReply = inputTaker.nextLine();
+                }
             } else if (userReply.contains("event")) {
                 String[] firstSplitter = userReply.split(" ", 2);
-                String[] secondSplitter = firstSplitter[1].split("/from", 2);
-                String[] thirdSplitter = secondSplitter[1].split("/to", 2);
-                Event eventObject = new Event(secondSplitter[0], thirdSplitter[0], thirdSplitter[1]);
-                toDoList[messageCount] = eventObject;
-                messageCount += 1;
-                System.out.println("Got it. I've added this task:");
-                eventObject.Printer();
-                System.out.println("Now you have " + messageCount + " tasks in the list.");
-                userReply = inputTaker.nextLine();
+                try {
+                    if (firstSplitter.length == 1) {
+                        throw new gopsException();
+                    }
+                    String[] secondSplitter = firstSplitter[1].split("/from", 2);
+                    String[] thirdSplitter = secondSplitter[1].split("/to", 2);
+                    Event eventObject = new Event(secondSplitter[0], thirdSplitter[0], thirdSplitter[1]);
+                    toDoList[messageCount] = eventObject;
+                    messageCount += 1;
+                    System.out.println("Got it. I've added this task:");
+                    eventObject.Printer();
+                    System.out.println("Now you have " + messageCount + " tasks in the list.");
+                    userReply = inputTaker.nextLine();
+                } catch (gopsException e) {
+                    System.out.println("Please follow the format for setting events\nevent [your-task-here] /from [start-time-of-your-task] /by [end-time-of-your-task]");
+                    userReply = inputTaker.nextLine();
+                }
             } else if (userReply.contains("unmark")) {
-                int toDoListIndex =  Integer.parseInt(userReply.substring(userReply.length() -1)) - 1;
-                toDoList[toDoListIndex].todoStatus = false;
-                System.out.println("OK! I've marked this task as not done yet:");
-                System.out.println("[" + toDoList[toDoListIndex].doneOrNot() + "] " + toDoList[toDoListIndex].itemDescription);
-                userReply = inputTaker.nextLine();
+                try {
+                    if (userReply.length() == 6) {
+                        throw new gopsException();
+                    }
+                    int toDoListIndex = Integer.parseInt(userReply.substring(userReply.length() - 1)) - 1;
+                    toDoList[toDoListIndex].todoStatus = false;
+                    System.out.println("OK! I've marked this task as not done yet:");
+                    System.out.println("[" + toDoList[toDoListIndex].doneOrNot() + "] " + toDoList[toDoListIndex].itemDescription);
+                    userReply = inputTaker.nextLine();
+                } catch (gopsException e) {
+                    System.out.println("Please follow the format for unmarking tasks\nunmark [task-number]");
+                    userReply = inputTaker.nextLine();
+                }
             }
             else if (userReply.contains("mark")) {
-                int toDoListIndex =  Integer.parseInt(userReply.substring(userReply.length() -1)) - 1;
-                toDoList[toDoListIndex].todoStatus = true;
-                System.out.println("Nice! I've marked this task as done:");
-                toDoList[toDoListIndex].Printer();
-                userReply = inputTaker.nextLine();
+                try {
+                    if (userReply.length() == 4) {
+                        throw new gopsException();
+                    }
+                    int toDoListIndex =  Integer.parseInt(userReply.substring(userReply.length() -1)) - 1;
+                    toDoList[toDoListIndex].todoStatus = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    toDoList[toDoListIndex].Printer();
+                    userReply = inputTaker.nextLine();
+                } catch (gopsException e) {
+                    System.out.println("Please follow the format for marking tasks as done\nmark [task-number]");
+                    userReply = inputTaker.nextLine();
+                }
             }
 
             else if (userReply.equals("list")) {
@@ -129,10 +167,7 @@ public class gops {
                 }
                 userReply = inputTaker.nextLine();
             } else {
-                Todo toDo = new Todo(userReply);
-                toDoList[messageCount] = toDo;
-                messageCount += 1;
-                System.out.println("added: " + userReply);
+                System.out.println("Please choose from the available prompts\n[todo/deadline/event/mark/unmark/list/bye]");
                 userReply = inputTaker.nextLine();
             }
         }
