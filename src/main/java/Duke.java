342305b1e2a6290fb9233fd2cbf6f32760ccf54f
meenulekha <e0969684@u.nus.edu>
2024-01-24 23:06:57 +0800

    Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -7,9 +10,21 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         String line = "------------------------------" ;
+        Scanner obj = new Scanner(System.in);
         System.out.println("\n Hello! I'm Leo\n" +
                 " What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
+        while(obj.hasNextLine()){
+            String res = obj.nextLine();
+            if (res.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                return;
+            } else {
+                System.out.println(res);
+            }
+            System.out.println(line);
+        }
+
     }
 }
