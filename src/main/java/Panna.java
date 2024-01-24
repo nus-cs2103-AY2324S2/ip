a95b480f316a987a9745c53e7836e0376de58eb6
AjayArvind2207 <alwayssactheexchange@gmail.com>
2024-01-24 15:11:49 +0800

    Revert "Staged the commit, and am seeing if this fixes my issue earlier of not being able to push!"
    This reverts commit 828d0fc9f2976c02b78cf5b669363c547144cb53.

--- src/main/java/Panna.java
+++ src/main/java/Panna.java
@@ -1,11 +1,2 @@
-public class Panna {
-    public static void main(String[] args) {
-        System.out.println("----------------------------------------------------------\n"
-        + "Hello! I'm Panna.\n" +
-                "What can I do for you?\n\n" +
-                "----------------------------------------------------------");
-
-        System.out.println("Bye. Hope to see you again soon!\n\n" +
-                "----------------------------------------------------------");
-    }
+package PACKAGE_NAME;public class Panna {
 }
