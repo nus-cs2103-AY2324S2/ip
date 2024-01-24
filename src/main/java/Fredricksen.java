c2ce2e7dea168475c6047854dfbac07c238a407f
Zack-Tay <tayzhongkai@gmail.com>
2024-01-25 01:30:10 +0800

    add mark as done for task in Fredicksen.java Level-3

--- src/main/java/Fredricksen.java
+++ src/main/java/Fredricksen.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Fredricksen {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList list = new ArrayList();
+        ArrayList<String> list = new ArrayList();
+        ArrayList<Boolean> boolList = new ArrayList();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Fredricksen");
         System.out.println("What can I do for you?");
@@ -11,7 +12,22 @@ public class Fredricksen {
         while (true) {
             System.out.println("");
             String s = in.nextLine();
-            switch (s.toLowerCase()) {
+            String[] split = s.split(" ");
+            switch (split[0].toLowerCase()) {
+                case "mark":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("    [X] " + list.get(Integer.parseInt(split[1]) - 1));
+                    boolList.set(Integer.parseInt(split[1]) - 1, true);
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "unmark":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("    [] " + list.get(Integer.parseInt(split[1]) - 1));
+                    boolList.set(Integer.parseInt(split[1]) - 1, false);
+                    System.out.println("____________________________________________________________");
+                    break;
                 case "bye":
                     System.out.println("____________________________________________________________");
                     System.out.println("Bye. Hope to see you again soon!");
@@ -24,9 +40,11 @@ public class Fredricksen {
                     break;
                 case "list":
                     System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     if (list.size() > 0) {
                         for (int i = 1; i <= list.size(); i++) {
-                            System.out.println(" " + i + ". " + list.get(i-1));
+                            String t = boolList.get(i - 1) ? "X" : "";
+                            System.out.println(" " + i + ". " + "[" + t + "] " + list.get(i - 1));
                         }
                     }
                     System.out.println("____________________________________________________________");
@@ -35,6 +53,7 @@ public class Fredricksen {
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + s);
                     list.add(s);
+                    boolList.add(false);
                     System.out.println("____________________________________________________________");
                     break;
             }
