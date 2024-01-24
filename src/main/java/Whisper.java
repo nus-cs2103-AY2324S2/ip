6086ed600dbf37d79a2d549a11b413f67183ca20
choowengyan <wengstudies0407@gmail.com>
2024-01-24 14:31:48 +0800

    echo command and exit func

--- src/main/java/Whisper.java
+++ src/main/java/Whisper.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Whisper {
+    static String line = "-------------------------------------------------\n";
+    static String name = "Whisper";
+    static String welcomeMsg = "Hello! I'm " + name + " , your personal chatbot!\n" +
+            "What can I do for you?\n";
+    static String byeMsg = line + "Bye. Hope to see you soon!\n" + line;
+
+    // Main method
     public static void main(String[] args) {
-        String line = "-------------------------------------------------";
-        String name = "Whisper";
-        String welcomeMsg = "Hello! I'm " + name + " , your personal chatbot!\n" +
-                    "What can I do for you?\n";
-        String byeMsg = "Bye. Hope to see you soon!\n";
-        System.out.println(line + "\n" + welcomeMsg + line);
-        System.out.println(byeMsg + line);
+        System.out.println(line + welcomeMsg + line);
+        startChat();
+    }
+
+    // method for chats
+    public static void startChat() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            // read user input
+            System.out.println("Enter your input: ");
+            String input = sc.nextLine();
+
+            // break if user exists the bot
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(byeMsg);
+                break;
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
+        sc.close();
     }
 }
