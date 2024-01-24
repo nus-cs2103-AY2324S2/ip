2baa2dfc7a197e66c16fc5c5c7fb1e427f7e43d2
yisiox <yisiong.y22@gmail.com>
2024-01-24 21:37:42 +0800

    Added list functionality

--- src/main/java/Earl.java
+++ src/main/java/Earl.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Earl {
+
+    public static String[] data = new String[100];
+    public static int dataSize = 0;
+
     public static void divider() {
         System.out.println("\t" + "_".repeat(50));
     }
@@ -15,6 +19,7 @@ public class Earl {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         // starting messages
         String logo = " ______           _ \n"
                 + "\t|  ____|         | |\n"
@@ -23,13 +28,29 @@ public class Earl {
                 + "\t| |___| (_| | |  | |\n"
                 + "\t|______\\__,_|_|  |_|";
         respond(logo, "Hello! I'm Earl", "What can I do for you?");
+
         // main loop
         String command = sc.nextLine();
         while (!command.equals("bye")) {
-            respond(command);
+            switch (command) {
+                case "list":
+                    if (dataSize == 0) {
+                        respond("There is nothing to list.");
+                    } else {
+                        String[] temp = new String[dataSize];
+                        for (int i = 0; i < dataSize; ++i) {
+                            temp[i] = i + 1 + ". " + data[i];
+                        }
+                        respond(temp);
+                    }
+                    break;
+                default:
+                    data[dataSize++] = command;
+                    respond("Item added to list: " + command);
+            }
             command = sc.nextLine();
         }
-        respond("Bye. Hope to see you again soon!");
+        respond("Goodbye! See you soon.");
         sc.close();
     }
 
