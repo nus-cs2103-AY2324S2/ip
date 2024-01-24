c9b0d61aac8f6bb855242af6d846e3097c5cef8b
jayne1010 <e0970489@u.nus.edu>
2024-01-24 14:53:34 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         // Greeting
@@ -6,8 +7,30 @@ public class Duke {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Farewell
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        // Scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Main chat loop
+        while (true) {
+            // Read user input
+            String userInput = scanner.nextLine();
+
+            // Echo the user command
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+
+            // Check if the user wants to exit
+            if (userInput.equalsIgnoreCase("bye")) {
+                // Farewell
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break; // Exit the loop
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
