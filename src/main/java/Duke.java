3d77c469958d6ba094e7726d2af10c94d68c4e8c
Yadunand Prem <yadunand@yadunut.com>
2024-01-24 14:47:36 +0800

    feat: add ability to echo back and quit on bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,18 @@
 public class Duke {
+    static String line = "____________________________________________________________";
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+
+        System.out.println(line);
         System.out.println("Hello! I'm Brian\nWhat can I do for you?");
-        System.out.println( "____________________________________________________________");
+        System.out.println(line);
+        String input = "";
+        while (!input.equals("bye")) {
+            input = new java.util.Scanner(System.in).nextLine();
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println( "____________________________________________________________");
+        System.out.println(line);
     }
 }
