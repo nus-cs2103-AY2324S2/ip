1dcc2cda24c485b6dca650c1f5f5b85d241595bc
DOMOPC\domin <Dominiclimyongen1@gmail.com>
2024-01-24 20:52:29 +0800

    Changed name from duke to zoe and changed function from displaying duke to required IP level 0 text.

---
+++ src/main/java/Zoe.java
@@ -0,0 +1,8 @@
+public class Zoe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Zoe");
+        System.out.println("What can I do for you?");
+        System.out.println("\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
