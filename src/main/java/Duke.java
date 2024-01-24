0b9d9fabc8dad03a7c5382b67e654518a57df198
yap-zong-xin <yapzongxin@hotmail.com>
2024-01-24 21:15:29 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,22 +7,36 @@ public class Duke {
         System.out.println("    Hello! I'm Yappy\n    What can I do for you?");
         System.out.println("    ____________________________________________________________");
 
-        ArrayList<String> AL = new ArrayList<>();
+        ArrayList<Task> AL = new ArrayList<>();
 
         while(true) {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String input = br.readLine();
+            String[] token = input.split(" ");
 
-            if(input.equals("bye")) {
+            if (input.equals("bye")) {
                 break;
-            } else if(input.equals("list")) {
+            } else if (input.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                for(int i = 1; i <= AL.size(); i++) {
-                    System.out.println("    " + i + ". " + AL.get(i-1));
+                for (int i = 1; i <= AL.size(); i++) {
+                    System.out.println("    " + i + ".[" + AL.get(i-1).getStatusIcon() + "] " + AL.get(i-1).getDescription());
                 }
                 System.out.println("    ____________________________________________________________");
+            } else if(token[0].equals("mark")) {
+                AL.get(Integer.parseInt(token[1])-1).markAsDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [" + AL.get(Integer.parseInt(token[1])-1).getStatusIcon() + "] " + AL.get(Integer.parseInt(token[1])-1).getDescription());
+                System.out.println("    ____________________________________________________________");
+            } else if(token[0].equals("unmark")) {
+                AL.get(Integer.parseInt(token[1])-1).markAsUndone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     OK, I've marked this task as not done yet:");
+                System.out.println("       [" + AL.get(Integer.parseInt(token[1])-1).getStatusIcon() + "] " + AL.get(Integer.parseInt(token[1])-1).getDescription());
+                System.out.println("    ____________________________________________________________");
             } else {
-                AL.add(input);
+                Task t = new Task(input);
+                AL.add(t);
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    added: " + input);
                 System.out.println("    ____________________________________________________________");
