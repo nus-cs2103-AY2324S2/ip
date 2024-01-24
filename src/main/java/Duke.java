3eb35ab8836014212ff6da04881a004e3c70f0db
YowSiaoKang <kangyow4@gmail.com>
2024-01-24 13:45:22 +0800

    implemented Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String logo = " ____        _        \n"
+                                + "|  _ \\ _   _| | _____ \n"
+                                + "| | | | | | | |/ / _ \\\n"
+                                + "| |_| | |_| |   <  __/\n"
+                                + "|____/ \\__,_|_|\\_\\___|\n";
+
     private static final String LINE = "____________________________________________________________";
 
     private static void sayGreetings() {
@@ -9,20 +17,27 @@ public class Duke {
     }
 
     private static void sayBye() {
-        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
 
+    private static void echo(String input) {
+        System.out.println(input);
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
 
         sayGreetings();
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(LINE);
+            if (input.equals("bye")) {
+                break;
+            }
+            echo(input);
+        }
         sayBye();
     }
 }
