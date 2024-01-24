edd27a70f3497a6942e7337d8a660b16ae1d32a7
Tang Hao Liang <tang.hao.liang.thl@gmail.com>
2024-01-24 18:28:58 +0800

    Added PrintingString and completed level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        //Starting and Ending Text
         String start = "Hello! I'm Unknown \n"
                 + "What can I do for you? \n";
         String end = "Bye. Hope to see you again soon!\n";
+
+        //Boolean value to indicate whether the user has finished
+        boolean finished = false;
+
+        //Printing of Start text
+        PrintingString(start);
+
+        Scanner in = new Scanner(System.in);
+
+        while(!finished) {
+            String s = in.nextLine();
+
+            if (s.equalsIgnoreCase("bye")) {
+                finished = true;
+            } else {
+                PrintingString(s + "\n");
+            }
+        }
+
+        //Printing of End text
+        PrintingString(end);
+    }
+
+    public static void PrintingString(String str) {
+        //Function to add the line in front and behind the text
         String lnBreak = "_______________________________________________________________\n";
-        System.out.println(lnBreak + start + lnBreak + end + lnBreak);
+        System.out.println(lnBreak + str + lnBreak);
     }
 }
