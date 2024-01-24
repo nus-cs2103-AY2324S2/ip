62533b4c2e78aab14dd33e701b631c08a5e25ee6
kaitinghh <114789615+kaitinghh@users.noreply.github.com>
2024-01-25 01:28:07 +0800

    Level 1. Echo

--- src/main/java/BotChat.java
+++ src/main/java/BotChat.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class BotChat {
+    static boolean terminate = false;
+
+    public static String response(String s) {
+        if (s.equals("bye")) {
+            terminate = true;
+            return "Bye. Hope to see you again soon!";
+        } else {
+            return s;
+        }
+    }
+
     public static void main(String[] args) {
         String greeting = "Hello! I'm BotChat.\n What can I do for you?\n Bye. Hope to see you again soon!";
         System.out.println(greeting);
+
+        Scanner userInput = new Scanner(System.in);
+
+        while (!terminate) {
+            String command = userInput.nextLine();
+            System.out.println(response(command));
+        }
     }
 }
