9d81d771ed95616e501390922b7d6e812ac1c2ad
Ko-Khan <e0970313@u.nus.edu>
2024-01-24 12:27:44 +0800

    Bit now echoes commands and exits when command "bye" is entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import  java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -5,7 +6,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";*/
-        System.out.println("Hi! This is Bit!\nWhat shall we do today?");
+        System.out.println("Hi! This is Bit!\nWhat shall we do today?\n");
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+
+        }
+
+
         System.out.println("Alright. See you soon!");
     }
 }
