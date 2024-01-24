f30f077969e07d827e54c90a975dc82a53b9dc0b
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-01-24 19:41:52 +0800

    Add command handling for todo, deadline, event and fix argument parsing bug

--- src/main/java/Chimp.java
+++ src/main/java/Chimp.java
@@ -18,31 +18,58 @@ public class Chimp {
         while (!inp.equalsIgnoreCase("bye")) {
             inp = inp.toLowerCase();
             String command = inp.split(" ")[0];
-            Integer arg = null;
+            String arg = null;
             if (inp.split(" ").length > 1) {
-                arg = Integer.parseInt(inp.split(" ")[1]);
-                if (arg < 1 || arg > chimp.list.size())
-                    throw new InvalidParameterException();
+                // split at first space, and take everything on the right
+                arg = inp.substring(inp.indexOf(' '), inp.length());
             }
+            int num;
             switch (command) {
                 case "list":
                     chimp.print();
                     break;
                 case "mark":
-                    chimp.list.get(arg - 1).mark();
-                    chimp.say(phrases.get("mark"), chimp.list.get(arg - 1));
+                    num = Integer.parseInt(arg);
+                    if (num < 1 || num > chimp.list.size())
+                        throw new InvalidParameterException();
+                    chimp.list.get(num - 1).mark();
+                    chimp.say(phrases.get("mark"), chimp.list.get(num - 1));
                     break;
                 case "unmark":
-                    chimp.list.get(arg - 1).unmark();
-                    chimp.say(phrases.get("unmark"), chimp.list.get(arg - 1));
+                    num = Integer.parseInt(arg);
+                    if (num < 1 || num > chimp.list.size())
+                        throw new InvalidParameterException();
+                    chimp.list.get(num - 1).unmark();
+                    chimp.say(phrases.get("unmark"), chimp.list.get(num - 1));
+                    break;
+//                case "todo":
+//                    chimp.addToList(inp);
+//                    chimp.say(chimp.list.get(chimp.list.size() - 1));
+//                    break;
+                case "event":
+                    String fromSubCommand = arg.split("/")[1];
+                    String from = fromSubCommand.substring(fromSubCommand.indexOf(' '), fromSubCommand.length());
+                    from = from.strip();
+                    String toSubCommand = arg.split("/")[2];
+                    String to = toSubCommand.substring(toSubCommand.indexOf(' '), toSubCommand.length());
+                    to = to.strip();
+                    chimp.addToList(arg.split("/")[0].strip(), from, to);
+                    chimp.say(chimp.list.get(chimp.list.size() - 1));
+                    break;
+                case "deadline":
+                    String bySubCommand = arg.split("/")[1];
+                    String by = bySubCommand.substring(3, bySubCommand.length());
+                    by = by.strip();
+                    chimp.addToList(arg.split("/")[0].strip(), by);
+                    chimp.say(chimp.list.get(chimp.list.size() - 1));
                     break;
                 default:
-                    chimp.say("added: " + inp);
                     chimp.addToList(inp);
+                    chimp.say(chimp.list.get(chimp.list.size() - 1));
             }
             inp = sc.nextLine();
         }
-        chimp.bye();
+        chimp.say(phrases.get("bye"));
         sc.close();
     }
 
@@ -63,13 +90,29 @@ public class Chimp {
     }
 
     private void addToList(String task){
-        this.list.add(new Task(task, TaskStatus.UNMARKED));
+        this.list.add(new Todo(task, TaskStatus.UNMARKED));
+    }
+    private void addToList(String task, String by) {
+        this.list.add(new Deadline(task, TaskStatus.UNMARKED, by));
+    }
+
+    private void addToList(String task, String from, String to) {
+        this.list.add(new Event(task, TaskStatus.UNMARKED, from, to));
     }
 
     private void say(String phrase) {
         String divider = "____________________________________________________________\n";
         System.out.println(divider);
         System.out.println(phrase);
+        System.out.println(divider);
+    }
+
+    private void say(Task task) {
+        String divider = "____________________________________________________________\n";
+        System.out.println(divider);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + this.list.size() + " tasks in the list.");
         System.out.println(divider);
     }
 
@@ -81,18 +124,10 @@ public class Chimp {
         System.out.println(divider);
     }
 
-    // Treat bye as a special case for extensibility
-    private void bye() {
-        String divider = "____________________________________________________________\n";
-        System.out.println(divider);
-        String bye = "Bye. Hope to see you again soon!\n";
-        System.out.println(bye);
-        System.out.println(divider);
-    }
-
     private void print() {
         String divider = "____________________________________________________________\n";
         System.out.println(divider);
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < this.list.size(); i++) {
             System.out.println((i + 1) + ". " + this.list.get(i));
         }
