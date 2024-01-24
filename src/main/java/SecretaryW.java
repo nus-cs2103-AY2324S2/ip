be1a42279abb0a19ec51528a7fed601227791efd
tayziyi <ziyitay@Zis-MacBook-Pro.local>
2024-01-24 11:48:54 +0800

    Level-1

--- src/main/java/SecretaryW.java
+++ src/main/java/SecretaryW.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 public class SecretaryW {
     public static void main(String[] args) {
+        // Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
         String line = "------------------------------------------\n";
         String greeting = "Hello! I'm SecretaryW\n" + "What can I do for you?\n";
         String farewell = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + greeting + line + farewell);
+
+        // Greetings
+        System.out.println(line + greeting + line);
+
+        // Read user input in the loop
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")){
+                break; // Exits loop
+            }
+
+            // Echo
+            String echo = userInput + "\n";
+            System.out.println(line + echo + line);
+        }
+
+        // Farewell
+        System.out.println(line + farewell + line);
+        scanner.close();
     }
 }
