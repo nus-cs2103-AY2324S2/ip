2f1d4f776095410bac1e833242bbc0724ad7cee8
bachletuankhai <bachletuankhai@gmail.com>
2024-01-25 00:06:56 +0800

    Add greet() and exit() methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
 public class Duke {
-    public static void main(String[] args) {
+    private static final String chatbotName = "Sylvia";
+
+    private static void greet() {
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Sylvia\nWhat can I do for you?");
+        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void exit() {
         System.out.println("____________________________________________________________");
         System.out.println("Cya!");
+        System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
