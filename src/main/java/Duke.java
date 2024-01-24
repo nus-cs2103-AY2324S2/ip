2dd5983f98953c16bfe68259cb3f6b9d556b9f39
Ashley <>
2024-01-25 01:46:55 +0800

    add Todos, Events and Deadlines class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,11 +28,14 @@ public class Duke {
             System.out.println(" ");
             input = sc.next();
 
+                // end the program
             if (input.equals("bye")) {
                 System.out.println(indent + "------------------------------------------------");
                 System.out.println(indent + "Bye. Hope to see you again soon!");
                 System.out.println(indent + "------------------------------------------------");
                 output = false;
+
+                // print the entire task list
             } else if (input.equals("list")) {
                 System.out.println(indent + "------------------------------------------------");
                 System.out.println(indent + "Here are the tasks in your list:");
@@ -41,6 +44,8 @@ public class Duke {
                     System.out.println(indent + (i + 1) + "." + t.toString());
                 }
                 System.out.println(indent + "------------------------------------------------");
+
+                // mark an item in list
             } else if (input.equals("mark")) {
                 System.out.println(indent + "------------------------------------------------");
                 int position = sc.nextInt() - 1;
@@ -51,6 +56,8 @@ public class Duke {
                 t.markAsDone();
                 System.out.println(indent + "  " +  t.toString());
                 System.out.println(indent + "------------------------------------------------");
+
+                // unmark an item in list
             } else if (input.equals("unmark")) {
                 System.out.println(indent + "------------------------------------------------");
                 int position = sc.nextInt() - 1;
@@ -61,12 +68,50 @@ public class Duke {
                 t.markAsUndone();
                 System.out.println(indent + "  " + t.toString());
                 System.out.println(indent + "------------------------------------------------");
+
+            } else if (input.equals("todo")) {
+                System.out.println(indent + "------------------------------------------------");
+                System.out.println(indent + "Got it. I've added this task:");
+                String des = sc.nextLine().trim();
+                Todo t = new Todo(des);
+                list.add(t);
+                System.out.println(indent + "  " + t.toString());
+                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                System.out.println(indent + "------------------------------------------------");
+
+            } else if (input.equals("deadline")) {
+                System.out.println(indent + "------------------------------------------------");
+                System.out.println(indent + "Got it. I've added this task:");
+                String temp = sc.nextLine();
+                String[] arrOfStr = temp.split("/by");
+                String des = arrOfStr[0].trim();
+                String by = arrOfStr[1].trim();
+                Deadline t = new Deadline(des, by);
+                list.add(t);
+                System.out.println(indent + "  " + t.toString());
+                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                System.out.println(indent + "------------------------------------------------");
+
+            } else if (input.equals("event")) {
+                System.out.println(indent + "------------------------------------------------");
+                System.out.println(indent + "Got it. I've added this task:");
+                String temp = sc.nextLine();
+                String[] arrOfStr = temp.split("/");
+                String des = arrOfStr[0].trim();
+                String start = arrOfStr[1].substring(5).trim();
+                String end = arrOfStr[2].substring(3).trim();
+                Event t = new Event(des, start, end);
+                list.add(t);
+                System.out.println(indent + "  " + t.toString());
+                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                System.out.println(indent + "------------------------------------------------");
+
+                // echo the words
             } else {
                 System.out.println(indent + "------------------------------------------------");
                 String des = input + sc.nextLine();
-                System.out.println(indent + "added: " + des);
+                System.out.println(indent + des);
                 System.out.println(indent + "------------------------------------------------");
-                list.add(new Task(des));;
             }
         }
 
