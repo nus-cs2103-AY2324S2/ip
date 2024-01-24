1e4ad267618558af7a26b62fdd4b1a6018261466
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-01-24 10:27:08 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Harvard.java
@@ -0,0 +1,11 @@
+public class Harvard {
+    public static void main(String[] args) {
+        String initial = "____________________________________________________________\n"
+                + "Hello! I'm Harvard\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(initial);
+    }
+}
