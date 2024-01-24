4b564d97619d934d4e20e93789fc80cdc24630df
Ryo-Seah <seah_ryo@yahoo.com.sg>
2024-01-24 21:40:49 +0800

    Complete Level-0

---
+++ src/main/java/GPT.java
@@ -0,0 +1,9 @@
+public class GPT {
+    public static void main(String[] args) {
+        String name = " GPT";
+        String secLine = "What can I do for you?\n\n" + "Bye. Hope to see you soon";
+
+        System.out.println("Hello! I'm" + name);
+        System.out.println(secLine);
+    }
+}
