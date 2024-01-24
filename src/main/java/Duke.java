6c8e4b8125e8826de0ddf4f82a287ec550503bd6
Benson <bensonang36@gmail.com>
2024-01-24 23:47:08 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,11 @@ import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     private String name;
-    private ArrayList<String> list;
+    private ArrayList<Task> list;
 
     public Duke(String name) {
         this.name = name;
-        this.list = new ArrayList<String>();
+        this.list = new ArrayList<Task>();
     }
 
     public void greet() {
@@ -22,22 +22,44 @@ public class Duke {
         System.out.println(message);
     }
 
-    public void addToList(String task) {
+    public void addToList(String taskMessage) {
+        Task task = new Task(taskMessage);
         this.list.add(task);
-        System.out.println(String.format("added: %s", task));
+        System.out.println(String.format("added: %s", task.getDescription()));
     }
 
     public void displayList() {
         int index = 1;
-        for (String task : this.list){
-            System.out.println(String.format("%d. %s", index, task));
+        for (Task task : this.list){
+            System.out.println(String.format("%d. [%s] %s", index, task.getStatusIcon(), task.getDescription()));
             index++;
         }
     }
 
+    public void markTask(int index) {
+        this.list.get(index - 1).markAsDone();
+    }
+
+    public void unmarkTask(int index) {
+        this.list.get(index - 1).maskAsUndone();
+    }
     public void readCommand(String command){
         if (Objects.equals(command, "list")) {
             this.displayList();
+            return;
+        }
+
+        if (command.startsWith("mark ")) {
+            String[] split = command.split(" ");
+            String s = split[1];
+            this.markTask(Integer.valueOf(s));
+            return;
+        }
+
+        if (command.startsWith("unmark ")) {
+            String[] split = command.split(" ");
+            String s = split[1];
+            this.unmarkTask(Integer.valueOf(s));
             return;
         }
 
@@ -45,7 +67,6 @@ public class Duke {
             this.exit();
             System.exit(0);
         }
-
         this.addToList(command);
     }
 
@@ -59,3 +80,4 @@ public class Duke {
         }
     }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println(String.format("Wow! you finally did it huh ..:\n [%s] %s",this.getStatusIcon(),
+                this.getDescription()));
+    }
+
+    public void maskAsUndone() {
+        this.isDone = false;
+        System.out.println(String.format("Hurry up and do it!:\n [%s] %s",this.getStatusIcon(),
+                this.getDescription()));
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+}
