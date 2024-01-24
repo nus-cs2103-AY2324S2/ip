24d654a00cdc1362226898838cf6afc8ff815b10
jessicawyz <jessicawuyizheng@.com>
2024-01-24 17:59:42 +0800

    "Level-0 commit"

---
+++ src/main/java/Echo.java
@@ -0,0 +1,13 @@
+public class Echo {
+    public static void main(String[] args) {
+        greetUser();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void greetUser() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Echo");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+}
