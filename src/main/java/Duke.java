a1ca0cb4b6ee47d7f64cdd0128486da5d9a3b839
cocoanautz <isaacong1@hotmail.com>
2024-01-24 18:04:20 +0800

    Added Add, List functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,35 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String DELIMITER = "______________________________________";
+
+        System.out.println(DELIMITER);
         System.out.println("Hello! I'm Drew");
         System.out.println("What can I do for you?");
+        System.out.println(DELIMITER);
 
         String input = sc.nextLine();
+        String[] storage = new String[100];
+        int listLength = 0;
+
         while (!input.equalsIgnoreCase("bye")){
-            System.out.println(input);
+
+            String reply = "";
+
+            if (input.equalsIgnoreCase("list")) {
+                for(int i = 0; i < listLength; i++) {
+                    reply = reply + Integer.toString(i + 1) + ". " +  storage[i] + "\n";
+                }
+            } else {
+                storage[listLength] = input;
+                reply = String.format("added: %s\n", input);
+                listLength ++;
+            }
+
+            System.out.println(DELIMITER);
+            System.out.print(reply);
+            System.out.println(DELIMITER);
+
             input = sc.nextLine();
         }
 
