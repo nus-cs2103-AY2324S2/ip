a44fd3c10f1587b65995a919c28fda77d2950c27
Dexter-Wong <dexterwongster@gmail.com>
2024-01-25 04:06:37 +0800

    Level 0 commit

---
+++ src/main/java/Fireraya.java
@@ -0,0 +1,9 @@
+public class Fireraya {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Fireraya!");
+        System.out.println("What can I do for you today sir?");
+        System.out.println("--------------------------------");
+        System.out.println("Bye, hope to see you soon!");
+
+    }
+}
