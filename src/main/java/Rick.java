90871b86a4336003ede550f2f3b847dd6c4b205f
laney0808 <e0969207@u.nus.edu>
2024-01-25 00:27:59 +0800

    no message

--- src/main/java/Rick.java
+++ src/main/java/Rick.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 public class Rick {
     public static void main(String[] args) {
-        String hello = "____________________________________________________________\n"+
-        "Hello! I'm Rick\n"+
-        "What can I do for you ?\n";
+        hello();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                exit();
+                break;
+            } else {
+                echo(input);
+            }
+        }
+    }
+    public static void hello() {
+        String hello = "Hello! I'm Rick\n"+
+                "What can I do for you ?\n";
+        reply(hello);
+    }
 
-        String exit = "____________________________________________________________\n"+
-                "Bye. Hope to see you again soon !\n"+
-                "____________________________________________________________";
-        System.out.println(hello + exit);
+    public static void reply(String arg) {
+        String divider = "\n____________________________________________________________\n";
+        System.out.println(divider + arg + divider);
+    }
+
+    public static void exit(){
+        String exit = "Bye. Hope to see you again soon !";
+        reply(exit);
+    }
+    public static void echo(String args){
+        reply(args);
     }
 }
