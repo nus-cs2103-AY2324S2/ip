f88764f9219be41dd18be6e083db1e660b0be28e
Kaya3842 <haichiendhnn1@gmail.com>
2024-01-25 07:33:29 +0800

    Level-0

---
+++ src/main/java/Liv.java
@@ -0,0 +1,14 @@
+public class Liv {
+    public static void greet() {
+        System.out.println("Liv, under your instructions!");
+        System.out.println("What is your command?\n");
+    }
+    public static void exit() {
+        System.out.println("Farewell, see you next time!");
+    }
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+}
+
