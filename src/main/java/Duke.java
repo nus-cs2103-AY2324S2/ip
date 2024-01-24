884e13657044e4585675ae7ef5b277a84c3c2f8f
YowSiaoKang <kangyow4@gmail.com>
2024-01-24 13:10:20 +0800

    updated to Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+    private static final String LINE = "____________________________________________________________";
+
+    private static void sayGreetings() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm SKY");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void sayBye() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        sayGreetings();
+        sayBye();
     }
 }
