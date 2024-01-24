101cce510e4dcf625ed1e06dd32c24a27c383e59
tzaph <maximilliano.utomo@gmail.com>
2024-01-24 16:42:12 +0800

    level-1: echo command

--- src/main/java/Arona.java
+++ src/main/java/Arona.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class Arona {
-    private static void greet() {
+    private void greet() {
         System.out.println("Welcome, sensei! Arona has been waiting for you.\n");
     }
 
-    private static void exit() {
+    private void exit() {
         System.out.println("Thanks for the hard work, Sensei.\n");
     }
-    public static void main(String[] args) {
-        greet();
 
-        exit();
+    private void echoCommand(String command) {
+        System.out.println(command);
+    }
+    public static void main(String[] args) {
+        Arona arona = new Arona();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        arona.greet();
+
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                arona.echoCommand(command);
+            }
+        }
+
+        arona.exit();
+        scanner.close();
     }
 }
