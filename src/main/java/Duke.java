f6e60d486163b86100fae8901026d2cda64da733
Ashley <>
2024-01-25 02:57:49 +0800

    handle exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
 
         String logo = " ____ \n"
@@ -71,46 +71,73 @@ public class Duke {
 
             } else if (input.equals("todo")) {
                 System.out.println(indent + "------------------------------------------------");
-                System.out.println(indent + "Got it. I've added this task:");
-                String des = sc.nextLine().trim();
-                Todo t = new Todo(des);
-                list.add(t);
-                System.out.println(indent + "  " + t.toString());
-                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                // check for error
+                if (!sc.hasNext()) {
+                    System.out.println(indent + "OOPS!!! The description of a todo cannot be empty.");
+                    
+                } else {
+                    String des = sc.nextLine().trim();
+                    Todo t = new Todo(des);
+                    list.add(t);
+                    System.out.println(indent + "Got it. I've added this task:");
+                    System.out.println(indent + "  " + t.toString());
+                    System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                }
                 System.out.println(indent + "------------------------------------------------");
 
             } else if (input.equals("deadline")) {
                 System.out.println(indent + "------------------------------------------------");
-                System.out.println(indent + "Got it. I've added this task:");
-                String temp = sc.nextLine();
-                String[] arrOfStr = temp.split("/by");
-                String des = arrOfStr[0].trim();
-                String by = arrOfStr[1].trim();
-                Deadline t = new Deadline(des, by);
-                list.add(t);
-                System.out.println(indent + "  " + t.toString());
-                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                // check for error
+                if (!sc.hasNext()) {
+                    System.out.println(indent + "OOPS!!! The description of a deadline cannot be empty.");
+                } else {
+                    String temp = sc.nextLine();
+                    String[] arrOfStr = temp.split("/by");
+                    String des = arrOfStr[0].trim();
+                    if (arrOfStr.length < 2) {
+                        System.out.println(indent + "Please provide the end time for the deadline.");
+
+                    } else {
+                        String by = arrOfStr[1].trim();
+                        Deadline t = new Deadline(des, by);
+                        list.add(t);
+                        System.out.println(indent + "Got it. I've added this task:");
+                        System.out.println(indent + "  " + t.toString());
+                        System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                    }
+                }
                 System.out.println(indent + "------------------------------------------------");
 
             } else if (input.equals("event")) {
                 System.out.println(indent + "------------------------------------------------");
-                System.out.println(indent + "Got it. I've added this task:");
-                String temp = sc.nextLine();
-                String[] arrOfStr = temp.split("/");
-                String des = arrOfStr[0].trim();
-                String start = arrOfStr[1].substring(5).trim();
-                String end = arrOfStr[2].substring(3).trim();
-                Event t = new Event(des, start, end);
-                list.add(t);
-                System.out.println(indent + "  " + t.toString());
-                System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                // check for error
+                if (!sc.hasNext()) {
+                    System.out.println(indent + "OOPS!!! The description of an event cannot be empty.");
+                } else {
+                    String temp = sc.nextLine();
+                    String[] arrOfStr = temp.split("/");
+                    if (arrOfStr.length < 2) {
+                        System.out.println(indent + "Please provide the start time of the event.");
+                    } else if (arrOfStr.length < 3) {
+                        System.out.println(indent + "Please provide the end time of the event.");
+
+                    } else {
+                        String des = arrOfStr[0].trim();
+                        String start = arrOfStr[1].substring(5).trim();
+                        String end = arrOfStr[2].substring(3).trim();
+                        Event t = new Event(des, start, end);
+                        list.add(t);
+                        System.out.println(indent + "Got it. I've added this task:");
+                        System.out.println(indent + "  " + t.toString());
+                        System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                    }
+                }
                 System.out.println(indent + "------------------------------------------------");
 
                 // echo the words
             } else {
                 System.out.println(indent + "------------------------------------------------");
-                String des = input + sc.nextLine();
-                System.out.println(indent + des);
+                System.out.println(indent + "Sorry this is an invalid input :(");
                 System.out.println(indent + "------------------------------------------------");
             }
         }
