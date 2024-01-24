addc3aca6c351408d018fce974efa6faa7f54c73
aman zainal <aman@u.nus.edu>
2024-01-25 01:20:50 +0800

    added simple array to store user input and return it

--- src/main/java/Coat.java
+++ src/main/java/Coat.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Coat {
     public static void main(String[] args) {
-        System.out.printf(Messages.WELCOME);
-
         Scanner scanner = new Scanner(System.in);
+        List<String> userInputs = new ArrayList<>();
+
+        System.out.printf(Messages.WELCOME);
         while (true) {
-            System.out.printf("-> ");
+            System.out.printf("\n-> ");
             String userInput = scanner.nextLine();
+
+            // switch (userInput) {
+            // case ""
+            // }
 
             // Exit condition: say "bye"
             if (userInput.equalsIgnoreCase("bye")) {
@@ -15,11 +22,23 @@ public class Coat {
                 break;
             }
 
-            // Level-1: Echo user input
-            System.out.printf(">>> %s\n", userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                listUserInputs(userInputs);
+            } else {
+                userInputs.add(userInput);
+                System.out.printf("\n(^-^)~~   Added: %s\n", userInput);
+            }
+
         }
 
-
         scanner.close();
+    }
+
+
+    private static void listUserInputs(List<String> userInputs) {
+        System.out.printf("\n(^-^)~~   Here's your list!\n");
+        for (int i = 0; i < userInputs.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, userInputs.get(i));
+        }
     }
 }
