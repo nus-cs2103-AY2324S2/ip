59f2d6f3541438726323fc319fe11d8b3ce281fe
maiyasaliha <s.maiya095@gmail.com>
2024-01-24 12:12:02 +0800

    Level 1: Echoing complete

--- src/main/java/Ken.java
+++ src/main/java/Ken.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
 public class Ken {
     public static void main(String[] args) {
+        //greet
         System.out.println("Hi Barbie!");
         System.out.println("I'm Ken!");
+
         System.out.println("What would you like to beach today?\n");
+
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        //response
+        do {
+            command = scanner.nextLine();
+            echoCommand(command);
+        } while (!command.equalsIgnoreCase("bye"));
+
+
+        //byee
         System.out.println("Beach off!\n");
 
+        scanner.close();
+    }
+
+    private static  void echoCommand(String command) {
+        System.out.println(command);
     }
 }
