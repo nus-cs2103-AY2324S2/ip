05279cd218d67cb741f6cd86d1f9f1d84ee2aa9f
ThitipatC <thitipatchaiyakul@gmail.com>
2024-01-25 00:09:05 +0800

    Rename chat bot to HASSTNT and change the skeleton

---
+++ src/main/java/HASSTNT.java
@@ -0,0 +1,13 @@
+public class HASSTNT{
+    private static final String welcome_message = "____________________________________________________________\n" +
+            "Hello! I'm HASSTNT\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________\n" +
+            " Bye. Hope to see you again soon!\n"+
+            "____________________________________________________________\n";
+
+    public static void main(String[] args) {
+        System.out.println(welcome_message);
+
+    }
+    }
