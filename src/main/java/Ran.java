e7174ab69f4ef95f1e79dce83fbdfb2da56e559a
Lee Wei Ming <leeweiming3@gmail.com>
2024-01-25 00:26:07 +0800

    Rename Duke to Ran

---
+++ src/main/java/Ran.java
@@ -0,0 +1,17 @@
+public class Ran {
+    public static void main(String[] args) {
+        System.out.println("Hello. I am ");
+        String art = "__________                \n" +
+                "\\______   \\_____    ____  \n" +
+                " |       _/\\__  \\  /    \\ \n" +
+                " |    |   \\ / __ \\|   |  \\\n" +
+                " |____|_  /(____  /___|  /\n" +
+                "        \\/      \\/     \\/ ";
+        System.out.println(art);
+        System.out.println("What would you like to do today?");
+        System.out.println("____________________________________________________________");
+
+
+        System.out.println("Goodbye, please return soon.");
+    }
+}
