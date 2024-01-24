7b7c9116e31eba52d9b99a5d02a58d814721c0c8
appleraincoat <james20111951@gmail.com>
2024-01-24 17:36:44 +0800

    Added chatbot Ypxmm, with only simple greetings before exiting

---
+++ src/main/java/Ypxmm.java
@@ -0,0 +1,8 @@
+public class Ypxmm {
+    public static void main(String[] args) {
+        System.out.println("Hallo boss, I'm Ypxmm.");
+        System.out.println("Need me do what for you?");
+        System.out.println("Okok I zao first, see you again bro!");
+
+    }
+}
