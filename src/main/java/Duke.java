939734b131c87dc7f52332fced3e903c768c4991
chengjunyuan <122239268+chengjunyuan@users.noreply.github.com>
2024-01-24 08:52:11 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+    public static String welcome = "Hi, I'm Gronk! \uD83D\uDDFF \n"
+            + "What are we up to today?";
+
+    public static String goodbye = "System closing. Goodbye!";
+
+    public static void lines() {
+        System.out.println("----------------------------------------");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+        lines();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        lines();
+        printMessage(welcome);
+        printMessage(goodbye);
     }
 }
