f02c9a9166d787e899769f53aff67dbad76a2830
ashleyclx <e0958221@u.nus.edu>
2024-01-24 16:58:59 +0800

    Level-1: Echo
    Added scanner, echo text and changed some text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
-    private static final String LINE = "____________________________________________________________\n";
+    private static final String LINE = "    ____________________________________________________________\n";
+    private static final String INDENT = "    ";
     public static void main(String[] args) {
         Duke.hello();
-        Duke.bye();
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                Duke.bye();
+                break;
+            } else {
+                System.out.print(Duke.LINE + Duke.INDENT + input + "\n" + Duke.LINE);
+            }
+        }
+
     }
 
     public static void hello() {
-        String logo = "   :::   :::           :::        :::::::::       :::::::::       ::::::::::       ::::::::: \n" +
-                "  :+:   :+:         :+: :+:      :+:    :+:      :+:    :+:      :+:              :+:    :+: \n" +
-                "  +:+ +:+         +:+   +:+     +:+    +:+      +:+    +:+      +:+              +:+    +:+  \n" +
-                "  +#++:         +#++:++#++:    +#++:++#+       +#++:++#+       +#++:++#         +#++:++#:    \n" +
-                "  +#+          +#+     +#+    +#+             +#+             +#+              +#+    +#+    \n" +
-                " #+#          #+#     #+#    #+#             #+#             #+#              #+#    #+#     \n" +
-                "###          ###     ###    ###             ###             ##########       ###    ###      \n\n";
-        System.out.println(Duke.LINE + " \uD83D\uDC4B✨What's poppin' fam, it's ya boi \n\n" + logo +
-                "Hit me up with those deets and let's vibe together! \uD83D\uDCAF\uD83D\uDD25\uD83D\uDE1C");
+        String logo = "       :::   :::           :::        :::::::::       :::::::::       ::::::::::       ::::::::: \n" +
+                "      :+:   :+:         :+: :+:      :+:    :+:      :+:    :+:      :+:              :+:    :+: \n" +
+                "      +:+ +:+         +:+   +:+     +:+    +:+      +:+    +:+      +:+              +:+    +:+  \n" +
+                "      +#++:         +#++:++#++:    +#++:++#+       +#++:++#+       +#++:++#         +#++:++#:    \n" +
+                "      +#+          +#+     +#+    +#+             +#+             +#+              +#+    +#+    \n" +
+                "     #+#          #+#     #+#    #+#             #+#             #+#              #+#    #+#     \n" +
+                "    ###          ###     ###    ###             ###             ##########       ###    ###      \n\n";
+        System.out.println(Duke.LINE + "    What's poppin' fam, it's ya boi \n\n" + logo +
+                "    Hit me up with those deets and let's vibe together!" + Duke.LINE);
     }
 
     public static void bye() {
-        System.out.print(Duke.LINE + "Peace out, fam!✌️ Stay lit and keep those good vibes rollin'! \uD83D\uDE80\uD83D\uDC96\n" + Duke.LINE);
+        System.out.print(Duke.LINE + "    Peace out, fam! Stay lit and keep those good vibes rollin'!\n" + Duke.LINE);
     }
 }
 
