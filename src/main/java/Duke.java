187aeb9b4614dd3ed0a89209957498e5bd897c60
whelan-low <e0958047@u.nus.edu>
2024-01-24 16:00:06 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class Duke {
+    private String[] tasks;
+    private int no_of_tasks;
     public static void main(String[] args) {
-        greet();
-        echo();
+        Duke greg = new Duke();
     }
 
-    public static void greet() {
+    public Duke() {
+        tasks = new String[100];
+        no_of_tasks = 0;
+        greet();
+        listen();
+    }
+    public void greet() {
         fillerLine();
         System.out.println("    Hello! I am Greg.\n    What can I do for you?");
         fillerLine();
     }
 
-    public static void bye() {
+    public void bye() {
         fillerLine();
         System.out.println("    Goodbye! Hope to see you again soon!");
         fillerLine();
@@ -21,16 +28,38 @@ public class Duke {
         System.out.println("    _______________________________________");
     }
 
-    public static void echo() {
+    public void listen() {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
 
-        while (!(s.equals("bye"))) {
-            fillerLine();
-            System.out.println("    " + s);
-            fillerLine();
-            s = sc.next();
+        while (true) {
+            if (s.equals("bye")) {
+                bye();
+                break;
+            } else {
+                if (s.equals("list")) {
+                    list();
+                } else {
+                    add(s);
+                }
+                s = sc.next();
+            }
         }
-        bye();
+    }
+
+    public void list() {
+        for(int i = 0; i < no_of_tasks; i++) {
+            String str = "";
+            str = String.format("%s: %s",i + 1, tasks[i]);
+            System.out.println(str);
+        }
+    }
+
+    public void add(String s) {
+        tasks[no_of_tasks] = s;
+        no_of_tasks++;
+        fillerLine();
+        System.out.println("    added: " + s);
+        fillerLine();
     }
 }
