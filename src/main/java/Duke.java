584c32fd2800e755790c97e0ac8daeac0274d3b6
Benson <bensonang36@gmail.com>
2024-01-24 17:06:05 +0800

    Added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     private String name;
+    
     public Duke(String name) {
         this.name = name;
     }
@@ -11,9 +14,20 @@ public class Duke {
     public void exit(){
         System.out.println("Bye. Hope to see you again soon!\n");
     }
+
+    public void say(String message) {
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
         Duke chatbot = new Duke("Bob");
         chatbot.greet();
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!Objects.equals(command, "bye")) {
+            chatbot.say(command);
+            command = scanner.nextLine();
+        }
         chatbot.exit();
     }
 }
