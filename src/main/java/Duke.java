5fee527f2f90d14d9aadb2d7451e4721c8f34c1d
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-01-25 02:22:32 +0800

    Level_1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String horizontalLine = "_".repeat(60);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
+
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(horizontalLine);
+            System.out.println(userInput);
+            System.out.println(horizontalLine);
+            userInput = sc.nextLine();
+        }
+        System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
+        sc.close();
     }
 }
