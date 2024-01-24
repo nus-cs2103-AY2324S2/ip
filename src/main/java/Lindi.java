ecb439b0d4407c69887b9bc522e98567d83c1447
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-24 15:49:47 +0800

    Refactored greeting messages into a function call greeting().

--- src/main/java/Lindi.java
+++ src/main/java/Lindi.java
@@ -2,18 +2,21 @@ public class Lindi {
     public static void printSeparator() {
         System.out.println("-".repeat(50));
     }
+    public static void greeting() {
+        String name = "Lindi";  // Log It N Do It -> LINDI
 
+        printSeparator();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        printSeparator();
+    }
     public static void goodByeAndExit() {
         System.out.println("Bye. Hope to see you again soon!");
         printSeparator();
         System.exit(0); // exit with code 0, terminates program
     }
     public static void main(String[] args) {
-        String name = "Lindi";  // Log It N Do It -> LINDI
-        printSeparator();
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        printSeparator();
+        greeting();
         goodByeAndExit();
     }
 }
