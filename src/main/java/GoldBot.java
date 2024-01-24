d338aeccb278ce45b0b51403ab37cee9b39390b6
Darylgolden <darylgolden@gmail.com>
2024-01-24 15:16:02 +0800

    level 0

---
+++ src/main/java/GoldBot.java
@@ -0,0 +1,11 @@
+public class GoldBot {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________\n" +
+        "Hello! I'm GoldBot!\n" +
+        "What can I do for you?\n" +
+       "____________________________________________________________\n" +
+        "Bye. Hope to see you again soon!\n" +
+       "____________________________________________________________";
+        System.out.println(message);
+    }
+}
