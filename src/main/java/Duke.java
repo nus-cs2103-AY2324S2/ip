7a54b497cdd9910935ece52159e4cbe403cda8ee
jamessinmaojun <jamessinmaojun@gmail.com>
2024-01-24 17:46:07 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     String divider = "--------------------------------------------------";
     ArrayList<Task> taskList = new ArrayList<Task>();
+
+    public String[] splitCommand(String command) {
+        return command.split("\\s+");
+    }
 
     public boolean parseCommand() {
         Scanner scanner = new Scanner(System.in);
@@ -12,19 +15,40 @@ public class Duke {
 
         System.out.println(this.divider);
 
-        if (Objects.equals(command, "bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-            return false;
-        } else if (Objects.equals(command, "list")) {
-            for (int i = 0; i < taskList.size(); i++) {
-                System.out.println(i + 1 + ". " + taskList.get(i).description);
-            }
-        } else {
-            this.taskList.add(new Task(command));
-            System.out.println("added: " + command);
+        switch (splitCommand(command)[0]) {
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                return false;
+            case "list":
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < this.taskList.size(); i++) {
+                    Task currentTask = this.taskList.get(i);
+                    System.out.println(i + 1 + "." + currentTask.display + " " + currentTask.description);
+                }
+                System.out.println(this.divider);
+                break;
+            case "mark":
+                String markMessage = "Nice! I've marked this task as done:\n  ";
+                int index = Integer.parseInt(splitCommand(command)[1]);
+                Task currentTask = this.taskList.get(index - 1);
+                currentTask.changeDone();
+                System.out.println(markMessage + currentTask.display + " " + currentTask.description);
+                System.out.println(this.divider);
+                break;
+            case "unmark":
+                markMessage = "Ok, I've marked this task as not done yet:\n  ";
+                index = Integer.parseInt(splitCommand(command)[1]);
+                currentTask = this.taskList.get(index - 1);
+                currentTask.changeDone();
+                System.out.println(markMessage + currentTask.display + " " + currentTask.description);
+                System.out.println(this.divider);
+                break;
+            default:
+                this.taskList.add(new Task(command));
+                System.out.println("added: " + command);
+                System.out.println(this.divider);
         }
 
-        System.out.println(this.divider);
         return true;
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,20 @@
 public class Task {
     String description;
+    boolean done;
+    String display;
 
     public Task(String description) {
         this.description = description;
+        this.done = false;
+        this.display = "[ ]";
+    }
+
+    public void changeDone() {
+        this.done = !this.done;
+        if (this.done) {
+            this.display = "[X]";
+        } else {
+            this.display = "[ ]";
+        }
     }
 }
