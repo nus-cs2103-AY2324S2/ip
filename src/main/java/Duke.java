1339f1786e3cb0bb3668f3be9ec23f1e7b5ae76d
ashleyclx <e0958221@u.nus.edu>
2024-01-24 16:05:16 +0800

    Changed text personality and logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        System.out.println(line + "Hello! I'm Yapper\n" + "What can I do for you?");
-        System.out.print(line + "Bye. Hope to see you again soon!\n" + line);
+        String logo = "   :::   :::           :::        :::::::::       :::::::::       ::::::::::       ::::::::: \n" +
+                "  :+:   :+:         :+: :+:      :+:    :+:      :+:    :+:      :+:              :+:    :+: \n" +
+                "  +:+ +:+         +:+   +:+     +:+    +:+      +:+    +:+      +:+              +:+    +:+  \n" +
+                "  +#++:         +#++:++#++:    +#++:++#+       +#++:++#+       +#++:++#         +#++:++#:    \n" +
+                "  +#+          +#+     +#+    +#+             +#+             +#+              +#+    +#+    \n" +
+                " #+#          #+#     #+#    #+#             #+#             #+#              #+#    #+#     \n" +
+                "###          ###     ###    ###             ###             ##########       ###    ###      \n\n";
+        System.out.println(line + " \uD83D\uDC4B✨What's poppin' fam, it's ya boi \n\n" + logo + "Hit me up with those deets and let's vibe together! \uD83D\uDCAF\uD83D\uDD25\uD83D\uDE1C");
+        System.out.print(line + "Bye! Talk to you again later!\n" + line);
     }
 }
 
