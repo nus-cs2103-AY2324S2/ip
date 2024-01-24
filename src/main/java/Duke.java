0c15365d9bc4bfa90b8bdf0eeb02dfd11aa6c860
R-Laksh <ramulakshmanan12@gmail.com>
2024-01-24 16:58:44 +0800

    implemented level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
+    private static String format(String text){
+        return "\t____________________________________________________________\n" +
+                "\t" +
+                text +
+                "\n" +
+                "\t____________________________________________________________";
+
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(format("Heyo, how u doing, Im Quacky. How can I help you?"));
+        String command = scanner.nextLine();
+        while(!command.equals("bye")){
+            System.out.println(format(command));
+            command = scanner.nextLine();
+        }
+        System.out.println(format("bye bye"));
+        scanner.close();
     }
 }
