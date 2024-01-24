96c5bbd3dc18e738d7b10687579a9771826e0f5f
Shaun Lai <shaun@littlebit.sg>
2024-01-24 23:16:17 +0800

    Have Lai echo commands and exit on the command 'bye'

--- src/main/java/Lai.java
+++ src/main/java/Lai.java
@@ -1,9 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Lai {
     public static void main(String[] args) {
         System.out.println("Hi there, I am Lai. Your friendly fairly useless chatbot.");
         System.out.println("What can I assist you with today?");
         System.out.println("---------------------------------------------------------");
-        System.out.println("See you again!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String command = scanner.nextLine();
+        while (!Objects.equals(command, "bye")) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(command);
+            System.out.println("---------------------------------------------------------");
+
+            System.out.print("> ");
+            command = scanner.nextLine();
+        }
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Goodbye, we shall meet again. Hopefully.");
         System.out.println("---------------------------------------------------------");
     }
 }
