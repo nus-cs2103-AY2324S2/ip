25ca0e223199e11423123f6b00a83f1ed181e094
Ty-stan0417 <yisheng0417@gmail.com>
2024-01-25 00:26:07 +0800

    Level-1

--- src/main/java/Tyler.java
+++ src/main/java/Tyler.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class Tyler {
     public static void main(String[] args) {
-        System.out.println("Hello from Tyler");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again");
+        System.out.println("    Hello from Tyler");
+        System.out.println("    What can I do for you?");
+        System.out.println("    --------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(input.equals("bye") != true) {
+            System.out.println("    --------------------------------------------------");
+            System.out.println("    " + input);
+            System.out.println("    --------------------------------------------------");
+            input = sc.nextLine();
+        }
+        System.out.println("    Bye. Hope to see you again");
     }
 }
