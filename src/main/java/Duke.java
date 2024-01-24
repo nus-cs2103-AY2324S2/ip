c2deb2f239bc4db31953fbbddeab81e721cd28fa
S-Aishvarya <aish.varya1801@gmail.com>
2024-01-24 20:19:03 +0800

    Level-0 commit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello, I'm Lighthouse.\nWhat can I do for you? \nBye, Hope to see you soon!\n";
+        System.out.println(greeting);
     }
 }
