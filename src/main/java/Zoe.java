88d12d69065f9539f5af2f2263c491a9e5f037f5
DOMOPC\domin <Dominiclimyongen1@gmail.com>
2024-01-24 22:14:39 +0800

    added mark/unmark capability

--- src/main/java/Zoe.java
+++ src/main/java/Zoe.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.*;
 public class Zoe {
     public static void main(String[] args) {
-        ArrayList<String> addedCommands = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         System.out.println("Hello! I'm Zoe");
         System.out.println("What can I do for you?");
 
@@ -11,13 +11,27 @@ public class Zoe {
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                for (int i = 0; i < addedCommands.size(); i++) {
+                for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(String.format
-                            ("%d. %s", i + 1, addedCommands.get(i)));
+                            ("%d.%s", i + 1, tasks.get(i).getStatus()));
                 }
             } else {
-                addedCommands.add(command);
-                System.out.println("added: " + command);
+                //at this stage there is only the mark and unmark command
+                String[] markUnmark = command.split(" ");
+                if (markUnmark[0].equals("mark")) {
+                    int idx = Integer.parseInt(markUnmark[1]) - 1;
+                    tasks.get(idx).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(idx).getStatus());
+                } else if (markUnmark[0].equals("unmark")){
+                    int idx = Integer.parseInt(markUnmark[1]) - 1;
+                    tasks.get(idx).unmark();
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    System.out.println(tasks.get(idx).getStatus());
+                } else {
+                    tasks.add(new Task(command));
+                    System.out.println("added: " + command);
+                }
             }
 
             command = scanner.nextLine();
