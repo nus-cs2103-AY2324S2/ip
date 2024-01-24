af0953af94bfdad856cd1f4461db41cc0263d2ed
jessicawyz <jessicawuyizheng@.com>
2024-01-24 18:37:31 +0800

    "REAL level 1"

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
         greetUser();
+        startConversation();
         System.out.println("Bye. Hope to see you again soon!");
     }
 
@@ -8,6 +11,23 @@ public class Echo {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Echo");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void startConversation() {
+        Scanner scanner = new Scanner(System.in);
+        String userCommand;
+
+        do {
+            System.out.print("\n");
+            userCommand = scanner.nextLine();
+            echoCommand(userCommand);
+        } while (!userCommand.equalsIgnoreCase("bye"));
+    }
+
+    public static void echoCommand(String command) {
+        System.out.println("____________________________________________________________");
+        System.out.println(command);
         System.out.println("____________________________________________________________");
     }
 }
