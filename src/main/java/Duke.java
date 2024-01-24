4e656c39a82fa65e239bf4f8284c3b7e3388a5e6
Ma-Yirui <saltiestwonton@gmail.com>
2024-01-25 00:19:36 +0800

    level-2: implemented echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,43 @@
-import java.io.PrintWriter;
+import java.io.*;
+
 public class Duke {
     private static final PrintWriter pw = new PrintWriter(System.out);
-    public static void main(String[] args){
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final String SEPERATOR = "_______________________________________\n";
+    public static void main(String[] args) throws IOException{
+        String line;
+
         greet();
+
+        while (!(line = br.readLine()).equals("bye")){
+            echo(line);
+        }
+
         exit();
+        pw.close();
+        br.close();
+    }
+
+    private static void greet(){
+        pw.print(SEPERATOR
+                + "Hello, Notnow here!\n"
+                + "What can I do for you?\n"
+                + SEPERATOR);
         pw.flush();
     }
 
-    private static void greet() {
-        pw.print("_______________________________________\n"
-                + "Hello, Notnow here!\n"
-                + "What can I do for you?\n"
-                + "_______________________________________\n");
+    private static void exit(){
+        pw.print(SEPERATOR
+                + "Bye. Hope to see u again soon!\n"
+                + SEPERATOR);
+        pw.flush();
     }
 
-    private static void exit() {
-        pw.print("_______________________________________\n"
-                + "Bye. Hope to see u again soon!\n"
-                + "_______________________________________\n");
+    private static void echo(String input) {
+        pw.print(SEPERATOR
+                + input + "\n"
+                + SEPERATOR);
+        pw.flush();
     }
 
 }
