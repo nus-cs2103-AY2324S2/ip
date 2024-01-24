f76d7f81513be86c5ff43cbef8b344a910957aa5
chenyixin0 <122212724+chenyixin0@users.noreply.github.com>
2024-01-25 01:40:56 +0800

    Level 0: Rename, Greet and Exit

--- src/main/java/TalkingBox.java
+++ src/main/java/TalkingBox.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
+        String NAME = "Talking Box";
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+
+        System.out.println("Hello, I am the " + NAME);
+        System.out.println("What do you want to talk about?");
+        System.out.println("Goodbye!");
+
+
     }
 }
