1c2e439f233eded71015b827a0d041eb6f131424
guohuang <guohuang88@gmail.com>
2024-01-24 13:25:46 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm GHBot");
         System.out.println("What can I do for you?");
+        List<String> lst = new ArrayList<>();
 
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -11,8 +14,14 @@ public class Duke {
             if (str.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (str.equalsIgnoreCase("list")) {
+                for (int i = 0; i < lst.size(); i++) {
+                    System.out.println(i + 1 + ". " + lst.get(i));
+                }
+            } else {
+                lst.add(str);
+                System.out.println("added: " + str);
             }
-            System.out.println(str);
         }
     }
 }
