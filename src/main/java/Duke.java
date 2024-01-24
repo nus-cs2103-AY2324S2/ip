f0a2a7ed0fbf2e5115018316b4dfb90d5a310051
cocoanautz <isaacong1@hotmail.com>
2024-01-24 17:40:52 +0800

    Added echo and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Drew");
         System.out.println("What can I do for you?");
+
+        String input = sc.nextLine();
+        while (!input.equalsIgnoreCase("bye")){
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
