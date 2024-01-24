93c6727c5db637c3773211372a2ca3526f1e616a
Ko-Khan <e0970313@u.nus.edu>
2024-01-24 10:01:23 +0800

    Renamed chatbot to Bit and added skeleton code that says hi and exits..

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("Hi! This is Bit!\nWhat shall we do today?");
+        System.out.println("Alright. See you soon!");
     }
 }
