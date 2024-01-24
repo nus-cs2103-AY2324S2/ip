7cd82b5b9d76c31bbc20f8433c7f9c36272b9e6a
danielwangcinch <daniel.wang@cinch.sg>
2024-01-24 15:37:21 +0800

    feat: mark as done w/ inheritance

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Tony {
     public static void main(String[] args) {
@@ -19,8 +20,24 @@ public class Tony {
             } else if (firstWord.equals("unmark")) {
                 String secondWord = words[1];
                 lst.unmark(secondWord);
+
+            } else if (firstWord.equals("todo")) {
+                String result = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                Task toDo = new Todo(result);
+                lst.add(toDo);
+            } else if (firstWord.equals("deadline")) {
+                String result = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                String[] parts = result.split("/");
+                Task deadline = new Deadline(parts[0], parts[1]);
+                lst.add(deadline);
+
+            } else if (firstWord.equals("event")) {
+                String result = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                String[] parts = result.split("/");
+                Task event = new Event(parts[0], parts[1], parts[2]);
+                lst.add(event);
             } else {
-                lst.add(input);
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
             input = scanner.nextLine();
         }
@@ -46,21 +63,22 @@ public class Tony {
     }
 
     static class ToDoList {
-        List<String> list = new ArrayList<>();
-        List<String> done = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
-        public void add(String item) {
+        public void add(Task item) {
             list.add(item);
-            done.add(" ");
+            int numberOfTasks = list.size();
             line();
-            System.out.println("Added: " + item);
+            System.out.println("Got it. I've added this task: \n");
+            item.toString();
+            System.out.println("Now you have "+ numberOfTasks + " tasks in the list \n");
             line();
         }
 
         public void mark(String input) {
             try {
                 int index = Integer.parseInt(input);
-                done.set(index - 1, "X");
+                list.get(index - 1).markAsDone();
                 line();
                 System.out.println("Marked item " + index + " as done.");
                 line();
@@ -74,7 +92,7 @@ public class Tony {
         public void unmark(String input) {
             try {
                 int index = Integer.parseInt(input);
-                done.set(index - 1, " ");
+                list.get(index - 1).markAsUndone();
                 line();
                 System.out.println("Unmarked item " + index + " as done.");
                 line();
@@ -87,10 +105,75 @@ public class Tony {
 
         public void print() {
             line();
+            System.out.println("Here are the tasks in your list: \n");
             for (int i = 0; i < list.size(); i++) {
-                System.out.println((i + 1) + ". " + "[" + done.get(i) + "] " + list.get(i) + "\n");
+                System.out.println(list.get(i).toString() + "\n");
             }
             line();
         }
+    }
+}
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    private String dueDate;
+
+    public Deadline(String description, String dueDate) {
+        super(description);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (" + dueDate + ")";
+    }
+}
+
+class Event extends Task {
+    private String from;
+    private String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (" + from + " " + to + ")";
     }
 }
