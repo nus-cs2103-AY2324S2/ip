075140dd123caf96724034669369b90a2e4ff85a
Shaun Lai <shaun@littlebit.sg>
2024-01-24 23:34:00 +0800

    Add ability to store and list commands entered by user

--- src/main/java/Lai.java
+++ src/main/java/Lai.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Lai {
     public static void main(String[] args) {
+        List<String> commands = new ArrayList<String>();
+
         System.out.println("Hi there, I am Lai. Your friendly fairly useless chatbot.");
         System.out.println("What can I assist you with today?");
         System.out.println("---------------------------------------------------------");
@@ -11,12 +15,24 @@ public class Lai {
         System.out.print("> ");
         String command = scanner.nextLine();
         while (!Objects.equals(command, "bye")) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println(command);
-            System.out.println("---------------------------------------------------------");
+            if (!Objects.equals(command, "list")) {
+                commands.add(command);
+                System.out.println("---------------------------------------------------------");
+                System.out.println("Added: " + command);
+                System.out.println("---------------------------------------------------------");
 
-            System.out.print("> ");
-            command = scanner.nextLine();
+                System.out.print("> ");
+                command = scanner.nextLine();
+            } else {
+                System.out.println("---------------------------------------------------------");
+                for (int i = 0; i < commands.size(); i++) {
+                    System.out.println(String.format("%s. %s", i + 1, commands.get(i)));
+                }
+                System.out.println("---------------------------------------------------------");
+
+                System.out.print("> ");
+                command = scanner.nextLine();
+            }
         }
 
         System.out.println("---------------------------------------------------------");
