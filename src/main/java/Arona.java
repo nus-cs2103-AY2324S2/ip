b45b6421bd7942f45d380657370045693e13d5e7
tzaph <maximilliano.utomo@gmail.com>
2024-01-24 16:06:37 +0800

    level-0: rename file and modify gitignore

---
+++ src/main/java/Arona.java
@@ -0,0 +1,14 @@
+public class Arona {
+    private static void greet() {
+        System.out.println("Welcome, sensei! Arona has been waiting for you.\n");
+    }
+
+    private static void exit() {
+        System.out.println("Thanks for the hard work, Sensei.\n");
+    }
+    public static void main(String[] args) {
+        greet();
+
+        exit();
+    }
+}
