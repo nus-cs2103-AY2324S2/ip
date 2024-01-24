b0a4661bee511bf9e6c4f371863f94202b9f84b3
Joshy837 <joshuayipsujun@gmail.com>
2024-01-25 00:56:13 +0800

    Echo

--- src/main/java/Joshy.java
+++ src/main/java/Joshy.java
@@ -1,10 +1,24 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Joshy {
     public static void main(String[] args) {
-        System.out.println("______________________________________________");
-        System.out.println("Hello! I'm Joshy");
-        System.out.println("What can I do for you?");
-        System.out.println("______________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________");
+        System.out.println("   ______________________________________________");
+        System.out.println("   Hello! I'm Joshy");
+        System.out.println("   What can I do for you?");
+        System.out.println("   ______________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println("   ______________________________________________");
+            System.out.println("   " + input);
+            System.out.println("   ______________________________________________");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("   ______________________________________________");
+        System.out.println("   Bye. Hope to see you again soon!");
+        System.out.println("   ______________________________________________");
     }
 }
