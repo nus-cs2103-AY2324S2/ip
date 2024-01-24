2ccb59e0088600fafa73d7a3c67ce284fa4fee9e
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-01-25 02:08:51 +0800

    Level_0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         String horizontalLine = "_".repeat(60);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Friday");
