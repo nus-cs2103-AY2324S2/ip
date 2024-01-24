788b52ae66a5649f099ef6dc0dbe0188cf390948
Yiyang <liuyiyang0929@gmail.com>
2024-01-24 17:46:22 +0800

    Added echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Yippee";
 
-        System.out.println("____________________________________________________________");
-        System.out.printf("Hello! I'm %s\n", name);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        //greeting
+        System.out.println("    ____________________________________________________________");
+        System.out.printf("    Hello! I'm %s\n", name);
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        //scan for input
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        //echo until user inputs bye
+        while(!command.toLowerCase().equals("bye")) {
+            echoText(command);
+            command = sc.nextLine();
+        }
+
+        //exit
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon wooo!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void echoText(String text) {
+        System.out.println("    ____________________________________________________________");
+        System.out.printf("    %s\n", text);
+        System.out.println("    ____________________________________________________________");
+
     }
 }
