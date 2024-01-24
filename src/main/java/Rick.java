86e7fc36bb409b833cfee5d8be8a5ad0fd98cf93
laney0808 <e0969207@u.nus.edu>
2024-01-24 23:57:15 +0800

    rename, greeting and exit

---
+++ src/main/java/Rick.java
@@ -0,0 +1,12 @@
+public class Rick {
+    public static void main(String[] args) {
+        String hello = "____________________________________________________________\n"+
+        "Hello! I'm Rick\n"+
+        "What can I do for you ?\n";
+
+        String exit = "____________________________________________________________\n"+
+                "Bye. Hope to see you again soon !\n"+
+                "____________________________________________________________";
+        System.out.println(hello + exit);
+    }
+}
