86958aec3628e8897d771b1f191258c941a1b44e
Darylgolden <darylgolden@gmail.com>
2024-01-24 15:26:13 +0800

    refactored logic into chatsession

--- src/main/java/ChatSession.java
+++ src/main/java/ChatSession.java
@@ -1,4 +1,20 @@
+import java.util.List;
+
 public class ChatSession {
-    
-    
+    public List<Task> taskList;
+
+    ChatSession() {
+        this.taskList = List.of();
+        this.initChat();
+    }
+
+    public void initChat() {
+        String message = "____________________________________________________________\n" +
+        "Hello! I'm GoldBot!\n" +
+        "What can I do for you?\n" +
+       "____________________________________________________________" +
+        "Bye. Hope to see you again soon!" +
+       "____________________________________________________________";
+        System.out.println(message);
+    }
 }
--- src/main/java/GoldBot.java
+++ src/main/java/GoldBot.java
@@ -1,11 +1,5 @@
 public class GoldBot {
     public static void main(String[] args) {
-        String message = "____________________________________________________________\n" +
-        "Hello! I'm GoldBot!\n" +
-        "What can I do for you?\n" +
-       "____________________________________________________________\n" +
-        "Bye. Hope to see you again soon!\n" +
-       "____________________________________________________________";
-        System.out.println(message);
+        ChatSession session = new ChatSession();
     }
 }
