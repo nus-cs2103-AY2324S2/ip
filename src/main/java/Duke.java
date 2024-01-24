9491e039af430622e088bb248557067584f15476
sdevih <e0960282@u.nus.edu>
2024-01-25 02:09:22 +0800

    Added level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -5,7 +7,17 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
+        //Chatbot greets user
         System.out.println("Hello! I'm Chatteroo\n" + "What can I do for you?\n");
+
+        //Chatbot echos user commands
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input + "\n");
+            input = sc.nextLine();
+        }
+        //Chatbot exits
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
