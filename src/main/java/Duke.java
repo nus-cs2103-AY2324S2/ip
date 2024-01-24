611105eacd8f927d6cbe2fc4c28545752100cffc
Zhi Wei <tanzhiwei7@gmail.com>
2024-01-24 22:02:58 +0800

    Level 2. Add, List, Changed the horizontal line to a variable so its easier to reuse.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,35 @@
+import java.util.ArrayList; // import the ArrayList class
 import java.util.Scanner;  // Import the Scanner class
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Homie");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        String line = "____________________________________________________________";
+        System.out.println(line + "\nHello! I'm Homie");
+        System.out.println("What can I do for you?\n" + line);
 
         Scanner scanner = new Scanner(System.in); // Create scanner
         String command = scanner.nextLine();  // Read user command
+        ArrayList<String> myList = new ArrayList<String>(); // Create an ArrayList object
+
+        int index = 1;
+
         while (!command.equals("bye")) {
-            System.out.println(command);  // Echo command
-            command = scanner.nextLine();
+            if (command.equals("list")) {
+                System.out.println(line);
+                for (String s : myList) {
+                    System.out.println(index + ". " + s);
+                    index++;
+                }
+                System.out.println(line + "\n");
+                index = 1; // Reset index to 1
+                command = scanner.nextLine(); // Read next command
+                continue;
+            }
+            myList.add(command); // add command to list
+            System.out.println(line + "\nadded: " + command + "\n" + line);  // Echo added
+            command = scanner.nextLine(); // Read next command
         }
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
 }
