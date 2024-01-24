18ba89a22f464776f46e48f669a8f5a14c5ffed2
Wongqingyee <qingyeewong@gmail.com>
2024-01-25 02:24:59 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,10 +8,14 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
 
-    public Scanner scanner;
+    private Scanner scanner;
+    private String[] tasks;
+    private int id;
 
     public Duke() {
         this.scanner = new Scanner(System.in);
+        this.tasks = new String[100];
+        this.id = 0;
     }
 
     public void run() {
@@ -20,17 +24,39 @@ public class Duke {
 
         while (true) {
             String input = scanner.nextLine();
-            System.out.println("  " + input);
 
-            if (input.equals("bye")) {
-                exit();
-                break;
+            switch (input) {
+                case "bye":
+                    exit();
+                    break;
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    addTasks(input);
+                    break;
             }
+        }
+
+
+    }
+
+    // add task to the list and print out the current task
+    public void addTasks(String task) {
+        tasks[id] = task;
+        id++;
+        System.out.println("added: " + task);
+    }
+
+    // list out all the tasks being added to the list
+    public void listTasks() {
+        for(int i = 0; i < id; i++) {
+            System.out.println((i + 1) + "." + tasks[i]);
         }
     }
 
     public void exit() {
-        System.out.println("  " + "Bye. Have a great day ahead!");
+        System.out.println("Goodbye. Have a great day ahead!");
     }
     public static void main(String[] args) {
         Duke duke = new Duke();
