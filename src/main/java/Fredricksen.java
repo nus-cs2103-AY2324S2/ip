7606a0b556e10f587755aa4e510f5efb26d2a6e2
Zack-Tay <tayzhongkai@gmail.com>
2024-01-25 00:27:08 +0800

    Add echo and bye commands to Fredricksen.java and completed Level-1

--- src/main/java/Fredricksen.java
+++ src/main/java/Fredricksen.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Fredricksen {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Fredricksen");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            System.out.println("");
+            String s = in.next();
+            if (s.toLowerCase().equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(s);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
