7b5a35cdbdf2848942272568a777b942f3f104cc
Yiyang <liuyiyang0929@gmail.com>
2024-01-25 02:32:50 +0800

    Updated error catching, exception classes, and tests

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> validCommands = new ArrayList<>(List.of("todo", "deadline", "event"));
     public static void echoText(String text) {
         System.out.println("    ____________________________________________________________");
         System.out.printf("      %s\n", text);
@@ -29,6 +32,81 @@ public class Duke {
         }
         System.out.println("    ____________________________________________________________");
     }
+
+    private static void markTask(String[] split, ArrayList<Task> list) throws InvalidCommandException {
+        if (split.length == 1) {
+            throw new InvalidCommandException("Wrong format! Please include the number that you want me to mark >:(");
+        } else {
+            int number = Integer.parseInt(split[1]);
+            if (number < 1 || number > list.size()) {
+                throw new InvalidTaskNumberException("Invalid number! Index does not exist >:((");
+            } else {
+                Task task = list.get(number - 1);
+                task.markDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("      Yippee! I have marked this task as done ;D");
+                System.out.printf("        %s\n", task.toString());
+                System.out.println("    ____________________________________________________________");
+            }
+        }
+    }
+
+    private static void unmarkTask(String[] split, ArrayList<Task> list) throws InvalidCommandException {
+        if (split.length == 1) {
+            throw new InvalidCommandException("Wrong format! Please include the number that you want me to unmark >:(");
+        } else {
+            int number = Integer.parseInt(split[1]);
+            if (number < 1 || number > list.size()) {
+                throw new InvalidTaskNumberException("Invalid number! Index does not exist >:((");
+            } else {
+                Task task = list.get(number - 1);
+                task.markNotDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("      Awww...I have marked this task as not done yet :(");
+                System.out.printf("        %s\n", task.toString());
+                System.out.println("    ____________________________________________________________");
+            }
+        }
+    }
+
+    private static void createTask(String[] split, ArrayList<Task> list) throws InvalidCommandException {
+        Task newTask;
+        if (!validCommands.contains(split[0].toLowerCase())) {
+            throw new InvalidCommandException("I don't quite understand that command :'( Sorry...");
+        }
+        if (split.length == 1) {
+            throw new InvalidCommandException("You need to tell me the task name >:0");
+        }
+        if (split[0].toLowerCase().equals("todo")) {
+            newTask = new ToDo(split[1].trim());
+        } else if (split[0].toLowerCase().equals("deadline")) {
+            String[] deadlineSplit = split[1].trim().split("/by");
+            if (deadlineSplit.length == 1) {
+                throw new InvalidCommandException(
+                        "Invalid format >:( Make sure you used '/by' to indicate the deadline!");
+            }
+            newTask = new Deadline(deadlineSplit[0].trim(), deadlineSplit[1].trim());
+        } else {
+            String[] fromSplit = split[1].split("/from");
+            if (fromSplit.length == 1) {
+                throw new InvalidCommandException(
+                        "Invalid format >:( Make sure you used '/from' to indicate event start time!"
+                );
+            }
+            String eventName = fromSplit[0].trim();
+
+            String[] toSplit = fromSplit[1].split("/to");
+            if (toSplit.length == 1) {
+                throw new InvalidCommandException(
+                        "Invalid format >:( Make sure you used '/to' to indicate event end time!"
+                );
+            }
+            String from = toSplit[0].trim();
+            String to = toSplit[1].trim();
+            newTask = new Event(eventName, from, to);
+        }
+        addList(newTask, list);
+    }
     public static void main(String[] args) {
         String name = "Yippee";
         ArrayList<Task> list = new ArrayList<>();
@@ -47,68 +125,32 @@ public class Duke {
         while(!command.toLowerCase().equals("bye")) {
             // split command by spaces
             String[] split = command.split("\\s+", 2);
-
             if (split[0].toLowerCase().equals("list")) {
                 printList(list);
             } else if (split[0].toLowerCase().equals("mark")) {
-                if (split.length == 1) {
+                try {
+                    markTask(split, list);
+                } catch(InvalidCommandException e) {
                     System.out.println("    ____________________________________________________________");
-                    System.out.println("      Wrong format! Please include the number that you want me to mark >:(");
+                    System.out.printf("      %s\n", e.getMessage());
                     System.out.println("    ____________________________________________________________");
-                } else {
-                    int number = Integer.parseInt(split[1]);
-                    if (number < 1 || number > list.size()) {
-                        System.out.println("    ____________________________________________________________");
-                        System.out.println("      Invalid number! Index does not exist >:((");
-                        System.out.println("    ____________________________________________________________");
-                    } else {
-                        Task task = list.get(number - 1);
-                        task.markDone();
-                        System.out.println("    ____________________________________________________________");
-                        System.out.println("      Yippee! I have marked this task as done ;D");
-                        System.out.printf("        %s\n", task.toString());
-                        System.out.println("    ____________________________________________________________");
-                    }
                 }
             } else if (split[0].toLowerCase().equals("unmark")) {
-                if (split.length == 1) {
+                try {
+                    unmarkTask(split, list);
+                } catch(InvalidCommandException e) {
                     System.out.println("    ____________________________________________________________");
-                    System.out.println("      Wrong format! Please include the number that you want me to unmark >:(");
+                    System.out.printf("      %s\n", e.getMessage());
                     System.out.println("    ____________________________________________________________");
-                } else {
-                    int number = Integer.parseInt(split[1]);
-                    if (number < 1 || number > list.size()) {
-                        System.out.println("    ____________________________________________________________");
-                        System.out.println("      Invalid number! Index does not exist >:((");
-                        System.out.println("    ____________________________________________________________");
-                    } else {
-                        Task task = list.get(number - 1);
-                        task.markNotDone();
-                        System.out.println("    ____________________________________________________________");
-                        System.out.println("      Awww...I have marked this task as not done yet :(");
-                        System.out.printf("        %s\n", task.toString());
-                        System.out.println("    ____________________________________________________________");
-                    }
                 }
             } else {
-                Task newTask;
-                if (split[0].toLowerCase().equals("todo")) {
-                    newTask = new ToDo(split[1].trim());
-                } else if (split[0].toLowerCase().equals("deadline")) {
-                    String[] deadlineSplit = split[1].trim().split("/by");
-                    newTask = new Deadline(deadlineSplit[0].trim(), deadlineSplit[1].trim());
-                } else if (split[0].toLowerCase().equals("event")) {
-                    String[] fromSplit = split[1].split("/from");
-                    String eventName = fromSplit[0].trim();
-
-                    String[] toSplit = fromSplit[1].split("/to");
-                    String from = toSplit[0].trim();
-                    String to = toSplit[1].trim();
-                    newTask = new Event(eventName, from, to);
-                } else {
-                    newTask = new Task(split[1]);
+                try {
+                    createTask(split, list);
+                } catch(InvalidCommandException e) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.printf("      %s\n", e.getMessage());
+                    System.out.println("    ____________________________________________________________");
                 }
-                addList(newTask, list);
             }
             command = sc.nextLine();
         }
---
+++ src/main/java/InvalidTaskNumberException.java
@@ -0,0 +1,5 @@
+public class InvalidTaskNumberException extends InvalidCommandException {
+    public InvalidTaskNumberException(String message) {
+        super(message);
+    }
+}
