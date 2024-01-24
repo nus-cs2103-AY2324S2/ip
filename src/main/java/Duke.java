ceec1c0777eb2ab2166a2a933e8a94418d0ed34c
triciiaaa <angjeanee@gmail.com>
2024-01-24 17:44:04 +0800

    Level-3 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import Task.Task;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,36 +10,59 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
+        String divider = "        ------------------------------------------------------------";
 
-        System.out.println("        ------------------------------------------------------------");
+        System.out.println(divider);
         System.out.println("        Hello! I'm Chronos.");
         System.out.println("        What can I do for you?");
-        System.out.println("        ------------------------------------------------------------");
+        System.out.println(divider);
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         int noOfTasks = 0;
 
         while (true) {
             String command = sc.nextLine();
+            String[] token = command.split(" ");
+            Task task = new Task(command);
 
-            if (command.equals("bye")) {
-                System.out.println("        ------------------------------------------------------------");
+            if (token[0].equals("bye")) {
+                System.out.println(divider);
                 System.out.println("        Bye. Hope to see you again soon!");
-                System.out.println("        ------------------------------------------------------------");
+                System.out.println(divider);
                 break;
-            } else if (command.equals("list")) {
-                System.out.println("        ------------------------------------------------------------");
+            } else if (token[0].equals("list")) {
+                System.out.println(divider);
+                System.out.println("        Here are the tasks in your list:");
                 for (int i = 1; i < noOfTasks + 1; i++) {
-                    System.out.println("        " + i + ". " + tasks.get(i - 1));
+                    Task currentTask = tasks.get(i - 1);
+                    System.out.println("        " + i + ". [" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
                 }
-                System.out.println("        ------------------------------------------------------------");
+                System.out.println(divider);
+            } else if (token[0].equals("mark")) {
+                int selectedTaskNumber = Integer.parseInt(token[1]);
+                Task selectedTask = tasks.get(selectedTaskNumber - 1);
+                selectedTask.setMarked();
+                tasks.set(selectedTaskNumber - 1, selectedTask);
+                System.out.println(divider);
+                System.out.println("        Nice! I've marked this task as done:");
+                System.out.println("           [" + selectedTask.getStatusIcon() + "] " + selectedTask.getDescription());
+                System.out.println(divider);
+            } else if (token[0].equals("unmark")) {
+                int selectedTaskNumber = Integer.parseInt(token[1]);
+                Task selectedTask = tasks.get(selectedTaskNumber - 1);
+                selectedTask.setUnmarked();
+                tasks.set(selectedTaskNumber - 1, selectedTask);
+                System.out.println(divider);
+                System.out.println("        OK, I've marked this task as not done yet:");
+                System.out.println("           [" + selectedTask.getStatusIcon() + "] " + selectedTask.getDescription());
+                System.out.println(divider);
             } else {
-                System.out.println("        ------------------------------------------------------------");
-                System.out.println("        added: " + command);
-                System.out.println("        ------------------------------------------------------------");
-                tasks.add(command);
+                tasks.add(task);
                 noOfTasks++;
+                System.out.println(divider);
+                System.out.println("        added: " + task.getDescription());
+                System.out.println(divider);
             }
         }
     }
