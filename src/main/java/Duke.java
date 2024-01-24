87c8f89ead9cdfeab5ef640fab3eddba4920a613
Ma-Yirui <saltiestwonton@gmail.com>
2024-01-24 23:11:20 +0800

    level0: initial skeleton code with greeting and exiting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.io.PrintWriter;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static final PrintWriter pw = new PrintWriter(System.out);
+    public static void main(String[] args){
+        greet();
+        exit();
+        pw.flush();
     }
+
+    private static void greet() {
+        pw.print("_______________________________________\n"
+                + "Hello, Notnow here!\n"
+                + "What can I do for you?\n"
+                + "_______________________________________\n");
+    }
+
+    private static void exit() {
+        pw.print("_______________________________________\n"
+                + "Bye. Hope to see u again soon!\n"
+                + "_______________________________________\n");
+    }
+
 }
