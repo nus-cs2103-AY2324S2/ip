78be716e221792128cad184258c6c50dd7b59f49
fy17ohhh <hester03sfy@gmail.com>
2024-01-24 15:23:21 +0800

    Add support for tracking three types of tasks

--- src/main/java/jade/Duke.java
+++ src/main/java/jade/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
                     + "\t|\\|  | | ___ | | |__| | | |____  \n"
                     + "\t \\___| |_| |_| |_____/  \\_|____\\ \n";
 
-        System.out.printf("%s%s\tHello! I'm Jade\n\that can I do for you?\n%s", logo, line, line);
+        System.out.printf("%s%s\tHello! I'm Jade\n\twhat can I do for you?\n%s", logo, line, line);
         Scanner scanner = new Scanner(System.in);
         while(!exitProg) {
             String command = scanner.nextLine();
@@ -27,8 +27,29 @@ public class Duke {
     public static void echo(String command) {
         String[] splitCommand = command.split(" ");
         switch(splitCommand[0]) {
+            case "todo":
+                String todoDescription = String.join(" ", Arrays.copyOfRange(splitCommand, 1, splitCommand.length));
+                Task todoT = new Todo(todoDescription);
+                userList.add(todoT);
+                System.out.printf("%s\tGot it. I've added this task:\n\t %s\n\tNow you have %d task(s) in the list.\n%s", line, todoT , userList.size(), line);
+                break;
+            case "deadline":
+                String deadlineDescription =  String.join(" ", Arrays.copyOfRange(splitCommand, 1, Arrays.asList(splitCommand).indexOf("/by")));
+                String deadlineDate = String.join(" ", Arrays.copyOfRange(splitCommand, Arrays.asList(splitCommand).indexOf("/by") + 1, splitCommand.length));
+                Task deadlineT = new Deadline(deadlineDescription, deadlineDate);
+                userList.add(deadlineT);
+                System.out.printf("%s\tGot it. I've added this task:\n\t %s\n\tNow you have %d task(s) in the list.\n%s", line, deadlineT , userList.size(), line);
+                break;
+            case "event":
+                String eventDescription =  String.join(" ", Arrays.copyOfRange(splitCommand, 1, Arrays.asList(splitCommand).indexOf("/from")));
+                String startDate = String.join(" ", Arrays.copyOfRange(splitCommand, Arrays.asList(splitCommand).indexOf("/from") + 1, Arrays.asList(splitCommand).indexOf("/to")));
+                String endDate = String.join(" ", Arrays.copyOfRange(splitCommand, Arrays.asList(splitCommand).indexOf("/to") + 1, splitCommand.length));
+                Task eventT = new Event(eventDescription, startDate, endDate);
+                userList.add(eventT);
+                System.out.printf("%s\tGot it. I've added this task:\n\t %s\n\tNow you have %d task(s) in the list.\n%s", line, eventT , userList.size(), line);
+                break;
             case "list":
-                System.out.println(line + "\tHere are the tasks in your list:");
+                System.out.println(line + "\tHere are the task(s) in your list:");
                 for (int i = 1; i <= userList.size(); i++) {
                     System.out.printf("\t%d. %s\n", i, userList.get(i-1));
                 }
@@ -49,8 +70,6 @@ public class Duke {
                 exitProg = true;
                 break;
             default:
-                userList.add(new Task(command));
-                System.out.printf("%s\tadded: %s\n%s", line, command, line);
                 break;
         }
 
