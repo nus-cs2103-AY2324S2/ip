fc160fe9751acdd084550f69dc4e097153d03887
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-01-24 21:30:00 +0800

    Level-1
    1) renamed file back to Duke.
    2) Added echo.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,33 @@
-public class someBOTy {
-    public static void main(String[] args) {
-        String greet = "Wassup! I'm someBOTy.\n"
-                     + "What are you here for?\n"
-                     + "============================================";
-        System.out.println(greet);
+import java.util.Scanner;
 
-        String bye = "Aight. Imma head out.\n"
-                   + "============================================";
-        System.out.println(bye);
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String line = "============================================";
+
+        String greet = "Wassup! I'm someBOTy.\n"
+                     + "What are you here for?";
+
+        Scanner scanner = new Scanner(System.in);
+        String input = greet;
+
+        System.out.println(logo + line);
+
+        while (!input.equals("bye")) {
+            System.out.println(">>> " + input + "\n" + line);
+            input = scanner.nextLine();
+        }
+
+        scanner.close();
+
+        String exit_message = "Aight. Imma head out.\n"
+                   + line;
+        System.out.println(exit_message);
     }
+
 }
