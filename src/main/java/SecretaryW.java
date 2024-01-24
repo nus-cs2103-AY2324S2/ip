d8f178a4c34743f0124196533a7792d6cb867806
tayziyi <ziyitay1@gmail.com>
2024-01-25 02:43:41 +0800

    A-TextUiTesting

--- src/main/java/SecretaryW.java
+++ src/main/java/SecretaryW.java
@@ -17,85 +17,83 @@ public class SecretaryW {
 
         // Read user input in the loop
         while (true) {
-            String userInput = scanner.nextLine();
+            try {
+                String userInput = scanner.nextLine();
 
-            if (userInput.equals("bye")){
-                break; // Exits loop
-            } else if (userInput.equals("list")) {
-                System.out.println(line);
-                if (count == 0) {
-                    System.out.println("No tasks available");
+                if (userInput.equals("bye")) {
+                    break; // Exits loop
+                } else if (userInput.equals("list")) {
                     System.out.println(line);
-                } else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < count; i++) {
-                        Task currTask = tasklist[i];
-                        System.out.println(" " + (i + 1) + ". " + tasklist[i]);
+                    if (count == 0) {
+                        System.out.println("No tasks available");
+                        System.out.println(line);
+                    } else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < count; i++) {
+                            Task currTask = tasklist[i];
+                            System.out.println(" " + (i + 1) + ". " + tasklist[i]);
+                        }
+                        System.out.println(line);
                     }
-                    System.out.println(line);
-                }
-            } else if (userInput.startsWith("mark")) {
-                int index = Integer.parseInt(userInput.substring(5).trim()) - 1;
-                // check for bounds
-                if (index >= 0 && index < count) {
-                    tasklist[index].markAsDone();
-                    System.out.println(line + " Nice! I've marked this task as done:");
-                    System.out.println("  " + tasklist[index].getStatusIcon() + " " + tasklist[index].getDescription() + "\n" + line);
-                } else {
-                    System.out.println(line + " Index is out of bounds!\n" + line);
-                }
-            } else if (userInput.startsWith("unmark")) {
-                int index = Integer.parseInt(userInput.substring(7).trim()) - 1;
-                // check for bounds
-                if (index >= 0 && index < count) {
-                    tasklist[index].markAsUndone();
-                    System.out.println(line + " OK, I've marked this task as not done yet");
-                    System.out.println("  " + tasklist[index].getStatusIcon() + " " + tasklist[index].getDescription() + "\n" + line);
-                } else {
-                    System.out.println(line + " Index is out of bounds!\n" + line);
-                }
-            } else if (userInput.startsWith("todo")) {
-                // add to do task
-                String description = userInput.substring(5).trim();
-                tasklist[count] = new Todo(description);
-                count++;
-                printTaskAdded(tasklist, count);
-            } else if (userInput.startsWith("deadline")) {
-                // Add a deadline task
-                String[] parts = userInput.substring(8).split("/by");
-                if (parts.length == 2) {
+                } else if (userInput.startsWith("mark")) {
+                    int index = Integer.parseInt(userInput.substring(5).trim()) - 1;
+                    // check for bounds
+                    if (index >= 0 && index < count) {
+                        tasklist[index].markAsDone();
+                        System.out.println(line + " Nice! I've marked this task as done:");
+                        System.out.println("  " + tasklist[index].getStatusIcon() + " " + tasklist[index].getDescription() + "\n" + line);
+                    } else {
+                        System.out.println(line + " Index is out of bounds!\n" + line);
+                    }
+                } else if (userInput.startsWith("unmark")) {
+                    int index = Integer.parseInt(userInput.substring(7).trim()) - 1;
+                    // check for bounds
+                    if (index >= 0 && index < count) {
+                        tasklist[index].markAsUndone();
+                        System.out.println(line + " OK, I've marked this task as not done yet");
+                        System.out.println("  " + tasklist[index].getStatusIcon() + " " + tasklist[index].getDescription() + "\n" + line);
+                    } else {
+                        System.out.println(line + " Index is out of bounds!\n" + line);
+                    }
+                } else if (userInput.startsWith("todo")) {
+                    // add to do task
+                    String description = userInput.substring(4).trim();
+                    checkTodo(description);
+                    tasklist[count] = new Todo(description);
+                    count++;
+                    printTaskAdded(tasklist, count);
+                } else if (userInput.startsWith("deadline")) {
+                    // Add a deadline task
+                    String[] parts = userInput.substring(8).split("/by");
+                    checkParts(parts, 2);
                     String description = parts[0].trim();
                     String by = parts[1].trim();
+                    checkDeadline(by);
                     tasklist[count] = new Deadline(description, by);
                     count++;
                     printTaskAdded(tasklist, count);
-                } else {
-                    printInvalid();
-                }
-            } else if (userInput.startsWith("event")) {
-                // Add an event task
-                String[] parts = userInput.substring(5).split("/from"); // first split
-                if (parts.length == 2) {
+                } else if (userInput.startsWith("event")) {
+                    // Add an event task
+                    String[] parts = userInput.substring(5).split("/from"); // first split
+                    checkParts(parts, 2);
                     String description = parts[0].trim();
                     String[] time = parts[1].split("/to"); // second split
-                    if (time.length == 2) {
-                        String from = time[0].trim();
-                        String to = time[1].trim();
-                        tasklist[count] = new Event(description, from, to);
-                        count++;
-                        printTaskAdded(tasklist, count);
-                    } else {
-                        printInvalid();
-                    }
+                    checkParts(time, 2);
+                    String from = time[0].trim();
+                    String to = time[1].trim();
+                    checkEvent(from, to);
+                    tasklist[count] = new Event(description, from, to);
+                    count++;
+                    printTaskAdded(tasklist, count);
+
                 } else {
-                    printInvalid();
+                    // Invalid command
+                    throw new WException("I'm sorry, but I don't know what that means :-(");
                 }
-            } else {
-                // Invalid command
-                    printInvalid();
+            } catch (WException e) {
+                System.out.println(line + "OOPS!!! " + e.getMessage() + "\n" + line);
             }
         }
-
         // Farewell
         System.out.println(line + farewell + line);
         scanner.close();
@@ -110,4 +108,34 @@ public class SecretaryW {
     }
 
     private static String line = "-----------------------------------------------------------------\n";
+
+    static class WException extends Exception {
+        public WException(String msg) {
+            super(msg);
+        }
+    }
+
+    private static void checkTodo(String description) throws WException {
+        if (description.isEmpty()) {
+            throw new WException("The description of a todo cannot be empty");
+        }
+    }
+
+    private static void checkParts(String[] parts, int length) throws WException {
+        if (parts.length != length) {
+            throw new WException("Wrong format. Please retype");
+        }
+    }
+
+    private static void checkDeadline(String by) throws WException {
+        if (by.isEmpty()) {
+            throw new WException("The deadline cannot be empty");
+        }
+    }
+
+    private static void checkEvent(String from, String to) throws WException {
+        if (from.isEmpty() || to.isEmpty()) {
+            throw new WException("The event from and to times cannot be empty");
+        }
+    }
 }
