b23d3105e7bd14f1c8955cda3ee2b05f16bdf97d
Zhi Wei <tanzhiwei7@gmail.com>
2024-01-25 00:55:31 +0800

    Level 3. Mark As Done Added new Task class, Overriden the toString to display description of task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,15 +9,18 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in); // Create scanner
         String command = scanner.nextLine();  // Read user command
-        ArrayList<String> myList = new ArrayList<String>(); // Create an ArrayList object
+        ArrayList<Task> myList = new ArrayList<>(); // Create an ArrayList object
 
         int index = 1;
+        String[] tempArr;
+        Task currTask;
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 System.out.println(line);
-                for (String s : myList) {
-                    System.out.println(index + ". " + s);
+                System.out.println("Here are the tasks in your list:");
+                for (Task s : myList) {
+                    System.out.println(index + ".[" + s.getStatusIcon() + "] " + s);
                     index++;
                 }
                 System.out.println(line + "\n");
@@ -25,7 +28,33 @@ public class Duke {
                 command = scanner.nextLine(); // Read next command
                 continue;
             }
-            myList.add(command); // add command to list
+
+            if (command.contains("mark")) {
+                tempArr = command.split(" ");
+                currTask = myList.get(Integer.parseInt(tempArr[1]) - 1);
+                switch (tempArr[0]) {
+                    case ("mark"):
+                        System.out.println(line);
+                        currTask.setDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("   [" + currTask.getStatusIcon() + "] " + currTask);
+                        System.out.println(line);
+                        break;
+                    case ("unmark"):
+                        System.out.println(line);
+                        currTask.setNotDone();
+                        System.out.println("Ok, I've marked this task as not done yet:");
+                        System.out.println("   [" + currTask.getStatusIcon() + "] " + currTask);
+                        System.out.println(line);
+                        break;
+                    default:
+                        System.out.println("Wrong Command");
+                }
+                command = scanner.nextLine(); // Read next command
+                continue;
+            }
+
+            myList.add(new Task(command)); // add command to list
             System.out.println(line + "\nadded: " + command + "\n" + line);  // Echo added
             command = scanner.nextLine(); // Read next command
         }
