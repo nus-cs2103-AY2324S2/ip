f2ebe6063fadb66ecb5c99efd897dcbd38784fe3
Alteqa <e0969816@u.nus.edu>
2024-01-25 01:46:38 +0800

    added add, list

--- src/main/java/hirwan.java
+++ src/main/java/hirwan.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class hirwan {
     public static void main(String[] args) {
@@ -7,13 +9,22 @@ public class hirwan {
                 + "_________________________________\n";
 
         System.out.println("Hello! " + logo);
+        int count = 1;
+        List<String> List = new ArrayList<>();
         while(true) {
             Scanner scanner = new Scanner(System.in);
             String text = scanner.nextLine();
             if (text.equals("bye")) {
                 break;
+            } else if(text.equals("list")) {
+                for (String element : List) {
+                    System.out.println(element);
+                }
+            } else {
+                System.out.println("added: " + text);
+                List.add(count + ". " + text);
+                count++;
             }
-            System.out.println(text);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
