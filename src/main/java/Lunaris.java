88eae7a4c90b0b622c2f7884b876dc65d755be27
Derekljh <E0960128@u.nus.edu>
2024-01-24 10:33:32 +0800

    Changed style of speech and added ability to echo

--- src/main/java/Lunaris.java
+++ src/main/java/Lunaris.java
@@ -1,8 +1,35 @@
+import java.util.*;
 public class Lunaris {
     public static void main(String[] args) {
         String name = "Lunaris";
-        System.out.println("Hello! I'm " + name + "\n"
-            + "What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        String indentation = "  ";
+        String indentedLine = "  _________________________________________________________";
+        // Just for convenience of copy paste.
+        // System.out.println(indentedLine)
+
+        // Print out greeting message
+        System.out.println(indentedLine);
+        System.out.println(indentation + "Hey! I'm " + name + "\n"
+            + "Is there anything I can do for you?");
+        System.out.println(indentedLine);
+
+        // Scan for Input and echo it.
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println(indentedLine);
+                System.out.println(indentation + input);
+                System.out.println(indentedLine);
+            }
+            else {
+                break;
+            }
+        }
+
+        // Print out goodbye message
+        System.out.println(indentedLine);
+        System.out.println("Leaving so soon? Alright, have a great day ahead!");
+        System.out.println(indentedLine);
     }
 }
