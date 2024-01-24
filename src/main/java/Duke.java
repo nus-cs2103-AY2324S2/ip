1cfd9144677fd5655c05e0a2709ae3a3ec781c2e
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2024-01-24 21:29:06 +0800

    add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +10,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Hao Wen\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        //System.out.println("Bye. Hope to see you again soon!");
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter pw = new PrintWriter(System.out)) {
+            String userInput;
+            while ((userInput = br.readLine()) != null) {
+                if (userInput.equalsIgnoreCase("bye")) {
+                    break;
+                }
+                pw.println(userInput);
+                pw.flush();
+            }
+            pw.println("Bye. Hope to see you again soon!");
+
+        } catch (IOException e) {
+            System.err.println("Error");
+            //e.printStackTrace();
+        }
     }
 }
