61f38fc3f1abd1d9cf0f22934adf5a8568cfd490
rizkidelta <rizkibayuaji83@gmail.com>
2024-01-25 00:51:36 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,66 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        class Task {
+            protected String description;
+            protected boolean isDone;
+
+            public Task(String description) {
+                this.description = description;
+                this.isDone = false;
+            }
+
+            public void mark() {
+                isDone = true;
+            }
+
+            public void unMark() {
+                isDone = false;
+            }
+
+            public String getStatusIcon() {
+                return (isDone ? "[X]" : "[ ]");
+            }
+
+            @Override
+            public String toString() {
+                return getStatusIcon() + " " + description;
+            }
+        }
+
         String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Tom \nWhat can I do for you?\n");
         System.out.println(line);
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskcount = 0;
 
 
         while (true){
             String input = scanner.nextLine();
-            if ("bye".equals(input)){
+            String[] splitInput = input.split(" ", 2);
+
+            if ("bye".equals(splitInput[0])){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if ("list".equals(input)) {
+            } else if ("list".equals(splitInput[0])) {
                 for(int i=0; i< taskcount;i++){
                     System.out.println((i+1)+". " + tasks[i]);
                 }
+
+            } else if("mark".equals(splitInput[0])){
+                int index = Integer.parseInt(splitInput[1])-1;
+                tasks[index].mark();
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+
+            } else if ("unmark".equals(splitInput[0])) {
+                int index = Integer.parseInt(splitInput[1])-1;
+                tasks[index].unMark();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+
             } else{
-                tasks[taskcount] = input;
+                tasks[taskcount] = new Task(input);
                 System.out.println("added: "+input);
                 taskcount++;
             }
@@ -29,7 +69,6 @@ public class Duke {
 
         }
 
-
-
     }
+
 }
