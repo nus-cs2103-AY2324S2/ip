4d0dd49b6283732f0c9559b2fc1cee78ba555262
eliotlee68 <eliotlee68@gmail.com>
2024-01-24 22:28:27 +0800

    Implemented level 1.

--- src/main/java/AaronBot.java
+++ src/main/java/AaronBot.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class AaronBot {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm AaronBot \nWhat can I do for you?\nBye. Hope to see you again soon!");
+        boolean notBye = true;
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Hello, I am AaronBot, please talk to me I love my students very much :)");
+        while (notBye) {
+            notBye = chat(inputScanner.nextLine());
+        }
+        inputScanner.close();
+    }
+
+    private static boolean chat(String echoString) {
+        if (echoString.equalsIgnoreCase("what is your favorite module")) {
+            System.out.println("CS1231S :)");
+            return true;
+        } else if (echoString.equalsIgnoreCase("bye")) {
+            System.out.println("Ok byebye, HAND.");
+            return false;
+        } else {
+            System.out.println(echoString + " student.");
+            return true;
+        }
     }
 }
