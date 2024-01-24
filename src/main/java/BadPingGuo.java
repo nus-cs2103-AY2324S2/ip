d98b184d3257e5ed622648dc9bdb94731117ce06
Guo Yuhao <guoyuhao4@gmail.com>
2024-01-24 13:08:45 +0800

    Added Echoing function

--- src/main/java/BadPingGuo.java
+++ src/main/java/BadPingGuo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BadPingGuo {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,15 @@ public class BadPingGuo {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Welcome to White Space!");
         System.out.println("Waiting for something to happen?");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String response = sc.nextLine();
+            if (response.equalsIgnoreCase("bye")) break;
+            System.out.println(response);
+        }
+
         System.out.println("--------------------------------");
         System.out.println("Everything is going to be okay.");
     }
