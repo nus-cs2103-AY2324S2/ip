ae7633f9855f7a06ea81d46e5b6b17545206d86f
Jules <julia.poo.poo.poo@gmail.com>
2024-01-24 21:04:53 +0800

    Feat: Completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,42 +1,73 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
+
+class DukeContext {
+
+  public String name;
+  public Scanner scanner;
+  public ArrayList<String> stored_messages;
+
+  DukeContext(String name) {
+    this.name = name;
+    this.scanner = new Scanner(System.in);
+    this.stored_messages = new ArrayList<>();
+  }
+}
 
 public class Duke {
 
-  private static final String NAME = ">uwu<";
+  private static final String MESSAGE_DELINEATOR =
+    "___________________________";
+  private static final String GREET_FORMAT =
+    "Hello! I'm %s\n" + "What can I do for you?\n";
+  private static final String BYE_MESSAGE = "Bye. Hope to see you again soon!";
 
-  private static void message_end() {
-    System.out.println("___________________________");
+  private static void input_prompt() {
+    System.out.printf(">> ");
   }
 
-  private static void greet() {
-    System.out.printf(
-      "Hello! I'm %s\n" + "What can I do for you?\n",
-      Duke.NAME
-    );
-    Duke.message_end();
+  private static void message_start() {
+    System.out.println(MESSAGE_DELINEATOR);
+  }
+
+  private static void greet(DukeContext ctx) {
+    System.out.printf(GREET_FORMAT, ctx.name);
   }
 
   private static void bye() {
-    System.out.println("Bye. Hope to see you again soon!");
-    Duke.message_end();
+    System.out.println(BYE_MESSAGE);
   }
 
-  private static boolean handle_command(Scanner scanner) {
-    String command = scanner.nextLine();
+  private static boolean handle_command(DukeContext ctx) {
+    String command = ctx.scanner.nextLine();
+
+    message_start();
     switch (command) {
       case "end":
+        bye();
         return false;
+      case "list":
+        {
+          ListIterator<String> iter = ctx.stored_messages.listIterator();
+          while (iter.hasNext()) {
+            System.out.printf("%d. %s\n", iter.nextIndex(), iter.next());
+          }
+          break;
+        }
       default:
-        System.out.println(command);
-        return true;
+        ctx.stored_messages.add(command);
+        System.out.printf("added: %s\n", command);
     }
+    return true;
   }
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    DukeContext ctx = new DukeContext(">uwu<");
 
-    Duke.greet();
-    while (Duke.handle_command(scanner)) {}
-    Duke.bye();
+    greet(ctx);
+    do {
+      input_prompt();
+    } while (handle_command(ctx));
   }
 }
