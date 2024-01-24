444b8fadb75f3f19c0f30257d3cf9c00ff2a11b2
jamessinmaojun <jamessinmaojun@gmail.com>
2024-01-24 16:31:17 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String divider = "--------------------------------------------------";
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(divider);
         System.out.println("Hello! I'm TheGiantPeach\nWhat can I do for you?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println(divider);
+
+            if (Objects.equals(command, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(divider);
+        }
+
         System.out.println(divider);
     }
 }
