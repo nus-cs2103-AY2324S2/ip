68be1beaa9b5968ff894570b3f5f5a829ef102ed
Joshy837 <joshuayipsujun@gmail.com>
2024-01-25 01:52:56 +0800

    Mark as Done

--- src/main/java/Joshy.java
+++ src/main/java/Joshy.java
@@ -4,34 +4,65 @@ import java.util.Scanner;
 public class Joshy {
     public static void main(String[] args) {
 
-        String[] listText = new String[100];
+        // instantiate new tasklist
+        Task[] taskList = new Task[100];
         int counter = 0;
 
+        // Introduction
         System.out.println("   ______________________________________________");
         System.out.println("   Hello! I'm Joshy");
         System.out.println("   What can I do for you?");
         System.out.println("   ______________________________________________");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        // Read the text entered by user line by line
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        // runs the program until user inputs "bye"
         while (!Objects.equals(input, "bye")) {
             System.out.println("   ______________________________________________");
 
-            if (Objects.equals(input, "list")) {
+            // delimited using " "
+            String[] tokens = input.split(" ");
+
+            // mark task
+            if (Objects.equals(tokens[0], "mark")) {
+                int item = Integer.parseInt(tokens[1]) - 1;
+                taskList[item].markTask();
+
+                System.out.println("   Nice! I've marked this task as done:");
+                System.out.println("      " + taskList[item].getStatus());
+            }
+
+            // unmark task
+            else if (Objects.equals(tokens[0], "unmark")) {
+                int item = Integer.parseInt(tokens[1]) - 1;
+                taskList[item].unmarkTask();
+
+                System.out.println("   OK, I've marked this task as not done yet:");
+                System.out.println("      " + taskList[item].getStatus());
+            }
+
+            // list out items
+            else if (Objects.equals(input, "list")) {
                 for (int i = 0; i < counter; i++) {
-                    System.out.println("   " + (i + 1) + ". " + listText[i]);
+                    System.out.println("   " + (i + 1) + ". " + taskList[i].getStatus());
                 }
-            } else {
-                listText[counter] = input;
+            }
+
+            // normal inputs
+            else {
+                taskList[counter] = new Task(input);
                 counter++;
                 System.out.println("   added: " + input);
             }
 
             System.out.println("   ______________________________________________");
 
-            input = scanner.nextLine();
+            input = sc.nextLine();
         }
 
+        // Bye message
         System.out.println("   ______________________________________________");
         System.out.println("   Bye. Hope to see you again soon!");
         System.out.println("   ______________________________________________");
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+  protected String description;
+  protected boolean isDone;
+
+  // instantiate a new Task Object
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+  }
+
+  // gets Status icon for the Task itself
+  public String getStatusIcon() {
+    return (this.isDone ? "X" : " ");
+  }
+
+  // get status for task
+  public String getStatus() {
+    return "[" + getStatusIcon() + "] " + this.description;
+  }
+
+  // mark task
+  public void markTask() {
+    this.isDone = true;
+  }
+
+  // unmark task
+  public void unmarkTask() {
+    this.isDone = false;
+  }
+}
