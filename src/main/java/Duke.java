061cd41b9683def08a01f0400824e95a789b38b4
shavonneg <e0967851@u.nus.edu>
2024-01-24 18:18:48 +0800

    Changed to echo user input unless input is bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //SAY HI
         String name = "____________________________________________________________ \n"
                 + "Hello! I'm RATZCHAT \n"
-                + "How can I help you today? \n"
-                + "____________________________________________________________ \n"
-                + "BYEBYE. Thank you for using RATZCHAT \n"
-                + "____________________________________________________________ \n";
+                + "How can I help you today?";
         System.out.println(name);
+        printLine();
+
+        //RESPONSIVE
+        while(true) {
+            String input = scanner.nextLine();
+            printLine();
+
+            if("bye".equalsIgnoreCase(input)) {
+                System.out.println("BYEBYE. Thank you for using RATZCHAT!");
+                printLine();
+                break;
+            }
+
+
+            System.out.println(input);
+            printLine();
+        }
+
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
