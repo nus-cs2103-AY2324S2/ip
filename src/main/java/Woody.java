061e2d4762e209049e8913c21745c2cf06070ca0
ryanlimdx <ryanlimdx@gmail.com>
2024-01-24 11:27:32 +0800

    added personality

--- src/main/java/Woody.java
+++ src/main/java/Woody.java
@@ -3,7 +3,14 @@ import java.util.Random;
 public class Woody {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        String[] catchphrases = {};
+        String[] catchphrases = {
+                "Yee-haw!",
+                "So long, partner.",
+                "To infinity and beyond!",
+                "Reach for the sky!"
+        };
+        Random random = new Random();
+
         String greeting =
                 line +
                 " Hello! I'm Woody\n" +
@@ -13,16 +20,18 @@ public class Woody {
         System.out.print(greeting);
 
         Scanner input = new Scanner(System.in);
-        String currInput = input.nextLine() + "\n";
+        String currInput = input.nextLine();
 
         String[] terminateKeywords = {"bye", "BYE", "Bye"};
         List<String> exitProgramme = Arrays.asList(terminateKeywords);
 
         while (!exitProgramme.contains(currInput)) {
-            System.out.println(currInput + line);
+            System.out.println(currInput  + "\n" + line);
             currInput = input.nextLine();
         }
 
-        System.out.println("Bye! \n" + line);
+        System.out.println(
+                "Bye! " + catchphrases[random.nextInt(catchphrases.length)] + "\n" +
+                        line);
     }
 }
