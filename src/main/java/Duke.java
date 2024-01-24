1ef278a7d27af5e0275b606b8f3e9448b4a16ecc
Ashley <>
2024-01-25 03:50:09 +0800

    delete task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -49,34 +49,53 @@ public class Duke {
             } else if (input.equals("mark")) {
                 System.out.println(indent + "------------------------------------------------");
                 int position = sc.nextInt() - 1;
-                Task t = list.get(position);
-                if (t.getStatusIcon().equals(" ")) {
-                    System.out.println(indent + "Nice! I've marked this task as done:");
+
+                // check for error
+                if (position + 1 > list.size()) {
+                    System.out.println(indent + "Task does not exist");
+                } else if (position < 0) {
+                    System.out.println(indent + "Task does not exist");
+
+                } else {
+                    Task t = list.get(position);
+                    if (t.getStatusIcon().equals(" ")) {
+                        System.out.println(indent + "Nice! I've marked this task as done:");
+                    }
+                    t.markAsDone();
+                    System.out.println(indent + "  " + t.toString());
                 }
-                t.markAsDone();
-                System.out.println(indent + "  " +  t.toString());
                 System.out.println(indent + "------------------------------------------------");
 
                 // unmark an item in list
             } else if (input.equals("unmark")) {
                 System.out.println(indent + "------------------------------------------------");
                 int position = sc.nextInt() - 1;
-                Task t = list.get(position);
-                if (t.getStatusIcon().equals("X")) {
-                    System.out.println(indent + "OK, I've marked this task as not done yet:");
+
+                // check for error
+                if (position + 1 > list.size()) {
+                    System.out.println(indent + "Task does not exist");
+                } else if (position < 0) {
+                    System.out.println(indent + "Task does not exist");
+
+                } else {
+                    Task t = list.get(position);
+                    if (t.getStatusIcon().equals("X")) {
+                        System.out.println(indent + "OK, I've marked this task as not done yet:");
+                    }
+                    t.markAsUndone();
+                    System.out.println(indent + "  " + t.toString());
                 }
-                t.markAsUndone();
-                System.out.println(indent + "  " + t.toString());
                 System.out.println(indent + "------------------------------------------------");
 
             } else if (input.equals("todo")) {
                 System.out.println(indent + "------------------------------------------------");
                 // check for error
-                if (!sc.hasNext()) {
+                String des = input + sc.nextLine();
+                des = des.trim();
+                if (des.equals("todo")) {
                     System.out.println(indent + "OOPS!!! The description of a todo cannot be empty.");
-                    
                 } else {
-                    String des = sc.nextLine().trim();
+                    des = des.substring(4).trim();
                     Todo t = new Todo(des);
                     list.add(t);
                     System.out.println(indent + "Got it. I've added this task:");
@@ -87,11 +106,14 @@ public class Duke {
 
             } else if (input.equals("deadline")) {
                 System.out.println(indent + "------------------------------------------------");
+
                 // check for error
-                if (!sc.hasNext()) {
+                String temp = input + sc.nextLine();
+                temp = temp.trim();
+                if (temp.equals("deadline")) {
                     System.out.println(indent + "OOPS!!! The description of a deadline cannot be empty.");
                 } else {
-                    String temp = sc.nextLine();
+                    temp = temp.substring(8);
                     String[] arrOfStr = temp.split("/by");
                     String des = arrOfStr[0].trim();
                     if (arrOfStr.length < 2) {
@@ -111,10 +133,12 @@ public class Duke {
             } else if (input.equals("event")) {
                 System.out.println(indent + "------------------------------------------------");
                 // check for error
-                if (!sc.hasNext()) {
-                    System.out.println(indent + "OOPS!!! The description of an event cannot be empty.");
+                String temp = input + sc.nextLine();
+                temp = temp.trim();
+                if (temp.equals("event")) {
+                    System.out.println(indent + "OOPS!!! The description of a event cannot be empty.");
                 } else {
-                    String temp = sc.nextLine();
+                    temp = temp.substring(5);
                     String[] arrOfStr = temp.split("/");
                     if (arrOfStr.length < 2) {
                         System.out.println(indent + "Please provide the start time of the event.");
@@ -123,8 +147,8 @@ public class Duke {
 
                     } else {
                         String des = arrOfStr[0].trim();
-                        String start = arrOfStr[1].substring(5).trim();
-                        String end = arrOfStr[2].substring(3).trim();
+                        String start = arrOfStr[1].substring(4).trim();
+                        String end = arrOfStr[2].substring(2).trim();
                         Event t = new Event(des, start, end);
                         list.add(t);
                         System.out.println(indent + "Got it. I've added this task:");
@@ -134,7 +158,26 @@ public class Duke {
                 }
                 System.out.println(indent + "------------------------------------------------");
 
-                // echo the words
+                // delete a task
+            } else if (input.equals("delete")) {
+                int position = sc.nextInt() - 1;
+                System.out.println(indent + "------------------------------------------------");
+
+                // check for error
+                if (position + 1 > list.size()) {
+                    System.out.println(indent + "Task does not exist");
+                } else if (position < 0) {
+                    System.out.println(indent + "Task does not exist");
+
+                } else {
+                    Task t = list.get(position);
+                    list.remove(t);
+                    System.out.println(indent + "Noted. I've removed this task:");
+                    System.out.println(indent + "  " + t.toString());
+                    System.out.println(indent + "Now you have " + list.size() + " tasks in the list.");
+                }
+                System.out.println(indent + "------------------------------------------------");
+
             } else {
                 System.out.println(indent + "------------------------------------------------");
                 System.out.println(indent + "Sorry this is an invalid input :(");
