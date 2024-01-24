f44fca49f3e532f50884be9e4df6d591a60ec30b
AidanG <agoh0185@gmail.com>
2024-01-24 13:04:41 +0800

    feat: Added support for user input (echo) and exiting the program

--- src/main/java/Steven.java
+++ src/main/java/Steven.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Steven {
 
     public static void main(String args[]) {
+        String line = "========\n";
+        String bootMsg = ("This is Steven!\nHow can I advise?\n");
+        System.out.println(line + bootMsg + line);
 
-        String bootMsg = ("=========\nThis is Steven!\nHow can I advise?\n=========");
-        System.out.println(bootMsg);
+        boolean exit = false;
+        while (!exit){
+            Scanner userInput = new Scanner(System.in);
+            String command = userInput.nextLine();
+            switch (command){
+                case "bye":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println(command);
+            }
+            System.out.println(line);
+        }
 
-        System.out.println("I'll see you soon then!");
+        System.out.println("I'll see you soon then!\n" + line);
     }
 }
