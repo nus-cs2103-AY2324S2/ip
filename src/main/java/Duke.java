ee04a87202c34830745c9ecfb59827c80eb8ba4b
whelan-low <e0958047@u.nus.edu>
2024-01-24 15:18:47 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        fillerLine();
+        bye();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I am Greg.\nWhat can I do for you?");
+    }
+
+    public static void bye() {
+        System.out.println("Goodbye! Hope to see you again soon!");
+    }
+
+    public static void fillerLine() {
+        System.out.println("_______________________________________");
     }
 }
