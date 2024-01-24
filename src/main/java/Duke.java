08770d6092a7d24f6bc3bb269d9a1e0bd577ec6b
jamessinmaojun <jamessinmaojun@gmail.com>
2024-01-24 16:12:06 +0800

    Implemented Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "--------------------------------------------------";
+        System.out.println(divider);
+        System.out.println("Hello! I'm TheGiantPeach\nWhat can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
