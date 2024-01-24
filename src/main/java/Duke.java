ae969695ca5d8cadc2e4d4ea1efa94093977b4e5
S-Aishvarya <aish.varya1801@gmail.com>
2024-01-24 22:42:35 +0800

    Level-1 commit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "Hello, I'm Lighthouse.\nWhat can I do for you? \nBye, Hope to see you soon!\n";
+        String greeting = "Hi, I'm Lighthouse.\nHow can I help you?\n";
         System.out.println(greeting);
+
+        Scanner scan = new Scanner(System.in);
+        boolean continueRun = true;
+
+        while (continueRun) {
+            String info = scan.nextLine();
+            if (info.equals("bye")) {
+                continueRun = false;
+                System.out.println("Goodbye! See you next time!");
+                break;
+            }
+            System.out.println(info);
+
+        }
+
+
+
+
+
+
     }
 }
