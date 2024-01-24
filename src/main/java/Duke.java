af928413fd169b2c6b95c0cec5211dee1eea3ba3
caitlyntang <caitlyntong6@gmail.com>
2024-01-24 22:31:58 +0800

    add basic user input functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
+import java.io.*;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = "   __     __   _  _    _  _           \n"
                     + " / __ \\  |  \\ | | \\ \\/ / | |            \n"
                     + "| |__| | |   \\| |  \\  /  | |     \n"
                     + "|  __  | | |\\   |  /  \\  | |\n"
                     + "|_|  |_| |_| \\__| /_/\\_\\ |_|\n";
         String name = "Anxi";
-        String line = "-----------------------------";
+        String line = "--------------------------------------";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println(line);
         System.out.println(logo);
         System.out.println("Hello! I'm " + name + "\r\nWhat can I do for you? \r\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\r\n" + line);
+
+        String task = br.readLine();
+        while(!task.equalsIgnoreCase("bye")) {
+            System.out.println(line + "\r\n" + task + "\r\n" + line);
+            task = br.readLine();
+        }
+
+        System.out.println(line + "\r\nBye. Hope to see you again soon!\r\n" + line);
     }
 }
