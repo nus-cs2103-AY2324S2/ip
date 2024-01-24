3126f232d73c7f954b38df4465fed85ad60f0d5d
Yskie <shikang@u.nus.edu>
2024-01-24 21:56:37 +0800

    Implemented Add and List Functions with a Task class.

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+class Task {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
--- src/main/java/Tiny.java
+++ src/main/java/Tiny.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 public class Tiny {
     public static void main(String[] args) throws IOException {
+
+        Task[] tasks = new Task[100];
+        int totalTasks = 0;
+
         PrintWriter out = new PrintWriter(System.out);
         out.println("   ____________________________________________________________");
         out.println("   Hello! I'm Tiny!");
@@ -13,8 +17,19 @@ public class Tiny {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String input = br.readLine();   
             if (input.equals("bye")) break;
+
+
+
             out.println("   ____________________________________________________________");
-            out.println("   " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < totalTasks; i++) {
+                    out.println("   "+ (i + 1) + ". " + tasks[i].getName());
+                }
+            } else {
+                tasks[totalTasks] = new Task(input);
+                totalTasks++; 
+                out.println("   added: " + input);
+            }
             out.println("   ____________________________________________________________\n");
             out.flush();
         }
