eebba9202023689e9ae482f437a3cac67cb15026
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-01-24 23:58:35 +0800

    Add todo task type

---
+++ src/main/java/duke/storage/Todo.java
@@ -0,0 +1,17 @@
+package duke.storage;
+
+/**
+ * The Todo class defined a 'ToDo' task used for the application
+ *
+ * @author Ryan NgWH
+ */
+public class Todo extends Task {
+  /**
+   * Constructor for a Todo object
+   *
+   * @param description Description of the todo task
+   */
+  public Todo(String description) {
+    super(description);
+  }
+}
