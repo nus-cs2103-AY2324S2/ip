7fb7e4e334d3ad726bda8cfb1cf66c473461e5ee
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-01-24 15:23:15 +0800

    Improve skeletal chatbot
    - Echo commands entered by user
    - Exits on `bye`

--- src/main/java/duke/ui/Cli.java
+++ src/main/java/duke/ui/Cli.java
@@ -1,16 +1,49 @@
 package duke.ui;
 
+import java.util.Scanner;
+
+/**
+ * The UI CLI class handles the displaying of UI elements in
+ * the application
+ *
+ * @author Ryan NgWH
+ */
 public class Cli {
+  /**
+   * Method to print the CLI UI
+   */
   public static void printUI() {
+    // UI Greeting
     String greeting = "------------------------------------------------------------\n" +
         "Hello! I'm Ciara\n" +
-        "What can I do for you?\n";
+        "What can I do for you?\n" +
+        "------------------------------------------------------------";
 
+    // UI Goodbye message
     String goodbye = "------------------------------------------------------------\n" +
         "Bye. Hope to see you again soon!\n" +
         "------------------------------------------------------------";
 
+    // Display greeting
     System.out.println(greeting);
+
+    // Scanner for getting user input
+    Scanner sc = new Scanner(System.in);
+
+    // Echo user input
+    while (true) {
+      String input = sc.nextLine();
+
+      if (input.equals("bye")) {
+        break;
+      }
+      System.out.println("------------------------------------------------------------\n" +
+          input +
+          "\n------------------------------------------------------------");
+    }
+
+    // Print exit message
     System.out.println(goodbye);
+    sc.close();
   }
 }
