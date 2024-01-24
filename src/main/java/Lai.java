966e9aefee8577a5687e2ab6942f344897404953
Shaun Lai <shaun@littlebit.sg>
2024-01-24 22:59:41 +0800

    Changed the name of the chatbot from Duke to Lai

---
+++ src/main/java/Lai.java
@@ -0,0 +1,9 @@
+public class Lai {
+    public static void main(String[] args) {
+        System.out.println("Hi there, I am Lai. Your friendly fairly useless chatbot.");
+        System.out.println("What can I assist you with today?");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("See you again!");
+        System.out.println("---------------------------------------------------------");
+    }
+}
