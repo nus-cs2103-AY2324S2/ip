89b8e910eab080cc0048cedb60b6e3f00a84451e
aman zainal <aman@u.nus.edu>
2024-01-25 01:20:03 +0800

    more ui changes

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -28,9 +28,9 @@ public class Messages {
             " |___/\\__,_|\\__, |___/ |_|  |_|\\___|_|_|\\___/(_)                   \n" +
             "             __/ |                                                     \n" +
             "            |___/                                                      \n" +
-            "\n>>> What are we doing today?\n";
+            "\n(^-^)~~   What are we doing today?\n";
 
     public static final String EXIT =
-    ">>> Aw, see you soon!";
+    "\n(^-^)~~   Aw, see you soon!\n";
 
 }
