d6f63f4cf2bbdee369b4399bb8ca62eacc2b3fff
Doye <doyelee0313@gmail.com>
2024-01-24 11:27:16 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,49 +16,50 @@ public class Duke {
 
         List<Task> array = new ArrayList<>();
 
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
-            Scanner sc = new Scanner(System.in);
             String order = sc.nextLine();
 
-            if (order.equals("end")) {
+            if (order.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.print("____________________________________________________________");
                 break;
             } else if (order.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list: ");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < array.size(); i++) {
                     Task addTask = array.get(i);
                     System.out.println((i + 1) + "." + addTask.toString());
                 }
-                System.out.println("____________________________________________________________");
+                System.out.print("____________________________________________________________");
             } else if (order.contains("unmark")) {
                 String[] tokens = order.split(" ");
                 int number = Integer.parseInt(tokens[1]);
                 System.out.println("____________________________________________________________");
-                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println("OK, I've marked this task as not done yet:");
                 Task t = array.get(number - 1);
                 t.markAsUnDone();
                 System.out.println(t.toString());
-                System.out.println("____________________________________________________________");
+                System.out.print("____________________________________________________________");
             } else if (order.contains("mark")) {
                 String[] tokens = order.split(" ");
                 int number = Integer.parseInt(tokens[1]);
                 System.out.println("____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("Nice! I've marked this task as done:");
                 Task t = array.get(number - 1);
                 t.markAsDone();
                 System.out.println(t.toString());
-                System.out.println("____________________________________________________________");
+                System.out.print("____________________________________________________________");
             } else if(order.contains("todo")) {
                 String task = order.substring(5);
                 Task t = new Todo(task);
                 array.add(t);
                 System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task: ");
+                System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
-                System.out.println("Now you have " + array.size() + " tasks in the list.\n");
+                System.out.println("Now you have " + array.size() + " tasks in the list.");
                 System.out.println("____________________________________________________________");
             } else if (order.contains("deadline")) {
                 int byIndex = order.indexOf("/by");
@@ -67,10 +68,10 @@ public class Duke {
                 Task t = new Deadline(task, due);
                 array.add(t);
                 System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task: ");
+                System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
-                System.out.println("Now you have " + array.size() + " tasks in the list.\n");
-                System.out.println("____________________________________________________________");
+                System.out.println("Now you have " + array.size() + " tasks in the list.");
+                System.out.print("____________________________________________________________");
             } else if (order.contains("event")) {
                 int fromIndex = order.indexOf("/from");
                 int toIndex = order.indexOf("/to");
@@ -80,16 +81,16 @@ public class Duke {
                 Task t = new Event(task, from, to);
                 array.add(t);
                 System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task: ");
+                System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
-                System.out.println("Now you have " + array.size() + " tasks in the list.\n");
-                System.out.println("____________________________________________________________");
+                System.out.println("Now you have " + array.size() + " tasks in the list.");
+                System.out.print("____________________________________________________________");
             } else {
                 Task t = new Task(order);
                 array.add(t);
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + t.description);
-                System.out.println("____________________________________________________________");
+                System.out.print("____________________________________________________________");
             }
         }
     }
