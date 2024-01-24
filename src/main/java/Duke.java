a3596aa9a88ce450eb9c333355475f114cfd768d
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-24 17:29:25 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Jojo :)");
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String[] listArr = new String[100];
+        String userTxt = sc.nextLine();
+        while (!userTxt.equals("bye")) {
+            System.out.println(userTxt);
+            userTxt = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
