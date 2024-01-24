c91fbcf62ce0d4fbaac7f6ebee0f600b30a19a43
Jules <julia.poo.poo.poo@gmail.com>
2024-01-24 20:08:42 +0800

    Feat: Completed level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+
+  private static final String NAME = ">uwu<";
+
+  private static void message_end() {
+    System.out.println("___________________________");
+  }
+
+  private static void greet() {
+    System.out.printf(
+      "Hello! I'm %s\n" + "What can I do for you?\n",
+      Duke.NAME
+    );
+    Duke.message_end();
+  }
+
+  private static void bye() {
+    System.out.println("Bye. Hope to see you again soon!");
+    Duke.message_end();
+  }
+
+  public static void main(String[] args) {
+    Duke.greet();
+    Duke.bye();
+  }
 }
