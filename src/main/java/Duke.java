0cd0f19034856e8199819df133cda358c13fbc44
wapisai <e0968302@u.nus.edu>
2024-01-24 21:05:39 +0800

    Add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
+import java.util.*;
 public class    Duke {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n"
                 + "        Hello! I'm sibehupzcoder9000\n"
-                + "        What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "        Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + "        What you want sia\n"
+                + "____________________________________________________________\n";
+        String outro = "____________________________________________________________\n"
+                + "        wow so ur gg to leave me...\n"
+                + "____________________________________________________________\n";
+
+        Scanner sc= new Scanner(System.in);
         System.out.println(intro);
+        String str = sc.nextLine();
+        while (!str.equals("exit")) {
+            String output = "____________________________________________________________\n"
+                    + str
+                    + "\n____________________________________________________________\n";
+            System.out.print(output);
+            str = sc.nextLine();
+        }
+        System.out.print(outro);
     }
 }
