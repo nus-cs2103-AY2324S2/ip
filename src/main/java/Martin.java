48658a6d6575670e3651d8a36357382f083295f5
yunchao <tanyunchao@gmail.com>
2024-01-25 00:39:31 +0800

    added echo function

--- src/main/java/Martin.java
+++ src/main/java/Martin.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Martin {
     static String NAME = "Martin";
     public static void main(String[] args) {
         sayGreeting();
-        sayBye();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                sayBye();
+                break;
+            }
+            System.out.println(line);
+        }
     }
 
     public static void sayGreeting() {
