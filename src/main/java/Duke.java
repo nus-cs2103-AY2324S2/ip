ac93cde03bee3433454681ea869b92f23464d04d
iamtr <jettzeong@gmail.com>
2024-01-24 18:51:09 +0800

    Completed Level 1 - Echo
    Program now will echo whatever user types

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String sep = "__________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        String sep = "\t__________________________________________";
         System.out.println(sep);
-        System.out.println("Hello! I'm JOSEPH JOSHTUR!!!");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm JOSEPH JOSHTUR!!!");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(sep);
-        System.out.println("Bye. Hope to see you again soon!");
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(sep);
+            System.out.println("\t" + command);
+            System.out.println(sep);
+            command = scanner.nextLine();
+        }
+        System.out.println(sep);
+        System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(sep);
     }
-
-
 }
