4b9768e7781ee7466664e76b24cb5081e3d76293
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2024-01-24 18:08:26 +0800

    implement event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,22 @@
-public class Event {
+public class Event extends Task {
+
+    String start;
+    String end;
+
+    public Event(String name, String start, String end) {
+        super(name);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String fullStatus() { //TODO: add type of task later
+        String checkbox;
+        if (isDone) {
+            checkbox = "[E][X] ";
+        } else {
+            checkbox = "[E][ ] ";
+        }
+        return checkbox + name + "(from: " + start +  " to " + end + ")";
+    }
 }
