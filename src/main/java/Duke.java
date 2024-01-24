1efc8620f89368857be25f0219632cd25931f504
Pluiexo <nortonng@Nortons-MacBook-Pro.local>
2024-01-24 13:33:47 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String bar = "____________________________________________________________";
+        String first = "Hello! I'm Pluiexo";
+        String second = "What can I do for you?";
+        String third = "Hope to see you again soon!";
+        String[] data = new String[]{bar, first, second, bar, third};
+        for (String line : data) {
+            System.out.println(line);
+        }
+
     }
 }
