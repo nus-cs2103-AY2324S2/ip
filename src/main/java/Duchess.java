529bf20269a8ac5d6215d186e1bb40188f38fcfc
Jaime Ng <jaimengjm@gmail.com>
2024-01-24 19:40:30 +0800

    Print an echo of any input given, exit if input is bye.

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
+
 public class Duchess {
     public static void main(String[] args) {
-        horizontalLine();
+        printHorizontalLine();
 
         printOpeningGreeting();
 
-        printClosingGreeting();
-        
-        horizontalLine();
+        printHorizontalLine();
 
+        printEcho();
+
+        printHorizontalLine();
+
+    }
+
+    //Prints an echo of user's input, exits if user inputs "bye"
+    public static void printEcho() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Loop to read user input
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            // Display the user input
+            printHorizontalLine();
+            System.out.println(" " + userInput);
+            printHorizontalLine();
+
+            // Check if the user wants to exit
+            if (userInput.equalsIgnoreCase("bye")) {
+                printClosingGreeting();
+                break;  // Exit the loop
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 
     //Print opening greeting
@@ -18,18 +46,20 @@ public class Duchess {
                 + "| |_| | |_| | |__| ___ |   __/\\__ \\\\__ \\  \n"
                 + "|____/ \\__,_|____|_| |_|\\ ___||___/|___/\n";
         System.out.println(logo);
-        horizontalLine();
+        printHorizontalLine();
         System.out.println("Hello! I'm Duchess.");
         System.out.println("What can I do for you today?");
     }
 
+
+    //Prints closing greeting
     public static void printClosingGreeting() {
-        horizontalLine();
+        printHorizontalLine();
         System.out.println("Goodbye. Hope to see you again soon!");
     }
 
     //Prints a Horizontal Line of 50 dashes
-    public static void horizontalLine() {
+    public static void printHorizontalLine() {
         int lineLength = 50; // Specify the length of the line
 
         // Print the horizontal line
