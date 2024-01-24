f281b2d435cdc1e640426c880c85fd968d1de483
Giselle Tan <giselletan1008@gmail.com>
2024-01-24 15:17:15 +0800

    Level-4: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,9 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Task> lsts = new HashMap<>();
-        int counter = 0;
+        ArrayList<Task> al = new ArrayList<>();
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Emis!\n \tWhat can I do for you?");
@@ -16,32 +14,63 @@ public class Duke {
             if (spaceIndex == -1) {
                 if (user_input.equals("bye")) {
                     exit();
+
                 } else if (user_input.equals("list")) {
-                    print_list(lsts);
-                } else {
-                    counter += 1;
-                    addTasks(user_input, counter, lsts);
+                    print_list(al);
+
                 }
             } else {
+                
                 String action = user_input.substring(0, user_input.indexOf(" "));
                 try {
                     Integer taskNo = Integer.parseInt(user_input.substring(spaceIndex + 1));
-                    if (taskNo <= 0 || taskNo > counter) {
+                    if (taskNo <= 0 || taskNo > al.size()) {
                         System.out.println("Task does not exist");
+
                     } else {
-                        Task t = lsts.get(taskNo);
+                        // to mark as done or undone
+                        Task t = al.get(taskNo - 1);
                 
                         if (action.equals("mark")) {
                             t.markAsDone();
-                            lsts.replace(taskNo, t);
+                            al.set(taskNo - 1, t);
+
                         } else if (action.equals("unmark")) {
                             t.markAsUndone();
-                            lsts.replace(taskNo, t);
+                            al.set(taskNo - 1, t);
                         } 
                     }
                 } catch (NumberFormatException nfe) {
-                    counter += 1;
-                    addTasks(user_input, counter, lsts);
+                    // todo, deadline, event
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\tGot it. I've added this task:");
+                    String d = user_input.substring(spaceIndex + 1);
+                    if (action.equals("todo")) {
+                        ToDo td = new ToDo(d);
+                        al.add(td);
+                        System.out.println("\t\t" + td.toString());
+
+                    } else if (action.equals("deadline")) {
+                        int slashIndex = d.indexOf("/by");
+                        String des = d.substring(0, slashIndex);
+                        String by = d.substring(slashIndex + 3);
+                        Deadline dl = new Deadline(des, by);
+                        al.add(dl);
+                        System.out.println("\t\t" + dl.toString());
+
+                    } else if (action.equals("event")) {
+                        int slashIndex_from = d.indexOf("/from");
+                        int slashIndex_to = d.indexOf("/to");
+                        String des = d.substring(0, slashIndex_from);
+                        String from = d.substring(slashIndex_from + 5, slashIndex_to);
+                        String to = d.substring(slashIndex_to + 3);
+                        Event eve = new Event(des, from, to);
+                        al.add(eve);
+                        System.out.println("\t\t" + eve.toString());
+
+                    }
+                    System.out.println("\tNow you have " + al.size() + " tasks in the list.");
+                    System.out.println("\t____________________________________________________________");
                 }
             }
         }
@@ -54,38 +83,28 @@ public class Duke {
         System.exit(0);
     }
 
-    public static void print_list(HashMap<Integer, Task> hs) {
+    public static void print_list(ArrayList<Task> al) {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHere are the tasks in your list:");
-        for (Integer i : hs.keySet()) {
-            System.out.println("\t" + hs.get(i).toString());
+        for (int i = 0; i < al.size(); i++) {
+            System.out.println("\t\t" + (i + 1) + "." + al.get(i).toString());
         }
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void addTasks(String input, int counter, HashMap<Integer, Task> hs) {
-        Task t = new Task(input, counter);
-        hs.put(counter, t);
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tadded: " + input);
         System.out.println("\t____________________________________________________________");
     }
 
     public static class Task {
         protected String description;
-        protected int taskNumber;
         protected boolean isDone;
-
-        public Task(String description, int taskNumber) {
+    
+        public Task(String description) {
             this.description = description;
-            this.taskNumber = taskNumber;
             this.isDone = false;
         }
-
+    
         public String getStatusIcon() {
             return (isDone ? "X" : " ");
         }
-
+    
         public void markAsDone() {
             this.isDone = true;
             System.out.println("\t____________________________________________________________");
@@ -93,7 +112,7 @@ public class Duke {
             System.out.println("\t\t[" + this.getStatusIcon() + "] " + this.description);
             System.out.println("\t____________________________________________________________");
         }
-
+    
         public void markAsUndone() {
             this.isDone = false;
             System.out.println("\t____________________________________________________________");
@@ -101,10 +120,51 @@ public class Duke {
             System.out.println("\t\t[" + this.getStatusIcon() + "] " + this.description);
             System.out.println("\t____________________________________________________________");
         }
-
+    
         @Override
         public String toString() {
-            return taskNumber + ". [" + this.getStatusIcon() + "] " + this.description;
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        }
+    }
+
+    public static class Deadline extends Task {
+        protected String by;
+    
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+    
+        @Override
+        public String toString() {
+            return"[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    public static class ToDo extends Task {
+        public ToDo(String description) {
+            super(description);
+        }
+    
+        @Override
+        public String toString() {
+            return"[T]" + super.toString();
+        }
+    }
+
+    public static class Event extends Task {
+        protected String from;
+        protected String to;
+    
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+    
+        @Override
+        public String toString() {
+            return"[E]" + super.toString() + "(from: " + this.from + " to: " + this.to + ")";
         }
     }
 }
