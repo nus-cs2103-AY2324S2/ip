9d2bdaae9b6ff9bfcd3ee11e203cdf9bdda3c194
AjayArvind2207 <alwayssactheexchange@gmail.com>
2024-01-24 15:14:38 +0800

    Fixed the issue with the previous commit. I think it should work now!

--- src/main/java/Panna.java
+++ src/main/java/Panna.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Panna {
+public class Panna {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------------\n"
+        + "Hello! I'm Panna.\n" +
+                "What can I do for you?\n\n" +
+                "----------------------------------------------------------");
+
+        System.out.println("Bye. Hope to see you again soon!\n\n" +
+                "----------------------------------------------------------");
+    }
 }
