88004561d0c249a7ca098f015958a93fa0be025b
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-24 15:48:18 +0800

    Refactored good bye message to a function call goodByeAndExit(), which also terminates the program. Removed junk comments

--- src/main/java/Lindi.java
+++ src/main/java/Lindi.java
@@ -2,20 +2,18 @@ public class Lindi {
     public static void printSeparator() {
         System.out.println("-".repeat(50));
     }
-    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
 
+    public static void goodByeAndExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printSeparator();
+        System.exit(0); // exit with code 0, terminates program
+    }
+    public static void main(String[] args) {
         String name = "Lindi";  // Log It N Do It -> LINDI
         printSeparator();
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         printSeparator();
-        System.out.println("Bye. Hope to see you again soon!");
-        printSeparator();
+        goodByeAndExit();
     }
 }
