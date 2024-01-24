08485a7879cbe877e5bba182ac4f35b184248788
chengjunyuan <122239268+chengjunyuan@users.noreply.github.com>
2024-01-24 10:40:52 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static String welcome = "Hi, I'm Gronk! \uD83D\uDDFF \n"
-            + "What are we up to today?";
+    public static String welcome = "   Hi, I'm Gronk! \uD83D\uDDFF \n"
+            + "   What are we up to today?";
 
-    public static String goodbye = "System closing. Goodbye!";
+    public static String goodbye = "   System closing. Goodbye!";
 
     public static void lines() {
-        System.out.println("----------------------------------------");
+        System.out.println("  ----------------------------------------");
     }
 
     public static void printMessage(String message) {
@@ -20,12 +20,26 @@ public class Duke {
         String message = "";
         lines();
         printMessage(welcome);
+        String[] tasks = new String[100];
+        int i = 0;
         while (true) {
-            message = sc.next();
+            message = sc.nextLine();
+            lines();
             if (message.equals("bye")) {
                 break;
+            } else if (message.equals("list")) {
+                if (i == 0) {
+                    System.out.println("   nothing added yet!");
+                } else {
+                    for (int j = 0; j < i; j++) {
+                        System.out.println("   " + Integer.toString(j + 1) + ". " + tasks[j]);
+                    }
+                }
+                lines();
             } else {
-                printMessage(message);
+                printMessage("   added: " + message);
+                tasks[i] = message;
+                i += 1;
             }
         }
         printMessage(goodbye);
