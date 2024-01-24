a478dc56becff060dddcdccdc9f91c7a5d159844
Allysa Tan <allysa@u.nus.edu>
2024-01-25 00:21:45 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,33 @@
 import java.lang.reflect.Array;
 import java.util.*;
 public class Duke {
+
+    protected static ArrayList<Task> lst = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> lst = new ArrayList<>();
         intro();
         while (sc.hasNext()) {
             String s = sc.nextLine();
             if (s.equals("list")) {
-                displayList(lst);
+                displayList();
+                continue;
             } else if (s.equals("bye")) {
                 exit();
                 return;
             } else {
-                addToList(lst, s);
+                String[] words = s.split(" ");
+                String firstWord = words[0];
+                if (firstWord.equals("mark")) {
+                    markComplete(Integer.parseInt(s.split(" ")[1]));
+                }
+                else if (firstWord.equals("unmark")) {
+                    unmarkComplete(Integer.parseInt(s.split(" ")[1]));
+                }
+                else {
+                    addToList(s);
+                }
             }
         }
-//        echo(sc);
     }
     private static void intro() {
         String logo = "        _  _        \n  __ _ | || | _   _ \n / _` || || || | | |\n| (_| || || || |_| |\n \\__,_||_||_| \\__, |\n              |___/ \n";
@@ -34,14 +45,27 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static void addToList(ArrayList<String> lst, String s) {
-        lst.add(s);
+    private static void addToList(String s) {
+        lst.add(new Task(s));
         System.out.println("added: " + s);
     }
 
-    private static void displayList(ArrayList<String> lst) {
+    private static void displayList() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < lst.size(); i++) {
-            System.out.println(i+1 + ": " + lst.get(i));
+            System.out.println(i+1 + ":" + lst.get(i));
         }
+    }
+
+    private static void markComplete(int i) {
+        System.out.println("Nice! I've marked this task as done:");
+        lst.get(i-1).markComplete();
+        System.out.println(lst.get(i-1));
+    }
+
+    private static void unmarkComplete(int i) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        lst.get(i-1).unmarkComplete();
+        System.out.println(lst.get(i-1));
     }
 }
