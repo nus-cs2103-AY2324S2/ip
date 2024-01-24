dbd53d9edda3e0295f22d51b60543166b7727e3c
NatLeong <>
2024-01-25 00:48:41 +0800

    Add Level 0

---
+++ src/main/java/Dav.java
@@ -0,0 +1,20 @@
+public class Dav {
+    public static void main(String[] args) {
+        greetUser();
+        exit();
+    }
+
+    public static void greetUser() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Dav");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
