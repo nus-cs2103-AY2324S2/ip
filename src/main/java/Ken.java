4125ca6600d3052feff38b7b7dab43700713f9b0
maiyasaliha <s.maiya095@gmail.com>
2024-01-24 11:56:48 +0800

    Level 0: Added Hi and Bye

---
+++ src/main/java/Ken.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ken {
+    public static void main(String[] args) {
+        System.out.println("Hi Barbie!");
+        System.out.println("I'm Ken!");
+        System.out.println("What would you like to beach today?\n");
+        System.out.println("Beach off!\n");
+
+    }
+}
