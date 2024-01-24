7af89c0984f366193f21f54a0700ab88c9518eb6
ziyang27 <tanziyang39@gmail.com>
2024-01-24 11:08:26 +0800

    Rename, Greet, and Exit

---
+++ src/main/java/Dibo.java
@@ -0,0 +1,9 @@
+public class Dibo {
+    private static String name = "Dibo the Gift Chatbot";
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
