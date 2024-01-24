2dec2a08b3ff1814befdf4e9159b8e5ca4a09b1f
fy17ohhh <hester03sfy@gmail.com>
2024-01-24 13:18:57 +0800

    Use a class to represent tasks

---
+++ src/main/java/jade/Task.java
@@ -0,0 +1,29 @@
+package jade;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    protected void mark() {
+        this.isDone = true;
+    }
+
+    protected void unMark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s",this.getStatusIcon(),  this.description);
+    }
+
+}
