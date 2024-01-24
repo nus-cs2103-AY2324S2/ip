c3f14f43fd2c2e2fa599025d9a66ad480908734c
zengzihui <zeng_zihui@outlook.com>
2024-01-24 17:32:56 +0800

    Finish Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo =  " _____ _               _\n"
+                +       "/  __ (_)             | |      \n"
+                +       "| /  \\/_  ___ __ _  __| | __ _ \n"
+                +       "| |   | |/ __/ _` |/ _` |/ _` |\n"
+                +       "| \\__/\\ | (_| (_| | (_| | (_| |\n"
+                +       " \\____/_|\\___\\__,_|\\__,_|\\__,_|\n";
+
         System.out.println("Hello from\n" + logo);
+        String horizontalLine = "-".repeat(60);
+        greeting("Cicada", horizontalLine);
+        ending(horizontalLine);
+    }
+
+    public static void greeting(String chatbotName, String horizontalLine) {
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + chatbotName
+                + "\nWhat can I do for you?");
+        System.out.println(horizontalLine);
+    }
+
+    public static void ending(String horizontalLine) {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
