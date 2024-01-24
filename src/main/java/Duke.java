5126903ee91f63b377e914ae960ede429865013d
Yetong <tangyetong666@gmail.com>
2024-01-24 16:15:26 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+
+    final static String HORIZONTAL_LINE = "____________________________________________________________ \n";
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________ \n");
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello from Kewgy! \n");
         System.out.println("What can I do for you? \n");
-        System.out.println("____________________________________________________________ \n");
+        System.out.println("Type \"bye\" to exit! \n");
+        System.out.println(HORIZONTAL_LINE);
+
+        String userMsg = reader.next();
+        while(!Objects.equals(userMsg, "bye")) {
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(userMsg);
+            System.out.println(HORIZONTAL_LINE);
+            userMsg = reader.next();
+        }
+
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye! Hope to see you again soon! \n");
-        System.out.println("____________________________________________________________ \n");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
