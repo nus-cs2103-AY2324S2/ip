a9fa3cf1947c51e209833d53a5f26b13a82905e5
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2024-01-24 19:05:21 +0800

    add level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Hao Wen\n" + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
