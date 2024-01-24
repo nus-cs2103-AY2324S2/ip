230083587e173854f143594e5883844288ab17df
Derekljh <derekljh21@gmail.com>
2024-01-24 09:19:52 +0800

    Add Lunaris.java

---
+++ src/main/java/Lunaris.java
@@ -0,0 +1,8 @@
+public class Lunaris {
+    public static void main(String[] args) {
+        String name = "Lunaris";
+        System.out.println("Hello! I'm " + name + "\n"
+            + "What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
