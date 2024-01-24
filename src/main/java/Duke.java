8b165446dba7e1c13bc88b8cd244edac5613e679
JOELPC\joelw <omegabeeminecraft@gmail.com>
2024-01-24 23:26:50 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,31 +7,32 @@ public class Duke {
         System.out.println(line);
     }
     public static void spacing() {
+        separate();
         System.out.println("");
     }
+
     public static void intro() {
         separate();
-        System.out.println("    Hello! I'm " + name + "\n    What can I do for you?");
-        separate();
+        System.out.println("     Hello! I'm " + name + "\n     What can I do for you?");
         spacing();
     }
     public static void leave() {
-        System.out.println("    Bye. Hope to see you again soon!");
-        separate();
+        System.out.println("     Bye. Hope to see you again soon!");
         spacing();
     }
     public static void echo() {
         intro();
         Scanner input = new Scanner(System.in);
+
         while(true) {
             String echoed = input.nextLine();
             if(echoed.equals("bye")) {
+                separate();
                 leave();
                 break;
             } else {
                 separate();
-                System.out.println("    " + echoed);
-                separate();
+                System.out.println("     " + echoed);
                 spacing();
             }
         }
