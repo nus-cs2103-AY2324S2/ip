d3c80c2f0a151f79f5653ee1a8b2000b5751baa5
LIU JIAYAO <>
2024-01-24 23:59:37 +0800

    level-0

---
+++ src/main/java/Jiayou.java
@@ -0,0 +1,11 @@
+public class Jiayou {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Jiayou.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
