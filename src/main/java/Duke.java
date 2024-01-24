f3213144cf861f5a5dbaa59dba555838fdb3f21c
tanqinyong <tqinyong@yahoo.com.sg>
2024-01-24 15:12:25 +0800

    [Level-2] Add ability to store text and display them using an array.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+// @author Tan Qin Yong
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -5,15 +6,29 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
 
+        // initialise tasklist array
+        String[] tasks = new String[100];
+        int count = 0;
+
         while (true) {
             String echoCommand = sc.nextLine();
             if (echoCommand.equalsIgnoreCase("bye")) {
                 System.out.println("Goodbye! Till we meet again ~");
                 break;
+            } else if (echoCommand.equalsIgnoreCase("list")) {
+                System.out.println("------------------------------------");
+                for (int i = 0; i < count; i++) {
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
+                System.out.println("------------------------------------");
+            } else {
+                tasks[count] =  echoCommand;
+                count++;
+                System.out.println("------------------------------------");
+                System.out.println("Added: " + echoCommand);
+                System.out.println("------------------------------------");
             }
-            System.out.println("------------------------------------");
-            System.out.println(echoCommand);
-            System.out.println("------------------------------------");
+
         }
 
 
