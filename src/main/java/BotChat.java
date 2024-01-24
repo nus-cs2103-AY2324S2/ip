ee76e17f84685b88e56677ecb72f6c7f32f988a2
kaitinghh <114789615+kaitinghh@users.noreply.github.com>
2024-01-25 01:39:07 +0800

    Level 2. Add, List

--- src/main/java/BotChat.java
+++ src/main/java/BotChat.java
@@ -2,13 +2,26 @@ import java.util.Scanner;
 
 public class BotChat {
     static boolean terminate = false;
+    static String[] dataStore = new String[100];
+    static int lastIdx = 0;
 
     public static String response(String s) {
         if (s.equals("bye")) {
             terminate = true;
             return "Bye. Hope to see you again soon!";
+        } else if (s.equals("list")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i <= lastIdx; i++) {
+                stringBuilder.append(i);
+                stringBuilder.append(". ");
+                stringBuilder.append(dataStore[i-1]);
+                stringBuilder.append("\n ");;
+            }
+            return stringBuilder.toString();
         } else {
-            return s;
+            dataStore[lastIdx] = s;
+            lastIdx++;
+            return "added: " + s;
         }
     }
 
