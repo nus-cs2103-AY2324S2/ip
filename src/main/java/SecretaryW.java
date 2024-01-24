95066230ddcf0b428763e5f0e56cb3a8ca030ea3
tayziyi <ziyitay@Zis-MacBook-Pro.local>
2024-01-24 13:52:39 +0800

    Level-2

--- src/main/java/SecretaryW.java
+++ src/main/java/SecretaryW.java
@@ -4,6 +4,12 @@ public class SecretaryW {
         // Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
+        // Array to store task items
+        String[] tasklist = new String[100];
+
+        // counter for number of task
+        int count = 0;
+
         String line = "------------------------------------------\n";
         String greeting = "Hello! I'm SecretaryW\n" + "What can I do for you?\n";
         String farewell = "Bye. Hope to see you again soon!\n";
@@ -17,11 +23,24 @@ public class SecretaryW {
 
             if (userInput.equals("bye")){
                 break; // Exits loop
+            } else if (userInput.equals("list")){
+                System.out.println(line);
+                if (count == 0) {
+                    System.out.println("No tasks available");
+                    System.out.println(line);
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasklist[i]);
+                    }
+                    System.out.println(line);
+                }
+            } else {
+                // Add to tasklist
+                tasklist[count] = userInput;
+                count++;
+                String echo = userInput + "\n";
+                System.out.println(line + "added: " + echo + line);
             }
-
-            // Echo
-            String echo = userInput + "\n";
-            System.out.println(line + echo + line);
         }
 
         // Farewell
