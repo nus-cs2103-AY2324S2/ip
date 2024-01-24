ca48f730df6a30c563577961cf9a779bb2724fe7
khushi833 <71274442+khushi833@users.noreply.github.com>
2024-01-25 01:25:56 +0800

    todos, events, deadlines

--- src/main/java/Aether.java
+++ src/main/java/Aether.java
@@ -31,8 +31,7 @@ public class Aether {
                 if (taskCounter > 0) {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCounter; i++) {
-                        System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + " " +
-                        tasks[i].getDescription());
+                        System.out.println((i + 1) + "." + tasks[i]);
                     }
                 } else {
                     System.out.println("No tasks added yet.");
@@ -56,6 +55,37 @@ public class Aether {
                     System.out.println(tasks[taskIndex].getStatusIcon() + " " + tasks[taskIndex].getDescription());
                 } else {
                     System.out.println("Invalid task index.");
+                }
+            } else if (input.startsWith("todo")) {
+                // Add a ToDo task
+                tasks[taskCounter] = new ToDo(input.substring(5).trim());
+                taskCounter++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCounter - 1]);
+                System.out.println("Now you have " + taskCounter + " tasks in the list.");
+            } else if (input.startsWith("deadline")) {
+                // Add a Deadline task
+                String[] deadlineDetails = input.substring(9).split("/by");
+                if (deadlineDetails.length == 2) {
+                    tasks[taskCounter] = new Deadline(deadlineDetails[0].trim(), deadlineDetails[1].trim());
+                    taskCounter++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCounter - 1]);
+                    System.out.println("Now you have " + taskCounter + " tasks in the list.");
+                } else {
+                    System.out.println("Invalid deadline format. Please use: deadline <description> /by <date/time>");
+                }
+            } else if (input.startsWith("event")) {
+                // Add an Event task
+                String[] eventDetails = input.substring(6).split("/from|/to");
+                if (eventDetails.length == 3) {
+                    tasks[taskCounter] = new Event(eventDetails[0].trim(), eventDetails[1].trim(), eventDetails[2].trim());
+                    taskCounter++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCounter - 1]);
+                    System.out.println("Now you have " + taskCounter + " tasks in the list.");
+                } else {
+                    System.out.println("Invalid event format. Please use: event <description> /from <start> /to <end>");
                 }
             } else {
                 tasks[taskCounter] = new Task(input);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -21,4 +21,9 @@ public class Task {
     public void markAsNotDone() {
         this.isDone = false;
     }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + getDescription();
+    }
 }
