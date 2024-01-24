b3c6a5f8bec45e677ab4588db69350962dbfb5b9
minirei <minrei101@gmail.com>
2024-01-25 00:19:54 +0800

    Echo user input

--- src/main/java/Derek.java
+++ src/main/java/Derek.java
@@ -1,21 +1,51 @@
+import java.util.Scanner;
+
 public class Derek {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String indent = "    ";
+
         String welcomeMessage = String.join(
             "\n",
-            "____________________________________________________________",
-            "Hello! I'm DEREK",
-            "What can I do for you?",
-            "____________________________________________________________"
+            indent + line,
+            indent + "Hello! I'm DEREK",
+            indent + "What can I do for you?",
+            indent + line,
+            ""
         );
 
         String exitMessage = String.join(
             "\n",
-            "____________________________________________________________",
-            "Bye. Hope to see you again soon!",
-            "____________________________________________________________"
+            indent + line,
+            indent + "Bye. Hope to see you again soon!",
+            indent + line,
+            ""
         );
 
         System.out.println(welcomeMessage);
+
+        // user input loop
+        Scanner scanner = new Scanner(System.in);
+
+        String userPrompt = "";
+        while(true) {
+            userPrompt = scanner.nextLine();
+
+            if ("bye".equalsIgnoreCase(userPrompt)) {
+                break;
+            }
+
+            System.out.println(String.join(
+                "\n",
+                indent + line,
+                indent + userPrompt,
+                indent + line,
+                ""
+            ));
+        }
+
+        scanner.close();
+
         System.out.println(exitMessage);
     }
 }
