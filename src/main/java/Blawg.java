2a8781406cff8f724bc958c1056c00c40fee2b1f
joel <joelgoh16@gmail.com>
2024-01-25 00:48:36 +0800

    Rename Duke.java to Blawg.java, add greet and exit functionality

---
+++ src/main/java/Blawg.java
@@ -0,0 +1,12 @@
+public class Blawg {
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________\n" +
+                " Hello! I'm Blawg\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        System.out.println(intro);
+        String outro = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(outro);
+    }
+}
