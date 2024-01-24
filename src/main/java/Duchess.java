d4972ed00e11a8811875c30e4e3b85618c45efce
Jaime Ng <jaimengjm@gmail.com>
2024-01-24 18:58:50 +0800

    Add greeting and exit skeleton

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,10 +1,29 @@
 public class Duchess {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____            __     \n"
+                    + "|  _ \\ _   ______| |      ___  ___  ___ \n"
+                    + "| | | | | | |  __| |__  /  _ \\/ __|/ __|  \n"
+                    + "| |_| | |_| | |__| ___ |   __/\\__ \\\\__ \\  \n"
+                    + "|____/ \\__,_|____|_| |_|\\ ___||___/|___/\n";
+        horizontalLine();
+        System.out.println(logo);
+        horizontalLine();
+        System.out.println("Hello! I'm Duchess.");
+        System.out.println("What can I do for you today?");
+        horizontalLine();
+        System.out.println("Goodbye. Hope to see you again soon!");
+        horizontalLine();
+    }
+
+    //Prints a Horizontal Line of 50 dashes
+    public static void horizontalLine() {
+        int lineLength = 50; // Specify the length of the line
+
+        // Print the horizontal line
+        for (int i = 0; i < lineLength; i++) {
+            System.out.print("_");
+        }
+
+        System.out.println();
     }
 }
