f2f92fb071d4a1086d55aa95ebb90c1b32d04313
Zack-Tay <tayzhongkai@gmail.com>
2024-01-24 23:58:31 +0800

    Add Fredricksen.java and completed level-0

---
+++ src/main/java/Fredricksen.java
@@ -0,0 +1,10 @@
+public class Fredricksen {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Fredricksen");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
