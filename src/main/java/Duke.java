5d276a172a22042d9d0c4e1adb7d89b48aef2fbc
iamtr <jettzeong@gmail.com>
2024-01-24 17:40:20 +0800

    Changed greeting message for Duke
    Changed name to Joseph and also added greeting messages for Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String sep = "__________________________________________";
+        System.out.println(sep);
+        System.out.println("Hello! I'm JOSEPH JOSHTUR!!!");
+        System.out.println("What can I do for you?");
+        System.out.println(sep);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(sep);
     }
+
+
 }
