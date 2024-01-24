23dfff5a65dd68bce1a783dac75c9f708b76f64f
ryanlimdx <ryanlimdx@gmail.com>
2024-01-24 17:28:12 +0800

    added list to store user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,35 +3,49 @@ import java.util.Random;
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
+        Random random = new Random();
+
         String[] catchphrases = {
                 "Yee-haw!",
                 "So long, partner.",
                 "To infinity and beyond!",
                 "Reach for the sky!"
         };
-        Random random = new Random();
 
         String greeting =
                 line +
-                " Hello! I'm Woody\n" +
-                " What can I do for you?\n" +
+                "   Hello! I'm Woody\n" +
+                "   What can I do for you?\n" +
                 line;
 
+        ArrayList<String> userTexts = new ArrayList<>();
+
+        String[] terminateKeywords = {"bye", "BYE", "Bye"};
+        List<String> exitProgramme = Arrays.asList(terminateKeywords);
+
+        // Programme start
         System.out.print(greeting);
 
         Scanner input = new Scanner(System.in);
         String currInput = input.nextLine();
 
-        String[] terminateKeywords = {"bye", "BYE", "Bye"};
-        List<String> exitProgramme = Arrays.asList(terminateKeywords);
-
         while (!exitProgramme.contains(currInput)) {
-            System.out.println(currInput  + "\n" + line);
+
+            if (currInput.equals("list")) { // request for list
+                for (int i = 0; i < userTexts.size(); i++) {
+                    String listIdx = i + 1 + ". ";
+                    System.out.println("    " + listIdx + userTexts.get(i));
+                }
+            } else {                        // tasks
+                userTexts.add(currInput);
+                System.out.println("    added: " + currInput);
+            }
+            System.out.println(line);
             currInput = input.nextLine();
         }
 
         System.out.println(
-                "Bye! " + catchphrases[random.nextInt(catchphrases.length)] + "\n" +
+                "   Bye! " + catchphrases[random.nextInt(catchphrases.length)] + "\n" +
                         line);
     }
 }
