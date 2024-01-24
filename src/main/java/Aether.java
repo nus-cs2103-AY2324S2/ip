505bf7345216c0657279d3c09543e0137710b294
khushi833 <71274442+khushi833@users.noreply.github.com>
2024-01-24 13:52:55 +0800

    echo + study + bye

--- src/main/java/Aether.java
+++ src/main/java/Aether.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class Aether {
+
+    private static void printHorizontalLine() {
+        System.out.println(" _____________________________");
+    }
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -6,11 +12,33 @@ public class Aether {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //                System.out.println("Hello from\n" + logo);
+
+        String chatbotName = "Aether";
         System.out.println("_____________________________");
-        System.out.println("Hello! I'm Aether!");
+        System.out.println("Hello! I'm " + chatbotName + "!");
         System.out.println("What can I do for you?");
         System.out.println("_____________________________");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("_____________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            input = scanner.nextLine();
+            printHorizontalLine();
+            if (input.equalsIgnoreCase("bye")) {
+                printHorizontalLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printHorizontalLine();
+            } else if (input.equalsIgnoreCase("study")) {
+                System.out.println("Sure! What topic do you want to study?");
+            } else {
+                System.out.println(input);
+                printHorizontalLine();
+            }
+
+
+        }
+        while (!input.equalsIgnoreCase("bye"));
+        scanner.close();
     }
 }
+
