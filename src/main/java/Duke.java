5ac0f388d9685b88cd312041e1354735c73a97cf
Tang Hao Liang <tang.hao.liang.thl@gmail.com>
2024-01-24 17:48:03 +0800

    Edited Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String start = "Hello! I'm Unknown \n"
+                + "What can I do for you? \n";
+        String end = "Bye. Hope to see you again soon!\n";
+        String lnBreak = "_______________________________________________________________\n";
+        System.out.println(lnBreak + start + lnBreak + end + lnBreak);
     }
 }
