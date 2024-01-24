16226b49a3b1b342b75ac2641a8aa1b67343529b
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-01-25 02:44:55 +0800

    Add todos, events, deadlines

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public final class Config {
     private Config() {}
 
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Duke {
     public static void main(String[] args) {
@@ -16,9 +16,9 @@ public class Duke {
         while (!userIn.equals("bye")) {
             String echoMsg;
 
-            String[] tokens = userIn.split(" ");
+            String[] tokens = Parser.parse(userIn);
             switch (tokens[0]) {
-                case "list":   // echos stored user inputs
+                case "list":  // list stored tasks
                     StringBuilder strItems = new StringBuilder();
                     for (int i = 0; i < al.size() - 1; i++) {
                         strItems.append(String.format("%d. %s\n\t\t", i + 1, al.get(i)));
@@ -46,12 +46,54 @@ public class Duke {
                                     al.get(itemIndex));
                     break;
                 }
-                default:   // echo and store the user input
-                    al.add(new Task(userIn));
+                case "todo": {  // saves task as todo
+                    Todo todo = new Todo(tokens[1]);
+                    al.add(todo);
                     echoMsg = String.format(
                             Config.PLACEHOLDER,
-                            "added: " + userIn);
+                            "Got it. I've added this task:\n\t\t\t" +
+                                    todo +
+                                    "\n\t\t" +
+                                    "Now you have " +
+                                    al.size() +
+                                    " task(s) in the list."
+                    );
                     break;
+                }
+                case "deadline": {  // saves task as deadline
+                    Deadline deadline = new Deadline(tokens[1], tokens[3]);
+                    al.add(deadline);
+                    echoMsg = String.format(
+                            Config.PLACEHOLDER,
+                            "Got it. I've added this task:\n\t\t\t" +
+                                    deadline +
+                                    "\n\t\t" +
+                                    "Now you have " +
+                                    al.size() +
+                                    " task(s) in the list."
+                    );
+                    break;
+                }
+                case "event": {  // saves task as event
+                    Event event = new Event(tokens[1], tokens[3], tokens[5]);
+                    al.add(event);
+                    echoMsg = String.format(
+                            Config.PLACEHOLDER,
+                            "Got it. I've added this task:\n\t\t\t" +
+                                    event +
+                                    "\n\t\t" +
+                                    "Now you have " +
+                                    al.size() +
+                                    " task(s) in the list."
+                    );
+                    break;
+                }
+                default: {  // no such action
+                    echoMsg = String.format(
+                            Config.PLACEHOLDER,
+                            "no such action, cannot simp for you!");
+                    break;
+                }
             }
             System.out.println(echoMsg);
 
