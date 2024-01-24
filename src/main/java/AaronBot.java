7de0e2a2af9235f64ae7986f1ffc0c80865cd74e
eliotlee68 <eliotlee68@gmail.com>
2024-01-24 20:21:50 +0800

    level 0

---
+++ src/main/java/AaronBot.java
@@ -0,0 +1,5 @@
+public class AaronBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm AaronBot \nWhat can I do for you?\nBye. Hope to see you again soon!");
+    }
+}
