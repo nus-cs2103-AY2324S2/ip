6bedfbb9e0337659aab59cb7c3dce0bbb0616ca6
gosongying <gosongying7@gmail.com>
2024-01-25 00:14:32 +0800

    chatbot can greet the user and exit

---
+++ src/main/java/Harper.java
@@ -0,0 +1,22 @@
+/**
+ * A chatbot called Harper.
+ *
+ * @author gosongying
+ * @version CS2103T AY23/24 Sem 2
+ */
+public class Harper {
+
+    /**
+     * Simply greets the user and exits.
+     */
+    public void greetAndExit() {
+        String line = "_________________________________________________________";
+
+        System.out.println(line + "\n"
+                + "Hello! I am Harper.\n"
+                + "What can I do for you?\n"
+                + line + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + line);
+    }
+}
