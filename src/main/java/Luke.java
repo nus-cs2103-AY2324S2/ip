39951edbc6c5d4023e699f45c9f47caf1505f7b4
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2024-01-24 17:10:34 +0800

    add tasklist

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Luke {
 
     //Logo created using https://patorjk.com/software/taag/#p=display&f=Varsity&t=Luke
-    public static String logo = "  _____             __             \n"
+    private static String logo = "  _____             __             \n"
             + " |_   _|           [  |  _         \n"
             + "   | |     __   _   | | / ] .---.  \n"
             + "   | |   _[  | | |  | '' < / /__\\\\ \n"
             + "  _| |__/ || \\_/ |, | |`\\ \\| \\__., \n"
             + " |________|'.__.'_/[__|  \\_]'.__.' ";
 
-    public static void greet() {
+    private static ArrayList<String> history = new ArrayList<>();
+
+    private static void greet() {
         System.out.println("Hello! I'm\n" + logo + "\n");
     }
 
-    public static void bye() {
+    private static void bye() {
         System.out.println("Don't be ridiculous!\n" +
                 "It's... it's not like I want to see you again or anything!\n");
     }
@@ -23,14 +26,34 @@ public class Luke {
         greet();
         Scanner sc = new Scanner(System.in);
         while (true) {
-            //echo mode
-            String cmd = sc.nextLine();
-            System.out.println(">>> " + cmd);
-            if (cmd.equals("bye")) {
+            //task mode
+            String task = sc.nextLine();
+            if (task.equals("bye")) {
                 bye();
                 sc.close();
                 break;
+            } else if (task.equals("list")) {
+                int num = 1;
+                for (String s : history) {
+                    System.out.println(num + ". " + s);
+                    num += 1;
+                }
+            } else {
+                history.add(task);
+                System.out.println(">>> Added: " + task);
             }
         }
+//        while (true) {
+//            //echo mode
+//            String cmd = sc.nextLine();
+//            System.out.println(">>> " + cmd);
+//            if (cmd.equals("bye")) {
+//                bye();
+//                sc.close();
+//                break;
+//            }
+//        }
     }
 }
+
+
