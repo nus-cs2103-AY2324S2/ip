da8c99e1d497da0d68b712b4913f8254aa00b53b
saiutkarsh33 <122259179+saiutkarsh33@users.noreply.github.com>
2024-01-24 11:06:46 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Hello! I'm TFamilyBot\n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n";     
+        System.out.println(logo);
     }
 }
