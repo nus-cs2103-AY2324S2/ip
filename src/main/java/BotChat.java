5fdea35fff91972a377bc47631ae843a91503e64
kaitinghh <114789615+kaitinghh@users.noreply.github.com>
2024-01-25 01:08:16 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/BotChat.java
@@ -0,0 +1,6 @@
+public class BotChat {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm BotChat.\n What can I do for you?\n Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+    }
+}
