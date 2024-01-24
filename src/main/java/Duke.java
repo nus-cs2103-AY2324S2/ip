54fb49c6ac2ae8be93eb7b62479b82c04813f5a2
yap-zong-xin <yapzongxin@hotmail.com>
2024-01-24 18:57:04 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Yappy\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+    public static void main(String[] args) throws IOException {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Yappy\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while(true) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String input = br.readLine();
+
+            if(input.equals("bye")) {
+                break;
+            }
+
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + input);
+            System.out.println("    ____________________________________________________________");
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
