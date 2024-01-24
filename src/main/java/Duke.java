5f3c8b10c2424b073ce4eca98dc8be2b86a71b80
ashleyclx <e0958221@u.nus.edu>
2024-01-24 16:17:39 +0800

    Changed bye text and changed to function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
+    private static final String LINE = "____________________________________________________________\n";
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
         String logo = "   :::   :::           :::        :::::::::       :::::::::       ::::::::::       ::::::::: \n" +
                 "  :+:   :+:         :+: :+:      :+:    :+:      :+:    :+:      :+:              :+:    :+: \n" +
                 "  +:+ +:+         +:+   +:+     +:+    +:+      +:+    +:+      +:+              +:+    +:+  \n" +
@@ -8,8 +8,12 @@ public class Duke {
                 "  +#+          +#+     +#+    +#+             +#+             +#+              +#+    +#+    \n" +
                 " #+#          #+#     #+#    #+#             #+#             #+#              #+#    #+#     \n" +
                 "###          ###     ###    ###             ###             ##########       ###    ###      \n\n";
-        System.out.println(line + " \uD83D\uDC4B✨What's poppin' fam, it's ya boi \n\n" + logo + "Hit me up with those deets and let's vibe together! \uD83D\uDCAF\uD83D\uDD25\uD83D\uDE1C");
-        System.out.print(line + "Bye! Talk to you again later!\n" + line);
+        System.out.println(Duke.LINE + " \uD83D\uDC4B✨What's poppin' fam, it's ya boi \n\n" + logo + "Hit me up with those deets and let's vibe together! \uD83D\uDCAF\uD83D\uDD25\uD83D\uDE1C");
+        Duke.bye();
+    }
+
+    public static void bye() {
+        System.out.print(Duke.LINE + "Peace out, fam!✌️ Stay lit and keep those good vibes rollin'! \uD83D\uDE80\uD83D\uDC96\n" + Duke.LINE);
     }
 }
 
