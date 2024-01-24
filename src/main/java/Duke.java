890e9f6f9c2bbe359803e6bfe5a1fd618c88add3
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-24 17:19:43 +0800

    Removed unnecessary comments for Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Jojo");
+        System.out.println("Hello! I'm Jojo :)");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        String userTxt = sc.nextLine();
-        System.out.println("added: " + userTxt);
         System.out.println("Bye. Hope to see you again soon!");
-
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
     }
 }
