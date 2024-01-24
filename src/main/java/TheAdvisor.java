3f46c94b97da19ad4c5a36a6d3d217eabbb951fe
HENRY\YS <yusoonng3@gmail.com>
2024-01-24 15:11:38 +0800

    Added an echo function

--- src/main/java/TheAdvisor.java
+++ src/main/java/TheAdvisor.java
@@ -1,8 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TheAdvisor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String intro = "Hello, I am TheAdvisor. The one and only advisor you will ever need in your investing " +
                 "journey. What can I do for you?";
-        String out = "Thank you for using TheAdvisor. Have a great day";
-        System.out.println(intro + "\n" + out);
+        System.out.println(intro + "\n");
+
+        while (true) {
+            String toEcho = br.readLine();
+            if (toEcho.equals("bye")) {
+                System.out.println("    Goodbye. Thank you for using TheAdvisor chatbox and I hope that my advice has managed" +
+                        "to help you in your investing journey!");
+                break;
+            } else {
+                System.out.println("    " + toEcho);
+            }
+        }
     }
 }
