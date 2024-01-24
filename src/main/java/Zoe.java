26e13874ec001b66abf00e336ac7276a635be64e
DOMOPC\domin <Dominiclimyongen1@gmail.com>
2024-01-24 21:27:41 +0800

    Added the echo capability, bot will only terminate when the command "bye" is given.

--- src/main/java/Zoe.java
+++ src/main/java/Zoe.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 public class Zoe {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Zoe");
         System.out.println("What can I do for you?");
-        System.out.println("\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = scanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
