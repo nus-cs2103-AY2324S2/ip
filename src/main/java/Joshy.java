0ca5b419014d33d1470f647c2731acc683c73987
Joshy837 <joshuayipsujun@gmail.com>
2024-01-25 00:47:12 +0800

    Rename, greet, exit

---
+++ src/main/java/Joshy.java
@@ -0,0 +1,10 @@
+public class Joshy {
+    public static void main(String[] args) {
+        System.out.println("______________________________________________");
+        System.out.println("Hello! I'm Joshy");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________");
+    }
+}
