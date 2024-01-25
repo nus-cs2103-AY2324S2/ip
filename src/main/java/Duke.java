cb1cfe064af4e2a50c8ff771bd4731576b65d22b
Rahul <sithanathan.rahul@gmail.com>
2024-01-25 23:15:14 +0800

    Level-1 code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = 
                         " _____   _____  _    _ \n"
                         + "|  __ \\ / ____|| |  | |\n"
@@ -8,7 +11,18 @@ public class Duke {
                         + "| | \\ \\ ____) || |  | |\n"
                         + "|_|  \\_|_____/ |_|  |_|\n";
         System.out.println(layer("Hello! I'm \n" + logo));
-        System.out.println(layer("Bye. Hope to see you again soon!"));
+        while (true) {
+            String input = sc.nextLine(); // Read user input
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(layer("Bye. Hope to see you again soon!"));
+                break; // Break the loop to end the program
+            }
+
+            System.out.println(layer(input)); // Echo the input
+        }
+
+        sc.close(); // Close the scanner
     }
 
     public static String layer(String s) {
