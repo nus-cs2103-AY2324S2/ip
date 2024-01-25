9e51482eda1fcccc727bd54531a658d147f0f0b8
cleowenxuan <>
2024-01-25 12:45:29 +0800

    Level3 Completed

--- src/main/java/Asher.java
+++ src/main/java/Asher.java
@@ -1,48 +1,85 @@
 import java.util.Scanner;
 
 public class Asher {
-    public String name;
-    private static String[] userCommands = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int count = 0;
 
-    public static void greet() {
+    private static void greet() {
         System.out.println("Hello! I'm Asher. What can I do for you?");
     }
 
-    public static void exit() {
+    private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void echoCommand(String command) {
+    private static void echoCommand(String command) {
         System.out.println("added:" + " " + command);
     }
 
-    public static boolean isExitCommand(String command) {
-        return command.toLowerCase().equals("bye");
-    }
-
-    public static void addCommand(String command) {
-        if (count < userCommands.length) {
-            userCommands[count] = command;
+    private static void addTask(String task) {
+        Task newTask = new Task(task);
+        if (count < tasks.length) {
+            tasks[count] = newTask;
             count++;
         } else {
-            System.out.println("Task is full, unable to add more");
+            System.out.println("Task List is full, unable to add more.");
         }
     }
 
-    public static void displayList() {
+    private static void displayTasks() {
+        System.out.println("Here are the tasks in your list:");
+
         for (int i = 0; i < count; i++) {
-            System.out.println((i + 1) + "." + " " + userCommands[i]);
+            System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + tasks[i]);
+        }
+    }
+
+    private static int getTaskNumber(String task) {
+        String[] word = task.split(" ");
+        if (word.length == 2) {
+            int number = Integer.parseInt(word[1]) - 1;
+            if (number >= 0 && number < count) {
+                return number;
+            }
+        }
+        return -1;
+    }
+
+    private static void markTaskDone(String task) {
+        int taskNumber = getTaskNumber(task);
+        if (taskNumber != -1) {
+            tasks[taskNumber].markDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + "[X]" + " " + tasks[taskNumber]);
+        } else {
+            System.out.println("Invalid Task!");
+        }
+    }
+
+    private static void markTaskUndone(String task) {
+        int taskNumber = getTaskNumber(task);
+        if (taskNumber != -1) {
+            tasks[taskNumber].markUndone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + "[X]" + " " + tasks[taskNumber]);
+        } else {
+            System.out.println("Invalid Task!");
         }
     }
 
     public static void processCommand(String command) {
-        if (isExitCommand(command)) {
+        String[] word = command.split(" ");
+
+        if (word[0].equals("bye")) {
             Asher.exit();
-        } else if (command.equals("list")) {
-            displayList();
+        } else if (word[0].equals("list")) {
+            displayTasks();
+        } else if (word[0].equals("mark")) {
+            markTaskDone(command);
+        } else if (word[0].equals("unmark")) {
+            markTaskUndone(command);
         } else {
-            addCommand(command);
+            addTask(command);
             echoCommand(command);
         }
     }
@@ -56,6 +93,6 @@ public class Asher {
             command = scanner.nextLine();
             Asher.processCommand(command);
 
-        } while (!Asher.isExitCommand(command));
+        } while (!command.equals("bye"));
     }
 }
