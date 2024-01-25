fb2d401cc5cb1f4649b979a1d35b916529f86de4
Irwyn <irwynliong01@gmail.com>
2024-01-25 10:46:39 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Irwyn.java
@@ -0,0 +1,16 @@
+public class Irwyn {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Irwyn\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
+
+
+
+
+
