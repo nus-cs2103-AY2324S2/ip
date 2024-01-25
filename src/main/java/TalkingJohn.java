4f4aef6ee82dabaec891d2385be471d30a0af08d
John Yoo <e0968839@u.nus.edu>
2024-01-25 18:18:34 +0800

    Implemented TalkingJohn Level3

--- src/main/java/TalkingJohn.java
+++ src/main/java/TalkingJohn.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class TalkingJohn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] stringList = new String[100];
+        Task[] TaskArr = new Task[100];
 
         System.out.println("Hello, I am TalkingJohn\n"
                 + "What can I do for you?\n");
@@ -15,17 +15,58 @@ public class TalkingJohn {
             if (Objects.equals(input, "bye")) {
                 System.out.println("Bye, hope to see you soon");
                 break;
-            } else if (Objects.equals(input, "list")){
+            } else if (Objects.equals(input, "list")) {
                 for (int i = 0; i < j; i++) {
-                    String nthElement = stringList[i];
-                    System.out.println((i + 1) + ". " + nthElement + "\n");
+                    Task nthElement = TaskArr[i];
+                    System.out.println((i + 1) + ". " + nthElement.getStatusIcon() + "\n");
+                }
+            } else if (input.length() > 4 && Objects.equals(input.substring(0, 4), "mark")) {
+                if (input.charAt(4) == ' ') {
+                    String[] parts = input.split(" ", 2);
+                    String integer = parts[1];
+                    try {
+                        int i = Integer.parseInt(integer);
+                        Task task_to_mark = TaskArr[i - 1];
+                        task_to_mark.mark();
+                        System.out.println("Nice! I've marked this task as done:\n"
+                                + task_to_mark.getStatusIcon());
+                    } catch (NumberFormatException e) {
+                        TaskArr[j] = new Task(input);
+                        System.out.println("added: " + input);
+                        j++;
+                    }
+                } else {
+                    TaskArr[j] = new Task(input);
+                    System.out.println("added: " + input);
+                    j++;
+                }
+            } else if (input.length() > 6 && Objects.equals(input.substring(0, 6), "unmark")) {
+                if (input.charAt(6) == ' ') {
+                    String[] parts = input.split(" ", 2);
+                    String integer = parts[1];
+                    try {
+                        int i = Integer.parseInt(integer);
+                        Task task_to_unmark = TaskArr[i - 1];
+                        task_to_unmark.unMark();
+                        System.out.println("OK, I've marked this task as not done yet:\n"
+                                + task_to_unmark.getStatusIcon());
+                    } catch (NumberFormatException e) {
+                        TaskArr[j] = new Task(input);
+                        System.out.println("added: " + input);
+                        j++;
+                    }
+                } else {
+                    TaskArr[j] = new Task(input);
+                    System.out.println("added: " + input);
+                    j++;
                 }
             } else {
-                stringList[j] = input;
-                j++;
+                TaskArr[j] = new Task(input);
                 System.out.println("added: " + input);
+                j++;
             }
         }
         scanner.close();
     }
 }
+
