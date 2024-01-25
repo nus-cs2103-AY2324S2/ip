5411fc1e9d11a8645ca05335dc0c15a7bdee7bc3
rertyy <nxtms3@gmail.com>
2024-01-25 08:57:24 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo =
                 "____________________________________________________________\n" +
                         " Hello! I'm Numerator\n" +
                         " What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        String bye = "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(logo);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(bye);
+                break;
+            }
+
+            System.out.println(input);
+
+
+        }
     }
 }
