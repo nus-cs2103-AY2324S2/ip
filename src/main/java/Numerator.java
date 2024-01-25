8c3cfd298ae4c524cead2b27680c50ff8748ca37
rertyy <nxtms3@gmail.com>
2024-01-25 12:45:36 +0800

    fix regex errors

--- src/main/java/Numerator.java
+++ src/main/java/Numerator.java
@@ -26,40 +26,52 @@ public class Numerator {
                     System.out.println("Bye. Hope to see you again soon!\n");
                     System.out.println(line);
                     break;
-                } else if (input.startsWith("mark ")) {
-                    String[] inputArray = input.split(" ");
+                } else if (input.startsWith("mark")) {
                     try {
-                        int taskNum = Integer.parseInt(inputArray[1]) - 1;
-                        taskList.markAsDone(taskNum);
-                        System.out.println("Nice! I've marked this task as done:");
-                        taskList.printTask(taskNum);
-                    } catch (NumberFormatException e) {
-                        System.out.print("Please input a valid number");
+                        Pattern p = Pattern.compile("mark (\\d+)");
+                        Matcher m = p.matcher(input);
+                        if (!m.find()) {
+                            System.out.println("Please use the format: mark <task number>");
+                        } else {
+                            int taskNum = Integer.parseInt(m.group(1)) - 1;
+                            taskList.markAsDone(taskNum);
+                            System.out.println("Nice! I've marked this task as done:");
+                            taskList.printTask(taskNum);
+                        }
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Task does not exist");
                     }
 
-                } else if (input.startsWith("unmark ")) {
-                    String[] inputArray = input.split(" ");
+                } else if (input.startsWith("unmark")) {
                     try {
-                        int taskNum = Integer.parseInt(inputArray[1]) - 1;
-                        taskList.markAsUndone(taskNum);
-                        System.out.println("OK, I've marked this task as not done yet:");
-                        taskList.printTask(taskNum);
-                    } catch (NumberFormatException e) {
-                        System.out.print("Please input a valid number");
+                        Pattern p = Pattern.compile("unmark (\\d+)");
+                        Matcher m = p.matcher(input);
+                        if (!m.find()) {
+                            System.out.println("Please use the format: unmark <task number>");
+                        } else {
+                            int taskNum = Integer.parseInt(m.group(1)) - 1;
+                            taskList.markAsUndone(taskNum);
+                            System.out.println("OK, I've marked this task as not done yet:");
+                            taskList.printTask(taskNum);
+                        }
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Task does not exist");
                     }
-                } else if (input.startsWith("todo ")) {
-                    String taskDesc = input.substring("todo ".length());
-                    Task t = taskList.addToDo(taskDesc);
-                    taskList.printAddTask(t);
-
-                } else if (input.startsWith("deadline ")) {
-                    Pattern p = Pattern.compile("deadline (.+) /by (.+)");
+                } else if (input.startsWith("todo")) {
+                    Pattern p = Pattern.compile("todo (\\S+.*)");
                     Matcher m = p.matcher(input);
-                    if (!m.matches()) {
+                    if (!m.find()) {
+                        System.out.println("Please use the format: todo <task>");
+                    } else {
+                        String taskDesc = m.group(1);
+                        Task t = taskList.addToDo(taskDesc);
+                        taskList.printAddTask(t);
+                    }
+
+                } else if (input.startsWith("deadline")) {
+                    Pattern p = Pattern.compile("deadline (\\S+.*) /by (\\S+.*)");
+                    Matcher m = p.matcher(input);
+                    if (!m.find()) {
                         System.out.println("Please use the format: deadline <task> /by <date>");
                     } else {
                         String taskDesc = m.group(1);
@@ -68,10 +80,10 @@ public class Numerator {
                         taskList.printAddTask(t);
                     }
 
-                } else if (input.startsWith("event ")) {
-                    Pattern p = Pattern.compile("event (.+) /from (.+) /to (.+)");
+                } else if (input.startsWith("event")) {
+                    Pattern p = Pattern.compile("event (\\S+.*) /from (\\S+.*) /to (\\S+.*)");
                     Matcher m = p.matcher(input);
-                    if (!m.matches()) {
+                    if (!m.find()) {
                         System.out.println("Please use the format: event <task> /from <date> /to <date>");
                     } else {
                         String taskDesc = m.group(1);
