a699e366b6da96a1115eed17e503b5d438c9ad83
JustWeiHao <e0960531@u.nus.edu>
2024-01-25 17:22:44 +0800

    Modify Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    public static final String botName = "Raphael";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\n"
+                + "  _____                _                   _ \n"
+                + " |  __ \\              | |                 | |\n"
+                + " | |__) | __ _  _ __  | |__    __ _   ___ | |\n"
+                + " |  _  / / _` || '_ \\ | '_ \\  / _` | / _ \\| |\n"
+                + " | | \\ \\| (_| || |_) || | | || (_| ||  __/| |\n"
+                + " |_|  \\_\\\\__,_|| .__/ |_| |_| \\__,_| \\___||_|\n"
+                + "               | |                           \n"
+                + "               |_|                           \n";
+        System.out.println(logo);
+        Ui.start();
+        Ui.end();
     }
 }
