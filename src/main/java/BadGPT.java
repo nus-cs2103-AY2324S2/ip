cd96e5eb23bb67812e3a017599a9163b02318293
Ronn <ronnng1510@gmail.com>
2024-01-25 21:53:22 +0800

    Level-2
    - Line printing is now handled in a method
    - Added functionality to store user input and display it in order when requested

--- src/main/java/BadGPT.java
+++ src/main/java/BadGPT.java
@@ -1,37 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BadGPT {
     public static void main(String[] args) {
         String name = "BadGPT";
+        List<String> tasks = new ArrayList<>(100);
 
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm " + name + ".\n" + "What can I do for you?\n" +
-                "____________________________________________________________");
+        line();
+        System.out.println("Hello! I'm " + name + ".\n" + "What can I do for you?");
+        line();
 
         while (true) {
             Scanner sc = new Scanner(System.in);
             String cmd = sc.next();
 
-            // Exits the program if user enters "bye". Else, echoes the input.
+            // If user enters "bye", exit the loop and the program.
+            // If user enters "list", list out all currently stored tasks.
+            // Else, store the string entered as a task.
             if (cmd.equals("bye")) {
                 break;
+            } else if (cmd.equals("list")){
+                list(tasks);
             } else {
-                echo(cmd);
+                String task = cmd + sc.nextLine();
+                store(task, tasks);
             }
         }
 
         bye();
     }
 
-    public static void echo(String input) {
-        System.out.println("____________________________________________________________\n" +
-                input + "\n____________________________________________________________\n");
+    public static void line() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void store(String task, List<String> tasks) {
+        tasks.add(task);
+        line();
+        System.out.println("Added: " + task);
+        line();
+    }
+
+    public static void list(List<String> tasks) {
+        line();
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        line();
     }
 
     public static void bye() {
-        System.out.println("____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        line();
+        System.out.println("Bye. Hope to see you again soon!");
+        line();
         System.exit(0);
     }
 }
