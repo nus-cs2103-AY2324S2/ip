5c6a18234d376ba9e42c4e8470de94b2ea906429
shunjieee <shunjie2000@gmail.com>
2024-01-26 02:07:58 +0800

    Duke.java: Add Level-3 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import helperpackage.Task;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Duke {
     public static void main(String[] args) {
@@ -18,25 +21,52 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (!userInput.toLowerCase().equals("bye")) {
             
             if (userInput.toLowerCase().equals("list")) {
                 System.out.println("\n________________________________________");
+                System.out.println("Here are the tasks in your list:");
 
                 for (int i = 1; i <= taskList.size(); i++) {
-                    System.out.println(i + ". " + taskList.get(i - 1));
+                    Task t = taskList.get(i - 1);
+                    System.out.println(i + ". " + t.toString());
                 }
 
                 System.out.println("________________________________________\n");
 
             } else {
-                taskList.add(userInput);
+                StringTokenizer st = new StringTokenizer(userInput);
+                String cmd = st.nextToken().toLowerCase();
 
-                System.out.println("\n________________________________________");
-                System.out.println("added:" + userInput);
-                System.out.println("________________________________________\n");
+                if (cmd.equals("mark")) {
+                    int index = Integer.parseInt(st.nextToken());
+                    Task t = taskList.get(index - 1);
+                    t.markAsDone();
+                    
+                    System.out.println("\n________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + t.toString());
+                    System.out.println("________________________________________\n");
+
+                } else if (cmd.equals("unmark")) {
+                    int index = Integer.parseInt(st.nextToken());
+                    Task t = taskList.get(index - 1);
+                    t.unmark();
+                    
+                    System.out.println("\n________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + t.toString());
+                    System.out.println("________________________________________\n");
+
+                } else {
+                    taskList.add(new Task(userInput));
+
+                    System.out.println("\n________________________________________");
+                    System.out.println("added:" + userInput);
+                    System.out.println("________________________________________\n");
+                }
             }
 
             userInput = scanner.nextLine();
