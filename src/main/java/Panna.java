8d3954b29b791410eb95b40dbee48f074cf88e43
AjayArvind2207 <alwayssactheexchange@gmail.com>
2024-01-26 03:00:46 +0800

    Revert ". Added Exception handling to each of the features!"
    This reverts commit 67d729c0bf9d4a56cb426c90e2299b9c9411d2a1.

--- src/main/java/Panna.java
+++ src/main/java/Panna.java
@@ -8,7 +8,7 @@ public class Panna {
 
 
 
-    public static void main(String[] args) throws PannaException {
+    public static void main(String[] args) {
 
         ArrayList<Task> tasks = new ArrayList<>();
 
@@ -39,89 +39,66 @@ public class Panna {
 
             else if (Panna.command.equals("mark")) {
                 System.out.println("Which one should I mark? Write the label number :] ");
-                try {
-                    int label = s.nextInt();
-                    tasks.get(label - 1).setDone(true);
+                int label = s.nextInt();
+                tasks.get(label-1).setDone(true);
 
-                    System.out.println("----------------------------------------------------------");
-                    System.out.println("Nice! I've marked this task as done: \n"
-                            + tasks.get(label - 1));
-                    System.out.println("----------------------------------------------------------");
+                System.out.println("----------------------------------------------------------");
+                System.out.println("Nice! I've marked this task as done: \n"
+                        +  tasks.get(label-1));
+                System.out.println("----------------------------------------------------------");
 
-                }
-                catch (Exception e) {
-                    throw new PannaException("Invalid label! The number of tasks now is " + tasks.size() +
-                            "\nPlease try with a more appropriate value! ");
-                }
+
             }
 
             else if (Panna.command.equals("unmark")) {
                 System.out.println("Which one should I unmark? Write the label number :] ");
-                try {
-                    int label = s.nextInt();
-                    tasks.get(label - 1).setDone(false);
+                int label = s.nextInt();
+                tasks.get(label-1).setDone(false);
 
-                    System.out.println("----------------------------------------------------------");
-                    System.out.println("Okay! I've unmarked this task as done: \n"
-                            + "[X] " + tasks.get(label - 1));
-                    System.out.println("----------------------------------------------------------");
-                }
-                catch (Exception e) {
-                    throw new PannaException("Invalid label! The number of tasks now is" + tasks.size() +
-                                                "\nPlease try with a more appropriate value! ");
-                }
+                System.out.println("----------------------------------------------------------");
+                System.out.println("Okay! I've unmarked this task as done: \n"
+                        + "[X] " + tasks.get(label-1));
+                System.out.println("----------------------------------------------------------");
+
 
             }
 
             else if (Panna.command.equals("event")) {
-                try {
-                    System.out.println("What kind of event? ");
-                    String input = s.nextLine();
-                    System.out.println("When does it start? ");
-                    String start = s.nextLine();
-                    System.out.println("When does it end? ");
-                    String end = s.nextLine();
-                    Task t = new Event(input, start, end);
-                    tasks.add(t);
-                    System.out.println("Got it! I've added the \n" + t + "\n event!");
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list! ");
-                }
-                catch (Exception e) {
-                    throw new PannaException("All inputs must be Strings! Please ensure it is not empty :D");
-                }
+                System.out.println("What kind of event? ");
+                String input = s.nextLine();
+                System.out.println("When does it start? ");
+                String start = s.nextLine();
+                System.out.println("When does it end? ");
+                String end = s.nextLine();
+                Task t = new Event(input, start, end);
+                tasks.add(t);
+                System.out.println("Got it! I've added the \n" + t + "\n event!");
+                System.out.println("Now you have " + tasks.size() + " task(s) in the list! ");
 
             }
 
             else if (Panna.command.equals("todo")) {
-                try {
                     System.out.println("What kind of todo? ");
                     String input = s.nextLine();
                     Task t = new Todo(input);
                     tasks.add(t);
                     System.out.println("Got it! I've added the \n" + t + "\n todo!");
                     System.out.println("Now you have " + tasks.size() + " task(s) in the list! ");
-                }
-                catch (Exception e){
-                    throw new PannaException("All inputs must be Strings! Please ensure it is not empty :D");
-                }
+
             }
 
             else if (Panna.command.equals("deadline")) {
-                try {
-                    System.out.println("What kind of deadline? ");
-                    String input = s.nextLine();
-                    System.out.println("When is the deadline? ");
-                    String deadline = s.nextLine();
+                System.out.println("What kind of deadline? ");
+                String input = s.nextLine();
+                System.out.println("When is the deadline? ");
+                String deadline = s.nextLine();
 
-                    Task t = new Deadline(input, deadline);
-                    tasks.add(t);
+                Task t = new Deadline(input, deadline);
+                tasks.add(t);
 
-                    System.out.println("Got it! I've added the \n" + t + "\n deadline!");
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list! ");
-                }
-                catch (Exception e) {
-                    throw new PannaException("All inputs must be Strings! Please ensure it is not empty :D");
-                }
+                System.out.println("Got it! I've added the \n" + t + "\n deadline!");
+                System.out.println("Now you have " + tasks.size() + " task(s) in the list! ");
+
 
             }
 
--- src/main/java/PannaException.java
+++
@@ -1,6 +0,0 @@
-public class PannaException extends Exception{
-    public PannaException(String errorMessage) {
-        super(errorMessage);
-    }
-
-}
