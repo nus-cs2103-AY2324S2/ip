562553ac1a5acd09fe79b3ed12983c468cf84bfa
meowwtama <122257205+meowwtama@users.noreply.github.com>
2024-01-25 13:34:11 +0800

    Level 0. Rename, Greet, Exit
    Added the greet() and line() methods.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke.greet();
+    }
+
+    public static void greet() {
+        Duke.line();
+        System.out.println("Hello! I'm Anita MaxWynn");
+        System.out.println("What can I do for you?");
+        Duke.line();
+        System.out.println("Bye. Hope to see you again soon!");
+        Duke.line();
+    }
+
+    public static void line() {
+        System.out.println("____________________________________________________________");
     }
 }
