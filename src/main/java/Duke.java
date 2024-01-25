6358858f7a0cd4c8bc37f37ced2523a618bdad76
sdevih <e0960282@u.nus.edu>
2024-01-25 23:37:26 +0800

    Added Leevl-5 with error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,7 +22,7 @@ public class Duke {
         System.out.println("OK, I've marked this task as not done yet:\n" + listStore[taskNum - 1].toString() + "\n");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello! I'm Chatteroo\n" + "What can I do for you?\n");
 
         //Chatbot stores user commands in a fixed array
@@ -33,47 +33,65 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            // print list of stored tasks if user inputs "list"
-            if (input.equals("list")) {
-                printTasks(listStore, listCount);
-            } else if (input.startsWith("mark")) { // mark task as done
-                String[] inputArr = input.split(" ");
-                int taskNum = Integer.parseInt(inputArr[1]); //retrieve task number from input array
-                markTaskAsDone(listStore, taskNum);
-            } else if (input.startsWith("unmark")) { // mark task as not done
-                String[] inputArr = input.split(" ");
-                int taskNum = Integer.parseInt(inputArr[1]); //retrieve task number from input array
-                markTaskAsNotDone(listStore, taskNum);
-            } else {
-                // add user inputs to list based on what task it is accordingly
-                Task newTask = new Task(input);
-                if (input.startsWith("todo")) {
-                    input = input.substring(5);
-                    newTask = new ToDo(input);
-                } else if (input.startsWith("deadline")) {
-                    String[] inputArr = input.substring(9).split("/by");
-                    input = inputArr[0];
-                    String by = inputArr[1];
-                    newTask = new Deadline(input, by);
-                } else if (input.startsWith("event")) {
-                    input = input.substring(6);
-                    String[] inputArr = input.split("/from");
-                    input = inputArr[0];
-                    String[] timeArr = inputArr[1].split("/to");
-                    String from = timeArr[0];
-                    String to = timeArr[1];
-                    newTask = new Event(input, from, to);
+            try {
+                // print list of stored tasks if user inputs "list"
+                if (input.equals("list")) {
+                    printTasks(listStore, listCount);
+                } else if (input.startsWith("mark")) { // mark task as done
+                    String[] inputArr = input.split(" ");
+                    int taskNum = Integer.parseInt(inputArr[1]); //retrieve task number from input array
+                    markTaskAsDone(listStore, taskNum);
+                } else if (input.startsWith("unmark")) { // mark task as not done
+                    String[] inputArr = input.split(" ");
+                    int taskNum = Integer.parseInt(inputArr[1]); //retrieve task number from input array
+                    markTaskAsNotDone(listStore, taskNum);
+                } else {
+                    // add user inputs to list based on what task it is accordingly
+                    Task newTask = null;
+                    if (input.startsWith("todo")) {
+                        if (input.length() < 5) {
+                            throw new Exception("ChatterOOHNOO! A todOO's description cannot be empty!");
+                        }
+                        input = input.substring(5);
+                        newTask = new ToDo(input);
+                    } else if (input.startsWith("deadline")) {
+                        if (input.length() < 9) {
+                            throw new Exception("ChatterOOHNOO! A deadline's description cannot be empty!");
+                        }
+                        String[] inputArr = input.substring(9).split("/by");
+                        input = inputArr[0];
+                        String by = inputArr[1];
+                        newTask = new Deadline(input, by);
+                    } else if (input.startsWith("event")) {
+                        if (input.length() < 6) {
+                            throw new Exception("ChatterOOHNOO! An event's description cannot be empty!");
+                        }
+                        input = input.substring(6);
+                        String[] inputArr = input.split("/from");
+                        input = inputArr[0];
+                        String[] timeArr = inputArr[1].split("/to");
+                        String from = timeArr[0];
+                        String to = timeArr[1];
+                        newTask = new Event(input, from, to);
+                    } else {
+                        throw new Exception("ChatterOOHNOO! Chatteroo doesn't understand what yoo mean!");
+                    }
+                    if (newTask != null) {
+                        listStore[listCount] = newTask;
+                        listCount++;
+                        System.out.println("Got it. I've added this task:\n" + newTask.toString());
+                        if (listCount == 1) {
+                            System.out.println("Now you have " + listCount + " task in the list.\n");
+                        } else
+                            System.out.println("Now you have " + listCount + " tasks in the list.\n");
+                    }
                 }
-                listStore[listCount] = newTask;
-                listCount++;
-                System.out.println("Got it. I've added this task:\n" + newTask.toString());
-                if (listCount == 1) {
-                    System.out.println("Now you have " + listCount + " task in the list.\n");
-                } else
-                    System.out.println("Now you have " + listCount + " tasks in the list.\n");
-            }
 //            System.out.println("added: " + input + "\n");
-            input = sc.nextLine();
+                input = sc.nextLine();
+            } catch (IllegalArgumentException e) {
+                throw new Exception("ChatterOOHNOO! Chatteroo doesn't understand what yoo mean!");
+            }
+
         }
         //Chatbot exits
         System.out.println("Bye. Hope to see you again soon!\n");
