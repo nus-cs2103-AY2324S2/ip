4ae5d5e3767e0fe4cde38fe1b2777d4ff872e24d
Jaspertzx <jaspertzx905@gmail.com>
2024-01-25 15:54:52 +0800

    Changed name of bot and included more System prints

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____        _     ____        _   \n"
+                + " |  _ \\      | |   |  _ \\      | |  \n"
+                + " | |_) | ___ | |__ | |_) | ___ | |_ \n"
+                + " |  _ < / _ \\| '_ \\|  _ < / _ \\| __|\n"
+                + " | |_) | (_) | |_) | |_) | (_) | |_ \n"
+                + " |____/ \\___/|_.__/|____/ \\___/ \\__|\n";
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
