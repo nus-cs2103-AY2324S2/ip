db395684a40c7211d6a2310d7c252daa194a2a17
Zoe <za.zoeang@gmail.com>
2024-01-25 23:57:24 +0800

    Added list functionality

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Alpaca {
     static String name = "Alpaca";
+    static Scanner scanner;
+    static ArrayList<String> list = new ArrayList<String>();
 
     private static void divider() {
         System.out.println("____________________________________________________________");
@@ -19,22 +22,40 @@ public class Alpaca {
         divider();
     }
 
-    private static void processInput() {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        if (line.equals("bye")) {
-            scanner.close();
-            bye();
-            return;
+    private static void list() {
+        divider();
+        int counter = 1;
+        for (String i : list) {
+            System.out.println(counter + ". " + i);
+            counter++;
         }
         divider();
-        System.out.println(line);
+    }
+
+    private static void add(String item) {
         divider();
+        System.out.println("added: " + item);
+        list.add(item);
+        divider();
+    }
+
+    private static void processInput() {
+        String line = scanner.nextLine();
+        if (line.equals("bye")) {
+            bye();
+            return;
+        } else if (line.equals("list")) {
+            list();
+        } else {
+            add(line);
+        }
         processInput();
     }
     
     public static void main(String[] args) {
         greeting();
+        scanner = new Scanner(System.in);
         processInput();
+        scanner.close();
     }
 }
