6e18f30b2d4a35f3615e31d4c9fa11d3f38eacb1
Ronn <ronnng1510@gmail.com>
2024-01-25 21:19:12 +0800

    Level-0
    - Renamed Duke to BadGPT
    - Added greeting and exit

---
+++ src/main/java/BadGPT.java
@@ -0,0 +1,17 @@
+public class BadGPT {
+    public static void main(String[] args) {
+        String name = "BadGPT";
+
+        System.out.println("____________________________________________________________\n" +
+                "Hello! I'm " + name + ".\n" + "What can I do for you?");
+
+        bye();
+    }
+
+    public static void bye() {
+        System.out.println("____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+        System.exit(0);
+    }
+}
