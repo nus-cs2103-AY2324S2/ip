15ef225e271767857b1568fd65ec79af46b0cac9
ChenXy128 <e0968917@u.nus.edu>
2024-01-25 17:12:32 +0800

    Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void markDone() {
+        this.isDone = true;
+    }
+    
+    public void markUndone() {
+        this.isDone = false;
+    }
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
