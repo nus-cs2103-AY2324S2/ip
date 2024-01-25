96b779b333578ecdf6e17b932d37ff4544dba4bb
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-01-25 23:01:41 +0800

    Level-3

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -8,26 +8,39 @@ public class Eve {
         System.out.println(" What can I do for you?");
 
         String input = "";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while(!input.equals("bye")){
             input = sc.nextLine();
-
             if (input.equals("bye")){
                 System.out.println(" Bye. Hope to see you again soon !");
             } else if (input.equals("list")) {
                 for (int i = 0; i < list.size(); i++ ){
                     int j = i + 1;
-                    String temp = list.get(i);
-                    System.out.println(j + "." + temp);
+                    Task temp = list.get(i);
+                    System.out.println(j + ".[" + temp.getStatusIcon() + "] " + temp.toString());
                 }
                 
-            } else {
-                list.add(input);
+            } else if (input.substring(0,4).equals("mark")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task temp = list.get(index);
+                temp.markAsDone();
+                System.out.println(" Nice! I've marked this task as done: ");
+                System.out.println(" [" + temp.getStatusIcon() + "]" + temp.toString());
+                
+            // } else if (input.substring(0,7).equals("ummark")) {
+            //     int index = Integer.parseInt(input.substring(5)) - 1;
+            //     Task temp = list.get(index);
+            //     temp.markAsNotDone();
+            //     System.out.println(" Nice! I've marked this task as done: ");
+            //     System.out.println(" [" + temp.getStatusIcon() + "]" + temp.toString());
+                
+            // } else {
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println("added: " + input);
             }
         }
-
             
 
         sc.close();
