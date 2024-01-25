df3ea37ddae78946d6a9432912bfcc5a40f7442d
ashleyclx <e0958221@u.nus.edu>
2024-01-26 06:10:37 +0800

    Changed tasks from Array to Array list
    Updated all methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String LINE = "    ____________________________________________________________\n";
     private static final String INDENT = "    ";
-    private static int taskCount = 0;
-    private static final Task[] tasks = new Task[100];
+    private static final ArrayList<Task> tasks = new ArrayList<Task>();
     public static void main(String[] args) {
         Duke.hello();
         Scanner in = new Scanner(System.in);
@@ -17,26 +17,26 @@ public class Duke {
             } else if (cmdarg[0].equals("list")) {
                 System.out.print(LINE);
                 System.out.println(INDENT + "I gotchu bruv. Here's your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(INDENT + (i + 1) + "." + tasks[i]);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(INDENT + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.print(LINE);
             } else if (cmdarg[0].equals("mark") || cmdarg[0].equals("unmark")) {
                 if (cmdarg.length == 2) {
                     try {
                         int i = Integer.parseInt(cmdarg[1]);
-                        if (i > taskCount) { // incorrect index
+                        if (i > tasks.size()) { // incorrect index
                             System.out.print(LINE + INDENT + "You ain't got that many tasks bruh!\n" + LINE);
                         } else if (i < 1) { // incorrect index
                             System.out.print(LINE + INDENT + "Start from task 1 lil bro!\n" + LINE);
                         } else if (cmdarg[0].equals("mark")) {
-                            tasks[i - 1].markAsDone();
+                            tasks.get(i - 1).markAsDone();
                             System.out.print(LINE + INDENT + "Yasss King/Queen! This task is officially slayed:\n  "
-                                    + INDENT + tasks[i - 1] + "\n" + LINE);
+                                    + INDENT + tasks.get(i - 1) + "\n" + LINE);
                         } else {
-                            tasks[i - 1].unmark();
+                            tasks.get(i - 1).unmark();
                             System.out.print(LINE + INDENT + "Bruh! This task never happened:\n  "
-                                    + INDENT + tasks[i - 1] + "\n" + LINE);
+                                    + INDENT + tasks.get(i - 1) + "\n" + LINE);
                         }
                     } catch (java.lang.NumberFormatException e) { // non number typed
                         System.out.print(LINE + INDENT + "Ain't no way! We lackin' just numbers after mark/unmark.\n"
@@ -96,18 +96,18 @@ public class Duke {
     public static void addTask(String arg, int id) {
         switch (id) {
             case 0:
-                tasks[taskCount] = new Todo(arg);
-                System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + tasks[taskCount]);
-                taskCount++;
-                System.out.print(INDENT + "Yo, we're " + taskCount + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
+                Todo todo = new Todo(arg);
+                tasks.add(todo);
+                System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + todo);
+                System.out.print(INDENT + "Yo, we're " + tasks.size() + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
                 break;
             case 1: {
                 String[] descTime = arg.split(" /by "); // [description, by]
                 if (descTime.length == 2) {
-                    tasks[taskCount] = new Deadline(descTime[0], descTime[1]);
-                    System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + tasks[taskCount]);
-                    taskCount++;
-                    System.out.print(INDENT + "Yo, we're " + taskCount + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
+                    Deadline deadline = new Deadline(descTime[0], descTime[1]);
+                    tasks.add(deadline);
+                    System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + deadline);
+                    System.out.print(INDENT + "Yo, we're " + tasks.size() + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
                 } else { // incorrect formatting for /by
                     System.out.print(LINE + INDENT + "When you wanna do this task by lil bro?\n"
                             + INDENT + "e.g. deadline <task> /by <date/time>\n" + LINE);
@@ -119,10 +119,10 @@ public class Duke {
                 if (descTime.length == 2) {
                     String[] fromTo = descTime[1].split(" /to "); // [from , to]
                     if (fromTo.length == 2) {
-                        tasks[taskCount] = new Event(descTime[0], fromTo[0], fromTo[1]);
-                        System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + tasks[taskCount]);
-                        taskCount++;
-                        System.out.print(INDENT + "Yo, we're " + taskCount + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
+                        Event event = new Event(descTime[0], fromTo[0], fromTo[1]);
+                        tasks.add(event);
+                        System.out.println(LINE + INDENT + "Ayo new task just dropped:\n  " + INDENT + event);
+                        System.out.print(INDENT + "Yo, we're " + tasks.size() + " task(s) deep! Let's keep this SIGMA GRINDSET!\n" + LINE);
                     } else { // incorrect formatting for /to
                         System.out.println(LINE + INDENT + "When does this event end lil bro?\n"
                                 + INDENT + "e.g. event <task> /from <start date/time> /to <start date/time>\n" + LINE);
