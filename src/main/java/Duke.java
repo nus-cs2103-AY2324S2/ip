2b51742d709dfb667c29d4005c0b70b9fd5df653
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-25 10:21:09 +0800

    Created createList method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void createList(Task[] listArr) {
+        System.out.println("Here are the tasks in your list:");
+        for (int j = 0; j < 100; j++) { // printing out all items in the list
+            if (listArr[j] == null) { // for early termination
+                break;
+            } else {
+                System.out.println(j + 1 + ". " + listArr[j].toString());
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello! I'm Jojo :)");
         System.out.println("What can I do for you?");
@@ -10,14 +21,11 @@ public class Duke {
         int i = 0;
         while (!userTxt.equals("bye")) {
             if (userTxt.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int j = 0; j < 100; j++) { // printing out all items in the list
-                    if (listArr[j] == null) { // for early termination
-                        break;
-                    } else {
-                        System.out.println(j + 1 + ". " + listArr[j].toString());
-                    }
-                }
+                createList(listArr);
+            } else if (userTxt.startsWith("mark")) {
+
+            } else if (userTxt.startsWith("unmark")) {
+
             } else {
                 System.out.println("added: " + userTxt);
                 Task t = new Task(userTxt);
