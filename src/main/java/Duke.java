55c7387e749e3921c75cb3c2a0119151b89a60e4
yeoshuheng <yeoshuheng@gmail.com>
2024-01-25 21:04:15 +0800

    feat: integrated logging feature into chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,9 +24,14 @@ public class Duke {
       System.out.println(name_display);
       String current_input = "";
 
+      FileManager manager = new FileManager("data/log.txt");
+      manager.createLog();
       ArrayList<Task> history = new ArrayList<>();
-
-
+      try {
+        manager.loadHistory(history);
+      } catch (CorruptedLogException e) {
+        System.out.println("Cannot load history!");
+      }
 
       mainloop: while (true) {
         current_input = sc.nextLine();
@@ -68,6 +73,7 @@ public class Duke {
               "_________________________\n" +
               "Now you have " + history.size() + " items in your list!\n";
             System.out.println(print_out);
+            manager.writeLog(history);
             break;
           case MARK:
             focus_index = -1;
@@ -84,6 +90,7 @@ public class Duke {
               focus_task.getFullStatus() + "\n" +
               "_________________________\n";
             System.out.println(print_out);
+            manager.writeLog(history);
             break;
           case UNMARK:
             focus_index = -1;
@@ -100,6 +107,7 @@ public class Duke {
               focus_task.getFullStatus() + "\n" +
               "_________________________\n";
             System.out.println(print_out);
+            manager.writeLog(history);
             break;
           case EVENT:
             Task event = null;
@@ -118,6 +126,7 @@ public class Duke {
               "_________________________\n" +
               "Now you have " + history.size() + " items in your list!\n";
             System.out.println(to_print);
+            manager.writeLog(history);
             break;
           case TODO:
             event = null;
@@ -135,6 +144,7 @@ public class Duke {
               "_________________________\n" +
               "Now you have " + history.size() + " items in your list!\n";
             System.out.println(to_print);
+            manager.writeLog(history);
             break;
           case DEADLINE:
             try {
@@ -152,6 +162,7 @@ public class Duke {
               "_________________________\n" +
               "Now you have " + history.size() + " items in your list!\n";
             System.out.println(to_print);
+            manager.writeLog(history);
             break;
         }
       }
