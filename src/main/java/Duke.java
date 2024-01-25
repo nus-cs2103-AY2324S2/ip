6e43bb3c7d06bb27b6fc5ac24f646c0b3a24a7ed
donwong2308 <65356386+donwong2308@users.noreply.github.com>
2024-01-25 20:47:28 +0800

    Did Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+import java.util.Objects;
+
 public class Duke {
     public static void main(String[] args) {
-
         System.out.println("Hello! I'm PeWPeWPeW");
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("What can I do for you?");
+        String userInput = scanner.nextLine();
+        while (!Objects.equals(userInput.toLowerCase(), "bye")) {
+            if (Objects.equals(userInput.toLowerCase(), "pewpewpew")) {
+                System.out.println("PeWPeWPeWPeWPeWPeWPeWPeWPeWPeWPeWPeW");
+            }
+            else {
+                System.out.println(userInput);
+            }
+            System.out.println("What else can I do for you? (try typing my name 3 times with no space in between)" );
+            userInput = scanner.nextLine();
+
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
