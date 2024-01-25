12aa8035e9df6a0abeabb3cf143c6ac4493980dd
Zoe <za.zoeang@gmail.com>
2024-01-25 23:15:50 +0800

    Echo until bye

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -1,14 +1,40 @@
+import java.util.Scanner;
+
 public class Alpaca {
+    static String name = "Alpaca";
 
     private static void divider() {
         System.out.println("____________________________________________________________");
     }
+
+    private static void greeting() {
+        divider();
+        System.out.println("Hihi! I'm " + Alpaca.name + "\nWhat can I do for you?");
+        divider();
+    }
+
+    private static void bye() {
+        divider();
+        System.out.println("cucu");
+        divider();
+    }
+
+    private static void processInput() {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        if (line.equals("bye")) {
+            scanner.close();
+            bye();
+            return;
+        }
+        divider();
+        System.out.println(line);
+        divider();
+        processInput();
+    }
+    
     public static void main(String[] args) {
-        String name = "Alpaca";
-        divider();
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
-        divider();
-        System.out.println("Bye. Hope to see you again!");
-        divider();
+        greeting();
+        processInput();
     }
 }
