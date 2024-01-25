868cc942fdbcc852e344211b106c14297f671538
wolffe88 <yonglian1845@gmail.com>
2024-01-25 11:02:29 +0800

    echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ___  __    ________  ________  ___    \n"
+                + "|\\  \\|\\  \\ |\\   __  \\|\\   __  \\|\\  \\\n"
+                + "\\ \\  \\/  /|\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\\n"
+                + " \\ \\   ___  \\ \\   __  \\ \\   _  _\\ \\  \\\n"
+                + "  \\ \\  \\\\ \\  \\ \\  \\ \\  \\ \\  \\\\  \\\\ \\  \\____  \n"
+                + "   \\ \\__\\\\ \\__\\ \\__\\ \\__\\ \\__\\\\ _\\\\ \\_______\\\n"
+                + "    \\|__| \\|__|\\|__|\\|__|\\|__|\\|__|\\|_______|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I am KARL");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
