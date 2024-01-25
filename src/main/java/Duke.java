87fa686ec6ecd93bd76f0b3341b3ee77fd23c464
wolffe88 <yonglian1845@gmail.com>
2024-01-25 12:49:46 +0800

    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,17 +9,24 @@ public class Duke {
                 + "██║  ██║╚██████╔╝███████╗██║  ██║██║ ╚████║██████╔╝\n"
                 + "╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ \n";
         System.out.println("Hello from\n" + logo);
-        String bot = "[ROLAND]: ";
+        String bot = "[ROLAND] ";
         System.out.println(bot + "Hello! I am ROLAND");
         System.out.println(bot + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
-
+        List<String> taskList  = new ArrayList<>();
         while(true){
             String reply = sc.nextLine();
             if (reply.equals("bye")) {
                 break;
+            } else if (reply.startsWith("list")) {
+                System.out.println(bot + "Here are your tasks:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(String.format(i+1 + ". " + taskList.get(i)));
+                }
+                continue;
             }
-            System.out.println(bot + reply);
+            taskList.add(reply);
+            System.out.println(bot + "added: " + reply);
         }
 
 
