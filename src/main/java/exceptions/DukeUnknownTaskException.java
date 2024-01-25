82865c81fe52bfe3d0701d005d1ff16c6baabab6
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-26 00:20:57 +0800

    Updated test cases

--- src/main/java/exceptions/DukeUnknownTaskException.java
+++ src/main/java/exceptions/DukeUnknownTaskException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class DukeUnknownTaskException extends DukeException {
     public DukeUnknownTaskException() {
-        super("Sorry, but this task is unknown to me. Please try again using any of the following keywords: 'todo', 'deadline' or 'event'. ");
+        super("Sorry, but this task is unknown to me. Please try again using any of the following keywords: 'todo', 'deadline' or 'event'.");
     }
 }
