26ec5ee17a92b9293d2cad256302f4975052859f
kjw142857 <e0958424@u.nus.edu>
2024-01-25 19:46:05 +0800

    Revert "Level-0"
    This reverts commit d60e29ba0ca7cd5aa2c6703a111e8b4b44524239.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String name = "Sleepy";
-        String welcomeLine = "Hello! I'm " + name;
-        String questionLine = "What can I do for you?";
-        String exitLine = "Bye. Hope to see you again soon!";
-        System.out.println(welcomeLine);
-        System.out.println(questionLine);
-        System.out.println(exitLine);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
