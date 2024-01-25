133b25366563592d9c0ee89fb8d52a006b949841
Bowei Jiang <>
2024-01-25 17:15:10 +0800

    Delete + ArrayList

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jerry {
     public static void main(String[] args) {
@@ -6,8 +7,7 @@ public class Jerry {
         String message = "Hello! I'm Jerry.\n" + "Anything I can do for you?";
         System.out.println(message);
 
-        Task[] tasks =  new Task[100];
-        int index = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine().trim();
@@ -23,24 +23,43 @@ public class Jerry {
 
                 case "list":
                     System.out.println("Here are the tasks in your list:");
-                    for (int x = 0; tasks[x] != null; x++) {
-                        System.out.println((x + 1) + "." + tasks[x]);
+                    for (int x = 0; x < tasks.size() ; x++) {
+                        System.out.println((x + 1) + "." + tasks.get(x));
                     }
                     break;
 
                 case "mark":
                     taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < index) {
-                        tasks[taskIndex].markDone();
-                        System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskIndex]);
+                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                        tasks.get(taskIndex).markDone();
+                        System.out.println("Nice! I've marked this task as done:\n  " + tasks.get(taskIndex));
                     }
                     break;
 
                 case "unmark":
                     taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < index) {
-                        tasks[taskIndex].markNotDone();
-                        System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[taskIndex]);
+                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                        tasks.get(taskIndex).markNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:\n  " + tasks.get(taskIndex));
+                    }
+                    break;
+
+                case "delete":
+                    if (parts.length < 2) {
+                        System.out.println("Error: Please specify the task number to delete\nUsage: delete <task number>");
+                    } else {
+                        try {
+                            taskIndex = Integer.parseInt(parts[1]) - 1;
+                            if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                                Task removedTask = tasks.remove(taskIndex);
+                                System.out.println("I've removed this task:\n  " + removedTask);
+                                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                            } else {
+                                System.out.println("Error: Task does not exist.");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Error: Please enter a number.");
+                        }
                     }
                     break;
 
@@ -48,10 +67,10 @@ public class Jerry {
                     if (parts.length < 2) {
                         System.out.println("Error: Wrong format \nUsage: todo <task description>");
                     } else {
-                        tasks[index] = new ToDo(parts[1]);
-                        index++;
-                        System.out.println("Got it. I've added this task:\n  " + tasks[index - 1]);
-                        System.out.println("Now you have " + index + " tasks in the list.");
+                        ToDo todo = new ToDo(parts[1]);
+                        tasks.add(todo);
+                        System.out.println("Got it. I've added this task:\n  " + todo);
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
 
                     }
                     break;
@@ -61,10 +80,10 @@ public class Jerry {
                         System.out.println("Error: Wrong format, please include deadline \nUsage: deadline <task description> /by <date/time>");
                     } else {
                         String[] deadlineParts = parts[1].split(" /by ", 2);
-                        tasks[index] = new Deadline(deadlineParts[0], deadlineParts[1]);
-                        index++;
-                        System.out.println("Got it. I've added this task:\n  " + tasks[index - 1]);
-                        System.out.println("Now you have " + index + " tasks in the list.");
+                        Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                        tasks.add(deadline);
+                        System.out.println("Got it. I've added this task:\n  " + deadline);
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     }
                     break;
 
@@ -74,10 +93,10 @@ public class Jerry {
                     } else {
                         String[] eventParts = parts[1].split(" /from ", 2);
                         String[] fromTo = eventParts[1].split(" /to ", 2);
-                        tasks[index] = new Event(eventParts[0], fromTo[0], fromTo[1]);
-                        index++;
-                        System.out.println("Got it. I've added this task:\n  " + tasks[index - 1]);
-                        System.out.println("Now you have " + index + " tasks in the list.");
+                        Event event = new Event(eventParts[0], fromTo[0], fromTo[1]);
+                        tasks.add(event);
+                        System.out.println("Got it. I've added this task:\n  " + event);
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     }
                     break;
 
