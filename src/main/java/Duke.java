b89478c64873d218d1c67421e182d0edba8c24f2
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-01-25 13:15:31 +0800

    Level-0: Renamed the chatbot. and added greet and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+    static String name = "Alfred";
+    public static void greet(){
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit(){
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again");
+        System.out.println("____________________________________________________________");
     }
 }
