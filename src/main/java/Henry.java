405d52fdacb183e61bdf5ecd0f0984f18e44aeb9
tohlh <tohliheng@gmail.com>
2024-01-25 09:30:00 +0800

    Mark and unmark Tasks

--- src/main/java/Henry.java
+++ src/main/java/Henry.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Henry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int numOfItems = 0;
 
         String logo = "  _    _                       \n" +
@@ -30,11 +30,22 @@ public class Henry {
                     System.out.printf("%d. %s\n", i + 1, items[i]);
                 }
                 System.out.println();
+            } else if (currentMessage.startsWith("mark")) {
+                int index = Integer.parseInt(currentMessage.split(" ")[1]) - 1;
+                items[index].markAsDone();
+                System.out.println("This task is marked as done XD");
+                System.out.println(items[index]);
+                System.out.println();
+            } else if (currentMessage.startsWith("unmark")) {
+                int index = Integer.parseInt(currentMessage.split(" ")[1]) - 1;
+                items[index].unmarkAsDone();
+                System.out.println("This task is marked as undone :(");
+                System.out.println(items[index]);
+                System.out.println();
             } else {
                 System.out.println("added: " + currentMessage);
                 System.out.println();
-
-                items[numOfItems] = currentMessage;
+                items[numOfItems] = new Task(currentMessage);
                 numOfItems = numOfItems + 1;
             }
         }
