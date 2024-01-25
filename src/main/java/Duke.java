d5bbd63dd43a64123c51432e0e721e65b05702b2
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-01-25 21:49:28 +0800

    Duke.java: Add error handling when trying to execute commands without required parameters

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,96 +16,129 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
-            String input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                break;
-            }
-            if (input.equalsIgnoreCase("list")) {
-                String taskList = "";
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task curr = tasks.get(i);
-                    taskList += ((i+1) + ". " + curr.toString() + "\n");
-                }
-                System.out.println(gap + "Here are your tasks:\n" + taskList + gap);
-            } else if (input.toLowerCase().contains("mark")) {
+            try {
+                String input = sc.nextLine();
                 StringTokenizer st = new StringTokenizer(input);
-                String command = st.nextToken().toLowerCase();
-                int taskNum = Integer.parseInt(st.nextToken()) - 1;
-                Task selectedTask = tasks.get(taskNum);
-                if (command.equals("mark")) {
-                    selectedTask.markDone();
-                    System.out.println(gap + "Well done, task marked as completed.\n"
-                                      + selectedTask.toString() + "\n" + gap);
+                String identifier = st.nextToken().toLowerCase();
+                if (identifier.equals("bye")) {
+                    sc.close();
+                    System.out.println(gap + "Goodbye! Have a pleasant day!\n" + gap);
+                    break;
+                }
+                if (identifier.equals("list")) {
+                    if (st.hasMoreTokens()) {
+                        throw new DukeException("list command does not accept arguments.\n"
+                                               + "Enter 'list' to view the current list of tasks");
+                    }
+                    String taskList = "";
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task curr = tasks.get(i);
+                        taskList += ((i+1) + ". " + curr.toString() + "\n");
+                    }
+                    System.out.println(gap + "Here are your tasks:\n" + taskList + gap);
+                } else if (identifier.contains("mark")) {
+                    String command = identifier.toLowerCase();
+                    int taskNum = Integer.parseInt(st.nextToken()) - 1;
+                    Task selectedTask = tasks.get(taskNum);
+                    if (command.equals("mark")) {
+                        selectedTask.markDone();
+                        System.out.println(gap + "Well done, task marked as completed.\n"
+                                + selectedTask.toString() + "\n" + gap);
+                    } else {
+                        selectedTask.markNotDone();
+                        System.out.println(gap + "Understood, task marked as uncompleted.\n"
+                                + selectedTask.toString() + "\n" + gap);
+                    }
+                } else if (identifier.equals("todo") || identifier.equals("deadline")
+                        || identifier.equals("event")) {
+                    switch (identifier) {
+                    case "todo":
+                        if (st.hasMoreTokens()) {
+                            String todoDesc = "";
+                            while(st.hasMoreTokens()) {
+                                todoDesc += st.nextToken() + " ";
+                            }
+                            ToDo newToDo = new ToDo(todoDesc.trim());
+                            tasks.add(newToDo);
+                            System.out.println(gap + "Confirmed. New task added:\n"
+                                    + newToDo.toString() + "\n");
+                        } else {
+                            throw new DukeException("todo command requires a description for the task."
+                                                   + "\n\nPlease leave a space after 'todo' and enter"
+                                                   + " the task description.");
+                        }
+                        break;
+                    case "deadline":
+                        if (st.hasMoreTokens()) {
+                            String[] deadlineInfo = input.split("/");
+                            st = new StringTokenizer(deadlineInfo[0].trim());
+                            st.nextToken();
+                            String deadlineDesc = "";
+                            while(st.hasMoreTokens()) {
+                                deadlineDesc += st.nextToken() + " ";
+                            }
+                            st = new StringTokenizer(deadlineInfo[1].trim());
+                            st.nextToken();
+                            String by = "";
+                            while(st.hasMoreTokens()) {
+                                by += st.nextToken() + " ";
+                            }
+                            Deadline newDeadline = new Deadline(deadlineDesc.trim(), by.trim());
+                            tasks.add(newDeadline);
+                            System.out.println(gap + "Confirmed. New task added:\n"
+                                    + newDeadline.toString() + "\n");
+                        } else {
+                            throw new DukeException("deadline command requires a description for the task"
+                                                   + " and a deadline. \n\nPlease leave a space after 'deadline'"
+                                                   + " and enter the task description, \nfollowed by a space and a"
+                                                   + " forward slash then the deadline of the task.");
+                        }
+                        break;
+                    case "event":
+                        if (st.hasMoreTokens()) {
+                            String[] eventInfo = input.split("/");
+                            st = new StringTokenizer(eventInfo[0].trim());
+                            st.nextToken();
+                            String eventDesc = "";
+                            while(st.hasMoreTokens()) {
+                                eventDesc += st.nextToken() + " ";
+                            }
+                            st = new StringTokenizer(eventInfo[1].trim());
+                            st.nextToken();
+                            String from = "";
+                            while (st.hasMoreTokens()) {
+                                from += st.nextToken() + " ";
+                            }
+                            st = new StringTokenizer(eventInfo[2].trim());
+                            st.nextToken();
+                            String end = "";
+                            while (st.hasMoreTokens()) {
+                                end += st.nextToken() + " ";
+                            }
+                            Event newEvent = new Event(eventDesc.trim(), from.trim(), end.trim());
+                            tasks.add(newEvent);
+                            System.out.println(gap + "Confirmed. New task added:\n"
+                                    + newEvent.toString() + "\n");
+                        } else {
+                            throw new DukeException("event command requires a description for the task,"
+                                    + " start time and end time. \n\nPlease leave a space after 'event'"
+                                    + " and enter the task description, \nfollowed by a space and forward slash"
+                                    + " before the start time, \nfollowed by another space and forward slash"
+                                    + " before the end time.");
+                        }
+                        break;
+                    default:
+                        System.out.println("なに？！");
+                        break;
+                    }
+                    System.out.println("Total task count: " + tasks.size() + ".\n" + gap);
                 } else {
-                    selectedTask.markNotDone();
-                    System.out.println(gap + "Understood, task marked as uncompleted.\n"
-                            + selectedTask.toString() + "\n" + gap);
+                    throw new DukeException("I do not recognize that command.\n"
+                                           + "Please enter a valid command.");
                 }
-            } else {
-                StringTokenizer st = new StringTokenizer(input);
-                String taskType = st.nextToken().toLowerCase();
-                switch (taskType) {
-                case "todo":
-                    String todoDesc = "";
-                    while(st.hasMoreTokens()) {
-                        todoDesc += st.nextToken() + " ";
-                    }
-                    ToDo newToDo = new ToDo(todoDesc.trim());
-                    tasks.add(newToDo);
-                    System.out.println(gap + "Confirmed. New task added:\n"
-                                      + newToDo.toString() + "\n");
-                    break;
-                case "deadline":
-                    String[] deadlineInfo = input.split("/");
-                    st = new StringTokenizer(deadlineInfo[0].trim());
-                    st.nextToken();
-                    String deadlineDesc = "";
-                    while(st.hasMoreTokens()) {
-                        deadlineDesc += st.nextToken() + " ";
-                    }
-                    st = new StringTokenizer(deadlineInfo[1].trim());
-                    st.nextToken();
-                    String by = "";
-                    while(st.hasMoreTokens()) {
-                        by += st.nextToken() + " ";
-                    }
-                    Deadline newDeadline = new Deadline(deadlineDesc.trim(), by.trim());
-                    tasks.add(newDeadline);
-                    System.out.println(gap + "Confirmed. New task added:\n"
-                                      + newDeadline.toString() + "\n");
-                    break;
-                case "event":
-                    String[] eventInfo = input.split("/");
-                    st = new StringTokenizer(eventInfo[0].trim());
-                    st.nextToken();
-                    String eventDesc = "";
-                    while(st.hasMoreTokens()) {
-                        eventDesc += st.nextToken() + " ";
-                    }
-                    st = new StringTokenizer(eventInfo[1].trim());
-                    st.nextToken();
-                    String from = "";
-                    while (st.hasMoreTokens()) {
-                        from += st.nextToken() + " ";
-                    }
-                    st = new StringTokenizer(eventInfo[2].trim());
-                    st.nextToken();
-                    String end = "";
-                    while (st.hasMoreTokens()) {
-                        end += st.nextToken() + " ";
-                    }
-                    Event newEvent = new Event(eventDesc.trim(), from.trim(), end.trim());
-                    tasks.add(newEvent);
-                    System.out.println(gap + "Confirmed. New task added:\n"
-                                      + newEvent.toString() + "\n");
-                    break;
-                default:
-                    System.out.println("なに？！");
-                    break;
-                }
-                System.out.println("Total task count: " + tasks.size() + ".\n" + gap);
+            } catch (DukeException e) {
+                System.out.println(gap + e.getMessage() + "\n" + gap);
             }
         }
-        System.out.println(gap + "Goodbye! Have a pleasant day!\n" + gap);
     }
 }
