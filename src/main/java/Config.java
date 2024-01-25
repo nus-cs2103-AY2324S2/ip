4fc176bf7a066bf3081ad5070215187ff32f1aa8
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-01-25 10:13:12 +0800

    Add text ui testing

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -2,7 +2,7 @@ public final class Config {
     private Config() {}
 
     // chatbot response placeholder
-    public static final String PLACEHOLDER = "\t----------------------------------------\n" +
+    public static final String PLACEHOLDER = "\t------------------------------------------------------------\n" +
             "\t\t%s\n" +
-            "\t----------------------------------------";
+            "\t------------------------------------------------------------";
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
     }
 }
