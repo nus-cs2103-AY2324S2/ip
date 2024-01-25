211968d44a0c821201c95cda1a2cfcdaea780f44
jessicawyz <jessicawuyizheng@.com>
2024-01-25 10:20:07 +0800

    "level 2"

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class Echo {
+    private String[] store = new String[100];
+    private int indexPt = 0;
     public static void main(String[] args) {
+        Echo echo = new Echo();
         greetUser();
-        startConversation();
-        System.out.println("Bye. Hope to see you again soon!");
+        echo.startConversation();
+        endConversation();
     }
 
     public static void greetUser() {
@@ -14,20 +17,53 @@ public class Echo {
         System.out.println("____________________________________________________________");
     }
 
-    public static void startConversation() {
+    public void startConversation() {
         Scanner scanner = new Scanner(System.in);
         String userCommand;
 
         do {
             System.out.print("\n");
             userCommand = scanner.nextLine();
-            echoCommand(userCommand);
+            if (userCommand.equals("list")) {
+                listCommand();
+            } else {
+                storeCommand(userCommand);
+            }
+            //echoCommand(userCommand);
         } while (!userCommand.equalsIgnoreCase("bye"));
     }
 
-    public static void echoCommand(String command) {
+    public static void endConversation() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void echoCommand(String command) {
         System.out.println("____________________________________________________________");
         System.out.println(command);
+        System.out.println("____________________________________________________________");
+    }
+
+    public void storeCommand(String command) {
+        if (!command.equals("bye")) {
+            store[indexPt] = command;
+            indexPt++;
+            System.out.println("____________________________________________________________");
+            System.out.println("added: " + command);
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    public void listCommand() {
+        System.out.println("____________________________________________________________");
+        if (indexPt == 0) {
+            System.out.println("No items in the list");
+        } else {
+            for (int i = 0; i < indexPt; i++) {
+                System.out.println((i+1) + ". " + store[i]);
+            }
+        }
         System.out.println("____________________________________________________________");
     }
 }
