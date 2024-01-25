b672d8722896fd139a10f85457697227e6cb4e03
Jajared <jajawong09@gmail.com>
2024-01-25 10:44:38 +0800

    feat: added user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
   public static void main(String[] args) {
+    String input = "";
     String botName = "GeePeeTee";
     // Welcome Message
     System.out.println("Hello! I'm " + botName + "\nWhat can I do for you?");
+    Scanner scanner = new Scanner(System.in);
+    while (!input.equals("bye")) {
+      input = scanner.nextLine();
+      if (input.equals("bye")) {
+        scanner.close();
+        break;
+      }
+      System.out.println(input);
+    }
     // Exit Message
     System.out.println("Bye. Hope to see you again soon!");
   }
