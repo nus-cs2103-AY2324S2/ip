3b2d0a4829f18baddc264c300f0e318ff260eac2
ChenXy128 <e0968917@u.nus.edu>
2024-01-25 23:14:53 +0800

    DukeException

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class DukeException {
+public class DukeException extends Exception{
+    protected String message;
+    
+    public DukeException(String message) {
+        super(message);
+    }
 }
