f4b0ef25a7ee7588939e77c558c7a73e536d2b40
Felix <felixwang121@gmail.com>
2024-01-25 19:57:42 +0800

    Added add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.println("\t\tHello, my name is Xilef.\n\t\tHow may I help you today??\n");
 
@@ -9,8 +11,14 @@ public class Duke {
             String str = scanner.nextLine();
             if (str.equals("bye")) {
                 break;
+            } else if (str.equals("list")) {
+                for (int i = 1; i < list.size() + 1; i++) {
+                    System.out.println("\t\t" + i +". " + list.get(i-1));
+                }
+            } else {
+                list.add(str);
+                System.out.println("\t\tadded: " + str);
             }
-            System.out.println("\t\t"+str);
         }
         System.out.println("\t\tBye bye, see you next time !!!");
     }
