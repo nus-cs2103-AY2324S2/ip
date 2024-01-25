a3288c701bf9bc9291bc7093d9f6157fbd7195bd
shuyuan <crayy.0803@gmail.com>
2024-01-25 18:12:30 +0800

    Level-6

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,19 +3,23 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM delete output from previous runs
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist ACTUAL2.TXT del ACTUAL2.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+REM run the first test
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
-
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+REM run the second test
+java -classpath ..\bin Duke < input2.txt > ACTUAL2.TXT
+REM compare the output to the expected output
+FC ACTUAL2.TXT EXPECTED2.TXT
