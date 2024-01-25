97820d209b309a1914a37d7c491b5e8f6d2d804f
Emberlynn-Loo <E0960284@u.nus.edu>
2024-01-26 06:08:35 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
 
         Scanner inputs = new Scanner(System.in); //this is the scanner obj
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int tasknum = 0;
 
         while (true) {
@@ -24,17 +24,63 @@ public class Duke {
 
             if (echo.equals("list") || echo.equals("List")) {
                 System.out.println("    _______________________________________________________");
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < tasknum; i++) {
                     System.out.println("     " + (i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("    _______________________________________________________");
+            } else if (echo.startsWith("mark") || echo.startsWith("Mark")) {
+                int pos = echo.indexOf(" ");
+                if (pos != -1 && pos + 1 < echo.length()) {
+                    String taskStr = echo.substring(pos + 1);
+
+                    int taskNumber = Integer.parseInt(taskStr) - 1; //cause array
+                    if (taskNumber >= 0 && taskNumber < tasknum) {
+                        tasks[taskNumber].markAsDone();
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       " + tasks[taskNumber]);
+                        System.out.println("    ____________________________________________________________");
+                    } else if (taskNumber >= tasknum) {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("    ERROR! YOU DON'T HAVE THAT MANY TASKS. TRY AGAIN.");
+                        System.out.println("    ____________________________________________________________");
+                    } else {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("    NEGATIVE TASKS? THAT DOESN'T EXIST. TRY AGAIN.");
+                        System.out.println("    ____________________________________________________________");
+                    }
+                }
+            } else if (echo.startsWith("unmark") || echo.startsWith("Unmark")) {
+                int pos = echo.indexOf(" ");
+                if (pos != -1 && pos + 1 < echo.length()) {
+                    String taskStr = echo.substring(pos + 1);
+
+                    int taskNumber = Integer.parseInt(taskStr) - 1;
+                    if (taskNumber >= 0 && taskNumber < tasknum) {
+                        tasks[taskNumber].markAsNotDone();
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Nice! I've marked this task as not done:");
+                        System.out.println("       " + tasks[taskNumber]);
+                        System.out.println("    ____________________________________________________________");
+                    } else if (taskNumber >= tasknum) {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("    ERROR! YOU DON'T HAVE THAT MANY TASKS. TRY AGAIN.");
+                        System.out.println("    ____________________________________________________________");
+                    } else {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("    NEGATIVE TASKS? THAT DOESN'T EXIST. TRY AGAIN.");
+                        System.out.println("    ____________________________________________________________");
+                    }
+                }
             } else {
                 System.out.println("    _______________________________________________________");
                 System.out.println("     added:" + echo);
-                tasks[tasknum] = echo;
+                tasks[tasknum] = new Task(echo);
                 tasknum++;
                 System.out.println("    _______________________________________________________");
             }
         }
     }
 }
+
