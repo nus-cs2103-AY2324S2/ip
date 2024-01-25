2b5315c306e502aec41b99c307532363f32505d3
James Shen <95962838+shamesjen@users.noreply.github.com>
2024-01-25 23:07:19 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> userInputList = new ArrayList<>();
         String name = "James";
         System.out.println("Hello! I'm " + name + "\n");
         System.out.println("What can I do for you? \n");
@@ -14,12 +16,18 @@ public class Duke {
                 break;
             }
 
-            if (input.equalsIgnoreCase("list")) {
-                System.out.println("list");
+            else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < userInputList.size(); i++) {
+                    System.out.println((i + 1) + ". " + userInputList.get(i));
+                }
             }
 
-            if (input.equalsIgnoreCase("blah")) {
+            else if (input.equalsIgnoreCase("blah")) {
                 System.out.println("blah");
+            }
+
+            else {
+                userInputList.add(input);
             }
         }
     }
