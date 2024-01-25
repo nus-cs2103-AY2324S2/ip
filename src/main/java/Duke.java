6f77c47b02b00e9e7a3fddec2b341c6b38a4cd3a
wolffe88 <yonglian1845@gmail.com>
2024-01-25 13:01:55 +0800

    mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,20 +13,36 @@ public class Duke {
         System.out.println(bot + "Hello! I am ROLAND");
         System.out.println(bot + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        List<String> taskList  = new ArrayList<>();
+        List<Task> taskList  = new ArrayList<>();
         while(true){
             String reply = sc.nextLine();
+
             if (reply.equals("bye")) {
                 break;
             } else if (reply.startsWith("list")) {
                 System.out.println(bot + "Here are your tasks:");
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(String.format(i+1 + ". " + taskList.get(i)));
+                    Task task = taskList.get(i);
+                    System.out.println(String.format(i+1+". ["+task.getStatusIcon()+"] " + task.getDescription() ));
                 }
                 continue;
+            } else if (reply.startsWith("mark")) {
+                int index = Integer.parseInt(reply.replaceAll("[\\D]", ""));
+                Task task = taskList.get(index-1);
+                task.markDone();
+                System.out.println(String.format(bot + "I have marked ["+task.getStatusIcon()+"] " + task.getDescription() + " as done") );
+                continue;
+            } else if (reply.startsWith("unmark")) {
+                int index = Integer.parseInt(reply.replaceAll("[\\D]", ""));
+                Task task = taskList.get(index-1);
+                task.markUndone();
+                System.out.println(String.format(bot +"I have marked ["+task.getStatusIcon()+"] " + task.getDescription() + " as not done") );
+                continue;
             }
-            taskList.add(reply);
-            System.out.println(bot + "added: " + reply);
+
+            Task task = new Task(reply);
+            taskList.add(task);
+            System.out.println(bot + "added: " + task.getDescription());
         }
 
 
