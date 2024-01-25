a2a4cec204ee95dd4e29e27a82e538c61d94b346
VatsalVitosh <vatsalvitosh@gmail.com>
2024-01-26 03:28:18 +0800

    Level 0 - Rename, Greet, Exit

---
+++ src/main/java/Lia.java
@@ -0,0 +1,7 @@
+public class Lia {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Lia");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
