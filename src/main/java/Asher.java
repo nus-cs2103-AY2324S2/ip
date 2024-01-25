85640cf970c430b55cca61eaf3956147f97e53df
cleowenxuan <>
2024-01-25 11:07:52 +0800

    Level2 Completed

--- src/main/java/Asher.java
+++ src/main/java/Asher.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Asher {
+    public String name;
+    private static String[] userCommands = new String[100];
+    private static int count = 0;
+
     public static void greet() {
         System.out.println("Hello! I'm Asher. What can I do for you?");
     }
@@ -10,11 +14,37 @@ public class Asher {
     }
 
     public static void echoCommand(String command) {
-        System.out.println(command);
+        System.out.println("added:" + " " + command);
     }
 
     public static boolean isExitCommand(String command) {
         return command.toLowerCase().equals("bye");
+    }
+
+    public static void addCommand(String command) {
+        if (count < userCommands.length) {
+            userCommands[count] = command;
+            count++;
+        } else {
+            System.out.println("Task is full, unable to add more");
+        }
+    }
+
+    public static void displayList() {
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + "." + " " + userCommands[i]);
+        }
+    }
+
+    public static void processCommand(String command) {
+        if (isExitCommand(command)) {
+            Asher.exit();
+        } else if (command.equals("list")) {
+            displayList();
+        } else {
+            addCommand(command);
+            echoCommand(command);
+        }
     }
 
     public static void main(String[] args) {
@@ -24,9 +54,8 @@ public class Asher {
 
         do {
             command = scanner.nextLine();
-            Asher.echoCommand(command);
-        } while (!Asher.isExitCommand(command));
+            Asher.processCommand(command);
 
-        Asher.exit();
+        } while (!Asher.isExitCommand(command));
     }
 }
