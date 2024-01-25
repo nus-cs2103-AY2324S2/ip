506669edadfece6c96c5b386762842d98b63bbcf
Murugan-Maniish <muruganmaniish1@gmail.com>
2024-01-25 08:07:43 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,12 +6,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String greet = "____________________________________________________________\n"
+        String horizontalLine = "____________________________________________________________\n";
+        String greet = horizontalLine
                 + "Greetings, mortal! I am Alastor, the Radio Demon at your service.\n"
                 + "What desires or inquiries do you bring to my infernal realm?\n";
-        String exit = "____________________________________________________________\n"
+        String exit = horizontalLine
                 + "Farewell, fleeting soul! 'Til our paths entwine once more.\n"
-                + "____________________________________________________________\n";
-        System.out.println(greet + exit);
+                + horizontalLine;
+        System.out.println(greet);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(horizontalLine + input +"\n" + horizontalLine);
+        }
+        System.out.println(exit);
     }
 }
