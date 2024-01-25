3a70965d4ba567336f402c4e8211a93327391e3c
ChrisHo1341 <christopher.hwh@gmail.com>
2024-01-25 21:56:44 +0800

    Level-0
    Level-0 thing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        String new_line = "____________________________________________________________ \n";
+        String greeting = "Good day good sir! I am Chatimous Maximous, here to help you with your every need!\n";
+        System.out.println(new_line + greeting);
+
+        String goodbye = "It has been a pleasure! I do hope to see you again!\n";
+        System.out.println(new_line + goodbye + new_line);
     }
 }
