6122d410e935803c797bdaec5f4d3b958cd407c8
Gowthaman <gowthaman169@gmail.com>
2024-01-25 11:28:56 +0800

    Implement echo functionality

--- src/main/java/Chitty.java
+++ src/main/java/Chitty.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Chitty {
     private static final String CHATBOT_NAME = "Chitty";
     private static final String GREETING_MESSAGE = String.format("Hello! I'm %s \nWhat can I do for you?\n", CHATBOT_NAME);
     private static final String GOODBYE_MESSAGE = "Bye. Hope to see you again soon!\n";
     private static final String SPACING = "---------------------------------------------------\n";
+    private static final String BYE = "bye";
+
+    private static void greet() {
+        System.out.println(SPACING + GREETING_MESSAGE + SPACING);
+    }
+
+    private static void speak(String message) {
+        System.out.println(SPACING + message + "\n" + SPACING);
+    }
+    private static void bye() {
+        System.out.println(SPACING + GOODBYE_MESSAGE + SPACING);
+    }
 
     public static void main(String[] args) {
-        System.out.println(SPACING + GREETING_MESSAGE + SPACING + GOODBYE_MESSAGE + SPACING);
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(BYE)) {
+                break;
+            } else {
+                speak(input);
+            }
+        }
+        bye();
     }
 }
