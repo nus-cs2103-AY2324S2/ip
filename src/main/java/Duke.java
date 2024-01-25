0d07656cd30cd12fbb8b5f907ac5e31fa39b0dec
Rishit02 <r02ishit@gmail.com>
2024-01-25 09:06:00 +0530

    Rename, Greet and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Riri";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("-------------------------");
+        System.out.println("Hello I am " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------");
     }
 }
