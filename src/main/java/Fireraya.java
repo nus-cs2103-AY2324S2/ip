9562e85c71a91ccc01f6830ee937ebdde87a1383
Dexter Wong <dexterwongster@gmail.com>
2024-01-25 11:48:06 +0800

    Level-3 Added functionality to mark and unmark tasks

--- src/main/java/Fireraya.java
+++ src/main/java/Fireraya.java
@@ -6,33 +6,40 @@ public class Fireraya {
 
     public static ArrayList<Task> tasks = new ArrayList<Task>();
 
-    public static void Start() {
+    public static void start() {
         System.out.println("--------------------------------");
         System.out.println("Hello, my name is Fireraya!");
         System.out.println("What can I do for you today sir?");
         System.out.println("--------------------------------");
     }
 
-    public static void AddTask(String a) {
+    public static void addTask(String a) {
         tasks.add(new Task(a));
         System.out.println("added: " + a);
     }
 
-    public static void ListTasks() {
-        System.out.println("Here are your tasks!");
+    public static void listTasks() {
+        System.out.println("Here are your tasks, please don't dissapoint me");
     for (int i = 0; i < tasks.size(); i++) {
-        System.out.println( i + ". " + tasks.get(i).getTask());
+        Task current = tasks.get(i);
+        int num = i + 1;
+        System.out.println( num + "." + current.getCompletionIcon() + current.getTask());
         }
     }
 
-    public static void End() {
+    public static void listTask(int i) {
+        Task current = tasks.get(i);
+        int num = i + 1;
+        System.out.println( num + "." + current.getCompletionIcon() + current.getTask());
+    }
 
+    public static void end() {
         System.out.println("Bye, hope to see you soon!");
     }
 
     public static void main(String[] args) {
 
-        Start();
+        start();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -40,16 +47,36 @@ public class Fireraya {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
-                End();
+                end();
                 break;
             }
 
             if (input.equals("list")) {
-                ListTasks();
+                listTasks();
                 continue;
             }
 
-            AddTask(input);
+            if (input.length() >= 4) {
+                if (input.substring(0, 4).equals("mark")) {
+                    int curr = Integer.parseInt(input.substring(5)) - 1;
+                    tasks.get(curr).markAsDone();
+                    System.out.println("About time you did something with your life:");
+                    listTask(curr);
+                    continue;
+                }
+            }
+
+            if (input.length() >= 6) {
+                if (input.substring(0, 6).equals("unmark")) {
+                    int curr = Integer.parseInt(input.substring(7)) - 1;
+                    tasks.get(curr).markAsUndone();
+                    System.out.println("Get a grip. What are you doing man. Task Undone.");
+                    listTask(curr);
+                    continue;
+                }
+            }
+
+            addTask(input);
         }
         scanner.close();
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getCompletionIcon() {
+        return (isDone ? "[X] " : "[ ] ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
     }
 
     public String getTask() {
-        return this.description;
+        return description;
     }
 
 }
