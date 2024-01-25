8e06bf934cc69a8ce9aa8b55aed2ddfefb5ca73d
moguries <celestehz03@gmail.com>
2024-01-25 18:51:47 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
+    private static final String NAME = "Fatnom";
+    private static final int lineLength = 60;
+
+    public static void printLine() {
+        for (int i = 0; i < lineLength; i++) {
+            System.out.print("_");
+        }
+        System.out.print("\n");
+    }
+
+    public static void printMessage(String message) {
+        Duke.printLine();
+        System.out.println(message);
+        Duke.printLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMessage = "Hello! I'm " + NAME + "\nWhat can I do for you?";
+        Duke.printMessage(welcomeMessage);
+        System.out.println("Bye. Hope to see you again soon!");
+        Duke.printLine();
     }
 }
