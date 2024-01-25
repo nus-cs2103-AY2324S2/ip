96e0d6dfea113fcbe8149e2d7723103f3e9b15bd
iamtr <jettzeong@gmail.com>
2024-01-25 15:45:45 +0800

    Added getTaskDescription method for Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,4 +10,8 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
+
+    public String getTaskDescription() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
