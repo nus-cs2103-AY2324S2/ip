ce69be53cdf6c499ce9e7a3018fdd031980e4aef
evedaktyl <evedaktyl@gmail.com>
2024-01-25 14:29:19 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
     private static Scanner sc = new Scanner(System.in);
-    private static ArrayList<String> messages = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
     static void breakLine() {
         System.out.println("---------------------------------------");
     }
@@ -35,14 +35,17 @@ public class Duke {
 
     static void add(String input) {
         Duke.breakLine();
-        messages.add(input);
+        Task t = new Task(input);
+        tasks.add(t);
         System.out.println("added: " + input);
         Duke.breakLine();
     }
     static void list() {
         Duke.breakLine();
-        for (int i = 0; i < messages.size(); i++) {
-            System.out.printf("%d. %s\n", i + 1, messages.get(i));
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task t = tasks.get(i);
+            System.out.printf("%d.[%s] %s\n", i + 1, t.getStatusIcon(), t.getDescription());
         }
         Duke.breakLine();
     }
@@ -58,6 +61,16 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 Duke.list();
+            } else if (input.startsWith("mark")) {
+                Duke.breakLine();
+                int taskNumber = (int) input.charAt(input.length() - 1) - (int) '0';
+                tasks.get(taskNumber-1).markAsDone();
+                Duke.breakLine();
+            } else if (input.startsWith("unmark")) {
+                Duke.breakLine();
+                int taskNumber = (int) input.charAt(input.length() - 1) - (int) '0';
+                tasks.get(taskNumber-1).unmark();
+                Duke.breakLine();
             } else {
                 Duke.add(input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+    }
+
+    public void unmark() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet");
+        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+    }
+}
