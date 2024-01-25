63aaba617f50ca60f6d7c6f420d129529c23652a
shunjieee <shunjie2000@gmail.com>
2024-01-26 00:18:30 +0800

    Duke.java: Add Level-1 requirements
    The chatbot now take user inputs.
    
    Implementing echo functionality allows user to interact with the
    chatbot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,10 +11,23 @@ public class Duke {
         System.out.println("________________________________________");
 
         System.out.println("Hello! I'm NextGenerationJarvis.");
-        System.out.println("What can I do for you?\n");
-        System.out.println("________________________________________");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________\n");
 
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        while (!userInput.toLowerCase().equals("bye")) {
+            System.out.println("\n________________________________________");
+            System.out.println("\'" + userInput + "\' command is not found!");
+            System.out.println("________________________________________\n");
+            userInput = scanner.nextLine();
+        }    
+
+        System.out.println("\n________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________\n");
+
+        scanner.close();
     }
 }
