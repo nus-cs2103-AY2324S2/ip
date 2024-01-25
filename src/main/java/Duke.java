a21eb5a5934af5c7b0e69a4307f3021de45a109e
tejas-1405 <tejas.sadasivam@gmail.com>
2024-01-25 10:24:44 +0800

    Level 1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,8 +12,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
 
-        System.out.println("Hello! I'm tars.\n");
+        System.out.println("Hello! I'm tars.");
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String comd = scanner.nextLine();
+            if (comd.equals("bye")) {
+                break;
+            }
+            System.out.println(comd);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
