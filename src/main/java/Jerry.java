4fd31d0a528c15bb9c9b469e9f602269f97824d1
Bowei Jiang <>
2024-01-25 12:03:00 +0800

    Mark as Done + Task Class

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -5,27 +5,42 @@ public class Jerry {
         Scanner scanner = new Scanner(System.in);
         String message = "Hello! I'm Jerry.\n" + "Anything I can do for you?";
         System.out.println(message);
-        String[] list =  new String[100];
+
+        Task[] tasks =  new Task[100];
         int index = 0;
 
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
+            String[] parts = input.split(" ", 2);
 
             if (input.trim().equalsIgnoreCase("bye")) {
                 System.out.println("Bye!");
                 break;
             }
 
-            if (input.trim().equalsIgnoreCase("list")) {
-                for (int x = 0; list[x] != null; x++){
-                    System.out.println((x+1) + ". " + list[x]);
+            else if (input.trim().equalsIgnoreCase("list")) {
+                System.out.println("Here are the items in your list:");
+                for (int x = 0; tasks[x] != null; x++){
+                    System.out.println((x+1) + "." + tasks[x]);
                 }
-                continue;
             }
-            list[index] = input;
-            index++;
-
-            System.out.println("added: " + input);
+            else if (parts[0].equalsIgnoreCase("mark") && parts.length > 1) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < index) {
+                    tasks[taskIndex].markDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskIndex]);
+                }
+            } else if (parts[0].equalsIgnoreCase("unmark") && parts.length > 1) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < index) {
+                    tasks[taskIndex].markNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[taskIndex]);
+                }
+            } else {
+                tasks[index] = new Task(input);
+                index++;
+                System.out.println("added: " + input);
+            }
         }
 
         scanner.close();
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
+
