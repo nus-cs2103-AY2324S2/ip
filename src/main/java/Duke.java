869ab1dfa4c474cc57987e9d7574fd4dad57b6b0
James Shen <95962838+shamesjen@users.noreply.github.com>
2024-01-25 23:24:13 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> userInputList = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String name = "James";
         System.out.println("Hello! I'm " + name + "\n");
         System.out.println("What can I do for you? \n");
 
         while(true) {
             String input = scanner.nextLine();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon! \n");
                 break;
-            }
-
-            else if (input.equalsIgnoreCase("list")) {
-                for (int i = 0; i < userInputList.size(); i++) {
-                    System.out.println((i + 1) + ". " + userInputList.get(i));
+            } else if (command.equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
                 }
-            }
-
-            else if (input.equalsIgnoreCase("blah")) {
+            } else if (command.equalsIgnoreCase("blah")) {
                 System.out.println("blah");
-            }
-
-            else {
-                userInputList.add(input);
+            } else if (command.equalsIgnoreCase("mark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                Task task = tasks.get(index);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + task);
+            } else if (command.equalsIgnoreCase("unmark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                Task task = tasks.get(index);
+                task.markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + task);
+            } else {
+                tasks.add(new Task(input));
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return (this.isDone ? "[X] " : "[ ] ") + this.description;
+    }
+}
