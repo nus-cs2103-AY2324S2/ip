731fc3445277cfd4851447ba38d43f7915572c42
Zhi Wei <tanzhiwei7@gmail.com>
2024-01-25 20:09:36 +0800

    Level 4. ToDos, Events, Deadlines Added Todo, Event and Deadline classes.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,15 +16,55 @@ public class Duke {
         Task currTask;
 
         while (!command.equals("bye")) {
+
             if (command.equals("list")) {
                 System.out.println(line);
                 System.out.println("Here are the tasks in your list:");
                 for (Task s : myList) {
-                    System.out.println(index + ".[" + s.getStatusIcon() + "] " + s);
+                    System.out.println(index + "." + s);
                     index++;
                 }
                 System.out.println(line + "\n");
                 index = 1; // Reset index to 1
+                command = scanner.nextLine(); // Read next command
+                continue;
+            }
+
+            if (command.contains("todo") || command.contains("deadline") || command.contains("event")) {
+                tempArr = command.split(" ");
+                switch (tempArr[0]) {
+                    case ("todo"):
+                        System.out.println(line);
+                        Todo currTodo = new Todo(command.substring(5));
+                        myList.add(currTodo);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(currTodo);
+                        System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                        System.out.println(line);
+                        break;
+
+                    case ("deadline"):
+                        System.out.println(line);
+                        tempArr = (command.substring(9)).split("/");
+                        Deadline currDeadline = new Deadline(tempArr[0], tempArr[1].substring(3));
+                        myList.add(currDeadline);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(currDeadline);
+                        System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                        System.out.println(line);
+                        break;
+
+                    case ("event"):
+                        System.out.println(line);
+                        tempArr = (command.substring(6)).split("/");
+                        Event currEvent = new Event(tempArr[0], tempArr[1].substring(5), tempArr[2].substring(3));
+                        myList.add(currEvent);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(currEvent);
+                        System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                        System.out.println(line);
+                        break;
+                }
                 command = scanner.nextLine(); // Read next command
                 continue;
             }
@@ -37,16 +77,18 @@ public class Duke {
                         System.out.println(line);
                         currTask.setDone();
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("   [" + currTask.getStatusIcon() + "] " + currTask);
+                        System.out.println(currTask);
                         System.out.println(line);
                         break;
+
                     case ("unmark"):
                         System.out.println(line);
                         currTask.setNotDone();
                         System.out.println("Ok, I've marked this task as not done yet:");
-                        System.out.println("   [" + currTask.getStatusIcon() + "] " + currTask);
+                        System.out.println(currTask);
                         System.out.println(line);
                         break;
+
                     default:
                         System.out.println("Wrong Command");
                 }
