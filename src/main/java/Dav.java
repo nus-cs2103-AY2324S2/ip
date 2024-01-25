ec68e0a7a5605bb80f56cd00408ef55787cfedac
NatLeong <>
2024-01-25 12:51:01 +0800

    Add Level 3

--- src/main/java/Dav.java
+++ src/main/java/Dav.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Dav {
     private static final int MAX_TASKS = 100;
-    private static String[] tasks = new String[MAX_TASKS];
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -35,9 +35,16 @@ public class Dav {
     }
 
     public static void processUserInput(String input) {
+        System.out.println("____________________________________________________________");
 
         if (input.equalsIgnoreCase("list")) {
             listTasks();
+        } else if (input.startsWith("mark ")) {
+            int taskIndex = Integer.parseInt(input.substring(5).trim());
+            markTaskDone(taskIndex);
+        } else if (input.startsWith("unmark ")) {
+            int taskIndex = Integer.parseInt(input.substring(7).trim());
+            unmarkTaskDone(taskIndex);
         } else if (!input.equalsIgnoreCase("bye")) {
             addTask(input);
         }
@@ -45,10 +52,10 @@ public class Dav {
         System.out.println("____________________________________________________________");
     }
 
-    public static void addTask(String task) {
+    public static void addTask(String taskDescription) {
         if (taskCount < MAX_TASKS) {
-            tasks[taskCount++] = task;
-            System.out.println(" added: " + task);
+            tasks[taskCount++] = new Task(taskDescription);
+            System.out.println(" added: " + taskDescription);
         } else {
             System.out.println(" Sorry, task list is full. Cannot add more tasks.");
         }
@@ -58,16 +65,69 @@ public class Dav {
         if (taskCount == 0) {
             System.out.println(" No tasks added yet.");
         } else {
+            System.out.println(" Here are the tasks in your list:");
             for (int i = 0; i < taskCount; i++) {
-                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                System.out.println(" " + (i + 1) + "." + tasks[i]);
             }
         }
     }
 
+    public static void markTaskDone(int taskIndex) {
+        if (isValidIndex(taskIndex)) {
+            tasks[taskIndex - 1].markAsDone();
+            System.out.println(" I've marked this task as done:");
+            System.out.println("   " + tasks[taskIndex - 1]);
+        } else {
+            System.out.println(" Invalid task index.");
+        }
+    }
+
+    public static void unmarkTaskDone(int taskIndex) {
+        if (isValidIndex(taskIndex)) {
+            tasks[taskIndex - 1].unmarkAsDone();
+            System.out.println(" I've marked this task as not done yet:");
+            System.out.println("   " + tasks[taskIndex - 1]);
+        } else {
+            System.out.println(" Invalid task index.");
+        }
+    }
+
+    public static boolean isValidIndex(int index) {
+        return index >= 1 && index <= taskCount;
+    }
+
     public static void exit() {
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(" Goodbye.");
         System.out.println("____________________________________________________________");
     }
 }
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with [X]
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
+
 
 
