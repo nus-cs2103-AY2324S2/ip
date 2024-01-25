dd433d3268a3ca51c2a9c936bccb6cab9aa4ee90
jeong-jaeho <e0960435@u.nus.edu>
2024-01-25 22:17:42 +0800

    Added Level-0: Rename, Greet, Exit

---
+++ src/main/java/Linus.java
@@ -0,0 +1,11 @@
+public class Linus {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Linus!\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+    }
+}
