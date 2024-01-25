a7cf3732dec1f330af8ef7062987788e61d2cb11
ChenXy128 <e0968917@u.nus.edu>
2024-01-25 15:16:37 +0800

    L1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String skeletalChatBox = "___________________________________\n"
-                            + "Hello! I'm Jinni\n"
-                            + "What can I do for you?\n"
-                            + "___________________________________\n"
-                            + "Bye. Hope to see you again soon!\n"
-                            + "___________________________________";
-        System.out.println(skeletalChatBox);
+        Scanner sc = new Scanner(System.in);
+        String greeting = "___________________________________\n"
+            + "Hello! I'm Jinni\n"
+            + "What can I do for you?\n"
+            + "___________________________________" ;
+        
+        System.out.println(greeting);
+        
+        while(true) {
+            String inputFromUser = sc.nextLine();
+            if ("bye".equals(inputFromUser.toLowerCase())) {
+                String bye = "___________________________________\n"
+                    + "Bye. Hope to see you again soon!\n"
+                    + "___________________________________";
+                System.out.println(bye);
+                break;
+            }
+            String echo = inputFromUser + "\n___________________________________" ;
+            System.out.println(echo);
+        }
+        sc.close();
     }
 }
