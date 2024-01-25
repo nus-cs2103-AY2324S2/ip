1e58daa725c6aa0d1c3664f9a6f99e143217a2ce
Jaspertzx <jaspertzx905@gmail.com>
2024-01-25 16:25:28 +0800

    added reader, and some personality!

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         String logo = "  ____        _     ____        _   \n"
                 + " |  _ \\      | |   |  _ \\      | |  \n"
                 + " | |_) | ___ | |__ | |_) | ___ | |_ \n"
                 + " |  _ < / _ \\| '_ \\|  _ < / _ \\| __|\n"
                 + " | |_) | (_) | |_) | |_) | (_) | |_ \n"
                 + " |____/ \\___/|_.__/|____/ \\___/ \\__|\n";
-        System.out.println("Hello! I'm \n" + logo);
-        System.out.println("What can I do for you?");
+        System.out.println("Hellooo! I'm \n" + logo);
+        System.out.println("As I am a Chatbot, I therefore have no arms.");
+        System.out.println("Knock knock, who's there? Definitely not BobBot!");
+        System.out.println("Jokes aside, what can I do for you?");
         System.out.println("______________________________________");
+        String command = reader.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            System.out.println("______________________________________");
+            command = reader.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+        reader.close();
     }
 }
