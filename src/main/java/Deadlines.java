bd06a289812ef8b7ddeb7f870cf5cdc3c85f3580
ChenXy128 <e0968917@u.nus.edu>
2024-01-25 21:37:53 +0800

    Deadlines

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Deadlines {
+public class Deadlines extends Task{
+    protected String by;
+    
+    public Deadlines(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+    
+    public String getBy() {
+        return this.by;
+    }
+    
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.getBy() + ")";
+    }
 }
