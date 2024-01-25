3ba4b686a3a9f0173fd7178665951bf54365ef2c
evedaktyl <evedaktyl@gmail.com>
2024-01-25 13:35:17 +0800

    Updated Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
+    private static Scanner sc = new Scanner(System.in);
     static void breakLine() {
         System.out.println("---------------------------------------");
     }
@@ -12,21 +14,24 @@ public class Duke {
         breakLine();
     }
     static void echo() {
-        Scanner sc = new Scanner(System.in);
 
         while (true) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
+                Duke.breakLine();
                 Duke.exit();
                 break;
             } else {
+                Duke.breakLine();
                 System.out.println(input);
+                Duke.breakLine();
             }
         }
 
         sc.close();
     }
+
     public static void main(String[] args) {
         Duke.breakLine();
         Duke.greet();
