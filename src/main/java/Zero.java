e856a1f306af537d115d3101563f114bf89b0cbb
Jeremiah Lim <43368034+jeremiahlzz@users.noreply.github.com>
2024-01-25 20:28:32 +0800

    Level 1. Echo

--- src/main/java/Zero.java
+++ src/main/java/Zero.java
@@ -1,11 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class Zero {
-    public static void main(String[] args) {
-        String divider = "____________________________________________________________\n";
-        String name = "Zero";
-        String greeting = "Hello! I'm " + name + ".";
-        String whatDo = "What can I do for you?";
-        String goodbye = "Bye. Hope to see you again soon!";
-        System.out.println(divider + greeting + '\n' + whatDo);
-        System.out.println(divider + goodbye + '\n' + divider);
+    private static final String name = "Zero";
+    private static final String divider = "____________________________________________________________\n";
+    private static final String greeting = "Hello! I'm " + name + ".\nWhat can I do for you?";
+    private static final String goodbye = "Bye. Hope to see you again soon!";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+        
+        pw.println(divider + greeting + '\n' + divider);
+        pw.flush();
+
+        String input;
+        do {
+            input = br.readLine();
+            switch (input) {
+                case "bye":
+                    pw.println(divider + goodbye + '\n' + divider);
+                    break;
+                default:
+                    pw.println(divider + input + '\n' + divider);
+                    break;
+            }
+            pw.flush();
+        } while (!input.equals("bye"));
+
+        br.close();
+        pw.close();
     }
 }
