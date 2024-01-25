3341cc0433ae3a519d4a87d52bcd6d417aef9a87
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-01-25 14:27:59 +0800

    Duke.java: Add basic greeting with new chatbot name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+        String lineGap = "____________________________________________________________";
+        System.out.println(logo);
+        System.out.println(lineGap + "\nHello! I'm Aegis.\n"
+                         + "What can I do for you?\n" + lineGap);
+        System.out.println("Bye. Hope to see you again soon!\n" + lineGap);
     }
 }
