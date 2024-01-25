591910d711ffe0203c57a105cc193a4668d9c4be
PallonCX <cheexiang03@gmail.com>
2024-01-25 15:30:20 +0800

    Modify Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Print logo
+        String logo = " __   ___  _____   __       __       __    __      ___ \n"
+                + "|  | /  / |_   _| |  |     |  |     |  |  |  |    / _ \\ \n"
+                + "|  |/  /    | |   |  |     |  |     |  |  |  |   / /_\\ \\ \n"
+                + "|  |\\  \\   _| |_  |  |___  |  |___  |  |__|  |  / _____ \\ \n"
+                + "|__| \\__\\ |_____| |______| |______|  \\______/  /_/     \\_\\ \n";
+        System.out.println(logo);
+
+        // Initial skeletal version that simply greets the user and exits.
+        String longLine = "_______________________________________________";
+        System.out.println(longLine);
+        System.out.println("Hello, I'm Killua.");
+        System.out.println("What can I do for you?");
+        System.out.println(longLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(longLine);
+
     }
 }
