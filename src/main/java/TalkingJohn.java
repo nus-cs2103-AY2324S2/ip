c99d397b690d88d9ae16f856a5dfac3130b5aec4
John Yoo <e0968839@u.nus.edu>
2024-01-25 11:22:46 +0800

    Implemented TalkingJohn Level0

---
+++ src/main/java/TalkingJohn.java
@@ -0,0 +1,8 @@
+public class TalkingJohn {
+    public static void main(String[] args) {
+        String logo = "Hello, I am TalkingJohn\n"
+                + "What can I do for you?\n"
+                + "Bye, hope to see you soon";
+        System.out.println(logo);
+    }
+}
