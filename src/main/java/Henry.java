241099d42b65ebac0af33cfc5b0496d4dd27e9eb
tohlh <tohliheng@gmail.com>
2024-01-25 09:11:41 +0800

    Rename, greet and exit

---
+++ src/main/java/Henry.java
@@ -0,0 +1,18 @@
+public class Henry {
+    public static void main(String[] args) {
+        String logo = "  _    _                       \n" +
+                " | |  | |                      \n" +
+                " | |__| | ___ _ __  _ __ _   _ \n" +
+                " |  __  |/ _ \\ '_ \\| '__| | | |\n" +
+                " | |  | |  __/ | | | |  | |_| |\n" +
+                " |_|  |_|\\___|_| |_|_|   \\__, |\n" +
+                "                          __/ |\n" +
+                "                         |___/ \n";
+        String greetMessage = "Hello! I'm Henry\nWhat can I do for you?";
+        String exitMessage = "See you again bro!";
+
+        System.out.println(logo);
+        System.out.println(greetMessage);
+        System.out.println(exitMessage);
+    }
+}
