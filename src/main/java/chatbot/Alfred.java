0a102454ac03cfb69aa08c1fc72ed4c3757ca0f5
theman-oj10 <manojnarender@gmail.com>
2024-01-25 22:26:41 +0800

    Alfred Class: Implement TaskTypes using Enums
    Alfred class uses three types of Tasks: Todos, Deadlines and Events.
    
    Using Enums will prevent accidental string comparisons that might lead to unpredictable behaviour.
    
    Let's
    * Implement TaskType Enum
    * Update addList method to use TaskType
    * Abstract out repeated functionality to reduce code duplication

--- src/main/java/chatbot/Alfred.java
+++ src/main/java/chatbot/Alfred.java
@@ -6,18 +6,15 @@ import java.util.Scanner;
 public class Alfred {
     protected static final String line = "________________________________________________________________________________________";
     protected static ArrayList<Task> list = new ArrayList<Task>();
-
+    public enum TaskType {
+        TODO, DEADLINE, EVENT
+    }
     public void greet() {
-        System.out.println(line);
-        System.out.println("Hello! I'm Alfred");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
+        this.echo("Hello! I'm Alfred\nWhat can I do for you?");
     }
 
     public void bye() {
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        this.echo("Bye. Hope to see you again soon!");
     }
 
     public void echo(String input) {
@@ -26,49 +23,90 @@ public class Alfred {
         System.out.println(line);
     }
 
+    private TaskType determineTaskType(String input) throws AlfredException {
+        if (input.startsWith("todo")) {
+            return TaskType.TODO;
+        } else if (input.startsWith("deadline")) {
+            return TaskType.DEADLINE;
+        } else if (input.startsWith("event")) {
+            return TaskType.EVENT;
+        } else {
+            throw new AlfredException("Sorry Master Bruce. I don't understand what you mean.");
+        }
+    }
     public void addList(String input) {
         try {
+            TaskType taskType = null;
             if (input.startsWith("todo")) {
+                taskType = TaskType.TODO;
+                if (input.length() <= 5) {
+                    throw new AlfredException("Sorry Master Bruce. The description of a todo cannot be empty.");
+                }
                 input = input.substring(5).trim();
-                if (input.isEmpty()) {
-                    throw new AlfredException("Sorry Master Bruce. The description of a todo cannot be empty. Please specify a description after todo.");
-                }
-                list.add(new ToDo(input));
             } else if (input.startsWith("deadline")) {
-                String[] splitResult = input.split("/by", 2);
-                String description = splitResult[0].substring(9).trim();
-                if (description.isEmpty()) {
-                    throw new AlfredException("Sorry Master Bruce. The description of an deadline cannot be empty. Please specify a description between deadline and /by.");
-                } else if (splitResult.length == 1 || splitResult[1].trim().isEmpty()) {
-                    // first condition checks for cases where /by is not present
-                    throw new AlfredException("Sorry Master Bruce. Please specify the due date or time by including /by due-date.");
+                taskType = TaskType.DEADLINE;
+                if (input.length() <= 9) {
+                    throw new AlfredException("Sorry Master Bruce. Please specify the description and due-date/time of the deadline by including /by.");
                 }
-                String by = splitResult[1].trim();
-                list.add(new Deadline(description, by));
+                input = input.substring(9).trim();
             } else if (input.startsWith("event")) {
-
-                String[] splitResults = input.split("/", 3);
-                String description = splitResults[0].substring(6).trim();
-                if (description.isEmpty()) {
-                    throw new AlfredException("Sorry Master Bruce. The description of an event cannot be empty. Please specify a description between event and /from.");
-                } else if (!(input.contains("/from") && input.contains("/to"))) {
-                    throw new AlfredException("Sorry Master Bruce. Please specify the start time and end time of the event by including /from start-time /to end-time.");
-                } else if (!input.contains("/from")) {
-                    throw new AlfredException("Sorry Master Bruce. Please specify the start time of the event by including /from start-time.");
-                } else if (!input.contains("/to")) {
-                    throw new AlfredException("Sorry Master Bruce. Please specify the end time of the event by including /to end-time.");
+                taskType = TaskType.EVENT;
+                if (input.length() <= 6) {
+                    throw new AlfredException("Sorry Master Bruce. Please specify the description, start time and end time of the event by including /from start-time /to end-time.");
                 }
-                String startTime = splitResults[1].substring(4).trim();
-                if (startTime.isEmpty()) {
-                    throw new AlfredException("Sorry Master Bruce, you have not indicated the start time of your event. Please specify the start time of the event by including /from start-time.");
-                }
-                String endTime = splitResults[2].substring(2).trim();
-                if (endTime.isEmpty()) {
-                    throw new AlfredException("Sorry Master Bruce. you have not indicated the end time of your event.Please specify the end time of the event by including /to end-time.");
-                }
-                list.add(new Event(description, startTime, endTime));
-            } else {
+                input = input.substring(6).trim();
+            }
+            if (taskType == null) {
                 throw new AlfredException("Sorry Master Bruce. I don't understand what you mean.");
+            }
+            if (input.isEmpty()) {
+                throw new AlfredException("Sorry Master Bruce. The description of a " + taskType + " cannot be empty.");
+            }
+
+            switch (taskType) {
+                case TODO:
+                    list.add(new ToDo(input));
+                    break;
+                case DEADLINE:
+                    String[] splitResult = input.split("/by", 2);
+                    String description = splitResult[0].trim();
+                    String by = null;
+                    try {
+                        by = splitResult[1].trim();
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        throw new AlfredException("Sorry Master Bruce. Please specify the due date or time by including /by due-date.");
+                    }
+                    if (description.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. The description of a deadline cannot be empty.");
+                    } else if (by.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. Please specify the due date or time by including /by due-date.");
+                    }
+                    list.add(new Deadline(description, by));
+                    break;
+                case EVENT:
+                    String[] splitResults = input.split("/", 3);
+                    String descriptionEvent = splitResults[0].trim();
+                    String startTime = null;
+                    String endTime = null;
+                    for (String s : splitResults) {
+                        if (s.startsWith("from")) {
+                            startTime = s.substring(4).trim();
+                        } else if (s.startsWith("to")) {
+                            endTime = s.substring(2).trim();
+                        }
+                    }
+                    if (descriptionEvent.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. The description of an event cannot be empty.");
+                    } else if (startTime == null || endTime == null || startTime.isEmpty() && endTime.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. Please specify the start time and end time of the event by including /from start-time /to end-time.");
+                    } else if (startTime.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. Please specify the start time of the event by including /from start-time");
+                    } else if (endTime.isEmpty()) {
+                        throw new AlfredException("Sorry Master Bruce. Please specify the end time of the event by including /to end-time");
+                    }
+                    list.add(new Event(descriptionEvent, startTime, endTime));
+                    break;
             }
         } catch(AlfredException e){
             e.printError();
@@ -78,11 +116,10 @@ public class Alfred {
         this.echo(String.format("Got it. I've added this task:\n  %s\nNow you have %d %s in the list.", list.get(list.size()-1).toString(), list.size(), singularTask));
     }
 
+
     public void printList() {
         if (list == null || list.size() == 0) {
-            System.out.println(line);
-            System.out.println("Sorry Master Bruce. There are no tasks in the list.");
-            System.out.println(line);
+            this.echo("Sorry Master Bruce. There are no tasks in the list.");
             return;
         }
         System.out.println(line);
@@ -93,120 +130,62 @@ public class Alfred {
     }
 
     public void markList(String index) {
-        int extractedIdx = 0;
-        try {
-            if (index.isEmpty()) {
-                throw new AlfredException("Sorry Master Bruce. Please enter a number after 'mark'.");
-            }
-            try {
-                extractedIdx = Integer.parseInt(index);
-            }
-            catch (NumberFormatException e) {
-                if (list.size() == 1) {
-                    throw new AlfredException("Sorry Master Bruce. Please enter a number. There is only 1 task in the list.");
-                } else {
-                    throw new AlfredException("Sorry Master Bruce. Please enter a number in the range of 1 to " + list.size() + ".");
-                }
-            }
-            extractedIdx -= 1;
-            if (list.size() == 0) {
-                throw new AlfredException("Sorry Master Bruce. There are no tasks in the list.");
-            } else if (extractedIdx >= list.size() || extractedIdx < 0) {
-                if (list.size() == 1) {
-                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. There is only 1 task in the list.");
-                } else {
-                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. Please enter a number in the range of 1 to " + list.size() + ".");
-                }
-            } else if (list.get(extractedIdx).isDone()) {
-                throw new AlfredException("Sorry Master Bruce. This task has already been marked as done.");
-            }
-        } catch (AlfredException e) {
-            e.printError();
+        int extractedIdx = validateAndExtractIndex(index);
+        if (extractedIdx == -1) {
+            return;
+        }
+        if (list.get(extractedIdx).isDone()) {
+            this.echo("Sorry Master Bruce. This task has already been marked as done.");
             return;
         }
         list.get(extractedIdx).toggleDone();
-        System.out.println(line);
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + list.get(extractedIdx));
-        System.out.println(line);
+        this.echo("Nice! I've marked this task as done:\n  " + list.get(extractedIdx).toString());
     }
 
     public void unmarkList(String index) {
-        int extractedIdx = 0;
-        try {
-        if (index.isEmpty()) {
-            throw new AlfredException("Sorry Master Bruce. Please enter a number after 'unmark'.");
+        int extractedIdx = validateAndExtractIndex(index);
+        if (extractedIdx == -1) {
+            return;
         }
-        try {
-            extractedIdx = Integer.parseInt(index);
-        }
-        catch (NumberFormatException e) {
-            if (list.size() == 1) {
-                throw new AlfredException("Sorry Master Bruce. Please enter a number. There is only 1 task in the list.");
-            } else {
-                throw new AlfredException("Sorry Master Bruce. Please enter a number in the range of 1 to " + list.size() + ".");
-            }
-        }
-        extractedIdx -= 1;
-        if (list.size() == 0) {
-            throw new AlfredException("Sorry Master Bruce. There are no tasks in the list.");
-        } else if (extractedIdx >= list.size() || extractedIdx < 0) {
-            if (list.size() == 1) {
-                throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. There is only 1 task in the list.");
-            } else {
-                throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. Please enter a number in the range of 1 to " + list.size() + ".");
-            }
-        } else if (!list.get(extractedIdx).isDone()) {
-            throw new AlfredException("Sorry Master Bruce. This task has already been marked as not done.");
-        }
-        } catch (AlfredException e) {
-            e.printError();
+        if (!list.get(extractedIdx).isDone()) {
+            this.echo("Sorry Master Bruce. This task has already been marked as not done.");
             return;
         }
         list.get(extractedIdx).toggleDone();
-        System.out.println(line);
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + list.get(extractedIdx));
-        System.out.println(line);
+        this.echo("OK, I've marked this task as not done yet:\n  " + list.get(extractedIdx).toString());
     }
 
     public void deleteList(String index) {
-        int extractedIdx = 0;
-        try {
-            if (index.isEmpty()) {
-                throw new AlfredException("Sorry Master Bruce. Please enter a number after 'delete'.");
-            }
-            try {
-                extractedIdx = Integer.parseInt(index);
-            }
-            catch (NumberFormatException e) {
-                if (list.size() == 1) {
-                    throw new AlfredException("Sorry Master Bruce. Please enter a number. There is only 1 task in the list.");
-                } else {
-                    throw new AlfredException("Sorry Master Bruce. Please enter a number in the range of 1 to " + list.size() + ".");
-                }
-            }
-            extractedIdx -= 1;
-            if (list.size() == 0) {
-                throw new AlfredException("Sorry Master Bruce. There are no tasks in the list.");
-            } else if (extractedIdx >= list.size() || extractedIdx < 0) {
-                if (list.size() == 1) {
-                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. There is only 1 task in the list.");
-                } else {
-                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. Please enter a number in the range of 1 to " + list.size() + ".");
-                }
-            }
-        } catch (AlfredException e) {
-            e.printError();
+        int extractedIdx = validateAndExtractIndex(index);
+        if (extractedIdx == -1) {
             return;
         }
         String removedTask = list.get(extractedIdx).toString();
         list.remove(extractedIdx);
-        System.out.println(line);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("  " + removedTask);
-        System.out.println("Now you have " + list.size() + " tasks in the list.");
-        System.out.println(line);
+        this.echo("Noted. I've removed this task:\n  " + removedTask + "\nNow you have " + list.size() + " tasks in the list.");
+    }
+    private int validateAndExtractIndex(String index) {
+        try {
+            if (index.isEmpty()) {
+                throw new AlfredException("Please enter a number after the command.");
+            }
+            int extractedIdx = Integer.parseInt(index) - 1;
+            if (list.isEmpty()) {
+                throw new AlfredException("Sorry Master Bruce. There are no tasks in the list.");
+            } else if (extractedIdx < 0 || extractedIdx >= list.size()) {
+                if (list.size() == 1) {
+                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. There is only one item in the list.");
+                } else {
+                    throw new AlfredException("Sorry Master Bruce. The task number you have entered is not in the list. Please enter a number in the range of 1 to " + list.size() + ".");
+                }
+            }
+            return extractedIdx;
+        } catch (NumberFormatException e) {
+            this.echo("Sorry Master Bruce. Please enter a valid number.");
+        } catch (AlfredException e) {
+            e.printError();
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
