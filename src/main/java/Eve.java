eb7eeb0fee3ed1d13e84d7e593fdf42a7fce1498
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-01-25 21:50:01 +0800

    Level-2

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Eve {
     public static void main(String[] args) {
@@ -7,14 +8,23 @@ public class Eve {
         System.out.println(" What can I do for you?");
 
         String input = "";
+        ArrayList<String> list = new ArrayList<>();
 
         while(!input.equals("bye")){
             input = sc.nextLine();
 
             if (input.equals("bye")){
                 System.out.println(" Bye. Hope to see you again soon !");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++ ){
+                    int j = i + 1;
+                    String temp = list.get(i);
+                    System.out.println(j + "." + temp);
+                }
+                
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
 
