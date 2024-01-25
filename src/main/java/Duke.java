fc27e293efa29c74130a77a2daf65f90bc0286a9
vannykin <e0969161@u.nus.edu>
2024-01-25 17:46:26 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        List<String> todo = new ArrayList<>();
+        List<String> marked = new ArrayList<>();
+        int count = 1;
+        System.out.println("Hello! I'm Lumiere\n" + "What can I do for you?\n");
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < count - 1; i++) {
+                    System.out.println(marked.get(i) + " " + todo.get(i));
+                }
+            } else if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (command.contains("unmark")) {
+                int space = command.indexOf(" ");
+                int task = Integer.valueOf(command.substring(space + 1));
+                String curr = marked.get(task - 1);
+                String newMark = curr.replace("X", " ");
+                marked.set(task - 1, newMark);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + todo.get(task - 1));
+            } else if (command.contains("mark")) {
+                int space = command.indexOf(" ");
+                int task = Integer.valueOf(command.substring(space + 1));
+                String curr = marked.get(task - 1);
+                String newMark = curr.replace(" ", "X");
+                marked.set(task - 1, newMark);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + todo.get(task - 1));
+            } else { // to add new item
+                System.out.println("added: " + command);
+                todo.add(command);
+                marked.add(Integer.toString(count) + ".[ ]");
+                count++;
+            }
+        }
+        sc.close();
     }
 }
