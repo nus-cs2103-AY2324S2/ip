87a6b7d3833b5656a6aebf77d77b415ccdeabb01
nuyer <98694104+nuyer@users.noreply.github.com>
2024-01-25 20:40:47 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,27 @@
-public class Duke {
+import java.util.Scanner;
+public class Duke{
     public static void main(String[] args) {
 
         String horiLine = "____________________________________________________________\n";
 
         String greet = "Hello! I'm Dino\n"
                 + "What can I do for you?\n"
-                + horiLine
-                + "Bye. Hope to see you again soon!\n"
-                + horiLine;
+                +  horiLine;
         System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+        while(!input.equalsIgnoreCase("bye")) {
+            String echo = horiLine
+                    + input + "\n"
+                    + horiLine;
+            System.out.println(echo);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
 
