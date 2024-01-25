03892e0674ef665ad7ba4c40410bb999ffb59d9c
chenyixin0 <e0968053@u.nus.edu>
2024-01-25 09:32:53 +0800

    Chatbot echoes user input

--- src/main/java/TalkingBox.java
+++ src/main/java/TalkingBox.java
@@ -1,18 +1,29 @@
+import java.util.Scanner;
+
 public class TalkingBox {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String NAME = "Talking Box";
         /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__,_|_s|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         */
-        
+
         System.out.println("Hello, I am the " + NAME);
         System.out.println("What do you want to talk about?");
+
+        String input = in.nextLine();
+        while (!(input.equals("bye"))) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
+
         System.out.println("Goodbye!");
+
     }
 }
 
