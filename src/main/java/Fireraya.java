8009b56da95f65e84f46072e6241990decabd533
Dexter Wong <dexterwongster@gmail.com>
2024-01-25 16:43:42 +0800

    Level-4 Added functionality to Todo, Deadline, and Event commands

--- src/main/java/Fireraya.java
+++ src/main/java/Fireraya.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,19 +19,48 @@ public class Fireraya {
         System.out.println("added: " + a);
     }
 
+    public static void addTodo(String a) {
+        tasks.add(new Todo(a));
+        System.out.println("I've added this Todo:");
+        Task last = tasks.get(tasks.size() - 1);
+        System.out.println(last.toString());
+        taskCount();
+    }
+
+    public static void taskCount() {
+        int number = tasks.size();
+        System.out.println("Now you have " + number + " tasks in the list");
+    }
+
+    public static void addDeadline(String task, String deadline) {
+        tasks.add(new Deadline(task, deadline));
+        System.out.println("I've added this Deadline:");
+        Task last = tasks.get(tasks.size() - 1);
+        System.out.println(last.toString());
+        taskCount();
+    }
+
+    public static void addEvent(String task, String from, String to) {
+        tasks.add(new Event(task, from, to));
+        System.out.println("I've added this Event:");
+        Task last = tasks.get(tasks.size() - 1);
+        System.out.println(last.toString());
+        taskCount();
+    }
+
     public static void listTasks() {
-        System.out.println("Here are your tasks, please don't dissapoint me");
+        System.out.println("Here is a list of your tasks!");
     for (int i = 0; i < tasks.size(); i++) {
         Task current = tasks.get(i);
         int num = i + 1;
-        System.out.println( num + "." + current.getCompletionIcon() + current.getTask());
+        System.out.println(num + "." + current.toString());
         }
     }
 
     public static void listTask(int i) {
         Task current = tasks.get(i);
         int num = i + 1;
-        System.out.println( num + "." + current.getCompletionIcon() + current.getTask());
+        System.out.println( num + "." + current.toString());
     }
 
     public static void end() {
@@ -45,35 +75,87 @@ public class Fireraya {
 
         while (true) {
             String input = scanner.nextLine();
+            String[] all = input.split(" ");
+            String keyword = all[0];
+            int arrLen = all.length;
 
-            if (input.equals("bye")) {
+            if (keyword.equals("bye")) {
                 end();
                 break;
             }
 
-            if (input.equals("list")) {
+            if (keyword.equals("list")) {
                 listTasks();
                 continue;
             }
 
-            if (input.length() >= 4) {
-                if (input.substring(0, 4).equals("mark")) {
-                    int curr = Integer.parseInt(input.substring(5)) - 1;
-                    tasks.get(curr).markAsDone();
-                    System.out.println("About time you did something with your life:");
-                    listTask(curr);
-                    continue;
-                }
+            if (keyword.equals("mark")) {
+                int curr =  Integer.parseInt(all[1]) - 1;
+                tasks.get(curr).markAsDone();
+                System.out.println("Good job completing these tasks!:");
+                listTask(curr);
+                continue;
             }
 
-            if (input.length() >= 6) {
-                if (input.substring(0, 6).equals("unmark")) {
-                    int curr = Integer.parseInt(input.substring(7)) - 1;
-                    tasks.get(curr).markAsUndone();
-                    System.out.println("Get a grip. What are you doing man. Task Undone.");
-                    listTask(curr);
+            if (keyword.equals("unmark")) {
+                int curr = Integer.parseInt(all[1]) - 1;
+                tasks.get(curr).markAsUndone();
+                System.out.println("Oh no :<. Task Undone.");
+                listTask(curr);
                     continue;
                 }
+
+            if (keyword.equals("todo")) {
+                    String rest = String.join(" ",Arrays.copyOfRange(all, 1, arrLen));
+                    addTodo(rest);
+                    continue;
+                }
+
+            if (keyword.equals("deadline")) {
+                int index = -1;
+                int i = 0;
+                String breakpoint = "/by";
+                for (String element : all) {
+                    if (element.equals(breakpoint)) {
+                        index = i;
+                        break;
+                        }
+                    i++;
+                    }
+                String deadline = String.join(" ",Arrays.copyOfRange(all, index + 1, arrLen));
+                String task = String.join(" ",Arrays.copyOfRange(all, 1, index));
+                addDeadline(task, deadline);
+                continue;
+                }
+
+
+            if (keyword.equals("event")) {
+                int indexf = -1;
+                int indext = -1;
+                int f = 0;
+                int t = 0;
+                String fromPoint = "/from";
+                String toPoint = "/to";
+
+                for (String element : all) {
+                    if (element.equals(fromPoint)) {
+                        indexf = f;
+                        break; }
+                    f++;
+                }
+
+                for (String element : all) {
+                    if (element.equals(toPoint)) {
+                        indext = t;
+                        break; }
+                    t++;
+                }
+
+                String task = String.join(" ",Arrays.copyOfRange(all, 1, indexf));
+                String from = String.join(" ",Arrays.copyOfRange(all, indexf + 1, indext));
+                String to = String.join(" ",Arrays.copyOfRange(all, indext + 1, arrLen));
+                addEvent(task, from, to);
+                continue;
             }
 
             addTask(input);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,4 +23,9 @@ public class Task {
         return description;
     }
 
+    @Override
+    public String toString() {
+        return this.getCompletionIcon() + this.getTask();
+    }
+
 }
