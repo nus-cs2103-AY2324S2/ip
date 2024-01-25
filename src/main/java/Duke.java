0d3b93913843be8681e1157c333ee3def8bd000c
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-01-25 20:59:07 +0800

    Add new bot name, greeting and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String DIV = "____________________________________________________________";
+
+        System.out.println(DIV);
+        System.out.println("Hello! I'm Puffin.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIV);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIV);
     }
 }
