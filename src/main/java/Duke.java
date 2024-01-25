487f16afd44796cc566226fa98cbf1b019ff32ba
Tan Jie Ling <tanjieling3@gmail.com>
2024-01-25 13:10:45 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
         //Greetings
         String logo = "Tommy";
         String divider = "____________________________";
@@ -12,19 +15,50 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
-        //Echo
+
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            if (!userInput.equals("list")) {
-                list.add(userInput);
-                System.out.println("added: " + userInput);
+
+            if (userInput.equals("list")) {
+                // listing
                 System.out.println(divider);
-            } else {
-                System.out.println(divider);
-                for (String item: list) {
-                    int index = list.indexOf(item) + 1;
-                    System.out.println(index + ": " + item);
+
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + "." + hashMap.get(index) + " " + list.get(i));
                 }
+
+                System.out.println(divider);
+            } else if (userInput.contains("unmark")) {
+                // Unmarking
+                System.out.println(divider);
+
+                int index = Integer.parseInt(userInput.substring(7));
+                hashMap.put(index, "[ ]");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(hashMap.get(index) + " " + list.get(index - 1));
+
+                System.out.println(divider);
+
+            } else if (userInput.contains("mark")) {
+                // Marking done
+                System.out.println(divider);
+
+                int index = Integer.parseInt(userInput.substring(5));
+                hashMap.put(index, "[X]");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(hashMap.get(index) + " " + list.get(index - 1));
+
+                System.out.println(divider);
+
+            } else {
+                // Adding tasks
+                System.out.println(divider);
+
+                list.add(userInput);
+                hashMap.put(list.size(), "[ ]");
+                System.out.println("added: " + userInput);
+
                 System.out.println(divider);
 
             }
@@ -36,5 +70,7 @@ public class Duke {
         System.out.println(divider);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
+
+        scanner.close();
     }
 }
