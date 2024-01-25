381d9f6957d42a1c338896f0983ec31c09ebc48f
erv-teo <107994607+erv-teo@users.noreply.github.com>
2024-01-25 17:55:02 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private static ArrayList<String> list = new ArrayList<>();
+    private static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -23,17 +23,33 @@ public class Duke {
         while (true) {
             Scanner input = new Scanner(System.in);
             String str = input.nextLine();
-            if (str.equals("bye") || str.equals("Bye")) {
+            String[] arr = str.split(" ");
+            String action = arr[0];
+            if (action.equals("bye") || action.equals("Bye")) {
                 System.out.println(bye);
                 break;
-            } else if (str.equals("list")){
+            } else if (action.equals("list")){
                 for (int i = 0; i < list.size(); i++) {
                     int n = i + 1;
                     System.out.println(indent + n + ". " + list.get(i));
                 }
+            } else if (action.equals("mark")) {
+                int index = Integer.valueOf(arr[1]);
+                Task target = list.get(index - 1);
+                target.mark();
+                System.out.println(indent + "Task have been marked as done.");
+                System.out.println(indent + target);
+            } else if (action.equals("unmark")) {
+                int index = Integer.valueOf(arr[1]);
+                Task target = list.get(index - 1);
+                target.unmark();
+                System.out.println(indent + "Task have been unmarked.");
+                System.out.println(indent + target);
+
             } else {
-                list.add(str);
-                System.out.println(indent + "added: " + str);
+                Task cur = new Task(str);
+                list.add(cur);
+                System.out.println(indent + "added: " + cur);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String name;
+    private boolean done;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return "[X] " + this.name;
+        } else {
+            return "[ ] " + this.name;
+        }
+    }
+}
