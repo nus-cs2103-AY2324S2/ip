f52bb26957f25e6768b7b0c8cf246a7f80c78c4c
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-26 02:35:23 +0800

    Level-1: Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public void greet() {
         System.out.println("Hello! I'm Bot\nWhat can I do for you? \n");
@@ -7,9 +9,24 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon! \n");
     }
 
+    public void echo(String input) {
+        System.out.println(input + "\n");
+    }
+
     public static void main(String[] args) {
         Duke bot = new Duke();
         bot.greet();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            bot.echo(input);
+            input = sc.nextLine();
+        }
+
+        sc.close();
+
         bot.exit();
     }
 }
