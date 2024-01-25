85c5fa47a2aaf09845ad89eead91839c98223af9
Bowei Jiang <>
2024-01-25 11:00:37 +0800

    Add, List

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -5,6 +5,8 @@ public class Jerry {
         Scanner scanner = new Scanner(System.in);
         String message = "Hello! I'm Jerry.\n" + "Anything I can do for you?";
         System.out.println(message);
+        String[] list =  new String[100];
+        int index = 0;
 
         while (true) {
             String input = scanner.nextLine();
@@ -14,7 +16,16 @@ public class Jerry {
                 break;
             }
 
-            System.out.println(input);
+            if (input.trim().equalsIgnoreCase("list")) {
+                for (int x = 0; list[x] != null; x++){
+                    System.out.println((x+1) + ". " + list[x]);
+                }
+                continue;
+            }
+            list[index] = input;
+            index++;
+
+            System.out.println("added: " + input);
         }
 
         scanner.close();
