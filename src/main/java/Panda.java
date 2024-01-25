70b4944f254382099f852e29698ca6cb8b39f559
modembcc <modemhappy@gmail.com>
2024-01-25 13:43:56 +0800

    Level-1

--- src/main/java/Panda.java
+++ src/main/java/Panda.java
@@ -1,11 +1,42 @@
+import java.util.Scanner;
+
 public class Panda {
-    public static void main(String[] args) {
+
+    private static boolean running = false;
+
+    private static void startUp() {
         System.out.println(
             "Hello! I'm Panda\n" + 
-            "What can I do for you?\n"
+            "What can I do for you?"
         );
+        running = true;
+    }
+
+    private static void shutDown() {
         System.out.println(
             "Bye. Hope to see you again soon!"
         );
+        running = true;
+    }
+
+    private static void comm() {
+        while(running) {
+            Scanner myObj  = new Scanner(System.in);
+            System.out.print("> ");
+            String userInput = myObj.nextLine();
+            if(userInput.equals("bye")) {
+                running = false;
+                myObj.close();
+            }
+            else {
+                System.out.println(userInput);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Panda.startUp();
+        Panda.comm();
+        Panda.shutDown();
     }
 }
