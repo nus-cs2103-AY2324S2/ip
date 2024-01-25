70165e9e8fe9b14c839e089efd867e66d79e983f
James Shen <95962838+shamesjen@users.noreply.github.com>
2024-01-26 00:20:43 +0800

    level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,31 +10,58 @@ public class Duke {
 
         while(true) {
             String input = scanner.nextLine();
-            String[] parts = input.split(" ", 2);
-            String command = parts[0];
-
-            if (command.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon! \n");
                 break;
-            } else if (command.equalsIgnoreCase("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
-                }
-            } else if (command.equalsIgnoreCase("blah")) {
-                System.out.println("blah");
-            } else if (command.equalsIgnoreCase("mark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
-                Task task = tasks.get(index);
-                task.markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n" + task);
-            } else if (command.equalsIgnoreCase("unmark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
-                Task task = tasks.get(index);
-                task.markAsNotDone();
-                System.out.println("OK, I've marked this task as not done yet:\n" + task);
-            } else {
-                tasks.add(new Task(input));
+            }
+
+            String[] parts = input.split(" ", 2);
+            String command = parts[0].toLowerCase();
+
+            switch (command) {
+                case "list":
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
+                    }
+                    break;
+                case "blah":
+                    System.out.println("blah");
+                    break;
+                case "todo":
+                    Todo newTodo = new Todo(parts[1]);
+                    tasks.add(newTodo);
+                    System.out.println("Got it. I've added this task:\n" + newTodo);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
+                case "deadline":
+                    String[] deadlineParts = parts[1].split(" /by ");
+                    Deadline newDeadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                    tasks.add(newDeadline);
+                    System.out.println("Got it. I've added this task:\n" + newDeadline);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
+                case "event":
+                    String[] eventParts = parts[1].split(" /from ");
+                    String[] fromto = eventParts[1].split("/to");
+                    Event newEvent = new Event(eventParts[0], fromto[0], fromto[1]);
+                    tasks.add(newEvent);
+                    System.out.println("Got it. I've added this task:\n" + newEvent);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
+                case "mark":
+                    int taskIndexToMark = Integer.parseInt(parts[1]) - 1;
+                    tasks.get(taskIndexToMark).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + tasks.get(taskIndexToMark));
+                    break;
+                case "unmark":
+                    int taskIndexToUnmark = Integer.parseInt(parts[1]) - 1;
+                    tasks.get(taskIndexToUnmark).markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:\n" + tasks.get(taskIndexToUnmark));
+                    break;
+                default:
             }
         }
+        scanner.close();
     }
 }
---
+++ src/main/java/Event.java
@@ -0,0 +1,23 @@
+public class Event extends Task {
+    private String from;
+    private String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.getFrom() + "to:" + this.getTo() + ")";
+    }
+}
