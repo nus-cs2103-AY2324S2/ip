dbaceeef628637201b03b3ba2de0024296c8f904
Emberlynn-Loo <E0960284@u.nus.edu>
2024-01-26 05:12:02 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,25 +3,38 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("    _______________________________________________________");
-        System.out.println("    Hello! I'm TALKTOMEORILLDIE");
-        System.out.println("    What can I do for you?");
+        System.out.println("     Hello! I'm TALKTOMEORILLDIE");
+        System.out.println("     What can I do for you?");
         System.out.println("    _______________________________________________________");
 
         Scanner inputs = new Scanner(System.in); //this is the scanner obj
+
+        String[] tasks = new String[100];
+        int tasknum = 0;
 
         while (true) {
             String echo = inputs.nextLine(); //this is getting the input
 
             if (echo.equals("bye") || echo.equals("Bye")) {
                 System.out.println("    _______________________________________________________");
-                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    _______________________________________________________");
                 break;
             }
 
-            System.out.println("    _______________________________________________________");
-            System.out.println("    " + echo);
-            System.out.println("    _______________________________________________________");
+            if (echo.equals("list") || echo.equals("List")) {
+                System.out.println("    _______________________________________________________");
+                for (int i = 0; i < tasknum; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("    _______________________________________________________");
+            } else {
+                System.out.println("    _______________________________________________________");
+                System.out.println("     added:" + echo);
+                tasks[tasknum] = echo;
+                tasknum++;
+                System.out.println("    _______________________________________________________");
+            }
         }
     }
 }
