b2ff5e667d34e50aac9eec155e06b6d51b97e7e5
VatsalVitosh <vatsalvitosh@gmail.com>
2024-01-26 03:52:29 +0800

    Level 1 - Echo

--- src/main/java/Lia.java
+++ src/main/java/Lia.java
@@ -1,7 +1,17 @@
+import java.util.*;
 public class Lia {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Lia");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Lia :)");
         System.out.println("What can I do for you?");
+        while (true) {
+            String readInput = sc.nextLine();
+            if (readInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(readInput);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
