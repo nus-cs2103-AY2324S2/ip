8c162fba4916392f2d2e11ca6a1b9af004675f4b
cleowenxuan <>
2024-01-25 10:37:34 +0800

    Level1 Completed

--- src/main/java/Asher.java
+++ src/main/java/Asher.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Asher {
     public static void greet() {
         System.out.println("Hello! I'm Asher. What can I do for you?");
@@ -6,8 +8,25 @@ public class Asher {
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static void echoCommand(String command) {
+        System.out.println(command);
+    }
+
+    public static boolean isExitCommand(String command) {
+        return command.toLowerCase().equals("bye");
+    }
+
     public static void main(String[] args) {
         Asher.greet();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        do {
+            command = scanner.nextLine();
+            Asher.echoCommand(command);
+        } while (!Asher.isExitCommand(command));
+
         Asher.exit();
     }
 }
