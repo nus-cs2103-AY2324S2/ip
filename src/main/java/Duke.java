23749534aeb41365013b87e8483b2e1154743e39
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2024-01-26 05:40:16 +0800

    add enum

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Duke {
+    enum Request {
+        BYE, LIST, MARK, TODO, DEADLINE, EVENT, DELETE, INVALID
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,77 +22,103 @@ public class Duke {
         try {
             while (true) {
                 String userInput = br.readLine();
-                if (userInput.equalsIgnoreCase("bye")) {
-                    break;
-                }
+                Request request = getRequest(userInput);
 
-                if (userInput.equalsIgnoreCase("list")) {
-                    System.out.println(myList.getItems());
-                } else if (userInput.startsWith("mark")) {
-                    try {
-                        int index = Integer.parseInt(userInput.substring("mark".length()).trim());
-                        System.out.println(myList.markTask(index));
-                    } catch (NumberFormatException e) {
-                        System.out.println("Please enter a number after mark.");
-                    }
-                } else if (userInput.startsWith("todo")) {
-                    try {
-                        String s = userInput.substring("todo".length()).trim();
-                        if (s.isEmpty()) {
-                            throw new DukeException("Task description cannot be empty.");
+                switch (request) {
+                    case BYE:
+                        System.out.println("Bye. Hope to see you again soon!");
+                        return;
+                    case LIST:
+                        System.out.println(myList.getItems());
+                        break;
+                    case MARK:
+                        try {
+                            int index = Integer.parseInt(userInput.substring("mark".length()).trim());
+                            System.out.println(myList.markTask(index));
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a number after mark.");
                         }
-                        Task task = new Todo(s);
-                        System.out.println(myList.addItem(task));
-                    } catch (DukeException e) {
-                        System.out.println("Error: " + e.getMsg());
-                    }
-                } else if (userInput.startsWith("deadline")) {
-                    try {
-                        String s = userInput.substring("deadline".length()).trim();
-                        String[] s1 = s.split("/by");
-                        if (s1.length > 2) {
-                            throw new DukeException("Multiple /by");
+                        break;
+                    case TODO:
+                        try {
+                            String s = userInput.substring("todo".length()).trim();
+                            if (s.isEmpty()) {
+                                throw new DukeException("Task description cannot be empty.");
+                            }
+                            Task task = new Todo(s);
+                            System.out.println(myList.addItem(task));
+                        } catch (DukeException e) {
+                            System.out.println("Error: " + e.getMsg());
                         }
-                        Task task = new Deadline(s1[0].trim(), s1[1].trim());
-                        System.out.println(myList.addItem(task));
-                    } catch (DukeException e) {
-                        System.out.println("Error: " + e.getMsg());
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        System.out.println("Please enter format deadline (task) /by (input)");
-                    }
-                } else if (userInput.startsWith("event")) {
-                    try {
-                        String s = userInput.substring("event".length()).trim();
-                        String[] s1 = s.split("/from");
-                        if (s1.length > 2) {
-                            throw new DukeException("Multiple /from");
+                        break;
+                    case DEADLINE:
+                        try {
+                            String s = userInput.substring("deadline".length()).trim();
+                            String[] s1 = s.split("/by");
+                            if (s1.length > 2) {
+                                throw new DukeException("Please enter format deadline (task) /by (input)");
+                            } else if (s1[1].trim().isEmpty()) {
+                                throw new DukeException("Empty timing. Please enter format deadline (task) /by (input)");
+                            }
+                            Task task = new Deadline(s1[0].trim(), s1[1].trim());
+                            System.out.println(myList.addItem(task));
+                        } catch (DukeException e) {
+                            System.out.println("Error: " + e.getMsg());
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("Please enter format deadline (task) /by (input)");
                         }
-                        String[] s2 = s1[1].split("/to");
-                        if (s2.length > 2) {
-                            throw new DukeException("Multiple /to");
+                        break;
+                    case EVENT:
+                        try {
+                            String s = userInput.substring("event".length()).trim();
+                            String[] s1 = s.split("/from");
+                            if (s1.length > 2) {
+                                throw new DukeException("Please enter format event (task) /from (input) /to (input)");
+                            } else if (s1[1].trim().isEmpty()) {
+                                throw new DukeException("Empty timing. Please enter format event (task) /from (input) /to (input)");
+                            }
+                            String[] s2 = s1[1].split("/to");
+                            if (s2.length > 2) {
+                                throw new DukeException("Please enter format event (task) /from (input) /to (input)");
+                            } else if (s2[1].trim().isEmpty()) {
+                                throw new DukeException("Empty timing. Please enter format event (task) /from (input) /to (input)");
+                            }
+                            Task task = new Event(s1[0].trim(), s2[0].trim(), s2[1].trim());
+                            System.out.println(myList.addItem(task));
+                        } catch (DukeException e) {
+                            System.out.println("Error: " + e.getMsg());
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("Please enter format event (task) /from (input) /to (input)");
                         }
-                        Task task = new Event(s1[0].trim(), s2[0].trim(), s2[1].trim());
-                        System.out.println(myList.addItem(task));
-                    } catch (DukeException e) {
-                        System.out.println("Error: " + e.getMsg());
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        System.out.println("Please enter format event (task) /from (input) /to (input)");
-                    }
-                } else if (userInput.startsWith("delete")) {
-                    try {
-                        int index = Integer.parseInt(userInput.substring("delete".length()).trim());
-                        System.out.println(myList.delete(index));
-                    } catch (NumberFormatException e) {
-                        System.out.println("Please enter a number after delete.");
-                    }
-                } else {
-                    System.out.println("OOPS! That was an invalid input");
+                        break;
+                    case DELETE:
+                        try {
+                            int index = Integer.parseInt(userInput.substring("delete".length()).trim());
+                            System.out.println(myList.delete(index));
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a number after delete.");
+                        }
+                        break;
+                    case INVALID:
+                        System.out.println("OOPS! That was an invalid input");
+                        break;
                 }
             }
-            System.out.println("Bye. Hope to see you again soon!");
 
         } catch (IOException e) {
             System.err.println("Error");
         }
+    }
+
+    private static Request getRequest(String userInput) {
+        String inputUpper = userInput.toUpperCase();
+
+        for (Request request : Request.values()) {
+            if (inputUpper.startsWith(request.name())) {
+                return request;
+            }
+        }
+
+        return Request.INVALID;
     }
 }
