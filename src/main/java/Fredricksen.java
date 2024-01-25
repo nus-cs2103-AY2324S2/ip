3cf667372c423356ebdb9f6ab9fbad48fcf48278
Zack-Tay <tayzhongkai@gmail.com>
2024-01-25 15:10:16 +0800

    add error message for some invalid inputs to Fredricksen.java and completed Level-5

--- src/main/java/Fredricksen.java
+++ src/main/java/Fredricksen.java
@@ -7,6 +7,19 @@ public class Fredricksen {
         System.out.println("What can I do for you?");
         System.out.println(line);
     }
+
+    public static void listOfCommands() {
+        System.out.println("You might have entered an invalid command!");
+        System.out.println("Below are the available commands and formats to follow!");
+        System.out.println("1. To view all your current task: list");
+        System.out.println("2. To add new task:");
+        System.out.println("    a. todos: todo <task>");
+        System.out.println("    b. deadlines: deadline <task> /by <deadline>");
+        System.out.println("    c. event: event <event> /from <startdate, starttiming> /to <enddate, endtiming>");
+        System.out.println("3. To mark task as completed: mark <task number>");
+        System.out.println("4. To unmark completed task: unmark <task number>");
+        System.out.println("5. To exit program: bye");
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<>();
@@ -22,18 +35,36 @@ public class Fredricksen {
             switch (first) {
                 case "mark":
                     System.out.println(line);
-                    System.out.println("Nice! I've marked this task as done:");
-                    Task t1 = list.get(Integer.parseInt(split[1]) - 1);
-                    t1.setDone();
-                    System.out.println("    " + t1.printTask(t1.getType(), t1.getDone(), t1.getTask()));
+                    if (split.length <= 1) {
+                        listOfCommands();
+                        break;
+                    }
+                    try {
+                        Task t1 = list.get(Integer.parseInt(split[1]) - 1);
+                        System.out.println("Nice! I've marked this task as done:");
+                        t1.setDone();
+                        System.out.println("    " + t1.printTask(t1.getType(), t1.getDone(), t1.getTask()));
+                    } catch (IndexOutOfBoundsException e) {
+                        String single = list.size() <= 1 ? "task" : "tasks";
+                        System.out.println("You only have " + list.size() + " " + single + " currently. Type \"list\" to view all your current " + single);
+                    }
                     System.out.println(line);
                     break;
                 case "unmark":
                     System.out.println(line);
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    Task t2 = list.get(Integer.parseInt(split[1]) - 1);
-                    t2.setUndone();
-                    System.out.println("    " + t2.printTask(t2.getType(), t2.getDone(), t2.getTask()));
+                    if (split.length <= 1) {
+                        listOfCommands();
+                        break;
+                    }
+                    try {
+                        Task t2 = list.get(Integer.parseInt(split[1]) - 1);
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        t2.setUndone();
+                        System.out.println("    " + t2.printTask(t2.getType(), t2.getDone(), t2.getTask()));
+                    } catch (IndexOutOfBoundsException e) {
+                        String single = list.size() <= 1 ? "task" : "tasks";
+                        System.out.println("You only have " + list.size() + " " + single + " currently. Type \"list\" to view all your current " + single);
+                    }
                     System.out.println(line);
                     break;
                 case "bye":
@@ -44,7 +75,7 @@ public class Fredricksen {
                     break;
                 case "":
                     System.out.println(line);
-                    System.out.println("Please specify a task.");
+                    listOfCommands();
                     System.out.println(line);
                     break;
                 case "list":
@@ -58,10 +89,14 @@ public class Fredricksen {
                 case "todo":
                 case "event":
                 case "deadline":
+                    if (split.length <= 1) {
+                        listOfCommands();
+                        break;
+                    }
                     System.out.println(line);
                     System.out.println("Got it. I've added this task: ");
                     String res = "";
-                    switch(first) {
+                    switch (first) {
                         case "todo":
                             res = "T";
                             break;
@@ -100,6 +135,9 @@ public class Fredricksen {
                     String single = list.size() == 1 ? "task" : "tasks";
                     System.out.println("Now you have " + list.size() + " " + single + " in the list.");
                     System.out.println(line);
+                    break;
+                default:
+                    listOfCommands();
                     break;
             }
         }
