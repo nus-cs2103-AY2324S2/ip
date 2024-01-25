d58bb62b698e32e0d36634b334242b7b37c51a2f
kjw142857 <e0958424@u.nus.edu>
2024-01-26 00:52:12 +0800

    Level-0

--- src/main/java/Sleepy.java
+++ src/main/java/Sleepy.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Duke {
+public class Sleepy {
     public static void main(String[] args) {
         // Initialise chatbot
         ItemList itemList = new ItemList();
