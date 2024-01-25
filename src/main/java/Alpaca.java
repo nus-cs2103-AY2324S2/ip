87884c7e4a59af46acbd25db86e38631f93a7050
Zoe <za.zoeang@gmail.com>
2024-01-25 21:50:33 +0800

    Rename & Stuff

---
+++ src/main/java/Alpaca.java
@@ -0,0 +1,14 @@
+public class Alpaca {
+
+    private static void divider() {
+        System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        String name = "Alpaca";
+        divider();
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        divider();
+        System.out.println("Bye. Hope to see you again!");
+        divider();
+    }
+}
