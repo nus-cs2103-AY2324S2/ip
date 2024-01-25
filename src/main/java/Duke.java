67db755d8a0849c64124658bbe85804c6e14f9fd
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-01-25 21:59:30 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Area.\n" +
-                "What can I do for you?\n" +
-                "\n"+
-                "Bye. Hope to see you again soon!");
+                "What can I do for you?\n"
+        );
+
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String instruction = sc.nextLine();
+            if(instruction.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }else{
+                System.out.println(instruction);
+            }
+        }
     }
 }
