babb77580a345fa3be2e0b6d6fd42eba196d3895
Kaya3842 <haichiendhnn1@gmail.com>
2024-01-25 16:06:16 +0800

    Add, List

--- src/main/java/Liv.java
+++ src/main/java/Liv.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Liv {
+    public static ArrayList<String> list = new ArrayList<>();
     public static void greet() {
         System.out.println("Liv, under your instructions!");
         System.out.println("What is your command?\n");
+    }
+    public static void displayCommand() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
     }
     public static void exit() {
         System.out.println("Farewell, see you next time!");
@@ -13,8 +20,12 @@ public class Liv {
 
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        while(!command.equals("bye")) {
-            System.out.println("     " + command + "\n");
+        while (!command.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("list")) {
+                displayCommand();
+            }
+            list.add(command);
+            System.out.println("     added: " + command + "\n");
             command = scanner.nextLine();
         }
 
