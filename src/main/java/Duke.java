d51b81672b6ca4aa48691dc655e3fe9a06acfdf2
Jajared <jajawong09@gmail.com>
2024-01-25 10:52:44 +0800

    feat: added task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+
+  private static String[] taskList = new String[100]; // List of tasks
+
   public static void main(String[] args) {
     String input = "";
     String botName = "GeePeeTee";
@@ -12,10 +15,37 @@ public class Duke {
       if (input.equals("bye")) {
         scanner.close();
         break;
+      } else if (input.equals("list")) {
+        printList(taskList);
+      } else {
+        addTask(input);
+        System.out.println("added: " + input);
       }
-      System.out.println(input);
     }
     // Exit Message
     System.out.println("Bye. Hope to see you again soon!");
+  }
+
+  /*
+   * Prints the list of tasks
+   */
+  public static void printList(String[] list) {
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] != null) {
+        System.out.println(i + 1 + ". " + list[i]);
+      }
+    }
+  }
+
+  /*
+   * Adds a task to the taskList
+   */
+  private static void addTask(String task) {
+    for (int i = 0; i < taskList.length; i++) {
+      if (taskList[i] == null) {
+        taskList[i] = task;
+        break;
+      }
+    }
   }
 }
