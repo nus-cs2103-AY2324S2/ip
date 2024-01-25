69d8a0b87a1f54dcdda016555c2a37e13533c609
Tsenrae <E0957554@u.nus.edu>
2024-01-26 02:33:47 +0800

    Added echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,12 +9,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
         printLine();
         System.out.println("Hello! I'm Bozo");
         System.out.println("What can I do for you?");
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (true) {
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                printLine();
+                System.out.println(input);
+                printLine();
+                input = sc.nextLine();
+            }
+        }
     }
 
     public static void printLine() {
