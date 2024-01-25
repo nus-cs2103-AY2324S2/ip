a358b098b24fb9c89a2d4be9361df24f95fd8110
bachletuankhai <bachletuankhai@gmail.com>
2024-01-25 18:32:56 +0800

    Add Command class and implement abstract methods

---
+++ src/main/java/Task/Command.java
@@ -0,0 +1,36 @@
+package Task;
+
+public abstract class Command {
+  private String body;
+
+  protected Command(String body) {
+    this.body = body;
+  }
+
+  protected String getBody() {
+    return body;
+  }
+
+  public static Command newCommand(String name, String body) {
+    switch (name) {
+      case "list":
+        return new ListCommand();
+      case "mark":
+        return new MarkCommand(body);
+      case "unmark":
+        return new UnmarkCommand(body);
+      case "bye":
+        return new ExitCommand();
+      case "todo":
+        return new TodoCommand(body);
+      case "deadline":
+        return new DeadlineCommand(body);
+      case "event":
+        return new EventCommand(body);
+      default:
+        return new AddCommand(body);
+    }
+  }
+
+  abstract String execute(TaskList list);
+}
