856de8eb8eab3f5e3df520514546771307482cd4
Ma-Yirui <saltiestwonton@gmail.com>
2024-01-25 13:31:32 +0800

    added indentation, implemented level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,24 @@
 import java.io.*;
+import java.util.LinkedList;
 
 public class Duke {
     private static final PrintWriter pw = new PrintWriter(System.out);
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final String SEPERATOR = "_______________________________________\n";
+    private static final String INDENTATION = "    ";
+    private static LinkedList<String> list = new LinkedList<String>();
+
     public static void main(String[] args) throws IOException{
         String line;
 
         greet();
 
         while (!(line = br.readLine()).equals("bye")){
-            echo(line);
+            if (line.equals("list")){
+                list();
+            } else {
+                add(line);
+            }
         }
 
         exit();
@@ -19,24 +27,38 @@ public class Duke {
     }
 
     private static void greet(){
-        pw.print(SEPERATOR
-                + "Hello, Notnow here!\n"
-                + "What can I do for you?\n"
-                + SEPERATOR);
+        pw.print(INDENTATION + SEPERATOR
+                + INDENTATION + "Hello, Notnow here!\n"
+                + INDENTATION + "What can I do for you?\n"
+                + INDENTATION + SEPERATOR);
         pw.flush();
     }
 
     private static void exit(){
-        pw.print(SEPERATOR
-                + "Bye. Hope to see u again soon!\n"
-                + SEPERATOR);
+        pw.print(INDENTATION + SEPERATOR
+                + INDENTATION + "Bye. Hope to see u again soon!\n"
+                + INDENTATION + SEPERATOR);
         pw.flush();
     }
 
     private static void echo(String input) {
-        pw.print(SEPERATOR
-                + input + "\n"
-                + SEPERATOR);
+        pw.print(INDENTATION + SEPERATOR
+                + INDENTATION + input + "\n"
+                + INDENTATION + SEPERATOR);
+        pw.flush();
+    }
+
+    private static void add(String input){
+        echo("added: " + input);
+        list.add(input);
+    }
+
+    private static void list(){
+        pw.print(INDENTATION + SEPERATOR);
+        for (int i = 0; i < list.size(); i += 1) {
+            pw.print(INDENTATION + String.format("%d. %s\n", i + 1, list.get(i)));
+        }
+        pw.print(INDENTATION + SEPERATOR);
         pw.flush();
     }
 
