76610d9195cd424c1f0f28931691edfcbeafd824
iynixil <lixinyilxy02@gmail.com>
2024-01-25 18:01:52 +0800

    removed some code used for debugging and old code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -65,7 +65,7 @@ public class Duke {
                         newTask = new Deadline(taskName, deadline);
                     }
                     if (inputArr[0].equals("event")) {
-                        System.out.println(input);
+                        // System.out.println(input);
                         int idxFrom = input.indexOf("/from");
                         int idxTo = input.indexOf("/to");
                         String taskName = input.substring(6, idxFrom - 1);
@@ -73,7 +73,6 @@ public class Duke {
                         String to = input.substring(idxTo + "/to ".length());
                         newTask = new Event(taskName, from, to);
                     }
-                    // Task newTask = new Task(input);
                     taskList.add(newTask);
                     System.out.println(String.format("%s\nDave added the task:\n  %s\nYou now have %d task(s).\n%s", horizontalLine, newTask.toString(), taskList.size(), horizontalLine));
                 } else {
@@ -85,58 +84,6 @@ public class Duke {
                 System.out.println(String.format("%s\n%s\n%s", horizontalLine, exc.getMessage(), horizontalLine));
             }
         }
-
-
-        // while (!input.equals(exitCmd)) {
-        //     if (input.equals("list")) {
-        //         System.out.println(horizontalLine + "\nHere are the tasks in your list:\n");
-        //         for (int i = 0; i < listIdx; i++) {
-        //             System.out.println(String.format("%d. %s", i+1, taskList[i].toString()));
-        //         }
-        //         System.out.println(horizontalLine);
-        //     } else {
-        //         String[] inputArr = input.split(" ");
-        //         if (inputArr[0].equals("mark") || inputArr[0].equals("unmark")) {
-        //             int taskNumber = Integer.parseInt(inputArr[1]) - 1;
-
-        //             if (inputArr[0].equals("mark")) {
-        //                 taskList[taskNumber].isCompleted();
-        //                 System.out.println(String.format("%s\nGood job! I'll mark this task as done:\n%s\n%s", horizontalLine, taskList[taskNumber].toString(), horizontalLine));    
-        //             } else {
-        //                 taskList[taskNumber].isNotCompleted();
-        //                 System.out.println(String.format("%s\nAlright, I believe you'll get this done eventually:\n%s\n%s", horizontalLine, taskList[taskNumber].toString(), horizontalLine));
-        //             }
-        //         } else {
-        //             Task newTask = new Task(input);
-        //             if (inputArr[0].equals("todo")) {
-        //                 String taskName = input.substring(5);
-        //                 newTask = new Todo(taskName);
-        //             }
-        //             if (inputArr[0].equals("deadline")) {
-        //                 int idxDeadline = input.indexOf("/by");
-        //                 String taskName = input.substring(9, idxDeadline - 1);
-        //                 String deadline = input.substring(idxDeadline + "/by ".length());
-        //                 newTask = new Deadline(taskName, deadline);
-        //             }
-        //             if (inputArr[0].equals("event")) {
-        //                 int idxFrom = input.indexOf("/from");
-        //                 int idxTo = input.indexOf("/to");
-        //                 String taskName = input.substring(6, idxFrom - 1);
-        //                 String from = input.substring(idxFrom + "/from ".length(), idxTo - 1);
-        //                 String to = input.substring(idxTo + "/to ".length());
-        //                 newTask = new Event(taskName, from, to);
-        //             }
-        //             // Task newTask = new Task(input);
-        //             taskList[listIdx] = newTask;
-        //             listIdx++;
-        //             System.out.println(String.format("%s\nDave added the task:\n  %s\nYou now have %d task(s).\n%s", horizontalLine, newTask.toString(), listIdx, horizontalLine));
-        //         }
-        //     }
-        //     input = sc.nextLine();
-        // }
-
-        // // end program if user says bye
-        // System.out.println(String.format("%s\nDave: Thank you for your patronage, goodbye and have a nice day!\n%s", horizontalLine, horizontalLine));
 
         sc.close();
     }
