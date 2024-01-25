aea820f1a9b672564f33f5604cec222ba3d8efb4
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-01-25 14:59:51 +0800

    Add user inputs

--- src/main/java/Eggy.java
+++ src/main/java/Eggy.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Eggy {
     public static void main(String[] args) {
         String name = "Eggy";
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + name);
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm " + name + "\uD83E\uDD5A.");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        String command = sc.nextLine();;
+        while (!command.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + command);
+            System.out.println("    ____________________________________________________________");
+            command = sc.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye\uD83D\uDC4B. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        sc.close();
     }
 }
