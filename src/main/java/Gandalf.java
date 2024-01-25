1737d318706f1197201c7ee0e97acdb0a12b4bc0
dabzpengu <mahathir66@gmail.com>
2024-01-25 16:18:21 +0800

    echo

--- src/main/java/Gandalf.java
+++ src/main/java/Gandalf.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Gandalf {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Gandalf");
+        System.out.println("Through fire and shadow, I'm Gandalf");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                scanner.close();
+                System.out.println("So here at last, comes the end of our fellowship. I will not say: Do not weep. For not all tears are an evil.");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
