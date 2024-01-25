0de5e194520c0f54ed16336e9c60a90de5017ce3
Ryo-Seah <seah_ryo@yahoo.com.sg>
2024-01-25 15:07:17 +0800

    Complete Level-1

--- src/main/java/GPT.java
+++ src/main/java/GPT.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class GPT {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
         String name = " GPT";
-        String secLine = "What can I do for you?\n\n" + "Bye. Hope to see you soon";
+        String secLine = "What can I do for you?\n\n";
 
         System.out.println("Hello! I'm" + name);
         System.out.println(secLine);
+        String s = scn.nextLine();
+        while(!s.equals("bye")) {
+            System.out.println(s);
+            s = scn.nextLine();
+        }
+        System.out.println( "Bye. Hope to see you soon");
     }
 }
