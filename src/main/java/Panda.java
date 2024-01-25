bf686c7b3b617f3ca5142d631a4100ca340e65af
modembcc <modemhappy@gmail.com>
2024-01-25 13:16:36 +0800

    Level 0

---
+++ src/main/java/Panda.java
@@ -0,0 +1,11 @@
+public class Panda {
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello! I'm Panda\n" + 
+            "What can I do for you?\n"
+        );
+        System.out.println(
+            "Bye. Hope to see you again soon!"
+        );
+    }
+}
