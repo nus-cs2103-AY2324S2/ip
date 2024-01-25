b7acea8cf21b64a3d714f21c19f0a2403d3a6578
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-01-25 15:13:27 +0800

    Allow add task and return task list

--- src/main/java/Eggy.java
+++ src/main/java/Eggy.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Eggy {
     public static void main(String[] args) {
         String name = "Eggy";
+        List<String> taskList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("    ____________________________________________________________");
@@ -12,9 +13,11 @@ public class Eggy {
 
         String command = sc.nextLine();;
         while (!command.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " + command);
-            System.out.println("    ____________________________________________________________");
+            if (!command.equals("list")) {
+                addTask(command, taskList);
+            } else {
+                showList(taskList);
+            }
             command = sc.nextLine();
         }
 
@@ -22,5 +25,20 @@ public class Eggy {
         System.out.println("     Bye\uD83D\uDC4B. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
         sc.close();
+    }
+
+    public static void addTask(String task, List<String> taskList) {
+        taskList.add(task);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + task);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void showList(List<String> taskList) {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println("     " + (i + 1) + ". " + taskList.get(i));
+        }
+        System.out.println("    ____________________________________________________________");
     }
 }
