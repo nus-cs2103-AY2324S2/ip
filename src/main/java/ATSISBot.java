4fdc88ca9e220c97e6b912ba5238d209aef31f06
Puri Virakarin <puri.prv@gmail.com>
2024-01-25 09:10:49 +0800

    ToDos, Events, Deadlines

--- src/main/java/ATSISBot.java
+++ src/main/java/ATSISBot.java
@@ -1,34 +1,77 @@
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-class Pair<S, T> {
-  private S first;
-  private T second;
+abstract class Task {
+  protected String description;
+  protected boolean isDone;
 
-  public Pair(S first, T second) {
-    this.first = first;
-    this.second = second;
+  private static Integer taskNum = 0;
+
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+    Task.taskNum += 1;
   }
 
-  public S getFirst() {
-    return this.first;
+  public String getStatusIcon() {
+    return (isDone ? "X" : " "); // mark done task with X
   }
 
-  public T getSecond() {
-    return this.second;
+  public void markAsDone() {
+    this.isDone = true;
   }
 
-  public void setFirst(S first) {
-    this.first = first;
+  public void markAsUndone() {
+    this.isDone = false;
   }
 
-  public void setSecond(T second) {
-    this.second = second;
+  public String getDescription() {
+    return this.description;
+  }
+
+  public static String getTaskNum() {
+    return "Now you have " + Task.taskNum + " tasks in the list.";
+  }
+
+  public abstract String toString();
+}
+
+class Todo extends Task {
+  public Todo(String description) {
+    super(description);
   }
 
   @Override
   public String toString() {
-    return this.first + " " + this.second;
+    return "[T][" + this.getStatusIcon() + "] " + this.getDescription();
+  }
+}
+
+class Deadline extends Task {
+  protected String by;
+
+  public Deadline(String description, String by) {
+    super(description);
+    this.by = by;
+  }
+
+  @Override
+  public String toString() {
+    return "[D][" + this.getStatusIcon() + "] " + this.getDescription() + " (by: " + this.by + ")";
+  }
+}
+
+class Event extends Task {
+  protected String fromTo;
+
+  public Event(String description, String fromTo) {
+    super(description);
+    this.fromTo = fromTo;
+  }
+
+  @Override
+  public String toString() {
+    return "[E][" + this.getStatusIcon() + "] " + this.getDescription() + " (at: " + this.fromTo + ")";
   }
 }
 
@@ -41,8 +84,9 @@ public class ATSISBot {
   private static final String listMessage = "Here are the tasks in your list:\n";
   private static final String markMessage = "Nice! I've marked this task as done:\n";
   private static final String unmarkMessage = "OK, I've marked this task as not done yet:\n";
+  private static final String addTaskMessage = "Got it. I've added this task:\n";
 
-  private static ArrayList<Pair<Boolean, String>> list = new ArrayList<>();
+  private static ArrayList<Task> list = new ArrayList<>();
 
   public static void main(String[] args) {
     System.out.println(ATSISBot.welcomeMessage);
@@ -56,23 +100,35 @@ public class ATSISBot {
         AtomicInteger index = new AtomicInteger(1);
         System.out.print(listMessage);
         list.forEach(element -> System.out
-            .println(index.getAndIncrement() + ". [" + (element.getFirst() ? "X" : " ") + "] " + element.getSecond()));
+            .println(index.getAndIncrement() + ". " + element.toString()));
       } else if (input.startsWith("mark")) {
         int elementIdx = Integer.parseInt(input.split(" ")[1]);
-        list.get(elementIdx - 1).setFirst(true);
+        list.get(elementIdx - 1).markAsDone();
         System.out.println(markMessage);
-        System.out.println("  [X] "
-            + list.get(elementIdx - 1).getSecond());
+        System.out.println("  " + list.get(elementIdx - 1).toString());
       } else if (input.startsWith("unmark")) {
         int elementIdx = Integer.parseInt(input.split(" ")[1]);
-        list.get(elementIdx - 1).setFirst(false);
+        list.get(elementIdx - 1).markAsUndone();
         System.out.println(unmarkMessage);
-        System.out.println("  [ ] "
-            + list.get(elementIdx - 1).getSecond());
-      } else {
-        list.add(new Pair<>(false, input));
-        String output = input;
-        System.out.println(output);
+        System.out.println("  " + list.get(elementIdx - 1).toString());
+      } else if (input.startsWith("todo")) {
+        String description = input.split(" ", 2)[1];
+        list.add(new Todo(description));
+        System.out.print(addTaskMessage);
+        System.out.println("  " + list.get(list.size() - 1).toString());
+        System.out.println(Task.getTaskNum());
+      } else if (input.startsWith("deadline")) {
+        String[] descriptionAndBy = input.split(" ", 2)[1].split(" /by ");
+        list.add(new Deadline(descriptionAndBy[0], descriptionAndBy[1]));
+        System.out.print(addTaskMessage);
+        System.out.println("  " + list.get(list.size() - 1).toString());
+        System.out.println(Task.getTaskNum());
+      } else if (input.startsWith("event")) {
+        String[] descriptionAndFromTo = input.split(" ", 2)[1].split(" /from ");
+        list.add(new Event(descriptionAndFromTo[0], descriptionAndFromTo[1]));
+        System.out.print(addTaskMessage);
+        System.out.println("  " + list.get(list.size() - 1).toString());
+        System.out.println(Task.getTaskNum());
       }
       System.out.print(line);
       input = sc.nextLine();
