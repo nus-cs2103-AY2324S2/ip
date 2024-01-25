879782d92cbf44888c5413cdc97b7633cf6d6895
whitesnowx <whitesnowx88@gmail.com>
2024-01-25 14:39:58 +0800

    Added greetings, horizontal line and sendoff text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    public static final String HORIZONTAL_LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String name = "[NAME_PLACERHOLDER]";
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("heyo! my name is " + name + "૮ ˶ᵔ ᵕ ᵔ˶ ა");
+        System.out.println("What would you like for me to do?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("okay then, bye ( •̯́ ^ •̯̀)");
+        System.out.println(HORIZONTAL_LINE);
+
+
     }
 }
