753613615f8c3b585b3fcdc690facc0ec0c5b932
John Yoo <e0968839@u.nus.edu>
2024-01-25 16:49:38 +0800

    Implemented TalkingJohn Level2

--- src/main/java/TalkingJohn.java
+++ src/main/java/TalkingJohn.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 public class TalkingJohn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] stringList = new String[100];
 
         System.out.println("Hello, I am TalkingJohn\n"
                 + "What can I do for you?\n");
 
-
+        int j = 0;
         while (true) {
             String input = scanner.nextLine();
             if (Objects.equals(input, "bye")) {
                 System.out.println("Bye, hope to see you soon");
                 break;
+            } else if (Objects.equals(input, "list")){
+                for (int i = 0; i < j; i++) {
+                    String nthElement = stringList[i];
+                    System.out.println((i + 1) + ". " + nthElement + "\n");
+                }
             } else {
-                System.out.println(input);
+                stringList[j] = input;
+                j++;
+                System.out.println("added: " + input);
             }
         }
         scanner.close();
