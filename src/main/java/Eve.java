ecfc50097c5f38e4c98b42dbb74873ad6a80ecc1
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-01-25 21:40:14 +0800

    Level-1 Echo

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Eve {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println(" Hello! I'm Eve");
         System.out.println(" What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon !");
+
+        String input = "";
+
+        while(!input.equals("bye")){
+            input = sc.nextLine();
+
+            if (input.equals("bye")){
+                System.out.println(" Bye. Hope to see you again soon !");
+            } else {
+                System.out.println(input);
+            }
+        }
+
+            
+
+        sc.close();
     }
 }
