cd9202d282f7682fc00022ddaabeadc83811262f
Wongqingyee <qingyeewong@gmail.com>
2024-01-25 17:58:20 +0800

    Level 5:Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,6 +11,17 @@ public class Duke {
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static final String[] COMMANDS = {
+            "bye: Terminate the program.\n" +
+            "list: Display the list of tasks.\n" +
+            "mark <index>: Mark a task as done.\n" +
+            "unmark <index>: Mark a task as not done.\n" +
+            "todo <description>: Add a todo task.\n" +
+            "deadline <description> /by <dueDate>: Add a deadline task.\n" +
+            "event <description> /from <startDate> /to <endDate>: Add an event task.\n"
+
+    };
 
     private Scanner scanner;
 
@@ -30,39 +42,48 @@ public class Duke {
      */
     public void run() {
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hey there! This is Chitty-Chatty\n" + "What can I do for you?\n");
 
         while (scanner.hasNextLine()) {
-            String[] input = scanner.nextLine().split(" ", 2);
-            String command = input[0];
+            try {
+                String[] input = scanner.nextLine().split(" ", 2);
+                String command = input[0];
 
-            switch (command) {
-                case "bye":
-                    exit();
-                    break;
-                case "list":
-                    listTasks();
-                    break;
-                case "mark":
-                    markTaskAsDone(input);
-                    break;
-                case "unmark":
-                    markTaskAsUndone(input);
-                    break;
-                case "todo":
-                    addToDoTask(input);
-                    break;
-                case "deadline":
-                    addDeadline(input);
-                    break;
-                case "event":
-                    addEvent(input);
-                    break;
-                default:
-                    Task t = new Task(String.join(" "));
-                    addTasks(t);
-                    break;
+                switch (command) {
+                    case "hello":
+                        System.out.println("Hey there! This is Dooloodoodooloodoo!\n" + "What can I do for you?\n");
+                        break;
+                    case "bye":
+                        exit();
+                        break;
+                    case "list":
+                        listTasks();
+                        break;
+                    case "mark":
+                        markTaskAsDone(input);
+                        break;
+                    case "unmark":
+                        markTaskAsUndone(input);
+                        break;
+                    case "todo":
+                        addToDoTask(input);
+                        break;
+                    case "deadline":
+                        addDeadline(input);
+                        break;
+                    case "event":
+                        addEvent(input);
+                        break;
+                    case "help":
+                        System.out.println(Arrays.toString(COMMANDS));
+                        break;
+                    default:
+                        throw new DukeException("I'm sorry, but I don't know what '" + command + "' means :-(\n" +
+                                "Please enter 'help' command to find out more.");
+                }
+            } catch (DukeException e) {
+                System.out.println("OOPS!!! An error occurred: " + e.getMessage());
             }
+
         }
     }
 
@@ -71,7 +92,11 @@ public class Duke {
      *
      * @param input A string array containing the task description.
      */
-    public void addToDoTask(String[] input) {
+    public void addToDoTask(String[] input) throws DukeException {
+        if (input.length == 1) {
+            throw new DukeException("OOPS! The description of a todo cannot be left blank.\n" +
+                    "Please enter 'help' command to find out more.");
+        }
         ToDo todo = new ToDo(input[1]);
         addTasks(todo);
     }
@@ -81,7 +106,14 @@ public class Duke {
      *
      * @param input A string array containing the task description with the "/by" as separator.
      */
-    public void addDeadline(String[] input) {
+    public void addDeadline(String[] input) throws DukeException {
+        if (input.length == 1) {
+            throw new DukeException("OOPS! The description of a deadline cannot be left blank.\n" +
+                    "Please enter 'help' command to find out more.");
+        } else if (!input[1].contains("/by")) {
+            throw new DukeException("OOPS! The date/time for the deadline cannot be left blank.\n" +
+                    "Please enter 'help' command to find out more.");
+        }
         String[] description = input[1].split("/by");
         Deadline dd = new Deadline(description[0], description[1]);
         addTasks(dd);
@@ -92,7 +124,14 @@ public class Duke {
      *
      * @param input A string array containing the task description with the "/from" and "/to" as separators.
      */
-    public void addEvent(String[] input) {
+    public void addEvent(String[] input) throws DukeException {
+        if (input.length == 1) {
+            throw new DukeException("OOPS! The description of an event cannot be left blank.\n" +
+                    "Please enter 'help' command to find out more.");
+        } else if (!input[1].contains("/from") && !input[1].contains("/to")) {
+            throw new DukeException("OOPS! The start time and end time cannot be left blank.\n" +
+                    "Please enter 'help' command to find out more.");
+        }
         String[] description = input[1].split("/from|/to");
         Event e = new Event(description[0], description[1], description[2]);
         addTasks(e);
@@ -149,6 +188,7 @@ public class Duke {
      */
     public void exit() {
         System.out.println("Goodbye. Have a great day ahead!");
+        System.exit(0);
     }
 
     public static void main(String[] args) {
