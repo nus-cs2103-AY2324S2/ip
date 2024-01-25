031fb30d50669c8955206f6a1deffe69362a930e
Ronn <ronnng1510@gmail.com>
2024-01-25 21:29:03 +0800

    Level-1
    - Added echo functionality

--- src/main/java/BadGPT.java
+++ src/main/java/BadGPT.java
@@ -1,17 +1,37 @@
+import java.util.Scanner;
+
 public class BadGPT {
     public static void main(String[] args) {
         String name = "BadGPT";
 
         System.out.println("____________________________________________________________\n" +
-                "Hello! I'm " + name + ".\n" + "What can I do for you?");
+                "Hello! I'm " + name + ".\n" + "What can I do for you?\n" +
+                "____________________________________________________________");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String cmd = sc.next();
+
+            // Exits the program if user enters "bye". Else, echoes the input.
+            if (cmd.equals("bye")) {
+                break;
+            } else {
+                echo(cmd);
+            }
+        }
 
         bye();
+    }
+
+    public static void echo(String input) {
+        System.out.println("____________________________________________________________\n" +
+                input + "\n____________________________________________________________\n");
     }
 
     public static void bye() {
         System.out.println("____________________________________________________________\n" +
                 "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                "____________________________________________________________");
         System.exit(0);
     }
 }
