cdf53d5f714692b64e0c76369def330417a66a47
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-01-25 17:00:32 +0800

    Add 3 types of tasks

--- src/main/java/Eggy.java
+++ src/main/java/Eggy.java
@@ -13,7 +13,7 @@ public class Eggy {
 
         String command = sc.nextLine();
         while (!command.equals("bye")) {
-            String[] commandArr = command.split(" ");
+            String[] commandArr = command.split(" ", 2);
             System.out.println("    ____________________________________________________________");
             if (command.equals("list")) {
                 showList(taskList);
@@ -22,7 +22,7 @@ public class Eggy {
             } else if (commandArr[0].equals("unmark")) {
                 taskList.get(Integer.parseInt(commandArr[1]) - 1).unmarkDone();
             } else {
-                addTask(command, taskList);
+                addTask(commandArr, taskList);
             }
             System.out.println("    ____________________________________________________________");
             command = sc.nextLine();
@@ -34,9 +34,37 @@ public class Eggy {
         sc.close();
     }
 
-    public static void addTask(String taskName, List<Task> taskList) {
-        taskList.add(new Task(taskName));
-        System.out.println("     added: " + taskName);
+    public static void addTask(String[] commandArr, List<Task> taskList) {
+        switch (commandArr[0]) {
+            case "todo":
+                Todo newTodo = new Todo(commandArr[1]);
+                taskList.add(newTodo);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newTodo.toString());
+                System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+                break;
+            case "deadline":
+                String[] deadlineSplit = commandArr[1].split(" /by ");
+                Deadline newDeadline = new Deadline(deadlineSplit[0], deadlineSplit[1]);
+                taskList.add(newDeadline);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newDeadline.toString());
+                System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+                break;
+            case "event":
+                String[] eventSplit = commandArr[1].split(" /from | /to ");
+                Event newEvent = new Event(eventSplit[0], eventSplit[1], eventSplit[2]);
+                taskList.add(newEvent);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newEvent.toString());
+                System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+                break;
+            default:
+                Task newTask = new Task(String.join(" ", commandArr));
+                taskList.add(newTask);
+                System.out.println("     added: " + String.join(" ", commandArr));
+                break;
+        }
     }
 
     public static void showList(List<Task> taskList) {
