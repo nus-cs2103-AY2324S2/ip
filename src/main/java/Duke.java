0787074a98a94b72bbbf38e32df54b4798820098
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-01-25 15:03:38 +0800

    Task 3. Mark as Done With Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,8 +18,7 @@ public class Duke {
                 "____________________________________________________________";
         String barrier = "____________________________________________________________";
         Boolean exit = true;
-        ArrayList<String> todoList = new ArrayList<String>();
-        ArrayList<Boolean> checkList = new ArrayList<Boolean>();
+        ArrayList<Task> todoList = new ArrayList<Task>();
         System.out.println(Greeting);
         while (exit) {
             Scanner myObj = new Scanner(System.in);
@@ -31,55 +30,59 @@ public class Duke {
                 System.out.println(barrier);
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < todoList.size(); i++) {
-                    if (checkList.get(i)) {
-                        System.out.println(i+1 + ".[X] " + todoList.get(i));
-                    } else {
-                        System.out.println(i+1 + ".[ ] " + todoList.get(i));
-                    }
+                    System.out.println(i+1 + ".[" + todoList.get(i).getStatusIcon() + "] " + todoList.get(i).getDes());
                 }
                 System.out.println(barrier);
             } else if (inputList[0].equals("mark")) {
                 int position = Integer.parseInt(inputList[1]);
                 System.out.println(barrier);
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + todoList.get(position-1));
-                checkList.set(position-1, true);
+                todoList.get(position-1).markAsDone();
+                System.out.println("[X] " + todoList.get(position-1).getDes());
                 System.out.println(barrier);
             } else if (inputList[0].equals("unmark")) {
                 int position = Integer.parseInt(inputList[1]);
                 System.out.println(barrier);
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[ ] " + todoList.get(position-1));
-                checkList.set(position-1, false);
+                todoList.get(position-1).unmarkAsDone();
+                System.out.println("[ ] " + todoList.get(position-1).getDes());
                 System.out.println(barrier);
             } else if (inputList[0].equals("todo")) {
-                todoList.add(userInput);
-                checkList.add(false);
+                Task userTask = new Task(inputList[1],"T");
+                todoList.add(userTask);
                 System.out.println(barrier);
-                System.out.println("added: " + userInput);
-                System.out.println("Now you have " + todoList.size() + "in the list.");
+                System.out.println("[T][ ] " + inputList[1]);
+                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
                 System.out.println(barrier);
                 System.out.println(inputList[1]);
             } else if (inputList[0].equals("deadline")) {
                 String[] differentParts = inputList[1].split("/");
                 System.out.println(differentParts[0]);
                 System.out.println(differentParts[1]);
-                todoList.add(userInput);
-                checkList.add(false);
+                Task userTask = new Task(inputList[1],"D");
+                todoList.add(userTask);
                 System.out.println(barrier);
-                System.out.println("added: " + userInput);
-                System.out.println("Now you have " + todoList.size() + "in the list.");
+                System.out.println("[D][ ] " + differentParts[0]);
+                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
                 System.out.println(barrier);
             } else if (inputList[0].equals("event")) {
                 String[] differentParts = inputList[1].split("/");
                 System.out.println(differentParts[0]);
                 System.out.println(differentParts[1]);
                 System.out.println(differentParts[2]);
-                todoList.add(userInput);
-                checkList.add(false);
+                Task userTask = new Task(inputList[1],"E");
+                todoList.add(userTask);
                 System.out.println(barrier);
-                System.out.println("added: " + userInput);
-                System.out.println("Now you have " + todoList.size() + "in the list.");
+                System.out.println("[E][ ] " + differentParts[0]);
+                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
+                System.out.println(barrier);
+            }
+            else {
+                Task userTask = new Task(inputList[0] + " " + inputList[1],"E");
+                todoList.add(userTask);
+                System.out.println(barrier);
+                System.out.println("added: " + inputList[0] + " " + inputList[1]);
+                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
                 System.out.println(barrier);
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,13 +2,24 @@ public class Task {
         protected String description;
         protected boolean isDone;
 
-        public Task(String description) {
+        protected String type;
+
+        public Task(String description, String type) {
             this.description = description;
             this.isDone = false;
+            this.type = type;
         }
 
         public String getStatusIcon() {
             return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String getDes() {
+            return this.description;
+        }
+
+        public String getType() {
+            return this.type;
         }
 
         public void markAsDone() {
@@ -18,4 +29,5 @@ public class Task {
         public void unmarkAsDone() {
             this.isDone = false;
         }
+
 }
