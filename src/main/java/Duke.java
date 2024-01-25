ea0e873878c39dc77c564317673a022924d40084
javierng2k <javierng2knus@gmail.com>
2024-01-25 12:28:58 +0800

    Added greeting functionality
    Added greeting function for greeting user when starting the program.
    Updated Duke to JAV

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = 
+                //   " ____        _        \n"
+                // + "|  _ \\ _   _| | _____ \n"
+                // + "| | | | | | | |/ / _ \\\n"
+                // + "| |_| | |_| |   <  __/\n"
+                // + "|____/ \\__,_|_|\\_\\___|\n";
+
+                  "      ____.  _________   ____\n"
+                + "     |    | /  _  \\   \\ /   /\n"
+                + "     |    |/  /_\\  \\   Y   / \n"
+                + " /\\__|    /    |    \\     /  \n"
+                + " \\________\\____|__  /\\___/   \n"
+                + "                  \\/         \n"
+                + "The Joy Amplifying Virtuoso!\n";
+                // Got ASCII Word Art from https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Jav
+
         System.out.println("Hello from\n" + logo);
+        
+        System.out.println("____________________________________________________________");
+        PrintGreeting();
+        System.out.println("____________________________________________________________");
+
+    }
+
+    /*
+
+    * The function prints the greeting to greet the user
+    * Returns void
+    */
+    public static void PrintGreeting() {
+        String greetingMessage = "Hello! I'm JAV\n"
+                               + "What can I do for you?\n";
+
+        System.out.println(greetingMessage);
     }
 }
