85399859087c58af6fbd6423f28e0ea14945f5d8
sdevih <e0960282@u.nus.edu>
2024-01-25 13:56:55 +0800

    Added Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,11 +10,27 @@ public class Duke {
         //Chatbot greets user
         System.out.println("Hello! I'm Chatteroo\n" + "What can I do for you?\n");
 
+        //Chatbot stores user commands in a fixed array
+        String[] listStore = new String[100];
+        int listCount = 0;
+
         //Chatbot echos user commands
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input + "\n");
+            // print list of stored tasks if user inputs "list"
+            if (input.equals("list")) {
+                for (int i = 0; i < listCount; i++) {
+                    int bulletNum = i + 1;
+                    System.out.println(bulletNum + ". " + listStore[i]);
+                }
+                System.out.println();
+            } else { // add user inputs to list
+                listStore[listCount] = input;
+                listCount++;
+                System.out.println("added: " + input + "\n");
+            }
+//            System.out.println("added: " + input + "\n");
             input = sc.nextLine();
         }
         //Chatbot exits
