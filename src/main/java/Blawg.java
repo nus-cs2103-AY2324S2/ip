9267d7bb694762392a127c735d1d58f9876a0656
joel <joelgoh16@gmail.com>
2024-01-25 10:59:56 +0800

    add add and list functionality to Blawg.java

--- src/main/java/Blawg.java
+++ src/main/java/Blawg.java
@@ -1,25 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Blawg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>(100);
         String intro = "____________________________________________________________\n" +
-                " Hello! I'm Blawg\n" +
+                " Paws what you're doing! I'm Blawg\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
         System.out.println(intro);
         String outro = "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+                " Alright, this kitty's got to go chase some shadows. See you later!\n" +
                 "____________________________________________________________\n";
         while (true) {
             String userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(outro);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")){
+                StringBuilder output = new StringBuilder();
+                output.append("____________________________________________________________\n");
+                for (int i = 0; i < list.size(); i++) {
+                    output.append(i+1).append(".");
+                    output.append(list.get(i)).append("\n");
+                }
+                output.append("____________________________________________________________\n");
+                System.out.println(output);
             } else {
                 System.out.println("____________________________________________________________\n" +
-                        userInput + "\n" +
-                        " ____________________________________________________________\n");
+                        "added: " + userInput + "\n" +
+                        "____________________________________________________________\n");
+                list.add(userInput);
             }
         }
         sc.close();
