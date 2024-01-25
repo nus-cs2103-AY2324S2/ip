1f0da0bd6d8625f00c3fbb5ad7e7b689a42cce32
ChrisHo1341 <christopher.hwh@gmail.com>
2024-01-25 22:28:04 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -6,12 +8,30 @@ public class Duke {
         //         + "| |_| | |_| |   <  __/\n"
         //         + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
-
-        String new_line = "____________________________________________________________ \n";
-        String greeting = "Good day good sir! I am Chatimous Maximous, here to help you with your every need!\n";
+        String indent = "    ";
+        String new_line = indent + "____________________________________________________________ \n";
+        String greeting = indent + "Good day good sir! I am Chatimous Maximous, here to help you with your every need!\n";
         System.out.println(new_line + greeting);
 
-        String goodbye = "It has been a pleasure! I do hope to see you again!\n";
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+
+        // if (input == "bye") {
+        //     String goodbye = indent + "It has been a pleasure! I do hope to see you again!\n";
+        //     System.out.println(new_line + goodbye + new_line);
+        //     reader.close();
+        // } else {
+        //     System.out.println(new_line + indent + input + "\n" + new_line);
+         
+        // }
+         
+        while (input.contains("bye") == false) {
+            System.out.println(new_line + indent + input + "\n" + new_line);
+            input = reader.next();
+        }
+        String goodbye = indent + "It has been a pleasure! I do hope to see you again!\n";
         System.out.println(new_line + goodbye + new_line);
+        reader.close();
+
     }
 }
