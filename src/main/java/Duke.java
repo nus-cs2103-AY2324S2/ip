3170ca15ff3affcf67074609d2188094a72a8d53
Jajared <jajawong09@gmail.com>
2024-01-25 17:47:05 +0800

    feat: added delete task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-  private static Task[] taskList = new Task[100]; // Array of tasks
+  private static ArrayList<Task> taskList = new ArrayList<>();// Array of tasks
 
   public static void main(String[] args) {
     String input = "";
@@ -17,50 +18,8 @@ public class Duke {
         if (input.equals("bye")) {
           scanner.close();
           System.out.println("Bye. Hope to see you again soon!");
-        } else if (input.equals("list")) {
-          printList(taskList);
-        } else if (input.startsWith("mark")) {
-          if (input.trim().equals("mark")) {
-            throw new DukeException("The index of a task cannot be empty.");
-          }
-          int index = Integer.parseInt(input.split(" ")[1]);
-          if (index > getTaskCount()) {
-            throw new DukeException("The index of a task cannot be greater than the number of tasks.");
-          }
-          if (index <= 0) {
-            throw new DukeException("The index of a task cannot be 0 or negative.");
-          }
-          if (taskList[index - 1] == null) {
-            throw new DukeException("The task at index " + index + " does not exist.");
-          }
-          taskList[index - 1].markAsDone();
-        } else if (input.startsWith("unmark")) {
-          if (input.trim().equals("unmark")) {
-            throw new DukeException("The index of a task cannot be empty.");
-          }
-          int index = Integer.parseInt(input.split(" ")[1]);
-          if (index > getTaskCount()) {
-            throw new DukeException("The index of a task cannot be greater than the number of tasks.");
-          }
-          if (index <= 0) {
-            throw new DukeException("The index of a task cannot be 0 or negative.");
-          }
-          if (taskList[index - 1] == null) {
-            throw new DukeException("The task at index " + index + " does not exist.");
-          }
-          taskList[index - 1].unmarkAsDone();
-        } else if (input.startsWith("event")) {
-          Event newEvent = Event.createFromInput(input);
-          addTask(newEvent);
-        } else if (input.startsWith("deadline")) {
-          Deadline newDeadline = Deadline.createFromInput(input);
-          addTask(newDeadline);
-        } else if (input.startsWith("todo")) {
-          ToDo newToDo = ToDo.createFromInput(input);
-          addTask(newToDo);
-        } else {
-          throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
+        processInput(input);
 
       } catch (DukeException e) {
         e.printErrorMessage();
@@ -70,15 +29,78 @@ public class Duke {
   }
 
   /*
+   * Process input
+   */
+  private static void processInput(String input) throws DukeException {
+    if (input.equals("list")) {
+      printList(taskList);
+    } else if (input.startsWith("mark")) {
+      if (input.trim().equals("mark")) {
+        throw new DukeException("The index of a task cannot be empty.");
+      }
+      int index = Integer.parseInt(input.split(" ")[1]);
+      if (index > getTaskCount()) {
+        throw new DukeException("The index of a task cannot be greater than the number of tasks.");
+      }
+      if (index <= 0) {
+        throw new DukeException("The index of a task cannot be 0 or negative.");
+      }
+      if (taskList.get(index - 1) == null) {
+        throw new DukeException("The task at index " + index + " does not exist.");
+      }
+      taskList.get(index - 1).markAsDone();
+    } else if (input.startsWith("unmark")) {
+      if (input.trim().equals("unmark")) {
+        throw new DukeException("The index of a task cannot be empty.");
+      }
+      int index = Integer.parseInt(input.split(" ")[1]);
+      if (index > getTaskCount()) {
+        throw new DukeException("The index of a task cannot be greater than the number of tasks.");
+      }
+      if (index <= 0) {
+        throw new DukeException("The index of a task cannot be 0 or negative.");
+      }
+      if (taskList.get(index - 1) == null) {
+        throw new DukeException("The task at index " + index + " does not exist.");
+      }
+      taskList.get(index - 1).unmarkAsDone();
+    } else if (input.startsWith("delete")) {
+      if (input.trim().equals("delete")) {
+        throw new DukeException("The index of a task cannot be empty.");
+      }
+      int index = Integer.parseInt(input.split(" ")[1]);
+      if (index > getTaskCount()) {
+        throw new DukeException("The index of a task cannot be greater than the number of tasks.");
+      }
+      if (index <= 0) {
+        throw new DukeException("The index of a task cannot be 0 or negative.");
+      }
+      if (taskList.get(index - 1) == null) {
+        throw new DukeException("The task at index " + index + " does not exist.");
+      }
+      removeTask(index);
+
+    } else if (input.startsWith("event")) {
+      Event newEvent = Event.createFromInput(input);
+      addTask(newEvent);
+    } else if (input.startsWith("deadline")) {
+      Deadline newDeadline = Deadline.createFromInput(input);
+      addTask(newDeadline);
+    } else if (input.startsWith("todo")) {
+      ToDo newToDo = ToDo.createFromInput(input);
+      addTask(newToDo);
+    } else {
+      throw new DukeException("I'm sorry, but I don't know what that means :-(");
+    }
+  }
+
+  /*
    * Prints the list of tasks
    */
-  private static void printList(Task[] list) {
+  private static void printList(ArrayList<Task> list) {
     System.out.println("Here are the tasks in your list:");
-    for (int i = 0; i < list.length; i++) {
-      if (list[i] == null) {
-        break;
-      }
-      System.out.println((i + 1) + ". " + list[i]);
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println((i + 1) + ". " + list.get(i));
     }
   }
 
@@ -86,14 +108,18 @@ public class Duke {
    * Returns the number of tasks in the list
    */
   private static int getTaskCount() {
-    int count = 0;
-    for (int i = 0; i < taskList.length; i++) {
-      if (taskList[i] == null) {
-        break;
-      }
-      count++;
+    return taskList.size();
+  }
+
+  /*
+   * Return number of task message
+   */
+  private static String getTaskCountMessage() {
+    if (getTaskCount() == 1) {
+      return "Now you have " + getTaskCount() + " task in the list.";
+    } else {
+      return "Now you have " + getTaskCount() + " tasks in the list.";
     }
-    return count;
   }
 
   /*
@@ -101,18 +127,19 @@ public class Duke {
    */
   private static void addTask(Task task) {
     System.out.println("Got it. I've added this task:");
-    for (int i = 0; i < taskList.length; i++) {
-      if (taskList[i] == null) {
-        taskList[i] = task;
-        System.out.println(task);
-        break;
-      }
-    }
-    if (getTaskCount() == 1) {
-      System.out.println("Now you have " + getTaskCount() + " task in the list.");
-    } else {
-      System.out.println("Now you have " + getTaskCount() + " tasks in the list.");
-    }
+    taskList.add(task);
+    System.out.println(task);
+    System.out.println(getTaskCountMessage());
+  }
+
+  /*
+   * Removes a task from the list
+   */
+  private static void removeTask(int index) {
+    System.out.println("Noted. I've removed this task:");
+    System.out.println(taskList.get(index - 1));
+    taskList.remove(index - 1);
+    System.out.println(getTaskCountMessage());
   }
 
 }
