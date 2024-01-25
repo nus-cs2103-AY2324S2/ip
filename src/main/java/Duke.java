480177d13d5e5e9325c31e076719e37c34016487
Jajared <jajawong09@gmail.com>
2024-01-25 10:34:07 +0800

    refactor: changed message and updated git ignore file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+  public static void main(String[] args) {
+    String botName = "GeePeeTee";
+    // Welcome Message
+    System.out.println("Hello! I'm " + botName + "\nWhat can I do for you?");
+    // Exit Message
+    System.out.println("Bye. Hope to see you again soon!");
+  }
 }
