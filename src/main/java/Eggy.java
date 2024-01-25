feb2f0199ec00b5cf1dc03cddb60ce032b49c5f9
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-01-25 14:33:03 +0800

    Rename bot and add greeting and goodbye

---
+++ src/main/java/Eggy.java
@@ -0,0 +1,11 @@
+public class Eggy {
+    public static void main(String[] args) {
+        String name = "Eggy";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
