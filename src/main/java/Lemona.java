52c8e422cd17e9c5b6a523abc22f313ccd1b60fd
Howlong11 <e1093040@u.nus.edu>
2024-01-25 18:50:44 +0800

    FIrst commit, level-0

---
+++ src/main/java/Lemona.java
@@ -0,0 +1,9 @@
+public class Lemona {
+    public static void main(String[] args) {
+        System.out.println("______________________________________________________");
+        System.out.println("Hello! I'm Lemona" + "\n" + "What can I do for you?");
+        System.out.println("______________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________________");
+    }
+}
