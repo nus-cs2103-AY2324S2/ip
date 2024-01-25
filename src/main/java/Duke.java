f4d5bc2de3ecda9cc8a17d070765fcbda1c27be7
James Shen <95962838+shamesjen@users.noreply.github.com>
2024-01-25 22:58:09 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "James";
         System.out.println("Hello! I'm " + name + "\n");
         System.out.println("What can I do for you? \n");
 
-        System.out.println("Bye. Hope to see you again soon! \n");
+        while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon! \n");
+                break;
+            }
+
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("list");
+            }
+
+            if (input.equalsIgnoreCase("blah")) {
+                System.out.println("blah");
+            }
+        }
     }
 }
