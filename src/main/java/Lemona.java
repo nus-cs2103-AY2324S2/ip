957599c11bb2f0b7ea6d3d5b6261d5333126dc08
Howlong11 <e1093040@u.nus.edu>
2024-01-25 19:40:05 +0800

    Second commit, Level-1

--- src/main/java/Lemona.java
+++ src/main/java/Lemona.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Lemona {
     public static void main(String[] args) {
-        System.out.println("______________________________________________________");
-        System.out.println("Hello! I'm Lemona" + "\n" + "What can I do for you?");
-        System.out.println("______________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        //greeting
+        System.out.println("\t______________________________________________________" +
+                "\n\t" + " Hello! I'm Lemona" + "\n\t" + " What can I do for you?" +
+                "\n\t______________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("\t______________________________________________________");
+                System.out.println("\t" +" Bye. Hope to see you again soon!");
+                System.out.println("\t______________________________________________________");
+                break;
+            } else {
+                System.out.println("\t______________________________________________________");
+                System.out.println("\t " + input);
+                System.out.println("\t______________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
