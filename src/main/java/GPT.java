9bc978185ec5b5840659b68433938e0023a681c7
Ryo-Seah <seah_ryo@yahoo.com.sg>
2024-01-25 16:20:36 +0800

    Complete Level-3

--- src/main/java/GPT.java
+++ src/main/java/GPT.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 import java.util.*;
 public class GPT {
     public static void main(String[] args) {
+         class Task {
+            boolean done;
+            String todo;
+            public Task(String todo) {
+                this.todo = todo;
+                this.done = false;
+            }
+
+            public void mark() {
+                this.done = true;
+            }
+            public void unmark() {
+                this.done = false;
+            }
+        }
         Scanner scn = new Scanner(System.in);
         String name = " GPT";
         String secLine = "What can I do for you?\n\n";
@@ -9,23 +24,36 @@ public class GPT {
         System.out.println("Hello! I'm" + name);
         System.out.println(secLine);
         String s = scn.nextLine();
-        ArrayList<String> sl = new ArrayList<>();
-        while(!s.equals("bye")) {
+        ArrayList<Task> tl = new ArrayList<>();
+//        ArrayList<String> sl = new ArrayList<>();
+//        ArrayList<Boolean> done = new ArrayList<>();
+        while (!s.equals("bye")) {
             if (s.equals("list")) {
-                for (int i=1; i<=sl.size(); i++) {
-                    System.out.println( i + ". " + sl.get(i-1));
+                for (int i = 1; i <= tl.size(); i++) {
+                    System.out.println(i + (tl.get(i - 1).done ? ". [x] " : ". [ ] ") + tl.get(i - 1));
                 }
 
-            } else {
-                System.out.println("added " + s);
-                sl.add(s);
+            } else if (s.contains("unmark")) {
+                String[] splited = s.split("\\s+");
+                if (splited[0].equals("unmark") && Integer.valueOf(splited[1]) <= tl.size()) {
+                    tl.get(Integer.valueOf(splited[1])-1).unmark();
+                }
+
+            } else if (s.contains("mark")) {
+                String[] splited = s.split("\\s+");
+                if (splited[0].equals("mark") && Integer.valueOf(splited[1]) <= tl.size()) {
+                    tl.get(Integer.valueOf(splited[1])-1).mark();
+                }
+            }else {
+                    System.out.println("added " + s);
+                    tl.add(new Task(s));
+                }
+                s = scn.nextLine();
 
             }
-            s = scn.nextLine();
 
+
+            System.out.println("ByeBye. Hope to see you soon");
         }
-
-
-        System.out.println( "Bye. Hope to see you soon");
     }
-}
+
