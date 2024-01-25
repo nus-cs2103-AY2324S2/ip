518dad98b18629c9ad33bed45ce946090e93a192
erv-teo <107994607+erv-teo@users.noreply.github.com>
2024-01-25 11:01:35 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -8,9 +10,20 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 
         String hello = "Hello!\n" + "What can I do for you?\n";
+        System.out.println(hello);
+
         String bye = "See you soon!";
 
-        System.out.println(hello);
-        System.out.println(bye);
+
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String str = input.nextLine();
+            if (str.equals("bye") || str.equals("Bye")) {
+                System.out.println(bye);
+                break;
+            } else {
+                System.out.println(str);
+            }
+        }
     }
 }
