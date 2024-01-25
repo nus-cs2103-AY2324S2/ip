0bbac0c05cf10363ff26c3e286cdbb786cf61dc2
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-25 16:52:46 +0800

    Updated Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,8 @@
-public class Event implements Task {
+public class Event extends Task {
     protected String from;
     protected String to;
 
-    public Deadline(String description, String from, String to) {
+    public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
