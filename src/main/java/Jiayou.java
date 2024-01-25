a40d6f5e00f64c538541337271124ba836347ea0
LIU JIAYAO <>
2024-01-25 14:13:13 +0800

    level 1 echo

--- src/main/java/Jiayou.java
+++ src/main/java/Jiayou.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Jiayou {
+    private static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        Jiayou jiayou = new Jiayou();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Jiayou.LINE);
         System.out.println("Hello! I'm Jiayou.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(Jiayou.LINE);
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(Jiayou.LINE);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(Jiayou.LINE);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(Jiayou.LINE);
+        }
+        sc.close();
     }
 }
