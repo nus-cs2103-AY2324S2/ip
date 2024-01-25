1cc201db4839adb8fd5710242fc97120085b0025
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-01-25 13:36:34 +0800

    Level-1: Added echo function and customized the personality of the chatbox

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -7,18 +8,36 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         greet();
-        exit();
+        command();
     }
     static String name = "Alfred";
     public static void greet(){
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tGreetings! I am " + name +".");
+        System.out.println("\tHow may I be of service to you today?");
+        System.out.println("\t____________________________________________________________\n");
     }
 
     public static void exit(){
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tFarewell. Wishing for the opportunity to meet you again soon.");
+        System.out.println("\t____________________________________________________________");
     }
-}
+
+    public static void command() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = scanner.nextLine();
+        }
+        exit();
+    }
+
+    public static void echo(String content){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t"+content);
+        System.out.println("\t____________________________________________________________\n" );
+    }
+
+    }
