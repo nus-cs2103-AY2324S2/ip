66d691351aa8bf2fbfeefe3965744b226a6be928
VatsalVitosh <vatsalvitosh@gmail.com>
2024-01-26 04:07:19 +0800

    Level 2 - Add, List

--- src/main/java/Lia.java
+++ src/main/java/Lia.java
@@ -2,14 +2,23 @@ import java.util.*;
 public class Lia {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         System.out.println("Hello! I'm Lia :)");
         System.out.println("What can I do for you?");
+
         while (true) {
-            String readInput = sc.nextLine();
-            if (readInput.equals("bye")) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for(int j = 1; j < tasks.size() + 1; j++) {
+                    System.out.println(j + ". " + tasks.get(j - 1));
+                }
             } else {
-                System.out.println(readInput);
+                tasks.add(input);
+                System.out.println("added: " + input);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
