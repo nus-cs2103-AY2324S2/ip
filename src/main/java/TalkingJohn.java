6be7f8fb2b5a7ba96b9b41ee04fe3dc18be764a6
John Yoo <e0968839@u.nus.edu>
2024-01-25 21:01:29 +0800

    Implemented TalkingJohn Level5

--- src/main/java/TalkingJohn.java
+++ src/main/java/TalkingJohn.java
@@ -12,6 +12,17 @@ public class TalkingJohn {
         int j = 0;
         while (true) {
             String input = scanner.nextLine();
+            if (Objects.equals(input, "todo") || Objects.equals(input, "deadline") || Objects.equals(input, "event")) {
+                System.out.println("OOPS!!! The description of " + input + " cannot be empty.");
+                continue;
+            } else if (Objects.equals(input, "mark")) {
+                System.out.println("OOPS!!! mark cannot be empty.");
+                continue;
+            } else if (Objects.equals(input, "unmark")) {
+                System.out.println("OOPS!!! unmark cannot be empty.");
+                continue;
+            }
+
             if (Objects.equals(input, "bye")) {
                 System.out.println("Bye, hope to see you soon");
                 break;
@@ -29,10 +40,8 @@ public class TalkingJohn {
                     task_to_mark.mark();
                     System.out.println("Nice! I've marked this task as done:\n"
                             + task_to_mark);
-                } catch (NumberFormatException e) {
-                    TaskArr[j] = new Task(input);
-                    System.out.println("added: " + input);
-                    j++;
+                } catch (NumberFormatException | NullPointerException e) {
+                    System.out.println("OOPS!!! Invalid mark expression.");
                 }
             } else if (input.length() > 6 && Objects.equals(input.substring(0, 6), "unmark") && input.charAt(6) == ' ') {
                 String[] parts = input.split(" ", 2);
@@ -43,10 +52,8 @@ public class TalkingJohn {
                     task_to_unmark.unMark();
                     System.out.println("OK, I've marked this task as not done yet:\n"
                             + task_to_unmark);
-                } catch (NumberFormatException e) {
-                    TaskArr[j] = new Task(input);
-                    System.out.println("added: " + input);
-                    j++;
+                } catch (NumberFormatException | NullPointerException e) {
+                    System.out.println("OOPS!!! Invalid unmark expression.");
                 }
             } else if (input.length() > 4 && Objects.equals(input.substring(0, 4), "todo") && input.charAt(4) == ' ') {
                 String[] parts = input.split(" ", 2);
@@ -58,38 +65,43 @@ public class TalkingJohn {
                         + to_do + "\n"
                         + "Now you have " + j + " tasks in the list.");
             } else if (input.length() > 8 && Objects.equals(input.substring(0, 8), "deadline") && input.charAt(8) == ' ') {
-                String[] parts = input.split(" ", 2);
-                String buffer = parts[1];
-                String[] secPart = buffer.split("/", 2);
-                String description = secPart[0];
-                String by = secPart[1];
-                Deadline deadline = new Deadline(description, by);
-                TaskArr[j] = deadline;
-                j++;
-                System.out.println("Got it. I've added this task:\n"
-                        + deadline + "\n"
-                        + "Now you have " + j + " tasks in the list.");
+                try {
+                    String[] parts = input.split(" ", 2);
+                    String buffer = parts[1];
+                    String[] secPart = buffer.split("/", 2);
+                    String description = secPart[0];
+                    String by = secPart[1];
+                    Deadline deadline = new Deadline(description, by);
+                    TaskArr[j] = deadline;
+                    j++;
+                    System.out.println("Got it. I've added this task:\n"
+                            + deadline + "\n"
+                            + "Now you have " + j + " tasks in the list.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("OH NO! It seems like the format is wrong. Did you include at least 1 '/' in the description?");
+                }
             } else if (input.length() > 5 && Objects.equals(input.substring(0, 5), "event") && input.charAt(5) == ' ') {
-                String[] parts = input.split(" ", 2);
-                String buffer = parts[1];
-                String[] secPart = buffer.split("/", 3);
-                String description = secPart[0];
-                String by1 = secPart[1];
-                String by2 = secPart[2];
-                Event event = new Event(description, by1, by2);
-                TaskArr[j] = event;
-                j++;
-                System.out.println("Got it. I've added this task:\n"
-                        + event + "\n"
-                        + "Now you have " + j + " tasks in the list.");
+                try {
+                    String[] parts = input.split(" ", 2);
+                    String buffer = parts[1];
+                    String[] secPart = buffer.split("/", 3);
+                    String description = secPart[0];
+                    String by1 = secPart[1];
+                    String by2 = secPart[2];
+                    Event event = new Event(description, by1, by2);
+                    TaskArr[j] = event;
+                    j++;
+                    System.out.println("Got it. I've added this task:\n"
+                            + event + "\n"
+                            + "Now you have " + j + " tasks in the list.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("OH NO! It seems like the format is wrong. Did you include at least 2 '/' in the description?");
+                }
             } else {
-                TaskArr[j] = new Task(input);
-                System.out.println("added: " + input);
-                j++;
+                System.out.println("HMM? This is an activity planner and you are not making sense. Please repeat \uD83E\uDD72");
             }
         }
         scanner.close();
     }
 }
 
-//dnqnd
