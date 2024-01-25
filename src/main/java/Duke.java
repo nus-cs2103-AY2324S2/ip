3c1a054c7e897cd580ce85542f4fc126e46d5c1b
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-26 01:25:45 +0800

    Completed Level-6 and updated test cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,7 +23,7 @@ public class Duke {
                 Task t = new ToDo(userTxt.substring(5));
                 listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
-                System.out.println(t.toString());
+                System.out.println(t);
                 System.out.println("Now you have " + listArr.size() + " tasks in the list.");
             }
         } catch (DukeException ex) {
@@ -43,7 +43,7 @@ public class Duke {
                 Task t = new Deadline(desc, by);
                 listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
-                System.out.println(t.toString());
+                System.out.println(t);
                 System.out.println("Now you have " + listArr.size() + " tasks in the list.");
             }
         } catch (DukeException ex) {
@@ -65,7 +65,7 @@ public class Duke {
                 Task t = new Event(desc, frm, to);
                 listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
-                System.out.println(t.toString());
+                System.out.println(t);
                 System.out.println("Now you have " + listArr.size() + " tasks in the list.");
             }
         } catch (DukeException ex) {
@@ -74,6 +74,7 @@ public class Duke {
     }
 
     public static void deleteTask(ArrayList<Task> listArr, int taskNum) {
+        listArr.get(taskNum);
         System.out.println("Noted. I've removed this task:");
         System.out.println(listArr.get(taskNum));
         listArr.remove(taskNum);
@@ -92,38 +93,48 @@ public class Duke {
             if (userTxt.equals("list")) {
                 createList(listArr);
             } else if (userTxt.startsWith("mark")) {
-                int taskNum = Integer.parseInt(userTxt.substring(5)) - 1;
-                listArr.get(taskNum).setDone();
+                try {
+                    int taskNum = Integer.parseInt(userTxt.substring(5)) - 1;
+                    listArr.get(taskNum).setDone();
+                } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                    System.out.println("Hmm...seems like the task to mark does not exist. To mark a task, input the keyword followed by the task's no. in the list. E.g.: mark 3");
+                }
             } else if (userTxt.startsWith("unmark")) {
-                int taskNum = Integer.parseInt(userTxt.substring(7)) - 1;
-                listArr.get(taskNum).setUndone();
+                try {
+                    int taskNum = Integer.parseInt(userTxt.substring(7)) - 1;
+                    listArr.get(taskNum).setUndone();
+                } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                    System.out.println("Hmm...seems like the task to unmark does not exist. To unmark a task, input the keyword followed by the task's no. in the list. E.g.: unmark 3");
+                }
             } else if (userTxt.startsWith("delete")) {
-                int taskNum = Integer.parseInt(userTxt.substring(7)) - 1;
-                deleteTask(listArr, taskNum);
+                try {
+                    int taskNum = Integer.parseInt(userTxt.substring(7)) - 1;
+                    deleteTask(listArr, taskNum);
+                } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                    System.out.println("Hmm...seems like the task to delete does not exist. To delete a task, input the keyword followed by the task's no. in the list. E.g.: delete 3");
+                }
+            } else if (userTxt.startsWith("todo")) {
+                createTodo(userTxt, listArr);
+            } else if (userTxt.startsWith("deadline")) {
+                createDeadline(userTxt, listArr);
+            } else if (userTxt.startsWith("event")) {
+                createEvent(userTxt, listArr);
             } else {
-                if (userTxt.startsWith("todo")) {
-                    createTodo(userTxt, listArr);
-                } else if (userTxt.startsWith("deadline")) {
-                    createDeadline(userTxt, listArr);
-                } else if (userTxt.startsWith("event")) {
-                    createEvent(userTxt, listArr);
-                } else {
-                    try {
-                        throw new DukeUnknownTaskException();
-                    } catch (DukeException ex) {
-                        System.out.println(ex.getMessage());
-                    }
+                try {
+                    throw new DukeUnknownTaskException();
+                } catch (DukeException ex) {
+                    System.out.println(ex.getMessage());
                 }
             }
+
             System.out.println("-----------------------------------------------------------------------");
             System.out.println("-----------------------------------------------------------------------");
             userTxt = sc.nextLine();
         }
-        if (userTxt.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("-----------------------------------------------------------------------");
-            System.out.println("-----------------------------------------------------------------------");
-        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------");
     }
 }
 
