fe1305569eddb86e237d218cca277c56cce4b88a
marclamp <lamjj25@gmail.com>
2024-01-26 05:15:33 +0800

    Added Echo Functionality

--- src/main/java/Chaterpillar.java
+++ src/main/java/Chaterpillar.java
@@ -1,20 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Chaterpillar {
+    public static boolean exited = false;
     public static void greet() {
+        print_horizontal_line();
         System.out.println("Hello! I'm Chaterpillar");
         System.out.println("What can I do for you?");
+        print_horizontal_line();
     }
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void echo(BufferedReader reader) throws IOException {
+        String input = reader.readLine();
+        print_horizontal_line();
+        if (!input.equals("bye")) {
+            System.out.println(input);
+        } else {
+            exited = true;
+            exit();
+        }
+        print_horizontal_line();
     }
     public static void print_horizontal_line() {
         String line = "-".repeat(50);
         System.out.println(line);
     }
     public static void main(String[] args) {
-        print_horizontal_line();
         greet();
-        print_horizontal_line();
-        exit();
-        print_horizontal_line();
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader((System.in)));
+
+        while (!exited) {
+            try {
+                echo(reader);
+            } catch (IOException e) {
+                System.out.println("IOException occurred.");
+            }
+        }
     }
 }
