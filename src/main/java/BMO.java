c856ca2433c62042545df01991f724b35f01296f
huekoh <huexkoh@gmail.com>
2024-01-25 19:57:15 +0800

    Implemented list adder

--- src/main/java/BMO.java
+++ src/main/java/BMO.java
@@ -1,35 +1,48 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+
 public class BMO {
+
+    static List<String> backLog = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
-        echo();
+        receive();
     }
 
     static void greet() {
-        String hi = "-----------------------------------------\n"
+        String hiPrint = "-----------------------------------------\n"
                     + "    BMO chop!\n    Do you want to play video games?\n"
                     + "-----------------------------------------";
-        System.out.println(hi);
+        System.out.println(hiPrint);
     }
 
     static void salute() {
-        String bye = "-----------------------------------------\n"
+        String byePrint = "-----------------------------------------\n"
                     + "    Beep boop BMO shut down :(\n"
                     + "-----------------------------------------";
-        System.out.println(bye);
+        System.out.println(byePrint);
     }
 
-    static void echo() {
+    static void receive() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         if (input.equals("bye")) {
             salute();
+        } else if (input.equals("backlog")) {
+            StringBuilder logPrint = new StringBuilder();
+            for (int i = 0; i < backLog.size(); i++) {
+                logPrint.append(i + 1).append(". ").append(backLog.get(i)).append("\n");
+            }
+            System.out.println(logPrint.toString());
+            receive();
         } else {
-            String output = "-----------------------------------------\n"
-                    + "    " + input + "\n"
+            backLog.add(input);
+            String addPrint = "-----------------------------------------\n"
+                    + "added: " + input + "\n"
                     + "-----------------------------------------";
-            System.out.println(output);
-            echo();
+            System.out.println(addPrint);
+            receive();
         }
     }
 }
