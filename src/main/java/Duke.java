6280b3eea514ecf616f3c0db4edc4d89ee8cfa65
SampsonYe1999 <sampsonye2017@gmail.com>
2024-01-26 06:08:23 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String basic = "____________________________________________________________\n"
+                + " Hello! I'm Dikey.\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "_____________________________________________________________ \n";
+        System.out.println("Hello from\n" + basic);
     }
 }
