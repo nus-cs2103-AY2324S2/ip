ecad19c403e68672bc1cb5f392c07b6aa45dd225
joel <joelgoh16@gmail.com>
2024-01-25 10:01:26 +0800

    add echo functionality to Blawg.java

--- src/main/java/Blawg.java
+++ src/main/java/Blawg.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Blawg {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String intro = "____________________________________________________________\n" +
                 " Hello! I'm Blawg\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
         System.out.println(intro);
-        String outro = " Bye. Hope to see you again soon!\n" +
+        String outro = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println(outro);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(outro);
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        userInput + "\n" +
+                        " ____________________________________________________________\n");
+            }
+        }
+        sc.close();
     }
 }
