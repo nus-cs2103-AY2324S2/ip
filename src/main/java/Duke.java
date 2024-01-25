2865bf46e27f2af6489b3e7f90e53b13d48707ec
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-01-25 12:06:52 +0800

    Level_2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int counter = 0;
-        String[] l = new String[100];
+        Task[] task = new Task[100];
         String horizontalLine = "_".repeat(60);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Friday");
@@ -12,23 +12,32 @@ public class Duke {
         System.out.println(horizontalLine);
 
         String userInput = sc.nextLine().trim();
+        Task t = new Task(userInput);
         while (!userInput.equals("bye")) {
+            System.out.println(horizontalLine);
             if (userInput.equals("list")) {
-                System.out.println(horizontalLine);
-                for (int i = 1; i <= counter; i++) {
-                    System.out.println(i + ". " + l[i - 1]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + "[" + task[i].getStatusIcon()+ "] " + task[i]);
                 }
-                System.out.println(horizontalLine);
-                userInput = sc.nextLine().trim();
+            } else if(userInput.matches("mark \\d+")) {
+                System.out.println("Nice! I've marked this task as done:");
+                int id = Integer.parseInt(userInput.split(" ")[1]);
+                task[id - 1].markAsDone();
+                System.out.println("[" + task[id - 1].getStatusIcon() + "] " + task[id - 1].toString());
+            } else if(userInput.matches("unmark \\d+")) {
+                System.out.println("OK, I've marked this task as not done yet:");
+                int id = Integer.parseInt(userInput.split(" ")[1]);
+                task[id - 1].markAsUndone();
+                System.out.println("[" + task[id - 1].getStatusIcon() + "] " + task[id - 1].toString());
             } else {
-                l[counter] = userInput;
+                task[counter] = t;
                 counter++;
-                System.out.println(horizontalLine);
                 System.out.println("added: " + userInput);
-                System.out.println(horizontalLine);
-                userInput = sc.nextLine().trim();
             }
-
+            System.out.println(horizontalLine);
+            userInput = sc.nextLine().trim();
+            t = new Task(userInput);
         }
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
@@ -36,3 +45,4 @@ public class Duke {
         sc.close();
     }
 }
+
