9168cd08cfc2b52b457b37cba5061a1e30c3e745
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-01-25 23:15:39 +0800

    commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,30 +38,4 @@ public class Duke {
         }
     }
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public String getDescription(){
-            return this.description;
-        }
-
-        public void taskDone(){
-            isDone = true;
-        }
-
-        public void taskUndone(){
-            isDone = false;
-        }
-    }
-
 }
