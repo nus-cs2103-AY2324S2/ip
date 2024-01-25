ad7d70be5dd4cd81a89d9b186250f81a98d2ec7a
thewongdylan <dylanwong.p@gmail.com>
2024-01-25 21:28:17 +0800

    Level-1
    Added echo and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,7 +9,58 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String greeting = "Hello I am DylanBot! \nWhat can I do for you? \n\nBye! Hope to see you again soon";
+        String greeting = "Hello I am DylanBot! \nWhat can I do for you?";
+        String exit = "Bye! Hope to see you again soon";
+
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (!"bye".equals((input = scanner.nextLine()))) {
+            System.out.println("\t" + input);
+        }
+        System.out.println(exit);
+    }
+
+    public static class FastScanner {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+        String nextLine() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken("\n");
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 }
+
+
