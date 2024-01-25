56508696ce7e1da2f5721408d0718ffe7e802b45
Jeremiah Lim <43368034+jeremiahlzz@users.noreply.github.com>
2024-01-25 19:06:56 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Zero.java
@@ -0,0 +1,11 @@
+public class Zero {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________\n";
+        String name = "Zero";
+        String greeting = "Hello! I'm " + name + ".";
+        String whatDo = "What can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(divider + greeting + '\n' + whatDo);
+        System.out.println(divider + goodbye + '\n' + divider);
+    }
+}
