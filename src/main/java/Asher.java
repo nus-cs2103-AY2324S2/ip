a3bdb36d110cab4a362d65d4c2e878c451b183ca
cleowenxuan <>
2024-01-25 09:50:24 +0800

    Level0 Completed

---
+++ src/main/java/Asher.java
@@ -0,0 +1,13 @@
+public class Asher {
+    public static void greet() {
+        System.out.println("Hello! I'm Asher. What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void main(String[] args) {
+        Asher.greet();
+        Asher.exit();
+    }
+}
