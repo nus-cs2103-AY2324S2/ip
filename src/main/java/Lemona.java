58bba397eb1bf260b399b8d25f4aa7d7cdfa7230
Howlong11 <e1093040@u.nus.edu>
2024-01-25 21:31:10 +0800

    Third commit, Level-2

--- src/main/java/Lemona.java
+++ src/main/java/Lemona.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lemona {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> list = new ArrayList<>();
         //greeting
         System.out.println("\t______________________________________________________" +
                 "\n\t" + " Hello! I'm Lemona" + "\n\t" + " What can I do for you?" +
@@ -16,9 +17,16 @@ public class Lemona {
                 System.out.println("\t" +" Bye. Hope to see you again soon!");
                 System.out.println("\t______________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("\t______________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("\t " + (i + 1) + ". " + list.get(i));
+                }
+                System.out.println("\t______________________________________________________");
             } else {
                 System.out.println("\t______________________________________________________");
-                System.out.println("\t " + input);
+                list.add(input);
+                System.out.println("\t " + "added: " + input);
                 System.out.println("\t______________________________________________________");
             }
         }
