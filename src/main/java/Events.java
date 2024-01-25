d6c496d1d32087075ea219d503b0774e0dd062ab
ChenXy128 <e0968917@u.nus.edu>
2024-01-25 21:42:03 +0800

    Events

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Events {
+public class Events extends Task{
+    protected String start;
+    protected String end;
+    
+    public Events(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+    
+    public String getStart() {
+        return this.start;
+    }
+    
+    public String getEnd() {
+        return this.end;
+    }
+    
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from " + this.getStart() + " to " + this.getEnd() + ")";
+    }
 }
