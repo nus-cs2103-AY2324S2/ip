e7adaa0a145b3390eea5b8062aba1a8d52c0095e
Lim Pei En <limpeien123@gmail.com>
2024-01-25 21:20:42 +0800

    Implement level 1 - Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Cookie");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(input);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
