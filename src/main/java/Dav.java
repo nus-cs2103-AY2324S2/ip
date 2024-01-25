4d8f8cefe88bfb275f85d14757db951c05853974
NatLeong <>
2024-01-25 12:39:14 +0800

    Add Level 2

--- src/main/java/Dav.java
+++ src/main/java/Dav.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Dav {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         greetUser();
         startChat();
@@ -9,8 +13,8 @@ public class Dav {
 
     public static void greetUser() {
         System.out.println("____________________________________________________________");
-        System.out.println(" What's up! I'm Dav");
-        System.out.println(" How may I help you?");
+        System.out.println(" Hello! I'm Dav");
+        System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
     }
 
@@ -20,7 +24,7 @@ public class Dav {
 
         do {
             userInput = getUserInput(scanner);
-            echoUserInput(userInput);
+            processUserInput(userInput);
 
         } while (!userInput.equalsIgnoreCase("bye"));
     }
@@ -30,13 +34,40 @@ public class Dav {
         return scanner.nextLine();
     }
 
-    public static void echoUserInput(String input) {
-        System.out.println(" " + input);
+    public static void processUserInput(String input) {
+
+        if (input.equalsIgnoreCase("list")) {
+            listTasks();
+        } else if (!input.equalsIgnoreCase("bye")) {
+            addTask(input);
+        }
+
         System.out.println("____________________________________________________________");
     }
 
+    public static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount++] = task;
+            System.out.println(" added: " + task);
+        } else {
+            System.out.println(" Sorry, task list is full. Cannot add more tasks.");
+        }
+    }
+
+    public static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println(" No tasks added yet.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
+    }
+
     public static void exit() {
-        System.out.println(" Goodbye.");
+        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
+
+
