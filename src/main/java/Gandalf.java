8ad365406d8e4fd7894265449647429ed32ca6e2
dabzpengu <mahathir66@gmail.com>
2024-01-25 19:17:37 +0800

    add, list

--- src/main/java/Gandalf.java
+++ src/main/java/Gandalf.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.Hashtable;
 
 public class Gandalf {
+
     public static void main(String[] args) {
+        Hashtable<Integer, String> list = new Hashtable<>();
+        int numOfActions = 0;
         System.out.println("Through fire and shadow, I'm Gandalf");
         System.out.println("What can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +16,16 @@ public class Gandalf {
                 System.out.println("So here at last, comes the end of our fellowship. I will not say: Do not weep. For not all tears are an evil.");
                 break;
             }
-            System.out.println(input);
+            if(input.equals("list")){
+                for(int i = 1; i <= numOfActions; i++){
+                    String action = list.get(i);
+                    System.out.println(i + ". " + action);
+                }
+                continue;
+            }
+            numOfActions++;
+            list.put(numOfActions, input);
+            System.out.println("added: " + input);
         }
     }
 }
