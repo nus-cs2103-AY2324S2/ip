7b02c11997f3ad049d5dba6a6d03dc1226f533cd
Shaun Lee <shaunlxw@gmail.com>
2024-01-25 20:20:21 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Shon.java
@@ -0,0 +1,12 @@
+public class Shon {
+
+    final static String LINE = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Shon\n");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
