4a158540614607a94d8c887f774bb7baf6ca0add
wolffe88 <yonglian1845@gmail.com>
2024-01-25 15:28:38 +0800

    Handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,9 +22,9 @@ public class Duke {
                 break;
             } else if (reply.startsWith("list")) {
                 System.out.println(bot + "Here are your tasks:");
-                for (int i = 0; i < taskList.size(); i++) {
-                    task = taskList.get(i);
-                    System.out.println(i+1 + ". " + task.toString());
+                for (int i = 1; i < taskList.size()+1; i++) {
+                    task = taskList.get(i-1);
+                    System.out.println("    " + i + ". " + task.toString());
                 }
                 continue;
             } else if (reply.startsWith("mark")) {
@@ -40,24 +40,64 @@ public class Duke {
                 System.out.println(bot + task.toString());
                 continue;
             } else if (reply.startsWith("todo")) {
-                String description = reply.substring(5,reply.length());
-                task = new ToDos(description);
+                try {
+                    if (reply.length() <= 5) {
+                        throw new RolandException("Please provide description for todo");
+                    }
+                    String description = reply.substring(5,reply.length());
+                    task = new ToDos(description);
+                } catch (RolandException e) {
+                    System.out.println(bot + e.getMessage());
+                    continue;
+                }
+
             } else if (reply.startsWith("deadline")) {
-                String split[] = reply.split(" /");
-                String description = split[0].substring(9, split[0].length());
-                String by = split[1].substring(3, split[1].length());
-                task = new Deadlines(description, by);
+                try {
+                    if (reply.length() <= 9) {
+                        throw new RolandException("Please provide description for deadline");
+                    }
+                    if (reply.split("/").length != 2) {
+                        throw new RolandException("Please include when is the deadline by with /by <deadline>");
+                    }
+                    String split[] = reply.split(" /");
+                    String description = split[0].substring(9, split[0].length());
+                    String by = split[1].substring(3, split[1].length());
+                    task = new Deadlines(description, by);
+                } catch (RolandException e) {
+                    System.out.println(bot + e.getMessage());
+                    continue;
+                }
+
 
             } else if (reply.startsWith("event")) {
-                String split[] = reply.split(" /");
-                String description = split[0].substring(6, split[0].length());
-                String from = split[1].substring(5, split[1].length());
-                String to = split[2].substring(4, split[2].length());
-                task = new Events(description, from, to);
+                try {
+                    if (reply.length() <= 6) {
+                        throw new RolandException("Please provide description for event");
+                    }
+                    if (reply.split("/").length != 3) {
+                        throw new RolandException("Please include when is the start and end of the event with /from <start> /to <end>");
+                    }
+                    String split[] = reply.split(" /");
+                    String description = split[0].substring(6, split[0].length());
+                    String from = split[1].substring(5, split[1].length());
+                    String to = split[2].substring(4, split[2].length());
+                    task = new Events(description, from, to);
+                } catch (RolandException e){
+                    System.out.println(bot + e.getMessage());
+                    continue;
+                }
+
+
             } else {
-                System.out.println(reply);
-                continue;
+                try {
+                    throw new RolandException("I do not understand you :(.");
+
+                } catch (RolandException e) {
+                    System.out.println(bot + e.getMessage());
+                    continue;
+                }
             }
+
             taskList.add(task);
             System.out.println(bot + "I have added " + task.toString() + " to your list of tasks. You have " + taskList.size() + " task(s) in list");
 
---
+++ src/main/java/RolandException.java
@@ -0,0 +1,5 @@
+class RolandException extends Exception {
+    public RolandException(String message) {
+        super(message);
+    }
+}
