c461f9ee24f47a9a5b176c0f70ddf8ab10d0372d
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-25 15:55:58 +0800

    Added function to mark tasks as done and undone

--- src/main/java/Lindi.java
+++ src/main/java/Lindi.java
@@ -1,3 +1,7 @@
+import javax.xml.stream.events.Characters;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Lindi {
     private final static Task[] taskList = new Task[100];
     private static int taskListCount = 0;
@@ -23,11 +27,68 @@ public class Lindi {
                 printSeparator();
                 System.exit(0); // exit with code 0, terminates program
             }
-        }
 
-        switch (userInput) {
+            /**
+             * @param listIndex: listIndex as shown by the `list` command, starts from 1
+             */
+            void executeMark(int listIndex) {
+                // Index starts from 1, up to 100
+                if (listIndex < 1 || listIndex > taskListCount) {
+                    System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
+                            "You can see the tasks list by inputting \"list\"");
+                }
+                Task task = taskList[listIndex - 1];
+                task.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("\t" + task.toString());
+            }
+            /**
+             * @param listIndex: listIndex as shown by the `list` command, starts from 1
+             */
+            void executeUnmark(int listIndex) {
+                // Index starts from 1, up to 100
+                if (listIndex < 1 || listIndex > taskListCount) {
+                    System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
+                            "You can see the tasks list by inputting \"list\"");
+                }
+                Task task = taskList[listIndex - 1];
+                task.unmarkDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("\t" + task.toString());
+            }
+        }
+        String[] userInputTokens = userInput.split(" ", 3);
+        System.out.println(userInputTokens.length);
+        switch (userInputTokens[0]) {
             case "list":
                 new LocalFuncs().executeList();
+                break;
+            case "unmark":
+                // Now check if the second token 1) exists, 2) is a number. Else, treat it as a new task.
+                if (userInputTokens.length == 2) { // check if there is only 1 argument after unmark command
+                    // check if it is a number
+                    if (isNumber(userInputTokens[1])) {
+                        System.out.println("is number");
+                        int taskIndex = Integer.parseInt(userInputTokens[1]);
+                        new LocalFuncs().executeUnmark(taskIndex);
+                    }
+                }
+                else {
+                    new LocalFuncs().executeNone(new Task(userInput));
+                }
+                break;
+            case "mark":
+                // Now check if the second token 1) exists, 2) is a number. Else, treat it as a new task.
+                if (userInputTokens.length == 2) { // check if there is only 1 argument after unmark command
+                    // check if it is a number
+                    if (isNumber(userInputTokens[1])) {
+                        int taskIndex = Integer.parseInt(userInputTokens[1]);
+                        new LocalFuncs().executeMark(taskIndex);
+                    }
+                }
+                else {
+                    new LocalFuncs().executeNone(new Task(userInput));
+                }
                 break;
             case "bye":
                 new LocalFuncs().goodByeAndExit();
@@ -67,6 +128,17 @@ public class Lindi {
             printSeparator();
             parseInputAndExecute(userInput);
             printSeparator();
+        }
+    }
+
+    private static boolean isNumber(String s) {
+        try {
+            Integer.valueOf(s);
+            System.out.println("true number");
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("false number");
+            return false;
         }
     }
     public static void main(String[] args) {
