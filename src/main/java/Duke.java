3d32b2668328e80575ea792cd3c84d9a7f8cac44
PallonCX <cheexiang03@gmail.com>
2024-01-26 01:02:10 +0800

    Modify Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,63 +20,142 @@ public class Duke {
         while (true) {
             String task = inputReader.nextLine();
             String[] splitedTask = task.split(" ");
+            String taskType = "";
 
-            String taskType = splitedTask[0];
+            // Empty command handler
+            try {
+                taskType = splitedTask[0];
+                if (taskType.isBlank()) {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                ui.print("Sorry, we are not sync enough to communicate through empty command.");
+                continue;
+            }
+
             boolean isEnd = false;
 
-            switch (taskType) {
-                case "bye": {
-                    isEnd = true;
-                    break;
-                }
-                case "list": {
-                    ui.list(storage);
-                    break;
-                }
-                case "mark": {
-                    storage.markDone(Integer.parseInt(splitedTask[1]) - 1);
-                    ui.mark(storage.getItem(Integer.parseInt(splitedTask[1]) - 1));
-                    break;
-                }
-                case "unmark": {
-                    storage.unmarkDone(Integer.parseInt(splitedTask[1]) - 1);
-                    ui.mark(storage.getItem(Integer.parseInt(splitedTask[1]) - 1));
-                    break;
-                }
-                case "todo": {
-                    Task newTask = new Todo(task);
-                    storage.add(newTask);
-                    ui.add(newTask, storage);
-                    break;
-                }
-                case "deadline": {
-                    // Find the keyword /by, and form description and due date
-                    String[] splitedBy = task.split(" /by ");
-                    String description = splitedBy[0].substring(9);
+            try {
+                switch (taskType) {
+                    case "bye": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length > 1) {
+                            throw new DukeException("bye");
+                        }
 
-                    // Create deadline task
-                    Task newTask = new Deadline(description, splitedBy[1]);
-                    storage.add(newTask);
-                    ui.add(newTask, storage);
-                    break;
-                }
-                case "event": {
-                    // Find the keyword /from and /to, and form description, from, and to
-                    String[] splitedFrom = task.split(" /from ");
-                    String description = splitedFrom[0].substring(6);
-                    String[] splitedTo = splitedFrom[1].split(" /to ");
+                        isEnd = true;
+                        break;
+                    }
+                    case "list": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length > 1) {
+                            throw new DukeException("list");
+                        }
 
-                    // Create event task
-                    Task newTask = new Event(description, splitedTo[0], splitedTo[1]);
-                    storage.add(newTask);
-                    ui.add(newTask, storage);
-                    break;
+                        ui.list(storage);
+                        break;
+                    }
+                    case "mark": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length != 2) {
+                            throw new DukeException("mark");
+                        }
+                        int index = 0;
+                        try {
+                            index = Integer.parseInt(splitedTask[1]) - 1;
+                        } catch (NumberFormatException e) {
+                            throw new DukeException("mark");
+                        }
+
+                        storage.markDone(index);
+                        ui.mark(storage.getItem(index));
+                        break;
+                    }
+                    case "unmark": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length != 2) {
+                            throw new DukeException("unmark");
+                        }
+                        int index = 0;
+                        try {
+                            index = Integer.parseInt(splitedTask[1]) - 1;
+                        } catch (NumberFormatException e) {
+                            throw new DukeException("unmark");
+                        }
+
+                        storage.unmarkDone(index);
+                        ui.mark(storage.getItem(index));
+                        break;
+                    }
+                    case "todo": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length == 1) {
+                            throw new DukeException("todo");
+                        }
+
+                        Task newTask = new Todo(task);
+                        storage.add(newTask);
+                        ui.add(newTask, storage);
+                        break;
+                    }
+                    case "deadline": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length == 1) {
+                            throw new DukeException("deadline");
+                        }
+
+                        // Split the string with /by
+                        String[] splitedBy = task.split(" /by ");
+
+                        // Incorrect command syntax handler
+                        if (splitedBy.length != 2 || splitedBy[0].length() <= 9
+                                || splitedBy[0].substring(9).isBlank()
+                                || splitedBy[1].isBlank()) {
+                            throw new DukeException("deadline");
+                        }
+
+                        // Create deadline task
+                        Task newTask = new Deadline(splitedBy[0].substring(9), splitedBy[1]);
+                        storage.add(newTask);
+                        ui.add(newTask, storage);
+                        break;
+                    }
+                    case "event": {
+                        // Incorrect command syntax handler
+                        if (splitedTask.length == 1) {
+                            throw new DukeException("event");
+                        }
+
+                        // Split the string with /from
+                        String[] splitedFrom = task.split(" /from ");
+
+                        // Incorrect command syntax handler
+                        if (splitedFrom.length != 2 || splitedFrom[0].length() <= 6
+                                || splitedFrom[0].substring(6).isBlank()
+                                || splitedFrom[1].isBlank()) {
+                            throw new DukeException("deadline");
+                        }
+
+                        // Split the string with /to
+                        String[] splitedTo = splitedFrom[1].split(" /to ");
+
+                        // Incorrect command syntax handler
+                        if (splitedTo.length != 2 || splitedTo[0].isBlank() || splitedTo[1].isBlank()) {
+                            throw new DukeException("deadline");
+                        }
+
+                        // Create event task
+                        Task newTask = new Event(splitedFrom[0].substring(6), splitedTo[0], splitedTo[1]);
+                        storage.add(newTask);
+                        ui.add(newTask, storage);
+                        break;
+                    }
+                    default: {
+                        ui.print("Syntax error, unknown command.");
+                    }
                 }
-                default: {
-                    Task newTask = new Task(task);
-                    storage.add(newTask);
-                    ui.add(newTask, storage);
-                }
+            } catch (DukeException e) {
+                ui.print(e.handle());
             }
 
             // Loop breaker check
