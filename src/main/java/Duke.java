2c3430ddfd12c374a4e63169a234c2053ac5959d
Emberlynn-Loo <E0960284@u.nus.edu>
2024-01-26 04:53:42 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("_______________________________________________________");
-        System.out.println("Hello! I'm TALKTOMEORILLDIE");
-        System.out.println("What can I do for you?");
-        System.out.println("_______________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_______________________________________________________");
+        System.out.println("    _______________________________________________________");
+        System.out.println("    Hello! I'm TALKTOMEORILLDIE");
+        System.out.println("    What can I do for you?");
+        System.out.println("    _______________________________________________________");
+
+        Scanner inputs = new Scanner(System.in); //this is the scanner obj
+
+        while (true) {
+            String echo = inputs.nextLine(); //this is getting the input
+
+            if (echo.equals("bye") || echo.equals("Bye")) {
+                System.out.println("    _______________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    _______________________________________________________");
+                break;
+            }
+
+            System.out.println("    _______________________________________________________");
+            System.out.println("    " + echo);
+            System.out.println("    _______________________________________________________");
+        }
     }
 }
