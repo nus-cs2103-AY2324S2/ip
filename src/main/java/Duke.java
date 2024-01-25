115bf1aec43511302cf2ecba4ab0625013e40339
Justin <justinlim60@gmail.com>
2024-01-25 14:04:05 +0800

    mark as done part 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>(100);
+        ArrayList<Task> list = new ArrayList<>(100);
         String message = "____________________________________________________________\n" +
                 "Hello! I'm Jux\n" +
                 "What can I do for you?\n";
@@ -17,14 +17,34 @@ public class Duke {
             if (input.equals("bye")) {
                 break;
             } else if(input.equals("list")) {
+                System.out.println("Here are the tasks in your list!");
                 for (int i = 0; i < list.size();i++) {
-                    int j = i+1;
-                    String listMessage = j + "." + list.get(i);
-                    System.out.println(listMessage);
+                    int j = i + 1;
+                    String listMessage = j + "." + "["+
+                            list.get(i).getStatusIcon() + "]" + list.get(i);
+                    System.out.println(listMessage + "\n");
                 }
+            } else if(input.startsWith("mark")) {
+                if (input.length() > 5 ) {
+                    String listStringNumber =  input.substring(5);
+                    int convertedToNumber = Integer.parseInt(listStringNumber) - 1;
+                    System.out.println(convertedToNumber);
+                    String doneness = list.get(convertedToNumber).toggleIsDone();
+                    // check if already marked then return error
+                    System.out.println("Nice! I've marked this task as done:" + "\n");
+                    System.out.println("[" + doneness + "] " + list.get(convertedToNumber));
+                }
+            } else if (input.startsWith("unmark")) {
+                String listStringNumber =  input.substring(7);
+                int convertedToNumber = Integer.parseInt(listStringNumber) - 1;
+                System.out.println(convertedToNumber);
+                String doneness = list.get(convertedToNumber).toggleIsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + doneness + "] " + list.get(convertedToNumber));
             } else {
-                list.add(input);
-                System.out.println("You entered: " + input);
+                Task task = new Task(input);
+                list.add(task);
+                System.out.println("You entered: " + task);
             }
 
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+    public String toggleIsDone() {
+        isDone = !isDone;
+        return getStatusIcon();
+    }
+    @Override
+    public String toString(){
+        return description;
+    }
+    //...
 }
