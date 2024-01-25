fb41fe80c1a2226c27bf2db5a3f785cc1109f894
Shauryan100 <shauryan100@gmail.com>
2024-01-25 07:09:48 -0800

    Level 0 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void exitMessage() {
+        String divider = "-----------------------------------------------------";
+        System.out.println(divider);
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(exitMessage);
+    }
+
+    public static void greeting() {
+        String divider = "-----------------------------------------------------";
+        System.out.println(divider);
+        String welcomeMessage = "Hello! I'm ShaunBot";
+        String greet = "What can I do for you?";
+        System.out.println(welcomeMessage);
+        System.out.println(greet);
+
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "-----------------------------------------------------";
+        Duke.greeting();
+
+        Duke.exitMessage();
     }
 }
