6f03c4ab897b22604eab2ec9c804bff780821ac7
dhlee03 <dhclearsky@gmail.com>
2024-01-25 11:19:49 +0800

    adds echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
 public class Duke {
+    private boolean isActive;
+
+    public Duke() {
+        this.isActive = true;
+    }
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n"
-                + "Hello! I'm FriendlyTool\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________"
+        Duke ft = new Duke();
+        ft.init();
+    }
+    public void init() {
+        System.out.println("    ____________________________________________________________\n"
+                + "    Hello! I'm FriendlyTool\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________"
         );
-        System.out.println("Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________" );
+        Scanner sc = new Scanner(System.in);
+        while (this.isActive) {
+            String input = sc.nextLine();
+            nextAction(input);
+        }
+        System.out.println("    ____________________________________________________________\n"
+                + "    Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________" );
+    }
+
+    private void nextAction(String input) {
+        if (input.equalsIgnoreCase("bye")) {
+            this.isActive = false;
+        } else {
+            System.out.println("    ____________________________________________________________\n"
+                    + "    "
+                    + input
+                    + "\n    ____________________________________________________________\n");
+        }
     }
 }
