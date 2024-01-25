5d110a81d692ad637d989ab48967c8a87e7aedfd
LIU JIAYAO <>
2024-01-25 17:39:59 +0800

    A-Enums

--- src/main/java/Jiayou.java
+++ src/main/java/Jiayou.java
@@ -5,6 +5,10 @@ public class Jiayou {
     private static final String LINE = "____________________________________________________________";
     private ArrayList<Task> taskList = new ArrayList<Task>();
 
+    private static enum CommandType {
+        TODO, DEADLINE, EVENT, LIST, DELETE, MARK, UNMARK, BYE
+    }
+
     private void printList() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < this.taskList.size(); i++) {
@@ -16,35 +20,36 @@ public class Jiayou {
     private void parse(String input) {
         try {
             String[] parts = input.split(" ", 2);
-            String command = parts[0];
+            String commandString = parts[0];
+            CommandType command = CommandType.valueOf(commandString.toUpperCase());
             String content = parts.length > 1 ? parts[1] : "";
 
             switch (command) {
-                case "list":
+                case LIST:
                     printList();
                     break;
-                case "mark":
+                case MARK:
                     if (content.isEmpty()) {
                         throw new ParseException("OOPS!!! I don't know which task to mark. Please add the index after the keyword mark!");
                     } else {
                         markTask(content);
                         break;
                     }
-                case "unmark":
+                case UNMARK:
                     if (content.isEmpty()) {
                         throw new ParseException("OOPS!!! I don't know which task to unmark. Please add the index after the keyword unmark!");
                     } else {
                         unmarkTask(content);
                         break;
                     }
-                case "delete":
+                case DELETE:
                     if (content.isEmpty()) {
                         throw new ParseException("OOPS!!! I don't know which task to delete. Please add the index after the keyword delete!");
                     } else {
                         deleteTask(content);
                         break;
                     }
-                case "todo":
+                case TODO:
                     if (content.isEmpty()) {
                         throw new ParseException("OOPS!!! The description of a todo cannot be empty. Please add a description after the keyword todo!");
                     } else {
@@ -55,7 +60,7 @@ public class Jiayou {
                         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
                         break;
                     }
-                case "deadline":
+                case DEADLINE:
                     String[] deadlineParts = content.split(" /by ");
                     Deadline newDeadline = new Deadline(deadlineParts[0], deadlineParts[1]);
                     taskList.add(newDeadline);
@@ -63,7 +68,7 @@ public class Jiayou {
                     System.out.println("  " + newDeadline);
                     System.out.println("Now you have " + taskList.size() + " tasks in the list.");
                     break;
-                case "event":
+                case EVENT:
                     String[] eventParts = content.split(" /from | /to ");
                     Event newEvent = new Event(eventParts[0], eventParts[1], eventParts[2]);
                     taskList.add(newEvent);
@@ -71,9 +76,9 @@ public class Jiayou {
                     System.out.println("  " + newEvent);
                     System.out.println("Now you have " + taskList.size() + " tasks in the list.");
                     break;
-                default:
-                    throw new ParseException("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
+        } catch (IllegalArgumentException e) {
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
