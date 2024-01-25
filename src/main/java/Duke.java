982af9211447a31d81648b5758cc058a0ba97dc7
jocelyn-soh <s.sohjoce@gmail.com>
2024-01-26 00:26:29 +0800

    Changed from Task[] to ArrayList<Task>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,30 +2,27 @@ import exceptions.DukeException;
 import exceptions.DukeTaskNoDescException;
 import exceptions.DukeUnknownTaskException;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static int i = 0;
 
-    public static void createList(Task[] listArr) {
+    public static void createList(ArrayList<Task> listArr) {
         System.out.println("Here are the tasks in your list:");
-        for (int j = 0; j < 100; j++) { // printing out all items in the list
-            if (listArr[j] == null) { // for early termination
-                break;
-            } else {
-                System.out.println(j + 1 + ". " + listArr[j].toString());
-            }
+        for (int j = 0; j < listArr.size(); j++) { // printing out all items in the list
+            System.out.println(j + 1 + ". " + listArr.get(j));
         }
     }
 
-    public static void createTodo(String userTxt, Task[] listArr) {
+    public static void createTodo(String userTxt, ArrayList<Task> listArr) {
         String test = userTxt.substring(4);
         try {
             if (test.strip().equals("")) {
                 throw new DukeTaskNoDescException();
             } else {
                 Task t = new ToDo(userTxt.substring(5));
-                listArr[Duke.i] = t; // add task to list
+                listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
                 Duke.i++;
@@ -36,7 +33,7 @@ public class Duke {
         }
     }
 
-    public static void createDeadline(String userTxt, Task[] listArr) {
+    public static void createDeadline(String userTxt, ArrayList<Task> listArr) {
         String test = userTxt.substring(8);
         try {
             if (test.strip().equals("")) {
@@ -46,7 +43,7 @@ public class Duke {
                 String desc = userTxt.substring(9, splitIdx - 1);
                 String by = userTxt.substring(splitIdx + 3);
                 Task t = new Deadline(desc, by);
-                listArr[Duke.i] = t; // add task to list
+                listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
                 Duke.i++;
@@ -57,7 +54,7 @@ public class Duke {
         }
     }
 
-    public static void createEvent(String userTxt, Task[] listArr) {
+    public static void createEvent(String userTxt, ArrayList<Task> listArr) {
         String test = userTxt.substring(5);
         try {
             if (test.strip().equals("")) {
@@ -69,7 +66,7 @@ public class Duke {
                 String frm = userTxt.substring(frmIdx + 6, toIdx);
                 String to = userTxt.substring(toIdx + 3);
                 Task t = new Event(desc, frm, to);
-                listArr[Duke.i] = t; // add task to list
+                listArr.add(t); // add task to list
                 System.out.println("Got it. I've added this task:");
                 System.out.println(t.toString());
                 Duke.i++;
@@ -86,17 +83,17 @@ public class Duke {
         System.out.println("-----------------------------------------------------------------------");
         System.out.println("-----------------------------------------------------------------------");
         Scanner sc = new Scanner(System.in);
-        Task[] listArr = new Task[100];
+        ArrayList<Task> listArr = new ArrayList<>();
         String userTxt = sc.nextLine();
         while (!userTxt.equals("bye")) {
             if (userTxt.equals("list")) {
                 createList(listArr);
             } else if (userTxt.startsWith("mark")) {
                 Integer taskNum = Integer.valueOf(userTxt.substring(5)) - 1;
-                listArr[taskNum].setDone();
+                listArr.get(taskNum).setDone();
             } else if (userTxt.startsWith("unmark")) {
                 Integer taskNum = Integer.valueOf(userTxt.substring(7)) - 1;
-                listArr[taskNum].setUndone();
+                listArr.get(taskNum).setUndone();
             } else {
                 if (userTxt.startsWith("todo")) {
                     createTodo(userTxt, listArr);
