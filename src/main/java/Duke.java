a1e560286b2dcfc1d6d9e37eb4f0d4f946b7951e
Rahul <sithanathan.rahul@gmail.com>
2024-01-25 23:48:45 +0800

    Level-5 Code!

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,63 +19,106 @@ public class Duke {
         System.out.println(layer("Hello! I'm \n" + logo + "What can I do for you?"));
 
         while (true) {
-            String input = scanner.nextLine(); 
-
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println(layer("Bye. Hope to see you again soon!"));
-                break; 
-            }
-            else if (input.equalsIgnoreCase("list")) {
-                String result = "";
-                int count = 1;
-                for (Task s : inventory) {
-                    result += count + ". " + s.toString() + "\n";
-                    count++;
+            try {
+                String input = scanner.nextLine(); 
+                if (input.equalsIgnoreCase("bye")) {
+                    System.out.println(layer("Bye. Hope to see you again soon!"));
+                    break; 
                 }
-                System.out.println(layer(result));
-                //if marked
-            } 
-            else if (input.substring(0,4).equalsIgnoreCase("mark")) {
-                int index = Integer.parseInt(input.substring(5));
-                inventory.get(index - 1).mark();
-                String temp = "Nice! I've marked this task as done: \n";
-                temp += inventory.get(index - 1).toString();
-                System.out.println(layer(temp));
-                //if unmarked
-            } 
-            else if (input.substring(0, 6).equalsIgnoreCase("unmark")) {
-                int index = Integer.parseInt(input.substring(7));
-                inventory.get(index - 1).unmark();
-                String temp = "OK, I've marked this task as not done yet: \n";
-                temp += inventory.get(index - 1).toString();
-                System.out.println(layer(temp));
-            } 
-            else if (input.substring(0, 4).equalsIgnoreCase("todo")) {
-                String task = input.substring(5);
-                num++;
-                inventory.add(new ToDo(task, num));
-                String temp = "Got it. I've added this task: \n";
-                temp += " " + inventory.get(inventory.size() - 1).toString();
-                temp += "\nNow you have " + num + " tasks in the list.";
-                System.out.println(layer(temp));
-            }
-            else if (input.substring(0, 8).equalsIgnoreCase("deadline")) {
-                String[] parts = input.substring(9).split("/");
-                num++;
-                inventory.add(new Deadlines(parts[0], parts[1], num));
-                String temp = "Got it. I've added this task: \n";
-                temp += " " + inventory.get(inventory.size() - 1).toString();
-                temp += "\nNow you have " + num + " tasks in the list.";
-                System.out.println(layer(temp));
-            }
-            else if (input.substring(0, 5).equalsIgnoreCase("event")) {
-                String[] parts = input.substring(6).split("/");
-                num++;
-                inventory.add(new Events(parts[0], parts[1], parts[2], num));
-                String temp = "Got it. I've added this task: \n";
-                temp += " " + inventory.get(inventory.size() - 1).toString();
-                temp += "\nNow you have " + num + " tasks in the list.";
-                System.out.println(layer(temp));
+                else if (input.equalsIgnoreCase("list")) {
+                    if (!input.trim().equals("list")) {
+                        throw new DukeException("OOPS!!! This is an invalid call of list command.");
+                    } else {
+                        String result = "";
+                        int count = 1;
+                        for (Task s : inventory) {
+                            result += count + ". " + s.toString() + "\n";
+                            count++;
+                        }
+                        System.out.println(layer(result));
+                        //if marked
+                    }
+                } 
+                else if (input.startsWith("mark")) {
+                    if (input.trim().equals("mark")) {
+                        throw new DukeException("OOPS!!! Invalid Command, highlight which task to mark");
+                    } else {
+                        int index = Integer.parseInt(input.substring(5));
+                        inventory.get(index - 1).mark();
+                        String temp = "Nice! I've marked this task as done: \n";
+                        temp += inventory.get(index - 1).toString();
+                        System.out.println(layer(temp));
+                        //if unmarked
+                    }
+                } 
+                else if (input.startsWith("unmark")) {
+                    if (input.trim().equals("unmark")) {
+                        throw new DukeException("OOPS!!! Invalid Command, highlight which task to unmark");
+                    } else {
+                        int index = Integer.parseInt(input.substring(7));
+                        inventory.get(index - 1).unmark();
+                        String temp = "OK, I've marked this task as not done yet: \n";
+                        temp += inventory.get(index - 1).toString();
+                        System.out.println(layer(temp));
+                    }
+                } 
+                else if (input.startsWith("todo")) {
+                    if (input.trim().equals("todo")) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    } else {
+                        String task = input.substring(5);
+                        num++;
+                        inventory.add(new ToDo(task, num));
+                        String temp = "Got it. I've added this task: \n";
+                        temp += " " + inventory.get(inventory.size() - 1).toString();
+                        temp += "\nNow you have " + num + " tasks in the list.";
+                        System.out.println(layer(temp));
+                    }
+                }
+                else if (input.startsWith("deadline")) {
+                    if (input.trim().equals("deadline")) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    } 
+                    else if (!input.contains("/")) {
+                        throw new DukeException("OOPS!!! Your deadline command's format is invalid.");
+                    } 
+                    else if (input.substring(9).split("/").length < 2) {
+                        throw new DukeException("OOPS!!! Your deadline command's format is invalid.");
+                    }
+                     else {
+                        String[] parts = input.substring(9).split("/");
+                        num++;
+                        inventory.add(new Deadlines(parts[0], parts[1], num));
+                        String temp = "Got it. I've added this task: \n";
+                        temp += " " + inventory.get(inventory.size() - 1).toString();
+                        temp += "\nNow you have " + num + " tasks in the list.";
+                        System.out.println(layer(temp));
+                    }
+                }
+                else if (input.startsWith("event")) {
+                    if (input.trim().equals("event")) {
+                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                    }
+                    else if (!input.contains("/")) {
+                        throw new DukeException("OOPS!!! Your event command's format is invalid.");
+                    } else if (input.substring(6).split("/").length < 3) {
+                        throw new DukeException("OOPS!!! Your event command's format is invalid.");
+                    }  
+                    else { 
+                        String[] parts = input.substring(6).split("/");
+                        num++;
+                        inventory.add(new Events(parts[0], parts[1], parts[2], num));
+                        String temp = "Got it. I've added this task: \n";
+                        temp += " " + inventory.get(inventory.size() - 1).toString();
+                        temp += "\nNow you have " + num + " tasks in the list.";
+                        System.out.println(layer(temp));
+                    }
+                }
+                else {
+                    System.out.println(layer("OOPS!!! I'm sorry, but that's an invalid command :-("));
+                }
+            } catch (DukeException e) {
+                System.out.println(layer(e.getMessage()));
             }
         }
 
@@ -87,3 +130,4 @@ public class Duke {
         return line + "\n" + s + "\n" + line; 
     }
 }
+
