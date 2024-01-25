3b5323e06a1c59f310b845037eaa984bec7c40fe
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-01-25 21:20:35 +0800

    Level-0

---
+++ src/main/java/Eve.java
@@ -0,0 +1,7 @@
+public class Eve {
+    public static void main(String[] args) {
+        System.out.println(" Hello! I'm Eve");
+        System.out.println(" What can I do for you?");
+        System.out.println(" Bye. Hope to see you again soon !");
+    }
+}
