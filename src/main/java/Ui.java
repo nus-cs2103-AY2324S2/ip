540d7b37df34fa39969af31a05eb3db63d544354
JustWeiHao <e0960531@u.nus.edu>
2024-01-25 17:45:22 +0800

    Modify Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,18 +1,50 @@
+import java.util.Scanner;
+
 public abstract class Ui {
-    private static void horizontalLines() {
-        System.out.println("----------------------------------------------------------------");
+    private static final String startLine = "----------------"
+            + "------------[Rep"
+            + "ort]------------"
+            + "----------------\n";
+
+    private static final String endLine = "----------------"
+            + "------------[End"
+            + "ing]------------"
+            + "----------------\n";
+    private static void printStartLine() {
+        System.out.print(Ui.startLine);
+    }
+
+    private static void printEndLine() {
+        System.out.print(Ui.endLine);
     }
     public static void start() {
-        Ui.horizontalLines();
+        Ui.printStartLine();
         final String greetings = String.format( "Hello! I'm %s\n"
                 + "What can I do for you?\n", Duke.botName);
         System.out.print(greetings);
-        Ui.horizontalLines();
+        Ui.printEndLine();
     }
 
     public static void end() {
-        final String endings = "Bye. Hope to see you again soon!";
+        Ui.printStartLine();
+        final String endings = "Bye. It is an honor to serve you.\n"
+                + "Hope to see you again soon!";
         System.out.println(endings);
-        Ui.horizontalLines();
+        Ui.printEndLine();
+    }
+
+    public static void readCommand() {
+        Scanner sc = new Scanner(System.in);
+        boolean terminate = false;
+        while (true) {
+            final String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                Ui.printStartLine();
+                System.out.println(command);
+                Ui.printEndLine();
+            }
+        }
     }
 }
