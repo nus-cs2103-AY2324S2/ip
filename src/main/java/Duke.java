e6ea8a3c53493c80c6fe747ed8f865b850a0022d
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-01-27 02:17:45 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,16 +27,14 @@ public class Duke {
                         " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
         String barrier = "____________________________________________________________";
-        Boolean exit = true;
         ArrayList<Task> todoList = new ArrayList<Task>();
         System.out.println(Greeting);
         Scanner sc = new Scanner(System.in);
-        while (exit) {
-            String userInput = sc.nextLine();
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            userInput = sc.nextLine();
             String[] inputList = userInput.split(" ", 2);
-            if (userInput.equals("bye")) {
-                exit = false;
-            } else if (userInput.equals("list")) {
+            if (userInput.equals("list")) {
                 System.out.println(barrier);
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < todoList.size(); i++) {
@@ -44,54 +42,122 @@ public class Duke {
                 }
                 System.out.println(barrier);
             } else if (inputList[0].equals("mark")) {
-                int position = Integer.parseInt(inputList[1]);
-                System.out.println(barrier);
-                System.out.println("Nice! I've marked this task as done:");
-                todoList.get(position-1).markAsDone();
-                System.out.println(todoList.get(position-1).toString());
-                System.out.println(barrier);
+                try {
+                    int position = Integer.parseInt(inputList[1]);
+                    Task currentTask = todoList.get(position-1);
+                    System.out.println(barrier);
+                    System.out.println("Nice! I've marked this task as done:");
+                    todoList.get(position-1).markAsDone();
+                    System.out.println(currentTask.toString());
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("Check how many items are in the list again. The number you gave is too high");
+                    System.out.println("Can't mark an item not in the list");
+                    System.out.println("The format to mark a task is: mark (task list number)");
+                    System.out.println(barrier);
+                } catch (NumberFormatException e) {
+                    System.out.println(barrier);
+                    System.out.println("Sorry, I'm only smart enough to find the task based on numbers.");
+                    System.out.println("Please give a number. If you refuse, too bad, this is all I can do.");
+                    System.out.println(barrier);
+                }
             } else if (inputList[0].equals("unmark")) {
-                int position = Integer.parseInt(inputList[1]);
-                System.out.println(barrier);
-                System.out.println("OK, I've marked this task as not done yet:");
-                todoList.get(position-1).unmarkAsDone();
-                System.out.println(todoList.get(position-1).toString());
-                System.out.println(barrier);
+                try {
+                    int position = Integer.parseInt(inputList[1]);
+                    System.out.println(barrier);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    todoList.get(position-1).unmarkAsDone();
+                    System.out.println(todoList.get(position-1).toString());
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("Check how many items are in the list again. The number you gave is too high.");
+                    System.out.println("Can't unmark an item not in the list");
+                    System.out.println("The format to unmark a task is: unmark (task list number)");
+                    System.out.println(barrier);
+                } catch (NumberFormatException e) {
+                    System.out.println(barrier);
+                    System.out.println("Sorry, I'm only smart enough to find the task based on numbers.");
+                    System.out.println("Please give a number. If you refuse, too bad, this is all I can do.");
+                    System.out.println(barrier);
+                }
             } else if (inputList[0].equals("todo")) {
-                Task userTask = new Todo(inputList[1]);
-                todoList.add(userTask);
-                System.out.println(barrier);
-                System.out.println(userTask.toString());
-                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
-                System.out.println(barrier);
+                try {
+                    Task userTask = new Todo(inputList[1]);
+                    todoList.add(userTask);
+                    System.out.println(barrier);
+                    System.out.println(userTask.toString());
+                    System.out.println("Now you have " + todoList.size() + " tasks in the list.");
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("Sorry pal, but your description is empty.");
+                    System.out.println("Please redo the command and remember to add a description of the action");
+                    System.out.println("The format to schedule a todo task is: todo (Description)");
+                    System.out.println(barrier);
+                }
             } else if (inputList[0].equals("deadline")) {
-                String[] differentParts = inputList[1].split("/");
-                String[] deadLine = differentParts[1].split(" ", 2);
-                Task userTask = new Deadline(differentParts[0],deadLine[1]);
-                todoList.add(userTask);
-                System.out.println(barrier);
-                System.out.println(userTask.toString());
-                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
-                System.out.println(barrier);
+                try {
+                    String[] differentParts = inputList[1].split("/");
+                    String[] deadLine = differentParts[1].split(" ", 2);
+                    Task userTask = new Deadline(differentParts[0],deadLine[1]);
+                    todoList.add(userTask);
+                    System.out.println(barrier);
+                    System.out.println(userTask.toString());
+                    System.out.println("Now you have " + todoList.size() + " tasks in the list.");
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("Oh, you forgot to indicate when is the deadline or maybe you forgot the description.");
+                    System.out.println("Please redo the command and remember to add the necessary information.");
+                    System.out.println("The format to schedule a deadline is: deadline (Description) /by (Deadline Date + time)");
+                    System.out.println(barrier);
+                }
             } else if (inputList[0].equals("event")) {
-                String[] differentParts = inputList[1].split("/");
-                String[] startDate = differentParts[1].split(" ", 2);
-                String[] endDate = differentParts[2].split(" ", 2);
-                Task userTask = new Event(differentParts[0],startDate[1],endDate[1]);
-                todoList.add(userTask);
-                System.out.println(barrier);
-                System.out.println(userTask.toString());
-                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
-                System.out.println(barrier);
+                try {
+                    String[] differentParts = inputList[1].split("/");
+                    String[] startDate = differentParts[1].split(" ", 2);
+                    String[] endDate = differentParts[2].split(" ", 2);
+                    Task userTask = new Event(differentParts[0],startDate[1],endDate[1]);
+                    todoList.add(userTask);
+                    System.out.println(barrier);
+                    System.out.println(userTask.toString());
+                    System.out.println("Now you have " + todoList.size() + " tasks in the list.");
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("You forgot to provide either a description, an start date or an end date for this event.");
+                    System.out.println("Sorry, but mind reading is not installed in me yet.");
+                    System.out.println("Please redo the command and remember to add the necessary information.");
+                    System.out.println("The format to schedule a event is: event (Description) /from (Start date + time) /to (End date + time)");
+                    System.out.println(barrier);
+                }
+
             } else if (inputList[0].equals("delete")) {
-                int position = Integer.parseInt(inputList[1]);
-                Task chosenTask = todoList.get(position-1);
-                System.out.println(barrier);
-                System.out.println("Noted. I've removed this task:");
-                System.out.println(chosenTask.toString());
-                todoList.remove(position-1);
-                System.out.println("Now you have " + todoList.size() + " tasks in the list.");
-                System.out.println(barrier);
+                try {
+                    int position = Integer.parseInt(inputList[1]);
+                    Task chosenTask = todoList.get(position-1);
+                    System.out.println(barrier);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(chosenTask.toString());
+                    todoList.remove(position-1);
+                    System.out.println("Now you have " + todoList.size() + " tasks in the list.");
+                    System.out.println(barrier);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(barrier);
+                    System.out.println("The number you gave exceeds how many items is in the list.");
+                    System.out.println("Can't delete an item not in the list. Please try again.");
+                    System.out.println("The format to delete a task is: delete (task list number)");
+                    System.out.println(barrier);
+                } catch (NumberFormatException e) {
+                    System.out.println(barrier);
+                    System.out.println("Sorry, I'm only smart enough to find the task based on numbers.");
+                    System.out.println("Please give a number. If you refuse, too bad, this is all I can do.");
+                    System.out.println("The format to delete a task is: delete (task list number)");
+                    System.out.println(barrier);
+                }
+
             }
 //            else {
 //                Task userTask = new Task(inputList[0] + " " + inputList[1]);
