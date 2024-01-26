2eaab1e163fc70ce873a86c5e9ed96295409d080
jimseah0116 <134794564+jimseah-0116@users.noreply.github.com>
2024-01-26 21:57:12 +0800

    updates for Level-2

--- src/main/java/ChatBro.java
+++ src/main/java/ChatBro.java
@@ -10,15 +10,25 @@ public class ChatBro {
                 "  /_/\\___/ (_)\n\n" +
                 "I'm ChatBro!\n" +
                 "What can I do for you bro?\n" +
-                "Available commands: list, blah, bye\n" +
+                "Use the available commands: list, blah, bye\n" +
+                "or type anything else to store it in your list bro.\n" +
                 "_________________________\n");
 
         boolean isQuit = false;
+        String[] storedList = new String[100];
         while (!isQuit) {
-            String input = sc.next();
+            String input = sc.nextLine();
             switch (input) {
                 case "list":
-                    System.out.println("This is the list reply bro.\n");
+                    System.out.println("Here are the items in your list bro:\n" +
+                            "_________________________");
+                    for (int i = 0; i < 100; i++) {
+                        if (storedList[i] == null) {
+                            break;
+                        }
+                        System.out.println(i + 1 + ". " + storedList[i]);
+                    }
+                    System.out.println("_________________________\n");
                     break;
                 case "blah":
                     System.out.println("This is the blah reply bro.\n");
@@ -29,8 +39,16 @@ public class ChatBro {
                             "Bye bro! Hope to see you again soon!\n" +
                             "_________________________\n");
                     break;
-                default:
-                    System.out.println("I don't understand that command bro.\n");
+                default: // Other input will be stored into storedList
+                    for (int i = 0; i < 100; i++) {
+                        if (storedList[i] == null) {
+                            storedList[i] = input;
+                            break;
+                        }
+                    }
+                    System.out.println("_________________________\n" +
+                            "Ok bro, I've added: " + input + "\n" +
+                            "_________________________\n");
                     break;
             }
         }
