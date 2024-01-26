6d2eb91b2b9a8eed65ca46dd87f7944f0f4d69d7
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-01-26 23:15:39 +0800

    add date to tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,9 +5,8 @@ public class Duke {
     public static void main(String[] args) throws DukeException {
         Scanner scanner = new Scanner(System.in);
 
-        Storage storage = new Storage("./test.txt");
+        Storage storage = new Storage("./data/test.txt");
         ArrayList<Task> items = storage.readData();
-        int itemCount = items.size();
         boolean isValid = true;
 
         System.out.println("Hello! I'm GuanGuanBot!");
@@ -17,9 +16,7 @@ public class Duke {
         while (isValid) {
             String command = scanner.nextLine();
             try {
-                Pair<Boolean, Integer> pair = handleInput(command, items, itemCount);
-                isValid = pair.first();
-                itemCount = pair.second();
+                isValid = handleInput(command, items);
                 storage.saveData(items);
             } catch (DukeException e) {
                 System.out.printf("[!] %s\n", e.getMessage());
@@ -28,28 +25,40 @@ public class Duke {
         }
     }
 
-    private static Pair<Boolean, Integer> handleInput(String command, ArrayList<Task> items, int itemCount) throws DukeException {
+    private static boolean handleInput(String command, ArrayList<Task> items) throws DukeException {
         if (command.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
-            return Pair.of(false, itemCount);
+            return false;
         } else if (command.equals("list")) {
-            System.out.println("Here are the tasks in your list:");
-            for (int i=0; i<itemCount; i++) {
-                System.out.printf("%s. %s%n", i+1, items.get(i));
+            if (items.isEmpty()) {
+                System.out.println("You have no tasking available.");
+            } else {
+                System.out.println("Here are the tasks in your list:");
+                for (int i=0; i<items.size(); i++) {
+                    System.out.printf("%s. %s%n", i+1, items.get(i));
+                }
             }
         } else if (command.startsWith("mark")) {
-            int index = Integer.parseInt(command.split(" ")[1]) - 1;
-            items.get(index).markDone();
+            try {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                items.get(index).markDone();
 
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println(items.get(index));
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(items.get(index));
+            } catch (IndexOutOfBoundsException e) {
+                throw new DukeException("Invalid task ID to mark");
+            }
 
         } else if (command.startsWith("unmark")) {
-            int index = Integer.parseInt(command.split(" ")[1]) - 1;
-            items.get(index).unmarkDone();
+            try {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                items.get(index).unmarkDone();
 
-            System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println(items.get(index));
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(items.get(index));
+            } catch (IndexOutOfBoundsException e) {
+                throw new DukeException("Invalid task ID to unmark");
+            }
         }
 
         else if (command.startsWith("todo")) {
@@ -59,52 +68,56 @@ public class Duke {
 
             System.out.println("Got it. I've added this task:");
 
-            Todo todo = new Todo(command.substring(5, command.length()));
+            Todo todo = new Todo(command.substring(5));
 
             items.add(todo);
-            itemCount++;
 
             System.out.println(todo);
-            System.out.printf("Now you have %s tasks in the list.%n", itemCount);
+            System.out.printf("Now you have %s tasks in the list.%n", items.size());
 
         } else if (command.startsWith("deadline")) {
             if (command.length() <= 9) {
                 throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
             }
 
-            System.out.println("Got it. I've added this task:");
+            try{
+                String[] splittedCommand = command.split(" /by ");
+                String task = splittedCommand[0].substring(9);
+                String by = splittedCommand[1];
 
-            String[] splittedCommand = command.split(" /by ");
-            String task = splittedCommand[0].substring(9, splittedCommand[0].length());
-            String by = splittedCommand[1];
+                Deadline deadline = new Deadline(task, Utils.convertStringToDateTime(by));
+                items.add(deadline);
 
-            Deadline deadline = new Deadline(task, by);
-            items.add(deadline);
-            itemCount++;
-
-            System.out.println(deadline);
-            System.out.printf("Now you have %s tasks in the list.%n", itemCount);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(deadline);
+                System.out.printf("Now you have %s tasks in the list.%n", items.size());
+            } catch (IndexOutOfBoundsException e) {
+                throw new DukeException("Invalid deadline date. Use /by");
+            }
 
         } else if (command.startsWith("event")) {
             if (command.length() <= 6) {
                 throw new DukeException("OOPS!!! The description of a event cannot be empty.");
             }
 
-            System.out.println("Got it. I've added this task:");
+            try {
+                String[] splittedCommand = command.split(" /from ");
+                String task = splittedCommand[0].substring(6);
 
-            String[] splittedCommand = command.split(" /from ");
-            String task = splittedCommand[0].substring(6, splittedCommand[0].length());
+                String[] splittedTime = splittedCommand[1].split(" /to ");
+                String from = splittedTime[0];
+                String to = splittedTime[1];
 
-            String[] splittedTime = splittedCommand[1].split(" /to ");
-            String from = splittedTime[0];
-            String to = splittedTime[1];
+                Event event = new Event(task, Utils.convertStringToDateTime(from), Utils.convertStringToDateTime(to));
+                items.add(event);
 
-            Event event = new Event(task, from, to);
-            items.add(event);
-            itemCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(event);
+                System.out.printf("Now you have %s tasks in the list.%n", items.size());
+            } catch (IndexOutOfBoundsException e) {
+                throw new DukeException("Invalid event date. Use /from and /to");
+            }
 
-            System.out.println(event);
-            System.out.printf("Now you have %s tasks in the list.%n", itemCount);
         } else if (command.startsWith("delete")) {
             if (command.length() <= 7) {
                 throw new DukeException("OOPS!!! Task ID cannot be empty.");
@@ -114,13 +127,12 @@ public class Duke {
 
             int index = Integer.parseInt(command.split(" ")[1]) - 1;
             Task task = items.remove(index);
-            itemCount -= 1;
             System.out.println(task);
 
-            System.out.printf("Now you have %s tasks in the list.%n", itemCount);
+            System.out.printf("Now you have %s tasks in the list.%n", items.size());
         } else {
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
-        return Pair.of(true, itemCount);
+        return true;
     }
 }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Utils {
+    public static LocalDate convertStringToDateTime(String datetime) throws DukeException {
+        int lenDatetime = datetime.length();
+        LocalDate localDate;
+        // date format: 2019-12-01
+        if (lenDatetime == 10) {
+            localDate = LocalDate.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } else {
+            throw new DukeException("Invalid datetime format!");
+        }
+        return localDate;
+    }
+
+    public static String printTime(LocalDate datetime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return datetime.format(formatter);
+    }
+
+    public static String convertDateTimeToString(LocalDate datetime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return datetime.format(formatter);
+    }
+}
