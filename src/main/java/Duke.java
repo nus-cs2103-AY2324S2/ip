30db664515c471c00d6b2cac5d0c553d161b3f27
sarjinius <sarjielijahbona0923@gmail.com>
2024-01-26 15:19:48 +0800

    Exits with goodbye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Virtue \nWhat can I do for you? \n");
+        System.out.println("Bye. Hope to see you again soon! \n");
+
     }
 }
