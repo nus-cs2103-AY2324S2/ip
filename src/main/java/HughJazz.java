6224f9a7c8642c1789f00f82fc3603cfcfa6cc84
Greg-Tan <e0969217@u.nus.edu>
2024-01-27 01:56:29 +0800

    rename, greet, exit

---
+++ src/main/java/HughJazz.java
@@ -0,0 +1,14 @@
+public class HughJazz {
+    public static void main(String[] args) {
+        greeting();
+        ending();
+    }
+
+    public static void greeting() {
+        System.out.println("Hello! I'm HughJazz");
+        System.out.println("What can I do for you?");
+    }
+    public static void ending() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
