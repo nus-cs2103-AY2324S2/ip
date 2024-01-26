fb430c75fab0427b790c313c0973d2df990fbd4f
JustWeiHao <e0960531@u.nus.edu>
2024-01-26 13:30:11 +0800

    Modify Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,14 @@ public class Duke {
     public static final TaskList taskList = new TaskList();
     public static void main(String[] args) {
         String logo = "\n"
-                + "  _____                _                   _ \n"
+                + "  _____                _                   _\n"
                 + " |  __ \\              | |                 | |\n"
                 + " | |__) | __ _  _ __  | |__    __ _   ___ | |\n"
                 + " |  _  / / _` || '_ \\ | '_ \\  / _` | / _ \\| |\n"
                 + " | | \\ \\| (_| || |_) || | | || (_| ||  __/| |\n"
                 + " |_|  \\_\\\\__,_|| .__/ |_| |_| \\__,_| \\___||_|\n"
-                + "               | |                           \n"
-                + "               |_|                           \n";
+                + "               | |\n"
+                + "               |_|\n";
         System.out.println(logo);
         Ui.start();
         Ui.readCommand();
