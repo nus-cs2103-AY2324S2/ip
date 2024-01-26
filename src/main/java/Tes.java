66adbf726e3bfa43a643fc8ca748c7f6b00cb6cb
KSSWSept20 <wongxucheng12345@gmail.com>
2024-01-26 19:35:59 +0800

    Changed name from Duke to Tes

---
+++ src/main/java/Tes.java
@@ -0,0 +1,16 @@
+public class Tes {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "_______________________________________________________________\n";
+        System.out.println(line +
+                "Hello! I'm Tes\n" +
+                "What can I do for you?\n" +
+                line +
+                "Bye. Hope to see you again soon!\n" +
+                line);
+    }
+}
