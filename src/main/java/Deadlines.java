f107358a09d6fb9e7941757ed9020f9ae0efa7a6
wolffe88 <yonglian1845@gmail.com>
2024-01-26 12:10:12 +0800

    dates and times

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,14 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class Deadlines extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
-    public Deadlines(String description, String by) {
+    public Deadlines(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
