a268be2ed7a382d5e58b5ca2b194a2108effa433
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2024-01-26 18:16:53 +0800

    Added Task class and Mark/Unmark Task feature

--- src/main/java/Tam.java
+++ src/main/java/Tam.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Tam {
     static String dividerText = "____________________________________________________________\n";
     static Scanner scannerObj = new Scanner(System.in);
-    static String[] taskList = new String[100];
+    //static String[] taskList = new String[100];
+
+    static ArrayList<Task>  taskList = new ArrayList<>();
     static int numTasks = 0;
     public static void main(String[] args) {
         Tam.greet();
@@ -35,19 +38,62 @@ public class Tam {
         }
         // list tasks command
         else if (input.toLowerCase().equals("list")) {
-            for (int i = 0; i < numTasks; i++) {
-                System.out.println((i+1) + ". " + taskList[i]);
+            Tam.listTasks();
+            return 1;
+        }
+        // mark or unmark a task
+        else if (input.split(" ")[0].toLowerCase().equals("mark") ||
+                input.split(" ")[0].toLowerCase().equals("unmark")) {
+            String command = input.split(" ")[0].toLowerCase();
+            int taskNum = Integer.parseInt(input.split(" ")[1]);
+            switch (command) {
+                case "mark":
+                    Tam.markTaskDone(taskNum);
+                    break;
+                case "unmark":
+                    Tam.markTaskUndone(taskNum);
             }
-            System.out.print(dividerText);
             return 1;
         }
         // add task command
         else {
-            System.out.println("Added: " + input);
-            taskList[numTasks] = input;
-            numTasks++;
-            System.out.print(dividerText);
+            Tam.addTask(input);
             return 1;
         }
+    }
+
+    public static void addTask(String taskDescription) {
+        System.out.println("Added: " + taskDescription);
+        taskList.add(new Task(taskDescription));
+        numTasks++;
+        System.out.print(dividerText);
+    }
+
+    public static void listTasks() {
+        for (int i = 0; i < numTasks; i++) {
+            Task thisTask = taskList.get(i);
+            System.out.println((i+1) + ".[" + thisTask.getStatusIcon() + "] " + thisTask.getDescription());
+        }
+        System.out.print(dividerText);
+    }
+
+    public static void markTaskDone(int taskNum) {
+        Task thisTask = taskList.get(taskNum-1);
+        thisTask.markDone();
+        String markedDoneText = "Nice! I've marked this task as done:\n";
+        String taskText = "    [" + thisTask.getStatusIcon() + "] " + thisTask.getDescription() + "\n";
+        System.out.print(markedDoneText);
+        System.out.print(taskText);
+        System.out.print(dividerText);
+    }
+
+    public static void markTaskUndone(int taskNum) {
+        Task thisTask = taskList.get(taskNum-1);
+        thisTask.markUndone();
+        String markedUndoneText = "Ok, i've marked this task as not done yet:\n";
+        String taskText = "    [" + thisTask.getStatusIcon() + "] " + thisTask.getDescription() + "\n";
+        System.out.print(markedUndoneText);
+        System.out.print(taskText);
+        System.out.print(dividerText);
     }
 }
