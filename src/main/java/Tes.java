57d12ff8f25490fa1d5af2d078d5a6d551dbece8
KSSWSept20 <wongxucheng12345@gmail.com>
2024-01-26 20:02:36 +0800

    Echo

--- src/main/java/Tes.java
+++ src/main/java/Tes.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
 public class Tes {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "_______________________________________________________________\n";
+        String line = "    _______________________________________________________________\n";
+        Scanner scanner = new Scanner(System.in);
         System.out.println(line +
-                "Hello! I'm Tes\n" +
-                "What can I do for you?\n" +
-                line +
-                "Bye. Hope to see you again soon!\n" +
+                "    Hello! I'm Tes\n" +
+                "    What can I do for you?\n" +
                 line);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(line
+                        + "    Bye. Hope to see you again soon!\n"
+                        + line);
+                break;
+            }
+
+            System.out.println(line
+                    + "    " + command + "\n"
+                    + line);
+        }
     }
 }
