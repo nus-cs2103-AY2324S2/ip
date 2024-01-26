de20497e7669c98a09913c814ad72231c7e7dc0b
H4mes <e0960344@u.nus.edu>
2024-01-26 21:10:07 +0800

    Level-0 first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _  _   __    ____  ____ \n" +
+                "( \\/ ) /__\\  (  _ \\(  _ \\\n" +
+                " \\  / /(__)\\  )   / )   /\n" +
+                " (__)(__)(__)(_)\\_)(_)\\_)\n";
+
+        System.out.println(logo);
+        printDivider(90);
+        System.out.println("Ahoy! I be Yarr \nWhat be yer command, me heartie?");
+        printDivider(90);
+        System.out.println("Fair winds to ye, me hearty! May the tide carry ye safely until our paths cross again.");
+        printDivider(90);
+
+    }
+    private static void printDivider(int length){
+        char divider = 0x2500 ;
+        for (int i = 0; i < length; i++) {
+            System.out.print(divider);
+        }
+        System.out.println();
     }
 }
