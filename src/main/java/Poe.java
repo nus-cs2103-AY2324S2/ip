db8159abe97a9d4501d67f9d1dfea7eedb22b6c2
leepoeaik <poeaik.lee@gmail.com>
2024-01-26 21:42:05 +0800

    Level-1

--- src/main/java/Poe.java
+++ src/main/java/Poe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Poe {
     String line = "=================================================";
     public void greetings(){
@@ -10,10 +12,25 @@ public class Poe {
         System.out.println(byebye);
     }
 
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String str = sc.nextLine();
+            if (str.toLowerCase().equals("bye")){
+                bye();
+                break;
+            }else{
+                System.out.println(line+"\n"+str+"\n"+line);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Poe poe1 = new Poe();
+        Scanner sc = new Scanner(System.in);
         poe1.greetings();
-        poe1.bye();
+        poe1.input();
 
     }
 }
