386efcc82c76443fb253300bfb3a776d2b62df0c
Greg-Tan <e0969217@u.nus.edu>
2024-01-27 02:49:13 +0800

    Add, List

--- src/main/java/HughJazz.java
+++ src/main/java/HughJazz.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class HughJazz {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
     public static void main(String[] args) {
         greeting();
         Scanner scn = new Scanner(System.in);
         String userInput;
 
         while (true) {
-            userInput = scn.nextLine();
+            userInput = scn.nextLine().trim();
 
             if("bye".equalsIgnoreCase(userInput)) {
                 break;
+            } else if("list".equalsIgnoreCase(userInput)) {
+                printTasks();
+            } else{
+                addTask(userInput);
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
         }
         ending();
         scn.close();
@@ -23,5 +30,16 @@ public class HughJazz {
     }
     public static void ending() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
+        }
+    }
+    private static void printTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
     }
 }
