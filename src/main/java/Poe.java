b28eff6ebadf907b699e3198646bfc21961d992a
leepoeaik <poeaik.lee@gmail.com>
2024-01-26 22:07:01 +0800

    Level-2

--- src/main/java/Poe.java
+++ src/main/java/Poe.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Poe {
     String line = "=================================================";
+    ArrayList<String> list = new ArrayList<String>(100);
     public void greetings(){
         String hello =  line + "\nYo! I'm Poe\nWhat can I do for you bro\n" + line;
         System.out.println(hello);
@@ -12,15 +14,30 @@ public class Poe {
         System.out.println(byebye);
     }
 
+    public void addList(String str){
+        list.add(str);
+    }
+    public void printList(){
+        System.out.println(line);
+        for (int i = 0; i < list.size(); i++){
+            System.out.printf("%d. %s\n",i+1,list.get(i));
+        }
+        System.out.println(line);
+
+    }
+
     public void input(){
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while(true) {
             String str = sc.nextLine();
-            if (str.toLowerCase().equals("bye")){
+            if (str.toLowerCase().equals("bye")) {
                 bye();
                 break;
+            }else if(str.toLowerCase().equals("list")){
+                printList();
             }else{
-                System.out.println(line+"\n"+str+"\n"+line);
+                addList(str);
+                System.out.println(line+"\nadded : "+str+"\n"+line);
             }
         }
     }
