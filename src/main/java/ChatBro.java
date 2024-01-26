ce98fe1f6efe9201b73df5c60ec0714a9169ae79
jimseah0116 <134794564+jimseah-0116@users.noreply.github.com>
2024-01-26 21:32:41 +0800

    updated code for Level-1

--- src/main/java/ChatBro.java
+++ src/main/java/ChatBro.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class ChatBro {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("_________________________\n" +
                 " __  __       __\n" +
                 " \\ \\/ /__    / /\n" +
@@ -7,8 +10,29 @@ public class ChatBro {
                 "  /_/\\___/ (_)\n\n" +
                 "I'm ChatBro!\n" +
                 "What can I do for you bro?\n" +
-                "_________________________\n" +
-                "Bye bro! See you soon!\n" +
+                "Available commands: list, blah, bye\n" +
                 "_________________________\n");
+
+        boolean isQuit = false;
+        while (!isQuit) {
+            String input = sc.next();
+            switch (input) {
+                case "list":
+                    System.out.println("This is the list reply bro.\n");
+                    break;
+                case "blah":
+                    System.out.println("This is the blah reply bro.\n");
+                    break;
+                case "bye":
+                    isQuit = true;
+                    System.out.println("_________________________\n" +
+                            "Bye bro! Hope to see you again soon!\n" +
+                            "_________________________\n");
+                    break;
+                default:
+                    System.out.println("I don't understand that command bro.\n");
+                    break;
+            }
+        }
     }
 }
