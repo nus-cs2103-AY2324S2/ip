df2e098ad0b7d14ec0668ba2b0f08262337152c1
ChenXy128 <e0968917@u.nus.edu>
2024-01-26 11:09:54 +0800

    Enum Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,109 +14,130 @@ public class Duke {
         while(true) {
             String inputFromUser = sc.nextLine();
             
-            if ("bye".equalsIgnoreCase(inputFromUser)) {
-                String bye = "___________________________________\n"
-                    + "Bye. Hope to see you again soon!\n"
-                    + "___________________________________";
-                System.out.println(bye);
-                break;
-            }
-            else if ("list".equalsIgnoreCase(inputFromUser)){
-                System.out.println("Here are the tasks in your list\n");
-                if(list.size() == 0) {
-                    System.out.println("\n___________________________________");
-                } else {
-                    int num = 1;
-                    for(Task t: list) {
-                        System.out.println(num + "." + t.toString() + "\n");
-                        num++;
+            Instruction instruction = Duke.getInstr(inputFromUser);
+            
+            switch (instruction) {
+                case LIST:
+                    System.out.println("Here are the tasks in your list\n");
+                    if(list.size() == 0) {
+                        System.out.println("\n___________________________________");
+                    } else {
+                        int num = 1;
+                        for(Task t: list) {
+                            System.out.println(num + "." + t.toString() + "\n");
+                            num++;
+                        }
+                        System.out.println("___________________________________");
                     }
+                    break;
+                case MARK:
+                    if (Integer.parseInt(inputFromUser.substring(5)) > list.size()) {
+                        throw new DukeException("You do not have that many tasks");
+                    }
+                    if (Integer.parseInt(inputFromUser.substring(5)) < 1) {
+                        throw new DukeException("No negative task number");
+                    }
+                    int num = Integer.parseInt(inputFromUser.substring(5));
+                    Task taskToBeMarked = list.get(num - 1);
+                    taskToBeMarked.markDone();
+                    System.out.println("Nice! I have marked this task as done\n");
+                    System.out.println(taskToBeMarked.toString());
                     System.out.println("___________________________________");
-                }
-            } else if (inputFromUser.toLowerCase().startsWith("mark")) {
-                if (Integer.parseInt(inputFromUser.substring(5)) > list.size()) {
-                    throw new DukeException("You do not have that many tasks");
-                }
-                if (Integer.parseInt(inputFromUser.substring(5)) < 1) {
-                    throw new DukeException("No negative task number");
-                }
-                int num = Integer.parseInt(inputFromUser.substring(5));
-                Task taskToBeMarked = list.get(num - 1);
-                taskToBeMarked.markDone();
-                System.out.println("Nice! I have marked this task as done\n");
-                System.out.println(taskToBeMarked.toString());
-                System.out.println("___________________________________");
-            } else if (inputFromUser.toLowerCase().startsWith("unmark")) {
-                if (Integer.parseInt(inputFromUser.substring(7)) > list.size()) {
-                    throw new DukeException("You do not have that many tasks");
-                }
-                if (Integer.parseInt(inputFromUser.substring(7)) < 1) {
-                    throw new DukeException("No negative task number");
-                }
-                int num = Integer.parseInt(inputFromUser.substring(7));
-                Task taskToBeUnmarked = list.get(num - 1);
-                taskToBeUnmarked.markUndone();
-                System.out.println("Ok, I've marked this task as not done yet\n");
-                System.out.println(taskToBeUnmarked.toString());
-                System.out.println("___________________________________");
-            } else if (!inputFromUser.toLowerCase().startsWith("mark")
-            && !inputFromUser.toLowerCase().startsWith("unmark")
-            && !inputFromUser.toLowerCase().startsWith("todo")
-            && !inputFromUser.toLowerCase().startsWith("deadline")
-            && !inputFromUser.toLowerCase().startsWith("event")
-            && !inputFromUser.toLowerCase().startsWith("list")
-            && !inputFromUser.toLowerCase().startsWith("bye")
-            && !inputFromUser.toLowerCase().startsWith("delete")) {
-                throw new DukeException("Can't understand your command");
-            } else if (inputFromUser.toLowerCase().startsWith("delete")) {
-                int indexOfTaskToDelete = Integer.parseInt(inputFromUser.substring(7));
-                Task taskToDelete = list.get(indexOfTaskToDelete - 1);
-                String toPrint = "Noted. I've removed this task:\n" + "  " + taskToDelete.toString() + "\n"
-                    + "Now you have " + (list.size() - 1) + " tasks in  the list"
-                    + "\n___________________________________" ;
-                System.out.println(toPrint);
-                list.remove(taskToDelete);
-            }
-            else {
-                Task task = null;
-                if (inputFromUser.toLowerCase().startsWith("todo")) {
+                    break;
+                case UNMARK:
+                    if (Integer.parseInt(inputFromUser.substring(7)) > list.size()) {
+                        throw new DukeException("You do not have that many tasks");
+                    }
+                    if (Integer.parseInt(inputFromUser.substring(7)) < 1) {
+                        throw new DukeException("No negative task number");
+                    }
+                    int taskNum = Integer.parseInt(inputFromUser.substring(7));
+                    Task taskToBeUnmarked = list.get(taskNum - 1);
+                    taskToBeUnmarked.markUndone();
+                    System.out.println("Ok, I've marked this task as not done yet\n");
+                    System.out.println(taskToBeUnmarked.toString());
+                    System.out.println("___________________________________");
+                    break;
+                case TODO:
                     if (!(inputFromUser.substring(4).matches(".*\\S.*"))) {
                         throw new DukeException("Description of the task can't be empty");
                     }
-                    String description = inputFromUser.substring(5);
-                    task = new ToDos(description);
-                }
-                if (inputFromUser.toLowerCase().startsWith("deadline")) {
+                    String todoDescription = inputFromUser.substring(5);
+                    Task todoTask = new ToDos(todoDescription);
+                    Duke.echo(todoTask, list);
+                    break;
+                case DEADLINE:
                     if (!(inputFromUser.substring(8).matches(".*\\S.*"))) {
                         throw new DukeException("Description of the task can't be empty");
                     }
                     int indexOfBy = inputFromUser.indexOf("/by");
-                    String description = inputFromUser.substring(9, indexOfBy - 1);
+                    String deadlineDescription = inputFromUser.substring(9, indexOfBy - 1);
                     String by = inputFromUser.substring(indexOfBy + 4);
-                    task = new Deadlines(description, by);
-                }
-                if (inputFromUser.toLowerCase().startsWith("event")) {
+                    Task deadlineTask = new Deadlines(deadlineDescription, by);
+                    Duke.echo(deadlineTask, list);
+                    break;
+                case EVENT:
                     if (!(inputFromUser.substring(5).matches(".*\\S.*"))) {
                         throw new DukeException("Description of the task can't be empty");
                     }
                     int indexOfFrom = inputFromUser.indexOf("/from");
                     int indexOfTo = inputFromUser.indexOf("/to");
-                    String description = inputFromUser.substring(6, indexOfFrom - 1);
+                    String eventDescription = inputFromUser.substring(6, indexOfFrom - 1);
                     String start = inputFromUser.substring(indexOfFrom + 6, indexOfTo - 1);
                     String end = inputFromUser.substring(indexOfTo + 4);
-                    task = new Events(description, start, end);
-                }
-                if (task != null) {
-                    String echo = "Got it. I've added this task:\n" + "  " + task.toString() + "\n"
-                        + "Now you have " + (list.size() + 1) + " tasks in the list"
+                    Task eventTask = new Events(eventDescription, start, end);
+                    Duke.echo(eventTask, list);
+                    break;
+                case DELETE:
+                    int indexOfTaskToDelete = Integer.parseInt(inputFromUser.substring(7));
+                    Task taskToDelete = list.get(indexOfTaskToDelete - 1);
+                    String toPrint = "Noted. I've removed this task:\n" + "  " + taskToDelete.toString() + "\n"
+                        + "Now you have " + (list.size() - 1) + " tasks in  the list"
                         + "\n___________________________________" ;
-                    System.out.println(echo);
-                    list.add(task);
-                } else {
-                 throw new DukeException("Task is null");
-                }
+                    System.out.println(toPrint);
+                    list.remove(taskToDelete);
+                    break;
+                case BYE:
+                    String bye = "___________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "___________________________________";
+                    System.out.println(bye);
+                    return;
+                default:
+                    throw new DukeException("Can't understand your instruction");
             }
         }
-        sc.close();
     }
+    public enum Instruction {
+        LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE
+    }
+    
+    private static Instruction getInstr(String input) {
+        if (input.toLowerCase().startsWith("list")) {
+            return Instruction.LIST;
+        } else if (input.toLowerCase().startsWith("mark")) {
+            return Instruction.MARK;
+        } else if (input.toLowerCase().startsWith("unmark")) {
+            return Instruction.UNMARK;
+        } else if (input.toLowerCase().startsWith("todo")) {
+            return Instruction.TODO;
+        } else if (input.toLowerCase().startsWith("deadline")) {
+            return Instruction.DEADLINE;
+        } else if (input.toLowerCase().startsWith("event")) {
+            return Instruction.EVENT;
+        } else if (input.toLowerCase().startsWith("delete")) {
+            return Instruction.DELETE;
+        } else {
+            return Instruction.BYE;
+        }
+    }
+    
+    private static void echo(Task task, ArrayList<Task> list) {
+        String echo = "Got it. I've added this task:\n" + "  " + task.toString() + "\n"
+            + "Now you have " + (list.size() + 1) + " tasks in the list"
+            + "\n___________________________________" ;
+        System.out.println(echo);
+        list.add(task);
+    }
+    
 }
