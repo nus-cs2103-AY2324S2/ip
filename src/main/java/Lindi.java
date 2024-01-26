25a524f47cc19fa8b1b6b553aa81a665ba8a24ca
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-26 22:24:14 +0800

    Compiled functionality of mark, unmark and delete into singular function executeModifyTask. Removed redundant functions

--- src/main/java/Lindi.java
+++ src/main/java/Lindi.java
@@ -77,48 +77,6 @@ public class Lindi {
                             "there shouldn't be any characters!! :(");
                 }
             }
-
-            /**
-             * @param listIndex: listIndex as shown by the `list` command, starts from 1
-             */
-            void executeMark(int listIndex) {
-                // Index starts from 1, up to 100
-                if (listIndex < 1 || listIndex > taskList.size()) {
-                    System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
-                            "You can see the tasks list by inputting \"list\"");
-                    return;
-                }
-                Task task = taskList.get(listIndex - 1);
-                task.markDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t" + task);
-            }
-            /**
-             * @param listIndex: listIndex as shown by the `list` command, starts from 1
-             */
-            void executeUnmark(int listIndex) {
-                // Index starts from 1, up to 100
-                if (listIndex < 1 || listIndex > taskList.size()) {
-                    System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
-                            "You can see the tasks list by inputting \"list\"");
-                    return;
-                }
-                Task task = taskList.get(listIndex - 1);
-                task.unmarkDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("\t" + task.toString());
-            }
-
-            public void executeDeleteTask(int listIndex) {
-                if (listIndex < 1 || listIndex > taskList.size()) {
-                    System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
-                            "You can see the tasks list by inputting \"list\"");
-                    return;
-                }
-                Task task = taskList.remove(listIndex - 1);
-                System.out.println("Okay. I've deleted this task:\n\t" + task +
-                        "\nNow you have " + taskList.size() + " tasks in the list.");
-            }
         }
         // if command called, will be index 0, rest of string in index 1
         String[] userInputTokens = userInput.split(" ", 2); 
