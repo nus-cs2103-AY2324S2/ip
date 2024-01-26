b9f9c70b201d596602ec408eeb9897eaa09497fe
lihaoquan <64543157+lihaoquan@users.noreply.github.com>
2024-01-27 03:16:02 +0800

    Extract Parser from Bob.java

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,33 +1,22 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bob {
 
-    private static final String TERMINATE_COMMAND = "bye";
-
-    private static final String LIST_COMMAND = "list";
-    private static final String MARK_COMMAND = "mark";
-    private static final String UNMARK_COMMAND = "unmark";
-
-    private static final String TODO_COMMAND = "todo";
-    private static final String DEADLINE_COMMAND = "deadline";
-    private static final String EVENT_COMMAND = "event";
-    private static final String DELETE_COMMAND = "delete";
-
-
     private ArrayList<Task> list;
     private BobUI ui;
     private BobStorage storage;
+    private BobParser parser;
 
-    public Bob(BobUI ui, BobStorage storage) {
+    public Bob(BobUI ui, BobStorage storage, BobParser parser) {
         this.list = new ArrayList<>();
         this.ui = ui;
         this.storage = storage;
+        this.parser = parser;
+    }
+
+    public ArrayList<Task> getList() {
+        return this.list;
     }
 
     private void deleteTask(int taskId) {
@@ -69,39 +58,12 @@ public class Bob {
         while (this.ui.acceptingInput()) {
 
             String input = this.ui.getUserInput().trim();
-            final String command = input.split("\\s+")[0];
+            this.parser.processInput(input);
 
-            try {
-                switch (command) {
-                case Bob.TERMINATE_COMMAND:
-                    this.ui.terminate();
-                    System.exit(0);
-                    break;
-                case Bob.LIST_COMMAND:
-                    this.ui.printList(false, this.list);
-                    continue;
-                case Bob.MARK_COMMAND:
-                case Bob.UNMARK_COMMAND:
-                    this.handleTaskMarking(input);
-                    break;
-                case Bob.TODO_COMMAND:
-                case Bob.DEADLINE_COMMAND:
-                case Bob.EVENT_COMMAND:
-                    this.handleTaskCreation(input);
-                    break;
-                case Bob.DELETE_COMMAND:
-                    this.handleTaskDeletion(input);
-                    break;
-                default:
-                    throw new BobException.InvalidCommand("Sorry, I'm not sure what command that is.");
-                }
-            } catch (BobException e) {
-                System.out.println(e.getMessage());
-            }
         }
     }
 
-    private void handleTaskMarking(String input) throws BobException {
+    public void handleTaskMarking(String input) throws BobException {
 
         String[] args = input.split("\\s+");
         if (args.length < 2) {
@@ -117,7 +79,7 @@ public class Bob {
 
         String userCommand = args[0];
 
-        if (userCommand.equals(Bob.MARK_COMMAND)) {
+        if (userCommand.equals(BobParser.MARK_COMMAND)) {
             this.markDone(taskId);
             System.out.println("    You have marked task as done:");
         } else {
@@ -130,39 +92,39 @@ public class Bob {
         this.ui.printLine();
     }
 
-    private void handleTaskCreation(String input) throws BobException {
+    public void handleTaskCreation(String input) throws BobException {
 
         Task t = null;
 
         try {
-            if (input.contains(Bob.TODO_COMMAND)) {
+            if (input.contains(BobParser.TODO_COMMAND)) {
 
-                if (input.length() == Bob.TODO_COMMAND.length()) {
-                    throw new BobException("The command " + Bob.TODO_COMMAND + " requires a task description.");
+                if (input.length() == BobParser.TODO_COMMAND.length()) {
+                    throw new BobException("The command " + BobParser.TODO_COMMAND + " requires a task description.");
                 }
 
-                String description = input.substring(Bob.TODO_COMMAND.length() + 1);
+                String description = input.substring(BobParser.TODO_COMMAND.length() + 1);
 
                 t = this.storage.addItem(new Task(description), this.list);
             }
 
-            if (input.contains(Bob.DEADLINE_COMMAND)) {
+            if (input.contains(BobParser.DEADLINE_COMMAND)) {
 
                 if (!input.contains("/by")) {
-                    throw new BobException("The command " + Bob.DEADLINE_COMMAND
+                    throw new BobException("The command " + BobParser.DEADLINE_COMMAND
                             + " requires both a task description and a deadline.");
                 }
 
-                if (input.length() == Bob.DEADLINE_COMMAND.length()) {
-                    throw new BobException("The command " + Bob.DEADLINE_COMMAND
+                if (input.length() == BobParser.DEADLINE_COMMAND.length()) {
+                    throw new BobException("The command " + BobParser.DEADLINE_COMMAND
                             + " requires both a task description and a deadline.");
                 }
 
-                input = input.substring(Bob.DEADLINE_COMMAND.length() + 1);
+                input = input.substring(BobParser.DEADLINE_COMMAND.length() + 1);
 
                 String[] split = input.split("/by");
                 if (split.length < 2) {
-                    throw new BobException("The command " + Bob.DEADLINE_COMMAND
+                    throw new BobException("The command " + BobParser.DEADLINE_COMMAND
                             + " requires both a task description and a deadline.");
                 }
 
@@ -170,24 +132,24 @@ public class Bob {
                         split[1].substring(1)), this.list);
             }
 
-            if (input.contains(Bob.EVENT_COMMAND)) {
+            if (input.contains(BobParser.EVENT_COMMAND)) {
 
                 if (!input.contains("/from") && !input.contains("/to")) {
-                    throw new BobException("The command " + Bob.EVENT_COMMAND
+                    throw new BobException("The command " + BobParser.EVENT_COMMAND
                             + " requires a task description, a start date, and an end date.");
                 }
 
-                if (input.length() == Bob.EVENT_COMMAND.length()) {
-                    throw new BobException("The command " + Bob.EVENT_COMMAND
+                if (input.length() == BobParser.EVENT_COMMAND.length()) {
+                    throw new BobException("The command " + BobParser.EVENT_COMMAND
                             + " requires a task description, a start date, and an end date.");
                 }
 
-                input = input.substring(Bob.EVENT_COMMAND.length() + 1);
+                input = input.substring(BobParser.EVENT_COMMAND.length() + 1);
 
                 String[] split = input.split("/");
 
                 if (split.length < 3) {
-                    throw new BobException("The command " + Bob.EVENT_COMMAND
+                    throw new BobException("The command " + BobParser.EVENT_COMMAND
                             + " requires a task description, a start date, and an end date.");
                 }
 
@@ -203,7 +165,7 @@ public class Bob {
         }
     }
 
-    private void handleTaskDeletion(String input) throws BobException {
+    public void handleTaskDeletion(String input) throws BobException {
         String[] args = input.split("\\s+");
         if (args.length < 2) {
             throw new BobException("The command " + args[0] + " requires a task ID.");
@@ -228,9 +190,13 @@ public class Bob {
 
     public static void main(String[] args) {
 
-        Bob bob = new Bob(
-                new BobUI(new Scanner(System.in)),
-                new BobStorage());
+        BobUI ui = new BobUI(new Scanner(System.in));
+        BobStorage storage = new BobStorage();
+        BobParser parser = new BobParser();
+
+        Bob bob = new Bob(ui, storage, parser);
+
+        parser.setSelf(bob).setUi(ui);
 
         bob.start();
     }
---
+++ src/main/java/BobParser.java
@@ -0,0 +1,59 @@
+public class BobParser {
+
+    public static final String TERMINATE_COMMAND = "bye";
+
+    public static final String LIST_COMMAND = "list";
+    public static final String MARK_COMMAND = "mark";
+    public static final String UNMARK_COMMAND = "unmark";
+
+    public static final String TODO_COMMAND = "todo";
+    public static final String DEADLINE_COMMAND = "deadline";
+    public static final String EVENT_COMMAND = "event";
+    public static final String DELETE_COMMAND = "delete";
+
+    private Bob self;
+    private BobUI ui;
+
+    public BobParser setSelf(Bob self) {
+        this.self = self;
+        return this;
+    }
+
+    public BobParser setUi(BobUI ui) {
+        this.ui = ui;
+        return this;
+    }
+
+    public void processInput(String input) {
+
+        final String command = input.split("\\s+")[0];
+
+        try {
+            switch (command) {
+            case BobParser.TERMINATE_COMMAND:
+                this.ui.terminate();
+                System.exit(0);
+                break;
+            case BobParser.LIST_COMMAND:
+                this.ui.printList(false, this.self.getList());
+                break;
+            case BobParser.MARK_COMMAND:
+            case BobParser.UNMARK_COMMAND:
+                this.self.handleTaskMarking(input);
+                break;
+            case BobParser.TODO_COMMAND:
+            case BobParser.DEADLINE_COMMAND:
+            case BobParser.EVENT_COMMAND:
+                this.self.handleTaskCreation(input);
+                break;
+            case BobParser.DELETE_COMMAND:
+                this.self.handleTaskDeletion(input);
+                break;
+            default:
+                throw new BobException.InvalidCommand("Sorry, I'm not sure what command that is.");
+            }
+        } catch (BobException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
