5abe2085923993e6196cc502be9ca0560abb540d
evedaktyl <evedaktyl@gmail.com>
2024-01-26 15:40:22 +0800

    Level-4 Text UI Testing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,13 +18,15 @@ public class Task {
     public void markAsDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as done:");
-        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+//        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+        System.out.println("  " + this.toString());
     }
 
     public void unmark() {
         this.isDone = false;
         System.out.println("OK, I've marked this task as not done yet");
-        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+//        System.out.printf("  [%s] %s\n", this.getStatusIcon(), this.getDescription());
+        System.out.println("  " + this.toString());
     }
 
     public String toString() {
