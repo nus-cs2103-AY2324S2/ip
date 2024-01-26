7007da9aba005a3bd609bd00f6a68227418f5739
jimseah0116 <134794564+jimseah-0116@users.noreply.github.com>
2024-01-26 21:00:55 +0800

    Renaming Duke to ChatBro

---
+++ src/main/java/ChatBro.java
@@ -0,0 +1,14 @@
+public class ChatBro {
+    public static void main(String[] args) {
+        System.out.println("_________________________\n" +
+                " __  __       __\n" +
+                " \\ \\/ /__    / /\n" +
+                "  \\  / _ \\  /_/ \n" +
+                "  /_/\\___/ (_)\n\n" +
+                "I'm ChatBro!\n" +
+                "What can I do for you bro?\n" +
+                "_________________________\n" +
+                "Bye bro! See you soon!\n" +
+                "_________________________\n");
+    }
+}
