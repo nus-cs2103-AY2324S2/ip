551cd4dce87af1597cc0723ab49d849df3e58598
DESKTOP-5H6OF5G\Owen <owenong@gmail.com>
2024-01-26 10:42:52 +0800

    Implemented echo feature, adjusted code structure

--- src/main/java/BotBot.java
+++ src/main/java/BotBot.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
+
 public class BotBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         BotBot.greet();
-        BotBot.exit();
+
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            if (next.equals("bye")) {
+                BotBot.exit();
+                break;
+            }
+            BotBot.print(next);
+            BotBot.divider();
+        }
+
+        scanner.close();
+    }
+
+    private static void print(String string) {
+        System.out.println(string);
     }
     private static void divider() {
-        System.out.println("###############################\n");
+        BotBot.print("########################################");
     }
     public static void greet() {
         BotBot.divider();
-        System.out.println("I am BotBot!\nWhat can I do for you?\n");
+        BotBot.print("I am BotBot!\nWhat can I do for you?\n");
         BotBot.divider();
     }
     public static void exit() {
-        System.out.println("Goodbye! See you soon!\n");
+        BotBot.print("Goodbye! See you soon!\n");
         BotBot.divider();
     }
 }
