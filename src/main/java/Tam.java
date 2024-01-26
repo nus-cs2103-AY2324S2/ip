c7f91d75ac6136f460c7690a58f469dd9261706f
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2024-01-26 17:34:11 +0800

    Added Echo feature

--- src/main/java/Tam.java
+++ src/main/java/Tam.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Tam {
     static String dividerText = "____________________________________________________________\n";
+    static Scanner scannerObj = new Scanner(System.in);
     public static void main(String[] args) {
         Tam.greet();
+        int status = readCommand();
+        while (status == 0) {
+            status = readCommand();
+        }
         Tam.exit();
     }
 
@@ -16,5 +22,19 @@ public class Tam {
         String exitText = "Bye. Hope to see you again soon!\n";
         System.out.print(exitText);
         System.out.print(dividerText);
+    }
+
+    public static int readCommand() {
+        String input = scannerObj.nextLine();
+        if (input.toLowerCase().equals("bye")) {
+            System.out.print(dividerText);
+            return 1;
+        }
+        else {
+            System.out.print(dividerText);
+            System.out.println(input);
+            System.out.print(dividerText);
+            return 0;
+        }
     }
 }
