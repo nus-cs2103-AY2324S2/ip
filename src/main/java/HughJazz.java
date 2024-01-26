8c68189c637f4faba3ace37d86bbe4f4f082498d
Greg-Tan <e0969217@u.nus.edu>
2024-01-27 02:18:47 +0800

    Echo

--- src/main/java/HughJazz.java
+++ src/main/java/HughJazz.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class HughJazz {
     public static void main(String[] args) {
         greeting();
+        Scanner scn = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scn.nextLine();
+
+            if("bye".equalsIgnoreCase(userInput)) {
+                break;
+            }
+            System.out.println(userInput);
+        }
         ending();
+        scn.close();
     }
 
     public static void greeting() {
