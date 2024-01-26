1bf6b7a4e5e270546b2fc53747b343307bec1e98
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-01-26 21:53:34 +0800

    Changed implementation of taskList to ArrayList<Task> instead of static Task[] array

--- src/main/java/Lindi.java
+++ src/main/java/Lindi.java
@@ -1,16 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.Collections.*;
 public class Lindi {
-    private final static Task[] taskList = new Task[100];
-    private static int taskListCount = 0;
+    private final static ArrayList<Task> taskList = new ArrayList<>(10);
+//    private final static Task[] taskList = new Task[100];
+//    private static int taskListCount = 0;
 
     private static void parseInputAndExecute(String userInput) {
         class LocalFuncs {
             void executeList(){
-                for (int i = 0; i < taskListCount; i++) {
-                    System.out.println(i+1 + ". " + taskList[i]);
-                }
-                if (taskListCount == 0) {
+                taskList.forEach(System.out::println);
+                if (taskList.size() == 0) {
                     System.out.println("There are no tasks for you today. Enjoy :)");
                 }
             }
@@ -24,13 +24,13 @@ public class Lindi {
                     return;
                 }
                 try {
-                    taskList[taskListCount++] = task;
+                    taskList.add(task);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Oh no! I can't handle that many tasks yet.. shutting down :(((((");
                     throw new RuntimeException(e.getMessage());
                 }
                 System.out.println("Got it. I've added this task:\n\t" + task +
-                                   "\nNow you have " + taskListCount + " tasks in the list.");
+                                   "\nNow you have " + taskList.size() + " tasks in the list.");
             }
 
             /**
@@ -47,27 +47,27 @@ public class Lindi {
              */
             void executeMark(int listIndex) {
                 // Index starts from 1, up to 100
-                if (listIndex < 1 || listIndex > taskListCount) {
+                if (listIndex < 1 || listIndex > taskList.size()) {
                     System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
                             "You can see the tasks list by inputting \"list\"");
                     return;
                 }
-                Task task = taskList[listIndex - 1];
+                Task task = taskList.get(listIndex - 1);
                 task.markDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t" + task.toString());
+                System.out.println("\t" + task);
             }
             /**
              * @param listIndex: listIndex as shown by the `list` command, starts from 1
              */
             void executeUnmark(int listIndex) {
                 // Index starts from 1, up to 100
-                if (listIndex < 1 || listIndex > taskListCount) {
+                if (listIndex < 1 || listIndex > taskList.size()) {
                     System.out.println("Sorry, I can't find that task. Please enter a valid index\n" +
                             "You can see the tasks list by inputting \"list\"");
                     return;
                 }
-                Task task = taskList[listIndex - 1];
+                Task task = taskList.get(listIndex - 1);
                 task.unmarkDone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("\t" + task.toString());
