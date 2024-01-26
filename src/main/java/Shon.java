65ccc69b4acd5a5daba4bfac84b08a4f34887e57
Shaun Lee <shaunlxw@gmail.com>
2024-01-27 03:36:05 +0800

    Handle Errors
    - Added ParameterException class
    - Packaged each command into methods that throws exceptions
    - Shows different text on "list" command with empty list

--- src/main/java/Shon.java
+++ src/main/java/Shon.java
@@ -10,37 +10,43 @@ public class Shon {
 
         print("Hello! I'm Shon", "What can I do for you?");
         String input = scanner.nextLine();
+        input = input.strip();
         while (!input.equals("bye")) {
-            String[] s = input.split(" ", 2);
-            String action = s[0];
-            switch (action) {
-                case "list":
-                    print(list.getList());
-                    break;
-                case "mark":
-                    print(list.mark(Integer.parseInt(s[1])));
-                    break;
-                case "unmark":
-                    print(list.unmark(Integer.parseInt(s[1])));
-                    break;
-                case "todo":
-                    print(list.addTodo(s[1]));
-                    break;
-                case "deadline":
-                    String[] d = s[1].split(" /by ", 2);
-                    print(list.addDeadline(d[0], d[1]));
-                    break;
-                case "event":
-                    String[] e = s[1].split(" /from ", 2);
-                    String[] duration = e[1].split(" /to ", 2);
-                    print(list.addEvent(e[0], duration[0], duration[1]));
-                    break;
-                default:
-
+            String action = input.split(" ")[0];
+            try {
+                switch (action) {
+                    case "":
+                        print("Please enter a command.");
+                        break;
+                    case "list":
+                        print(list.getList());
+                        break;
+                    case "mark":
+                        print(markTask(input, list));
+                        break;
+                    case "unmark":
+                        print(unmarkTask(input, list));
+                        break;
+                    case "todo":
+                        print(addTodo(input, list));
+                        break;
+                    case "deadline":
+                        print(addDeadline(input, list));
+                        break;
+                    case "event":
+                        print(addEvent(input, list));
+                        break;
+                    default:
+                        print("OOPS!!! I'm sorry, but I don't know what that means :-)");
+                }
+            } catch (ParameterException e) {
+                print(e.getMessage());
             }
 
             input = scanner.nextLine();
+            input = input.strip();
         }
+        scanner.close();
         print("Bye. Hope to see you again soon!");
     }
 
@@ -51,5 +57,84 @@ public class Shon {
         }
         System.out.println(LINE);
         System.out.println();
+    }
+
+    private static String[] markTask(String input, TodoList list) throws ParameterException {
+        return list.mark(getIndex(input));
+    }
+
+    private static String[] unmarkTask(String input, TodoList list) throws ParameterException {
+        return list.unmark(getIndex(input));
+    }
+
+    private static int getIndex(String input) throws ParameterException {
+        String[] split = input.split(" ");
+        if (split.length == 1) {
+            throw new ParameterException("Please enter which task number to mark.");
+        }
+        if (split.length > 2) {
+            throw new ParameterException("Please enter only one task number to mark.");
+        }
+        try {
+            return Integer.parseInt(split[1]);
+        } catch (NumberFormatException e) {
+            throw new ParameterException("Please enter a number for task number.");
+        }
+    }
+
+    private static String[] addTodo(String input, TodoList list) throws ParameterException {
+        String[] split = input.split(" ", 2);
+        if (split.length == 1) {
+            throw new ParameterException("Todo description cannot be empty.");
+        }
+        String description = split[1];
+        return list.addTodo(description);
+    }
+
+    private static String[] addDeadline(String input, TodoList list) throws ParameterException {
+        String[] split = input.split(" ", 2);
+        if (split.length == 1) {
+            throw new ParameterException("Please enter Deadline description and due date/time.");
+        }
+        if (!split[1].contains("/by")) {
+            throw new ParameterException("Please indicate due date/time after \"/by\".");
+        }
+        if (split[1].strip().startsWith("/by")) {
+            throw new ParameterException("Deadline description cannot be empty.");
+        }
+        if (split[1].strip().endsWith("/by")) {
+            throw new ParameterException("Deadline due date/time cannot be empty.");
+        }
+        String[] task = split[1].split("/by", 2);
+        return list.addDeadline(task[0].strip(), task[1].strip());
+    }
+
+    private static String[] addEvent(String input, TodoList list) throws ParameterException {
+        String[] split = input.split(" ", 2);
+        if (split.length == 1) {
+            throw new ParameterException("Please enter Event description and from/to date/time.");
+        }
+        if (!split[1].contains("/from")) {
+            throw new ParameterException("Please indicate from date/time after \"/from\".");
+        }
+        if (!split[1].contains("/to")) {
+            throw new ParameterException("Please indicate to date/time after \"/to\".");
+        }
+        if (split[1].strip().startsWith("/from")) {
+            throw new ParameterException("Event description cannot be empty.");
+        }
+        if (split[1].strip().endsWith("/to")) {
+            throw new ParameterException("Event to date/time cannot be empty.");
+        }
+        String[] task = split[1].strip().split("/from", 2);
+        String description = task[0].strip();
+        if (!task[1].contains("/to")) {
+            throw new ParameterException("\"/to\" must come after \"/from\".");
+        }
+        if (task[1].strip().startsWith("/to")) {
+            throw new ParameterException("Event from date/time cannot be empty.");
+        }
+        String[] datetimes = task[1].split("/to", 2);
+        return list.addEvent(description, datetimes[0].strip(), datetimes[1].strip());
     }
 }
--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -9,25 +9,32 @@ public class TodoList {
         this.list = new ArrayList<>(100);
     }
 
+    public String[] add(Task task) {
+        this.list.add(task);
+        int size = this.list.size();
+        return new String[]{"Got it. I've added this task:", "  " + task,
+                String.format("Now you have %s %s in the list.", size, size < 2 ? "task" : "tasks")};
+    }
+
     public String[] addTodo(String description) {
         Task task = new Todo(description);
-        this.list.add(task);
-        return addedTask(task);
+        return add(task);
     }
 
     public String[] addDeadline(String description, String by) {
         Task task = new Deadline(description, by);
-        this.list.add(task);
-        return addedTask(task);
+        return add(task);
     }
 
     public String[] addEvent(String description, String from, String to) {
         Task task = new Event(description, from, to);
-        this.list.add(task);
-        return addedTask(task);
+        return add(task);
     }
 
     public String[] getList() {
+        if (this.list.size() == 0) {
+            return new String[]{"Your list is currently empty."};
+        }
         String[] output = new String[this.list.size()+1];
         output[0] = "Here are the tasks in your list:";
         for (int i = 1; i < this.list.size()+1; i++) {
@@ -36,21 +43,21 @@ public class TodoList {
         return output;
     }
 
-    public String[] mark(int i) {
+    public String[] mark(int i) throws ParameterException {
+        if (i > this.list.size() || i < 1) {
+            throw new ParameterException("Please select a valid task number from the list.");
+        }
         Task task = this.list.get(i-1);
         String description = task.markAsDone();
         return new String[]{"Nice! I've marked this task as done:", "  " + description};
     }
 
-    public String[] unmark(int i) {
+    public String[] unmark(int i) throws ParameterException {
+        if (i > this.list.size() || i < 1) {
+            throw new ParameterException("Please select a valid task number from the list.");
+        }
         Task task = this.list.get(i-1);
         String description = task.markAsNotDone();
         return new String[]{"OK, I've marked this task as not done yet:", "  " + description};
-    }
-
-    public String[] addedTask(Task task) {
-        int size = this.list.size();
-        return new String[]{"Got it. I've added this task:", "  " + task,
-        String.format("Now you have %s %s in the list.", size, size < 2 ? "task" : "tasks")};
     }
 }
