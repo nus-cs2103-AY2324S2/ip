b605d88ed9304aab05f116a2cbb6eb53f6691ff0
minghowC <e0725080@u.nus.edu>
2024-01-26 22:12:32 +0800

    My Level-1 submission

--- src/main/java/IreneAI.java
+++ src/main/java/IreneAI.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 /**
  * The IreneAI Class represents a simple chatbot.
  * For the starter, it greets the user and say goodbye before exiting
  */
 public class IreneAI {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
         String chatbotName = "IreneAI";
         String line = "____________________________________________________________";
         dividingLine(line);
         System.out.println(" Hello! I'm " + chatbotName);
         System.out.println(" What can I do for you?");
+        dividingLine(line);
+
+        // Echo message
+        while (true) {
+            System.out.println("You: ");
+            String userInput = scanner.nextLine();
+            dividingLine(line);
+
+            // Terminating condition
+            if (userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println("IreneAI: I heard you said '" + userInput + "'.");
+            dividingLine(line);
+        }
 
         // Print the farewell message
-        dividingLine(line);
         System.out.println(" Bye. Hope to see you again soon!");
         dividingLine(line);
     }
