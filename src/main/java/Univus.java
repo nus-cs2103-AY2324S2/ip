14f19130cc2097ce5dc603e2d3623d874687e40d
Hibeom0929 <E1093045@u.nus.edu>
2024-01-26 15:51:48 +0800

    Level-1 Echo

--- src/main/java/Univus.java
+++ src/main/java/Univus.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
 public class Univus {
+    private static Scanner scanner;
+    public Univus() {
+        this.scanner = new Scanner(System.in);
+    }
     public void greet() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Univus");
@@ -10,9 +15,26 @@ public class Univus {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+    public void echo(String message) {
+        if(message.equals("bye")){
+            bye();
+            scanner.close();
+        }
+        else {
+            System.out.println("____________________________________________________________");
+            System.out.println(message);
+            System.out.println("____________________________________________________________");
+        }
+    }
     public static void main(String[] args) {
         Univus univus = new Univus();
         univus.greet();
-        univus.bye();
+        while (true) {
+            String message = scanner.nextLine();
+            univus.echo(message);
+            if (message.equals("bye")) {
+                break;
+            }
+        }
     }
 }
