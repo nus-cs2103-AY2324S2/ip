26d3ece911cb5d1139e28a3ece13455d46922bb2
wallacexuhanxiao <wallacexuhanxiao@gmail.com>
2024-01-27 22:22:21 +0800

    Add JUnit tests for TaskList and Parser

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,42 @@
+package duke;
+
+import duke.command.CurrentTask;
+import duke.command.List;
+import duke.command.Mark;
+import duke.command.Unmark;
+import duke.exception.DukeException;
+import duke.task.Deadline;
+import duke.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    void test_parse_throwDukeException() throws DukeException {
+        TaskList tasks = new TaskList(new ArrayList<>());
+        tasks.addTask(new Todo("todo task"));
+        tasks.addTask(new Deadline("deadline task", LocalDate.parse("2024-11-21")));
+        Parser parser = new Parser(tasks);
+        String textOne = "list";
+        String textTwo = "current";
+        String textThree = "mark 1";
+        String textFour = "unmark 1";
+
+        List ls = new List(tasks);
+        assertEquals(ls.reply(), parser.parse(textOne).reply());
+
+        CurrentTask curr = new CurrentTask(tasks);
+        assertEquals(curr.reply(), parser.parse(textTwo).reply());
+
+        Mark mark = new Mark(0,tasks);
+        assertEquals(mark.reply(), parser.parse(textThree).reply());
+
+        Unmark unmark = new Unmark(0,tasks);
+        assertEquals(unmark.reply(),parser.parse(textFour).reply());
+    }
+}
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class TaskListTest {
 
     @Test
-    void testGetLength() {
+    void test_getLength() {
         ArrayList<Task> input = new ArrayList<>();
         input.add(new Todo("todo task"));
         input.add(new Deadline("deadline task", LocalDate.parse("2002-11-21")));
@@ -26,5 +26,45 @@ public class TaskListTest {
         input.remove(0);
         tl = new TaskList(input);
         assertEquals(1, tl.getListLength());
+    }
+
+    @Test
+    void test_removeTask() {
+        ArrayList<Task> input = new ArrayList<>();
+        Task taskOne=new Deadline("deadline task", LocalDate.parse("2002-11-21"));
+        Task taskTwo=new Todo("todo task");
+        input.add(taskOne);
+        input.add(taskTwo);
+        TaskList tl = new TaskList(input);
+
+        assertEquals(taskOne,tl.removeTask(0));
+
+        assertEquals(taskTwo,tl.removeTask(0));
+    }
+
+    @Test
+    void test_getTask() {
+        ArrayList<Task> input = new ArrayList<>();
+        Task taskOne=new Deadline("deadline task", LocalDate.parse("2002-11-21"));
+        Task taskTwo=new Todo("todo task");
+        input.add(taskOne);
+        input.add(taskTwo);
+        TaskList tl = new TaskList(input);
+
+        assertEquals(input.get(0),tl.getTask(0));
+
+        assertEquals(input.get(1),tl.getTask(1));
+    }
+
+    @Test
+    void test_getTaskList() {
+        ArrayList<Task> input = new ArrayList<>();
+        Task taskOne=new Deadline("deadline task", LocalDate.parse("2002-11-21"));
+        Task taskTwo=new Todo("todo task");
+        input.add(taskOne);
+        input.add(taskTwo);
+        TaskList tl = new TaskList(input);
+
+        assertEquals(input,tl.getTaskList());
     }
 }
