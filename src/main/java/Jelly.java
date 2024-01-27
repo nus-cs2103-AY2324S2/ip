1171b45c4c827c8878e9a88415f87fbaa35bb087
Newton Koh <newtonkoh1998@hotmail.com>
2024-01-27 12:30:46 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Jelly.java
@@ -0,0 +1,12 @@
+public class Jelly {
+    public static void main(String[] args) {
+        String Line = "\n-------------------------------------------";
+        String Welcome = "Hello! I'm Jelly\nWhat can I do for you?";
+        String Farewell = "Bye. Hope to see you again soon!";
+        System.out.println(Line);
+        System.out.println(Welcome);
+        System.out.println(Line);
+        System.out.println(Farewell);
+        System.out.println(Line);
+    }
+}
