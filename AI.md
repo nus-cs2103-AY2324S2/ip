792f0dbd48e4da8a0814df9f9453fd6990f45435
ys112 <yusian@u.nus.edu>
2024-01-27 15:24:06 +0800

    Modify Duke.java to enable save
    Users can save their tasks and work on the same tasks in
    next session.

---
+++ AI.md
@@ -0,0 +1,18 @@
+## Productivity observation with AI assist
+
+Using github copilot for autocomplete
+
+### Performance
+Does best when the structure of the top level codes are there,
+hence providing a better context for the AI to work with.
+
+When there is no structure yet, it has higher chance of hallucinating
+the wrong code.
+
+It is also a 60-40 mix of good and bad suggestions, sometimes the details are inaccurate.
+
+If you have coded similar function before, it will be able to suggest the right code when repeated.
+Thus, it is better with laborous tasks with certain existing code for better code suggestions.
+
+Personally think that it saves some time by helping you type similar code again 
+or very trivial next few steps that you expect to take.
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 import static java.lang.Integer.parseInt;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -6,6 +10,34 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) throws DukeException {
         List<Task> tasksList = new ArrayList<Task>();
+
+        try {
+            File file = new File("data/duke.txt");
+            if (!file.exists()) {
+                boolean isCreated = false;
+                isCreated = file.getParentFile().mkdirs();
+                isCreated = file.createNewFile();
+
+                if (!isCreated) {
+                    throw new DukeException("Error: File could not be created.");
+                }
+            }
+            else {
+                Scanner s = new Scanner(file);
+                while (s.hasNext()) {
+                    String line = s.nextLine();
+                    Task task = getTask(line);
+                    tasksList.add(task);
+                }
+                s.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File could not be found.");
+        } catch (IOException e) {
+            System.out.println("Error: File could not be created.");
+        }
+
+
         System.out.println("DevGPT:\n\tHello! I'm DevGPT\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         boolean isExit = false;
@@ -82,7 +114,7 @@ public class Duke {
                         Task task = tasksList.get(index - 1);
                         task.markAsUndone();
                         System.out.println("DevGPT:\n\t Got it! I've marked this task as not done yet:");
-                        System.out.println("\t" + task.toString());
+                        System.out.println("\t\t" + task.toString());
                     } else if (command.equals("mark")) {
                         String[] split = input.split(" ");
                         if (split.length == 1) {
@@ -95,7 +127,7 @@ public class Duke {
                         Task task = tasksList.get(index - 1);
                         task.markAsDone();
                         System.out.println("DevGPT:\n\t Nice! I've marked this task as done:");
-                        System.out.println("\t" + task.toString());
+                        System.out.println("\t\t" + task.toString());
                     } else if (command.equals("delete")) {
                         String[] split = input.split(" ");
                         if (split.length == 1) {
@@ -107,7 +139,7 @@ public class Duke {
                         int index = Integer.parseInt(split[1]);
                         Task task = tasksList.remove(index - 1);
                         System.out.println("DevGPT:\n\t Poof! I've removed this task:");
-                        System.out.println("\t" + task.toString());
+                        System.out.println("\t\t" + task.toString());
                         System.out.println("DevGPT:\n\t Now you have " + tasksList.size() + " tasks in the list.");
                     } else {
                         throw new DukeException(
@@ -120,12 +152,58 @@ public class Duke {
 
                 scanner.close();
                 System.out.println("DevGPT:\n\tBye. Hope to see you again soon!");
+                writeToFile(tasksList);
+
             } catch (NumberFormatException e) {
                 System.out.println("DevGPT:\n\t" + "Input error: Please enter a valid number to modify task");
-            }
-            catch (DukeException e) {
+            } catch (DukeException e) {
                 System.out.println("DevGPT:\n\t" + e.getMessage());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
+    }
+
+    private static Task getTask(String line) throws DukeException {
+        String[] split = line.split(" \\| ");
+
+        // common fields
+        String taskType = split[0];
+        boolean isDone = split[1].equals("1");
+        String description = split[2];
+
+        Task task = null;
+        // class specific fields
+        if (taskType.equals("T")) {
+            task = new Todo(description);
+        } else if (taskType.equals("D")) {
+            String by = split[3];
+            task = new Deadline(description, by);
+        } else if (taskType.equals("E")) {
+            String from = split[3];
+            String to = split[4];
+            task = new Event(description, from, to);
+        }
+
+        if (task == null) {
+            throw new DukeException("Invalid task type");
+        }
+        if (isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    private static void writeToFile(List<Task> tasksList) throws IOException {
+        FileWriter fileWriter = new FileWriter("data/duke.txt");
+        for (Task task : tasksList) {
+            if (task instanceof Todo) {
+                fileWriter.write(task.getTaskType() + " | " + (task.isDone ? "1" : "0") + " | " + task.getDescription() + "\n");
+            } else if (task instanceof Deadline) {
+                fileWriter.write(task.getTaskType() + " | " + (task.isDone ? "1" : "0") + " | " + task.getDescription() + " | " + ((Deadline) task).getBy() + "\n");
+            } else if (task instanceof Event)
+                fileWriter.write(task.getTaskType() + " | "  + (task.isDone ? "1" : "0") + " | " + task.getDescription() + " | " + ((Event) task).getFrom() + " | " + ((Event) task).getTo() + "\n");
+        }
+        fileWriter.close();
     }
 }
