99c1d4efa725d1c496447028604cbc1f850c5b2a
bertrandong <bertrandongchangheng@gmail.com>
2024-01-27 21:51:27 +0800

    Add level 7: Save

---
+++ src/main/java/SaveFile.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import Task.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class SaveFile {
+    private String fileName;
+    private String directoryName;
+    public SaveFile(String fileName, String directoryName) {
+        this.fileName = fileName;
+        this.directoryName = directoryName;
+    }
+
+    public void createDirectory() {
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            if (directory.mkdir()) {
+            } else {
+                System.err.println("Error creating directory");
+            }
+        }
+    }
+
+    public void createFile() {
+        File file = new File(directoryName, fileName);
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error generating task list");
+            }
+        }
+    }
+
+    public void saveToFile(ArrayList<Task> taskList) {
+        try {
+            FileWriter fw = new FileWriter(this.directoryName + "/" + this.fileName);
+            for (Task t : taskList) {
+                fw.write(t.toFileString());
+                fw.write((System.lineSeparator()));
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.err.println("Error writing task list to file: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<Task> readFile() {
+        ArrayList<Task> taskList = new ArrayList<>();
+        try {
+            File f = new File(this.directoryName + "/" + this.fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String data = s.nextLine();
+                boolean isDone;
+
+                if (data.split("[|]")[1] == "0") {
+                    isDone = false;
+                } else {
+                    isDone = true;
+                }
+
+                if (data.startsWith("T")) {
+                    String description = data.split("[|]")[2];
+                    Todo newTodo = new Todo(description, isDone);
+                    taskList.add(newTodo);
+                } else if (data.startsWith("D")) {
+                    String description = data.split("[|]")[2];
+                    String by = data.split("[|]")[3];
+                    Deadline newDeadline = new Deadline(description, isDone, by);
+                    taskList.add(newDeadline);
+                } else if (data.startsWith("E")) {
+                    String description = data.split("[|]")[2];
+                    String from = data.split("[|]")[3];
+                    String to = data.split("[|]")[4];
+                    Event newEvent = new Event(description, isDone, from, to);
+                    taskList.add(newEvent);
+                }
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+
+        }
+        return taskList;
+    }
+}
--- src/main/java/Task/Task.java
+++ src/main/java/Task/Task.java
@@ -8,6 +8,11 @@ public class Task {
         this.isDone = false;
     }
 
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
     public void setDone(boolean done) {
         this.isDone = done;
     }
@@ -15,5 +20,9 @@ public class Task {
     @Override
     public String toString() {
         return "[" + (this.isDone ? "X" : " ") + "] " + this.description;
+    }
+
+    public String toFileString() {
+        return "|" + (this.isDone ? "1" : "0") + "|" + this.description;
     }
 }
