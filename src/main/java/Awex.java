fbeec99b1aad07c062283c204e85f3f17b1890bf
alex-setyawan <alexgenrong@gmail.com>
2024-01-27 17:28:33 +0800

    started level 0

---
+++ src/main/java/Awex.java
@@ -0,0 +1,12 @@
+public class Awex {
+    public static void main(String[] args) {
+        /* String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+        System.out.println("Hello! I'm AWEX!\nWhat can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
