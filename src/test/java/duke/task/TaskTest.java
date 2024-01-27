0d3eea0da364bab7056319e675dbfa1ba9bbcd16
Miffi <miffi@decadent.rest>
2024-01-27 21:20:26 +0800

    Add more Task test cases

--- src/test/java/duke/task/TaskTest.java
+++ src/test/java/duke/task/TaskTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+import java.util.List;
 
 class TaskTest {
     @Test
@@ -13,5 +14,15 @@ class TaskTest {
         });
 
         Assertions.assertEquals("No description given", e.getMessage());
+    }
+
+    @Test
+    public void validateComponentKeys_tooManyActual_invalidComponentsThrown() {
+        HashSet<String> expected = new HashSet<>(), actual = new HashSet<>(List.of("DESCRIPTION", "/by", "/from"));
+        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class, () -> {
+            Task.validateComponentKeys(expected, actual);
+        });
+
+        Assertions.assertEquals("Invalid task components: " + actual + "; expected: " + expected, e.getMessage());
     }
 }
