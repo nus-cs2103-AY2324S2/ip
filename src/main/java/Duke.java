26f03e9666206c7c9cd795a89e99d8e327218cba
Chu Jie <chujie2003@gmail.com>
2024-01-28 00:05:30 +0800

    add save function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 /**
  * The main class of Mitsuki the Chat bot.
@@ -23,6 +32,22 @@ public class Duke {
     static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
+        // Greeting the user.
+        System.out.println("Hello! My name is Mitsuki, nice to meet you!\n"
+                + "What can I do for you today?\n");
+
+        try {
+            list = get("list.txt");
+            System.out.println("I have loaded your previously saved list for you. :)");
+        } catch(FileNotFoundException ex) {
+            System.out.println("No saved list found. A new list will be used!");
+            File saveFile = new File("list.txt");
+            try {
+                saveFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         // Adding the commands to the list of commands Mitsuki can execute.
         commands.add("help");
@@ -35,8 +60,7 @@ public class Duke {
         commands.add("delete");
         commands.add("bye");
 
-        // Greeting the user.
-        System.out.println("Hello! My name is Mitsuki, nice to meet you!\n" + "What can I do for you today?\n");
+
 
         // Initiating the 'command' variable.
         String command = "nil";
@@ -89,6 +113,11 @@ public class Duke {
                     break;
 
                 case "bye":
+                    try {
+                        save("list.txt");
+                    } catch(IOException ex) {
+                        System.out.println("Something went wrong: " + ex.getMessage());
+                    }
                     bye();
 
                 default:
@@ -273,6 +302,69 @@ public class Duke {
 
         System.out.println("Alright, I have removed the following task from your list:\n" + goneTask);
         System.out.println("Now you have " + list.size() + " task(s) in the list.");
+    }
+
+    /**
+     * Saves the user's list as a text file in the hard disk.
+     * @param fileName the file name of the file to be saved.
+     * @throws IOException if file cannot be created.
+     */
+    public static void save(String fileName) throws IOException {
+        File list = new File(fileName);
+
+        FileWriter saver = new FileWriter(list);
+
+        for (int i = 0; i < Duke.list.size(); i++) {
+            saver.write(Duke.list.get(i).toString() + System.lineSeparator());
+        }
+
+        System.out.println("I have saved your list for your future reference. :D");
+        saver.close();
+    }
+
+    /**
+     * Retrieves the saved list file from the hard disk.
+     * @param fileName the file name of the file to be loaded.
+     * @return an array list of tasks that the user previously added.
+     * @throws FileNotFoundException if file does not exist yet.
+     */
+    public static ArrayList<Task> get(String fileName) throws FileNotFoundException {
+        File list = new File(fileName);
+        Scanner scanner = new Scanner(list);
+
+        while (scanner.hasNext()) {
+            String fullString = scanner.nextLine();
+            if (fullString.contains("[T]")) {
+                String description = fullString.substring(7);
+
+                Task nextTask = new Todo(description);
+                Duke.list.add(nextTask);
+
+            } else if (fullString.contains("[D]")) {
+                int descriptionEnd = fullString.indexOf('(');
+                int deadlineEnd = fullString.indexOf(')');
+
+                String description = fullString.substring(7, descriptionEnd - 1);
+                String deadline = fullString.substring(descriptionEnd + 1, deadlineEnd);
+
+                Task nextTask = new Deadline(description, deadline);
+                Duke.list.add(nextTask);
+
+            } else if (fullString.contains("[E]")) {
+                int descriptionEnd = fullString.indexOf('(');
+                int startTimeEnd = fullString.indexOf("to");
+                int endTimeEnd = fullString.indexOf(')');
+
+                String description = fullString.substring(7, descriptionEnd - 2);
+                String startTime = fullString.substring(descriptionEnd + 1, startTimeEnd - 1);
+                String endTime = fullString.substring(startTimeEnd - 1, endTimeEnd);
+
+                Task nextTask = new Event(description, startTime, endTime);
+                Duke.list.add(nextTask);
+            }
+        }
+
+        return Duke.list;
     }
 
     /**
