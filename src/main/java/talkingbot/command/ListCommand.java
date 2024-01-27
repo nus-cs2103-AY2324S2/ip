4145df0f7fdb602e0ea1c7a086dd1924b8470f50
Wilson W <wilsonwidyadhana681@gmail.com>
2024-01-27 16:54:46 +0800

    Add Javadoc comments for more classes

--- src/main/java/talkingbot/command/ListCommand.java
+++ src/main/java/talkingbot/command/ListCommand.java
@@ -5,10 +5,24 @@ import talkingbot.util.TaskList;
 import talkingbot.util.Ui;
 
 public class ListCommand extends Command {
+
+    /**
+     * Constructor for the ListCommand class.
+     *
+     * @param commandArr String array containing the command.
+     */
     public ListCommand(String[] commandArr) {
         super(commandArr);
     }
 
+    /**
+     * Runs this command.
+     * Prints out the list of Tasks to the standard output.
+     *
+     * @param taskList List of tasks.
+     * @param saveFile Abstraction for a file.
+     * @param ui User interface.
+     */
     @Override
     public void runCommand(TaskList taskList, SaveFile saveFile, Ui ui) {
         System.out.println(taskList);
--- src/main/java/talkingbot/command/ModifyMarkCommand.java
+++ src/main/java/talkingbot/command/ModifyMarkCommand.java
@@ -6,10 +6,26 @@ import talkingbot.util.TaskList;
 import talkingbot.util.Ui;
 
 public class ModifyMarkCommand extends Command {
+
+    /**
+     * Constructor for the ModifyMarkCommand class.
+     *
+     * @param commandArr String array containing the command.
+     */
     public ModifyMarkCommand(String[] commandArr) {
         super(commandArr);
     }
 
+    /**
+     * Runs this command.
+     * Gets the index of the Task to be modified,
+     * modifies the Task's mark, and sets the Task in the same
+     * index of the list of Tasks to the modified Task.
+     *
+     * @param taskList List of tasks.
+     * @param saveFile Abstraction for a file.
+     * @param ui User interface.
+     */
     @Override
     public void runCommand(TaskList taskList, SaveFile saveFile, Ui ui) {
         String[] curCommand = super.getCommandArr();
