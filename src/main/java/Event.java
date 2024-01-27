705a5e72c1b217a20e6774269d0a26d057dbf57f
shawnnlimm <shawnlim2525@gmail.com>
2024-01-27 16:18:49 +0800

    feat: Add Storage feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,18 @@
 public class Event extends Task {
 
     private static final String EVENT_MESSAGE = "[E]%s (from: %s to: %s)";
-    private String startTime;
-    private String endTime;
+    private static final String EVENT_FILE_TEMPLATE = "E | %s | %s | %s";
+    private final String startTime;
+    private final String endTime;
 
     public Event(String description, String startTime, String endTime) {
         super(description);
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public String taskFileTemplate() {
+        return String.format(EVENT_FILE_TEMPLATE, super.taskFileTemplate(), startTime, endTime);
     }
 
     @Override
