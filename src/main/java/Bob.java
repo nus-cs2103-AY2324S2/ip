0625d19153b90a4c1d73884f302e08f7327d1ef3
Chong Chin Herng <chongahzong@gmail.com>
2024-01-27 23:41:14 +0800

    Implement echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Bob {
     private static void printFormatted(String[] lines) {
-        String horizontalLine = "\t____________________________________________________________\n";
+        String horizontalLine = "    ____________________________________________________________\n";
         StringBuilder formatted = new StringBuilder(horizontalLine);
         for (String line : lines) {
-            formatted.append("\t ");
+            formatted.append("     ");
             formatted.append(line);
             formatted.append('\n');
         }
@@ -17,6 +19,16 @@ public class Bob {
 
     public static void main(String[] args) {
         printFormatted(new String[]{ "Hello! I'm Bob", "What can I do for you?" });
-        printFormatted(new String[]{ "Bye. Hope to see you again soon!" });
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            printFormatted(command);
+        }
+
+        printFormatted("Bye. Hope to see you again soon!");
     }
 }
