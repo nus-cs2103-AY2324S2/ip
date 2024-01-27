0bc7eebf9a65190d9d782707c25b3f338b3d7d8b
Wilson W <wilsonwidyadhana681@gmail.com>
2024-01-27 17:26:33 +0800

    Add more Javadocs

--- src/main/java/talkingbot/task/Deadline.java
+++ src/main/java/talkingbot/task/Deadline.java
@@ -3,23 +3,49 @@ package talkingbot.task;
 import java.time.LocalDateTime;
 import talkingbot.type.TaskType;
 
+/**
+ * A class that represents the Deadline task.
+ */
 public class Deadline extends Task {
     private final LocalDateTime endTime;
+
+    /**
+     * A constructor for the Deadline class.
+     *
+     * @param description Description of the task.
+     * @param isDone Marks whether the task has been done.
+     * @param endTime Marks the ending time of the deadline.
+     */
     public Deadline(String description, boolean isDone, String endTime) {
         super(description, isDone, TaskType.DEADLINE);
         this.endTime = LocalDateTime.parse(endTime, super.dateTimeEntryForm);
     }
 
+    /**
+     * Returns the ending time of the Deadline.
+     *
+     * @return Deadline ending time.
+     */
     public LocalDateTime getEndTime() {
         return this.endTime;
     }
 
+    /**
+     * Returns the String describing this task that will be saved into the save file.
+     *
+     * @return A formatted String.
+     */
     @Override
     public String getSaveFileString() {
         return String.format("D | %d | %s | %s", super.getDoneAsInt(),
                 super.getDescription(), this.getEndTime().format(super.dateTimeEntryForm));
     }
 
+    /**
+     * Returns how the Deadline object will be printed as a String.
+     *
+     * @return A formatted String.
+     */
     @Override
     public String toString() {
         return String.format("[D] %s (by: %s)", super.toString(),
--- src/main/java/talkingbot/task/Task.java
+++ src/main/java/talkingbot/task/Task.java
@@ -4,21 +4,44 @@ import java.time.format.DateTimeFormatter;
 import talkingbot.exception.TalkingBotException;
 import talkingbot.type.TaskType;
 
+/**
+ * An abstract class for tasks.
+ */
 public abstract class Task {
     private final String description;
     private boolean isDone;
     private final TaskType taskType;
+
+    /** Formatting of datetime used during entry and saving */
     protected static final DateTimeFormatter dateTimeEntryForm = DateTimeFormatter
             .ofPattern("yyyy-MM-dd kkmm");
+
+    /** Formatting of datetime outputs used for displaying to the user */
     protected static final DateTimeFormatter dateTimeOutForm = DateTimeFormatter
             .ofPattern("MMM dd yyyy kkmm");
 
+    /**
+     * Constructor for the Task class.
+     *
+     * @param description Description of the task.
+     * @param isDone Marks whether the task has been done.
+     * @param taskType Type of the task.
+     */
     public Task(String description, boolean isDone, TaskType taskType) {
         this.description = description;
         this.isDone = isDone;
         this.taskType = taskType;
     }
 
+    /**
+     * Factory method that generates the task object
+     * corresponding to the given type.
+     *
+     * @param fullDescription Full description of the task.
+     * @param type Type of the task.
+     * @return A task: either a Todo, Deadline, or Event.
+     * @throws TalkingBotException If fullDescription is empty, or if any time property is missing.
+     */
     public static Task generateTask(String fullDescription, String type)
             throws TalkingBotException {
         if (type.equals("todo")) {
@@ -50,6 +73,12 @@ public abstract class Task {
         }
     }
 
+    /**
+     * Generates a task from a line in the save file.
+     *
+     * @param line The current line being processed.
+     * @return A task: either a Todo, Deadline, or Event.
+     */
     public static Task generateTaskFromFile(String line) {
         String[] lineArr = line.split(" \\| ");
         boolean mark = lineArr[1].equals("1") ? true : false;
@@ -62,28 +91,56 @@ public abstract class Task {
         }
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return Task description.
+     */
     public String getDescription() {
         return this.description;
     }
 
-    public boolean getTypeEquals(TaskType taskType) {
-        return this.taskType.equals(taskType);
-    }
-
+    /**
+     * Returns whether a task is done.
+     *
+     * @return Whether a task is done.
+     */
     public boolean getDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns whether a task is done as an integer.
+     * 1 is for tasks done and 0 for tasks undone.
+     *
+     * @return Whether a task is done as an integer.
+     */
     public int getDoneAsInt() {
         return this.isDone ? 1 : 0;
     }
 
+    /**
+     * Sets the isDone property depending on the given parameter.
+     *
+     * @param isDone Value to set the isDone property to.
+     */
     public void setDone(boolean isDone) {
         this.isDone = isDone;
     }
 
+    /**
+     * Abstract method to return the task's representation
+     * in the save file.
+     *
+     * @return String that represents the task during saving.
+     */
     public abstract String getSaveFileString();
 
+    /**
+     * Returns how the Task object will be printed as a String.
+     *
+     * @return A formatted String.
+     */
     @Override
     public String toString() {
         String addMark = this.getDone() ? "X" : " ";
