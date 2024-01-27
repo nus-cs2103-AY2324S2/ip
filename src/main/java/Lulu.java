adee5353b63f1fd9421efbd248e53c1adb2cbaf8
alfaloo <zhiylu528@gmail.com>
2024-01-27 14:28:47 +0800

    minor changes

--- src/main/java/Lulu.java
+++ src/main/java/Lulu.java
@@ -9,7 +9,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
