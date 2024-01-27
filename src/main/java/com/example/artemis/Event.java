3a87aa42c3022497115358f46fec39c1d881a29c
kaiyi27 <e1070556@u.nus.edu>
2024-01-27 21:54:48 +0800

    Add javadocs to Event, Parser and Storage

--- src/main/java/com/example/artemis/Event.java
+++ src/main/java/com/example/artemis/Event.java
@@ -3,31 +3,60 @@ package com.example.artemis;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents an event task with a specified start and end time in the Artemis application.
+ * Extends the Task class.
+ */
 public class Event extends Task {
     private LocalDateTime byDateTime;
     private LocalDateTime to;
 
+    /**
+     * Constructs an Event task with the specified description, start time, and end time.
+     *
+     * @param description The description of the event.
+     * @param from         The start time of the event in the format "dd-MM-yyyy HHmm".
+     * @param to           The end time of the event in the format "dd-MM-yyyy HHmm".
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.byDateTime = parseDateTime(from);
         this.to = parseDateTime(to);
     }
 
+    /**
+     * Parses the date-time string and converts it to a LocalDateTime object.
+     *
+     * @param dateTimeString The date-time string in the format "dd-MM-yyyy HHmm".
+     * @return The LocalDateTime representation of the date-time.
+     */
     private LocalDateTime parseDateTime(String dateTimeString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
         return LocalDateTime.parse(dateTimeString, formatter);
     }
 
+    /**
+     * Returns a string representation of the Event task.
+     *
+     * @return A string representation of the Event task.
+     */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + byDateTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma"))
-                + " to: " + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma")) + ")";
+        return "[E]" + super.toString() + " (from: " +
+                byDateTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma")) +
+                " to: " + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma")) + ")";
     }
 
+    /**
+     * Returns a string representation of the Event task for storage in a file.
+     *
+     * @return A string representation of the Event task for storage in a file.
+     */
     @Override
     public String toFileString() {
         return "E | " + (isDone ? "1" : "0") + " | " + description + " | " +
                 byDateTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma")) + " - " +
-                        to.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma"));
+                to.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma"));
     }
 }
+
