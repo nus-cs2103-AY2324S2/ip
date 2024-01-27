c765cf2dca364f2029b6d92bacf59652be19cb1e
Chong Chin Herng <chongahzong@gmail.com>
2024-01-27 22:34:26 +0800

    Rename chatbot, implement greet and exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,11 @@
+public class Bob {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________\n";
+        System.out.println(horizontalLine
+                + " Hello! I'm Bob\n"
+                + " What can I do for you?\n"
+                + horizontalLine
+                + " Bye. Hope to see you again soon!\n"
+                + horizontalLine);
+    }
+}
