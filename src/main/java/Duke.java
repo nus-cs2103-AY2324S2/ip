7b920732be8548e8090f6bf27ab6b40526039afd
Sia Zhong Qing <zhongqing2001@gmail.com>
2024-01-27 13:38:51 +0800

    Add Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        printBreak();
+        System.out.println("Hello! I'm Klara");
+        System.out.println("What can I do for you?");
+        printBreak();
+        System.out.println("Bye. Hope to see you again soon!");
+        printBreak();
+
+        //        String logo = " ____        _        \n"
+        //                + "|  _ \\ _   _| | _____ \n"
+        //                + "| | | | | | | |/ / _ \\\n"
+        //                + "| |_| | |_| |   <  __/\n"
+        //                + "|____/ \\__,_|_|\\_\\___|\n";
+        //        System.out.println("Hello from\n" + logo);
     }
+
+    private static void printBreak() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+    }
+
+
 }
