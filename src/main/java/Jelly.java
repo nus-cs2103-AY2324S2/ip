09f543d4fc0b4fbeed4073b8678f1169f5dd93cb
Newton Koh <newtonkoh1998@hotmail.com>
2024-01-27 12:43:42 +0800

    Echo

--- src/main/java/Jelly.java
+++ src/main/java/Jelly.java
@@ -1,12 +1,26 @@
+import java.util.*;
+
 public class Jelly {
     public static void main(String[] args) {
-        String Line = "\n-------------------------------------------";
-        String Welcome = "Hello! I'm Jelly\nWhat can I do for you?";
-        String Farewell = "Bye. Hope to see you again soon!";
-        System.out.println(Line);
-        System.out.println(Welcome);
-        System.out.println(Line);
-        System.out.println(Farewell);
-        System.out.println(Line);
+        String line = "\n-------------------------------------------";
+        String welcome = "Hello! I'm Jelly\nWhat can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(line);
+        System.out.println(welcome);
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(!Read(scanner).equals("Bye"));
+
+        System.out.println(farewell);
+        System.out.println(line);
+    }
+
+    public static String Read(Scanner scanner){
+
+        String message = scanner.nextLine();
+        System.out.println(message);
+        return message;
     }
 }
