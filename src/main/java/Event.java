c6cc85dcf6e9dd764b2de656f428f00961981071
alfaloo <zhiylu528@gmail.com>
2024-01-27 14:54:46 +0800

    add LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,12 @@
-public class Event extends Task {
-    private String startDate;
-    private String endDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
-    public Event(String name, String startDate, String endDate) {
+public class Event extends Task {
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Event(String name, LocalDate startDate, LocalDate endDate) {
         super(name);
         this.startDate = startDate;
         this.endDate = endDate;
@@ -10,6 +14,8 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[E]%s %s (from: %s to: %s)", (super.status ? "[X]" : "[ ]"), super.name, this.startDate, this.endDate);
+        String from = this.startDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        String to = this.endDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        return String.format("[E]%s %s (from: %s to: %s)", (super.status ? "[X]" : "[ ]"), super.name, from, to);
     }
 }
