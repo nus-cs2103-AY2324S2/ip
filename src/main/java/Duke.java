7b30b80502d962eb5196eb91dfd030433b305e74
Sia Zhong Qing <zhongqing2001@gmail.com>
2024-01-28 04:22:25 +0800

    Add "List" and "Add" commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,27 +3,53 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+
+        // Start-up introduction
         printBreak();
         System.out.println("Hello! I'm Klara");
         System.out.println("What can I do for you?");
+        printBreak();
 
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
         while (!word.equals("bye")) {
-            printBreak();
-            System.out.println(word);
-            printBreak();
-            word = sc.nextLine();
+            // print list on demand
+            if (word.equals("list")) {
+                printList(list);
+                word = sc.nextLine();
+            }
+            else {
+                list.add(word);
+                printBreak();
+                System.out.println("added: " + word);
+                printBreak();
+                word = sc.nextLine();
+            }
         }
 
+        // Logging off upon "bye" command
         printBreak();
         System.out.println("Bye. Hope to see you again soon!");
         printBreak();
     }
 
+
     private static void printBreak() {
         String line = "____________________________________________________________";
         System.out.println(line);
+    }
+
+    /**
+     * Print all elements in the list
+      * @param list
+     */
+    private static void printList(ArrayList<String> list) {
+        printBreak();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(Integer.toString(i + 1) + ". " + list.get(i));
+        }
+        printBreak();
     }
 
 
