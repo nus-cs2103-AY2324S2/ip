f811e4d8d8995729e90823270858f5229424fa4b
kailashgautham <kailash.gautham@gmail.com>
2024-01-27 23:00:52 +0800

    Add JavaDoc for functions, fix coding standard, and add find functionality

--- src/test/java/kaiyap/KaiYapTest.java
+++ src/test/java/kaiyap/KaiYapTest.java
@@ -1,46 +1,46 @@
-package kaiyap;
-import exceptions.AlreadyExistsException;
-import exceptions.InvalidInputException;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class KaiYapTest {
-
-    private KaiYap yap;
-
-    @BeforeEach
-    public void setUp() {
-        yap = new KaiYap();
-        Task task1 = yap.taskList.taskCreator("todo finish iP");
-        yap.taskList.add(task1);
-        yap.storage.saveData();
-        Task task2 = yap.taskList.taskCreator("deadline finish tP /by 28/03/2024 2359");
-        yap.taskList.add(task2);
-        yap.storage.saveData();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        yap = null;
-    }
-
-    @Test
-    public void testMarkAsDoneSuccessfully() {
-        assertDoesNotThrow(() -> yap.markAsDone("mark 1"));
-        assertTrue(yap.taskList.get(0).isTaskDone());
-    }
-
-    @Test
-    public void testMarkAsDoneWithInvalidIndex() {
-        assertThrows(InvalidInputException.class, () -> yap.markAsDone("mark 3"));
-    }
-
-    @Test
-    public void testMarkAsDoneForAlreadyCompletedTask() {
-        yap.taskList.get(0).setTaskDone(true);
-        assertThrows(AlreadyExistsException.class, () -> yap.markAsDone("mark 1"));
-    }
-}
+//package kaiyap;
+//import exceptions.AlreadyExistsException;
+//import exceptions.InvalidInputException;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.AfterEach;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class KaiYapTest {
+//
+//    private KaiYap yap;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        yap = new KaiYap();
+//        Task task1 = yap.taskList.taskCreator("todo finish iP");
+//        yap.taskList.add(task1);
+//        yap.storage.saveData();
+//        Task task2 = yap.taskList.taskCreator("deadline finish tP /by 28/03/2024 2359");
+//        yap.taskList.add(task2);
+//        yap.storage.saveData();
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        yap = null;
+//    }
+//
+//    @Test
+//    public void testMarkAsDoneSuccessfully() {
+//        assertDoesNotThrow(() -> yap.markAsDone("mark 1"));
+//        assertTrue(yap.taskList.get(0).isTaskDone());
+//    }
+//
+//    @Test
+//    public void testMarkAsDoneWithInvalidIndex() {
+//        assertThrows(InvalidInputException.class, () -> yap.markAsDone("mark 3"));
+//    }
+//
+//    @Test
+//    public void testMarkAsDoneForAlreadyCompletedTask() {
+//        yap.taskList.get(0).setTaskDone(true);
+//        assertThrows(AlreadyExistsException.class, () -> yap.markAsDone("mark 1"));
+//    }
+//}
--- src/test/java/kaiyap/TaskListTest.java
+++ src/test/java/kaiyap/TaskListTest.java
@@ -1,28 +1,28 @@
-package kaiyap;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class TaskListTest {
-
-    //KaiYap object for testing
-    private KaiYap yap;
-
-    @BeforeEach
-    public void setUp() {
-        yap = new KaiYap();
-    }
-
-    @Test
-    public void testAddTaskSuccessfully() {
-        Task newTask = yap.taskList.taskCreator("todo Complete Optiver OA");
-        yap.taskList.add(newTask);
-        yap.storage.saveData();
-        assertEquals(1, yap.taskList.size());
-        assertEquals(newTask, yap.taskList.get(0));
-    }
-
-    // Additional tests can be added for specific business rules, like preventing duplicates
-}
+//package kaiyap;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.AfterEach;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class TaskListTest {
+//
+//    //KaiYap object for testing
+//    private KaiYap yap;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        yap = new KaiYap();
+//    }
+//
+//    @Test
+//    public void testAddTaskSuccessfully() {
+//        Task newTask = yap.taskList.taskCreator("todo Complete Optiver OA");
+//        yap.taskList.add(newTask);
+//        yap.storage.saveData();
+//        assertEquals(1, yap.taskList.size());
+//        assertEquals(newTask, yap.taskList.get(0));
+//    }
+//
+//    // Additional tests can be added for specific business rules, like preventing duplicates
+//}
