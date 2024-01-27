d9dedd88e101251253693e05dbf6f3b7f06d282f
Zer0Legion <lengkhai@gmail.com>
2024-01-27 16:57:24 +0800

    Testing: add tests for 4 methods

--- src/test/java/squid/tasks/TodoTest.java
+++ src/test/java/squid/tasks/TodoTest.java
@@ -1,18 +1,51 @@
 package squid.tasks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import squid.constants.REGEX;
 
 import static org.junit.Assert.assertEquals;
 
 public class TodoTest {
+    Todo task;
+    @BeforeEach
+    public void setup() {
+        task = new Todo("testTest");
+    }
     @Test
     public void testTodoIcon() {
-        Todo task = new Todo("testTest");
         assertEquals("[T]", task.getType());
     }
 
-    @Test public void testTodoName() {
-        Todo task = new Todo("testTest");
+    @Test
+    public void testTodoName() {
         assertEquals("testTest", task.task);
     }
+
+    @Test
+    public void testDefaultUnmarked() {
+        assertEquals(false, task.completed);
+    }
+
+    @Test
+    public void testSetMarked() {
+        task.setCompleted(true);
+        assertEquals(true, task.completed);
+
+        // After
+        task.setCompleted(false);
+    }
+
+    @Test
+    public void testString() {
+        String expected = "[T][ ]: testTest";
+        assertEquals(expected, task.toString());
+    }
+
+    @Test
+    public void testParseString() {
+        String expected = String.format("[T]%s-%stestTest\n", REGEX.TASK_SPLIT, REGEX.TASK_SPLIT);
+        assertEquals(expected, task.parseStr());
+    }
+
 }
