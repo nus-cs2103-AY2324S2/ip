5421fddf19cc19d8dba23046905df6177a379c1a
ChocoRagdoll <E0960356@u.nus.edu>
2024-01-27 17:32:24 +0800

    add task and list all function

--- src/main/java/Ragdoll.java
+++ src/main/java/Ragdoll.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Ragdoll{
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -22,14 +26,16 @@ public class Ragdoll{
         System.out.println("____________________________________________________________");
 
         while(true) {
-            String input = scanner.nextLine();
-            System.out.println("____________________________________________________________");
+            String input = scanner.nextLine().trim();
+            System.out.println("___________________________________list_________________________");
 
-            if ("bye".equalsIgnoreCase(input.trim())) {
+            if ("bye".equalsIgnoreCase(input)) {
                 break;
+            } else if ("list".equalsIgnoreCase(input)) {
+                listTasks();
+            } else {
+                addTask(input);
             }
-
-            System.out.println(input);
 
             System.out.println("____________________________________________________________");
 
@@ -38,5 +44,25 @@ public class Ragdoll{
         scanner.close();
         System.out.println("See ya!");
         System.out.println("____________________________________________________________");
+    }
+
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
+            System.out.println("added: " + task);
+        } else {
+            System.out.println("Task list is full!");
+        }
+    }
+
+    private static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println("There is no task yet!");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
     }
 }
