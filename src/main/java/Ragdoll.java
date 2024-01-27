3146b9e80c8bda1d394247c15dbe14fb04647bbc
ChocoRagdoll <E0960356@u.nus.edu>
2024-01-27 21:42:10 +0800

    added support for deleting and testing, fixed lowercase issue

--- src/main/java/Ragdoll.java
+++ src/main/java/Ragdoll.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ragdoll{
     private static String USER = "Master";
-    private static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
+    private static List<Task> tasks = new ArrayList<>();
     private static int taskCount = 0;
     private static final String LINE = "____________________________________________________________";
 
@@ -28,17 +29,22 @@ public class Ragdoll{
         System.out.println(LINE);
 
         while(true) {
-            String input = scanner.nextLine().trim().toLowerCase();
+            String input = scanner.nextLine().trim();
             System.out.println(LINE);
+
+            String[] parts = input.split(" ", 2);
+            String command = parts[0].toLowerCase();
 
             if ("bye".equalsIgnoreCase(input)) {
                 break;
             } else if ("list".equalsIgnoreCase(input)) {
                 listTasks();
-            } else if (input.startsWith("mark ")) {
+            } else if ("mark".equalsIgnoreCase(command)) {
                 markTask(input);
-            } else if (input.startsWith("unmark")) {
+            } else if ("unmark".equalsIgnoreCase(command)) {
                 unmarkTask(input);
+            } else if ("delete".equalsIgnoreCase(command)) {
+                deleteTask(input);
             } else {
                 addTask(input);
             }
@@ -52,11 +58,6 @@ public class Ragdoll{
     }
 
     private static void addTask(String input) {
-        if (taskCount >= MAX_TASKS) {
-            System.out.println(USER + ", task list is full!");
-            return;
-        }
-
         String[] parts = input.split(" ", 2);
         if (parts.length < 2 || parts[1].trim().isEmpty()) {
             System.out.println("Sorry, " + USER + ", please format your tasks as [task type] [task description]!\n" +
@@ -99,7 +100,9 @@ public class Ragdoll{
                     return;
             }
 
-            tasks[taskCount++] = task;
+            tasks.add(task);
+            taskCount++;
+
             System.out.println(USER + ", I've added this task:\n  " + task);
             if (taskCount <= 1) {
                 System.out.println("Now you have " + taskCount + " task in the list, " + USER + "!");
@@ -111,13 +114,34 @@ public class Ragdoll{
         }
     }
 
+    public static void deleteTask(String input) {
+        try {
+            int idx = Integer.parseInt(input.substring(7)) - 1;
+            if (idx < 0 || idx >= taskCount) {
+                System.out.println("No task numbered " + (idx + 1) + ", " + USER + "!");
+            } else {
+                Task removed = tasks.remove(idx);
+                taskCount--;
+
+                System.out.println(USER + ", I've removed this task:\n  " + removed);
+                if (taskCount <= 1) {
+                    System.out.println("Now you have " + taskCount + " task in the list, " + USER + "!");
+                } else {
+                    System.out.println("Now you have " + taskCount + " tasks in the list, " + USER + "!");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(USER + ", please format it as delete [task index].");
+        }
+    }
+
     private static void listTasks() {
         if (taskCount == 0) {
             System.out.println("There is no task yet, " + USER + "!");
         } else {
             System.out.println(USER + ", your task list has the following tasks:");
             for (int i = 0; i < taskCount; i++) {
-                System.out.println((i + 1) + "." + tasks[i]);
+                System.out.println((i + 1) + "." + tasks.get(i));
             }
         }
     }
@@ -125,11 +149,11 @@ public class Ragdoll{
     private static void markTask(String input) {
         try {
             int idx = Integer.parseInt(input.substring(5)) - 1;
-            if (idx < 0 || idx >= taskCount || tasks[idx] == null) {
+            if (idx < 0 || idx >= taskCount || tasks.get(idx) == null) {
                 System.out.println("No task numbered " + (idx + 1) + ", " + USER + "!");
             } else {
-                tasks[idx].mark();
-                System.out.println(USER + "! I've marked this task as done:\n" + tasks[idx]);
+                tasks.get(idx).mark();
+                System.out.println(USER + "! I've marked this task as done:\n" + tasks.get(idx));
             }
         } catch (NumberFormatException e) {
             System.out.println(USER + ", please format it as mark [task index].");
@@ -139,11 +163,11 @@ public class Ragdoll{
     private static void unmarkTask(String input) {
         try {
             int idx = Integer.parseInt(input.substring(7)) - 1;
-            if (idx < 0 || idx >= taskCount || tasks[idx] == null) {
+            if (idx < 0 || idx >= taskCount || tasks.get(idx) == null) {
                 System.out.println("No task numbered " + (idx + 1) + ", " + USER + "!");
             } else {
-                tasks[idx].unmark();
-                System.out.println("Ok, " + USER + "! I've undone this task:\n" + tasks[idx]);
+                tasks.get(idx).unmark();
+                System.out.println("Ok, " + USER + "! I've undone this task:\n" + tasks.get(idx));
             }
         } catch (NumberFormatException e) {
             System.out.println(USER + ", please format it as mark [task index].");
