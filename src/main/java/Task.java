bb81fff487192333939f156e90e9a07e8ac75749
Miffi <miffi@decadent.rest>
2024-01-27 13:16:20 +0800

    Add state saving and loading
    This uses java's built in serialization as it's the easiest and most
    fault tolerant way to save the entire state.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,11 @@
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class Task {
+public abstract class Task implements Serializable {
     private enum Status {
         Complete("completed", 'X'), Incomplete("pending", ' ');
 
