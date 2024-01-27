d23974f8cb0cf4fd20de3e66145e4beed9176681
HM33-Stu <120201372+HM33-Stu@users.noreply.github.com>
2024-01-27 22:51:27 +0800

    Level-3

--- src/main/java/Hari.java
+++ src/main/java/Hari.java
@@ -57,7 +57,7 @@ class handlerbot
         System.out.println("____________________________________________________________");
         System.out.println(" What's new to do? : " + taskings); //Display message that tasks has been added
         System.out.println("____________________________________________________________");
-        arrtaskings[countertaskings] = taskings; //Add this task to the array storing all tasks
+        arrtaskings[countertaskings] ="[ ] " + taskings; //Add this task to the array storing all tasks
         countertaskings++; // Increase count of tasks stored
     }
 
@@ -65,15 +65,53 @@ class handlerbot
     // No modification done to userechoedinput function as not all inputs are tasks
     public void taskingsdisplay() {
         if (countertaskings == 0) {
+            System.out.println("____________________________________________________________");
             System.out.println(" Your task list is empty. Add tasks by simply typing them in."); //If there are no tasks, a message to guide user
+            System.out.println("____________________________________________________________");
         } else {
+            System.out.println("____________________________________________________________");
             System.out.println(" Here are your tasks:"); //Display all tasks
             for (int i = 0; i < countertaskings; i++) {
                 System.out.println(" " + (i + 1) + ". " + arrtaskings[i]);
             }
+            System.out.println("____________________________________________________________");
         }
     }
 
+    // Function to mark task as completed
+    public void completionmark(int taskrecorder) {
+        if (taskrecorder > 0 && taskrecorder <= countertaskings) // If there are tasks
+        {
+            System.out.println("____________________________________________________________");
+            System.out.println("Another one in the bag! Well done!");
+            arrtaskings[taskrecorder - 1] = arrtaskings[taskrecorder - 1].replace("[ ] ", "[X] "); //To replace the empty [ ] with a [X]
+            System.out.println(" " + (taskrecorder) + "." + arrtaskings[taskrecorder - 1]);
+            System.out.println("____________________________________________________________");
+        } else // Error handling: There are no tasks
+        {
+            System.out.println("____________________________________________________________");
+            System.out.println("Hmmm...I dont see to have a task under this number");
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    // Function to unmark a previously marked as completed task
+    public void incompletionmark(int taskrecorder)
+    {
+        if (taskrecorder > 0 && taskrecorder <= countertaskings) // If there are tasks
+        {
+            System.out.println("____________________________________________________________");
+            System.out.println("Oh dear, better get on it!");
+            arrtaskings[taskrecorder - 1] = arrtaskings[taskrecorder - 1].replace("[X] ", "[ ] "); //To replace the [X] with an empty [ ]
+            System.out.println(" " + (taskrecorder) + "." + arrtaskings[taskrecorder- 1]);
+            System.out.println("____________________________________________________________");
+        } else // Error handling: There are no tasks
+        {
+            System.out.println("____________________________________________________________");
+            System.out.println("Hmmm...I dont see to have a task under this number");
+            System.out.println("____________________________________________________________");
+        }
+    }
 }
 
 //Main Class
@@ -87,16 +125,25 @@ public class Hari
 
         String readerinput; // To store user input
 
-        while(true) //Modified do-while to a while as I have now streamlined all the code in the main body and reduced number of function calls
+        while (true) //Modified do-while to a while as I have now streamlined all the code in the main body and reduced number of function calls
         {
             readerinput = inputread.nextLine(); //Read and store user input inside readerinput variable
 
-            if (readerinput.equalsIgnoreCase("bye")) // If "bye" is written as an input, the chatbot exits with the farewell message
-            {
+            if (readerinput.equalsIgnoreCase("bye")) {  // If "bye" is written as an input, the chatbot exits with the farewell message
                 break;
+            } else if (readerinput.equalsIgnoreCase("list")) { // To list out tasks
+                hari.taskingsdisplay();
+            } else if (readerinput.startsWith("mark")) { // To mark a task as completed
+                // Extract the task number from user input
+                int taskindexer = Integer.parseInt(readerinput.substring(5).trim()); // 5 is because of the word "mark"
+                hari.completionmark(taskindexer);
+            } else if (readerinput.startsWith("unmark")) { // To unmark a previously marked as completed tasks
+                // Extract task number from user input
+                int taskindexer= Integer.parseInt(readerinput.substring(7).trim()); // 7 is because of the word "unmark"
+                hari.incompletionmark(taskindexer);
+            } else {
+                hari.userechoedinput(readerinput); //Else, it proceeds to call the user input processing function
             }
-
-            hari.userechoedinput(readerinput); //Else, it proceeds to call the user input processing function
         }
 
         hari.messagefarewell(); // Call the messagefarewell function to display farewell message and exit the program
