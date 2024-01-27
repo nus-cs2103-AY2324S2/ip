6bfc4b3e8600c2a6e752557709100d03988c19b0
jimseah0116 <134794564+jimseah-0116@users.noreply.github.com>
2024-01-27 22:25:50 +0800

    Level-6 updates

--- src/main/java/ChatBro.java
+++ src/main/java/ChatBro.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatBro {
@@ -9,13 +10,16 @@ public class ChatBro {
                 "  /_/\\___/ (_)\n\n" +
                 "I'm ChatBro!\n" +
                 "What can I do for you bro?\n" +
-                "Use the available commands: list, bye, mark, unmark, OR\n" +
+                "Use the available commands: list, bye, mark, unmark, delete, OR\n" +
                 "create a new task (todo, deadline, event) to store in your list bro.\n" +
                 "_________________________\n");
 
         Scanner sc = new Scanner(System.in);
         boolean isQuit = false;
-        Task[] taskList = new Task[101]; // First element left empty for 1-based indexing
+        ArrayList<Task> taskList = new ArrayList<>(101); // First element left empty for 1-based indexing
+        for (int i = 0; i < 102; i++) {
+            taskList.add(null);
+        }
         int taskCount = 0;
 
         while (!isQuit) {
@@ -27,10 +31,10 @@ public class ChatBro {
                     System.out.println("Here are the tasks in your list bro:\n" +
                             "_________________________");
                     for (int i = 1; i <= 100; i++) {
-                        if (taskList[i] == null) {
+                        if (taskList.get(i) == null) {
                             break;
                         }
-                        System.out.println(i + "." + taskList[i].toString());
+                        System.out.println(i + "." + taskList.get(i).toString());
                     }
                     System.out.println("_________________________\n");
                     break;
@@ -46,11 +50,11 @@ public class ChatBro {
                             break;
                         }
                         for (int i = 1; i <= 100; i++) {
-                            if (taskList[i] == null) {
-                                taskList[i] = new ToDo(todoName);
+                            if (taskList.get(i) == null) {
+                                taskList.add(i, new ToDo(todoName));
                                 taskCount++;
                                 System.out.println("_________________________\n" +
-                                        "Ok bro, I've added: \n" + taskList[i].toString() + "\n into your list.\n" +
+                                        "Ok bro, I've added: \n" + taskList.get(i).toString() + "\n into your list.\n" +
                                         "You've got " + taskCount + " task(s) now.\n" +
                                         "_________________________\n");
                                 break;
@@ -82,11 +86,11 @@ public class ChatBro {
                             break;
                         }
                         for (int i = 1; i <= 100; i++) {
-                            if (taskList[i] == null) {
-                                taskList[i] = new Deadline(deadlineName, deadlineSplit[1]);
+                            if (taskList.get(i) == null) {
+                                taskList.add(i, new Deadline(deadlineName, deadlineSplit[1]));
                                 taskCount++;
                                 System.out.println("_________________________\n" +
-                                        "Ok bro, I've added: \n" + taskList[i].toString() + "\n into your list.\n" +
+                                        "Ok bro, I've added: \n" + taskList.get(i).toString() + "\n into your list.\n" +
                                         "You've got " + taskCount + " task(s) now.\n" +
                                         "_________________________\n");
                                 break;
@@ -126,11 +130,11 @@ public class ChatBro {
                             break;
                         }
                         for (int i = 1; i <= 100; i++) {
-                            if (taskList[i] == null) {
-                                taskList[i] = new Event(eventName, eventToSplit[0], eventToSplit[1]);
+                            if (taskList.get(i) == null) {
+                                taskList.add(i, new Event(eventName, eventToSplit[0], eventToSplit[1]));
                                 taskCount++;
                                 System.out.println("_________________________\n" +
-                                        "Ok bro, I've added: \n" + taskList[i].toString() + "\n into your list.\n" +
+                                        "Ok bro, I've added: \n" + taskList.get(i).toString() + "\n into your list.\n" +
                                         "You've got " + taskCount + " task(s) now.\n" +
                                         "_________________________\n");
                                 break;
@@ -148,32 +152,49 @@ public class ChatBro {
 
                 case "mark":
                     int taskNum = Integer.parseInt(inputSplit[1]);
-                    if (taskList[taskNum] == null) {
+                    if (taskList.get(taskNum) == null) {
                         System.out.println("_________________________\n" +
                                 "Sorry bro, this task doesn't exist in your list.\n" +
                                 "_________________________\n");
                         break;
                     }
-                    taskList[taskNum].markAsDone();
+                    taskList.get(taskNum).markAsDone();
                     System.out.println("_________________________\n" +
                             "Got it bro! Task " + taskNum + " is marked as done:\n" +
-                            taskList[taskNum].toString() +
+                            taskList.get(taskNum).toString() +
                             "\n_________________________\n");
                     break;
 
                 case "unmark":
                     int taskNum2 = Integer.parseInt(inputSplit[1]);
-                    if (taskList[taskNum2] == null) {
+                    if (taskList.get(taskNum2) == null) {
                         System.out.println("_________________________\n" +
                                 "Sorry bro, this task doesn't exist in your list.\n" +
                                 "_________________________\n");
                         break;
                     }
-                    taskList[taskNum2].markAsUndone();
+                    taskList.get(taskNum2).markAsUndone();
                     System.out.println("_________________________\n" +
                             "Got it bro! Task " + taskNum2 + " is marked as not done yet:\n" +
-                            taskList[taskNum2].toString() +
+                            taskList.get(taskNum2).toString() +
                             "\n_________________________\n");
+                    break;
+
+                case "delete":
+                    int taskNum3 = Integer.parseInt(inputSplit[1]);
+                    if (taskList.get(taskNum3) == null) {
+                        System.out.println("_________________________\n" +
+                                "Sorry bro, this task doesn't exist in your list.\n" +
+                                "_________________________\n");
+                        break;
+                    }
+                    String taskName = taskList.get(taskNum3).toString();
+                    taskList.remove(taskNum3);
+                    taskCount--;
+                    System.out.println("_________________________\n" +
+                            "Got it bro! Task " + taskNum3 + " has been removed:\n" +
+                            taskName + "\n" +
+                            "_________________________\n");
                     break;
 
                 case "bye":
