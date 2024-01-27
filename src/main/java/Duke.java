583ffe286f80c02f3659539d7558f2a97e26e7e6
Sia Zhong Qing <zhongqing2001@gmail.com>
2024-01-27 14:35:29 +0800

    Add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,24 @@
-public class Duke {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class Duke {
+    public static void main(String[] args) throws IOException {
         printBreak();
         System.out.println("Hello! I'm Klara");
         System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        while (!word.equals("bye")) {
+            printBreak();
+            System.out.println(word);
+            printBreak();
+            word = sc.nextLine();
+        }
+
         printBreak();
         System.out.println("Bye. Hope to see you again soon!");
         printBreak();
-
-        //        String logo = " ____        _        \n"
-        //                + "|  _ \\ _   _| | _____ \n"
-        //                + "| | | | | | | |/ / _ \\\n"
-        //                + "| |_| | |_| |   <  __/\n"
-        //                + "|____/ \\__,_|_|\\_\\___|\n";
-        //        System.out.println("Hello from\n" + logo);
     }
 
     private static void printBreak() {
