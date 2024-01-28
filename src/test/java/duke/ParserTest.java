2aeeeeb543c3c1ad29347b35f3c6896a94ab9d7f
yeoshuheng <yeoshuheng@gmail.com>
2024-01-28 16:30:38 +0800

    test: added enhanced test cases for parser

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -11,9 +11,14 @@ class ParserTest {
   @Test
   public void testGetCommand() throws InvalidInputException {
     assertEquals(duke.UI.Command.BYE, Parser.getCommand(new String[]{"bye"}));
+    assertEquals(duke.UI.Command.TODO, Parser.getCommand(new String[]{"todo"}));
+    assertEquals(duke.UI.Command.EVENT, Parser.getCommand(new String[]{"event"}));
+    assertEquals(UI.Command.DEADLINE, Parser.getCommand(new String[]{"deadline"}));
     assertEquals(UI.Command.LIST, Parser.getCommand(new String[]{"list"}));
     assertThrows(InvalidInputException.class, () -> Parser.getCommand(new String[]{"not valid"}),
       "It should throw a InvalidInputException");
+    assertEquals(UI.Command.UNMARK, Parser.getCommand(new String[]{"unmark"}));
+    assertEquals(UI.Command.MARK, Parser.getCommand(new String[]{"mark"}));
   }
 
   @Test
@@ -36,6 +41,15 @@ class ParserTest {
       Parser.extractDescriptionData(test_array_3)[0]);
     assertEquals(null,
       Parser.extractDescriptionData(test_array_3)[1]);
+    String[] test_array_4 = new String[]{"deadline", "do stuff", "/by", "date1"};
+    assertEquals("do stuff",
+      Parser.extractDescriptionData(test_array_4)[0]);
+    assertEquals("date1",
+      Parser.extractDescriptionData(test_array_4)[1]);
+    String[] test_array_5 = new String[]{"deadline", "do stuff", "date1"};
+    assertThrows(
+      InvalidParametersException.class, () -> Parser.extractDescriptionData(test_array_5),
+      "It should throw a InvalidParameterException"
+    );
   }
-
 }
