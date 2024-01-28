98f4e4fcd04c978f4ac9c2b4c57df82ce9b0c463
JOHN\Flare <derpicon12@gmail.com>
2024-01-28 12:57:25 +0800

    Split the main method to include more OOP by adding the classes Storage, TaskList, Ui and Parser.

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,5 +1,5 @@
 public class DukeException extends Exception{
-
+    public DukeException() { }
     public DukeException(String message) {
         super(message);
     }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    public List<String> parse(String input) {
+        List<String> stringList = new ArrayList<>();
+
+        String firstWord;
+        String trail = "";
+        int space = input.indexOf(" ");
+        if (space == -1) {
+            firstWord = input;
+        } else {
+            firstWord = input.substring(0, space);
+            trail = input.substring(space + 1);
+        }
+
+        stringList.add(firstWord);
+        stringList.add(trail);
+
+        return stringList;
+    }
+}
