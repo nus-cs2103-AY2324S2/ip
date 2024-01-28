27a4db1323bcaaa508f5464ab85c458d106a4fbb
jmsandiegoo <jmsandiego0710@gmail.com>
2024-01-28 13:20:29 +0800

    Refactor DateTime class

--- src/main/java/DateTime.java
+++ src/main/java/DateTime.java
@@ -2,51 +2,28 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAccessor;
 
 public class DateTime {
-    private final String inputParseFormatStr;
-    private final String outputParseFormatStr;
-    private final String outputSerializeFormatStr;
+    private final DateTimeFormatter inputFormatter;
+    private final DateTimeFormatter outputFormatter;
     private final TemporalAccessor dateTime;
 
     public DateTime(String dateTimeStr) throws DateTimeParseException {
-        this.inputParseFormatStr = "yyyy-MM-dd[ HH:mm]";
-        this.outputParseFormatStr = "MMM dd yyyy[ hh:mma] ";
-        this.outputSerializeFormatStr = "yyyy-MM-dd[ HH:mm]";
+        this.inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd[ HH:mm]");
+        this.outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy[ hh:mma]");
         this.dateTime = this.parseDateTimeString(dateTimeStr);
     }
 
     private TemporalAccessor parseDateTimeString(String dateTimeStr) throws DateTimeParseException {
-        LocalDateTime dateTime;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(this.inputParseFormatStr);
-        TemporalAccessor temporalAccessor = formatter.parseBest(dateTimeStr, LocalDateTime::from, LocalDate::from);
-        return temporalAccessor;
+        return this.inputFormatter.parseBest(dateTimeStr, LocalDateTime::from, LocalDate::from);
     }
 
     public String formatDateTime() {
-        String output;
-        if (this.dateTime instanceof LocalDateTime) {
-            output = ((LocalDateTime)dateTime)
-                    .format(java.time.format.DateTimeFormatter.ofPattern(this.outputParseFormatStr));
-        } else {
-            output = (((LocalDate)dateTime))
-                    .format(java.time.format.DateTimeFormatter.ofPattern(this.outputParseFormatStr));
-        }
-
-        return output;
+        return this.outputFormatter.format(this.dateTime);
     }
 
     public String serializeDateTime() {
-        String output;
-        if (this.dateTime instanceof LocalDateTime) {
-            output = ((LocalDateTime)dateTime)
-                    .format(java.time.format.DateTimeFormatter.ofPattern(this.outputSerializeFormatStr));
-        } else {
-            output = (((LocalDate)dateTime))
-                    .format(java.time.format.DateTimeFormatter.ofPattern(this.outputSerializeFormatStr));
-        }
-        return output;
+        return this.inputFormatter.format(this.dateTime);
     }
 }
