6cdd8b1440c63b493e30f9e9eddda6f700bcd328
jskimdev <kim.junseo1128@gmail.com>
2024-01-29 05:28:20 +0800

    Add types to task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,36 +12,79 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         */
         ArrayList<Task> storage = new ArrayList<>();
-
-        Task message;
         System.out.println("    -----------------------------------");
         System.out.println("    Hello! I'm ByteTalker");
         System.out.println("    What can I do for you?");
         System.out.println("    -----------------------------------");
         while (true) {
             Scanner userInput = new Scanner(System.in);
-            message = new Task(userInput.nextLine().strip());
-            String[] split_message = message.getMessage().split(" ");
+            String userInput_String = userInput.nextLine().strip();
+            String[] split_message = userInput_String.split(" ");
             System.out.println("    -----------------------------------");
-            if (message.getMessage().equals("bye")) {
+            if (userInput_String.equals("bye")) {
                 break;
-            } else if (message.getMessage().equals("list")) {
+            } else if (userInput_String.equals("list")) {
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < storage.size(); i++) {
-                    System.out.println("    " + (i + 1) + ".[" + storage.get(i).getStatusIcon() + "] " + storage.get(i).getMessage());
+                    System.out.println("    " + (i + 1) + "." + storage.get(i).getMessage());
                 }
             } else if (split_message[0].equals("mark")) {
                 Integer index = Integer.parseInt(split_message[1]) - 1;
                 storage.get(index).setStatus(true);
                 System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("    " + "  [" + storage.get(index).getStatusIcon() + "] " + storage.get(index).getMessage());
+                System.out.println("      " + storage.get(index).getMessage());
             } else if (split_message[0].equals("unmark")) {
                 Integer index = Integer.parseInt(split_message[1]) - 1;
                 storage.get(index).setStatus(false);
                 System.out.println("    OK, I've marked this task as not done yet:");
-                System.out.println("    " + "  [" + storage.get(index).getStatusIcon() + "] " + storage.get(index).getMessage());
+                System.out.println("      " + storage.get(index).getMessage());
             } else {
+                Task message;
+                if (split_message[0].equals("todo")) {
+                    message = Task.TODO;
+                    String task = " ";
+                    for (int i = 1; i < split_message.length; i++) {
+                        task = task + split_message[i] + " ";
+                    }
+                    task = task.strip();
+                    message.setTask(task);
+                } else if (split_message[0].equals("deadline")) {
+                    message = Task.DEADLINE;
+                    String content = " ";
+                    for (int i = 1; i < split_message.length; i++) {
+                        if (split_message[i].equals("/by")) {
+                            content = content.strip();
+                            message.setTask(content);
+                            content = "";
+                        } else {
+                            content = content + split_message[i] + " ";
+                        }
+                    }
+                    content = content.strip();
+                    message.setTo(content);
+                } else {
+                    message = Task.Event;
+                    String content = " ";
+                    for (int i = 1; i < split_message.length; i++) {
+                        if (split_message[i].equals("/from")) {
+                            content = content.strip();
+                            message.setTask(content);
+                            content = "";
+                        } else if (split_message[i].equals("/to")) {
+                            content = content.strip();
+                            message.setFrom(content);
+                            content = "";
+                        } else {
+                            content = content + split_message[i] + " ";
+                        }
+                    }
+                    content = content.strip();
+                    message.setTo(content);
+                }
                 storage.add(message);
-                System.out.println("    added: " + message.getMessage());
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("       " + message.getMessage());
+                System.out.println("    Now you have " + storage.size() + " tasks in the list.");
             }
             System.out.println("    -----------------------------------");
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,42 @@
-public class Task {
-    private String message;
+enum Task {
+    TODO("T") {
+        @Override
+        public String getMessage() {
+            return "[" + getIcon() + "]" + "[" + getStatusIcon() + "] " + getTask();
+        }
+    },
+    DEADLINE("D") {
+        @Override
+        public String getMessage() {
+            return "[" + getIcon() + "]" + "[" + getStatusIcon() + "] " + getTask() + " (by: " + getTo() + ")";
+        }
+    },
+    Event("E") {
+        @Override
+        public String getMessage() {
+            return "[" + getIcon() + "]" + "[" + getStatusIcon() + "] " + getTask() + " (from: " + getFrom() + " to:" + getTo() + ")";
+        }
+    };
+
+    private String task;
     private boolean isDone;
-    Task(String message) {
-        this.message = message;
+    private String icon;
+    private String to;
+    private String from;
+    Task(String icon) {
+        this.icon = icon;
         this.isDone = false;
     }
 
-    public String getMessage() {
-        return this.message;
+    public void setTask(String task) {
+        this.task = task;
     }
+
+    public String getTask() {
+        return this.task;
+    }
+
+    public abstract String getMessage();
 
     public String getStatusIcon() {
         return this.isDone ? "X" : " ";
@@ -16,5 +44,25 @@ public class Task {
 
     public void setStatus(boolean status) {
         this.isDone = status;
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getFrom() {
+        return this.from;
     }
 }
