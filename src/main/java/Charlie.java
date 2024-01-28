7ab7cb8c8063dfe73bc8b079615961ec632750e6
ananya21 <shahiananya50@gmail.com>
2024-01-29 01:57:20 +0800

    Add Level-2: Adding to a List

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Charlie {
     public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+
         System.out.println("Hello, I'm Charlie");
         System.out.println("What can I do for you?");
 
@@ -10,8 +16,14 @@ public class Charlie {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }
+            if (input.equals("list")) {
+                for (int i = 0; i < stringList.size(); i++) {
+                    System.out.println((i + 1) + ". " + stringList.get(i));
+                }
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                stringList.add(input);
             }
         }
     }
