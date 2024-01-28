9ac76251731139634c333968d3932ccb3a0cbaad
jonpwk <jonpwk@gmail.com>
2024-01-28 18:44:58 +0800

    add Level-3

--- src/main/java/Cro.java
+++ src/main/java/Cro.java
@@ -4,17 +4,45 @@ public class Cro {
                             + "Hello! I'm Cro!\n"
                             + "What can I do for you?\n"
                             + "-----------------------------------\n";
-    static List<String> taskList = new ArrayList<>();
+    static List<Task> taskList = new ArrayList<>();
     public static void addToTasks(String input) {
-        taskList.add(input);
+        taskList.add(new Task(input));
         System.out.println("-----------------------------------");
         System.out.println("added: " + input);
         System.out.println("-----------------------------------");
     }
     public static void displayTasks() {
         for (int i = 0; i < taskList.size(); i++) {
-            String output = String.format("%d. %s", i+1, taskList.get(i));
+            String output = String.format("%d. %s", i+1, taskList.get(i).getDisplayString());
             System.out.println(output);
+        }
+    }
+
+    public static void markTaskAsDone(int taskNo) {
+        if (taskNo > taskList.size()) {
+            System.out.println("-----------------------------------");
+            System.out.println("Task not found!");
+            System.out.println("-----------------------------------");
+        } else {
+            taskList.get(taskNo-1).markAsDone();
+            System.out.println("-----------------------------------");
+            System.out.println("Nice! I've marked this task as done!");
+            System.out.println(taskList.get(taskNo-1).getDisplayString());
+            System.out.println("-----------------------------------");
+        }
+    }
+
+    public static void markTaskAsUndone(int taskNo) {
+        if (taskNo > taskList.size()) {
+            System.out.println("-----------------------------------");
+            System.out.println("Task not found!");
+            System.out.println("-----------------------------------");
+        } else {
+            taskList.get(taskNo-1).markAsUndone();
+            System.out.println("-----------------------------------");
+            System.out.println("OK, I've marked this task as not done yet.");
+            System.out.println(taskList.get(taskNo-1).getDisplayString());
+            System.out.println("-----------------------------------");
         }
     }
     public static void main(String[] args) {
@@ -22,13 +50,18 @@ public class Cro {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String inText = sc.nextLine();
-            if (inText.equals("bye")) {
+            String[] splitStr = inText.trim().split("\\s+");
+            if (splitStr[0].equals("bye")) {
                 System.out.println("-----------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-----------------------------------");
                 break;
-            } else if (inText.equals("list")) {
+            } else if (splitStr[0].equals("list")) {
                 displayTasks();
+            } else if (splitStr[0].equals("mark")) {
+                markTaskAsDone(Integer.valueOf(splitStr[1]));
+            } else if (splitStr[0].equals("unmark")) {
+                markTaskAsUndone(Integer.valueOf(splitStr[1]));
             } else {
                 addToTasks(inText);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDisplayString() {
+        return String.format("[%s] %s", getStatusIcon(), this.description);
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
