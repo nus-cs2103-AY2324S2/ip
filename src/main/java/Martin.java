5b48b9de97d07935989fc8e017d5017d5d4aa73d
yunchao <tanyunchao@gmail.com>
2024-01-28 22:47:50 +0800

    feat: break tasks and use enum

--- src/main/java/Martin.java
+++ src/main/java/Martin.java
@@ -7,53 +7,94 @@ public class Martin {
 
     public static void main(String[] args) {
         // stop gap solution to magic numbers for task numbering
-        todoList.add(new Task("dummy offset"));
+        todoList.add(new Todo("dummy offset"));
 
         sayGreeting();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String line = sc.nextLine().strip();
             String[] words = line.split(" ");
-            String command = words[0];
-            if (command.equals("bye")) {
-                break;
-            } else if (command.equals("list")) {
-                printList();
-            } else if (command.equals("mark")) {
-                // pending error throwing for invalid input
-                if(words.length < 2) {
-                    System.out.println("Please specify a task number to mark");
-                    continue;
-                }
-                int taskNumber = Integer.parseInt(words[1]);
-                todoList.get(taskNumber).markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(todoList.get(taskNumber));
-            } else if (command.equals("unmark")) {
-                // pending error throwing for invalid input
-                if(words.length < 2) {
-                    System.out.println("Please specify a task number to mark");
-                    continue;
-                }
-                int taskNumber = Integer.parseInt(words[1]);
-                todoList.get(taskNumber).unmarkAsDone();
-                System.out.println("Okay, I've unmarked it");
-                System.out.println(todoList.get(taskNumber));
-            } else {
-                addToList(command);
+            String firstWord = words[0].toUpperCase();
+            String remainingWords = line.substring(firstWord.length());
+            try {
+                ChatbotKeyword command = ChatbotKeyword.valueOf(firstWord);
+                handleCommand(command, remainingWords);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid command: " + firstWord);
+                continue;
             }
         }
-        sayBye();
         sc.close();
     }
 
-    public static void addToList(String item) {
-        todoList.add(new Task(item));
-        System.out.println("added: " + item);
+    public static void handleCommand(ChatbotKeyword command, String inputs) {
+        String[] inputsArray = inputs.split(" ");
+        switch (command) {
+            case BYE:
+                sayBye();
+                break;
+            case LIST:
+                printList();
+                break;
+            case MARK:
+                if (inputsArray.length < 1) {
+                    System.out.println("Please specify a task number to mark");
+                    break;
+                }
+                int taskToMark = Integer.parseInt(inputsArray[0]);
+                todoList.get(taskToMark).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(todoList.get(taskToMark));
+                break;
+            case UNMARK:
+                if (inputsArray.length < 1) {
+                    System.out.println("Please specify a task number to unmark");
+                    break;
+                }
+                int taskToUnmark = Integer.parseInt(inputsArray[0]);
+                todoList.get(taskToUnmark).unmarkAsDone();
+                System.out.println("Okay, I've unmarked it");
+                System.out.println(todoList.get(taskToUnmark));
+                break;
+            case TODO:
+                if (inputsArray.length < 1) {
+                    System.out.println("Please specify a todo description");
+                    break;
+                }
+                String todoDescription = String.join(" ", inputs);
+                Todo todo = new Todo(todoDescription);
+                todoList.add(todo);
+                System.out.println("Got it. I've added this todo: " + todoDescription);
+                break;
+            case EVENT:
+                if (inputsArray.length < 3) {
+                    System.out.println("Please specify a event description, from, and to");
+                    break;
+                }
+                String eventDescription = String.join(" ", inputs);
+                String from = inputsArray[1];
+                String to = inputsArray[2];
+                Event event = new Event(eventDescription, from, to);
+                todoList.add(event);
+                System.out.println("Got it. I've added this event: " + eventDescription);
+                break;
+            case DEADLINE:
+                if (inputsArray.length < 2) {
+                    System.out.println("Please specify a deadline description and deadline");
+                    break;
+                }
+                String deadlineDescription = String.join(" ", inputs);
+                String deadline = inputsArray[1];
+                Deadline deadlineTask = new Deadline(deadlineDescription, deadline);
+                todoList.add(deadlineTask);
+                System.out.println("Got it. I've added this deadline: " + deadlineDescription);
+                break;
+        }
+
     }
 
     public static void printList() {
-        // 1-indexed todolist 
+        // 1-indexed todolist
         for (int i = 1; i < todoList.size(); i++) {
             System.out.println(i + "." + todoList.get(i));
         }
