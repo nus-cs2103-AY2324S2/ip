139ca93fa772dd4c9b09a49f8f4b1b3ca6bdbfb7
macareonie <ryanchiang189@gmail.com>
2024-01-28 15:51:18 +0800

    Refactor closely related code as classes
    Source code contains code handling many different aspects in the same
    files.
    
    Separating code into their respective aspects is more readable
    and allows for tidy code.
    
    Let's,
    * Abstract all the code into 4 main classes: Ui, Storage, Parser and
      TaskList

---
+++ src/main/java/Parser.java
@@ -0,0 +1,34 @@
+public class Parser {
+
+    public static String name;
+    public Parser(String name) {
+        Parser.name = name;
+    }
+
+    public void execute() throws GeneralException {
+
+        if (name.contains("unmark")) {
+            TaskList.unmark();
+        } else if (name.contains("mark")) {
+            TaskList.mark();
+
+        } else if (name.contains("delete")) {
+            TaskList.delete();
+
+        } else if (name.contains("list")) {
+            TaskList.list();
+        } else if (name.contains("deadline")) {
+            TaskList.addDeadline();
+        } else if (name.contains("event")) {
+            TaskList.addEvent();
+        } else if (name.contains("todo")) {
+            TaskList.addToDo();
+        } else {
+            System.out.println("Don't talk to me!\nGive me proper instructions!");
+        }
+
+    }
+
+
+
+}
--- src/main/java/Tsundere.java
+++ src/main/java/Tsundere.java
@@ -1,72 +1,23 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Tsundere {
 
-
-
-    public static final Scanner sc = new Scanner(System.in);
-
-    public static ArrayList<Task> taskList = new ArrayList<>();
-
-    public static final String filepath = "./data/data.txt";
-
-    public static void greet() {
-        System.out.println("_________________________________________________________________________________\n" +
-                            "Don't get the wrong idea!\n" +
-                            "I'm not doing this to help you or anything!\n" +
-                            "_________________________________________________________________________________\n");
+    private Storage storage;
+    private Ui ui;
+    public Tsundere() {
+        storage = new Storage();
+        ui = new Ui();
     }
 
-    public static void exit() {
-        System.out.println("_________________________________________________________________________________\n" +
-                            //"Don't forget about me!\n" +
-                            "You better come back soon!\n" +
-                            "_________________________________________________________________________________");
-    }
-
-    public static void echo() {
-        String str = sc.nextLine();
-        while (!str.equals("bye")) {
-            System.out.println("_________________________________________________________________________________\n" +
-                                str +
-                                "\n_________________________________________________________________________________\n");
-            str = sc.nextLine();
-        }
-        exit();
-    }
-
-    public static void chat() {
-        String str = sc.nextLine();
-        while (!str.equals("bye")) {
-            System.out.println("_________________________________________________________________________________");
-
-            Action action = new Action(str);
-            try {
-                action.execute();
-            } catch (GeneralException e) {
-                System.out.println(e);
-            }
-            System.out.println("_________________________________________________________________________________\n");
-            str = sc.nextLine();
-        }
-        exit();
-    }
-    public static void main(String[] args) {
-
+    public void run() {
+        ui.chat();
         try {
-            taskList = TaskManager.loadTasksFromFile(filepath);
-        } catch (IOException | GeneralException e) {
-            System.out.println("Something went wrong with loading your previous session data!");
-        }
-        greet();
-        chat();
-        try {
-            TaskManager.saveTasksToFile(taskList, filepath);
+            storage.saveTasksToFile();
         } catch (IOException e) {
             System.out.println("Something went wrong with saving your current session data!");
         }
-
+    }
+    public static void main(String[] args) {
+        new Tsundere().run();
     }
 }
