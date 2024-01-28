de18a2b2320da018d59252fe1dc1b7e2107cdba2
AwJoey <awjoey26@gmail.com>
2024-01-29 00:18:09 +0800

    Reapply "Implemented enumerators for command types for easy additiona nd modification in future"
    This reverts commit 385595d0df1d9034a8405eed0ce71246cd167fe0.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,24 @@
 import java.io.*;
 import java.util.*;
+
+enum Command {
+    TODO("todo", "todo <task_name>"),
+    DEADLINE("deadline", "deadline <task_name> /by <due_date>"),
+    EVENT("event","event <task_name> /from <start_date> /to <end_date"),
+    LIST("list", "list"),
+    MARK ("mark", "mark <task_number>"),
+    UNMARK("unmark", "unmark <task_number>"),
+    DELETE("delete", "delete <task_number>"),
+    TERMINATE("bye", "bye");
+
+    public final String commandName;
+    public final String commandSignature;
+
+    private Command(String commandName, String commandSignature) {
+        this.commandName = commandName;
+        this.commandSignature = commandSignature;
+    }
+}
 public class Duke {
     private static Scanner sc = new Scanner(System.in);
 
@@ -12,7 +31,7 @@ public class Duke {
         String input = sc.nextLine();
         ArrayList<Task> list = new ArrayList<>();
 
-        while (!input.equals("bye")) {
+        while (!input.equals(Command.TERMINATE.commandName)) {
             int cmdSplit = input.indexOf(" ");
             String command = input;
             String task = null;
@@ -21,12 +40,12 @@ public class Duke {
                     command = input.substring(0, cmdSplit);
                     task = input.substring(input.indexOf(" ") + 1);
                 } else {
-                    if (command.equals("mark") | command.equals("unmark") | command.equals("delete")) {
+                    if (command.equals(Command.MARK.commandName) | command.equals(Command.UNMARK.commandName) | command.equals(Command.DELETE.commandName)) {
                         System.out.println("-------------------------------- \n" +
                                 "Oops, I'm not sure which task you are referring to! Please indicate a task number (e.g. " + command + " 1) \n" +
                                 "-------------------------------- \n");
                         continue;
-                    } else if (command.equals("todo")) {
+                    } else if (command.equals(Command.TODO.commandName)) {
                         System.out.println("-------------------------------- \n" +
                                 "Oops, wrong format! Please indicate task details (e.g. todo CS2103 Lab 1) \n" +
                                 "-------------------------------- \n");
@@ -34,9 +53,9 @@ public class Duke {
                     }
                 }
 
-                if (command.equals("list")) {
+                if (command.equals(Command.LIST.commandName)) {
                     printTaskList(list);
-                } else if (command.equals("mark") | command.equals("unmark") | command.equals("delete")) {
+                } else if (command.equals(Command.MARK.commandName) | command.equals(Command.UNMARK.commandName) | command.equals(Command.DELETE.commandName)) {
                     int taskNo = Integer.parseInt(task) - 1;
 
                     if (taskNo >= list.size()) {
@@ -47,63 +66,58 @@ public class Duke {
                         System.out.println("-------------------------------- \n");
                     } else {
                         Task t = list.get(taskNo);
-                        switch (command) {
-                            case "mark":
-                                t.done();
-                                System.out.println("-------------------------------- \n" +
-                                        "Nice! I've marked task " + task + " as done: \n" +
-                                        t.toString() + "\n" +
-                                        "-------------------------------- \n");
-                                break;
-                            case "unmark":
-                                t.undone();
-                                System.out.println("-------------------------------- \n" +
-                                        "Sure, I've marked task " + task + " as not done yet: \n" +
-                                        t.toString() + "\n" +
-                                        "-------------------------------- \n");
-                                break;
-                            case "delete":
-                                list.remove(taskNo);
-                                System.out.println("-------------------------------- \n" +
-                                        "Okay, I will delete this task: \n" +
-                                        t.toString() + "\n" +
-                                        "You now have " + list.size() + " in the list. \n" +
-                                        "-------------------------------- \n");
-                                break;
+                        if (command.equals(Command.MARK.commandName)) {
+                            t.done();
+                            System.out.println("-------------------------------- \n" +
+                                    "Nice! I've marked task " + task + " as done: \n" +
+                                    t.toString() + "\n" +
+                                    "-------------------------------- \n");
+                        }
+                        else if (command.equals(Command.UNMARK.commandName)) {
+                            t.undone();
+                            System.out.println("-------------------------------- \n" +
+                                    "Sure, I've marked task " + task + " as not done yet: \n" +
+                                    t.toString() + "\n" +
+                                    "-------------------------------- \n");
+                        }
+                        else {
+                            list.remove(taskNo);
+                            System.out.println("-------------------------------- \n" +
+                                    "Okay, I will delete this task: \n" +
+                                    t.toString() + "\n" +
+                                    "You now have " + list.size() + " in the list. \n" +
+                                    "-------------------------------- \n");
                         }
                     }
-                } else if (command.equals("todo") | command.equals("deadline") | command.equals("event")) {
+                } else if (command.equals(Command.TODO.commandName) | command.equals(Command.DEADLINE.commandName) | command.equals(Command.EVENT.commandName)) {
                     Task t = null;
                     boolean success = true;
-                    switch (command) {
-                        case "todo":
-                            t = new ToDo(task);
-                            break;
-                        case "deadline":
-                            if (task == null || !task.contains(" /by ")) {
-                                success = false;
-                                System.out.println("-------------------------------- \n" +
-                                        "Oops, wrong format! Please follow this format for deadline task entries (e.g. deadline submit report /by 11/10/2019 5pm ) \n" +
-                                        "-------------------------------- \n");
-                            } else {
-                                String deadline[] = task.split(" /by ");
-                                t = new Deadline(deadline[0], deadline[1]);
-                            }
-                            break;
-                        case "event":
-                            if (task == null || !(task.contains(" /from ") && task.contains(" /to "))) {
-                                success = false;
-                                System.out.println("-------------------------------- \n" +
-                                        "Oops, wrong format! Please follow this format for event task entries (e.g. event team project meeting /from June 9th 2pm /to 4pm ) \n" +
-                                        "-------------------------------- \n");
-                            } else {
-                                String event = task.substring(0, task.indexOf(" /from "));
-                                String from = task.substring(task.indexOf("/from ") + 6, task.indexOf(" /to "));
-                                String to = task.substring((task.indexOf("/to ") + 4));
-                                t = new Event(event, from, to);
-                            }
-                            break;
-
+                    if (command.equals(Command.TODO.commandName)) {
+                        t = new ToDo(task);
+                    }
+                    else if (command.equals(Command.DEADLINE.commandName)) {
+                        if (task == null || !task.contains(" /by ")) {
+                            success = false;
+                            System.out.println("-------------------------------- \n" +
+                                    "Oops, wrong format! Please follow this format for deadline task entries (e.g. deadline submit report /by 11/10/2019 5pm ) \n" +
+                                    "-------------------------------- \n");
+                        } else {
+                            String[] deadline = task.split(" /by ");
+                            t = new Deadline(deadline[0], deadline[1]);
+                        }
+                    }
+                    else {
+                        if (task == null || !(task.contains(" /from ") && task.contains(" /to "))) {
+                            success = false;
+                            System.out.println("-------------------------------- \n" +
+                                    "Oops, wrong format! Please follow this format for event task entries (e.g. event team project meeting /from June 9th 2pm /to 4pm ) \n" +
+                                    "-------------------------------- \n");
+                        } else {
+                            String event = task.substring(0, task.indexOf(" /from "));
+                            String from = task.substring(task.indexOf("/from ") + 6, task.indexOf(" /to "));
+                            String to = task.substring((task.indexOf("/to ") + 4));
+                            t = new Event(event, from, to);
+                        }
                     }
                     if (success) {
                         list.add(t);
@@ -114,16 +128,13 @@ public class Duke {
                                 "-------------------------------- \n");
                     }
                 } else {
-                    System.out.println("-------------------------------- \n" +
-                            "Oops, I'm not sure what you meant by that! Commands you can use: \n" +
-                            "todo <task_name> \n" +
-                            "deadline <task_name> /by <due_date> \n" +
-                            "event <task_name> /from <start_date> /to <end_date \n" +
-                            "list \n" +
-                            "mark <task_number> \n" +
-                            "unmark <task_number> \n" +
-                            "delete <task_number> \n" +
-                            "-------------------------------- \n");
+                    System.out.println( "-------------------------------- \n" +
+                                        "Oops, I'm not sure what you meant by that! Commands available:");
+                    for (Command c: EnumSet.allOf(Command.class)) {
+                        System.out.println(c.commandSignature);
+                    }
+
+                    System.out.println("-------------------------------- \n");
                 }
             }
             finally {
