80ea6e42020ef977614f9ba138aac8d8e1120121
kinjalagarwal1810 <105915730+kinjalagarwal1810@users.noreply.github.com>
2024-01-28 10:42:54 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Declare Scanner instance
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String chatbotName = "Jamie";
@@ -19,14 +19,60 @@ public class Duke {
 
             } else if (userInput.equalsIgnoreCase("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "]" + tasks[i].getDescription());
+                }
+
+            } else if (userInput.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println("Nice! I've marked this task as done: \n [" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex].getDescription());
+
+                } else {
+                    System.out.println("Invalid task index.");
+                }
+
+            } else if (userInput.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) -1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsUndone();
+                    System.out.println("Ok, I've marked this task as not done yet: \n [" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex].getDescription());
+                } else {
+                    System.out.println("Invalid task index.");
                 }
 
             } else {
-                tasks[taskCount++] = userInput;
+                tasks[taskCount++] = new Task (userInput);
                 System.out.println(" added: " + userInput);
             }
         }
         scanner.close(); // Close the Scanner to avoid resource leaks
+    }
+}
+
+
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
     }
 }
