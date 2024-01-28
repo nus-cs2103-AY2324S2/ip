39b41c94d636fac8656850bba86e275d4924ddf4
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-01-28 22:53:09 +0800

    Refactor CLI UI & parser to use new command classes

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -4,6 +4,13 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
+import duke.commands.Command;
+import duke.commands.ExitCommand;
+import duke.commands.ListCommand;
+import duke.exceptions.DukeException;
+import duke.exceptions.InvalidArgumentException;
+import duke.exceptions.InvalidCommandException;
+
 /**
  * The Parser class handles the making sense of user commands
  *
@@ -27,5 +34,44 @@ public class Parser {
                 Integer.parseInt(time.substring(0, 2)),
                 Integer.parseInt(time.substring(3, 5)))
                 .toInstant(OffsetDateTime.now().getOffset());
+    }
+
+    /**
+     * Parse a string and return a command if valid
+     *
+     * @param input String to be parsed
+     * @return A valid command for the application
+     */
+    public static Command parse(String input) throws DukeException {
+        String[] splitInput = input.split(" ");
+        switch (splitInput[0].toLowerCase()) {
+        case "bye": // Exit
+            return new ExitCommand();
+
+        case "list":
+            // Check if date filter exists
+            if (splitInput.length > 1) {
+                switch (splitInput[1]) {
+                case "/date":
+                    try {
+                        Instant filterDate = userDateToInstant(splitInput[2], "00:00");
+
+                        return new ListCommand(filterDate);
+                    } catch (NumberFormatException | StringIndexOutOfBoundsException
+                            | ArrayIndexOutOfBoundsException e) {
+                        throw new InvalidArgumentException(
+                                "Date/time format is invalid. Please enter the date/time in the format 'YYYY/MM/DD'");
+                    }
+
+                default:
+                    throw new InvalidArgumentException(
+                            String.format("Unknown argument '%s' for the 'list' command", splitInput[1]));
+                }
+            } else { // Return full list
+                return new ListCommand();
+            }
+        default:
+            throw new InvalidCommandException(String.format("Unknown command '%s'", splitInput[0]));
+        }
     }
 }
--- src/main/java/duke/ui/Cli.java
+++ src/main/java/duke/ui/Cli.java
@@ -1,12 +1,10 @@
 package duke.ui;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-import duke.exceptions.InvalidArgumentException;
-import duke.exceptions.MissingArgumentException;
+import duke.commands.Command;
+import duke.exceptions.DukeException;
 import duke.parser.Parser;
-import duke.storage.Task;
 import duke.storage.TaskList;
 
 /**
@@ -17,120 +15,145 @@ import duke.storage.TaskList;
  */
 public class Cli extends Ui {
     /**
+     * Shared scanner for user input
+     */
+    private final Scanner sc = new Scanner(System.in);
+
+    /**
      * Displays the CLI UI of the application
      */
     @Override
     public void startUI(TaskList taskList) {
-        // UI Greeting
-        String greeting = "------------------------------------------------------------\n" +
-                "Hello! I'm Ciara\n" +
-                "What can I do for you?\n" +
-                "------------------------------------------------------------";
+        // Print greeting
+        printGreeting();
 
-        // UI Goodbye message
-        String goodbye = "Bye. Hope to see you again soon!";
+        boolean isExit = false;
 
-        // Display greeting
-        System.out.println(greeting);
-
-        // Scanner for getting user input
-        Scanner sc = new Scanner(System.in);
-
-        String input;
-        // Store and echo user input
-        do {
-            input = sc.nextLine();
-            String[] splitInput = input.split(" ");
-
-            System.out.println("------------------------------------------------------------");
-
+        while (!isExit) {
             try {
-                switch (splitInput[0].toLowerCase()) {
-                case "bye": // Exit
-                    // Print exit message
-                    System.out.println(goodbye);
-                    break;
+                // Get user input
+                String input = readCommand();
+                printDividerLine();
 
-                case "list": // List items
-                    String tasks;
+                // Parse user input
+                Command command = Parser.parse(input);
 
-                    // Check if date filter exists
-                    if (splitInput.length > 1) {
-                        switch (splitInput[1]) {
-                        case "/date":
-                            try {
-                                tasks = taskList.getTasks(Parser.userDateToInstant(splitInput[2], "00:00"));
-                            } catch (StringIndexOutOfBoundsException e) {
-                                throw new InvalidArgumentException(
-                                        "Date/time format is invalid. Please enter the date/time in the format 'YYYY/MM/DD'");
-                            }
-                            break;
+                // Execute command
+                command.execute(taskList, this);
 
-                        default:
-                            throw new InvalidArgumentException(
-                                    String.format("Unknown argument '%s' for the 'list' command", splitInput[1]));
-                        }
-                    } else {
-                        tasks = taskList.getTasks();
-                    }
-
-                    System.out.print(tasks);
-                    break;
-
-                case "mark": // Mark item
-                    if (splitInput.length <= 1) {
-                        throw new MissingArgumentException("Missing argument - Index of task required");
-                    }
-
-                    // Print success message
-                    System.out.println("Nice! I've marked this task as done:");
-                    Task markedTask = taskList.markTask(Integer.parseInt(splitInput[1]) - 1);
-                    System.out.println(String.format("  %s", markedTask.toString()));
-                    break;
-
-                case "unmark": // Unmark item
-                    if (splitInput.length <= 1) {
-                        throw new MissingArgumentException("Missing argument - Index of task required");
-                    }
-
-                    // Print success message
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    Task unmarkedTask = taskList.unmarkTask(Integer.parseInt(splitInput[1]) - 1);
-                    System.out.println(String.format("  %s", unmarkedTask.toString()));
-                    break;
-
-                case "delete": // Delete item
-                    if (splitInput.length <= 1) {
-                        throw new MissingArgumentException("Missing argument - Index of task required");
-                    }
-
-                    // Print success message
-                    System.out.println("Noted. I've removed this task:");
-                    Task deletedTask = taskList.deleteTask(Integer.parseInt(splitInput[1]) - 1);
-                    System.out.println(String.format("  %s", deletedTask.toString()));
-                    System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
-
-                    break;
-
-                default: // Store and echo items
-                    // Store item
-                    taskList.addTask(splitInput[0], Arrays.copyOfRange(splitInput, 1, splitInput.length));
-                    break;
-                }
-            } catch (Exception exception) {
-                System.out.println(String.format("ERROR: %s", exception.getMessage()));
+                // Exit (if applicable)
+                isExit = command.isExit();
+            } catch (DukeException e) {
+                printError(e.getMessage());
+            } finally {
+                printDividerLine();
             }
-
-            System.out.println("------------------------------------------------------------");
-        } while (!input.equals("bye"));
+        }
 
         sc.close();
+
+        // // Store and echo user input
+        // do {
+        // input = sc.nextLine();
+        // String[] splitInput = input.split(" ");
+
+        // System.out.println("------------------------------------------------------------");
+
+        // try {
+        // switch (splitInput[0].toLowerCase()) {
+        // case "mark": // Mark item
+        // if (splitInput.length <= 1) {
+        // throw new MissingArgumentException("Missing argument - Index of task
+        // required");
+        // }
+
+        // // Print success message
+        // System.out.println("Nice! I've marked this task as done:");
+        // Task markedTask = taskList.markTask(Integer.parseInt(splitInput[1]) - 1);
+        // System.out.println(String.format(" %s", markedTask.toString()));
+        // break;
+
+        // case "unmark": // Unmark item
+        // if (splitInput.length <= 1) {
+        // throw new MissingArgumentException("Missing argument - Index of task
+        // required");
+        // }
+
+        // // Print success message
+        // System.out.println("OK, I've marked this task as not done yet:");
+        // Task unmarkedTask = taskList.unmarkTask(Integer.parseInt(splitInput[1]) - 1);
+        // System.out.println(String.format(" %s", unmarkedTask.toString()));
+        // break;
+
+        // case "delete": // Delete item
+        // if (splitInput.length <= 1) {
+        // throw new MissingArgumentException("Missing argument - Index of task
+        // required");
+        // }
+
+        // // Print success message
+        // System.out.println("Noted. I've removed this task:");
+        // Task deletedTask = taskList.deleteTask(Integer.parseInt(splitInput[1]) - 1);
+        // System.out.println(String.format(" %s", deletedTask.toString()));
+        // System.out.println(String.format("Now you have %d tasks in the list.",
+        // taskList.size()));
+
+        // break;
+
+        // default: // Store and echo items
+        // // Store item
+        // taskList.addTask(splitInput[0], Arrays.copyOfRange(splitInput, 1,
+        // splitInput.length));
+        // break;
+        // }
+        // } catch (Exception exception) {
+        // System.out.println(String.format("ERROR: %s", exception.getMessage()));
+        // }
+
+        // System.out.println("------------------------------------------------------------");
+        // } while (!input.equals("bye"));
     }
 
     /**
-     * Displays warning when application fails to load tasks from file
+     * Print warning on standard output when application fails to load tasks from
+     * file
      */
-    public static void displayLoadFromFileWarning() {
+    public static void printLoadFromFileWarning() {
         System.out.println("WARNING: Failed to load from file, starting with empty list");
+    }
+
+    /**
+     * Get user input and return a complete command for the application
+     */
+    private String readCommand() {
+        return sc.nextLine();
+    }
+
+    /**
+     * Print greeting on standard output
+     */
+    private void printGreeting() {
+        // UI Greeting
+        String greeting = "Hello! I'm Ciara\n"
+                + "What can I do for you?";
+
+        // Display greeting
+        printDividerLine();
+        System.out.println(greeting);
+        printDividerLine();
+    }
+
+    /**
+     * Print divider line on standard output
+     */
+    private void printDividerLine() {
+        System.out.println("------------------------------------------------------------");
+    }
+
+    /**
+     * Print error on standard output
+     */
+    private void printError(String error) {
+        System.out.println(String.format("ERROR: %s", error));
     }
 }
