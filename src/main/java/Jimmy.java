b3a27d4967fa86d3984e65f61ea3ad0115fe6f86
yuechen2001 <zhuyuechen@gmail.com>
2024-01-29 00:35:03 +0800

    Level 6. Delete

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,12 +1,14 @@
-public class Jimmy {
-    private final Task[] list = new Task[100];
-    private int counter = 0;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public void greetUser() {
+public class Jimmy {
+    private static final ArrayList<Task> list = new ArrayList<>();
+
+    private static void greetUser() {
         System.out.println("Hello! I'm Jimmy\nWhat can I do for you?\n");
     }
 
-    public void createNewTask(String instruction, String details) throws JimmyException {
+    private static void createNewTask(String instruction, String details) throws JimmyException {
         Task newTask = null;
         switch (instruction) {
             case "todo":
@@ -20,18 +22,17 @@ public class Jimmy {
                 break;
         }
 
-        list[counter] = newTask;
-        counter++;
+        list.add(newTask);
         System.out.println("Got it. I've added this task:");
         System.out.println(newTask);
-        System.out.println(this.generateListCounter() + "\n");
+        System.out.println(generateListCounter() + "\n");
     }
 
-    private Todo createNewTodo(String details) {
+    private static Todo createNewTodo(String details) {
         return new Todo(details);
     }
 
-    private Deadline createNewDeadline(String details) throws JimmyException {
+    private static Deadline createNewDeadline(String details) throws JimmyException {
         if (!details.contains(" /by ")) {
             throw new JimmyException("Sorry! " +
                     "Please use the correct format when creating a new deadline.");
@@ -44,7 +45,7 @@ public class Jimmy {
         return new Deadline(deadlineName, deadline);
     }
 
-    private Event createNewEvent(String details) throws JimmyException {
+    private static Event createNewEvent(String details) throws JimmyException {
         if (!details.contains(" /from ") || !details.contains(" /to ")) {
             throw new JimmyException("Sorry! " +
                     "Please use the correct format when creating a new event.");
@@ -57,54 +58,122 @@ public class Jimmy {
         return new Event(eventName, start, end);
     }
 
-    public String generateListCounter() {
-        if (counter == 0) {
+    private static String generateListCounter() {
+        if (list.size() == 0) {
             return "You have no tasks, create some now!";
-        } else if (counter == 1) {
+        } else if (list.size() == 1) {
             return "Now you have 1 task in the list.";
         } else {
-            return "Now you have " + counter + " tasks in the list.";
+            return "Now you have " + list.size() + " tasks in the list.";
         }
     }
 
-    public void displayTasks() {
+    private static void deleteTask(String taskIndex) throws JimmyException {
+        int deleteTask;
+        try {
+            deleteTask = Integer.parseInt(taskIndex) - 1;
+        } catch (NumberFormatException e) {
+            throw new JimmyException("Please only enter an integer.");
+        }
+        if (deleteTask < 0) {
+            throw new JimmyException("Please only enter a positive integer.");
+
+        } else if (deleteTask >= getListSize()) {
+            throw new JimmyException("The task you are looking for does not exist.");
+        }
+        Task deletedTask = list.remove(deleteTask);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(deletedTask);
+        System.out.println(generateListCounter() + "\n");
+    }
+
+    private static void displayTasks() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < counter; i++) {
-            if (i == counter - 1) {
-                System.out.println((i + 1) + "." + list[i].toString() + "\n");
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                System.out.println((i + 1) + "." + list.get(i).toString() + "\n");
             } else {
-                System.out.println((i + 1) + "." + list[i].toString());
+                System.out.println((i + 1) + "." + list.get(i).toString());
             }
         }
     }
 
-    public void markTaskComplete(int taskIndex) throws JimmyException {
-        if (taskIndex < 0) {
+    private static void markTaskComplete(String taskIndex) throws JimmyException {
+        int completeTask;
+        try {
+            completeTask = Integer.parseInt(taskIndex) - 1;
+        } catch (NumberFormatException e) {
+            throw new JimmyException("Please only enter an integer.");
+        }
+        if (completeTask < 0) {
             throw new JimmyException("Please only enter a positive integer.");
 
-        } else if (taskIndex >= this.getListSize()) {
+        } else if (completeTask >= getListSize()) {
             throw new JimmyException("The task you are looking for does not exist.");
         }
-        Task curr = list[taskIndex];
+        Task curr = list.get(completeTask);
         curr.markAsComplete();
     }
 
-    public void markTaskIncomplete(int taskIndex) throws JimmyException {
-        if (taskIndex < 0) {
+    private static void markTaskIncomplete(String taskIndex) throws JimmyException {
+        int incompleteTask;
+        try {
+            incompleteTask = Integer.parseInt(taskIndex) - 1;
+        } catch (NumberFormatException e) {
+            throw new JimmyException("Please only enter an integer.");
+        }
+        if (incompleteTask < 0) {
             throw new JimmyException("Please only enter a positive integer.");
 
-        } else if (taskIndex >= this.getListSize()) {
+        } else if (incompleteTask >= getListSize()) {
             throw new JimmyException("The task you are looking for does not exist.");
         }
-        Task curr = list[taskIndex];
+        Task curr = list.get(incompleteTask);
         curr.markAsIncomplete();
     }
 
-    public int getListSize() {
-        return counter;
+    private static int getListSize() {
+        return list.size();
     }
 
-    public void exit() {
+    private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) throws JimmyException{
+        Scanner sc = new Scanner(System.in);
+        greetUser();
+
+        while (true) {
+            String userInput = sc.nextLine();
+            String[] inputArray = userInput.split(" ", 2);
+            String instruction;
+            String details = "";
+            if (inputArray.length > 1) {
+                instruction = inputArray[0];
+                details = inputArray[1];
+            } else {
+                instruction = inputArray[0];
+            }
+            switch (instruction) {
+                case "bye":
+                    exit();
+                    return;
+                case "list":
+                    displayTasks();
+                    break;
+                case "mark":
+                    markTaskComplete(details);
+                    break;
+                case "unmark":
+                    markTaskIncomplete(details);
+                    break;
+                case "delete":
+                    deleteTask(details);
+                    break;
+                default:
+                    createNewTask(instruction, details);
+            }
+        }
     }
 }
