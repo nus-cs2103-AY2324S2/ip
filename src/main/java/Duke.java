bd09f26af219f15dd20f23932febb75075073978
jjchee7 <jingjie.chee@u.nus.edu>
2024-01-29 01:52:19 +0800

    Add user commands echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String NAME = "Arctic";
     private static final Integer BORDER_LEN = 60;
@@ -5,20 +7,45 @@ public class Duke {
     private static String duplicateChar(Character c, Integer num) {
         return String.valueOf(c).repeat(num);
     }
+
+    private static String getBorder() {
+        return duplicateChar('_', BORDER_LEN);
+    }
+
     private static void userGreeting () {
-        System.out.println(duplicateChar('_', BORDER_LEN));
+        System.out.println(getBorder());
         System.out.printf("Hello! I'm %s\n", NAME);
         System.out.println("What can I do for you?");
-        System.out.println(duplicateChar('_', BORDER_LEN));
+        System.out.println(getBorder());
     }
 
     private static void userFarewell() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(duplicateChar('_', BORDER_LEN));
+        System.out.println(getBorder());
+    }
+
+    private static void echoCommandOutput(String command) {
+        System.out.println(getBorder());
+        System.out.println(command);
+        System.out.println(getBorder());
+    }
+
+    private static void handleCommands() {
+        Scanner scnr = new Scanner(System.in);
+        while (true) {
+            String command = scnr.nextLine();
+            if (command.equals("bye")) {
+                echoCommandOutput("Bye. Hope to see you again soon!");
+                break;
+            }
+            echoCommandOutput(command);
+        }
     }
 
     public static void main(String[] args) {
         userGreeting();
         userFarewell();
+
+        handleCommands();
     }
 }
