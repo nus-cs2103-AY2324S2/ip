203af478890492c26148fc000bb56ed7557326a8
macareonie <ryanchiang189@gmail.com>
2024-01-28 22:41:48 +0800

    Add JavaDoc to source code
    JavaDoc allows for ease of understanding code.

--- src/main/java/tsundere/task/Task.java
+++ src/main/java/tsundere/task/Task.java
@@ -4,29 +4,55 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Initializes Task with its name.
+     * Completion status is false by default.
+     *
+     * @param description Name of Task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-
+    /**
+     * Returns completion status of Task.
+     *
+     * @return X if Task is done, blank otherwise.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Sets Task completion status to true.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Sets Task completion status to false.
+     */
     public void unMark() {
         this.isDone = false;
     }
 
+    /**
+     * Returns formatted String for storage purposes.
+     *
+     * @return formatted saveString.
+     */
     public String toSaveString() {
         int x = this.isDone ? 1 : 0;
         return x + "," + this.description;
     }
 
+    /**
+     * Returns String representation of Task.
+     *
+     * @return String representation of Task.
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
