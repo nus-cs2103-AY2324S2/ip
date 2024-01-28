8316d9c42a8aa1900c22e4d975ea57d9a4e8ee79
ongzhili <ongchili234@gmail.com>
2024-01-28 16:48:06 +0800

    Add some plaintext file reading functionality 1. Create plaintext file if does not exist 2. Read plaintext file contents

--- src/main/java/Lamball.java
+++ src/main/java/Lamball.java
@@ -1,5 +1,9 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Lamball {
     String indent = "    ____________________________________________________________\n";
     String welcomeMessage = "    Lamball\n" +
@@ -12,6 +16,7 @@ public class Lamball {
             "    ____________________________________________________________\n";
 
     ArrayList<Task> tasks;
+    File savedList;
     public Lamball() {
         tasks = new ArrayList<>();
     }
@@ -32,7 +37,7 @@ public class Lamball {
         System.out.println(indent);
     }
 
-    public void mark(String[] parts) throws LamballParseException {
+    private void mark(String[] parts, boolean isInit) throws LamballParseException {
         // If not numeric
         if (parts.length < 2 || !parts[1].matches("-?\\d+")) {
             throw new LamballParseException(indent + "    Invalid number, baa.\n" + indent);
@@ -42,13 +47,15 @@ public class Lamball {
         if (idx >= tasks.size() || idx < 0) {
             throw new LamballParseException(indent + "    Taaask index out of range, baa.\n" + indent);
         }
-        System.out.println(indent + "    I have maaarked the task as done:\n");
         Task temp = tasks.get(idx);
         temp.mark();
-        System.out.println("        " + temp.toString() + "\n" + indent);
+        if (!isInit) {
+            System.out.println(indent + "    I have maaarked the task as done:\n");
+            System.out.println("        " + temp.toString() + "\n" + indent);
+        }
     }
 
-    public void unMark(String[] parts) throws LamballParseException {
+    private void unMark(String[] parts) throws LamballParseException {
         // If not numeric
         if (parts.length < 2 || !parts[1].matches("-?\\d+")) {
             throw new LamballParseException(indent + "    Invalid number, baa.\n" + indent);
@@ -64,44 +71,57 @@ public class Lamball {
         System.out.println("        " + temp.toString() + "\n" + indent);
     }
 
-    public void toDo(String[] parts) throws LamballParseException {
+    private void toDo(String[] parts, boolean isInit) throws LamballParseException {
         // Checks if empty string (nothing after command) or only whitespaces
         if (parts.length < 2 || parts[1] == null || parts[1].trim().isEmpty()) {
             throw new LamballParseException(indent + "    Your todo field is empty, baaaaka.\n" + indent);
         }
         Task temp = new ToDo(parts[1]);
         tasks.add(temp);
-        System.out.println(indent + "    Added ToDo:\n        " + temp.toString() + "\n    Now you have " + tasks.size() + " tasks in the list.\n" + indent);
+        if (!isInit) {
+            System.out.println(indent + "    Added ToDo:\n        " + temp.toString() + "\n    Now you have " + tasks.size()
+                    + " tasks in the list.\n" + indent);
+        }
     }
-    public void deadline(String[] parts) throws LamballParseException {
+    private void deadline(String[] parts, boolean isInit) throws LamballParseException {
         // Checks if empty string (nothing after command) or only whitespaces
         if (parts.length < 2 || parts[1] == null || parts[1].trim().isEmpty()) {
             throw new LamballParseException(indent + "    Your deadline field is empty, baaaaka.\n" + indent);
         }
         String[] furtherSplit = parts[1].split(" /", 2);
         if (furtherSplit.length < 2 || !furtherSplit[1].substring(0,3).equals("by ")) {
-            throw new LamballParseException(indent + "    Deadline is in the wrong formaaaaaaat, baa. :(\n    Correct formaaat is: deadline <name> /by <time>, baa.\n" + indent);
+            throw new LamballParseException(indent + "    Deadline is in the wrong formaaaaaaat, baa. :(\n    Correct fo" +
+                    "rmaaat is: deadline <name> /by <time>, baa.\n" + indent);
         }
         Task temp = new Deadline(furtherSplit[0], furtherSplit[1].replaceFirst("by ", ""));
         tasks.add(temp);
-        System.out.println(indent + "    Added Deadline:\n        " + temp.toString() + "\n    Now you have " + tasks.size() + " tasks in the list.\n" + indent);
+        if (!isInit) {
+            System.out.println(indent + "    Added Deadline:\n        " + temp.toString() + "\n    Now you have " +
+                    tasks.size() + " tasks in the list.\n" + indent);
+        }
     }
 
-    public void event(String[] parts) throws LamballParseException {
+    private void event(String[] parts, boolean isInit) throws LamballParseException {
         // Checks if empty string (nothing after command) or only whitespaces
         if (parts.length < 2 || parts[1] == null || parts[1].trim().isEmpty()) {
             throw new LamballParseException(indent + "    Your event field is empty, baaaaka.\n" + indent);
         }
         String[] furtherSplit = parts[1].split(" /", 3);
-        if (furtherSplit.length < 3 || !furtherSplit[1].substring(0,5).equals("from ") || !furtherSplit[2].substring(0,3).equals("to ")) {
-            throw new LamballParseException(indent + "    Event is in the wrong formaaaaaaat, baa. :(\n    Correct formaaat is: event <name> /from <time> /to <time>, baa.\n" + indent);
+        if (furtherSplit.length < 3 || !furtherSplit[1].substring(0,5).equals("from ") ||
+                !furtherSplit[2].substring(0,3).equals("to ")) {
+            throw new LamballParseException(indent + "    Event is in the wrong formaaaaaaat, baa. :(\n    Correct " +
+                    "formaaat is: event <name> /from <time> /to <time>, baa.\n" + indent);
         }
-        Task temp = new Event(furtherSplit[0], furtherSplit[1].replaceFirst("from ", ""), furtherSplit[2].replaceFirst("to ", ""));
+        Task temp = new Event(furtherSplit[0], furtherSplit[1].replaceFirst("from ", ""),
+                furtherSplit[2].replaceFirst("to ", ""));
         tasks.add(temp);
-        System.out.println(indent + "    Added Event:\n        " + temp.toString() + "\n    Now you have " + tasks.size() + " tasks in the list.\n" + indent);
+        if (!isInit) {
+            System.out.println(indent + "    Added Event:\n        " + temp.toString() + "\n    Now you have "
+                    + tasks.size() + " tasks in the list.\n" + indent);
+        }
     }
 
-    public void deleteFromList(String[] parts) throws LamballParseException {
+    private void deleteFromList(String[] parts) throws LamballParseException {
         // If not numeric
         if (!parts[1].matches("-?\\d+")) {
             throw new LamballParseException(indent + "    Invalid number, baa.\n" + indent);
@@ -117,12 +137,14 @@ public class Lamball {
         System.out.println("    Now you have " + tasks.size() + " tasks in the list.\n" + indent);
     }
 
-    public boolean parse(String msg) throws LamballParseException {
+    public boolean parse(String msg, boolean isInit) throws LamballParseException {
         String[] parts = msg.split(" ", 2);
-        System.out.println("\n    Lamball");
+        if (!isInit) {
+            System.out.println("\n    Lamball");
+        }
         switch(parts[0]) {
             case "mark": {
-                mark(parts);
+                mark(parts, isInit);
                 return true;
             }
             case "unmark": {
@@ -136,15 +158,15 @@ public class Lamball {
                 printList();
                 return true;
             case "todo": {
-                toDo(parts);
+                toDo(parts, isInit);
                 return true;
             }
             case "event": {
-                event(parts);
+                event(parts, isInit);
                 return true;
             }
             case "deadline": {
-                deadline(parts);
+                deadline(parts, isInit);
                 return true;
             }
             case "delete": {
@@ -155,4 +177,43 @@ public class Lamball {
                 throw new LamballParseException(indent + "    Sorry, I don't understaaaaaand your commaaaaand, baa. :(\n" + indent);
         }
     }
+
+    public void obtainSavedFile() {
+        savedList = new File("src/main/java/data/list.txt");
+        try {
+            // Try to create file
+            if (savedList.createNewFile()) {
+                System.out.println("List created successfully at: " + savedList.getAbsolutePath());
+            } else {
+                System.out.println("Seems like I haave helped you before, so no new list is needed!");
+            }
+            this.initializeListFromText();
+        } catch (IOException e) {
+            System.err.println("An error occurred while creating the file: " + e.getMessage());
+        }
+    }
+
+    private void initializeListFromText() throws FileNotFoundException {
+        Scanner scanner = new Scanner(savedList);
+        System.out.println("Initializing saved file...");
+        int count = 0;
+        while(scanner.hasNext()) {
+            try {
+                String[] parts = scanner.nextLine().split(" \\| ", 2);
+                if (parts.length != 2) {
+                    throw new LamballParseException("Corrupt format, ignoring...");
+                }
+                this.parse(parts[1], true);
+                count++;
+                if (Integer.valueOf(parts[0]) == 1) {
+                    this.parse("mark " + count, true);
+                }
+            } catch (LamballParseException e) {
+                System.out.println("Corrupt format, ignoring...");
+            }
+        }
+        System.out.println("Done!");
+    }
+
+
 }
