66365b46c14a53e414a4683f651812457095bfdf
kinjalagarwal1810 <105915730+kinjalagarwal1810@users.noreply.github.com>
2024-01-28 13:27:37 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
 
             } else if (userInput.equalsIgnoreCase("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "]" + tasks[i].getDescription());
+                    System.out.println(" " + (i + 1) + "." + tasks[i].toString());
                 }
 
             } else if (userInput.startsWith("mark")) {
@@ -42,8 +42,10 @@ public class Duke {
                 }
 
             } else {
-                tasks[taskCount++] = new Task (userInput);
-                System.out.println(" added: " + userInput);
+                Task newTask = TaskParser.parseTask(userInput);
+                tasks[taskCount++] = newTask;
+                System.out.println("Got it. I've added this task:\n " + newTask.toString() +
+                        "\nNow you have " + taskCount + " tasks in the list.");
             }
         }
         scanner.close(); // Close the Scanner to avoid resource leaks
@@ -74,5 +76,90 @@ class Task {
 
     public void markAsUndone() {
         isDone = false;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + getDescription();
+    }
+}
+
+
+class ToDo extends Task {
+
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+
+
+class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String decription, String from, String to) {
+        super(decription);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return  "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+    }
+}
+
+
+class TaskParser {
+    public static Task parseTask(String userInput) {
+        String[] words = userInput.split(" ", 2);
+        if (words.length < 2) {
+            return null;
+        }
+
+        String command = words[0].toLowerCase();
+        String details = words[1];
+
+        switch (command) {
+            case "todo":
+                return new ToDo(details);
+            case "deadline":
+                String[] deadlineDetails = details.split(" /by ");
+                if (deadlineDetails.length == 2) {
+                    return new Deadline(deadlineDetails[0], deadlineDetails[1]);
+                }
+                break;
+            case "event":
+                String[] eventDetails = details.split(" /from ");
+                if (eventDetails.length == 2) {
+                    String[] eventTiming = eventDetails[1].split(" /to ");
+                    if (eventTiming.length == 2) {
+                        return new Event(eventDetails[0], eventTiming[0], eventTiming[1]);
+                    }
+                }
+                break;
+        }
+        return null;
     }
 }
