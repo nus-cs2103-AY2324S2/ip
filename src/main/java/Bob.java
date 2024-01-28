de308b558ca39012f61e8d665219e172506049e0
Chong Chin Herng <chongahzong@gmail.com>
2024-01-28 11:15:44 +0800

    Implement add and list

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Bob {
+    private static int numberOfTasks = 0;
+    private static String[] tasks;
+
     private static void printFormatted(String[] lines) {
         String horizontalLine = "    .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n";
         StringBuilder formatted = new StringBuilder(horizontalLine);
@@ -17,16 +20,33 @@ public class Bob {
         printFormatted(new String[]{ line });
     }
 
+    private static void handleList() {
+        String[] numberedTasks = new String[numberOfTasks];
+        for (int i = 0; i < numberOfTasks; i++) {
+            numberedTasks[i] = (i + 1) + ". " + tasks[i];
+        }
+        printFormatted(numberedTasks);
+    }
+
     public static void main(String[] args) {
         printFormatted(new String[]{ "yo im bob", "what do you want" });
 
         Scanner scanner = new Scanner(System.in);
+        tasks = new String[100];
         while (true) {
             String command = scanner.nextLine();
+
             if (command.equals("bye")) {
                 break;
             }
-            printFormatted(command);
+
+            if (command.equals("list")) {
+                handleList();
+            } else {
+                tasks[numberOfTasks] = command;
+                numberOfTasks++;
+                printFormatted("added: " + command);
+            }
         }
 
         printFormatted("ok");
