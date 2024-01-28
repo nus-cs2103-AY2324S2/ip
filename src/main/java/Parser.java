326fa948b2f623e5acf64a6fcd05bb27cdb29f70
JustWeiHao <e0960531@u.nus.edu>
2024-01-28 17:20:56 +0800

    Modify Parser.java to check for date format using simpler code

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,12 @@
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Parser {
+    private static final Pattern datePattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     public static String getCommand(String input) {
         if (input.isEmpty()) {
             return null;
@@ -24,13 +30,24 @@ public class Parser {
                 String[] inputArr = input.split("/from ");
                 String task = inputArr[0];
                 String[] dates = inputArr[1].split("/to ");
-                return new String[]{task.trim(), dates[0].trim(), dates[1].trim()};
+                try {
+                    for (int i = 0; i < dates.length; ++i) {
+                        dates[i] = dates[i].trim();
+                        dates[i] = LocalDate.parse(dates[i], Parser.DATE_TIME_FORMATTER).toString();
+                    }
+                    return new String[]{task.trim(), dates[0], dates[1]};
+                } catch (DateTimeParseException e) {
+                    return null;
+                }
             }
         } else {
             if (!input.contains("/by ")) {
                 return null;
             } else {
                 final String[] res = input.split("/by ");
+                if (!Parser.datePattern.matcher(res[1]).matches()) {
+                    return null;
+                }
                 res[0] = res[0].trim();
                 res[1] = res[1].trim();
                 return res;
@@ -38,11 +55,15 @@ public class Parser {
         }
     }
     public static String getDate(String input) {
-        if (!input.contains("by")) {
+        String[] inputArr = input.split(" ");
+        if (inputArr.length != 2) {
             return null;
         } else {
-            String[] inputArr = input.split(" ");
-            return String.join(" ", Arrays.copyOfRange(inputArr, 1, inputArr.length));
+            try {
+                return LocalDate.parse(inputArr[1], Parser.DATE_TIME_FORMATTER).toString();
+            } catch (DateTimeParseException e) {
+                return null;
+            }
         }
     }
 
