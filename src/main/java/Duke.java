025297a4a84ec1543ea761d3876a609349267666
jjchee7 <jingjie.chee@u.nus.edu>
2024-01-29 04:16:26 +0800

    Add event, todo, deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -33,45 +34,117 @@ public class Duke {
         System.out.println(getBorder());
     }
 
+    private static String handleList() {
+        String[] output = new String[TASKS.size()];
+        for (int i = 0; i < TASKS.size(); i++) {
+            Task task = TASKS.get(i);
+            output[i] = String.format("%d. %s", i + 1, task.toString());
+        }
+        return String.join("\n", output);
+    }
+
+    private static String handleMarkUnMark(String[] args) {
+        String command = args[0];
+        int taskNum = Integer.parseInt(args[1]);
+        Task task = TASKS.get(taskNum - 1);
+
+        if (command.equals("mark")) {
+            task.mark();
+            return String.format("Nice! I've marked this task as done:\n  %s", task.toString());
+        } else {
+            task.unmark();
+            return String.format("OK, I've marked this task as not done yet:\n  %s", task.toString());
+        }
+    }
+
+    private static Task addTodo(String[] taskArgs) {
+        String description = String.join(" ", taskArgs);
+
+        Todo todo = new Todo(description);
+        TASKS.add(todo);
+
+        return todo;
+    }
+
+    private static Task addDeadline(String[] taskArgs) {
+        String byDelim = "/by";
+
+        String argsStr = String.join(" ", taskArgs);
+        int byIndex = argsStr.indexOf(byDelim);
+
+        String by = argsStr.substring(byIndex + byDelim.length()).trim();
+        String description = argsStr.substring(0, byIndex).trim();
+
+        Deadline deadline = new Deadline(description, by);
+        TASKS.add(deadline);
+
+        return deadline;
+    }
+
+    private static Task addEvent(String[] taskArgs) {
+        String fromDelim = "/from";
+        String toDelim = "/to";
+
+        String argsStr = String.join(" ", taskArgs);
+        int fromIndex = argsStr.indexOf(fromDelim);
+        int toIndex = argsStr.indexOf(toDelim);
+
+        String from = argsStr.substring(fromIndex + fromDelim.length(), toIndex).trim();
+        String to = argsStr.substring(toIndex + toDelim.length()).trim();
+        String description = argsStr.substring(0, fromIndex).trim();
+
+        Event event = new Event(description, from , to);
+        TASKS.add(event);
+
+        return event;
+    }
+
+    private static String handleAddTask(String[] args) {
+        String taskType = args[0];
+        String[] taskArgs = Arrays.copyOfRange(args, 1, args.length);
+
+        Task task;
+        switch (taskType) {
+            case "todo":
+                task = addTodo(taskArgs);
+                break;
+            case "deadline":
+                task = addDeadline(taskArgs);
+                break;
+            case "event":
+                task = addEvent(taskArgs);
+                break;
+            default:
+                return "";
+        }
+        return String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.", task.toString(), TASKS.size());
+    }
+
     private static void handleCommands() {
         Scanner scnr = new Scanner(System.in);
         while (true) {
             String userInput = scnr.nextLine();
-            String[] userInputSplit = userInput.split(" ");
-            String command = userInputSplit[0];
+            String[] args = userInput.split(" ");
+            String command = args[0];
 
+            String commandOutput;
             switch (command) {
                 case "list":
-                    StringBuilder output  = new StringBuilder();
-                    for (int i = 0; i < TASKS.size(); i++) {
-                        Task task = TASKS.get(i);
-                        output.append(String.format("%d. %s\n", i + 1, task.toString()));
-                    }
-                    printCommandOutput(output.toString());
-
+                    commandOutput = handleList();
                     break;
                 case "bye":
-                    break;
+                    return;
                 case "mark":
                 case "unmark":
-                    int taskNum = Integer.parseInt(userInputSplit[1]);
-                    Task task = TASKS.get(taskNum - 1);
-
-                    if (command.equals("mark")) {
-                        task.mark();
-                        printCommandOutput(String.format("Nice! I've marked this task as done:\n  %s\n", task.toString()));
-                    } else {
-                        task.unmark();
-                        printCommandOutput(String.format("OK, I've marked this task as not done yet::\n  %s\n", task.toString()));
-                    }
-
+                    commandOutput = handleMarkUnMark(args);
                     break;
                 default:
-                    TASKS.add(new Task(userInput));
-                    printCommandOutput(String.format("added: %s\n", userInput));
-
+                    commandOutput = handleAddTask(args);
                     break;
             }
+            System.out.println(getBorder());
+            System.out.println(commandOutput);
+            System.out.println(getBorder());
         }
     }
 
