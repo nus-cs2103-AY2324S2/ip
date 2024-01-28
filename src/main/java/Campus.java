a741c56404adb9753298e8836b69078cc5507a25
Kappaccinoh <lim.jia28@gmail.com>
2024-01-28 15:03:58 +0800

    refactoring code for readability

--- src/main/java/Campus.java
+++ src/main/java/Campus.java
@@ -131,6 +131,82 @@ public class Campus {
         return Files.readAllLines(path);
     }
 
+    public static Task getIthTask(String userInput) {
+        String listNumber = userInput.substring(userInput.length() - 1);
+        int index = Integer.parseInt(listNumber) - 1;
+        return Campus.tasks.get(index);
+    }
+
+    public static void handleUpdateCommands (String command, String userInput) {
+        Task task = getIthTask(userInput);
+        switch (command) {
+            case "mark":
+                Campus.markDone(task);
+                break;
+            case "unmark":
+                Campus.markUndone(task);
+            case "delete":
+                Campus.delete(task);
+        }
+    }
+
+    public static void handleTodoCommand(String remaining) throws CampusException {
+        if (remaining.isEmpty()) {
+            throw new CampusException("Error! A todo task must have a name, please follow the following syntax: todo <task name>\n");
+        } else {
+            ToDos todo = new ToDos(remaining);
+            Campus.add(todo);
+        }
+    }
+
+    public static void handleDeadlineCommand(String remaining) throws CampusException {
+        String[] temp = remaining.split("/by", 2);
+        if (temp.length == 1) {
+            throw new CampusException("Error! A deadline task must have parameters, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
+        }
+        String deadlineName = temp[0].trim();
+        String endDateTime = temp[1].trim();
+
+        if (deadlineName.isEmpty()) {
+            throw new CampusException("Error! A deadline task must have a name, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
+        } else if (endDateTime.isEmpty()) {
+            throw new CampusException("Error! A deadline task must have an end datetime, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
+        } else {
+            Deadline deadline = new Deadline(deadlineName, endDateTime);
+            Campus.add(deadline);
+        }
+    }
+
+    public static void handleEventCommand(String remaining) throws CampusException {
+        String[] temp1 = remaining.split("/from", 2);
+
+        if (temp1.length == 1) {
+            throw new CampusException("Error! An event task must have parameters, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
+        }
+
+        String eventName = temp1[0].trim();
+        String remaining1 = temp1[1].trim();
+        String[] temp2 = remaining1.split("/to", 2);
+
+        if (temp2.length == 1) {
+            throw new CampusException("Error! An event task must have parameters, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
+        }
+
+        String from = temp2[0].trim();
+        String to = temp2[1].trim();
+
+        if (eventName.isEmpty()) {
+            throw new CampusException("Error! An event task must have a name, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
+        } else if (from.isEmpty()) {
+            throw new CampusException("Error! An event task must have a start datetime, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
+        } else if (to.isEmpty()) {
+            throw new CampusException("Error! An event task must have an end datetime, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
+        } else {
+            Event event = new Event(eventName, from, to);
+            Campus.add(event);
+        }
+    }
+
     /**
      * Main Logic to keep the Chat Bot running until a user decides to quit talking by sending 'bye'
      * @param userInput A String i/o input
@@ -155,83 +231,18 @@ public class Campus {
                 Campus.display();
                 break;
             case "mark":
-                // Getting the ith Task in the List<Task> field
-                // Again Looks Clunky - Need to refactor
-                String listNumber = userInput.substring(userInput.length() - 1);
-                int index = Integer.parseInt(listNumber) - 1;
-                Task task = Campus.tasks.get(index);
-                Campus.markDone(task);
-                break;
             case "unmark":
-                // Getting the ith Task in the List<Task> field
-                // Again Looks Clunky - Need to refactor
-                String listNumber1 = userInput.substring(userInput.length() - 1);
-                int index1 = Integer.parseInt(listNumber1) - 1;
-                Task task1 = Campus.tasks.get(index1);
-                Campus.markUndone(task1);
-                break;
             case "delete":
-                // Getting the ith Task in the List<Task> field
-                // Again Looks Clunky - Need to refactor
-                String listNumber2 = userInput.substring(userInput.length() - 1);
-                int index2 = Integer.parseInt(listNumber2) - 1;
-                Task task2 = Campus.tasks.get(index2);
-                Campus.delete(task2);
+                handleUpdateCommands(firstWord, userInput);
                 break;
             case "todo":
-                if (remaining.isEmpty()) {
-                    throw new CampusException("Error! A todo task must have a name, please follow the following syntax: todo <task name>\n");
-                } else {
-                    ToDos todo = new ToDos(remaining);
-                    Campus.add(todo);
-                }
+                handleTodoCommand(remaining);
                 break;
             case "deadline":
-                String[] temp = remaining.split("/by", 2);
-                if (temp.length == 1) {
-                    throw new CampusException("Error! A deadline task must have parameters, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
-                }
-                String deadlineName = temp[0].trim();
-                String endDateTime = temp[1].trim();
-
-                if (deadlineName.isEmpty()) {
-                    throw new CampusException("Error! A deadline task must have a name, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
-                } else if (endDateTime.isEmpty()) {
-                    throw new CampusException("Error! A deadline task must have an end datetime, please follow the following syntax: deadline <deadline name> /by <endDateTime>\n");
-                } else {
-                    Deadline deadline = new Deadline(deadlineName, endDateTime);
-                    Campus.add(deadline);
-                }
+                handleDeadlineCommand(remaining);
                 break;
             case "event":
-                // seems a little clunky but it will do for now
-                String[] temp1 = remaining.split("/from", 2);
-
-                if (temp1.length == 1) {
-                    throw new CampusException("Error! An event task must have parameters, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
-                }
-
-                String eventName = temp1[0].trim();
-                String remaining1 = temp1[1].trim();
-                String[] temp2 = remaining1.split("/to", 2);
-
-                if (temp2.length == 1) {
-                    throw new CampusException("Error! An event task must have parameters, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
-                }
-
-                String from = temp2[0].trim();
-                String to = temp2[1].trim();
-
-                if (eventName.isEmpty()) {
-                    throw new CampusException("Error! An event task must have a name, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
-                } else if (from.isEmpty()) {
-                    throw new CampusException("Error! An event task must have a start datetime, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
-                } else if (to.isEmpty()) {
-                    throw new CampusException("Error! An event task must have an end datetime, please follow the following syntax: event <event name> /from <startDateTime> /to <endDateTime>\n");
-                } else {
-                    Event event = new Event(eventName, from, to);
-                    Campus.add(event);
-                }
+                handleEventCommand(remaining);
                 break;
             case "bye":
                 return 1;
