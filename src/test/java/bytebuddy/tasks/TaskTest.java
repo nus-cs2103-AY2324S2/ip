aaa6e1cf3edffec7f3147a3495d45ebe639a9552
NeoHW <neohaowei@gmail.com>
2024-01-28 16:04:39 +0800

    Add TaskTest and TodoTest that tests Task and Todo

---
+++ src/test/java/bytebuddy/tasks/TaskTest.java
@@ -0,0 +1,53 @@
+package bytebuddy.tasks;
+
+import bytebuddy.tasks.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+
+    @Test
+    public void testTaskMarkAsDone() {
+        Task task = new Task("Sample task");
+
+        String expectedOutput = "Nice! I've mark this task as done:\n\t\t[✓] " + task.description;
+        String actualOutput = task.markAsDone();
+
+        assertEquals(expectedOutput, actualOutput);
+        assertTrue(task.isDone);
+    }
+
+    @Test
+    public void testTaskUnmarkAsDone() {
+        Task task = new Task("Sample task", true);
+
+        String expectedOutput = "OK, I've marked this task as not done yet:\n\t\t[✕] " + task.description;
+        String actualOutput = task.unmarkAsDone();
+
+        assertEquals(expectedOutput, actualOutput);
+        assertFalse(task.isDone);
+    }
+
+    @Test
+    public void testGetStatusIcon() {
+        Task task = new Task("Sample task");
+        task.markAsDone();
+
+        String expectedOutput = "✓";
+        String actualOutput = task.getStatusIcon();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testTaskToString() {
+        Task task = new Task("Sample task", true);
+
+        String expectedOutput = "[" + task.getStatusIcon() + "] Sample task";
+        String actualOutput = task.toString();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
+
---
+++ src/test/java/bytebuddy/tasks/TodoTest.java
@@ -0,0 +1,30 @@
+package bytebuddy.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TodoTest {
+
+    @Test
+    public void testTodoToString() {
+        Todo todo = new Todo("Buy groceries");
+
+        String expectedOutput = "[T][✕] Buy groceries";
+        String actualOutput = todo.toString();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testTodoTextFormattedOutput() {
+        Todo todo = new Todo("Buy groceries");
+
+        String expectedOutput = "T | 0 | Buy groceries";
+        String actualOutput = todo.textFormattedOutput();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
