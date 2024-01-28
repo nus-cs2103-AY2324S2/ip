e3dd353a5f3117e0770d355cbb78abe7f0ee7ab8
YuZizhen <yuzizhen123@gmail.com>
2024-01-29 00:13:15 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    static void greeting(String botName) {
+        System.out.println("Hello! I'm " + botName + "\n"
+                + "What can I do for you?");
+    }
+
+    static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String botName = "Zizhen";
+        greeting(botName);
+
+        exit();
+
     }
 }
