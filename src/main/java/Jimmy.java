ad63ad37a04aa6e49d32b8454f826236504cb030
yuechen2001 <zhuyuechen@gmail.com>
2024-01-28 19:10:41 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Jimmy.java
@@ -0,0 +1,9 @@
+public class Jimmy {
+    public void greetUser() {
+        System.out.println("Hello! I'm Jimmy \nWhat can I do for you?");
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Jimmy bot = new Jimmy();
+        bot.greetUser();
+        bot.exit();
+    }
+}
