56d1da9eff298003adbccc62ad050f54cd8c8385
jjchee7 <jingjie.chee@u.nus.edu>
2024-01-29 01:33:12 +0800

    Add initial skeleton version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+    private static final String NAME = "Arctic";
+    private static final Integer BORDER_LEN = 60;
+
+    private static String duplicateChar(Character c, Integer num) {
+        return String.valueOf(c).repeat(num);
+    }
+    private static void userGreeting () {
+        System.out.println(duplicateChar('_', BORDER_LEN));
+        System.out.printf("Hello! I'm %s\n", NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(duplicateChar('_', BORDER_LEN));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(duplicateChar('_', BORDER_LEN));
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        userGreeting();
     }
 }
