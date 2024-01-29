c93bb6e6b0782997ae9486d7fd6ece54259d1220
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-29 21:38:14 +0800

    Reformat strings for Tasks for easier parsing for Storage

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,6 +19,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return "[" + (isDone ? "X" : " ") + "] " + description;
+        return (isDone ? "X" : " ") + " | " + description;
     }
 }
