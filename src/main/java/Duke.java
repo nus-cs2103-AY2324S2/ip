ac14ee7f1ade6e4dbb2011b25ed404934f6770c0
matthewken19 <matthewkenelmer@gmail.com>
2024-01-29 23:23:50 +0800

    Added greeting and closing statements and line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "_____________________________________________\n";
+        String greeting = "Hello! I'm Donald.\nWhat can I do for you?";
+        String closing = "Bye. Hope to see you again soon!";
+        System.out.println(line + greeting);
+        System.out.println(line + closing);
+        System.out.println(line);
     }
 }
