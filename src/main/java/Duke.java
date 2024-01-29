315320795738703f158ab736c100cddebb5575a5
KiatLun <louiskhoo368@gmail.com>
2024-01-29 15:14:52 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "__________________________________\n" +
                 "Hello! I'm Tim \n" +
                 "What can i do for you? \n" +
-                "__________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
+                "__________________________________\n";
+
+        String exit = "Bye. Hope to see you again soon!\n" +
                 "__________________________________";
 
         System.out.println(logo);
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while (!input.isEmpty()) {
+            if (input.equals("bye")) {
+                System.out.println(exit);
+                break;
+            }
+
+            String msg = "__________________________________\n" +
+                    input +
+                    "\n__________________________________\n";
+            System.out.println(msg);
+            input = scan.nextLine();
+        }
+
     }
 }
