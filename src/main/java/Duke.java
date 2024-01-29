9bf0fa5d59564d2d3c016a9820340898d1213a5d
tanguoyang <64419996+tanguoyang@users.noreply.github.com>
2024-01-29 14:13:44 +0800

    add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello im Jelly!");
+        System.out.println("What can i do for you?");
+        System.out.println("Byeee!");
+
     }
 }
