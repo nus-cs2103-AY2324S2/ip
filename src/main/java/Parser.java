6fe098e6e5861db8e2d1c9fac32b50665bc8e01c
swtan346 <swtan346@gmail.com>
2024-01-29 17:32:48 +0800

    Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,21 @@
+public class Parser {
+
+    public Command parse(String input) throws WeiException {
+        if (input.equals("bye")) {
+            return new ExitCommand();
+        } else if (input.equals("list")) {
+            return new ListCommand();
+        } else if (input.startsWith("mark")) {
+            return new MarkCommand(input);
+        } else if (input.startsWith("unmark")) {
+            return new UnmarkCommand(input);
+        } else if (input.startsWith("delete")) {
+            return new DeleteCommand(input);
+        } else if (input.startsWith("todo") || input.startsWith("deadline") || input.startsWith("event")) {
+            return new AddCommand(input);
+        }
+        else {
+            throw new WeiException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+}
