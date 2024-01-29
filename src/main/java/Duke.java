33f0aee646362cfc6e5e688525742fbbe3aa2ff5
ziyi22 <ziyi160073@gmail.com>
2024-01-29 13:36:21 +0800

    Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String line = "____________________________________\n";
+        greetUser();
+        echo();
+        exit();
+    }
+    private static void greetUser() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Judy");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________");
+    }
 
-        System.out.println(line + "Hello! I'm Judy\n");
-        System.out.println(" What can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String  command;
 
+        do {
+            System.out.print("\n");
+            command = scanner.nextLine();
+            if(! command.equals("bye")){
+                System.out.println("_________________________________________");
+                System.out.println(" " + command);
+                System.out.println("_________________________________________");
+            }
+        } while (!command.equalsIgnoreCase("bye"));
+
+        scanner.close();
+    }
+
+    private static void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________");
     }
 }
