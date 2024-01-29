40818015c1adde223da20ec6d9943fa1494f937b
matthewken19 <matthewkenelmer@gmail.com>
2024-01-30 00:03:09 +0800

    Structuring of the line string, added list function, removed echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
 
-        String line = "_____________________________________________\n";
+        String line = "_____________________________________________";
         String greeting = "Hello! I'm Donald.\nWhat can I do for you?\n";
-        System.out.println(line + greeting + line);
+        System.out.println(line);
+        System.out.println(greeting + line + "\n");
 
         Scanner sc = new Scanner(System.in);
         String input;
+        List<String> list = new ArrayList<String>();
 
         while (sc.hasNextLine()) {
             input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                int index = 1;
+                for (String s: list) {
+                    System.out.println(index + ". " + s);
+                    index++;
+                }
+                System.out.println(line + "\n");
+            } else {
+                list.add(input);
+                System.out.println(line);
+                System.out.println("added: " + input + "\n" + line + "\n");
             }
-            System.out.println(line + input + "\n" + line);
         }
 
         String closing = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + closing + line);
+        System.out.println(line);
+        System.out.println(closing + line);
     }
 }
