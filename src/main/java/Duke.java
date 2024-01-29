7d6c7ae6bbb567938323ac08a23424399193aace
matthewken19 <matthewkenelmer@gmail.com>
2024-01-29 23:40:17 +0800

    Added echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+
         String line = "_____________________________________________\n";
-        String greeting = "Hello! I'm Donald.\nWhat can I do for you?";
-        String closing = "Bye. Hope to see you again soon!";
-        System.out.println(line + greeting);
-        System.out.println(line + closing);
-        System.out.println(line);
+        String greeting = "Hello! I'm Donald.\nWhat can I do for you?\n";
+        System.out.println(line + greeting + line);
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (sc.hasNextLine()) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(line + input + "\n" + line);
+        }
+
+        String closing = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + closing + line);
     }
 }
