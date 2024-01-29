1025469ed3bf2e660415d4109650882213dfc2c0
Vision-2000 <122250743+Vision-2000@users.noreply.github.com>
2024-01-29 17:45:23 +0800

    Rename Duke.java to Stille.java

---
+++ src/main/java/Stille.java
@@ -0,0 +1,11 @@
+public class Stille {
+    public static void main(String[] args) {
+        String openingMessage = "Hello! I'm Stille\n" + "What can I do for you?\n";
+        String closingMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(openingMessage);
+        System.out.println(closingMessage);
+
+
+
+    }
+}
