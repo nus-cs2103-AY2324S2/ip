70697c89234f709e07ab7d68d5ccd98f6059a6f9
Cedricaca <>
2024-01-29 10:49:14 +0800

    A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
+if [ ! -d "/Users/cedrictan/git/ip/bin" ]
 then
-    mkdir ../bin
+    mkdir /Users/cedrictan/git/ip/bin
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+if [ -e "/Users/cedrictan/git/ip/text-ui-test/ACTUAL.TXT" ]
 then
-    rm ACTUAL.TXT
+    rm /Users/cedrictan/git/ip/text-ui-test/ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp /Users/cedrictan/git/ip/src/main/java -Xlint:none -d /Users/cedrictan/git/ip/bin /Users/cedrictan/git/ip/src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath /Users/cedrictan/git/ip/bin Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
