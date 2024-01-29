ce92999ef93454868448799619035c31a379e116
jasperng-nus <e0959074@u.nus.edu>
2024-01-30 00:51:29 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Luke.java
@@ -0,0 +1,14 @@
+public class Luke {
+    public static void main(String[] args) {
+        // Greetings
+        System.out.println("__________________________________________");
+        System.out.println("Hello! I'm Luke");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________");
+
+        // Bye and exits
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________________");
+
+    }
+}
