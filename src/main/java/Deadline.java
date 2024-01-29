dc610ba235295f28775929e7019457203fbddeec
Shaun Lai <shaun@littlebit.sg>
2024-01-29 16:24:13 +0800

    Have Lai parse input into LocalDate object

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            this.by = LocalDate.parse(by, DateTimeFormatter.ofPattern("dd MMM yyyy"));
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")";
     }
 }
