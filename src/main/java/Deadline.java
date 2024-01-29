0b0b750ea559d607ea2f63e7795804f2fea6e1af
Lim Zi Jia <zijialim@gmail.com>
2024-01-29 15:17:22 +0800

    Change date time format
    Changed from accepting any string to only parse using the ISO_LOCAL_DATE format.
    
    Changed the saved format for date time to ISO_LOCAL_DATE.
    
    Changed toString() of tasks to pattern "MMM dd yyyy"

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String time;
+    private LocalDate time;
+
+    public Deadline(boolean done, String name, String time) {
+        super(done, name);
+        this.time = LocalDate.parse(time);
+    }
 
     public Deadline(String name, String time) {
         super(name);
-        this.time = time;
+        this.time = LocalDate.parse(time);
+    }
+
+    @Override
+    public String toSavedString() {
+        return String.format("D,%s,%s,%s"
+                , this.done ? '1' : '0'
+                , this.name
+                , this.time.format(DateTimeFormatter.ISO_LOCAL_DATE));
     }
 
     public String toString() {
-        return String.format("[D][%s] %s (by: %s)\n", this.done ? "X" : " ", this.name, this.time);
+        return String.format("[D][%s] %s (by: %s)\n"
+                , this.done ? "X" : " "
+                , this.name
+                , this.time.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,26 @@
-public class Event extends Task {
-    private String from;
-    private String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class Event extends Task {
+    private LocalDate from;
+    private LocalDate to;
+
+    public Event(boolean done, String name, String from, String to) {
+        super(done, name);
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
+    }
     public Event(String name, String from, String to) {
         super(name);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     public String toString() {
-        return String.format("[E][%s] %s (from: %s to: %s)\n", this.done ? "X" : " ", this.name, this.from, this.to);
+        return String.format("[E][%s] %s (from: %s to: %s)\n"
+                , this.done ? "X" : " "
+                , this.name
+                , this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
+                , this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
