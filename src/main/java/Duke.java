581728466a277301ca49aa2a114107c5aaeb3b14
taufiq <taufiqmmhd@gmail.com>
2024-01-29 14:55:03 +0800

    feat: Add echo
    I've added a way for the bot to echo back whatever the user said.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello it's a-me! Mario!\nWhat-a can I do fo' ya!");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String command = scanner.next();
+            System.out.println(command + "-o!");
+            if (command.equals("bye")) {
+                break;
+            }
+        }
     }
 }
