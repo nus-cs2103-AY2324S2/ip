349b0ebf5e354f0115d017259dd01808aea2e459
vnnamng <e0969384@u.nus.edu>
2024-01-29 13:42:46 +0800

    Clean unused methods

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -7,7 +7,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.nio.file.Files;
@@ -20,77 +19,14 @@ public class ChatBot {
     private String icon;
     private List<String> greetings;
     private List<String> farewells;
-    private List<String> commands;
     private List<Task> tasks;
-
-    public ChatBot(String name, String icon, List<String> greetings, List<String> farewells, List<String> commands) {
-        this.name = name;
-        this.icon = icon;
-        this.greetings = greetings;
-        this.farewells = farewells;
-        this.commands = commands;
-        this.tasks = new ArrayList<Task>();
-    }
 
     public ChatBot(String name, String icon, List<String> greetings, List<String> farewells) {
         this.name = name;
         this.icon = icon;
         this.greetings = greetings;
         this.farewells = farewells;
-        this.commands = List.<String>of("greet", "bye");
-        this.tasks = new ArrayList<Task>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public List<String> getGreetings() {
-        return greetings;
-    }
-
-    public void setGreetings(List<String> greetings) {
-        this.greetings = greetings;
-    }
-
-    public void addGreeting(String greeting) {
-        this.greetings.add(greeting);
-    }
-
-    public List<String> getFarewells() {
-        return farewells;
-    }
-
-    public void setFarewells(List<String> farewells) {
-        this.farewells = farewells;
-    }
-
-    public void addFarewell(String farewell) {
-        this.farewells.add(farewell);
-    }
-
-    public List<String> getCommands() {
-        return commands;
-    }
-
-    public void setCommands(List<String> commands) {
-        this.commands = commands;
-    }
-
-    public void addCommand(String command) {
-        this.commands.add(command);
+        this.tasks = new ArrayList<>();
     }
 
     public void greet() {
