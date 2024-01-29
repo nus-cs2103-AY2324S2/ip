68ccf487943b79b028187272cded842373f20596
matthewken19 <matthewkenelmer@gmail.com>
2024-01-30 01:20:44 +0800

    add case for todo, deadline, and event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.function.ToDoubleFunction;
+
 public class Duke {
     public static void main(String[] args) {
 
@@ -18,10 +20,12 @@ public class Duke {
                 break;
             }
 
-            String[] split = input.split(" ");
-            String command = split[0];
+            String command = input.split(" ")[0];
             Integer index;
             Task t;
+            String desc;
+            String[] split;
+
             switch(command) {
                 case "list":
                     System.out.println("Here are the tasks in your list:");
@@ -33,6 +37,7 @@ public class Duke {
                     break;
                 case "mark":
                     System.out.println("Nice! I've marked this task as done:");
+                    split = input.split(" ");
                     index = Integer.parseInt(split[1]) - 1;
                     t = list.get(index);
                     t.mark();
@@ -41,11 +46,44 @@ public class Duke {
                     break;
                 case "unmark":
                     System.out.println("OK, I've marked this task as not done yet:");
+                    split = input.split(" ");
                     index = Integer.parseInt(split[1]) - 1;
                     t = list.get(index);
                     t.unmark();
                     System.out.println(t.toString());
                     list.set(index, t);
+                    break;
+                case "todo":
+                    System.out.println("Got it. I've added this task:");
+                    desc = input.split(command + " ")[1];
+                    t = new ToDo(desc);
+                    System.out.println(t.toString());
+                    list.add(t);
+                    System.out.println("Now you have " + list.size() + " tasks in this list.");
+                    break;
+                case "deadline":
+                    System.out.println("Got it. I've added this task:");
+                    desc = input.split(command + " ")[1];
+                    split = desc.split("/by");
+                    String by = split[1];
+                    desc = split[0];
+                    t = new Deadline(desc, by);
+                    System.out.println(t.toString());
+                    list.add(t);
+                    System.out.println("Now you have " + list.size() + " tasks in this list.");
+                    break;
+                case "event":
+                    System.out.println("Got it. I've added this task:");
+                    desc = input.split(command + " ")[1];
+                    split = desc.split("/from");
+                    desc = split[0];
+                    split = split[1].split("/to");
+                    String start = split[0];
+                    String end = split[1];
+                    t = new Event(desc, start, end);
+                    System.out.println(t.toString());
+                    list.add(t);
+                    System.out.println("Now you have " + list.size() + " tasks in this list.");
                     break;
                 default:
                     t = new Task(input);
