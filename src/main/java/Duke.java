5d5128cbd1b6b82706b7581b16881863b1789add
YuZizhen <yuzizhen123@gmail.com>
2024-01-29 16:40:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,16 +11,20 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    static void Echo() {
-        boolean isExit = false;
-        while (!isExit) {
-            Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.next();
-            if (userInput.equals("bye")) {
-                isExit = true;
-            } else {
-                System.out.println(userInput);
-            }
+    static void echo(String userInput, ArrayList<String> TodoList) {
+        System.out.println("added: " + userInput);
+        addToList(TodoList, userInput);
+    }
+
+    static void addToList(ArrayList<String> TodoList, String userInput) {
+        TodoList.add(userInput);
+    }
+
+    static void printList(ArrayList<String> TodoList) {
+        int length = TodoList.size();
+        for (int i = 0; i < length; i++) {
+            String pos = String.valueOf(i + 1);
+            System.out.println(pos + ". " + TodoList.get(i));
         }
     }
 
@@ -34,9 +39,21 @@ public class Duke {
         String botName = "Zizhen";
         greeting(botName);
 
-        Echo();
+        ArrayList<String> TodoList = new ArrayList<>();
+
+        boolean isExit = false;
+        while (!isExit) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                isExit = true;
+            } else if (userInput.equals("list")) {
+                printList(TodoList);
+            } else {
+                echo(userInput, TodoList);
+            }
+        }
 
         exit();
-
     }
 }
