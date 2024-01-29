5b94b1f62ac219ab01c91e16f8b05b35cc15612d
Vision-2000 <122250743+Vision-2000@users.noreply.github.com>
2024-01-29 19:02:18 +0800

    Stille.java: Add functionality to echo commands

--- src/main/java/Stille.java
+++ src/main/java/Stille.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Stille {
     public static void main(String[] args) {
         String openingMessage = "Hello! I'm Stille\n" + "What can I do for you?\n";
         String closingMessage = "Bye. Hope to see you again soon!\n";
+        String input = "";
+        String closeCommand = "bye";
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(openingMessage);
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals(closeCommand)) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println(closingMessage);
 
 
