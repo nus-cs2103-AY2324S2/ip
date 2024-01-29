3c2c5c99e6a0836f28e4b8c5cebf623a70bb574d
matthewken19 <matthewkenelmer@gmail.com>
2024-01-30 00:42:55 +0800

    Added function to mark and unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,29 +9,53 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input;
-        List<String> list = new ArrayList<String>();
+        List<Task> list = new ArrayList<Task>();
 
         while (sc.hasNextLine()) {
             input = sc.nextLine();
+            System.out.println(line);
             if (input.equals("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                System.out.println(line);
-                int index = 1;
-                for (String s: list) {
-                    System.out.println(index + ". " + s);
-                    index++;
-                }
-                System.out.println(line + "\n");
-            } else {
-                list.add(input);
-                System.out.println(line);
-                System.out.println("added: " + input + "\n" + line + "\n");
             }
+
+            String[] split = input.split(" ");
+            String command = split[0];
+            Integer index;
+            Task t;
+            switch(command) {
+                case "list":
+                    System.out.println("Here are the tasks in your list:");
+                    index = 1;
+                    for (Task s : list) {
+                        System.out.println(index + "." + s);
+                        index++;
+                    }
+                    break;
+                case "mark":
+                    System.out.println("Nice! I've marked this task as done:");
+                    index = Integer.parseInt(split[1]) - 1;
+                    t = list.get(index);
+                    t.mark();
+                    System.out.println(t.toString());
+                    list.set(index, t);
+                    break;
+                case "unmark":
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    index = Integer.parseInt(split[1]) - 1;
+                    t = list.get(index);
+                    t.unmark();
+                    System.out.println(t.toString());
+                    list.set(index, t);
+                    break;
+                default:
+                    t = new Task(input);
+                    list.add(t);
+                    System.out.println("added: " + input);
+            }
+            System.out.println(line + "\n");
         }
 
         String closing = "Bye. Hope to see you again soon!\n";
-        System.out.println(line);
         System.out.println(closing + line);
     }
 }
