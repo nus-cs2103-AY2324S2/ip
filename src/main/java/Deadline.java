50d65dad464b97f2f7aab71cc1ac74e0c13c0508
Lin-Shuang-Shuang <e0970548@u.nus.edu>
2024-01-29 14:32:51 +0800

    Add dates and times to deadline tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,27 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
     private final String TYPE = "D";
-    private String deadline;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private LocalDate deadline;
+
 
     public Deadline(String description) {
         super(description.split("/")[0]);
-        this.deadline = description.split("/by ")[1];
+        this.deadline = LocalDate.parse(description.split("/by ")[1], formatter);
     }
 
     public  Deadline(String event, String extraInfo) {
         super(event);
-        this.deadline = extraInfo;
+
+        this.deadline = LocalDate.parse(extraInfo.replace('/','-'), formatter);;
     }
 
     public String getType() {
         return this.TYPE;
     }
     public String getExtraInfoShortened() {
-        return this.deadline;
+        return this.deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
 
     public String getExtraInfo() {
-        return "(by: " + this.deadline + ")";
+        return "(by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
 }
