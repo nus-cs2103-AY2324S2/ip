3f61622a9bb73c3144b69516bd03987019790328
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-01-29 09:44:24 +0800

    Add Junit test cases for DateFormatter and Parser classes.
    This automates testing to make testing faster.
    
    Add Junit tests in the src/java/test folder for greater convenience.

---
+++ src/test/java/others/DateFormatterTest.java
@@ -0,0 +1,26 @@
+package others;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateFormatterTest {
+
+    private DateFormatter correctForm = new DateFormatter("2019-10-15");
+    private DateFormatter incorrectForm = new DateFormatter("2019-10-1");
+
+    @Test
+    public void isValidDate_forCorrectDate_Test(){
+        assertEquals(true,correctForm.isValidDate());
+    }
+
+    @Test
+    public void isValidDate_forIncorrectDate_Test(){
+        assertEquals(false,incorrectForm.isValidDate());
+    }
+
+    @Test
+    public void convertDateTest(){
+        assertEquals("Oct 15 2019",correctForm.convertDate());
+    }
+}
---
+++ src/test/java/run/ParserTest.java
@@ -0,0 +1,86 @@
+package run;
+
+import org.junit.jupiter.api.Test;
+import others.BelleException;
+import command.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+
+    Parser parser = new Parser();
+
+    @Test
+    public void parseDeleteTest(){
+        try {
+            assertEquals(true, parser.parse("delete 2") instanceof DeleteCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseListTest(){
+        try {
+            assertEquals(true, parser.parse("List") instanceof ListCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseByeTest(){
+        try {
+            assertEquals(true, parser.parse("bye") instanceof ByeCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseMarkTest(){
+        try {
+            assertEquals(true, parser.parse("mark 2") instanceof MarkCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseUnmarkTest(){
+        try {
+            assertEquals(true, parser.parse("unmark 2") instanceof UnmarkCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void parseToDoTest(){
+        try {
+            assertEquals(true, parser.parse("todo 2") instanceof AddTaskCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    public void parseDeadlineTest(){
+        try {
+            assertEquals(true, parser.parse("deadline return book /by Sunday\n") instanceof AddTaskCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseEventTest(){
+        try {
+            assertEquals(true, parser.parse("event project meeting /from Mon 2pm /to 4pm") instanceof AddTaskCommand );
+        } catch (BelleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
