6735ed5463c95a79f70098dca7ed393f490f2a72
KiatLun <louiskhoo368@gmail.com>
2024-01-29 14:49:18 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "__________________________________\n" +
+                "Hello! I'm Tim \n" +
+                "What can i do for you? \n" +
+                "__________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "__________________________________";
+
+        System.out.println(logo);
     }
 }
