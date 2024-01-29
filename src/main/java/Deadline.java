cbc1fbc38e2bdd263da2e125d9edd292669ef698
appleraincoat <james20111951@gmail.com>
2024-01-29 17:03:43 +0800

    Change byWhen variable in Deadline class to store in LocalDateTime format.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String byWhen;
+    protected LocalDateTime byWhen;
 
     public Deadline(String name, String byWhen) {
         super(name);
-        this.byWhen = byWhen;
+        this.byWhen = parseByWhen(byWhen);
     }
 
+    private LocalDateTime parseByWhen(String byWhenString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        return LocalDateTime.parse(byWhenString, formatter);
+    }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + byWhen + ")";
+        return "[D]" + super.toString() + " (by: " + byWhen.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm")) + ")";
     }
 
     @Override
