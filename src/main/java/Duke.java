66a1c467d3b62dd013c221cff575451e75b5466b
ashleyclx <e0958221@u.nus.edu>
2024-01-29 16:56:02 +0800

    Encapsulate main program flow into parse method
    Main program flow of parsing commanf in main.
    
    Bloated main function and hard readability.
    
    Created parseCommand method to handle main assigning of methods and created list, mark, unmark, delete helper methods to encapsulate program.
    
    Pa

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,8 @@ public class Duke { // put LINE at start and end of switch then can remove all i
     private static final String LINE = "    ____________________________________________________________\n";
     private static final String INDENT = "    ";
     private static final ArrayList<Task> tasks = new ArrayList<Task>();
+
+    private static boolean running = true;
     public enum Command {
         BYE("bye"),
         LIST("list"),
@@ -36,98 +38,97 @@ public class Duke { // put LINE at start and end of switch then can remove all i
     public static void main(String[] args) {
         Duke.hello();
         Scanner in = new Scanner(System.in);
-        boolean running = true;
+
         while (running) {
-            System.out.print(LINE);
+
+
+            // get next command and arguments
             String input = in.nextLine().trim();
-            String[] cmdarg = input.split(" ", 2); // [command, arguments]
-            Command cmd = Command.valueOfCommandName(cmdarg[0]);
-            //add try catch here
+            String[] cmdArg = input.split(" ", 2); // [command, arguments]
+
+            System.out.print(LINE);
             try {
-                if (cmd != null) {
-                    switch (cmd) { // commands
-                        case BYE:
-                            Duke.bye();
-                            running = false;
-                            break;
-                        case LIST:
-                            System.out.println(INDENT + "I gotchu bruv. Here's your list:");
-                            for (int i = 0; i < tasks.size(); i++) {
-                                System.out.println(INDENT + (i + 1) + "." + tasks.get(i));
-                            }
-                            break;
-                        case MARK:
-                        case UNMARK:
-                        case DELETE:
-                            if (cmdarg.length == 2) {
-                                try {
-                                    int i = Integer.parseInt(cmdarg[1]);
-                                    if (i > tasks.size()) { // incorrect index
-                                        throw(new DukeException(INDENT + "You ain't got that many tasks bruh!"));
-                                    } else if (i < 1) { // incorrect index
-                                        throw(new DukeException(INDENT + "Start from task 1 lil bro!"));
-                                    } else if (cmdarg[0].equals("mark")) {
-                                        Task task = tasks.get(i - 1);
-                                        task.markAsDone();
-                                        System.out.println(INDENT + "Yasss King/Queen! This task is officially slayed:\n  "
-                                                + INDENT + task);
-                                    } else if (cmdarg[0].equals("unmark")) {
-                                        Task task = tasks.get(i - 1);
-                                        task.unmark();
-                                        System.out.println(INDENT + "You forgor:\n  " + INDENT + task);
-                                    } else {
-                                        Task task = tasks.get(i - 1);
-                                        tasks.remove(i - 1);
-                                        System.out.println(INDENT + "Zamn! This task never happened:\n  "
-                                                + INDENT + task);
-                                    }
-                                } catch (java.lang.NumberFormatException e) { // non number typed
-                                    throw(new DukeException(INDENT + "Ain't no way! We lackin' just numbers after mark/unmark/delete.\n"
-                                            + INDENT + "e.g. unmark 2"));
-                                } catch (DukeException e) {
-                                    throw(e);
-                                }
-                            } else { // no arguments
-                                throw(new DukeException(INDENT + "Ain't no way! Which task in the list we vibin' with?\n"
-                                        + INDENT + "e.g. mark/unmark/delete 1"));
-                            }
-                            break;
-                        case TODO:
-                            if (cmdarg.length == 2) {
-                                addTask(cmdarg[1], Task.ID.TODO);
-                            } else { // no arguments
-                                throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
-                                        + INDENT + "e.g. todo <task>"));
-                            }
-                            break;
-                        case DEADLINE:
-                            if (cmdarg.length == 2) {
-                                addTask(cmdarg[1], Task.ID.DEADLINE);
-                            } else { // no arguments
-                                throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
-                                        + INDENT + "e.g. deadline <task> /by <date/time>"));
-                            }
-                            break;
-                        case EVENT:
-                            if (cmdarg.length == 2) {
-                                addTask(cmdarg[1], Task.ID.EVENT);
-                            } else { // no arguments
-                                throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
-                                        + INDENT + "e.g. event <task> /from <start date/time> /to <start date/time>"));
-                            }
-                            break;
-                        default: // Shouldn't reach here, invalid commands should be null
-                            throw(new DukeException(INDENT + "What is blud yappin'? Here's the legit commands:"
-                                    + INDENT + "list, todo, deadline, event, mark, unmark, delete, bye"));
-                    }
-                } else { // Invalid Command
-                    throw(new DukeException(INDENT + "What is blud yappin'? Here's the legit commands:\n"
-                            + INDENT + "list, todo, deadline, event, mark, unmark, delete, bye"));
-                }
+                parseCommand(cmdArg);
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
             }
+
             System.out.print(LINE);
+        }
+    }
+
+    public static void parseCommand(String[] cmdArg) throws DukeException {
+        Command cmd = Command.valueOfCommandName(cmdArg[0]);
+
+        // invalid command
+        if (cmd == null) throw (new DukeException(INDENT + "What is blud yappin'? Here's the legit commands:\n"
+                    + INDENT + "list, todo, deadline, event, mark, unmark, delete, bye"));
+
+        // Commands
+        switch (cmd) {
+            case BYE:
+                Duke.bye();
+                break;
+            case LIST:
+                listTasks();
+                break;
+            case MARK:
+            case UNMARK:
+            case DELETE:
+                if (cmdArg.length == 2) {
+                    try {
+                        int i = Integer.parseInt(cmdArg[1]);
+
+                        // incorrect index
+                        if (i > tasks.size()) {
+                            throw(new DukeException(INDENT + "You ain't got that many tasks bruh!"));
+                        } else if (i < 1) { // incorrect index
+                            throw(new DukeException(INDENT + "Start from task 1 lil bro!"));
+                        }
+
+                        // Execute MARK/UNMARK/DELETE
+                        if (cmdArg[0].equals("mark")) {
+                            markTask(i);
+                        } else if (cmdArg[0].equals("unmark")) {
+                            unmarkTask(i);
+                        } else {
+                            deleteTask(i);
+                        }
+                    } catch (java.lang.NumberFormatException e) { // non number typed
+                        throw(new DukeException(INDENT + "Ain't no way! We lackin' just numbers after mark/unmark/delete.\n"
+                                + INDENT + "e.g. unmark 2"));
+                    } catch (DukeException e) {
+                        throw(e);
+                    }
+                } else { // no arguments
+                    throw(new DukeException(INDENT + "Ain't no way! Which task in the list we vibin' with?\n"
+                            + INDENT + "e.g. mark/unmark/delete 1"));
+                }
+                break;
+            case TODO:
+                if (cmdArg.length != 2) { // no arguments
+                    throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
+                            + INDENT + "e.g. todo <task>"));
+                }
+                addTask(cmdArg[1], Task.ID.TODO);
+                break;
+            case DEADLINE:
+                if (cmdArg.length != 2) { // no arguments
+                    throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
+                            + INDENT + "e.g. deadline <task> /by <date/time>"));
+                }
+                addTask(cmdArg[1], Task.ID.DEADLINE);
+                break;
+            case EVENT:
+                if (cmdArg.length != 2) { // no arguments
+                    throw(new DukeException(INDENT + "Ain't no way! You got caught lackin' the format!\n"
+                            + INDENT + "e.g. event <task> /from <start date/time> /to <start date/time>"));
+                }
+                addTask(cmdArg[1], Task.ID.EVENT);
+                break;
+            default: // Shouldn't reach here, invalid commands should be null
+                throw(new DukeException(INDENT + "What is blud yappin'? Here's the legit commands:"
+                        + INDENT + "list, todo, deadline, event, mark, unmark, delete, bye"));
         }
     }
 
@@ -146,6 +147,14 @@ public class Duke { // put LINE at start and end of switch then can remove all i
 
     public static void bye() {
         System.out.print(INDENT + "Peace out, fam! Stay lit and keep those good vibes rollin'!\n");
+        running = false;
+    }
+
+    public static void listTasks() {
+        System.out.println(INDENT + "I gotchu bruv. Here's your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(INDENT + (i + 1) + "." + tasks.get(i));
+        }
     }
 
     // add task according to what type they are
@@ -193,5 +202,23 @@ public class Duke { // put LINE at start and end of switch then can remove all i
             default: // Invalid Task ID
                 throw(new DukeException("Invalid Task ID, user shouldn't reach here"));
         }
+    }
+
+    public static void markTask(int i) {
+        Task task = tasks.get(i - 1);
+        task.markAsDone();
+        System.out.println(INDENT + "Yasss King/Queen! This task is officially slayed:\n  " + INDENT + task);
+    }
+
+    public static void unmarkTask(int i) {
+        Task task = tasks.get(i - 1);
+        task.unmark();
+        System.out.println(INDENT + "You forgor:\n  " + INDENT + task);
+    }
+
+    public static void deleteTask(int i) {
+        Task task = tasks.get(i - 1);
+        tasks.remove(i - 1);
+        System.out.println(INDENT + "Zamn! This task never happened:\n  " + INDENT + task);
     }
 }
