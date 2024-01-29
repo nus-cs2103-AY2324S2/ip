4c8ee92450311540e26ecb8f157e265c7323eee8
Alteqa <e0969816@u.nus.edu>
2024-01-29 13:50:42 +0800

    added delete

--- src/main/java/hirwan.java
+++ src/main/java/hirwan.java
@@ -13,6 +13,7 @@ public class hirwan {
         int count = 1;
         List<String> List = new ArrayList<>();
         List<String> type = new ArrayList<>();
+//        List<Integer> indexes = new ArrayList<>();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -24,18 +25,20 @@ public class hirwan {
             boolean todo = text.startsWith("todo");
             boolean deadline = text.startsWith("deadline");
             boolean event = text.startsWith("event");
+            boolean delete = text.startsWith("delete");
             boolean bye = text.equals("bye");
 
             if (bye) {
                 break;
             } else if (isList) {
                 for (String element : List) {
-                    System.out.println(element);
+                    System.out.println(List.indexOf(element) + 1 + element);
                 }
             } else if (todo) {
                 try {
                     System.out.println("Got it. I've added this task: \n  " + "[T][ ] " + text.substring(5));
-                    List.add(count + ". " + "[T][ ] " + text.substring(5));
+                    List.add(". " + "[T][ ] " + text.substring(5));
+//                    indexes.add(count);
                     type.add("[T]");
                     System.out.println("Now you have " + count + " tasks in the list.");
                     count++;
@@ -48,7 +51,8 @@ public class hirwan {
                     int Index = text.indexOf(delimiter);
                     String Day = text.substring(Index + 5);
                     String item = text.substring(9, Index);
-                    List.add(count + ". " + "[D][ ] " + item + " (by: " + Day + ")");
+                    List.add(". " + "[D][ ] " + item + " (by: " + Day + ")");
+//                    indexes.add(count);
                     System.out.println("Got it. I've added this task:\n  " + "[D][ ] " + item + " (by: " + Day + ")");
                     type.add("[D]");
                     System.out.println("Now you have " + count + " tasks in the list.");
@@ -65,7 +69,8 @@ public class hirwan {
                     String start = text.substring(Indexstart + 7, Indexend);
                     String end = text.substring(Indexend + 5);
                     String item = text.substring(6, Indexstart);
-                    List.add(count + ". " + "[E][ ] " + item + " (from: " + start + " to: " + end + ")");
+                    List.add(". " + "[E][ ] " + item + " (from: " + start + " to: " + end + ")");
+//                    indexes.add(count);
                     System.out.println("Got it. I've added this task:\n  " + "[E][ ] " + item + " (from: " + start + " to: " + end + ")");
                     type.add("[E]");
                     System.out.println("Now you have " + count + " tasks in the list.");
@@ -77,8 +82,8 @@ public class hirwan {
                 try {
                     String number = text.substring(5);
                     int numberint = Integer.parseInt(number);
-                    String temp = List.get(numberint - 1).substring(10);
-                    List.set(numberint - 1, numberint + ". " + type.get(numberint - 1) + "[X] " + temp);
+                    String temp = List.get(numberint - 1).substring(9);
+                    List.set(numberint - 1, ". " + type.get(numberint - 1) + "[X] " + temp);
                     System.out.println("Nice! I've marked this task as done: \n" + "[X] " + temp);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Error: Please enter a valid index for marking!");
@@ -89,14 +94,28 @@ public class hirwan {
                 try {
                     String number = text.substring(7);
                     int numberint = Integer.parseInt(number);
-                    String temp = List.get(numberint - 1).substring(10);
-                    String typestr = "";
-                    List.set(numberint - 1, numberint + ". " + type.get(numberint - 1) + "[ ] " + temp);
+                    String temp = List.get(numberint - 1).substring(9);
+                    List.set(numberint - 1, ". " + type.get(numberint - 1) + "[ ] " + temp);
                     System.out.println("OK, I've marked this task as not done yet: \n" + "[ ] " + temp);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Error: Please enter a valid index for unmarking!");
                 } catch (NumberFormatException e) {
                     System.out.println("Error: Please enter a numerical index to unmark!");
+                }
+            } else if (delete) {
+                try {
+                    int numberint = Integer.parseInt(text.substring(7)) - 1;
+                    System.out.println("Noted. I've removed this task:\n"
+                            + "  " + List.get(numberint).substring(2) + "\n"
+                            + "Now you have " + (count - 2) + " tasks in the list.");
+                    List.remove(numberint);
+                    type.remove(numberint);
+                    count--;
+//                    indexes.remove();
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Error: Please enter a valid index for deletion!");
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Please enter a numerical index to delete!");
                 }
             } else {
 //                System.out.println("added: " + text);
