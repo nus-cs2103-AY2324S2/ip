5eaf7171f353c41e9c9b1b18cb698d59f3ed9e08
Vision-2000 <122250743+Vision-2000@users.noreply.github.com>
2024-01-29 19:55:27 +0800

    Stille.java: Change echo to list functionality

--- src/main/java/Stille.java
+++ src/main/java/Stille.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Stille {
     public static void main(String[] args) {
@@ -7,15 +8,26 @@ public class Stille {
         String input = "";
         String closeCommand = "bye";
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>(100);
 
         System.out.println(openingMessage);
 
         while (true) {
             input = sc.nextLine();
+            if (input.equals("")) {
+                continue;
+            }
             if (input.equals(closeCommand)) {
                 break;
             }
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (String line : list) {
+                    System.out.println(line);
+                }
+                continue;
+            }
+            list.add((list.size() + 1) + ". " + input);
+            System.out.println("added: " + input);
         }
 
         System.out.println(closingMessage);
