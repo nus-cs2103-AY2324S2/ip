70686786299fb41dc75d344abd299a13d657625b
chonghaoen <cocopowder999@gmail.com>
2024-01-29 23:31:12 +0800

    Add saving of task list in the hard disk.

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,13 +1,18 @@
+import java.io.IOException;
 import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileWriter;
 public class TaskManager {
     ArrayList<Task> list = new ArrayList<>();
-    private int NoOfTask = 0;
+    private int numOfTask = 0;
     public void addTask(Task t) {
         System.out.println("added: ");
         System.out.println(t.toString());
         this.list.add(t);
-        this.NoOfTask++;
-        System.out.println("There are now " + this.NoOfTask + " tasks in the list.");
+        this.numOfTask++;
+        System.out.println("There are now " + this.numOfTask + " tasks in the list.");
+        this.saveTaskList();
     }
     public void listTasks() {
         int count = 1;
@@ -19,17 +24,44 @@ public class TaskManager {
     public void markDone(int i) {
         Task t = list.get(i - 1);
         t.setDone(true);
+        this.saveTaskList();
     }
 
     public void undo(int i) {
         Task t = list.get(i - 1);
         t.setDone(false);
+        this.saveTaskList();
     }
 
-    public void delete(int i) {
+    public void deleteTask(int i) {
         Task t = list.remove(i - 1);
-        this.NoOfTask--;
+        this.numOfTask--;
         System.out.println(t.toString());
-        System.out.println("There are now " + this.NoOfTask + " tasks in the list.");
+        System.out.println("There are now " + this.numOfTask + " tasks in the list.");
+        this.saveTaskList();
+    }
+
+    public void saveTaskList() {
+        try {
+            File taskList = new File("./data/duke.txt");
+            if (!taskList.getParentFile().exists()) {
+                taskList.getParentFile().mkdirs();
+            }
+            if (!taskList.exists()) {
+                taskList.createNewFile();
+            }
+            FileWriter fw = new FileWriter(taskList);
+            if (this.numOfTask == 1) {
+                fw.write("There is 1 task in the list.\n");
+            } else {
+                fw.write("There are " + this.numOfTask + " tasks in the list.\n");
+            }
+            for (Task t : list) {
+                fw.append(t.toString() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 }
