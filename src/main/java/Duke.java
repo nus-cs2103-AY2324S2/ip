540b1aa56117e9f51ebbfa617645b9df4b490158
kinjalagarwal1810 <105915730+kinjalagarwal1810@users.noreply.github.com>
2024-01-31 01:52:56 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Duke {
-    private static final String FILE_PATH = "./data/duke.txt";
+    private static final String FILE_PATH = "./data/jamie.txt";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Declare Scanner instance
 
