e641c28798a2ed5076938e68e7cbc3ba56d17ac4
yashma-sonara <yashma.sonara04@gmail.com>
2024-01-30 20:42:48 +0800

    add task package

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,4 +1,8 @@
 package duke;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
 import java.io.IOError;
 import java.io.IOException;
 import java.time.LocalDate;
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,4 +1,8 @@
 package duke;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
