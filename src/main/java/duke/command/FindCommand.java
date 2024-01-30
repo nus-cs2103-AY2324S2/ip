c32e54efb08bdf5ef72e771da968a6daa88768fb
vnnamng <e0969384@u.nus.edu>
2024-01-30 22:25:37 +0800

    Add Working Find Command into the ChatBot

---
+++ src/main/java/duke/command/FindCommand.java
@@ -0,0 +1,21 @@
+package duke.command;
+
+import duke.exception.ChatBotParameterException;
+import duke.storage.Storage;
+import duke.task.Task;
+import duke.task.TaskList;
+import duke.ui.Ui;
+
+import java.util.List;
+
+public class FindCommand extends Command {
+    public FindCommand(String keyword, String parameters) {
+        super(keyword, parameters);
+    }
+
+    @Override
+    public void execute(Storage storage, Ui ui, TaskList taskList) throws ChatBotParameterException {
+        List<Task> taskListSearchResult = taskList.findFromKeyword(parameters);
+        ui.showFindResult(taskListSearchResult);
+    }
+}
