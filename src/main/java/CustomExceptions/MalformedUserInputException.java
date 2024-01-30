1cd73c1019b6b1fc109b7a0840dd2505b0a9734f
Jerome-j <jerome@outlook.sg>
2024-01-30 09:01:09 +0800

    Enable custom message in MalformedUserInputException

--- src/main/java/CustomExceptions/MalformedUserInputException.java
+++ src/main/java/CustomExceptions/MalformedUserInputException.java
@@ -2,6 +2,6 @@ package CustomExceptions;
 
 public class MalformedUserInputException extends Exception {
     public MalformedUserInputException(String message) {
-        super("There appears to be some problem with your user input.");
+        super(message);
     }
 }
