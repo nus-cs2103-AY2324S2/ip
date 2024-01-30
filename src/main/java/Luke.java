6413cfaa9668fd180b2ff62a53be3d4d8475c12e
LWS49 <e0970244@u.nus.edu>
2024-01-30 11:56:45 +0800

    added Level-7

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,8 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Luke {
 
     static String[] validCommands = {"bye", "list", "unmark", "mark", "todo", "event", "deadline", "delete"};
+
+    static String directoryPath = "./data";
+
+    static String fileName = "list.txt";
+
+    static File file = new File (directoryPath, fileName);
+
+    static boolean fileIsEmpty = true;
 
     private static boolean isCommandValid(String command) {
         for (String validCommand: validCommands) {
@@ -19,7 +31,63 @@ public class Luke {
 
         ArrayList<Task> taskList = new ArrayList<Task>();
 
+        ArrayList<String> saveTaskList = new ArrayList<String>();
+
         int noTasks = 0;
+
+        try {
+            File directory = file.getParentFile();
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to save file: " + e.getMessage());
+        }
+
+        try {
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (data != "") {
+                    saveTaskList.add(data);
+                    noTasks++;
+                }
+            }
+            myReader.close();
+            fileIsEmpty = saveTaskList.isEmpty();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+
+        saveTaskList.forEach(taskString -> {
+           String taskType = taskString.substring(1, 2);
+           switch (taskType) {
+               case "T":
+                   Todo todo = new Todo(taskString.substring(7).trim());
+                   taskList.add(todo);
+                   break;
+               case "D":
+                   String[] deadlineSplit = taskString.split("by: ");
+                   String deadlineDescription = deadlineSplit[0].substring(7, deadlineSplit[0].length() - 1);
+                   String by = deadlineSplit[1].substring(0, deadlineSplit[1].length() - 1).trim();
+                   Deadline deadline = new Deadline(deadlineDescription, by);
+                   taskList.add(deadline);
+                   break;
+               case "E":
+                   String[] eventFirstSplit = taskString.split("from: ");
+                   String[] eventSecondSplit = eventFirstSplit[1].split(" to: ");
+                   String eventDescription = eventFirstSplit[0].substring(7, eventFirstSplit[0].length() - 1);
+                   String from = eventSecondSplit[0].trim();
+                   String to = eventSecondSplit[1].substring(0, eventSecondSplit[1].length() - 1).trim();
+                   Event event = new Event(eventDescription, from, to);
+                   taskList.add(event);
+                   break;
+           }
+        });
 
         System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
 
@@ -161,6 +229,18 @@ public class Luke {
                     }
             }
             input = scanner.nextLine();
+        }
+        try {
+            FileWriter writer = new FileWriter(directoryPath + "/" + fileName);
+            for (Task task : taskList) {
+                writer.write(task.toString());
+                writer.write("\n");
+            }
+            writer.close();
+            System.out.println("Successfully saved file to local.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving.");
+            e.printStackTrace();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
