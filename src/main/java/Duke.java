ad5e2fc5ec3c44dbbc7986c51ba6c3edef0fd18e
HusseinSafwan02 <safwanhussein.ri@gmail.com>
2024-01-30 13:24:28 +0800

    Echo and "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String cat = "(\\_/)\n" +
                 "( •,•)\n" +
                 "(\")_(\")";
         System.out.println("RAWR!\n" + cat);
-        String msg = "____________________________________________________________\r\n" +
+        String intro = "____________________________________________________________\r\n" +
                 " Hello! I'm Mickey\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\r\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\r\n";
-        System.out.println(msg);
+
+        String outro = "____________________________________________________________\r\n" +
+                " Bye. See you soon!\n" +
+                "____________________________________________________________\r\n";
+
+        System.out.println(intro);
+
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+
+        while(true) {
+            userInput = sc.nextLine();
+             if (userInput.equals("bye")) {
+                 System.out.println(outro);
+                 break;
+             } else {
+                 System.out.println(userInput);
+             }
+        }
 
     }
 }
