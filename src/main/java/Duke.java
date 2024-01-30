61ac06aab93e320376e3a8e64692bdea2942b673
Tan Xinhao <e0774267@u.nus.edu>
2024-01-31 00:21:40 +0800

    level 0 & level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "CMU_bot";
+        System.out.println("Hello! Boss I'm your" + logo);
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        boolean isExit = true;
+        while (isExit) {
+            String command = sc.nextLine();
+            if(command.equals("bye")) {
+                System.out.println("Bye. CMU_bot is always here for you!");
+                isExit = false;
+            } else {
+                System.out.println(command);
+            }
+
+        }
     }
 }
