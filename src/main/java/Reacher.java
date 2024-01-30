de1de05e4473ca59a372958a6d252e596fb37102
zhenghuil <e0959119@u.nus.edu>
2024-01-30 18:48:06 +0800

    added echo

--- src/main/java/Reacher.java
+++ src/main/java/Reacher.java
@@ -1,6 +1,19 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Reacher {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello!\n" +
-                "I'm Reacher.\n");
+                "I'm Reacher.\n" +
+                "What do u want?\n" +
+                "Pls say bye");
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(input);
+            if (Objects.equals(input,"bye")) {
+                break;
+            }
+        }
+        scanner.close();
     }
 }
