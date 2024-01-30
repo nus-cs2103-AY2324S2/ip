aa0776b7a6b9d278f39d94c819107083c5f35067
gerteck <changerteck@gmail.com>
2024-01-31 06:57:56 +0800

    Add Ui Class

---
+++ src/main/java/ellie/Ui.java
@@ -0,0 +1,34 @@
+package ellie;
+
+public class Ui {
+
+    final static String horizontalLine = "____________________________________________________________";
+    final static String logoEllie =
+            " _______   ___       ___       ___  _______          \n" +
+                    "|\\  ___ \\ |\\  \\     |\\  \\     |\\  \\|\\  ___ \\         \n" +
+                    "\\ \\   __/|\\ \\  \\    \\ \\  \\    \\ \\  \\ \\   __/|        \n" +
+                    " \\ \\  \\_|/_\\ \\  \\    \\ \\  \\    \\ \\  \\ \\  \\_|/__      \n" +
+                    "  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\____\\ \\  \\ \\  \\_|\\ \\     \n" +
+                    "   \\ \\_______\\ \\_______\\ \\_______\\ \\__\\ \\_______\\    \n" +
+                    "    \\|_______|\\|_______|\\|_______|\\|__|\\|_______|    \n";
+
+
+    public Ui() {
+
+    }
+
+    public void hello() {
+        System.out.println("Hello! I'm ellie.Ellie, your CS2103T chat bot! I help by tracking your tasks!");
+        System.out.println(logoEllie + "\n" + horizontalLine);
+        System.out.println("What can I do for you? Type 'help' to see available commands! \n");
+    }
+
+    public void goodbye() {
+        System.out.println("\n Bye! Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+
+
+
+
+}
