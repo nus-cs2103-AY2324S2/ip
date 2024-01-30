0c579e51b186d77cebc077308e715e7ce2c2f6f7
nigel <nigellim27022001@gmail.com>
2024-01-31 00:56:48 +0800

    Add saving of task list in the hard disk

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
 import java.util.*;
 
 public class Duke {
 
-    private static ArrayList<Task> storage = new ArrayList<>();
+    private static TaskList taskList;
+
+    private static Storage fileStorage;
     private static void greeting() {
         System.out.println("Hello! I'm Pengu\n" + "What can I do for you?\n" +
                 "\nDid you know that the noise penguins make are called \"honks\"");
@@ -14,173 +17,70 @@ public class Duke {
         System.out.println("―――――――――――――――――――――――――――――――――――");
     }
 
-    private static void addTask(String description) throws DukeException{
-        Task newTask;
-        validTaskCommand(description);
-        if (description.toLowerCase().startsWith("todo")) {
-            String[] descriptionArr = description.split(" ");
-            StringBuilder descBuilder = new StringBuilder();
-            for (int k = 1; k < descriptionArr.length; k++) {
-                if (k == descriptionArr.length - 1) {
-                    descBuilder.append(descriptionArr[k]);
-                } else {
-                    descBuilder.append(descriptionArr[k] + " ");
-                }
+    protected static boolean commandParser(String userInput, boolean isLoaded) throws DukeException, IOException{
+        if (userInput.toLowerCase().equals("bye")) {
+            exit();
+            return false;
+        } else if (userInput.toLowerCase().equals("list")) {
+            taskList.listTasks();
+            return true;
+        } else if (userInput.toLowerCase().startsWith("mark")) {
+            String[] inputArr = userInput.split(" ");
+            try {
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                taskList.markTask(index);
+                fileStorage.saveStorage(userInput);
+                return true;
+            } catch (NumberFormatException e) {
+                throw new DukeException("*HONK* Pengu thinks you need a valid task number to mark, " +
+                        "consider checking the list command");
             }
-            newTask = new ToDo(descBuilder.toString());
-        } else if (description.toLowerCase().startsWith("deadline")) {
-            String[] descriptionArr = description.split(" ");
-            int byIndex = Arrays.asList(descriptionArr).indexOf("/by");
-            StringBuilder descBuilder = new StringBuilder();
-            for (int k = 1; k < byIndex; k++) {
-                if (k == byIndex - 1) {
-                    descBuilder.append(descriptionArr[k]);
-                } else {
-                    descBuilder.append(descriptionArr[k] + " ");
-                }
+        } else if (userInput.toLowerCase().startsWith("unmark")) {
+            String[] inputArr = userInput.split(" ");
+            try {
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                taskList.unmarkTask(index);
+                fileStorage.saveStorage(userInput);
+                return true;
+            } catch (NumberFormatException e) {
+                throw new DukeException("*HONK* Pengu thinks you need a valid task number to unmark, " +
+                        "consider checking the list command");
             }
-            StringBuilder byBuilder = new StringBuilder();
-            for (int k = byIndex + 1; k < descriptionArr.length; k++) {
-                if (k == descriptionArr.length - 1) {
-                    byBuilder.append(descriptionArr[k]);
-                } else {
-                    byBuilder.append(descriptionArr[k] + " ");
-                }
+        } else if (userInput.toLowerCase().startsWith("delete")) {
+            String[] inputArr = userInput.split(" ");
+            try {
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                taskList.deleteTask(index);
+                fileStorage.saveStorage(userInput);
+                return true;
+            } catch (NumberFormatException e) {
+                throw new DukeException("*HONK* Pengu thinks you need a valid task number to delete, " +
+                        "consider checking the list command");
             }
-            /*if (descriptionArr[2].toLowerCase().equals("/by")) {
-                System.out.println("*HONKS ANGRILIY* Pengu thinks that the description of the deadline has to be followed by '/by'");
-            }*/
-            newTask = new Deadline(descBuilder.toString(), byBuilder.toString());
-        } else {
-            String[] descriptionArr = description.split(" ");
-            int fromIndex = Arrays.asList(descriptionArr).indexOf("/from");
-            StringBuilder descBuilder = new StringBuilder();
-            for (int k = 1; k < fromIndex; k++) {
-                if (k == fromIndex - 1) {
-                    descBuilder.append(descriptionArr[k]);
-                } else {
-                    descBuilder.append(descriptionArr[k] + " ");
-                }
-            }
-            int toIndex = Arrays.asList(descriptionArr).indexOf("/to");
-            StringBuilder fromBuilder = new StringBuilder();
-            for (int k = fromIndex + 1; k < toIndex; k++) {
-                if (k == toIndex - 1) {
-                    fromBuilder.append(descriptionArr[k]);
-                } else {
-                    fromBuilder.append(descriptionArr[k] + " ");
-                }
-            }
-            StringBuilder toBuilder = new StringBuilder();
-            for (int k = toIndex + 1; k < descriptionArr.length; k++) {
-                if (k == descriptionArr.length - 1) {
-                    toBuilder.append(descriptionArr[k]);
-                } else {
-                    toBuilder.append(descriptionArr[k] + " ");
-                }
-            }
-            newTask = new Event(descBuilder.toString(), fromBuilder.toString(), toBuilder.toString());
         }
-        Duke.storage.add(newTask);
-        System.out.println(String.format("*Honk! Honk!* Pengu has added this task:\n" + newTask.toString()
-                + "\nGet back to work! you have %s tasks in the list\n"
-                + "―――――――――――――――――――――――――――――――――――", Duke.storage.size()));
-    }
-
-    private static void listTasks() {
-        int storageSize = Duke.storage.size();
-        System.out.println("*Honk!* Pengu has listed your current tasks below:");
-        for (int k = 0; k < storageSize; k++){
-            int curr = k + 1;
-            Task currTask = storage.get(k);
-            System.out.println(curr + ". " + currTask.toString());
-        }
-        System.out.println("―――――――――――――――――――――――――――――――――――");
-    }
-
-    private static void markTask(int index) {
-        Task currTask = storage.get(index);
-        currTask.updateTask(true);
-        System.out.println("*Honk!* Good Job!, Pengu has marked this task as done:\n" + currTask.toString());
-        System.out.println("―――――――――――――――――――――――――――――――――――");
-    }
-
-    private static void unmarkTask(int index) {
-        Task currTask = storage.get(index);
-        currTask.updateTask(false);
-        System.out.println("*Honk!* Pengu has marked this task as not done yet:\n" + currTask.toString());
-        System.out.println("―――――――――――――――――――――――――――――――――――");
-    }
-
-    private static void deleteTask(int index) {
-        Task deletedTask = storage.remove(index);
-        System.out.println(String.format("*Honk* Pengu has removed the following task:\n" + deletedTask.toString()
-                + "\nNow you have %s tasks left", storage.size()));
-        System.out.println("―――――――――――――――――――――――――――――――――――");
-    }
-
-    private static boolean validTaskCommand(String str) throws DukeException {
-        List<String> strArr = Arrays.asList(str.split(" "));
-        String keyword = str.split(" ")[0].toLowerCase();
+        String keyword = userInput.split(" ")[0].toLowerCase();
         if (!(keyword.equals("todo") || keyword.equals("deadline") || keyword.equals("event"))){
             throw new DukeException("*HONK* Pengu has never seen such a command before, some commands Pengu can do are: list, todo, deadline");
-        } else if (keyword.equals("todo") && !(strArr.size() > 1)) {
-            throw new DukeException("*HONK* Pengu needs a To Do Task description to record this down");
-        } else if (keyword.equals("deadline") && !(strArr.contains("/by"))) {
-            throw new DukeException("*HONK* Pengu needs a /by followed by a end date for your task");
-        } else if (keyword.equals("event") && !(strArr.contains("/from") && (strArr.contains("/to")))) {
-            throw new DukeException("*HONK* Pengu needs a /from followed by a from date and a /to followed by a end date for your task");
-        } else if (keyword.equals("deadline") && strArr.get(1).equals("/by")) {
-            throw new DukeException("Honk* Pengu cannot accept a deadline task without a description");
-        } else if (keyword.equals("event") && (strArr.get(1).equals("/from") || strArr.get(1).equals("/to"))) {
-            throw new DukeException("*Honk* Pengu cannot accept a event without a description");
-        } else {
-            return true;
         }
+        if (isLoaded) {
+            taskList.addTask(userInput, true);
+        } else {
+            taskList.addTask(userInput, false);
+            fileStorage.saveStorage(userInput);
+        }
+        return true;
     }
-    public static void main(String[] args) throws DukeException {
-        greeting();
+
+    public static void main(String[] args) throws DukeException, IOException {
+        Duke.greeting();
         Scanner s = new Scanner(System.in);
+        taskList = new TaskList();
+        fileStorage = new Storage();
         while (true){
             String userInput = s.nextLine();
-            if (userInput.toLowerCase().equals("bye")) {
-                exit();
+            if(!commandParser(userInput, false)){
                 break;
-            } else if (userInput.toLowerCase().equals("list")) {
-                listTasks();
-                continue;
-            } else if (userInput.toLowerCase().startsWith("mark")) {
-                String[] inputArr = userInput.split(" ");
-                try {
-                    int index = Integer.parseInt(inputArr[1]) - 1;
-                    markTask(index);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("*HONK* Pengu thinks you need a valid task number to mark, " +
-                            "consider checking the list command");
-                }
-                continue;
-            } else if (userInput.toLowerCase().startsWith("unmark")) {
-                String[] inputArr = userInput.split(" ");
-                try {
-                    int index = Integer.parseInt(inputArr[1]) - 1;
-                    unmarkTask(index);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("*HONK* Pengu thinks you need a valid task number to unmark, " +
-                            "consider checking the list command");
-                }
-                continue;
-            } else if (userInput.toLowerCase().startsWith("delete")) {
-                String[] inputArr = userInput.split(" ");
-                try {
-                    int index = Integer.parseInt(inputArr[1]) - 1;
-                    deleteTask(index);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("*HONK* Pengu thinks you need a valid task number to delete, " +
-                            "consider checking the list command");
-                }
-                continue;
             }
-            addTask(userInput);
         }
     }
 }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class Storage {
+
+    private static String DATAFILEPATH = "./data";
+    private static String STORAGEFILEPATH = "./data/duke.txt";
+
+    private File directory;
+    private File storageFile;
+
+    public Storage() throws IOException,DukeException {
+        directory = new File(DATAFILEPATH);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        storageFile = new File(STORAGEFILEPATH);
+        if(!storageFile.exists()) {
+            storageFile.createNewFile();
+        }
+        this.loadStorage();
+    }
+
+    private void loadStorage() throws IOException,DukeException {
+        Scanner s = new Scanner(storageFile);
+        while (s.hasNextLine()) {
+            String storedInput = s.nextLine();
+            Duke.commandParser(storedInput, true);
+        }
+    }
+
+    protected void saveStorage(String command) throws IOException {
+        FileWriter fw = new FileWriter(STORAGEFILEPATH, true);
+        fw.append("\n" + command);
+        fw.close();
+    }
+}
