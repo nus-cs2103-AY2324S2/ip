ec99ab5ae273eaf1c089ceea1907b59467a16a54
Chia Rena <Chocorena@gmail.com>
2024-01-30 19:18:29 +0800

    Level 1-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,11 +9,28 @@ public class Duke {
         String intro = "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+
+        String outro = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
 
         System.out.println(intro);
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Message");
+
+        while (true) {
+            String message = input.nextLine();
+            if (message.equals("bye")) {
+                System.out.println(outro);
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        message + "\n" +
+                        "____________________________________________________________\n");
+            }
+
+        }
     }
 }
