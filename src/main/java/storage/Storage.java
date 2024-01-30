e3d3af9091fe18c22b7f4da8472802416c552e73
ivan24004 <ivan@ivant.com>
2024-01-30 23:42:43 +0800

    Parse the storage file

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,18 +1,24 @@
 package storage;
 
+import model.StoredTaskList;
 import model.TaskList;
-import utils.RemiError;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Storage {
+    private static final String FILE_PATH = "tasks.txt";
+
     /**
      * Stores the string representation of the taskList in a designated file.
      */
-    public static void store(TaskList taskList) {
-        try (FileWriter out = new FileWriter("output.txt");){
-            out.write(taskList.toString());
+    public static void store(StoredTaskList taskList) {
+        try (FileWriter out = new FileWriter(FILE_PATH)){
+            out.write(taskList.parsableString());
             out.close();
         } catch (IOException err) {
             // do nothing
@@ -25,8 +31,25 @@ public class Storage {
      *
      * @return the stored TaskList object
      */
-    public static TaskList get() {
-        TaskList output = new TaskList();
+    public static StoredTaskList get() {
+        StoredTaskList output = new StoredTaskList();
+        File f = new File(FILE_PATH);
+        try {
+            // only creates file if it doesn't exist yet
+            f.createNewFile();
+        } catch (IOException err) {
+            System.out.println(err.toString());
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(f))) {
+            Stream<String> allLines = in.lines();
+            Stream<ArrayList<String>> splitParams = allLines.map((line) ->
+                    new ArrayList<>(Arrays.asList(line.split("\\|"))));
+            ArrayList<ArrayList<String>> data = splitParams.collect(Collectors.toCollection(ArrayList::new));
+            System.out.println(data);
+        } catch (IOException err) {
+            System.out.println(err.toString());
+        }
 
         return output;
     }
