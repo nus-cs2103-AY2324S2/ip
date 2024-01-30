95e38afc601438e925c0b2c64485e87a20de765d
Jajared <jajawong09@gmail.com>
2024-01-31 00:36:36 +0800

    refactor: change to use date object for tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,26 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-  protected String by;
+  protected LocalDate by;
 
   public static Deadline createFromInput(String input) throws DukeException {
     try {
       String[] parts = input.split("/by ");
       String description = parts[0].split("deadline ")[1];
       String by = parts[1];
-      return new Deadline(description, by);
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      LocalDate parsedBy = LocalDate.parse(by, formatter);
+      return new Deadline(description, parsedBy);
     } catch (ArrayIndexOutOfBoundsException e) {
       throw new DukeException("Invalid deadline format. Please use 'deadline description /by date'.");
+    } catch (DateTimeParseException e) {
+      throw new DukeException("Invalid date format. Please use 'yyyy-MM-dd'.");
     }
   }
 
-  public Deadline(String description, String by) {
+  public Deadline(String description, LocalDate by) {
     super(description);
     this.by = by;
   }
 
   @Override
   public String toString() {
-    return "[D]" + super.toString() + " (by: " + by + ")";
+    return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
   }
 
   @Override
