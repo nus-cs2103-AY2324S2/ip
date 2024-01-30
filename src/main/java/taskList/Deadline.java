63de1e56627e3ee5a1a40459ab0701ba4ab628b2
unknown <xuelinglow09@gmail.com>
2024-01-31 01:48:45 +0800

    Duke Level-8: Dates and Times

--- src/main/java/taskList/Deadline.java
+++ src/main/java/taskList/Deadline.java
@@ -1,24 +1,34 @@
 package taskList;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Deadline extends Task {
 
-    protected String byDate;
+    protected LocalDateTime deadlineDate;
+    protected String dateTimeString;
 
     public Deadline(String item, String byDate) {
         super(item);
         try {
             String[] dateString = byDate.split(" ", 2);
-            //LocalDate thedate = LocalDate.parse(dateString[1].trim());
-            this.byDate = dateString[0].trim() + ": " + dateString[1].trim();
+            //System.out.println(dateString[1].trim());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy HHmm");
+
+            LocalDateTime deadlineDate = LocalDateTime.parse(dateString[1].trim(),formatter);
+            this.deadlineDate = deadlineDate;
+            this.dateTimeString = dateString[0].trim() + ": " + dateString[1].trim();
          } catch (ArrayIndexOutOfBoundsException e) {
             throw new ArrayIndexOutOfBoundsException("Please enter a date");
+        } catch (DateTimeParseException dte) {
+            throw new DateTimeParseException("Date format is incorrect, please try again", byDate, 0, dte);
         }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (" + byDate + ")";
+        return "[D]" + super.toString() + " (" + dateTimeString + ")";
     }
 }
