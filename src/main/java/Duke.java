64633b55a3228af7e9463b9bb3871af6d4039943
Jerome-j <jerome@outlook.sg>
2024-01-30 18:49:48 +0800

    Remove old code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -66,59 +66,8 @@ public class Duke {
             ui.showLine();
             System.out.println(commandResult.feedbackToUser);
             ui.showLine();
-//            else if (userInput.startsWith("mark")) {
-//                handleCommandWithIndex(dataStorage, userInput, TypeOfActions.MARK);
-//            } else if (userInput.startsWith("unmark")) {
-//                handleCommandWithIndex(dataStorage, userInput, TypeOfActions.UNMARK);
-//            } else if (userInput.startsWith("delete")) {
-//                handleCommandWithIndex(dataStorage, userInput, TypeOfActions.DELETE);
-//            } else if (userInput.startsWith("todo")) {
-//
 
         } while (!ByeCommand.isExit(command));
-    }
-
-
-    public static void handleCommandWithIndex(DataStorage dataStorage, String userInput, TypeOfActions typeOfActions) {
-        try {
-            int idToMark = Integer.parseInt(userInput.split(" ")[1]) - 1;
-
-            if (typeOfActions == TypeOfActions.UNMARK) {
-                dataStorage.setTaskStatus(idToMark, false);
-
-                System.out.println("\t ____________________________________________________________");
-                System.out.println("\t  Nice! I've marked this task as not completed yet:");
-                System.out.println("\t " + dataStorage.getTask(idToMark).toString());
-                System.out.println("\t ____________________________________________________________");
-            } else if (typeOfActions == TypeOfActions.MARK) {
-                dataStorage.setTaskStatus(idToMark, true);
-
-                System.out.println("\t ____________________________________________________________");
-                System.out.println("\t Nice! I've marked this task as done:");
-                System.out.println("\t " + dataStorage.getTask(idToMark).toString());
-                System.out.println("\t ____________________________________________________________");
-
-            } else if (typeOfActions == TypeOfActions.DELETE) {
-                Task task = dataStorage.getTask(idToMark);
-                dataStorage.deleteTask(idToMark);
-
-                System.out.println("\t ____________________________________________________________");
-                System.out.println("\t Noted. I've removed this task:");
-                System.out.println("\t " + task.toString());
-                System.out.println("\t Now you have " + dataStorage.getTaskCount() + " tasks in the list.");
-                System.out.println("\t ____________________________________________________________");
-            }
-
-
-        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
-            System.out.println("\t ____________________________________________________________");
-            System.out.println("\t Please do not enter an invalid index. There are " + dataStorage.getTaskCount() + " task(s) currently.");
-            System.out.println("\t ____________________________________________________________");
-        } catch (NumberFormatException | MalformedUserInputException numberFormatException) {
-            System.out.println("\t ____________________________________________________________");
-            System.out.println("\t  Please enter positive integers 1, 2, 3, ... etc only.");
-            System.out.println("\t ____________________________________________________________");
-        }
     }
 
 
