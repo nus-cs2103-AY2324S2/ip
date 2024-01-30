cfef695cc95a7d9fe9336b7fc7e134cab4952039
Chua Zi Long <e0957913@u.nus.edu>
2024-01-30 15:47:29 +0800

    no message

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -1,2 +1,23 @@
-package seedu.duke;public class ParserTest {
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+    @Test
+    public void parse_addTodoCommand() throws DukeException {
+        Command command = Parser.parse("todo read book");
+        assertTrue(command instanceof AddTodoCommand, "Parser should return AddTodoCommand for 'todo' input.");
+    }
+
+    @Test
+    public void parse_invalidCommand() {
+        assertThrows(DukeException.class, () -> {
+            Parser.parse("invalid command");
+        });
+    }
+
+    // ... add more test methods if necessary ...
 }
+
