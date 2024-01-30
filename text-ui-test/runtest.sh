46f6ebc56c801ecec38d56c4aee5f6b5573d7f38
bachletuankhai <bachletuankhai@gmail.com>
2024-01-30 20:42:12 +0800

    Add OUT-DATA.TXT to .gitignore and update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -24,6 +24,10 @@ fi
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 F:/Misc/jdk-11.0.17/bin/java.exe -classpath ../bin Duke < input.txt > ACTUAL.TXT
 
+# copy generated data and delete the generated data folder
+mv ./data/duke.txt ./OUT-DATA.txt
+rm -r ./data
+
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
@@ -33,11 +37,13 @@ diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
+    # Change back to the original directory
+    popd > /dev/null
     exit 0
 else
     echo "Test result: FAILED"
+    # Change back to the original directory
+    popd > /dev/null
     exit 1
 fi
 
-# Change back to the original directory
-popd > /dev/null
