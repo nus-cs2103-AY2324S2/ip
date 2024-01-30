0cd451e3bc9c02392a305ac76ae81a4fb5349857
ivan24004 <ivan@ivant.com>
2024-01-30 22:31:13 +0800

    Added Storage class for interacting with the storage

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,4 +1,31 @@
 package storage;
 
+import model.TaskList;
+import utils.RemiError;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Storage {
+    /**
+     * Stores the string representation of the taskList in a designated file.
+     */
+    public static void store(TaskList taskList) {
+        try (FileWriter out = new FileWriter("output.txt");){
+            out.write(taskList.toString());
+        } catch (IOException err) {
+            // do nothing
+        }
+    }
+
+    /**
+     * Retrieves a previously stored data file and returns its representation as a TaskList object.
+     *
+     * @return the stored TaskList object
+     */
+    public static TaskList get() {
+        TaskList output = new TaskList();
+
+        return output;
+    }
 }
