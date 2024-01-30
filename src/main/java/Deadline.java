689cc640970a31a81da93ec7971b45afea1cb633
yespiggy <skylarye7@gmail.com>
2024-01-30 22:05:39 +0800

    Level-8 Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = by.trim(); // Trim leading and trailing whitespace
+        this.date = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";
+        //return "[D]" + super.toString() + "(by:" + getBy() + ")";
+        return "[D]" + super.toString() + "(by:" + getDate().format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 
     public String getBy() {
         return by;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 }
