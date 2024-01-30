2ca1db28e88b045a4abd669c00f355dc240e4353
kokerinks <limkokliang01@gmail.com>
2024-01-30 19:15:42 +0800

    Add JUnits test classes for Todo and Deadline

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -1,2 +1,22 @@
-package duke.task;public class DeadlineTest {
+package duke.task;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void toString_normalInput_success() {
+        LocalDateTime dateTime = LocalDateTime.parse("2021-08-24T18:00");
+        assertEquals("[D][ ] read book (by: Aug 24 2021 6:00PM)",
+                new Deadline("read book", dateTime).toString());
+    }
+
+    @Test
+    public void toFileString_normalInput_success() {
+        LocalDateTime dateTime = LocalDateTime.parse("2021-08-24T18:00");
+        assertEquals("D, ,read book,2021-08-24T18:00",
+                new Deadline ("read book", dateTime).toFileString());
+    }
 }
--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -1,2 +1,18 @@
-package duke.task;public class TodoTest {
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+
+    @Test
+    public void toString_normalInput_success() {
+        assertEquals("[T][ ] read book", new Todo("read book").toString());
+    }
+
+    @Test
+    public void toFileString_normalInput_success() {
+        assertEquals("T, ,read book", new Todo ("read book").toFileString());
+    }
 }
