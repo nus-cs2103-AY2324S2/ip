d887e493c16e62878e3fcf687d71c5595e9f36a5
julia-cp <>
2024-01-30 17:58:08 +0800

    echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Blob.\nWhat can I do for you?\n");
-        System.out.println("\nBye. Hope to see you again soon!\n");
+        String message = sc.nextLine();
+
+        while (!message.equalsIgnoreCase("bye")) {
+            System.out.println(message + "\n");
+            message = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
