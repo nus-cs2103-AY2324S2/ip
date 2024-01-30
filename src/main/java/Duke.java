2b96e96186c8b151ae4d2c1d4c3b39ac1635376d
Cedricaca <>
2024-01-30 15:16:11 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
+import java.io.*;
 public class Duke {
     public static class Task {
         String action;
@@ -16,9 +17,9 @@ public class Duke {
         @Override
         public String toString() {
             if (isDone) {
-                return " [X] " + action;
+                return "[X]" + action;
             } else {
-                return " [ ] " + action;
+                return "[ ]" + action;
             }
         }
         public void mark() {
@@ -26,6 +27,11 @@ public class Duke {
         }
         public void unmark() {
             isDone = false;
+        }
+
+        public String
+        export() {
+            return toString();
         }
     }
     public static class Event extends Task {
@@ -39,7 +45,12 @@ public class Duke {
         @Override
         public String toString() {
             String s = super.toString();
-            return "[E]" + s + "(from " + from + " to " + to + " ).";
+            return "[E]" + s + "(from" + from + "to" + to + ")";
+        }
+        @Override
+        public String export() {
+            String s = super.toString();
+            return "[E]" + s + "/from" + from + "/to" + to ;
         }
     }
     public static class Todo extends Task {
@@ -51,6 +62,10 @@ public class Duke {
             String s = super.toString();
             return "[T]" + s;
         }
+        @Override
+        public String export() {
+            return toString();
+        }
     }
     public static class Deadline extends Task {
         String by;
@@ -61,7 +76,13 @@ public class Duke {
         @Override
         public String toString() {
             String s = super.toString();
-            return "[D]" + s + " (by: " + by + ")";
+            return "[D]" + s + "(by:" + by +")";
+        }
+
+        @Override
+        public String export() {
+            String s = super.toString();
+            return "[D]" + s + "/by" + by ;
         }
 
     }
@@ -69,7 +90,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<Task> List = new ArrayList<>();
-        int length = 0;
+        int length;
         System.out.println("Hello! I'm Dukey.");
 	    System.out.println("What can I do for you?");
 
@@ -83,6 +104,11 @@ public class Duke {
         String by = "/by";
         String from = "/from";
         String to = "/to";
+        String todo2 = "[T]";
+        String deadline2 = "[D]";
+        String event2 = "[E]";
+        String unmarked = "[ ]";
+        String marked = "[X]";
         Pattern pMark = Pattern.compile(mark);
         Pattern pUnmark = Pattern.compile(unmark);
         Pattern pTodo = Pattern.compile(todo);
@@ -92,6 +118,86 @@ public class Duke {
         Pattern pFrom = Pattern.compile(from);
         Pattern pTo = Pattern.compile(to);
         Pattern pDelete = Pattern.compile(delete);
+        Pattern pTodo2 = Pattern.compile(todo2);
+        Pattern pDeadline2 = Pattern.compile(deadline2);
+        Pattern pEvent2 = Pattern.compile(event2);
+        Pattern pUnmarked = Pattern.compile(unmarked);
+        Pattern pMarked = Pattern.compile(marked);
+
+        String folderName = "data";
+        String fileName = "data.txt";
+        File folder = new File(folderName);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        File file = new File(folder, fileName);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String currentLine;
+                int currLine = 0;
+                while ((currentLine = reader.readLine()) != null) {
+                    Matcher mTodo2 = pTodo2.matcher(currentLine);
+                    Matcher mEvent2 = pEvent2.matcher(currentLine);
+                    Matcher mDeadline2 = pDeadline2.matcher(currentLine);
+                    Matcher mUnmarked = pUnmarked.matcher(currentLine);
+                    Matcher mMarked = pMarked.matcher(currentLine);
+                    if (mTodo2.find()) {
+                        if (mMarked.find()) {
+                            Todo n = new Todo(currentLine.substring(7), true );
+                            n.mark();
+                            List.add(n);
+                        } else if (mUnmarked.find()){
+                            Todo n = new Todo(currentLine.substring(7), false);
+                            n.unmark();
+                            List.add(n);
+                        }
+                    } else if (mDeadline2.find()) {
+                        int finalIndex = currentLine.indexOf(by) + by.length();
+                        String dL = currentLine.substring(finalIndex);
+
+                        String newInput = currentLine.substring(currentLine.indexOf(deadline) + deadline.length(), currentLine.indexOf(by));
+
+                        if (mMarked.find()) {
+                            Deadline n = new Deadline(newInput, true, dL);
+                            n.mark();
+                            List.add(n);
+                        } else if (mUnmarked.find()){
+                            Deadline n = new Deadline(newInput, false, dL);
+                            n.unmark();
+                            List.add(n);
+                        }
+
+                    } else if (mEvent2.find()) {
+                        int startIndex = currentLine.indexOf(from);
+                        int startIndexTo = currentLine.indexOf(to);
+                        String subFrom = currentLine.substring(startIndex + from.length(), startIndexTo);
+
+                        String subTo = currentLine.substring(startIndexTo + to.length());
+
+                        String newInput = currentLine.substring(currentLine.indexOf(event) + event.length() + 3, startIndex);
+
+                        if (mMarked.find()) {
+                            Event n = new Event(newInput, true, subFrom, subTo);
+                            n.mark();
+                            List.add(n);
+                        } else if (mUnmarked.find()){
+                            Event n = new Event(newInput, false, subFrom, subTo);
+                            n.unmark();
+                            List.add(n);
+                        }
+
+                    }
+                    currLine = currLine + 1;
+                }
+                length = currLine;
+            } catch (IOException e) {
+                System.err.println("Error reading file: " + e.getMessage());
+                return;
+            }
+        } else {
+            length = 0;
+        }
+
 
         while (true) {
             input = scanner.nextLine();
@@ -104,7 +210,17 @@ public class Duke {
             Matcher mFrom = pFrom.matcher(input);
             Matcher mTo = pTo.matcher(input);
             Matcher mDelete = pDelete.matcher(input);
-            if (input.equals("bye")) {
+            if (input.equals("reset")) {
+                List.clear();
+                length = 0;
+                try (PrintWriter writer = new PrintWriter(file)) {
+                    writer.print(""); // This clears all lines in the file
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("List cleared!");
+
+            } else if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
@@ -127,6 +243,37 @@ public class Duke {
                     length = length - 1;
                     System.out.println("OK! I have deleted this task:");
                     System.out.println(t);
+
+                    int lineToDelete = number - 1;
+
+                    // Read the content of the file
+                    ArrayList<String> lines = new ArrayList<>();
+
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                        String currentLine;
+                        int currLine = 0;
+                        while ((currentLine = reader.readLine()) != null) {
+                            // Check if the line should be deleted
+                            if (currLine != lineToDelete) {
+                                lines.add(currentLine);
+
+                            }
+                            currLine = currLine + 1;
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error reading file: " + e.getMessage());
+                        return;
+                    }
+
+                    // Write the modified content back to the file
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                        for (String modifiedLine : lines) {
+                            writer.write(modifiedLine);
+                            writer.newLine(); // Add a newline character after each line
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error writing to file: " + e.getMessage());
+                    }
                 } else {
                     System.out.println("Please input a valid number.");
                 }
@@ -140,6 +287,37 @@ public class Duke {
                     t.unmark();
                     System.out.println("Oh no! I have marked this as not done:");
                     System.out.println(t);
+                    int lineToUnmark = number - 1;
+
+                    // Read the content of the file
+                    ArrayList<String> lines = new ArrayList<>();
+
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                        String currentLine;
+                        int currLine = 0;
+                        while ((currentLine = reader.readLine()) != null) {
+                            // Check if the line should be deleted
+                            if (currLine == lineToUnmark) {
+                               currentLine=t.export();
+
+                            }
+                            lines.add(currentLine);
+                            currLine = currLine + 1;
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error reading file: " + e.getMessage());
+                        return;
+                    }
+
+                    // Write the modified content back to the file
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                        for (String modifiedLine : lines) {
+                            writer.write(modifiedLine);
+                            writer.newLine(); // Add a newline character after each line
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error writing to file: " + e.getMessage());
+                    }
                 } else {
                     System.out.println("Please input a valid number.");
                 }
@@ -153,6 +331,37 @@ public class Duke {
                     t.mark();
                     System.out.println("Nice! I have marked this as done:");
                     System.out.println(t);
+                    int lineToMark = number - 1;
+
+                    // Read the content of the file
+                    ArrayList<String> lines = new ArrayList<>();
+
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                        String currentLine;
+                        int currLine = 0;
+                        while ((currentLine = reader.readLine()) != null) {
+                            // Check if the line should be deleted
+                            if (currLine == lineToMark) {
+                                currentLine = t.export();
+                            }
+                            lines.add(currentLine);
+                            currLine = currLine + 1;
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error reading file: " + e.getMessage());
+                        return;
+                    }
+
+                    // Write the modified content back to the file
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                        for (String modifiedLine : lines) {
+                            writer.write(modifiedLine);
+                            writer.newLine(); // Add a newline character after each line
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error writing to file: " + e.getMessage());
+                    }
+
                 } else {
                     System.out.println("Please input a valid number.");
                 }
@@ -168,6 +377,12 @@ public class Duke {
                     System.out.println("OK, I have added this task :");
                     System.out.println(n);
                     System.out.println("You now have " + length + " items in the list.");
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                        writer.write(n.export());
+                        writer.newLine();
+                    } catch (IOException e) {
+                        System.err.println("Error writing to file: " + e.getMessage());
+                    }
                 }
             } else if (mEvent.find()) {
                 if (mFrom.find() && mTo.find()) {
@@ -185,6 +400,12 @@ public class Duke {
                         System.out.println("OK, I have added this task :");
                         System.out.println(n);
                         System.out.println("You now have " + length + " items in the list.");
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                            writer.write(n.export());
+                            writer.newLine();
+                        } catch (IOException e) {
+                            System.err.println("Error writing to file: " + e.getMessage());
+                        }
                     }
                 } else {
                     System.out.println("pls input your start and end of the event.");
@@ -204,6 +425,12 @@ public class Duke {
                         System.out.println("OK, I have added this task :");
                         System.out.println(n);
                         System.out.println("You now have " + length + " items in the list.");
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                            writer.write(n.export());
+                            writer.newLine();
+                        } catch (IOException e) {
+                            System.err.println("Error writing to file: " + e.getMessage());
+                        }
                     }
                 } else {
                     System.out.println("please include a deadline");
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "/Users/cedrictan/git/ip/bin" ]
+if [ ! -d "../bin" ]
 then
-    mkdir /Users/cedrictan/git/ip/bin
+    mkdir ../bin
 fi
 
 # delete output from previous run
-if [ -e "/Users/cedrictan/git/ip/text-ui-test/ACTUAL.TXT" ]
+if [ -e "./ACTUAL.TXT" ]
 then
-    rm /Users/cedrictan/git/ip/text-ui-test/ACTUAL.TXT
+    rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp /Users/cedrictan/git/ip/src/main/java -Xlint:none -d /Users/cedrictan/git/ip/bin /Users/cedrictan/git/ip/src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+
+# compare the output to the expected output
+diff ACTUAL.TXT EXPECTED.TXT
+if [ $? -eq 0 ]
+then
+    echo "Test result: PASSED"
+    exit 0
+else
+    echo "Test result: FAILED"
     exit 1
 fi
 
