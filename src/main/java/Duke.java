29f0fb423d410bc4075f1fc90dc557f67b5d61e8
julia-cp <>
2024-01-30 18:57:33 +0800

    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
         System.out.println("Hello! I'm Blob.\nWhat can I do for you?\n");
         String message = sc.nextLine();
 
         while (!message.equalsIgnoreCase("bye")) {
-            System.out.println(message + "\n");
+            if (message.equalsIgnoreCase("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(list[i] + "\n");
+                }
+            } else {
+                list[count] = (count + 1) + ". " + message;
+                count++;
+                System.out.println("added: " + message + "\n");
+            }
             message = sc.nextLine();
         }
 
