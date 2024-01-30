1950516f684e03cf08c12fc75d8e502a6a97f791
Jolonauh <jeremiahau016@gmail.com>
2024-01-31 01:08:06 +0800

    remove total field and related methods in Task class

--- src/main/java/ConsoleUserInterface.java
+++ src/main/java/ConsoleUserInterface.java
@@ -40,132 +40,131 @@ public class ConsoleUserInterface {
         String args = input.substring(input.indexOf(' ') + 1);
         boolean isSingleCommand = input.indexOf(' ') == -1;
         switch (command) {
-            case "list":
-                if (!isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! Too many arguments provided.");
-                }
-                printSeparator();
-                if (this.tasks.isEmpty()) {
-                    System.out.println("Yay! You have no tasks!");
-                } else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < this.tasks.size(); i++) {
-                        System.out.println(i + 1 + "." + tasks.get(i));
-                    }
-                }
-                printSeparator();
-                break;
-            case "bye":
-                if (!isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! Too many arguments provided.");
-                }
-                this.isPolling = false;
-                break;
-            case "mark": {
-                if (this.tasks.isEmpty()) {
-                    throw new NoSuchElementException("OOPS!!! There are no tasks to mark.");
-                }
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
-                }
-                try {
-                    int taskIdx = Integer.parseInt(args);
-                    if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
-                        throw new IndexOutOfBoundsException();
-                    }
-                } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
-                }
-                Task target = this.tasks.get(Integer.parseInt(args) - 1);
-                target.markDone();
-                printSeparator();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + target);
-                printSeparator();
-                break;
+        case "list":
+            if (!isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! Too many arguments provided.");
             }
-            case "unmark": {
-                if (this.tasks.isEmpty()) {
-                    throw new NoSuchElementException("OOPS!!! There are no tasks to unmark.");
+            printSeparator();
+            if (this.tasks.isEmpty()) {
+                System.out.println("Yay! You have no tasks!");
+            } else {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < this.tasks.size(); i++) {
+                    System.out.println(i + 1 + "." + tasks.get(i));
                 }
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
-                }
-                try {
-                    int taskIdx = Integer.parseInt(args);
-                    if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
-                        throw new IndexOutOfBoundsException();
-                    }
-                } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
-                }
-                Task target = this.tasks.get(Integer.parseInt(args) - 1);
-                target.unmarkDone();
-                printSeparator();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + target);
-                printSeparator();
-                break;
             }
-            case "todo":
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! The description of a todo cannot be empty.");
+            printSeparator();
+            break;
+        case "bye":
+            if (!isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! Too many arguments provided.");
+            }
+            this.isPolling = false;
+            break;
+        case "mark": {
+            if (this.tasks.isEmpty()) {
+                throw new NoSuchElementException("OOPS!!! There are no tasks to mark.");
+            }
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
+            }
+            try {
+                int taskIdx = Integer.parseInt(args);
+                if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
+                    throw new IndexOutOfBoundsException();
                 }
-                Task todo = new Todo(args);
-                addTask(todo);
-                break;
-            case "deadline":
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! The description of a deadline cannot be empty.");
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
+            }
+            Task target = this.tasks.get(Integer.parseInt(args) - 1);
+            target.markDone();
+            printSeparator();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + target);
+            printSeparator();
+            break;
+        }
+        case "unmark": {
+            if (this.tasks.isEmpty()) {
+                throw new NoSuchElementException("OOPS!!! There are no tasks to unmark.");
+            }
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
+            }
+            try {
+                int taskIdx = Integer.parseInt(args);
+                if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
+                    throw new IndexOutOfBoundsException();
                 }
-                String[] deadlineArgs = args.split(" /by ");
-                if (deadlineArgs.length != 2) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
+            }
+            Task target = this.tasks.get(Integer.parseInt(args) - 1);
+            target.unmarkDone();
+            printSeparator();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + target);
+            printSeparator();
+            break;
+        }
+        case "todo":
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! The description of a todo cannot be empty.");
+            }
+            Task todo = new Todo(args);
+            addTask(todo);
+            break;
+        case "deadline":
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! The description of a deadline cannot be empty.");
+            }
+            String[] deadlineArgs = args.split(" /by ");
+            if (deadlineArgs.length != 2) {
+                throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
+            }
+            Task deadline = new Deadline(deadlineArgs[0], deadlineArgs[1]);
+            addTask(deadline);
+            break;
+        case "event":
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! The description of an event cannot be empty.");
+            }
+            String[] eventArgs = args.split(" /from ");
+            if (eventArgs.length != 2) {
+                throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
+            }
+            String[] startEnd = eventArgs[1].split(" /to ");
+            if (startEnd.length != 2) {
+                throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
+            }
+            Task event = new Event(eventArgs[0], startEnd[0], startEnd[1]);
+            addTask(event);
+            break;
+        case "delete":
+            if (this.tasks.isEmpty()) {
+                throw new NoSuchElementException("OOPS!!! There are no tasks to delete.");
+            }
+            if (isSingleCommand) {
+                throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
+            }
+            try {
+                int taskIdx = Integer.parseInt(args);
+                if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
+                    throw new IndexOutOfBoundsException();
                 }
-                Task deadline = new Deadline(deadlineArgs[0], deadlineArgs[1]);
-                addTask(deadline);
-                break;
-            case "event":
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! The description of an event cannot be empty.");
-                }
-                String[] eventArgs = args.split(" /from ");
-                if (eventArgs.length != 2) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
-                }
-                String[] startEnd = eventArgs[1].split(" /to ");
-                if (startEnd.length != 2) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid arguments provided.");
-                }
-                Task event = new Event(eventArgs[0], startEnd[0], startEnd[1]);
-                addTask(event);
-                break;
-            case "delete":
-                if (this.tasks.isEmpty()) {
-                    throw new NoSuchElementException("OOPS!!! There are no tasks to delete.");
-                }
-                if (isSingleCommand) {
-                    throw new IllegalArgumentException("OOPS!!! Some arguments are missing.");
-                }
-                try {
-                    int taskIdx = Integer.parseInt(args);
-                    if (taskIdx <= 0 || taskIdx > this.tasks.size()) {
-                        throw new IndexOutOfBoundsException();
-                    }
-                } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
-                }
-                int targetIdx = Integer.parseInt(args) - 1;
-                printSeparator();
-                System.out.println("Noted. I've removed this task");
-                System.out.println("  " + this.tasks.get(targetIdx));
-                this.tasks.remove(targetIdx);
-                Task.setTotal(Task.getTotal() - 1);
-                System.out.printf("Now you have %d tasks in the list.%n", Task.getTotal());
-                printSeparator();
-                break;
-            default:
-                throw new IllegalArgumentException("OOPS!!! Command not found.");
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("OOPS!!! Invalid task index provided.");
+            }
+            int targetIdx = Integer.parseInt(args) - 1;
+            printSeparator();
+            System.out.println("Noted. I've removed this task");
+            System.out.println("  " + this.tasks.get(targetIdx));
+            this.tasks.remove(targetIdx);
+            System.out.printf("Now you have %d tasks in the list.%n", tasks.size());
+            printSeparator();
+            break;
+        default:
+            throw new IllegalArgumentException("OOPS!!! Command not found.");
         }
     }
 
@@ -174,7 +173,7 @@ public class ConsoleUserInterface {
         printSeparator();
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + task);
-        System.out.printf("Now you have %d tasks in the list.%n", Task.getTotal());
+        System.out.printf("Now you have %d tasks in the list.%n", tasks.size());
         printSeparator();
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,8 @@
 public abstract class Task {
     protected boolean isDone;
     protected String taskDescription;
-    private static int total = 0;
 
     public Task(String taskDescription) {
-        total++;
         this.isDone = false;
         this.taskDescription = taskDescription;
     }
@@ -15,13 +13,5 @@ public abstract class Task {
 
     public void unmarkDone() {
         this.isDone = false;
-    }
-
-    public static int getTotal() {
-        return total;
-    }
-
-    public static void setTotal(int total) {
-        Task.total = total;
     }
 }
