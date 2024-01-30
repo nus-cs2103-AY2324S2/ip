360964a49aece33f2cc187fcaedd32e81662aa1a
iamtr <jettzeong@gmail.com>
2024-01-30 21:45:50 +0800

    Change toString() implementation for Todo.java

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -3,9 +3,9 @@ public class Todo extends Task {
     public Todo(String description) {
         super(description);
     }
-    
+
     @Override
     public String toString() {
-        return "[T] " + "[" + getStatusIcon() + "] " + description;
+        return "[T] " + super.toString();
     }
 }
