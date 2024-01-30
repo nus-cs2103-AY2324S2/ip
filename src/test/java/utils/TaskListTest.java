357e3054fcc8f4c4b7beddf5667f8d0604a1ba5c
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-31 04:56:40 +0800

    Remove unnecessary import

--- src/test/java/utils/TaskListTest.java
+++ src/test/java/utils/TaskListTest.java
@@ -2,7 +2,6 @@ package utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
