4c221ca9a8c0eb81758587477ece7c0250a35bfd
bachletuankhai <bachletuankhai@gmail.com>
2024-01-30 20:49:50 +0800

    Add exception class for handling wrong date format

--- src/main/java/Task/TaskDateTime.java
+++ src/main/java/Task/TaskDateTime.java
@@ -2,15 +2,20 @@ package task;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class TaskDateTime {
     private LocalDateTime dateTime;
+    private static final String INPUT_FORMAT = "yyyy-MM-dd HH:mm";
     private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("EEE, MMM d yyyy HH:mm");
 
-    public TaskDateTime(String dateTime) {
-        this.dateTime = LocalDateTime.parse(dateTime, INPUT_FORMATTER);
+    public TaskDateTime(String dateTime) throws DukeDateTimeParseException {
+        try {
+            this.dateTime = LocalDateTime.parse(dateTime, INPUT_FORMATTER);
+        } catch (Exception e) {
+            throw new DukeDateTimeParseException("Wrong date and time format: " + dateTime,
+                    "Please enter the date and time in the format: " + INPUT_FORMAT);
+        }
     }
 
     @Override
