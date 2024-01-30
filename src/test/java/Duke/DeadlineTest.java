0a7a4c91a5cf137b6a8b013000face2aaeed4791
nichee <E0957568@u.nus.edu>
2024-01-30 21:54:24 +0800

    Add test for string representation to EventTest, fix test naming convention in DeadlineTest

--- src/test/java/Duke/DeadlineTest.java
+++ src/test/java/Duke/DeadlineTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeadlineTest {
     @Test
-    public void validDateTest(){
+    public void stringRepresentationTest(){
         Deadline testDeadline = new Deadline("test", "2019-10-15 2300" );
         assertEquals("[D][ ] test (by: Oct 15 2019 23:00)", testDeadline.toString());
     }
--- src/test/java/Duke/EventTest.java
+++ src/test/java/Duke/EventTest.java
@@ -11,5 +11,9 @@ public class EventTest {
         Event testEvent = new Event("test", "2019-10-15 1800","2019-10-15 2300" );
         assertEquals(true, testEvent.hasValidDate());
     }
-
+    @Test
+    public void stringRepresentationTest(){
+        Event testDeadline = new Event("test", "2019-10-15 1800", "2019-10-15 2300" );
+        assertEquals("[E][ ] test (from: Oct 15 2019 18:00 to: Oct 15 2019 23:00)", testDeadline.toString());
+    }
 }
