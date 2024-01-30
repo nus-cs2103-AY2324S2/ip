ce4e9f8b7f8b61621545853d507d00d03cbcbe67
ziyi22 <ziyi160073@gmail.com>
2024-01-30 10:14:21 +0800

    Level-3: Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private final static String[] tasks =  new String[100];
+    private final static Task[] tasks =  new Task[100];
     private static int count = 0;
     public static void main(String[] args) {
         greetUser();
@@ -17,46 +17,64 @@ public class Duke {
 
     private static void echo() {
         Scanner scanner = new Scanner(System.in);
-        String  command;
+        String command;
 
         do {
-            System.out.print("\n");
             command = scanner.nextLine();
+            System.out.println("____________________________________");
             if(command.equalsIgnoreCase("bye")) {
                 break;
             } else if (command.equalsIgnoreCase("list")) {
                 listOutTasks();
+            } else if (command.startsWith("mark")){
+                markTask(command);
+            } else if (command.startsWith("unmark")) {
+                unmarkTask(command);
             } else {
                 addTask(command);
             }
+            System.out.println("____________________________________");
         } while (true);
 
         scanner.close();
     }
 
+    private static void markTask(String command) {
+        String[] words = command.split(" ");
+        int taskId = Integer.parseInt(words[1]) -1;
+        Task t = tasks[taskId];
+        tasks[taskId].markAsDone();
+        System.out.println("Nice ! I've marked this task as done: \n" + t.toString());
+    }
+
+    private static void unmarkTask(String command) {
+        String[] words = command.split(" ");
+        int taskId = Integer.parseInt(words[1]) - 1;
+        Task t = tasks[taskId];
+        t.markAsUndone();
+        System.out.println("Ok, I've marked this task as not done yet: \n" + t.toString());
+    }
+
     private static void addTask(String task) {
-        tasks[count++] = task;
-        System.out.println("_________________________________________");
+        Task t = new Task(task);
+        tasks[count++] = t;
         System.out.println("added: " + task);
-        System.out.println("_________________________________________");
     }
 
     private static void listOutTasks() {
-        System.out.println("_________________________________________");
         if (count == 0) {
             System.out.println("No task added");
         } else {
             for(int i = 0; i < count; i++) {
                 int id = i+1;
-                System.out.print(" " + id + ". " + tasks[i] + "\n");
+                Task t = tasks[i];
+                System.out.println(id + ". " + t.toString());
             }
         }
-        System.out.println("_________________________________________");
     }
 
     private static void exit() {
-        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________");
+        System.out.println("____________________________________");
     }
 }
