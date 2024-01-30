2648a1d20a0ddcab6ec415f637880e8eb5aa04e7
Anant Shanker <shankeranant@gmail.com>
2024-01-30 19:26:25 +0800

    Add javadocs

--- src/main/java/drake/Storage.java
+++ src/main/java/drake/Storage.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 
 import drake.task.Task;
 
+/**
+ * Handles storage operations for tasks, such as saving to and loading from a file.
+ */
 public class Storage {
     private String filePath;
 
+    /**
+     * Constructs a new Storage object.
+     *
+     * @param filePath The file path where tasks will be saved and loaded from.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Saves the list of tasks to the file.
+     * 
+     * @param tasks The ArrayList of Task objects to be saved.
+     */
     public void saveTasks(ArrayList<Task> tasks) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
             oos.writeObject(tasks);
@@ -19,6 +32,13 @@ public class Storage {
         }
     }
 
+    /**
+     * Loads and returns the list of tasks from the file.
+     * If the file does not exist, returns an empty list.
+     * If the file exists but an error occurs during reading, also returns an empty list.
+     * 
+     * @return The ArrayList of Task objects loaded from the file.
+     */
     @SuppressWarnings("unchecked")
     public ArrayList<Task> loadTasks() {
         File file = new File(filePath);
--- src/main/java/drake/Ui.java
+++ src/main/java/drake/Ui.java
@@ -1,10 +1,16 @@
 package drake;
 import java.util.ArrayList;
-
 import drake.task.Task;
 import drake.task.TaskList;
 
+/**
+ * Handles UI operations for the chatbot.
+ */
 public class Ui {
+
+    /**
+     * Displays a welcome message to the user.
+     */
     public void showWelcome() {
         System.out.println("____________________________________________________________");
         System.out.println(" What's up everyone. I'm Drake.");
@@ -12,18 +18,33 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a goodbye message to the user.
+     */
     public void showGoodbye() {
         System.out.println("____________________________________________________________");
         System.out.println(" See you later, alligator! ");
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays an error message.
+     *
+     * @param message The error message to be displayed.
+     */
     public void showError(String message) {
         System.out.println("____________________________________________________________");
         System.out.println(message);
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a task along with a custom message and the total number of tasks.
+     *
+     * @param message The custom message to display before the task.
+     * @param task    The task to be displayed.
+     * @param size    The total number of tasks in the list.
+     */
     public void showTask(String message, Task task, int size) {
         System.out.println("____________________________________________________________");
         System.out.println(message);
@@ -32,6 +53,11 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays the list of tasks.
+     *
+     * @param tasks The TaskList containing the tasks to display.
+     */
     public void showTaskList(TaskList tasks) {
         System.out.println("____________________________________________________________");
         System.out.println("You asked for the tasks in your list? Here:");
@@ -42,6 +68,12 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a message confirming the addition of a task, the task itself, and the new total number of tasks.
+     *
+     * @param task The task that has been added.
+     * @param size The new total number of tasks in the list.
+     */
     public void showAddTask(Task task, int size) {
         System.out.println("____________________________________________________________");
         System.out.println("Got it. I've added this task:");
@@ -50,6 +82,11 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a message confirming that a task has been marked as done.
+     *
+     * @param task The task that has been marked.
+     */
     public void showMarkTask(Task task) {
         System.out.println("____________________________________________________________");
         System.out.println("Cool. I now declare this task marked as, done:");
@@ -57,6 +94,11 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a message confirming that a task has been marked as not done.
+     *
+     * @param task The task that has been unmarked.
+     */
     public void showUnmarkTask(Task task) {
         System.out.println("____________________________________________________________");
         System.out.println("OK, I've marked this task as not done yet:");
@@ -64,6 +106,12 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a message confirming the deletion of a task, the task itself, and the new total number of tasks.
+     *
+     * @param task The task that has been deleted.
+     * @param size The new total number of tasks in the list.
+     */
     public void showDeleteTask(Task task, int size) {
         System.out.println("____________________________________________________________");
         System.out.println("Noted. I've removed this task:");
@@ -72,6 +120,4 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
-
-    // ... Other UI methods ...
 }
