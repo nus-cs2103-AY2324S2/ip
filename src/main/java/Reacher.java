44f42481030cc03ffdd3ff9ed30c3af9b0648a4b
zhenghuil <e0959119@u.nus.edu>
2024-01-30 21:14:45 +0800

    added function to change task done status and task class

--- src/main/java/Reacher.java
+++ src/main/java/Reacher.java
@@ -6,23 +6,44 @@ import java.util.Scanner;
 public class Reacher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> memory = new ArrayList<>();
+        List<Task> memory = new ArrayList<>();
         System.out.println("Hello!\n" +
                 "I'm Reacher.\n" +
                 "What do u want?\n" +
                 "Pls say bye");
         while (true) {
             String input = scanner.nextLine();
-            if (Objects.equals(input,"bye")) {
+            if (Objects.equals(input,"bye")) { //check for end request
                 System.out.println("bye");
                 break;
             }
-            if (Objects.equals(input, "list")) {
-                for(String task:memory) {
-                    System.out.println(task);
+            else if (Objects.equals(input, "list")) {// check for list request
+                System.out.println("Tasks:");
+                int c = 1;
+                for(Task task:memory) {
+                    System.out.println(c + task.toString());
+                    c++;
                 }
-            } else {
-                memory.add(input);
+            }
+            else if (Objects.equals(input, "edit")) {
+                System.out.println("Which task number would u like to edit?");
+                int num = scanner.nextInt();
+                scanner.nextLine();
+                Task task = memory.get(num - 1);
+                System.out.println("Mark Done or Undone?");
+                String change = scanner.nextLine();
+                if (Objects.equals(change, "done")) {
+                    task.markDone();
+                    System.out.println("Task " + num + " marked done");
+                }
+                else  {
+                    task.markNotDone();
+                    System.out.println("Task " + num + " marked Undone");
+                }
+            }
+            else {
+                Task t = new Task(input);
+                memory.add(t);
                 System.out.println("I've added " + input);
             }
         }
