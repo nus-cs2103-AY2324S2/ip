131cae5f78fcd94b67f987146b49758aa74ca504
leepoeaik <poeaik.lee@gmail.com>
2024-01-31 00:12:51 +0800

    Level-3

--- src/main/java/Poe.java
+++ src/main/java/Poe.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Poe {
     String line = "=================================================";
-    ArrayList<String> list = new ArrayList<String>(100);
+    ArrayList<Task> list = new ArrayList<Task>(100);
     public void greetings(){
         String hello =  line + "\nYo! I'm Poe\nWhat can I do for you bro\n" + line;
         System.out.println(hello);
@@ -15,17 +15,32 @@ public class Poe {
     }
 
     public void addList(String str){
-        list.add(str);
+        Task task = new Task(str,false);
+        list.add(task);
+        System.out.println(line+"\nadded : "+str+"\n"+line);
     }
     public void printList(){
         System.out.println(line);
         for (int i = 0; i < list.size(); i++){
-            System.out.printf("%d. %s\n",i+1,list.get(i));
+            System.out.printf("%d. %s\n",i+1,list.get(i).toString());
         }
         System.out.println(line);
 
     }
 
+    public void update(String str){
+        String[] splitStr =str.split("\\s+");
+        int index = Integer.parseInt(splitStr[1])-1;
+        boolean bool = !splitStr[0].equals("unmark");
+        Task curr = list.get(index);
+        if(bool){
+            curr.markMark();
+            System.out.println(line+"\nCongrats on completing the task!\n" + curr.toString()+"\n"+line);
+        }else{
+            curr.unmarkMark();
+            System.out.println(line + "\nUnmarked the task, :(\n" + curr.toString()+"\n"+line);
+        }
+    }
     public void input(){
         Scanner sc = new Scanner(System.in);
         while(true) {
@@ -35,9 +50,10 @@ public class Poe {
                 break;
             }else if(str.toLowerCase().equals("list")){
                 printList();
+            }else if(str.toLowerCase().contains("mark")){
+                update(str);
             }else{
                 addList(str);
-                System.out.println(line+"\nadded : "+str+"\n"+line);
             }
         }
     }
@@ -45,7 +61,6 @@ public class Poe {
 
     public static void main(String[] args) {
         Poe poe1 = new Poe();
-        Scanner sc = new Scanner(System.in);
         poe1.greetings();
         poe1.input();
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    private String name;
+    private boolean mark;
+    public Task(String name, boolean mark){
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean getMark(){
+        return mark;
+    }
+    public void toggleMark(){
+        this.mark = !this.mark;
+    }
+
+    public void markMark(){
+        this.mark = true;
+    }
+
+    public void unmarkMark(){
+        this.mark = false;
+    }
+
+
+    @Override
+    public String toString(){
+        if  (getMark()){
+            return "[X] " + getName();
+        }else{
+            return "[ ] " + getName();
+        }
+    }
+}
