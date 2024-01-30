70828148b73f403292b97fe673d6ea270aab6ff2
Jerome-j <jerome@outlook.sg>
2024-01-30 13:37:18 +0800

    Create a command class to be the template of each individual command

---
+++ src/main/java/commands/Command.java
@@ -0,0 +1,20 @@
+package commands;
+
+import common.DataStorage;
+
+/**
+ * Represents an executable command.
+ */
+public class Command {
+
+    protected DataStorage dataStorage;
+
+    /**
+     * Executes the command and returns the result.
+     * TODO: Code referenced from: https://github.com/se-edu/addressbook-level2
+     */
+    public CommandResult execute() {
+        throw new UnsupportedOperationException("This method is to be implemented by child classes");
+    };
+
+}
