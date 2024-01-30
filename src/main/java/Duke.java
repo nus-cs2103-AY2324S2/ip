f8d119250456b5b5ff0e7256e1feb876486fe622
HusseinSafwan02 <safwanhussein.ri@gmail.com>
2024-01-30 19:02:49 +0800

    Added ability to store tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static String addComment (String task) {
+        return "____________________________________________________________\r\n" +
+                " Added: " + task + "\n" +
+                "____________________________________________________________\r\n";
+    }
+
+    public static void listTasks(String[] taskList, int count) {
+        for (int i = 0; i < count; i++ ) {
+            int number = i + 1;
+            String result = number + ". " + taskList[i];
+            System.out.println(result);
+        }
+    }
+
     public static void main(String[] args) {
-        String cat = "(\\_/)\n" +
+        String mickey = "(\\_/)\n" +
                 "( •,•)\n" +
                 "(\")_(\")";
-        System.out.println("RAWR!\n" + cat);
+        System.out.println("RAWR!\n" + mickey);
         String intro = "____________________________________________________________\r\n" +
                 " Hello! I'm Mickey\n" +
                 " What can I do for you?\n" +
@@ -15,19 +30,28 @@ public class Duke {
                 " Bye. See you soon!\n" +
                 "____________________________________________________________\r\n";
 
+
         System.out.println(intro);
 
         Scanner sc = new Scanner(System.in);
         String userInput;
+        String[] tasks = new String[100];
+        int count = 0;
 
         while(true) {
             userInput = sc.nextLine();
              if (userInput.equals("bye")) {
                  System.out.println(outro);
                  break;
+             } else if (userInput.equals("list")) {
+                 System.out.print("____________________________________________________________\r\n");
+                 listTasks(tasks, count);
+                 System.out.println("____________________________________________________________\r\n");
              } else {
-                 System.out.println(userInput);
-             }
+                 tasks[count] = userInput;
+                 count++;
+                 System.out.println(addComment(userInput));
+            }
         }
 
     }
