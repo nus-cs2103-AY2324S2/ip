bebf24a9b7629aab21b4adf699ac2fb2e5a031cc
Yadunand Prem <yadunand@yadunut.com>
2024-01-31 17:06:01 +0800

    Add Abstraction over Tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getTask(int id) {
+        return this.tasks.get(id);
+    }
+
+    public void markAsDone(int id) {
+        this.tasks.get(id).todoState = TodoState.DONE;
+    }
+
+    public void unmarkAsDone(int id) {
+        this.tasks.get(id).todoState = TodoState.UNDONE;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public void remove(int id) {
+        this.tasks.remove(id);
+    }
 }
