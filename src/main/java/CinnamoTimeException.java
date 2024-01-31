34d5c6eb5d42cf187c9a666b65dd6fcd25e74c28
Ikhoon <e0908299@u.nus.edu>
2024-02-01 00:58:29 +0800

    More OOP Done

---
+++ src/main/java/CinnamoTimeException.java
@@ -0,0 +1,6 @@
+class CinnamoTimeException extends CinnamoException {
+    @Override
+    public String toString() {
+        return "Hi! Please provide time information in a correct format of \"mm-dd-yyyy hh:mm\" >.<";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Duke {
 
-    public static void main(String[] args) throws Exception {
+    private Ui ui;
+    private Storage storage;
+    private TaskList tasklist;
+
+    public Duke(String filepath) throws IOException {
+        this.ui = new Ui();
+        storage = new Storage(filepath);
+        this.tasklist = new TaskList(storage.load_data(), filepath);
+    }
+
+    public void run() throws Exception {
         Scanner sc = new Scanner(System.in);
-        CinnamoRoll cin = new CinnamoRoll();
-        cin.greet();
-        cin.load_data();
+        this.ui.greet();
         while(true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
             }
-            cin.respond(input);
+            ui.respond(this.tasklist, input);
         }
-        cin.exit();
+        ui.exit();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Duke("src/main/Cinnamo.txt").run();
     }
 }
