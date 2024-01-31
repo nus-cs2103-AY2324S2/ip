ee5e6fbd7390c3d8621e3d40d68d6b63dc06dcc2
jessicawyz <jessicawuyizheng@.com>
2024-01-31 17:23:33 +0800

    oop changes, taskmanager is the same as tasklist

---
+++ src/main/java/Parser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Parser {
+    public static void parse(TaskManager taskManager) {
+        Scanner scanner = new Scanner(System.in);
+        String userCommand;
+
+        do {
+            System.out.print("\n");
+            userCommand = scanner.nextLine();
+            taskManager.executeCommand(userCommand);
+        } while (!userCommand.equalsIgnoreCase("bye"));
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ui {
+    public static void greetUser() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Echo");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void startConversation(TaskManager taskManager) {
+        Parser.parse(taskManager);
+    }
+
+    public static void endConversation() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
