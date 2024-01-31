6a7c1663df81a5a6eea4a8ad78eeaef14bdb9cfe
jeong-jaeho <e0960435@u.nus.edu>
2024-02-01 04:10:16 +0800

    ToDos, Events, Deadlines

--- src/main/java/Linus.java
+++ src/main/java/Linus.java
@@ -19,26 +19,49 @@ public class Linus {
                 break;
             } else if (input.equals("list")) { // list tasks
                 System.out.println("Here are the tasks in your list:");
+
                 for (int i = 0; i < tasks.size(); i++) {
                     Task currTask = tasks.get(i);
-                    System.out.println((i + 1) + ". [" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+                    System.out.println((i + 1) + ". " + currTask);
                 }
             } else if (input.startsWith("mark")) {
                 Integer indexOfTask = Integer.parseInt(input.substring(5));
                 Task currTask = tasks.get(indexOfTask);
                 currTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+                System.out.println(currTask);
             } else if (input.startsWith("unmark")) {
                 Integer indexOfTask = Integer.parseInt(input.substring(7));
                 Task currTask = tasks.get(indexOfTask);
                 currTask.markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
-            } else { // add tasks
-                Task addedTask = new Task(input);
-                System.out.println("added: " + addedTask.getDescription()); // println automatically inserts new line
-                tasks.add(addedTask);
+                System.out.println(currTask);
+            } else if (input.startsWith("todo")) {
+                String description = input.substring(5);
+                Task todo = new Todo(description);
+                tasks.add(todo);
+                System.out.println("Got it. I've added this task: \n" + todo);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (input.startsWith("deadline")) {
+                String[] substrings = input.split(" /by ");
+                String description = substrings[0].substring(9);
+                String by = substrings[1];
+                Task deadline = new Deadline(description, by);
+                tasks.add(deadline);
+                System.out.println("Got it. I've added this task: \n" + deadline);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (input.startsWith("event")) {
+                String[] substrings = input.split(" /from ");
+                String description = substrings[0].substring(6);
+                String[] substrings2 = substrings[1].split(" /to ");
+                String from = substrings2[0];
+                String to = substrings2[1];
+                Task event = new Event(description, from, to);
+                tasks.add(event);
+                System.out.println("Got it. I've added this task: \n" + event);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else {
+
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,4 +23,9 @@ public class Task {
     public String getDescription() {
         return this.description;
     }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
 }
