6878805c8885709d9b9108236121007210631d51
Carl <e0959394@u.nus.edu.sg>
2024-01-31 21:06:02 +0800

    Fix bug that causes the data.txt to be not found when running runtest.bat

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -25,6 +25,8 @@ public class Storage {
         if (new File(FILE_PATH).exists()) {
             return Storage.readFromStorage();
         } else {
+            File file = new File(FILE_PATH);
+            file.getParentFile().mkdirs();
             return new ArrayList<>();
         }
     }
