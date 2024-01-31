ffa334b2d4c7b521cec5ed58ff1880fbe6cb7777
yashma-sonara <yashma.sonara04@gmail.com>
2024-01-31 18:56:00 +0800

    add javadocs for task package

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,28 +1,54 @@
 package duke.task;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
+/**
+ * Represents a Deadline task in the task list.
+ * A Deadline task is a task with a by time.
+ */
 public class Deadline extends Task{
 
     protected LocalDate by;
     protected String originalBy;
     private static final DateTimeFormatter OUTPUT_DATE_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
+    /**
+     * Constructs a Deadline task with the given by time.
+     *
+     * @param description The description of the Deadline task.
+     * @param by The by time of the Deadline task.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.originalBy = by;
         this.by = LocalDate.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
+    /**
+     * Returns a string representation of the Deadline task for saving to a file.
+     *
+     * @return A formatted string representing the Deadline task for saving to a file.
+     */
     @Override
     public String toFileString() {
         // Format: D | 0/1 | description | by date
         return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + this.originalBy;
     }
 
+    /**
+     * Returns by time of the Deadline task.
+     *
+     * @return by time of the Deadline task.
+     */
     public String getBy() {
         return  " (by: " + this.by.format(OUTPUT_DATE_FORMATTER) + ")";
     }
 
+    /**
+     * Returns a string representation of the Deadline task.
+     *
+     * @return A string representation of the Deadline task.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + this.getBy();
--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -1,17 +1,37 @@
 package duke.task;
+
+/**
+ * Represents a Todo task in the task list.
+ * A Todo task is a basic task without a specific deadline or event time.
+ */
 public class Todo extends  Task{
 
-
+    /**
+     * Constructs a Todo task with the given description.
+     *
+     * @param description The description of the Todo task.
+     */
     public Todo(String description) {
         super(description);
 
     }
+
+    /**
+     * Returns a string representation of the Todo task for saving to a file.
+     *
+     * @return A formatted string representing the Todo task for saving to a file.
+     */
     @Override
     public String toFileString() {
         // Format: T | 0/1 | description
         return "T | " + (isDone ? "1" : "0") + " | " + description;
     }
 
+    /**
+     * Returns a string representation of the Todo task.
+     *
+     * @return A string representation of the Todo task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString() ;
