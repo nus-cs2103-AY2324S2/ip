77b586944de19c4b8f54eb51ed7f0d321202e202
Archit <architg.sch@gmail.com>
2024-02-01 00:16:30 +0800

    Create ParserTest file along with testcases
    Created a new class called PaserTest that contains comprehensive testcases to test the parseInput method in the parser class.

---
+++ src/test/java/Yapchit/ParserTest.java
@@ -0,0 +1,98 @@
+package Yapchit;
+
+import Yapchit.Tasks.Deadline;
+import Yapchit.YapchitExceptions.InvalidDetailException;
+import Yapchit.YapchitExceptions.InvalidKeywordException;
+import Yapchit.YapchitExceptions.YapchitException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class ParserTest {
+
+    private Parser parser;
+
+    public ParserTest() {
+        parser = new Parser();
+    }
+
+    @Test
+    void testParseInputOperationSuccess() throws YapchitException {
+        String input = "deadline";
+        Yapchit.Operations op = Yapchit.Operations.DEADLINE;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "todo";
+        op = Yapchit.Operations.TODO;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "event";
+        op = Yapchit.Operations.EVENT;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "mark";
+        op = Yapchit.Operations.MARK;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "unmark";
+        op = Yapchit.Operations.UNMARK;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "list";
+        op = Yapchit.Operations.LIST;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "delete";
+        op = Yapchit.Operations.DELETE;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "Delete";
+        op = Yapchit.Operations.DELETE;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "toDo";
+        op = Yapchit.Operations.TODO;
+        assertEquals(op, parser.parseInputOperation(input));
+
+        input = "MARK";
+        op = Yapchit.Operations.MARK;
+        assertEquals(op, parser.parseInputOperation(input));
+    }
+
+    @Test
+    void testParseInputMissingInput() {
+        String input = "";
+        assertThrows(InvalidKeywordException.class, () -> {
+            parser.parseInputOperation(input);
+        });
+    }
+
+    @Test
+    void testParseInputWrongKeyword() {
+        String input = "blah";
+        assertThrows(InvalidKeywordException.class, () -> {
+            parser.parseInputOperation(input);
+        });
+    }
+
+    @Test
+    void testParseInputSpaceInKeyword() {
+        String input = "dele te";
+        assertThrows(InvalidKeywordException.class, () -> {
+            parser.parseInputOperation(input);
+        });
+    }
+
+    @Test
+    void testParseInputInvalidCharacterAfterKeyword() {
+        String input = "mark6";
+        assertThrows(InvalidKeywordException.class, () -> {
+            parser.parseInputOperation(input);
+        });
+    }
+
+}
+
+
