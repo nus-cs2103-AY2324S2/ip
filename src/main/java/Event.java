5bf4c1713db3261f7794abeedaf82fa8ff993b4e
iynixil <lixinyilxy02@gmail.com>
2024-01-31 22:44:25 +0800

    Add functionality to save tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,16 +2,20 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
-
     public Event(String desc, String from, String to) {
         super(desc);
         this.from = from;
         this.to = to;
-    }
+    }    
 
     @Override
     public String toString() {
         return String.format("[Event]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+    }
+
+    @Override
+    public String fileString() {
+        return String.format("EVENT | %s | FROM %s TO %s", super.fileString(), this.from, this.to);
     }
 
 }
