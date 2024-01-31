00cd97275aefe8f0e09d844b7d7b0b211714419d
peterXGD <mizishanming@gmail.com>
2024-01-31 13:52:02 +0800

    l0

---
+++ src/main/java/Venus.java
@@ -0,0 +1,16 @@
+public class Venus {
+    public static void main(String[] args) {
+        String start =
+             "____________________________________________________________\n"
+             + "Hello! I'm Venus\n"
+             + "What can I do for you?\n"
+             + "____________________________________________________________\n";
+
+        String end =
+             "Bye. Hope to see you again soon!\n"
+             +  "____________________________________________________________\n";
+
+        System.out.println(start);
+        System.out.println(end);
+    }
+}
