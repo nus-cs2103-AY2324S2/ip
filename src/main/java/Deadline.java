8dcf3a48224b552c43859b2b8a9656ebf8e64a10
iynixil <lixinyilxy02@gmail.com>
2024-02-01 02:21:38 +0800

    Update deadline and event tasks to use DateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String deadline;
+    protected LocalDateTime deadline;
+    static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    static final DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy ha");
 
     public Deadline(String desc, String deadline) {
         super(desc);
-        this.deadline = deadline;
-    }
-
-    public String getDeadline() {
-        return this.deadline;
+        this.deadline = LocalDateTime.parse(deadline, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return String.format("[Deadline]%s (by: %s)", super.toString(), this.deadline);
+        return String.format("[Deadline]%s (by: %s)", super.toString(), this.deadline.format(outputFormatter));
     }
 
     @Override
     public String fileString() {
-        return String.format("DEADLINE | %s | BY %s", super.fileString(), this.deadline);
+        return String.format("DEADLINE | %s | BY %s", super.fileString(), this.deadline.format(inputFormatter));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    static final DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy ha");
 
     public Event(String desc, String from, String to) {
         super(desc);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, inputFormatter);
+        this.to = LocalDateTime.parse(to, inputFormatter);
     }    
 
     @Override
     public String toString() {
-        return String.format("[Event]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[Event]%s (from: %s to: %s)", super.toString(), this.from.format(outputFormatter), this.to.format(outputFormatter));
     }
 
     @Override
     public String fileString() {
-        return String.format("EVENT | %s | FROM %s TO %s", super.fileString(), this.from, this.to);
+        return String.format("EVENT | %s | FROM %s TO %s", super.fileString(), this.from.format(inputFormatter), this.to.format(inputFormatter));
     }
 
 }
