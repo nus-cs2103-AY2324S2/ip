fc3428bf4906d7d91f49b94d1b2a46a96eed39f7
peterXGD <mizishanming@gmail.com>
2024-01-31 14:21:28 +0800

    level-1

--- src/main/java/Venus.java
+++ src/main/java/Venus.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
 public class Venus {
     public static void main(String[] args) {
         String start =
-             "____________________________________________________________\n"
-             + "Hello! I'm Venus\n"
-             + "What can I do for you?\n"
-             + "____________________________________________________________\n";
-
+             "    ____________________________________________________________\n"
+             + "    Hello! I'm Venus\n"
+             + "    What can I do for you?\n"
+             + "    ____________________________________________________________\n";
         String end =
-             "Bye. Hope to see you again soon!\n"
-             +  "____________________________________________________________\n";
-
+             "    ____________________________________________________________\n"
+             +  "    Bye. Hope to see you again soon!\n"
+             +  "    ____________________________________________________________\n";
+        String indented_lines = "    ____________________________________________________________\n";
         System.out.println(start);
+
+        Scanner sc = new Scanner(System.in);
+        String words = sc.nextLine();
+        while (!words.equals("bye")) {
+            System.out.println(indented_lines + "    " + words + "\n" + indented_lines);
+            words = sc.nextLine();
+        }
+        sc.close();
         System.out.println(end);
     }
 }
