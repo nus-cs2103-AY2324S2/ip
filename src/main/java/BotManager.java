cff5c47e7be80911c82befff5187f612b2c8b92a
JerryWang0000 <jerrywang0200@gmail.com>
2024-02-01 06:02:47 +0800

    Add task.java and modify BotManager.java
    Create a new Task class to facilitate the increasingly complicated features, and make amendment to the BotManager in accordance.

--- src/main/java/BotManager.java
+++ src/main/java/BotManager.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class BotManager {
     private final String name;
-    private ArrayList<String> tasks;
+    private ArrayList<Task> tasks;
 
     BotManager(String name) {
         this.name = name;
-        this.tasks = new ArrayList<>();
+        this.tasks = new ArrayList<Task>();
     }
 
     void greeting() {
@@ -19,13 +19,24 @@ public class BotManager {
     }
 
     void answer(String prompt) {
-        if (prompt.equals("list")) {
+        String[] order = prompt.split(" ");
+        if (order[0].equals("list")) {
             for (int i = 0; i < tasks.size(); i++) {
-                System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+                System.out.println("    " + (i + 1) + "." + tasks.get(i));
             }
             System.out.println();
+        } else if (order[0].equals("mark")) {
+            Task task = tasks.get(Integer.parseInt(order[1]) - 1);
+            task.mark();
+            System.out.println("    Nice! I've marked this task as done:");
+            System.out.println("      " + task);
+        } else if (order[0].equals("unmark")) {
+            Task task = tasks.get(Integer.parseInt(order[1]) - 1);
+            task.unmark();
+            System.out.println("    OK, I've marked this task as not done yet:");
+            System.out.println("      " + task);
         } else {
-            tasks.add(prompt);
+            tasks.add(new Task(prompt));
             System.out.println("    added " + prompt + "\n");
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private final String name;
+    private boolean done;
+
+    Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    void mark() {
+        this.done = true;
+    }
+
+    void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (done) {
+            return "[X] " + name;
+        } else {
+            return "[ ] " + name;
+        }
+    }
+}
