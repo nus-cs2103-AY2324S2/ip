85a345763f1c44ef4a2c03b15bc9cb900ded8709
faaheem13 <faaheemrahman@gmail.com>
2024-02-01 00:56:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Your Only Friend\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
+                "____________________________________________________________\n");
+
+        Scanner whatToDo = new Scanner(System.in);
+        String whatToDoCall = whatToDo.nextLine().trim();
+
+        while (!whatToDoCall.equals("bye")) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(whatToDoCall);
+            System.out.println("____________________________________________________________\n");
+            whatToDoCall = whatToDo.nextLine().trim();
+        }
+
+        System.out.println("____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n -Your Only Friend\n"
                 + "____________________________________________________________\n");
+
     }
 }
