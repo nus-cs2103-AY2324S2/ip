f3e33adc0920d5b92735e21d9729ad4613262b94
chuahjiajie <99877894+Nauseous-Spartan@users.noreply.github.com>
2024-01-31 23:24:52 +0800

    Made changes according to "Level 0" - Changed name, added a greeting and an exit.

---
+++ src/main/java/OedipusRex.java
@@ -0,0 +1,11 @@
+public class OedipusRex {
+    public static void main(String[] args) {
+        String line = "------------------------------------------\n";
+        System.out.println(line +
+                "Hello! I'm OedipusRex\n" +
+                "What can I do for you?" + "\n" +
+                line +
+                "Bye. Hope to see you again soon!" + "\n" +
+                line);
+    }
+}
