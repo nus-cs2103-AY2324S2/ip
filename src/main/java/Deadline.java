fbfb1780f66c200736a859d9d37037cfb978901a
gavingoh99 <gavingoh99@gmail.com>
2024-01-31 17:41:35 +0800

    feat: added datetime functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
+    protected DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("d 'of' MMMM yyyy, ha");
+    protected static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy HHmm");
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
     @Override
     public String parseToLogRepresentation() {
         int completionStatus = this.isDone ? 1 : 0;
-        return "D|" + completionStatus + "|" + this.description + "|" + this.by;
+        return "D|" + completionStatus + "|" + this.description + "|" + this.by.format(formatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by.format(outputFormatter) + ")";
     }
 }
