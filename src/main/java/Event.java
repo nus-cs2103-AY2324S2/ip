e2c88f1a6e91d87814fa83ee37332c200d97045f
sdevih <e0960282@u.nus.edu>
2024-01-31 20:07:40 +0800

    Read date and time for tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,36 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.DateTimeException;
+
 public class Event extends Task {
-    protected String from, to;
+    protected LocalDateTime from, to;
     protected String taskType = "E";
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        try {
+            this.from = LocalDateTime.parse(from.trim(), dateFormat);
+            this.to = LocalDateTime.parse(to.trim(), dateFormat);
+        } catch (DateTimeException e) {
+            System.out.println("Please enter a valid date and time in the format yyyy-mm-dd HHmm!\n" +
+                    "For example, 2024-01-31 1800");
+        }
     }
     //Overridden toString method to print type of task, description and time
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from:" + from + "to:" + to + ")";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm");
+        String formattedFrom = this.from.format(dateFormat);
+        String formattedTo = this.to.format(dateFormat);
+        return "[E]" + super.toString() + "(from: " + formattedFrom + " to: " + formattedTo + ")";
     }
 
-    public String getFrom() {
+    public LocalDateTime getFrom() {
         return this.from;
     }
-    public String getTo() {
+    public LocalDateTime getTo() {
         return this.to;
     }
 
