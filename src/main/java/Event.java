5f35b3026f7ef3dbb6475480c69b46a96a69af8c
Yiyang <liuyiyang0929@gmail.com>
2024-02-01 02:54:42 +0800

    Improve error catching for invalid dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,14 +7,14 @@ import java.time.format.DateTimeFormatter;
 public class Event extends Task {
     LocalDate from;
     LocalDate to;
-    public Event(String name, String from, String to) {
+    public Event(String name, String from, String to) throws InvalidCommandException {
         super(name);
         try {
-            LocalDate fromDate = LocalDate.parse(from);
-            this.from = fromDate;
-            LocalDate toDate = LocalDate.parse(to);
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
         } catch (DateTimeException e) {
-            System.out.println("Error in date formats D: It should follow yyyy-mm-dd");
+            throw new InvalidCommandException(
+                    "Invalid input format for date :( Please use the format yyyy-mm-dd instead!");
         }
     }
 
@@ -27,8 +27,6 @@ public class Event extends Task {
 
     @Override
     public String dataString() {
-        String formattedFrom = this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-        String formattedTo = this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-        return String.format("%s|%s|%s|%s\n", "E", super.dataString(), formattedFrom, formattedTo);
+        return String.format("%s|%s|%s|%s\n", "E", super.dataString(), this.from, this.to);
     }
 }
