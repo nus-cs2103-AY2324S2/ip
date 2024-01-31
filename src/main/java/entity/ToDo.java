bde059b434bc960a7b0d1e95dfda40adfaab310a
sethteo <hongruiseth.teo@gmail.com>
2024-01-31 20:12:43 +0800

    Add Javadoc for ToDo

--- src/main/java/entity/ToDo.java
+++ src/main/java/entity/ToDo.java
@@ -1,10 +1,18 @@
 package entity;
 
+/**
+ * The ToDo class which extends the Task class
+ */
 public class ToDo extends Task {
     public ToDo(String title) {
         super(title);
     }
 
+    /**
+     * Helper function to generate a string representation of the ToDo object for saving purposes.
+     *
+     * @return A string representation of the Event object for saving to a file.
+     */
     @Override
     public String save() {
         if (this.marked) {
