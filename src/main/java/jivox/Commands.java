79a36a125f40e5285aeedfd4a882120ef7df9a37
dedsecrattle <itsprabxxx@gmail.com>
2024-02-01 05:16:24 +0800

    Add Find functionality Level-9

--- src/main/java/jivox/Commands.java
+++ src/main/java/jivox/Commands.java
@@ -1,6 +1,11 @@
 package jivox;
-public enum COMMANDS {
-    TODO,DEADLINE,EVENT,
-    MARK,UNMARK,DELETE,BYE,LIST,
+
+
+/**
+ * List of all Possible Commands that can be Excuted with Bot
+ */
+public enum Commands {
+    TODO, DEADLINE, EVENT,
+    MARK, UNMARK, DELETE, BYE, LIST,
     SHOW, FIND
 }
