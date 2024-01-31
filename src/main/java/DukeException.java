dacd1d96e2081799821a67061a74a3f065a5c1ca
peterXGD <mizishanming@gmail.com>
2024-01-31 20:40:50 +0800

    The real level 5 v2

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DukeException {
+public class DukeException extends Exception {
+    //nothing here at the moment
+    public DukeException() {
+        super("This is a duke exception of unknown cause.");
+    }
+
+    public DukeException(String s){
+        super(s);
+    }
 }
--- src/main/java/Venus.java
+++ src/main/java/Venus.java
@@ -19,93 +19,138 @@ public class Venus {
         Scanner sc = new Scanner(System.in);
         String words = sc.nextLine();
 
-        while (!words.equals("bye")) {
-            if (words.equals("list")) {
-                int i = 1;
-                System.out.println(indented_lines
-                        + "     Here are the tasks in your list:");
-                for (Task s : data) {
-                    System.out.println("     " + i + "." + s);
-                    i++;
-                }
-                System.out.println(indented_lines);
-            } else if (words.startsWith("mark ")) {
-                try {
-                    int index = Integer.valueOf(words.substring(5)) - 1;
-                    data.get(index).mark();
+            while (!words.equals("bye")) {
+                if (words.equals("list")) {
+                    int i = 1;
                     System.out.println(indented_lines
-                            + "     Nice! I've marked this task as done:\n"
-                            + "       "
-                            + data.get(index).toString()
-                            + "\n"
-                            + indented_lines);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (words.startsWith("unmark ")) {
-                try {
-                    int index = Integer.valueOf(words.substring(7)) - 1;
-                    data.get(index).unmark();
+                            + "     Here are the tasks in your list:");
+                    for (Task s : data) {
+                        System.out.println("     " + i + "." + s);
+                        i++;
+                    }
+                    System.out.println(indented_lines);
+                } else if (words.startsWith("mark ")) {
+                    try {
+                        int index = Integer.valueOf(words.substring(5)) - 1;
+                        data.get(index).mark();
+                        System.out.println(indented_lines
+                                + "     Nice! I've marked this task as done:\n"
+                                + "       "
+                                + data.get(index).toString()
+                                + "\n"
+                                + indented_lines);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println(indented_lines
+                                + "     Incorrect name or spelling for mark, please check\n"
+                                + indented_lines);
+                    } catch (NumberFormatException e) {
+                        System.out.println(indented_lines
+                                + "     Incorrect arguments for mark, please check\n"
+                                + indented_lines);
+                    }
+                } else if (words.startsWith("unmark ")) {
+                    try {
+                        int index = Integer.valueOf(words.substring(7)) - 1;
+                        data.get(index).unmark();
+                        System.out.println(indented_lines
+                                + "     OK, I've marked this task as not done yet:\n"
+                                + "       "
+                                + data.get(index).toString()
+                                + "\n"
+                                + indented_lines);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println(indented_lines
+                                + "     Incorrect name or spelling for unmark, please check\n"
+                                + indented_lines);
+                    } catch (NumberFormatException e) {
+                        System.out.println(indented_lines
+                                + "     Incorrect arguments for unmark, please check\n"
+                                + indented_lines);
+                    }
+                } else if (words.startsWith("todo ")) {
+                    try {
+                        String word = words.substring(5);
+                        TODO todo = new TODO(word);
+                        data.add(todo);
+                        System.out.println(indented_lines
+                                + "     Got it. I've added this task:\n"
+                                + "       "
+                                + todo.toString()
+                                + "\n"
+                                + "     Now you have " + data.size() + " tasks in the list.\n"
+                                + indented_lines);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println(indented_lines
+                                + "     Incorrect arguments for todo, please check\n"
+                                + indented_lines);
+                    }
+                } else if (words.startsWith("deadline ")) {
+                    try {
+                        String dString = words.substring(9);
+                        String[] parts = dString.split("/by");
+                        if (parts.length != 1) {
+                            throw new DukeException("Incorrect, choose a specific deadline only please");
+                        }
+                        System.out.println(parts.length);
+                        parts[0] = parts[0].trim();
+                        parts[1] = parts[1].trim();
+                        Deadline deadline = new Deadline(parts[0], parts[1]);
+                        data.add(deadline);
+                        System.out.println(indented_lines
+                                + "     Got it. I've added this task:\n"
+                                + "       "
+                                + deadline.toString()
+                                + "\n"
+                                + "     Now you have " + data.size() + " tasks in the list.\n"
+                                + indented_lines);
+                    } catch (Exception e) {
+                        System.out.println(indented_lines
+                                + "    "
+                                + e.getMessage()
+                                + "\n"
+                                + indented_lines);
+                    }
+                } else if (words.startsWith("event ")) {
+                    try {
+                        String dString = words.substring(6);
+                        String[] parts = dString.split("/");
+                        if (parts.length != 2) {
+                            throw new DukeException("Incorrect arguments for events");
+                        }
+                        for (int i = 0; i < 3; i++) {
+                            parts[i] = parts[i].trim();
+                        }
+                        Event event = new Event(parts[0], parts[1], parts[2]);
+                        data.add(event);
+                        System.out.println(indented_lines
+                                + "     Got it. I've added this task:\n"
+                                + "       "
+                                + event.toString()
+                                + "\n"
+                                + "     Now you have " + data.size() + " tasks in the list.\n"
+                                + indented_lines);
+                    } catch (DukeException e) {
+                        System.out.println(indented_lines
+                                + "    "
+                                + e.getMessage()
+                                + "\n"
+                                + indented_lines);
+                    } catch (Exception e) {
+                        System.out.println(indented_lines
+                                + "An unknown exception is created, please report to the devs"
+                                + "\n"
+                                + indented_lines);
+                    }
+                } else {
+                    Task ts = new Task(words);
+                    data.add(ts);
                     System.out.println(indented_lines
-                            + "     OK, I've marked this task as not done yet:\n"
-                            + "       "
-                            + data.get(index).toString()
-                            + "\n"
-                            + indented_lines);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                            + "     added: "
+                            + words +
+                            "\n" + indented_lines);
                 }
-            } else if (words.startsWith("todo ")){
-                String word = words.substring(5);
-                TODO todo = new TODO(word);
-                data.add(todo);
-                System.out.println(indented_lines
-                        + "     Got it. I've added this task:\n"
-                        + "       "
-                        + todo.toString()
-                        + "\n"
-                        + "     Now you have "+ data.size() +" tasks in the list.\n"
-                        + indented_lines);
-            } else if (words.startsWith("deadline ")) {
-                String dString = words.substring(9);
-                String[] parts = dString.split("/by");
-                parts[0] = parts[0].trim();
-                parts[1] = parts[1].trim();
-                Deadline deadline = new Deadline(parts[0], parts[1]);
-                data.add(deadline);
-                System.out.println(indented_lines
-                        + "     Got it. I've added this task:\n"
-                        + "       "
-                        + deadline.toString()
-                        + "\n"
-                        + "     Now you have "+ data.size() +" tasks in the list.\n"
-                        + indented_lines);
-            } else if (words.startsWith("event ")) {
-                String dString = words.substring(6);
-                String[] parts = dString.split("/");
-                for (int i = 0; i < 3; i++){
-                    parts[i] = parts[i].trim();
-                }
-                Event event = new Event(parts[0], parts[1], parts[2]);
-                data.add(event);
-                System.out.println(indented_lines
-                        + "     Got it. I've added this task:\n"
-                        + "       "
-                        + event.toString()
-                        + "\n"
-                        + "     Now you have "+ data.size() +" tasks in the list.\n"
-                        + indented_lines);
-            } else {
-                Task ts = new Task(words);
-                data.add(ts);
-                System.out.println(indented_lines
-                        + "     added: "
-                        + words +
-                        "\n" + indented_lines);
+                words = sc.nextLine();
             }
-            words = sc.nextLine();
-        }
-
         sc.close();
         System.out.println(end);
     }
