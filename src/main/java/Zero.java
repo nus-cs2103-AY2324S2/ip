1f51a4236fe7a3d1bb4f040c3c1c453a813e73ff
Jeremiah Lim <43368034+jeremiahlzz@users.noreply.github.com>
2024-02-01 00:24:31 +0800

    Add save task data to file

--- src/main/java/Zero.java
+++ src/main/java/Zero.java
@@ -1,16 +1,22 @@
 import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StreamCorruptedException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.List;
 
 public class Zero {
     private static final String name = "Zero";
-    private static final String divider = "______\n";
+    private static final File saveFile = new File("data/save.ser");
 
     // Checks if the string is null, empty or filled with spaces
     private static boolean isNullOrEmpty(String s) {
@@ -56,14 +62,45 @@ public class Zero {
         return result;
     }
     
+    // Updates the save file with the task data
+    // Reopen and close the ObjectOutputStream each time to override the file and update properly
+    private static void saveTasks(ArrayList<Task> tasks, File saveFile) throws IOException {
+        try{
+            FileOutputStream fos = new FileOutputStream(saveFile);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(tasks);
+            oos.flush();
+            oos.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-        
+        saveFile.getParentFile().mkdirs();
+        if (!saveFile.exists()) {
+            saveFile.createNewFile();
+        }
+        ArrayList<Task> tasks;
+        try {
+            FileInputStream fis = new FileInputStream(saveFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            tasks = (ArrayList<Task>) ois.readObject();
+            ois.close();
+            fis.close();
+        }
+        catch (FileNotFoundException | ClassNotFoundException | ClassCastException |
+            EOFException | StreamCorruptedException e) {
+            tasks = new ArrayList<>();
+        }
+        saveTasks(tasks, saveFile); // Rewrite task data as readObject deletes save data
+
         pw.printf(Messages.GREET.toString(), name);
         pw.flush();
-
-        ArrayList<Task> tasks = new ArrayList<>();
         String[] cmd;
         int r; // Temporary integer for results
         Hashtable<String, String> dict; // Temporary dictionary for results
@@ -88,18 +125,21 @@ public class Zero {
                     r = checkIndex(cmd[1], pw, tasks);
                     if (r == -1) break;
                     tasks.set(r, tasks.get(r).mark());
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.MARKED.toString(), tasks.get(r));
                     break;
                 case "unmark":
                     r = checkIndex(cmd[1], pw, tasks);
                     if (r == -1) break;
                     tasks.set(r, tasks.get(r).unmark());
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.UNMARKED.toString(), tasks.get(r));
                     break;
                 case "delete":
                     r = checkIndex(cmd[1], pw, tasks);
                     if (r == -1) break;
                     t = tasks.remove(r);
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.REMOVE.toString(), t, tasks.size());
                     break;
                 case "todo":
@@ -111,6 +151,7 @@ public class Zero {
                     }
                     t = new ToDo(s);
                     tasks.add(t);
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.ADD.toString(), t, tasks.size());
                     break;
                 case "deadline":
@@ -127,6 +168,7 @@ public class Zero {
                     }
                     t = new Deadline(s, deadlineBy);
                     tasks.add(t);
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.ADD.toString(), t, tasks.size());
                     break;
                 case "event":
@@ -148,6 +190,7 @@ public class Zero {
                     }
                     t = new Event(s, eventFrom, eventTo);
                     tasks.add(t);
+                    saveTasks(tasks, saveFile);
                     pw.printf(Messages.ADD.toString(), t, tasks.size());
                     break;
                 default:
