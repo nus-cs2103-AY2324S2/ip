e36ab8f06c53b3df78f61211ea70de46306a7654
khushi833 <71274442+khushi833@users.noreply.github.com>
2024-02-01 02:42:39 +0800

    gradle

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -6,6 +6,7 @@ public class Ui {
         public void showWelcome() {
 
             System.out.println("Hello! I'm Aether!");
+
             System.out.println("What can I do for you?");
 
         }
