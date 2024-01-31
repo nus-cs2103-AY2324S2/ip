2a7382aa5dbb79c653c7d787cb3e9294340fad97
Tan Xinhao <e0774267@u.nus.edu>
2024-01-31 23:38:52 +0800

    Duke.java: add level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "CMU_bot";
+        String[] TaskList = new String[100];
+        int NumOfTask = 0;
         System.out.println("Hello! Boss I'm your" + logo);
         System.out.println("What can I do for you?");
 
@@ -13,8 +15,15 @@ public class Duke {
             if(command.equals("bye")) {
                 System.out.println("Bye. CMU_bot is always here for you!");
                 isExit = false;
+            } else if (command.equals("List")) {
+                for (int i = 0; i < NumOfTask; i ++) {
+                    System.out.println(TaskList[i]);
+                }
+
             } else {
-                System.out.println(command);
+                TaskList[NumOfTask] = Integer.toString(NumOfTask+1)+". " + command;
+                NumOfTask ++;
+                System.out.println("added: " + command);
             }
 
         }
