ff13ff7e54a7267aaab23f3ff7c3ae704e6c437c
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-01-31 23:52:39 +0800

    Level-5 Error Class Made

---
+++ src/main/java/EveExceptions.java
@@ -0,0 +1,11 @@
+public class EveExceptions extends RuntimeException {
+
+    public EveExceptions(String error) {
+        super(error);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
