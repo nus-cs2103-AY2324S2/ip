0ec14de12e4f459c7db4c4a4b348a85d2cefae58
jeong-jaeho <e0960435@u.nus.edu>
2024-01-31 12:26:35 +0800

    Add Scanner for user input and exiting with "bye".

--- src/main/java/Linus.java
+++ src/main/java/Linus.java
@@ -1,11 +1,24 @@
+import java.util.*;
+
 public class Linus {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Linus!\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Linus!\nWhat can I do for you?\n\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        // while loop to repeat printing of multiple Scanner inputs
+        while (true) {
+            String input = sc.nextLine();
+
+            // Check if the user wants to exit.
+            // When comparing strings for equality, you should use the equals() method, not the == operator.
+            // The == operator checks if two string references point to the same object in memory.
+            if (input.equals("bye")) {
+                System.out.println("Bye. It's been a pleasure chatting with you!");
+                break; // Exit the loop
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
