32ffc89990ccca1dbe41deb25985e4c4a7a95137
Derekljh <E0960128@u.nus.edu>
2024-01-31 20:27:15 +0800

    Add JUnit ToDo and Deadline test classes

---
+++ src/test/java/duke/task/DeadlineTest.java
@@ -0,0 +1,19 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void deadlineTestFile() {
+        assertEquals(new Deadline("0", "homework", "2023-02-01 2359").toFile(),
+                "D | 0 |  homework | Feb 01 2023 2359");
+    }
+
+    @Test
+    public void deadlineTestString() {
+        assertEquals(new Deadline("0", "homework", "2023-02-01 2359").toString(),
+                "[D][ ]  homework (by: Feb 01 2023 2359)");
+    }
+}
---
+++ src/test/java/duke/task/ToDoTest.java
@@ -0,0 +1,17 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void toDoTestFile() {
+        assertEquals(new ToDo("1", "return book").toFile(), "T | 1 |  return book");
+    }
+
+    @Test
+    public void toDoTestString() {
+        assertEquals(new ToDo("return book").toString(), "[T][ ]  return book");
+    }
+}
