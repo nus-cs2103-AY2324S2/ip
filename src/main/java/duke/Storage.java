3e5b18cc82359f78edd80dc6dec41cc0629ba106
soons1 <103474763+soons1@users.noreply.github.com>
2024-01-31 22:52:59 +0800

    Update to fit A-JavaDoc requirements by adding JavaDocs

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -6,12 +6,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The Storage class handles the loading and saving of tasks to and from a file.
+ * It provides functionality to read tasks from a file and write tasks back to the file,
+ * allowing for persistent storage of tasks across application sessions.
+ */
 public class Storage {
     private final File f;
+
+    /**
+     * Constructs a new Storage object.
+     * Initializes a File object to handle read and write operations to the specified file path.
+     *
+     * @param filePath The file path used for storing and retrieving task data.
+     */
     public Storage(String filePath) {
         this.f = new File(filePath);
     }
 
+    /**
+     * Loads tasks from the file associated with this Storage object.
+     * Parses the file content and constructs a list of tasks based on the data.
+     *
+     * @return ArrayList of Task objects read from the file.
+     * @throws DukeException If the file cannot be found or read from.
+     */
     public ArrayList<Task> load() throws DukeException {
         ArrayList<Task> loadedLst = new ArrayList<>();
         Scanner s;
@@ -40,6 +59,13 @@ public class Storage {
         return loadedLst;
     }
 
+    /**
+     * Saves tasks to the file associated with this Storage object.
+     * Writes the list of tasks from the provided TaskList into the file in a specified format.
+     *
+     * @param tl The TaskList containing the tasks to be saved.
+     * @throws IOException If an I/O error occurs during writing to the file.
+     */
     public void save(TaskList tl) throws IOException{
         f.getParentFile().mkdirs();
         FileWriter cfw = new FileWriter(f);
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,9 +1,18 @@
 package duke;
 
+/**
+ * The Ui class is responsible for handling all user interface elements
+ * of the application, such as greeting users, displaying messages, and showing errors.
+ * It provides methods to print various types of messages to the console.
+ */
 public class Ui{
 
     private static final String LINEBREAK = "_______________________________________________________________________________";
 
+    /**
+     * Displays a greeting message to the user.
+     * This includes an ASCII art representation of a snowman and a welcome message.
+     */
     public void greet() {
         String snowBoyAscii =
                 "      *      \n" +
@@ -21,17 +30,32 @@ public class Ui{
         Ui.beautify(toPrint);
     }
 
+    /**
+     * Beautifies and formats a string message for display.
+     * This method adds line breaks before and after the message for better readability.
+     *
+     * @param toPrint The string message to be formatted and displayed.
+     */
     public static void beautify(String toPrint) {
         System.out.println(LINEBREAK);
         System.out.println(toPrint);
         System.out.println(LINEBREAK);
     }
 
+    /**
+     * Displays a loading error message.
+     * This is typically called when the application fails to load an existing task list
+     * and needs to create a new one.
+     */
     public void showLoadingError() {
         String toPrint = " No existing list detected. Creating new list...";
         Ui.beautify(toPrint);
     }
 
+    /**
+     * Displays an exit message to the user.
+     * This message is shown when the user decides to exit the application.
+     */
     public void exit() {
         String toPrint = " Bye. Hope to see you again soon!";
         Ui.beautify(toPrint);
