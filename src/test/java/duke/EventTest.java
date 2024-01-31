0783315ac6d42ea925dde8fce34f7e2dc79e272d
Ragnapop <jinhunl@gmail.com>
2024-01-31 17:04:59 +0800

    Update EventTest.java

--- src/test/java/duke/EventTest.java
+++ src/test/java/duke/EventTest.java
@@ -7,5 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 public class EventTest {
-
+    public void Event_Test_Success(){
+        //assertEquals(new Event("This is a test", "2019-10-15" , "2019-10-16").ToString(), );
+    }
 }
