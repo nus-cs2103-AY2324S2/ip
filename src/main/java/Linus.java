59dc80a62012026f869d2701510a1148e7cf1173
jeong-jaeho <e0960435@u.nus.edu>
2024-01-31 18:37:15 +0800

    Mark as done

--- src/main/java/Linus.java
+++ src/main/java/Linus.java
@@ -5,9 +5,10 @@ public class Linus {
         System.out.println("Hello! I'm Linus!\nWhat can I do for you?\n\n");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // while loop to repeat printing of multiple Scanner inputs
+        // adapted with help of AI
         while (true) {
             String input = sc.nextLine();
 
@@ -17,12 +18,27 @@ public class Linus {
                 System.out.println("Bye. It's been a pleasure chatting with you!");
                 break;
             } else if (input.equals("list")) { // list tasks
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task currTask = tasks.get(i);
+                    System.out.println((i + 1) + ". [" + currTask.getStatusIcon() + "] " + currTask.getDescription());
                 }
+            } else if (input.startsWith("mark")) {
+                Integer indexOfTask = Integer.parseInt(input.substring(5));
+                Task currTask = tasks.get(indexOfTask);
+                currTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+            } else if (input.startsWith("unmark")) {
+                Integer indexOfTask = Integer.parseInt(input.substring(7));
+                Task currTask = tasks.get(indexOfTask);
+                currTask.markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
             } else { // add tasks
-                System.out.println("added: " + input); // println automatically inserts new line
-                tasks.add(input);
+                Task addedTask = new Task(input);
+                System.out.println("added: " + addedTask.getDescription()); // println automatically inserts new line
+                tasks.add(addedTask);
             }
         }
 
