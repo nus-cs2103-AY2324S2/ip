f9e03f304632aaa69612982972c1cbc853d8d4e2
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-01-31 17:29:06 +0800

    Level-7

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,13 +1,39 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
 
-public class Duke {
+public class Friday {
+    private static final String DATA_FILE_PATH = "./src/main/java/data/Friday.txt";
+    private static final String horizontalLine = "_".repeat(60);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int counter = 0;
-        List<Task> task = new ArrayList<>();
-        String horizontalLine = "_".repeat(60);
+        File f = new File(DATA_FILE_PATH);
+
+        try {
+            if (!f.exists()) {
+                if (f.createNewFile()) {
+                    System.out.println("Data file created.");
+                } else {
+                    System.out.println("Error creating data file.");
+                }
+            } else {
+                System.out.println("Loading data from Duke.txt...");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        List<Task> tasks = new ArrayList<>();
+        try {
+            tasks = loadDataFromFile(f);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int counter = tasks.size();
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
@@ -21,7 +47,7 @@ public class Duke {
                 case "list":
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < counter; i++) {
-                        System.out.println((i + 1) + ". " + task.get(i).toString());
+                        System.out.println((i + 1) + ". " + tasks.get(i).toString());
                     }
                     break;
                 case "mark":
@@ -36,8 +62,13 @@ public class Duke {
                         break;
                     }
                     System.out.println("Nice! I've marked this task as done:");
-                    task.get(id - 1).markAsDone();
-                    System.out.println(task.get(id - 1).toString());
+                    tasks.get(id - 1).markAsDone();
+                    System.out.println(tasks.get(id - 1).toString());
+                    try {
+                        writeToFile(DATA_FILE_PATH, tasks);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "unmark":
                     String[] toUnmark = userInput.split(" ");
@@ -51,8 +82,13 @@ public class Duke {
                         break;
                     }
                     System.out.println("OK, I've marked this task as not done yet:");
-                    task.get(num - 1).markAsUndone();
-                    System.out.println(task.get(num - 1).toString());
+                    tasks.get(num - 1).markAsUndone();
+                    System.out.println(tasks.get(num - 1).toString());
+                    try {
+                        writeToFile(DATA_FILE_PATH, tasks);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "todo":
                     String[] todos = userInput.split(" ");
@@ -63,9 +99,14 @@ public class Duke {
                     System.out.println("Got it. I've added this task:");
                     Todo t = new Todo(userInput.substring(5));
                     System.out.println(t);
-                    task.add(t);
+                    tasks.add(t);
                     counter++;
                     System.out.println(taskCounter(counter));
+                    try {
+                        appendToFile(DATA_FILE_PATH, t.toString() + System.lineSeparator());
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "deadline":
                     String[] deadlines = userInput.split(" ");
@@ -86,9 +127,14 @@ public class Duke {
                     System.out.println("Got it. I've added this task:");
                     Deadline d = new Deadline(description, by);
                     System.out.println(d);
-                    task.add(d);
+                    tasks.add(d);
                     counter++;
                     System.out.println(taskCounter(counter));
+                    try {
+                        appendToFile(DATA_FILE_PATH, d.toString() + System.lineSeparator());
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "event":
                     String[] events = userInput.split(" ");
@@ -115,9 +161,14 @@ public class Duke {
                     Event e = new Event(descr, from, to);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(e);
-                    task.add(e);
+                    tasks.add(e);
                     counter++;
                     System.out.println(taskCounter(counter));
+                    try {
+                        appendToFile(DATA_FILE_PATH, e.toString() + System.lineSeparator());
+                    } catch (IOException err) {
+                        System.out.println(err.getMessage());
+                    }
                     break;
                 case "delete":
                     String[] toDelete = userInput.split(" ");
@@ -131,10 +182,15 @@ public class Duke {
                         break;
                     }
                     System.out.println("Noted. I have removed this task:");
-                    System.out.println(task.get(j - 1));
-                    task.remove(j - 1);
+                    System.out.println(tasks.get(j - 1));
+                    tasks.remove(j - 1);
                     counter--;
                     System.out.println(taskCounter(counter));
+                    try {
+                        writeToFile(DATA_FILE_PATH, tasks);
+                    } catch (IOException err) {
+                        System.out.println(err.getMessage());
+                    }
                     break;
                 default:
                     System.out.println("HUH? What do you mean?");
@@ -156,5 +212,35 @@ public class Duke {
         } else {
             return "Now you have " + counter + " tasks in the list.";
         }
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    private static void writeToFile(String filePath, List<Task> tasks) throws IOException {
+        FileWriter fw = new FileWriter(DATA_FILE_PATH);
+        for (Task t : tasks) {
+            fw.write(t.toString() + System.lineSeparator());
+        }
+        fw.close();
+    }
+    private static List<Task> loadDataFromFile(File file) throws IOException {
+        List<Task> taskList = new ArrayList<>();
+
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNextLine()) {
+                String taskData = fileScanner.nextLine();
+                taskList.add(Task.parseTask(taskData));
+                System.out.println("loaded");
+            }
+        }
+        if (taskList.isEmpty()) {
+            // If the file was empty, instantiate a new task list
+            taskList = new ArrayList<>();
+        }
+        return taskList;
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -19,6 +21,43 @@ public class Task {
         this.isDone = false;
     }
 
+    public static Task parseTask(String taskString) {
+        String cat = taskString.substring(1, 2);
+        switch (cat) {
+            case "T":
+                Todo t = new Todo(taskString.substring(7).trim());
+                if (taskString.substring(4, 5).equals("X")) {
+                    t.markAsDone();
+                } else {
+                    t.markAsUndone();
+                }
+                return t;
+            case "D":
+                String by = taskString.split(":")[1].split("\\)")[0].trim();
+                String description = taskString.split("\\(")[0].substring(7).trim();
+                Deadline d = new Deadline(description, by);
+                if (taskString.substring(4, 5).equals("X")) {
+                    d.markAsDone();
+                } else {
+                    d.markAsUndone();
+                }
+                return d;
+            case "E":
+                String from = taskString.split(":")[1].trim();
+                String to = taskString.split(":")[2].split("\\)")[0].trim();
+                String des = taskString.split("\\(")[0].substring(7).trim();
+                Event e = new Event(des, from, to);
+                if (taskString.substring(4, 5).equals("X")) {
+                    e.markAsDone();
+                } else {
+                    e.markAsUndone();
+                }
+                return e;
+            default:
+                System.out.println("Cannot resolve task.");
+                return null;
+        }
+    }
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
