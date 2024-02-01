ccc5affa4c4753ece1f80584e370820afeec9085
sebin0817 <aumsebin@Aums-MacBook-Pro.local>
2024-02-01 23:09:07 +0800

    Completed A-JUnit: Add JUnit Tests

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -2,58 +2,66 @@ package duke.ui;
 
 import duke.task.TaskList;
 
+import java.io.PrintStream;
+
 public class Ui {
+
+    private static PrintStream out;
     private final static String LINE = "-----------------------------------------------------------------------";
 
+    public static void setOutputStream(PrintStream out) {
+        Ui.out = out;
+    }
+
     public static void greet(String name) {
-        System.out.println(LINE);
-        System.out.println("Heyoo I'm " + name + ".");
-        System.out.println("What can I do for you?");
-        System.out.println(LINE);
+        out.println(LINE);
+        out.println("Heyoo I'm " + name + ".");
+        out.println("What can I do for you?");
+        out.println(LINE);
     }
 
     public static void bye(TaskList tasklist) {
-        System.out.println(LINE);
-        System.out.println("Bye lol see you again!");
-        System.out.println(LINE);
+        out.println(LINE);
+        out.println("Bye lol see you again!");
+        out.println(LINE);
         tasklist.saveTasks();
     }
 
     public static void echo(String input) {
-        System.out.println(LINE);
-        System.out.println(input);
-        System.out.println(LINE);
+        out.println(LINE);
+        out.println(input);
+        out.println(LINE);
     }
 
     public static void listTasks(TaskList tasklist) {
-        System.out.println(LINE);
-        System.out.println("Here are the tasks in your list: ");
+        out.println(LINE);
+        out.println("Here are the tasks in your list: ");
         tasklist.listTask();
-        System.out.println(LINE);
+        out.println(LINE);
     }
 
     public static void addTask(TaskList tasklist, String input) {
-        System.out.println(LINE);
+        out.println(LINE);
 
         boolean added = tasklist.addTask(input);
         int tasksCount = TaskList.tasksCount;
 
         if (added) {
-            System.out.println("Gotcha. I've added this task: ");
-            System.out.println("    " + tasklist.getTaskDescription(tasksCount - 1));
-            System.out.println("Now you have " + tasksCount + " tasks in the list.");
+            out.println("Gotcha. I've added this task: ");
+            out.println("    " + tasklist.getTaskDescription(tasksCount - 1));
+            out.println("Now you have " + tasksCount + " tasks in the list.");
         }
-        System.out.println(LINE);
+        out.println(LINE);
     }
 
     public static void mark(TaskList tasklist, String input, boolean status) {
         int num = Integer.parseInt(input) - 1; // duke.task.Task num starts from 0
         tasklist.markTask(num, status);
 
-        System.out.println(LINE);
-        System.out.println(status ? "Good job on finishing this task: " : "Aw OK, I've marked this task as not done yet: ");
-        System.out.println("    " + tasklist.getTaskDescription(num));
-        System.out.println(LINE);
+        out.println(LINE);
+        out.println(status ? "Good job on finishing this task: " : "Aw OK, I've marked this task as not done yet: ");
+        out.println("    " + tasklist.getTaskDescription(num));
+        out.println(LINE);
     }
 
     public static void delete(TaskList tasklist, String input) {
@@ -61,10 +69,10 @@ public class Ui {
         String deletedTask = tasklist.getTaskDescription(num);
         tasklist.deleteTask(num);
 
-        System.out.println(LINE);
-        System.out.println("Sure, I've removed this task:");
-        System.out.println("    " + deletedTask);
-        System.out.println("Now you have " + TaskList.tasksCount + " tasks in the list.");
-        System.out.println(LINE);
+        out.println(LINE);
+        out.println("Sure, I've removed this task:");
+        out.println("    " + deletedTask);
+        out.println("Now you have " + TaskList.tasksCount + " tasks in the list.");
+        out.println(LINE);
     }
 }
