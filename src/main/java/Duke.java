2cb67c16a626975b85dc3e70b266a55994195062
jjchee7 <jingjie.chee@u.nus.edu>
2024-02-02 02:56:25 +0800

    Add checkstyles

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 
+import exceptions.tasks.EmptyDescriptionException;
 import tasks.Deadline;
 import tasks.Event;
 import tasks.Task;
 import tasks.Todo;
 
-import exceptions.tasks.EmptyDescriptionException;
-
+/**
+ * The duke class represents the entry point to the chat-bot.
+ */
 public class Duke {
+
     private static final String NAME = "Arctic";
+    private static final Character BORDER_CHAR = '_';
     private static final Integer BORDER_LEN = 60;
     private static final ArrayList<Task> TASKS = new ArrayList<>();
 
-    private static String duplicateChar(Character c, Integer num) {
-        return String.valueOf(c).repeat(num);
-    }
-
     private static String getBorder() {
-        return duplicateChar('_', BORDER_LEN);
+        return String.valueOf(BORDER_CHAR).repeat(BORDER_LEN);
     }
 
-    private static void userGreeting () {
+    private static void userGreeting() {
         System.out.println(getBorder());
         System.out.printf("Hello! I'm %s\n", NAME);
         System.out.println("What can I do for you?");
@@ -32,12 +32,6 @@ public class Duke {
     private static void userFarewell() {
         System.out.println(getBorder());
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(getBorder());
-    }
-
-    private static void printCommandOutput(String output) {
-        System.out.println(getBorder());
-        System.out.printf(output);
         System.out.println(getBorder());
     }
 
@@ -57,10 +51,10 @@ public class Duke {
 
         if (command.equals("mark")) {
             task.mark();
-            return String.format("Nice! I've marked this task as done:\n  %s", task.toString());
+            return String.format("Nice! I've marked this task as done:\n  %s", task);
         } else {
             task.unmark();
-            return String.format("OK, I've marked this task as not done yet:\n  %s", task.toString());
+            return String.format("OK, I've marked this task as not done yet:\n  %s", task);
         }
     }
 
@@ -100,7 +94,7 @@ public class Duke {
         String to = argsStr.substring(toIndex + toDelim.length()).trim();
         String description = argsStr.substring(0, fromIndex).trim();
 
-        Event event = new Event(description, from , to);
+        Event event = new Event(description, from, to);
         TASKS.add(event);
 
         return event;
@@ -113,23 +107,24 @@ public class Duke {
         Task task;
         try {
             switch (taskType) {
-                case "todo":
-                    task = addTodo(taskArgs);
-                    break;
-                case "deadline":
-                    task = addDeadline(taskArgs);
-                    break;
-                case "event":
-                    task = addEvent(taskArgs);
-                    break;
-                default:
-                    return "";
+            case "todo":
+                task = addTodo(taskArgs);
+                break;
+            case "deadline":
+                task = addDeadline(taskArgs);
+                break;
+            case "event":
+                task = addEvent(taskArgs);
+                break;
+            default:
+                return "";
             }
         } catch (EmptyDescriptionException ede) {
             return ede.toString();
         }
 
-        return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.", task.toString(), TASKS.size());
+        return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.",
+                task, TASKS.size());
     }
 
     private static String handleDeleteTask(String[] args) {
@@ -137,39 +132,42 @@ public class Duke {
         Task task = TASKS.get(taskNum - 1);
         TASKS.remove(taskNum);
 
-        return String.format("Noted. I've removed this task:\n  %s\nNow you have %d tasks in the list.", task.toString(), TASKS.size());
+        return String.format("Noted. I've removed this task:\n  %s\nNow you have %d tasks in the list.",
+                task, TASKS.size());
     }
 
     private static void handleCommands() {
-        Scanner scnr = new Scanner(System.in);
-        while (scnr.hasNextLine()) {
-            String userInput = scnr.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userInput = scanner.nextLine();
             String[] args = userInput.split(" ");
             String command = args[0];
 
             String commandOutput;
             switch (command) {
-                case "list":
-                    commandOutput = handleList();
-                    break;
-                case "bye":
-                    return;
-                case "mark":
-                case "unmark":
-                    commandOutput = handleMarkUnMark(args);
-                    break;
-                case "todo":
-                case "deadline":
-                case "event":
-                    commandOutput = handleAddTask(args);
-                    break;
-                case "delete":
-                    commandOutput = handleDeleteTask(args);
-                    break;
-                default:
-                    commandOutput = "OOPS!!! I don't understand that command, try again later.";
-                    break;
+            case "list":
+                commandOutput = handleList();
+                break;
+            case "bye":
+                return;
+            case "mark":
+                // Fallthrough
+            case "unmark":
+                commandOutput = handleMarkUnMark(args);
+                break;
+            case "todo":
+            case "deadline":
+            case "event":
+                commandOutput = handleAddTask(args);
+                break;
+            case "delete":
+                commandOutput = handleDeleteTask(args);
+                break;
+            default:
+                commandOutput = "OOPS!!! I don't understand that command, try again later.";
+                break;
             }
+
             System.out.println(getBorder());
             System.out.println(commandOutput);
             System.out.println(getBorder());
