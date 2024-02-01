687f0a46ec012da4c413077080d1734c823125bf
CJerrong <chong.jer.rong@gmail.com>
2024-02-01 22:59:12 +0800

    Add functionality to store deadline, event start and end as LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,56 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     String start;
     String end;
+    LocalDate startDate;
+    LocalDate endDate;
     public Event(String task, String start, String end) {
         super(task);
         this.start = start;
         this.end = end;
+
+        try {
+            startDate = LocalDate.parse(start);
+            endDate = LocalDate.parse(end);
+        } catch (DateTimeParseException e) {
+            startDate = null;
+            endDate = null;
+        }
     }
 
     public Event(String task, String start, String end, boolean done) {
         super(task, done);
         this.start = start;
         this.end = end;
+
+        try {
+            startDate = LocalDate.parse(start);
+            endDate = LocalDate.parse(end);
+        } catch (DateTimeParseException e) {
+            startDate = null;
+            endDate = null;
+        }
     }
 
     @Override
     public String printTask() {
-        return "[E]" + super.printTask() + " (from: " + this.start + " to: " + this.end + ")";
+        String startString;
+        String endString;
+        if (startDate != null) {
+            startString = startDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } else {
+            startString = this.start;
+        }
+
+        if (endDate != null) {
+            endString = endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } else {
+            endString = this.start;
+        }
+        return "[E]" + super.printTask() + " (from: " + startString + " to: " + endString + ")";
     }
 
     @Override
