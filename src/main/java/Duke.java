933acf5e6463545cca64f9d7e8441e0c49710f58
logical-1985516 <ngyanjie0418@gmail.com>
2024-02-01 13:42:10 +0800

    Move Main logic into Parser and ui; expand InvalidSyntaxException
    Typing "bye ..." or "list ..." would now throw InvalidSyntaxException
    instead of UnknownCommandException. This makes it clearer to the user
    on what to do

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,8 @@ import java.io.*;
 public class Duke {
     public static void main(String[] args) throws TaskNotFoundException, UnknownCommandException,
             InvalidSyntaxException, IOException, InvalidDateTimeException {
+        Ui ui = new Ui();
+        Parser parser = new Parser();
         List taskList = new List(new ArrayList<>());
         String fileName = "./data/duke.txt";
         File f = new File(fileName);
@@ -17,111 +19,11 @@ public class Duke {
         } else {
             taskList.loadTasks();
         }
-        System.out.println(
-                "__________________________________________________________\n"
-                        + "Hello! I'm KitchenSink!\n"
-                        + "What can I do for you?\n"
-                        + "__________________________________________________________\n"
-        );
-        Scanner sc = new Scanner(System.in);
-        String input;
-        while (true) {
-            input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println(
-                        "__________________________________________________________\n"
-                                + "Bye. Hope to see you again soon!\n"
-                                + "__________________________________________________________"
-                );
-                break;
-            }
-            if (input.equalsIgnoreCase("list")) {
-                taskList.displayTasks();
-                continue;
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("mark")) {
-                if (input.split(" ").length != 2) {
-                    throw new InvalidSyntaxException("mark");
-                } else if (taskList.validTaskNum(Integer.parseInt(input.split(" ")[1]))) {
-                    taskList.markTask(Integer.parseInt(input.split(" ")[1]) - 1);
-                    continue;
-                } else {
-                    throw new TaskNotFoundException(taskList);
-                }
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("unmark")) {
-                if (input.split(" ").length != 2) {
-                    throw new InvalidSyntaxException("unmark");
-                } else if (taskList.validTaskNum(Integer.parseInt(input.split(" ")[1]))) {
-                    taskList.unmarkTask(Integer.parseInt(input.split(" ")[1]) - 1);
-                    continue;
-                } else {
-                    throw new TaskNotFoundException(taskList);
-                }
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("todo")) {
-                if (input.split(" ").length > 1) {
-                    taskList.addTask(new ToDo(input.substring(5)));
-                    continue;
-                } else {
-                    throw new InvalidSyntaxException("todo");
-                }
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("deadline")) {
-                if (input.split(" ").length > 1 &&
-                        input.split(" /by ").length == 2 &&
-                        input.substring(9).split("/by ")[0].length() > 0) {
-                    try {
-                        LocalDateTime.parse(input.split("/by ")[1],
-                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-                    } catch (Exception e) {
-                        throw new InvalidDateTimeException();
-                    }
-                    taskList.addTask(new Deadline(
-                            input.substring(9).split(" /by")[0],
-                            LocalDateTime.parse(input.split("/by ")[1],
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))));
-                    continue;
-                } else {
-                    throw new InvalidSyntaxException("deadline");
-                }
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("event")) {
-                if (input.split(" ").length > 1 &&
-                    input.split(" /from ").length == 2 &&
-                    input.split(" /from ")[1].split(" /to ").length == 2 &&
-                    input.split(" /to ").length == 2 &&
-                    input.substring(6).split("/from ")[0].length() > 0) {
-                    try {
-                        LocalDateTime.parse(input.split("/from ")[1].split(" /to")[0],
-                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-                        LocalDateTime.parse(input.split("/to ")[1],
-                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-                    } catch (Exception e) {
-                        throw new InvalidDateTimeException();
-                    }
-                    taskList.addTask(new Event(
-                            input.substring(6).split(" /from")[0],
-                            LocalDateTime.parse(input.split("/from ")[1].split(" /to")[0],
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
-                            LocalDateTime.parse(input.split("/to ")[1],
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))));
-                    continue;
-                } else {
-                    throw new InvalidSyntaxException("event");
-                }
-            }
-            if (input.split(" ")[0].equalsIgnoreCase("delete")) {
-                if (input.split(" ").length != 2) {
-                    throw new InvalidSyntaxException("delete");
-                } else if (taskList.validTaskNum(Integer.parseInt(input.split(" ")[1]))) {
-                    taskList.deleteTask(Integer.parseInt(input.split(" ")[1]) - 1);
-                    continue;
-                } else {
-                    throw new TaskNotFoundException(taskList);
-                }
-            }
-            throw new UnknownCommandException();
+        ui.welcome();
+        boolean isExit = false;
+        while (!isExit) {
+            String input = ui.readInput();
+            isExit = parser.parse(input, taskList, ui);
         }
     }
 
