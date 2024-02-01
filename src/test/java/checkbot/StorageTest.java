994f72fe62295321eee60a8fc3e71e168e6aafb6
Zhekai <limzhekai1999@live.com>
2024-02-01 16:55:09 +0800

    test: add unit tests for AddCommand and Storage

---
+++ src/test/java/checkbot/StorageTest.java
@@ -0,0 +1,42 @@
+package checkbot;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Scanner;
+
+import checkbot.task.Todo;
+import checkbot.task.TodoList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StorageTest {
+    @Test
+    public void saveToFile_success() throws Exception {
+        Storage storage = new Storage("./taskstest.txt");
+        TodoList todoList = new TodoList();
+        todoList.addTask(new Todo("test task"));
+        storage.saveTasks(todoList);
+
+        File f = new File("./taskstest.txt");
+        f.deleteOnExit();
+        Scanner scanner = new Scanner(f);
+        String testString = scanner.nextLine();
+        scanner.close();
+        assertEquals(testString, "T | 0 | test task");
+    }
+
+    @Test
+    public void loadFile_success() throws Exception {
+        Writer writer = new FileWriter("./taskstest.txt");
+        writer.write("T | 0 | test task");
+        writer.close();
+        File f = new File("./taskstest.txt");
+        f.deleteOnExit();
+
+        Storage storage = new Storage("./taskstest.txt");
+        TodoList todoList = storage.loadTasks();
+        assertEquals(todoList.getTask(0).formatForFile(), "T | 0 | test task");
+    }
+}
