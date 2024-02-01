0f36bb289ff7e5136099524da5f8f329d7fa43dd
sindhurajain <sindhura394@gmail.com>
2024-02-02 01:12:22 +0800

    Add JUnit tests for TaskList

--- src/test/java/duke/main/ParserTest.java
+++ src/test/java/duke/main/ParserTest.java
@@ -14,21 +14,19 @@ import duke.exception.DukeException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 public class ParserTest {
 
-    //test instanceof
     @Test
     void parserTest() throws DukeException  {
-        assertTrue(Parser.parse("deadline return book /by 2019-10-15 1800") instanceof DeadlineCommand);
-        assertTrue(Parser.parse("delete 50") instanceof DeleteCommand);
-        assertTrue(Parser.parse("event write essay /from 2019-10-15 1800 /to 2019-10-17 2000") instanceof EventCommand);
-        assertTrue(Parser.parse("bye") instanceof ExitCommand);
-        assertTrue(Parser.parse("list") instanceof ListCommand);
-        assertTrue(Parser.parse("mark 1") instanceof MarkCommand);
-        assertTrue(Parser.parse("on Oct 15 2019") instanceof OnCommand);
-        assertTrue(Parser.parse("on Oct 17 2019") instanceof OnCommand);
-        assertTrue(Parser.parse("todo go to school") instanceof ToDoCommand);
-        assertTrue(Parser.parse("unmark 20") instanceof UnMarkCommand);
+        assertTrue(Parser.parse("deadline return book /by 2019-10-15 1800") instanceof DeadlineCommand, "deadline parser passed");
+        assertTrue(Parser.parse("delete 50") instanceof DeleteCommand, "delete parser passed");
+        assertTrue(Parser.parse("event write essay /from 2019-10-15 1800 /to 2019-10-17 2000") instanceof EventCommand, "event parser passed");
+        assertTrue(Parser.parse("bye") instanceof ExitCommand, "exit parser passed");
+        assertTrue(Parser.parse("list") instanceof ListCommand, "list parser passed");
+        assertTrue(Parser.parse("mark 1") instanceof MarkCommand, "mark parser passed");
+        assertTrue(Parser.parse("on Oct 15 2019") instanceof OnCommand, "on parser passed");
+        assertTrue(Parser.parse("on Oct 17 2019") instanceof OnCommand, "on parser passed");
+        assertTrue(Parser.parse("todo go to school") instanceof ToDoCommand, "todo parser passed");
+        assertTrue(Parser.parse("unmark 20") instanceof UnMarkCommand, "unmark parser passed");
     }
 }
