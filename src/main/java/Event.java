53e8e129a96c78a1e13fb756caea4b2cefc7ebf5
PallonCX <cheexiang03@gmail.com>
2024-02-01 17:09:19 +0800

    Modify Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,9 +3,14 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
-
     public Event(String description, String from, String to) {
         super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public Event(String description, boolean isDone, String from, String to) {
+        super(description, isDone);
         this.from = from;
         this.to = to;
     }
@@ -13,5 +18,15 @@ public class Event extends Task {
     @Override
     public String getDescriptionStatus() {
         return "[E]" + (isDone ? "[X] " : "[ ] ") + this.description + " (from: " + this.from + " to: " + this.to + ")";
+    }
+
+    @Override
+    public String[] getFields() {
+        String[] result = new String[4];
+        result[0] = this.description;
+        result[1] = this.isDone ? "Y" : "N";
+        result[2] = this.to;
+        result[3] = this.from;
+        return result;
     }
 }
