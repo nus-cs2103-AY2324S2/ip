5276245154f47a7a36682a1d6c3c6b0dc812ded8
narwhalsilent <110954060+narwhalsilent@users.noreply.github.com>
2024-02-01 11:20:40 +0800

    Add save command

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -16,7 +16,8 @@ public class Knight {
         DELETE,
         TODO,
         EVENT,
-        DEADLINE;
+        DEADLINE,
+        SAVE;
     }
 
     private static void speak(String s) {
@@ -24,16 +25,29 @@ public class Knight {
     }
 
     private static Command parseCommand(String s) {
-        if (s.equals("bye")) return Command.BYE;
-        else if (s.equals("list")) return Command.LIST;
-        else if (s.matches("mark [1-9]\\d*")) return Command.MARK;
-        else if (s.matches("unmark [1-9]\\d*")) return Command.UNMARK;
-        else if (s.matches("delete [1-9]\\d*")) return Command.DELETE;
-        else if (s.matches("todo \\S.*")) return Command.TODO;
-        else if (s.matches("deadline \\S.* /by \\S.*")) return Command.DEADLINE;
-        else if (s.matches("event \\S.* /from \\S.* /to \\S.*")) return Command.EVENT;
-        else throw new NonstandardCommandException();
+        if (s.equals("bye")) {
+            return Command.BYE;
+        } else if (s.equals("save")) {
+            return Command.SAVE;
+        } else if (s.equals("list")) {
+            return Command.LIST;
+        } else if (s.matches("mark [1-9]\\d*")) {
+            return Command.MARK;
+        } else if (s.matches("unmark [1-9]\\d*")) {
+            return Command.UNMARK;
+        } else if (s.matches("delete [1-9]\\d*")) {
+            return Command.DELETE;
+        } else if (s.matches("todo \\S.*")) {
+            return Command.TODO;
+        } else if (s.matches("deadline \\S.* /by \\S.*")) {
+            return Command.DEADLINE;
+        } else if (s.matches("event \\S.* /from \\S.* /to \\S.*")) {
+            return Command.EVENT;
+        } else {
+            throw new NonstandardCommandException();
+        }
     }
+
     public static void main(String[] args) {
         speak("Greetings, Your Excellency! Thy humble\n"
                 + logo
@@ -51,43 +65,35 @@ public class Knight {
             } catch (NonstandardCommandException e) {
                 if (message.startsWith("bye")) {
                     speak("Thou canst bid me farewell simply with:\n    bye");
-                }
-                else if (message.startsWith("list")) {
+                } else if (message.startsWith("list")) {
                     speak("Though canst view thy list simply with:\n    list");
-                }
-                else if (message.startsWith("mark")) {
+                } else if (message.startsWith("mark")) {
                     speak("Take heed, for thou shouldst reference the task thou wishest to alter by its index:");
                     speak("mark [index]");
-                }
-                else if (message.startsWith("unmark")) {
+                } else if (message.startsWith("unmark")) {
                     speak("Take heed, for thou shouldst reference the task thou wishest to alter by its index:");
                     speak("unmark [index]");
-                }
-                else if (message.startsWith("delete")) {
+                } else if (message.startsWith("delete")) {
                     speak("Take heed, for thou shouldst reference the task thou wishest to alter by its index:");
                     speak("delete [index]");
-                }
-                else if (message.startsWith("todo")) {
+                } else if (message.startsWith("todo")) {
                     speak("Thou shouldst forge a todo task such as so:");
                     speak("todo [description]");
-                }
-                else if (message.startsWith("deadline")) {
+                } else if (message.startsWith("deadline")) {
                     speak("Thou shouldst forge a deadline task such as so:");
                     speak("deadline [description] /by [time]");
-                }
-                else if (message.startsWith("event")) {
+                } else if (message.startsWith("event")) {
                     speak("Thou shouldst forge an event task such as so:");
                     speak("event [description] /from [start time] /to [end time]");
-                }
-                else {
+                } else {
                     speak("I beg thine pardon, but I am clueless of the meaning of your utterance.");
                 }
                 continue;
             }
 
-            if (command == Command.BYE) break;
-
-            else if (command == Command.LIST) {
+            if (command == Command.BYE) {
+                break;
+            } else if (command == Command.LIST) {
                 if (tasks.isEmpty()) {
                     speak("Your Excellency, thy list remaineth free of tasks at this present moment.");
                 } else {
@@ -98,9 +104,10 @@ public class Knight {
                         i++;
                     }
                 }
-            }
-
-            else if (command == Command.MARK) {
+            } else if (command == Command.SAVE) {
+                FileIO.writeToFile(tasks);
+                speak("Thy list hath been saved to thy scrolls of history.");
+            } else if (command == Command.MARK) {
                 int index = Integer.parseInt(message.substring(5));
                 Task task;
 
@@ -113,9 +120,7 @@ public class Knight {
 
                 task.mark();
                 speak("Well met! This task hath been marked as fulfilled:\n    " + task);
-            }
-
-            else if (command == Command.UNMARK) {
+            } else if (command == Command.UNMARK) {
                 int index = Integer.parseInt(message.substring(7));
                 Task task;
 
@@ -128,9 +133,7 @@ public class Knight {
 
                 task.unmark();
                 speak("Verily, I have marked this task as not yet done:\n    " + task);
-            }
-
-            else if (command == Command.DELETE) {
+            } else if (command == Command.DELETE) {
                 int index = Integer.parseInt(message.substring(7));
                 Task task;
                 try {
@@ -142,22 +145,16 @@ public class Knight {
 
                 tasks.remove(index - 1);
                 speak("Indeed, this task hath been wiped from thy list:\n    " + task);
-            }
-
-            else if (command == Command.TODO) {
+            } else if (command == Command.TODO) {
                 Task task = new ToDo(message.substring(5));
                 tasks.add(task);
                 speak("Understood. This task hath been added to thy list:\n    " + task);
-            }
-
-            else if (command == Command.DEADLINE) {
+            } else if (command == Command.DEADLINE) {
                 String[] params = message.split(" /");
                 Task task = new Deadline(params[0].substring(9), params[1].substring(3));
                 tasks.add(task);
                 speak("Understood. This task hath been added to thy list:\n    " + task);
-            }
-
-            else if (command == Command.EVENT) {
+            } else if (command == Command.EVENT) {
                 String[] params = message.split(" /");
                 Task task = new Event(params[0].substring(6), params[1].substring(5), params[2].substring(3));
                 tasks.add(task);
