9f30eb56be6f7ea1155220bde14c27f686863f05
Miffi <miffi@decadent.rest>
2024-02-01 23:21:24 +0800

    style: use expression lambdas

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -6,18 +6,14 @@ import org.junit.jupiter.api.Test;
 class ParserTest {
     @Test
     public void parse_onlyCommandName_invalidCommandDataThrown() {
-        Parser.InvalidCommandData thrown = Assertions.assertThrows(Parser.InvalidCommandData.class, () -> {
-            Parser.parse("event");
-        });
+        Parser.InvalidCommandData thrown = Assertions.assertThrows(Parser.InvalidCommandData.class, () -> Parser.parse("event"));
 
         Assertions.assertEquals("Parameters to command not given.", thrown.getMessage());
     }
 
     @Test
     public void parse_noDescription_invalidCommandDataThrown() {
-        Parser.InvalidCommandData thrown = Assertions.assertThrows(Parser.InvalidCommandData.class, () -> {
-            Parser.parse("deadline /by 12/12/2023 1200");
-        });
+        Parser.InvalidCommandData thrown = Assertions.assertThrows(Parser.InvalidCommandData.class, () -> Parser.parse("deadline /by 12/12/2023 1200"));
 
         Assertions.assertEquals("No value given to DESCRIPTION", thrown.getMessage());
     }
--- src/test/java/duke/task/TaskTest.java
+++ src/test/java/duke/task/TaskTest.java
@@ -9,9 +9,8 @@ import java.util.List;
 class TaskTest {
     @Test
     public void validateComponentKeys_noDescription_invalidComponentsThrown() {
-        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class, () -> {
-            Task.validateComponentKeys(new HashSet<>(), new HashSet<>());
-        });
+        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class,
+                () -> Task.validateComponentKeys(new HashSet<>(), new HashSet<>()));
 
         Assertions.assertEquals("No description given", e.getMessage());
     }
@@ -19,9 +18,8 @@ class TaskTest {
     @Test
     public void validateComponentKeys_tooManyActual_invalidComponentsThrown() {
         HashSet<String> expected = new HashSet<>(), actual = new HashSet<>(List.of("DESCRIPTION", "/by", "/from"));
-        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class, () -> {
-            Task.validateComponentKeys(expected, actual);
-        });
+        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class,
+                () -> Task.validateComponentKeys(expected, actual));
 
         Assertions.assertEquals("Invalid task components: " + actual + "; expected: " + expected, e.getMessage());
     }
