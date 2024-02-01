91f1da0d5f4829a1b6768764f177df0b7ef699c1
julia-cp <>
2024-02-02 00:09:32 +0800

    automated text UI testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,8 +10,9 @@ public class Duke {
 
         while (!message.equalsIgnoreCase("bye")) {
             if (message.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i] + "\n");
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
             } else if (message.split(" ")[0].equalsIgnoreCase("mark")) {
                 try {
@@ -19,17 +20,17 @@ public class Duke {
                     int n = Integer.parseInt(number);
                     if ((n > 0) && (tasks[n - 1] != null)) {
                         tasks[n - 1].setDone();
-                        System.out.println("Nice! I've marked this task done:\n");
+                        System.out.println("Nice! I've marked this task done:");
                         System.out.println("[" + tasks[n - 1].getStatusIcon() + "] "
                                 + tasks[n - 1].getDescription() + "\n");
                     } else {
                         System.out.println("You don't have that task!\n");
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("You forgot to type which task!\n");
+                    System.out.println("You forgot to type which task!");
                     System.out.println("Type: 'mark n' to mark the n-th task.\n");
                 } catch (NumberFormatException e) {
-                    System.out.println("Type: 'mark n' to mark the n-th task.\n");
+                    System.out.println("Type: 'mark n' to mark the n-th task.");
                     System.out.println("For example type: 'mark 1' to mark the first task.\n");
                 }
             } else if (message.split(" ")[0].equalsIgnoreCase("unmark")) {
@@ -38,17 +39,17 @@ public class Duke {
                     int n = Integer.parseInt(number);
                     if ((n > 0) && (tasks[n - 1] != null)) {
                         tasks[n - 1].setUndone();
-                        System.out.println("OK, I've marked this task as not done yet:\n");
+                        System.out.println("OK, I've marked this task as not done yet:");
                         System.out.println("[" + tasks[n - 1].getStatusIcon() + "] "
                                 + tasks[n - 1].getDescription() + "\n");
                     } else {
                         System.out.println("You don't have that task!\n");
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("You forgot to type which task!\n");
+                    System.out.println("You forgot to type which task!");
                     System.out.println("Type: 'unmark n' to unmark the n-th task.\n");
                 } catch (NumberFormatException e) {
-                    System.out.println("Type: 'unmark n' to unmark the n-th task.\n");
+                    System.out.println("Type: 'unmark n' to unmark the n-th task.");
                     System.out.println("For example, type: 'unmark 1' to mark the first task.\n");
                 }
             } else if (message.split(" ")[0].equalsIgnoreCase("todo")) {
@@ -57,11 +58,11 @@ public class Duke {
                     ToDo todo = new ToDo(task);
                     tasks[count] = todo;
                     count++;
-                    System.out.println("Got it. I've added this task:\n");
-                    System.out.println(todo + "\n");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(todo);
                     System.out.println("Now you have " + count + " tasks in the list.\n");
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("The correct format is:\n");
+                    System.out.println("The correct format is:");
                     System.out.println("todo <description>\n");
                 }
             } else if (message.split(" ")[0].equalsIgnoreCase("deadline")) {
@@ -72,11 +73,11 @@ public class Duke {
                     Deadline deadline = new Deadline(description, by);
                     tasks[count] = deadline;
                     count++;
-                    System.out.println("Got it. I've added this task:\n");
-                    System.out.println(deadline + "\n");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
                     System.out.println("Now you have " + count + " tasks in the list.\n");
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("The correct format is:\n");
+                    System.out.println("The correct format is:");
                     System.out.println("deadline <description> /by <deadline time>\n");
                 }
             } else if (message.split(" ")[0].equalsIgnoreCase("event")) {
@@ -89,11 +90,11 @@ public class Duke {
                     Event event = new Event(description, from, to);
                     tasks[count] = event;
                     count++;
-                    System.out.println("Got it. I've added this task:\n");
-                    System.out.println(event + "\n");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
                     System.out.println("Now you have " + count + " tasks in the list.\n");
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("The correct format is:\n");
+                    System.out.println("The correct format is:");
                     System.out.println("event <description> /from <start time> /to <end time>\n");
                 }
             } else {
