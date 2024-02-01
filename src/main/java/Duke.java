bf00c0d000887856f23ab86851182f5d88df491f
zoebelle-pang <zoebellepang11@gmail.com>
2024-02-02 07:06:31 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,31 @@
 import java.util.Scanner;
 
+class tasks {
+    private String taskDescription;
+    private boolean isDone;
+
+    public tasks(String taskDescription) {
+        this.taskDescription = taskDescription;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + taskDescription;
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] userInputs = new String[100];
+        tasks[] task = new tasks[100];
         int Count = 0;
 
         System.out.println("Hello! I'm Bentley\n" + "What can I do for you?\n");
@@ -11,21 +33,36 @@ public class Duke {
         while (Count < 100) {
             String userInput = scanner.nextLine();
 
-            userInputs[Count] = userInput;
-
-            Count++;
-
             if (userInput.equals("Bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (userInput.equals("List")) {
 
                 for (int i = 0; i < Count; i++) {
-                    System.out.println(userInputs[i]);
+                    System.out.println((i + 1) + ". " + task[i]);
                 }
-            } else {
-                System.out.println("added: " + userInput);
+            } else if (userInput.startsWith("mark")) {
+                System.out.println(" Nice! I've marked this task as done:");
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                task[taskNumber - 1].markAsDone();
 
+                for (int i = 0; i < Count; i++) {
+                    System.out.println((i + 1) + ". " + task[i]);
+                }
+
+            } else if (userInput.startsWith("unmark")) {
+                System.out.println(" OK, I've marked this task as not done yet:");
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                task[taskNumber - 1].markAsUndone();
+
+                for (int i = 0; i < Count; i++) {
+                    System.out.println((i + 1) + ". " + task[i]);
+                }
+
+            } else {
+                task[Count] = new tasks(userInput);
+                System.out.println("added: " + task[Count]);
+                Count++;
             }
         }
 
