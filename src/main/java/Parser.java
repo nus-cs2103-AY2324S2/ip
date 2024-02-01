80f94c8fad61907ddf3425d87a5758c2e3d3aa31
Bowei Jiang <>
2024-02-01 11:23:32 +0800

    Add more OOP, create Ui, TaskList, Command, Parser

---
+++ src/main/java/Parser.java
@@ -0,0 +1,40 @@
+public class Parser {
+    public Command parse(String input) {
+        String[] parts = input.split(" ", 2);
+        String command = parts[0].toLowerCase();;
+        int taskIndex;
+
+        switch (command) {
+            case "bye":
+                return new Command(Command.CommandType.BYE);
+
+            case "list":
+                return new Command(Command.CommandType.LIST);
+
+            case "mark":
+                taskIndex = Integer.parseInt(parts[1]) - 1;
+                return new Command(Command.CommandType.MARK, taskIndex);
+
+            case "unmark":
+                taskIndex = Integer.parseInt(parts[1]) - 1;
+                return new Command(Command.CommandType.UNMARK, taskIndex);
+
+            case "delete":
+                taskIndex = Integer.parseInt(parts[1]) - 1;
+                return new Command(Command.CommandType.DELETE,taskIndex);
+
+            case "todo":
+                return new Command(Command.CommandType.ADD_TODO, parts[1]);
+
+            case "deadline":
+
+                return new Command(Command.CommandType.ADD_DEADLINE, parts[1]);
+
+            case "event":
+                return new Command(Command.CommandType.ADD_EVENT, parts[1]);
+
+            default:
+                return new Command(Command.CommandType.INVALID);
+        }
+    }
+}
