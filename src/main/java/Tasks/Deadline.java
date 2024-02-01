332220e4ac8fcbe8e4dcdecfe6b7c30d9ca89d0f
Lalelulilulela <e0967877@u.nus.edu>
2024-02-01 14:12:39 +0800

    Update Deadline.java to store deadline as Date and Time

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -4,11 +4,17 @@ import Exceptions.InvalidFormatException;
 import Exceptions.InvalidInputException;
 import Exceptions.LeluException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String deadline;
-    public Deadline(String taskName, String deadline) {
+    protected LocalDateTime deadline;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public Deadline(String taskName, String deadline) throws DateTimeParseException {
         super(taskName);
-        this.deadline = deadline;
+        this.deadline = LocalDateTime.parse(deadline, formatter);
+
     }
 
     public static Deadline DeadlineOf(String input) throws InvalidInputException {
@@ -24,11 +30,13 @@ public class Deadline extends Task {
     @Override
     public String saveFormat() {
         int check = this.completed ? 1 : 0;
-        return String.format("D | %d | %s | %s\n", check, this.taskName, this.deadline);
+        return String.format("D | %d | %s | %s\n", check, this.taskName,
+                this.deadline.format(formatter));
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                this.deadline.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mma")));
     }
 }
