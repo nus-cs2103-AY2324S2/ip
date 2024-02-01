35557ddf0af25ad86bc7c3d75bf6dd94d607aa7a
yyccbb <yuchenbo0130@gmail.com>
2024-02-01 09:56:26 +0800

    Add echo functionality. Change personality.

--- src/main/java/Nihao.java
+++ src/main/java/Nihao.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public final class Nihao {
     public static final Nihao instance = new Nihao();
 //    private static final String LOGO =
@@ -14,14 +16,24 @@ public final class Nihao {
         "#+#   #+#+#     #+#     #+#    #+# #+#     #+# #+#    #+# \n" +
         "###    #### ########### ###    ### ###     ###  ########  ";
 
-    private final String GREETINGS = "Hello! I'm Nihao.\nWhat can I do for you?";
-    private final String GOODBYE = "Hope to see you again soon. Goodbye!";
+    private final String GREETINGS = "Hello! I'm Nihao.\nI'm lazy and I don't want to do anything for you.";
+    private final String GOODBYE = "Hope to never see you again. Goodbye!";
     private final PrintHandler printer = PrintHandler.instance;
     private Nihao() {}
 
     public void run() {
         printer.printWithDivider(LOGO);
         printer.printWithDivider(GREETINGS);
-        printer.printWithDivider(GOODBYE);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printer.printWithDivider(GOODBYE);
+                break;
+            }
+            printer.printWithDivider(input);
+        }
+
     }
 }
