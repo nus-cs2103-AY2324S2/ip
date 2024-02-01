dd58980ac960c2794a2230efaf99ec6cd01e4bdc
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-02-01 16:43:24 +0800

    Make minor changes to test files to reflect the change in logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = "\t  __   __    ____  ____  ____  ____ \n"
+        String logo = "\t  __   __    ____  ____  ____  ____\n"
                 + "\t / _\\ (  )  (  __)(  _ \\(  __)(    \\\n"
                 + "\t/    \\/ (_/\\ ) _)  )   / ) _)  ) D (\n"
                 + "\t\\_/\\_/\\____/(__)  (__\\_)(____)(____/\n";
