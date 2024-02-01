f511683c6d9daec9dc0a76706eec2e799c375d03
caitlyntang <caitlyntong6@gmail.com>
2024-02-01 17:48:20 +0800

    Add some compatability with different date formats

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,15 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Deadline extends Task {
     protected LocalDateTime by;
-    
+
     public Deadline (String description, String by) {
         super(description);
-        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
+        this.by = parseDate(by);
     }
 
     public Deadline (String description, boolean done, String by) {
@@ -24,5 +27,20 @@ public class Deadline extends Task {
     public String toString() {
         return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: "
                 + this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")) + ")";
+    }
+
+    private LocalDateTime parseDate(String dateTime) {
+        List<String> dateCombi = Arrays.asList("dd-MM-yyyy ", "MM-dd-yyyy ", "yyyy-dd-MM ", "yyyy-MM-dd ");
+        List<String> timeCombi = Arrays.asList("HH:mm", "HHmm", "hh:mm a");
+
+        for (String d : dateCombi) {
+            for (String t : timeCombi) {
+                try {
+                    return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern(d + t));
+                } catch (DateTimeParseException dt) { }
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,9 @@
-import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Event extends Task {
     protected LocalDateTime from;
@@ -8,14 +11,13 @@ public class Event extends Task {
 
     public Event (String description, String from, String to) throws IllegalArgumentException {
         super(description);
-        this.from = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
+        this.from = parseDate(from);
         this.to = LocalTime.parse(to, DateTimeFormatter.ofPattern("HHmm"));
     }
 
     public Event (String description, boolean done, String from, String to) {
         super(description);
         super.updateIsDone(done);
-        System.out.println(from);
         this.from = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
         this.to = LocalTime.parse(to, DateTimeFormatter.ofPattern("HH:mm"));
     }
@@ -32,5 +34,20 @@ public class Event extends Task {
                 + this.description + " (from: "
                 + this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")) + " to: "
                 + this.to.format(DateTimeFormatter.ofPattern("hh:mm a")) + ")";
+    }
+
+    private LocalDateTime parseDate(String dateTime) {
+        List<String> dateCombi = Arrays.asList("dd-MM-yyyy ", "MM-dd-yyyy ", "yyyy-dd-MM ", "yyyy-MM-dd ");
+        List<String> timeCombi = Arrays.asList("HH:mm", "HHmm", "hh:mm a");
+
+        for (String d : dateCombi) {
+            for (String t : timeCombi) {
+                try {
+                    return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern(d + t));
+                } catch (DateTimeParseException dt) { }
+            }
+        }
+
+        return null;
     }
 }
