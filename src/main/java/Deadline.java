aaebf35aadd53db4ba0d2a38e428b1626687f195
tengcharmaine <e0969596@u.nus.edu>
2024-02-01 13:52:44 +0800

    Update class to be able to read and convert date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,32 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        if (isValidDateFormat(by)) {
+            this.by = by;
+        } else {
+            this.by = convertDate(by);
+        }
+    }
+
+    private boolean isValidDateFormat(String by) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        try {
+            LocalDate date = LocalDate.parse(by, formatter);
+            String formattedDate = date.format(formatter);
+            return formattedDate.equals(by);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String convertDate(String by) {
+        LocalDate date = LocalDate.parse(by);
+        return date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
     }
 
     @Override
