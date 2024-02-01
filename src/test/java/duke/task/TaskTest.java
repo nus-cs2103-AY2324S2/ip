3086a5b33610da438961b3f6a9e18520b389d917
Miffi <miffi@decadent.rest>
2024-02-02 00:01:56 +0800

    Add test cases for valid Task components

--- src/test/java/duke/task/TaskTest.java
+++ src/test/java/duke/task/TaskTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Stream;
 
 class TaskTest {
     @Test
@@ -16,11 +17,46 @@ class TaskTest {
     }
 
     @Test
-    public void validateComponentKeys_tooManyActual_invalidComponentsThrown() {
-        HashSet<String> expected = new HashSet<>(), actual = new HashSet<>(List.of("DESCRIPTION", "/by", "/from"));
-        Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class,
-                () -> Task.validateComponentKeys(expected, actual));
+    public void validateComponentKeys_notMatch_invalidComponentsThrown() {
+        List<HashSet<String>> expecteds = List.of(
+                new HashSet<>(),
+                new HashSet<>(List.of("/by", "/from"))
+        );
 
-        Assertions.assertEquals("Invalid task components: " + actual + "; expected: " + expected, e.getMessage());
+        List<HashSet<String>> actuals = List.of(
+                new HashSet<>(List.of("DESCRIPTION", "/by", "/from")),
+                new HashSet<>(List.of("DESCRIPTION", "/by"))
+        );
+
+        for (int i = 0; i < expecteds.size(); i++) {
+            var expected = expecteds.get(i);
+            var actual = actuals.get(i);
+
+            Task.InvalidComponents e = Assertions.assertThrows(Task.InvalidComponents.class,
+                    () -> Task.validateComponentKeys(expected, actual));
+
+            Assertions.assertEquals("Invalid task components: " + actual + "; expected: " + expected, e.getMessage());
+        }
+    }
+
+    @Test
+    public void validateComponentKeys_edgeCaseInputs_nothingThrown() {
+        List<HashSet<String>> expecteds = List.of(
+                new HashSet<>(),
+                new HashSet<>(List.of("/from", "/to", "/from"))
+        );
+
+        List<HashSet<String>> actuals = List.of(
+                new HashSet<>(List.of("DESCRIPTION")),
+                new HashSet<>(List.of("DESCRIPTION", "/from", "/to"))
+        );
+
+        for (int i = 0; i < expecteds.size(); i++) {
+            var expected = expecteds.get(i);
+            var actual = actuals.get(i);
+
+            Assertions.assertDoesNotThrow(() -> Task.validateComponentKeys(expected, actual));
+        }
+
     }
 }
