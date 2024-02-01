0fbdbfb1ae230ce9e8caf1a1ecd8d812bfe69884
gavin331 <e0957939@u.nus.edu>
2024-02-01 20:58:51 +0800

    Implement save feature

--- src/main/java/BartenderBob.java
+++ src/main/java/BartenderBob.java
@@ -1,8 +1,10 @@
-import java.util.ArrayList;
 public class BartenderBob {
     private static final String NAME = "BartenderBob";
-    private final ArrayList<Task> STORAGE = new ArrayList<>();
+//    private final ArrayList<Task> STORAGE = new ArrayList<>();
+    private final Storage STORAGE;
     public BartenderBob() {
+        STORAGE = new Storage("./data/duke.txt");
+        STORAGE.load();
         System.out.println("Welcome back! I'm " + NAME + "\nHow's it going out there?");
     }
 
@@ -15,24 +17,24 @@ public class BartenderBob {
     }
 
     public void store(Task task) {
-        STORAGE.add(task);
-        int totalTasks = STORAGE.size();
+        STORAGE.save(task);
+        int totalTasks = STORAGE.getSize();
         System.out.println("Got it. I've added this task:");
         System.out.println(task.show());
         System.out.println("Now you have " + totalTasks + " tasks in the list.");
     }
     public void list() {
         System.out.println("Here are the tasks in your list!");
-        for (int i = 0; i < STORAGE.size(); i++) {
+        for (int i = 0; i < STORAGE.getSize(); i++) {
             int number = i + 1;
-            System.out.println(number + "." + STORAGE.get(i).show());
+            System.out.println(number + "." + STORAGE.getTask(i).show());
         }
     }
 
     public void markDone(String index) throws BartenderBobException{
         try {
             int integerIndex = Integer.parseInt(index);
-            Task task = STORAGE.get(integerIndex - 1);
+            Task task = STORAGE.getTask(integerIndex - 1);
             task.mark();
             System.out.println("Nice! I've marked this task as done:");
             System.out.println(task.show());
@@ -44,7 +46,7 @@ public class BartenderBob {
     public void unmarkDone(String index) throws BartenderBobException{
         try {
             int integerIndex = Integer.parseInt(index);
-            Task task = STORAGE.get(integerIndex - 1);
+            Task task = STORAGE.getTask(integerIndex - 1);
             task.unmark();
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println(task.show());
@@ -56,9 +58,9 @@ public class BartenderBob {
     public void delete(String index) throws BartenderBobException{
         try {
             int integerIndex = Integer.parseInt(index);
-            String display = STORAGE.get(integerIndex - 1).show();
-            STORAGE.remove(integerIndex - 1);
-            int totalTasks = STORAGE.size();
+            String display = STORAGE.getTask(integerIndex - 1).show();
+            STORAGE.removeTask(integerIndex - 1);
+            int totalTasks = STORAGE.getSize();
             System.out.println("Noted. I've removed this task:");
             System.out.println(display);
             System.out.println("Now you have " + totalTasks + " tasks in the list.");
