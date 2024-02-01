c6833e13aa394af11f21760efc5ffc60390f37e3
Teoh Tze Tzun <northan616@gmail.com>
2024-02-01 23:43:21 +0800

    fix path issue for JAR file

--- src/main/java/chatbot/Main.java
+++ src/main/java/chatbot/Main.java
@@ -1,25 +1,13 @@
 package chatbot;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import chatbot.cortana.Cortana;
 
 public class Main {
 
     // We will build the project around the location of this class
-
-    private static Path getBaseDir(Class<?> clazz) {
-        String classFilePath = clazz.getName().replace(".", "/") + ".class";
-        String classLocation = clazz.getClassLoader().getResource(classFilePath).getPath();
-
-        Path basePath = Paths.get(classLocation).getParent();
-        return basePath.toAbsolutePath();
-    }
     
     public static void main(String[] args) {
-        Path basePath = getBaseDir(Main.class);
-        Cortana cortana = new Cortana(basePath.toString());
+        Cortana cortana = new Cortana(".");
         cortana.run();
     }
 
