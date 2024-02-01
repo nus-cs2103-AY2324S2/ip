661634b72e9985542f245ba328838244a9dec170
PallonCX <cheexiang03@gmail.com>
2024-02-01 17:09:37 +0800

    Modify Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -7,8 +7,22 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    public Deadline(String description, boolean isDone, String by) {
+        super(description, isDone);
+        this.by = by;
+    }
+
     @Override
     public String getDescriptionStatus() {
         return "[D]" + (isDone ? "[X] " : "[ ] ") + this.description + " (by: " + this.by + ")";
+    }
+
+    @Override
+    public String[] getFields() {
+        String[] result = new String[3];
+        result[0] = this.description;
+        result[1] = this.isDone ? "Y" : "N";
+        result[2] = this.by;
+        return result;
     }
 }
