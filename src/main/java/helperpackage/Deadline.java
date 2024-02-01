c527c2b177f9ec85c6c500c98053f7dd66a57f82
shunjieee <shunjie2000@gmail.com>
2024-02-01 08:58:11 +0800

    Deadline.java: Add DukeException for invalid inputs

--- src/main/java/helperpackage/Deadline.java
+++ src/main/java/helperpackage/Deadline.java
@@ -3,9 +3,15 @@ package helperpackage;
 public class Deadline extends Task {
     protected String deadline;
     
-    public Deadline(String description) {
+    public Deadline(String description) throws DukeException {
         super(description.substring(0, description.indexOf("/")));
-        this.deadline = description.substring(description.indexOf("/")).replace("/by", "by:");
+        String deadline = description.substring(description.indexOf("/")).replace("/by", "by:");
+
+        if (!deadline.contains("by:")) {
+            throw new DukeException("Invalid deadline input. :(");
+        }
+        
+        this.deadline = deadline;
     }
 
     @Override
