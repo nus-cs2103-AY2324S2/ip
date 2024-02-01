bbea510a2b1c891ba68545223056cb6ed4130737
Giselle Tan <giselletan1008@gmail.com>
2024-02-01 18:39:30 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,6 +21,60 @@ public class Duke {
         System.out.println("\tEmis can mark tasks as undone with the command 'unmark (task no)'.");
         System.out.println("\tTo stop talking to Emis, please say 'bye'.");
 
+
+        // extract saved tasks from ./data/emis.txt
+        String saved_Tasks = "./data/emis.txt";
+        try {
+            Scanner sT = new Scanner(new File(saved_Tasks));
+            while (sT.hasNextLine()) {
+                // read from file, add to arraylist al
+                String input = sT.nextLine();
+                String[] data = input.split("\\|");
+                String taskType = data[0].trim();
+                String retrieveStatus = data[1].trim();
+                boolean convertStatus = retrieveStatus.equals("1");
+                String retrieveDescription = data[2].trim();
+
+                if (taskType.equals("T")) {
+                    al.add(new ToDo(convertStatus, retrieveDescription));
+
+                } else if (taskType.equals("D")) {
+                    String finishBy = data[3].trim();
+                    al.add(new Deadline(convertStatus, retrieveDescription, finishBy));
+
+                } else if (taskType.equals("E")) {
+                    String startFrom = data[3].trim();
+                    String endBy = data[4].trim();
+                    al.add(new Event(convertStatus, retrieveDescription, startFrom, endBy));
+
+                } else {
+                    System.out.println("wrong format! no such task type");
+                }
+            } 
+            sT.close();
+
+        } catch (FileNotFoundException FNFe) {
+            // file does not exist yet, so create
+            try {
+                File emisTxt = new File(saved_Tasks);
+                emisTxt.getParentFile().mkdirs();
+                if (emisTxt.createNewFile()) {
+                    System.out.println("File created");
+                } else {
+                    System.out.println("File already exists");
+                }
+            } catch (IOException IOe) {
+                System.out.println("An error occurred while creating the file.");
+            }
+        } 
+        /*
+        catch () {
+            // handle folder-does-not-exist-yet case
+            // handle data file being corrupted (i.e. content not in expected format)
+        }
+        */
+
+        // read user input
         while (sc.hasNextLine()) {
             String user_input = sc.nextLine();
             Integer spaceIndex = user_input.indexOf(" ");
@@ -43,12 +103,15 @@ public class Duke {
                         if (action.equals("mark")) {
                             t.markAsDone();
                             al.set(taskNo - 1, t);
+                            updateTasks(al);
 
                         } else if (action.equals("unmark")) {
                             t.markAsUndone();
                             al.set(taskNo - 1, t);
+                            updateTasks(al);
                         } else if (action.equals("delete")) {
                             deleteTask(al, taskNo - 1);
+                            updateTasks(al);
                         }
                     }
                 } catch (NumberFormatException nfe) {
@@ -63,6 +126,7 @@ public class Duke {
                         if (action.equals("todo")) {
                             ToDo td = new ToDo(d);
                             al.add(td);
+                            updateTasks(al);
                             System.out.println("\t\t" + td.toString());
 
                         } else if (action.equals("deadline")) {
@@ -71,6 +135,7 @@ public class Duke {
                             String by = d.substring(slashIndex + 3);
                             Deadline dl = new Deadline(des, by);
                             al.add(dl);
+                            updateTasks(al);
                             System.out.println("\t\t" + dl.toString());
 
                         } else if (action.equals("event")) {
@@ -81,6 +146,7 @@ public class Duke {
                             String to = d.substring(slashIndex_to + 3);
                             Event eve = new Event(des, from, to);
                             al.add(eve);
+                            updateTasks(al);
                             System.out.println("\t\t" + eve.toString());
 
                         }
@@ -90,6 +156,7 @@ public class Duke {
                 }
             }
         }
+        sc.close();
     }
 
     public static void exit() {
@@ -117,6 +184,23 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
     }
 
+    public static void updateTasks(ArrayList<Task>al) {
+        try {
+            FileWriter fw = new FileWriter("./data/emis.txt");
+            String s = "";
+            for (int i = 0; i < al.size(); i++) {
+                s += al.get(i).storeString();
+                s += "\n";
+            }
+            fw.write(s);
+            fw.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException IOe) {
+            System.out.println("Error occurred while writing to the file.");
+            IOe.printStackTrace();
+        }
+    }
+
     public static class Task {
         protected String description;
         protected boolean isDone;
@@ -124,6 +208,11 @@ public class Duke {
         public Task(String description) {
             this.description = description;
             this.isDone = false;
+        }
+
+        public Task(boolean isDone, String description) {
+            this.description = description;
+            this.isDone = isDone;
         }
     
         public String getStatusIcon() {
@@ -145,6 +234,12 @@ public class Duke {
             System.out.println("\t\t[" + this.getStatusIcon() + "] " + this.description);
             System.out.println("\t____________________________________________________________");
         }
+
+        public String storeString() {
+            // done = 1, not done = 0
+            int status = isDone ? 1 : 0;
+            return status + " | " + this.description;
+        }
     
         @Override
         public String toString() {
@@ -152,6 +247,7 @@ public class Duke {
         }
     }
 
+    // Deadline class
     public static class Deadline extends Task {
         protected String by;
     
@@ -159,16 +255,36 @@ public class Duke {
             super(description);
             this.by = by;
         }
+
+        public Deadline(boolean isDone, String description, String by) {
+            super(isDone, description);
+            this.by = by;
+        }
+
+        @Override
+        public String storeString() {
+            return "D | " + super.storeString() + " | " + this.by;
+        }
     
         @Override
         public String toString() {
-            return"[D]" + super.toString() + " (by: " + by + ")";
+            return"[D]" + super.toString() + " (by: " + this.by + ")";
         }
     }
 
+    // ToDo class
     public static class ToDo extends Task {
         public ToDo(String description) {
             super(description);
+        }
+
+        public ToDo(boolean isDone, String description) {
+            super(isDone, description);
+        }
+
+        @Override
+        public String storeString() {
+            return "T | " + super.storeString();
         }
     
         @Override
@@ -185,6 +301,17 @@ public class Duke {
             super(description);
             this.from = from;
             this.to = to;
+        }
+
+        public Event(boolean isDone, String description, String from, String to) {
+            super(isDone, description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String storeString() {
+            return "E | " + super.storeString() + " | " + this.from + " | " + this.to;
         }
     
         @Override
