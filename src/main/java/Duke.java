221e258b0b8f75b76caa210945dc2da3330cd7c5
LimZiJia <zijialim@gmail.com>
2024-02-01 13:05:40 +0800

    Add classes
    First try at it. Have not tested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,21 @@
 public class Duke {
+    private Storage storage;
+    private TaskList tasks = new TaskList();
+    private Ui ui;
+
+    public Duke(String filePath, String fileName) {
+        ui = new Ui();
+        storage = new Storage(filePath, fileName);
+    }
+
+    public void run() {
+        ui.greet();
+        tasks = storage.load();
+        ui.chat(tasks, storage);
+        ui.bye();
+    }
+
     public static void main(String[] args) {
-        Skibidi bot = new Skibidi();
-        bot.greet();
-        bot.load();
-        bot.chat();
-        bot.bye();
+        new Duke("/data/", "duke.txt").run();
     }
 }
