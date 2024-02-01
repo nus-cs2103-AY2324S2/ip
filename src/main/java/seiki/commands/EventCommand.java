19a45c7ad60e2aa24eac5d8534ba63fe6f9f2878
tsulim <limxiujia18@gmail.com>
2024-02-02 06:26:59 +0800

    Refactor code to comply with the Checkstyle rules

--- src/main/java/seiki/commands/EventCommand.java
+++ src/main/java/seiki/commands/EventCommand.java
@@ -1,16 +1,16 @@
 package seiki.commands;
 
-import seiki.data.TaskList;
-import seiki.data.exception.SeikiException;
-import seiki.data.task.Event;
-import seiki.storage.Storage;
-import seiki.ui.Ui;
+import static seiki.common.DateTime.DATE_TIME_FORMATTER;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
-import static seiki.common.DateTime.DATE_TIME_FORMATTER;
+import seiki.data.TaskList;
+import seiki.data.exception.SeikiException;
+import seiki.data.task.Event;
+import seiki.storage.Storage;
+import seiki.ui.Ui;
 
 public class EventCommand extends Command {
     public static final String COMMAND_WORD = "event";
--- src/main/java/seiki/ui/Ui.java
+++ src/main/java/seiki/ui/Ui.java
@@ -1,13 +1,22 @@
 package seiki.ui;
 
-import seiki.data.TaskList;
-import seiki.data.task.Task;
+import static seiki.common.Messages.MESSAGE_ADD_SUCCESS;
+import static seiki.common.Messages.MESSAGE_DELETE_SUCCESS;
+import static seiki.common.Messages.MESSAGE_FAREWELL;
+import static seiki.common.Messages.MESSAGE_GREETING;
+import static seiki.common.Messages.MESSAGE_LOGO;
+import static seiki.common.Messages.MESSAGE_MARK_SUCCESS;
+import static seiki.common.Messages.MESSAGE_REMAINING_TASKS;
+import static seiki.common.Messages.MESSAGE_TASK;
+import static seiki.common.Messages.MESSAGE_UNMARK_SUCCESS;
 
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-import static seiki.common.Messages.*;
+import seiki.data.TaskList;
+import seiki.data.task.Task;
+
 
 public class Ui {
 
