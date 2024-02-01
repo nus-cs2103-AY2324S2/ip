c9774a0c3dc1aaeb379a5dd4a2372fe0f1ee283f
chuahjiajie <99877894+Nauseous-Spartan@users.noreply.github.com>
2024-02-01 16:34:07 +0800

    Level-1 Added echo

--- src/main/java/OedipusRex.java
+++ src/main/java/OedipusRex.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
 public class OedipusRex {
     public static void main(String[] args) {
         String line = "------------------------------------------\n";
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println(line +
                 "Hello! I'm OedipusRex\n" +
                 "What can I do for you?" + "\n" +
-                line +
-                "Bye. Hope to see you again soon!" + "\n" +
                 line);
+
+        while(true) {
+            String command = input.nextLine();
+            if(command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!" + "\n" +
+                        line);
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
