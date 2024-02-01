b1f4b7bc3b0ba102bfb3625eb560778bc8b444fc
tankh99 <khanghou99@gmail.com>
2024-02-01 11:27:38 +0800

    chore: add tests for utils files

---
+++ src/test/java/Utils/DateUtilsTest.java
@@ -0,0 +1,64 @@
+package Utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.MonthDay;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DateUtilsTest {
+    @Test
+    public void parseDateString_correctDateFormat_dateReturned() {
+        String dateString = "9/10/1999 1600";
+        LocalDateTime actual = DateUtils.parseDate(dateString);
+        LocalDateTime expected = LocalDateTime.of(1999, 10, 9, 16, 0, 0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void parseDateString_incorrectDateFormat_dateReturned() {
+        // Invalid day
+        assertThrows(IllegalArgumentException.class, () -> {
+            String dateString = "32/10/1999 1600";
+            LocalDateTime actual = DateUtils.parseDate(dateString);
+        });
+
+        // Invalid month
+        assertThrows(IllegalArgumentException.class, () -> {
+            String dateString = "1/13/1999 1600";
+            LocalDateTime actual = DateUtils.parseDate(dateString);
+        });
+
+        // Invalid year
+        assertThrows(IllegalArgumentException.class, () -> {
+            String dateString = "1/13/199 1600";
+            LocalDateTime actual = DateUtils.parseDate(dateString);
+        });
+    }
+
+    @Test
+    public void parseDateString_missingValues_dateReturned() {
+        // No time
+        assertThrows(IllegalArgumentException.class, () -> {
+            String dateString = "32/10/1999";
+            LocalDateTime actual = DateUtils.parseDate(dateString);
+        });
+
+        // No month
+        assertThrows(IllegalArgumentException.class, () -> {
+            String dateString = "1/1999 1600";
+            LocalDateTime actual = DateUtils.parseDate(dateString);
+        });
+    }
+
+    @Test()
+    public void formatDate_dateStringReturned() {
+        LocalDateTime input = LocalDateTime.of(1999, 10, 9, 16, 0, 0);
+        String actual = DateUtils.formatDate(input);
+        String expected = "9/10/1999, 4PM";
+        assertEquals(actual, expected);
+    }
+}
---
+++ src/test/java/Utils/FileUtilsTest.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileUtilsTest {
+    public static final String BASE_DIR = System.getProperty("user.dir") + "/src/test/java/Database";
+
+    public FileUtilsTest() {
+
+    }
+    @Test
+    public void readFile_fileExists_linesReturned() {
+        Path filePath = Path.of(BASE_DIR + "/todo.txt");
+        List<String> actual = FileUtils.read(filePath);
+        List<String> expected = Arrays.asList("0 | 1 | I'm a todo actually, you know?", "1 | 1 | Yeah what he said, I'm one too!");
+        assertEquals(actual, expected);
+    }
+
+}
