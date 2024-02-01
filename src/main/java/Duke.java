23f01516a8b10fdd3bea27a4cd43e823cb7d5005
zoebelle-pang <zoebellepang11@gmail.com>
2024-02-02 05:57:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,28 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] userInputs = new String[100];
+        int Count = 0;
 
         System.out.println("Hello! I'm Bentley\n" + "What can I do for you?\n");
 
-        while (true) {
+        while (Count < 100) {
             String userInput = scanner.nextLine();
 
-            if (userInput.equals("bye")) {
+            userInputs[Count] = userInput;
+
+            Count++;
+
+            if (userInput.equals("Bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("List")) {
 
+                for (int i = 0; i < Count; i++) {
+                    System.out.println(userInputs[i]);
+                }
             } else {
-                System.out.println(userInput);
-                System.out.println();
+                System.out.println("added: " + userInput);
 
             }
         }
