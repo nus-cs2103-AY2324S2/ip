92e5ed2affbfb707b198269e7aef59de2897b1c8
bennyLCK <e0969875@u.nus.edu>
2024-02-02 00:16:46 +0800

    Implement Date and Time recognition and representation

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,16 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class EventTask extends Task {
 
-  private String start;
-  private String end;
+  private LocalDate startDate;
+  private String startTiming;
+  private LocalDate endDate;
+  private String endTiming;
 
   public EventTask(String name, String start, String end) {
     super(name);
-    this.start = start;
-    this.end = end;
+
+    String[] startDateTime = start.split(" ");
+    this.startDate = LocalDate.parse(startDateTime[0]);
+    this.startTiming = startDateTime[1];
+    String[] endDateTime = end.split(" ");
+    this.endDate = LocalDate.parse(endDateTime[0]);
+    this.endTiming = endDateTime[1];
   }
 
   @Override
   public String toString() {
-    return String.format("[E]%s (from: %s to: %s)", super.toString(), this.start, this.end);
+    return String.format("[E]%s (from: %s %s to: %s %s)", super.toString(),
+        this.startDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")), this.startTiming,
+        this.endDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")), this.endTiming);
   }
 }
