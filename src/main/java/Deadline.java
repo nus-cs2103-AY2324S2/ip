7139f73e1207ff7d3e9d4fa42802659f768a207a
DingBao-sys <dinghaosim@gmail.com>
2024-02-02 00:29:57 +0800

    Complete Level 7, Add Save

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,6 +5,17 @@ public class Deadline extends Task {
         this.deadlineDate = deadline;
     }
 
+    public Deadline(String status, String descr, String deadline) {
+        super(descr);
+        this.deadlineDate = deadline;
+        super.setStatus(status);
+    }
+
+
+    @Override
+    public String writeObject() {
+        return String.format("deadline %s | %s \n", super.writeObject(), this.deadlineDate);
+    }
     @Override
     public String toString() {
         return String.format("[D]%s(by: %s)", super.toString(), this.deadlineDate);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,15 @@
-public class Task {
+import java.io.Serializable;
+
+public abstract class Task implements Serializable {
     private final static String hRULER = "____________________________________________________________\n";
     private final String description;
     private String status = "[ ]";
+    public String writeObject() {
+        return String.format("| %s | %s", this.status, this.description);
+    }
+    public void setStatus(String s) {
+        this.status = s;
+    }
     public Task(String token) {
         this.description = token;
     }
@@ -10,7 +18,6 @@ public class Task {
         System.out.printf("%s Nice! I've marked this task as done:\n   %s\n%s",
                 hRULER, this.toString(), hRULER);
     }
-
     public void unMarkDone() {
         this.status = "[ ]";
         System.out.printf("%s OK, I've marked this task as not done yet:\n   %s\n%s",
