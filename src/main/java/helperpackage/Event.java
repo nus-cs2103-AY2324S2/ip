8f063a8b28eb193d00efc63c53dab9ef7523c648
shunjieee <shunjie2000@gmail.com>
2024-02-01 08:59:03 +0800

    Event.java: Add DukeException for invalid inputs

--- src/main/java/helperpackage/Event.java
+++ src/main/java/helperpackage/Event.java
@@ -3,10 +3,16 @@ package helperpackage;
 public class Event extends Task {
     protected String startAndEndTime;
     
-    public Event(String description) {
+    public Event(String description) throws DukeException {
         super(description.substring(0, description.indexOf("/")));
-        this.startAndEndTime = description.substring(description.indexOf("/")).replace("/from", 
+        String time = description.substring(description.indexOf("/")).replace("/from", 
                 "from:").replace("/to", "to:");
+
+        if (!(time.contains("from:") || time.contains("to:"))) {
+            throw new DukeException("Invalid event input. :(");
+        }
+        
+        this.startAndEndTime = time;
     }
 
     @Override
