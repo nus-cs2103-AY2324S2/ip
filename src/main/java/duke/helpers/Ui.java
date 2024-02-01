2d7f0ee0eb8a5f65e460593d32f7bcde6b28e71e
chiageng <chiageng12@gmail.com>
2024-02-01 17:51:48 +0800

    Update code to follow coding standard.

--- src/main/java/duke/helpers/Ui.java
+++ src/main/java/duke/helpers/Ui.java
@@ -39,15 +39,24 @@ public class Ui {
     public void printCommandList() {
         drawLine();
         System.out.println("These are the available commands :");
-        System.out.println("0. Do " + CommandType.LISTCOMMANDS.toString() + " : " + CommandType.LISTCOMMANDS.getCommand());
-        System.out.println("1. Add " + CommandType.TODO.toString() + " task : " + CommandType.TODO.getCommand());
-        System.out.println("2. Add " + CommandType.DEADLINE.toString() + " task : " + CommandType.DEADLINE.getCommand());
-        System.out.println("3. Add " + CommandType.EVENT.toString() + " task : " + CommandType.EVENT.getCommand());
-        System.out.println("4. To " + CommandType.LIST.toString() +" all tasks : " + CommandType.LIST.getCommand());
-        System.out.println("5. To " + CommandType.MARK.toString() + " task done : " + CommandType.MARK.getCommand());
-        System.out.println("6. To " + CommandType.UNMARK.toString() + " task : " + CommandType.UNMARK.getCommand());
-        System.out.println("7. To " + CommandType.DELETE.toString() + " a task : " + CommandType.DELETE.getCommand());
-        System.out.println("8. To tasks related to a date : " + CommandType.CHECKDATE.getCommand());
+        System.out.println("0. Do " + CommandType.LISTCOMMANDS.toString() + " : "
+                + CommandType.LISTCOMMANDS.getCommand());
+        System.out.println("1. Add " + CommandType.TODO.toString() + " task : "
+                + CommandType.TODO.getCommand());
+        System.out.println("2. Add " + CommandType.DEADLINE.toString() + " task : "
+                + CommandType.DEADLINE.getCommand());
+        System.out.println("3. Add " + CommandType.EVENT.toString() + " task : "
+                + CommandType.EVENT.getCommand());
+        System.out.println("4. To " + CommandType.LIST.toString() +" all tasks : "
+                + CommandType.LIST.getCommand());
+        System.out.println("5. To " + CommandType.MARK.toString() + " task done : "
+                + CommandType.MARK.getCommand());
+        System.out.println("6. To " + CommandType.UNMARK.toString() + " task : "
+                + CommandType.UNMARK.getCommand());
+        System.out.println("7. To " + CommandType.DELETE.toString() + " a task : "
+                + CommandType.DELETE.getCommand());
+        System.out.println("8. To tasks related to a date : "
+                + CommandType.CHECKDATE.getCommand());
         System.out.println("9. Close chatbot : " + CommandType.BYE.getCommand());
         drawLine();
     }
