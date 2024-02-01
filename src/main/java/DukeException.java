fd8838f7385479a6a055a70ce1d61fba59d6eea3
PallonCX <cheexiang03@gmail.com>
2024-02-02 00:12:46 +0800

    Modify DukeException.java

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -24,12 +24,13 @@ public class DukeException extends Exception{
                         + "E.g. todo say hi to neighbour";
             }
             case "deadline": {
-                return "Syntax of deadline: deadline {task description} /by {when}\n"
-                        + "E.g. deadline breakfast /by 10am";
+                return "Syntax of deadline: deadline {task description} /by ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd})\n"
+                        + "E.g. deadline breakfast /by 2022-12-31 15:00";
             }
             case "event": {
-                return "Syntax of event: deadline {task description} /from {when start} /to {when end}\n"
-                        + "E.g. event exam /from 5pm /to 7pm";
+                return "Syntax of event: deadline {task description} /from ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd}) "
+                        + "/to ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd})\n"
+                        + "E.g. event exam /from 2022-12-31 15:00 /to 17:00";
             }
             default: {
                 return "Syntax error, unknown command.";
