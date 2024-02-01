d0623a56b303ff2ef0fa30051a2cff9789a0ce97
minghowC <e0725080@u.nus.edu>
2024-02-02 07:50:46 +0800

    My Level-2 submission

--- src/main/java/IreneAI.java
+++ src/main/java/IreneAI.java
@@ -1,11 +1,18 @@
+/**
+ * The IreneAI program implements a simple chat application that
+ * provides simple feedbacks to the user's inputs
+ *
+ * @author Chai Ming How
+ * @since 2024-02-01
+ */
+
 import java.util.Scanner;
 
-/**
- * The IreneAI Class represents a simple chatbot.
- * For the starter, it greets the user and say goodbye before exiting
- */
 public class IreneAI {
     public static void main(String[] args){
+        final int MAX_TASKS = 100;
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
 
         String chatbotName = "IreneAI";
@@ -22,16 +29,26 @@ public class IreneAI {
             dividingLine(line);
 
             // Terminating condition
-            if (userInput.equalsIgnoreCase("bye")){
+            if (userInput.equalsIgnoreCase("bye")) {
+                // Print the farewell message
+                System.out.println(" Bye. Hope to see you again soon!");
+                dividingLine(line);
                 break;
             }
-            System.out.println("IreneAI: I heard you said '" + userInput + "'.");
-            dividingLine(line);
-        }
 
-        // Print the farewell message
-        System.out.println(" Bye. Hope to see you again soon!");
-        dividingLine(line);
+            if (userInput.equalsIgnoreCase("list")) {
+                dividingLine(line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ": " + tasks[i]);
+                }
+                dividingLine(line);
+            } else {
+                tasks[taskCount++] = userInput;
+                dividingLine(line);
+                System.out.println("added: " + userInput);
+                dividingLine(line);
+            }
+        }
     }
 
     public static void dividingLine(String line){
