91b5877566488462232cfb3c3938eb9093e4087a
caitlyntang <caitlyntong6@gmail.com>
2024-02-01 17:13:25 +0800

    Store dates using java.time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
+    
     public Deadline (String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
     }
 
     public Deadline (String description, boolean done, String by) {
         super(description);
         super.updateIsDone(done);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     @Override
     public String getSaveTask() {
-        return "D | " + super.getSaveTask() + " | " + by;
+        return "D | " + super.getSaveTask() + " | " + by.toString().replace("T", " ");
     }
 
     @Override
     public String toString() {
-        return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: " + this.by + ")";
+        return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: "
+                + this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,27 +1,36 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
-    public Event (String description, String from, String to) {
+    protected LocalDateTime from;
+    protected LocalTime to;
+
+    public Event (String description, String from, String to) throws IllegalArgumentException {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
+        this.to = LocalTime.parse(to, DateTimeFormatter.ofPattern("HHmm"));
     }
 
     public Event (String description, boolean done, String from, String to) {
         super(description);
         super.updateIsDone(done);
-        this.from = from;
-        this.to = to;
+        System.out.println(from);
+        this.from = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.to = LocalTime.parse(to, DateTimeFormatter.ofPattern("HH:mm"));
     }
 
     @Override
     public String getSaveTask() {
-        return "E | " + super.getSaveTask() + " | " + from + "-" + to;
+        return "E | " + super.getSaveTask() + " | "
+                + from.toString().replace("T", " ") + " | " + to;
     }
 
     @Override
     public String toString() {
         return "[E][" + this.getStatusIcon() + "] "
-                + this.description + " (from: " + this.from + " to: " + this.to + ")";
+                + this.description + " (from: "
+                + this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")) + " to: "
+                + this.to.format(DateTimeFormatter.ofPattern("hh:mm a")) + ")";
     }
 }
