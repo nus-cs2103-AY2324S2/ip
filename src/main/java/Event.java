15fb6058a80a56ee2c2b897d0902179e4567732a
yisiox <yisiong.y22@gmail.com>
2024-02-01 19:38:54 +0800

    Add support for date and time

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,36 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    private static final DateTimeFormatter DATETIME_FORMAT = DateTimeFormatter
+            .ofPattern("dd/MM/yyyy HHmm");
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public Event(String status, String description,
+                 LocalDateTime from, LocalDateTime to) {
+        super(status, description);
         this.from = from;
         this.to = to;
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)",
-                super.toString(), from, to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+                from.format(DATETIME_FORMAT), to.format(DATETIME_FORMAT));
     }
 
     @Override
     public String toStorageString() {
-        return String.format("E,%s,%s,%s", description, from, to);
+        return String.format("E,%s,%s,%s,%s",
+                super.getStatusIcon(), description,
+                from.format(DATETIME_FORMAT), to.format(DATETIME_FORMAT));
     }
 }
