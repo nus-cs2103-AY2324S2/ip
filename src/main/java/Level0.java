b19d5622a664a2ae988c30e5c5201f34eec07ec3
beefsausagee <alvislow07@gmail.com>
2024-02-01 21:47:17 +0800

    Implement changes for Level-0

--- src/main/java/Level0.java
+++ src/main/java/Level0.java
@@ -1,8 +1,9 @@
-public class Duke {
+public class Level0 {
+
     public static void main(String[] args) {
-        String logo = "Hello! I'm Yube \n"
+        String output = "Hello! I'm Yube \n"
                 + "What can I do for you? \n" +
                 "Bye. Hope to see you again soon!";
-        System.out.println(logo);
+        System.out.println(output);
     }
 }
