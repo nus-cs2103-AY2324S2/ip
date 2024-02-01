9e7052d7049d7f6c5c6c6ad1068bbfe544018b63
appleraincoat <james20111951@gmail.com>
2024-02-01 21:33:59 +0800

    Remove CommandTest for now, add DateTimeParserTest class and DateTimeParserStub to help in testing.

--- src/main/java/Parsing/DateTimeParserStub.java
+++ src/main/java/Parsing/DateTimeParserStub.java
@@ -1,18 +1,15 @@
 package Parsing;
 
-import Exceptions.YpxmmException;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class DateTimeParser {
+public class DateTimeParserStub {
 
     protected DateTimeFormatter formatter;
-    public DateTimeParser() {
+    public DateTimeParserStub() {
         this.formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
     }
     public LocalDateTime parseDateTime(String timeString) {
-        LocalDateTime parsedDateTime = LocalDateTime.parse(timeString, this.formatter);
-        return parsedDateTime;
+        return LocalDateTime.parse(timeString, this.formatter);
     }
 }
