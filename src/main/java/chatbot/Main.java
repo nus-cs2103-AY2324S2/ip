7f1540f6bca278933713781bfd6fc405fdd83d42
Teoh Tze Tzun <northan616@gmail.com>
2024-02-01 23:58:20 +0800

    fix coding standards

--- src/main/java/chatbot/Main.java
+++ src/main/java/chatbot/Main.java
@@ -3,8 +3,6 @@ package chatbot;
 import chatbot.cortana.Cortana;
 
 public class Main {
-
-    // We will build the project around the location of this class
     
     public static void main(String[] args) {
         Cortana cortana = new Cortana(".");
