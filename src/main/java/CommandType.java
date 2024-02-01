4d517dffaaf53c96fc1d60848fe46394644c3d10
Eu Jin <an.eu.jin@u.nus.edu>
2024-02-01 16:05:47 +0800

    Implement enum for task types

---
+++ src/main/java/CommandType.java
@@ -0,0 +1,12 @@
+public enum CommandType {
+    BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, INVALID;
+
+    public static CommandType fromString(String commandString) {
+        try {
+            return CommandType.valueOf(commandString.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return INVALID;
+        }
+    }
+}
+
