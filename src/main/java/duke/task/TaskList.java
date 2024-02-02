f4114a13e14c3304e8726711ee80ffa06c855b79
taeewonnn <htw10192000@gmail.com>
2024-02-02 14:44:24 +0800

    Update coding convention
    Name of the variable tasks is not updated in TaskList find method, resolve the conflict

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -14,6 +14,11 @@ public class TaskList {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds task to the list.
+     *
+     * @param task
+     */
     public void addTask(Task task) {
         tasks.add(task);
         System.out.println("Got it. I've added this task:\n"
@@ -22,10 +27,18 @@ public class TaskList {
                 + " tasks in the list.");
     }
 
+    /**
+     * Get size of the list.
+     *
+     * @return List size.
+     */
     public int getSize() {
         return tasks.size();
     }
 
+    /**
+     * Lists down tasks in order when command is called.
+     */
     public void listDownTask() {
         int size = getSize();
         System.out.println("Here are the tasks in your list:");
@@ -34,6 +47,11 @@ public class TaskList {
         }
     }
 
+    /**
+     * Deletes the task at that index from the list.
+     *
+     * @param index
+     */
     public void deleteTask(int index) {
         Task removed = getTask(index - 1);
         System.out.println("Noted. I've removed this task:\n" + removed);
@@ -41,21 +59,36 @@ public class TaskList {
         System.out.println("Now you have " + getSize() + " tasks in the list");
     }
 
+    /**
+     * Change the specific task at index as done.
+     *
+     * @param index
+     */
     public void markTask(int index) {
         Task modTask = tasks.get(index - 1);
         modTask.setIsDone(true);
         System.out.println("Nice! I've marked this task as done:\n" + modTask);
     }
 
+    /**
+     * Change the specific task at index as not done.
+     *
+     * @param index
+     */
     public void unmarkTask(int index) {
         Task modTask = tasks.get(index - 1);
         modTask.setIsDone(false);
         System.out.println("OK, I've marked this task as not done yet:\n" + modTask);
     }
 
+    /**
+     * Find the task with the keyword given and list it in order.
+     * 
+     * @param keyword
+     */
     public void findTask(String keyword) {
         List<Task> temp = new ArrayList<>();
-        for (Task task : listOfTasks) {
+        for (Task task : tasks) {
             if (task.getDescription().contains(keyword)) {
                 temp.add(task);
             }
