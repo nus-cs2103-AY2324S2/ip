b0dbf2f0cac85f9c3662f39e9066d1f18f8661fb
ivan24004 <ivan@ivant.com>
2024-02-02 14:21:03 +0800

    Add test cases for CommandList.runKeyword

--- src/test/java/remi/model/commands/CommandListTest.java
+++ src/test/java/remi/model/commands/CommandListTest.java
@@ -1,4 +1,29 @@
 package remi.model.commands;
 
+import org.junit.jupiter.api.Test;
+import remi.io.Message;
+import remi.model.TaskList;
+import remi.model.Ui;
+import remi.utils.RemiError;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CommandListTest {
+    @Test
+    public void addTodo_NoError() {
+        Ui dummyChatBot = new Ui();
+        TaskList dummyTaskList = new TaskList();
+        CommandList commandList = new CommandList(dummyTaskList, dummyChatBot);
+
+        assertDoesNotThrow(() -> commandList.runKeyword("todo", "stuff"));
+    }
+
+    @Test
+    public void nonsenseCommand_ShouldThrowError() {
+        Ui dummyChatBot = new Ui();
+        TaskList dummyTaskList = new TaskList();
+        CommandList commandList = new CommandList(dummyTaskList, dummyChatBot);
+
+        assertThrows(RemiError.class, () -> commandList.runKeyword("nonsense", ""));
+    }
 }
