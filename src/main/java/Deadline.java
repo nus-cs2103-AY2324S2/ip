c1eda41af76481095d297e4278b25215de63ef85
Shaun Lee <shaunlxw@gmail.com>
2024-02-02 21:41:39 +0800

    Change to use LocalDateTime instead of String for date/time
    * include simple exception handling for datetime parsing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,30 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    private String by;
+    private LocalDateTime by;
+    private static DateTimeFormatter ioFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    private static DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("dd LLL yyyy HHmm");
 
-    public Deadline(String description, String by, boolean isDone) {
+    public Deadline(String description, String by, boolean isDone) throws DateTimeParseException {
         super(description, isDone);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, ioFormatter);
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.by);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                this.by.format(displayFormatter));
     }
 
     @Override
     public String formatData() {
-        return "D" + " | " + this.formatTask() + " | " + this.by;
+        return "D" + " | " + this.formatTask() + " | " + this.by.format(ioFormatter);
+    }
+
+    public static void setDisplayFormatter(String pattern) {
+        Deadline.displayFormatter = DateTimeFormatter.ofPattern(pattern);
     }
 }
