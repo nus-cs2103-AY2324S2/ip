bf3e19aef023e188880e2b8d370a36cebfe60c99
LIU JIAYAO <e1092092@u.nus.edu>
2024-02-02 23:33:26 +0800

    Create ToDoTest class to test ToDo class There are three tests that individually test different methods in the ToDo class.

---
+++ src/test/java/jiayou/task/ToDoTest.java
@@ -0,0 +1,29 @@
+package jiayou.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ToDoTest {
+    @Test
+    public void testToString() {
+        ToDo todo = new ToDo("Complete homework");
+        String expected = "[T][ ] Complete homework";
+        assertEquals(expected, todo.toString());
+    }
+
+    @Test
+    public void testToStringForStore() {
+        ToDo todo = new ToDo("Read a book");
+        String expected = "T | 0 | Read a book";
+        assertEquals(expected, todo.toStringForStore());
+    }
+
+    @Test
+    public void testToDoDescription() {
+        String description = "Go for a run";
+        ToDo todo = new ToDo(description);
+        assertEquals(description, todo.getDescription());
+    }
+}
