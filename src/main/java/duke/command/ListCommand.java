483a5c266681cdfcde533a7bd26fb3caff087d2e
sindhurajain <sindhura394@gmail.com>
2024-02-02 16:28:18 +0800

    Write Javadoc comments fr EventCommand, ExitCommand and ListCommand

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -2,7 +2,19 @@ package duke.command;
 import duke.main.Storage;
 import duke.main.TaskList;
 import duke.main.Ui;
+
+/**
+ * Represents a command to list the tasks in the task list.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Executes the ListCommand by listing the tasks in the task list using Ui.
+     *
+     * @param tasks   TaskList that contains the task list.
+     * @param ui      Ui that deals with user interactions.
+     * @param storage Storage used to load and save tasks.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.displayList(tasks.getTasks());
