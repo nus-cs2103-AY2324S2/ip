097eb3165d3357edeeedbf9d22f7bdccb8ee7071
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2024-02-02 16:39:16 +0800

    Add list retrieving on startup and saving after changes made

--- src/main/java/Tam.java
+++ src/main/java/Tam.java
@@ -1,12 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Tam {
-    static String dividerText = "____________________________________________________________\n";
-    static Scanner scannerObj = new Scanner(System.in);
 
-    static ArrayList<Task>  taskList = new ArrayList<>();
-    static int numTasks = 0;
+public class Tam {
+    private static String dividerText = "____________________________________________________________\n";
+    private static Scanner scannerObj = new Scanner(System.in);
+
+    private static ArrayList<Task>  taskList = new ArrayList<>();
+    private static int numTasks = 0;
     public static void main(String[] args) {
+        taskList = FileRetriever.getSavedTasks();
+        numTasks = taskList.size();
         Tam.greet();
         int status = readCommand();
         while (status == 1) {
@@ -33,160 +40,159 @@ public class Tam {
         String[] splitInput = input.split(" ");
         String command = splitInput[0].toLowerCase();
         System.out.print(dividerText);
-        switch (command) {
-            case "bye":
-            case "exit":
-                return 0;
+        try {
+            switch (command) {
+                case "bye":
+                case "exit":
+                    return 0;
 
-            case "list":
-                Tam.listTasks();
-                return 1;
+                case "list":
+                    Tam.listTasks();
+                    return 1;
 
-            case "mark":
-            case "unmark":
-                try {
-                    int taskNum = Integer.parseInt(splitInput[1]);
-                    if (command.equals("mark")) {
-                        Tam.markTaskDone(taskNum);
-                    } else {
-                        Tam.markTaskUndone(taskNum);
+                case "mark":
+                case "unmark":
+                    try {
+                        int taskNum = Integer.parseInt(splitInput[1]);
+                        if (command.equals("mark")) {
+                            Tam.markTaskDone(taskNum);
+                        } else {
+                            Tam.markTaskUndone(taskNum);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.print("Invalid task number entered\n");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.print("Task does not exist! Type 'list' to check task numbers\n");
                     }
-                }
-                catch (NumberFormatException e) {
-                    System.out.print("Invalid task number entered\n");
-                }
-                catch (IndexOutOfBoundsException e) {
-                    System.out.print("Task does not exist! Type 'list' to check task numbers\n");
-                }
-                return 1;
+                    FileRetriever.saveTaskList(taskList);
+                    return 1;
 
-            case "delete":
-                try {
-                    int taskNum = Integer.parseInt(splitInput[1]);
-                    Tam.deleteTask(taskNum);
-                }
-                catch (NumberFormatException e) {
-                    System.out.print("Invalid task number entered\n");
-                    System.out.print(dividerText);
-                }
-                catch (IndexOutOfBoundsException e) {
-                    System.out.print("Task does not exist! Type 'list' to check task numbers\n");
-                    System.out.print(dividerText);
-                }
-                return 1;
+                case "delete":
+                    try {
+                        int taskNum = Integer.parseInt(splitInput[1]);
+                        Tam.deleteTask(taskNum);
+                    } catch (NumberFormatException e) {
+                        System.out.print("Invalid task number entered\n");
+                        System.out.print(dividerText);
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.print("Task does not exist! Type 'list' to check task numbers\n");
+                        System.out.print(dividerText);
+                    }
+                    FileRetriever.saveTaskList(taskList);
+                    return 1;
 
-            case "todo":
-                String todoDescription = input.replace("todo", "").trim();
-                if (todoDescription.equals("")) { // missing description
-                    System.out.print("Missing task description!\n");
-                    System.out.print(dividerText);
-                }
-                else if (todoDescription.contains("/by")) { // unnecessary due date
-                    System.out.print("Todo task cannot have a due date!\n");
-                    System.out.print(dividerText);
-                }
-                else if (todoDescription.contains("/from") ||
-                        todoDescription.contains("/to")) { // unnecessary from and to date
-                    System.out.print("Todo task cannot have a from and to date!\n");
-                    System.out.print(dividerText);
-                }
-                else {
-                    Tam.addTodo(todoDescription);
-                }
-                return 1;
+                case "todo":
+                    String todoDescription = input.replace("todo", "").trim();
+                    if (todoDescription.equals("")) { // missing description
+                        System.out.print("Missing task description!\n");
+                        System.out.print(dividerText);
+                    } else if (todoDescription.contains("/by")) { // unnecessary due date
+                        System.out.print("Todo task cannot have a due date!\n");
+                        System.out.print(dividerText);
+                    } else if (todoDescription.contains("/from") ||
+                            todoDescription.contains("/to")) { // unnecessary from and to date
+                        System.out.print("Todo task cannot have a from and to date!\n");
+                        System.out.print(dividerText);
+                    } else {
+                        Tam.addTodo(todoDescription);
+                    }
+                    FileRetriever.saveTaskList(taskList);
+                    return 1;
 
-            case "deadline":
-                String deadlineDescriptionDueDate = input.replace("deadline", "").trim();
-                if (!deadlineDescriptionDueDate.contains(" /by ")) { // missing '/by' keyword
-                    System.out.print("Missing due date!\n");
+                case "deadline":
+                    String deadlineDescriptionDueDate = input.replace("deadline", "").trim();
+                    if (!deadlineDescriptionDueDate.contains(" /by ")) { // missing '/by' keyword
+                        System.out.print("Missing due date!\n");
+                        System.out.print(dividerText);
+                        FileRetriever.saveTaskList(taskList);
+                        return 1;
+                    }
+                    String[] deadlineDescriptionDueDateArray = input.replace("deadline", "")
+                            .trim().split(" /by ");
+                    String deadlineDescription = deadlineDescriptionDueDateArray[0];
+                    String deadlineDueDate = deadlineDescriptionDueDateArray[1];
+                    if (deadlineDescription.equals("")) { // empty description
+                        System.out.print("Missing task description!\n");
+                        System.out.print(dividerText);
+                    } else if (deadlineDueDate.equals("")) { // empty due date
+                        System.out.print("Missing task due date!\n");
+                        System.out.print(dividerText);
+                    } else if (deadlineDescription.contains("/from") ||
+                            deadlineDescription.contains("/to")) { // unnecessary from and to date
+                        System.out.print("Deadline task cannot have a from and to date!\n");
+                        System.out.print(dividerText);
+                    } else {
+                        Tam.addDeadline(deadlineDescription, deadlineDueDate);
+                    }
+                    FileRetriever.saveTaskList(taskList);
+                    return 1;
+
+                case "event":
+                    String eventDescriptionFromTo = input.replace("event", "").trim();
+                    if (!eventDescriptionFromTo.contains(" /from ") ||
+                            !eventDescriptionFromTo.contains(" /to ")) { // missing '/from' or '/to' keywords
+                        System.out.print("Missing event from and/or to date!\n");
+                        System.out.print(dividerText);
+                        FileRetriever.saveTaskList(taskList);
+                        return 1;
+                    }
+                    String[] eventDescriptionFromToArray = input.replace("event", "")
+                            .trim().split(" /from ");
+                    String eventDescription = eventDescriptionFromToArray[0];
+                    String eventFromTo = eventDescriptionFromToArray[1];
+                    String[] eventFromToArray;
+                    String eventFrom = "";
+                    String eventTo = "";
+                    if (eventFromTo.contains(" /to ")) {
+                        eventFromToArray = eventDescriptionFromToArray[1].trim().split(" /to ");
+                        eventFrom = eventFromToArray[0];
+                        eventTo = eventFromToArray[1];
+                    }
+                    if (eventDescription.equals("")) { // missing description
+                        System.out.print("Missing task description!\n");
+                        System.out.print(dividerText);
+                    } else if (eventFrom.equals("")) { // missing from date
+                        System.out.print("Missing from date!\n");
+                        System.out.print(dividerText);
+                    } else if (eventTo.equals("")) { // missing to date
+                        System.out.print("Missing to date!\n");
+                        System.out.print(dividerText);
+                    } else if (eventDescription.contains("/by")) { // unnecessary due date
+                        System.out.print("Event task cannot have a due date!\n");
+                        System.out.print(dividerText);
+                    } else {
+                        Tam.addEvent(eventDescription, eventFrom, eventTo);
+                    }
+                    FileRetriever.saveTaskList(taskList);
+                    return 1;
+
+                case "help":
+                    System.out.print("LIST OF COMMANDS\n");
+                    System.out.print("Create todo: todo [task name] (eg: todo borrow book)\n");
+                    System.out.print("Create deadline: deadline [task name] /by [due date] " +
+                            "(eg: deadline homework /by Sun 6pm)\n");
+                    System.out.print("Create event: event [task name] /from [from date] /to [to date] " +
+                            "(eg: event concert /from Mon 6pm /to Mon 8pm)\n");
+                    System.out.print("List current tasks: list\n");
+                    System.out.print("Mark a task as complete: mark [task number on list] " +
+                            "(eg: mark 2)\n");
+                    System.out.print("Mark a task as incomplete: unmark [task number on list] " +
+                            "(eg: unmark 2)\n");
+                    System.out.print("Delete a task: delete [task number on list] " +
+                            "(eg: delete 3)\n");
+                    System.out.print("Close Tam the Task Manager: bye\n");
                     System.out.print(dividerText);
                     return 1;
-                }
-                String[] deadlineDescriptionDueDateArray = input.replace("deadline", "")
-                        .trim().split(" /by ");
-                String deadlineDescription = deadlineDescriptionDueDateArray[0];
-                String deadlineDueDate = deadlineDescriptionDueDateArray[1];
-                if (deadlineDescription.equals("")) { // empty description
-                    System.out.print("Missing task description!\n");
-                    System.out.print(dividerText);
-                }
-                else if (deadlineDueDate.equals("")) { // empty due date
-                    System.out.print("Missing task due date!\n");
-                    System.out.print(dividerText);
-                }
-                else if (deadlineDescription.contains("/from") ||
-                        deadlineDescription.contains("/to")) { // unnecessary from and to date
-                    System.out.print("Deadline task cannot have a from and to date!\n");
-                    System.out.print(dividerText);
-                }
-                else {
-                    Tam.addDeadline(deadlineDescription, deadlineDueDate);
-                }
-                return 1;
 
-            case "event":
-                String eventDescriptionFromTo = input.replace("event", "").trim();
-                if (!eventDescriptionFromTo.contains(" /from ") ||
-                        !eventDescriptionFromTo.contains(" /to ")) { // missing '/from' or '/to' keywords
-                    System.out.print("Missing event from and/or to date!\n");
+                default:
+                    System.out.print("Invalid command entered. Type 'help' for list of commands\n");
                     System.out.print(dividerText);
                     return 1;
-                }
-                String[] eventDescriptionFromToArray = input.replace("event", "")
-                        .trim().split(" /from ");
-                String eventDescription = eventDescriptionFromToArray[0];
-                String eventFromTo = eventDescriptionFromToArray[1];
-                String[] eventFromToArray;
-                String eventFrom = "";
-                String eventTo = "";
-                if (eventFromTo.contains(" /to ")) {
-                    eventFromToArray = eventDescriptionFromToArray[1].trim().split(" /to ");
-                    eventFrom = eventFromToArray[0];
-                    eventTo = eventFromToArray[1];
-                }
-                if (eventDescription.equals("")) { // missing description
-                    System.out.print("Missing task description!\n");
-                    System.out.print(dividerText);
-                }
-                else if (eventFrom.equals("")) { // missing from date
-                    System.out.print("Missing from date!\n");
-                    System.out.print(dividerText);
-                }
-                else if (eventTo.equals("")) { // missing to date
-                    System.out.print("Missing to date!\n");
-                    System.out.print(dividerText);
-                }
-                else if (eventDescription.contains("/by")) { // unnecessary due date
-                    System.out.print("Event task cannot have a due date!\n");
-                    System.out.print(dividerText);
-                }
-                else {
-                    Tam.addEvent(eventDescription, eventFrom, eventTo);
-                }
-                return 1;
-
-            case "help":
-                System.out.print("LIST OF COMMANDS\n");
-                System.out.print("Create todo: todo [task name] (eg: todo borrow book)\n");
-                System.out.print("Create deadline: deadline [task name] /by [due date] " +
-                        "(eg: deadline homework /by Sun 6pm)\n");
-                System.out.print("Create event: event [task name] /from [from date] /to [to date] " +
-                        "(eg: event concert /from Mon 6pm /to Mon 8pm)\n");
-                System.out.print("List current tasks: list\n");
-                System.out.print("Mark a task as complete: mark [task number on list] " +
-                        "(eg: mark 2)\n");
-                System.out.print("Mark a task as incomplete: unmark [task number on list] " +
-                        "(eg: unmark 2)\n");
-                System.out.print("Delete a task: delete [task number on list] " +
-                        "(eg: delete 3)\n");
-                System.out.print("Close Tam the Task Manager: bye\n");
-                System.out.print(dividerText);
-                return 1;
-
-            default:
-                System.out.print("Invalid command entered. Type 'help' for list of commands\n");
-                System.out.print(dividerText);
-                return 1;
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
         }
     }
 
