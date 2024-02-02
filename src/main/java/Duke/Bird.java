9ed6073ce6f3bd4a3011785f9265044c8cf283b6
Apzure <block4556@gmail.com>
2024-02-02 19:46:39 +0800

    Add Basic GUI outline
    Created a Basic GUI to serve as a test by following tutorial
    
    To be improved upon and integrated with chatbot in future commits

--- src/main/java/Duke/Bird.java
+++ src/main/java/Duke/Bird.java
@@ -1,21 +1,21 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * This class represents the main driver of the application.
  * It initializes the task list, storage, and user interface, and starts the user interface.
  */
-public class Bird {
+public class Bird extends Application {
     private Ui ui;
-
-    /**
-     * The main method of the application.
-     *
-     * @param args command-line arguments
-     */
-    public static void main(String[] args) {
-        new Bird();
-    }
-
 
     /**
      * Constructs a new Bird ChatBot.
@@ -25,6 +25,52 @@ public class Bird {
         TaskList.create();
         Storage.init();
         this.ui = new Ui();
-        ui.run();
+        // ui.run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        // Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        Scene scene = new Scene(mainLayout);
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
