4a410c6ec2cbb1c48d3d3251ead0f1663a7936b2
ChenXy128 <e0968917@u.nus.edu>
2024-02-02 14:44:34 +0800

    Tasklist class to deal with the tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+public class TaskList {
+    protected ArrayList<Task> list;
+    
+    public TaskList(ArrayList<Task> list) {
+        this.list = list;
+    }
+    
+    public ArrayList<Task> getList() {
+        return this.list;
+    }
+    
+    public void add(Task t) {
+        this.getList().add(t);
+    }
+    
+    public void delete(Task t) {
+        this.getList().remove(t);
+    }
+    
+    public int size() {
+        return this.getList().size();
+    }
+    
+    public Task getTask(int index) {
+        return index >= 0 && index < this.size() ? this.getList().get(index) : null;
+    }
+    
+    public boolean contains(Task t) {
+        return this.getList().contains(t);
+    }
+    
+    
 }
