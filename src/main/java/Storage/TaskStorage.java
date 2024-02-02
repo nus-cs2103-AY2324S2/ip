e6a4d1f3c337753cc9ac925b26438fb2e516f42e
RunjiaChen <runjia@u.nus.edu>
2024-02-02 21:54:52 +0800

    Add TaskStorage

--- src/main/java/Storage/TaskStorage.java
+++ src/main/java/Storage/TaskStorage.java
@@ -1,2 +1,45 @@
-package Storage;public class TaskStorage {
+package Storage;
+
+import SnomExceptions.InvalidCommandIndexException;
+import TaskList.TaskList;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TaskStorage {
+
+    /**
+     * Saves the tasks to a .txt file
+     */
+    private void saveTask(TaskList lst) {
+        File f = new File("data/tasks.txt");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            f.getParentFile().mkdirs();
+            saveTask(lst);
+
+        }
+        updateFile("data/tasks.txt", lst);
+    }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    public void updateFile (String filename, TaskList lst){
+        try {
+            for (int i = 0; i < lst.getCounter(); i++) {
+                writeToFile(filename, lst.getTask(i) + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        } catch (InvalidCommandIndexException e) {
+            System.out.println("this should not happen");
+        }
+    }
 }
