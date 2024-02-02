e997dacfd96486bcf97aa9e02285ba13856fac93
tengcharmaine <e0969596@u.nus.edu>
2024-02-02 23:22:10 +0800

    Update BotChatException to have JavaDocs

--- src/main/java/botChat/botChatException.java
+++ src/main/java/botChat/botChatException.java
@@ -1,6 +1,15 @@
 package BotChat;
 
+/**
+ * Custom exception class for handling exceptions specific to the botChat application.
+ */
 public class BotChatException extends Exception {
+
+    /**
+     * Constructs a new botChatException with the specified detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by the getMessage() method).
+     */
     public BotChatException(String message) {
         super(message);
     }
