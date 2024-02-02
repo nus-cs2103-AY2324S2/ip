972bfdf5d6a449200235199cbece2a00901baa35
sindhurajain <sindhura394@gmail.com>
2024-02-02 15:53:18 +0800

    Write javadoc comments for Command.java, DeadlineCommand.java and DeleteCommand.java

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -4,11 +4,28 @@ import duke.main.Storage;
 import duke.main.TaskList;
 import duke.main.Ui;
 
+/**
+ * Represents a command that can be executed.
+ */
 public class Command {
+
+    /**
+     * Executes the command given the TaskList, Ui, and Storage.
+     *
+     * @param tasks   TaskList that contains the task list.
+     * @param ui      Ui that deals with user interactions.
+     * @param storage Storage used to load and save tasks.
+     * @throws DukeException If there is an error executing the command.
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
 
     }
 
+    /**
+     * Checks if command is an exit command.
+     *
+     * @return True if the command is an exit command, false otherwise.
+     */
     public boolean isExit() {
         return false;
     }
