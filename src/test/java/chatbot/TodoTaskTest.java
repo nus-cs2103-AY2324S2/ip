2b455e02341974a502e39abdab230b455a234e0d
Jia En <tujiaen@gmail.com>
2024-02-02 17:15:06 +0800

    Add JUnit tests

---
+++ src/test/java/chatbot/TodoTaskTest.java
@@ -0,0 +1,23 @@
+package chatbot;
+
+import chatbot.exceptions.AlreadyMarkedException;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TodoTaskTest {
+    @Test
+    public void toString_newTask_success() {
+        assertEquals(new TodoTask("test").toString(), "[T][ ] test");
+    }
+
+    @Test
+    public void mark_existingTask_success() throws AlreadyMarkedException {
+        TodoTask t = new TodoTask("test");
+        t.mark();
+        assertEquals(t.toString(), "[T][X] test");
+    }
+}
