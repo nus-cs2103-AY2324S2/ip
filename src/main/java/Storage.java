10f94e9004ff07cdd485df4fef3517f29e2ea254
ChenXy128 <e0968917@u.nus.edu>
2024-02-02 14:42:56 +0800

    Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Storage {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    protected String filePath;
+    protected Parser parser;
+    
+    public Storage(String filePath, Parser parser) {
+        this.filePath = filePath;
+        this.parser = parser;
+    }
+    
+    public Parser getParser() {
+        return this.parser;
+    }
+    
+    public String getFilePath() {
+        return this.filePath;
+    }
+    public TaskList loadFile(TaskList list) throws IOException {
+        String filePath = this.getFilePath();
+        File file = new File(filePath);
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            String taskContent = sc.nextLine();
+            Task t = this.getParser().parseStringToTask(taskContent);
+            list.add(t);
+        }
+        return list;
+    }
+    
+    public void addTaskToFile(Task task) throws IOException{
+        FileWriter fw = new FileWriter(this.getFilePath(), true);
+        fw.write(task.toString() + System.lineSeparator());
+        fw.close();
+    }
+    
+    public void writeToFile(String textToAdd) throws IOException{
+        FileWriter fw = new FileWriter(this.getFilePath());
+        fw.write(textToAdd);
+        fw.close();
+    }
+    
+    public void changeFileContent(TaskList list) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+        for (Task t: list.getList()) {
+            contentBuilder.append(t.toString()).append(System.lineSeparator());
+        }
+        writeToFile(contentBuilder.toString());
+    }
 }
