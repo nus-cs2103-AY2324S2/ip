d9f7457fdf1caeaa7e1ccac65316e7a6e88d18f0
jyztintan <justintanwk2001@gmail.com>
2024-02-02 10:27:19 +0800

    Create new JUnit tests

---
+++ src/test/java/UiTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import main.java.Ui;
+
+public class UiTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testGreet() {
+        Ui ui = new Ui();
+        ui.greet();
+        String expectedOutput = "*YAP* Good morning YAPPER! *YAP*\nGreetings from\n" + "▀█▀ ▄▀█ █▀ █▄▀ █▄█ ▄▀█ █▀█ █▀█ █▀▀ █▀█\n"
+                + "░█░ █▀█ ▄█ █░█ ░█░ █▀█ █▀▀ █▀▀ ██▄ █▀▄\n" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void testExit() {
+        Ui ui = new Ui();
+        ui.exit();
+        String expectedOutput = "Stoppin' the YAP...\n" + "█▀▀ █▀█ █▀█ █▀▄ █▄▄ █▄█ █▀▀ █\n"
+                + "█▄█ █▄█ █▄█ █▄▀ █▄█ ░█░ ██▄ ▄\n" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
