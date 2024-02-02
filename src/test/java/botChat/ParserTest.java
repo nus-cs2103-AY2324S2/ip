db4ea033167123c88a538520dafecf66f300c9c8
tengcharmaine <e0969596@u.nus.edu>
2024-02-02 21:07:43 +0800

    Remove unecessary import from ParserTest

--- src/test/java/botChat/ParserTest.java
+++ src/test/java/botChat/ParserTest.java
@@ -3,7 +3,6 @@ package botChat;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParserTest {
