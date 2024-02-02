b09ea081a3b92ff190fac0c55312fcbabefc1082
RunjiaChen <runjia@u.nus.edu>
2024-02-02 18:51:23 +0800

    Add TaskList Class

--- src/main/java/TaskList/TaskList.java
+++ src/main/java/TaskList/TaskList.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class TaskList {
+package TaskList;
+
+import SnomTask.Task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> task_lst;
+
+
+    private TaskList() {
+        this.task_lst = new ArrayList<>();
+    }
+
+    public static TaskList makeTaskList() {
+        return new TaskList();
+    }
+
+    public Task getTask(int pos) {
+        // anth that involves indices can throw index out of bounds exception
+        return this.task_lst.get(pos);
+    }
+
+    public void markTask(int pos) {
+        this.task_lst.get(pos).doTask();
+    }
+
+    public void unmarkTask(int pos) {
+        this.task_lst.get(pos).undoTask();
+    }
+
+    public void deleteTask(int pos) {
+
+        this.task_lst.remove(pos);
+    }
 }
