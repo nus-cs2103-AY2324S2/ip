a4bd0a57deab210c247b818d2b14e9c7697a0e27
taeewonnn <htw10192000@gmail.com>
2024-02-02 14:14:36 +0800

    Comply with coding standard

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -1,23 +1,21 @@
 package duke.task;
 
-import duke.util.DukeException;
-
 import java.util.List;
 import java.util.ArrayList;
 
 public class TaskList {
-    private List<Task> listOfTasks;
+    private List<Task> tasks;
 
     public TaskList() {
-        listOfTasks = new ArrayList<>();
+        tasks = new ArrayList<>();
     }
 
     public TaskList(List<Task> tasks) {
-        listOfTasks = tasks;
+        this.tasks = tasks;
     }
 
     public void addTask(Task task) {
-        listOfTasks.add(task);
+        tasks.add(task);
         System.out.println("Got it. I've added this task:\n"
                 + task + "\n"
                 + "Now you have " + getSize()
@@ -25,37 +23,37 @@ public class TaskList {
     }
 
     public int getSize() {
-        return listOfTasks.size();
+        return tasks.size();
     }
 
     public void listDownTask() {
         int size = getSize();
         System.out.println("Here are the tasks in your list:");
         for(int i = 1; i <= size; i++) {
-            System.out.println(i +": " + listOfTasks.get(i - 1));
+            System.out.println(i +": " + tasks.get(i - 1));
         }
     }
 
     public void deleteTask(int index) {
         Task removed = getTask(index - 1);
         System.out.println("Noted. I've removed this task:\n" + removed);
-        listOfTasks.remove(index - 1);
+        tasks.remove(index - 1);
         System.out.println("Now you have " + getSize() + " tasks in the list");
     }
 
     public void markTask(int index) {
-        Task modTask = listOfTasks.get(index - 1);
+        Task modTask = tasks.get(index - 1);
         modTask.setIsDone(true);
         System.out.println("Nice! I've marked this task as done:\n" + modTask);
     }
 
     public void unmarkTask(int index) {
-        Task modTask = listOfTasks.get(index - 1);
+        Task modTask = tasks.get(index - 1);
         modTask.setIsDone(false);
         System.out.println("OK, I've marked this task as not done yet:\n" + modTask);
     }
 
     public Task getTask(int index) {
-            return listOfTasks.get(index);
+            return tasks.get(index);
     }
 }
