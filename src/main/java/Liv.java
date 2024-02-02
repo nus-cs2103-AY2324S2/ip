825754c204fe9ca63e9fd6bdbecf7ee75d0e6631
Wang Xinrong <wangxinrong8@gmail.com>
2024-02-02 21:03:28 +0800

    add Ui.class

--- src/main/java/Liv.java
+++ src/main/java/Liv.java
@@ -5,44 +5,156 @@ import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 
+/*
+ things to do now
+    1. categorise the code into different segments for different functionalities
+    2. create a new class for each of the segment to achieve the goal of more oop
+ */
+
 // name of the chat bot
 public class Liv {
-    // to adhere to the singleton pattern
-    private enum State {
-        ACTIVE_TALKING,
-        /*
-            things to be handled in ACTIVE_TALKING state:
-            - print words to say
-         */
-        ACTIVE_LISTENING,
-        /*
-            things to be handled in ACTIVE_LISTENING state:
-            - take in user input
-            - process the input
-         */
-        INACTIVE
+
+
+    private boolean IsActive() {
+        return currentState != LivState.INACTIVE;
     }
-    private static Liv instance = null;
-    private static HorizontalLine horizontalLine = null;
-    private State currentState;
-    private static Scanner scanner = null;
-    private static LinkedList<Task> tasks = null;
 
-    private static String dataFilePath = "Data/savedTasks.txt";
 
-    private Liv() {
-        // initial setup
-        horizontalLine = HorizontalLine.getInstance();
-        currentState = State.INACTIVE;
-        scanner = new Scanner(System.in);
-        tasks = new LinkedList<>();
+
+
+
+    // parser
+    private boolean isInteger(String str) {
         try {
-            loadFromMemory();
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
+    private void ProcessInput(String input) throws InputException {
+
+        String[] words = input.split(" ");
+
+        // for multi-word commands
+        if (words[0].equals("mark") || words[0].equals("unmark")) {
+            if (isInteger(words[1])) {
+                boolean isDone = words[0].equals("mark");
+                int taskIndex = Integer.parseInt(words[1]);
+                ui.speak(setTaskDoneWithIndex(taskIndex, words[0], isDone));
+            } else {
+                ui.speak("Action failed: task index input is not an integer");
+            }
+            return;
+        }
+
+        if (words[0].equals("delete")) {
+            if (isInteger(words[1])) {
+                int taskIndex = Integer.parseInt(words[1]);
+                Task deletedTask = deleteTask(taskIndex);
+                ui.speak("Noted. I've removed this task:"
+                        + "\n"
+                        + "    "
+                        + deletedTask
+                        + "\n"
+                        + "Now you have " + getNumOfTasks() + " tasks in the list.");//input);
+                return;
+            } else {
+                ui.speak("Action failed: task index input is not an integer");
+            }
+            return;
+        }
+
+        if (words[0].equals("todo")
+                || words[0].equals("deadline")
+                || words[0].equals("event")) {
+
+            Task newTask = null;
+            newTask = Task.createTask(words[0], input);
+            addTask(newTask);
+            ui.speak("Got it. I've added this task:"
+                    + "\n"
+                    + "    "
+                    + newTask
+                    + "\n"
+                    + "Now you have " + getNumOfTasks() + " tasks in the list.");//input);
+            return;
+        }
+
+
+        if (input.equals("bye")) {
+            ui.EndSession();
+            saveToMemory();
+            return;
+        }
+
+        if (input.equals("list")) {
+            listTasks();
+            return;
+        }
+
+        if (input.equals("print tasks")) {
+            ui.speak(tasks.toString());
+            return;
+        }
+
+        throw new CommandNotFoundException(input);
+    }
+
+
+
+
+    // tasklist
+    private static LinkedList<Task> tasks = null;
+    private int getNumOfTasks() {
+        return tasks.size();
+    }
+    private void listTasks() {
+        ui.ToggleConversationState();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= getNumOfTasks(); i++) {
+            System.out.println(i + "." + tasks.get(i - 1).toString());
+        }
+        ui.ToggleConversationState();
+    }
+
+    private void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    private String setTaskDoneWithIndex(int index, String isDoneUpdateString, boolean isDone)
+            throws TaskIndexOutOfBoundsException {
+        try {
+            tasks.get(index - 1).setIsDone(isDoneUpdateString, isDone);
+            return tasks.get(index - 1).updateIsDoneMessage();
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+            throw new TaskIndexOutOfBoundsException(index);
+        }
+    }
+
+    private Task deleteTask(int index) throws TaskIndexOutOfBoundsException {
+        try {
+            Task deletedTask = tasks.remove(index - 1);
+            return deletedTask;
+        } catch (IndexOutOfBoundsException e) {
+            throw new TaskIndexOutOfBoundsException(index);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // storage
+    private static String dataFilePath = "Data/savedTasks.txt";
     private void loadFromMemory() throws FileNotFoundException {
         File file = new File(dataFilePath);
         Scanner scanner = new Scanner(file);
@@ -51,14 +163,9 @@ public class Liv {
         }
     }
 
-    private int getNumOfTasks() {
-        return tasks.size();
-    }
-
     private void loadSingleRowOfData(String s) {
         tasks.add(Task.convertDataToTask(s));
     }
-
 
     private void saveToMemory() {
         try {
@@ -79,200 +186,90 @@ public class Liv {
         fileWriter.close();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // main
+
+    private Liv() {
+        // break the initialisation into the initialization function of different classes
+        currentState = LivState.INACTIVE;
+        tasks = new LinkedList<>();
+        try {
+            loadFromMemory();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static Liv getInstance() {
         if (instance == null) {
             instance = new Liv();
         }
-
         return instance;
     }
     public static void main(String[] args) {
         getInstance().Start();
     }
 
+    private Ui ui = null;
     private void Start() {
+
+        // initialize Ui
+        ui = Ui.getInstance();
+        ui.initUi();
         instance.ToggleActiveState();
-        instance.Greet();
+
         while (IsActive()) {
             // should start the cycle talking
-            String userInput = StartListening();
+            String userInput = ui.StartListening();
             try{
                 ProcessInput(userInput);
             } catch (InputException e) {
-                speak(e.getMessage());
+                ui.speak(e.getMessage());
             }
         }
     }
 
-    private void Greet() {
-        System.out.println("Hello there, Liv here.");
-        System.out.println("How may I help you?");
+    private enum LivState {
+        ACTIVE,
+
+        INACTIVE
     }
+    private static Liv instance = null;
+    private LivState currentState = null;
 
-    private void EndSession() {
-        // should be called from ACTIVE_LISTENING STATE, exception handling?
-        ToggleConversationState();
-        System.out.println("Hope you find my service helpful.");
-        System.out.println("Till next time!");
-        ToggleActiveState();
-    }
-    private void ToggleConversationState() {
+    public void ToggleActiveState() {
 
-        horizontalLine.printLine();
+        //ui.printHorizontalLine();
 
-        if (currentState == State.ACTIVE_TALKING) {
-            currentState = State.ACTIVE_LISTENING;
+        if (currentState != LivState.INACTIVE) {
+            currentState = LivState.INACTIVE;
             return;
         }
 
-        if (currentState == State.ACTIVE_LISTENING) {
-            currentState = State.ACTIVE_TALKING;
+        if (currentState == LivState.INACTIVE) {
+            currentState = LivState.ACTIVE;
             return;
         }
     }
-
-    private void ToggleActiveState() {
-
-        horizontalLine.printLine();
-
-        if (currentState != State.INACTIVE) {
-            currentState = State.INACTIVE;
-            return;
-        }
-
-        if (currentState == State.INACTIVE) {
-            currentState = State.ACTIVE_TALKING;
-            return;
-        }
-    }
-    private String StartListening() {
-            // should be called from ACTIVE_TALKING STATE
-            if (currentState == State.ACTIVE_TALKING) ToggleConversationState();
-            return scanner.nextLine();
-    }
-
-    private void EndListening() {
-        if (currentState == State.ACTIVE_TALKING) ToggleConversationState();
-    }
-
-    private boolean IsActive() {
-        return currentState != State.INACTIVE;
-    }
-
-    private void ProcessInput(String input) throws InputException {
-
-        String[] words = input.split(" ");
-
-        // for multi-word commands
-        if (words[0].equals("mark") || words[0].equals("unmark")) {
-            if (isInteger(words[1])) {
-                boolean isDone = words[0].equals("mark");
-                int taskIndex = Integer.parseInt(words[1]);
-                speak(setTaskDoneWithIndex(taskIndex, words[0], isDone));
-            } else {
-                speak("Action failed: task index input is not an integer");
-            }
-            return;
-        }
-
-        if (words[0].equals("delete")) {
-            if (isInteger(words[1])) {
-                int taskIndex = Integer.parseInt(words[1]);
-                Task deletedTask = deleteTask(taskIndex);
-                speak("Noted. I've removed this task:"
-                        + "\n"
-                        + "    "
-                        + deletedTask
-                        + "\n"
-                        + "Now you have " + getNumOfTasks() + " tasks in the list.");//input);
-                return;
-            } else {
-                speak("Action failed: task index input is not an integer");
-            }
-            return;
-        }
-
-        if (words[0].equals("todo")
-                || words[0].equals("deadline")
-                || words[0].equals("event")) {
-
-            Task newTask = null;
-            newTask = Task.createTask(words[0], input);
-            addTask(newTask);
-            speak("Got it. I've added this task:"
-                    + "\n"
-                    + "    "
-                    + newTask
-                    + "\n"
-                    + "Now you have " + getNumOfTasks() + " tasks in the list.");//input);
-            return;
-        }
-
-
-        if (input.equals("bye")) {
-            EndSession();
-            saveToMemory();
-            return;
-        }
-
-        if (input.equals("list")) {
-            listTasks();
-            return;
-        }
-
-        if (input.equals("print tasks")) {
-            speak(tasks.toString());
-            return;
-        }
-
-        throw new CommandNotFoundException(input);
-    }
-
-    private void speak(String output) {
-        ToggleConversationState();
-        System.out.println(output);
-        ToggleConversationState();
-    }
-
-    private void listTasks() {
-        ToggleConversationState();
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i <= getNumOfTasks(); i++) {
-            System.out.println(i + "." + tasks.get(i - 1).toString());
-        }
-        ToggleConversationState();
-    }
-
-    private void addTask(Task task) {
-        tasks.add(task);
-    }
-
-    private boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
     // takes in the listed index of the task (1 larger than storage index)
-    private String setTaskDoneWithIndex(int index, String isDoneUpdateString, boolean isDone)
-            throws TaskIndexOutOfBoundsException {
-        try {
-            tasks.get(index - 1).setIsDone(isDoneUpdateString, isDone);
-            return tasks.get(index - 1).updateIsDoneMessage();
-        } catch (NullPointerException | IndexOutOfBoundsException e) {
-            throw new TaskIndexOutOfBoundsException(index);
-        }
-    }
 
-    private Task deleteTask(int index) throws TaskIndexOutOfBoundsException {
-        try {
-            Task deletedTask = tasks.remove(index - 1);
-            return deletedTask;
-        } catch (IndexOutOfBoundsException e) {
-            throw new TaskIndexOutOfBoundsException(index);
-        }
-    }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,10 +1,106 @@
+import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Ui {
-    private static DateTimeFormatter printDateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd YYYY HH:mm");
+
+    private enum UiState {
+        /*
+            things to be handled in ACTIVE_TALKING state:
+            - print words to say
+         */
+        ACTIVE_TALKING,
+        /*
+            things to be handled in ACTIVE_LISTENING state:
+            - take in user input
+            - process the input
+         */
+        ACTIVE_LISTENING,
+    }
+
+    private static Ui instance = null;
+    private static HorizontalLine horizontalLine = null;
+    private UiState currentState = null;
+    private static final DateTimeFormatter PRINT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd YYYY HH:mm");
+    private Liv livInstance = null;
+    private static Scanner scanner = null;
+
+    private Ui() {
+        // break the initialisation into the initialization function of different classes
+        horizontalLine = HorizontalLine.getInstance();
+        currentState = UiState.ACTIVE_TALKING;
+        this.livInstance = Liv.getInstance();
+        scanner = new Scanner(System.in);
+        /*
+        scanner = new Scanner(System.in);
+        tasks = new LinkedList<>();
+
+        try {
+            loadFromMemory();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        */
+    }
+    public static Ui getInstance() {
+        if (instance == null) {
+            instance = new Ui();
+        }
+        return instance;
+    }
+
+    public void initUi() {
+        Greet();
+    }
 
     public static String printTime(LocalDateTime localDateTime) {
-        return localDateTime.format(printDateTimeFormatter);
+        return localDateTime.format(PRINT_DATE_TIME_FORMATTER);
+    }
+
+    public void speak(String output) {
+        ToggleConversationState();
+        System.out.println(output);
+        ToggleConversationState();
+    }
+    private void Greet() {
+        speak("Hello there, Liv here." + '\n' + "How may I help you?");
+    }
+    public void ToggleConversationState() {
+
+        horizontalLine.printLine();
+
+        if (currentState == Ui.UiState.ACTIVE_TALKING) {
+            currentState = UiState.ACTIVE_LISTENING;
+            return;
+        }
+
+        if (currentState == UiState.ACTIVE_LISTENING) {
+            currentState = Ui.UiState.ACTIVE_TALKING;
+            return;
+        }
+    }
+
+    public String StartListening() {
+        // should be called from ACTIVE_TALKING STATE
+        if (currentState == UiState.ACTIVE_LISTENING) ToggleConversationState();
+        return scanner.nextLine();
+    }
+
+    public void EndListening() {
+        if (currentState == UiState.ACTIVE_TALKING) ToggleConversationState();
+    }
+
+    public void EndSession() {
+        // should be called from ACTIVE_LISTENING STATE, exception handling?
+        ToggleConversationState();
+        System.out.println("Hope you find my service helpful.");
+        System.out.println("Till next time!");
+        livInstance.ToggleActiveState();
+    }
+
+    public static void printHorizontalLine() {
+        horizontalLine.printLine();
     }
 }
