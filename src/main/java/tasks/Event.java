3982c9db4a64083adcd3fd0f0d7efe7d13fd7b24
jjchee7 <jingjie.chee@u.nus.edu>
2024-02-03 00:56:06 +0800

    Add date and time parsing

--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -1,33 +1,38 @@
 package tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import exceptions.tasks.EmptyDescriptionException;
 
 public class Event extends Task {
-    private String from;
-    private String to;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    private LocalDate from;
+    private LocalDate to;
 
-    public Event(String description, String from, String to) throws EmptyDescriptionException {
+    public Event(String description, LocalDate from, LocalDate to) throws EmptyDescriptionException {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public Event(String description, boolean isDone, String from, String to) throws EmptyDescriptionException {
+    public Event(String description, boolean isDone, LocalDate from, LocalDate to) throws EmptyDescriptionException {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
 
-    public String getFrom() {
+    public LocalDate getFrom() {
         return this.from;
     }
 
-    public String getTo() {
+    public LocalDate getTo() {
         return this.to;
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+            this.from.format(DATE_TIME_FORMATTER), this.to.format(DATE_TIME_FORMATTER));
     }
 }
