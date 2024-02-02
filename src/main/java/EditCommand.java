5e4675d5f3eacc454bcb1a199b6fff843f2b46c0
PallonCX <cheexiang03@gmail.com>
2024-02-02 19:32:11 +0800

    Modify EditCommand.java

---
+++ src/main/java/EditCommand.java
@@ -0,0 +1,36 @@
+public class EditCommand extends Command{
+
+    private final boolean isMark;
+    private final int index;
+
+    public EditCommand(boolean isMark, int index) {
+        this.isMark = isMark;
+        this.index = index;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        // Index out of bound handler
+        if (index >= tasks.getItems().size()) {
+            throw new DukeException("The index of task cannot be larger than number of task.");
+        } else if (index < 0) {
+            throw new DukeException("The index of task must be positive integer.");
+        }
+
+        if (this.isMark) {
+            tasks.markDone(index);
+            ui.mark(tasks.getItem(index));
+        } else {
+            tasks.unmarkDone(index);
+            ui.mark(tasks.getItem(index));
+        }
+
+        storage.saveChanges(tasks);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+}
