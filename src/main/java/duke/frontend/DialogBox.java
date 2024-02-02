9f13c36863a269792614ec1cc90d3ef2c9abbb15
yeoshuheng <yeoshuheng@gmail.com>
2024-02-02 17:12:16 +0800

    feat: Add inverted dialog boxes for shuheng & user

--- src/main/java/duke/frontend/DialogBox.java
+++ src/main/java/duke/frontend/DialogBox.java
@@ -1,6 +1,9 @@
 package duke.frontend;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -28,5 +31,35 @@ public class DialogBox extends HBox {
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+    }
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+    /**
+     * Gets dialog box for the user's perspective.
+     * @param l The label.
+     * @param iv The image to add.
+     * @return A dialog box for users.
+     */
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    /**
+     * Gets dialog box for duke's perspective.
+     * @param l The label.
+     * @param iv The image to add.
+     * @return A dialog box for duke.
+     */
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
     }
 }
