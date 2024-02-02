bf90ec04034c099b1ae853446547b7df7750b53f
yespiggy <skylarye7@gmail.com>
2024-02-02 14:39:25 +0800

    Add Some JUnit Tests

---
+++ src/test/java/duke/DukeTest.java
@@ -0,0 +1,28 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    Task taskTodo = new ToDo("finish hk");
+    Task taskDdl = new Deadline("read book", "2022-12-12");
+    Task taskEvent = new Event("attend meeting", "8am", "10am");
+
+    @Test
+    public void testMarkDone() {
+        taskTodo.mark();
+        assertEquals(taskTodo.getStatus(), true);
+    }
+
+    @Test
+    public void testMarkNotDone() {
+        taskDdl.unmark();
+        assertEquals(taskDdl.getStatus(), false);
+    }
+
+    @Test
+    public void testEventDescription() {
+        assertEquals(taskEvent.getDescription(),"attend meeting");
+    }
+
+}
