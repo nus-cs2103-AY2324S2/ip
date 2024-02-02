25e52cfbe0aafa11c6f09cbd8c6892b376db2d98
Martin <martin.ng@live.com>
2024-02-02 17:39:53 +0800

    Divide classes into packages

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,3 +1,5 @@
+package duke;
+
 public class Parser {
     /**
      * Handles user input and performs corresponding actions
--- src/main/java/duke/TodoTask.java
+++ src/main/java/duke/TodoTask.java
@@ -1,3 +1,5 @@
+package duke;
+
 public class TodoTask extends Task {
 
     /**
@@ -6,7 +8,7 @@ public class TodoTask extends Task {
      * @param name The name of the to-do task.
      * @throws DukeException If the name of the to-do task is an empty string.
      */
-    public TodoTask(String name) throws DukeException{
+    public TodoTask(String name) throws DukeException {
         super(name);
         if (name.isEmpty()) {
             throw new DukeException("oi the task needs a name la \uD83D\uDE21\uD83D\uDE21");
@@ -16,7 +18,7 @@ public class TodoTask extends Task {
     /**
      * Constructs a to-do task with the given name and done status.
      *
-     * @param name The name of the to-do task.
+     * @param name   The name of the to-do task.
      * @param isDone The done status of the to-do task.
      * @throws DukeException If the name of the to-do task is an empty string.
      */
