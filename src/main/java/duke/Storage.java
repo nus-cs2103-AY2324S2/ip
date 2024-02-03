bf00fd3d70b5823dd1f7655cd7af55a64393b70b
Allysa Tan <allysa@u.nus.edu>
2024-02-03 19:07:25 +0800

    Update Storage.java
    Previous version does not support creation of Duke.txt file if not found

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -7,14 +7,29 @@ public class Storage {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataPath))) {
             return (TaskList) ois.readObject();
         } catch (IOException | ClassCastException | ClassNotFoundException e) {
-            // do nothing
+            //
         }
         return new TaskList();
     }
 
     protected static void saveTasks() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataPath))) {
-            oos.writeObject(Duke.lst);
+        try {
+            // Ensure the parent directories exist
+            File file = new File(dataPath);
+            File parentDir = file.getParentFile();
+            if (!parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+
+            // Create the file if it doesn't exist
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            // Save the tasks
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+                oos.writeObject(Duke.lst);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
