9748aea0f492b22e9cc4812308431a6ea2f4c429
JustWeiHao <e0960531@u.nus.edu>
2024-02-04 01:27:30 +0800

    Modify AddCommandTest.java

--- src/test/java/raphael/command/AddCommandTest.java
+++ src/test/java/raphael/command/AddCommandTest.java
@@ -18,7 +18,7 @@ public class AddCommandTest {
     @Test
     public void addCommandTest2() {
         try {
-            new AddCommand(new raphael.task.Task("stub", true)).execute(
+            new AddCommand(new raphael.task.Task("stub", false)).execute(
                     new raphael.task.TaskList(), new raphael.ui.Ui(),
                     new raphael.storage.Storage("./data/tasks.txt")
             );
