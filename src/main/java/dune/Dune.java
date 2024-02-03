9159f2ccc39eb46a285db653ff075bd2f8edffc6
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-02-04 01:18:45 +0800

    Set up JavaFX

--- src/main/java/dune/Dune.java
+++ src/main/java/dune/Dune.java
@@ -1,13 +1,18 @@
 package dune;
 
 import dune.task.TaskList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 
 import java.util.Scanner;
 
 /**
  * The main class for the program.
  */
-public class Dune {
+public class Dune extends Application {
 
 
     private TaskList tasks;
@@ -26,6 +31,15 @@ public class Dune {
         this.storage = new Storage();
         this.tasks = new TaskList();
         this.storage.loadTasks(this.tasks);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
