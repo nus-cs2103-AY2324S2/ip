4434b91d6fa20fe691eb293a3f3f3f54f247d256
Hong Jun <zappy.tan1234@gmail.com>
2024-02-03 22:30:10 +0800

    Add javadoc comments for all task classes in the toothless tasks package.

--- src/main/java/toothless/tasks/Deadline.java
+++ src/main/java/toothless/tasks/Deadline.java
@@ -2,14 +2,29 @@ package toothless.tasks;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a deadline task in the Toothless application. A deadline task includes a description,
+ * and end date.
+ */
 public class Deadline extends Task {
     private LocalDateTime date;
 
+    /**
+     * Constructs a Deadline with the specified description and end date.
+     * @param description The deadline's description.
+     * @param date The end date of the deadline in a parsable datetime format.
+     */
     public Deadline(String description, String date){
         super.description = description;
         this.date = LocalDateTime.parse(date);
     }
 
+    /**
+     * Constructs a Deadline with the specified description, end date, and completion status.
+     * @param description The deadline's description.
+     * @param date The end date of the deadline in a parsable datetime format.
+     * @param isDone The deadline's completion status.
+     */
     public Deadline(String description, String date, boolean isDone){
         super.description = description;
         super.isDone = isDone;
