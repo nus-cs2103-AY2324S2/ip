b2d9d42c7580977bc3c8de41a2f86f8b54eb3960
Sherwyn Ng <>
2024-02-04 02:58:39 +0800

    fix some indentations

--- src/main/java/academicweapon/ui/Ui.java
+++ src/main/java/academicweapon/ui/Ui.java
@@ -7,7 +7,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * The Ui class handles the user interface
+ */
 public class Ui {
+    /**
+     * Displays a welcome message when the application starts.
+     */
     public void showWelcome() {
         Ui.showLine();
         System.out.println("Hello! I'm AcademicWeapon");
@@ -15,22 +21,40 @@ public class Ui {
         Ui.showLine();
     }
 
+    /**
+     * Displays a horizontal line as a separator in the console.
+     */
     public static void showLine() {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays an error message when there is an issue loading a file.
+     */
     public void showLoadingError() {
         Ui.showLine();
         System.out.println("Error loading file.");
         Ui.showLine();
     }
 
+    /**
+     * Reads a user command from the console.
+     *
+     * @return The user command as a string
+     * @throws IOException If an I/O error occurs while reading the command
+     * @throws DukeExceptions If there is an issue with the command
+     */
     public String readCommand() throws IOException, DukeExceptions {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         return input;
     }
 
+    /**
+     * Prints a message indicating that a task has been marks as done.
+     *
+     * @param markTask The task that has been marked as done.
+     */
     public void printMarkDone(Task markTask) {
         Ui.showLine();
         System.out.println("Nice! I've marked this task as done:");
@@ -38,6 +62,11 @@ public class Ui {
         Ui.showLine();
     }
 
+    /**
+     * Prints a message indicating that a task has been masked as not done.
+     *
+     * @param unmarkTask The task that has been marks as not done
+     */
     public void printUnmark(Task unmarkTask) {
         Ui.showLine();
         System.out.println("OK, I've marked this task as not done yet:");
@@ -45,6 +74,12 @@ public class Ui {
         Ui.showLine();
     }
 
+    /**
+     * Prints a message indicating that a task has been successfully added
+     *
+     * @param addTask The task that has been added
+     * @param size The current size of the task list
+     */
     public void printAddSuccessful(Task addTask, int size) {
         Ui.showLine();
         System.out.println("Got it. I've added this task:");
@@ -53,6 +88,12 @@ public class Ui {
         Ui.showLine();
     }
 
+    /**
+     * Prints a message indicating that a task has been successfully removed.
+     *
+     * @param toBeRemoved The task that has been removed
+     * @param size The current size of the task list
+     */
     public void removeSuccessful(Task toBeRemoved, int size) {
         Ui.showLine();
         System.out.println("Noted. I've removed this task:");
@@ -61,6 +102,9 @@ public class Ui {
         Ui.showLine();
     }
 
+    /**
+     * Displays a goodbye message when the application is about to exit.
+     */
     public void showBye() {
         Ui.showLine();
         System.out.println("Bye. Hope to see you again soon!");
