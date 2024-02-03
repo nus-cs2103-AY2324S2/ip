08b8d617b2debff0c021287e6e94e511278ccee4
Murugan-Maniish <muruganmaniish1@gmail.com>
2024-02-03 18:49:42 +0800

    Fix bug in Event.java which caused from and to to have the same values

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,9 +2,10 @@ public class Event extends Task {
 
     protected String from;
     protected String to;
+
     public Event(String description, String from, String to) {
         super(description);
-        this.from = to;
+        this.from = from;
         this.to = to;
     }
 
