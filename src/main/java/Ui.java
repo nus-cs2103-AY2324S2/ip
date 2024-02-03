f7b8536b0ca2590ff237b5265a21b0151728ba14
maiyasaliha <s.maiya095@gmail.com>
2024-02-03 15:24:33 +0800

    Create Ui Class with user interacting messages

---
+++ src/main/java/Ui.java
@@ -0,0 +1,11 @@
+public class Ui {
+    public void welcomeMessage() {
+        System.out.println("Hi Barbie!");
+        System.out.println("I'm Ken!");
+        System.out.println("What would you like to beach today?\n");
+    }
+
+    public void byeMessage() {
+        System.out.println("Beach off!\n");
+    }
+}
