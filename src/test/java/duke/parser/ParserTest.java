f50308152747648c357d515bcfdc431f865df19b
menatahari <natasyabuhari@gmail.com>
2024-02-04 00:32:19 +0800

    Add JUnit test for Parser and TaskList

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -1,2 +1,34 @@
-package duke.parser;public class ParserTest {
+package duke.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void parseDueTime_dateTimeString_success() {
+        assertEquals(LocalDateTime.parse("2022-12-03T18:00:00"),
+                new Parser("task /by 2022-12-03 18:00", "deadline").parseDueTime());
+
+        assertEquals(LocalDateTime.parse("2023-10-03T05:00:00"),
+                new Parser("other task /by 2023-10-03 05:00", "deadline").parseDueTime());
+    }
+
+    @Test
+    public void parseDueTime_wrongFormatDateTimeString_exceptionThrown() {
+        try {
+            new Parser("task /by 2022-12-0318:00", "deadline").parseDueTime();
+        } catch (DateTimeParseException e) {
+            assertEquals("Text '2022-12-0318:00' could not be parsed at index 10", e.getMessage());
+        }
+
+        try {
+            new Parser("other task /by 2023/10/03 0500", "deadline").parseDueTime();
+        } catch (DateTimeParseException e) {
+            assertEquals("Text '2023/10/03 0500' could not be parsed at index 4", e.getMessage());
+        }
+    }
 }
--- src/test/java/duke/task/TaskListTest.java
+++ src/test/java/duke/task/TaskListTest.java
@@ -1,2 +1,36 @@
-package duke.task;public class TaskListTest {
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void addTask_toDo_successStringReturned() {
+        assertEquals("[T][ ] task",
+                new TaskList().addTask(new ToDo("task", false)));
+
+        assertEquals("[T][X] task",
+                new TaskList().addTask(new ToDo("task", true)));
+    }
+
+    @Test
+    public void addTask_deadline_successStringReturned() {
+        assertEquals("[D][ ] task (by: Oct 5 2024)",
+                new TaskList().addTask(
+                        new Deadline(
+                                "task",
+                                false,
+                                LocalDate.parse("2024-10-05"))));
+    }
+
+    @Test
+    public void addTask_event_successStringReturned() {
+        assertEquals("[E][ ] task (from: now to: tomorrow)",
+                new TaskList().addTask(
+                        new Event("task", false, "now", "tomorrow")));
+    }
 }
