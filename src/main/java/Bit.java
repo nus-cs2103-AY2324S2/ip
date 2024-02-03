850b41024304cb76a7974a5f5f392f02434ce7ec
Ko-Khan <e0970313@u.nus.edu>
2024-02-03 16:44:35 +0800

    Revert "Finish class Parser which parses through inputs and return strings containing the respective commands."
    This reverts commit 16731f4e8a543ea4f66bc6906fee211d0d187fb4.

--- src/main/java/Bit.java
+++ src/main/java/Bit.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import  java.util.Scanner;
 
 
@@ -6,31 +7,60 @@ public class Bit {
 
     private static final Ui UI = new Ui();
 
+
+
+
+    private static  String fileName = "./data/bit.txt";
     public static void main(String[] args) {
 
         UI.greet();
         Tasklist tasklist = new Tasklist(UI);
         Scanner scanner = new Scanner(System.in);
         Storage store = new Storage();
+        ArrayList<Task> list = new ArrayList<>();
+
+
         store.loadFile(tasklist);
         store.cleanList();
-        Parser parser = new Parser();
 
         while(true) {
             UI.printLineBreak();
             String input = scanner.nextLine();
-            String s = parser.parse(input);
-            if (s.equals("bye")) {
+            if (input.equals("bye")) {
                 break;
-            } else if (s.equals("list")) {
+            } else if (input.equals("list")) {
                 UI.listOut(tasklist);
-            } else if (s.equals("mark")) {
-                tasklist.mark(parser.getIndex());
-            } else if (s.equals("unmark")) {
-                tasklist.unmark(parser.getIndex());
-            } else if (s.equals("delete")) {
-                tasklist.delete(parser.getIndex());
-            } else if (s.equals("add")){
+            } else if (input.contains("mark ")) {
+                String[] parts = input.split(" ");
+                try {
+                    int i = Integer.parseInt(parts[1]);
+                    if (parts[0].equals("mark")) {
+                        tasklist.mark(i);
+                    } else if (parts[0].equals("unmark")) {
+                        tasklist.unmark(i);
+                    } else {
+                        try{tasklist.addTo(input);} catch(DukeException e) {System.out.println(e.getMessage());}
+
+
+                    }
+
+                } catch (NumberFormatException e) {
+                    try{
+                        tasklist.addTo(input);
+                    } catch(DukeException x) {
+                        System.out.println(x.getMessage());}
+                }
+            } else if (input.startsWith("delete")) {
+                try {
+                    String[] strings = input.split(" ", 2);
+                    int i = Integer.parseInt(strings[1]);
+                    tasklist.delete(i);
+                } catch (NumberFormatException x) {
+                    UI.handleErrorMessage("Not a number");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    UI.handleErrorMessage("forget");
+                }
+            } else {
                 try{
                     tasklist.addTo(input);
                 } catch(DukeException e) {
