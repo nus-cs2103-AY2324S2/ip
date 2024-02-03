1bb132ded50c67907092dc171a10b70191be5efb
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-02-03 19:21:18 +0800

    Add Storage.java TaskManager.java Ui.java

---
+++ src/main/java/simpli/TaskManager.java
@@ -0,0 +1,77 @@
+package simpli;
+
+import simpli.tasks.Deadline;
+import simpli.tasks.Event;
+import simpli.tasks.Task;
+import simpli.tasks.Todo;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class TaskManager {
+    ArrayList<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void mark(int taskNum) {
+        int itemIndex = taskNum - 1;
+        tasks.get(itemIndex).done();
+    }
+
+    public void unmark(int taskNum) {
+        int itemIndex = taskNum - 1;
+        tasks.get(itemIndex).undone();
+    }
+
+    public Task addTodo(String[] tokens) {
+        Todo todo = new Todo(tokens[1].equals("1"), tokens[2]);
+        tasks.add(todo);
+        return todo;
+    }
+
+    public Task addDeadline(String[] tokens, LocalDateTime[] dateTimes) {
+        Deadline deadline = new Deadline(tokens[1].equals("1"), tokens[2], dateTimes[0]);
+        tasks.add(deadline);
+        return deadline;
+    }
+
+    public Task addEvent(String[] tokens, LocalDateTime[] dateTimes) {
+        Event event = new Event(tokens[1].equals("1"), tokens[2], dateTimes[0], dateTimes[1]);
+        tasks.add(event);
+        return event;
+    }
+
+    public Task deleteTask(int taskNum) {
+        Task removedTask = tasks.get(taskNum - 1);
+        tasks.remove(taskNum - 1);
+        return removedTask;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder strItems = new StringBuilder();
+        for (int i = 0; i < tasks.size() - 1; i++) {
+            strItems.append(String.format("%d. %s\n", i + 1, tasks.get(i)));
+        }
+        if (!tasks.isEmpty()) {
+            strItems.append(String.format("%d. %s", tasks.size(), tasks.getLast()));
+        }
+
+        return strItems.toString();
+    }
+
+    public Task getTask(int taskNum) {
+        return this.tasks.get(taskNum - 1);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public ArrayList<Task> tasks() {
+        return this.tasks;
+    }
+
+}
---
+++ src/main/java/simpli/Ui.java
@@ -0,0 +1,11 @@
+package simpli;
+
+import simpli.configs.SimpliConfiguration;
+
+public class Ui {
+    public void display(String content) {
+        String msg = String.format(SimpliConfiguration.PLACEHOLDER, content.replace("\n", "\n\t\t"));
+
+        System.out.println(msg);
+    }
+}
