b897bdbb9904b7f647cdb8850e5f7b510cca25a6
Tan Xinhao <e0774267@u.nus.edu>
2024-02-03 13:39:36 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
 
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "CMU_bot";
-        String[] TaskList = new String[100];
+        Task[] TaskList = new Task[100];
         int NumOfTask = 0;
         System.out.println("Hello! Boss I'm your" + logo);
         System.out.println("What can I do for you?");
@@ -11,19 +12,35 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         boolean isExit = true;
         while (isExit) {
-            String command = sc.nextLine();
-            if(command.equals("bye")) {
-                System.out.println("Bye. CMU_bot is always here for you!");
+            String commandString = sc.nextLine();
+            String[] command = commandString.split(" ");
+
+            if(command[0].equals("bye")) {
+                System.out.println("Bye. CMU_bot is always here for you, see you again!");
                 isExit = false;
-            } else if (command.equals("List")) {
+            } else if (command[0].equals("List")) {
                 for (int i = 0; i < NumOfTask; i ++) {
-                    System.out.println(TaskList[i]);
+                    System.out.println(Integer.toString(i + 1) + ". " +  TaskList[i].toString());
                 }
+            }  else if (command[0].equals("mark")) {
+                int NumToMark = Integer.parseInt(command[1]) - 1;
+                TaskList[NumToMark].setStatusIcon(true);
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(Integer.toString(NumToMark + 1) + ". " +  TaskList[NumToMark].toString());
+
+            } else if (command[0].equals("unmark")) { // error need to be paid attention
+                int NumToUnmark = Integer.parseInt(command[1]) - 1;
+                TaskList[NumToUnmark].setStatusIcon(false);
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(Integer.toString(NumToUnmark + 1) + ". " +  TaskList[NumToUnmark].toString());
 
             } else {
-                TaskList[NumOfTask] = Integer.toString(NumOfTask+1)+". " + command;
+                Task task = new Task(commandString);
+                TaskList[NumOfTask] = task;
                 NumOfTask ++;
-                System.out.println("added: " + command);
+                System.out.println("added: " + task.description);
             }
 
         }
