f65bcaac4aa30e69500ada40974f8671041aa240
meowwtama <122257205+meowwtama@users.noreply.github.com>
2024-02-03 20:17:58 +0800

    A-JUnit
    Added JUnit tests for Todo event and parser

---
+++ src/test/java/anita/TodoTest.java
@@ -0,0 +1,11 @@
+package anita;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void todoString_Success() throws Exception{
+        assertEquals("[T][ ] max win", new Todo("max win").toString());
+    }
+}
