ffba1ac84b3bad85fc75121c612ce47c05644997
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-02-03 23:40:40 +0800

    Add JavaFX Part 2

--- src/main/java/chatbot/ChatBot.java
+++ src/main/java/chatbot/ChatBot.java
@@ -11,7 +11,12 @@ import chatbot.task.TaskList;
 import chatbot.ui.Printer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -34,16 +39,63 @@ public class ChatBot extends Application {
      * Creates the JavaFX Application instance by calling the no-argument class constructor.
      */
     public ChatBot() {
-        run();
     }
 
+    /**
+     * Reused from https://se-education.org/guides/tutorials/javaFx.html,
+     * with minor modifications.
+     */
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Step 1. Setting up required components
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        //The container for the content of the chat to scroll.
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle(CHATBOT_NAME);
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        //You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //More code to be added here later
     }
 
     /**
--- src/main/java/chatbot/Launcher.java
+++ src/main/java/chatbot/Launcher.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 /**
  * A launcher class to workaround classpath issues.
- * Reused from https://se-education.org/guides/tutorials/javaFxPart1.html.
+ * Reused from https://se-education.org/guides/tutorials/javaFx.html.
  */
 public class Launcher {
     public static void main(String[] args) {
