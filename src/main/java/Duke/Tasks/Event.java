312ceb6442cba04128af38f617a694969f7b1a6d
alvinnzz <alvinng01@live.com>
2024-02-03 21:29:37 +0800

    Update javadoc for Duke.Tasks package

--- src/main/java/Duke/Tasks/Event.java
+++ src/main/java/Duke/Tasks/Event.java
@@ -4,9 +4,20 @@ import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represent an event task.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
+
+    /**
+     * Constructor for the event class
+     *
+     * @param description Description of the event.
+     * @param from Start date of the event.
+     * @param to End date of the event.
+     */
     public Event(String description, String from, String to) throws DateTimeException {
         super(description, 'E');
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
@@ -15,6 +26,12 @@ public class Event extends Task {
         this.from = fromParsed;
         this.to = toParsed;
     }
+
+    /**
+     * Convert from date from LocalDateTime to String
+     *
+     * @return String From date of the event.
+     */
     public String fromDateInWords() {
         String dayWeek = from.getDayOfWeek().toString();
         int dayMonth = from.getDayOfMonth();
@@ -22,6 +39,12 @@ public class Event extends Task {
         int year = from.getYear();
         return dayWeek + " " + dayMonth + " " + month + " " +year;
     }
+
+    /**
+     * Convert to date from LocalDateTime to String
+     *
+     * @return String To date of the event.
+     */
     public String toDateInWords() {
         String dayWeek = to.getDayOfWeek().toString();
         int dayMonth = to.getDayOfMonth();
@@ -29,6 +52,7 @@ public class Event extends Task {
         int year = to.getYear();
         return dayWeek + " " + dayMonth + " " + month + " " + year;
     }
+
     @Override
     public String toString() {
         String str = String.format(
