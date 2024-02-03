1f412583454563dfb23a5e92c02895af890e95ec
KSSWSept20 <wongxucheng12345@gmail.com>
2024-02-03 23:55:51 +0800

    Add UiTest.java and TaskListTest.java
    Test the greet() method in Ui.java and the storeDeadline() method in
    TaskList.java.

--- src/test/java/tes/taskmanager/TaskListTest.java
+++ src/test/java/tes/taskmanager/TaskListTest.java
@@ -1,2 +1,20 @@
-package tes.taskmanager;public class TaskListTest {
+package tes.taskmanager;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void testStoreDeadLine() {
+        String taskDescription = "return book";
+        String by = "2022-12-12 1900";
+        ArrayList<Task> tasks = new ArrayList<>();
+        TaskList taskList = new TaskList(tasks);
+        taskList.storeDeadline(taskDescription, by);
+        String actual = taskList.getTaskDescription(0);
+        assertEquals("D |   | return book | by: Dec 12 2022 07:00 PM", actual);
+    }
 }
