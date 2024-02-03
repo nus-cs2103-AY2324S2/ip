8a50dab173e9172e70e0a00b7c1ea10e94a90039
Kin Tat <110956850+KinTatHo@users.noreply.github.com>
2024-02-03 23:11:09 +0800

    TaskList.java: added comments for methods

--- src/main/java/yoda/task/TaskList.java
+++ src/main/java/yoda/task/TaskList.java
@@ -3,20 +3,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * Represents a list of tasks. It provides operations to add, delete, and retrieve tasks.
+ */
 public class TaskList {
     private final List<Task> tasks; // List to store the tasks
 
     public TaskList(List<Task> loadedTasks) {
         this.tasks = loadedTasks != null ? new ArrayList<>(loadedTasks) : new ArrayList<>();
     }
-
+    /**
+     * Adds a task to the list.
+     * @param task The task to be added.
+     */
     public void addTask(Task task) {
         if (task != null) {
             tasks.add(task);
         }
     }
 
-
+    /**
+     * Deletes a task from the list.
+     * @param taskNumber The number of the task to be deleted.
+     * @throws Exception If the task number is invalid.
+     */
     public void deleteTask(int taskNumber) throws Exception {
         if (taskNumber <= 0 || taskNumber > tasks.size()) {
             throw new Exception("Valid task number, provide you must.");
@@ -24,6 +34,11 @@ public class TaskList {
         tasks.remove(taskNumber - 1);
     }
 
+    /**
+     * Marks a task as done.
+     * @param taskNumber The number of the task to mark as done.
+     * @throws Exception If the task number is invalid.
+     */
     public void markTaskAsDone(int taskNumber) throws Exception {
         if (taskNumber <= 0 || taskNumber > tasks.size()) {
             throw new Exception("Valid task number, provide you must.");
@@ -31,6 +46,11 @@ public class TaskList {
         tasks.get(taskNumber - 1).markAsDone();
     }
 
+    /**
+     * Marks a task as undone.
+     * @param taskNumber The number of the task to mark as not done.
+     * @throws Exception If the task number is invalid.
+     */
     public void markTaskAsUndone(int taskNumber) throws Exception {
         if (taskNumber <= 0 || taskNumber > tasks.size()) {
             throw new Exception("Valid task number, provide you must.");
@@ -38,6 +58,12 @@ public class TaskList {
         tasks.get(taskNumber - 1).markAsUndone();
     }
 
+    /**
+     * Retrieves a task from the list.
+     * @param taskNumber The number of the task to retrieve.
+     * @return The task at the specified position.
+     * @throws Exception If the task number is invalid.
+     */
     public Task getTask(int taskNumber) throws Exception {
         if (taskNumber <= 0 || taskNumber > tasks.size()) {
             throw new Exception("Valid task number, provide you must.");
@@ -45,11 +71,18 @@ public class TaskList {
         return tasks.get(taskNumber - 1);
     }
 
-
+    /**
+     * Returns the number of tasks in the list.
+     * @return The number of tasks in the list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Checks if the list is empty.
+     * @return true if the list is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return tasks.isEmpty();
     }
