b4308ca730ca6845728fc3a2070d712a9a85fde2
Zhi Heng <tanzhiheng26@gmail.com>
2024-02-03 12:16:00 +0800

    Add GUI from JavaFx tutorial

--- src/main/java/ezra/Ezra.java
+++ src/main/java/ezra/Ezra.java
@@ -3,13 +3,28 @@ package ezra;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * Main class for the Ezra chatbot.
  */
-public class Ezra {
+public class Ezra extends Application {
 
     private Storage storage;
     private TaskList tasks;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Constructs an Ezra object with the specified file path to load saved tasks from.
@@ -40,6 +55,33 @@ public class Ezra {
             }
         }
         scanner.close();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Ezra heard: " + input;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.io.IOException;
+
+import ezra.Ezra;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Ezra ezra = new Ezra("data/ezra.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setEzra(ezra);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
