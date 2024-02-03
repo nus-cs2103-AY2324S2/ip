917118f825f16d4be58a07e0db175a2effb80d0d
tejas-1405 <tejas.sadasivam@gmail.com>
2024-02-03 19:44:18 +0800

    Add Testcases

---
+++ src/test/java/duke/TodoTest.java
@@ -0,0 +1,15 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void taskCreation(){
+        Todo task = new Todo("description");
+        assertEquals(task.toString(), "[T][ ] description");
+    }
+
+
+}
