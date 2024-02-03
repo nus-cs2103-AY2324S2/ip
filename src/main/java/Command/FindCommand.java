04aaf8943dd9560de0a94a6ce5b8d0a81a54a462
LifHoshi <e0907413@u.nus.edu>
2024-02-03 11:36:44 +0800

    Add related file about Level-9 find

---
+++ src/main/java/Command/FindCommand.java
@@ -0,0 +1,23 @@
+package Command;
+
+import Ping.TaskList;
+import Ping.UI;
+
+
+/**
+ * This class is used to find the tasks
+ */
+public class FindCommand extends Command {
+    private String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public TaskList perform(TaskList tasks, UI ui) {
+        ui.findTaskMessage(tasks.findTasks(keyword));
+        return tasks;
+    }
+    
+}
--- src/main/java/Ping/UI.java
+++ src/main/java/Ping/UI.java
@@ -16,6 +16,7 @@ public class UI {
     public String readLines() {
         return sc.nextLine();
     }
+
     /**
      * This method is used to print the welcome message
      */
@@ -24,6 +25,7 @@ public class UI {
         System.out.println("Hello! I'm Ping" + "\nWhat can I do for you?");
         System.out.println("----------------------------------------------------------");
     }
+
     public void goodBye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
@@ -39,6 +41,7 @@ public class UI {
     public void hiMessage() {
         System.out.println("Hi, there");
     }
+
     /**
      * This function below used for event and deadline command
      */
@@ -57,8 +60,10 @@ public class UI {
         System.out.println(task.toString());
         System.out.printf("Now you have %d tasks in the list." + "%n", numOfWork);
     }
+
     /**
      * This method is used to show the list of tasks
+     *
      * @param tasks the list of tasks
      */
     public void showList(ArrayList<Task> tasks) {
@@ -76,20 +81,41 @@ public class UI {
             }
         }
     }
+
     /**
      * This method is used to mark the task as done
+     *
      * @param t the task
      */
     public void markTaskMessage(Task t) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(t.toString());
     }
+
     /**
      * This method is used to unmark the task as not done
+     *
      * @param t the task
      */
     public void unmarkTaskMessage(Task t) {
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(t.toString());
+    }
+
+    /**
+     * This method is used to find the task
+     *
+     * @param tasks the list of tasks
+     */
+    public void findTaskMessage(ArrayList<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("No matching task found");
+        } else {
+            System.out.println("Here are the matching tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                int idx = i + 1;
+                System.out.printf("%d." + tasks.get(i).toString() + "%n", idx);
+            }
+        }
     }
 }
