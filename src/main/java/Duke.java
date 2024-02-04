890cd1311981bb9eb3b3ef8e1ff46dfef2e1435f
KiatLun <louiskhoo368@gmail.com>
2024-02-04 14:59:47 +0800

    Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -45,26 +45,77 @@ public class Duke {
                 }
             } else if (input.startsWith("mark")) {
                 String[] temp = input.split(" ");
-                int index = Integer.parseInt(temp[1]) - 1;
-                Task task = storage.get(index);
-                task.markTask();
-                String output = "Nice! I've marked this task as done:\n" +
-                        "   " + task.toString();
-                System.out.println(output);
+                try {
+                    if (temp.length <= 1) {
+                        throw new DukeException("__________________________________\n" +
+                                "What tasks do you want to mark?");
+                    }
+
+                    int number = Integer.parseInt(temp[1]);
+                    if (number > storage.size() || number <= 0) {
+                        throw new DukeException("__________________________________\n" +
+                                "The task is not in the list!!");
+                    }
+                    int index = number - 1;
+                    Task task = storage.get(index);
+                    task.markTask();
+                    String output = "OK, I've marked this task as done:\n" +
+                            "   " + task.toString();
+                    System.out.println(output);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
 
 
             } else if (input.startsWith("unmark")) {
                 String[] temp = input.split(" ");
-                int index = Integer.parseInt(temp[1]) - 1;
-                Task task = storage.get(index);
-                task.unmarkTask();
-                String output = "OK, I've marked this task as not done yet:\n" +
-                        "   " + task.toString();
-                System.out.println(output);
+                try {
+                    if (temp.length <= 1) {
+                        throw new DukeException("__________________________________\n" +
+                                "What tasks do you want to unmark?");
+                    }
+
+                    int number = Integer.parseInt(temp[1]) ;
+                    if (number > storage.size() || number <= 0 ) {
+                        throw new DukeException("__________________________________\n" +
+                                "The task is not in the list!!");
+                    }
+                    int index = number - 1;
+                    Task task = storage.get(index);
+                    task.unmarkTask();
+                    String output = "OK, I've marked this task as not done yet:\n" +
+                            "   " + task.toString();
+                    System.out.println(output);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            } else if (input.startsWith("delete")) {
+                String[] temp = input.split(" ");
+                try {
+                    if (temp.length <= 1) {
+                        throw new DukeException("__________________________________\n" +
+                                "What tasks do you want to delete?");
+                    }
+                    int number = Integer.parseInt(temp[1]);
+                    if (number > storage.size() || number <= 0) {
+                        throw new DukeException("__________________________________\n" +
+                                "The task is not in the list!!");
+                    }
+                    int index = number - 1;
+                    Task task = storage.get(index);
+                    storage.remove(index);
+                    String output = "OK, I've removed this task:\n" +
+                            "   " + task.toString() +
+                            "\nNow you have " + storage.size() + " tasks in the list.";
+                    System.out.println(output);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
 
             } else if (input.startsWith("todo")) {
                 try {
-                    String[] test = input.split("");
+                    String[] test = input.split(" ");
                     if (test.length <= 1) {
                         throw new DukeException("__________________________________\n" +
                                 "So where is the todo task????");
