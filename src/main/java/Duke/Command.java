ad4aa1f1b1e854ca4edfb2a46d16d649357e5439
jayne1010 <e0970489@u.nus.edu>
2024-02-04 20:03:25 +0800

    A-CheckStyle

--- src/main/java/Duke/Command.java
+++ src/main/java/Duke/Command.java
@@ -5,7 +5,6 @@ package Duke;
  * on a TaskList. Classes implementing this interface must provide an execute
  * method to carry out the specific command.
  */
-
 public interface Command {
     /**
      * Executes the command, performing the specified action on the TaskList.
