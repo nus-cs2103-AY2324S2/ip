36869d9278a34689e71304f9d27549badab406ba
DESKTOP-KIADS1H\Chuan Yao <kaizerfang2@gmail.com>
2024-02-04 11:24:24 +0800

    Add Javadoc headers for better API documentation

--- src/main/java/demon/Todo.java
+++ src/main/java/demon/Todo.java
@@ -1,8 +1,11 @@
 package demon;
 
+/**
+ * Represents a task that is to do, only contains description.
+ */
 public class Todo extends Task {
 
-    public Todo(String description){
+    public Todo(String description) {
         super(description);
     }
 
