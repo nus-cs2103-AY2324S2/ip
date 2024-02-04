56cc93e1bf83b7b2a95f5c862cc6783ee6aa67cd
ryanlimdx <ryanlimdx@gmail.com>
2024-02-04 18:59:25 +0800

    Add more accepted date formats
    Users may be more familiar with just typing in the date.
    
    The string is split to determine if time is specified. If it is,
    datetime is used. Otherwise, it will be of date type.
    
    Let's allow multiple input format.

--- src/main/java/duke/utils/DateTime.java
+++ src/main/java/duke/utils/DateTime.java
@@ -2,35 +2,63 @@ package duke.utils;
 
 import duke.DukeException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 
 public class DateTime {
-    private final static String PRINT_FORMAT = "MMM dd yyyy HHmm";
+    private final static String PRINT_DATE_TIME_FORMAT = "MMM dd yyyy HHmm";
+    private final static String PRINT_DATE_FORMAT = "MMM dd yyyy";
 
-    private static DateTimeFormatterBuilder acceptedFormats
+    private static DateTimeFormatterBuilder acceptedDateTimeFormats
             = new DateTimeFormatterBuilder().append(
                     DateTimeFormatter.ofPattern("[dd/MM/yyyy HHmm]"
                             + "[yyyy/MM/dd HHmm]" + "[dd-MM-yyyy HHmm]"
                             + "[yyyy-MM-dd HHmm]" + "[MMM dd yyyy HHmm]"
                     ));
 
+    private static DateTimeFormatterBuilder acceptedDateFormats
+            = new DateTimeFormatterBuilder().append(
+            DateTimeFormatter.ofPattern("[dd/MM/yyyy]"
+                    + "[yyyy/MM/dd]" + "[dd-MM-yyyy]"
+                    + "[yyyy-MM-dd]" + "[MMM dd yyyy]"
+            ));
+
     protected LocalDateTime datetime;
+    protected LocalDate date;
 
     public DateTime(String datetime) {
-        try {
-            DateTimeFormatter dateTimeFormatter = acceptedFormats.toFormatter();
-            this.datetime = LocalDateTime.parse(datetime, dateTimeFormatter);
-        } catch (DateTimeParseException e) {
-            throw new DukeException(
-                    "Wrong date time format! Try the default: 'yyyy-MM-dd HHmm'");
+        String[] currInput = datetime.split(" ", 2);
+
+        if (currInput.length == 2) {
+            try {
+                DateTimeFormatter dateTimeFormatter = acceptedDateTimeFormats.toFormatter();
+                this.datetime = LocalDateTime.parse(datetime, dateTimeFormatter);
+            } catch (DateTimeParseException e) {
+                throw new DukeException(
+                        "Wrong date time format! Try the default: 'yyyy-MM-dd HHmm'");
+            }
+        } else {
+            try {
+                DateTimeFormatter dateTimeFormatter = acceptedDateFormats.toFormatter();
+                this.date = LocalDate.parse(datetime, dateTimeFormatter);
+            } catch (DateTimeParseException e) {
+                throw new DukeException(
+                        "Wrong date format! Try the default: 'dd-MM-yyyy'");
+            }
         }
+
+
     }
 
     @Override
     public String toString() {
-        return this.datetime.format(DateTimeFormatter.ofPattern(PRINT_FORMAT));
+        if (datetime != null) {
+            return this.datetime.format(DateTimeFormatter.ofPattern(PRINT_DATE_TIME_FORMAT));
+        } else {
+            return this.date.format(DateTimeFormatter.ofPattern(PRINT_DATE_FORMAT));
+        }
     }
 }
