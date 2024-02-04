eb13465ffb4791b5cdfb86738ecb2fa4ea793124
quelinxiao <122249170+quelinxiao@users.noreply.github.com>
2024-02-04 22:07:00 +0800

    Add echo function

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String greet = " Hello! I'm Bob.\n"
                 + " What can I do for you?\n";
 
         String exit = " Bye. Hope to see you again soon!";
-        System.out.println(greet + exit);
+
+        System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(exit);
+                break;
+            }
+
+            System.out.println(" " + input);
+        }
+
+        scanner.close();
     }
 }
