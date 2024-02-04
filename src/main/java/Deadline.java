cd5a135d02dcfb6039cfde5a6b3ef0f6d4a8f9b8
TheodoreKooo <theodorekoo@gmail.com>
2024-02-04 19:21:12 +0800

    made all of the subclasses ToDo, Deadline and Event public

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Deadline extends Task {
+    private LocalDateTime by;
+    public Deadline (String deadline, String by) {
+        super(deadline);
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        this.by = LocalDateTime.parse(by, inputFormatter);
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a");
+        return "D" + super.toString() + " | " + this.by.format(outputFormatter);
+    }
+}
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,3 +1,5 @@
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
