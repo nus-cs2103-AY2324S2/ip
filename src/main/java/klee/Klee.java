6060ae197680ab7bb0d005f9e803ec4efb975fc9
BryanL2303 <bryanlee0033@gmail.com>
2024-02-04 21:12:23 +0800

    Recode main class and Ui to create GUi
    Instead of having a text based Ui, generate a GUi using JavaFX.
    
    Recode the entire Ui class and added function to echo user input.
    All functions are now changed to output to GUi rather than command line.
    
    Main class was recoded to set up stage and handle commands onAction.
    Added Launcher class to launch GUi, app should now be run from Launch
    class rather than Klee class.

--- src/main/java/klee/Klee.java
+++ src/main/java/klee/Klee.java
@@ -2,51 +2,117 @@ package klee;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import klee.command.Bye;
 import klee.command.Command;
 
-/**
- * Main program to store list of upcoming tasks.
- */
-public class Klee {
-    /**
-     * Function to run the program.
-     *
-     * @param ui
-     * @param parser
-     * @param storage
-     * @param tasks
-     */
-    public static void run(Ui ui, Parser parser, Storage storage, TaskList tasks) {
-        Scanner getInput = new Scanner(System.in);
-        while (true) {
-            String input = getInput.nextLine();
-            try {
-                Command command = parser.parseInput(input);
-                command.runCommand(ui, storage, tasks);
-                if (command.getClass() == Bye.class) {
-                    break;
-                }
-            } catch (KleeException e) {
-                ui.showError(e.getMessage());
-            }
-        }
+
+public class Klee extends Application {
+
+    public Klee () {
+
     }
 
-    /**
-     * Main function to instantiate classes and execute run function.
-     *
-     * @param args
-     */
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     public static void main(String[] args) {
-        Ui ui = new Ui();
+        // ...
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
         Parser parser = new Parser();
         Storage storage = new Storage();
-
+        Ui ui = new Ui(dialogContainer);
         TaskList tasks = storage.retrieveTasks(ui);
 
-        ui.showWelcome();
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-        run(ui, parser, storage, tasks);
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Klee");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        //You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ui.echoUser(userInput.getText());
+                try {
+                    Command command = parser.parseInput(userInput.getText());
+                    command.runCommand(ui, storage, tasks);
+                    if (command.getClass() == Bye.class) {
+                        stage.close();
+                    }
+                    userInput.setText("");
+                } catch (KleeException e) {
+                    ui.showError(e.getMessage());
+                    userInput.setText("");
+                }
+                scrollPane.setVvalue(scrollPane.getVmax());
+            }
+        });
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //More code to be added here later
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        ui.showWelcome();
     }
 }
+
+
