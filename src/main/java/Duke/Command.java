3961baf3b0b40b18c4d2fe3ba94c52927116c619
jayne1010 <e0970489@u.nus.edu>
2024-02-04 20:01:45 +0800

    Level-8

--- src/main/java/Duke/Command.java
+++ src/main/java/Duke/Command.java
@@ -5,6 +5,7 @@ package Duke;
  * on a TaskList. Classes implementing this interface must provide an execute
  * method to carry out the specific command.
  */
+
 public interface Command {
     /**
      * Executes the command, performing the specified action on the TaskList.
