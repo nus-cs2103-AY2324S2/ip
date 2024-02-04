f758352666909b5d0db9540dca93899065880769
Yuhi Narita <yuhi.narita26@gmail.com>
2024-02-04 19:51:42 +0800

    Rename chatbot to Chipchat

--- src/main/java/Chipchat.java
+++ src/main/java/Chipchat.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public class Duke {
+public class Chipchat {
     private static List<Task> taskList = new ArrayList<>();
+    private static String BOT_NAME = "Chipchat";
 
     public static void main(String[] args) {
-        String bot_name = "Calvin";
-        String greeting_msg = "Hello! I'm " + bot_name + "\n"
+        String greeting_msg = "Hello! I'm " + Chipchat.BOT_NAME + "\n"
                 + "What can I do for you?";
         String exit_msg = "Bye. Hope to see you again soon!\n";
         System.out.println(greeting_msg);
@@ -17,69 +17,69 @@ public class Duke {
             String[] inputs = user_input.split(" ", 2);
 
             try {
-                if (inputs[0].isEmpty()) throw new DukeException("Please enter something");
+                if (inputs[0].isEmpty()) throw new ChipchatException("Please enter something");
                 else if (inputs[0].equals("bye")) {
                     break;
 
                 } else if (inputs[0].equals("list")) {
                     System.out.println("\nHere are the tasks in your list:");
-                    for (int i = 0; i < Duke.taskList.size(); i++) {
+                    for (int i = 0; i < Chipchat.taskList.size(); i++) {
                         int k = i + 1;
-                        System.out.println(k + ". " + Duke.taskList.get(i));
+                        System.out.println(k + ". " + Chipchat.taskList.get(i));
                     }
 
                 } else if (inputs[0].equals("mark")) {
-                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new DukeException("Sorry. Please specify the item no. you want to mark");
+                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new ChipchatException("Sorry. Please specify the item no. you want to mark");
                     int idx = Integer.parseInt(inputs[1]);
-                    Task tmp = Duke.taskList.get(idx-1);
+                    Task tmp = Chipchat.taskList.get(idx-1);
                     tmp.mark();
                     System.out.println("\nNice! I've marked this task as done:\n" + tmp);
 
                 } else if (inputs[0].equals("unmark")) {
                     if (inputs.length == 1 || inputs[1].isEmpty())
-                        throw new DukeException("Sorry. Please specify the item no. you want to unmark");
+                        throw new ChipchatException("Sorry. Please specify the item no. you want to unmark");
                     int idx = Integer.parseInt(inputs[1]);
-                    Task tmp = Duke.taskList.get(idx - 1);
+                    Task tmp = Chipchat.taskList.get(idx - 1);
                     tmp.unmark();
                     System.out.println("\nNice! I've marked this task as done:\n" + tmp);
 
                 } else if (inputs[0].equals("delete")) {
                     if (inputs.length == 1 || inputs[1].isEmpty())
-                        throw new DukeException("Sorry. Please specify the item no. you want to delete");
+                        throw new ChipchatException("Sorry. Please specify the item no. you want to delete");
                     int idx = Integer.parseInt(inputs[1]);
                     if (idx < 1 || idx > taskList.size())
-                        throw new DukeException("Sorry that item no. is not the list");
-                    Task tmp = Duke.taskList.get(idx - 1);
+                        throw new ChipchatException("Sorry that item no. is not the list");
+                    Task tmp = Chipchat.taskList.get(idx - 1);
                     System.out.println("\nNoted. I've removed this task:\n" + tmp);
-                    Duke.taskList.remove(idx - 1);
+                    Chipchat.taskList.remove(idx - 1);
                 } else if (inputs[0].equals("todo")) {
-                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new DukeException("Sorry. Please specify a description");
+                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new ChipchatException("Sorry. Please specify a description");
                     Task tmp = new Todo(inputs[1]);
-                    Duke.taskList.add(tmp);
+                    Chipchat.taskList.add(tmp);
                     System.out.println("\nGot it. I've added this task:");
                     System.out.println(tmp);
-                    System.out.println("Now you have " + Duke.taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + Chipchat.taskList.size() + " tasks in the list.");
                 } else if (inputs[0].equals("deadline")) {
-                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new DukeException("Sorry. Please specify a description");
+                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new ChipchatException("Sorry. Please specify a description");
                     String[] str = inputs[1].split(" /by ");
                     Task tmp = new Deadline(str[0], str[1]);
-                    Duke.taskList.add(tmp);
+                    Chipchat.taskList.add(tmp);
                     System.out.println("\nGot it. I've added this task:");
                     System.out.println(tmp);
-                    System.out.println("Now you have " + Duke.taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + Chipchat.taskList.size() + " tasks in the list.");
                 } else if (inputs[0].equals("event")) {
-                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new DukeException("Sorry. Please specify a description");
+                    if (inputs.length == 1 || inputs[1].isEmpty()) throw new ChipchatException("Sorry. Please specify a description");
                     String[] str = inputs[1].split(" /from ");
                     String[] from_to = str[1].split(" /to ");
                     Task tmp = new Event(str[0], from_to[0], from_to[1]);
-                    Duke.taskList.add(tmp);
+                    Chipchat.taskList.add(tmp);
                     System.out.println("\nGot it. I've added this task:");
                     System.out.println(tmp);
-                    System.out.println("Now you have " + Duke.taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + Chipchat.taskList.size() + " tasks in the list.");
                 } else {
-                    throw new DukeException("Sorry. I don't know what that means.");
+                    throw new ChipchatException("Sorry. I don't know what that means.");
                 }
-            } catch (DukeException e) {
+            } catch (ChipchatException e) {
                 System.out.println(e.toString());
             }
         }
---
+++ src/main/java/ChipchatException.java
@@ -0,0 +1,5 @@
+public class ChipchatException extends RuntimeException {
+    public ChipchatException(String msg) {
+        super(msg);
+    }
+}
