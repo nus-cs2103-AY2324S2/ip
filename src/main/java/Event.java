749578888db659953ebfb82d722f0f3c3fdd7812
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-02-04 20:39:02 +0800

    Event.java: Add overridden toTaskSaveString()

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,9 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
     }
+
+    @Override
+    public String toTaskSaveString() {
+        return "E|" + this.getStatusInt() + "|" + this.description + "|" + this.start + "|" + this.end;
+    }
 }
