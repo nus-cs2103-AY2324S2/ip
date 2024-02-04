10918c1bcda84c9f2c430bd2799336dcf94a6222
iamtr <jettzeong@gmail.com>
2024-02-04 22:37:48 +0800

    Add restoreData() method

--- src/main/java/Tasklist.java
+++ src/main/java/Tasklist.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tasklist {
     private List<Task> todolist = new ArrayList<>();
+
+    public Tasklist() {
+        restoreData();
+    }
 
     public void addItem(Task item) {
         todolist.add(item);
@@ -40,5 +46,30 @@ public class Tasklist {
 
     public String getTaskString(int taskNumber) {
         return todolist.get(taskNumber).toString();
+    }
+
+    public void restoreData() {
+        // TODO: Handle details[1]
+        try {
+            Boolean isDone;
+            List<String> tasks = Storage.loadData();
+            for (String task : tasks) {
+                String[] details = task.split("\\|");
+                isDone = details[1] != "0";
+                for(int i = 0; i < details.length; i++) {
+                     details[i] = details[i].trim();
+                }
+                if (details[0].equals("T")) {
+                    addItem(new Todo(details[2], isDone));
+                } else if (details[0].equals("D")) {
+                    addItem(new Deadline(details[2], LocalDate.parse(details[3]), isDone));
+                } else if (details[0].equals("E")) {
+                    addItem(new Event(details[2], LocalDate.parse(details[3]), LocalDate.parse(details[4]), isDone));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error has occured while executing loadData()");
+        }
+
     }
 }
