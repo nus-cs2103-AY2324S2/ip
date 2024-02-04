bfc6912b7a8510073a3c3df80997ee5cc38070d5
Theodore Koo <e0958222@u.nus.edu>
2024-02-05 00:24:24 +0800

    added in the tests for the Parser class

--- src/test/java/riz/io/ParserTest.java
+++ src/test/java/riz/io/ParserTest.java
@@ -1,4 +1,4 @@
-/*package riz.io;
+package riz.io;
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -8,14 +8,20 @@ import riz.data.*;
 
 public class ParserTest {
     private final Scanner scanner = new Scanner(System.in);
-    private TaskList taskList = new TaskList(new ArrayList<>());
-    private Storage storage = new Storage("riz/data/parsertest.txt");
+    private final TaskList taskList = new TaskList(new ArrayList<>());
+    private final Storage storage = new Storage("riz/data/parsertest.txt");
 
     @Test
     public void parseTest() {
-        taskList.add(new ToDo("shower"));
-        assertEquals("added: " + "T |  | shower...\n"
-        + "You currently have 1 things to do...", Parser.parse(scanner, this.taskList, storage, "todo shower"));
+        //taskList.add(new ToDo("shower"));
+        //taskList.add(new Deadline("birthday", "19/06/2001 1800"));
+        Parser.parse(scanner, this.taskList, this.storage,"todo shower");
+        Parser.parse(scanner,this.taskList, this.storage, "deadline birthday /by 19/06/2001 1800");
+        assertEquals("T |   | shower", taskList.get(0).toString());
+        assertEquals("D |   | birthday | 19 Jun 2001 06:00 pm", taskList.get(1).toString());
+        Parser.parse(scanner, this.taskList, this.storage, "mark 1");
+        assertEquals("T | X | shower", taskList.get(0).toString());
+        Parser.parse(scanner, this.taskList, this.storage, "unmark 1");
+        assertEquals("T |   | shower", taskList.get(0).toString());
     }
 }
-*/
