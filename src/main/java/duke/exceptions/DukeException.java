bedc84a6e6afa7056cd573e5de50f4133a2beab3
Max Yang <>
2024-02-05 00:54:32 +0800

    Add comments to DukeException.java file

--- src/main/java/duke/exceptions/DukeException.java
+++ src/main/java/duke/exceptions/DukeException.java
@@ -1,6 +1,7 @@
 package duke.exceptions;
 
 public class DukeException extends Exception {
+    // Ex: Invalid commands
     public DukeException (String str)
     {
         // calling the constructor of parent Exception
