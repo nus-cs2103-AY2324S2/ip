90cd5ec364c0f7f54078866ea66ad3b9fd3e02f5
quelinxiao <122249170+quelinxiao@users.noreply.github.com>
2024-02-04 20:58:19 +0800

    Tweak greet and exit message

---
+++ src/main/java/Bob.java
@@ -0,0 +1,9 @@
+public class Bob {
+    public static void main(String[] args) {
+        String greet = " Hello! I'm Bob.\n"
+                + " What can I do for you?\n";
+
+        String exit = " Bye. Hope to see you again soon!";
+        System.out.println(greet + exit);
+    }
+}
