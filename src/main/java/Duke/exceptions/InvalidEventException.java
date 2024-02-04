1158c4ee12248fd40b6c54d3ed49350a39a2a21d
DingBao-sys <dinghaosim@gmail.com>
2024-02-04 22:19:25 +0800

    Complete A-JUnit

--- src/main/java/Duke/exceptions/InvalidEventException.java
+++ src/main/java/Duke/exceptions/InvalidEventException.java
@@ -7,7 +7,8 @@ public class InvalidEventException extends DukeException {
     @Override
     public String toString() {
         return "After I try so hard, you want to cook me by keying the date\n" +
-                "and time wrongly try again by adding the event in the format:\n" +
-                "event [task description] /from: dd/mm/yyyy HHmm /to dd/mm/yyyy HHmm\n";
+                "and time wrongly try again, by adding the event in the format:\n" +
+                "event [task description] /from: dd/mm/yyyy HHmm /to dd/mm/yyyy HHmm\n" +
+                "/from date must be before /to date\n";
     }
 }
