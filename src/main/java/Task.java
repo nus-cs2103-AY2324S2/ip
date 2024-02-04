1524ca6ad86f9e2850d224c78b4b7c8657529aeb
RingoftheKing <90739493+RingoftheKing@users.noreply.github.com>
2024-02-04 22:06:06 +0800

    Add Ui, TaskList, Storage, Parser classes
    Code logic was all placed in Tracker.java
    This impedes new developers from understanding
    what the Tracker's function is.
    
    It is better to have a segregation of concerns
    so that each feature is placed in its own classes
    
    As such 4 new classes are made to accomodate
    such functions

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,12 +11,12 @@ public class Task {
 
     public void mark(boolean b, int taskIndex) {
         String MARK = b ? "marked" : "unmarked"; //use enums if more needed
-        if (Tracker.tasks.isEmpty()) {
+        if (TaskList.tasks.isEmpty()) {
             System.out.println("There's nothing to mark, Yay!");
             return;
         }
         try {
-            if (taskIndex > Tracker.tasks.size() || taskIndex < 0) {
+            if (taskIndex > TaskList.tasks.size() || taskIndex < 0) {
                 System.out.println("You don't have that task silly!");
                 return;
             }
