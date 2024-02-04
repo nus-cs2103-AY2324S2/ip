d2ee8675a046994d200a4f5a4a948370841bbaa4
Ryan Chia <e0959970@u.nus.edu>
2024-02-05 02:31:52 +0800

    Add JavaDoc for Exceptions

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Exception thrown by the Duke chatbot.
+ */
 public class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
--- src/main/java/duke/DukeOptionParsingException.java
+++ src/main/java/duke/DukeOptionParsingException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * To be thrown when command option parsing fails, which occurs for incorrect user input.
+ */
 public class DukeOptionParsingException extends DukeException {
     public DukeOptionParsingException(String message) {
         super(message);
