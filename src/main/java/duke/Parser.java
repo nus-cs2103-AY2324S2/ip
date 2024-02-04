3621d98a46f8ce129ce3d068ca26beeab79fffa7
Martin Ng <martin.ng@live.com>
2024-02-04 17:33:22 +0800

    Resolve conflicts

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -14,6 +14,7 @@ import duke.commands.ListCommand;
 import duke.commands.MarkCommand;
 import duke.commands.TodoCommand;
 import duke.commands.UnmarkCommand;
+import duke.commands.FindCommand;
 
 public class Parser {
 
