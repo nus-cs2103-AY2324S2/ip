e3e486e9bf838792734abbfaddbdab0982b9f4da
tzaph <maximilliano.utomo@gmail.com>
2024-02-04 22:21:05 +0800

    Revert "no message"
    This reverts commit 46a26f3dfc77867f4cfe8a6ff51c8e1987423135.

--- src/main/java/arona/task/Deadline.java
+++ src/main/java/arona/task/Deadline.java
@@ -43,7 +43,6 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: "
-                + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        return "[D]" + super.toString() + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
