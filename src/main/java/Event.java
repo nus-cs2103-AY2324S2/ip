bb55420e074d85cf2c54337737c01b55221bbbe5
xuwen-ho <ho.xu.wen@gmail.com>
2024-02-04 18:34:47 +0800

    Subject: Imported LocalDate libary to try to implement Date parsing ability
    Current State: See date entered as purely a string
    
    What was Wrong: Limits what we can do with entered date, cannot compare etc
    
    How Commit changes Current State: Lets parse the date so we can do other actions like list all task before a certain date

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String deadlineFrom;
-    protected String deadlineTo;
+    protected LocalDate deadlineFrom;
+    protected LocalDate deadlineTo;
+    String DATE_FORMAT = "MMM d yyyy";
+
 
 
     public Event(String description, String deadlineFrom, String deadlineTo) {
         super(description);
-        this.deadlineFrom = deadlineFrom;
-        this.deadlineTo = deadlineTo;
+        this.deadlineFrom = LocalDate.parse(deadlineFrom);
+        this.deadlineTo = LocalDate.parse(deadlineTo);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + deadlineFrom + " to: " + deadlineTo + ")";
+        return "[E]" + super.toString() + " (from: " + deadlineFrom.format(DateTimeFormatter.ofPattern(DATE_FORMAT))
+                + " to: " + deadlineTo.format(DateTimeFormatter.ofPattern(DATE_FORMAT)) + ")";
     }
 }
