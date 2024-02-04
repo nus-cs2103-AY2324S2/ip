c92aeb244e40eede35a2c1437795b1ff5eb78fa1
iamtr <jettzeong@gmail.com>
2024-02-04 22:37:37 +0800

    Add loadData() method

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,9 +2,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Storage {
+    public static final Path path = Paths.get("data", "duke.txt");
+
+    public static List<String> loadData() throws IOException {
+        Path dirPath = path.getParent();
+        Files.createDirectories(dirPath);
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        List<String> lines = Files.readAllLines(path);
+        return lines;
+    }
+
     public void saveData (List<Task> todolist) throws IOException {
         String entry;
         Path dirPath = java.nio.file.Paths.get("data");
