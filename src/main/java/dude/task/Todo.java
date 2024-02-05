7701f8baefbd7c28b6659642926a716e83aa2087
blaukc <brandonlau1@hotmail.com>
2024-02-05 10:03:11 +0800

    Make arbitrary commit for testing

--- src/main/java/dude/task/Todo.java
+++ src/main/java/dude/task/Todo.java
@@ -14,7 +14,7 @@ public class Todo extends Task {
 
     /**
      * Creates Todo object.
-     * @param description Todo descriptin.
+     * @param description Todo description.
      * @param done  Whether todo is done.
      */
     public Todo(String description, boolean done) {
