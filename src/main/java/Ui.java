4430d9c2b9e0f3ed8dfa239f95649ff91982b7f6
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-02-05 23:08:55 +0800

    More-OOP

---
+++ src/main/java/Ui.java
@@ -0,0 +1,7 @@
+public class Ui {
+    //not sure what to put here yet as of A-MoreOOP
+
+    public static void showLoadingError(Exception e){
+        System.out.println(e.getMessage());
+    }
+}
