c720d90f1f78606f05debdc652ec2aa11f244a18
Jajared <jajawong09@gmail.com>
2024-02-06 00:44:44 +0800

    style: Fix javadocs styling

--- src/main/java/exception/InvalidDateException.java
+++ src/main/java/exception/InvalidDateException.java
@@ -6,8 +6,7 @@ package exception;
  * <p>
  * This class extends the {@link Exception} class, allowing
  * {@code InvalidDateException} to be caught and handled specifically for cases
- * related to date
- * validation failures.
+ * related to date validation failures.
  * </p>
  */
 public class InvalidDateException extends Exception {
