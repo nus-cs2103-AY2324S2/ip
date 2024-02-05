67c41d39c9fc046f0fdc3dc36c02dd8bf09e89c9
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-02-05 10:23:54 +0800

    Add JavaDoc comments to Storage class

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,15 +1,27 @@
 package duke;
+
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Storage class is responsible for reading and writing tasks to a file.
+ */
 public class Storage {
     private String filePath;
     private Ui ui = new Ui();
 
+    /**
+     * Constructs a new Storage object.
+     *
+     * @param filePath The path of the file to read from and write to.
+     * @throws DukeException If an error occurs while creating the file.
+     */
     public Storage(String filePath) throws DukeException {
         this.filePath = filePath;
         try {
@@ -35,6 +47,12 @@ public class Storage {
         }
     }
 
+    /**
+     * Loads tasks from the file.
+     *
+     * @return A list of tasks.
+     * @throws IOException If an error occurs while reading the file.
+     */
     public List<Task> load() throws IOException {
         List<Task> tasks = new ArrayList<>();
         Path path = Paths.get(filePath);
@@ -47,6 +65,12 @@ public class Storage {
         return tasks;
     }
 
+    /**
+     * Saves tasks to the file.
+     *
+     * @param tasks The list of tasks to save.
+     * @throws IOException If an error occurs while writing to the file.
+     */
     public void save(List<Task> tasks) throws IOException {
         List<String> lines = new ArrayList<>();
         for (Task task : tasks) {
