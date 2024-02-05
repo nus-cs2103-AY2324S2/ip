9b99e86ea42458d1390af46059b36f63f05aaa75
RunjiaChen <runjia@u.nus.edu>
2024-02-05 18:52:16 +0800

    Add TaskList

--- src/main/java/snomtasklist/TaskList.java
+++ src/main/java/snomtasklist/TaskList.java
@@ -1,21 +1,17 @@
-package snomtasklist;
+package SnomTaskList;
 
-import snomexceptions.InvalidCommandIndexException;
-import snomtask.Task;
+import SnomExceptions.InvalidCommandIndexException;
+import SnomTask.Task;
 
 import java.util.ArrayList;
 
-/**
- * TaskList implements a class that keeps track
- * of the tasks added by the user.
- */
 public class TaskList {
-    private ArrayList<Task> tasklst;
+    private ArrayList<Task> task_lst;
     private int counter;
 
 
     private TaskList() {
-        this.tasklst = new ArrayList<>();
+        this.task_lst = new ArrayList<>();
         this.counter = 0;
     }
 
@@ -23,65 +19,40 @@ public class TaskList {
         return new TaskList();
     }
 
-    /**
-     * Retrieves the task from the TaskList based on the index.
-     *
-     * @param pos is the index of the task in the TaskList.
-     * @return the instance of the Task at the specified index.
-     * @throws InvalidCommandIndexException if the index specified by the user
-     * is not valid.
-     */
     public Task getTask(int pos) throws InvalidCommandIndexException {
 
         if (counter < pos || pos <= 0) {
             throw new InvalidCommandIndexException();
         } else {
             pos -= 1;
-            return this.tasklst.get(pos);
+            return this.task_lst.get(pos);
         }
 
     }
 
-    /**
-     * Changes the status of the task in TaskList to complete.
-     * @param pos is the index of the task in the TaskList.
-     * @throws InvalidCommandIndexException
-     */
     public void markTask(int pos) throws InvalidCommandIndexException {
 
         if (counter < pos || pos <= 0) {
             throw new InvalidCommandIndexException();
         } else {
             pos -= 1;
-            this.tasklst.get(pos).doTask();
+            this.task_lst.get(pos).doTask();
         }
 
     }
 
-    /**
-     * Changes the status of the task in TaskList to incomplete.
-     * @param pos is the index of the task in the TaskList.
-     * @throws InvalidCommandIndexException if the index specified by the user
-     * is not valid.
-     *
-    */
     public void unmarkTask(int pos) throws InvalidCommandIndexException {
 
         if (counter < pos || pos <= 0) {
             throw new InvalidCommandIndexException();
         } else {
             pos -= 1;
-            this.tasklst.get(pos).undoTask();
+            this.task_lst.get(pos).undoTask();
         }
     }
 
-    /**
-     * Deletes the instance of the Task from TaskList.
-     * @param pos is the index of the task in the TaskList.
-     * @throws InvalidCommandIndexException if the index specified by the user
-     * is not valid.
-     *
-     */
+
+
     public void deleteTask(int pos) throws InvalidCommandIndexException {
 
         if (counter < pos || pos <= 0) {
@@ -89,39 +60,40 @@ public class TaskList {
         } else {
             pos -= 1;
             this.counter -= 1;
-            this.tasklst.remove(pos);
+            this.task_lst.remove(pos);
         }
 
     }
 
-<<<<<<< HEAD:src/main/java/snomtasklist/TaskList.java
-    public void addTask(Task t) {
-=======
-    /**
-     * Adds the task to the TaskList.
-     * @param t is the instance of the task to be added.
-     */
     public void AddTask(Task t) {
->>>>>>> branch-A-JavaDoc:src/main/java/SnomTaskList/TaskList.java
         this.counter += 1;
-        this.tasklst.add(t);
+        this.task_lst.add(t);
     }
 
-    /**
-     * Retrieves the number of tasks in the TaskList.
-     * @return an integer representing the number of tasks
-     * in the TaskList.
-     */
     public int getCounter() {
         return this.counter;
     }
 
-    /**
-     * Prints the tasks for the user to see.
-     */
     public void displayTaskList() {
-        for (int i = 0; i < counter; i++) {
-            System.out.println(this.tasklst.get(i));
+        for (int i = 0; i<counter; i++) {
+            System.out.println(this.task_lst.get(i));
+        }
+    }
+
+    public void printMatchingTasks(String cmd) {
+        ArrayList<Task> found_tasks = new ArrayList<>();
+        for (int i = 0; i<counter; i++) {
+            if (this.task_lst.get(i).match(cmd)) {
+                found_tasks.add(task_lst.get(i));
+            }
+        }
+        if (found_tasks.size() > 0) {
+            System.out.println("Here are the tasks that match your description");
+            for (int j = 0; j < found_tasks.size(); j++) {
+                System.out.println(found_tasks.get(j));
+            }
+        } else {
+            System.out.println("Sorry, we did not find any tasks that matched your description");
         }
     }
 }
