77989e5c3fa2c52bf191352abf7658f32f079e33
Howlong11 <e1093040@u.nus.edu>
2024-02-06 04:09:41 +0800

    Save dates as LocalDateTime objects, level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,35 +1,39 @@
-public class Event extends Task{
-    private String startTime;
-    private String endTime;
-    private String type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description) {
-        super(description);
-        this.type = "E";
-    }
+public class Event extends Task{
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    private DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy HHmm");
 
     public Event(String description, String startTime, String endTime) {
         super(description);
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.type = "E";
+        this.startTime = LocalDateTime.parse(startTime, inputFormat);
+        this.endTime = LocalDateTime.parse(endTime, inputFormat);
     }
 
     @Override
     public String print() {
+        String start = startTime.format(outputFormat);
+        String end = endTime.format(outputFormat);
         String str = "[E]" + super.print() + "(from: " +
-                this.startTime + "to: " + this.endTime + ")";
+                start + "to: " + end + ")";
         return str;
     }
     @Override
     public String getDescription() {
-        String str = "[E] " + super.getDescription() + " " + this.startTime + " " + this.endTime;
+        String start = startTime.format(outputFormat);
+        String end = endTime.format(outputFormat);
+        String str = "[E] " + super.getDescription() + " " + start + " " + end;
         return str;
     }
 
     @Override
     public String getTaskInfo() {
-        return "[E] " + "/ [" + super.getStatusIcon() + "] / " + super.getTaskInfo() + " / " + this.startTime
-                + " / " + this.endTime;
+        String start = startTime.format(outputFormat);
+        String end = endTime.format(outputFormat);
+        return "[E] " + "/ [" + super.getStatusIcon() + "] / " + super.getTaskInfo() + " / " + start
+                + " / " + end;
     }
 }
