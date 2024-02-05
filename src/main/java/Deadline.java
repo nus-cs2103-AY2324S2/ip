8ff06eb3bf6ad3e511aaafe9b204b2381ea556c0
YowSiaoKang <kangyow4@gmail.com>
2024-02-05 15:10:32 +0800

    Implement LocalDateTIme

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,38 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String by;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[yyyy-MM-dd HH:mm][yyyy/MM/dd HH:mm][yyyy MM dd HH:mm][yyyy.MM.dd HH:mm]");
+    private LocalDateTime by;
 
     public Deadline (String description, String by) {
         super(description);
-        this.by = by;
+        if (by.length() < 10 || (by.length() > 16 && by.length() != 64)) {
+            throw new IllegalArgumentException("Please enter a valid date and time in the format yyyy-MM-dd HH:mm");
+        }
+        if (by.length() == 10) {
+            by += " 00:00";
+        }
+        this.by = LocalDateTime.parse(by, this.formatter);
     }
 
     public Deadline (String description, String by, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        if (by.length() < 10 || (by.length() > 16 && by.length() != 64)) {
+            throw new IllegalArgumentException("Please enter a valid date and time in the format yyyy-MM-dd HH:mm");
+        }
+        if (by.length() == 10) {
+            by += " 00:00";
+        }
+        this.by = LocalDateTime.parse(by, this.formatter);
     }
 
     public String getBy() {
-        return this.by;
+        return this.by.format(this.formatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")) + ")";
     }
 }
