7aac38cf236481dc08fb0471cad9dc1d1f409a83
yunchao <tanyunchao@gmail.com>
2024-02-06 00:21:32 +0800

    Add greeting and farewell messages to Ui class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,14 @@
 public class Ui {
-    
+    public Ui() {
+
+    }
+
+    public void sayGreeting() {
+        System.out.println("Hello from Martin");
+        System.out.println("What can I do for you?");
+    }
+
+    public void sayBye() {
+        System.out.println("Bye from Martin");
+    }
 }
