ec30354b1cc67bfb26c2195390c8cf9ec8d8d658
David <david@dzhy.dev>
2024-02-05 15:50:04 +0800

    Initial javafx setup from tutorial

--- src/main/java/cappy/Cappy.java
+++ src/main/java/cappy/Cappy.java
@@ -1,12 +1,19 @@
 package cappy;
 
 import cappy.command.CommandType;
+import cappy.controller.MainWindow;
 import cappy.error.CappyException;
 import cappy.parser.ParsedInput;
 import cappy.parser.Parser;
 import cappy.storage.Storage;
 import cappy.task.TaskList;
 import cappy.ui.Ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -16,11 +23,13 @@ import java.io.IOException;
  * <p>The {@code Cappy} class is the entry point of the Cappy application. It initializes the
  * application components and starts the main input loop to interact with the user.
  */
-public class Cappy {
+public class Cappy extends Application {
     private static final String STORAGE_PATH = "./cappy.csv";
     private static final Ui UI = new Ui();
     private static TaskList taskList;
     private static Storage storage;
+
+    public Cappy() {}
 
     public static void main(String[] args) {
         UI.showBanner();
@@ -34,6 +43,21 @@ public class Cappy {
             UI.showError(e);
         } finally {
             Cappy.UI.close();
+        }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(Cappy.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setCappy(new Cappy());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
