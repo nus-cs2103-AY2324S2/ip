ae499962fe58ed28dca453aa82f485b6cb773320
LWS49 <111121080+LWS49@users.noreply.github.com>
2024-02-05 16:44:37 +0800

    Revert "Merging branch level 8 to origin"

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,33 +1,13 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-
 public class Deadline extends Task {
     protected String by;
-    protected LocalDateTime byDate;
-    protected boolean hasByDate;
-
-    protected DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("M/dd/yyyy HHmm");
-
-    protected DateTimeFormatter dateTimeStringFormatter = DateTimeFormatter.ofPattern("dd MMM uuuu ha");
 
     public Deadline(String description, String by) {
         super(description);
-        try {
-            this.byDate = LocalDateTime.parse(by, dateTimeFormatter);
-            hasByDate = true;
-        } catch (DateTimeParseException e) {
-            this.by = by;
-            hasByDate = false;
-        }
+        this.by = by;
     }
 
     @Override
     public String toString() {
-        if (hasByDate) {
-            return "[D]" + super.toString() + " (by: " + dateTimeStringFormatter.format(byDate) + ")";
-        } else {
-            return "[D]" + super.toString() + " (by: " + by + ")";
-        }
+        return "[D]" + super.toString() + "(by: " + by + ")";
     }
 }
