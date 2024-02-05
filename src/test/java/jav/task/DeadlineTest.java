72d04bc973d7d34cd3a86798bbfbd85b39f144e2
javierng2k <javierng2knus@gmail.com>
2024-02-05 15:00:01 +0800

    Add JUnit tests for Deadlines and Events
    Add JUnit tests for Deadlines and Events to test the behavior of the respective classes.
    
    There are cases where construction of a deadline or an event could result in a error due to the invalid parameters.
    
    The addition of JUnit tests will allow for such bugs to be caught with greater ease.

--- src/test/java/jav/task/DeadlineTest.java
+++ src/test/java/jav/task/DeadlineTest.java
@@ -1,20 +1,33 @@
 package jav.task;
 
-// Temp class for testing junit.
-// TODO: Delete or repurpose for actual tests.
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class DeadlineTest {
     @Test
-    public void DummyTestTrue() {
-        assertEquals(2, 2);
+    public void deadline_constructor_invalidParam() {
+        // Params need to follow format
+        try {
+            assertEquals(0, new Deadline("", false));
+            fail(); // the test should not reach this line
+        } catch (Exception e) {
+            assertEquals("Invalid param for deadline", e.getMessage());
+        }
     }
-
+    
     @Test
-    public void DummyTestFalse() {
-        assertEquals(1, 2);
+    public void deadline_updateMark_true() {
+        Deadline d = new Deadline("do smt /by tmr", false);
+        d.updateMark(true);
+        assertEquals(d.isMarked(), true);
+    }
+    
+    @Test
+    public void deadline_updateMark_false() {
+        Deadline d = new Deadline("do smt /by tmr", true);
+        d.updateMark(false);
+        assertEquals(d.isMarked(), false);
     }
 }
