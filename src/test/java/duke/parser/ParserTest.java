311e752bb83dcdffe00eb6585268dd4c92afaa96
iamtr <jettzeong@gmail.com>
2024-02-05 12:55:33 +0800

    Add ParserTest for testing Parser

---
+++ src/test/java/duke/parser/ParserTest.java
@@ -0,0 +1,53 @@
+package duke.parser;
+
+import duke.storage.Storage;
+import duke.ui.Ui;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+    private Parser parser;
+    private Ui ui;
+    private Storage storage;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @BeforeEach
+    public void setUp() {
+        parser = new Parser();
+        ui = new Ui();
+        storage = new Storage();
+        System.setOut(new PrintStream(outContent)); // Redirect System.out to capture the output for testing
+    }
+
+    @Test
+    public void testMarkTaskAsDone() {
+        // Assuming there's already a task in the list
+        parser.parseCommand("todo read book", ui, storage); // Add a task first
+        String markInput = "mark 1";
+        parser.parseCommand(markInput, ui, storage);
+        assertTrue(outContent.toString().contains("Nice! I've marked this task as done"));
+    }
+
+    @Test
+    public void testAddTodo() {
+        String input = "todo read book";
+        parser.parseCommand(input, ui, storage);
+        assertTrue(outContent.toString().contains("added todo: [T][ ] read book"));
+    }
+
+    @Test
+    public void testInvalidCommand() {
+        String input = "dance";
+        parser.parseCommand(input, ui, storage);
+        assertTrue(outContent.toString().contains("OOPS!!! I'm sorry, but I don't know what that means"));
+    }
+
+    @Test
+    public void testEmptyTodo() {
+        parser.parseCommand("todo", ui, storage);
+        assertTrue(outContent.toString().contains("OOPS!!! The description of a todo cannot be empty."));
+    }
+}
