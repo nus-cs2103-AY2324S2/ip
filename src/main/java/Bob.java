96a82ab1788cd7810e6df69fdaa661ac72fdc4e6
quelinxiao <122249170+quelinxiao@users.noreply.github.com>
2024-02-06 01:02:48 +0800

    Add mark and unmark

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -11,7 +11,7 @@ public class Bob {
         System.out.println(greet);
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>(100);
+        ArrayList<Task> taskList = new ArrayList<>(100);
 
         while (true) {
             String input = scanner.nextLine();
@@ -22,19 +22,76 @@ public class Bob {
             }
 
             if (input.equals("list")) {
-                int size = list.size();
+                int size = taskList.size();
 
                 for (int count = 0; count < size; count++) {
-                    System.out.println(" " + (count + 1) + ". " + list.get(count));
+                    System.out.println(" " + (count + 1) + "." + taskList.get(count));
                 }
             }
 
+            else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task task = taskList.get(index);
+                task.markAsDone();
+
+                System.out.println(" Nice! I've marked this task as done:\n"
+                        + "  " + task);
+            }
+
+            else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task task = taskList.get(index);
+                task.markAsUndone();
+
+                System.out.println(" OK, I've marked this task as not done yet:\n"
+                        + "  " + task);
+            }
+
             else {
-                list.add(input);
+                taskList.add(new Task(input));
                 System.out.println(" added: " + input);
             }
         }
 
         scanner.close();
+    }
+}
+
+/*
+ * This class represents a task we want to record.
+ */
+class Task {
+    private String description;
+    private boolean isDone;
+
+    /*
+     * A constructor that depicts a new task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /*
+     * A method that marks a task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /*
+     * A method that will undo the mark on a task.
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    /*
+     * A method that returns the task status as a string.
+     * @return A check-box followed by the description of the task.
+     */
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
     }
 }
