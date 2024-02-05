ae39bfe00c594199c8482dea6e6207798916a97e
blaukc <brandonlau1@hotmail.com>
2024-02-05 14:54:27 +0800

    Add FXML and style changes

---
+++ src/main/java/dude/Launcher.java
@@ -0,0 +1,13 @@
+package dude;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    /**
+     * Runs the Dude program.
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
--- src/main/java/dude/Main.java
+++ src/main/java/dude/Main.java
@@ -1,13 +1,31 @@
 package dude;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class Main {
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
 
-    /**
-     * Runs the Dude program.
-     */
-    public static void main(String[] args) {
-        Application.launch(Gui.class, args);
+    private Dude dude = new Dude();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDude(dude);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
