2f8fddb3e31ce32d2719468348bd5ecfa636d78f
kaitinghh <114789615+kaitinghh@users.noreply.github.com>
2024-02-06 00:42:11 +0800

    Convert datetime String to LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String deadline;
-    public Deadline(String s, String deadline) {
+    private LocalDateTime deadline;
+    private static final DateTimeFormatter ORIGINAL_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyyHHmm");
+    public Deadline(String s, String deadlineString) {
         super(s);
+
+        String cleanedString = deadlineString.replaceAll("\\s", "");
+
+        LocalDateTime deadline = LocalDateTime.parse(cleanedString, ORIGINAL_FORMATTER);
         this.deadline = deadline;
     }
 
@@ -12,6 +20,7 @@ public class Deadline extends Task {
 
     @Override
     public String convertToDataStoreLine() {
-        return String.format("D|%s|%s|%s", super.convertToDataStoreLine(), super.getTaskString(), this.deadline);
+        return String.format("D|%s|%s|%s", super.convertToDataStoreLine(), super.getTaskString(),
+                this.deadline.format(ORIGINAL_FORMATTER));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Event extends Task {
-    private String startTime;
-    private String endTime;
-    public Event(String s, String startTime, String endTime) {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private static final DateTimeFormatter ORIGINAL_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyyHHmm");
+    public Event(String s, String startTimeString, String endTimeString) {
         super(s);
+
+        startTimeString = startTimeString.replaceAll("\\s", "");
+        DateTimeFormatter originalFormatter = DateTimeFormatter.ofPattern("d/M/yyyyHHmm");
+        LocalDateTime startTime = LocalDateTime.parse(startTimeString, originalFormatter);
+
+        endTimeString = endTimeString.replaceAll("\\s", "");
+        LocalDateTime endTime = LocalDateTime.parse(endTimeString, originalFormatter);
+
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
     @Override
     public String toString() {
-        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.startTime, this.endTime);
+        return String.format("[E] %s (from: %s to: %s)", super.toString(),
+                this.startTime.format(ORIGINAL_FORMATTER),
+                this.endTime.format(ORIGINAL_FORMATTER));
     }
 
     @Override
