cce34ff063d1a463cac30faa557397bf1a208434
tahsinhasem <tahsinhasem@gmail.com>
2024-02-06 02:34:09 +0800

    Changed TaskListFullException to be public

--- src/main/java/TaskListFullException.java
+++ src/main/java/TaskListFullException.java
@@ -1,4 +1,4 @@
-class TaskListFullException extends Exception {
+public class TaskListFullException extends Exception {
     //Used to indicate that the task list is full
     public TaskListFullException(String message) {
         super(message);
