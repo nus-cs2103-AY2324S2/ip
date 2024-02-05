e71150fa675025d83e8f13a3ebaec9eefa2096d1
ShanylOng <shanylong33@gmail.com>
2024-02-05 10:46:51 +0800

    Added the echo function, and indentations to the output. Minor changes to the wording of the greet and exit functions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,34 @@
+import java.util.Scanner;
 public class Duke {
     private static void greet() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Barry.\n" + "What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tGreetings! I'm Barry.\n" + "\tHow could I assist you?");
+        System.out.println("\t____________________________________________________________\n");
     }
 
     private static void exit() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye, see you next time! :)");
+        System.out.println("\t____________________________________________________________\n");
     }
 
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // String input
+            String inputText = scanner.nextLine().strip();
+            if (inputText.equals("bye")) {
+                break;
+            } else {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t" + inputText);
+                System.out.println("\t____________________________________________________________\n");
+            }
+        }
+    }
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
         greet();
+        echo();
         exit();
     }
 }
