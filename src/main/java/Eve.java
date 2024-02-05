fe0ad512373e93ba571cf75097d7a08f6c5cc74d
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-02-05 18:30:36 +0800

    Attempting File Storage

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -1,10 +1,32 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Eve {
+
+    private static final String directoryPath = "./data";
+    private static final String filePath = "./data/Eve.txt";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Hello! I'm Eve");
         System.out.println(" What can I do for you?");
+        
+        //from CS2103T website
+        try {
+            printFileContents(filePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        String file2 = "./temp/lines.txt";
+        try {
+            writeToFile(file2, "first line" + System.lineSeparator() + "second line");
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
 
         String input = "";
         ArrayList<Task> list = new ArrayList<>();
@@ -143,5 +165,27 @@ public class Eve {
         System.out.println(t.toString());
         System.out.println("Now you have " + list.size() +" tasks in the list.");
     }
+
+    // The below functions are from CS2103T website Week 3
+    private static void printFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        while (s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+    }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
+        fw.write(textToAppend);
+        fw.close();
+    }
+    
 
 }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,8 @@
+// will split into this next time
+
+public class Storage {
+    private static final String directoryPath = "./data";
+    private static final String filePath = "./data/Eve.txt";
+
+
+}
