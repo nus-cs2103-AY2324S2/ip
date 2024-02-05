f286c21220f95a1964513e8ebddd930a6799db8a
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-02-05 10:04:26 +0800

    Add JavaDoc headers to Event.java

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,13 +1,26 @@
 package duke;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * The Event class represents a task with a start and end time.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
     private static Ui ui = new Ui();
 
+    /**
+     * Constructs a new Event object.
+     *
+     * @param description The description of the task.
+     * @param from        The start time of the task, in the format "dd/MM/yyyy
+     *                    HHmm".
+     * @param to          The end time of the task, in the format "dd/MM/yyyy HHmm".
+     * @throws DukeException If the start or end time is not in the correct format.
+     */
     public Event(String description, String from, String to) throws DukeException {
         super(description);
         try {
@@ -26,6 +39,12 @@ public class Event extends Task {
                 + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mma"));
     }
 
+    /**
+     * Creates a new Event object from a string.
+     *
+     * @param input The string to create the Event object from.
+     * @return A new Event object, or null if the Event object could not be created.
+     */
     public static Event fromString(String input) {
         String[] split = input.split(" \\| ");
         try {
@@ -40,6 +59,11 @@ public class Event extends Task {
         }
     }
 
+    /**
+     * Returns a string representation of the task that can be written to a file.
+     *
+     * @return A string representation of the task that can be written to a file.
+     */
     public String toFileString() {
         return "E | " + super.toString() + " | " + from.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm")) + " | "
                 + to.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
