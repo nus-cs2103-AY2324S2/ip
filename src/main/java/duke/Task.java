584e4651bd64c984fede7ac17fd41807fbeeb7a3
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-02-05 10:27:27 +0800

    Add JavaDoc headers to Task class

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,19 +1,34 @@
 package duke;
+
+/**
+ * The Task class represents a task with a description and a completion status.
+ */
 public class Task {
     private String description;
     private boolean isDone;
     private Ui ui = new Ui();
 
+    /**
+     * Constructs a new Task object.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done and prints a message.
+     */
     public void markAsDone() {
         this.isDone = true;
         ui.printMessage("Nice! I've marked this task as done: \n" + this.toString() + "\n");
     }
 
+    /**
+     * Unmarks the task as done and prints a message.
+     */
     public void unmarkDone() {
         this.isDone = false;
         ui.printMessage("OK, I've marked this task as not done yet: \n" + this.toString() + "\n");
@@ -24,6 +39,12 @@ public class Task {
         return (isDone ? "X" : " ") + " | " + description;
     }
 
+    /**
+     * Creates a Task object from a string.
+     *
+     * @param input The string to parse.
+     * @return A new Task object.
+     */
     public static Task fromString(String input) {
         Task task;
         if (input.startsWith("T")) {
@@ -36,6 +57,11 @@ public class Task {
         return task;
     }
 
+    /**
+     * Converts the task to a string for file storage.
+     *
+     * @return A string representation of the task for file storage.
+     */
     public String toFileString() {
         return toString();
     }
