cface250f0b4410314304ae28323c623d7946f4b
tahsinhasem <tahsinhasem@gmail.com>
2024-02-06 02:15:54 +0800

    Added Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+        private String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        }
+}
