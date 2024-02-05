3788eb8f8f25ae8f23a6904a9ac37b6274c75898
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-02-05 20:25:54 +0800

    Added mark and unmark functionality to Charmbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,18 +14,37 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
             System.out.println("____________________________________________________________");
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
+                }
+            } else if (command.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(command.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks[taskIndex]);
+                } else {
+                    System.out.println("Invalid task index.");
+                }
+            } else if (command.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(command.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(tasks[taskIndex]);
+                } else {
+                    System.out.println("Invalid task index.");
                 }
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("added: " + command);
             }
@@ -36,5 +55,28 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+}
+
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
     }
 }
