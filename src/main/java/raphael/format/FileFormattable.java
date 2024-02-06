a9bd99e5def54b15a75683a5f6a0bd77fd4cbea8
JustWeiHao <e0960531@u.nus.edu>
2024-02-06 20:05:24 +0800

    Fix FileFormattable.java checkstyle

--- src/main/java/raphael/format/FileFormattable.java
+++ src/main/java/raphael/format/FileFormattable.java
@@ -1,5 +1,8 @@
 package raphael.format;
 
+/**
+ * Interface for object that is convertable into task file format.
+ */
 public interface FileFormattable {
     /**
      * Returns the string representation of the object that respect the task file format
