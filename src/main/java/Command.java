bdfdc45cfd515daeb10d60e96f5ca72393cd4bc9
AwJoey <awjoey26@gmail.com>
2024-02-06 22:33:47 +0800

    Command.java: Enum class for saving command names and description

---
+++ src/main/java/Command.java
@@ -0,0 +1,18 @@
+public enum Command {
+    TODO("todo", "todo <task_name>"),
+    DEADLINE("deadline", "deadline <task_name> /by <due_date>"),
+    EVENT("event","event <task_name> /from <start_date> /to <end_date"),
+    LIST("list", "list"),
+    MARK ("mark", "mark <task_number>"),
+    UNMARK("unmark", "unmark <task_number>"),
+    DELETE("delete", "delete <task_number>"),
+    TERMINATE("bye", "bye");
+
+    public final String commandName;
+    public final String commandSignature;
+
+    private Command(String commandName, String commandSignature) {
+        this.commandName = commandName;
+        this.commandSignature = commandSignature;
+    }
+}
