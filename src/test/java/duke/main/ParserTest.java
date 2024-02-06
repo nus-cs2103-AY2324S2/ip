0cf79387e9170bd6955efacf65206663ddd2efe3
chenyixin0 <e0968053@u.nus.edu>
2024-02-06 22:42:27 +0800

    Added JUnit testing

--- src/test/java/duke/main/ParserTest.java
+++ src/test/java/duke/main/ParserTest.java
@@ -1,2 +1,29 @@
-package duke.main;public class ParserTest {
+package duke.main;
+import duke.exception.DateFormatException;
+import duke.exception.UnknownInputException;
+import duke.task.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    TaskList tempList = new TaskList();
+    UI tempUI = new UI(tempList);
+    Parser tempParse = new Parser(tempList, tempUI);
+
+    @Test
+    public void parse_success() throws Exception {
+        tempParse.parse("todo read book");
+        assertEquals(new ToDo("read book").toString(), tempList.get(0).toString());
+    }
+
+    public void parse_unknownInput_exceptionThrown() {
+        String errorMessage = "Sorry, I don't know what that command means";
+        try {
+            tempParse.parse("this is an illegal input");
+        } catch (UnknownInputException e) {
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
 }
--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,2 +1,63 @@
-package duke.task;public class EventTest {
+package duke.task;
+
+import duke.exception.DateFormatException;
+import duke.main.TaskList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+
+public class EventTest {
+    @Test
+    public void event_constructor_success() throws Exception {
+        DateTimeFormatter returnFormat = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        LocalDateTime time1 = LocalDateTime.of(2024, 2, 3, 16, 00);
+        LocalDateTime time2 = LocalDateTime.of(2024, 2, 3, 18, 00);
+        String success = "[E] [ ] project meeting (" + returnFormat.format(time1) + " to " + returnFormat.format(time2) + ")";
+        assertEquals(success, new Event("project meeting", "3/2/2024 1600", "3/2/2024 1800").toString());
+    }
+
+    @Test
+    public void event_constructor_invalidDate_exceptionThrown() {
+        String errorMessage = "Please check the format of your date input! Accepted format: DD/MM/YYYY TTTT";
+        try {
+            new Event("project meeting", "3.2.24 4pm", "3.2.24 pm");
+            fail();
+        } catch (DateFormatException e) {
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    /*
+
+    @Test
+    public void intDivision_nonZeroDivisor_success() throws Exception {
+        // normal division results in an integer answer 2
+        assertEquals(2, new IntPair(4, 2).intDivision());
+
+        // normal division results in a decimal answer 1.9
+        assertEquals(1, new IntPair(19, 10).intDivision());
+
+        // dividend is zero but devisor is not
+        assertEquals(0, new IntPair(0, 5).intDivision());
+    }
+
+    @Test
+    public void intDivision_zeroDivisor_exceptionThrown() {
+        try {
+            assertEquals(0, new IntPair(1, 0).intDivision());
+            fail(); // the test should not reach this line
+        } catch (Exception e) {
+            assertEquals("Divisor is zero", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("4,7", new IntPair(4, 7).toString());
+    }
+     */
 }
