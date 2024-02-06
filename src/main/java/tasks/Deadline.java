b915f4cecd704ed3e137319874bc6d674cf24ca5
Vision-2000 <122250743+Vision-2000@users.noreply.github.com>
2024-02-06 18:41:53 +0800

    Modify task classes to use LocalDate instead of String

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,15 +1,18 @@
 package tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
-    public Deadline(String description, String by, String status) {
+    public Deadline(String description, LocalDate by, String status) {
         super(description, status);
         this.by = by;
     }
@@ -21,6 +24,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -1,16 +1,19 @@
 package tasks;
 
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task{
+    protected LocalDate from;
+    protected LocalDate to;
+
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public Event(String description, String from, String to, String status) {
+    public Event(String description, LocalDate from, LocalDate to, String status) {
         super(description, status);
         this.from = from;
         this.to = to;
@@ -23,6 +26,7 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to: " + to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
