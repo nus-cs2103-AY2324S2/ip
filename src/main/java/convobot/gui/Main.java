80a68274565aee707879ec460aea79e5e23c0d81
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-06 23:46:36 +0800

    Implement styling with dark theme and custom fonts

--- src/main/java/convobot/gui/Main.java
+++ src/main/java/convobot/gui/Main.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +24,9 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            Font.loadFont(getClass().getResourceAsStream("/styles/SourceSans3-Regular.otf"), 16);
+            Font.loadFont(getClass().getResourceAsStream("/styles/PeaxHandwritingbold.ttf"), 16);
+            scene.getStylesheets().add(getClass().getResource("/styles/dark-theme.css").toExternalForm());
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
