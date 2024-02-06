71228bc9ad047a00b43612546a98d58fcadfd028
AwJoey <awjoey26@gmail.com>
2024-02-06 22:58:47 +0800

    Add Storage.java: Operations to save/retrieve tasks from local file

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Array;
 import java.nio.file.Files;
 
@@ -8,57 +10,58 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Storage {
 
-    static String HOME = System.getProperty("user.home");
-    static Path FILEPATH = Paths.get(HOME, "Downloads", "NUS_CS",
+    private static final String HOME = System.getProperty("user.home");
+    private static final Path FILEPATH = Paths.get(HOME, "Downloads", "NUS_CS",
             "CS2103", "duke.txt");
 
-    public static ArrayList<Task> retrieveList() {
+    public Storage() {
+    }
+
+    public static ArrayList<Task> retrieveList() throws IOException {
         if (Files.exists(FILEPATH)) {
             ArrayList<Task> taskList = new ArrayList<>();
-            try (BufferedReader br = Files.newBufferedReader(FILEPATH)) {
-                String currentLine = null;
-                while ((currentLine = br.readLine()) != null) {
-                    String[] inputs = currentLine.split(",");
-                    try {
-                        boolean isDone = Boolean.parseBoolean(inputs[1]);
+            BufferedReader br = Files.newBufferedReader(FILEPATH);
+            String currentLine = null;
+            while ((currentLine = br.readLine()) != null) {
+                String[] inputs = currentLine.split(",");
+                try {
+                boolean isDone = Boolean.parseBoolean(inputs[1]);
 
-                        switch (inputs[0]) {
-                            case "T":
-                                ToDo t = new ToDo(inputs[2], isDone);
-                                taskList.add(t);
-                                break;
-                            case "D":
-                                LocalDateTime by = LocalDateTime.parse(inputs[3], Duke.dateTimeFormatter);
-                                Deadline d = new Deadline(inputs[2], isDone, by);
-                                taskList.add(d);
-                                break;
+                switch (inputs[0]) {
+                    case "T":
+                        ToDo t = new ToDo(inputs[2], isDone);
+                        taskList.add(t);
+                        break;
+                    case "D":
+                        LocalDateTime by = LocalDateTime.parse(inputs[3], Parser.dateTimeFormatter);
+                        Deadline d = new Deadline(inputs[2], isDone, by);
+                        taskList.add(d);
+                        break;
 
-                            case "E":
-                                LocalDateTime from = LocalDateTime.parse(inputs[3], Duke.dateTimeFormatter);
-                                LocalDateTime to = LocalDateTime.parse(inputs[4], Duke.dateTimeFormatter);
-                                Event e = new Event(inputs[0], isDone, from, to);
-                                taskList.add(e);
-                                break;
+                    case "E":
+                        LocalDateTime from = LocalDateTime.parse(inputs[3], Parser.dateTimeFormatter);
+                        LocalDateTime to = LocalDateTime.parse(inputs[4], Parser.dateTimeFormatter);
+                        Event e = new Event(inputs[0], isDone, from, to);
+                        taskList.add(e);
+                        break;
                         }
-                    } catch (IndexOutOfBoundsException e) {
-                        System.out.println("LOG: Task file corrupted.");
-                        return new ArrayList<>();
-                    } catch (DateTimeException e) {
-                        System.out.println("LOG: Dates of tasks corrupted.");
-                        return new ArrayList<>();
-                    }
+                } catch (DateTimeException e) {
+                    //System.out.println("LOG: Dates of tasks corrupted.");
+                    Ui.showLoadingError();
+                    return new ArrayList<>();
                 }
-            } catch (IOException e) {
-                System.out.println("No more tasks recorded.");
             }
             return taskList;
         } else {
-            System.out.println("LOG: File not found, empty list");
+            //System.out.println("LOG: File not found, empty list");
+            Ui.showLoadingError();
             return new ArrayList<Task>();
         }
     }
@@ -71,9 +74,9 @@ public class Storage {
         }
         try {
             Files.write(FILEPATH, taskContent);
-            System.out.println("LOG: Updated file contents.");
+//            System.out.println("LOG: Updated file contents.");
         } catch (IOException e) {
-            System.out.println("LOG: File cannot be found.");
+//            System.out.println("LOG: File cannot be found.");
         }
 
     }
@@ -81,10 +84,10 @@ public class Storage {
     public static void createFile() {
         try {
             Files.createFile(FILEPATH);
-            System.out.println("LOG: File created");
+//            System.out.println("LOG: File created");
         }
         catch (IOException e) {
-            System.out.println("LOG: File already exists.");
+//            System.out.println("LOG: File already exists.");
         }
 
     }
