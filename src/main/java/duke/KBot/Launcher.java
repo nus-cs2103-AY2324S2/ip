002889b5826b0eb1f11a614649a9540439c8c93a
c-wenlong <chenwenlongofficial@gmail.com>
2024-02-06 23:34:09 +0800

    Create GUI for chatbot

---
+++ src/main/java/duke/KBot/Launcher.java
@@ -0,0 +1,13 @@
+package duke.kbot;
+
+import javafx.application.Application;
+
+/**
+ * Entry point to the program JavaFx.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        TaskManager.loadLocalSavedTasks(); // checking if there are local files to load
+        Application.launch(Main.class, args);
+    }
+}
