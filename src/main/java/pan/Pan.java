a2b207f24401622c3c56bd0e3b1d0e2da057518a
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-02-06 15:12:40 +0800

    add javafx dependency and javafx boilerplate to main class

--- src/main/java/pan/Pan.java
+++ src/main/java/pan/Pan.java
@@ -2,17 +2,22 @@ package pan;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
- * Pan - Represents the Main Class
+ * Pan - Represents the Main Class that would override the Default Application cladss
  * @author Jerome Goh
  */
-public class Pan {
-    /**
-     * Main Entrypoint into the Pan application.
-     */
-    public static void main(String[] args) {
-        Parser parser = new Parser(new Ui(), new Scanner(System.in), new TaskList(new Storage()));
-        parser.parseInput();
-    }
+public class Pan extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!");
+        Scene scene = new Scene(helloWorld);
 
+        stage.setScene(scene);
+        stage.show();
+    }
 }
