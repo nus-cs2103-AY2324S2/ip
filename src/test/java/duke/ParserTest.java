a256630f50cee0c045099e4c3406b2c9fcb4df0f
yongkotaro <yongkotaro@gmail.com>
2024-02-06 10:51:12 +0800

    Added JUnit tests.

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,2 +1,29 @@
-package duke;public class ParserTest {
+package duke;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ParserTest {
+
+    @Test
+    public void testParseTodoCommand() {
+        Parser parser = new Parser();
+        Command command = parser.parse("todo Task description");
+        assertTrue(command != null);
+    }
+
+    @Test
+    public void testParseByeCommand() {
+        Parser parser = new Parser();
+        Command command = parser.parse("bye");
+        assertTrue(command instanceof ExitCommand);
+    }
+
+    @Test
+    public void testParseIncorrectCommand() {
+        Parser parser = new Parser();
+        Command command = parser.parse("randomCommand");
+        assertTrue(command instanceof IncorrectCommand);
+    }
 }
--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -1,4 +1,29 @@
 package duke;
 import org.junit.jupiter.api.Test;
-public class ToDoTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TaskTest {
+    @Test
+    public void testGetStatusIcon() {
+        Task taskUndone = new Task("", 0);
+        assertEquals(" ", taskUndone.getStatusIcon());
+
+        Task taskDone = new Task("", 1);
+        assertEquals("X", taskDone.getStatusIcon());
+    }
+
+    @Test
+    public void testMarkAsDone() {
+        Task task = new Task("Test Task", 0);
+        task.markAsDone();
+        assertEquals(1, task.isDone);
+    }
+
+    @Test
+    public void testMarkAsUndone() {
+        Task task = new Task("Test Task", 1);
+        task.markAsUndone();
+        assertEquals(0, task.isDone);
+    }
 }
