0c9a4e69a9cc96dd94e57a2232d399b6cb875506
Wang Xinrong <wangxinrong8@gmail.com>
2024-02-06 22:49:06 +0800

    add JavaFX Tutorial 2

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -10,9 +10,12 @@ import java.io.FileNotFoundException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +26,14 @@ public class Duke extends Application {
     private Parser parser = null;
     private TaskList taskList = null;
     private Storage storage = null;
+
+    // for GUI
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
 
     private enum LivState {
         ACTIVE,
@@ -41,20 +52,55 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        helloWorld.setFont(Font.font("Arial", FontWeight.BOLD, 50));
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-        Label notHelloWorld = new Label("not Hello World!"); // Creating a new Label control
-        notHelloWorld.setFont(Font.font("Arial", FontWeight.BOLD, 50));
-        Scene secondaryScene = new Scene(notHelloWorld); // Setting the scene to be our Label
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-        Stage secondaryStage = new Stage();
-        secondaryStage.setScene(secondaryScene); // Setting the stage to show our screen
-        secondaryStage.show(); // Render the stage.
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        //You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //More code to be added here later
+
     }
 
     /**
