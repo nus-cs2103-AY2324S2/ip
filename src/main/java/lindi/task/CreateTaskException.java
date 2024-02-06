ad369cf32c94c9a914c9597b60bc594be91697a3
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-02-07 01:00:28 +0800

    Improve Javadocs for task package

--- src/main/java/lindi/task/CreateTaskException.java
+++ src/main/java/lindi/task/CreateTaskException.java
@@ -1,5 +1,8 @@
 package lindi.task;
 
+/**
+ * Represents an Exception related to creation of a task
+ */
 public class CreateTaskException extends Exception {
     public CreateTaskException(String s) {
         super(s);
