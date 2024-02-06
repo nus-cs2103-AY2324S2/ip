8649c3d7be667993fb7961fba8faace582904ac1
JustWeiHao <e0960531@u.nus.edu>
2024-02-06 19:13:56 +0800

    Fix parser checkstyle

--- src/main/java/raphael/parser/Parser.java
+++ src/main/java/raphael/parser/Parser.java
@@ -1,18 +1,18 @@
 package raphael.parser;
 
+import raphael.command.AddCommand;
+import raphael.command.CheckCommand;
 import raphael.command.Command;
 import raphael.command.DeleteCommand;
+import raphael.command.EditCommand;
 import raphael.command.ExitCommand;
 import raphael.command.FindCommand;
-import raphael.command.CheckCommand;
-import raphael.command.EditCommand;
-import raphael.command.AddCommand;
 import raphael.command.ListCommand;
 import raphael.exception.RaphaelException;
-import raphael.task.Todo;
-import raphael.task.Event;
 import raphael.task.Deadline;
+import raphael.task.Event;
 import raphael.task.Task;
+import raphael.task.Todo;
 
 /**
  * Parses the command line input
@@ -32,58 +32,58 @@ public class Parser {
         }
         final String[] inputArr = input.split(" ", 2);
         switch(inputArr[0]) {
-            case "bye":
-                return new ExitCommand();
-            case "todo":
-                if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.TODO));
-                }
-                Task toDo = new Todo(inputArr[1]);
-                return new AddCommand(toDo);
-            case "deadline":
-                if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.DEADLINE));
-                }
-                final String[] descriptionDeadline = inputArr[1].split("/by ");
-                if (descriptionDeadline.length != 2
-                        || (descriptionDeadline[0] = descriptionDeadline[0].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.DEADLINE));
-                }
-                Task deadline = new Deadline(descriptionDeadline[0], descriptionDeadline[1]);
-                return new AddCommand(deadline);
-            case "event":
-                if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
-                }
-                final String[] descriptionTime = inputArr[1].split("/from ");
-                if (descriptionTime.length != 2
-                        || (descriptionTime[0] = descriptionTime[0].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
-                }
-                final String[] timeRange = descriptionTime[1].split("/to ");
-                if (timeRange.length != 2
-                        || (timeRange[0] = timeRange[0].trim()).isEmpty()) {
-                    throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
-                }
-                Event event = new Event(descriptionTime[0], timeRange[0], timeRange[1]);
-                return new AddCommand(event);
-            case "mark":
-                return Parser.processTaskWithIndex(inputArr, Command.TYPE.MARK);
-            case "unmark":
-                return Parser.processTaskWithIndex(inputArr, Command.TYPE.UNMARK);
-            case "delete":
-                return Parser.processTaskWithIndex(inputArr, Command.TYPE.DELETE);
-            case "list":
-                return new ListCommand();
-            case "any":
-                return new CheckCommand();
-            case "find":
-                if (inputArr.length == 1) {
-                    throw new raphael.exception.RaphaelException("find [keyword]");
-                }
-                return new FindCommand(inputArr[1]);
-            default:
-                throw new RaphaelException("I'm sorry that I can't recognize the command!");
+        case "bye":
+            return new ExitCommand();
+        case "todo":
+            if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.TODO));
+            }
+            Task toDo = new Todo(inputArr[1]);
+            return new AddCommand(toDo);
+        case "deadline":
+            if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.DEADLINE));
+            }
+            final String[] descriptionDeadline = inputArr[1].split("/by ");
+            if (descriptionDeadline.length != 2
+                    || (descriptionDeadline[0] = descriptionDeadline[0].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.DEADLINE));
+            }
+            Task deadline = new Deadline(descriptionDeadline[0], descriptionDeadline[1]);
+            return new AddCommand(deadline);
+        case "event":
+            if (inputArr.length == 1 || (inputArr[1] = inputArr[1].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
+            }
+            final String[] descriptionTime = inputArr[1].split("/from ");
+            if (descriptionTime.length != 2
+                    || (descriptionTime[0] = descriptionTime[0].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
+            }
+            final String[] timeRange = descriptionTime[1].split("/to ");
+            if (timeRange.length != 2
+                    || (timeRange[0] = timeRange[0].trim()).isEmpty()) {
+                throw new RaphaelException(RaphaelException.invalidFormat(Command.TYPE.EVENT));
+            }
+            Event event = new Event(descriptionTime[0], timeRange[0], timeRange[1]);
+            return new AddCommand(event);
+        case "mark":
+            return Parser.processTaskWithIndex(inputArr, Command.TYPE.MARK);
+        case "unmark":
+            return Parser.processTaskWithIndex(inputArr, Command.TYPE.UNMARK);
+        case "delete":
+            return Parser.processTaskWithIndex(inputArr, Command.TYPE.DELETE);
+        case "list":
+            return new ListCommand();
+        case "any":
+            return new CheckCommand();
+        case "find":
+            if (inputArr.length == 1) {
+                throw new raphael.exception.RaphaelException("find [keyword]");
+            }
+            return new FindCommand(inputArr[1]);
+        default:
+            throw new RaphaelException("I'm sorry that I can't recognize the command!");
         }
     }
     private static Command processTaskWithIndex(String[] inputArr, Command.TYPE commandType) throws RaphaelException {
