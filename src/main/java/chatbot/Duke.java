8001d25a52250db2dfbead73872a66c1ef885faa
moguries <celestehz03@gmail.com>
2024-02-07 07:53:36 +0800

    Add JavaDoc

--- src/main/java/chatbot/Duke.java
+++ src/main/java/chatbot/Duke.java
@@ -10,7 +10,9 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 /**
- * Encapsulates the data and behaviour of the chatbot.
+ * Encapsulates the data and behaviour of Fatnom.
+ * Fatnom is a chatbot application designed to help users manage their task lists.
+ * It provides functionalities to add, delete, mark, unmark, list and find tasks.
  *
  * @author Huang Zhuoyan, Celeste
  * @version CS2103T AY24/25 Semester 1, G07
@@ -21,6 +23,12 @@ public class Duke {
     private TaskList tasks;
     private Ui ui;
 
+    /**
+     * Constructs a new Duke object.
+     *
+     * @param filepath The file path of the storage file containing task data.
+     * @throws DukeException For initialisation errors while loading data from storage.
+     */
     public Duke(String filepath) throws DukeException {
         ui = new Ui();
         storage = new Storage(filepath);
@@ -32,6 +40,12 @@ public class Duke {
         }
     }
 
+    /**
+     * Runs the Fatnom application.
+     * Initialises the necessary components, displays a welcome message,
+     * loads data from storage (if any), and continuously reads and
+     * processes user input until the user exits the application.
+     */
     public void run() {
         Scanner sc = new Scanner(System.in);
         Ui.printWelcomeMessage(NAME);
@@ -52,6 +66,13 @@ public class Duke {
         }
     }
 
+    /**
+     * Starts the Fatnom application.
+     * Initialises a Duke object and runs the application.
+     *
+     * @param args The command-line arguments.
+     * @throws DukeException For initialisation errors.
+     */
     public static void main(String[] args) throws DukeException {
         new Duke("data/tasks.txt").run();
     }
