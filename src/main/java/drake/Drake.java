07b69498859c8e5e840859b97f196896647e80b2
Anant Shanker <shankeranant@gmail.com>
2024-02-07 01:43:12 +0800

    Add working GUI

--- src/main/java/drake/Drake.java
+++ src/main/java/drake/Drake.java
@@ -1,5 +1,6 @@
 package drake;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
@@ -10,6 +11,18 @@ import drake.task.Event;
 import drake.task.Task;
 import drake.task.TaskList;
 import drake.task.Todo;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 enum Command {
     BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, INVALID, FIND;
@@ -45,10 +58,17 @@ enum Command {
  * allows users to interact with their task list through various commands.
  */
 public class Drake {
-    private final Ui ui;
-    private final Storage storage;
-    private final TaskList tasks;
+    private Ui ui;
+    private Storage storage;
+    private TaskList tasks;
     private boolean isRunning;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/user.jpeg"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/drake.jpeg"));
 
     /**
      * Constructs a new instance of the Drake application.
@@ -63,83 +83,115 @@ public class Drake {
     }
 
     /**
-     * Starts the main command loop of the application. This method reads user input,
-     * processes commands, and interacts with the task list until the bye command is issued.
-     *
-     * @throws Exception if an unexpected error occurs during command processing.
+     * Constructs a new instance of the Drake application.
      */
-    public void run() throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        ui.showWelcome();
-        while (isRunning) {
-            String input = scanner.nextLine().trim();
-            String[] words = input.split(" ", 2);
-            String commandWord = words[0];
-            Command command = Command.fromString(commandWord);
-            try {
-                switch (command) {
-                case BYE:
-                    isRunning = false;
-                    storage.saveTasks(tasks.getTasks());
-                    ui.showGoodbye();
-                    break;
-                case LIST:
-                    ui.showTaskList(tasks);
-                    break;
-                case MARK:
-                    int markIndex = Parser.parseTaskIndex(input);
-                    tasks.markTask(markIndex);
-                    ui.showMarkTask(tasks.getTask(markIndex));
-                    break;
-                case UNMARK:
-                    int unmarkIndex = Parser.parseTaskIndex(input);
-                    tasks.unmarkTask(unmarkIndex);
-                    ui.showUnmarkTask(tasks.getTask(unmarkIndex));
-                    break;
-                case TODO:
-                    String todoDescription = Parser.parseDescription(input);
-                    Todo newTodo = new Todo(todoDescription);
-                    tasks.addTask(newTodo);
-                    ui.showAddTask(newTodo, tasks.size());
-                    break;
-                case DEADLINE:
-                    try {
-                        Object[] deadlineDetails = Parser.parseDeadline(input);
-                        Deadline newDeadline = new Deadline((String) deadlineDetails[0],
-                                (LocalDateTime) deadlineDetails[1]);
-                        tasks.addTask(newDeadline);
-                        ui.showAddTask(newDeadline, tasks.size());
-                    } catch (DateTimeParseException e) {
-                        ui.showError("Oops, format error! Type in a date in the form yy-mm-dd and try again!");
-                    }
-                    break;
-                case EVENT:
-                    String[] eventDetails = Parser.parseEvent(input);
-                    Event newEvent = new Event(eventDetails[0], eventDetails[1], eventDetails[2]);
-                    tasks.addTask(newEvent);
-                    ui.showAddTask(newEvent, tasks.size());
-                    break;
-                case DELETE:
-                    int deleteIndex = Parser.parseTaskIndex(input);
-                    Task deletedTask = tasks.deleteTask(deleteIndex);
-                    ui.showDeleteTask(deletedTask, tasks.size());
-                    break;
-                case FIND:
-                    String keyword = Parser.parseKeyword(input);
-                    ArrayList<Task> matchingTasks = tasks.findTasksByKeyword(keyword);
-                    ui.showMatchingTasks(matchingTasks);
-                    break;
-                default:
-                    throw new NotValidCommand("That's not a valid command!");
-                }
-            } catch (NotValidCommand | TodoLeftBlank e) {
-                ui.showError(e.getMessage());
+    public Drake() {
+        ui = new Ui();
+        storage = new Storage("./list.dat");
+        tasks = new TaskList(storage.loadTasks());
+        isRunning = true;
+    }
+
+    /**
+     * Gets the response from Drake.
+     *
+     * @param input the user's input.
+     * @return the response from Drake.
+     */
+    protected String getResponse(String input) {
+        String[] words = input.split(" ", 2);
+        String commandWord = words[0];
+        Command command = Command.fromString(commandWord);
+        try {
+            switch (command) {
+            case BYE:
+                return handleBye();
+            case LIST:
+                return handleList();
+            case MARK:
+                return handleMark(input);
+            case UNMARK:
+                return handleUnmark(input);
+            case TODO:
+                return handleTodo(input);
+            case DEADLINE:
+                return handleDeadline(input);
+            case EVENT:
+                return handleEvent(input);
+            case DELETE:
+                return handleDelete(input);
+            case FIND:
+                return handleFind(input);
+            default:
+                throw new NotValidCommand("That's not a valid command!");
             }
+        } catch (NotValidCommand | TodoLeftBlank e) {
+            return ui.showError(e.getMessage());
+        } catch (Exception e) { // General exception catch for unforeseen errors
+            return ui.showError("An unexpected error occurred.");
         }
-        scanner.close();
+    }
+    private String handleBye() throws IOException {
+        isRunning = false;
+        storage.saveTasks(tasks.getTasks());
+        return ui.showGoodbye();
+    }
+
+    private String handleList() {
+        return ui.showTaskList(tasks);
+    }
+
+    private String handleMark(String input) {
+        int markIndex = Parser.parseTaskIndex(input);
+        tasks.markTask(markIndex);
+        return ui.showMarkTask(tasks.getTask(markIndex));
+    }
+
+    private String handleUnmark(String input) {
+        int unmarkIndex = Parser.parseTaskIndex(input);
+        tasks.unmarkTask(unmarkIndex);
+        return ui.showUnmarkTask(tasks.getTask(unmarkIndex));
+    }
+
+    private String handleTodo(String input) {
+        String todoDescription = Parser.parseDescription(input);
+        Todo newTodo = new Todo(todoDescription);
+        tasks.addTask(newTodo);
+        return ui.showAddTask(newTodo, tasks.size());
+    }
+
+    private String handleDeadline(String input) {
+        try {
+            Object[] deadlineDetails = Parser.parseDeadline(input);
+            Deadline newDeadline = new Deadline((String) deadlineDetails[0],
+                    (LocalDateTime) deadlineDetails[1]);
+            tasks.addTask(newDeadline);
+            return ui.showAddTask(newDeadline, tasks.size());
+        } catch (Exception e) {
+            return ui.showError("Oops, format error! Type in a date in the form yy-mm-dd and try again!");
+        }
+    }
+
+    private String handleEvent(String input) {
+        String[] eventDetails = Parser.parseEvent(input);
+        Event newEvent = new Event(eventDetails[0], eventDetails[1], eventDetails[2]);
+        tasks.addTask(newEvent);
+        return ui.showAddTask(newEvent, tasks.size());
+    }
+
+    private String handleDelete(String input) {
+        int deleteIndex = Parser.parseTaskIndex(input);
+        Task deletedTask = tasks.deleteTask(deleteIndex);
+        return ui.showDeleteTask(deletedTask, tasks.size());
+    }
+
+    private String handleFind(String input) {
+        String keyword = Parser.parseKeyword(input);
+        ArrayList<Task> matchingTasks = tasks.findTasksByKeyword(keyword);
+        return ui.showMatchingTasks(matchingTasks);
     }
 
     public static void main(String[] args) throws Exception {
-        new Drake("./list.dat").run();
+        Application.launch(Main.class, args);
     }
 }
--- src/main/java/drake/Ui.java
+++ src/main/java/drake/Ui.java
@@ -10,114 +10,116 @@ import drake.task.TaskList;
 public class Ui {
 
     /**
-     * Displays a welcome message to the user.
+     * Returns a welcome message to the user.
      */
-    public void showWelcome() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" What's up everyone. I'm Drake.");
-        System.out.println(" How can I help?");
-        System.out.println("____________________________________________________________");
+    public static String showWelcome() {
+        return "____________________________________________________________\n" + " What's up everyone. I'm Drake.\n"
+                + " How can I help?\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays a goodbye message to the user.
+     * Returns a goodbye message to the user.
      */
-    public void showGoodbye() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" See you later, alligator! ");
-        System.out.println("____________________________________________________________");
+    public String showGoodbye() {
+        return "____________________________________________________________\n"
+                + " See you later, alligator! \n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays an error message.
+     * Returns an error message.
      *
-     * @param message The error message to be displayed.
+     * @param message The error message to be returned.
      */
-    public void showError(String message) {
-        System.out.println("____________________________________________________________");
-        System.out.println(message);
-        System.out.println("____________________________________________________________");
+    public String showError(String message) {
+        return "____________________________________________________________\n"
+                + message + "\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays the list of tasks.
+     * Returns the list of tasks as a string.
      *
      * @param tasks The TaskList containing the tasks to display.
      */
-    public void showTaskList(TaskList tasks) {
-        System.out.println("____________________________________________________________");
-        System.out.println("You asked for the tasks in your list? Here:");
+    public String showTaskList(TaskList tasks) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("____________________________________________________________\n");
+        sb.append("You asked for the tasks in your list? Here:\n");
         ArrayList<Task> taskList = tasks.getTasks();
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println((i + 1) + ". " + taskList.get(i));
+            sb.append(i + 1).append(". ").append(taskList.get(i)).append("\n");
         }
-        System.out.println("____________________________________________________________");
+        sb.append("____________________________________________________________");
+        return sb.toString();
     }
 
     /**
-     * Displays a message confirming the addition of a task, the task itself, and the new total number of tasks.
+     * Returns a message confirming the addition of a task, the task itself, and the new total number of tasks.
      *
      * @param task The task that has been added.
      * @param size The new total number of tasks in the list.
      */
-    public void showAddTask(Task task, int size) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Got it. I've added this task:");
-        System.out.println(task);
-        System.out.println("Now you have " + size + " tasks in the list.");
-        System.out.println("____________________________________________________________");
+    public String showAddTask(Task task, int size) {
+        return "____________________________________________________________\n"
+                + "Got it. I've added this task:\n"
+                + task + "\n"
+                + "Now you have " + size + " tasks in the list.\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays a message confirming that a task has been marked as done.
+     * Returns a message confirming that a task has been marked as done.
      *
      * @param task The task that has been marked.
      */
-    public void showMarkTask(Task task) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Cool. I now declare this task marked as, done:");
-        System.out.println(task);
-        System.out.println("____________________________________________________________");
+    public String showMarkTask(Task task) {
+        return "____________________________________________________________\n"
+                + "Cool. I now declare this task marked as, done:\n"
+                + task + "\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays a message confirming that a task has been marked as not done.
+     * Returns a message confirming that a task has been marked as not done.
      *
      * @param task The task that has been unmarked.
      */
-    public void showUnmarkTask(Task task) {
-        System.out.println("____________________________________________________________");
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(task);
-        System.out.println("____________________________________________________________");
+    public String showUnmarkTask(Task task) {
+        return "____________________________________________________________\n"
+                + "OK, I've marked this task as not done yet:\n"
+                + task + "\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays a message confirming the deletion of a task, the task itself, and the new total number of tasks.
+     * Returns a message confirming the deletion of a task, the task itself, and the new total number of tasks.
      *
      * @param task The task that has been deleted.
      * @param size The new total number of tasks in the list.
      */
-    public void showDeleteTask(Task task, int size) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(task);
-        System.out.println("Now you have " + size + " tasks in the list.");
-        System.out.println("____________________________________________________________");
+    public String showDeleteTask(Task task, int size) {
+        return "____________________________________________________________\n"
+                + "Noted. I've removed this task:\n"
+                + task + "\n"
+                + "Now you have " + size + " tasks in the list.\n"
+                + "____________________________________________________________";
     }
 
     /**
-     * Displays a message showing tasks that match the input.
+     * Returns a message showing tasks that match the input.
      *
      * @param matchingTasks The tasks that match the input.
      */
-    public void showMatchingTasks(ArrayList<Task> matchingTasks) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Here are the matching tasks in your list:");
+    public String showMatchingTasks(ArrayList<Task> matchingTasks) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("____________________________________________________________\n");
+        sb.append("Here are the matching tasks in your list:\n");
         for (int i = 0; i < matchingTasks.size(); i++) {
-            System.out.println((i + 1) + ". " + matchingTasks.get(i));
+            sb.append(i + 1).append(". ").append(matchingTasks.get(i)).append("\n");
         }
-        System.out.println("____________________________________________________________");
+        sb.append("____________________________________________________________");
+        return sb.toString();
     }
-
 }
