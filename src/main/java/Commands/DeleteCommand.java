c5a49395f246640719cc5543dc6011542df2e42c
Lalelulilulela <e0967877@u.nus.edu>
2024-02-06 17:09:01 +0800

    Add header comments to package Commands, Tasks, and Exceptions

--- src/main/java/Commands/DeleteCommand.java
+++ src/main/java/Commands/DeleteCommand.java
@@ -5,8 +5,22 @@ import TasksStorage.Storage;
 import TasksStorage.TaskList;
 import Ui.Ui;
 
-
+/**
+ * This class represents a command for deleting a task from the task list managed
+ * by the chatbot. When executed, this command removes the specified task from the
+ * task list based on its index.
+ */
 public class DeleteCommand extends Command {
+
+    /**
+     * Executes the command to remove a task from the list of recorded tasks.
+     *
+     * @param tasks Recorded list of tasks.
+     * @param ui Format of output shown.
+     * @param storage To save and load tasks recorded.
+     * @param message Input of user.
+     * @throws LeluException If the input is invalid or with the wrong format.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage, String message) throws LeluException {
         int i = Integer.parseInt(message.split(" ")[1]) - 1;
