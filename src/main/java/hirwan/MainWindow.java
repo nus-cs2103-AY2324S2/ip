0ced0792b31caa345b8fb112e7a4188fb4f9ab68
Alteqa <e0969816@u.nus.edu>
2024-02-06 19:48:42 +0800

    added GUI

--- src/main/java/hirwan/MainWindow.java
+++ src/main/java/hirwan/MainWindow.java
@@ -20,7 +20,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     private Button sendButton;
 
-    private Duke duke;
+    private Hirwan duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
@@ -31,7 +31,7 @@ public class MainWindow extends AnchorPane {
     }
 
     public void setDuke(Hirwan d) {
-        hirwan = d;
+        duke = d;
     }
 
     /**
@@ -41,7 +41,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = Hirwan.getResponse(input);
+        String response = duke.getResponse(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
