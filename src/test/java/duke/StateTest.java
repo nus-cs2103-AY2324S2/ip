521c88171856313ca6a8f73f7ef0cdf9d2f822c8
taufiq <taufiqmmhd@gmail.com>
2024-02-07 14:21:41 +0800

    test: Add StateTest test cases
    Added unit tests for State class to make sure State class works as expected.

---
+++ src/test/java/duke/StateTest.java
@@ -0,0 +1,34 @@
+package duke;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StateTest {
+    @Test
+    public void getTask_outOfBounds_returnsNull() {
+        State s = new State();
+        Task t = s.getTask(-1);
+        Assertions.assertNull(t);
+        Task t1 = s.getTask(1);
+        Assertions.assertNull(t1);
+    }
+
+    @Test
+    public void addTask_works() {
+        State s = new State();
+        Task t = new Task("",false);
+        s.addTask(t);
+        Task foundTask = s.getTasks().get(0);
+        Assertions.assertEquals(t, foundTask);
+    }
+
+    @Test
+    public void getTask_inBounds_returnsTask() {
+        State s = new State();
+        Task t = new Task("",false);
+        s.addTask(t);
+        Task t1 = s.getTask(0);
+        Assertions.assertEquals(t, t1);
+    }
+
+}
