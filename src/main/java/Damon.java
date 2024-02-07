bb601ef009e69b869b784475da7a0e054445eeca
Morninggloryyy <jinyanwu10@gmail.com>
2024-02-07 22:53:58 +0800

    Added commands to epochs commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Damon.java
+++ src/main/java/Damon.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Damon {
     public static void main(String[] args) {
         String logo = " ____\n"
@@ -10,9 +12,22 @@ public class Damon {
         System.out.println("__________________________________________________________\n"
                 + "Hello! Damon is here!\n"
                 + "What can I do for you\n"
-                + "__________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "__________________________________________________________\n");
 
+        while (true) {
+            //Solution below adapted from https://stackoverflow.com/a/16252290
+            Scanner scanner = new Scanner(System.in);
+            String inputString = scanner.nextLine();
+
+            if (inputString.equals("exit")) {
+                System.out.println("__________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "__________________________________________________________\n");
+                break;
+            }
+            System.out.println("__________________________________________________________\n"
+                    + inputString + "\n"
+                    + "__________________________________________________________\n");
+        }
     }
 }
