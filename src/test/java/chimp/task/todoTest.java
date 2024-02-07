553625d26026ee9d38c8e5235674208375bcadf6
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-02-08 00:21:57 +0800

    Setup dummy test

---
+++ src/test/java/chimp/task/todoTest.java
@@ -0,0 +1,12 @@
+package chimp.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class todoTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+}
