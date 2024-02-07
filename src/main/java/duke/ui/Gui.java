cff98e2c11cbcbdfd8d1ff3ca3caf779740bf818
shawnnlimm <shawnlim2525@gmail.com>
2024-02-07 14:20:10 +0800

    feat: Add GUI to chatbot using JavaFX

---
+++ src/main/java/duke/ui/Gui.java
@@ -0,0 +1,36 @@
+package duke.ui;
+
+import java.io.IOException;
+
+import duke.Duke;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+
+    private final Duke duke = new Duke();
+
+    /**
+     * Starts up the GUI.
+     *
+     * @param stage Stage to be used by GUI.
+     */
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setTitle("Waffles");
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            fxmlLoader.<MainWindow>getController().greetUser();
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/duke/ui/MainWindow.java
@@ -0,0 +1,67 @@
+package duke.ui;
+
+import duke.Duke;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.util.Objects;
+
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    private Duke duke;
+    private Image userImage = new Image(Objects.requireNonNull(
+            this.getClass().getResourceAsStream("/images/DaUser.png")));
+    private Image dukeImage = new Image(Objects.requireNonNull(
+            this.getClass().getResourceAsStream("/images/surprised_pikachu.png")));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /**
+     * Set the instance of Duke to be used by the GUI.
+     *
+     * @param d Duke instance to be used by GUI.
+     */
+    public void setDuke(Duke d) {
+        this.duke = d;
+    }
+
+    /**
+     * Greets the user upon loading up the GUI.
+     */
+    public void greetUser() {
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(duke.greet(), dukeImage)
+        );
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
