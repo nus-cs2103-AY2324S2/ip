c459d0fd5372ddfd929016b6a216599341ba5e9b
yyccbb <yuchenbo0130@gmail.com>
2024-02-07 17:45:10 +0800

    DeleteAction: support delete all

--- src/main/java/action/DeleteAction.java
+++ src/main/java/action/DeleteAction.java
@@ -7,16 +7,29 @@ import handler.PrintHandler;
 
 public class DeleteAction implements Action{
     private final int index;
+    private boolean isAll;
     public DeleteAction(String index) {
-        this.index = Integer.parseInt(index);
+        if (index.equals("all")) {
+            this.index = -1;
+            isAll = true;
+        } else {
+            this.index = Integer.parseInt(index);
+            isAll = false;
+        }
     }
+
     @Override
     public void execute() throws IndexOutOfBoundsException {
-        Task task = DataHandler.getTask(index);
-        DataHandler.deleteTask(index);
-        int noOfTasks = DataHandler.size();
-        PrintHandler.printWithDivider("Fine. I've removed this task:\n"
-                + "   " + task + "\n"
-                + "Now you have " + noOfTasks + " tasks.");
+        if (isAll) {
+            DataHandler.deleteAll();
+            PrintHandler.printWithDivider("Deleted everything for you. It was tiring :<");
+        } else {
+            Task task = DataHandler.getTask(index);
+            DataHandler.deleteTask(index);
+            int noOfTasks = DataHandler.size();
+            PrintHandler.printWithDivider("Fine. I've removed this task:\n"
+                    + "   " + task + "\n"
+                    + "Now you have " + noOfTasks + " tasks.");
+        }
     }
 }
