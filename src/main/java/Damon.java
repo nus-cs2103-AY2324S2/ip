dff91cccfb09d20c964157692f80a09c564e33c9
Morninggloryyy <jinyanwu10@gmail.com>
2024-02-07 21:17:13 +0800

    Renamed the chatbot and Duke.java, added Greets, Exit

---
+++ src/main/java/Damon.java
@@ -0,0 +1,18 @@
+public class Damon {
+    public static void main(String[] args) {
+        String logo = " ____\n"
+                + "|  _  \\\n"
+                + "| | | |\n"
+                + "| |_| |\n"
+                + "|____/ \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("__________________________________________________________\n"
+                + "Hello! Damon is here!\n"
+                + "What can I do for you\n"
+                + "__________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "__________________________________________________________\n");
+
+    }
+}
