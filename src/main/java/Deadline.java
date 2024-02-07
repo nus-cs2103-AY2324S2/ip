4a7ddf148ed7273e242c63a5e8c34eca654f0261
jasperng-nus <e0959074@u.nus.edu>
2024-02-07 22:37:00 +0800

    Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
-    public Deadline(String description, String by) {
+    protected LocalDateTime by;
+    public Deadline(String description, String by) throws DateException {
         super(description);
-        this.by = by;
+        DateTimeParser dateTimeParser = new DateTimeParser(by);
+
+        this.by = dateTimeParser.getDateTime();
+
     }
 
     @Override
     public String toDataString() {
-        return "D:" + super.toDataString() + ":" + this.by;
+        return "D:" + super.toDataString() + ":" + this.by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+
+    private String formatDateTime(LocalDateTime date) {
+        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + formatDateTime(this.by) + ")";
     }
 
 }
