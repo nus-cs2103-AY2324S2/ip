860d1d0ef959198be66211464953a58e7da124cf
yespiggy <skylarye7@gmail.com>
2024-02-08 00:43:20 +0800

    Level 10. Add a GUI to the chatbot

---
+++ src/main/java/duke/MainWindow.java
@@ -0,0 +1,57 @@
+package duke;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class MainWindow extends AnchorPane{
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duke duke;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaDog.jpeg"));
+    private Image botImage = new Image(this.getClass().getResourceAsStream("/images/DaMorningDog.jpeg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    public void handleUserInput() throws IOException {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        DialogBox user = DialogBox.getUserDialog(input, userImage);
+        DialogBox bot = DialogBox.getBotDialog(response, botImage);
+        dialogContainer.getChildren().addAll(user, bot);
+
+        VBox.setVgrow(bot, Priority.ALWAYS);
+
+        userInput.clear();
+    }
+
+}
