d09f72d4d63241a6c727c7f11808d74849852cd1
shunjieee <shunjie2000@gmail.com>
2024-02-07 16:19:08 +0800

    Add branch-Level-7 requirements
    Data can be read from a text file when the program is
    initialised. The text file is updated when the program
    terminated.
    
    Let's
    * create an instance of File to locate the text file
    * handle errors when locating the file, i.e. file / path
    does not exist
    * read existing data from the file and add them into the
    taskList, if any
    * update the text file when the program terminates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,10 @@ import helperpackage.Event;
 import helperpackage.Task;
 import helperpackage.ToDo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -23,11 +27,68 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("________________________________________\n");
 
+        /** Used to store Tasks */
+        LinkedList<Task> taskList = new LinkedList<>();
+
+        // Read file
+        File file = new File("./data/duke.txt");
+        int currentAttempt = 0;
+        int maxAttempt = 2;
+
+        while (++currentAttempt <= maxAttempt) {
+            try {
+                System.out.println("Startup Attempt #" + currentAttempt + "/" + maxAttempt + ":");
+                if (file.createNewFile()) {
+                    System.out.println("duke.txt does not exist.");
+                    System.out.println("duke.txt successfully created.");
+                } else {
+                    System.out.println("duke.txt exist.");
+                    System.out.println("duke.txt successfully loaded.");
+                }
+                break;
+            } catch (IOException e) {
+                System.out.println("IOException occured: " + e.getMessage());
+                
+                // creating directory 
+                File dir = new File("./data");
+                boolean isDirectoryCreated = dir.mkdir();
+                if (isDirectoryCreated) {
+                    System.out.println("Directory ./data created.");
+                }
+            } finally {
+                System.out.println("\n________________________________________\n");
+            }
+        }
+
+        // Reading inputs from the file
+        // e.g. E | 1 | resiDANCE | 6th Feb 8-10pm
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNext()) {
+                String[] tasks = fileScanner.nextLine().split(" \\| ");
+                String type = tasks[0];
+                boolean isDone =  Integer.parseInt(tasks[1]) == 1 ? true : false;
+                if (type.equals("T")) {
+                    ToDo td = new ToDo(isDone, tasks[2]);
+                    taskList.add(td);
+                } else if (type.equals("D")) {
+                    Deadline d = new Deadline(isDone, tasks[2], tasks[3]);
+                    taskList.add(d);
+                } else if (type.equals("E")) {
+                    Event e = new Event(isDone, tasks[2], tasks[3]);
+                    taskList.add(e);
+                }
+            }
+            fileScanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
-        /** Used to store Tasks */
-        LinkedList<Task> taskList = new LinkedList<>();
 
         // loop only exits if input is "bye"
         while (!userInput.toLowerCase().equals("bye")) {
@@ -97,7 +158,20 @@ public class Duke {
             }
             System.out.println("________________________________________\n");
             userInput = scanner.nextLine();
-        }    
+        }
+        
+        // writing to file
+        try {
+            FileWriter fw = new FileWriter(file, false);
+            for (Task t : taskList) {
+                fw.write(t.toData());
+                fw.write(System.lineSeparator());
+            }
+            fw.close();
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("\n________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
@@ -107,7 +181,8 @@ public class Duke {
     }
 
     // Level-3: mark & unmark
-    public static void changeStatus(LinkedList<Task> taskList, String cmd, StringTokenizer st) throws IndexOutOfBoundsException,
+    public static void changeStatus(LinkedList<Task> taskList, String cmd, 
+            StringTokenizer st) throws IndexOutOfBoundsException, 
             NumberFormatException, NoSuchElementException {
         
         int index = Integer.parseInt(st.nextToken());
@@ -126,7 +201,9 @@ public class Duke {
     }
 
     // Level-4: ToDo, Deadline, Event
-    public static void addTask(LinkedList<Task> taskList, String cmd, String userInput) throws DukeException {
+    public static void addTask(LinkedList<Task> taskList, String cmd, 
+            String userInput) throws DukeException {
+
         int firstSpaceIndex = userInput.indexOf(" ");
         String description = userInput.substring(firstSpaceIndex + 1);
         Task t = new Task(" ");
@@ -153,14 +230,15 @@ public class Duke {
     }
 
     // Level-6: Delete
-    public static void delete (LinkedList<Task> taskList, String cmd, StringTokenizer st) throws IndexOutOfBoundsException,
+    public static void delete (LinkedList<Task> taskList, String cmd, 
+            StringTokenizer st) throws IndexOutOfBoundsException,
             NumberFormatException, NoSuchElementException {
 
-    int index = Integer.parseInt(st.nextToken());
-    Task t = taskList.remove(index - 1);
+        int index = Integer.parseInt(st.nextToken());
+        Task t = taskList.remove(index - 1);
 
-    System.out.println("Noted, I've removed this task:");
-    System.out.println(" " + t.toString());
-    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-}
+        System.out.println("Noted, I've removed this task:");
+        System.out.println(" " + t.toString());
+        System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+    }
 }
