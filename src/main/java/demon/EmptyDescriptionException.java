a0ca9099f97dc091fbde94613052dc085d64a134
DESKTOP-KIADS1H\Chuan Yao <kaizerfang2@gmail.com>
2024-02-07 12:12:04 +0800

    Refactor: Introduce DemonException class for custom exceptions

--- src/main/java/demon/EmptyDescriptionException.java
+++ src/main/java/demon/EmptyDescriptionException.java
@@ -3,7 +3,7 @@ package demon;
 /**
  * An exception class that will be thrown when description of a task is not provided from the input.
  */
-public class EmptyDescriptionException extends Exception {
+public class EmptyDescriptionException extends DemonException {
     public EmptyDescriptionException(String msg) {
         super(msg);
     }
