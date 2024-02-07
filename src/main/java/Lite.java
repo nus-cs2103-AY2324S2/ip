914b0da1637c3a61754f22c1bc5cb52706a6abdd
Ryan-Reno <e0960450@nus.edu.sg>
2024-02-07 16:24:47 +0800

    Renamed the chatbot

---
+++ src/main/java/Lite.java
@@ -0,0 +1,13 @@
+public class Lite {
+    public static void main(String[] args) {
+        String logo = "  LLLLL      IIIIIIIII  TTTTTTTTTT  EEEEEEEEEE  \n" +
+                "  LLLLL         III         TTT     EEEEEEEEEE  \n" +
+                "  LLLLL         III         TTT     EEE         \n" +
+                "  LLLLL         III         TTT     EEEEEEEE   \n" +
+                "  LLLLL         III         TTT     EEEEEEEE   \n" +
+                "  LLLLL         III         TTT     EEE        \n" +
+                "  LLLLLLLLL  IIIIIIIII      TTT     EEEEEEEEEE  \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
