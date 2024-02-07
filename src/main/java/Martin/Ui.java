a52adeb83f69d531c7c8e8b5c67d2eda592044d3
yunchao <tanyunchao@gmail.com>
2024-02-08 02:14:55 +0800

    Add greeting and goodbye messages to Ui class

--- src/main/java/Martin/Ui.java
+++ src/main/java/Martin/Ui.java
@@ -1,15 +1,30 @@
 package Martin;
 
+/**
+ * The Ui class represents the user interface of the application.
+ * It provides methods to interact with the user and display messages.
+ */
 public class Ui {
+    /**
+     * Constructs a new Ui object.
+     */
     public Ui() {
 
     }
 
+    /**
+     * Displays a greeting message to the user.
+     * The greeting message includes the name "Martin" and a prompt for user input.
+     */
     public void sayGreeting() {
         System.out.println("Hello from Martin");
         System.out.println("What can I do for you?");
     }
 
+    /**
+     * Displays a goodbye message to the user.
+     * The goodbye message includes the name "Martin".
+     */
     public void sayBye() {
         System.out.println("Bye from Martin");
     }
