9a619cc38386f6eaeb95f1e7e5183bd108952384
xuwen-ho <ho.xu.wen@gmail.com>
2024-02-07 16:45:24 +0800

    Add javadocs to the Main class
    Previously, no javadocs were present
    
    This made the main class and its constituent methods difficult to understand their usage
    
    Lets make classes and their methods clearer in terms of their usage

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -5,13 +5,21 @@ import duke.task.TaskList;
 
 import java.io.FileNotFoundException;
 
+/**
+ * The Main class initializes and runs the Duke application.
+ */
 public class Main {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
     private String filePath;
-    
+
+    /**
+     * Constructs a Main object with the specified file path.
+     *
+     * @param filePath The file path to the list storage file.
+     */
     public Main(String filePath) {
         System.out.println("main run");
         this.filePath = filePath;
@@ -20,22 +28,28 @@ public class Main {
         storage = new Storage(filePath, taskList);
         try {
             taskList.initialisePrevTaskList(storage.readFromFile());
-
         } catch (FileNotFoundException e) {
-//            ui.showLoadingError();
+            // ui.showLoadingError();
             System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * Starts the Duke application.
+     */
     public void run() {
         // tasks added to tasklist already
         ui.start();
 
         // then we store it
         storage.writeToFile(filePath);
-
     }
 
+    /**
+     * The main method to run the Duke application.
+     *
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
         new Main("data/hal.txt").run();
     }
