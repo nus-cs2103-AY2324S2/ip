f779cb6d02c8879c48d2c0512fff794d3eba6527
Ryan-Reno <e0960450@nus.edu.sg>
2024-02-07 17:44:32 +0800

    Added a list that stores tasks

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -3,40 +3,71 @@ import java.util.Scanner;
 public class Chatbot {
     private Scanner scanner = new Scanner(System.in);
     private String currentInput;
+    private String[] list;
+    private int numOfItems;
+
 
     public Chatbot() {
         this.currentInput = "";
+        this.list = new String[100];
+        this.numOfItems = 0;
     }
 
     public void start() {
         greetings();
         this.currentInput = scanner.nextLine();
         while (!this.currentInput.equals("bye")) {
-            echo();
+            parseInput();
             this.currentInput = scanner.nextLine();
         }
         exit();
     }
 
-    public void echo() {
-        String msg = "____________________________________________________________\n" +
-                this.currentInput + "\n" +
-                "____________________________________________________________\n";
-        System.out.println(msg);
+    private void parseInput() {
+        if (this.currentInput.equals("list")) {
+            outputList();
+        } else {
+            addToList();
+            echo();
+        }
     }
 
-    public void greetings() {
-        String msg = "____________________________________________________________\n" +
-                " Hello! I'm LITE\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
-        System.out.println(msg);
+    private void outputList() {
+        printHorizontalLine();
+        for (int i = 0; i < this.numOfItems; i++) {
+            System.out.println((i+1) + ". " + this.list[i]);
+        }
+        printHorizontalLine();
     }
 
-    public void exit() {
-        String msg = "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+    private void addToList() {
+        this.list[this.numOfItems] = this.currentInput;
+        this.numOfItems++;
+    }
+
+    private void echo() {
+        printHorizontalLine();
+        String msg = "added: " + this.currentInput;
         System.out.println(msg);
+        printHorizontalLine();
+    }
+
+    private void greetings() {
+        printHorizontalLine();
+        String msg = " Hello! I'm LITE\n" +
+                " What can I do for you?" ;
+        System.out.println(msg);
+        printHorizontalLine();
+    }
+
+    private void exit() {
+        printHorizontalLine();
+        String msg = " Bye. Hope to see you again soon!";
+        System.out.println(msg);
+        printHorizontalLine();
+    }
+
+    private void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
     }
 }
