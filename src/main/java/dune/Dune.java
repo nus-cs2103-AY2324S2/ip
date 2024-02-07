f52f5ca8e15b6c39fa28dc1dee46e152e19f1736
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-02-07 23:51:34 +0800

    Set up Java FX
    Level 4

--- src/main/java/dune/Dune.java
+++ src/main/java/dune/Dune.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
 /**
  * The main class for the program.
  */
-public class Dune extends Application {
+public class Dune {
 
 
     private TaskList tasks;
@@ -67,75 +67,6 @@ public class Dune extends Application {
         }
     }
 
-    @Override
-    public void start(Stage stage) {
-
-        // Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        //You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-
-        // add the run logic here
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        //More code to be added here later
-    }
 
     /**
      * Iteration 1:
@@ -151,43 +82,12 @@ public class Dune extends Application {
         return textToAdd;
     }
 
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        // Get user input from text field
-        String userText = userInput.getText();
-        System.out.println(userText);
-
-        // Process user input
-        String duneText = getResponse(userText);
-        System.out.println(duneText);
-
-        Label user = new Label(userText);
-        Label dune = new Label(duneText);
-
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(user, new ImageView(userPic)),
-                DialogBox.getDukeDialog(dune, new ImageView(dunePic))
-        );
-        if (userText.equals("bye")) {
-            // Create a PauseTransition to introduce a delay
-            PauseTransition delay = new PauseTransition(Duration.seconds(3));
-            delay.setOnFinished(event -> Platform.exit());
-            delay.play();
-
-        }
-        userInput.clear();
+    public String response(String text) {
+        return this.parser.response(text, this.tasks, this.storage);
     }
 
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return this.parser.response(input, this.tasks, this.storage);
-    }
+
+
+
 
 }
---
+++ src/main/java/dune/MainWindow.java
@@ -0,0 +1,81 @@
+package dune;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Dune dune;
+
+    private Image userPic = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dunePic = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        this.scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Dune d) {
+        this.dune = d;
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        // Get user input from text field
+        String userText = userInput.getText();
+        System.out.println(userText);
+
+        // Process user input
+        String duneText = getResponse(userText);
+        System.out.println(duneText);
+
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userPic),
+                DialogBox.getDukeDialog(duneText, dunePic)
+        );
+        if (userText.equals("bye")) {
+            // Create a PauseTransition to introduce a delay
+            PauseTransition delay = new PauseTransition(Duration.seconds(3));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
+
+        }
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    private String getResponse(String input) {
+        return this.dune.response(input);
+    }
+}
+
