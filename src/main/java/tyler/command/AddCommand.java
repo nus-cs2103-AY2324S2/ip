c0461c6794c5955ffd77a69c58c34db7ecd33190
Ty-stan0417 <yisheng0417@gmail.com>
2024-02-08 03:40:08 +0800

    Renaming the folder

--- src/main/java/tyler/command/AddCommand.java
+++ src/main/java/tyler/command/AddCommand.java
@@ -1,6 +1,7 @@
 package tyler.command;
 
 public abstract class AddCommand extends Command {
+    
     protected String taskName;
     public AddCommand(String taskName) {
         this.taskName = taskName;
