a53b53c0fc597b9fa1edc31c73fcbcfbaba3df9f
Ryan-Reno <e0960450@nus.edu.sg>
2024-02-07 17:23:05 +0800

    Added echo

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,13 +1,40 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Chatbot {
-    public Chatbot() {}
+    private Scanner scanner = new Scanner(System.in);
+    private String currentInput;
+
+    public Chatbot() {
+        this.currentInput = "";
+    }
 
     public void start() {
+        greetings();
+        this.currentInput = scanner.nextLine();
+        while (!this.currentInput.equals("bye")) {
+            echo();
+            this.currentInput = scanner.nextLine();
+        }
+        exit();
+    }
+
+    public void echo() {
+        String msg = "____________________________________________________________\n" +
+                this.currentInput + "\n" +
+                "____________________________________________________________\n";
+        System.out.println(msg);
+    }
+
+    public void greetings() {
         String msg = "____________________________________________________________\n" +
                 " Hello! I'm LITE\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        System.out.println(msg);
+    }
+
+    public void exit() {
+        String msg = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(msg);
