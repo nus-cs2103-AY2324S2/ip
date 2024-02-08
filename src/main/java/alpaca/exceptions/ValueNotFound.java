f5cd5c111ca9f75c21e926745587ef9b7ff8c754
Zoe <za.zoeang@gmail.com>
2024-02-08 21:20:59 +0800

    Add jar file compatability

--- src/main/java/alpaca/exceptions/ValueNotFound.java
+++ src/main/java/alpaca/exceptions/ValueNotFound.java
@@ -1,7 +1,11 @@
 package alpaca.exceptions;
 
 public class ValueNotFound extends Exception {
-    public ValueNotFound(String errorMessage) {
+
+    /**
+    *  An error when a command is called without any information if said information is required
+    **/
+    public ValueNotFound(final String errorMessage) {
         super(errorMessage);
     }
 }
