a14d1ca491de6988511f63ca88e2d2e292eac80e
Shaun Lai <shaun@littlebit.sg>
2024-02-09 00:27:37 +0800

    Add tests for Parser and Storage classes

---
+++ src/test/java/lai/util/StorageTest.java
@@ -0,0 +1,31 @@
+package lai.util;
+
+import lai.task.Event;
+import lai.task.Task;
+import lai.task.ToDo;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StorageTest {
+    @Test
+    public void readTasksFileTest() throws IOException {
+        File file = File.createTempFile( "test-tasks", "txt");
+        file.deleteOnExit();
+
+        Task task = new Event("testing", "2024-01-01", "2024-02-02");
+        Task task2 = new ToDo("testing again");
+
+        FileWriter fw = new FileWriter(file);
+        fw.write(task + System.lineSeparator());
+        fw.write(task2 + System.lineSeparator());
+
+        Storage storage = new Storage(file.getPath());
+        List<Task> tasks = storage.readTasksFile();
+        assertEquals(tasks.get(0).toString(), task.toString());
+        assertEquals(tasks.get(1).toString(), task2.toString());
+    }
+}
