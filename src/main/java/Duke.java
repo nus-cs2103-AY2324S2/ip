c477636d2b65854ce857801788354ced195f74be
CJerrong <chong.jer.rong@gmail.com>
2024-02-09 12:25:27 +0800

    Add JavaFx Launcher

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,18 @@
 import haro.Haro;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 /**
  * The Duke class represents the main entry point for the Duke application.
  *
  */
-public class Duke {
+public class Duke extends Application {
+
+    public Duke() {
+        // New duke instance
+    }
+
     /**
      * Main method to execute the Duke application.
      * Initializes the Duke application with an instance of the Haro class
@@ -13,5 +22,14 @@ public class Duke {
     public static void main(String[] args) {
         Haro haro = new Haro("data/saveList.txt", "data/");
         haro.initialise();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
