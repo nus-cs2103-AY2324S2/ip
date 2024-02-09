c95ce752c211d6366f4c5fe39e0084e5483921fa
dhlee03 <dhclearsky@gmail.com>
2024-02-09 21:55:03 +0800

    fix merge error and add javadoc to exception

--- src/main/java/friendlytool/process/FTException.java
+++ src/main/java/friendlytool/process/FTException.java
@@ -1,6 +1,14 @@
 package friendlytool.process;
 
+/**
+ * Class for handling exceptions
+ */
 public class FTException extends Exception {
+    /**
+     * Prints out the message
+     *
+     * @param message
+     */
     public FTException(String message) {
         super(message);
     }
--- src/main/java/friendlytool/process/ftException.java
+++
@@ -1,7 +0,0 @@
-package friendlytool.process;
-
-public class FTException extends Exception {
-    public FTException(String message) {
-        super(message);
-    }
-}
