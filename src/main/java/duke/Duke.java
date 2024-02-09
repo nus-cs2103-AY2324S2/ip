b4197f3aa40ed6070dfebce10af95e3df41433fc
Chua Zi Long <e0957913@u.nus.edu>
2024-02-09 09:38:53 +0800

    Tweak the code to comply with the coding standard

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -816,7 +816,7 @@ class AddEventCommand extends Command {
     private LocalDateTime start;
     private LocalDateTime end;
 
-    public AddEventCommand(String description, LocalDateTime start, LocalDateTime end) {
+    public AddEventCommand(String description, LocalDateTime start, LocalDateTime end) { //
         this.description = description;
         this.start = start;
         this.end = end;
