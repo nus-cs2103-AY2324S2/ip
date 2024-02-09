c8e0fe0bcee04551fa7989fbd47bac52778e7879
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2024-02-09 22:21:27 +0800

    Attempt at JavaFX/GUI in vain

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,6 +2,10 @@ package duke;
 
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import parser.Parser;
 import processor.Processor;
 import tasks.TaskList;
@@ -10,7 +14,7 @@ import ui.Ui;
 /**
  * The Duke class is responsible for running the chatbot.
  */
-public class Duke {
+public class Duke extends Application{
     private final Ui chatbotUi;
     private final TaskList taskList;
     private final Processor processor;
@@ -49,10 +53,29 @@ public class Duke {
         System.out.println(chatbotUi.dividerWrapper(Ui.bye()));
     }
 
-    public static void main(String[] args) throws IOException {
-        Duke d = new Duke();
-        d.run();
+    /**
+     * Starts the chatbot.
+     * @param stage the stage to be used for the chatbot
+     * @throws IOException if an I/O error occurs while starting the chatbot
+     * @see Application#start(Stage)
+     */
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
+
+
+
+
+
+//    public static void main(String[] args) throws IOException {
+//         Duke d = new Duke();
+//         d.run();
+//    }
 
 }
 
