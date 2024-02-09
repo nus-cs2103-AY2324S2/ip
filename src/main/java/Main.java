a85a80903eb780179aee4ba3226a6703038c4c8b
belligerentbeagle <ethan.wei@u.nus.edu>
2024-02-09 14:46:18 +0800

    Update GUI with fxml loaders

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,33 @@
 import duke.Duke;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import view.MainWindow;
 
-public class Main {
-    
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]) {
         new Duke("./src/main/java/data/duke.txt").run();
     }
