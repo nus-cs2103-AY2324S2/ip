827571fc426fc4ce8556c440c6a60ef97d4b6f02
dillontkh <e0958762@u.nus.edu>
2024-02-09 23:20:05 +0800

    Use streams in FindCommand

--- src/main/java/nollid/commands/FindCommand.java
+++ src/main/java/nollid/commands/FindCommand.java
@@ -1,6 +1,8 @@
 package nollid.commands;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import nollid.Storage;
 import nollid.TaskList;
@@ -39,26 +41,22 @@ public class FindCommand extends Command {
     public String execute(TaskList tasks, Storage storage) throws NollidException {
         // If no keyword provided, throw error
         if (argsList.size() == 1) {
-            throw new InvalidArgumentException("Please enter a keyword to search for.\n"
-                    + FindCommand.USAGE_HINT);
+            throw new InvalidArgumentException("Please enter a keyword to search for.\n" + FindCommand.USAGE_HINT);
         }
 
         // If more than 1 keyword, throw error
         if (argsList.size() > 2) {
-            throw new InvalidArgumentException("Please enter only 1 keyword.\n"
-                    + FindCommand.USAGE_HINT);
+            throw new InvalidArgumentException("Please enter only 1 keyword.\n" + FindCommand.USAGE_HINT);
         }
 
         String keyword = argsList.get(1);
         // List to store tasks that contain keyword in their description
         TaskList results = new TaskList();
 
-        // Search through each task's description for the keyword
-        for (Task t : tasks) {
-            if (t.getDescription().contains(keyword)) {
-                results.add(t);
-            }
-        }
+        Stream<Task> taskStream = StreamSupport.stream(tasks.spliterator(), false);
+        taskStream
+                .filter(task -> task.getDescription().contains(keyword))
+                .forEach(results::add);
 
         // List items in the results list
         StringBuilder outputMessage = new StringBuilder("Here are the matching tasks in your list:\n");
