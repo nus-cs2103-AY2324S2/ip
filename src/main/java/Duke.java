35da6ebe8bc24ac9ccb0685bf6d0112b8f06b009
jyztintan <justintanwk2001@gmail.com>
2024-02-09 23:43:20 +0800

    Initiate JavaFX skeletal code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,15 @@ import main.java.Storage;
 
 import tasks.TaskList;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 /**
  * Duke is the main class for the task management application.
  * It initializes the application and starts the interaction loop with the user.
  */
-public class Duke {
+public class Duke extends Application {
 
     private Ui ui;
     private Storage storage;
@@ -32,6 +36,8 @@ public class Duke {
         run();
 
     }
+
+    public Duke() {}
 
     /**
      * Starts the application and enters the command processing loop.
@@ -67,6 +73,17 @@ public class Duke {
      */
     public static void main(String[] args) {
         new Duke("./data/taskyapper.txt");
+
+    }
+
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
 }
