aa0ea5041b15e18602a1d85d36488521b697ec4f
rertyy <nxtms3@gmail.com>
2024-02-09 21:48:34 +0800

    Modify method name to be more informative

--- src/main/java/fx/MainWindow.java
+++ src/main/java/fx/MainWindow.java
@@ -45,7 +45,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = numerator.getResponse(input);
+        String response = numerator.getResponseFromInputString(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
