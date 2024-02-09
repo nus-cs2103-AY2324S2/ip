f1ff4a26bb8bb4e2a2e602630ea66a634c355915
Rahul <sithanathan.rahul@gmail.com>
2024-02-09 15:51:58 +0800

    Final code after gui

--- src/main/java/duke/rahbot/Launcher.java
+++ src/main/java/duke/rahbot/Launcher.java
@@ -11,27 +11,6 @@ import java.io.IOException;
  */
 public class Launcher {
     public static void main(String[] args) {
-        /*
-        Storage storage = new Storage();
-        Parser parser = new Parser(storage);
-        Output output = new Output(parser, storage);
-        System.out.println(output.welcome());
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println(output.execute(input));
-                break;
-            } else {
-                System.out.println(output.execute(input));
-            }
-            //writing into the file
-            try {
-                storage.writeToFile(storage.load());
-            } catch (IOException e) {
-                System.out.println("Something went wrong: " + e.getMessage());
-            }
-        }*/
         Application.launch(Duke.class, args);
     }
 }
