2f5d720eb91d59b7535bc0e245ac3f79f2aa780e
dillontkh <e0958762@u.nus.edu>
2024-02-09 12:33:56 +0800

    Remove unused imports

--- src/test/java/nollid/TaskListTest.java
+++ src/test/java/nollid/TaskListTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import nollid.tasks.Deadline;
 import nollid.tasks.Todo;
 
 public class TaskListTest {
