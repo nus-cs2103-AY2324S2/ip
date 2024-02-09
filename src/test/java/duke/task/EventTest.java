6088e73676f5b912bf5c160f178fc28e016d49d8
H4mes <e0960344@u.nus.edu>
2024-02-09 14:41:55 +0800

    Add Junit tests for Parser and Event Classes

---
+++ src/test/java/duke/task/EventTest.java
@@ -0,0 +1,17 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class EventTest {
+    @Test
+    public void getData_success() {
+        LocalDateTime testFrom = LocalDateTime.of(2024, 2, 9, 18,00);
+        LocalDateTime testTo = LocalDateTime.of(2024, 2, 10, 9,00);
+        Event event = new Event("Test Event", testFrom, testTo);
+        String expectedData = "E | 0 | Test Event | 09/02/2024 1800 | 10/02/2024 0900";
+        assertEquals(event.getData(), expectedData);
+    }
+}
