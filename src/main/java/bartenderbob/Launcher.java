d379f921a9d878262a2d2527b9eec106215ae306
gavin331 <e0957939@u.nus.edu>
2024-02-09 13:20:54 +0800

    Update javafx UI

--- src/main/java/bartenderbob/Launcher.java
+++ src/main/java/bartenderbob/Launcher.java
@@ -8,6 +8,6 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         BartenderBob bartenderBob = new BartenderBob("./data/tasks.txt");
-        Application.launch(BartenderBob.class, args);
+        Application.launch(Main.class, args);
     }
 }
