1a3c1bd1b45b395081907774cfc944e45dd7f672
Tang Yetong <tangyetong666@gmail.com>
2024-02-10 12:34:12 +0800

    Current code base has slight inconsistencies in code quality
    Small refactoring to improve code quality in various classes
    
    Can continue with more intricate refactoring, currently only rough refactoring done

--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -13,19 +13,27 @@ import Exceptions.DukeException;
 public class Event extends Task {
     /**
      * Creation of event task
+     * 
      * @param description description with 2 dates included for parsing
      * @throws DukeException
      */
     public Event(String description) throws DukeException {
-        super(description);
+        super(parseEventDescription(description));
+    }
 
-        String desc = super.description;
-
-        if (!desc.contains("/from") || !desc.contains("/to")) {
+    /**
+     * Parses the event description to extract dates and format them properly
+     * 
+     * @param description the original description
+     * @return formatted event description
+     * @throws DukeException if there's an issue with the description format
+     */
+    private static String parseEventDescription(String description) throws DukeException {
+        if (!description.contains("/from") || !description.contains("/to")) {
             throw new DukeException("Dates missing in description!");
         }
 
-        String[] descParsed = desc.split("/from");
+        String[] descParsed = description.split("/from");
         String[] dateParsed = descParsed[1].split("/to");
 
         if (dateParsed.length <= 1 || dateParsed[0].isBlank() || dateParsed[1].isBlank()) {
@@ -36,19 +44,27 @@ public class Event extends Task {
             LocalDate date1 = LocalDate.parse(dateParsed[0].trim());
             LocalDate date2 = LocalDate.parse(dateParsed[1].trim());
 
-            String formattedDate1 = date1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-            String formattedDate2 = date2.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+            String formattedDate1 = formatDate(date1);
+            String formattedDate2 = formatDate(date2);
 
-            super.description = descParsed[0].trim() + " (from: " + formattedDate1 + " to: " + formattedDate2 + ")";
+            return descParsed[0].trim() + " (from: " + formattedDate1 + " to: " + formattedDate2 + ")";
         } catch (Exception e) {
-            // System.out.println("Unknown Date Format!");
-            super.description = descParsed[0].trim() + " (from: " + dateParsed[0].trim() + " to: "
-                    + dateParsed[1].trim() + ")";
+            return descParsed[0].trim() + " (from: " + dateParsed[0].trim() + " to: " + dateParsed[1].trim() + ")";
         }
+    }
+
+    /**
+     * Formats a LocalDate object into a string in the "MMM d yyyy" format
+     * 
+     * @param date the date to be formatted
+     * @return the formatted date string
+     */
+    private static String formatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[" + TaskType.E + "][" + super.getStatusIcon() + "] " + super.description;
+        return "[" + TaskType.E + "][" + super.getStatusIcon() + "] " + super.getDescription();
     }
 }
