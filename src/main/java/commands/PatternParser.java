828f9914a052a5083fe2a146ffe6a9c4bce97b6f
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-02-10 18:46:45 +0800

    Abstract the pattern regex from Storage class to adhere to SLAP
    Currently, storage class has no abstraction for the pattern regex
    and implementation to check for regex before adding to list of tasks
    is present alongside other abstraction.
    
    This violates SLAP and the method is simply too long.
    
    We abstract the pattern regex to PatternParser as earlier commits did.
    
    We adhere to OOP and make use of an already useful class that is made
    for such pattern regex abstraction.

--- src/main/java/commands/PatternParser.java
+++ src/main/java/commands/PatternParser.java
@@ -3,22 +3,29 @@ package commands;
 import exceptions.RyanGoslingException;
 import tasks.Deadline;
 import tasks.Events;
+import tasks.Task;
 import tasks.Todo;
 import utilities.Storage;
 import utilities.TaskList;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * The PatternParser class provides static methods for parsing different types of task commands using regular expressions.
- * It includes methods for parsing "todo," "deadline," "event," and "find" commands.
+ * It includes methods for parsing "todo," "deadline," "event," and "find" commands, as well as a utility method for adding tasks
+ * to a list from a formatted string.
  */
 public class PatternParser {
     private static final Pattern todoPattern = Pattern.compile("todo (.*?)");
     private static final Pattern deadlinePattern = Pattern.compile("deadline (.*?) /by (.*?) (.*?)");
     private static final Pattern eventPattern = Pattern.compile("event (.*?) /from (.*?) (.*?) /to (.*?) (.*?)");
     private static final Pattern findPattern = Pattern.compile("event (.*?) /from (.*?) (.*?) /to (.*?) (.*?)");
+    private static final String fileInputPattern = "^\\s*(\\w+)\\s*\\|\\s*(\\d+)\\s*\\|\\s*(\\S+)\\s*\\|\\s*"
+            + "(\\S+)\\s*"
+            + "\\|\\s*(\\S+)\\s*\\|\\"
+            + "s*(\\S+)\\s*\\|\\s*(\\S+)\\s*$";
 
     /**
      * Parses a "todo" command and adds the corresponding task to the task list.
@@ -100,4 +107,47 @@ public class PatternParser {
             throw new RyanGoslingException("Incomplete find command! find <task_word>");
         }
     }
+
+    /**
+     * Utility method to add a task to the list based on the formatted input line.
+     *
+     * @param listOfTasks The list of tasks to which the task will be added.
+     * @param inputLine   The formatted input line containing task details.
+     * @throws RyanGoslingException If the input line does not match the expected format.
+     */
+    public static void addTasktoList(ArrayList<Task> listOfTasks, String inputLine) throws RyanGoslingException {
+        Pattern regex = Pattern.compile(fileInputPattern);
+        Matcher matcher = regex.matcher(inputLine);
+        if (!matcher.matches()) {
+            throw new RyanGoslingException("Task lists stored in hard drive is not in expected format!\n"
+                                                   + "It should follow (T/E/D) | (0/1) | Description | dateFrom | "
+                                                   + "timeFrom "
+                                                   + "| dateTo | timeTo");
+        }
+        String typeOfTask = matcher.group(1);
+        int isTaskDone = Integer.parseInt(matcher.group(2));
+        String taskDescription = matcher.group(3);
+        String dateFrom = matcher.group(4);
+        String timeFrom = matcher.group(5);
+        String dateTo = matcher.group(6);
+        String timeTo = matcher.group(7);
+        switch (typeOfTask) {
+        case "T":
+            listOfTasks.add(new Todo(taskDescription, isTaskDone));
+            break;
+        case "D":
+            listOfTasks.add(new Deadline(taskDescription, dateFrom, timeFrom, isTaskDone));
+            break;
+        case "E":
+            listOfTasks.add(new Events(taskDescription, dateFrom, timeFrom, dateTo, timeTo, isTaskDone));
+            break;
+        default:
+
+        }
+    }
+
+
+
+
+
 }
