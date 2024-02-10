8424eca18a8a1dc55a7fa93353aa50f0448770f3
yeoshuheng <yeoshuheng@gmail.com>
2024-02-10 14:04:20 +0800

    test: Add test to consider priority

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -26,25 +26,29 @@ class ParserTest {
 
     @Test
     public void testExtractDescription() throws InvalidInputException {
-        String[] testArray = new String[]{"event", "do stuff", "/from", "date1", "/to", "date2"};
+        String[] testArray = new String[]{"event", "do stuff", "/from", "date1",
+            "/to", "date2", "/priority", "1"};
         assertEquals("do stuff",
             Parser.extractDescriptionData(testArray)[0]);
         assertEquals("date1",
             Parser.extractDescriptionData(testArray)[1]);
         assertEquals("date2",
             Parser.extractDescriptionData(testArray)[2]);
-        String[] testArray2 = new String[]{"event", "do stuff", "date1", "/to", "date2"};
+        String[] testArray2 = new String[]{"event", "do stuff", "date1",
+            "/to", "date2", "/priority", "1"};
         assertThrows(
             InvalidParametersException.class, () -> Parser.extractDescriptionData(testArray2),
             "It should throw a InvalidParameterException"
         );
-        String[] testArray3 = new String[]{"todo", "do stuff"};
-        assertEquals(3, Parser.extractDescriptionData(testArray3).length);
+        String[] testArray3 = new String[]{"todo", "do stuff",
+          "/priority", "1"};
+        assertEquals(4, Parser.extractDescriptionData(testArray3).length);
         assertEquals("do stuff",
             Parser.extractDescriptionData(testArray3)[0]);
         assertEquals(null,
             Parser.extractDescriptionData(testArray3)[1]);
-        String[] testArray4 = new String[]{"deadline", "do stuff", "/by", "date1"};
+        String[] testArray4 = new String[]{"deadline", "do stuff", "/by",
+            "date1", "/priority", "1"};
         assertEquals("do stuff",
             Parser.extractDescriptionData(testArray4)[0]);
         assertEquals("date1",
