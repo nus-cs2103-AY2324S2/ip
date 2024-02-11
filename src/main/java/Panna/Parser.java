2058f8be458ab6240ca0c37a79aff825e418269b
AjayArvind2207 <alwayssactheexchange@gmail.com>
2024-02-12 03:25:54 +0800

    Added JavaDoc

--- src/main/java/Panna/Parser.java
+++ src/main/java/Panna/Parser.java
@@ -3,13 +3,26 @@ package Panna;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Encapsulates a date parser which converts between LocalDate formats.
+ */
 public class Parser {
 
     private DateTimeFormatter df;
 
+    /**
+     * Constructor method
+     * @param format
+     */
     Parser(String format) {
         this.df = DateTimeFormatter.ofPattern(format);
     }
+
+    /**
+     * Parses the date in the format initialized in the constructor
+     * @param dt
+     * @return LocalDate object in format.
+     */
     public LocalDate parse(String dt) {
         return LocalDate.parse(dt, this.df);
     }
--- src/main/java/Panna/Todo.java
+++ src/main/java/Panna/Todo.java
@@ -1,13 +1,25 @@
 package Panna;
+
+/**
+ * Todo class encapsulates all Todo objects
+ */
 public class Todo extends Task{
 
 
-
+    /**
+     * Constructor to initialize object
+     * @param input
+     */
     public Todo(String input) {
         super(input);
 
 
     }
+
+    /**
+     * String representation of the object
+     * @return String representation
+     */
 
     @Override
     public String toString() {
