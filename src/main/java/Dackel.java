9a657a7ecd26cd124a1a2e6fbb4f77f1a7efd1fe
tcjazwei <oliver.j.tan@gmail.com>
2024-02-11 16:24:24 +0800

    Add echo functionality

--- src/main/java/Dackel.java
+++ src/main/java/Dackel.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Dackel {
-    /** String constants */
+    /** Command aliases */
+    private static final String QUIT = "bye";
+
+    /** Other String constants */
     private static final String NAME = "DACKEL";
     private static final String LOGO = " ____    __    ___  _  _  ____  __   \n"
         + "(  _ \\  /__\\  / __)( )/ )( ___)(  )  \n"
         + " )(_) )/(__)\\( (__  )  (  )__)  )(__ \n"
         + "(____/(__)(__)\\___)(_)\\_)(____)(____) ";
     private static final String LINE = "____________________________________________________________";
+    private static final String VERSION = "Version 0.1";
+
+    /** Scanner for receiving user input*/
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     /**
      * Simulates Dackel speaking to the user ala a chatroom interface
@@ -17,17 +26,43 @@ public class Dackel {
         System.out.println(NAME + ": " + message);
     }
 
+    /**
+     * Prompts user for input and returns it
+     * 
+     * @return User-inputted string
+     */
+    private static String receiveInput() {
+        System.out.print("> ");
+        String input = SCANNER.nextLine();
+        return input;
+    }
+
     public static void main(String[] args) {
-        // Title cards, etc.
+        // title cards, etc.
         System.out.println(LINE);
         System.out.println(LOGO);
+        System.out.println(VERSION);
         System.out.println(LINE);
         System.out.println();
 
-        // Greeting
-        // TODO: make dackel read its lines from a file
+        // greeting message
         speak("hihi!");
         speak("what can i do for you today?");
+        
+        // main command entry loop
+        // TODO: make dackel read its lines from a file
+        while (true) {
+            String input = receiveInput();
+            if (input.equals(QUIT)) {
+                break;
+            }
+            else {
+                speak(input);
+                // TODO: make a separate command for echo?
+            }
+        }
+
+        // goodbye message
         speak("hope to see you again soon!");
     }
 }
