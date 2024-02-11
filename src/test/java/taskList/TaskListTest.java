1602437cc3d8fdb49005fa2ee23280fb82a248f9
pughal <pughalraj77@gmail.com>
2024-02-12 00:03:20 +0800

    JUnit test for find method in TaskList

--- src/test/java/taskList/TaskListTest.java
+++ src/test/java/taskList/TaskListTest.java
@@ -30,23 +30,19 @@ public class TaskListTest {
     public void testFind() throws InvalidDateTimeFormat, InvalidCmd, EventEmptyException, DeadlineEmptyException {
         // storage already has 2 tasks that should already be loaded in with
         // the word 'book' in it
-        // D | 1 | book | Dec 12 1212 12:12
         // D | 1 | return book | Dec 2 2019 18:00
+        // D | 1 | book | Dec 12 1212 12:12
         String expected = "1. [D][X] return book (by: Dec 2 2019 18:00) \n" +
                 "2. [D][X] book (by: Dec 12 1212 12:12) \n";
         Command actualCmd = parser.parseCommand("find book");
         actualCmd.setTasksAndUi(taskList, ui);
         actualCmd.execute();
-        String actualText;
+        String actualText = "Check to see if ur sirduke.txt has the right preset tasks!";
         if (actualCmd instanceof FindCmd) {
             actualText = ((FindCmd) actualCmd).getResult().toString();
         } else {
             actualText = "actualCmd is not a FindCmd object";
         }
-
         assertEquals(expected, actualText);
-
-
-
     }
 }
