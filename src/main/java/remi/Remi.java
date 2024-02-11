0654ace8f673bc45fd1fbdde9ad35ecec7636024
ivan24004 <ivan@ivant.com>
2024-02-11 16:43:08 +0800

    Make simple javafx input box.

--- src/main/java/remi/Remi.java
+++ src/main/java/remi/Remi.java
@@ -1,13 +1,19 @@
 package remi;
 
+import javafx.application.Application;
 import remi.model.Ui;
+import remi.utils.GuiLauncher;
+
 
 /**
  * Main class and entrypoint for the Remi chatbot.
  */
 public class Remi {
+
     public static void main(String[] args) {
+        Application.launch(GuiLauncher.class, args);
         Ui chatbot = new Ui();
         chatbot.doIoLoop();
     }
+
 }
