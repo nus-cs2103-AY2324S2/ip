65ed8d745cc8de56effcc7ddf72fd2404e6a795f
fy17ohhh <hester03sfy@gmail.com>
2024-02-11 21:59:50 +0800

    Refactor code to improve code quality
    Too many magic literals used in the previous commits. Instead, use named
    constants to improve the readability of code.
    
    Add more comments to specify what the methods are supposed to do, and
    the rationale for the current implementations.
    
    Use SLAP in switch statements in Parser class, while also applying
    extract method to turn the fragment into a method whose name explains
    the purpose of the method.

--- src/main/java/jade/commands/ExitCommand.java
+++ src/main/java/jade/commands/ExitCommand.java
@@ -3,20 +3,19 @@ package jade.commands;
 import jade.data.TaskList;
 import jade.exception.JadeException;
 import jade.storage.Storage;
-import jade.ui.Ui;
 
 /**
  * The <code>ExitCommand</code> object represents the command to exit the program.
  */
 public class ExitCommand extends Command {
+    private static final String RESULT_MSG_FORMATTED = "Bye. Hope to see you again soon.";
     /**
      * @inheritDoc This implementation prints a farewell message after saving changes to the local file.
      */
     @Override
     public String execute(TaskList taskList, Storage storage) throws JadeException {
         storage.saveChange(taskList);
-        String result = "Bye. Hope to see you again soon.";
-        return result;
+        return RESULT_MSG_FORMATTED;
     }
 
     /**
