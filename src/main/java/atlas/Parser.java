abd5ff800d50d66569b0ea076e35c4f819c65151
zhikaiong2001 <ongzk2001@gmail.com>
2024-02-12 18:01:19 +0800

    Refactor code to adhere to code quality standards as much as possible
    Code does not adhere to code quality standards
    
    Code's readibility is poor due to long methods, deep nested and complicated expressions
    
    Let's extract out methods from long methods to adhere to SLAP and break down complicated expressions into smaller ones
    
    Adhering to SLAP and breaking down complex expressions allows for greater readability using abstraction

--- src/main/java/atlas/Parser.java
+++ src/main/java/atlas/Parser.java
@@ -31,6 +31,7 @@ public class Parser {
      */
     public static Command parse(String input, TaskList tasks, Ui ui, Storage storage) throws AtlasException {
         String[] parts = input.split(" ", 2);
+        assert parts.length > 0 : "Input command should not be empty";
         String command = parts[0];
         String details = parts.length > 1 ? parts[1] : null;
 
@@ -40,54 +41,78 @@ public class Parser {
         case "list":
             return new ListCommand(tasks, ui, storage);
         case "mark":
-            int taskIndex = Integer.parseInt(details) - 1;
-            return new MarkCommand(tasks, ui, storage, taskIndex);
+            return createMarkCommand(tasks, ui, storage, details);
         case "unmark":
-            taskIndex = Integer.parseInt(details) - 1;
-            return new UnmarkCommand(tasks, ui, storage, taskIndex);
+            return createUnmarkCommand(tasks, ui, storage, details);
         case "delete":
-            taskIndex = Integer.parseInt(details) - 1;
-            return new DeleteCommand(tasks, ui, storage, taskIndex);
+            return createDeleteCommand(tasks, ui, storage, details);
         case "todo":
             return new AddToDoCommand(tasks, ui, storage, details);
         case "deadline":
-            String[] deadlineDetails = details.split(" /by ");
-            LocalDateTime by;
-            try {
-                by = LocalDateTime.parse(deadlineDetails[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-            } catch (DateTimeParseException e) {
-                throw new InvalidDeadlineFormatException("Invalid date/time format. Please use 'yyyy-MM-dd HHmm'.\"");
-            }
-            return new AddDeadlineCommand(tasks, ui, storage, deadlineDetails[0], by);
+            return createAddDeadlineCommand(tasks, ui, storage, details);
         case "event":
-            String[] eventParts = details.split(" /from | /to "); // Split by both "/from" and "/to"
-            if (eventParts.length != 3) {
-                throw new AtlasException("Invalid event format."
-                        + " Please use 'event [description] /from yyyy-MM-dd HHmm /to yyyy-MM-dd HHmm'.");
-            }
-            String description = eventParts[0];
-            LocalDateTime start;
-            LocalDateTime end;
-            try {
-                start = LocalDateTime.parse(eventParts[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-                end = LocalDateTime.parse(eventParts[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-            } catch (DateTimeParseException e) {
-                throw new InvalidEventFormatException("Invalid date format for event. Please use 'yyyy-MM-dd HHmm'.");
-            }
-            return new AddEventCommand(tasks, ui, storage, description, start, end);
+            return createAddEventCommand(tasks, ui, storage, details);
         case "tasks_on":
-            LocalDate date;
-            try {
-                date = LocalDate.parse(details, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            } catch (DateTimeParseException e) {
-                throw new AtlasException("Invalid date format. Please use 'yyyy-MM-dd HHmm'.");
-            }
-            return new TasksOnDateCommand(tasks, ui, storage, date);
+            return createTasksOnDateCommand(tasks, ui, storage, details);
         case "find":
             return new FindCommand(tasks, ui, storage, details);
         default:
             return new InvalidCommand(tasks, ui, storage);
         }
+    }
+
+    private static TasksOnDateCommand createTasksOnDateCommand(TaskList tasks, Ui ui, Storage storage, String details) throws AtlasException {
+        LocalDate date;
+        try {
+            date = LocalDate.parse(details, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (DateTimeParseException e) {
+            throw new AtlasException("Invalid date format. Please use 'yyyy-MM-dd HHmm'.");
+        }
+        return new TasksOnDateCommand(tasks, ui, storage, date);
+    }
+
+    private static AddEventCommand createAddEventCommand(TaskList tasks, Ui ui, Storage storage, String details) throws AtlasException {
+        String[] eventParts = details.split(" /from | /to "); // Split by both "/from" and "/to"
+        if (eventParts.length != 3) {
+            throw new AtlasException("Invalid event format."
+                    + " Please use 'event [description] /from yyyy-MM-dd HHmm /to yyyy-MM-dd HHmm'.");
+        }
+        String description = eventParts[0];
+        LocalDateTime start;
+        LocalDateTime end;
+        try {
+            start = LocalDateTime.parse(eventParts[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+            end = LocalDateTime.parse(eventParts[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidEventFormatException("Invalid date format for event. Please use 'yyyy-MM-dd HHmm'.");
+        }
+        return new AddEventCommand(tasks, ui, storage, description, start, end);
+    }
+
+    private static AddDeadlineCommand createAddDeadlineCommand(TaskList tasks, Ui ui, Storage storage, String details) throws InvalidDeadlineFormatException {
+        String[] deadlineDetails = details.split(" /by ");
+        LocalDateTime by;
+        try {
+            by = LocalDateTime.parse(deadlineDetails[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidDeadlineFormatException("Invalid date/time format. Please use 'yyyy-MM-dd HHmm'.\"");
+        }
+        return new AddDeadlineCommand(tasks, ui, storage, deadlineDetails[0], by);
+    }
+
+    private static DeleteCommand createDeleteCommand(TaskList tasks, Ui ui, Storage storage, String details) {
+        int taskIndex = Integer.parseInt(details) - 1;
+        return new DeleteCommand(tasks, ui, storage, taskIndex);
+    }
+
+    private static UnmarkCommand createUnmarkCommand(TaskList tasks, Ui ui, Storage storage, String details) {
+        int taskIndex = Integer.parseInt(details) - 1;
+        return new UnmarkCommand(tasks, ui, storage, taskIndex);
+    }
+
+    private static MarkCommand createMarkCommand(TaskList tasks, Ui ui, Storage storage, String details) {
+        int taskIndex = Integer.parseInt(details) - 1;
+        return new MarkCommand(tasks, ui, storage, taskIndex);
     }
 
     /**
@@ -106,38 +131,57 @@ public class Parser {
 
         switch (type) {
         case "T":
-            ToDo todo = new ToDo(description);
-            if (isDone) {
-                todo.toggle();
-            }
-            return todo;
+            return parseLineToToDo(description, isDone);
         case "D":
-            try {
-                LocalDateTime by = LocalDateTime.parse(parts[3].trim());
-                Deadline deadline = new Deadline(description, by);
-                if (isDone) {
-                    deadline.toggle();
-                }
-                return deadline;
-            } catch (DateTimeParseException e) {
-                System.err.println("Failed to parse deadline date: " + parts[3]);
-                return null;
-            }
+            return parseLineToDeadline(parts, description, isDone);
         case "E":
-            try {
-                LocalDateTime start = LocalDateTime.parse(parts[3].trim());
-                LocalDateTime end = LocalDateTime.parse(parts[4].trim());
-                Event event = new Event(description, start, end);
-                if (isDone) {
-                    event.toggle();
-                }
-                return event;
-            } catch (DateTimeParseException e) {
-                System.err.println("Failed to parse event dates: " + parts[3] + " and " + parts[4]);
-                return null;
-            }
+            return parseLineToEvent(parts, description, isDone);
         default:
+            assert false : "Line saved in data file cannot be parsed into task";
             return null;
         }
+    }
+
+    private static Event parseLineToEvent(String[] parts, String description, boolean isDone) {
+        try {
+            LocalDateTime start = LocalDateTime.parse(parts[3].trim());
+            assert start != null : "start should not be null";
+            LocalDateTime end = LocalDateTime.parse(parts[4].trim());
+            assert end != null : "end should not be null";
+            Event event = new Event(description, start, end);
+            assert event != null : "Task object should not be null";
+            if (isDone) {
+                event.toggle();
+            }
+            return event;
+        } catch (DateTimeParseException e) {
+            System.err.println("Failed to parse event dates: " + parts[3] + " and " + parts[4]);
+            return null;
+        }
+    }
+
+    private static Deadline parseLineToDeadline(String[] parts, String description, boolean isDone) {
+        try {
+            LocalDateTime by = LocalDateTime.parse(parts[3].trim());
+            assert by != null : "by should not be null";
+            Deadline deadline = new Deadline(description, by);
+            assert deadline != null : "Task object should not be null";
+            if (isDone) {
+                deadline.toggle();
+            }
+            return deadline;
+        } catch (DateTimeParseException e) {
+            System.err.println("Failed to parse deadline date: " + parts[3]);
+            return null;
+        }
+    }
+
+    private static ToDo parseLineToToDo(String description, boolean isDone) {
+        ToDo todo = new ToDo(description);
+        assert todo != null : "Task object should not be null";
+        if (isDone) {
+            todo.toggle();
+        }
+        return todo;
     }
 }
