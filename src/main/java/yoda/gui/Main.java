bba03a8ff284dd417eebad84a8e26578351a595e
Kin Tat <110956850+KinTatHo@users.noreply.github.com>
2024-02-13 07:50:28 +0800

    adds comments to Main.java

--- src/main/java/yoda/gui/Main.java
+++ src/main/java/yoda/gui/Main.java
@@ -13,6 +13,13 @@ public class Main extends Application {
 
     private Yoda yoda = new Yoda();
 
+    /**
+     *
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     */
     @Override
     public void start(Stage stage) {
         try {
