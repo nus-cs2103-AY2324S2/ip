c70d672f1506e9679cc4faba8f53cea9a2e72587
alex-setyawan <alexgenrong@gmail.com>
2024-02-12 22:49:47 +0800

    Remove JavaDocs from master branch

--- src/main/java/TodoTask.java
+++ src/main/java/TodoTask.java
@@ -1,21 +1,11 @@
 public class TodoTask extends Task {
     private String type;
 
-    /**
-     * Constuctor for Task object of type "todo"
-     *
-     * @param what description of the task
-     */
     public TodoTask(String what) {
         super(what);
         this.type = "[T]";
     }
 
-    /**
-     * Returns string showing task type, completion status and description.
-     *
-     * @return string of task type, marked/unmarked status and description
-     */
     public String showAll() {
         return this.type + super.showAll();
     }
