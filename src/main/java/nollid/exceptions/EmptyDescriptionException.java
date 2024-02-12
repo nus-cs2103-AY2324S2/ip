c74e7444f298f02d5ff284793865a249851582e8
dillontkh <e0958762@u.nus.edu>
2024-02-12 22:53:02 +0800

    Add new exceptions

--- src/main/java/nollid/exceptions/EmptyDescriptionException.java
+++ src/main/java/nollid/exceptions/EmptyDescriptionException.java
@@ -1,5 +1,8 @@
 package nollid.exceptions;
 
+/**
+ * Exception thrown when an empty description is encountered.
+ */
 public class EmptyDescriptionException extends NollidException {
     public EmptyDescriptionException(String errorMessage) {
         super(errorMessage);
--- src/main/java/nollid/exceptions/MissingTagsException.java
+++ src/main/java/nollid/exceptions/MissingTagsException.java
@@ -1,2 +1,10 @@
-package nollid.exceptions;public class MissingTagsException {
+package nollid.exceptions;
+
+/**
+ * Exception thrown when tags are missing.
+ */
+public class MissingTagsException extends NollidException {
+    public MissingTagsException(String message) {
+        super(message);
+    }
 }
