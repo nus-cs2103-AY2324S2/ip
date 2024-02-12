321328a4015a3ace09e9b2322b801d6f466adc4e
AndrewOng2066 <e0959899@u.nus.edu>
2024-02-12 23:29:47 +0800

    Change the code in executeCommand of ListCommend to use Java Stream

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -5,6 +5,9 @@ import andelu.Storage;
 import andelu.TaskList;
 import andelu.Ui;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * A ListCommand to list down the current list of tasks.
  * A subclass of Command class.
@@ -23,6 +26,7 @@ public class ListCommand extends Command {
      */
     @Override
     public String executeCommand(TaskList tasks, Ui ui, Storage storage) throws AndeluException {
+        /*
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Here are the tasks in your list:\n");
         ui.printAnyStatement("Here are the tasks in your list:");
@@ -33,7 +37,11 @@ public class ListCommand extends Command {
             System.out.println("      " + (i + 1) + "." + tasks.getTasks().get(i).toString());
             stringBuilder.append((i + 1) + "." + tasks.getTasks().get(i).toString() + "\n");
         }
+        */
 
-        return stringBuilder.toString();
+        String tasksString = IntStream.range(0, tasks.getTasks().size()).mapToObj(i -> String.format("%d. %s", (i + 1), tasks.getTasks().get(i).toString())).collect(Collectors.joining("\n"));
+        String result = "Here are the tasks in your list:\n" + tasksString;
+
+        return result;
     }
 }
