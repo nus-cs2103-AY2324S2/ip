b1915f0e29da704dcc23cfaa82df9bb795fd7adf
The-Dudeski <>
2024-02-12 18:02:33 +0800

    Changed Duke.java to Kirby.java.

---
+++ src/main/java/Kirby.java
@@ -0,0 +1,8 @@
+public class Kirby {
+    public static void main(String[] args) {
+        String message =
+                "____________________________________________________________\nHello! I'm Kirby\nWhat can I do for you?\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________";
+
+        System.out.println(message);
+    }
+}
