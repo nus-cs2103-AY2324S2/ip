bd7ff5657abc2b2d477fee5022832a1abd6614f2
Chong Chin Herng <chongahzong@gmail.com>
2024-02-12 22:01:45 +0800

    Test parse

--- src/test/java/bob/task/EventTest.java
+++ src/test/java/bob/task/EventTest.java
@@ -6,7 +6,9 @@ import java.time.Month;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EventTest {
     @Test
