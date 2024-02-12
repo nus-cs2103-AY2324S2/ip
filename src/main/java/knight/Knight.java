b700464c2ef391ac50cbdc0cfcb0a65da3e3b7d1
narwhalsilent <110954060+narwhalsilent@users.noreply.github.com>
2024-02-12 12:11:45 +0800

    Implement GUI

--- src/main/java/knight/Knight.java
+++ src/main/java/knight/Knight.java
@@ -1,6 +1,14 @@
 package knight;
 
 import java.util.Scanner;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 /**
  * The main class of the program.
@@ -8,10 +16,13 @@ import java.util.Scanner;
 public class Knight {
     private static TaskList tasks = new TaskList();
 
+    public Knight() {
+        tasks = Storage.readFromFile();
+    }
+
     public static void main(String[] args) {
         tasks = Storage.readFromFile();
-        Ui.speak("Greetings, Your Excellency! Thy humble\n"
-                + Ui.LOGO
+        Ui.speak("Greetings, Your Excellency! Thy humble knight"
                 + "doth stand before thee. How may I serve thee on this day?");
 
         Scanner scan = new Scanner(System.in);
@@ -37,5 +48,32 @@ public class Knight {
 
         Storage.writeToFile(tasks);
         Ui.speak("Farewell, Your Excellency! May we cross paths once more in the near future.");
+    }
+
+    private static String knightExecute(String message) {
+        Command command;
+
+        try {
+            command = CommandParser.parseCommand(message);
+        } catch (NonstandardCommandException e) {
+            return e.getMessage();
+        }
+
+        if (command == Command.BYE) {
+            Storage.writeToFile(tasks);
+            return "Farewell, Your Excellency! May we cross paths once more in the near future.";
+        } else {
+            return tasks.executeCommandReturnString(command, message);
+        }
+    }
+
+    /**
+     * Returns the response of the knight to the user input.
+     *
+     * @param input The user input.
+     * @return The response of the knight to the user input.
+     */
+    String getResponse(String input) {
+        return knightExecute(input);
     }
 }
--- src/main/java/knight/TaskList.java
+++ src/main/java/knight/TaskList.java
@@ -122,6 +122,101 @@ public class TaskList {
         }
     }
 
+    String executeCommandReturnString(Command commandType, String message) {
+        if (commandType == Command.LIST) {
+            if (tasks.isEmpty()) {
+                return "Your Excellency, thy list remaineth free of tasks at this present moment.";
+            } else {
+                return "Behold, the duties thou hast assigned:\n" + this.toString();
+            }
+        } else if (commandType == Command.SAVE) {
+            Storage.writeToFile(this);
+            return "Thy list hath been saved to thy scrolls of history.";
+        } else if (commandType == Command.MARK) {
+            int index = Integer.parseInt(message.substring(5));
+            Task task;
+
+            try {
+                task = tasks.get(index - 1);
+            } catch (IndexOutOfBoundsException e) {
+                return "I regret to inform thee, Your Excellency, "
+                        + "that thou lackest a task bearing this index in thy list.";
+            }
+
+            task.mark();
+            return "Thy task hath been marked as done.\n" + task;
+        } else if (commandType == Command.UNMARK) {
+            int index = Integer.parseInt(message.substring(7));
+            Task task;
+
+            try {
+                task = tasks.get(index - 1);
+            } catch (IndexOutOfBoundsException e) {
+                return "I regret to inform thee, Your Excellency, "
+                        + "that thou lackest a task bearing this index in thy list.";
+            }
+
+            task.unmark();
+            return "Thy task hath been unmarked.\n" + task;
+        } else if (commandType == Command.DELETE) {
+            int index = Integer.parseInt(message.substring(7));
+            Task task;
+
+            try {
+                task = tasks.get(index - 1);
+            } catch (IndexOutOfBoundsException e) {
+                return "I regret to inform thee, Your Excellency,"
+                        + "that thou lackest a task bearing this index in thy list.";
+            }
+
+            tasks.remove(index - 1);
+            return "Thy task hath been removed from thy list.\n" + task;
+        } else if (commandType == Command.TODO) {
+            Task task = new ToDo(message.substring(5));
+            tasks.add(task);
+            return "Understood. This task hath been added to thy list:\n" + task;
+        } else if (commandType == Command.DEADLINE) {
+            String[] params = message.split(" /");
+            Task task;
+            try {
+                task = new Deadline(params[0].substring(9), params[1].substring(3));
+            } catch (DateTimeParseException e) {
+                return "Your Excellency, I struggle to understand thee. To specify a date, use format\n"
+                        + "yyyy-mm-dd";
+            }
+            tasks.add(task);
+            return "Understood. This task hath been added to thy list:\n" + task;
+        } else if (commandType == Command.EVENT) {
+            String[] params = message.split(" /");
+            Task task;
+            try {
+                task = new Event(params[0].substring(6),
+                        params[1].substring(5),
+                        params[2].substring(3));
+            } catch (DateTimeParseException e) {
+                return "Your Excellency, I struggle to understand thee. To specify a date, use format\n"
+                        + "yyyy-mm-dd";
+            }
+            tasks.add(task);
+            return "Understood. This task hath been added to thy list:\n" + task;
+        } else if (commandType == Command.FIND) {
+            String keyword = message.substring(5);
+            String output = "";
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).matches(keyword)) {
+                    output += "\n" + (i + 1) + ". " + tasks.get(i);
+                }
+            }
+            if (output.equals("")) {
+                return "I regret to inform thee, Your Excellency, "
+                        + "that no tasks bearing this keyword exist in thy list.";
+            } else {
+                return "Behold, the tasks that match thy keyword:\n" + output;
+            }
+        }
+        return "Thou hast reached a place previously deemed unreachable. How didst thou arrive here?";
+    }
+
     /**
      * Execute a command on the task list without speaking.
      *
