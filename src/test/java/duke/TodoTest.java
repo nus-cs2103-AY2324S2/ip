c21a07778613546d6c69677ced2c42d925d74590
kailinteoo <e0959354@u.nus.edu>
2024-02-13 00:22:19 +0800

    Solved the issue regarding to checkstyles and javafx error

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -2,19 +2,18 @@ package duke;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
     public void todoToStringTest() {
         Todo todo = new Todo("Buy candy");
-        assertEquals("[T][ ] Buy candy", todo.toString());
+        assert("[T][ ] Buy candy".equals(todo.toString()));
     }
 
     @Test
     public void todoMarkAsDoneTest() {
         Todo todo = new Todo("Read a book");
         todo.markAsDone();
-        assertEquals("[T][X] Read a book", todo.toString());
+        assert("[T][X] Read a book".equals(todo.toString()));
     }
 }
