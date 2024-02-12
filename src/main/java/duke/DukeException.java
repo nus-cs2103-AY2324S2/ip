803c5145a68683f488f970f788e6ff78cf79032a
PallonCX <cheexiang03@gmail.com>
2024-02-12 11:24:58 +0800

    Modify DukeException.java

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -3,7 +3,7 @@ package duke;
 /**
  * The `DukeException` class represents any exception related to the Duke program.
  */
-public class DukeException extends Exception{
+public class DukeException extends Exception {
 
     /**
      * Empty constructor of a `DukeException` object.
