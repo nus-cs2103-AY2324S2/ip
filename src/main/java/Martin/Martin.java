f84b4ad0402f21335a32339acc420ca8cc1baf92
yunchao <tanyunchao@gmail.com>
2024-02-13 22:38:01 +0800

    Remove unused method getDialogLabel()

--- src/main/java/Martin/Martin.java
+++ src/main/java/Martin/Martin.java
@@ -113,21 +113,6 @@ public class Martin extends Application {
     }
 
     /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * 
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
      * Iteration 2:
      * Creates two dialog boxes, one echoing user input and the other containing
      * Duke's reply and then appends them to
