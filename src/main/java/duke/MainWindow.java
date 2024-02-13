1552c8cbda13e420b4e505e7bf92b96dc95e9d54
xinen26 <xinenope@gmail.com>
2024-02-13 11:26:38 +0800

    Add javaFX tutorial four edits

---
+++ src/main/java/duke/MainWindow.java
@@ -0,0 +1,51 @@
+package duke;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duke duke;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/maltese1.png"));
+    private Image duke2 = new Image(this.getClass().getResourceAsStream("/images/maltese2.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, user),
+                DialogBox.getDukeDialog(response, duke2)
+        );
+        userInput.clear();
+    }
+}
