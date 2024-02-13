cffb48e4318ac53aa5ab32a095794f31a0715516
tengcharmaine <e0969596@u.nus.edu>
2024-02-13 13:50:01 +0800

    Implement help command to display commands in a separate window
    Added functionality to display the available commands in a separate window when the user enters the help command.
    
    Modified the BotChat class to include a showHelpWindow method responsible for creating a new JavaFX stage to display the help message.
    
    Enhances user experience by providing a clearer presentation of available commands.

--- src/main/java/BotChat/BotChat.java
+++ src/main/java/BotChat/BotChat.java
@@ -1,6 +1,11 @@
 package BotChat;
 
 import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * The main class for the botChat application, responsible for handling user input and managing tasks.
@@ -51,7 +56,8 @@ public class BotChat {
         case FIND:
             return tasks.findTasks(input);
         case HELP:
-            return showHelpMessage();
+            showHelpWindow();
+            return "";
         case UNKNOWN:
             throw new BotChatException("Sorry, I do not understand that command. Please try again.");
         default:
@@ -65,18 +71,34 @@ public class BotChat {
      * @return The help message.
      */
     private String showHelpMessage() {
-        StringBuilder helpMessage = new StringBuilder("=== Help ===\n");
-        helpMessage.append("Here are the available commands:\n");
-        helpMessage.append("/bye - Exit the application\n");
-        helpMessage.append("/list - List all tasks\n");
-        helpMessage.append("/mark [taskIndex] - Mark a task as done\n");
-        helpMessage.append("/unmark [taskIndex] - Mark a task as undone\n");
-        helpMessage.append("/event [event details] /from [start] /to [end] - Add an event task\n");
-        helpMessage.append("/deadline [deadline details] /by [date/time] - Add a deadline task\n");
-        helpMessage.append("/todo [task details] - Add a todo task\n");
-        helpMessage.append("/delete [taskIndex] - Delete a task\n");
-        helpMessage.append("/find [keyword] - Find tasks containing a keyword\n");
-        helpMessage.append("/help - Display this help message\n");
+        StringBuilder helpMessage = new StringBuilder("   === Help ===\n");
+        helpMessage.append("   Here are the available commands:\n");
+        helpMessage.append("   /bye - Exit the application\n");
+        helpMessage.append("   /list - List all tasks\n");
+        helpMessage.append("   /mark [taskIndex] - Mark a task as done\n");
+        helpMessage.append("   /unmark [taskIndex] - Mark a task as undone\n");
+        helpMessage.append("   /event [event details] /from [start] /to [end] - Add an event task\n");
+        helpMessage.append("   /deadline [deadline details] /by [date/time] - Add a deadline task\n");
+        helpMessage.append("   /todo [task details] - Add a todo task\n");
+        helpMessage.append("   /delete [taskIndex] - Delete a task\n");
+        helpMessage.append("   /find [keyword] - Find tasks containing a keyword\n");
+        helpMessage.append("   /help - Display this help message\n");
         return helpMessage.toString();
+    }
+
+    /**
+     * Displays a help message in another window.
+     */
+    private void showHelpWindow() {
+        Stage helpStage = new Stage();
+        helpStage.initModality(Modality.APPLICATION_MODAL);
+        helpStage.setTitle("Help");
+
+        Label helpLabel = new Label(showHelpMessage());
+        VBox vbox = new VBox(helpLabel);
+        Scene scene = new Scene(vbox, 500, 300);
+
+        helpStage.setScene(scene);
+        helpStage.show();
     }
 }
