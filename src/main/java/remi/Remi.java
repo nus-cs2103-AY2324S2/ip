c04e43533e8aa588fafd385382e824e528294c98
ivan24004 <ivan@ivant.com>
2024-02-13 09:01:05 +0800

    Remove indent.

--- src/main/java/remi/Remi.java
+++ src/main/java/remi/Remi.java
@@ -8,7 +8,6 @@ import remi.utils.GuiLauncher;
  * Main class and entrypoint for the Remi chatbot.
  */
 public class Remi {
-
     public static void main(String[] args) {
         Application.launch(GuiLauncher.class, args);
     }
