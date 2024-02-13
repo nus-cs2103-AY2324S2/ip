8817c6c440f2a0b96b41ab1ffbe6c1c658f42695
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-13 19:35:51 +0800

    Refactor Event.java

--- src/main/java/convobot/tasks/Event.java
+++ src/main/java/convobot/tasks/Event.java
@@ -45,7 +45,9 @@ public class Event extends Task {
      */
     @Override
     public String toFile() {
-        return "E | " + super.toFile() + " | " + DateTime.dateToFile(this.from) + " | " + DateTime.dateToFile(this.to);
+        return "E | " + super.toFile() + " | "
+                      + DateTime.dateToFile(this.from) + " | "
+                      + DateTime.dateToFile(this.to);
     }
 
     /**
@@ -55,7 +57,8 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + DateTime.dateToString(this.from) + " to: "
-                + DateTime.dateToString(this.to) + ")";
+        return "[E]" + super.toString() + " (from: "
+                     + DateTime.dateToString(this.from) + " to: "
+                     + DateTime.dateToString(this.to) + ")";
     }
 }
