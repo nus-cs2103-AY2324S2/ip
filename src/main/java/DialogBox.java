5761396cb4d6b29c234c4dcf86543cf1df81b6f2
AidanG <agoh0185@gmail.com>
2024-02-14 17:53:50 +0800

    Fix dialog box not resizing
    For Steven, the dialog box does not expand with more text, so for
    example, a very long command will appear cut-off with ellipsis.
    
    This prevents a a user from viewing the full message from Steven.
    
    This is unintended behavior and thus should be fixed.
    
    Let's,
    * Fix the above bug so the full output from Steven is properly
    observed

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -7,10 +7,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 /**
  * An example of a custom control using FXML.
@@ -19,7 +20,7 @@ import javafx.scene.layout.HBox;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
 
@@ -45,6 +46,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.setTextAlignment(TextAlignment.LEFT);
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
