6037c7b2912f9ae6405e63889bc520b8d1266d5f
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-02-14 14:44:12 +0800

    Extend Application and implement start method

--- src/main/java/chimp/Chimp.java
+++ src/main/java/chimp/Chimp.java
@@ -6,7 +6,23 @@ import chimp.command.Command;
 import chimp.core.*;
 import chimp.exception.*;
 
-public class Chimp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Chimp extends Application{
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
+
     /**
      * The main method is the entry point of the Chimp program.
      * It initializes the user interface, task list, and storage objects.
