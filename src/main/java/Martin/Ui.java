306fe3bb737e16895aa1f8e8a2818f00ff35a981
yunchao <tanyunchao@gmail.com>
2024-02-14 12:24:51 +0800

    Refactor Ui class methods to return strings instead of printing messages

--- src/main/java/Martin/Ui.java
+++ src/main/java/Martin/Ui.java
@@ -2,7 +2,6 @@ package Martin;
 
 import java.util.ArrayList;
 
-
 /**
  * The Ui class represents the user interface of the application.
  * It provides methods to interact with the user and display messages.
@@ -19,23 +18,29 @@ public class Ui {
      * Displays a greeting message to the user.
      * The greeting message includes the name "Martin" and a prompt for user input.
      */
-    public void sayGreeting() {
-        System.out.println("Hello from Martin");
-        System.out.println("What can I do for you?");
+    public String sayGreeting() {
+        String lineOne = "Hello from Martin" + "\n";
+        String lineTwo = "What can I do for you?" + "\n";
+        return lineOne + lineTwo;
     }
 
     /**
      * Displays a goodbye message to the user.
      * The goodbye message includes the name "Martin".
      */
-    public void sayBye() {
-        System.out.println("Bye from Martin");
+    public String sayBye() {
+        return "Bye from Martin";
     }
 
-    public void printFoundTasks(ArrayList<Task> foundTasks) {
-        System.out.println("Here are the matching tasks in your list:");
+    public String printFoundTasks(ArrayList<Task> foundTasks) {
+        String response = ("Here are the matching tasks in your list:" + "\n");
         for (int i = 0; i < foundTasks.size(); i++) {
-            System.out.println((i + 1) + "." + foundTasks.get(i));
+            response += ((i + 1) + "." + foundTasks.get(i) + "\n");
         }
+        return response;
+    }
+
+    public String sayDeleted(Task removed) {
+        return "Noted. I've removed this task:\n" + removed + "\n";
     }
 }
