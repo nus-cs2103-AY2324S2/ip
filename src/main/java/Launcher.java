a0582c465697e88a3772f5e35285f2524cbe7c47
iamtr <jettzeong@gmail.com>
2024-02-14 21:30:44 +0800

    Add Javadocs for Launcher

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 
+/**
+ * A launcher class to launch the GUI application.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
