43eadca1720a18d93b1c27597a7c2c2ca7d43aa2
darryl-chan <darrylchan8@yahoo.com.sg>
2024-02-14 17:50:15 +0800

    Remove enums as the default case is not reached
    Parser: default case can never be reached because there is try catch
    block that catches unknown inputs and making the code non-functional
    
    Now the enums commands have been removed and it relies on string inputs
    only

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -23,8 +23,6 @@ import duke.tasks.TaskList;
  */
 public class Parser {
 
-    enum CommandType {LIST, BYE, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, FIND, SORT};
-
     private Ui ui;
     private TaskList taskList;
     private Storage storage;
@@ -52,7 +50,7 @@ public class Parser {
 
         assert userInputList.size() >= 2: "There should be a user input here";
 
-        String userCommand = userInputList.get(0);
+        String userCommand = userInputList.get(0).toUpperCase();
         String description = userInputList.get(1);
         parseUserInput(userCommand, description);
     }
@@ -65,45 +63,42 @@ public class Parser {
     private void parseUserInput(String userCommand, String description) {
         try {
             Command command;
-            CommandType commandType = CommandType.valueOf(userCommand.toUpperCase());
-            switch (commandType) {
-                case BYE:
+            switch (userCommand) {
+                case "BYE":
                     storage.saveData(taskList, ui);
                     command = new CommandBye(taskList, ui);
                     break;
-                case LIST:
+                case "LIST":
                     command = new CommandList(taskList, ui);
                     break;
-                case MARK: 
+                case "MARK": 
                     command = new CommandMark(taskList, ui);
                     break;
-                case UNMARK: 
+                case "UNMARK": 
                     command = new CommandUnmark(taskList, ui);
                     break;
-                case DELETE:
+                case "DELETE":
                     command = new CommandDelete(taskList, ui);
                     break;
-                case TODO: 
+                case "TODO": 
                     command = new CommandToDo(taskList, ui);
                     break;
-                case DEADLINE:
+                case "DEADLINE":
                     command = new CommandDeadline(taskList, ui);
                     break;
-                case EVENT:
+                case "EVENT":
                     command = new CommandEvent(taskList, ui);
                     break;
-                case FIND:
+                case "FIND":
                     command = new CommandFind(taskList, ui);
                     break;
-                case SORT:
+                case "SORT":
                     command = new CommandSort(taskList, ui);
                     break;
-                default: // find
+                default:
                     throw new DukeCeption("Sorry I don't recognize that command :/");
             }
             command.execute(description);
-        } catch (IllegalArgumentException e) {
-            ui.add("Sorry I don't recognize that command :/");
         } catch (Exception e) {
             ui.add(e.getMessage());
         } 
