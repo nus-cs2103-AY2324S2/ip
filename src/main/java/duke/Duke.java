0bee2d93c4a926819cf1447b930ef9aa34b25445
Carl <e0959394@u.nus.edu.sg>
2024-02-15 00:12:06 +0800

    Add the basic GUI to chat bot

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,20 +1,48 @@
 package duke;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 import duke.commands.Command;
 import duke.commands.CommandException;
 import duke.commands.ParseCommand;
 import duke.storage.Storage;
 import duke.tasks.Task;
+import duke.ui.DialogBox;
 import duke.ui.Ui;
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Duke {
+
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     /**
-     * The main method of the application. This runs the required 
+     * The main method of the application. This runs the required
      * methods to start the application.
-     * 
+     *
      * @param args
      * @throws IOException if the file path is not found.
      */
@@ -31,6 +59,17 @@ public class Duke {
                 Ui.printOutput(e.getMessage());
             }
         }
+    }
 
+    public Duke() {
+
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
