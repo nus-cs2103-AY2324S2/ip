b9169cc917eee9975218b3e6e1ac3b973766203e
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2024-02-15 01:01:10 +0800

    Improve code quality
    Move parsing logic out of TaskList::addToString to NewTaskCommand.
    
    Refactored some longer methods.
    
    Make the happy path more prominent in TaskList mark and unmark method.
    
    Rename initFromFile to initFromCsvFile to be more clear.

--- src/main/java/me/ruibin/leto/parser/NewTaskCommand.java
+++ src/main/java/me/ruibin/leto/parser/NewTaskCommand.java
@@ -2,12 +2,39 @@ package me.ruibin.leto.parser;
 
 import java.util.function.Function;
 
+import me.ruibin.leto.tasklist.Deadline;
+import me.ruibin.leto.tasklist.Event;
+import me.ruibin.leto.tasklist.InvalidTaskException;
+import me.ruibin.leto.tasklist.Task;
 import me.ruibin.leto.tasklist.TaskList;
+import me.ruibin.leto.tasklist.Todo;
 
 /** Read input for new task to add to list and returns <code>ResultTypes.OK</code>*/
 public class NewTaskCommand implements Function<String, Result> {
     @Override
     public Result apply(String s) {
-        return TaskList.addToList(s);
+        Result r = Result.makeResultOk("");
+        try {
+            Task t;
+            String typeOfTask = s.split(" ")[0];
+            switch (typeOfTask.toLowerCase()) {
+            case "event":
+                t = Event.eventFromCommand(s);
+                break;
+            case "deadline":
+                t = Deadline.deadlineFromCommand(s);
+                break;
+            case "todo":
+                t = Todo.todoFromCmd(s);
+                break;
+            default:
+                throw new InvalidTaskException("This task does not fit known tasks (event, deadline, todo)");
+            } // end switch for type of task
+            assert t != null : "t should be a created task by now";
+            r.updateMessage(TaskList.addToList(t).getMessage());
+        } catch (InvalidTaskException e) {
+            r.updateWithException(e.printException(), e);
+        }
+        return r;
     }
 }
