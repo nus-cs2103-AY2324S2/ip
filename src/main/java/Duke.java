b9ebb3795fcc14f180c56499e3087be93d000310
ValenciaLim <valencialimrt@gmail.com>
2024-02-14 11:26:24 +0800

    Complete Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
-    public static void main(String[] args) {
+    public static void greet() {
+        String name = "Cal";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(logo);
+    }
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
