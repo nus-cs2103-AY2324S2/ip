e7789123c7905eb7c03d0dc66e9185d14b2a0b83
AwJoey <awjoey26@gmail.com>
2024-02-15 23:52:49 +0800

    Edit Command.java, Parser.java function

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -3,27 +3,27 @@ package duke.command;
 /**
  * A collection of Command Enum objects to store available commands and its signatures.
  */
-public enum Command {
-    TODO("todo", "todo <task_name>"),
-    DEADLINE("deadline", "deadline <task_name> /by <due_date>"),
-    EVENT("event","event <task_name> /from <start_date> /to <end_date"),
-    LIST("list", "list"),
-    MARK ("mark", "mark <task_number>"),
-    UNMARK("unmark", "unmark <task_number>"),
-    DELETE("delete", "delete <task_number>"),
-    FIND("find", "find <keyword>"),
-    TERMINATE("bye", "bye");
-
-    public final String COMMAND_NAME; // the command keyword
-    public final String COMMAND_SIGNATURE; // the command format
-
-    /**
-     * Create a command with a keyword and its format
-     * @param commandName
-     * @param commandSignature
-     */
-    private Command(String commandName, String commandSignature) {
-        this.COMMAND_NAME = commandName;
-        this.COMMAND_SIGNATURE = commandSignature;
-    }
+public class Command {
+//    TODO("todo", "todo <task_name>"),
+//    DEADLINE("deadline", "deadline <task_name> /by <due_date>"),
+//    EVENT("event","event <task_name> /from <start_date> /to <end_date"),
+//    LIST("list", "list"),
+//    MARK ("mark", "mark <task_number>"),
+//    UNMARK("unmark", "unmark <task_number>"),
+//    DELETE("delete", "delete <task_number>"),
+//    FIND("find", "find <keyword>"),
+//    TERMINATE("bye", "bye");
+//
+//    public final String COMMAND_NAME; // the command keyword
+//    public final String COMMAND_SIGNATURE; // the command format
+//
+//    /**
+//     * Create a command with a keyword and its format
+//     * @param commandName
+//     * @param commandSignature
+//     */
+//    private Command(String commandName, String commandSignature) {
+//        this.COMMAND_NAME = commandName;
+//        this.COMMAND_SIGNATURE = commandSignature;
+//    }
 }
