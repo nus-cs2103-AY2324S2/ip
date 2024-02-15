b5ff9b5c793a0c2cd7d74d0d1a4b57294e7d669d
Yskie <shikang@u.nus.edu>
2024-02-15 19:38:29 +0800

    Fix JUnit Test

--- src/test/java/tiny/ParserTest.java
+++ src/test/java/tiny/ParserTest.java
@@ -5,22 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import tiny.exceptions.TinyException;
-
-
+import tiny.lists.ControlList;
 
 public class ParserTest {
-
     @Test
     public void unknownCommandTest() throws TinyException {
         Parser parser = new Parser();
-        String actualSring = parser.parse("Random Unknown Command", new TaskList());
+        String actualSring = parser.parse("Random Unknown Command", new ControlList());
         assertEquals("I'm sorry, but I don't know what that means :-(", actualSring);
     }
 
     @Test
     public void byeTest() throws TinyException {
         Parser parser = new Parser();
-        String actualSring = parser.parse("bye", new TaskList());
+        String actualSring = parser.parse("bye", new ControlList());
         assertEquals(true, parser.isExit());
         assertEquals("Bye. Hope to see you again soon!", actualSring);
     }
--- src/test/java/tiny/TaskListTest.java
+++ src/test/java/tiny/TaskListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import tiny.exceptions.TinyException;
+import tiny.lists.TaskList;
 import tiny.tasks.Deadline;
 import tiny.tasks.Todo;
 
