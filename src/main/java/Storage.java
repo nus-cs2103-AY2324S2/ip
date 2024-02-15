908319e55c8bed8917691f90ae671cbf2769806d
huekoh <huexkoh@gmail.com>
2024-02-15 14:56:40 +0800

    Removed redundant imports

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,9 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Storage {
