30fc421ca6589e47dfd02838b22598f5f275d859
YuZizhen <yuzizhen123@gmail.com>
2024-02-15 11:14:56 +0800

    Simple javafx implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import task.Task;
 import task.TaskList;
@@ -10,7 +14,10 @@ import parser.Parser;
  * The main class for duke chat bot.
  */
 
-public class Duke {
+public class Duke extends Application {
+    public Duke() {
+
+    }
 
     public static void main(String[] args) {
         Ui duke = new Ui("Zizhen");
@@ -25,5 +32,14 @@ public class Duke {
         parser.parse();
 
         duke.exit();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
