1144348833cdbaefdf4648007b5867a394069c33
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2024-02-16 02:40:44 +0800

    Update Commands enumeration with specialized help messages
    Instructions provided on how to use each command (shown
    when user types the 'help' command)  is currently statically
    typed and printed in the parseAndExecuteCommand()
    method's switch.
    
    Placing the instructions for each of the possible commands in the
    Commands enumeration makes it easier to maintain and add to.
    
    Let's make sure to update the enumeration each time we add
    a new command instead of cluttering the parseAndExecuteCommand()
    method

--- src/main/java/commands/Commands.java
+++ src/main/java/commands/Commands.java
@@ -13,5 +13,38 @@ public enum Commands {
     UNMARK,
     BYE,
     DELETE,
-    FIND
+    FIND;
+
+    public String getHelpMessage() {
+        switch (this) {
+        case HELP:
+            return "Get list of commands: help";
+        case TODO:
+            return "Create todo: todo [task name] (eg: todo borrow book)";
+        case DEADLINE:
+            return "Create deadline: deadline [task name] /by [due date] "
+                    + "(eg: deadline homework /by Sun 6pm)";
+        case EVENT:
+            return "Create event: event [task name] /from [from date] /to [to date] "
+                    + "(eg: event concert /from Mon 6pm /to Mon 8pm)";
+        case LIST:
+            return "List current tasks: list";
+        case MARK:
+            return "Mark a task as complete: mark [task number on list] "
+                    + "(eg: mark 2)";
+        case UNMARK:
+            return "Mark a task as incomplete: unmark [task number on list] "
+                    + "(eg: unmark 2)";
+        case DELETE:
+            return "Delete a task: delete [task number on list] "
+                    + "(eg: delete 3)";
+        case FIND:
+            return "Find a task by keyword: find [keyword] "
+                    + "(eg: find book)";
+        case BYE:
+            return "Close Tam the Task Manager: bye";
+        default:
+            return "getHelpMessage() ERROR";
+        }
+    }
 }
