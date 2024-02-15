215f37124789cb3a3d5268e1a6dd8dd291301848
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-02-15 15:36:17 +0800

    Add JavaFX GUI

---
+++ src/main/java/simpli/ui/Gui.java
@@ -0,0 +1,36 @@
+//package simpli.ui;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.stage.Stage;
+//import simpli.core.Simpli;
+//
+//import java.io.IOException;
+//
+//
+///**
+// * GUI for simpli application.
+// */
+//public class Gui extends Application {
+//    private Simpli simpli;
+//
+//    @Override
+//    public void start(Stage stage) {
+//
+//    }
+//
+//    @Override
+//    public void stop() {
+//
+//    }
+//
+//    /**
+//     * You should have your own function to generate a response to user input.
+//     * Replace this stub with your completed method.
+//     */
+//    public String getResponse(String input) {
+//        return simpli.processInput(input);
+//    }
+//}
