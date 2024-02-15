5dc01311e0d54432b65f608ee29cf339e56052ec
LIU JIAYAO <e1092092@u.nus.edu>
2024-02-15 22:22:04 +0800

    Improve the code quality

--- src/test/java/jiayou/task/DeadlineTest.java
+++ src/test/java/jiayou/task/DeadlineTest.java
@@ -2,28 +2,33 @@ package jiayou.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
 
     @Test
     public void testToStringForStore() {
-        Deadline deadline = new Deadline("Watch movies", "2020-01-01");
+        LocalDate by = LocalDate.parse("2020-01-01");
+        Deadline deadline = new Deadline("Watch movies", by);
         String expected = "D | 0 | Watch movies | by2020-01-01";
         assertEquals(expected, deadline.toStringForStore());
     }
 
     @Test
     public void testToString() {
-        Deadline deadline = new Deadline("Watch movies", "2020-01-01");
+        LocalDate by = LocalDate.parse("2020-01-01");
+        Deadline deadline = new Deadline("Watch movies", by);
         String expected = "[D][ ] Watch movies (by: Jan 1 2020)";
         assertEquals(expected, deadline.toString());
     }
 
     @Test
     public void testToDoDescription() {
+        LocalDate by = LocalDate.parse("2020-01-01");
         String description = "Watch movies";
-        Deadline deadline = new Deadline("Watch movies", "2020-01-01");
+        Deadline deadline = new Deadline("Watch movies", by);
         assertEquals(description, deadline.getDescription());
     }
 }
--- src/test/java/jiayou/task/EventTest.java
+++ src/test/java/jiayou/task/EventTest.java
@@ -2,19 +2,25 @@ package jiayou.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
     public void testToString() {
-        Event event = new Event("Complete homework", "2010-01-01", "2020-01-01");
+        LocalDate from = LocalDate.parse("2010-01-01");
+        LocalDate to = LocalDate.parse("2020-01-01");
+        Event event = new Event("Complete homework", from, to);
         String expected = "[E][ ] Complete homework (from: Jan 1 2010 to: Jan 1 2020)";
         assertEquals(expected, event.toString());
     }
 
     @Test
     public void testToStringForStore() {
-        Event event = new Event("Complete homework", "2010-01-01", "2020-01-01");
+        LocalDate from = LocalDate.parse("2010-01-01");
+        LocalDate to = LocalDate.parse("2020-01-01");
+        Event event = new Event("Complete homework", from, to);
         String expected = "E | 0 | Complete homework | from 2010-01-01 to 2020-01-01";
         assertEquals(expected, event.toStringForStore());
     }
@@ -22,7 +28,9 @@ public class EventTest {
     @Test
     public void testToDoDescription() {
         String description = "Complete homework";
-        Event event = new Event("Complete homework", "2010-01-01", "2020-01-01");
+        LocalDate from = LocalDate.parse("2010-01-01");
+        LocalDate to = LocalDate.parse("2020-01-01");
+        Event event = new Event("Complete homework", from, to);
         assertEquals(description, event.getDescription());
     }
 }
