691326e5847d512b9a81cf0717c7143588d4cbe0
yunchao <tanyunchao@gmail.com>
2024-02-16 07:05:39 +0800

    Add docs

--- src/main/java/Martin/Task.java
+++ src/main/java/Martin/Task.java
@@ -1,38 +1,77 @@
 package Martin;
 
+/**
+ * Represents a task.
+ */
 public abstract class Task {
     private String description;
     private boolean isDone;
 
+    /**
+     * Constructs a task with the given description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Returns whether the task is done or not.
+     *
+     * @return {@code true} if the task is done, {@code false} otherwise.
+     */
     public boolean getIsDone() {
         return this.isDone;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void unmarkAsDone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return The status icon of the task. It is a tick symbol if the task is done, otherwise an empty space.
+     */
     public String getStatusIcon() {
-        return (this.isDone ? "\u2713" : " "); // return tick or X symbols
+        return (this.isDone ? "\u2713" : " ");
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string representation of the task, including the status icon and description.
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
+    /**
+     * Returns a string representation of the task for storing in a file.
+     *
+     * @return A string representation of the task for storing in a file.
+     */
     public abstract String toFileString();
 }
