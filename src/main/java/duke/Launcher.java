b0feff6ecc5a84b05b3ac3f38a9d820776b1c1ff
whelan-low <e0958047@u.nus.edu>
2024-02-16 23:31:29 +0800

    Revert "added commit"
    This reverts commit 6a3ad2da065a02700fcf8a7d8dd8b0d961714449.

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -2,9 +2,6 @@ package duke;
 
 import javafx.application.Application;
 
-/**
- * The class to launch the program
- */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Duke.class, args);;
