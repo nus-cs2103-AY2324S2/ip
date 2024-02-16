1d0b813f36944b52aac4ae81074c723347014d53
tahsinhasem <tahsinhasem@gmail.com>
2024-02-17 05:07:15 +0800

    Improved readability

--- src/main/java/Utils/Storage.java
+++ src/main/java/Utils/Storage.java
@@ -26,13 +26,9 @@ public class Storage {
         File file = new File(filepath + filename);
         boolean fileExists = file.exists();
 
-        //create the file if it does not exist
+        //save an empty task list to the file if it does not exist
         if (!fileExists) {
-            FileOutputStream fos = new FileOutputStream(filepath + filename);
-            java.io.ObjectOutputStream oos = new java.io.ObjectOutputStream(fos);
-            oos.writeObject(new ArrayList<Task>());
-            oos.close();
-            fos.close();
+            saveTasks(new TaskList());
         }
     }
 
@@ -53,7 +49,6 @@ public class Storage {
 
 
     public static void saveTasks(TaskList taskList) throws IOException, SecurityException{
-
         try{
             FileOutputStream fos = new FileOutputStream(filepath + filename);
             java.io.ObjectOutputStream oos = new java.io.ObjectOutputStream(fos);
@@ -61,13 +56,9 @@ public class Storage {
             oos.close();
             fos.close();
         }
-        catch (FileNotFoundException e){
-            throw new FileNotFoundException("The file to save the task list was not found.");
-        }
         catch (IOException e){
             throw new IOException("An error occurred while saving the task list.");
         }
-
     }
 
     /**
@@ -89,9 +80,6 @@ public class Storage {
             fis.close();
 
             return TaskList.from(list);
-
-        } catch (FileNotFoundException e) {
-            throw new FileNotFoundException("The file to load the task list was not found.");
         } catch (IOException e) {
             throw new IOException("An error occurred while loading the task list.");
         } catch (ClassNotFoundException e) {
@@ -99,22 +87,7 @@ public class Storage {
         }
     }
 
-    public static void main(String[] args) throws IOException, TaskListFullException, SecurityException, ClassNotFoundException {
-        //
-//        TaskList ls = new TaskList();
-//
-//        ls.add_task(new Event("event1", "from1", "to1"));
-//        ls.add_task(new Event("event2", "from2", "to2"));
-//        ls.add_task(new Todo("todo1"));
-//        ls.add_task(new Todo("todo2"));
-//        ls.add_task(new Deadline("deadline1", "by1"));
-//        ls.add_task(new Deadline("deadline2", "by2"));
-//
-//        System.out.println(ls);
-//
-//        System.out.println(ls.getList());
-//        saveTasks(ls);
-//        System.out.println("Saved");
+    public static void main(String[] args) throws IOException, SecurityException, ClassNotFoundException {
 
         TaskList ls2 = loadTasks();
         System.out.println(ls2);
