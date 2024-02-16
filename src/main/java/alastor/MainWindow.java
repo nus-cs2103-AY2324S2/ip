14fe8377270046e7e0e7cac35f72a8939554d908
Murugan-Maniish <muruganmaniish1@gmail.com>
2024-02-16 14:47:05 +0800

    Implement JavaFX
    Followed tutorial.
    Differences:
    * Added greetings() in Alastor.java and called in Main.java to enable showing greeting in app.
    * removed prefHeight in VBox in MainWindow.fxml to stop lists from being truncated.

---
+++ src/main/java/alastor/MainWindow.java
@@ -0,0 +1,68 @@
+package alastor;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Alastor duke;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Charlie.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Alastor.png"));
+
+    /**
+     * Initializes the main window.
+     */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Alastor d) {
+        duke = d;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        if (input.equals("bye")) {
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(3000), ae -> Platform.exit()));
+            timeline.play();
+        }
+        userInput.clear();
+    }
+
+    public void showGreet(String text) {
+        dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(text, dukeImage));
+    }
+}
+
