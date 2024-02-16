0c620736d542cc5e27705c539ac1bf4b4f95471b
AmanZainal <aman@u.nus.edu>
2024-02-17 03:03:19 +0800

    Add Dummy Test file

---
+++ src/test/java/duke/DukeTest.java
@@ -0,0 +1,20 @@
+package duke;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A test class for the Duke class.
+ */
+public class DukeTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
