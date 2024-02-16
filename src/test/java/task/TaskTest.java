7391543a5857ad764b565e142f00c3edb66a7bcf
alfaloo <zhiylu528@gmail.com>
2024-02-16 17:26:07 +0800

    add more junit tests

--- src/test/java/task/TaskTest.java
+++ src/test/java/task/TaskTest.java
@@ -3,7 +3,6 @@ package task;
 import exceptions.*;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
-import command.Command;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
