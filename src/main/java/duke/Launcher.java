6a3ad2da065a02700fcf8a7d8dd8b0d961714449
whelan-low <e0958047@u.nus.edu>
2024-02-16 23:27:57 +0800

    added commit

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -2,6 +2,9 @@ package duke;
 
 import javafx.application.Application;
 
+/**
+ * The class to launch the program
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Duke.class, args);;
