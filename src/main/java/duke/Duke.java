7cda908b5fff856753174bdc525f9625cada7da5
erv-teo <107994607+erv-teo@users.noreply.github.com>
2024-02-16 21:30:58 +0800

    Placed classes in one package

---
+++ src/main/java/duke/Duke.java
@@ -0,0 +1,56 @@
+package duke;
+
+public class Duke {
+    private static TaskList tasks;
+    private Storage storage;
+    private Command currentCommand;
+
+    private Ui ui;
+
+    Duke() {
+        this.tasks = new TaskList();
+        this.storage = new Storage(this.tasks);
+        this.ui = new Ui();
+
+    }
+
+    public void run() throws DukeException{
+        this.storage.createFile();
+        this.storage.load(tasks);
+
+        ui.hello();
+        
+
+        boolean exit = false;
+
+        while (!exit) {
+            String str = ui.getNextLine();
+            Command command = ui.getCommand(str);
+
+            try {
+                command.execute(tasks, str);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+            if (command == Command.BYE) {
+                this.storage.write(tasks);
+                exit = true;
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) throws DukeException {
+        Duke duke = new Duke();
+        duke.run();
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/duke/Event.java
@@ -0,0 +1,59 @@
+package duke;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Event extends Task {
+    private String name;
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private boolean done;
+    private static String identifier = "[E]";
+
+    DateTimeFormatter dtfoutput = DateTimeFormatter.ofPattern("dd MMM yyyy HHmm");
+
+    public Event(String name, LocalDateTime from, LocalDateTime to) {
+        super(name);
+        this.name = name;
+        this.from = from;
+        this.to = to;
+        this.done = false;
+    }
+
+    public Event(String name, LocalDateTime from, LocalDateTime to, boolean done) {
+        super(name);
+        this.name = name;
+        this.from = from;
+        this.to = to;
+        this.done = done;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return "[X]" + identifier + " " + this.name + " (from " + dtfoutput.format(from) + " to " + dtfoutput.format(to) + ")";
+        } else {
+            return "[ ]" + identifier + " " + this.name + " (from " + dtfoutput.format(from) + " to " + dtfoutput.format(to) + ")";
+        }
+    }
+
+    public String getInput() {
+        String mark;
+        if (this.done) {
+            mark = "1";
+        } else {
+            mark = "0";
+        }
+        String str = String.format("%s/event/%s/%s/%s", mark, name, from, to);
+        return str;
+    }
+
+}
