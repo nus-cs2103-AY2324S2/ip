a32f0e8ef45a9c5232a050efea704cbbbf1809f5
matthewken19 <matthewkenelmer@gmail.com>
2024-02-16 21:06:09 +0800

    Combine the two try catch clauses in load method into one.
    The two try catch clauses catch similar exceptions (FileNotFoundException and IOException) and output the same code.
    
    Combining the two clauses provide better readibility and reduce code duplication

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -28,34 +28,25 @@ class Storage {
     ArrayList<Task> load() {
         ArrayList<Task> tasks = new ArrayList<>();
         // If file doesn't exist, create the parent directories and the file
-        if (!file.exists()) {
-            try {
-                // Create parent directories
-                file.getParentFile().mkdirs();
-                // Create file
-                file.createNewFile();
-            } catch (IOException e) {
-                System.out.println("An error occurred while creating the file.\n" +
-                        "Program will run without saved file.");
-                return tasks;
-            }
-        }
         try {
+            // Create parent directories if it does not exist
+            file.getParentFile().mkdirs();
+            // Create file if it does not exist
+            file.createNewFile();
+            // Read file
             Scanner sc = new Scanner(file);
             String lineRead;
             while (sc.hasNext()) {
                 lineRead = sc.nextLine();
                 tasks.add(Parser.parseToTask(lineRead, "\\|"));
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println("An error occurred while opening the file\n" +
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file.\n" +
                     "Program will run without saved file.");
-            return tasks;
         } catch (DukeException ex) {
             System.out.println("An error occurred while reading the file.\n" +
                     "File may contain corrupted data!\n" +
                     "Program will run without saved file.");
-            return tasks;
         }
         return tasks;
     }
@@ -71,7 +62,7 @@ class Storage {
             fw.write(tasks.toStore());
             fw.close();
         } catch (IOException ex) {
-            System.out.println("Failed tot save progress!");
+            System.out.println("Failed to save progress!");
         }
     }
 }
