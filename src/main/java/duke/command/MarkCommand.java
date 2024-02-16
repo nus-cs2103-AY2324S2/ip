05398c3e16e26fad00b0afea0bb63def0a01d315
AmanZainal <aman@u.nus.edu>
2024-02-17 03:31:22 +0800

    Restructure code to remove "arrowhead" style

--- src/main/java/duke/command/MarkCommand.java
+++ src/main/java/duke/command/MarkCommand.java
@@ -20,35 +20,31 @@ public class MarkCommand extends Command {
 
     @Override
     public TaskList execute(TaskList tasks) throws DukeException {
-        // Assuming arguments is a list of one string
-        if (!arguments.isEmpty()) {
-            try {
-                int index = Integer.parseInt(arguments.get(0)) - 1;
-                if (index >= 0 && index < tasks.getNoOfTasks()) {
-                    Task task = tasks.getTask(index);
-                    if (!task.isDone()) {
-                        task.markAsDone();
-                        System.out.printf("Marked task %d as done: %s\nGood job! ~(^-^)\n",
-                            index + 1, task.getDescription());
-                    } else {
-                        throw new DukeException(String.format("I can't do that.. Task %d is already done! ~(T_T)\n",
-                            index + 1));
-                    }
-                } else {
-                    throw new DukeException(String.format("I can't do that.. Task index %s is out of range! ~(T_T)\n",
-                        arguments.get(0)));
-                }
-            } catch (DukeException e) {
-                System.out.printf("%s", e.getMessage());
-                return tasks;
-            } catch (NumberFormatException e) {
-                System.out.printf("Sigh.. That's not a valid number! Try 'mark <NUMBER>'.\n");
-                return tasks;
-            }
-        } else {
+        if (arguments.isEmpty()) {
             System.out.printf("I can't do that.. You need to specify a task index! Try \"mark <index>\".. \n");
+            return tasks;
+        }
+
+        try {
+            int index = Integer.parseInt(arguments.get(0)) - 1;
+            if (index < 0 || index >= tasks.getNoOfTasks()) {
+                throw new DukeException(String.format("I can't do that.."
+                    + " Task index %s is out of range! ~(T_T)\n", arguments.get(0)));
+            }
+
+            Task task = tasks.getTask(index);
+            if (task.isDone()) {
+                throw new DukeException(String.format("I can't do that.."
+                    + " Task %d is already done! ~(T_T)\n", index + 1));
+            }
+
+            task.markAsDone();
+            System.out.printf("Marked task %d as done: %s\nGood job! ~(^-^)\n", index + 1, task.getDescription());
+        } catch (NumberFormatException e) {
+            System.out.printf("Sigh.. That's not a valid number! Try 'mark <NUMBER>'.\n");
         }
 
         return tasks;
     }
+
 }
