2275841bd51dc52a2738d15b522b34cf0c5f42a8
soons1 <103474763+soons1@users.noreply.github.com>
2024-02-16 17:04:42 +0800

    Fix hardcoded filepath for release on jar file

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
      * @throws DukeException if the Duke instance cannot be initialised.
      */
     public Main() throws DukeException {
-        duke = new Duke("data/duke.txt");
+        duke = new Duke("duke.txt");
     }
 
     @Override
