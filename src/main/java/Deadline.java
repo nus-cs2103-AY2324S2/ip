ba8e46007ba4cfbd96bbf9854c8348e36fb4a795
minghowC <e0725080@u.nus.edu>
2024-02-16 13:19:01 +0800

    Implement dates and times feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     protected String deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+		try{
+			this.deadline = LocalDate.parse(deadline);
+		} catch (DateTimeParseException e) {
+			System.out.println("Invalid date format. Please use yyyy--mm-dd.");
+			this.deadline = null;
+		}
     }
 
     @Override
