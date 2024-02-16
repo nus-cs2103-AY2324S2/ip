502845f8a773660eb7c347d5991e2eaa501d4cc8
tahsinhasem <tahsinhasem@gmail.com>
2024-02-17 03:58:34 +0800

    Added create storage function

--- src/main/java/Utils/Storage.java
+++ src/main/java/Utils/Storage.java
@@ -1,4 +1,26 @@
 package Utils;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Storage {
+
+
+    /**
+     * Creates a storage file if it does not exist.
+     * @throws IOException
+     */
+    public static void createStorage() throws IOException, SecurityException{
+        String filepath = "data/dude.txt";
+        File file = new File(filepath);
+
+        boolean fileExists = file.exists();
+
+        if (!fileExists) {
+            boolean created = file.createNewFile();
+        }
+    }
+
+
+
 }
