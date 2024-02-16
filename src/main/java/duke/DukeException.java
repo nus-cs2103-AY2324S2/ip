996294a71954211242dc7b338c07cc82e2f7d954
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-02-16 12:07:38 +0800

    DukeException.java: Add JavaDoc comments

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,6 +1,14 @@
 package duke;
 
+/**
+ * Represents an exception that is specific to the duke assistant program.
+ */
 public class DukeException extends Exception {
+    /**
+     * Constructor for creating a DukeException object.
+     *
+     * @param message String containing more detailed information about the exception.
+     */
     public DukeException(String message) {
         super(message);
     }
