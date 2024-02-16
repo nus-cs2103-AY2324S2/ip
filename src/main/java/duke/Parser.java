ceaa1f214f52681f2127da5a74e5e2346e840066
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-02-16 12:29:17 +0800

    Parser.java: Add JavaDoc comments

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
+/**
+ * Parser class contains methods to check validity and decipher user input commands.
+ */
 public class Parser {
     private static HashSet<String> validCommands;
 
+    /**
+     * Constructor for creating a Parser object.
+     */
     public Parser() {
         validCommands = new HashSet<String>();
         validCommands.addAll(Arrays.asList("bye",
@@ -19,21 +25,45 @@ public class Parser {
                 "delete"));
     }
 
+    /**
+     * Returns a boolean that specifies whether the provided input string contains a valid command.
+     *
+     * @param input User input string.
+     * @return Boolean specifying validity of command.
+     */
     public boolean checkValidCommand(String input) {
         StringTokenizer st = new StringTokenizer(input);
         String identifier = st.nextToken().toLowerCase();
         return validCommands.contains(identifier);
     }
 
+    /**
+     * Returns the command portion (first word) of the user input string in lower case.
+     *
+     * @param input User input string.
+     * @return String containing a command.
+     */
     public String parseCommand(String input) {
         StringTokenizer st = new StringTokenizer(input);
         return st.nextToken().toLowerCase();
     }
 
+    /**
+     * Returns an int that corresponds to the index of a task.
+     *
+     * @param input User input string.
+     * @return Index of a task.
+     */
     public int parseTaskIndex(String input) {
         return Integer.parseInt(input) - 1;
     }
 
+    /**
+     * Returns a string that contains the arguments of a command in the user input string.
+     *
+     * @param input User input string.
+     * @return String containing arguments for a command.
+     */
     public String parseArguments(String input) {
         StringTokenizer st = new StringTokenizer(input);
         st.nextToken();
@@ -44,6 +74,14 @@ public class Parser {
         return arguments.trim();
     }
 
+    /**
+     * Returns a String array containing the arguments required for creating a deadline task.
+     * Index 0 contains the description of the deadline task.
+     * Index 1 contains the complete by date of the deadline task.
+     *
+     * @param input String containing command arguments.
+     * @return String array containing arguments for creating deadline task.
+     */
     public String[] parseDeadlineArguments(String input) {
         String[] deadlineInfo = new String[2];
         String[] inputSplit = input.split("/");
@@ -63,6 +101,15 @@ public class Parser {
         return deadlineInfo;
     }
 
+    /**
+     * Returns a String array containing the arguments required for creating an event task.
+     * Index 0 contains the description of the event task.
+     * Index 1 contains the start date of the event task.
+     * Index 2 contains the end date of the event task.
+     *
+     * @param input String containing command arguments.
+     * @return String array containing arguments for creating event task.
+     */
     public String[] parseEventArguments(String input) {
         String[] eventInfo = new String[3];
         String[] inputSplit = input.split("/");
