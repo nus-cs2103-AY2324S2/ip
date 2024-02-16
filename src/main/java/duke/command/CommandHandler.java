0472fffb0056a43a552e874e18b035abe18c3033
Ujjwal Gaurav <ujjwalgaurav.me@gmail.com>
2024-02-17 00:43:31 +0800

    improve code quality through division of command handlers

---
+++ src/main/java/duke/command/CommandHandler.java
@@ -0,0 +1,27 @@
+package duke.command;
+
+import duke.task.TaskManager;
+
+public abstract class CommandHandler {
+    protected final TaskManager taskManager;
+
+    public CommandHandler(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
+    protected boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    protected String printError(String command) {
+        return "Sorry, the task number is missing after " + command.toUpperCase() +
+                ". Can you please specify a valid task number from the list?";
+    }
+
+    public abstract String handle(String[] userMessage);
+}
---
+++ src/main/java/duke/command/FindCommandHandler.java
@@ -0,0 +1,33 @@
+package duke.command;
+
+import duke.task.Task;
+import duke.task.TaskDisplay;
+import duke.task.TaskManager;
+
+import java.util.List;
+
+public class FindCommandHandler extends CommandHandler {
+    private final TaskDisplay taskDisplay;
+
+    public FindCommandHandler(TaskManager taskManager, TaskDisplay taskDisplay) {
+        super(taskManager);
+        this.taskDisplay = taskDisplay;
+    }
+
+    @Override
+    public String handle(String[] userMessage) {
+        if (userMessage.length < 2) {
+            return "Can you specify a keyword after the find command so that I can help you better?";
+        }
+
+        String keyword = userMessage[1];
+        List<Task> matchingTasks = taskManager.findTask(keyword);
+
+        if (matchingTasks.isEmpty()) {
+            return "No matching tasks found.";
+        }
+
+        return taskDisplay.printFindTaskList(matchingTasks);
+    }
+}
+
