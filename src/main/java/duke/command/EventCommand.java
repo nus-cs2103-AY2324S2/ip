812376bdc056442a5cd7ed404915544696354502
bachletuankhai <bachletuankhai@gmail.com>
2024-02-17 17:19:32 +0800

    Refactor add task commands

--- src/main/java/duke/command/EventCommand.java
+++ src/main/java/duke/command/EventCommand.java
@@ -1,15 +1,13 @@
 package duke.command;
 
 import duke.DukeException;
-import duke.state.ProgramState;
 import duke.task.Event;
 import duke.task.Task;
-import duke.task.TaskList;
 
 /**
  * Represents a command to add a task with a starting time and an ending time.
  */
-public class EventCommand extends Command {
+public class EventCommand extends AddTaskCommand {
     private String taskDescription;
     private String startTime;
     private String endTime;
@@ -27,19 +25,8 @@ public class EventCommand extends Command {
         this.endTime = parts.length > 2 ? parts[2].trim() : "";
     }
 
-    /**
-     * Executes the command. This command adds a task with a starting time and an
-     * ending time to the task list. The program state is set to normal after the
-     * command is executed, even if the command fails to execute.
-     *
-     * @param list  The task list to be modified.
-     * @param state The program state to be modified.
-     * @return The response to be displayed to the user.
-     * @throws DukeException If the user input is invalid (empty task
-     *                       description/starting time/ending time), or if the
-     *                       command fails to execute.
-     */
-    public String execute(TaskList list, ProgramState state) throws DukeException {
+    @Override
+    protected Task getTaskFromDescription(String description) throws DukeException {
         if (taskDescription.isEmpty()) {
             throw new EmptyTaskDescriptionException("The description of an event cannot be empty.",
                     "The description of an event cannot be empty.");
@@ -50,10 +37,6 @@ public class EventCommand extends Command {
             throw new EmptyDateTimeException("The end time of an event cannot be empty.",
                     "The end time of an event cannot be empty.");
         }
-        Task task = new Event(taskDescription, startTime, endTime);
-        list.addTask(task);
-        String response = ("Added: " + task + "\nYou now have " + list.size() + " tasks in the list.");
-        state.setNormal();
-        return response;
+        return new Event(taskDescription, startTime, endTime);
     }
 }
--- src/main/java/duke/command/TodoCommand.java
+++ src/main/java/duke/command/TodoCommand.java
@@ -1,40 +1,23 @@
 package duke.command;
 
 import duke.DukeException;
-import duke.state.ProgramState;
 import duke.task.Task;
-import duke.task.TaskList;
 import duke.task.Todo;
 
 /**
  * Represents a command to add a simple todo.
  */
-public class TodoCommand extends Command {
+public class TodoCommand extends AddTaskCommand {
     public TodoCommand(String body) {
         super(body);
     }
 
-    /**
-     * Executes the command. This command adds a todo to the task list and sets the
-     * program state to normal after the command is executed, even if the command
-     * fails to execute.
-     *
-     * @param list  The task list to be modified.
-     * @param state The program state to be modified.
-     * @return The response to be displayed to the user.
-     * @throws DukeException If the body of the command is empty, or if the command
-     *                       fails to execute.
-     */
-    public String execute(TaskList list, ProgramState state) throws DukeException {
-        String body = getBody();
-        if (body.isEmpty()) {
+    @Override
+    protected Task getTaskFromDescription(String description) throws DukeException {
+        if (description.isEmpty()) {
             throw new DukeException("The description of a todo cannot be empty.",
                     "Sorry, you need to tell me what you want to add. I can't add empty tasks.");
         }
-        Task task = new Todo(body);
-        list.addTask(task);
-        String response = ("Added: " + task + "\nYou now have " + list.size() + " tasks in the list.");
-        state.setNormal();
-        return response;
+        return new Todo(description);
     }
 }
