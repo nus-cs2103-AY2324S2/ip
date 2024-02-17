5835426ae6e0e6aa12d99429d355e8a8d439bf7d
rik <vslimrik@gmail.com>
2024-02-17 11:39:30 +0800

    Remove unused imports

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -10,9 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 
 /**
  * An example of a custom control using FXML.
