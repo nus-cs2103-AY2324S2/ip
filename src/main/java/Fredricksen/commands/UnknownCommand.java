b2014b8c9dfe761b11b86fbd4bef74e0ed2d3f6d
Zack-Tay <tayzhongkai@gmail.com>
2024-02-17 23:22:43 +0800

    Add better GUI

---
+++ src/main/java/Fredricksen/commands/UnknownCommand.java
@@ -0,0 +1,14 @@
+package Fredricksen.commands;
+
+import Fredricksen.commands.Command;
+
+public class UnknownCommand extends Command {
+
+    public UnknownCommand() {}
+
+    @Override
+    public String execute() {
+        return "Sorry Old Man Fredricksen don't recognise this input! "
+                + "Type \"help\" if you need a guide on input format!";
+    }
+}
