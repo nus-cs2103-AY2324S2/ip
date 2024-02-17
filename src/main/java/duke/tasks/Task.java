e7196631811084b935fb7f87cfd8324470641543
Carl <e0959394@u.nus.edu.sg>
2024-02-18 02:39:58 +0800

    Add tagging feature for tasks

--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -1,5 +1,8 @@
 package duke.tasks;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * The Task class represents a task with a description and a status, and provides methods to set and
  * get the status, convert the task to a string representation, and convert the task to a storage
@@ -9,6 +12,8 @@ public class Task {
     protected final String description;
 
     private boolean status;
+
+    private HashSet<String> tags;
 
     public Task(String description) {
         this.description = description;
@@ -27,8 +32,28 @@ public class Task {
     }
 
     /**
+     * Adds tag(s) to a task. Duplicate tags are ignored on addition.
+     *
+     * @param tagDetails the array which contains the #tags of the task.
+     */
+    public void addTags(String[] tagDetails) {
+        this.tags.addAll(Arrays.asList(tagDetails).subList(1, tagDetails.length));
+    }
+
+    /**
+     * Removes tag(s) from a task. Non-existent tags are ignored.
+     *
+     * @param tags the array of strings which contains the tags to remove.
+     */
+    public void removeTags(String[] tags) {
+        for (String tag : tags) {
+            this.tags.remove(tag);
+        }
+    }
+
+    /**
      * Converts a task object to its string representation for storage.
-     * 
+     *
      * @return An empty string is being returned.
      */
     public String toStorageString() {
@@ -37,7 +62,7 @@ public class Task {
 
     /**
      * Returns a string representation of a task with its status and description.
-     * 
+     *
      * @return Returns a string representation of a task.
      */
     @Override
