f0d1b9e0c45396cb3ef448eb857db6c1d094d905
STELLA-LYE <e0967875@u.nus.edu>
2024-02-17 14:13:16 +0800

    Add A-CodeQuality

--- src/main/java/runner/Parser.java
+++ src/main/java/runner/Parser.java
@@ -9,32 +9,35 @@ import java.time.format.DateTimeParseException;
  * Parser to parse the command process the necessary actions.
  */
 public class Parser {
-    private final Duke duke;
+    private final TaskList taskList;
+    private final Storage storage;
 
     /**
      * Constructor for Parser.
      * @param duke a Duke chat-bot to work on.
      */
     public Parser(Duke duke) {
-        this.duke = duke;
+        this.taskList = duke.taskList;
+        this.storage = duke.storage;
     }
 
     /**
+     * Specifically handle user command.
      * @param info The information given by the user.
+     * @returns A response to the user command.
      */
     public String handle(String info) {
-        if (info.equals("bye")) {
-            return terminate();
-        } else if (info.equals("list")) {
-            return Ui.showList(duke.taskList);
-        } else {
-            return read(info);
-        }
-    }
-
-    public String read(String info) {
         if (!info.contains(" ")) {
-            return "Not Smart to Understand -_-";
+            switch (info) {
+                case "hello":
+                    return Ui.start();
+                case "bye":
+                    return Ui.ending();
+                case "list":
+                    return Ui.showList(taskList, 1);
+                default:
+                    return "Not Smart to Understand -_-";
+            }
         }
         try {
             String[] segments = info.split(" ", 2);
@@ -46,11 +49,11 @@ public class Parser {
                 case "unmark":
                     return unmark(arg);
                 case "todo":
-                    return add_todo(info);
+                    return addTodo(info);
                 case "deadline":
-                    return add_deadline(arg);
+                    return addDeadline(arg);
                 case "event":
-                    return add_event(arg);
+                    return addEvent(arg);
                 case "find":
                     return find(arg);
                 case "delete":
@@ -64,36 +67,32 @@ public class Parser {
     }
 
     /**
-     * Terminate the Duke.
-     */
-    public String terminate() {
-        return Ui.ending();
-    }
-
-    /**
      * Actions when finding keywords.
      * @param key Keyword input.
+     * @returns All tasks containing the keyword in a list.
      */
     public String find(String key) {
         TaskList ans = new TaskList();
-        for(Task tk: duke.taskList.get_list()) {
-            if (tk.getMsg().contains(key)) {
+        for (Task tk : taskList.get_list()) {
+            if (tk.getMSG().contains(key)) {
                 ans.add(tk);
             }
         }
-        return Ui.findList(ans);
+        return Ui.showList(ans, 0);
     }
 
     /**
      * Actions when marking.
      * @param s String format of the Task index.
+     * @returns Mark message.
      */
     public String mark(String s) {
         try {
-            int n = Integer.parseInt(s) - 1;
-            duke.taskList.get_task(n).complete();
-            duke.store.saveList();
-            return Ui.markMSG(duke.taskList.get_task(n));
+            int index = Integer.parseInt(s) - 1;
+            assert index < taskList.size(): "Index Invalid";
+            taskList.getTask(index).complete();
+            storage.saveList();
+            return Ui.markMSG(taskList.getTask(index));
         } catch (IndexOutOfBoundsException e) {
             return "Index Out";
         }
@@ -102,13 +101,15 @@ public class Parser {
     /**
      * Actions when unmarking.
      * @param s String format of the Task index.
+     * @returns Unmark message.
      */
     public String unmark(String s) {
         try {
-            int num = Integer.parseInt(s) - 1;
-            duke.taskList.get_task(num).uncomplete();
-            duke.store.saveList();
-            return Ui.unmarkMSG(duke.taskList.get_task(num));
+            int index = Integer.parseInt(s) - 1;
+            assert index < taskList.size(): "Index Invalid";
+            taskList.getTask(index).uncomplete();
+            storage.saveList();
+            return Ui.unmarkMSG(taskList.getTask(index));
         } catch (IndexOutOfBoundsException e) {
             return "Index Out";
         }
@@ -117,14 +118,16 @@ public class Parser {
     /**
      * Actions when deleting.
      * @param s String format of the Task index.
+     * @returns Delete message.
      */
     public String delete(String s) {
         try {
             int index = Integer.parseInt(s) - 1;
-            Task temp = duke.taskList.get_task(index);
-            duke.taskList.remove(index);
-            duke.store.saveList();
-            return Ui.deleteMSG(temp, duke.taskList.size());
+            assert index < taskList.size() : "Index Invalid";
+            Task temp = taskList.getTask(index);
+            taskList.remove(index);
+            storage.saveList();
+            return Ui.deleteMSG(temp, taskList.size());
         } catch (IndexOutOfBoundsException a) {
             return "OOPS!!! You can not delete air~";
         } catch (NumberFormatException b) {
@@ -135,27 +138,29 @@ public class Parser {
     /**
      * Actions when adding a Todo.
      * @param info Description of a Todo.
+     * @returns Add message.
      */
-    public String add_todo(String info) {
+    public String addTodo(String info) {
         String action = info.split(" ", 2)[1];
         Task t = new Todo(action);
-        duke.taskList.add(t);
-        duke.store.saveList();
-        return Ui.addMSG(t, duke.taskList.size());
+        taskList.add(t);
+        storage.saveList();
+        return Ui.addMSG(t, taskList.size());
     }
 
     /**
      * Actions when adding a Deadline.
      * @param s Description of a Deadline.
+     * @returns Add message.
      */
-    public String add_deadline(String s) {
+    public String addDeadline(String s) {
         try {
             String msg = s.split("/by ", 2)[0];
             String by = s.split("/by ", 2)[1];
             Task d = new Deadline(msg, by);
-            duke.taskList.add(d);
-            duke.store.saveList();
-            return Ui.addMSG(d, duke.taskList.size());
+            taskList.add(d);
+            storage.saveList();
+            return Ui.addMSG(d, taskList.size());
         } catch (DateTimeParseException e) {
             return "Date Unacceptable (YYYY-MM-DD PLZ)";
         }
@@ -164,15 +169,16 @@ public class Parser {
     /**
      * Actions when adding an Event.
      * @param s Description of an Event.
+     * @returns Add message.
      */
-    public String add_event(String s) {
+    public String addEvent(String s) {
         String event = s.split("/from", 2)[0];
         String time = s.split("/from", 2)[1];
         String from = time.split("/to")[0];
         String to = time.split("/to")[1];
         Task e = new Event(event, from, to);
-        duke.taskList.add(e);
-        duke.store.saveList();
-        return Ui.addMSG(e, duke.taskList.size());
+        taskList.add(e);
+        storage.saveList();
+        return Ui.addMSG(e, taskList.size());
     }
 }
--- src/main/java/runner/Ui.java
+++ src/main/java/runner/Ui.java
@@ -7,33 +7,36 @@ import task.Task;
 public class Ui {
     /**
      * Words shown when initiating.
+     * @returns Greeting.
      */
     public static String start() {
-        return "I am  Pororo, your personal task manager. How may I assist you today?";
+        return "Hello! I'm Duke\nWhat can I do for you?\n" ;
     }
 
     /**
      * Words shown when terminating.
+     * @returns Bye.
      */
     public static String ending() {
-        return "It's great working with you! "
-                + '\n' + "See you again soon. BYE!!!";
+        return "Bye. Hope to see you again soon!\n";
     }
 
     /**
      * Words shown when marking a Task.
      * @param t Task to be marked.
+     * @returns Message after marking.
      */
     public static String markMSG(Task t) {
-        return "Well Done! I've marked this task as done:\n" + ("[X] " + t.getMsg());
+        return "Nice! I've marked this task as done:\n" + ("[X] " + t.getMSG());
     }
 
     /**
      * Words shown when unmarking a Task.
      * @param t Task to be unmarked.
+     * @returns Message after unmarking.
      */
     public static String unmarkMSG(Task t) {
-        return "Noted, I've marked this task as not done yet:\n" + ("[ ] " + t.getMsg());
+        return "OK, I've marked this task as not done yet:\n" + ("[ ] " + t.getMSG());
     }
 
 
@@ -41,8 +44,10 @@ public class Ui {
      * Words shown when deleting a Task.
      * @param t Task to be deleted.
      * @param n Tasks in TaskList afterwards.
+     * @returns Message after deleting.
      */
     public static String deleteMSG(Task t, int n) {
+        assert n >= 0 : "Index Invalid";
         return "Noted. I've removed this task:\n"
                 + t + "\n"
                 + "Now you have " + n + " tasks in the list.";
@@ -52,30 +57,23 @@ public class Ui {
      * Words shown when adding a Task.
      * @param t Task to be added.
      * @param n Tasks in TaskList afterwards.
+     * @returns Message after adding.
      */
     public static String addMSG(Task t, int n) {
-        return "Noted. I've added this task:\n"
+        return "Got it. I've added this task:\n"
                 + t + "\n"
                 + "Now you have " + n + " tasks in the list.";
     }
 
     /**
      * Show the TaskList of a Duke.
+     * @returns Task information in a TaskList.
      */
-    public static String showList(TaskList t) {
-        StringBuilder ans = new StringBuilder("Here are the tasks in your list:\n");
-        for (Task tk : t.get_list()) {
-            String info = (t.get_list().indexOf(tk)+1) + "." + tk.toString() + "\n";
-            ans.append(info);
-        }
-        return ans.toString();
-    }
-
-    /**
-     * Show the TaskList after find keywords.
-     */
-    public static String findList(TaskList t) {
-        StringBuilder ans = new StringBuilder("Here are the matching tasks in your list:\n");
+    public static String showList(TaskList t, int i) {
+        assert i == 1 || i == 0 : "Invalid Sign";
+        String find_show  = (i == 1) ? "Here are the tasks in your list:\n"
+                : "Here are the matching tasks in your list:\n";
+        StringBuilder ans = new StringBuilder(find_show);
         for (Task tk : t.get_list()) {
             String info = (t.get_list().indexOf(tk)+1) + "." + tk.toString() + "\n";
             ans.append(info);
