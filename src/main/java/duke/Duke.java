a9d881f09a8711d37c95723ff22e512a488f3589
tejas-1405 <tejas.sadasivam@gmail.com>
2024-02-17 17:04:15 +0800

    Add Storage

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,18 +8,6 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.time.LocalDate;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * Main chatbot program
