0b53afbb88038c0080ce77fa085e084b2aa8f430
AwJoey <awjoey26@gmail.com>
2024-02-17 16:09:28 +0800

    Revert changes

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -124,14 +124,14 @@ public class Duke extends Application {
         //Scroll down to the end every time dialogContainer's height changes.
         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
 
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
+//        //Part 3. Add functionality to handle user input.
+//        sendButton.setOnMouseClicked((event) -> {
+//            handleUserInput();
+//        });
+//
+//        userInput.setOnAction((event) -> {
+//            handleUserInput();
+//        });
     }
 
     /**
@@ -153,23 +153,23 @@ public class Duke extends Application {
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
      * the dialog container. Clears the user input after processing.
      */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                new DialogBox(userText, new ImageView(user)),
-                new DialogBox(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
+//    private void handleUserInput() {
+//        Label userText = new Label(userInput.getText());
+//        Label dukeText = new Label(getResponse(userInput.getText()));
+//        dialogContainer.getChildren().addAll(
+//                new DialogBox(userText, new ImageView(user)),
+//                new DialogBox(dukeText, new ImageView(duke))
+//        );
+//        userInput.clear();
+//    }
+//
+//    /**
+//     * You should have your own function to generate a response to user input.
+//     * Replace this stub with your completed method.
+//     */
+//    private String getResponse(String input) {
 //        String fullCommand = ui.readCommand();
-//        Parser.parse(fullCommand, ui);
-         return "Duke heard: " + input;
-    }
+////        Parser.parse(fullCommand, ui);
+//         return "Duke heard: " + fullCommand;
+//    }
 }
--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -1,12 +1,15 @@
 package duke.parser;
 
-import duke.command.Command;
+import duke.command.CommandEnum;
 import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.TaskList;
 import duke.task.ToDo;
 import duke.ui.Ui;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
@@ -17,6 +20,7 @@ import java.time.format.DateTimeFormatter;
  */
 public class Parser {
     public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    // public static final Pattern COMMAND_FORMAT = Pattern.compile("?<command_word>\s" );
 
     /**
      * Processes a string input to decide which command to execute
@@ -28,19 +32,19 @@ public class Parser {
         String command = input;
         String task = null;
         if (cmdSplit == -1) {
-            if (command.equals(Command.MARK.COMMAND_NAME) | command.equals(Command.UNMARK.COMMAND_NAME) |
-                    command.equals(Command.DELETE.COMMAND_NAME)) {
+            if (command.equals(CommandEnum.MARK.COMMAND_NAME) | command.equals(CommandEnum.UNMARK.COMMAND_NAME) |
+                    command.equals(CommandEnum.DELETE.COMMAND_NAME)) {
                 System.out.println("-------------------------------- \n" +
                         "Oops, I'm not sure which task you are referring to! " +
                         "Please indicate a task number (e.g. " + command + " 1) \n" +
                         "-------------------------------- \n");
-            } else if (command.equals(Command.TODO.COMMAND_NAME)) {
+            } else if (command.equals(CommandEnum.TODO.COMMAND_NAME)) {
                 System.out.println("-------------------------------- \n" +
                         "Oops, wrong format! Please indicate task details (e.g. todo CS2103 Lab 1) \n" +
                         "-------------------------------- \n");
-            } else if (command.equals(Command.LIST.COMMAND_NAME)) {
+            } else if (command.equals(CommandEnum.LIST.COMMAND_NAME)) {
                 TaskList.printTaskList();
-            } else if (command.equals(Command.TERMINATE.COMMAND_NAME)) {
+            } else if (command.equals(CommandEnum.TERMINATE.COMMAND_NAME)) {
                 return true;
             } else {
                 Ui.showErrorAndPrintCommands();
@@ -49,29 +53,29 @@ public class Parser {
             command = input.substring(0, cmdSplit);
             task = input.substring(input.indexOf(" ") + 1);
 
-            if (command.equals(Command.MARK.COMMAND_NAME) | command.equals(Command.UNMARK.COMMAND_NAME) |
-                    command.equals(Command.DELETE.COMMAND_NAME)) {
+            if (command.equals(CommandEnum.MARK.COMMAND_NAME) | command.equals(CommandEnum.UNMARK.COMMAND_NAME) |
+                    command.equals(CommandEnum.DELETE.COMMAND_NAME)) {
                 try {
                     int taskNo = Integer.parseInt(task) - 1;
-                    if (command.equals(Command.MARK.COMMAND_NAME)) {
+                    if (command.equals(CommandEnum.MARK.COMMAND_NAME)) {
                         TaskList.markTask(taskNo);
-                    } else if (command.equals(Command.UNMARK.COMMAND_NAME)) {
+                    } else if (command.equals(CommandEnum.UNMARK.COMMAND_NAME)) {
                         TaskList.unmarkTask(taskNo);
-                    } else if (command.equals(Command.DELETE.COMMAND_NAME)) {
+                    } else if (command.equals(CommandEnum.DELETE.COMMAND_NAME)) {
                         TaskList.removeTask(taskNo);
                     }
                 } catch (NumberFormatException e) {
                     Ui.showErrorNumbersOnly();
                 }
-            } else if (command.equals(Command.TODO.COMMAND_NAME) | command.equals(Command.DEADLINE.COMMAND_NAME) |
-                    command.equals(Command.EVENT.COMMAND_NAME)) {
+            } else if (command.equals(CommandEnum.TODO.COMMAND_NAME) | command.equals(CommandEnum.DEADLINE.COMMAND_NAME) |
+                    command.equals(CommandEnum.EVENT.COMMAND_NAME)) {
                 try {
                     Task t = null;
                     boolean success = false;
-                    if (command.equals(Command.TODO.COMMAND_NAME)) {
+                    if (command.equals(CommandEnum.TODO.COMMAND_NAME)) {
                         t = new ToDo(task, false);
                         success = true;
-                    } else if (command.equals(Command.DEADLINE.COMMAND_NAME)) {
+                    } else if (command.equals(CommandEnum.DEADLINE.COMMAND_NAME)) {
                         if (task == null || !task.contains(" /by ")) {
                             Ui.showErrorDeadlineFormat();
                         } else {
@@ -103,7 +107,7 @@ public class Parser {
                 } catch (DateTimeException e) {
                     Ui.showErrorDatetimeFormat();
                 }
-            } else if (command.equals(Command.FIND.COMMAND_NAME)) {
+            } else if (command.equals(CommandEnum.FIND.COMMAND_NAME)) {
                 Ui.printMatchingTasks(TaskList.findTaskByKeyword(task));
             }
             else {
