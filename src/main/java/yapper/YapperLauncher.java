87fb592b85505b6ccac55e75fd783f754f2922ff
qinxutan <e0505785@u.nus.edu>
2024-02-17 21:49:08 +0800

    Better GUI

--- src/main/java/yapper/YapperLauncher.java
+++ src/main/java/yapper/YapperLauncher.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
  * The launcher class for the YapperGui application.
  * It uses JavaFX's Application's launch method to start the GUI.
  */
-
 public class YapperLauncher {
     public static void main(String[] args) {
         // Initialize Yapper
