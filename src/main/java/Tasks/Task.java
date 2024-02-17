28c57a3434bbb66ff3fcbcb0e2cc2ccfb8285078
tahsinhasem <tahsinhasem@gmail.com>
2024-02-17 23:12:05 +0800

    Added parseDate and fotmatDate functions to Task class

--- src/main/java/Tasks/Task.java
+++ src/main/java/Tasks/Task.java
@@ -1,6 +1,10 @@
 package Tasks;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Task implements Serializable {
     private final String description;
@@ -30,5 +34,26 @@ public class Task implements Serializable {
         @Override
         public String toString() {
             return "[" + this.getStatusIcon() + "] " + this.description;
+        }
+
+        private static LocalDateTime parseDate(String string) throws DateTimeParseException {
+            String dateTimePattern = "\\d{1,2}/\\d{1,2}/\\d{4} \\d{1,2}:\\d{2}";
+            String datePattern = "\\d{1,2}/\\d{1,2}/\\d{4}";
+
+
+            if (string.matches(dateTimePattern)) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy H:m");
+                return LocalDateTime.parse(string, formatter);
+            } else if (string.matches(datePattern)) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+                LocalDate date = LocalDate.parse(string, formatter);
+                return date.atStartOfDay();
+            } else {
+                throw new DateTimeParseException("Invalid date format. Use d/M/yyyy or d/M/yyy H:m.", string, 0);
+            }
+        }
+
+        private static String formatDate(LocalDateTime date) {
+            return date.format(DateTimeFormatter.ofPattern("d MMM yyyy h:mma"));
         }
 }
