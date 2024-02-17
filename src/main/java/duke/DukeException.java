fb4df8d49f9ee96296d47a215f57d92394df31fe
yleeyilin <yiiiliiinn@gmail.com>
2024-02-17 23:15:28 +0800

    Add system exit feature
    Code that reveals what type of exception is not user friendly.
    
    To make it more user friendly, abstract away error messages.
    
    This includes overriding the toString() function.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -13,4 +13,8 @@ public class DukeException extends Exception {
     public DukeException(String errormessage) {
         super(errormessage);
     }
+
+    public String toString() {
+        return "LAMMEEEEEEE...." + super.getMessage() + "\n"; 
+    }
 }
