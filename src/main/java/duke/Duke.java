03937fc7bc026dfb0ddd21d6ace95d09e1b03d87
AwJoey <awjoey26@gmail.com>
2024-02-17 16:35:51 +0800

    Edit Duke.java: Set up Java FX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,13 +5,17 @@ import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class Duke {
+public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-
 
     public Duke() {
         ui = new Ui();
@@ -38,4 +42,12 @@ public class Duke {
         new Duke().run();
     }
 
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
 }
