6984d063bf3ee8f0d15da57c2c2df9b9ab03e98e
nusliuyifan <tonyyifanliu@gmail.com>
2024-02-17 17:48:57 +0800

    level-1 echo

--- src/main/java/Goblin.java
+++ src/main/java/Goblin.java
@@ -4,22 +4,39 @@ public class Goblin {
     static String greetings = "Hello！ I'm NetGoblin\n"
             + "What can I do for you?\n";
     static String bye = "Bye. Hope to see you agian soon!\n";
+
     public static void main(String[] args) {
         line();
         sayHello();
         line();
-        sayBye();
-        line();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()){
+            String userInput = input.next();
+            if (userInput.equals("bye")) {
+                sayBye();
+                input.close();
+                break;
+            } else {
+                echo(userInput);
+            }
+        }
     }
-
     public static void line() {
         System.out.println("--------------------------------");
     }
+
     public static void sayHello() {
         System.out.println(greetings);
     }
 
     public static void sayBye() {
+        line();
         System.out.println(bye);
+    }
+
+    public static void echo(String input) {
+        line();
+        System.out.println(input);
+        line();
     }
 }
