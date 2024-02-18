490fcc654c23a94a18ceecd4e00dde3fba091744
bryanyee33 <bryanyee33@gmail.com>
2024-02-18 14:04:31 +0800

    UserInputTest.java: Test splitByKeywords()

--- src/test/java/fishstock/UserInputTest.java
+++ src/test/java/fishstock/UserInputTest.java
@@ -1,6 +1,7 @@
 package fishstock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
@@ -60,5 +61,88 @@ public class UserInputTest {
         } catch (Exception e) {
             assertEquals("OH NOSE! Task number cannot be empty..", e.getMessage());
         }
+    }
+
+    @Test
+    public void splitByKeywords_oneKeyword() {
+        String[] output;
+
+        // Normal use case
+        output = new UserInput("some_command description /by date1").splitByKeywords("/by");
+        assertEquals("description", output[0]);
+        assertEquals("date1", output[1]);
+        assertEquals(2, output.length);
+
+        // No spaces (success)
+        output = new UserInput("some_command description/bydate1").splitByKeywords("/by");
+        assertEquals("description", output[0]);
+        assertEquals("date1", output[1]);
+        assertEquals(2, output.length);
+
+        // No by-date
+        output = new UserInput("some_command description /by").splitByKeywords("/by");
+        assertEquals("description", output[0]);
+        assertEquals("", output[1]);
+        assertEquals(2, output.length);
+
+        // Not found
+        output = new UserInput("some_command description by date1").splitByKeywords("/by");
+        assertEquals("description by date1", output[0]);
+        assertNull(output[1]);
+        assertEquals(2, output.length);
+
+        // No description
+        output = new UserInput("some_command").splitByKeywords("/by");
+        assertEquals("", output[0]);
+        assertNull(output[1]);
+        assertEquals(2, output.length);
+    }
+
+    @Test
+    public void splitByKeywords_twoKeyword() {
+        String[] output;
+
+        // Normal use case
+        output = new UserInput("some_command description /from date1 /to date2").splitByKeywords("/from", "/to");
+        assertEquals("description", output[0]);
+        assertEquals("date1", output[1]);
+        assertEquals("date2", output[2]);
+        assertEquals(3, output.length);
+
+        // No spaces (success)
+        output = new UserInput("some_command description/fromdate1/todate2").splitByKeywords("/from", "/to");
+        assertEquals("description", output[0]);
+        assertEquals("date1", output[1]);
+        assertEquals("date2", output[2]);
+        assertEquals(3, output.length);
+
+        // Wrong keyword order in input
+        output = new UserInput("some_command description /to date1 /from date2").splitByKeywords("/from", "/to");
+        assertEquals("description /to date1", output[0]);
+        assertEquals("date2", output[1]);
+        assertNull(output[2]);
+        assertEquals(3, output.length);
+
+        // Not found
+        output = new UserInput("some_command description /frm date1 / to date2").splitByKeywords("/from", "/to");
+        assertEquals("description /frm date1 / to date2", output[0]);
+        assertNull(output[1]);
+        assertNull(output[2]);
+        assertEquals(3, output.length);
+    }
+
+    @Test
+    public void splitByKeywords_noKeyword() {
+        String[] output;
+
+        // Normal use case
+        output = new UserInput("some_command super long description").splitByKeywords();
+        assertEquals("super long description", output[0]);
+        assertEquals(1, output.length);
+
+        // No description
+        output = new UserInput("some_command").splitByKeywords();
+        assertEquals("", output[0]);
+        assertEquals(1, output.length);
     }
 }
