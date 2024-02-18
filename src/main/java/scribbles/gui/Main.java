6f2ad77cbfd971e562270e63259a253938b3524b
danielleloh <szehan.loh@gmail.com>
2024-02-19 01:46:09 +0800

    fix: edit name of directory for filepath for Scribbles
    the name is edited to be more specific that it is linked to Scribbles
    so that users know which file contains information for Scribbles.

--- src/main/java/scribbles/gui/Main.java
+++ src/main/java/scribbles/gui/Main.java
@@ -15,7 +15,7 @@ import scribbles.Scribbles;
  */
 public class Main extends Application {
 
-    private Scribbles scribbles = new Scribbles("./data/taskData.txt");
+    private Scribbles scribbles = new Scribbles("./scribblesData/taskData.txt");
 
     @Override
     public void start(Stage stage) {
