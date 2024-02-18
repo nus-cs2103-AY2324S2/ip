40b648867345b5247fe4fd5cf36a4a6f38b890da
bachletuankhai <bachletuankhai@gmail.com>
2024-02-18 12:58:17 +0800

    Change dialog style

--- src/main/java/sylvia/ui/DialogBox.java
+++ src/main/java/sylvia/ui/DialogBox.java
@@ -7,13 +7,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's
@@ -21,11 +24,17 @@ import javafx.scene.shape.Circle;
  */
 public class DialogBox extends HBox {
     private static final int IMAGE_RADIUS = 20;
+    private static final String userDialogStyle = "-fx-background-color: #0084ff;"
+            + " -fx-background-radius: 20 5 20 20;";
+    private static final String sylviaDialogStyle = "-fx-background-color: #f0f0f0;"
+            + " -fx-background-radius: 5 20 20 20;";
 
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
+    @FXML
+    private VBox dialogContainer;
 
     private DialogBox(String text, Image img) {
         try {
@@ -42,6 +51,12 @@ public class DialogBox extends HBox {
         Circle clip = new Circle(displayPicture.getLayoutX() + IMAGE_RADIUS, displayPicture.getLayoutY() + IMAGE_RADIUS,
                 IMAGE_RADIUS);
         displayPicture.setClip(clip);
+        HBox.setMargin(dialogContainer, new Insets(0, 10, 0, 10));
+
+        // when text overflows, wrap it to the next line
+        if (dialog.getLayoutBounds().getWidth() > 275) {
+            dialog.setWrappingWidth(275);
+        }
     }
 
     /**
@@ -52,7 +67,10 @@ public class DialogBox extends HBox {
      * @return The dialog box representing the user's input.
      */
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
+        db.dialogContainer.setStyle(userDialogStyle);
+        db.dialog.setFill(Paint.valueOf("#FFFFFF"));
+        return db;
     }
 
     /**
@@ -76,6 +94,8 @@ public class DialogBox extends HBox {
     public static DialogBox getSylviaDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.dialogContainer.setStyle(sylviaDialogStyle);
+        db.dialog.setFill(Paint.valueOf("#000000"));
         return db;
     }
 }
