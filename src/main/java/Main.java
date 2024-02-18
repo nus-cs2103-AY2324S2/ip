0209922cb98c3092dee103230083b3e691b116b4
appleraincoat <james20111951@gmail.com>
2024-02-18 16:32:15 +0800

    Add Ui.png

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Ypxmm Bot");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setYpxmm(ypxmm);
             fxmlLoader.<MainWindow>getController().setStage(stage);
