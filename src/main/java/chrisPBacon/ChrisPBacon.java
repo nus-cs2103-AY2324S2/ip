b584e700489a0f1fb90d351663e22357ce8642b9
agreatdayy <xy.lee1099@gmail.com>
2024-02-18 23:04:15 +0800

    Add Launcher for Gui app

--- src/main/java/chrisPBacon/ChrisPBacon.java
+++ src/main/java/chrisPBacon/ChrisPBacon.java
@@ -2,10 +2,6 @@ package chrisPBacon;
 
 import java.io.FileNotFoundException;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import task.TaskList;
 import util.Storage;
 import util.Ui;
@@ -15,7 +11,7 @@ import util.UserInput;
  *  This class contains the main method for the chatbot, ChrisP Bacon.
  *  ChrisP Bacon is a chatbot that manages the user's list of tasks.
  */
-public class ChrisPBacon extends Application {
+public class ChrisPBacon {
     private final Storage storage;
     private final Ui ui;
     private TaskList tasks;
@@ -34,27 +30,12 @@ public class ChrisPBacon extends Application {
         }
     }
 
-    /**
-     * Runs the chatbot program.
-     */
-    public void run() {
-        ui.printIntro();
-
-        UserInput userInput = new UserInput();
-        while (!userInput.isInputBye()) {
-            userInput.processInput(ui, this.tasks);
-        }
-        // if user entered "bye", save list and exit chatbot.
-        ui.printBye();
-        storage.save(this.tasks);
+    public String getGreeting() {
+        return ui.printIntro();
     }
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+    public String getResponse(String input) {
+        UserInput userInput = new UserInput(input);
+        return userInput.processInput(ui, this.tasks);
     }
 }
