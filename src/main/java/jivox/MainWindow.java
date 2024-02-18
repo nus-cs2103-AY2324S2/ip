0101e399e79d0d473bac43155ec763197ee78e4f
Prabhat Kumar <itsprabxxx@gmail.com>
2024-02-18 16:29:59 +0800

    Fix Checkstyle

--- src/main/java/jivox/MainWindow.java
+++ src/main/java/jivox/MainWindow.java
@@ -10,9 +10,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
