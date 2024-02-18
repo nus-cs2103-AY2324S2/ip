aa75aa13253e7df5836a7951005dd8b30f741772
huekoh <huexkoh@gmail.com>
2024-02-18 23:24:08 +0800

    Added more java documentation

--- src/main/java/bmo/command/DefaultCommand.java
+++ src/main/java/bmo/command/DefaultCommand.java
@@ -4,13 +4,29 @@ import bmo.util.Storage;
 import bmo.util.TaskList;
 import bmo.ui.Ui;
 
+/**
+ * Represents a default command to be executed.
+ */
 public class DefaultCommand extends Command {
 
     private final int type;
 
+    /**
+     * Constructor for a default command.
+     *
+     * @param type The type of default command.
+     */
     public DefaultCommand(int type) {
         this.type = type;
     }
+
+    /**
+     * Executes the default command.
+     *
+     * @param tasks   The list of tasks.
+     * @param ui      The user interface.
+     * @param storage The storage.
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         if (this.type == 0) {
             ui.printErrInvalidCommand();
--- src/main/java/bmo/command/EventCommand.java
+++ src/main/java/bmo/command/EventCommand.java
@@ -8,18 +8,35 @@ import bmo.task.Events;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents an event command to be executed.
+ */
 public class EventCommand extends Command {
 
     private final String desc;
     private final LocalDateTime start;
     private final LocalDateTime end;
 
+    /**
+     * Constructor for an event command.
+     *
+     * @param desc  The description of the task.
+     * @param start The start date and time of the task.
+     * @param end   The end date and time of the task.
+     */
     public EventCommand(String desc, LocalDateTime start, LocalDateTime end) {
         this.desc = desc;
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Executes the event command.
+     *
+     * @param tasks   The list of tasks.
+     * @param ui      The user interface.
+     * @param storage The storage.
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         Task newTask = new Events(this.desc, this.start, this.end);
         tasks.add(newTask);
