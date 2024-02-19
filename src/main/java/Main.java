3621f926e5bc35dc5d2249abb3407eab02fa4ac9
jyztintan <justintanwk2001@gmail.com>
2024-02-19 16:26:45 +0800

    Update screenshot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("TaskYapper");
             fxmlLoader.<MainWindow>getController().setTaskYapper(yapper);
             stage.show();
         } catch (IOException e) {
