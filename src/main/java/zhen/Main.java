859781024890b9916ab741f7850dfe911cf9b686
Li_Zizhen <e1075569@u.nus.edu>
2024-02-19 18:19:17 +0800

    change a field's name from duke to zhen

--- src/main/java/zhen/Main.java
+++ src/main/java/zhen/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Zhen duke = new Zhen("database.ser");
+    private Zhen zhen = new Zhen("database.ser");
 
     @Override
     public void start(Stage stage) {
@@ -23,7 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setZhen(duke);
+            fxmlLoader.<MainWindow>getController().setZhen(zhen);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
