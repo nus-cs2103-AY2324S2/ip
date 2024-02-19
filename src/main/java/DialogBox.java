c88f268aa5311d4cf39b5e4b7de0029e83edf4af
jyztintan <justintanwk2001@gmail.com>
2024-02-19 15:05:45 +0800

    Add padding between profile pic and text

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -34,6 +35,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        dialog.setPadding(new Insets(0, 10, 0, 10));
         displayPicture.setImage(img);
     }
 
