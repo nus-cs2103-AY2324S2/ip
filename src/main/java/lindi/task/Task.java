c638564f7e41c9b73574b57b07c65e12418644fa
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-02-19 22:05:00 +0800

    Break up Create factory method into seperate methods

--- src/main/java/lindi/task/Task.java
+++ src/main/java/lindi/task/Task.java
@@ -26,53 +26,53 @@ public abstract class Task { // Adapted from Course Website
     }
 
     /**
-     * Factory method for subclasses of Task.
-     *
-     * @param fullString full user input string
-     * @return ToDo, Deadline or Event depending on input string
-     * @throws CreateTaskException if arguments to create task are invalid
+     * @param taskString description of the ToDo
+     * @return a new ToDo task
+     * @throws CreateToDoException if taskString is null or empty
      */
-    public static Task create(String fullString) throws CreateTaskException {
-        // s is either `todo`, `event` or `deadline`
-        String[] sTokens = fullString.split(" ", 2);
-        if (sTokens.length < 2) {
-            throw new CreateTaskException("soo what exactly are you trying to do? :)");
+    public static ToDo createToDo(String taskString) throws CreateToDoException {
+        if (taskString == null || taskString.isBlank()) {
+            throw new CreateToDoException("soo what exactly are you trying to do? :)");
         }
-        String command = sTokens[0];
-        String taskString = sTokens[1];
-
-        switch (command) {
-        case "todo":
-            return createToDo(taskString);
-        case "deadline":
-            return createDeadline(taskString);
-        case "event":
-            return createEvent(taskString);
-        default:
-            assert false : "Invalid command word";
-            return null;
-        }
-    }
-
-    private static ToDo createToDo(String taskString) {
         return new ToDo(taskString);
     }
-    private static Deadline createDeadline(String taskString) throws CreateDeadlineException {
+
+    /**
+     * @param taskString description of the Deadline
+     * @return a new Deadline task
+     * @throws CreateDeadlineException if deadline input arguments are invalid
+     */
+    public static Deadline createDeadline(String taskString) throws CreateDeadlineException {
+        if (taskString == null || taskString.isBlank()) {
+            throw new CreateDeadlineException("soo what exactly are you trying to do? :)");
+        }
         if (!taskString.contains("/by")) {
             throw new CreateDeadlineException(
                     "You can't expect me to track a deadline if you don't give me the.. deadline right? haha\n"
                             + "please include '/by' followed by the deadline :)");
         }
         String[] descriptionNBy = taskString.split("/by");
+        if (descriptionNBy.length < 2 || descriptionNBy[0].isBlank()) {
+            throw new CreateDeadlineException("You can't expect me to track a deadline with no name! :)");
+        }
         if (descriptionNBy.length > 2) {
             throw new CreateDeadlineException("Don't procrastinate with multiple deadlines !! :)");
         }
         assert descriptionNBy.length == 2 : "descriptionNBy should have 2 elements: description, by.";
+
         return new Deadline(descriptionNBy[0].trim(),
                 descriptionNBy[1].trim());
     }
 
-    private static Event createEvent(String taskString) throws CreateEventException {
+    /**
+     * @param taskString description of the Event
+     * @return a new Event task
+     * @throws CreateEventException if event input arguments are invalid
+     */
+    public static Event createEvent(String taskString) throws CreateEventException {
+        if (taskString == null || taskString.isBlank()) {
+            throw new CreateEventException("soo what exactly are you trying to do? :)");
+        }
         if (!taskString.contains("/from") || !taskString.contains("/to")) {
             throw new CreateEventException(
                     "I'm not sure how you're going for an event without a start time or end time\n"
@@ -82,11 +82,15 @@ public abstract class Task { // Adapted from Course Website
             throw new CreateEventException("Sorry! /from must appear before /to :)");
         }
         String[] descriptionNFromNTo = taskString.split("/from |/to ");
+        if (descriptionNFromNTo.length < 3 || descriptionNFromNTo[0].isBlank()) {
+            throw new CreateEventException("You can't expect me to track an event with no name! :)");
+        }
         if (descriptionNFromNTo.length > 3) {
             throw new CreateEventException("um.. I'm not sure how I can track for you an event that has\n"
                     + "more than 1 start or end time! :)");
         }
         assert descriptionNFromNTo.length == 3 : "descriptionNFromNTo should have 3 elements: description, from, to.";
+
         return new Event(descriptionNFromNTo[0].trim(),
                 descriptionNFromNTo[1].trim(),
                 descriptionNFromNTo[2].trim());
