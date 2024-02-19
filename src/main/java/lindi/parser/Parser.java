354a6deac8246dc9e7b72dafa0c279273de2ee88
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-02-20 03:43:24 +0800

    Add support for mass operations for mark, unmark and delete
    Currently, operations can only be executed in isolation, that is,
    one index at a time.
    
    To support operation on multiple indices, we need the addition of
    a new Command subclass MassOpsCommand.
    
    Let's
    * Create the Command subclass MassOpsCommand.
    * Modify the Parser to support the creation of the MassOpsCommand
    to allow mass operations.

--- src/main/java/lindi/parser/Parser.java
+++ src/main/java/lindi/parser/Parser.java
@@ -1,5 +1,10 @@
 package lindi.parser;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
 import lindi.commands.Command;
 import lindi.commands.CreateDeadlineCommand;
 import lindi.commands.CreateEventCommand;
@@ -10,7 +15,10 @@ import lindi.commands.FindCommand;
 import lindi.commands.InvalidCommand;
 import lindi.commands.ListCommand;
 import lindi.commands.MarkCommand;
+import lindi.commands.MassOpsCommand;
 import lindi.commands.UnmarkCommand;
+
+
 
 /**
  * Deals with making sense (parsing) of the user command.
@@ -92,17 +100,7 @@ public class Parser {
      * @return UnmarkCommand if args are valid. <p>InvalidCommand if args is null or non-integer
      */
     private static Command prepareUnmark(String args) {
-        if (args == null) {
-            return new InvalidCommand("Uh oh ! You have to give me the index "
-                    + "of the task you want to modify :)");
-        }
-        try {
-            int listIndex = getIndexFromArgs(args);
-            return new UnmarkCommand(listIndex);
-        } catch (NumberFormatException e) {
-            return new InvalidCommand("I'm pretty sure I gave you the indexes in base 10...\n"
-                    + "there shouldn't be any characters!! :(");
-        }
+        return prepareModify(UnmarkCommand::new, args);
     }
 
     /**
@@ -112,17 +110,7 @@ public class Parser {
      * @return MarkCommand if args are valid. <p>InvalidCommand if args is null or non-integer
      */
     private static Command prepareMark(String args) {
-        if (args == null) {
-            return new InvalidCommand("Uh oh ! You have to give me the index "
-                    + "of the task you want to modify :)");
-        }
-        try {
-            int listIndex = getIndexFromArgs(args);
-            return new MarkCommand(listIndex);
-        } catch (NumberFormatException e) {
-            return new InvalidCommand("I'm pretty sure I gave you the indexes in base 10...\n"
-                    + "there shouldn't be any characters!! :(");
-        }
+        return prepareModify(MarkCommand::new, args);
     }
 
     /**
@@ -132,16 +120,121 @@ public class Parser {
      * @return DeleteCommand if args are valid. <p>InvalidCommand if args is null or non-integer
      */
     private static Command prepareDelete(String args) {
+        return prepareModify(DeleteCommand::new, args);
+    }
+
+    /**
+     * Returns the Command specified by the commandConstructor if args are valid, else InvalidCommand.
+     * Returns a MassOpsCommand if args contains multiple indices or ranges, and command supports mass operations.
+     *
+     * @param commandConstructor constructor for the command to be created
+     * @param args arguments from user input
+     * @return Command if args are valid. <p>InvalidCommand if args is invalid
+     */
+    private static Command prepareModify(Function<Integer, Command> commandConstructor, String args) {
         if (args == null) {
             return new InvalidCommand("Uh oh ! You have to give me the index "
                     + "of the task you want to modify :)");
         }
+        if (isMassOps(args)) {
+            return prepareMassOps(commandConstructor, args);
+        }
         try {
             int listIndex = getIndexFromArgs(args);
-            return new DeleteCommand(listIndex);
+            return commandConstructor.apply(listIndex);
         } catch (NumberFormatException e) {
             return new InvalidCommand("I'm pretty sure I gave you the indexes in base 10...\n"
                     + "there shouldn't be any characters!! :(");
         }
+    }
+
+    private static boolean isMassOps(String args) {
+        return args.contains(",") || args.contains("-");
+    }
+
+    /**
+     * Returns a MassOpsCommand if args are valid, else InvalidCommand
+     *
+     * @param commandConstructor constructor for the command to be created
+     * @param args arguments from user input
+     * @return MassOpsCommand if args are valid. <p>InvalidCommand if args is invalid
+     */
+    private static Command prepareMassOps(Function<Integer, Command> commandConstructor, String args) {
+        try {
+            IntStream indices = getIndicesFromArgs(args);
+
+            HashSet<Integer> indexSet = new HashSet<>();
+            for (int index : indices.toArray()) {
+                if (!indexSet.add(index)) {
+                    return new InvalidCommand("You have duplicate indexes in your input :(");
+                }
+            }
+
+            return populateMassOpsCommand(commandConstructor, indexSet);
+        } catch (NumberFormatException e) {
+            return new InvalidCommand("I'm pretty sure I gave you the indexes in base 10...\n"
+                    + "there shouldn't be any characters!! :(");
+        } catch (IllegalArgumentException e) {
+            return new InvalidCommand("Invalid mass operation" + e.getMessage());
+        }
+    }
+
+    /**
+     * Returns a MassOpsCommand with the commands created from the indexes
+     *
+     * @param commandConstructor constructor for the command to be created
+     * @param indexSet set of indexes to be used to create the Commands in MassOpsCommand
+     * @return MassOpsCommand with the commands created from the indexes
+     */
+    private static MassOpsCommand populateMassOpsCommand(
+            Function<Integer, Command> commandConstructor, HashSet<Integer> indexSet) {
+        MassOpsCommand massOpsCommand = new MassOpsCommand();
+        indexSet.stream().sorted((a, b) -> b - a)
+                .forEach(index -> massOpsCommand
+                        .addCommand(commandConstructor.apply(index)));
+        return massOpsCommand;
+    }
+
+    /**
+     * @param args string of indices or ranges
+     * @return IntStream of indices
+     * @throws IllegalArgumentException if range indices are invalid
+     * @throws NumberFormatException if indices are not numbers
+     */
+    private static IntStream getIndicesFromArgs(String args) throws IllegalArgumentException, NumberFormatException {
+        return Arrays.stream(args.split(","))
+                .map(String::trim)
+                .flatMapToInt(Parser::flattenRangeToIndices);
+    }
+
+    /**
+     * @param indexOrRange index or range string
+     * @return IntStream of indices
+     * @throws IllegalArgumentException if range indices are invalid
+     * @throws NumberFormatException if indices are not numbers
+     */
+    private static IntStream flattenRangeToIndices(String indexOrRange)
+            throws IllegalArgumentException, NumberFormatException {
+        if (indexOrRange.contains("-")) {
+            return getIndicesFromRange(indexOrRange);
+        } else {
+            return IntStream.of(Integer.parseInt(indexOrRange));
+        }
+    }
+
+    /**
+     * @param range string of range indices
+     * @return IntStream of indices
+     * @throws IllegalArgumentException if range indices are invalid
+     * @throws NumberFormatException if indices are not numbers
+     */
+    private static IntStream getIndicesFromRange(String range) throws IllegalArgumentException, NumberFormatException {
+        String[] endPoints = range.split("-");
+        int start = Integer.parseInt(endPoints[0]);
+        int end = Integer.parseInt(endPoints[1]);
+        if (start > end) {
+            throw new IllegalArgumentException("Start index cannot be greater than end index");
+        }
+        return IntStream.range(start, end + 1);
     }
 }
