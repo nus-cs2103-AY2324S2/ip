7c51015a3900cbccd3af076585a9cd9c8415dec1
John Yoo <e0968839@u.nus.edu>
2024-02-19 12:28:46 +0800

    Add CodeQuality
    There are several code quality issues for this
    project, which I solved by removing most of the
    nested loops, as well as changing some variable
    names, and limiting the function length to 30
    lines.

--- src/main/java/TalkingJohn/Ui.java
+++ src/main/java/TalkingJohn/Ui.java
@@ -6,21 +6,21 @@ package talkingjohn;
 public class Ui {
 
     /**
-     * Displays a greeting message when the application starts.
+     * Returns a greeting message when the application starts.
      */
     public String greeting() {
         return "Hello, I am TalkingJohn\nWhat can I do for you?\n";
     }
 
     /**
-     * Displays a goodbye message when the application exits.
+     * Returns a goodbye message when the application exits.
      */
     public String goodbye() {
         return "Bye, hope to see you soon";
     }
 
     /**
-     * Displays an error message when the user input is empty.
+     * Returns an error message when the user input is empty.
      *
      * @param input The empty user input.
      */
@@ -30,7 +30,7 @@ public class Ui {
     }
 
     /**
-     * Displays a message after adding a task to the task list.
+     * Returns a message after adding a task to the task list.
      *
      * @param task        The task that was added.
      * @param taskArrSize The size of the task array after adding the task.
@@ -56,7 +56,7 @@ public class Ui {
     }
 
     /**
-     * Displays an error message for invalid user input.
+     * Returns an error message for invalid user input.
      */
     public String invalidInput() {
         return "HMM? This is an activity planner. Please repeat \uD83E\uDD72";
