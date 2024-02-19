a82ad4d301b64d5112a36c8ec8e3dfcb970bc574
angsongyee <angsongyee@gmail.com>
2024-02-19 15:46:10 +0800

    Fix gradle config to build fat jars correctly

---
+++ src/main/java/shodan/Launcher.java
@@ -0,0 +1,15 @@
+package shodan;
+
+/**
+ * Wrapper class for launching the application.
+ */
+public class Launcher {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
