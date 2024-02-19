69b3fcf8314cd7843f4f6f27e96157c57e8acc6a
yuechen2001 <zhuyuechen@gmail.com>
2024-02-19 19:22:54 +0800

    Add title of application using JavaFX

--- src/main/java/jimmy/gui/Main.java
+++ src/main/java/jimmy/gui/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("My friend Jimmy");
             fxmlLoader.<MainWindow>getController().setJimmy(jimmy);
             stage.show();
         } catch (IOException e) {
