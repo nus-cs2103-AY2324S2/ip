d9610295906d3bd278894444db4ae6d2849e8717
jyztintan <justintanwk2001@gmail.com>
2024-02-19 17:24:05 +0800

    Add new JUnit tests

--- src/test/java/TaskYapperTest.java
+++ src/test/java/TaskYapperTest.java
@@ -1,22 +1,17 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
 public class TaskYapperTest {
     @Test
     public void dummyTest() {
-        assertEquals(2, 2);
-    }
-
-    @Test
-    public void anotherDummyTest() {
-        assertEquals(4, 4);
+        TaskYapper yapper = new TaskYapper("./data/taskyapper.txt");
+        String message = yapper.getResponse("abc");
+        assertEquals(message, "Error: What's YAPpening??!! Please yap your instruction more clearly");
     }
 
     public static void main(String[] args) {
         TaskYapperTest test = new TaskYapperTest();
         test.dummyTest();
-        test.anotherDummyTest();
     }
 
 }
