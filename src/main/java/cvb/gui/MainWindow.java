7debd5464b200427803d9abf4034ab6e8ba7478b
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-19 22:58:59 +0800

    Remove unused import

--- src/main/java/cvb/gui/MainWindow.java
+++ src/main/java/cvb/gui/MainWindow.java
@@ -4,7 +4,6 @@ import cvb.ConvoBot;
 import cvb.exceptions.ExitException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
