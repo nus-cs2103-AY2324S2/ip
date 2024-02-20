c663686e146b4bfc5e4414bd9d58d19e2d6ba897
vnnamng <e0969384@u.nus.edu>
2024-02-20 14:59:40 +0800

    Add GUI Window Title

--- src/main/java/paimon/Main.java
+++ src/main/java/paimon/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setBot(paimon);
+            stage.setTitle("Paimon Chat Window");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
