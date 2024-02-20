26319b2b9247ccdbe6e9c38d54e6fcc0235238e6
jskimdev <kim.junseo1128@gmail.com>
2024-02-20 15:43:18 +0800

    Test on a new device

--- src/main/java/bytetalker/ByteTalker.java
+++ src/main/java/bytetalker/ByteTalker.java
@@ -32,6 +32,7 @@ import javafx.scene.image.ImageView;
  * @since 2024-01-28
  */
 public class ByteTalker extends Application {
+    //testing
     private TaskList tasks;
     private Storage storage;
     private Ui ui;
