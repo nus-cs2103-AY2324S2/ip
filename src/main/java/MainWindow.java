f0653628323ad897c84017faa30de2283b026754
CJerrong <chong.jer.rong@gmail.com>
2024-02-20 15:58:48 +0800

    Add background colors and image crop

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -4,9 +4,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import haro.Haro;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.paint.Color;
+
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
@@ -21,6 +26,7 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
 
     private Haro haro;
+    private double imageRadius = 220.0;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Osaka.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Haro.png"));
@@ -28,7 +34,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
-        dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(Ui.greet(), dukeImage));
+        dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(Ui.greet(), cropImage(dukeImage, imageRadius)));
     }
 
     public void setHaro(Haro h) {
@@ -44,9 +50,18 @@ public class MainWindow extends AnchorPane {
         String input = userInput.getText();
         String response = haro.getResponse(input);
         dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
+                DialogBox.getUserDialog(input, cropImage(userImage, imageRadius)),
+                DialogBox.getDukeDialog(response, cropImage(dukeImage, imageRadius))
         );
         userInput.clear();
+    }
+
+    private Image cropImage(Image image, double radius) {
+        Circle clip = new Circle(image.getWidth() / 2, image.getHeight() / 2, radius);
+        ImageView imageView = new ImageView(image);
+        imageView.setClip(clip);
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        return imageView.snapshot(parameters, null);
     }
 }
