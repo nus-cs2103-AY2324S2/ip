5abe3c2d975193e3dee762fc750e05b8c3f3ee15
guohuang <guohuang88@gmail.com>
2024-02-20 11:44:13 +0800

    Update Parser class to refactor long method using SLAP

--- src/main/java/ghbot/parser/Parser.java
+++ src/main/java/ghbot/parser/Parser.java
@@ -1,6 +1,5 @@
 package ghbot.parser;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -25,60 +24,126 @@ public class Parser {
     private static final DateTimeFormatter OUT_TIME_FORMAT = DateTimeFormatter.ofPattern("dd MMM yyyy hhmma");
 
     /**
-     * Prints corresponding string for different commands.
+     * Creates specific executor instance corresponding to the different commands.
      * @param input User input.
-     * @throws IOException Throws an IOException when there is an issue updating the file.
+     * @return Specific executor instance corresponding to the different commands.
      */
-    public static Executor parse(String[] input) throws IOException {
+    public static Executor parse(String[] input) {
         String instr = input[0];
 
-        if (instr.equalsIgnoreCase(Instruction.BYE.toString())) {
-            return new ByeExecutor();
-
-        } else if (instr.equalsIgnoreCase(Instruction.LIST.toString())) {
-            return new ListExecutor();
-
-        } else if (instr.equalsIgnoreCase(Instruction.MARK.toString())) {
-            int lstNo = Integer.parseInt(input[1]);
-            return new MarkExecutor(lstNo);
-
-        } else if (instr.equalsIgnoreCase(Instruction.UNMARK.toString())) {
-            int lstNo = Integer.parseInt(input[1]);
-
-            return new UnmarkExecutor(lstNo);
-
-        } else if (instr.equalsIgnoreCase(Instruction.TODO.toString())) {
-            return new TodoExecutor(input[1]);
-
-        } else if (instr.equalsIgnoreCase(Instruction.DEADLINE.toString())) {
-            String[] subStr = input[1].split("/by");
-            LocalDateTime inputTime = LocalDateTime.parse(subStr[1].trim(), IN_TIME_FORMAT);
-            String formattedTime = inputTime.format(OUT_TIME_FORMAT);
-
-            return new DeadlineExecutor(subStr[0], formattedTime);
-
-        } else if (instr.equalsIgnoreCase(Instruction.EVENT.toString())) {
-            String[] subStr = input[1].split("/from");
-            String[] subStr2 = subStr[1].split("/to");
-            LocalDateTime inputFromTime = LocalDateTime.parse(subStr2[0].trim(), IN_TIME_FORMAT);
-            String formattedFromTime = inputFromTime.format(OUT_TIME_FORMAT);
-            LocalDateTime inputToTime = LocalDateTime.parse(subStr2[1].trim(), IN_TIME_FORMAT);
-            String formattedToTime = inputToTime.format(OUT_TIME_FORMAT);
-
-            return new EventExecutor(subStr[0], formattedFromTime, formattedToTime);
-
-        } else if (instr.equalsIgnoreCase(Instruction.DELETE.toString())) {
-            int lstNo = Integer.parseInt(input[1]);
-
-            return new DeleteExecutor(lstNo);
-
-        } else if (instr.equalsIgnoreCase(Instruction.FIND.toString())) {
-            return new FindExecutor(input[1]);
-
+        if (instr.equalsIgnoreCase(Instruction.BYE.name())) {
+            return createNewByeExecutor();
+        } else if (instr.equalsIgnoreCase(Instruction.LIST.name())) {
+            return createNewListExecutor();
+        } else if (instr.equalsIgnoreCase(Instruction.MARK.name())) {
+            return createNewMarkExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.UNMARK.name())) {
+            return createNewUnmarkExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.TODO.name())) {
+            return createNewTodoExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.DEADLINE.name())) {
+            return createNewDeadlineExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.EVENT.name())) {
+            return createNewEventExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.DELETE.name())) {
+            return createNewDeleteExecutor(input[1]);
+        } else if (instr.equalsIgnoreCase(Instruction.FIND.name())) {
+            return createNewFindExecutor(input[1]);
         } else {
-            // Unreachable code as ui.validateInput already checks for all possible instructions
             assert false : instr;
         }
         return null;
+    }
+
+    /**
+     * Creates a new instance of ByeExecutor.
+     * @return A new instance of ByeExecutor.
+     */
+    public static ByeExecutor createNewByeExecutor() {
+        return new ByeExecutor();
+    }
+
+    /**
+     * Creates a new instance of ListExecutor.
+     * @return A new instance of ListExecutor.
+     */
+    public static ListExecutor createNewListExecutor() {
+        return new ListExecutor();
+    }
+
+    /**
+     * Creates a new instance of MarkExecutor.
+     * @param description A string containing the number of the specific task on the list.
+     * @return A new instance of MarkExecutor.
+     */
+    public static MarkExecutor createNewMarkExecutor(String description) {
+        int listNo = Integer.parseInt(description);
+        return new MarkExecutor(listNo);
+    }
+
+    /**
+     * Creates a new instance of UnmarkExecutor.
+     * @param description A string containing the number of the specific task on the list.
+     * @return A new instance of UnmarkExecutor.
+     */
+    public static UnmarkExecutor createNewUnmarkExecutor(String description) {
+        int listNo = Integer.parseInt(description);
+        return new UnmarkExecutor(listNo);
+    }
+
+    /**
+     * Creates a new instance of TodoExecutor.
+     * @param description A string containing the description of the task.
+     * @return A new instance of TodoExecutor.
+     */
+    public static TodoExecutor createNewTodoExecutor(String description) {
+        return new TodoExecutor(description);
+    }
+
+    /**
+     * Creates a new instance of DeadlineExecutor.
+     * @param description A string containing the description of the task.
+     * @return A new instance of DeadlineExecutor.
+     */
+    public static DeadlineExecutor createNewDeadlineExecutor(String description) {
+        String[] subStr = description.split("/by");
+        LocalDateTime inputTime = LocalDateTime.parse(subStr[1].trim(), IN_TIME_FORMAT);
+        String formattedTime = inputTime.format(OUT_TIME_FORMAT);
+
+        return new DeadlineExecutor(subStr[0], formattedTime);
+    }
+
+    /**
+     * Creates a new instance of EventExecutor.
+     * @param description A string containing the description of the task.
+     * @return A new instance of EventExecutor.
+     */
+    public static EventExecutor createNewEventExecutor(String description) {
+        String[] subStr = description.split("/from");
+        String[] subStr2 = subStr[1].split("/to");
+        LocalDateTime inputFromTime = LocalDateTime.parse(subStr2[0].trim(), IN_TIME_FORMAT);
+        String formattedFromTime = inputFromTime.format(OUT_TIME_FORMAT);
+        LocalDateTime inputToTime = LocalDateTime.parse(subStr2[1].trim(), IN_TIME_FORMAT);
+        String formattedToTime = inputToTime.format(OUT_TIME_FORMAT);
+        return new EventExecutor(subStr[0], formattedFromTime, formattedToTime);
+    }
+
+    /**
+     * Creates a new instance of DeleteExecutor.
+     * @param description A string containing the number of the specific task on the list.
+     * @return A new instance of DeleteExecutor.
+     */
+    public static DeleteExecutor createNewDeleteExecutor(String description) {
+        int listNo = Integer.parseInt(description);
+        return new DeleteExecutor(listNo);
+    }
+
+    /**
+     * Creates a new instance of FindExecutor.
+     * @param description A string containing the description of a task that the user is finding.
+     * @return A new instance of FindExecutor.
+     */
+    public static FindExecutor createNewFindExecutor(String description) {
+        return new FindExecutor(description);
     }
 }
