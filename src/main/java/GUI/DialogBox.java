ef7e3d2c76e97ac09b2b5d98246d0256171279b4
tahnya <111273056+tahnya@users.noreply.github.com>
2024-02-20 21:12:56 +0800

    Add dialog box styling

--- src/main/java/GUI/DialogBox.java
+++ src/main/java/GUI/DialogBox.java
@@ -2,27 +2,58 @@ package GUI;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextAlignment;
 
 public class DialogBox extends HBox {
 
     private Label text;
     private ImageView displayPicture;
+    private Color textboxColor;
+    private int TEXTBOX_HEIGHT = 100;
+    private int TEXTBOX_WIDTH = 240;
 
-    public DialogBox(Label l, ImageView iv) {
+    public DialogBox(Label l, ImageView iv, String type) {
         text = l;
         displayPicture = iv;
+        if (type.equals("USER")) {
+            textboxColor = Color.LIGHTBLUE;
+        } else {
+            textboxColor = Color.LIGHTPINK;
+        }
 
         text.setWrapText(true);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
 
+        Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
+
+        // Create a rectangle shape with rounded corners to enclose the text
+        Rectangle rect = new Rectangle();
+        rect.setArcWidth(20);
+        rect.setArcHeight(20);
+        rect.setFill(textboxColor);
+        rect.setWidth(TEXTBOX_WIDTH); // Adjust the width as needed
+        rect.setHeight(TEXTBOX_HEIGHT); // Adjust the height as needed
+
+        StackPane textBox = new StackPane();
+        textBox.getChildren().addAll(rect, text);
+        text.setTextAlignment(TextAlignment.RIGHT);
+
         this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
+        this.setSpacing(10);
+        this.getChildren().addAll(textBox, displayPicture);
+        this.setPadding(new Insets(10));
     }
 
     /**
@@ -36,11 +67,11 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+        return new DialogBox(l, iv, "USER");
     }
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
-        var db = new DialogBox(l, iv);
+        var db = new DialogBox(l, iv, "");
         db.flip();
         return db;
     }
