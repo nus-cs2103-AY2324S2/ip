c621d97ada45dbc45017d3b4481c127ac2c868e0
tahnya <111273056+tahnya@users.noreply.github.com>
2024-02-20 19:44:14 +0800

    Create helloWorld GUI

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+
+import signal.Duke;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
--- src/main/java/signal/Duke.java
+++ src/main/java/signal/Duke.java
@@ -1,27 +1,27 @@
 package signal;
 
-import signal.task.*;
+import signal.task.Task;
+
 import signal.util.Parser;
 import signal.util.Storage;
 import signal.util.Ui;
 
-import java.io.*;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Duke {
+public class Duke extends Application {
     private static Storage fileManager = new Storage();
     private static ArrayList<Task> taskList = fileManager.loadTasks();
     private static Ui ui = new Ui(taskList, fileManager);
     public static Parser parser = new Parser(taskList, ui);
 
+    public Duke() {
+
+    }
 
     public static void main(String[] args) {
         ui.intro();
@@ -29,7 +29,6 @@ public class Duke {
 
         while(true) {
             String userInput = ui.scan();
-            String[] inputArray = userInput.split(" ");
 
             if (userInput.equals("bye")) {
                 // Exit program
@@ -38,6 +37,16 @@ public class Duke {
             parser.read(userInput);
         }
         ui.leave();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+
     }
 }
 
