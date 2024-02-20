b88ceb1a9850148afe59ea01e6c01dfde4cab869
ChenXy128 <e0968917@u.nus.edu>
2024-02-20 21:43:23 +0800

    Adds JavaDoc to class Command.java

--- src/main/java/duke/main/Command.java
+++ src/main/java/duke/main/Command.java
@@ -6,14 +6,37 @@ import duke.parser.Parser;
 import duke.ui.Ui;
 import duke.tasklist.TaskList;
 import duke.task.Task;
-
 import java.io.IOException;
 
+/**
+ * Represents the command processor of the Duke application, responsible for
+ * executing user commands and interfacing between the UI, parser, storage, and task list.
+ */
 public class Command {
+    
+    /**
+     * Retrieves a string representation of the list of tasks.
+     *
+     * @param ui  The UI component of the application.
+     * @param list The task list.
+     * @return A string representing the listed tasks.
+     */
     public String getListResponse(Ui ui, TaskList list) {
         return ui.listing(list);
     }
     
+    /**
+     * Processes the marking of a task as done, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input.
+     * @return A response message after marking a task as done.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getMarkResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         ui.handleMarkError(input, list);
@@ -23,6 +46,18 @@ public class Command {
         return ui.marking(taskToMark);
     }
     
+    /**
+     * Processes the unmarking of a task as not done, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input.
+     * @return A response message after unmarking a task as not done.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getUnmarkResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         ui.handleUnmarkError(input, list);
@@ -32,6 +67,18 @@ public class Command {
         return ui.unmarking(taskToUnmark);
     }
     
+    /**
+     * Processes the addition of a new ToDo task, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input.
+     * @return A response message after adding a new ToDo task.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getTodoResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         ui.handleTodoError(input);
@@ -40,6 +87,18 @@ public class Command {
         return ui.echo(todoTask, list);
     }
     
+    /**
+     * Processes the addition of a new Deadline task, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input.
+     * @return A response message after adding a new Deadline task.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getDeadlineResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         ui.handleDeadlineError(input);
@@ -48,6 +107,18 @@ public class Command {
         return ui.echo(deadlineTask, list);
     }
     
+    /**
+     * Processes the addition of a new Event task, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input.
+     * @return A response message after adding a new Event task.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getEventResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         ui.handleEventError(input);
@@ -56,6 +127,18 @@ public class Command {
         return ui.echo(eventTask, list);
     }
     
+    /**
+     * Processes the deletion of a task, updates storage, and returns a response message.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param storage  The storage handler to persist task changes.
+     * @param list     The task list.
+     * @param input    The user input, expected to contain the index of the task to be deleted.
+     * @return A response message after deleting a task.
+     * @throws DukeException If an error occurs during processing.
+     * @throws IOException If an I/O error occurs.
+     */
     public String getDeleteResponse(Ui ui, Parser parser, Storage storage, TaskList list, String input)
             throws DukeException, IOException {
         int index = Integer.parseInt(input.substring(7));
@@ -67,6 +150,15 @@ public class Command {
         return result;
     }
     
+    /**
+     * Processes a search for tasks containing a specific keyword and returns a response message with matching tasks.
+     *
+     * @param ui       The UI component of the application.
+     * @param parser   The parser to interpret user input.
+     * @param list     The task list.
+     * @param input    The user input, containing the keyword to search for.
+     * @return A response message listing tasks that contain the specified keyword.
+     */
     public String getFindResponse(Ui ui, Parser parser, TaskList list, String input) {
         String keyword = parser.getKeywordForFind(input);
         return ui.finding(list, keyword);
