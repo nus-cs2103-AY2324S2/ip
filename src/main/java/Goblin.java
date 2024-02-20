14fd327076709aeb28a48e950839106b7f40d27b
nusliuyifan <tonyyifanliu@gmail.com>
2024-02-21 02:14:15 +0800

    level-3 Mark as Done

--- src/main/java/Goblin.java
+++ src/main/java/Goblin.java
@@ -5,27 +5,33 @@ public class Goblin {
             + "What can I do for you?\n";
     static String bye = "Bye. Hope to see you agian soon!\n";
 
-    static ArrayList<String> list = new ArrayList<>();
+    static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         sayHello();
         Scanner input = new Scanner(System.in);
         while (input.hasNext()){
-            String userInput = input.nextLine();
-            if (userInput.equals("bye")) {
+            String inputWord = input.next();
+            if (inputWord.equals("mark")) {
+                mark(input);
+            } else if (inputWord.equals("unmark")) {
+                unmark(input);
+            } else if (inputWord.equals("list")) {
+                showList();
+            } else if (inputWord.equals("bye")) {
                 sayBye();
                 input.close();
                 break;
-            } else if (userInput.equals("list")){
-                showList();
             } else {
+                String inputLine = input.nextLine();
+                Task temptask = new Task(inputWord + inputLine);
                 line();
-                System.out.println("added: " + userInput);
+                System.out.println("added: " + inputWord + inputLine);
                 line();
-                list.add(userInput);
+                list.add(temptask);
+                }
             }
         }
-    }
     public static void line() {
         System.out.println("--------------------------------");
     }
@@ -49,8 +55,28 @@ public class Goblin {
 
     public static void showList() {
         line();
+        System.out.println("\t" + "Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
-            System.out.println("\t" + (i + 1) + ". " + list.get(i));
+            System.out.println("\t" + (i + 1) + "." + list.get(i).getStatusIcon() + list.get(i).getDescription());
         }
+        line();
+    }
+
+    public static void mark(Scanner input) {
+        int i = Integer.parseInt(input.next());
+        list.get(i - 1).done();
+        line();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t" + list.get(i - 1).getStatusIcon() + list.get(i - 1).getDescription());
+        line();
+    }
+
+    public static void unmark(Scanner input) {
+        int i = Integer.parseInt(input.next());
+        list.get(i - 1).undone();
+        line();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t" + list.get(i - 1).getStatusIcon() + list.get(i - 1).getDescription());
+        line();
     }
 }
