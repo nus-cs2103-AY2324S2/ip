7dd87568240840096811676bc715edf70fce855a
ValenciaLim <valencialimrt@gmail.com>
2024-02-20 13:15:18 +0800

    Complete Level-2

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,31 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner; 
 
 public class Cal {
+    static String line = "____________________________________________________________";
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void greet() {
-        String line = "____________________________________________________________";
         String name = "Cal";
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(line);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println(logo);
+        //System.out.println(logo);
+        System.out.println(line);
     }
     
     public static void exit() {
-        String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 
     public static void echo(String input) { 
-        String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println(input);
         System.out.println(line);
+    }
+
+    public static void list() {
+        System.out.println(line);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+        }
+        System.out.println(line);
+    }
+
+    public static String add(String input) {
+        tasks.add(input);
+        return String.format("added: %s", input);
     }
 
     public static void main(String[] args) {
@@ -34,9 +50,12 @@ public class Cal {
         Scanner sc = new Scanner(System.in);
         while(true) {
             String input = sc.nextLine();
-            if (input.toLowerCase() == "bye") {
+            if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")){
+                list();
             } else {
+                input = add(input); 
                 echo(input);
             }
         }
