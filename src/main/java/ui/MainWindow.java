6343527662053b06036fed33293246b2617cd11a
mahadhir247 <mahadhir123456@gmail.com>
2024-02-21 16:49:32 +0800

    Update user and buddy images

--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -25,8 +25,8 @@ public class MainWindow extends AnchorPane {
 
     private Buddy buddy;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Buddy.png"));
 
     /**
      * Controls behaviour when Buddy is initialized.
