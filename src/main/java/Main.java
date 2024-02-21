9271dd09cb41efd29ca1beb4b7dc6d28a3e81e4b
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-02-21 13:51:51 +0800

    Refine GUI and update profile photos
    Improve the user experience
    
    Add padding to text and changed some colours
    
    Looks better and more refined for the user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Harvard Chatbot");
             fxmlLoader.<MainWindow>getController().setHarvard(harvard);
             stage.show();
         } catch (IOException e) {
