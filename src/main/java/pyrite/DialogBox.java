5fff788a5860ef8093de1c34aae46fefac77d0db
ReflectiveObsidian <E0958151@u.nus.edu>
2024-02-21 09:56:18 +0800

    Change profile picture to circle shape

--- src/main/java/pyrite/DialogBox.java
+++ src/main/java/pyrite/DialogBox.java
@@ -11,8 +11,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Shape;
 
 /**
  * An example of a custom control using FXML.
@@ -23,7 +24,7 @@ public class DialogBox extends HBox {
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Shape profileCircle;
 
     private DialogBox(String text, Image img) {
         try {
@@ -36,7 +37,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        profileCircle.setFill(new ImagePattern(img));
     }
 
     /**
