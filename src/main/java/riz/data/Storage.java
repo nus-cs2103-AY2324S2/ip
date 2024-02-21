169e48dcb86b55e51fa329fd913e72c69d281991
TheodoreKooo <theodorekoo@gmail.com>
2024-02-21 21:23:46 +0800

    The code in the storage class is too deeply nested and convoluted.
    It has to be changed in case that more types of tasks are added,
    then we can add them easier.
    
    The three different tasks that can be created has been abstracted
    out into a method.
    
    It is easier to segregate and organize the code and where to focus
    on should we decide to look into or modify the code in the future.

--- src/main/java/riz/data/Storage.java
+++ src/main/java/riz/data/Storage.java
@@ -17,6 +17,7 @@ import java.time.LocalDateTime;
 public class Storage {
     private String filePath;
     public Storage(String filePath) {
+        assert filePath != null : "File path cannot be null";
         this.filePath = filePath;
     }
 
@@ -28,50 +29,56 @@ public class Storage {
     public ArrayList<Task> loadFromFile() {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File(this.filePath);
-         try (Scanner scanner = new Scanner(file)) {
+        try (Scanner scanner = new Scanner(file)) {
             while(scanner.hasNext()) {
                 String[] token = scanner.nextLine().split(" \\| ");
-                if (token[0].equals("T")) {
-                    ToDo todo = new ToDo(token[2]);
-                    if (token[1].equals("X")) {
-                        todo.mark();
-                    }
-                    tasks.add(todo);
-                } else if (token[0].equals("D")) {
-                    LocalDateTime by = LocalDateTime.parse(token[3], DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a"));
-                    Deadline deadline = new Deadline(token[2], by.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm")));
-                    if (token[1].equals("X")) {
-                        deadline.mark();
-                    }
-                    tasks.add(deadline);
-                } else {
-                    String[] time = token[3].split(" - ");
-                    DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a");
-                    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-                    LocalDateTime from = LocalDateTime.parse(time[0], inputFormatter);
-                    LocalDateTime to = LocalDateTime.parse(time[1], inputFormatter);
-                    Event event = new Event(token[2], from.format(outputFormatter), to.format(outputFormatter));
-                    if (token[1].equals("X")) {
-                        event.mark();
-                    }
-                    tasks.add(event);
-                }
+                tasks = createTaskList(token);
             }
-         } catch (FileNotFoundException e) {
-             File dir = new File("riz/data");
-             if(dir.mkdirs()) {
-                 System.out.println("Directory 'data' created successfully!");
-             }
-             File file2 = new File(dir, "riz.txt");
-             try {
-                 if (file2.createNewFile()) {
-                     System.out.println("File 'riz.txt' created successfully!");
-                 }
-             } catch (IOException ex) {
-                 System.out.println("Error creating file...");
-             }
-         }
-         return tasks;
+        } catch (FileNotFoundException e) {
+            File dir = new File("riz/data");
+            if(dir.mkdirs()) {
+                System.out.println("Directory 'data' created successfully!");
+            }
+            File file2 = new File(dir, "riz.txt");
+            try {
+                if (file2.createNewFile()) {
+                    System.out.println("File 'riz.txt' created successfully!");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error creating file...");
+            }
+        }
+        return tasks;
+    }
+
+    private ArrayList<Task> createTaskList(String[] token) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        if (token[0].equals("T")) {
+            ToDo todo = new ToDo(token[2]);
+            if (token[1].equals("X")) {
+                todo.mark();
+            }
+            tasks.add(todo);
+        } else if (token[0].equals("D")) {
+            LocalDateTime by = LocalDateTime.parse(token[3], DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a"));
+            Deadline deadline = new Deadline(token[2], by.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm")));
+            if (token[1].equals("X")) {
+                deadline.mark();
+            }
+            tasks.add(deadline);
+        } else {
+            String[] time = token[3].split(" - ");
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a");
+            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+            LocalDateTime from = LocalDateTime.parse(time[0], inputFormatter);
+            LocalDateTime to = LocalDateTime.parse(time[1], inputFormatter);
+            Event event = new Event(token[2], from.format(outputFormatter), to.format(outputFormatter));
+            if (token[1].equals("X")) {
+                event.mark();
+            }
+            tasks.add(event);
+        }
+        return tasks;
     }
 
     /**
@@ -79,6 +86,7 @@ public class Storage {
      * @param tasks ArrayList<Task> taken from the TaskList containing all current tasks.
      */
     public void writeToFile(ArrayList<Task> tasks) {
+        assert tasks != null : "Tasks list cannot be null";
         try (FileWriter fw = new FileWriter(this.filePath)) {
             for (Task task : tasks) {
                 fw.write(task.toString() + System.lineSeparator());
@@ -94,6 +102,7 @@ public class Storage {
     public String printFromFile() {
         StringBuilder sb = new StringBuilder();
         File file = new File(this.filePath);
+        assert file.exists() : "File does not exist";
         try (Scanner scanner = new Scanner(file)) {
             int count = 1;
             while (scanner.hasNext()) {
@@ -124,4 +133,3 @@ public class Storage {
         return i;
     }
 }
-
