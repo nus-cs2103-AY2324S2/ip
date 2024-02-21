a61ceec0b44516d1778e233572bfa0111ea18111
whelan-low <e0958047@u.nus.edu>
2024-02-22 01:40:09 +0800

    Add Test Cases

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -24,4 +24,43 @@ public class ParserTest {
         String actual = testList2.list();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void deleteTest2() {
+        Duke duke = new Duke();
+        TaskList testList = new TaskList(duke);
+        TaskList testList2 = new TaskList(duke);
+        String[] words = new String[] {"todo", "test", "123"};
+        String[] words2 = new String[] {"todo", "test", "456"};
+        String[] words3 = new String[] {"todo", "test", "789"};
+        testList.addToDo(words, true, false);
+        testList.addToDo(words2, true, false);
+        testList.addToDo(words3, true, false);
+        testList2.addToDo(words, true, false);
+        testList2.addToDo(words2, true, false);
+        testList2.addToDo(words3, true, false);
+        Parser.commands(testList, "delete 4", false, false, duke);
+        String expected = testList.list();
+        String actual = testList2.list();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteTest3() {
+        Duke duke = new Duke();
+        TaskList testList = new TaskList(duke);
+        TaskList testList2 = new TaskList(duke);
+        String[] words = new String[] {"todo", "test", "123"};
+        String[] words2 = new String[] {"todo", "test", "456"};
+        String[] words3 = new String[] {"todo", "test", "789"};
+        testList.addToDo(words, true, false);
+        testList.addToDo(words2, true, false);
+        testList.addToDo(words3, true, false);
+        testList2.addToDo(words, true, false);
+        testList2.addToDo(words2, true, false);
+        Parser.commands(testList, "delete 3", false, false, duke);
+        String expected = testList.list();
+        String actual = testList2.list();
+        assertEquals(expected, actual);
+    }
 }
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -8,12 +8,13 @@ public class TaskListTest {
 
     @Test
     public void listTest() {
-        TaskList testList = new TaskList(new Duke());
+        TaskList testList = new TaskList(null);
         String[] words = new String[] {"todo", "test", "123"};
         testList.addToDo(words, true, false);
         String actual = testList.list();
         String expected = "1: " + testList.getTaskList().get(0).toString();
         assertEquals(expected, actual);
     }
+
 
 }
