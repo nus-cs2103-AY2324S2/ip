328fce856f01228d64b37305440bba7aa4bdbf59
ruijietay <47778098+ruijietay@users.noreply.github.com>
2024-02-21 09:41:35 +0800

    Remove unused import statements

--- src/test/java/capone/commands/DeadlineCommandTest.java
+++ src/test/java/capone/commands/DeadlineCommandTest.java
@@ -3,9 +3,6 @@ package capone.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
