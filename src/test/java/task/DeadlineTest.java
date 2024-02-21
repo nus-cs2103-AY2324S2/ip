1ec2b85666f28022b0229983f22c7cc292082aac
0liverkong <oliverkong03@gmail.com>
2024-02-22 00:41:49 +0800

    Add more test cases for JUnit for DeadlineTest

--- src/test/java/task/DeadlineTest.java
+++ src/test/java/task/DeadlineTest.java
@@ -14,5 +14,11 @@ public class DeadlineTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
         Deadline d = new Deadline("homework", LocalDateTime.parse("2023-01-01 2359", formatter));
         assertEquals("D |   | homework | by: 2023-01-01 2359", d.fileRepresentation());
+        DoAfter a = new DoAfter("homework", LocalDateTime.parse("2023-01-01 2359", formatter));
+        assertEquals("A |   | homework | after: 2023-01-01 2359", a.fileRepresentation());
+        Event e = new Event("homework",
+                LocalDateTime.parse("2022-01-01 2359", formatter),
+                LocalDateTime.parse("2023-01-01 2359", formatter));
+        assertEquals("E |   | homework | from: 2022-01-01 2359 to: 2023-01-01 2359", e.fileRepresentation());
     }
 }
