80e345c6adf071e80d330b4cf2732e2c1964b6f9
tzaph <maximilliano.utomo@gmail.com>
2024-02-21 16:31:37 +0800

    Add short alias for commands and delete text-ui-test

--- src/main/java/arona/exception/AronaInvalidIndexException.java
+++ src/main/java/arona/exception/AronaInvalidIndexException.java
@@ -7,7 +7,7 @@ package arona.exception;
  */
 public class AronaInvalidIndexException extends AronaException {
     public AronaInvalidIndexException(String message) {
-        super("Sorry, sensei! You only have " + message + "task"
+        super("Sorry, sensei! You only have " + message + " task"
                 + (Integer.parseInt(message) == 1 ? "" : "s")
                 + ". Please choose a valid task number (>.<)");
     }
--- src/main/java/arona/parser/Parser.java
+++ src/main/java/arona/parser/Parser.java
@@ -20,23 +20,23 @@ public class Parser {
 
         String command = inputs[0].toLowerCase();
 
-        if (command.equals("bye")) {
+        if (command.equals("bye") || command.equals("exit") || command.equals("b")) {
             return CommandType.BYE;
-        } else if (command.equals("todo")) {
+        } else if (command.equals("todo") || command.equals("t") || command.equals("td")) {
             return CommandType.TODO;
-        } else if (command.equals("deadline")) {
+        } else if (command.equals("deadline") || command.equals("d") || command.equals("dl")) {
             return CommandType.DEADLINE;
-        } else if (command.equals("event")) {
+        } else if (command.equals("event") || command.equals("e") || command.equals("ev")) {
             return CommandType.EVENT;
-        } else if (command.equals("delete")) {
+        } else if (command.equals("delete") || command.equals("del")) {
             return CommandType.DELETE;
-        } else if (command.equals("list")) {
+        } else if (command.equals("list") || command.equals("l") || command.equals("ls") || command.equals("li")) {
             return CommandType.LIST;
-        } else if (command.equals("mark")) {
+        } else if (command.equals("mark") || command.equals("m")) {
             return CommandType.MARK;
-        } else if (command.equals("unmark")) {
+        } else if (command.equals("unmark") || command.equals("u") || command.equals("um")) {
             return CommandType.UNMARK;
-        } else if (command.equals("find")) {
+        } else if (command.equals("find") || command.equals("f")) {
             return CommandType.FIND;
         } else {
             throw new AronaInvalidCommandException("");
