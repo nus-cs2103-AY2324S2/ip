2903fc836413062f05f0831c4b3979078bc323e0
faaheem13 <faaheemrahman@gmail.com>
2024-02-21 14:28:07 +0800

    Java FX Part 1 - Launcher class added, Base Hello World Application created

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,13 +9,17 @@ import duke.task.Task;
 import duke.task.ToDo;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Represents the main class for the Duke application.
  * This class initializes the application, loads existing tasks from storage, and processes user commands.
  */
 
-public class Duke {
+public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -113,8 +117,18 @@ public class Duke {
         }
     }
 
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
     /**
-     * The entry point of the application.
+     * Serves as the entry point of the application.
      * @param args Command line arguments (unused).
      */
     public static void main(String[] args) {
