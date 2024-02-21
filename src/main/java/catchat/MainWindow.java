99f37d8216328988c8b95a4f77a1b0fc08daf3ee
Timothy <102574520+LargeCrowd@users.noreply.github.com>
2024-02-21 14:48:44 +0800

    Edit Scenebuilder
    Edited the GUI window in scenebuilder.
    Mde the helplist easier to read.
    Added one assert to MainWindow.java.

--- src/main/java/catchat/MainWindow.java
+++ src/main/java/catchat/MainWindow.java
@@ -50,5 +50,6 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getDukeDialog(response, dukeImage)
         );
         userInput.clear();
+        assert userInput.getText().isEmpty() : "User input should be cleared after processing";
     }
 }
