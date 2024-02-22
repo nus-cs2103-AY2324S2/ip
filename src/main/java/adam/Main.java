00d382f82aaf1c9e6e3ba3475c59bebb08c66a8f
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-02-22 16:42:19 +0800

    Add name to GUI

--- src/main/java/adam/Main.java
+++ src/main/java/adam/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Adam");
             fxmlLoader.<MainWindow>getController().setAdam(adam);
             fxmlLoader.<MainWindow>getController().showWelcome();
             stage.show();
