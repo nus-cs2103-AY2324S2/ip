269186b7669fc4d2da9107ffbdb8d1e39754048e
faaheem13 <faaheemrahman@gmail.com>
2024-02-23 02:26:30 +0800

    Added fxml files

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,19 +9,6 @@ import duke.task.Task;
 import duke.task.ToDo;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
-import javafx.application.Application;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 
 /**
@@ -127,8 +114,13 @@ public class Duke {
         }
     }
 
-
-
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Your Only Friend heard: " + input;
+    }
 
     /**
      * Serves as the entry point of the application.
