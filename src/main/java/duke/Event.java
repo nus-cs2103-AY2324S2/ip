804531ba5ec7da7d7b3d848dacfdae27572ae9ba
E0735389 <97508964+E0735389@users.noreply.github.com>
2024-02-23 06:53:44 +0700

    Improve the code quality
    Many methods that overrides a method or implements an abstract method
    in the parent class/interface does not have the @Override annotation.
    
    This is bad coding practice, and does not allow the compiler to
    raise warnings about incorrect overriding.
    
    This commit changes the situation by adding @Override at
    various places.

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -8,17 +8,19 @@ import java.time.LocalDateTime;
 public class Event extends Task {
     protected LocalDateTime from, to;
 
-    public Event (String description, String from, String to) {
+    public Event(String description, String from, String to) {
         super(description);
         this.from = LocalDateTime.parse(from, Constants.INPUT_FORMATTER);
         this.to = LocalDateTime.parse(to, Constants.INPUT_FORMATTER);
     }
 
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from.format(Constants.OUTPUT_FORMATTER)
             + " to: " + to.format(Constants.OUTPUT_FORMATTER) + ")";
     }
 
+    @Override
     public String serializeToCommand(int taskIndex) {
         return "event " + description + " /from " + from.format(Constants.INPUT_FORMATTER)
             + " /to " + to.format(Constants.INPUT_FORMATTER) + "\n" + serializeDoneMark(taskIndex);
