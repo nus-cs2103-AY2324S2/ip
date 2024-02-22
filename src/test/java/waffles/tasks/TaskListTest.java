c0e0358a73b6787cb37e769f85fabd49ce9be497
shawnnlimm <shawnlim2525@gmail.com>
2024-02-22 12:00:58 +0800

    fix: Fix bug for TaskListTest

--- src/test/java/waffles/tasks/TaskListTest.java
+++ src/test/java/waffles/tasks/TaskListTest.java
@@ -2,12 +2,23 @@ package waffles.tasks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 
 public class TaskListTest {
 
     @Test
     public void add_taskList_success() {
+        String root = System.getProperty("user.dir");
+        String path = Path.of(root, "src/test/java/waffles/tasks/task_list_test.txt").toString();
+        File f = new File(path);
+
+        if (f.exists()) {
+            f.delete();
+        }
+
         TaskList taskList = new TaskList("src/test/java/waffles/tasks/task_list_test.txt");
 
         taskList.addToDoTask("go to the gym");
@@ -17,6 +28,14 @@ public class TaskListTest {
 
     @Test
     public void missing_argument_exceptionThrown() {
+        String root = System.getProperty("user.dir");
+        String path = Path.of(root, "src/test/java/waffles/tasks/task_list_test.txt").toString();
+        File f = new File(path);
+
+        if (f.exists()) {
+            f.delete();
+        }
+
         TaskList taskList = new TaskList("src/test/java/waffles/tasks/task_list_test.txt");
 
         try {
