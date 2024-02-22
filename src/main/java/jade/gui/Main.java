b29d5cf3a58d8e89994ee8c7e4cdaafe757e2b0a
fy17ohhh <hester03sfy@gmail.com>
2024-02-22 12:00:24 +0800

    Add documentation using ChatGPT

--- src/main/java/jade/gui/Main.java
+++ src/main/java/jade/gui/Main.java
@@ -10,21 +10,34 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * A GUI for Jade using FXML.
+ * The main class responsible for starting the Jade GUI application.
+ * Some comments are improved using ChatGPT 3.5 using the prompt:
+ * "add comments for the following class: {code}"
  */
 public class Main extends Application {
+    // Path to the FXML file defining the main window
     private static final String FXML_FILE_PATH = "/view/MainWindow.fxml";
+    // Instance of the Jade class to manage the underlying logic
     private final Jade jade = new Jade();
 
+    /**
+     * The entry point of the application, responsible for initializing and displaying the main window.
+     *
+     * @param stage The primary stage for this application.
+     */
     @Override
     public void start(Stage stage) {
         try {
+            // Load the FXML file for the main window
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(FXML_FILE_PATH));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            // Set the scene to the primary stage
             stage.setScene(scene);
             stage.setTitle("Jade");
+            // Get the controller associated with the FXML file and set the Jade instance
             fxmlLoader.<MainWindow>getController().setJade(jade);
+            // Display the stage
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/jade/gui/UserDialogBox.java
+++ src/main/java/jade/gui/UserDialogBox.java
@@ -11,39 +11,62 @@ import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
 
 /**
- * This control represents a dialog box consisting of an ImageView to represent the user
- * and a label containing text from user.
+ * Represents a dialog box in the user interface,
+ * containing an ImageView for the user's display picture
+ * and a Label for displaying text entered by the user.
+ * Some comments are improved using ChatGPT 3.5 using the prompt:
+ * "add comments for the following class: {code}"
  */
 public class UserDialogBox extends HBox {
+    // Path to the FXML file defining the user dialog box
     private static final String FXML_FILE_PATH = "/view/UserDialogBox.fxml";
     @FXML
     private Label dialog;
     @FXML
     private ImageView displayPicture;
-
+    /**
+     * Private constructor to create a UserDialogBox instance.
+     * @param text The text content of the dialog.
+     * @param img The image representing the user's display picture.
+     */
     private UserDialogBox(String text, Image img) {
         try {
+            // Load the FXML file
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource(FXML_FILE_PATH));
+            // Set this instance as the controller for the FXML file
             fxmlLoader.setController(this);
+            // Set this instance as the root for the FXML file
             fxmlLoader.setRoot(this);
             fxmlLoader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // Set the text of the dialog Label
         dialog.setText(text);
+        // Set the image of the displayPicture ImageView
         displayPicture.setImage(img);
         setImgStyle();
     }
+
     /**
-     * Styles the user image and jade image which will appear in dialog boxes.
+     * Apply styling to the displayPicture ImageView, creating rounded corners.
      */
     private void setImgStyle() {
+        // Create a Rectangle to clip the displayPicture
         Rectangle clip = new Rectangle(displayPicture.getFitWidth(), displayPicture.getFitHeight());
         clip.setArcWidth(60);
         clip.setArcHeight(60);
+        // Apply the clipping to the displayPicture
         displayPicture.setClip(clip);
     }
 
+    /**
+     * Static method to create a new UserDialogBox instance with the given text and image.
+     *
+     * @param text The text to display in the dialog box.
+     * @param img  The image to display in the dialog box.
+     * @return A new UserDialogBox instance.
+     */
     public static UserDialogBox getDialog(String text, Image img) {
         return new UserDialogBox(text, img);
     }
