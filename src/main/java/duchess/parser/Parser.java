e3aa38b2b36876c3d2ca9b5b52d3fbca81c454b5
Jaime Ng <jaimengjm@gmail.com>
2024-02-22 19:18:34 +0800

    Catch error in case data file is in wrong format

--- src/main/java/duchess/parser/Parser.java
+++ src/main/java/duchess/parser/Parser.java
@@ -21,32 +21,36 @@ public class Parser {
      * @throws DuchessException if an error occurs during parsing
      */
     public static Task parseTaskFromFileString(String line) throws DuchessException {
-        Task task = null;
-        // Parse the line and create task objects accordingly
-        // Example line format: "T | 1 | read book"
-        String[] parts = line.split("\\|");
-        String type = parts[0].trim();
-        boolean isDone = parts[1].trim().equals("1");
-        String description = parts[2].trim();
+        try {
+            Task task;
+            // Parse the line and create task objects accordingly
+            // Example line format: "T | 1 | read book"
+            String[] parts = line.split("\\|");
+            String type = parts[0].trim();
+            boolean isDone = parts[1].trim().equals("1");
+            String description = parts[2].trim();
 
-        switch (type) {
-        case "T":
-            task = new ToDo(description, isDone);
-            break;
-        case "D":
-            String by = parts[3].trim();
-            task = new Deadline(description, isDone, by);
-            break;
-        case "E":
-            String from = parts[3].trim();
-            String to = parts[4].trim();
-            task = new Event(description, isDone, from, to);
-            break;
-        default:
-            System.out.println("Unknown task type: " + type);
+            switch (type) {
+            case "T":
+                task = new ToDo(description, isDone);
+                break;
+            case "D":
+                String by = parts[3].trim();
+                task = new Deadline(description, isDone, by);
+                break;
+            case "E":
+                String from = parts[3].trim();
+                String to = parts[4].trim();
+                task = new Event(description, isDone, from, to);
+                break;
+            default:
+                throw new DuchessException("Unknown task type: " + type);
+            }
+            return task;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DuchessException("Invalid task format: " + line);
         }
-
-        return task;
     }
 
     /**
