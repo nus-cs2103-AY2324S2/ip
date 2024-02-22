93661650571d69c7c3394d29209c32d13df81afc
KiatLun <louiskhoo368@gmail.com>
2024-02-23 00:27:01 +0800

    Level 10. GUI

--- src/main/java/Duke/Tim.java
+++ src/main/java/Duke/Tim.java
@@ -3,10 +3,32 @@ package Duke;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Duke {
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+
+public class Tim {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private final Path writeToFile;
+    TaskList storage = new TaskList();
+    ;
+
+
+    public Tim() throws IOException {
+        Path filePath = Paths.get("./data/duke.txt");
+        writeToFile = FileHandler.handleFile(storage, filePath);
+
+    }
 
     /**
      * Main function of the Duke program. Displays opening logo. Scans for user input.
@@ -16,6 +38,7 @@ public class Duke {
      * @throws IOException
      */
     public static void main(String[] args) throws DukeException, IOException {
+
         UI.showLogo();
         UI.showAvailCommands();
         TaskList storage = new TaskList();
@@ -33,6 +56,16 @@ public class Duke {
             input = scan.nextLine();
 
         }
+
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    @FXML
+    String getResponse(String input) throws IOException, DukeException {
+        return TaskHandler.doTasks(input, storage, writeToFile);
 
     }
 }
