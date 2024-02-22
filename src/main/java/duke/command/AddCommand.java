7fa8dd35947e3d58576cc841bac266f06594663e
JerryO3 <jieruioon@gmail.com>
2024-02-22 15:16:36 +0800

    Refactor AddCommand to switch statement to reduce arrowhead code

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -31,49 +31,46 @@ public class AddCommand implements Command {
     public String execute(String command, String[] info, ItemList itemList)
             throws CustomExceptions {
 
-        if (info[0].equals("todo")) {
-            if (info.length == 1) {
-                throw new CustomExceptions.InvalidTaskException(
-                        "Please re-enter Todo with a valid name");
-            } else {
-                return itemList.addToDo(info);
-            }
-        }
-
-        if (info[0].equals("deadline")) {
-            if (info.length == 1) {
-                throw new CustomExceptions.InvalidTaskException(
-                        "Please re-enter duke.item.Deadline with a valid name");
-            } else {
-                try {
-                    return itemList.addDeadline(info);
-                } catch (CustomExceptions.NamelessTaskException e) {
-                    throw new CustomExceptions.NamelessTaskException(
+        switch (info[0]) {
+            case "todo":
+                if (info.length == 1) {
+                    throw new CustomExceptions.InvalidTaskException(
+                            "Please re-enter Todo with a valid name");
+                } else {
+                    return itemList.addToDo(info);
+                }
+            case "deadline":
+                if (info.length == 1) {
+                    throw new CustomExceptions.InvalidTaskException(
                             "Please re-enter duke.item.Deadline with a valid name");
+                } else {
+                    try {
+                        return itemList.addDeadline(info);
+                    } catch (CustomExceptions.NamelessTaskException e) {
+                        throw new CustomExceptions.NamelessTaskException(
+                                "Please re-enter duke.item.Deadline with a valid name");
+                    }
                 }
-            }
-        }
-
-        if (info[0].equals("event")) {
-            if (info.length == 1) {
-                throw new CustomExceptions.InvalidTaskException(
-                        "Please re-enter duke.item.Event with a valid name");
-            } else {
-                try {
-                    return itemList.addEvent(info);
-                } catch (CustomExceptions.ToBeforeFromException e) {
-                    throw new CustomExceptions.ToBeforeFromException(
-                            "Please re-enter duke.item.Event /from BEFORE /to: " + command);
-                } catch (CustomExceptions.EventExceptionForFromTo e) {
-                    throw new CustomExceptions.EventExceptionForFromTo(
-                            "Could not parse /from and /to strings: " + command);
-                } catch (CustomExceptions.NamelessTaskException e) {
-                    throw new CustomExceptions.NamelessTaskException(
+            case "event":
+                if (info.length == 1) {
+                    throw new CustomExceptions.InvalidTaskException(
                             "Please re-enter duke.item.Event with a valid name");
+                } else {
+                    try {
+                        return itemList.addEvent(info);
+                    } catch (CustomExceptions.ToBeforeFromException e) {
+                        throw new CustomExceptions.ToBeforeFromException(
+                                "Please re-enter duke.item.Event /from BEFORE /to: " + command);
+                    } catch (CustomExceptions.EventExceptionForFromTo e) {
+                        throw new CustomExceptions.EventExceptionForFromTo(
+                                "Could not parse /from and /to strings: " + command);
+                    } catch (CustomExceptions.NamelessTaskException e) {
+                        throw new CustomExceptions.NamelessTaskException(
+                                "Please re-enter duke.item.Event with a valid name");
+                    }
                 }
-            }
+            default:
+                return "something went wrong";
         }
-
-        return "something went wrong";
     }
 }
