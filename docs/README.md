482d64df3bf7d40822e6ed9c41dd08d38dc2c441
PallonCX <cheexiang03@gmail.com>
2024-02-22 17:28:30 +0800

    Create user guide

--- docs/README.md
+++ docs/README.md
@@ -1,30 +1,93 @@
-# Duke User Guide
+# Killua Online User Guide
 
-// Update the title above to match the actual product name
+![Product screenshot](Ui.png)
 
-// Product screenshot goes here
+Killua Online is your best everyday partner, it helps you to keep track of various tasks with simple commands.
 
-// Product intro goes here
+## Add Tasks
 
-## Adding deadlines
+There are three types of "tasks" that are supported by Killua Online:
+1. Todo - Tasks with description.
+2. Deadline - Tasks with description, due date and time.
+3. Event - Tasks with description, start date and time, end date and time.
 
-// Describe the action and its outcome.
+### Add a Todo task
 
-// Give examples of usage
+Add a Todo task to be keep tracked.
 
-Example: `keyword (optional arguments)`
+Command Syntax: `todo {task description formed by at least one word}`
 
-// A description of the expected outcome goes here
+Example: `todo breakfast`, `todo return book`
 
-```
-expected output
-```
+### Add a Deadline task
 
-## Feature ABC
+Add a Deadline task to be keep tracked.
 
-// Feature details
+Syntax: `deadline {task description formed by at least one word} /by {yyyy-MM-dd HH:mm or yyyy-MM-dd}`
 
+Example: `deadline homework /by 2022-12-31 15:00`
 
-## Feature XYZ
+### Add an Event task
 
-// Feature details
+Add an Event task to be keep tracked.
+
+Syntax: `event {task description formed by at least one word} 
+/from {yyyy-MM-dd HH:mm or yyyy-MM-dd} /to {yyyy-MM-dd HH:mm or yyyy-MM-dd}`
+
+Example: `event exam /from 2022-12-31 15:00 /to 17:00`
+
+## List tasks 
+
+List out all the existing tasks being keep tracked.
+
+Syntax: `list`
+
+Example: `list`
+
+## Finding task by keyword
+
+Find all tasks containing a certain given keyword.
+
+Syntax: `find {keyword formed by at least one word}`
+
+Example: `find book`
+
+## Delete task
+
+Delete one of the task from the existing tasks by giving the index in tasklist.
+
+Syntax: `delete {index of task (integer in [1, total number of tasks])}`
+
+Example: `delete 1`
+
+## Mark task
+
+Mark one of the task from the existing tasks by giving the index in tasklist as done.
+
+Syntax: `mark {index of task (integer in [1, total number of tasks])}`
+
+Example: `mark 1`
+
+## Unmark task
+
+Mark one of the task from the existing tasks by giving the index in tasklist as not done.
+
+Syntax: `unmark {index of task (integer in [1, total number of tasks])}`
+
+Example: `unmark 1`
+
+## Add tag to task
+
+Add tag to one of the task from the existing tasks by giving the index in tasklist.
+
+Syntax: `tag {index of task (integer in [1, total number of tasks])} {tag (single word without space)}`
+
+Example: `tag 1 urgent`
+
+## Exit program
+
+Exit program.
+
+Syntax: `bye`
+
+Example: `bye`
--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -7,27 +7,33 @@ public class DukeException extends Exception {
 
     public static final String BYE_ERROR = "Error: Syntax of bye: bye";
     public static final String LIST_ERROR = "Error: Syntax of list: list";
-    public static final String TODO_ERROR = "Error: Syntax of todo: todo {task description} \n"
+    public static final String TODO_ERROR = "Error: Syntax of todo: "
+            + "todo {task description formed by at least one word} \n"
             + "E.g. todo breakfast";
-    public static final String DEADLINE_ERROR = "Error: Syntax of deadline: deadline {task description} "
-            + "/by ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd})\n"
+    public static final String DEADLINE_ERROR = "Error: Syntax of deadline: "
+            + "deadline {task description formed by at least one word} "
+            + "/by {yyyy-MM-dd HH:mm or yyyy-MM-dd}\n"
             + "E.g. deadline breakfast /by 2022-12-31 15:00";
-    public static final String EVENT_ERROR = "Error: Syntax of event: deadline {task description} "
-            + "/from ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd}) "
-            + "/to ({yyyy-MM-dd HH:mm} or {yyyy-MM-dd})\n"
+    public static final String EVENT_ERROR = "Error: Syntax of event: "
+            + "event {task description formed by at least one word} "
+            + "/from {yyyy-MM-dd HH:mm or yyyy-MM-dd} "
+            + "/to {yyyy-MM-dd HH:mm or yyyy-MM-dd}\n"
             + "E.g. event exam /from 2022-12-31 15:00 /to 17:00";
     public static final String UNKNOWN_ERROR = "Error: Unknown command.";
-    public static final String MARK_ERROR = "Error: Syntax of mark: mark {index of task (integer)}\n"
+    public static final String MARK_ERROR = "Error: Syntax of mark: "
+            + "mark {index of task (integer in [1, total number of tasks])}`\n"
             + "E.g. mark 1";
-    public static final String UNMARK_ERROR = "Error: Syntax of unmark: unmark {index of task (integer)}\n"
+    public static final String UNMARK_ERROR = "Error: Syntax of unmark: "
+            + "unmark {index of task (integer in [1, total number of tasks])}\n"
             + "E.g. unmark 1";
-    public static final String DELETE_ERROR = "Error: Syntax of delete: delete {index of task (integer)}\n"
+    public static final String DELETE_ERROR = "Error: Syntax of delete: "
+            + "delete {index of task (integer in [1, total number of tasks])}\n"
             + "E.g. delete 1";
-    public static final String FIND_ERROR = "Error: Syntax of find: find {keyword}\n"
+    public static final String FIND_ERROR = "Error: Syntax of find: find {keyword formed by at least one word}\n"
             + "E.g. find book";
-    public static final String TAG_ERROR = "Error: Syntax of tag: tag {index of task (integer)} {tag (string without space)}\n"
+    public static final String TAG_ERROR = "Error: Syntax of tag: "
+            + "tag {index of task (integer in [1, total number of tasks])} {tag (single word without space)}\n"
             + "E.g. tag 1 urgent";
-
 
 
     public static final String EMPTY_ERROR = "Error: Sorry, we are not sync enough to communicate through empty command.";
