98d7027654467c826ad0b8723b732390f8d2bb7a
Kenneth Seet <kenurfseet@gmail.com>
2024-02-23 17:05:47 +0800

    Update README.md

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Naruto");
             fxmlLoader.<MainWindow>getController().setNaruto(naruto);
             stage.show();
         } catch (IOException e) {
