51a2c046176584e5b2a966ada20209d049fdad54
Allysa Tan <allysa@u.nus.edu>
2024-02-23 23:14:46 +0800

    Update TaskList.java to fix crashing bug from incorrect user input

--- src/main/java/ally/task/Deadline.java
+++ src/main/java/ally/task/Deadline.java
@@ -23,7 +23,7 @@ public class Deadline extends Task {
             String[] splitDesc = desc.split(" /by ");
             this.description = splitDesc[0];
             this.by = LocalDate.parse(splitDesc[1]);
-        } catch (AllyException e) {
+        } catch (Exception e) {
             throw new AllyException();
         }
     }
