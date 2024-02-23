8bcb1389283cd19f98a2f6ca44233b3adab77be8
Jia En <tujiaen@gmail.com>
2024-02-23 23:24:54 +0800

    Prep for release

--- src/main/java/chatbot/controllers/DialogBox.java
+++ src/main/java/chatbot/controllers/DialogBox.java
@@ -15,8 +15,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
