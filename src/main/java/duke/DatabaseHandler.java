0277581fd8a676f20af6671b887560c971a2986c
HM33-Stu <120201372+HM33-Stu@users.noreply.github.com>
2024-02-23 15:41:06 +0800

    Amended JavaDocs and formatting

--- src/main/java/duke/DatabaseHandler.java
+++ src/main/java/duke/DatabaseHandler.java
@@ -14,17 +14,17 @@ import java.util.Objects;
  */
 public class DatabaseHandler {
 
-    private String databaseFilePath;
+    private String storageFilePath;
 
     /**
      * Initializes the database handler with the specified file path.
      *
-     * @param databaseFilePath the path to the database file
+     * @param storageFilePath the path to the database file
      */
-    public DatabaseHandler(String databaseFilePath) {
-        this.databaseFilePath = databaseFilePath;
+    public DatabaseHandler(String storageFilePath) {
+        this.storageFilePath = storageFilePath;
         try {
-            File file = new File(this.databaseFilePath);
+            File file = new File(this.storageFilePath);
             file.createNewFile();
         } catch (IOException e) {
             System.out.println("Oops! Failed to create the database file: " + e.getMessage());
@@ -37,9 +37,9 @@ public class DatabaseHandler {
      *
      * @return a list of strings containing all lines from the database file
      */
-    public List<String> readFromFile() {
+    public List<String> readOps() {
         try {
-            return Files.readAllLines(Paths.get(this.databaseFilePath));
+            return Files.readAllLines(Paths.get(this.storageFilePath));
         } catch (IOException e) {
             System.out.println("Oops! Failed to read from the file: " + e.getMessage());
             return null;
@@ -47,21 +47,23 @@ public class DatabaseHandler {
     }
 
     /**
-     * Writes the given list of strings to the database file.
+     * Writes provided list of strings to the database file.
      *
      * @param stringList the list of strings to be written to the file
      */
-    public void writeToFile(List<String> stringList) {
+    public void writeOps(List<String> stringList) {
         try {
-            File file = new File(this.databaseFilePath);
+            File file = new File(this.storageFilePath);
             file.createNewFile();
 
-            FileWriter fileWriter = new FileWriter(this.databaseFilePath);
+            FileWriter fileWriter = new FileWriter(this.storageFilePath);
 
             for (String line : stringList) {
                 fileWriter.write(line + "\n");
             }
+
             fileWriter.close();
+
         } catch (IOException e) {
             System.out.println("Oops! Failed to write to the file: " + e.getMessage());
             e.printStackTrace();
@@ -71,29 +73,28 @@ public class DatabaseHandler {
     /**
      * Loads data from the database file and returns it as a list of strings.
      *
-     * @return a list of strings representing data loaded from the database file
+     * @return a list of strings representing data loaded from storage
      */
-    public List<String> loadData() {
-        List<String> stringTasksList = this.readFromFile();
+    public List<String> loadOps() {
+        List<String> stringTasksList = this.readOps();
         return stringTasksList;
     }
 
     /**
-     * Clears the contents of the database file.
+     * Clears the contents of the storage file
      */
     public void clearFile() {
         try {
-            Files.write(Paths.get(databaseFilePath), new byte[0]);
+            Files.write(Paths.get(storageFilePath), new byte[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        String databaseFilePath = "data/duketest.txt";
-        DatabaseHandler databaseHandler = new DatabaseHandler(databaseFilePath);
+        String storageFilePath = "data/duketest.txt";
+        DatabaseHandler databaseHandler = new DatabaseHandler(storageFilePath);
 
-        // Clear and initialize the data/duke.txt
         databaseHandler.clearFile();
 
         List<String> input = new ArrayList<>();
@@ -101,9 +102,9 @@ public class DatabaseHandler {
         input.add("E | 0 | netflix marathon | 01 December 2022 00:00 | 15 December 2022 23:59");
         input.add("Test was a success!");
 
-        databaseHandler.writeToFile(input);
+        databaseHandler.writeOps(input);
 
-        List<String> output = databaseHandler.readFromFile();
+        List<String> output = databaseHandler.readOps();
 
         for (String line : output) {
             System.out.println(line);
