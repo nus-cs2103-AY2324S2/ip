1cce41304a326a040d48a4f3f5241250494e8322
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-02-23 23:13:41 +0800

    Add Ui.png
    Added UI.png as part of week 6 task requirements
    
    Add Ui.png to docs folder.

--- src/main/java/area/Main.java
+++ src/main/java/area/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Area");
             fxmlLoader.<MainWindow>getController().setArea(area);
             stage.show();
         } catch (IOException e) {
