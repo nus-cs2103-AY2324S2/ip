8b5240e56d6842a9edb62e95f54fc71e0aa0af52
unknown <xuelinglow09@gmail.com>
2024-02-24 03:04:55 +0800

    Updated some minor changes

--- src/main/java/tasklist/gui/MainWindow.java
+++ src/main/java/tasklist/gui/MainWindow.java
@@ -29,8 +29,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/ena.jpg"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/mizuki.jpg"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/cat.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/bear.jpg"));
 
     @FXML
     public void initialize() {
