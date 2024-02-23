20b09ad9383609a385ed787bb624be10494787c7
Allysa Tan <allysa@u.nus.edu>
2024-02-23 17:16:28 +0800

    Update JUnit Tests

---
+++ src/test/java/ally/utils/FinderTest.java
@@ -0,0 +1,35 @@
+package ally.utils;
+
+import ally.task.Todo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FinderTest {
+
+    TaskList lst;
+    Finder finder;
+
+    @BeforeEach
+    void setUp() {
+        lst = new TaskList();
+        lst.addToList(new Todo("eat"));
+        lst.addToList(new Todo("sleep"));
+        finder = new Finder(lst);
+    }
+
+    @Test
+    void testFindPositive() {
+        String result = this.finder.find("sleep");
+//        System.out.println(result);
+        assertEquals(result, "2: [T][ ] sleep\n" +
+                "All results shown.");
+    }
+
+    @Test
+    void testFindNegative() {
+        String result = this.finder.find("cs3230 homework");
+        assertEquals(result, "No results Found.");
+    }
+}
--- src/test/java/ally/utils/TaskListTest.java
+++ src/test/java/ally/utils/TaskListTest.java
@@ -1,7 +1,6 @@
-package ally;
+package ally.utils;
 
 import ally.task.Task;
-import ally.utils.TaskList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
