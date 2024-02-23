87523a7a8a0ec4a61593da2213b94020bfb8f181
fy17ohhh <hester03sfy@gmail.com>
2024-02-23 18:53:09 +0800

    Update Ui and delete logo

--- src/main/java/jade/ui/Ui.java
+++ src/main/java/jade/ui/Ui.java
@@ -5,15 +5,9 @@ package jade.ui;
  * A <code>Ui</code> saves messages for user feedback.
  */
 public class Ui {
-    public static final String LOGO = "  ____   ___    ____     ______\n"
-            + "  |  |  / _ \\  |  ___ \\ / |____/\n"
-            + "  |  | | | | | | |  | | | |____\n"
-            + "  |  | | |_| | | |  | | | |____|\n"
-            + "|\\|  | | ___ | | |__| | | |____\n"
-            + " \\___| |_| |_| |_____/  \\_|____\\\n"; // logo for the program
-    public static final String LAUNCH_MESSAGE = String.format("%s\nGreetings from Jade! I'm Jade,"
+    public static final String LAUNCH_MESSAGE = "Greetings from Jade! I'm Jade,"
             + " your esteemed task management companion.\n\n"
-            + "To check how I could assist you, access the help page by entering 'help'.", LOGO);
+            + "To check how I could assist you, access the help page by entering 'help'.";
     public static final String LOADING_ERROR_MESSAGE = "\t[There's no file under current "
              + "storage path, a new task file has been created.]\n";
 }
