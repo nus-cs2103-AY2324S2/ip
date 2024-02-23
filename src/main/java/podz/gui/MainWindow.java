14ba873eb79152d330aa00076b19d8d513c524c7
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-02-23 10:28:15 +0800

    Update profile pictures

--- src/main/java/podz/gui/MainWindow.java
+++ src/main/java/podz/gui/MainWindow.java
@@ -30,8 +30,8 @@ public class MainWindow extends AnchorPane {
 
     private Podz podz;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/podz.png"));
 
     @FXML
     public void initialize() {
