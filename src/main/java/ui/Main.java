1dccd0e2f58ad8846f339878d944f86c897c0953
Justin <justinlim60@gmail.com>
2024-02-23 17:23:44 +0800

    Add name to GUI

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Jux");
             fxmlLoader.<MainWindow>getController().setJux(jux);
             fxmlLoader.<MainWindow>getController().welcomeMessage(); // initialise the task list here instead of main window
             stage.show();
