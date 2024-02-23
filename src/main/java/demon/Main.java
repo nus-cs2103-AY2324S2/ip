ec8d4d3ac497231d12abe6318645e43bb829f19c
DESKTOP-KIADS1H\Chuan Yao <kaizerfang2@gmail.com>
2024-02-23 09:44:20 +0800

    Add GUI window title

--- src/main/java/demon/Main.java
+++ src/main/java/demon/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Demon Chatbot");
             fxmlLoader.<MainWindow>getController().setDemon(demon);
             stage.show();
         } catch (IOException e) {
