3d1bdbba6bdd34770889f8cf1bd4c54f06ee2f1b
Allysa Tan <allysa@u.nus.edu>
2024-02-23 17:21:28 +0800

    Update JUnit test

--- src/test/java/ally/utils/FinderTest.java
+++ src/test/java/ally/utils/FinderTest.java
@@ -1,7 +1,6 @@
 package ally.utils;
 
 import ally.task.Todo;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
