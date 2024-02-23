248c3d213f9e85a04655ced69fe96eb0ae7d8eca
Ko-Khan <e0970313@u.nus.edu>
2024-02-23 15:01:46 +0800

    Using remind with an non integer causes program to crash.
    Use a try catch block to catch situation where a non integer is used. Bit reminds user to enter a number instead.
    Issue with gui shortening messages if they are too long. Modify dialog box to fix this.

--- src/main/java/bit/Main.java
+++ src/main/java/bit/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bit");
             fxmlLoader.<MainWindow>getController().setBit(bit);
             stage.show();
         } catch (IOException e) {
