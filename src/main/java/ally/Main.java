3035bba10b56ee020ddc7670a1e9fa9927f605fa
Allysa Tan <allysa@u.nus.edu>
2024-02-23 18:54:15 +0800

    Add Title of Window in Main Window

--- src/main/java/ally/Main.java
+++ src/main/java/ally/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("ALLY");
             fxmlLoader.<MainWindow>getController().setAlly(ally);
             stage.show();
         } catch (IOException e) {
