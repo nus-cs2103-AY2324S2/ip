84c03866a0d65bd3d6bc8693c4816038e728deb9
zoebelle-pang <zoebellepang11@gmail.com>
2024-02-23 08:15:24 +0800

    Revert "Revert "A-JavaDoc all""
    This reverts commit db2313f3d156b29b71109c83ed3c1825bfe165d8.

--- src/main/java/Bentley/Deadline.java
+++ src/main/java/Bentley/Deadline.java
@@ -4,18 +4,36 @@ package Bentley;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task with a specific deadline.
+ * Extends the Task class.
+ */
 public class Deadline extends Task {
+    
+    /**
+     * The deadline date of the task.
+     */
     protected LocalDate by;
 
+    /**
+     * Constructs a Deadline object with the given description and deadline date.
+     *
+     * @param description The description of the task.
+     * @param by The deadline date in the format "yyyy-MM-dd".
+     */
     public Deadline(String description, String by) {
         super(description);
         
         this.by = LocalDate.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
+    /**
+     * Returns a formatted string representation of the Deadline object.
+     *
+     * @return A string containing task type, description, and deadline date.
+     */
     @Override
     public String toString() {
-        
         String formattedDate = by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
         return "D |" + super.toString() + " | " + formattedDate;
     }
--- src/main/java/Bentley/Event.java
+++ src/main/java/Bentley/Event.java
@@ -4,10 +4,29 @@ package Bentley;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task that is an event with a specific start and end date and time.
+ * Extends the Task class.
+ */
 public class Event extends Task {
+    
+    /**
+     * The start date and time of the event.
+     */
     protected LocalDateTime from;
+
+    /**
+     * The end date and time of the event.
+     */
     protected LocalDateTime to;
 
+    /**
+     * Constructs an Event object with the given description, start date, and end date.
+     *
+     * @param description The description of the event.
+     * @param from The start date and time of the event in the format "yyyy-MM-dd HHmm".
+     * @param to The end date and time of the event in the format "yyyy-MM-dd HHmm".
+     */
     public Event(String description, String from, String to) {
         super(description);
         // Parse input dates with both date and time components
@@ -15,6 +34,11 @@ public class Event extends Task {
         this.to = LocalDateTime.parse(to, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 
+    /**
+     * Returns a formatted string representation of the Event object.
+     *
+     * @return A string containing task type, description, start date, and end date.
+     */
     @Override
     public String toString() {
         // Format dates in MMM dd yyyy hh:mma format
