e305ff47d339d8b93e9079cf24aa4ca4b01bc3af
JustWeiHao <e0960531@u.nus.edu>
2024-02-25 03:45:36 +0800

    Update user guide

--- src/main/java/raphael/Main.java
+++ src/main/java/raphael/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             stage.setResizable(false);
+            stage.setTitle("Raphael");
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
