cc65400a0c2e825f95d99d68f3c9cedd885bff46
H4mes <e0960344@u.nus.edu>
2024-02-24 15:50:07 +0800

    Enforce adherence to style guide for tests
    Reorganised imports and edited code for ParserTest and EventTest classes
    to pass checkstyle automated testing.

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,15 +1,16 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class ParserTest {
     @Test
     public void parseDate_correctFormat_success() {
         String input1 = "09/02/2024 1800";
-        LocalDateTime expectedDate = LocalDateTime.of(2024, 2, 9, 18,00);
+        LocalDateTime expectedDate = LocalDateTime.of(2024, 2, 9, 18, 00);
         LocalDateTime testDate = Parser.parseDate(input1);
         assertEquals(testDate, expectedDate);
     }
--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,15 +1,16 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class EventTest {
     @Test
     public void getData_success() {
-        LocalDateTime testFrom = LocalDateTime.of(2024, 2, 9, 18,00);
-        LocalDateTime testTo = LocalDateTime.of(2024, 2, 10, 9,00);
+        LocalDateTime testFrom = LocalDateTime.of(2024, 2, 9, 18, 00);
+        LocalDateTime testTo = LocalDateTime.of(2024, 2, 10, 9, 00);
         Event event = new Event("Test Event", testFrom, testTo);
         String expectedData = "E | 0 | Test Event | 09/02/2024 1800 | 10/02/2024 0900";
         assertEquals(event.getData(), expectedData);
