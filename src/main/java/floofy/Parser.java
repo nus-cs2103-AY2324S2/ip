e2b46cf604919637b16e4ec0c0e1863bd36132d2
lynnetteeee <lynnette.oxh@gmail.com>
2024-02-24 21:42:55 +0800

    Apply SLAP to parse method in Parser.java, shorten too-long method

--- src/main/java/floofy/Parser.java
+++ src/main/java/floofy/Parser.java
@@ -20,84 +20,128 @@ public class Parser {
      * @throws FloofyException If the user input is invalid.
      */
     public String[] parse(String userInput) throws FloofyException {
-        String[] parsedInput = new String[4];
         if (userInput.startsWith("mark")) {
-            if (userInput.length() != 6) {
-                throw new FloofyException("To mark a task, type 'mark ' followed by the task number! e.g. 'mark 1':)");
-            }
-            parsedInput[0] = "mark";
-            parsedInput[1] = userInput.substring(5);
-            return parsedInput;
+            return parseMarkCommand(userInput);
         } else if (userInput.startsWith("unmark")) {
-            if (userInput.length() != 8) {
-                throw new FloofyException("To unmark a task, type 'unmark ' followed by the task number! e.g. 'unmark 1':)");
-            }
-            parsedInput[0] = "unmark";
-            parsedInput[1] = userInput.substring(7);
-            return parsedInput;
+            return parseUnmarkCommand(userInput);
         } else if (userInput.startsWith("find")) {
-            if (userInput.length() < 6) {
-                throw new FloofyException("To find a task, type 'find ' followed by the task to find! e.g. 'find book':)");
-            }
-            parsedInput[0] = "find";
-            parsedInput[1] = userInput.substring(5);
-            return parsedInput;
+            return parseFindCommand(userInput);
         } else if (userInput.startsWith("todo")) {
-            parsedInput[0] = "todo";
-            if (userInput.length() < 6) {
-                throw new FloofyException("Remember to add an actual task. Try again!");
-            }
-            parsedInput[1] = userInput.substring(5);
-            return parsedInput;
+            return parseTodoCommand(userInput);
         } else if (userInput.startsWith("deadline")) {
-            parsedInput[0] = "deadline";
-            if (userInput.length() < 10) {
-                throw new FloofyException("Remember to add an actual task. Try again!");
-            }
-            if (!(userInput.contains("/by"))) {
-                throw new FloofyException("Remember to state the deadline after a '/by'!");
-            }
-            // get description
-            parsedInput[1] = userInput.substring(9, userInput.indexOf("/by") - 1);
-            // get date, but lacking parsed date
-            parsedInput[2] = userInput.substring(userInput.indexOf("/by") + 4);
-            return parsedInput;
+            return parseDeadlineCommand(userInput);
         } else if (userInput.startsWith("event")) {
-            parsedInput[0] = "event";
-            if (userInput.length() < 7) {
-                throw new FloofyException("Remember to add an actual task. Try again!");
-            }
-            if (!(userInput.contains("/from"))) {
-                throw new FloofyException("Remember to state the start of your event after a '/from'!");
-            }
-            if (!(userInput.contains("/to"))) {
-                throw new FloofyException("Remember to state the end of your event after a '/to'!");
-            }
-            parsedInput[1] = userInput.substring(6, userInput.indexOf("/from") - 1);
-            parsedInput[2] = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") - 1);
-            parsedInput[3] = userInput.substring(userInput.indexOf("/to") + 4);
-            return parsedInput;
+            return parseEventCommand(userInput);
         } else if (userInput.startsWith("delete")) {
-            if (userInput.length() != 8) {
-                throw new FloofyException("To delete a task, type 'delete ' followed by the task number! e.g. 'delete 1':)");
-            }
-            parsedInput[0] = "delete";
-            parsedInput[1] = userInput.substring(7);
-            return parsedInput;
+            return parseDeleteCommand(userInput);
         } else if (userInput.startsWith("list")) {
-            if (userInput.length() > 4) {
-                throw new FloofyException("To list your tasks, just type 'list' :)");
-            }
-            parsedInput[0] = "list";
-            return parsedInput;
+            return parseListCommand(userInput);
         } else if (userInput.startsWith("bye")) {
-            if (userInput.length() > 3) {
-                throw new FloofyException("To exit, just type 'bye' :)");
-            }
-            parsedInput[0] = "bye";
-            return parsedInput;
+            return parseByeCommand(userInput);
         } else {
             throw new FloofyException("To add a task, please start with any of these commands: 'todo', 'deadline' or 'event'!");
         }
+    }
+
+    public String[] parseMarkCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[2];
+        if (userInput.length() != 6) {
+            throw new FloofyException("To mark a task, type 'mark ' followed by the task number! e.g. 'mark 1':)");
+        }
+        parsedInput[0] = "mark";
+        parsedInput[1] = userInput.substring(5);
+        return parsedInput;
+    }
+
+    public String[] parseUnmarkCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[2];
+        if (userInput.length() != 8) {
+            throw new FloofyException("To unmark a task, type 'unmark ' followed by the task number! e.g. 'unmark 1':)");
+        }
+        parsedInput[0] = "unmark";
+        parsedInput[1] = userInput.substring(7);
+        return parsedInput;
+    }
+
+    public String[] parseFindCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[2];
+        if (userInput.length() < 6) {
+            throw new FloofyException("To find a task, type 'find ' followed by the task to find! e.g. 'find book':)");
+        }
+        parsedInput[0] = "find";
+        parsedInput[1] = userInput.substring(5);
+        return parsedInput;
+    }
+
+    public String[] parseTodoCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[2];
+        if (userInput.length() < 6) {
+            throw new FloofyException("Remember to add an actual task. Try again!");
+        }
+        parsedInput[0] = "todo";
+        parsedInput[1] = userInput.substring(5);
+        return parsedInput;
+    }
+
+    public String[] parseDeadlineCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[3];
+        if (userInput.length() < 10) {
+            throw new FloofyException("Remember to add an actual task. Try again!");
+        }
+        if (!(userInput.contains("/by"))) {
+            throw new FloofyException("Remember to state the deadline after a '/by'!");
+        }
+        // get description
+        parsedInput[0] = "deadline";
+        parsedInput[1] = userInput.substring(9, userInput.indexOf("/by") - 1);
+        // get date, but lacking parsed date
+        parsedInput[2] = userInput.substring(userInput.indexOf("/by") + 4);
+        return parsedInput;
+    }
+
+    public String[] parseEventCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[4];
+        if (userInput.length() < 7) {
+            throw new FloofyException("Remember to add an actual task. Try again!");
+        }
+        if (!(userInput.contains("/from"))) {
+            throw new FloofyException("Remember to state the start of your event after a '/from'!");
+        }
+        if (!(userInput.contains("/to"))) {
+            throw new FloofyException("Remember to state the end of your event after a '/to'!");
+        }
+        parsedInput[0] = "event";
+        parsedInput[1] = userInput.substring(6, userInput.indexOf("/from") - 1);
+        parsedInput[2] = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") - 1);
+        parsedInput[3] = userInput.substring(userInput.indexOf("/to") + 4);
+        return parsedInput;
+    }
+
+    public String[] parseDeleteCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[2];
+        if (userInput.length() != 8) {
+            throw new FloofyException("To delete a task, type 'delete ' followed by the task number! e.g. 'delete 1':)");
+        }
+        parsedInput[0] = "delete";
+        parsedInput[1] = userInput.substring(7);
+        return parsedInput;
+    }
+
+    public String[] parseListCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[1];
+        if (userInput.length() > 4) {
+            throw new FloofyException("To list your tasks, just type 'list' :)");
+        }
+        parsedInput[0] = "list";
+        return parsedInput;
+    }
+
+    public String[] parseByeCommand(String userInput) throws FloofyException {
+        String[] parsedInput = new String[1];
+        if (userInput.length() > 3) {
+            throw new FloofyException("To exit, just type 'bye' :)");
+        }
+        parsedInput[0] = "bye";
+        return parsedInput;
     }
 }
