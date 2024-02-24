657a62802d425cc7a4b88b1735d801b4848115ea
Ondre Tann <ondre.tannzxc@gmail.com>
2024-02-25 00:00:58 +0800

    Fix bugs regarding the exiting of application via "bye" command

--- src/main/java/thames/MainWindow.java
+++ src/main/java/thames/MainWindow.java
@@ -1,6 +1,8 @@
 package thames;
 
 import javafx.fxml.FXML;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
@@ -44,6 +46,11 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
         );
+        if (input.trim().equalsIgnoreCase("bye")) {
+            PauseTransition pause = new PauseTransition(Duration.seconds(1)); // Adjust the delay as needed
+            pause.setOnFinished(event -> System.exit(0));
+            pause.play();
+        }
         userInput.clear();
     }
 }
