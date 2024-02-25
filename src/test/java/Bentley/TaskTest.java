6f8b8894721e7c68bcd3a01d6a6df7ed23ac97bb
zoebelle-pang <zoebellepang11@gmail.com>
2024-02-26 02:34:37 +0800

    fix conflict error

--- src/test/java/Bentley/TaskTest.java
+++ src/test/java/Bentley/TaskTest.java
@@ -2,7 +2,6 @@ package bentley;
 
 // import java.util.ArrayList;
 
-<<<<<<< HEAD
 import org.junit.jupiter.api.Test;
 
 import bentley.Deadline;
@@ -10,23 +9,12 @@ import bentley.Event;
 import bentley.Task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-=======
-// import bentley.Task;
-// import bentley.Deadline;
-// import bentley.Event;
-// import org.junit.jupiter.api.Test;
-
-// import static org.junit.jupiter.api.Assertions.assertEquals;
->>>>>>> branch-Level-10
 
 public class TaskTest {
 
-    // Test that a new task created should be unmarked
-    // @Test
-    // public void addUnmarkedDeadline(){
-    //     Task task = new Deadline("return book", "2025-09-09");
-    //     assertEquals("0", task.doneOrNot());
-    // }
+    @Test
+    public void addUnmarkedDeadline() {
+        Task task = new Deadline("return book", "2025-09-09");
+        assertEquals("0", task.doneOrNot());
+    }
 }
-
-    
