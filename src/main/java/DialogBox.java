b3f3edefdd114f86c9aab416a062aeef2aebad66
yaphowen <e0985925@u.nus.edu>
2024-02-25 15:58:30 +0800

    Change help text

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
--- src/main/java/commands/Help.java
+++ src/main/java/commands/Help.java
@@ -6,22 +6,21 @@ package commands;
  */
 public class Help implements Command {
 
-    /**
-     * Executes the Help command by displaying help information retrieved from the specified file path.
-     */
-    private String help =
-            "list: Lists all tasks\n" +
-                    "find: Finds task with matching name\n" +
-                    "todo: Creates todo\n" +
-                    "event: Creates event\n" +
-                    "deadline: Creates deadline\n" +
-                    "mark: Marks task as done\n" +
-                    "unmark: Marks task as not done\n" +
-                    "delete: Deletes task, use `0` to clear all\n" +
-                    "bye: Ends program";
-
     @Override
     public String execute() {
+        /**
+         * Executes the Help command by displaying help information retrieved from the specified file path.
+         */
+        String help = "list: Lists all tasks\n" +
+                "find: Finds task with matching name\n" +
+                "todo: Creates todo\n" +
+                "event: Creates event\n" +
+                "deadline: Creates deadline\n" +
+                "mark: Marks task as done\n" +
+                "unmark: Marks task as not done\n" +
+                "delete: Deletes task, use `0` to clear\n" +
+                "bye: Ends program";
+
         return help;
     }
 }
