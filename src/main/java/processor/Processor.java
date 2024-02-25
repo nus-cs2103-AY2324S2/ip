bc152cfdba2cf13e5974360ef36950a821cf3d48
minghowC <e0725080@u.nus.edu>
2024-02-25 23:26:59 +0800

    Modify code and package JAR

---
+++ src/main/java/processor/Processor.java
@@ -0,0 +1,34 @@
+package processor;
+
+import java.io.IOException;
+
+import storage.Storage;
+import tasks.TaskList;
+import ui.Ui;
+
+/**
+ * The Processor abstract class performs commands and defines a command function for it's children.
+ * It interacts with the TaskList, Ui, and Storage classes to handle user commands and manage tasks.
+ */
+public abstract class Processor {
+    protected TaskList taskList;
+    protected Ui chatbotUi;
+    protected Storage storage;
+
+    /**
+     * Constructor for AbstractProcessor element
+     * @param taskList
+     * @param chatbotUi
+     */
+    public Processor(TaskList taskList, Ui chatbotUi) {
+        this.taskList = taskList;
+        this.chatbotUi = chatbotUi;
+        this.storage = new Storage("src/data/tasks.txt", taskList);
+    }
+
+    /**
+     * Process the user input command
+     * @param userInput the user input command
+     */
+    public abstract void processCommand(String userInput) throws IOException;
+}
--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,42 +1,147 @@
-package task;
+package tasks;
+
 /**
- * The Task class is an abstraction for representing tasks in real life,
- * where a task could either be done or not done.
- *
- * @author Chai Ming How
- * @since 2024-02-01
+ * The Task class represents a task in the TaskList.
  */
+public class Task {
 
-public abstract class Task {
-    protected String description;
-    protected boolean isDone;
+    private String desc;
+    private boolean isDone;
+    private TaskType type;
+    private String start;
+    private String end;
 
-    public Task(String description) {
-        this.description = description;
+    /**
+     * The TaskType enum represents the type of task.
+     */
+    public enum TaskType {
+        TODO('T'), DEADLINE('D'), EVENT('E');
+
+        private char code;
+
+        TaskType(char code) {
+            this.code = code;
+        }
+
+        public char getCode() {
+            return code;
+        }
+    }
+
+    /**
+     * Constructor for the todo Task.
+     * @param desc
+     * @param isDone
+     */
+    public Task(String desc, boolean isDone) {
+        this.desc = desc;
+        this.isDone = isDone;
+        this.type = TaskType.TODO;
+    }
+
+    /**
+     * Constructor for the deadline task.
+     * @param desc
+     * @param isDone
+     * @param start
+     */
+    public Task(String desc, boolean isDone, String start) {
+        this.desc = desc;
+        this.isDone = isDone;
+        this.type = TaskType.DEADLINE;
+        this.start = start;
+    }
+
+    /**
+     * Constructor for the event Task
+     * @param desc
+     * @param isDone
+     * @param start
+     * @param end
+     */
+    public Task(String desc, boolean isDone, String start, String end) {
+        this.desc = desc;
+        this.isDone = isDone;
+        this.type = TaskType.EVENT;
+        this.start = start;
+        this.end = end;
+    }
+
+
+    /**
+     * Returns the task description.
+     * @return the task description
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * Returns true if the task is done, false otherwise.
+     * @return true if the task is done, false otherwise
+     */
+    public boolean isDone() {
+        return isDone;
+    }
+
+    /**
+     * Returns the type of the task.
+     * @return the type of the task
+     */
+    public TaskType getType() {
+        return type;
+    }
+
+    /**
+     * Returns the start date/time of the task.
+     * @return the start date/time of the task
+     */
+    public String getStart() {
+        return start;
+    }
+
+    /**
+     * Returns the end date/time of the task.
+     * @return the end date/time of the task
+     */
+    public String getEnd() {
+        return end;
+    }
+
+    /*
+     * used to mark a task as done.
+     */
+    public void mark() {
+        this.isDone = true;
+    }
+
+    /*
+     * used to unmark a task as not done.
+     */
+    public void unmark() {
         this.isDone = false;
     }
 
-    public abstract String getTaskIcon();
-    public void markAsDone() {
-        isDone = true;
-    }
-
-    public void markAsNotDone() {
-        isDone = false;
-    }
-
-    public String getDescription(){
-        return this.description;
-    }
-    public String getStatusIcon() {
-        return (isDone ? "X" : " ");
-    }
-
-    public String toFileFormat() {
-        return (this.isDone ? "1" : "0") + " | " + this.description;
-    }
-
+    /**
+     * Returns a String representation of the task.
+     * @return a String representation of the task
+     */
+    @Override
     public String toString() {
-        return "[" + getTaskIcon() + "]" + "[" + getStatusIcon() + "] " + description;
+        StringBuilder taskString = new StringBuilder();
+        taskString.append("[").append(type.getCode()).append("]");
+        taskString.append(isDone ? "[X]" : "[ ]");
+
+        // Append task description
+        taskString.append(" ").append(desc);
+
+        // Append date/time information if available
+        if (type == TaskType.DEADLINE) {
+            taskString.append(" (").append(start).append(")");
+        } else if (type == TaskType.EVENT) {
+            taskString.append(" (").append(start).append(" to ").append(end).append(")");
+        }
+
+        return taskString.toString();
     }
 }
