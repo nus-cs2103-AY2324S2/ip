20b6d0fbd84a7a9224f7a52e825bf7943e12464c
jocelyn-soh <s.sohjoce@gmail.com>
2024-02-25 20:03:51 +0800

    Update error message in JojoUnknownTaskException

--- src/main/java/exceptions/JojoUnknownTaskException.java
+++ src/main/java/exceptions/JojoUnknownTaskException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class JojoUnknownTaskException extends JojoException {
     public JojoUnknownTaskException() {
-        super("Sorry, but this task is unknown to me. Please try again using any of the following keywords: 'todo', 'deadline' or 'event'.");
+        super("Sorry, but this task is unknown to me. Please try again using any of the following keywords: 'todo', " +
+                "'deadline', 'event', 'list or 'find'.");
     }
 }
