a4b3c25b6fa5c316d9cde4b92cce012e28968218
lynnetteeee <lynnette.oxh@gmail.com>
2024-02-26 05:06:57 +0800

    Update ToDoTest: add javadocs

--- src/test/java/floofy/task/ToDoTest.java
+++ src/test/java/floofy/task/ToDoTest.java
@@ -3,13 +3,24 @@ package floofy.task;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests the ToDo class.
+ */
 public class ToDoTest {
+
+    /**
+     * Tests the string format of the todo task.
+     */
     @Test
     public void ToDoStringFormat_validInput_success() {
         ToDo todo = new ToDo("read book");
         assertEquals("[T][ ] read book", todo.toString());
     }
 
+    /**
+     * Tests the file format of the todo task.
+     */
     @Test
     public void ToDoFileFormat_validInput_success() {
         ToDo todo = new ToDo("read book");
