bedbdf07f4efa06f71e88a1e3121ea5772b9cc9a
Giselle Tan <giselletan1008@gmail.com>
2024-02-25 12:27:46 +0800

    Handle more errors

--- src/main/java/emis/Parser.java
+++ src/main/java/emis/Parser.java
@@ -12,6 +12,8 @@ import command.PrintCommand;
 import command.ToDoCommand;
 import command.UnmarkCommand;
 import exceptions.EmisException;
+import exceptions.DeadlineException;
+import exceptions.EventException;
 
 /**
  * The Parser class deals with making sense of user commands in the EMIS application.
@@ -37,29 +39,19 @@ public class Parser {
             } else if (fullCommand.equals("help")) {
                 return new HelpCommand();
             }else {
-                throw new EmisException("Meow? Emis doesn't understand.");
+                throw new EmisException("Meow? Emis doesn't understand.\n Please send 'help' if needed.");
             }
         } else {
             String action = fullCommand.substring(0, spaceIndex);
+            String prospectiveCommand = fullCommand.substring(spaceIndex + 1);
             if (action.equals("todo")) {
-                String description = fullCommand.substring(spaceIndex + 1);
-                return new ToDoCommand(description);
+                return new ToDoCommand(prospectiveCommand);
 
             } else if (action.equals("deadline")) {
-                String deadline = fullCommand.substring(spaceIndex + 1);
-                int slashIndex = deadline.indexOf("/by");
-                String description = deadline.substring(0, slashIndex);
-                String by = deadline.substring(slashIndex + 3);
-                return new DeadlineCommand(description, by);
+                return parseDeadline(prospectiveCommand);
 
             } else if (action.equals("event")) {
-                String event = fullCommand.substring(spaceIndex + 1);
-                int slashIndexFrom = event.indexOf("/from");
-                int slashIndexTo = event.indexOf("/to");
-                String description = event.substring(0, slashIndexFrom);
-                String from = event.substring(slashIndexFrom + 5, slashIndexTo);
-                String to = event.substring(slashIndexTo + 3);
-                return new EventCommand(description, from, to);
+                return parseEvent(prospectiveCommand);
 
             } else if (action.equals("mark")) {
                 Integer taskNo = Integer.valueOf(fullCommand.substring(spaceIndex + 1).trim());
@@ -80,6 +72,44 @@ public class Parser {
             } else {
                 throw new EmisException("Invalid command!");
             }
+        }
+    }
+
+    /**
+     * Parses a deadline command and returns the corresponding Command object.
+     *
+     * @param input The input string containing the deadline command.
+     * @return The DeadlineCommand object corresponding to the parsed command.
+     * @throws DeadlineException If the deadline command cannot be parsed.
+     */
+    public static Command parseDeadline(String input) throws DeadlineException {
+        int slashIndex = input.indexOf("/by");
+        if (slashIndex == -1) {
+            throw new DeadlineException("Please enter a deadline using '/by' after the task.");
+        } else {
+            String description = input.substring(0, slashIndex);
+            String by = input.substring(slashIndex + 3);
+            return new DeadlineCommand(description, by);
+        }
+    }
+
+    /**
+     * Parses an event command and returns the corresponding Command object.
+     *
+     * @param input The input string containing the event command.
+     * @return The EventCommand object corresponding to the parsed command.
+     * @throws EventException If the event command cannot be parsed.
+     */
+    public static Command parseEvent(String input) throws EventException {
+        int slashIndexFrom = input.indexOf("/from");
+        int slashIndexTo = input.indexOf("/to");
+        if (slashIndexFrom == -1 || slashIndexTo == -1) {
+            throw new EventException("Please enter event in this format: event /from (from date) /to (to date or time) regardless of if you have a from or to.");
+        } else {
+            String description = input.substring(0, slashIndexFrom);
+            String from = input.substring(slashIndexFrom + 5, slashIndexTo);
+            String to = input.substring(slashIndexTo + 3);
+            return new EventCommand(description, from, to);
         }
     }
 }
