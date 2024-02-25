0fa7409de3ae9a4201c6303e063ca1eb09eb377c
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-02-25 15:39:23 +0800

    Main.java: Add Aegis name to GUI title bar

--- src/main/java/aegis/Main.java
+++ src/main/java/aegis/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setAegis(aegis);
+            stage.setTitle("Aegis");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
