6c08945f6e1f1c54427bc1a0b63e09a3906ecb7a
logical-1985516 <ngyanjie0418@gmail.com>
2024-02-25 11:15:44 +0800

    Add JavaDoc to remove checkStyle violation

--- src/main/java/luke/MainWindow.java
+++ src/main/java/luke/MainWindow.java
@@ -29,6 +29,9 @@ public class MainWindow extends AnchorPane {
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
+    /**
+     * Adds a welcome message upon load.
+     */
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
