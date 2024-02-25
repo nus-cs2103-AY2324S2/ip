713a5e74e42800536f7aa46811e548c0c29bd6f8
minghowC <e0725080@u.nus.edu>
2024-02-25 23:32:43 +0800

    Add JavaDoc comments

--- src/main/java/main/IreneAI.java
+++ src/main/java/main/IreneAI.java
@@ -1,190 +1,59 @@
 package main;
-/**
- * The IreneAI program implements a simple chat application that
- * provides simple feedbacks to the user's inputs
- *
- * @author Chai Ming How
- * @since 2024-02-01
- */
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.io.IOException;
 
+import parser.Parser;
+import processor.Factory;
+import task.TaskList;
+import ui.Ui;
+/**
+ * The IreneAI class is a clever chatbot.
+ */
 public class IreneAI {
-	private static final String FILE_PATH = "./data/duke.txt";
-	private static Storage storage;
-    private static List<Task> tasks;
-    private static final String LINE = "____________________________________________________________";
-    public static void main(String[] args) throws DukeException {
-		storage = new Storage(FILE_PATH);
-		try {
-			tasks = Storage.loadTasks();
-		} catch (IOException e) {
-			System.out.println("Error loading tasks from file.");
-			tasks = new ArrayList<>();
-		}
-        Scanner scanner = new Scanner(System.in);
-
-        String chatbotName = "IreneAI";
-        dividingLine(LINE);
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you?");
-        dividingLine(LINE);
-
-        while (true) {
-            System.out.println("You: ");
-            String userInput = scanner.nextLine();
-            String[] splitInputs = userInput.split(" ");
-            Command command = Command.getCommand(splitInputs[0]);
-            String argument = splitInputs.length > 1 ? splitInputs[1] : "";
-            dividingLine(LINE);
-
-            try {
-                switch (command) {
-                    case BYE:
-                        Storage.saveTasks(tasks);
-                        System.out.println(" Bye. Hope to see you again soon!");
-                        dividingLine(LINE);
-                        return;
-                    case TODO:
-                        if (argument.isEmpty()) {
-                            throw new DukeException("The description of a todo cannot be empty.");
-                        }
-                        handleTodo(argument);
-                        break;
-                    case DEADLINE:
-                        // Checks command contains "/by"
-                        boolean hasBy = userInput.split(" /by ").length > 1;
-                        // Makes sure arguments are at least 3 (e.g. command, subject, deadline)
-                        boolean isValid = hasBy && splitInputs.length > 2;
-                        if (!isValid) {
-                            throw new DukeException("The deadline command requires both description and time inputs, separated by ' /by '");
-                        }
-                        handleDeadline(userInput);
-                        break;
-                    case EVENT:
-                        // Checks command contains '/from'
-                        boolean hasFrom = userInput.split(" /from ").length > 1;
-                        // Checks command contains '/to'
-                        boolean hasTo = userInput.split(" /to ").length > 1;
-                        isValid = hasTo && hasFrom && splitInputs.length > 3;
-                        if (!isValid) {
-                            throw new DukeException("The event command requires a description, from when, and to when"
-                                    + ".\n" + "For example, event project meeting /from Mon 2pm /to 4pm");
-                        }
-                        handleEvent(userInput);
-                        break;
-                    case LIST:
-                        listTasks();
-                        break;
-                    case UNMARK:
-                        if (argument.isEmpty()) {
-                            throw new DukeException("You must specific which task to unmark (in number).");
-                        }
-                        handleUnmark(userInput);
-                        break;
-                    case MARK:
-                        // future works: index out of bound cases
-                        if (argument.isEmpty()) {
-                            throw new DukeException("You must specific which task to mark (in number).");
-                        }
-                        handleMark(userInput);
-                        break;
-                    case DELETE:
-                        if (argument.isEmpty()) {
-                            throw new DukeException("You must specific which task to mark (in number).");
-                        }
-                        handleDelete(userInput);
-                        break;
-                    case INVALID:
-                        //Default error handling
-                    default:
-                        throw new DukeException("I don't get what you mean.. (╥﹏╥)");
-                }
-            } catch (DukeException e) {
-                dividingLine(LINE);
-                System.out.println("OOPS!! " + e.getMessage());
-                dividingLine(LINE);
+    private final Ui chatbotUi;
+    private final TaskList taskList;
+    private final Factory factory;
+    private final Parser parser;
+    /**
+     * Constructs a Duke object with the specified Ui, TaskList, Processor, and Parser.
+     */
+    public IreneAI() {
+        chatbotUi = new Ui();
+        taskList = new TaskList();
+        factory = new Factory(taskList, chatbotUi);
+        parser = new Parser(factory);
+    }
+    /**
+     * Executes the chatbot.
+     * @throws IOException if an I/O error occurs while running the chatbot
+     */
+    public void run() throws IOException {
+        // Greet User
+        System.out.print(chatbotUi.greetingBox());
+        String userInput;
+        do {
+            userInput = chatbotUi.getCommand();
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                parser.parse(userInput);
             }
+        } while (true);
+        System.out.println(chatbotUi.dividerWrapper(Ui.bye()));
+    }
+
+    public String run(String userInput) throws IOException {
+        if (userInput.equals("bye")) {
+            //return chatbotUi.dividerWrapper(Ui.bye());
+            System.exit(1);
+        } else {
+            // Assuming that the parse method updates the chatbotUi with the response
+            return chatbotUi.getResponse(userInput, parser);
         }
+        return null;
     }
-
-    private static void handleTodo(String userInput) {
-        tasks.add(new ToDo(userInput));
-        dividingLine(LINE);
-        System.out.println("Okie. I've added this task: ");
-        System.out.println("  " + tasks.get(tasks.size() - 1));
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-    }
-
-    private static void handleDeadline(String userInput) {
-        String[] parts = userInput.split(" /by ");
-        String description = parts[0].substring(9);
-        String deadline = parts[1];
-        tasks.add(new Deadline(description, deadline));
-        dividingLine(LINE);
-        System.out.println("Okie. I've added this task: ");
-        System.out.println("  " + tasks.get(tasks.size() - 1));
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-    }
-
-    private static void handleEvent(String userInput) {
-        String[] parts = userInput.split(" /from ");
-        String description = parts[0].substring(6);
-        String[] from_to = parts[1].split(" /to ");
-        String from = from_to[0];
-        String to = from_to[1];
-        tasks.add(new Event(description, from, to));
-        dividingLine(LINE);
-        System.out.println("Okie. I've added this task: ");
-        System.out.println("  " + tasks.get(tasks.size() - 1));
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-    }
-
-    private static void listTasks() {
-        dividingLine(LINE);
-        System.out.println("Documented tasks: ");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ": " + tasks.get(i));
-        }
-        dividingLine(LINE);
-    }
-
-    private static void handleUnmark(String userInput) {
-        int index = Integer.parseInt(userInput.substring(7)) - 1;
-        Task task = tasks.get(index);
-        task.markAsNotDone();
-        dividingLine(LINE);
-        System.out.println("Meow ~ I've marked " + task.getDescription() + " as not done : (");
-        System.out.println("  " + task);
-        dividingLine(LINE);
-    }
-
-    private static void handleMark(String userInput) {
-        // Index in an ArrayList starts from 0, therefore deducts 1 from actual counting
-        int index = Integer.parseInt(userInput.substring(5)) - 1;
-        Task task = tasks.get(index);
-        task.markAsDone();
-        dividingLine(LINE);
-        System.out.println("Brilliant! I've marked " + task.getDescription() + " as done : )");
-        System.out.println("  " + task);
-        dividingLine(LINE);
-    }
-
-    private static void handleDelete(String userInput) {
-        try {
-            int index = Integer.parseInt(userInput.substring((7))) - 1;
-            Task removedTask = tasks.remove(index);
-            System.out.println("Success! I have removed this task: " + removedTask + ".");
-            System.out.println("Now you still have " + tasks.size() + " tasks remaining.");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("OOPS!! I cannot delete the task! It does not exist.");
-        }
-    }
-
-    private static void dividingLine(String line){
-        System.out.println(line);
+    public static void main(String[] args) throws IOException {
+        IreneAI chatBot = new IreneAI();
+        chatBot.run();
     }
 }
