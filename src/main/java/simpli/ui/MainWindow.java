c7a9e5aed8c928abd33e5575c77a481649ef6a7d
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-02-25 13:52:35 +0800

    Update dialog box to fit show overflow contents

--- src/main/java/simpli/ui/MainWindow.java
+++ src/main/java/simpli/ui/MainWindow.java
@@ -43,6 +43,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+
         this.ui = new Ui();
     }
 
