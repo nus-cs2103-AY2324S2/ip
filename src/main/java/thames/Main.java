6fff4fc7ac6a2349d23647ff0c37193a5b23ae08
Ondre Tann <ondre.tannzxc@gmail.com>
2024-02-25 20:40:09 +0800

    Fix bugs for installation on different folder

--- src/main/java/thames/Main.java
+++ src/main/java/thames/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Thames thames = new Thames("./data/tasks.txt");
+    private Thames thames = new Thames("../data/tasks.txt");
 
     @Override
     public void start(Stage stage) {
