216f91a19741f734ea6f04db3349696cd3a3ccbb
JustWeiHao <e0960531@u.nus.edu>
2024-02-25 20:14:43 +0800

    Fix checkstyle

--- src/main/java/raphael/gui/MainWindow.java
+++ src/main/java/raphael/gui/MainWindow.java
@@ -1,13 +1,11 @@
 package raphael.gui;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import raphael.Raphael;
 
