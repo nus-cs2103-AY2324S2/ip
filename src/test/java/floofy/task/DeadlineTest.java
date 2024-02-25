9e8f41a9eafa45932fadd4aca6c5be2f7dcac991
lynnetteeee <lynnette.oxh@gmail.com>
2024-02-26 05:06:31 +0800

    Update DeadlineTest: add javadocs

--- src/test/java/floofy/task/DeadlineTest.java
+++ src/test/java/floofy/task/DeadlineTest.java
@@ -6,13 +6,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests the Deadline class.
+ */
 public class DeadlineTest {
+
+    /**
+     * Tests the string format of the deadline task.
+     */
     @Test
     public void deadlineStringFormat_validDescAndDateInput_success() {
         Deadline deadline = new Deadline("return book", LocalDate.parse("2021-08-27"));
         assertEquals("[D][ ] return book (by: 2021-08-27)", deadline.toString());
     }
 
+    /**
+     * Tests the file format of the deadline task.
+     */
     @Test
     public void deadlineFileFormat_validDescAndDateInput_success() {
         Deadline deadline = new Deadline("return book", LocalDate.parse("2021-08-27"));
