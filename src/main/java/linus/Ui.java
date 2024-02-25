8ec92379ec96ccd750e9af640d4aaaf4b7bbeb62
jeong-jaeho <e0960435@u.nus.edu>
2024-02-25 12:07:39 +0800

    Improve code quality
    The code has magic literals. The code also has deep nesting.
    The code also has duplicate conditional fragments.
    
    Let's,
    * apply SLAP to resolve deep nesting
    * use refactoring to resolve magic literals and duplicates

--- src/main/java/linus/Ui.java
+++ src/main/java/linus/Ui.java
@@ -7,39 +7,20 @@ import java.util.ArrayList;
  * Represents all the interactions with the user.
  */
 public class Ui {
-    /**
-     * Returns the next line of user input read from the Scanner.
-     *
-     * @param scanner Scanner object to read input.
-     * @return Next line's String representation.
-     */
-    public String getUserInput(Scanner scanner) {
-        return scanner.nextLine();
-    }
-
-    /**
-     * Prints out the input String message.
-     *
-     * @param message Input String message to be printed out.
-     */
-    public void showMessage(String message) {
-        System.out.println(message);
-    }
-
     public String findTasks(TaskList taskList, String keyword) {
         ArrayList<Task> matchingTasks = taskList.findMatchingTasks(keyword);
-        String returnedStringOfTasks = "";
+        String returnedStringOfTasks;
 
         if (matchingTasks.isEmpty()) {
-            return "No matching tasks found.";
+            returnedStringOfTasks = "No matching tasks found.";
         } else {
             returnedStringOfTasks = "Here are the matching tasks in your list:\n";
 
             for (int i = 0; i < matchingTasks.size(); i++) {
                 returnedStringOfTasks = returnedStringOfTasks + i + ". " + matchingTasks.get(i) + "\n";
             }
-
-            return returnedStringOfTasks;
         }
+
+        return returnedStringOfTasks;
     }
 }
