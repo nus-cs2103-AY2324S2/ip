650740b5c0250214074aa5d96925fb98af6d272a
PyromancerBoom <priyanshmagic@gmail.com>
2024-02-26 20:37:33 +0800

    Add Launcher JavaDocs

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 
 /**
  * A launcher class to workaround classpath issues.
+ * This class is used to launch the JavaFX application.
  */
 public class Launcher {
+    /**
+     * The main entry point for application.
+     * This method is used to launch the JavaFX application.
+     */
     public static void main(String[] args) {
         // Application.launch(Duke.class, args);
         Application.launch(Main.class, args);
     }
-
 }
