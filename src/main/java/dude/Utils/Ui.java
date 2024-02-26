7bb835029f4b0f30fcbe1e4e01973d8cecaf8261
tahsinhasem <tahsinhasem@gmail.com>
2024-02-26 21:56:14 +0800

    added JavaDoc and coding style changes

--- src/main/java/dude/Utils/Ui.java
+++ src/main/java/dude/Utils/Ui.java
@@ -1,24 +1,34 @@
 package dude.Utils;
 
+/**
+ * The Ui class handles the user interface of the application.
+ */
 public class Ui {
 
 
+    /**
+     * Displays the welcome message to system print stream.
+     */
     public void showWelcome() {
-        String msg = "\t-----------------------------------\n" +
-                "\t\tHello! I'm dude.Dude\n" +
-                "\t\tWhat can I do for you?\n" +
-                "\t-----------------------------------\n";
+        String msg = "\t-----------------------------------\n"
+                + "\t\tHello! I'm dude.Dude\n"
+                + "\t\tWhat can I do for you?\n"
+                + "\t-----------------------------------\n";
         System.out.println(msg);
     }
 
+    /**
+     * Display a message to system print stream.
+     *
+     * @param msg The message to be displayed.
+     */
     public void showMessage(String msg) {
 
         String temp = msg;
 
         temp = temp.replaceAll("\n", "\n\t\t");
-        temp = "\t-----------------------------------\n" +
-                "\t\t" + temp + "\n" +
-                "\t-----------------------------------";
+        temp = "\t\t" + "\t-----------------------------------\n" + temp
+                + "\n\t-----------------------------------";
         System.out.println(temp);
     }
 }
