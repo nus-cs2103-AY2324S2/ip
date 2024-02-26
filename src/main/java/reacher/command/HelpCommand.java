3c63c253a5f024e00d7e22494eca3b94b72b1994
zhenghuil <e0959119@u.nus.edu>
2024-02-26 22:41:47 +0800

    Bug fix: remove ui class form helpCommand

--- src/main/java/reacher/command/HelpCommand.java
+++ src/main/java/reacher/command/HelpCommand.java
@@ -2,11 +2,10 @@ package reacher.command;
 
 import reacher.Storage;
 import reacher.TaskList;
-import reacher.Ui;
 
 public class HelpCommand extends Command{
     @Override
-    public String execute(String input, TaskList tasks, Ui ui, Storage storage) {
+    public String execute(String input, TaskList tasks, Storage storage) {
         return "Command structure:\n" +
                 "Add, {name of task}, {task type}, {start}, {end}\n" +
                 "edit, {task no}, {done, undone or delete}\n" +
