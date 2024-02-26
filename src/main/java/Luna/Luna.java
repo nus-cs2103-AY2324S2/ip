db87f8079724ac9f4f00bacd64bc8d9b3bdc844b
whitesnowx <whitesnowx88@gmail.com>
2024-02-26 20:15:21 +0800

    Add Assertions
    Lack of Assertions is not safe.
    
    Increasing number of Assertions is safer.
    
    Lets
    * add more assertions
    
    Added to Storage.java, TaskList.java, Luna.java

--- src/main/java/Luna/Luna.java
+++ src/main/java/Luna/Luna.java
@@ -17,8 +17,13 @@ public class Luna {
     }
 
     public void run() {
+
+        assert ui != null : "ui should not be equals to null";
         String userInput = ui.readInput();
         Command c = Parser.parse(userInput);
+
+        assert storage != null : "storage should not be equals to null";
+        assert tasks != null : "tasks should not be equals to null";
         c.execute(tasks,ui,storage);
         run();
     }
@@ -30,8 +35,13 @@ public class Luna {
     }
 
     public void readIn(String input) {
+
         String userInput = ui.readInput(input);
+        assert userInput != null : "user input should not be null";
+
         Command c = Parser.parse(userInput);
+        assert c != null : "command should not be null";
+
         c.execute(tasks,ui,storage);
     }
 
