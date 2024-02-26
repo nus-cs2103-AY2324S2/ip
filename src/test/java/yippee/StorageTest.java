d666a7cfd64b0c135a3749775d0632fddfeb716a
Yiyang <liuyiyang0929@gmail.com>
2024-02-26 19:12:03 +0800

    Add test case for find command

--- src/test/java/yippee/StorageTest.java
+++ src/test/java/yippee/StorageTest.java
@@ -16,35 +16,6 @@ import yippee.tasks.Event;
 import yippee.tasks.ToDo;
 
 public class StorageTest {
-    /*@Test
-    public void storeDataTest() throws YippeeException, IOException {
-        File file = new File("storageTest.txt");
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new YippeeFileException("Error creating the file : " + e.getMessage());
-            }
-        }
-
-        Scanner fileSc = new Scanner(file);
-        Storage testStorage = new Storage("storageTest.txt");
-        TaskList testList = new TaskList();
-        testList.addStoredTask(new ToDo("Task A"));
-        testList.addStoredTask(new Deadline("Task B", "2022-09-28"));
-        testList.addStoredTask(new Event("Task C", "2023-09-09", "2023-09-18"));
-        testStorage.storeData(testList);
-
-        String currentTask = fileSc.nextLine();
-        assertEquals("T|false|Task A", currentTask);
-
-        currentTask = fileSc.nextLine();
-        assertEquals("D|false|Task B|2022-09-28", currentTask);
-
-        currentTask = fileSc.nextLine();
-        assertEquals("E|false|Task C|2023-09-09|2023-09-18", currentTask);
-    }*/
-
     @Test
     public void testLoadSuccessful() throws YippeeException, IOException {
         File file = new File("storageTest.txt");
