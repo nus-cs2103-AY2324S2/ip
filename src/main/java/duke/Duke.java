d7035d02a5d1262a460e65a46117b8ed4e3c5deb
julia-cp <>
2024-02-26 15:21:03 +0800

    remove unused import statements

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,17 +1,5 @@
 package duke;
 import duke.command.Command;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * The main class of the chatbot.
