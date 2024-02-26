e80de01344c7b9cbfb2fdef25a3d769ea3beabe3
tahsinhasem <tahsinhasem@gmail.com>
2024-02-27 00:31:30 +0800

    Added Javadoc to utils class

--- src/main/java/dude/utils/utils.java
+++ src/main/java/dude/utils/utils.java
@@ -1,9 +1,19 @@
 package dude.utils;
 
+/**
+ * The utils class contains utility methods that are used throughout the application.
+ */
 public class utils {
 
 
     // From chat GPT https://chat.openai.com/share/1848f5d2-1197-418e-86a9-bccdf69fc790
+
+    /**
+     * Discards the first word from the input string.
+     *
+     * @param input The input string.
+     * @return The input string with the first word discarded.
+     */
     public static String discardFirstWord(String input) {
         // Split the string by whitespace
         String[] words = input.split(" ", 2);
@@ -17,6 +27,13 @@ public class utils {
         }
     }
 
+    /**
+     * Counts the occurrences of a target string in an array of strings.
+     *
+     * @param array  The array of strings.
+     * @param target The target string.
+     * @return The number of occurrences of the target string in the array.
+     */
     public static int countOccurrences(String[] array, String target) {
         int count = 0;
         for (String str : array) {
@@ -27,6 +44,13 @@ public class utils {
         return count;
     }
 
+    /**
+     * Finds the index of the first occurrence of a target string in an array of strings.
+     *
+     * @param array  The array of strings.
+     * @param target The target string.
+     * @return The index of the first occurrence of the target string in the array.
+     */
     public static int findIndex(String[] array, String target) {
         for (int i = 0; i < array.length; i++) {
             if (array[i].equals(target)) {
