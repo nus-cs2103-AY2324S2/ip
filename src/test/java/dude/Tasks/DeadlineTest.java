a30505e46519192b85830f458641b9f66fc07abd
tahsinhasem <tahsinhasem@gmail.com>
2024-02-26 21:12:16 +0800

    changed method spelling mistake for deadline test

--- src/test/java/dude/Tasks/DeadlineTest.java
+++ src/test/java/dude/Tasks/DeadlineTest.java
@@ -16,7 +16,7 @@ public class DeadlineTest {
     }
 
     @Test
-    public void testEqualsCorretScenario() {
+    public void testEqualsCorrectScenario() {
         LocalDateTime dt = LocalDateTime.parse("2020-12-12T18:00");
         Deadline deadline = new Deadline("test", dt);
         Deadline deadline2 = new Deadline("test", dt);
