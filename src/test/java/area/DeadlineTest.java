950f864ef27aeecc074109a0d1ae53af3173c71d
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-02-26 10:23:50 +0800

    Update JUnit Tests
    Junit Tests did not include Priority
    
    Changed tests to include Priority attribute in Task creation
    
    Update output in assertEquals in DeadlineTest

--- src/test/java/area/DeadlineTest.java
+++ src/test/java/area/DeadlineTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
     @Test
     public void toString_returnDeadline_stringReturnedsuccess(){
-        Deadline  deadline = new Deadline("CS2103T" , "2021-12-31");
-        assertEquals("[D][ ] CS2103T (by: Dec 31 2021)", deadline.toString());
+        Deadline  deadline = new Deadline("CS2103T" , "2024-12-31");
+        assertEquals("[D][ ] CS2103T 0 (by: Dec 31 2024)", deadline.toString());
     }
 }
