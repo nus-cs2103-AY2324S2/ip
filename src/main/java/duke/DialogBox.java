bf6a44b3882e898ef0e4ab3c8a45f65c54ef5821
PyromancerBoom <priyanshmagic@gmail.com>
2024-02-26 20:34:40 +0800

    Add DialogBox javadocs

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -13,12 +13,10 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-//import javafx.scene.text.Text;
 
 /**
- * An example of a custom control using FXML.
- * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
- * containing text from the speaker.
+ * DialogBox is a custom HBox that contains a Label and an ImageView
+ * It is used to display a dialog in the GUI
  */
 public class DialogBox extends HBox {
     @FXML
@@ -26,6 +24,13 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    /**
+     * Private constructor for DialogBox
+     * It initializes the dialog box with the given text and image
+     * 
+     * @param text The text to be displayed in the dialog box
+     * @param img  The image to be displayed in the dialog box
+     */
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -39,20 +44,24 @@ public class DialogBox extends HBox {
         displayPicture.setImage(img);
     }
 
+    /**
+     * Public constructor for DialogBox
+     * It initializes the dialog box with the given Label and ImageView
+     * 
+     * @param l  The Label to be displayed in the dialog box
+     * @param iv The ImageView to be displayed in the dialog box
+     */
     public DialogBox(Label l, ImageView iv) {
         displayPicture = iv;
-
-//        l.setWrapText(true);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
-
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(l, displayPicture);
     }
 
-
     /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     * Flips the dialog box such that the ImageView is on the left and text on the
+     * right
      */
     private void flip() {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
@@ -61,10 +70,26 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+    /**
+     * Factory method to create a user dialog box.
+     * 
+     * @param text The text to be displayed in the dialog box.
+     * @param img  The image to be displayed in the dialog box.
+     * @return A new DialogBox with the given text and image.
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
+    /**
+     * Factory method to create a Duke dialog box.
+     * The Duke dialog box is flipped, with the ImageView on the left and text on
+     * the right.
+     * 
+     * @param text The text to be displayed in the dialog box.
+     * @param img  The image to be displayed in the dialog box.
+     * @return A new DialogBox with the given text and image, flipped.
+     */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
