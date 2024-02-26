14be28c1c4b6383776f88e9bc1c46bfa98d24dc7
huekoh <huexkoh@gmail.com>
2024-02-26 11:55:58 +0800

    Update Parser.java
    Parse parse method is too long and handles both input type checking and input format checking.
    
    Having smaller methods to handle the different types of possible inputs makes the code more readable.
    
    Let's update the parser method to handle only input type checking, and abstract out input format checking to appropriate methods.

--- src/main/java/bmo/util/Parser.java
+++ src/main/java/bmo/util/Parser.java
@@ -27,6 +27,210 @@ import java.util.regex.Pattern;
  */
 public class Parser {
 
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     * @throws IOException If there is an error reading the file.
+     */
+    public static Command parse(String input) throws IOException {
+        String[] inputArr = input.split(" ");
+        String keyword = inputArr[0];
+        Command output;
+        switch (keyword) {
+            case "hi":
+                return new GreetCommand();
+            case "bye":
+                return new ExitCommand();
+            case "log":
+                return new LogCommand();
+            case "done":
+                return parseDoneCommand(input);
+            case "redo":
+                return parseRedoCommand(input);
+            case "delete":
+                return parseDeleteCommand(input);
+            case "find":
+                return parseFindCommand(input);
+            case "todo":
+                return parseToDoCommand(input);
+            case "due":
+                return parseDueCommand(input);
+            case "event":
+                return parseEventCommand(input);
+            case "commands":
+                return new GuideCommand();
+            default:
+                return new DefaultCommand(0);
+        }
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseDoneCommand(String input) {
+        Command output;
+        String doneFormat = "^done\\s+(\\d+)$";
+        Pattern donePattern = Pattern.compile(doneFormat);
+        Matcher doneMatcher = donePattern.matcher(input);
+
+        if (doneMatcher.matches()) {
+            String index = doneMatcher.group(1);
+            output = new DoneCommand(index);
+        } else {
+            output = new DefaultCommand(0);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseRedoCommand(String input) {
+        Command output;
+        String redoFormat = "^redo\\s+(\\d+)$";
+        Pattern redoPattern = Pattern.compile(redoFormat);
+        Matcher redoMatcher = redoPattern.matcher(input);
+
+        if (redoMatcher.matches()) {
+            String index = redoMatcher.group(1);
+            output = new RedoCommand(index);
+        } else {
+            output = new DefaultCommand(0);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseDeleteCommand(String input) {
+        Command output;
+        String deleteFormat = "^delete\\s+(\\d+)$";
+        Pattern deletePattern = Pattern.compile(deleteFormat);
+        Matcher deleteMatcher = deletePattern.matcher(input);
+
+        if (deleteMatcher.matches()) {
+            String index = deleteMatcher.group(1);
+            output = new DeleteCommand(index);
+        } else {
+            output = new DefaultCommand(0);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseFindCommand(String input) {
+        Command output;
+        String findFormat = "^find\\s+(\\S+(\\s+\\w+)*)$";
+        Pattern findPattern = Pattern.compile(findFormat);
+        Matcher findMatcher = findPattern.matcher(input);
+
+        if (findMatcher.matches()) {
+            String key = findMatcher.group(1);
+            output = new FindCommand(key);
+
+        } else {
+            output = new DefaultCommand(1);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseToDoCommand(String input) {
+        Command output;
+        String toDoFormat = "^todo\\s+(\\S+(\\s+\\w+)*)$";
+        Pattern toDoPattern = Pattern.compile(toDoFormat);
+        Matcher toDoMatcher = toDoPattern.matcher(input);
+
+        if (toDoMatcher.matches()) {
+            String desc = toDoMatcher.group(1);
+            output = new ToDoCommand(desc);
+        } else {
+            output = new DefaultCommand(1);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseDueCommand(String input) {
+        Command output;
+        String dueFormat = "^due\\s+(\\w+(\\s+\\w+)*)\\s+/by\\s+(\\S+(\\s+\\w+|/)*)$";
+        Pattern duePattern = Pattern.compile(dueFormat);
+        Matcher dueMatcher = duePattern.matcher(input);
+
+        if (dueMatcher.matches()) {
+            String desc = dueMatcher.group(1);
+            String by = dueMatcher.group(3);
+
+            LocalDateTime byDateTime = formatDateTime(by);
+            if (byDateTime == null) {
+                output = new DefaultCommand(2);
+            } else {
+                output = new DueCommand(desc, byDateTime);
+            }
+        } else {
+            output = new DefaultCommand(1);
+        }
+        return output;
+    }
+
+    /**
+     * Parses the user input and returns the corresponding Command object.
+     *
+     * @param input String containing the user input.
+     * @return Command object corresponding to the user input.
+     */
+    private static Command parseEventCommand(String input) {
+        Command output;
+        String eventFormat = "^event\\s+(\\w+(\\s+\\w+)*)\\s+/from\\s+(\\S+(\\s+\\w+|/)*)\\s+/to\\s+(\\S+(\\s+\\w+|/)*)$";
+        Pattern eventPattern = Pattern.compile(eventFormat);
+        Matcher eventMatcher = eventPattern.matcher(input);
+
+        if (eventMatcher.matches()) {
+            String desc = eventMatcher.group(1);
+            String start = eventMatcher.group(3);
+            String end = eventMatcher.group(5);
+
+            LocalDateTime startDateTime = formatDateTime(start);
+            LocalDateTime endDateTime = formatDateTime(end);
+            if (startDateTime == null || endDateTime == null) {
+                output = new DefaultCommand(2);
+            } else {
+                output = new EventCommand(desc, startDateTime, endDateTime);
+            }
+        } else {
+            output = new DefaultCommand(1);
+        }
+        return output;
+    }
+
     /**
      * Formats the input string into a LocalDateTime object.
      *
@@ -41,144 +245,6 @@ public class Parser {
         } catch (Exception e) {
             return null;
         }
-    }
-
-    //to amend: break down into smaller methods
-
-    /**
-     * Parses the user input and returns the corresponding Command object.
-     *
-     * @param input String containing the user input.
-     * @return Command object corresponding to the user input.
-     * @throws IOException if unable to parse the input.
-     */
-    public static Command parse(String input) throws IOException {
-        String[] inputArr = input.split(" ");
-        String keyword = inputArr[0];
-        Command output;
-        switch (keyword) {
-            case "hi":
-                output = new GreetCommand();
-                break;
-            case "bye":
-                output = new ExitCommand();
-                break;
-            case "log":
-                output = new LogCommand();
-                break;
-            case "done":
-                String doneFormat = "^done\\s+(\\d+)$";
-                Pattern donePattern = Pattern.compile(doneFormat);
-                Matcher doneMatcher = donePattern.matcher(input);
-
-                if (doneMatcher.matches()) {
-                    String index = doneMatcher.group(1);
-                    output = new DoneCommand(index);
-                    break;
-                }
-                output = new DefaultCommand(0);
-                break;
-            case "redo":
-                String redoFormat = "^redo\\s+(\\d+)$";
-                Pattern redoPattern = Pattern.compile(redoFormat);
-                Matcher redoMatcher = redoPattern.matcher(input);
-
-                if (redoMatcher.matches()) {
-                    String index = redoMatcher.group(1);
-                    output = new RedoCommand(index);
-                    break;
-                }
-                output = new DefaultCommand(0);
-                break;
-            case "delete":
-                String deleteFormat = "^delete\\s+(\\d+)$";
-                Pattern deletePattern = Pattern.compile(deleteFormat);
-                Matcher deleteMatcher = deletePattern.matcher(input);
-
-                if (deleteMatcher.matches()) {
-                    String index = deleteMatcher.group(1);
-                    output = new DeleteCommand(index);
-                    break;
-                }
-
-                output = new DefaultCommand(0);
-                break;
-            case "find":
-                String findFormat = "^find\\s+(\\S+(\\s+\\w+)*)$";
-                Pattern findPattern = Pattern.compile(findFormat);
-                Matcher findMatcher = findPattern.matcher(input);
-
-                if (findMatcher.matches()) {
-                    String key = findMatcher.group(1);
-                    output = new FindCommand(key);
-                    break;
-                }
-                output = new DefaultCommand(1);
-                break;
-            case "todo":
-                String toDoFormat = "^todo\\s+(\\S+(\\s+\\w+)*)$";
-                Pattern toDoPattern = Pattern.compile(toDoFormat);
-                Matcher toDoMatcher = toDoPattern.matcher(input);
-
-                if (toDoMatcher.matches()) {
-                    String desc = toDoMatcher.group(1);
-                    output = new ToDoCommand(desc);
-                    break;
-                }
-                output = new DefaultCommand(1);
-                break;
-
-            case "due":
-                String dueFormat = "^due\\s+(\\w+(\\s+\\w+)*)\\s+/by\\s+(\\S+(\\s+\\w+|/)*)$";
-                Pattern duePattern = Pattern.compile(dueFormat);
-                Matcher dueMatcher = duePattern.matcher(input);
-
-                if (dueMatcher.matches()) {
-                    String desc = dueMatcher.group(1);
-                    String by = dueMatcher.group(3);
-
-                    LocalDateTime byDateTime = formatDateTime(by);
-                    if (byDateTime == null) {
-                        output = new DefaultCommand(2);
-                        break;
-                    } else {
-                        output = new DueCommand(desc, byDateTime);
-                        break;
-                    }
-                }
-                output = new DefaultCommand(1);
-                break;
-
-            case "event":
-                String eventFormat = "^event\\s+(\\w+(\\s+\\w+)*)\\s+/from\\s+(\\S+(\\s+\\w+|/)*)\\s+/to\\s+(\\S+(\\s+\\w+|/)*)$";
-                Pattern eventPattern = Pattern.compile(eventFormat);
-                Matcher eventMatcher = eventPattern.matcher(input);
-
-                if (eventMatcher.matches()) {
-                    String desc = eventMatcher.group(1);
-                    String start = eventMatcher.group(3);
-                    String end = eventMatcher.group(5);
-
-                    LocalDateTime startDateTime = formatDateTime(start);
-                    LocalDateTime endDateTime = formatDateTime(end);
-                    if (startDateTime == null || endDateTime == null) {
-                        output = new DefaultCommand(2);
-                        break;
-                    } else {
-                        output = new EventCommand(desc, startDateTime, endDateTime);
-                        break;
-                    }
-                }
-                output = new DefaultCommand(1);
-                break;
-            case "commands":
-                output = new GuideCommand();
-                break;
-            default:
-                output = new DefaultCommand(0);
-                break;
-        }
-        return output;
     }
 
 }
