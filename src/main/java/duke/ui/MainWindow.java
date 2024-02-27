a228622e31fc99a4eec2146260bcd48e5e44d89d
aman zainal <aman@u.nus.edu>
2024-02-27 15:14:24 +0800

    Add new user / bot images

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -25,8 +25,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Coat.png"));
 
     /**
      * Initializes the DialogBox and display the welcome message.
