5b4cc781f7064354e7d3ff9fc849a30124daad74
aman zainal <aman@u.nus.edu>
2024-02-28 17:59:03 +0800

    Add exit command to close application

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,10 +3,12 @@ package duke;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import duke.command.ByeCommand;
 import duke.command.Command;
 import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.task.TaskList;
+import duke.ui.Main;
 import duke.ui.Ui;
 
 /**
@@ -18,6 +20,7 @@ import duke.ui.Ui;
 public class Duke {
     private final Storage storage;
     private TaskList tasks;
+    private Parser parser;
     private Ui ui;
 
     /**
@@ -27,6 +30,7 @@ public class Duke {
         this.tasks = new TaskList();
         this.storage = new Storage();
         this.ui = new Ui();
+        this.parser = new Parser(ui);
 
         try {
             this.tasks = this.storage.load();
@@ -51,6 +55,10 @@ public class Duke {
             // Execute the command
             command.execute(this.tasks, this.ui);
             storage.save(tasks);
+
+            if (command instanceof ByeCommand) {
+                Main.getStage().close();
+            }
 
             // For debugging purposes.
             System.out.printf("\n------------\nInput: %s\nOutput: %s\n-----------", input, this.ui.getResponse());
