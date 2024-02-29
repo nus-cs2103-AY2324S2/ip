34b8e31c3a9a1675c5a81103cc4f0827cb6a1421
Chong Chin Herng <chongahzong@gmail.com>
2024-03-01 00:11:56 +0800

    DeadlineTest.java: remove unnecessary imports

--- src/test/java/bob/task/DeadlineTest.java
+++ src/test/java/bob/task/DeadlineTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
