a7ef7bee516725aea05f0abea43af7873d9c0799
tahsinhasem <tahsinhasem@gmail.com>
2024-03-03 22:31:56 +0800

    Added javadoc to Launcher.java

--- src/main/java/dude/Launcher.java
+++ src/main/java/dude/Launcher.java
@@ -2,8 +2,16 @@ package dude;
 
 import javafx.application.Application;
 
+/**
+ * A Launcher class that acts as the entry point for the application.
+ */
 public class Launcher {
 
+    /**
+     * Launches the application.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
