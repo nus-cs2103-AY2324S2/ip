6de2f05d83b4e258367c90b198b83c40fb1272da
tahsinhasem <tahsinhasem@gmail.com>
2024-03-03 21:49:33 +0800

    Extracted loadMainScene method out.

--- src/main/java/dude/Main.java
+++ src/main/java/dude/Main.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The Main class is used to boot up the GUI for the application.
+ */
 public class Main extends Application {
 
     @Override
@@ -15,6 +18,18 @@ public class Main extends Application {
         stage.setTitle("Dude");
         stage.setResizable(false);
 
+        Scene scene = loadMainScene();
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    /**
+     * Loads the main scene of the application from the FXML file.
+     *
+     * @return The scene that is loaded.
+     */
+    private Scene loadMainScene() {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainView.fxml"));
         AnchorPane anchorPane = null;
         try {
@@ -22,9 +37,8 @@ public class Main extends Application {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         Scene scene = new Scene(anchorPane);
-        stage.setScene(scene);
-        stage.show();
+        return scene;
     }
 }
+
