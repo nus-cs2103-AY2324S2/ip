a4ce754a41ea6f1b90dcf4b34f01fa7dcb418c9a
tahsinhasem <tahsinhasem@gmail.com>
2024-03-03 21:24:05 +0800

    added assertions to constructors of tasks packages

--- src/main/java/dude/tasks/Task.java
+++ src/main/java/dude/tasks/Task.java
@@ -19,6 +19,7 @@ public class Task implements Serializable {
      * @param description The description of the task.
      */
     public Task(String description) {
+        assert (description != null);
         this.description = description;
         this.isDone = false;
     }
