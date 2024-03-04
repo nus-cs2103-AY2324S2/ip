1572658a97749972ad2d25c9f65f5be03318109f
Irwyn <irwynliong01@gmail.com>
2024-03-04 15:59:23 +0800

    Fixing git standard 4

--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -16,7 +16,7 @@ public abstract class Command {
     private final boolean isExit;
 
     /**
-     * Constructor for a Command. git standard 3
+     * Constructor for a Command. git standard 4
      * @param isExit Exit status.
      */
     Command(boolean isExit) {
