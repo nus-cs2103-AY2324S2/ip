59064ef4564039cdeaab547ccca836d139f3b20a
zoebelle-pang <zoebellepang11@gmail.com>
2024-03-14 21:55:26 +0800

    Fix Jar file

--- src/main/java/Bentley/Main.java
+++ src/main/java/Bentley/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 
-    private Duke duke = new Duke("src/main/java/data/duke.txt");
+    private Duke duke = new Duke("./data/duke.txt");
     @Override
     public void start(Stage stage) {
         try {
