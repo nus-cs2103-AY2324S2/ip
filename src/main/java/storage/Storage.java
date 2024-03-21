674107ffd5759683cb3d67c524f602f953718efc
chuahjiajie <jiajiechuah4@gmail.com>
2024-03-22 02:21:46 +0800

    Fix Storage
    Storage file had methods that were
    too long.

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -82,130 +82,159 @@ public class Storage {
             throw new BalkanBotException(e.toString());
         }
 
-        int counter = 1;
         int current = 0;
         for (String task : textInput) {
             if (task == null) {
                 break;
             } else {
-                TaskType taskType = null;
-                boolean completed = false;
-                boolean proceed = true;
-
-                String taskNature = task.substring(0, 6);
-                String[] details = task.substring(7).split("\\s+");
-                String type = taskNature.substring(1, 2);
-                String complete = taskNature.substring(4, 5);
-                switch (type) {
-                    case "T": {
-                        taskType = TaskType.ToDo;
-                        break;
-                    }
-                    case "D": {
-                        taskType = TaskType.Deadline;
-                        break;
-                    }
-                    case "E": {
-                        taskType = TaskType.Event;
-                        break;
-                    }
-                    default: {
-                        System.out.println("Error with Task.Task: " + counter + "Type Unidentified Task.Task Found Save File. " +
-                                "Task.Task will be skipped");
-                        proceed = false;
-                    }
-                }
-
-                if (complete.equals("X")) {
-                    completed = true;
-                } else if (!complete.equals(" ")) {
-                    System.out.println("Completion status of task [" + counter + "] is invalid. Task.Task will be skipped");
-                    proceed = false;
-                }
-
-                counter++;
-
-                if (!proceed) {
-                    continue;
-                }
-
-                switch (taskType) {
-                    case ToDo: {
-                        String taskDescription = String.join(" ", details);
-                        try {
-                            tasks[current] = new ToDo(taskDescription);
-                            markCheck(tasks[current], completed);
-                            current++;
-                            break;
-                        } catch (InvalidInputException e) {
-                            System.out.println(e);
-                            break;
-                        }
-                    }
-                    case Deadline: {
-                        StringBuilder taskDescription = new StringBuilder();
-                        StringBuilder deadline = new StringBuilder();
-                        boolean foundDeadline = false;
-                        for (String currentString : details) {
-                            if (foundDeadline) {
-                                deadline.append(currentString).append(" ");
-                            } else if (currentString.equals("(by:")) {
-                                foundDeadline = true;
-                            } else {
-                                taskDescription.append(currentString).append(" ");
-                            }
-                        }
-
-                        String fixedDeadline = deadline.substring(0, deadline.toString().length() - 2);
-
-                        try {
-                            tasks[current] = new Deadline(taskDescription.toString(), fixedDeadline);
-                            markCheck(tasks[current], completed);
-                            current++;
-                            break;
-                        } catch (InvalidInputException | InvalidDateException e) {
-                            System.out.println(e);
-                            break;
-                        }
-                    }
-                    case Event: {
-                        StringBuilder taskDescription = new StringBuilder();
-                        StringBuilder from = new StringBuilder();
-                        StringBuilder to = new StringBuilder();
-                        boolean foundFrom = false;
-                        boolean foundTo = false;
-                        for (String currentString : details) {
-                            if (foundTo) {
-                                to.append(currentString).append(" ");
-                            } else if (foundFrom) {
-                                if (currentString.equals("to:")) {
-                                    foundTo = true;
-                                } else {
-                                    from.append(currentString).append(" ");
-                                }
-                            } else if (currentString.equals("(from:")) {
-                                foundFrom = true;
-                            } else {
-                                taskDescription.append(currentString).append(" ");
-                            }
-                        }
-
-                        String fixedTo = to.substring(0, to.toString().length() - 2);
-                        try {
-                            tasks[current] = new Event(taskDescription.toString(), from.toString(),
-                                    fixedTo);
-                            markCheck(tasks[current], completed);
-                            current++;
-                            break;
-                        } catch (InvalidInputException | InvalidDateException e) {
-                            System.out.println(e);
-                            break;
-                        }
-                    }
-                }
+                tasks = addTask(tasks, task, current);
+                current++;
             }
         }
         Parser.updateCurrent(current);
+        return tasks;
+    }
+
+    private Task[] addTask(Task[] tasks, String task, int current) {
+        boolean proceed = true;
+
+        String taskNature = task.substring(0, 6);
+        String[] details = task.substring(7).split("\\s+");
+        String type = taskNature.substring(1, 2);
+        String complete = taskNature.substring(4, 5);
+
+        TaskType taskType = getTaskType(type);
+
+        boolean[] checkCompleteResult = checkComplete(complete, proceed);
+        boolean completed = checkCompleteResult[0];
+
+        proceed = checkCompleteResult[1];
+        if (taskType == null) {
+            proceed = false;
+        }
+
+        if (proceed) {
+            switch (taskType) {
+                case ToDo: {
+                    tasks = addToDoTask(tasks, current, details, completed);
+                    break;
+                }
+                case Deadline: {
+                    tasks = addDeadlineTask(tasks, current, details, completed);
+                    break;
+                }
+                case Event: {
+                    tasks = addEventTask(tasks, current, details, completed);
+                    break;
+                }
+            }
+        }
+
+        return tasks;
+    }
+
+    private TaskType getTaskType(String type) {
+        TaskType taskType = null;
+        switch (type) {
+            case "T": {
+                taskType = TaskType.ToDo;
+                break;
+            }
+            case "D": {
+                taskType = TaskType.Deadline;
+                break;
+            }
+            case "E": {
+                taskType = TaskType.Event;
+                break;
+            }
+        }
+
+        return taskType;
+    }
+
+    private boolean[] checkComplete(String complete, boolean proceed) {
+        boolean[] result = new boolean[2];
+
+        if (complete.equals("X")) {
+            result[0] = true;
+            result[1] = proceed;
+        } else if (complete.equals(" ")) {
+            result[0] = false;
+            result[1] = proceed;
+        } else {
+            result[1] = false;
+        }
+
+        return result;
+    }
+
+    private Task[] addToDoTask(Task[] tasks, int current, String[] details, boolean completed) {
+        String taskDescription = String.join(" ", details);
+        try {
+            tasks[current] = new ToDo(taskDescription);
+            markCheck(tasks[current], completed);
+        } catch (InvalidInputException e) {
+            System.out.println(e);
+        }
+        return tasks;
+    }
+
+    private Task[] addDeadlineTask(Task[] tasks, int current, String[] details, boolean completed) {
+        StringBuilder taskDescription = new StringBuilder();
+        StringBuilder deadline = new StringBuilder();
+        boolean foundDeadline = false;
+        for (String currentString : details) {
+            if (foundDeadline) {
+                deadline.append(currentString).append(" ");
+            } else if (currentString.equals("(by:")) {
+                foundDeadline = true;
+            } else {
+                taskDescription.append(currentString).append(" ");
+            }
+        }
+
+        String fixedDeadline = deadline.substring(0, deadline.toString().length() - 2);
+
+        try {
+            tasks[current] = new Deadline(taskDescription.toString(), fixedDeadline);
+            markCheck(tasks[current], completed);
+        } catch (InvalidInputException | InvalidDateException e) {
+            System.out.println(e);
+        }
+        return tasks;
+    }
+
+    private Task[] addEventTask(Task[] tasks, int current, String[] details, boolean completed) {
+        StringBuilder taskDescription = new StringBuilder();
+        StringBuilder from = new StringBuilder();
+        StringBuilder to = new StringBuilder();
+        boolean foundFrom = false;
+        boolean foundTo = false;
+        for (String currentString : details) {
+            if (foundTo) {
+                to.append(currentString).append(" ");
+            } else if (foundFrom) {
+                if (currentString.equals("to:")) {
+                    foundTo = true;
+                } else {
+                    from.append(currentString).append(" ");
+                }
+            } else if (currentString.equals("(from:")) {
+                foundFrom = true;
+            } else {
+                taskDescription.append(currentString).append(" ");
+            }
+        }
+
+        String fixedTo = to.substring(0, to.toString().length() - 2);
+        try {
+            tasks[current] = new Event(taskDescription.toString(), from.toString(),
+                    fixedTo);
+            markCheck(tasks[current], completed);
+        } catch (InvalidInputException | InvalidDateException e) {
+            System.out.println(e);
+        }
         return tasks;
     }
 }
