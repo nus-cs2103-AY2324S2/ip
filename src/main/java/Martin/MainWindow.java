66624e8f102631803a4b20e8a16265249571cef7
yunchao <tanyunchao@gmail.com>
2024-03-22 10:29:48 +0800

    Handle exception in MainWindow.java

--- src/main/java/Martin/MainWindow.java
+++ src/main/java/Martin/MainWindow.java
@@ -45,7 +45,13 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = martin.getResponse(input);
+        String response;
+        try {
+            response = martin.getResponse(input);
+        } catch (Exception e) {
+            response = e.getMessage();
+        }
+        
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage));
